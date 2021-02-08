f93f2acad8ed901a443ef16cb549486ab8482cc9
krabaton <krabatua@gmail.com>
2021-02-08 02:57:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const indexRouter = require('./routes/index')
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'combined'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/contacts', indexRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
+
+module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.patch('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+module.exports = router
