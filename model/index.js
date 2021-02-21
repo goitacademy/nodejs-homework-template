23d543ff8b5cc6998fc0127bf3997d58b1ed7d01
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-22 01:22:50 +0200

    add code for each function

--- model/index.js
+++ model/index.js
@@ -1,15 +1,54 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const { v4: uuidv4 } = require("uuid");
+const db = require("./db");
 
-const listContacts = async () => {}
+// === GET all contacts ===
+const listContacts = async () => {
+  return db.get("contacts").value();
+};
 
-const getContactById = async (contactId) => {}
+// === GET contact by ID ===
+const getContactById = async (contactId) => {
+  return db
+    .get("contacts")
+    .find(({ id }) => String(id) === String(contactId))
+    .value();
+};
 
-const removeContact = async (contactId) => {}
+// === REMOVE contact by ID ===
+const removeContact = async (contactId) => {
+  const [contact] = db
+    .get("contacts")
+    .remove(({ id }) => String(id) === String(contactId))
+    .write();
 
-const addContact = async (body) => {}
+  return contact;
+};
 
-const updateContact = async (contactId, body) => {}
+// === ADD new contact ===
+const addContact = async (body) => {
+  const id = uuidv4();
+  const contact = {
+    id,
+    ...body,
+    ...(body.coordinates
+      ? {}
+      : { coordinates: "-13.162409912958838, -72.54494144232928" }),
+  };
+
+  db.get("contacts").push(contact).write();
+  return contact;
+};
+
+// === UPDATE contact ===
+const updateContact = async (contactId, body) => {
+  const contact = db
+    .get("contacts")
+    .find(({ id }) => String(id) === String(contactId))
+    .assign(body)
+    .value();
+  db.write();
+  return contact.id ? contact : null;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +56,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
