4ad209d5a97ae73864b733240b2202c7afc4cf5e
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-22 22:26:56 +0200

    code refactor, prettier formating

--- model/index.js
+++ model/index.js
@@ -1,54 +1,52 @@
-const { v4: uuidv4 } = require("uuid");
-const db = require("./db");
+const { v4: uuidv4 } = require('uuid')
+const db = require('./db')
 
 // === GET all contacts ===
 const listContacts = async () => {
-  return db.get("contacts").value();
-};
+  return db.get('contacts').value()
+}
 
 // === GET contact by ID ===
-const getContactById = async (contactId) => {
+const getContactById = async contactId => {
   return db
-    .get("contacts")
+    .get('contacts')
     .find(({ id }) => String(id) === String(contactId))
-    .value();
-};
+    .value()
+}
 
 // === REMOVE contact by ID ===
-const removeContact = async (contactId) => {
+const removeContact = async contactId => {
   const [contact] = db
-    .get("contacts")
+    .get('contacts')
     .remove(({ id }) => String(id) === String(contactId))
-    .write();
+    .write()
 
-  return contact;
-};
+  return contact
+}
 
 // === ADD new contact ===
-const addContact = async (body) => {
-  const id = uuidv4();
+const addContact = async body => {
+  const id = uuidv4()
   const contact = {
     id,
     ...body,
-    ...(body.coordinates
-      ? {}
-      : { coordinates: "-13.162409912958838, -72.54494144232928" }),
-  };
+    ...(body.email ? {} : { email: 'fill in!' }),
+  }
 
-  db.get("contacts").push(contact).write();
-  return contact;
-};
+  db.get('contacts').push(contact).write()
+  return contact
+}
 
 // === UPDATE contact ===
 const updateContact = async (contactId, body) => {
   const contact = db
-    .get("contacts")
+    .get('contacts')
     .find(({ id }) => String(id) === String(contactId))
     .assign(body)
-    .value();
-  db.write();
-  return contact.id ? contact : null;
-};
+    .value()
+  db.write()
+  return contact.id ? contact : null
+}
 
 module.exports = {
   listContacts,
@@ -56,4 +54,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-};
+}
