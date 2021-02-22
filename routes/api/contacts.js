36c036f80a585ff14e845fe96c614551639aa40c
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2021-02-22 12:19:38 +0200

    add MongoDB database

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const Contacts = require('../../model/contacts');
+// const Contacts = require('../../model/contacts');
+const { MongoClient, ObjectID } = require('mongodb');
+// const db = require('../../model/db');
+require('dotenv').config();
+const uriDb = process.env.DB_HOST;
 
 router.get('/', async (_req, res, next) => {
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
+
   try {
-    const contacts = await Contacts.listContacts();
+    const contacts = await client.db().collection('contacts').find().toArray();
     console.log('contacts', contacts);
     return res.json({
       status: 'success',
@@ -15,12 +23,24 @@ router.get('/', async (_req, res, next) => {
     });
   } catch (e) {
     next(e);
+  } finally {
+    await client.close();
   }
 });
 
 router.get('/:contactId', async (req, res, next) => {
+  const objectId = new ObjectID(req.params.contactId);
+
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
+
   try {
-    const contact = await Contacts.getContactById(req.params.contactId);
+    const contact = await client
+      .db()
+      .collection('contacts')
+      .find({ _id: objectId })
+      .toArray();
     if (contact) {
       return res.json({
         status: 'success',
@@ -38,12 +58,19 @@ router.get('/:contactId', async (req, res, next) => {
     }
   } catch (e) {
     next(e);
+  } finally {
+    await client.close();
   }
 });
 
 router.post('/', async (req, res, next) => {
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
   try {
-    const contact = await Contacts.addContact(req.body);
+    const {
+      ops: [contact],
+    } = await client.db().collection('contacts').insertOne(req.body);
     return res.status(201).json({
       status: 'success',
       code: 201,
@@ -53,12 +80,22 @@ router.post('/', async (req, res, next) => {
     });
   } catch (e) {
     next(e);
+  } finally {
+    await client.close();
   }
 });
 
 router.delete('/:contactId', async (req, res, next) => {
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
   try {
-    const contact = await Contacts.removeContact(req.params.contactId);
+    // const contact = await Contacts.removeContact(req.params.contactId);
+
+    const { value: contact } = await client
+      .db()
+      .collection('contacts')
+      .findOneAndDelete({ _id: req.params.contactId });
     if (contact) {
       return res.json({
         status: 'success',
@@ -76,15 +113,29 @@ router.delete('/:contactId', async (req, res, next) => {
     }
   } catch (e) {
     next(e);
+  } finally {
+    await client.close();
   }
 });
 
 router.patch('/:contactId', async (req, res, next) => {
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
   try {
-    const contact = await Contacts.updateContact(
-      req.params.contactId,
-      req.body
-    );
+    // const contact = await Contacts.updateContact(
+    //   req.params.contactId,
+    //   req.body
+    // );
+
+    const { value: contact } = await client
+      .db()
+      .collection('contacts')
+      .findOneAndUpdate(
+        { _id: req.params.contactId },
+        { $set: req.body },
+        { returnOriginal: false }
+      );
     if (contact) {
       return res.json({
         status: 'success',
@@ -102,6 +153,8 @@ router.patch('/:contactId', async (req, res, next) => {
     }
   } catch (e) {
     next(e);
+  } finally {
+    await client.close();
   }
 });
 
