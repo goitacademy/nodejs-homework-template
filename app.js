d30d5a89b0d8027c4e3abdcaaea33766bf0b4a22
Artem Zymovets <artwayprojects@gmail.com>
2021-02-22 23:52:07 +0200

    fixed eslint bags

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const contactsRouter = require('./routes/api/contacts')
-// const blogRouter = require('./routes/api/blog')
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -12,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res) => {
   res.status(500).json({ message: err.message })
 })
 
--- model/validation.js
+++ model/validation.js
@@ -6,7 +6,7 @@ const schema = Joi.object({
     .required(),
 
   password: Joi.string()
-    .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    .pattern(/'^[a-zA-Z0-9]{3,30}$'/)
     .required(),
 
   email: Joi.string()
