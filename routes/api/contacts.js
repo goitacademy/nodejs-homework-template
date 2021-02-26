74327c558dfa2115bd65e8856a939a5634c3d969
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-02-26 20:43:35 +0200

    1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template M" });
+  res.json({ message: "template message" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
