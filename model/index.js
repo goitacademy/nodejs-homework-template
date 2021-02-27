dfa50c3f3f768079b2d5fec57df37c1a01e8c3f5
Artem Zymovets <artwayprojects@gmail.com>
2021-02-27 22:44:26 +0200

    cleanup code

--- model/index.js
+++ model/index.js
@@ -1,22 +1,15 @@
-
 const Contact = require('./schemas/contact.js')
-
-const getCollection = async (db, name) => {
-  const contact = new CreateContact({name: name})
-  return contact.save
-}
-
 
 const listContacts = async () => {
   return await Contact.find({})
 }
 
 const getContactById = async (contactId) => {
-  return await Contact.findOne({_id: contactId})
+  return await Contact.findOne({ _id: contactId })
 }
 
 const removeContact = async (contactId) => {
-  return await Contact.findOneAndRemove({_id: contactId})
+  return await Contact.findOneAndRemove({ _id: contactId })
 }
 
 const addContact = async (body) => {
@@ -25,13 +18,13 @@ const addContact = async (body) => {
 }
 
 const updateContact = async (contactId, body) => {
-    const result = await Contact
+  const result = await Contact
     .findOneAndUpdate(
       contactId,
-      {...body},
-      {new: true}
+      { ...body },
+      { new: true }
     )
-    return result
+  return result
 }
 
 module.exports = {
