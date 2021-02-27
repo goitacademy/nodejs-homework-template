f5fadcc526ce84be41dbd2738001e9e271a28615
Artem Zymovets <artwayprojects@gmail.com>
2021-02-27 14:21:47 +0200

    changes

--- model/helpers.js
+++ model/helpers.js
@@ -1,7 +1,8 @@
 const generateId = (contacts) => {
-    return contacts.length > 0 ? (contacts[contacts.length - 1].id + 1) : 1
+  return contacts.length > 0 ? (contacts[contacts.length - 1].id + 1) : 1
 }
 
 module.exports = {
-    generateId
+  generateId
+  
 }
