8abff48852b670f58b0b8abfc171b77590ca8ccc
Ivan <vitanlhouse@gmail.com>
2021-02-28 18:48:42 +0200

    Додає виконане ДЗ-3

--- model/contacts.js
+++ model/contacts.js
@@ -1,39 +1,38 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
-
-const db = require('./db');
-const { v4: uuid } = require('uuid');
+const Contact = require('./schemas/contact');
 
 const listContacts = async () => {
-  return db.get('contacts').value();
+  const results = await Contact.find({});
+  return results;
 };
 
 const getContactById = async id => {
-  return db.get('contacts').find({ id }).value();
-};
-
-const removeContact = async id => {
-  const [record] = db.get('contacts').remove({ id }).write();
-  return record;
+  const result = await Contact.findOne({ _id: id });
+  console.log(result.id);
+  console.log(result._id);
+  return result;
 };
 
 const addContact = async body => {
-  const id = uuid();
-
-  const record = {
-    id,
-    ...body,
-    // ...(body.isVaccinated ? {} : { isVaccinated: false }), // властивість по замовчуванню
-  };
-
-  db.get('contacts').push(record).write();
-  return record;
+  const result = await Contact.create(body);
+  return result;
 };
 
 const updateContact = async (id, body) => {
-  const record = db.get('contacts').find({ id }).assign(body).value();
-  db.write();
-  return record.id ? record : null;
+  const result = await Contact.findByIdAndUpdate(
+    { _id: id },
+    { ...body },
+    { new: true },
+  );
+
+  return result;
+};
+
+const removeContact = async id => {
+  const result = await Contact.findByIdAndRemove({
+    _id: id,
+  });
+
+  return result;
 };
 
 module.exports = {
