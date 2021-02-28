b0172a5dd71f751d4f3e40084a8a8a832492e601
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-01 01:19:36 +0200

    додає код перевірки аварійної зупинки ДБ

--- model/db.js
+++ model/db.js
@@ -1,9 +1,17 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+const uriDb = process.env.URI_DB
 
-const adapter = new FileSync("./model/contacts.json");
-const db = low(adapter);
+const db = MongoClient.connect(uriDb, {
+  useUnifiedTopology: true,
+  poolSize: 5,
+})
 
-db.defaults({ contacts: [] }).write();
+process.on('SIGINT', async () => {
+  const client = await db
+  client.close()
+  console.log('button "C" was pressed and connection are terminated')
+  process.exit(1)
+})
 
-module.exports = db;
+module.exports = db
