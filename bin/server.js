7e19d0cc715a964715246a6ce117ce66c1bb4eca
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-01 01:16:45 +0200

    додає перевірку перед стартом сервера

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,13 @@
 const app = require('../app')
+const db = require('../model/db')
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Database connection successful`)
+  })
+}).catch(() => {
+  console.log('connection error')
+  process.exit(1)
 })
