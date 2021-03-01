f2603872416d2c0eb38d751298c07087f20b8d55
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2021-03-01 22:27:46 +0200

    add try catch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,19 +6,35 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    res.json({ message: 'template message' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    res.json({ message: 'template message' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    res.json({ message: 'template message' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    res.json({ message: 'template message' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
