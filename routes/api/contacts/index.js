6e3fd6a666d1b4826dbbbdcd213d2e95739a8fba
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-03 19:00:20 +0200

    renamed and relocated doc

---
+++ routes/api/contacts/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const contactsController = require('../../../controllers/contacts') // create, getAll, getById, update, remove
+const { addContact, updateContact } = require('../contacts/validation')
+
+// === router CREATE ===
+router.post('/', addContact, contactsController.create)
+
+// === router GET ===
+router.get('/', contactsController.getAll)
+
+// === router GET 'Id' ===
+router.get('/:contactId', contactsController.getById)
+
+// === router PATCH ===
+router.patch('/:contactId', updateContact, contactsController.update)
+
+// === router DELETE ===
+router.delete('/:contactId', contactsController.remove)
+
+module.exports = router
