3d5c3ec1fa833fe8022ab162902f1a00671d0feb
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-03 19:03:41 +0200

    inserted userRouter

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
+
 const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -11,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter)
 
 app.use((_req, res) => {
   res.status(404).json({ message: 'Not found' })
