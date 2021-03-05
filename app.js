db94365a130a5e69606f0d88595e98aa1e3a4f50
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-03-05 10:05:44 +0200

    hw-2

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const app = express()
 
 const contactsRouter = require('./routes/api/contacts')
-
-const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -19,7 +18,7 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = app
