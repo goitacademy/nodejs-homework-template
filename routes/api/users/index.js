b840f9221df26980dbefca96a040745d3fc4ac9c
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-07 00:58:37 +0200

    add guard

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../../controllers/users') // registration, logIn, logOut
 const { createUser } = require('../users/validation')
+const guard = require('../../../helpers/guard')
 
 // === router REGISTRATION ===
 router.post('/auth/register', createUser, userController.reg)
@@ -10,9 +11,9 @@ router.post('/auth/register', createUser, userController.reg)
 router.post('/auth/login', userController.logIn)
 
 // === router LOGOUT ===
-router.post('/auth/logout', userController.logOut)
+router.post('/auth/logout', guard, userController.logOut)
 
 // === router GET ===
-router.get('/current', userController.getUser)
+router.get('/current', guard, userController.getUser)
 
 module.exports = router
