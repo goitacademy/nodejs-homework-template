4271daafe59541cd52b91d698187521cbdd1802d
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-06 16:19:45 +0200

    Create users.js

---
+++ model/users.js
@@ -0,0 +1,30 @@
+const User = require('./schemas/user')
+
+// === FIND user "email" ===
+const findByEmail = async email => {
+  return await User.findOne({ email })
+}
+
+// === FIND user "id" ===
+const findById = async id => {
+  return await User.findOne({ _id: id })
+}
+
+// === CREATE user ===
+const create = async ({ email, password, subscription }) => {
+  const user = new User({ email, password, subscription })
+
+  return await user.save()
+}
+
+// === UPDATE user  ===
+const updateToken = async (id, token) => {
+  return await User.updateOne({ _id: id }, { token })
+}
+
+module.exports = {
+  findByEmail,
+  findById,
+  create,
+  updateToken,
+}
