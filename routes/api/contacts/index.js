4321f9073fe0a8fe20d15f028762fbc2fa09550c
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-06 23:45:06 +0200

    add guard

--- routes/api/contacts/index.js
+++ routes/api/contacts/index.js
@@ -2,20 +2,21 @@ const express = require('express')
 const router = express.Router()
 const contactsController = require('../../../controllers/contacts') // create, getAll, getById, update, remove
 const { addContact, updateContact } = require('../contacts/validation')
+const guard = require('../../../helpers/guard')
 
 // === router CREATE ===
-router.post('/', addContact, contactsController.create)
+router.post('/', guard, addContact, contactsController.create)
 
 // === router GET ===
-router.get('/', contactsController.getAll)
+router.get('/', guard, contactsController.getAll)
 
 // === router GET 'Id' ===
-router.get('/:contactId', contactsController.getById)
+router.get('/:contactId', guard, contactsController.getById)
 
 // === router PATCH ===
-router.patch('/:contactId', updateContact, contactsController.update)
+router.patch('/:contactId', guard, updateContact, contactsController.update)
 
 // === router DELETE ===
-router.delete('/:contactId', contactsController.remove)
+router.delete('/:contactId', guard, contactsController.remove)
 
 module.exports = router
