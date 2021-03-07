88058fa0fd24edaa597bfa06cf081c5c9cb86420
Yurii Zubar <barsuk0831@gmail.com>
2021-03-07 14:36:54 +0200

    changed forbidden on not authirized

--- helpers/guard.js
+++ helpers/guard.js
@@ -6,11 +6,11 @@ const guard = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user) => {
     const [, token] = req.get("Authorization")?.split(" ");
     if (!user || err || token !== user.token) {
-      return res.status(HttpCode.FORBIDDEN).json({
+      return res.status(HttpCode.UNAUTHORIZED).json({
         status: "error",
-        code: HttpCode.FORBIDDEN,
-        data: "Forbidden",
-        message: "Access is denied",
+        code: HttpCode.UNAUTHORIZED,
+        data: "Unauthorized",
+        message: "Not authorized",
       });
     }
     req.user = user;
