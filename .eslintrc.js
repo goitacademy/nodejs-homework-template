2658ed328a827ec59f3ab24ec7e2a6e985c75a51
AndrewGumenchuk <64306832+AndrewGumenchuk@users.noreply.github.com>
2021-03-10 13:48:25 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', "plugin:json/recommended", "prettier"],
+  extends: ['standard', "prettier"],
   parserOptions: {
     ecmaVersion: 12,
   },
