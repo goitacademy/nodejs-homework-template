183cae2f3b2444a0668b0972b5bd53511eda77cd
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-03-10 13:01:24 +0200

    mongoose install

--- model/db.js
+++ model/db.js
@@ -1,19 +1,30 @@
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const uriDb = process.env.URI_DB
 
-const db = MongoClient.connect(uriDb, {
+const db = mongoose.connect(uriDb, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
-    poolSize: 5,
+    useFindAndModify: false,
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connection to db')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose connection error: ${err.message}`)
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected')
 })
 
 process.on('SIGINT', async () => {
-    const client = await db
-    client.close()
+    await mongoose.connection.close()
     console.log('Connection for db closed and app termination')
     process.exit(1)
 })
 
 module.exports = db
-
-
