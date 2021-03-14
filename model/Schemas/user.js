fc7e45c551a45303163d8ce94cee69b51ccf3f2e
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2021-03-14 11:25:46 +0200

    Create user.js

---
+++ model/Schemas/user.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose')
+
+
+// const user = {
+//     email: String,
+//     password: String,
+//     subscription: {
+//         type: String,
+//         enum: ["free", "pro", "premium"],
+//         default: "free"
+//     },
+//     token: String
+// }
