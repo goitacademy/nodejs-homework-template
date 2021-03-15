cd9fdb486f7ee84034a08cd6cf623fd44fa2d0f7
Iryna Kosynienko <ikosynienko@gmail.com>
2021-03-15 21:29:57 +0100

    Changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,122 @@
 const express = require('express')
 const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact
+} = require('../../model/index');
+
+const {HttpCode} = require('../../helpers/constans')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+     const contacts  = await listContacts();
+    res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: {
+        contacts
+      }
+    })
+
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact  = await getContactById(req.params.contactId);
+   if (contact) {
+     res.status(HttpCode.OK).json({
+     status: 'success',
+     code: HttpCode.OK,
+     data: {
+       contact,
+     }}) }
+     else {
+       return next ({
+         status: HttpCode.NOT_FOUND,
+         message: 'Not found contact',
+         data: 'Not Found'
+       })
+     }
+ } catch (error) {
+   next(error)
+ }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    if(req.body.name && req.body.email && req.body.phone)
+    {
+      const contact = await addContact(req.body)
+     res.status(HttpCode.CREATED).json({
+     status: 'success',
+     code: HttpCode.OK,
+     data: {
+       contact
+     }
+   })}
+   else {
+    return next ({
+      status: HttpCode.NOT_FOUND,
+      message: 'missing required name field',
+      data: 'Not Found'
+    })
+   }
+
+ } catch (error) {
+   next(error)
+ }
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact  = await removeContact(req.params.contactId);
+   if (contact) {
+    return res.status(HttpCode.OK).json({
+     status: 'success',
+     code: HttpCode.OK,
+     message:"contact deleted",
+     /*data: {
+       contact,
+     }*/}) }
+     else {
+       return next ({
+         status: HttpCode.NOT_FOUND,
+         message: 'Not found contact',
+         data: 'Not Found'
+       })
+     }
+ } catch (error) {
+   next(error)
+ }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact  = await updateContact(req.params.contactId, req.body);
+   if (contact) {
+    return res.status(HttpCode.OK).json({
+     status: 'success',
+     code: HttpCode.OK,
+     data: {
+       contact,
+     }}) }
+     else {
+       return next ({
+         status: HttpCode.NOT_FOUND,
+         message: 'missing fields',
+         data: 'Not Found'
+       })
+     }
+ } catch (error) {
+   next(error)
+ }
 })
 
 module.exports = router
