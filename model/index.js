c609ad9f61874ee3d451806647df5849862894fd
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-03-17 00:58:50 +0200

    1

--- model/index.js
+++ model/index.js
@@ -1,15 +1,15 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
+const listContacts = async () => {};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -17,4 +17,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
