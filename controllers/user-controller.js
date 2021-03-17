111a7fd241f91d48308291bd0232f0b8030b1ca3
Yurii Zubar <barsuk0831@gmail.com>
2021-03-17 11:41:45 +0200

    created avatarUrl

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,13 @@
 const jwt = require("jsonwebtoken");
 const fs = require("fs").promises;
 const path = require("path");
+const Jimp = require("jimp");
 require("dotenv").config();
 const SECRET_KEY = process.env.JWT_SECRET;
 
 const Users = require("../model/users");
+const createFolderExist = require("../helpers/create-dir");
+
 const { HttpCode } = require("../helpers/constants");
 
 const reg = async (req, res, next) => {
@@ -104,7 +107,19 @@ const avatars = async (req, res, next) => {
     const id = req.user.id;
     const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
     const pathFile = req.file.path;
-    const newNameAvatar = `${Date.now()} - ${req.file.originalname}`;
+    const newNameAvatar = `${Date.now()}-${req.file.originalname}`;
+    const img = await Jimp.read(pathFile);
+    await img
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(pathFile);
+    await createFolderExist(path.join(AVATARS_OF_USERS, id));
+    await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar));
+    const avatarUrl = path.normalize(path.join(id, newNameAvatar));
   } catch (e) {
     next(e);
   }
