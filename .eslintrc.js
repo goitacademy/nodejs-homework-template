71e83aa9838f0f3f961f757bb5194b3d2b3872c0
Mykhailo Iskra <iskramichel@gmail.com>
2021-03-18 17:06:01 +0200

    start doing tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    "jest/globals": true,
   },
   extends: ["standard", "plugin:json/recommended", "prettier"],
   parserOptions: {
