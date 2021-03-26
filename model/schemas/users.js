d03abe8160526f8bb0cbf9769114f3cdc452f12c
transformator98 <vanushka16@gmail.com>
2021-03-26 19:36:35 +0200

    test

--- model/schemas/users.js
+++ model/schemas/users.js
@@ -38,6 +38,14 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verify: {
+      type: String,
+      required: [true, 'Verify token required'],
+    },
   },
   { versionKey: false, timestamps: true },
 );
