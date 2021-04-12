948b06fe6dc5269adcab8e53313735cff00f44b0
MazurKateryna <myEmail@example.com>
2021-04-12 22:51:56 +0300

    change path

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Contacts = require('../../model/contacts')
+const Contacts = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
   try {
