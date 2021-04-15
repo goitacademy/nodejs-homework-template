58d650b0aca254d14371e7901b130bf1bc2a08ff
Serhii Demchenko <dem.s.vl@gmail.com>
2021-04-15 11:50:20 +0300

    added logic for working with the database and endpoints for it

--- model/index.js
+++ model/index.js
@@ -1,20 +1,69 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const path = require('path')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json')
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath)
 
-const removeContact = async (contactId) => {}
+  return JSON.parse(data.toString())
+}
 
-const addContact = async (body) => {}
+const getContactById = async contactId => {
+  const data = await fs.readFile(contactsPath)
 
-const updateContact = async (contactId, body) => {}
+  return JSON.parse(data.toString()).find(({ id }) => id === contactId)
+}
+
+const removeContact = async contactId => {
+  const data = await fs.readFile(contactsPath)
+  const contacts = JSON.parse(data.toString())
+  const updatedContacts = contacts.filter(({ id }) => id !== contactId)
+  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, '\t'))
+
+  if (updatedContacts.length === contacts.length) return false
+  return true
+}
+
+const addContact = async body => {
+  const data = await fs.readFile(contactsPath)
+  const contacts = JSON.parse(data.toString())
+  const id = contacts[contacts.length - 1].id + 1
+
+  const newContact = { id, ...body }
+  contacts.push(newContact)
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, '\t'))
+
+  return newContact
+}
+
+const updateContact = async (contactId, body) => {
+  const data = await fs.readFile(contactsPath)
+  const contacts = JSON.parse(data.toString())
+
+  const updatedContacts = contacts.map(contact => {
+    if (contact.id === contactId) {
+      return { ...contact, ...body }
+    }
+    return contact
+  })
+  const newContact = updatedContacts.find(({ id }) => id === contactId)
+  console.log(newContact)
+  if (newContact) {
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(updatedContacts, null, '\t')
+    )
+    return newContact
+  }
+  return null
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContact
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,110 @@
 const express = require('express')
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact
+} = require('../../model')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await listContacts()
+    res.status(200).json({ contacts })
+  } catch (error) {
+    res.status(500).json({
+      message:
+        process.env.NODE_ENV === 'development'
+          ? error.message
+          : 'Something went wrong'
+    })
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const contact = await getContactById(+contactId)
+    if (contact) {
+      res.status(200).json({ contact })
+    } else {
+      res.status(404).json({
+        message: 'Not found'
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      message:
+        process.env.NODE_ENV === 'development'
+          ? error.message
+          : 'Something went wrong'
+    })
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { name, email, phone } = req.body
+    if (name && email && phone) {
+      const contact = await addContact(req.body)
+      res.status(201).json({ ...contact })
+    } else {
+      res.status(400).json({ message: 'missing required name field' })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error:
+        process.env.NODE_ENV === 'development'
+          ? error.message
+          : 'Something went wrong'
+    })
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const isRemove = await removeContact(+contactId)
+    if (isRemove) {
+      res.status(200).json({ message: 'contact deleted' })
+    } else {
+      res.status(404).json({
+        message: 'Not found'
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error:
+        process.env.NODE_ENV === 'development'
+          ? error.message
+          : 'Something went wrong'
+    })
+  }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const body = req.body
+    if (body) {
+      const response = await updateContact(+contactId, body)
+      if (response) {
+        res.status(200).json({ ...response })
+      } else {
+        res.status(404).json({ message: 'Not found' })
+      }
+    } else {
+      res.status(400).json({ message: 'missing fields' })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error:
+        process.env.NODE_ENV === 'development'
+          ? error.message
+          : 'Something went wrong'
+    })
+  }
 })
 
 module.exports = router
