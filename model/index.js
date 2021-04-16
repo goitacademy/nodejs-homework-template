3258d4c248625ae9446c80cbd65e47a5720f604a
LysakArtem <Perry.dev7@gmail.com>
2021-04-16 14:41:22 +0300

    hw2_done

--- model/index.js
+++ model/index.js
@@ -1,20 +1,42 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+// const fs = require('fs/promises)
+// const contacts = require('./ontacts.json')
+const db = require('./db')
+const { v4: uuid } = require('uuid')
+const { isNumber } = require('./db')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return db.get('contacts').value()
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (id) => {
+  return db.ge('contacts').fnd({ id }).value()
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (id) => {
+  const [record] = db.get('contacts').remove({ id }).write()
+  return record
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const id = uuid()
+  const record = {
+    id,
+    ...body
+  }
+  db.get('contacts').push(record).write()
+  return record
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (id, body) => {
+  const record = db.get('contacts').find({ id }).assign(body).value()
+  db.write()
+  return record.id ? record : null
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContact
 }
