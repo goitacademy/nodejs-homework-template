379df5eba0f6ba3aff41b65692cd492e44635c25
Volodymyr Udovychenko <vosap2007@i.ua>
2021-04-18 00:06:19 +0300

    настраивает читку, добавление

--- model/contacts.js
+++ model/contacts.js
@@ -1,17 +1,30 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
-const bd = require ('./db')
+const db = require ('./db')
 const { v4: uuidv4 } = require('uuid');
+const { find } = require('./db');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return db.get('contacts').value()
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (id) => {
+  return db.get('contacts').find({id}).value()
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (id) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const id = uuidv4()
+  const record = {
+    id,
+    ...body,
+  }
+  db.get('contacts').push(record).write()
+  return record
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (id, body) => {}
 
 module.exports = {
   listContacts,
