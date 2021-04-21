f65e7fc118299e49a2197697d24fff185f457b5e
Volodymyr Ivanov <3298901@gmail.com>
2021-04-22 00:43:29 +0300

    hw-2 's complete

---
+++ info.md
@@ -0,0 +1,1 @@
+hw02: https://github.com/goitacademy/nodejs-homework/blob/master/homework-02/README.md
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,107 @@
+const { json } = require('express')
 const express = require('express')
+const { reset } = require('nodemon')
 const router = express.Router()
+const Contacts = require('../../model/contacts')
+const {validationCreateContact, validationUpdateContact} = require('../../valid-contact-router');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+//, получить список контактов - метод GET:
+router.get('/', async (_req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts()
+    return res.status(200).json({ status: 'success', code: 200, data: { contacts, } })
+  } catch (e) {
+    next(e)
+  }
 })
 
+
+//, найти контакт по ID  - метод GET:
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId)
+    if (contact) {
+      return res.json({ status: 'success', code: 200, data: { contact, } })
+    }
+    else {
+      return res.status(404).json({ status: 'error', code: 404, data: 'Not Found' })
+    }
+  } catch (e) {
+    next(e)
+  }
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+//, добаить контакт  - метод PUSH:
+router.post('/', validationCreateContact, async (req, res, next) => {
+  try {
+    if (req.body.name && req.body.email && req.body.phone) {
+ //, проверку делал по хронологии ДЗ (до валидации), работает без валидации , убирать пока не хочу
+
+      const contact = await Contacts.addContact(req.body)
+        return res.status(201).json({ status: 'success', code: 201, data: { contact, } })
+    }
+    else {
+      return res.status(400).json({ status: 'error', code: 400, data: { "message": "missing required name field" } })
+    }
+ }  catch (e) {
+    next(e)
+  }
 })
 
+
+//, - обновить  заменить весь объект  - метод PUT:
+router.put("/:contactId", validationUpdateContact, async (req, res, next) => {
+  try {
+    if ((req.body.name || req.body.email || req.body.phone)) {
+ //, проверку делал по хронологии ДЗ (до валидации), работает без валидации , убирать пока не хочу
+      const contact = await Contacts.updateContact(req.params.contactId, req.body)
+      if (contact) {
+        return res.status(200).json({ status: 'success', code: 200, data: { contact, } })
+      }
+    }
+        return res.status(400).json({ status: 'error', code: 400, data: { "message": "missing fields" } })
+  } catch (e) {
+    next(e)
+  }
+})
+
+
+//, - удалить контакт по ID -  метод DELETE:
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await Contacts.removeContact(req.params.contactId)
+    if (contacts) {
+      return res.json({
+        status: 'success', code: 200, data: { "message": "contact deleted" }
+      })
+    }
+    else {
+      return res.status(404).json({
+        status: 'error', code: 404, data: { "message": "Not Found" }
+      })
+    }
+  }catch (e) {
+    next(e)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+//, - заменить статус (изменить что-то в существующем объекте) по ID  - метод PATCH
+router.patch('/:contactId', validationUpdateContact, async (req, res, next) => {
+    try {
+    if ((req.body.name || req.body.email || req.body.phone)) {
+ 
+      const contact = await Contacts.updateContact(req.params.contactId, req.body)
+      if (contact) {
+        return res.status(200).json({ status: 'success', code: 200, data: { contact, } })
+      }
+    }
+        return res.status(400).json({ status: 'error', code: 400, data: { "message": "missing fields" } })
+  } catch (e) {
+    next(e)
+  }
 })
+
 
 module.exports = router
