1e19020049e52f7eac3b918440da8145b7f8a059
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-04-22 15:31:39 +0300

    Changed contacts

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  res.status(500).json({ status: 'fail', message: err.message })
 })
 
 module.exports = app
