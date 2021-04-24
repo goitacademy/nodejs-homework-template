1d0773ad6fad7f8c7f2961537d901137fd670e7f
MikeYef96 <mike.yef@gmail.com>
2021-04-24 22:11:56 +0300

    nodejs express

---
+++ services/validation.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().min(3).max(30).optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string()
+    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
+    .optional(),
+});
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    return next({
+      status: 400,
+      message: 'Bad request',
+    });
+  }
+  next();
+};
+
+module.exports.createContact = (req, _res, next) => {
+  return validate(schemaCreateContact, req.body, next);
+};
+
+module.exports.updateContact = (req, _res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
+};
