bdfcc545deb63eac8480443dbd139f6cd6a61276
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2021-05-04 00:05:23 +0300

    added timestamps

--- model/schemas/contactsSchemas.js
+++ model/schemas/contactsSchemas.js
@@ -1,27 +1,24 @@
 const { Schema, model } = require('mongoose')
 
-const contactsSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-})
-
-// contactsSchema.path('name').validate((value) => {
-//   const re = /[A-Z]w*/
-
-//   return re.test(String(value))
-// })
+  { versionKey: false, timestamps: true }
+)
 
 const contacts = model('contacts', contactsSchema)
 
