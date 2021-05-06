2eb91c4911679ae8de7eef7c0129cda3109c93e8
madgav <madgav@mail.ru>
2021-05-06 20:28:31 +0600

    homework-03

---
+++ model/contact.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  }
+)
+
+const Contact = mongoose.model('Contact', contactSchema, 'contacts')
+
+module.exports = Contact
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,33 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const fs = require('fs')
-const contactsService = require('../../model/index.js')
 const ERROR_MESSAGES = require('../../constants/index.js')
-
-const contacts = JSON.parse(
-  fs.readFileSync('./model/contacts.json', { encoding: 'utf-8' }),
-)
+const Contact = require('../../model/contact.js')
 
 router.get('/', async (req, res, next) => {
-  const contactList = contactsService.listContacts()
+  const contactList = await Contact.find()
   res.status(200).json(contactList)
 })
 
 router.get('/:contactId', async (req, res, next) => {
   const contactId = req.params.contactId
 
-  const foundContact = contactsService.getContactById(
-    Number(contactId),
-    ERROR_MESSAGES.NOT_FOUND,
-  )
+  Contact.findOne({ _id: contactId })
+    .then((foundContact) => {
+      if (!foundContact) {
+        return res.status(404).json({
+          message: ERROR_MESSAGES.NOT_FOUND,
+        })
+      }
 
-  if (!foundContact) {
-    return res.status(404).json({
-      message: ERROR_MESSAGES.NOT_FOUND,
+      res.status(200).json({ found_contact: foundContact })
     })
-  }
-
-  res.status(200).json({ foundContact })
+    .catch((error) => res.status(400).json({ error: error }))
 })
 
 router.post('/', async (req, res, next) => {
@@ -39,33 +33,39 @@ router.post('/', async (req, res, next) => {
     })
   }
 
-  const updateContact = contactsService.addContact(name, email, phone)
+  try {
+    const newContact = new Contact({ name, email, phone })
+    const updateContact = await newContact.save()
 
-  res.status(201).json(updateContact)
+    res.status(201).json({
+      new_contact: updateContact,
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
   const contactId = req.params.contactId
 
-  const newContacts = contactsService.removeContact(
-    Number(contactId),
-    ERROR_MESSAGES.NOT_FOUND,
-  )
+  Contact.findOneAndDelete({ _id: contactId })
+    .then((foundContact) => {
+      if (!foundContact) {
+        return res.status(404).json({ message: ERROR_MESSAGES.NOT_FOUND })
+      }
 
-  if (newContacts.length === contacts.length) {
-    return res.status(400).json({
-      message: ERROR_MESSAGES.NOT_FOUND,
+      res.json({
+        found_contact: foundContact,
+        message: `Contact with ID=${contactId} deleted successfully!`,
+      })
     })
-  }
-
-  res.status(200).json({
-    message: `Contact with ID=${contactId} deleted successfully!`,
-  })
+    .catch((error) => res.status(400).json({ error: error }))
 })
 
 router.patch('/:contactId', async (req, res, next) => {
   const contactId = req.params.contactId
-  const { name, email, phone } = req.body
+  const newFields = req.body
+  const { name, email, phone } = newFields
 
   if (!name || !email || !phone) {
     return res.status(400).json({
@@ -73,21 +73,49 @@ router.patch('/:contactId', async (req, res, next) => {
     })
   }
 
-  const updatedContacts = contactsService.updateContact(
-    Number(contactId),
-    name,
-    email,
-    phone,
-    ERROR_MESSAGES.NOT_FOUND,
+  Contact.findOneAndUpdate(
+    { _id: contactId },
+    { $set: newFields },
+    { new: true, useFindAndModify: false },
   )
+    .then(foundContact => {
+      if (!foundContact) {
+        return res
+          .status(404)
+          .json({ message: ERROR_MESSAGES.NOT_FOUND })
+      }
 
-  if (!updatedContacts) {
-    return res.status(404).json({
-      message: ERROR_MESSAGES.NOT_FOUND,
+      res.json({ found_contact: foundContact })
+    })
+    .catch(error => res.status(400).json({ error: error }))
+})
+
+router.patch('/:contactId/favorite', async (req, res, next) => {
+  const contactId = req.params.contactId
+  const newFields = req.body
+  const { favorite } = newFields
+
+  if (!favorite) {
+    return res.status(400).json({
+      message: 'missing field favorite',
     })
   }
 
-  res.status(200).json(updatedContacts)
+  Contact.findOneAndUpdate(
+    { _id: contactId },
+    { $set: newFields },
+    { new: true, useFindAndModify: false },
+  )
+    .then(foundContact => {
+      if (!foundContact) {
+        return res
+          .status(404)
+          .json({ message: ERROR_MESSAGES.NOT_FOUND })
+      }
+
+      res.json({ found_contact: foundContact })
+    })
+    .catch(error => res.status(400).json({ error: error }))
 })
 
 module.exports = router
