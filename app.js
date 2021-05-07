7574101553cae02b99c1089e503cfec13135f7ab
Vadim-Vatrya <VatryaVV@gmail.com>
2021-05-07 23:24:17 +0300

    add error handler in app

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use((_req, res) => {
+  res.status(404).json({ status: 'Error', code: 404, message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+app.use((err, _req, res, next) => {
+  res.status(500).json({ status: 'Error', code: err.status || 500, message: err.message })
 })
 
 module.exports = app
