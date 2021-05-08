99fe0f734b64d4b67fa4fafd7bdf7b7f9b9cc6ee
Vadim-Vatrya <VatryaVV@gmail.com>
2021-05-08 17:35:07 +0300

    router.get

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
-// const contacts = require(../..)
+const Contacts = require('../../model/index')
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/', async (_req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts()
+    res.json({
+      status: 'Success',
+      code: 200,
+      data: { contacts }
+    })
+  } catch(error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
