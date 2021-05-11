9eb1cbcec7729431e528750d1c1a87e63edd7741
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-05-11 17:02:52 +0300

    чуть подправил коммиты и убрал лишнее

--- test/contacts.e2e.test.js
+++ test/contacts.e2e.test.js
@@ -9,22 +9,8 @@ const issueToken = (payload, secret) => jwt.sign(payload, secret);
 const token = issueToken({ id: User._id }, JWT_SECRET_KEY);
 User.token = token;
 
-// jest.mock("../model/contacts.js");
-// jest.mock("../model/users.js");
-
-// describe("Testing the route api/contacts", () => {
-//   describe("should handle GET request", () => {
-//     test("should return 200 status for GET: /contacts", async (done) => {
-//       const res = await request(app)
-//         .get("/api/contacts")
-//         .set("Authorzation", `Bearer ${token}`); //имитация авторизации
-//       expect(res.status).toEqual(200);
-//       expect(res.body).toBeDefined();
-//       expect(res.body.data.contacts).toBeInstanceOf(Array);
-//       done();
-//     });
-//   });
-// });
+jest.mock("../model/contacts.js");
+jest.mock("../model/users.js");
 
 describe("Testing the route api/contacts", () => {
   describe("should handle GET request", () => {
