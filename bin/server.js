7f7b7604450802260837608b5454f690fa002bd9
Anelliya <najdenova.elena@gmail.com>
2021-05-23 07:23:16 +0300

    Add mongodb - resolve conflicts
    Rebased to latest master head)

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,13 @@
 const app = require("../app");
+const db = require("../model/db");
+
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch((e) => {
+  console.log("Error:", e.message);
+  process.exit(1);
 });
