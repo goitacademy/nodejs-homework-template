b24d272f89ac8e1bdb669dd30a9b5ffbe5a1bc57
Oleksandr Ivashchuk <thelookout84@gmail.com>
2021-05-31 16:44:08 +0300

    update db and app

---
+++ routes/api/db.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const uriDb = process.env.URI_DB
+
+const db = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  poolSize: 5,
+})
+
+mongoose.connection.on('connected', () => {
+  console.log(`Database connection successful`)
+})
+
+mongoose.connection.on('error', (e) => {
+  console.log(`Error mongoose connection ${e.message}`)
+})
+
+mongoose.connection.on('disconnected', (e) => {
+  console.log(`Mongoose disconnected`)
+})
+
+process.on('SIGINT', async () => {
+  mongoose.connection.close(() => {
+    console.log('Connection to DB terminated')
+    process.exit(1)
+  })
+})
+
+module.exports = db
