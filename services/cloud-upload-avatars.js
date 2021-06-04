2c3e581ed520bafe95858eeedf6efa57de048013
Ihor Prokopets <kiringa23@gmail.com>
2021-06-04 18:26:49 +0300

    add avatars with cloud upload

--- services/cloud-upload-avatars.js
+++ services/cloud-upload-avatars.js
@@ -5,7 +5,18 @@ class Upload {
     this.uploadCloud = uploadCloud;
   }
 
-  async saveAvatarToCloud({ pathFile, userIdImg }) {}
+  async saveAvatarToCloud(pathFile, userIdImg) {
+    const { public_id: publicId, secure_url: avatar } = await this.uploadCloud(
+      pathFile,
+      {
+        public_id: userIdImg?.replace('Photo/', ''),
+        folder: 'Photo',
+        transformation: { width: 250, crop: 'pad' },
+      }
+    );
+    await this.deleteTempFile(pathFile);
+    return { userIdImg: publicId, avatarUrl: avatar };
+  }
 
   async deleteTempFile(pathFile) {
     try {
