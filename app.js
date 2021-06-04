1570ae0a55ddd73fc2d3f51ab3c7f9756b4c3e4c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-04 23:37:53 +0300

    Added tocken check middleware.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const tokenCheck = require('./helpers/token-check');
 
 const contactsRouter = require('./routes/api/contacts/contacts');
 const usersRouter = require('./routes/api/users/users');
@@ -13,7 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/api/contacts', tokenCheck, contactsRouter);
 app.use('/api/users', usersRouter);
 
 app.use((req, res) => {
--- routes/api/users/users.js
+++ routes/api/users/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const tokenCheck = require('../../../helpers/token-check');
 const { validationNewUser, validationLoginUser } = require('./validation');
 const Controllers = require('../../../controllers/users-controllers');
 
 router.post('/signup', validationNewUser, Controllers.signup);
 router.post('/login', validationLoginUser, Controllers.login);
-router.post('/logout', Controllers.logout);
+router.post('/logout', tokenCheck, Controllers.logout);
 
 module.exports = router;
