5432296c79a1f16e65666dd7e1f7b3e7905d4925
Anton Sannykov <antonsannykov@gmail.com>
2021-06-14 17:17:24 +0300

    final

---
+++ routes/api/validation.js
@@ -0,0 +1,35 @@
+const Joi = require('joi')
+const schemaCreate = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+
+  phone: Joi.string()
+    .length(10).pattern(/^[0-9]+$/).required()
+
+})
+
+const schemaChange = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .optional(),
+
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).optional(),
+
+  phone: Joi.string()
+    .length(10).pattern(/^[0-9]+$/).optional()
+
+})
+
+module.exports = {
+  schemaCreate,
+  schemaChange
+}
