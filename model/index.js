d8791b89b02b0e3ecb99e98ebabc4f1fb7540609
Holovchyk <golovchuk.misha@gmail.com>
2021-06-22 18:22:37 +0300

    First commit

--- model/index.js
+++ model/index.js
@@ -1,15 +1,32 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const db = require('./db')
+const shortid = require('shortid')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return db.get('contacts').value()
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (id) => {
+  return db.get('contacts').find({ id }).value()
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (id) => {
+  const [record] = db.get('contacts').remove({ id }).write()
+  return record
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const id = shortid.generate()
+  const record = { id, ...body }
+  db.get('contacts').push(record).write()
+  return record
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (id, body) => {
+  const record = db.get('contacts').find({ id }).assign(body).value()
+  db.write()
+
+  return record.id ? record : null
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,104 @@
 const express = require('express')
 const router = express.Router()
 
+const contactsData = require('../../model/index')
+
+const {
+  addValidationContact,
+  updateValidationContact,
+} = require('./validationContact')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await contactsData.listContacts()
+
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: { contacts },
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact = await contactsData.getContactById(req.params.contactId)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: { contact },
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'The data was not found',
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', addValidationContact, async (req, res, next) => {
+  try {
+    const contact = await contactsData.addContact(req.body)
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { contact },
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact = await contactsData.removeContact(req.params.contactId)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 201,
+        message: 'The contact was dleted',
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'The contact was not found',
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', updateValidationContact, async (req, res, next) => {
+  try {
+    const contact = await contactsData.updateContact(
+      req.params.contactId,
+      req.body
+    )
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: { contact },
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'The contact was not found',
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
