84484025d03c99b28502448cefd6f2b293abddbf
Julia Lavrenko <lavrenkojulia@gmail.com>
2021-06-26 16:05:22 +0200

    fixed bugs

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,7 @@
-const app = require("../app");
-const db = require("../model/db");
-const PORT = process.env.PORT || 3000;
+const app = require('../app')
 
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
-  });
-}).catch((e) => {
-  console.log(`Error: ${e.message}`);
-});
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
+})
