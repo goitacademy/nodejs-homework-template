f51918c13f637ee2c07622830fc243365d68004c
Olga Verbetska <verbetska150@gmail.com>
2021-07-03 14:38:25 +0300

    create step 4

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,6 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+// ============================================ files
+
+const multer = require('multer');
+const path = require('path');
+const tempDir = path.resolve('tmp');
+// const TEMP_DIR = path.join(__dirname, process.env.TEMP_DIR);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, tempDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+// const upload = multer({ storage: storage });
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2000000 },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.includes('image')) {
+      console.log(file);
+      cb(null, true);
+      return;
+    }
+    cb(null, false);
+  },
+});
+
+// ============================================ files
+
 const { asyncWrapper } = require('../../helpers/apiHelpers');
 const { authMiddleware } = require('../../middlewares/authMiddleware');
 
@@ -10,6 +41,7 @@ const {
 } = require('../../middlewares/userValidationMiddleware');
 
 const AuthController = require('../../controllers/authController');
+// const FilesController = require('../../controllers/filesController');
 
 router.post(
   '/signup',
@@ -20,8 +52,16 @@ router.post('/login', userDataValidation, asyncWrapper(AuthController.login));
 router.post('/logout', authMiddleware, asyncWrapper(AuthController.logout));
 router.post(
   '/current',
+  userDataValidation,
   authMiddleware,
   asyncWrapper(AuthController.receiveCurrentUser)
 );
+
+router.patch('/avatars', upload.single('avatar'), async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+
+  res.status(200).json({ message: 'file upload' });
+});
 
 module.exports = router;
