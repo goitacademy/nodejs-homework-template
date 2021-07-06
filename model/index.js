2e44934018e7e04a8b83fbd9cd14fdc4bdd49a80
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-07-07 00:20:07 +0300

    Update

--- model/index.js
+++ model/index.js
@@ -1,15 +1,82 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const path = require("path");
+const shortid = require("shortid");
+const contacts = path.join(__dirname, "./contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const response = await fs.readFile(contacts);
+    const result = JSON.parse(response);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const response = await fs.readFile(contacts);
+    const result = JSON.parse(response);
+    const findContact = result.find(({ id }) => id.toString() === contactId);
 
-const removeContact = async (contactId) => {}
+    return findContact || "contact is not found";
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const response = await fs.readFile(contacts);
+    const result = JSON.parse(response);
+    const newContactsList = result.filter(
+      ({ id }) => id.toString() !== contactId
+    );
 
-const updateContact = async (contactId, body) => {}
+    fs.writeFile(contacts, JSON.stringify(newContactsList), (error) => {
+      if (error) throw error;
+    });
+
+    return newContactsList.length !== result.length
+      ? `contact with id ${contactId} removed `
+      : "contact is not found";
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const response = await fs.readFile(contacts);
+    const result = JSON.parse(response);
+    const newContact = { id: shortid.generate(), ...body };
+    result.push(newContact);
+
+    fs.writeFile(contacts, JSON.stringify(result), (error) => {
+      if (error) throw error;
+    });
+
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  const response = await fs.readFile(contacts);
+  const result = JSON.parse(response);
+  const update = result.map((contact) => {
+    return contact.id.toString() === contactId
+      ? { id: contact.id, ...body }
+      : contact;
+  });
+
+  fs.writeFile(contacts, JSON.stringify(update), (error) => {
+    if (error) throw error;
+  });
+
+  return body.name;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +84,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ utils/validators.js
@@ -0,0 +1,17 @@
+const Joi = require("joi");
+
+const contactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      tlds: {
+        allow: false,
+      },
+    })
+    .required(),
+  phone: Joi.string()
+    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
+    .required(),
+});
+
+module.exports = { contactSchema };
