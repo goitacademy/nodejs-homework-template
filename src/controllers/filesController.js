a1c5e92c3a15ca6b4a5fede8ab87571c781a912d
Olga Verbetska <verbetska150@gmail.com>
2021-07-06 21:18:15 +0300

    create avatar with jimp and file transfer

--- src/controllers/filesController.js
+++ src/controllers/filesController.js
@@ -1,6 +1,25 @@
+// const {  } = require("jimp");
+const path = require('path');
+const fs = require('fs').promises;
+const jimp = require('jimp');
+
+const avatarsDir = path.resolve('./public/avatars');
+
 const avatarUpdater = async (req, res) => {
   console.log('req.file', req.file);
   console.log('req.body', req.body);
+  if (req.file) {
+    const { file } = req;
+    const ava = await jimp.read(file.path);
+    ava
+      .autocrop()
+      .cover(
+        250, 250, jimp.HORIZONTAL_ALIGN_CENTER | jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(file.path);
+    ava.resize(250, 250).writeAsync(file.path);
+    await fs.rename(file.path, path.join(avatarsDir, file.originalname));
+  }
 
   res.status(200).json({ message: 'file upload' });
 };
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 // ============================================ files
 
 const multer = require('multer');
 const path = require('path');
 const tempDir = path.resolve('tmp');
+
 // const TEMP_DIR = path.join(__dirname, process.env.TEMP_DIR);
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 2000000 },
+  limits: { fileSize: 3000000 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.includes('image')) {
       cb(null, true);
