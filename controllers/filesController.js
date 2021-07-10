074e957533068c9d051cc45626410515e7d34417
Filip Koza <peacefilip1989@gmail.com>
2021-07-10 11:31:37 +0200

    finish

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -5,13 +5,9 @@ const AVATARS_DIR = path.resolve('./public/avatars')
 const UPLOADS_DIR = path.resolve('./tmp')
 const Jimp = require('jimp')
 const uploadController = async (req, res) => {
-  Jimp.read(`${UPLOADS_DIR}/${req.fileName}`)
-    .then(avatar => {
-      avatar.resize(250, 250)
-    })
-    .catch(err => {
-      throw err
-    })
+  const avatar = await Jimp.read(`${UPLOADS_DIR}/${req.fileName}`)
+  avatar.resize(250, 250).writeAsync(`${UPLOADS_DIR}/${req.fileName}`)
+
   await fs.rename(
     `${UPLOADS_DIR}/${req.fileName}`,
     `${AVATARS_DIR}/${req.fileName}`,
