e2968c4f88ff1fa3cf2dcf653b03decc08c3383e
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-07-15 23:52:15 +0300

    update

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,27 @@
 const app = require('../app')
 
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const { DB_HOST } = process.env
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
+const connection = mongoose.connect(DB_HOST, {
+  promiseLibrary: global.Promise,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log(`Server running. Port: ${PORT}`)
+      console.log('Database connection successful!')
+    })
+  })
+  .catch(err => {
+    console.log(`Server not running. ${err.message}`)
+    process.exit(1)
+  })
