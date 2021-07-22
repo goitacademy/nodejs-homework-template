4e37b46d878f071865aa0576ef5b78436a8975f9
ViktorBaklar <baklarv@gmail.com>
2021-07-22 15:30:38 +0300

    Update listContacts.js

--- controllers/listContacts.js
+++ controllers/listContacts.js
@@ -4,18 +4,21 @@ const listContacts = async (req, res, next) => {
   try {
     const result = await Contact.find()
 
+    if (!result) {
+      res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'Not found'
+      })
+    }
+
     res.status(200).json({
       status: 'success',
       code: 200,
       data: result
     })
   } catch (error) {
-    res.status(404).json({
-      status: 'not found',
-      code: 404,
-      message: error.message,
-    })
-    // next(error)
+    next(error)
   }
 }
 
