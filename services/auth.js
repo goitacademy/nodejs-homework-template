2e086b9fe278c9c0642d4acbd46bf19b10595cc6
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-07-25 23:06:42 +0300

    update

---
+++ services/auth.js
@@ -0,0 +1,23 @@
+const passport = require('passport')
+require('../config/config-passport')
+
+const auth = (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (err, user) => {
+        let token = null
+        const header = req.get('Authorization')
+        if (header) {
+            token = header.split(' ')[1]
+        }
+
+        if (!user || err || token !== user.token) {
+            return res.json({
+                status: 401,
+                message: 'Not authorized'
+            })
+        }
+        req.user = user
+        return next()
+    })(req, res, next)
+}
+
+module.exports = auth
---
+++ services/validationUser.js
@@ -0,0 +1,26 @@
+const Joi = require('joi')
+
+const schemaForSign = Joi.object({
+    email: Joi.string()
+        .email()
+        .required(),
+    password: Joi.string()
+        .min(8)
+        .required()
+})
+
+const validate = (schema, obj, next) => {
+    const { error } = schema.validate(obj)
+    if (error) {
+        console.log(error)
+        return next({
+            status: 400,
+            message: 'Bad request'
+        })
+    }
+    next()
+}
+
+module.exports.validateSign = (req, _res, next) => {
+    return validate(schemaForSign, req.body, next)
+}
