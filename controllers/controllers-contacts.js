6d68a2a5020b4016f2f7f487037a2d8c9bea22bd
Max <maxminullin@gmail.com>
2021-07-29 15:12:59 +0300

    add validation

--- controllers/controllers-contacts.js
+++ controllers/controllers-contacts.js
@@ -14,7 +14,7 @@ const getAll = async (req, res, next) => {
     }
   }
   
-  const getById = async (req, res, next) => {
+const getById = async (req, res, next) => {
     try {
       const contactById = await getContactById(req.params.contactId)
       if (contactById) {
@@ -25,6 +25,73 @@ const getAll = async (req, res, next) => {
         })
       } else {
         res.status(code.NOT_FOUND).json({
+          status: code.NOT_FOUND,
+          massage: 'Not Found Contact',
+          data: 'Not Found',
+        })
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+
+const create = async (req, res, next) => {
+    try {
+      const { name, email, phone } = req.body
+      if (name && email && phone) {
+        const contact = await addContact(req.body)
+        res.status(code.CREATED).json({
+          status: 'success',
+          code: code.CREATED,
+          data: { contact },
+        })
+      } else {
+        res.status(code.BAD_REQUEST).json({
+          status: code.BAD_REQUEST,
+          massage: 'missing required name field',
+        })
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+  
+const update = async (req, res, next) => {
+    try {
+      const updatedContact = await updateContact(req.params.contactId, req.body)
+      const contact = await getContactById(req.params.contactId)
+      if (contact) {
+        return res.status(code.OK).json({
+          status: 'success',
+          code: code.OK,
+          data: { updatedContact },
+        })
+      } else {
+        return next({
+          status: code.NOT_FOUND,
+          massage: 'missing fields',
+          data: 'Not Found',
+        })
+      }
+    } catch (error) {
+      next(error)
+    }
+}
+
+const remove = async (req, res, next) => {
+    try {
+      const id = req.params.contactId
+      const contactList = await listContacts()
+      const contactListId = contactList.map(({ id }) => id)
+      if (contactListId.some(listId => listId === Number(id))) {
+        const contactById = await removeContact(id)
+        return res.status(code.OK).json({
+          status: 'success',
+          massage: 'contact deleted',
+          code: code.OK,
+        })
+      } else {
+        return next({
           status: code.NOT_FOUND,
           massage: 'Not Found Contact',
           data: 'Not Found',
