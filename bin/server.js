afdbe835165bd349ed37542bf384324d696f4831
GolovinaElia <eliagolovina@ukr.net>
2021-08-01 11:44:17 +0300

    Добавляет файлы

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,22 @@
 const app = require("../app")
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-const PORT = process.env.PORT || 3000
+const { DB_HOST, PORT = 3000 } = process.env
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () =>
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  )
+  .catch((error) => console.log(error))
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// const PORT = process.env.PORT || 3000
+
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
