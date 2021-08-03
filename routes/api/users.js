d65bbb1b6ff703f46e8d7c616e5759626b03988b
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-08-03 19:32:15 +0300

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const ctrlUsers = require('../../controller/users')
-const { validateSign } = require('../../services/validationUser.js')
-const auth = require('../../services/auth')
+const express = require("express");
+const router = express.Router();
+const ctrlUsers = require("../../controller/users");
+const { validateSign } = require("../../services/validationUser.js");
+const auth = require("../../services/auth");
+const upload = require("../../services/upload");
 
-router.post('/signup', validateSign, ctrlUsers.register)
+router.post("/signup", validateSign, ctrlUsers.register);
 
-router.post('/signin', validateSign, ctrlUsers.login)
+router.post("/signin", validateSign, ctrlUsers.login);
 
-router.post('/logout', auth, ctrlUsers.logout)
+router.post("/logout", auth, ctrlUsers.logout);
 
-router.get('/current', auth, ctrlUsers.currentUser)
+router.get("/current", auth, ctrlUsers.currentUser);
 
-module.exports = router
+router.patch("/avatars", auth, upload.single("avatar"), ctrlUsers.updateAvatar);
+
+module.exports = router;
