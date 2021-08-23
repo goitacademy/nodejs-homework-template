ee60c4c0cb80d0d55a9419ca280ad306ae182d7f
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-08-23 16:20:15 +0300

    contacts functions

---
+++ model/contacts/getContactById.js
@@ -0,0 +1,12 @@
+const listContacts = require("./listContacts");
+
+const getContactById = async (contactId) => {
+  const contactsList = await listContacts();
+  const contact = await contactsList.find((el) => el.id === +contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
+
+module.exports = getContactById;
