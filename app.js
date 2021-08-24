af7cef6530f444f5cc41af5c70dd64aa050e72bf
Alona-Mykhaylenko <79852771+Alona-Mykhaylenko@users.noreply.github.com>
2021-08-24 17:41:30 +0200

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+const { DB_HOST, PORT = 3000 } = process.env;
 
-const app = express()
+// const { Schema, model } = mongoose;
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// const contactSchema = Schema({
+//   name: {
+//     type: String,
+//     required: [true, "Set name for contact"],
+//   },
+//   email: {
+//     type: String,
+//   },
+//   phone: {
+//     type: String,
+//   },
+//   favorite: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// const Contact = model("contact", contactSchema);
 
-app.use('/api/contacts', contactsRouter)
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => console.log(error));
+
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
