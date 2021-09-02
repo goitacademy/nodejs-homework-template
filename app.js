759762fb1c46137115fa01a73e6fbc6abfdac6f1
AntonLovkin <anton28111987@gmail.com>
2021-09-02 22:36:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-// const api = require('./api')
+// const api = require('./api') //added
 
-const contactsRouter = require('./api/contacts')
+const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
@@ -12,16 +12,16 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
-// app.use(express.json())
+app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
-// })
+app.use((_, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
 
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message })
-// })
+app.use((err, _, res, __) => {
+  res.status(500).json({ message: err.message })
+})
 
 module.exports = app
