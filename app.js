6fcd50b4d4b5f207b492951be42046bf9353cd4b
VVariychuk <v.variychuk.90@gmail.com>
2021-09-18 12:37:24 +0300

    +

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const { contactsRouter, authRouter } = require('./routes')
 
 const app = express()
 
@@ -11,8 +11,10 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', authRouter)
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Not found' })
