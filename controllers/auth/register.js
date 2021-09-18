03abe65d1b4f083ca60bf573032b6d7bb2fcedbe
VVariychuk <v.variychuk.90@gmail.com>
2021-09-18 12:02:57 +0300

    +

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -4,6 +4,7 @@ const { User } = require('../../model')
 
 const register = async (req, res) => {
   const { email, password } = req.body
+
   const user = await User.findOne({ email })
 
   if (user) {
@@ -11,7 +12,9 @@ const register = async (req, res) => {
   }
 
   const newUser = new User({ email })
+
   newUser.setPassword(password)
+
   await newUser.save()
 
   res.status(201).json({ newUser })
