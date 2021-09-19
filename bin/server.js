15dec64258d925f86e34e370e5935f05f3b46902
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-09-20 00:04:28 +0300

    добавляет новые файлы для работы

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,20 @@
 const app = require('../app');
+const mongoose = require('mongoose');
+
+const { DB_HOST } = require('../config');
 
 // const PORT = process.env.PORT || 3000
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTOpology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(error => console.log(error));
