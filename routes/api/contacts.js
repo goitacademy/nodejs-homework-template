adcaee51bb42638297f8fc8a411ddfbcc613fe87
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-09-19 15:03:34 +0300

    add MiddleWare: controllerWrapper

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { contacts: ctrl } = require("../../controllers");
+const { controllerWrapper } = require("../../middlewares");
 
-router.get("/", ctrl.listContacts);
+router.get("/", controllerWrapper(ctrl.listContacts));
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", controllerWrapper(ctrl.getContactById));
 
-router.post("/", ctrl.addContact);
+router.post("/", controllerWrapper(ctrl.addContact));
 
-router.delete("/:contactId", ctrl.removeContactById);
+router.delete("/:contactId", controllerWrapper(ctrl.removeContactById));
 
-router.put("/:contactId", ctrl.updateContactById);
+router.put("/:contactId", controllerWrapper(ctrl.updateContactById));
 
 module.exports = router;
