1e455766ca03758c630bb91a72ee121e269b1134
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-09-24 21:38:12 +0300

    add token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-empty */
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { BadRequest } = require('http-errors');
 const { User } = require('../../models');
 
@@ -8,53 +9,22 @@ const login = async (req, res, next) => {
   const user = await User.findOne({ email });
   if (!user) {
     throw new BadRequest('Wrong email');
-    // res.status(400).json({
-    //   status: 'error',
-    //   code: 400,
-    //   message: 'Wrong email',
-    // });
   }
   const hashPassword = user.password;
   const compareResult = bcrypt.compareSync(password, hashPassword);
   if (!compareResult) {
     throw new BadRequest('Wrong password');
-    // return res.status(400).json({
-    //   status: 'error',
-    //   code: 400,
-    //   message: 'Wrong password',
-    // });
   }
-  const token = 'fhfsfgs.dfgsfhfhh.dfddsadfff';
+
+  const payload = {
+    id: user._id,
+  };
+
+  const { SECRET_KEY } = process.env;
+  const token = jwt.sign(payload, SECRET_KEY);
   res.json({
     token,
   });
-
-  // try {
-  //   const { email, password } = req.body;
-  //   const user = await User.findOne({ email });
-  //   if (!user) {
-  //     res.status(400).json({
-  //       status: 'error',
-  //       code: 400,
-  //       message: 'Wrong email',
-  //     });
-  //   }
-  //   const hashPassword = user.password;
-  //   const compareResult = bcrypt.compareSync(password, hashPassword);
-  //   if (!compareResult) {
-  //     return res.status(400).json({
-  //       status: 'error',
-  //       code: 400,
-  //       message: 'Wrong password',
-  //     });
-  //   }
-  //   const token = 'fhfsfgs.dfgsfhfhh.dfddsadfff';
-  //   res.json({
-  //     token,
-  //   });
-  // } catch (error) {
-  //   next(error);
-  // }
 };
 
 module.exports = login;
