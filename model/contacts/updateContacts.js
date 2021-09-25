f07935dd20ab3281e190fa9e84ca00a810b8362c
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-09-25 14:27:01 +0300

    Remove files

--- model/contacts/updateContacts.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs/promises");
-const { contactsPath } = require("../../utils");
-
-const updateContacts = async (newContact) => {
-  await fs.writeFile(contactsPath, JSON.stringify(newContact));
-};
-
-module.exports = updateContacts;
