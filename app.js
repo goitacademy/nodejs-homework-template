d5e546e1cbbab23c87b72d32041ee3f6c304c1bd
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-09-28 21:29:05 +0300

    решает ошибку добавления

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/contacts', contactsRouter);
-app.use('/api/users', authRouter);
+app.use('/api/auth', authRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Not found' });
