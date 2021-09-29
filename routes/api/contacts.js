db9d2df2cd1697c3fb955f94f0f56cdc70e652c5
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-09-29 17:43:27 +0300

    добавляет роути и схему

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,88 @@
 const express = require('express')
 const router = express.Router()
 
+const contactOperations = require("../../model/index")
+const {contactSchema} = require ("../../schemas")
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contactOperations.listContacts()
+    res.json({
+      status: "success",
+      code: 200,
+      data: { result }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { id } = req.params
+    const result = await contactOperations.getContactById(id)
+    if (!result) {
+      const error = new Error(`Contact with id=${id} not found`)
+      error.status = 404
+      throw error
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: { result }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    console.log(req.body)
+    const { error } = contactSchema.validate(req.body)
+    if (error) {
+      const err = new Error(error.message)
+      err.status = 400
+      throw err
+    }
+    const result = await contactOperations.addContact(req.body)
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: { result }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    const { error } = contactSchema.validate(req.body)
+    if (error) {
+      const err = new Error(error.message)
+      err.status = 400
+      throw err
+    }
+    const {id} = req.params
+    const result = await contactOperations.updateContact(id, req.body)
+    if (!result) {
+      const error = new Error(`Contact with id=${id} not found`)
+      error.status = 404
+      throw error
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: { result }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
