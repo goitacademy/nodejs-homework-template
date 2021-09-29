c09ff9198631a374e51bd5471cd3542c8ccb06ed
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-09-29 20:50:11 +0300

    getById

--- model/index.js
+++ model/index.js
@@ -1,9 +1,19 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const path = require('path');
+// const contactsPath = require('./contacts.json')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf8');
+  return JSON.parse(data);
+}
+
+const getById = async (contactId) => {
+    const contacts = await listContacts();
+     const contact = contacts.find(({ id }) => id.toString() === contactId);
+  return contact;
+}
 
 const removeContact = async (contactId) => {}
 
@@ -13,7 +23,7 @@ const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,48 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const Contacts = require('../../model/index')
+// const getAll = async () => contactsPath;
+
+router.get('/', async (_req, res, next) => {
+  try {
+    const contact = await Contacts.listContacts();
+    return res.json({
+      // status: "Success",
+      code: 200,
+      // message: "Contacts found",
+      data: {
+        contact,
+      },
+    })
+  }
+  catch(error) {
+    next(error)
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  // res.json({ message: 'template message' })
+  try {
+    const contact = await Contacts.getById(req.params.contactId);
+    if (contact) {
+      return res.json({
+        code: 200,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return res.status(400).json
+        ({
+        code: 404,
+        message: 'Not Found',
+        })
+      }
+      }
+    catch(error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
@@ -21,4 +57,8 @@ router.patch('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-module.exports = router
+ module.exports = router
+
+// module.exports = {
+//   listContacts
+// }
