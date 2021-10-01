e9086f4c178c34f41865473eba6f668d0b0386c6
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-10-01 21:36:52 +0300

    все

---
+++ routes/api/validation.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+
+const schemaContact = Joi.object({
+    
+            "name": Joi.string().alphanum().min(1).max(20).required(),
+            "email": Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+            "phone":  Joi.string().alphanum().min(10).max(17).required(),
+        
+})
+
+const validate = async(schema, obj, res, next) => {
+    try {
+        await schema.validateAsync(obj)
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ status: 'error', code: 400, message:error.message})
+    }
+}
+
+module.exports.validateContact = async(req, res, next) => {
+    return await validate(schemaContact, req.body, res, next)
+}
+
+// -> { value: {}, error: '"username" is required' }
+
+// Also -
