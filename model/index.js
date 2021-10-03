f679e7aaf70c212f96d46d615aa84f5ec8d0fa00
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-10-03 10:54:42 +0200

    зробив дз

--- model/index.js
+++ model/index.js
@@ -1,15 +1,47 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const contacts = require("./contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return contacts;
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const findContactById = contacts.find(({ id }) => id === contactId);
+  return findContactById;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const index = contacts.findIndex(({ id }) => id === Number(contactId));
+  let isFindedContact;
+  if (index === -1) {
+    isFindedContact = false;
+    return isFindedContact;
+  }
+  contacts.splice(index, 1);
+  isFindedContact = true;
+  return isFindedContact;
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const indexOfLastContact = contacts.length - 1;
+  const idOfLastContact = contacts[indexOfLastContact].id;
+  const newContact = { ...body, id: idOfLastContact + 1 };
+  contacts.push(newContact);
+  return newContact;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const index = contacts.findIndex(({ id }) => id === Number(contactId));
+  let isFindedContact;
+  if (index === -1) {
+    isFindedContact = false;
+    return { isFindedContact };
+  }
+  isFindedContact = true;
+  const updatedContact = { ...contacts[index], ...body };
+  contacts[index] = updatedContact;
+  return { updatedContact, isFindedContact };
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +49,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
