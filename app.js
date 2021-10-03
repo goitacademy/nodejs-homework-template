dc36a8068677c28782ccf2f217a56622aaa98fc6
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-10-03 13:40:23 +0300

    mongoDB

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const app = express()
+const mongoose = require("mongoose");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+require("dotenv").config();
+const { DB_Host } = process.env;
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+mongoose
+  .connect(DB_Host, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
-app.use('/api/contacts', contactsRouter)
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message })
-})
+  res.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = app;
