e7fc8cf07847af52dbf4d80bdf968695647b555f
Olechka Moiseenko <olechka.moiseenko@gmail.com>
2021-10-04 11:01:36 +0300

    post contact is worked

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -40,7 +40,18 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contactsOperations.addContact(req.body)
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        result
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
