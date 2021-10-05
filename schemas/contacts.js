29ab3b614da9c2fe84b2e6cdce9d6ddf16d1281d
dimamarjan <dima.marjan@gmail.com>
2021-10-05 15:36:01 +0300

    fix

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,15 +1,32 @@
 const { Schema, model } = require('mongoose')
 
 const contactSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
   email: {
     type: String,
     trim: true,
     lowercase: true,
   },
-  phone: String,
-  versionKey: false
-}, { versionKey: false })
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  versionKey: false,
+  toJSON: {
+    virtuals: true,
+    transform: function (_, ret) {
+      delete ret._id
+      return ret
+    }
+  }
+})
 
 const Contact = model('contact', contactSchema)
 
