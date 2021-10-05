3eb778565e4db53db32577a493d4c90dc5494c4c
oksanastezhko <oksana_stegko@ukr.net>
2021-10-05 16:47:20 +0300

    рабочий вариант

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,14 +3,18 @@ const { Contact } = require('../model')
 const { NotFound } = require('http-errors')
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find({}, '_id name email phone favorite owner')
+  const result = await Contact.find(
+    { owner: req.user._id },
+    '_id name email phone favorite'
+  )
   sendSuccess(res, { result })
 }
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params
-  const result = await Contact.findById(
-    contactId,
+  const result = await Contact.findOne(
+    { _id: contactId, owner: req.user._id },
+
     '_id name email phone favorite'
   )
   if (!result) {
@@ -20,7 +24,6 @@ const getContactById = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-  console.log('req:', req.body, req.user)
   const newContact = { ...req.body, owner: req.user._id }
   const result = await Contact.create(newContact)
   sendSuccess(res, { result }, 201)
@@ -28,9 +31,13 @@ const addContact = async (req, res) => {
 
 const updateContactById = async (req, res) => {
   const { contactId } = req.params
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
-    new: true,
-  })
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner: req.user._id },
+    req.body,
+    {
+      new: true,
+    }
+  )
 
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} not found!`)
@@ -40,7 +47,10 @@ const updateContactById = async (req, res) => {
 
 const removeContactById = async (req, res, next) => {
   const { contactId } = req.params
-  const result = await Contact.findByIdAndDelete(contactId)
+  const result = await Contact.findOneAndRemove({
+    _id: contactId,
+    owner: req.user._id,
+  })
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} not found!`)
   }
@@ -50,8 +60,8 @@ const removeContactById = async (req, res, next) => {
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params
   const { favorite } = req.body
-  const result = await Contact.findByIdAndUpdate(
-    contactId,
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner: req.user._id },
     { favorite },
     { new: true }
   )
