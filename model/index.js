be2ca8bb25eb498c989936ec077655bea5be8bf6
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-10-05 20:25:22 +0300

    save

--- model/index.js
+++ model/index.js
@@ -1,20 +1,60 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises');
+const contacts = require('./contacts.json');
+const { v4 } = require('uuid');
+const path = require('path');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => contacts;
 
-const removeContact = async (contactId) => {}
+const getContactById = async id => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(contact => contact.id.toString() === id);
+  if (idx === -1) {
+    return null;
+  }
+  return contacts[idx];
+};
 
-const addContact = async (body) => {}
+const removeContact = async id => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id.toString() === id);
+  if (idx === -1) {
+    return null;
+  }
+  contacts.splice(idx, 1);
+  await updateContacts(contacts);
+  return true;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async data => {
+  const newContact = { ...data, id: v4() };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+};
+
+const updateContacts = async contacts => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+};
+
+const updateById = async (id, data) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id.toString() === id);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { ...contacts[idx], ...data };
+  await updateContacts(contacts);
+  return contacts[idx];
+};
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+  updateContacts,
+  updateById,
+};
