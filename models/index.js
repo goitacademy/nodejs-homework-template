21dea600e7a64d9c0f4ecff3a27bb43df8bb2b13
marynazakvatska <maryna.zakvatska@gmail.com>
2021-10-06 00:53:43 +0200

    delete owner

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 const {Contact} = require("./contact")
 const { User } = require("./users");
-const {Owner} = require("./owner")
 
 module.exports = {
     Contact,
-    User,
-    Owner
+    User
+
 }
