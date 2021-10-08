00b2dcc455a90a534703975f01c57d34c17f69da
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-10-09 02:08:42 +0300

    Add remove contact by ID

--- model/index.js
+++ model/index.js
@@ -12,7 +12,16 @@ const getContactById = async (contactId) => {
   return contact;
 };
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const contacts = await db.read();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const [result] = contacts.splice(index, 1);
+    await db.write(contacts);
+    return result;
+  }
+  return null;
+};
 
 const addContact = async (body) => {
   const contacts = await db.read();
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -30,7 +30,19 @@ router.post("/", async (req, res, next) => {
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId);
+    if (contact) {
+      return res
+        .status(200)
+        .json({ status: "success", code: 200, data: { contact } });
+    }
+    return res
+      .status(404)
+      .json({ status: "error", code: 404, message: "Not Found" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
