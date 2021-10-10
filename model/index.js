2854eed44db6fb69f36f62905b7ed201b04b1b20
Halyna Sinko <galinasinko14@gmail.com>
2021-10-10 17:14:41 +0300

    write function

--- model/index.js
+++ model/index.js
@@ -1,15 +1,48 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const crypto = require("crypto");
+const DB = require("./db");
+const db = new DB("./contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return await db.read();
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await db.read();
+  const [contact] = contacts.filter((contact) => contact.id === contactId);
+  return contact;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contacts = await db.read();
+  const filterContacts = contacts.filter((contact) => contact.id !== contactId);
+  if (contacts.length !== filterContacts.length) {
+    await db.write(filterContacts);
+    return true;
+  }
+  return null;
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts = await db.read();
+  const newContact = {
+    id: crypto.randomUUID(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await db.write(contacts);
+  return newContact;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await db.read();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    contacts[index] = { ...contacts[index], ...body };
+    await db.write(contacts);
+    return contacts[index];
+  }
+  return null;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +50,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
