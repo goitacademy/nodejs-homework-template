82aabc597ec443ce9d8bbaff51e72e34253218e4
ichyryk <irachyryk2@gmail.com>
2021-10-11 18:42:21 +0300

    Add patch to auth routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,12 +3,15 @@ const express = require('express')
 const { joiSchema } = require('../../models/user')
 const { controllerWrapper, validation, authenticate } = require('../../middlewares')
 const { auth: ctrl } = require('../../controllers')
+const upload = require('../../middlewares/upload')
 
 const router = express.Router()
 
 router.post('/signup', validation(joiSchema), controllerWrapper(ctrl.signup))
 
 router.post('/login', validation(joiSchema), controllerWrapper(ctrl.login))
+
+router.patch('/avatars', authenticate, upload.single('avatar'), controllerWrapper(ctrl.avatars))
 
 router.post('/logout', authenticate, controllerWrapper(ctrl.logout))
 
