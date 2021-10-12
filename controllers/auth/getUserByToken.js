d2e5ca256d9219f8dcfe4063fc66a7cfef080e98
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-10-12 23:12:27 +0300

    редактирует получение текущего пользователя

--- controllers/auth/getUserByToken.js
+++ controllers/auth/getUserByToken.js
@@ -5,11 +5,20 @@ const getUserByToken = async (req, res, next) => {
   console.log('token :>> ', token)
   const user = await User.findOne({ token })
   console.log('user :>> ', user)
+  if (!user) {
+    res.status(401).json({
+      status: 'error',
+      code: 401,
+      message: 'Not authorized'
+    })
+  }
+  const { email, subscription } = user
   res.status(200).json({
     status: 'success',
     code: 200,
     data: {
-      user
+      email,
+      subscription
     }
   })
 }
