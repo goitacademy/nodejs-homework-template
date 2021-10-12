1f6d74ba6127db9218cdb5316ad90185c870485a
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-10-12 16:52:10 +0300

    Adds pagination (with mongoose). Additional.

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,8 +2,11 @@ const { Contact } = require('../../models');
 const { sendSuccessfulRes } = require('../../helpers');
 
 const listContacts = async (req, res) => {
+    const { page = 1, limit = 20 } = req.query;
+    console.log(req.query);
+    const skip = (page - 1) * limit;
     const { _id } = req.user;
-    const result = await Contact.find({ owner: _id }, '_id name email phone favorite');
+    const result = await Contact.find({ owner: _id }, '_id name email phone favorite', {skip, limit: +limit});
     sendSuccessfulRes(res, { result });
 };
 
