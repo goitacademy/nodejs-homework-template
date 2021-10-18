c1356314d1b5a065b62da3adf210c506e245c0c3
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-10-18 22:15:04 +0300

    add pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,9 @@
 const { Contact } = require('../../models/contact')
 
 const getAll = async (req, res, next) => {
-  const result = await Contact.find({}, '_id, name , email , phone , owner')
+  const { page = 1, limit = 20 } = req.query
+  const skip = (page - 1) * limit
+  const result = await Contact.find({}, '_id, name , email , phone , owner', { skip, limit: +limit })
   res.json({
     status: 'success',
     code: 200,
--- controllers/contacts/getAllByOwner.js
+++ controllers/contacts/getAllByOwner.js
@@ -1,8 +1,10 @@
 const { Contact } = require('../../models/contact')
 
 const getAllByOwner = async (req, res, next) => {
+  const { page = 1, limit = 10 } = req.query
+  const skip = (page - 1) * limit
   const { _id } = req.user
-  const result = await Contact.find({ owner: _id }, '_id, name , email , phone , owner')
+  const result = await Contact.find({ owner: _id }, '_id, name , email , phone , owner', { skip, limit: +limit })
   res.json({
     status: 'success',
     code: 200,
