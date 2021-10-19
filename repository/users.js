aa0eb6f07a9de71c4afcdb5582a208f33a7f02ee
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-10-19 22:30:37 +0300

    Fix registration

--- repository/users.js
+++ repository/users.js
@@ -8,12 +8,14 @@ const findByEmail = async (email) => {
   return await User.findOne({ email });
 };
 
-const create = async (options) => {
-  const user = new User(options);
+const createUser = async (credentials) => {
+  const user = new User(credentials);
+
   return await user.save();
 };
 
 const updateToken = async (id, token) => {
   return await User.updateOne({ _id: id }, { token });
 };
-module.exports = { findById, findByEmail, create, updateToken };
+
+module.exports = { findById, findByEmail, createUser, updateToken };
