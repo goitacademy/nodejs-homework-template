0d744c65d20399f83e71102bbd75cf81fc8ad7f0
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-10-22 11:10:45 +0300

    Add functions for get and post methods

--- model/index.js
+++ model/index.js
@@ -1,13 +1,53 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
+const path = require('path')
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join('model', 'contacts.json')
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath)
+    return JSON.parse(data.toString())
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath)
+
+    const structuredData = JSON.parse(data.toString())
+    const [contact] = structuredData.filter((el) => {
+      return el.id === Number(contactId)
+    })
+    return ({ contact })
+  } catch (err) {
+    console.log(err.message)
+  }
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  try {
+    const data = await fs.readFile(contactsPath)
+
+    const structuredData = JSON.parse(data.toString())
+
+    const newId = Number(Date.now().toString())
+    const newContact = { id: newId, name: body.name, email: body.email, phone: body.phone }
+
+    structuredData.push(newContact)
+
+    fs.writeFile(contactsPath, JSON.stringify(structuredData, null, 2))
+
+    return (newContact)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const { listContacts, getContactById, addContact } = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const data = await listContacts()
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result: data
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  try {
+    const data = await getContactById(contactId)
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result: data
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  // res.json({ message: 'template message' })
+  const { name, email, phone } = req.body
+  try {
+    const data = await addContact({ name, email, phone })
+    res.json({
+      status: 'success',
+      code: 201,
+      data: {
+        result: data
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
