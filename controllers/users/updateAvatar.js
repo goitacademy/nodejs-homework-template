28b4e5349d5b94bacc694435b8574b875963cc6d
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-10-22 23:38:49 +0300

    added avatars update

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -7,16 +7,19 @@ const updateAvatar = async(req, res, next) => {
     const { path: tempDir, originalname } = req.file;
     const [extension] = originalname.split(".").reverse();
     const filename = `${_id}.${extension}`;
-    const uploadDir = path.join(__dirname, "../../", "public\\avatars", filename);
+    const uploadDir = path.join(__dirname, "../../", "public//avatars", filename);
 
     try {
         await fs.rename(tempDir, uploadDir);
+
         const image = path.join("avatars", filename);
+
         await User.findByIdAndUpdate(_id, { avatarURL: image });
         res.json({
             status: "success",
             code: 201,
             message: "Update avatar success",
+            avatarURL: image,
         });
     } catch (error) {
         await fs.unlink(tempDir);
