443cdd0262328ae5d3c6b390094c79fb1da68fee
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-10-23 16:02:51 +0300

    Убрал логи

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(express.static('public'));
 app.use(helmet());
-app.use(logger(formatsLogger));
+app.get('env') !== 'test' && app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json({ limit: 10000 }));
 app.use(boolParser());
