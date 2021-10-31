e935cd088d79e6958ca021d15d3ae2c4ec1dd167
Hennadii_Mariiev <marieff1990@gmail.com>
2021-10-31 11:57:07 +0200

    remove all .js files

--- controller/getContacts.js
+++
@@ -1,9 +0,0 @@
-const {Contact} = require('../model/contactSchema');
-
-const getContacts = async (req, res, next) => {
-  const contacts = await Contact.find();
-
-  res.status(200).json({message: 'success', data: {contacts}});
-};
-
-module.exports = getContacts;
