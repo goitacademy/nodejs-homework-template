29fe4189128ca969c1c52969e9e7a9a4d4f1afc4
Dima <75967292+DukaDima@users.noreply.github.com>
2021-11-01 09:00:03 +0200

    оптимизирует папку роутер

---
+++ routes/api/contacts/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const contactsControllers = require("../../../controllers/contacts");
+
+/**
+ * 1.Получить все контакты
+ * 2.Получить один контакт по id
+ * 3.Добавить новый контакт
+ * 4.Удалить контакт по id
+ * 5.Обновить контакт по id
+ */
+
+router.get("/", contactsControllers.listContactsController);
+
+router.get("/:contactId", contactsControllers.getContactByIdController);
+
+router.post("/", contactsControllers.addContactController);
+
+router.delete("/:contactId", contactsControllers.removeContactController);
+
+router.put("/:contactId", contactsControllers.updateByIdController);
+
+module.exports = router;
