a4396687147e7c438b5010604c3e61842823a30e
unknown <74738757+leonid-br@users.noreply.github.com>
2021-11-04 23:51:35 +0300

    hw3 test

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
-const app = require('../app')
+// const app = require('../app')
 
-const { PORT = 3000 } = process.env
+// const { PORT = 3000 } = process.env
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
