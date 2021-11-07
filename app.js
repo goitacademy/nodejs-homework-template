758360a8031a9616465652c6e0b5d2b142916b9f
Anastasia199129 <Nastasia199126@gmail.com>
2021-11-07 22:55:30 +0200

    commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const { DB_HOST } = process.env
+console.log(DB_HOST);
 
 mongoose.connect(DB_HOST).then(() => {
   // app.listen(3000)
