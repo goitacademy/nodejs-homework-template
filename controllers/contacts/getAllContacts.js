7e58cd8340ff3f8ed59570e0b8f57701ce700c23
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-11-11 00:24:13 +0300

    Add simple pagination and filter

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,7 +2,7 @@ const { listContacts } = require('../../model/contacts')
 
 const getAllContacts = async (req, res, next) => {
   const owner = req.user._id
-  const data = await listContacts(owner)
+  const data = await listContacts(owner, req.query)
   res.json({
     status: 'success',
     code: 200,
