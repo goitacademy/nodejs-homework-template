e5b8ea184de17204ff0d9410b4f7398cb79ffaaa
AntonTymchenko <timchenkov_@ukr.net>
2021-11-13 08:02:44 +0200

    hw04
    Добавил плагинацию

--- controllers/getAllContacts.js
+++ controllers/getAllContacts.js
@@ -1,11 +1,14 @@
 const { Contact } = require('../model')
 
 const getAllContacts = async (req, res) => {
+  const { page, limit } = req.query
+  const skip = (page - 1) * limit
   const { _id } = req.user
-  const contacts = await Contact.find({ owner: _id }).populate(
-    'owner',
-    '_id email'
-  )
+  const contacts = await Contact.find(
+    { owner: _id },
+    '_id name email phone favorite owner',
+    { skip, limit: +limit }
+  ).populate('owner', '_id email')
   res.json({
     status: 'success',
     code: 200,
