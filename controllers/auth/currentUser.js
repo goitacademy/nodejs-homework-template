cb2eed7f078a7e9ec2a96bb234b70eb3b5db8257
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2021-11-16 00:37:52 +0200

    getAll

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -3,9 +3,6 @@ const { Unauthorized } = require("http-errors");
 const { User } = require("../../model");
 
 const currentUser = async (req, res) => {
-  if (!req.user) {
-    throw new Unauthorized("Not authorized");
-  }
   const { email, subscription } = req.user;
   res.status(200).json({
     status: "success",
--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,9 +1,15 @@
+const { BadRequest } = require("http-errors");
 const { Contact } = require("../../model");
 
 const getAll = async (req, res) => {
   const { page = 1, limit = 5 } = req.query;
   const { _id } = req.user;
   const skip = page * limit - limit;
+  const pageInt = Number(page);
+  const limitInt = Number(limit);
+  if (isNaN(pageInt) || isNaN(limitInt)) {
+    throw new BadRequest();
+  }
   const data = await Contact.find(
     { owner: _id },
     "_id name email phone owner",
