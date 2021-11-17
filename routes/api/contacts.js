6e1ddb708f3d4e24ba981a29941d7efcdd163239
yuragms <yura.gms@gmail.com>
2021-11-17 14:13:23 +0200

    Revert "Revert "Revert "Revert "Revert "1"""""
    This reverts commit 504015d07060e4cc787148c18e3dea0a9106fc1b.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const listContacts = require("../../model/index.js");
+const listContacts = require("../../model/index");
 
 router.get("/", listContacts);
 
