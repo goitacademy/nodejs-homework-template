1f7c8823a53030ea24ad5a7531a962f28a56e743
yuragms <yura.gms@gmail.com>
2021-11-17 11:21:18 +0200

    prettier

--- model/index.js
+++ model/index.js
@@ -1,15 +1,19 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const contacts = require("./contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contacts, "utf8");
+  const users = JSON.parse(data);
+  return users;
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -17,4 +21,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
