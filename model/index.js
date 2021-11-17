dbf7228208633dc38388cffd54412ce12dab4953
yuragms <yura.gms@gmail.com>
2021-11-17 14:14:41 +0200

    4

--- model/index.js
+++ model/index.js
@@ -2,21 +2,28 @@ const fs = require("fs/promises");
 const contacts = require("./contacts.json");
 // const path = require("path");
 // const contacts = path.resolve("model/contacts.json");
+let posts = [
+  { id: "1", topic: "test1", text: "test text1" },
+  { id: "2", topic: "test2", text: "test text2" },
+  { id: "3", topic: "test3", text: "test text3" },
+];
 
 const listContacts = async () => {
-  try {
-    // const contacts = await Contacts.listContacts();
-    return res.json({
-      status: "success",
-      code: 200,
-      data: {
-        contacts,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  await res.json({ posts });
 };
+// try {
+//   // const contacts = await Contacts.listContacts();
+//   return res.json({
+//     status: "success",
+//     code: 200,
+//     data: {
+//       contacts,
+//     },
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
+// };
 
 const getContactById = async (contactId) => {};
 
