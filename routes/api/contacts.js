c88e60fec04b1f8558765a2bd837ad4911c81826
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-11-18 20:46:32 +0000

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 const listContacts = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
