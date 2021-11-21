55f7f8ed2c0c28c1bdbd4c864fcbe0f057b7f857
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-11-21 15:59:37 +0000

    add ctrl wrap

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ const express = require('express')
 const router = express.Router()
 const { controllerWrapper, validator } = require('../../middlewares')
 const { contactsSchema } = require('../../schemas')
-// const { contacts: ctrl } = require('../../contactsOperations/contacts/contacts')
-const { contacts } = require('../../contactsOperations')
+const { contacts: ctrl } = require('../../contactsOperations')
+// const { contacts } = require('../../contactsOperations')
 
-router.get('/', controllerWrapper(contacts.listContacts))
-router.get('/:contactId', controllerWrapper(contacts.getContactById))
+router.get('/', controllerWrapper(ctrl.listContacts))
+router.get('/:contactId', controllerWrapper(ctrl.getContactById))
 
-router.post('/', validator(contactsSchema), controllerWrapper(contacts.addContact))
+router.post('/', validator(contactsSchema), controllerWrapper(ctrl.addContact))
 
-router.delete('/:contactId', controllerWrapper(contacts.removeContactById))
+router.delete('/:contactId', controllerWrapper(ctrl.removeContactById))
 
 router.put(
   '/:contactId',
   validator(contactsSchema),
-  controllerWrapper(contacts.updateContactById)
+  controllerWrapper(ctrl.updateContactById)
 )
 
 module.exports = router
