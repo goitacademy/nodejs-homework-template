9b922cb26c041f6c59b1aaca1224e164744ffbed
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-11-23 18:49:39 +0000

    change update contacts

--- model/contactsOperations/index.js
+++ model/contactsOperations/index.js
@@ -2,7 +2,7 @@ const listContacts = require('./listContacts')
 const getContactById = require('./getContactById')
 const addContact = require('./addContact')
 const removeContact = require('./removeContact')
-const updateContactById = require('./updateContacts')
+const updateContactById = require('./updateContactById')
 
 module.exports = {
   listContacts,
