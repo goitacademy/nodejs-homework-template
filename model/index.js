3015e4d320c6f617a927d086fc11e06623a17b5a
Kateryna Karpova <katyakarpova@gmail.com>
2021-11-23 15:35:23 +0100

    Update index.js
    Bugfix

--- model/index.js
+++ model/index.js
@@ -51,7 +51,7 @@ const updateContact = async (contactId, body) => {
   if (idx === -1) {
     return null
   }
-  contacts[idx] = { contactId, ...body }
+  contacts[idx] = { id: contactId, ...body }
   await newContacts(contacts)
   return contacts[idx]
 }
