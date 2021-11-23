efa0783425488f5db17f1808193405da37a4e27a
Kateryna Karpova <katyakarpova@gmail.com>
2021-11-23 13:36:53 +0100

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,15 +1,60 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const path = require('path')
+const { v4 } = require('uuid')
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, 'contacts.json')
 
-const removeContact = async (contactId) => {}
+const newContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts))
+}
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath)
+  const allContacts = JSON.parse(data)
+  return allContacts
+}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts()
+  const contactById = contacts.find(
+    (contact) => String(contact.id) === contactId
+  )
+  if (!contactById) {
+    return null
+  }
+  return contactById
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts()
+  const idx = contacts.findIndex((contact) => String(contact.id) === contactId)
+  if (idx === -1) {
+    return null
+  }
+  const updatedContacts = contacts.filter((_, index) => index !== idx)
+  await newContacts(updatedContacts)
+  return contacts[idx]
+}
+
+const addContact = async (body) => {
+  const contacts = await listContacts()
+  const newContact = { id: v4(), ...body }
+  contacts.push(newContact)
+  await newContacts(contacts)
+  return newContact
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts()
+  const idx = contacts.findIndex((contact) => String(contact.id) === contactId)
+  if (idx === -1) {
+    return null
+  }
+  contacts[idx] = { contactId, ...body }
+  await newContacts(contacts)
+  return contacts[idx]
+}
 
 module.exports = {
   listContacts,
