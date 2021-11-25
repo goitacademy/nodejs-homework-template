90231aefe9b1c41383886987a6b59962e7425f79
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-11-25 11:58:26 +0200

    'add env and db connection'

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const contactsRouter = require("./routes/api/contacts");
 
-const app = express();
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+const app = express("");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
