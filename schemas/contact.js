8fe00d76308ecaf2baa65154ca0ee321d429d322
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-11-25 14:56:55 +0100

    Update contact.js

--- schemas/contact.js
+++ schemas/contact.js
@@ -3,11 +3,7 @@ const Joi = require('joi')
 const contactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  phone: Joi.number()
-    .integer()
-    .min(10 ** 9)
-    .max(10 ** 10 - 1)
-    .required(),
+  phone: Joi.number().min(0.01).required(),
 })
 
 module.exports = contactSchema
