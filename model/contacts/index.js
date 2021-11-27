091bd0043a993b1a72a809ffd02ea0e12e351600
Wences81 <zhylinpochta@gmail.com>
2021-11-27 15:44:50 +0100

    hw2-start

---
+++ model/contacts/index.js
@@ -0,0 +1,18 @@
+
+const listContacts = require('./listContacts')
+
+const getContactById = require('./getContactById')
+
+const removeContact = require('./removeContact')
+
+const addContact = require('./addContact')
+
+const updateContactById = require('./updateContactById')
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactById,
+}
