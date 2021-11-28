b378bf025869c44af3be61015352dcfc5a174428
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-11-28 21:15:46 +0100

    in-process1

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 const app = require('../app')
 
-const { PORT = 3001 } = process.env
+const { PORT = 3030 } = process.env
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const contactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.number().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
   phone: Joi.string().required()
 })
 
