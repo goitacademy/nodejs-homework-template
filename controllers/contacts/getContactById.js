78b3ecda0f25ec1077321d9b6a21f2f25743fdf5
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2021-11-29 21:03:10 +0200

    Удалил консоль лог.

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -5,7 +5,6 @@ const { Contact } = require("../../models");
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findById(contactId); // Contact.findOne(поиск по параметрам).
-  console.log(result);
   if (!result) {
     const error = new Error(`Contact whith id=${contactId} not found.`);
     error.status = 404;
