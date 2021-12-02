f90e9a1fb8bc91ce1986e099c2e7b18e504a93bd
Oleksii83 <zaaytav@gmail.com>
2021-12-02 21:09:47 +0200

    delete commit

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,32 +1,17 @@
-const { Contact } = require('../../model')
+const { Contact } = require("../../model");
 
 const getAll = async (req, res, next) => {
-  // const { page, limit } = req.query;
-  // const skip = (page - 1) * limit;
   try {
-    const { _id } = req.user
+    const { _id } = req.user;
     const result = await Contact.find(
       { owner: _id },
-      '_id name email phone favorite owner'
+      "_id name email phone favorite owner"
       // { skip, limit: +limit }
-    ).populate('owner', '_id email')
-    res.json(result)
+    ).populate("owner", "_id email");
+    res.json(result);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-// const getAll = async (req, res) => {
-//   const { _id } = req.user;
-
-//   const result = await Contact.find({ owner: _id });
-//   res.json({
-//     status: "success",
-//     code: 200,
-//     data: {
-//       result,
-//     },
-//   });
-// };
-
-module.exports = getAll
+module.exports = getAll;
--- controllers/contacts/post.js
+++ controllers/contacts/post.js
@@ -1,27 +1,20 @@
-
-// const gravatar = require("gravatar");
-// const fs = require("fs/promises");
-// const path = require("path");
-const { Contact } = require('../../model')
-
-// const contactsDir = path.join(__dirname, "../../public/avatar");
+const { Contact } = require("../../model");
 
 const postContact = async (req, res, next) => {
   try {
-    const newProduct = { ...req.body, owner: req.user._id }
-    const result = await Contact.create(newProduct)
-    // const contactsFolder = path.join(contactsDir, String(result._id));
-    // await fs.mkdir(contactsFolder);
+    const newProduct = { ...req.body, owner: req.user._id };
+    const result = await Contact.create(newProduct);
+
     res.status(201).json({
-      status: 'success',
+      status: "success",
       code: 201,
       data: {
         result,
       },
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-module.exports = postContact
+module.exports = postContact;
