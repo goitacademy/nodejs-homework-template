61f9d9dbbeac2cdfc5bc07a8a88d4eff25cd4010
Kseniia-FS <snabukraine@ukr.net>
2021-12-06 16:18:08 +0200

    Added classes for controllers

---
+++ classControllers/contacts/Contacts.js
@@ -0,0 +1,84 @@
+const { HTTP400Error, HTTP404Error } = require("../../helpers/errorHandlers");
+const {
+  listContacts,
+  getContactsByQuery,
+  addContact,
+  updateContact,
+  removeContact,
+} = require("../../services/contacts");
+
+class Contacts {
+  async getContactsList(req, res) {
+    if (req.query.favorite) {
+      const result = await getContactsByQuery("favorite");
+      return res.json({ data: result, status: "success", code: 200 });
+    }
+    const { page, limit } = req.query;
+    const skip = (page - 1) * limit;
+    const { _id } = req.user;
+    const result = await listContacts(_id, skip, limit);
+
+    return res.json({ data: result, status: "success", code: 200 });
+  }
+
+  async getContactById(req, res) {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
+    if (!result) {
+      throw new HTTP400Error("Bad Request, change query parameters");
+    }
+    return res.json({ data: result, status: "success", code: 200 });
+  }
+
+  async addContact(req, res) {
+    const { _id } = req.user;
+    console.log(_id);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+      return res.json({ message: "missing required  field" });
+    }
+    const result = await addContact({ name, email, phone, _id });
+
+    return res.status(201).json({ data: result, status: "success", code: 201 });
+  }
+
+  async changeContact(req, res) {
+    const { contactId } = req.params;
+    const { name, email, phone } = req.body;
+
+    if (!name || !email || !phone) {
+      return res.json({
+        message: "missing fields",
+        status: "failed",
+        code: 400,
+      });
+    }
+
+    const result = await updateContact(contactId, {
+      name,
+      email,
+      phone,
+    });
+    if (!result) {
+      throw new HTTP404Error(`There is no such contact with id: ${contactId}`);
+    }
+    return res.json({ data: result, status: "success", code: 200 });
+  }
+
+  async deleteContact(req, res) {
+    const { contactId } = req.params;
+    const removedContact = await removeContact(contactId);
+
+    if (!removedContact) {
+      throw new HTTP404Error(`There is no such contact with id: ${contactId}`);
+    }
+
+    return res.json({
+      message: "contact deleted",
+      status: "success",
+      code: 200,
+    });
+  }
+}
+
+module.exports = Contacts;
--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -6,23 +6,29 @@ const {
 const { tokenValidation } = require("../../middlewares/auth");
 const { contacts: ctrl } = require("../../controllers");
 const ctrlWrapper = require("../../middlewares/ctrtWrapper");
+const Contacts = require("../../classControllers/contacts/Contacts");
+const contacts = new Contacts();
 
 const router = express.Router();
 
-router.get("/", tokenValidation, ctrlWrapper(ctrl.getContactsList));
+router.get("/", tokenValidation, ctrlWrapper(contacts.getContactsList));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getContactByIdHandler));
+router.get("/:contactId", ctrlWrapper(contacts.getContactById));
 
 router.post(
   "/",
   tokenValidation,
   conatctBodyValidation,
-  ctrlWrapper(ctrl.postContact)
+  ctrlWrapper(contacts.addContact)
 );
 
-router.delete("/:contactId", ctrlWrapper(ctrl.deleteContact));
+router.delete("/:contactId", ctrlWrapper(contacts.deleteContact));
 
-router.put("/:contactId", conatctBodyValidation, ctrlWrapper(ctrl.putContact));
+router.put(
+  "/:contactId",
+  conatctBodyValidation,
+  ctrlWrapper(contacts.changeContact)
+);
 
 router.patch(
   "/:contactId/favorite",
