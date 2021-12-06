8a9cab5f96de82f35e2e5191ad848df01b013ac2
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-12-06 23:00:16 +0200

    fix owner

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,10 @@ const { Contact } = require('../../models');
 
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const contacts = await Contact.find({ owner: _id });
+  const contacts = await Contact.find({ owner: _id }).populate(
+    'owner',
+    '_id name email subscription',
+  );
   res.json({
     status: 'success',
     code: 200,
