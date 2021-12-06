fce45becd1fa6faf30b3b9d8ef6f852a6afb113b
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-12-07 00:02:53 +0200

    пагинация

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,10 +2,15 @@ const { Contact } = require('../../models');
 
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const contacts = await Contact.find({ owner: _id }).populate(
+
+  const { page = 1, limit = 3 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const contacts = await Contact.find({ owner: _id }, '', { skip, limit: Number(limit) }).populate(
     'owner',
     '_id name email subscription',
   );
+
   res.json({
     status: 'success',
     code: 200,
