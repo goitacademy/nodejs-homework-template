eb9c5ee2d8e65dee0fdf2a23c9b713e66f4dc6a2
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2021-12-09 21:16:00 +0200

    Устранил ошибку-баг в middlewares/auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,13 +8,13 @@ const { SECRET_KEY } = process.env;
 const auth = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
-    const error = new Error("Not authorized.");
-    error.status = 401;
-    // or use package "http-errors"
-    throw error;
-  }
   try {
+    if (bearer !== "Bearer") {
+      const error = new Error("Not authorized.");
+      error.status = 401;
+      // or use package "http-errors"
+      throw error;
+    }
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) {
