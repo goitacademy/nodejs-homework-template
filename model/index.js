7f81ccfe91256fc6c649c4f2218f72dfcd2ca9e9
Vladyslav Sukachev <79707548+flamesty@users.noreply.github.com>
2021-12-11 16:52:47 +0200

    fix

--- model/index.js
+++ model/index.js
@@ -1,15 +1,15 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
+const listContacts = async () => { }
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => { }
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => { }
 
-const addContact = async (body) => {}
+const addContact = async (body) => { }
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => { }
 
 module.exports = {
   listContacts,
@@ -18,3 +18,4 @@ module.exports = {
   addContact,
   updateContact,
 }
+
