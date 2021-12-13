fcc215fca0034f09b0a9a7eef2505875aadb1ab5
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-12-13 13:56:48 +0200

    start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'changing message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
