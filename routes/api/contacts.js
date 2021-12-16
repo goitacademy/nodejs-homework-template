3429a26da3470baac55b80843a999cd12bd5994d
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-12-16 20:51:49 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,61 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const Contacts = require('../../model/index.js');
+const { validateContact, validateId } = require('./validation.js');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    console.log(req.method);
+    const contacts = await Contacts.listContacts();
+    res.json({ status: 'success', code: 200, data: { contacts } });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', validateId, async (req, res, next) => {
+  try {
+    const contact = await Contacts.getContactById(req.params.id);
+    if (contact) {
+      return res
+        .status(200)
+        .json({ status: 'success', code: 200, data: { contact } });
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not Found' });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.addContact(req.body);
+    res.status(201).json({ status: 'success', code: 201, data: { contact } });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', validateId, async (req, res, next) => {
+  try {
+    const contact = await Contacts.removeContact(req.params.id);
+    if (contact) {
+      return res
+        .status(200)
+        .json({ status: 'success', code: 200, data: { contact } });
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not Found' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'Home Work is done' });
+});
 
-module.exports = router
+module.exports = router;
