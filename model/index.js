4f1ee459f87b39da6b9aac54e17d90df7bb96112
AlekseyDibikov <79219724+Aleksey-Dibikov@users.noreply.github.com>
2021-12-18 01:25:31 +0200

    adds homework02 solution

--- model/index.js
+++ model/index.js
@@ -1,20 +1,63 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+import fs from 'fs/promises';
+import path from 'path';
+import { randomUUID } from 'crypto';
+import contacts from './contacts.json';
+import { fileURLToPath } from 'url';
 
-const listContacts = async () => {}
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+    return contacts;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+    const [contact] = contacts.filter(contact => contact.id === contactId);
+    return contact;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const removeId = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (removeId !== -1) {
+    const [newResult] = contacts.splice(removeId, 1);
+    await fs
+          .writeFile(path.join(__dirname, 'contacts.json'),
+            JSON.stringify(contacts, null, 2));
+    return newResult;
+  }
+  return null;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async ({name, email, phone}) => {
+    const newContact = { id: randomUUID(), name, email, phone };
+    contacts.push(newContact);
+    await fs
+        .writeFile(path.join(__dirname, 'contacts.json'),
+          JSON.stringify(contacts, null, 2));
+    return newContact;
+};
 
-module.exports = {
+const updateContact = async (contactId, body) => {
+  const removeId = contacts.findIndex(
+  (contact) => contact.id === contactId
+  );
+  if (removeId !== -1) {
+    const updatedContact = { id: contactId, ...contacts[removeId],...body };
+    contacts[removeId] = updatedContact;
+    await fs
+          .writeFile(path.join(__dirname, 'contacts.json'),
+            JSON.stringify(contacts, null, 2));
+    return updatedContact;
+  }
+  return null;
+}
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+  updateContact
+};
+
