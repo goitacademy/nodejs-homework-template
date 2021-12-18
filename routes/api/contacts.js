a5fada0a0a4bbc42553c7964c05d1026f0dd0e7b
AleksDu <aleksandr.dyshkin@gmail.com>
2021-12-18 16:22:12 +0200

    the end

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,43 @@
-const express = require('express')
-const router = express.Router()
+import { Router } from "express";
+import model from "../../model/index";
+import { validateCreate, validateUpdate, validateId } from "../validation";
+const router = new Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", validateId, async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getContactById(id);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
+  res.status(404).json({ message: "Not found" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateCreate, async (req, res, next) => {
+  const newContact = await model.addContact(req.body);
+  res.status(201).json(newContact);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", validateId, async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.removeContact(id);
+  if (contact) {
+    return res.status(200).json({ message: "contact deleted!" });
+  }
+  res.status(404).json({ message: "Not found" });
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:id", validateId, validateUpdate, async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.updateContact(id, req.body);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
+  res.status(404).json({ message: "Not found" });
+});
 
-module.exports = router
+export default router;
