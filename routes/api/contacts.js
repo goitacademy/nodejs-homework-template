d4f95b089ca2f39399d87c1b2715bb86bd84a7d7
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-12-19 21:16:13 +0200

    hw-2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,46 @@
-const express = require('express')
-const router = express.Router()
+import { Router } from "express";
+import model from "../../model/index";
+import { validateCreate, validateUpdate, validateId } from "../validation";
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = new Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getContactById(id);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateCreate, async (req, res, next) => {
+  const newContact = await model.addContact(req.body);
+  res.status(201).json(newContact);
+});
 
-module.exports = router
+router.delete("/:id", validateId, async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.removeContact(id);
+  if (contact) {
+    return res.status(200).json({ message: "contact deleted" });
+  }
+
+  res.status(404).json({ message: "Not found" });
+});
+
+router.put("/:id", validateId, validateUpdate, async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.updateContact(id, req.body);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
+
+  res.status(404).json({ message: "Not found" });
+});
+export default router;
