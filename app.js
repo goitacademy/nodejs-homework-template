a3c86b7d10d6e13949ab275e94ed5160da1fdf81
axer4 <79149727+axer4@users.noreply.github.com>
2021-12-19 14:54:47 +0200

    finnita

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
 
-const contactsRouter = require('./routes/api/contacts')
+import contactsRouter from './routes/api/contacts'
 
 const app = express()
 
@@ -22,4 +22,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+export default app
--- model/index.js
+++ model/index.js
@@ -1,20 +1,52 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+import fs from 'fs/promises'
+import path, { dirname } from 'path'
+import { randomUUID } from 'crypto'
+import contacts from './contacts.json'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const listContacts = async () => {}
+const listContacts = async () =>  {
+  return contacts
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) =>  {
+    const [result] = contacts.filter(contact => contact.id === contactId)
+    return result
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const index = contacts.findIndex((contact) => contact.id === contactId)
+  if (index !== -1) {
+   const [result] = contacts.splice(index, 1)
+    await fs.writeFile(path.join(__dirname, 'contacts.json'),
+      JSON.stringify(contacts,null,2)
+    )
+    return result
+  }
+  return null
+}
 
-const addContact = async (body) => {}
+const addContact = async ({ name, email, phone }) =>  {
+    const newContact = { name, email, phone, id:randomUUID() }
+    contacts.push(newContact)
+    await fs.writeFile(path.join(__dirname, 'contacts.json'), JSON.stringify(contacts, null, 2))
+    return newContact
 
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+}
+const updateContact = async (contactId, body) => {
+   const index = contacts.findIndex((contact) => contact.id === contactId)
+  if (index !== -1) {
+    const updateContact = {
+      id: contactId, ...contacts[index], ...body
+    }
+    contacts[index] = updateContact
+    await fs.writeFile(path.join(__dirname, 'contacts.json'),
+      JSON.stringify(contacts,null,2)
+    )
+    return updateContact
+  }
+  return null
+}
+export default  {
+    listContacts, getContactById, removeContact, addContact,updateContact
 }
