bcd3137029f52b9c440f3ec4d79039913eab0fe2
IhorHrytsenko <gisyshka@gmail.com>
2021-12-20 20:13:07 +0200

    fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
 
-const contactsRouter = require('./routes/api/contacts')
+import contactsRouter from './routes/api/contacts'
 
 const app = express()
 
@@ -10,7 +10,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) // json
+app.use(express.urlencoded({ extended: false })) // forms
 
 app.use('/api/contacts', contactsRouter)
 
@@ -22,4 +23,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+export default app
