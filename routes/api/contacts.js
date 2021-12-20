e5ae4f300d0ae7dfd9e8e58e5ba94f01da9fc45a
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2021-12-20 22:30:05 +0200

    add code

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,43 @@
-const express = require('express')
-const router = express.Router()
+import { Router } from 'express';
+const router = new Router();
+import model from '../../model/index';
+import { validateCreate, validateUpdate, validateId } from './validation';
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getById(id);
+  if (contact) {
+    return res.status(200).json(contact);
+  };
+  res.status(404).json({ message: 'not found' });
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validateCreate, async (req, res, next) => {
+  const newContact = await model.addContact(req.body);
+  res.status(201).json(newContact);
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', validateId, async (req, res, next) => {
+  const { id } = req.params;
+  const deletedContact = await model.removeContact(id);
+  if (deletedContact) {
+    res.status(200).json({ message: 'contact deleted' });
+  }
+  res.status(404).json({ message: 'Not found' });
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:id', validateUpdate, async (req, res, next) => {
+  const { id } = req.params;
+  const updatedContact = await model.updateContact(id, req.body);
+  if (updatedContact) {
+    return res.status(200).json(updatedContact);
+  };
+  res.status(404).json({ message: 'not found' });
 })
 
-module.exports = router
+export default router;
---
+++ routes/api/validation.js
@@ -0,0 +1,56 @@
+import Joi from 'joi';
+
+const createSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+});
+
+const updateSchema = Joi.object({
+    name: Joi.string().optional(),
+    email: Joi.string().email().optional(),
+    phone: Joi.string().optional(),
+}).or('name', 'email', 'phone');
+
+export const validateCreate = async (req, res, next) => {
+    try {
+        const value = await createSchema.validateAsync(req.body)
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ message: 'missing required name field' })
+    }
+    next()
+}
+
+export const validateUpdate = async (req, res, next) => {
+    try {
+        const value = await updateSchema.validateAsync(req.body)
+    } catch (err) {
+        const [{ type }] = err.details
+        if (type === 'object.unknown') {
+            return res
+            .status(400)
+            .json({ message: err.message })
+        }
+        return res
+            .status(400)
+            .json({ message: 'missing fields' })
+    }
+    next()
+}
+
+const idShema = Joi.object({
+    id: Joi.string().required()
+})
+
+export const validateId = async (req, res, next) => {
+    try {
+        const value = await idShema.validateAsync(req.params)
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ message: 'missing fields' }) 
+    }
+    next()
+}
