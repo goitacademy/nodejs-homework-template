4e3e810211259e090b0f1af3fa3dc56fa7016474
AndrewRiabets <Inferion5@gmail.com>
2021-12-21 01:19:46 +0200

    done

--- model/index.js
+++ model/index.js
@@ -1,20 +1,66 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { randomUUID } from "crypto";
+import contacts from "./contacts.json";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-const getContactById = async (contactId) => {}
+// TODO: задокументировать каждую функцию
+const listContacts = async () => {
+  return contacts;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const [contact] = contacts.filter((contact) => contact.id === contactId);
+  return contact;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(
+      path.join(__dirname, "contacts.json"),
+      JSON.stringify(contacts, null, 2)
+    );
+    return result;
+  }
+  return null;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async ({ name, email, phone }) => {
+  if (name && email && phone) {
+    const newContact = { id: randomUUID(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(
+      path.join(__dirname, "contacts.json"),
+      JSON.stringify(contacts, null, 2)
+    );
+    return newContact;
+  }
+  return "Please check your input data!";
+};
 
-module.exports = {
+const updateContact = async (contactId, body) => {
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const updatedContact = { id: contactId, ...contacts[index], ...body };
+    contacts[index] = updatedContact;
+    await fs.writeFile(
+      path.join(__dirname, "contacts.json"),
+      JSON.stringify(contacts, null, 2)
+    );
+    return updatedContact;
+  }
+  return null;
+};
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,48 @@
-const express = require('express')
-const router = express.Router()
+import { Router } from "express";
+import model from "../../model/index";
+import { validateCreate, validateUpdate, validateId } from "./validation";
+const router = new Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", validateId, async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getContactById(id);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateCreate, async (req, res, next) => {
+  const newContact = await model.addContact(req.body);
+  res.status(201).json(newContact);
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", validateId, async (req, res, next) => {
+  const { id } = req.params;
 
-module.exports = router
+  const contact = await model.removeContact(id);
+  if (contact) {
+    return res.status(200).json({ message: "contact deleted" });
+  }
+
+  res.status(404).json({ message: "not found" });
+});
+
+router.put("/:id", validateId, validateUpdate, async (req, res, next) => {
+  const { id } = req.params;
+
+  const contact = await model.updateContact(id, req.body);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
+
+  res.status(404).json({ message: "not found" });
+});
+
+export default router;
