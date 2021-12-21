b0d7cb9ec6a03fa8ad55637ace18b210eb9a6203
Oleksii <Alex13867777@gmail.com>
2021-12-21 02:48:10 +0200

    [hw02-express]_ready for peer review

--- model/index.js
+++ model/index.js
@@ -1,20 +1,57 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+import fs from 'fs/promises';
+import path from 'path';
+import { randomUUID } from 'crypto';
+import contacts from './contacts.json';
+import { fileURLToPath } from 'url';
 
-const listContacts = async () => {}
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+    return contacts;
+};
 
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+const getContactById = async (contactId) => {
+    const [contact] = contacts.filter((contact) => contact.id === contactId);
+    return contact;
 }
+
+const removeContact = async (contactId) => {
+    const [contact] = contacts.filter((contact) => contact.id === contactId);
+    contacts.splice(contacts.indexOf(contact), 1);
+    await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2),
+    )
+
+    return contact;
+}
+
+const addContact = async ({name, email, phone}) => {
+  const newContact = { id: randomUUID(), name, email, phone }
+  contacts.push(newContact)
+  await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2),
+  )
+  return newContact
+}
+
+const updateContact = async (contactId, body) => {
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const updatedContact = { id: contactId, ...contacts[index], ...body };
+    contacts[index] = updatedContact
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, null, 2),
+    );
+
+    return updatedContact;
+  };
+
+  return null;
+}
+
+export default { listContacts, getContactById, removeContact, addContact, updateContact };
