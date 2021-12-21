8c094ca3a4e87d3c5c8a30b251fbf0aa3ca578c3
Serg <serg@MacBook-Pro-Serg.local>
2021-12-21 17:27:50 +0200

    push

--- model/index.js
+++ model/index.js
@@ -1,15 +1,77 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises');
+const path = require('path');
+const {randomUUID}=require('crypto')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const readContacts = async  () => {
+  try {
+    const response= await fs.readFile(contactsPath,'utf8');
+    return JSON.parse(response);
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+const listContacts = async () => {
+  return await readContacts()
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
 
-const addContact = async (body) => {}
+  try {
+    const contacts= await readContacts();
+    const findContact= contacts.find(contact => contact.id === contactId);
+    return findContact;
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await readContacts();
+    const contact=contacts.find(contact => contact.id === contactId);
+    if(contact) {
+      const removeContactById=contacts.filter(contact => contact.id !== contactId);
+      await fs.writeFile(contactsPath,JSON.stringify(removeContactById,null,2));
+      return contact;
+    }
+    return null;
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const addContact = async (body) => {
+  const id = randomUUID()
+  try {
+    const contact = {id,...body};
+    const contacts = await readContacts();
+    contacts.push(contact);
+    await fs.writeFile(contactsPath,JSON.stringify(contacts,null,2));
+    return contact;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateContact = async (contactId, body) => {
+
+  try {
+    const contacts = await readContacts();
+    const contactIndex=contacts.findIndex(contact => contact.id === contactId);
+    if(contactIndex > -1) {
+      const updateContact= {id:contactId, ...contacts[contactIndex],...body};
+      contacts[contactIndex] = updateContact;
+      await fs.writeFile(contactsPath,JSON.stringify(contacts,null,2));
+      return updateContact;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 module.exports = {
   listContacts,
@@ -17,4 +79,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  readContacts
 }
---
+++ routes/api/validation.js
@@ -0,0 +1,34 @@
+const Joi = require('joi');
+
+const addSchema = Joi.object({
+    name:Joi.string().required(),
+    email:Joi.string().required(),
+    phone:Joi.string().required(),
+})
+
+const updateSchema=Joi.object({
+    name:Joi.string().optional(),
+    email:Joi.string().optional(),
+    phone:Joi.string().optional(),
+}).or('name', 'email', 'phone')
+
+
+const validateAdd = async (req,res,next) => {
+    try {
+        await addSchema.validateAsync(req.body)
+    } catch (error) {
+        const keyContact=error.message.split(' ').splice(0,1).join("").replace(/"/g,"");
+        return res.status(400).json({message:`missing required ${keyContact} field`});
+    }
+    next();
+}
+
+const validateUpdate = async(req,res,next) => {
+    try {
+        await updateSchema.validateAsync(req.body);
+    } catch (error) {
+        return res.status(400).json({message:error.message.replace(/"/g,"")})
+    }
+}
+
+module.exports= {validateAdd,validateUpdate}
