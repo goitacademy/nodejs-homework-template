b47c0035391ef33975478c2b1a2c51d7f5f3959a
Iliya Lunev <lunevig90@gmail.com>
2021-12-22 20:47:02 +0200

    add dotenv, add post create

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,20 @@
-const app = require('../app')
+const app = require('../app');
 
-const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log('Database connection successful'))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,29 +2,29 @@ const express = require('express');
 const router = express.Router();
 const { contactsSchema } = require('../../validation');
 
-const contactsOperations = require('../../model/contactsOperations.js');
+const contact = require('../../model');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts = await contactsOperations.listContacts();
-    res.json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const contacts = await contactsOperations.listContacts();
+//     res.json(contacts);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  try {
-    const contacts = await contactsOperations.getContactById(contactId);
-    if (!contacts) {
-      return res.status(404).json({ message: 'Not found', code: 404 });
-    }
-    res.json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get('/:contactId', async (req, res, next) => {
+//   const { contactId } = req.params;
+//   try {
+//     const contacts = await contactsOperations.getContactById(contactId);
+//     if (!contacts) {
+//       return res.status(404).json({ message: 'Not found', code: 404 });
+//     }
+//     res.json(contacts);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 router.post('/', async (req, res, next) => {
   try {
@@ -33,44 +33,44 @@ router.post('/', async (req, res, next) => {
       return res.status(400).json({ message: 'missing required name field' });
     }
 
-    const contacts = await contactsOperations.addContact(req.body);
+    const contacts = await contact.create(req.body);
     res.status(201).json({ contacts });
   } catch (error) {
     next(error);
   }
 });
 
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  try {
-    const contacts = await contactsOperations.removeContact(contactId);
-    if (!contacts) {
-      return res.status(404).json({ message: 'Not found', code: 404 });
-    }
-    res.json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.delete('/:contactId', async (req, res, next) => {
+//   const { contactId } = req.params;
+//   try {
+//     const contacts = await contactsOperations.removeContact(contactId);
+//     if (!contacts) {
+//       return res.status(404).json({ message: 'Not found', code: 404 });
+//     }
+//     res.json(contacts);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.patch('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const { error } = contactsSchema.validate(req.body);
-  try {
-    if (error) {
-      return res.status(400).json({ message: 'missing fields' });
-    }
-    const updateContacts = await contactsOperations.updateContactById(
-      contactId,
-      req.body,
-    );
-    if (!updateContacts) {
-      return res.status(404).json({ message: 'Not found', code: 404 });
-    }
-    res.json(updateContacts);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.patch('/:contactId', async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const { error } = contactsSchema.validate(req.body);
+//   try {
+//     if (error) {
+//       return res.status(400).json({ message: 'missing fields' });
+//     }
+//     const updateContacts = await contactsOperations.updateContactById(
+//       contactId,
+//       req.body,
+//     );
+//     if (!updateContacts) {
+//       return res.status(404).json({ message: 'Not found', code: 404 });
+//     }
+//     res.json(updateContacts);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 module.exports = router;
