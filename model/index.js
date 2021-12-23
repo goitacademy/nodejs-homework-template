e2167979927c329569036f24add823cc99f75704
Dimon-Stasyuk <dimon.stasyuk@gmail.com>
2021-12-23 12:08:19 +0200

    Squashed commit of the following:
    commit d22aa5cf05229eacd96d97dfc6d357e0911fa234
    Author: Dimon-Stasyuk <dimon.stasyuk@gmail.com>
    Date:   Sat Dec 18 10:50:07 2021 +0200
    
        Update contacts.js
    
    commit faed272decb3aab5a59fb5c29303382ddca7e4c4
    Author: Dimon-Stasyuk <dimon.stasyuk@gmail.com>
    Date:   Sat Dec 18 10:50:05 2021 +0200
    
        Update contacts.json
    
    commit fc225602af094a7e9f49c0c080c9f2fe88331801
    Author: Dimon-Stasyuk <dimon.stasyuk@gmail.com>
    Date:   Sat Dec 18 10:50:03 2021 +0200
    
        Update app.js
    
    commit ee34e383f4ae66bcbdfa198f665e5ef0f7a8a461
    Author: Dimon-Stasyuk <dimon.stasyuk@gmail.com>
    Date:   Fri Dec 17 23:49:18 2021 +0200
    
        выполнил дз

--- model/index.js
+++ model/index.js
@@ -1,15 +1,58 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const path = require("path");
+const shortid = require("shortid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const getAll = await fs.readFile(contactsPath);
+  const result = JSON.parse(getAll);
+  return result;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const getAll = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(getAll);
+  const result = contacts.find((contact) => contact.id === contactId);
 
-const addContact = async (body) => {}
+  return result;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const getAll = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(getAll);
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+
+  const updateContacts = contacts.filter((contact) => contact.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(updateContacts, null, 2));
+
+  return contacts[idx];
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const newContact = { id: shortid.generate(), name, email, phone };
+  const getAll = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(getAll);
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const getAll = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(getAll);
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { id: contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[idx];
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +60,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
