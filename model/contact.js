3d4a25118669cea85063fa9e49345d6ca1930ecd
Dimon-Stasyuk <dimon.stasyuk@gmail.com>
2021-12-24 19:10:35 +0200

    Update contact.js

--- model/contact.js
+++ model/contact.js
@@ -5,6 +5,7 @@ const joiSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 const productSchema = Schema({
