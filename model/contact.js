97c56c7cd7007f2ccf2b8ece77ea51492e323f9c
AleksDu <aleksandr.dyshkin@gmail.com>
2021-12-24 20:31:20 +0200

    end

---
+++ model/contact.js
@@ -0,0 +1,36 @@
+import pkg from "mongoose";
+const { Schema, model } = pkg;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
+  }
+);
+
+const Contact = model("contact", contactSchema);
+export default Contact;
