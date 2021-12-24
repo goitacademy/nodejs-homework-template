2f5b9b3fdd7775a85237b0194cc3ffc9721ea671
Anna Savchuk <anna060411@gmail.com>
2021-12-24 20:29:35 +0200

    done

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,23 @@
+const mongoose = require("mongoose");
+
 const app = require("../app");
 
-const { PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+//
+// const DB_HOST =
+//   "mongodb+srv://Anna:asarf32@cluster0.jr9ga.mongodb.net/db-contacts?retryWrites=true&w=majority";
+//
--- models/index.js
+++ models/index.js
@@ -1,13 +1,6 @@
-const listContacts = require("./listContacts");
-const getContactById = require("./getContactById");
-const addContact = require("./addContact");
-const updateById = require("./updateById");
-const removeContact = require("./removeContact");
+const { Contact, joiSchema } = require("./contact");
 
 module.exports = {
-  listContacts,
-  getContactById,
-  addContact,
-  updateById,
-  removeContact,
+  Contact,
+  joiSchema,
 };
