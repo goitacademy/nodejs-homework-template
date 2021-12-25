48c5c35390f76990a4f8260fe0b010cdac33ac00
Tatiana-goit <tanchou74@gmail.com>
2021-12-26 01:38:44 +0300

    get

--- model/index.js
+++ model/index.js
@@ -1,14 +1,30 @@
 const fs = require('fs/promises')
+const path = require('path')
+const crypto = require('crypto')
+
 
 const contacts = require('./contacts.json')
 
-const listContacts = async () =>{return contacts}
+const listContacts = async () => {
+  // throw new Error('we can not find contacts')
+  return contacts
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const [contact] = contacts.filter(contact => contact.id ===id)
+  return contact
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async ({name, email, phone}) => {
+  const contacts = await listContacts();
+  const newContact = {id: crypto.randomUUID(), name, email, phone}
+  contacts.push(newContact)
+  await fs.writeFile(path.join(__dirname,'contacts.json'),JSON.stringify(contacts,null,2))
+  return newContact
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,63 @@
 const express = require('express')
 const router = express.Router()
-const {listContacts,
+const {
+  listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact } = require('../../model/index')
+  updateContact,
+} = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
-  const products = await listContacts()
-  console.log('11111');
-  res.json({ products })
+  try {
+    const contacts = await listContacts()
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result: contacts },
+    })
+    // res.json({ contacts })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const result = await getContactById(id)
+    console.log(result)
+    if (!result) {
+      res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: `Product whith id=${id} not found`,
+      })
+      return
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result },
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await addContact(req.body)
+    console.log(req.body);
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { result },
+    })
+    console.log(result);
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
