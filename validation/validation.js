a12f234335ab644877bebbea3916c65608e112ba
OLEG <schastliviy.sluchay@ukr.net>
2021-12-26 16:47:07 +0200

    0.6

--- validation/validation.js
+++ validation/validation.js
@@ -3,8 +3,10 @@ const Joi = require("joi");
 const schema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   phone: Joi.string()
-    .length(10)
-    .pattern(/^[0-9]+$/)
+    .length(14)
+    // .pattern(/^[0-9]+$/)
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/)
+
     .required(),
 
   email: Joi.string().email({
