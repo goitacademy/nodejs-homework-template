79ee0ddc4335dc827ab56081d9b2f853e8e5e3de
Yana Sokurenko <77590795+YanaMoss@users.noreply.github.com>
2021-12-27 12:47:31 +0200

    fix

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 
 const userSchema = Schema({
   password: {
@@ -20,15 +21,15 @@ const userSchema = Schema({
     type: String,
     default: null,
   },
-}{ versionKey: false, timestamps: true },);
+},{ versionKey: false, timestamps: true },);
 
-// userSchema.methods.setPassword = function(password){
-//    this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-// }
+userSchema.methods.setPassword = function(password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
 
-// userSchema.methods.comparePassword = function(password){
-//    return bcrypt.compareSync(password, this.password);
-// }
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 const joiRegisterSchema = Joi.object({
    email: Joi.string().required(),
