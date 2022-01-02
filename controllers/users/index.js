5950dca0fce7f443ed2aa84af7bbfc097d243f83
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-01-02 18:46:41 +0200

    hw05 done

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,5 +2,6 @@ const signUp = require("./signUp");
 const login = require("./login");
 const getCurrent = require("./getCurrent")
 const logout = require("./logout")
+const updateAvatar = require("./updateAvatar")
 
-module.exports = { signUp, login,getCurrent, logout };
+module.exports = { signUp, login,getCurrent, logout,updateAvatar };
