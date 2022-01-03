8f0415b70159bdb670207aa81002b79f90af8c14
RybalovValentyn <0661629069@ukr.net>
2022-01-03 17:12:45 +0300

    pul

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,6 @@
-const app = require('../app')
-
+import app from '../app.js'
+import dotenv from 'dotenv'
+dotenv.config()
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,51 @@
-const express = require('express')
-const router = express.Router()
+import {Router} from "express";
+import model from '../../model/index.js';
+import {validateCreate, validateUpdte, validateId} from './validation.js';
+const router = new Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id',validateId,  async (req, res, next) =>{
+  const {id} = req.params;
+  const contact = await model.getContactById(id)
+  if (contact) {
+   return res.status(200).json(contact);
+  }
+  res.status(404).json({message: `not found contact id = ${id}`})
+});
+
+router.post('/', validateCreate, async (req, res, next) => {
+  if (req.body.name && req.body.email && req.body.phone) {
+    const newContact = await model.addContact(req.body)
+    return  res.status(201).json(newContact);
+  }
+  res.status(404).json({message: "missing required name field"})
+ 
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id',validateId,  async (req, res, next) => {
+  const {id} = req.params;
+  const contact = await model.removeContact(id)
+  if (contact) {
+      return res.status(200).json({message: `contact with id = ${id} is deleted`});
+  }
+  res.status(404).json({message: `not found contact id = ${id}`})
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.patch('/:id', validateUpdte, validateId, async (req, res, next) => {
+  const {id} = req.params;
+  const body = req.body
+  const updataContact = await model.updateContact(id, body)
+  if (updataContact) {
+    const contact = await model.getContactById(id)
+      if (contact) {
+           return res.status(200).json(contact);
+     }
+  }
+  res.status(404).json({message: `not found contact id = ${id}`})
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+export default router
