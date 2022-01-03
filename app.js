44b047e70b4b28c08e4aec8aece3845dc9b002b0
Dmyryi <muzalevskyid333@gmail.com>
2022-01-03 20:12:41 +0200

    hw02

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded())
 
 app.use('/api/contacts', contactsRouter)
 
@@ -19,7 +20,8 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const {status = 500, message= "Server error"}= err;
+  res.status(status).json({ message })
 })
 
 module.exports = app
