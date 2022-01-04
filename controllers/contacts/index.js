e3f763121cc6c7e365ac0c8f915502b421acbbc9
Evgeniya Bayda <baje75128@gmail.com>
2022-01-04 22:47:41 +0200

    delete files

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -1,15 +1,15 @@
-import { addContactCb } from "./addContact";
-import { getContactByIdCb } from "./getContactById";
-import { updateContactCb } from "./updateContact";
-import { getContacts } from "./listContacts";
-import { removeContactCb } from "./deleteContact";
+// import { addContactCb } from "./addContact";
+// import { getContactByIdCb } from "./getContactById";
+// import { updateContactCb } from "./updateContact";
+// import { getContacts } from "./listContacts";
+// import { removeContactCb } from "./deleteContact";
 import ContactsService from "./contactsControllers";
 
 export {
-  addContactCb,
-  getContactByIdCb,
-  updateContactCb,
-  getContacts,
-  removeContactCb,
+  // addContactCb,
+  // getContactByIdCb,
+  // updateContactCb,
+  // getContacts,
+  // removeContactCb,
   ContactsService,
 };
