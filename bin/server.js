fa60982fc45f7886b0d3cbb09a8b9caef8e103a1
Liudmyla Kozachenko <viergee@gmail.com>
2022-01-07 02:39:44 +0200

    commit1
    mongo db and mongoose

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,13 @@
-const app = require('../app')
+import app from '../app'
+import db from '../lib/database'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+db.then(()=> {
+  app.listen (PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  })
+}).catch(err => {
+    console.log(`Server is not running. Error: ${err.message}` )
+  })
+
