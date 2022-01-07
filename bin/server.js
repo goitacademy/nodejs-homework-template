c85244ed459367a76df72aec06cb0587b753709b
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2022-01-07 13:47:02 +0200

    final edits

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,18 @@
-const app = require('../app')
+const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const app = require('../app');
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  })
+  .catch(error => {
+    console.log(error.massage);
+    process.exit(1);
+  })
+
