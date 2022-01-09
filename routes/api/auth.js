b8b747d088737ab4bb4cfc883a58b241e8e85205
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2022-01-09 16:47:34 +0200

    registration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,36 @@
 const express = require("express");
 // const Joi = require("joi");
 
+const { BadRequest, Conflict } = require("http-errors");
+
 const { User } = require("../../models");
+const { joiSchema } = require("../../models/user");
 
 const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
+    try {
+        const { error } = joiSchema.validate(req.body)
+        if (error) {
+            throw new BadRequest(error.message);
+        }
+        const { name, email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            throw new Conflict("User already exist");
+        }
+        const newUser = await User.create(req.body);
+        res.status(201).json({
+            user: {
+                name: newUser.name,
+                email: newUser.email
+            }
+        })
+
+    } catch (error) {
+        next(error)
+
+    }
 
 })
 
