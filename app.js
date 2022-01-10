a04a2a3f72ea73957d0039524883c768c018ebdc
Tatiana-goit <tanchou74@gmail.com>
2022-01-10 20:59:51 +0200

    DB_HOST

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const dotenv = require('dotenv')
+dotenv.config();
+
+const {DB_HOST} = process.env
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database connect success');
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
 
 const contactsRouter = require('./routes/api/contacts')
 
