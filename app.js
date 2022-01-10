e262608b056dd00201deb7ade85a353e59bb0845
IhorHrytsenko <gisyshka@gmail.com>
2022-01-10 19:59:07 +0200

    done-hw03

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import { HttpCode } from './lib/constants'
 
-const contactsRouter = require('./routes/api/contacts')
+import contactsRouter from './routes/api/contacts'
 
 const app = express()
 
@@ -10,16 +11,23 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) // json
+app.use(express.urlencoded({ extended: false })) // forms
 
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res
+    .status(HttpCode.NOT_FOUND)
+    .json({ status: 'error', code: HttpCode.NOT_FOUND, message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+    status: 'fail',
+    code: HttpCode.INTERNAL_SERVER_ERROR,
+    message: err.message,
+  })
 })
 
-module.exports = app
+export default app
---
+++ model/contact.js
@@ -0,0 +1,52 @@
+import pkg from 'mongoose'
+import { MIN_AGE, MAX_AGE } from '../lib/constants'
+
+const { Schema, model } = pkg
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    age: {
+      type: Number,
+      min: MIN_AGE,
+      max: MAX_AGE,
+      default: null,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id
+        return ret
+      },
+    },
+    toObject: { virtuals: true },
+  },
+)
+
+contactSchema.virtual('status').get(function () {
+  if (this.age >= 40) {
+    return 'old'
+  }
+  return 'young'
+})
+
+const Contact = model('contact', contactSchema)
+
+export default Contact
