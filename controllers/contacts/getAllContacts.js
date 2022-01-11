0440335d56fa951f705ef20803f50c25e818442c
Alina Kasianova <avkasyanova@gmail.com>
2022-01-11 15:40:38 +0200

    Update getAllContacts.js

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,20 +1,31 @@
 const { Contact } = require("../../models");
 
 const getAllContacts = async (req, res) => {
-  const { page = 1, limit = 20, sortBy, sortByDesc, filter } = req.query; //pagination
+  const {
+    page = 1,
+    limit = 20,
+    sortBy,
+    sortByDesc,
+    filter,
+    favorite,
+  } = req.query;
+
   const { _id } = req.user;
 
   let sortParam = null;
+  let contacts = null;
+  let total = null;
 
-  const total = await Contact.find({ owner: _id }).countDocuments(); // total contacts counter
-
-  const skip = (page - 1) * limit; //pagination
-
-  let contacts = Contact.find(
-    { owner: _id },
-    "name _id email phone",
-    { skip: Number(skip), limit: Number(limit) } //pagination
-  ).populate("owner", "_id email"); //"owner"-field extends
+  if (favorite) {
+    total = await Contact.find({ owner: _id, favorite }).countDocuments(); // fav contacts counter
+    contacts = Contact.find({ owner: _id, favorite }).populate(
+      "owner",
+      "_id email"
+    ); //"owner"- field extends;
+  } else {
+    total = await Contact.find({ owner: _id }).countDocuments(); // total contacts counter
+    contacts = Contact.find({ owner: _id }).populate("owner", "_id email"); //"owner"-field extends;
+  }
 
   if (sortBy) {
     sortParam = { [`${sortBy}`]: 1 }; //sort by query "+"
@@ -25,7 +36,12 @@ const getAllContacts = async (req, res) => {
   if (filter) {
     contacts = contacts.select(filter.split("|").join(" ")); //'api/contacts?filter=name|email|favorite'
   }
-  contacts = await contacts.sort(sortParam);
+
+  const skip = (page - 1) * limit; //pagination
+  contacts = await contacts
+    .skip(Number(skip)) //pagination
+    .limit(Number(limit)) //pagination
+    .sort(sortParam); //sort by query
 
   res.status(200).json({ data: { total, contacts } });
 };
