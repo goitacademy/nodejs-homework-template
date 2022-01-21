7c3fab987bbc54e507c0727790f5b155813c22a9
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2022-01-21 19:07:29 +0100

    fixed errors

--- repository/users.js
+++ repository/users.js
@@ -1,23 +1,23 @@
 import User from '../model/user'
 
-const findById = async (id) => {
-  return await User.findById(id)
+const findById = (id) => {
+  return User.findById(id)
 }
 
-const findByEmail = async (email) => {
-  return await User.findOne({ email })
+const findByEmail = (email) => {
+  return User.findOne({ email })
 }
 
-const create = async (body) => {
+const create = (body) => {
   const user = new User(body)
-  return await user.save()
+  return user.save()
 }
 
-const updateToken = async (id, token) => {
-  return await User.updateOne({ _id: id }, { token })
+const updateToken = (id, token) => {
+  return User.updateOne({ _id: id }, { token })
 }
 
-const updateAvatar = async (id, avatar) => {
-  return await User.updateOne({ _id: id }, { avatar })
+const updateAvatar = (id, avatar) => {
+  return User.updateOne({ _id: id }, { avatar })
 }
 export default { findById, findByEmail, create, updateToken, updateAvatar }
