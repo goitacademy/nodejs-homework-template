8d30f32ba934fc49c5c7e6f7595deedfacd5b807
Dmytryi Muzalevskyi <muzalevskyid333@gmail.com>
2022-01-22 16:37:24 +0200

    .

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,106 @@
-const express = require('express')
-
+const express = require('express');
+const createError = require('http-errors');
+// const Joi = require('joi');
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const { Contact } = require('../../model');
+
+
+
+
+
+// GETALL
+router.get('/', async(req, res, next) => {
+        try {
+            const contacts = await Contact.find();
+            res.json(contacts);
+        } catch (error) {
+            next(error);
+        }
+
+    })
+    // GetByID
+router.get('/:id', async(req, res, next) => {
+    const { id } = req.params;
+    try {
+        const contacts = await Contact.findById(id);
+        res.json(contacts);
+        if (!contacts) {
+            throw createError(404, "Not found");
+        }
+    } catch (error) {
+        if (error.message.includes("Cast to ObjectId failed")) {
+            error.status = 404;
+        }
+        next(error);
+    }
+
+});
+// ADD
+router.post('/', async(req, res, next) => {
+        try {
+
+            const newContacts = await Contact.create(req.body);
+            res.status(201).json(newContacts);
+        } catch (error) {
+            if (error.message.includes("validation failed")) {
+                error.status = 400;
+            }
+            next(error);
+        }
+    })
+    // GEtBYREMOVE
+router.delete('/:id', async(req, res, next) => {
+        try {
+            const { id } = req.params;
+            const deleteContacts = await Contact.findByIdAndRemove(id);
+            if (!deleteContacts) {
+                throw createError(404, "Not found");
+            }
+            res.json({ "message": "contact deleted" })
+        } catch (error) {
+            if (error.message.includes("Cannot read")) {
+                error.status = 400;
+            }
+            next(error);
+        }
+    })
+    // GETBYUPDATE
+router.put('/:id', async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const updateContacts = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+
+        if (!updateContacts) {
+            throw createError(404, "Not found");
+        }
+        res.json(updateContacts);
+
+    } catch (error) {
+        if (error.message.includes("Cannot read")) {
+            error.status = 400;
+        }
+        next(error);
+    }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch("/:id/favorite", async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { favorite } = req.body;
+        const updateContacts = await Contact.findByIdAndUpdate(id, { favorite }, { new: true });
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+        if (!updateContacts) {
+            throw createError(404, "Not found");
+        }
+        res.json(updateContacts);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    } catch (error) {
+        if (error.message.includes("Cannot read")) {
+            error.status = 400;
+        }
+        next(error);
+    }
 })
 
 module.exports = router
