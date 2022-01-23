493b27907bce9769da95a08b76475a7304ca4b46
tanulka <tanulkako@gmail.com>
2022-01-23 17:01:01 +0200

    hw2-complited

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require('./app');
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
