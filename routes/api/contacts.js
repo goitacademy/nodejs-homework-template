54c15247351c645a10e67a028d5935e59cd346d7
nelli-belovol <nelli199020@gmail.com>
2022-01-25 10:15:55 +0200

    create hw-02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,102 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
+const CreateError = require("http-errors");
+const router = express.Router();
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const schema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+
+  email: Joi.string().email({
+    tlds: { allow: ["com", "net"] },
+  }),
+
+  phone: Joi.string().min(10).max(10).messages({
+    "string.max": `"phone" should be 10 characters`,
+    "string.min": `"phone" should be 10 characters`,
+  }),
 })
+  .with("name", "email")
+  .with("email", "phone");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    if (!result) {
+      throw new CreateError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw new CreateError(404, "Not found");
+    }
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const body = await schema.validateAsync(req.body).catch((error) => {
+      const { details } = error;
 
-module.exports = router
+      throw new CreateError(400, details[0].message);
+    });
+
+    const result = await contacts.addContact(body);
+    if (!result) {
+      throw new CreateError(404, "Not found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw new CreateError(404, "Not found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const body = await schema.validateAsync(req.body).catch((error) => {
+      const { details } = error;
+
+      throw new CreateError(400, details[0].message);
+    });
+
+    const result = await contacts.updateContact(contactId, body);
+    if (!result) {
+      throw new CreateError(404, "Not found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require("./app");
+const PORT = 4000;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
