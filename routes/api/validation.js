18dda70f7369f448f661a1af8311a557b6f83e0a
Anidamp <79271758+Anidamp@users.noreply.github.com>
2022-01-25 23:44:19 +0200

    1

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,41 +1,66 @@
-const Joi = require('joi');
+import Joi from 'joi';
+import mongooseService from 'mongoose';
 
-const createShema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required()
+const { Types } = mongooseService;
+
+const createSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool().optional(),
 });
 
 const updateSchema = Joi.object({
-   name: Joi.string(),
-    email: Joi.string().email(),
-    phone: Joi.string()
-}).or('name', 'email', 'phone')
+  name: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string().optional(),
+  favorite: Joi.bool().optional(),
+}).or('name', 'email', 'phone');
 
-const validateCreate = async (req, res, next) => {
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.bool().required(),
+});
+
+export const validateCreate = async (req, res, next) => {
   try {
-    await createShema.validateAsync(req.body)
+    await createSchema.validateAsync(req.body);
   } catch (err) {
     return res
       .status(400)
-      .json({ message: `Field ${err.message.replace(/"/g, '')}` })
+      .json({ message: `Field ${err.message.replace(/"/g, '')}` });
   }
-  next()
-}
+  next();
+};
 
-const validateUpdate = async (req, res, next) => {
+export const validateUpdate = async (req, res, next) => {
   try {
-    await updateSchema.validateAsync(req.body)
-  } catch (error) {
-    const [{ type }] = error.details
-    if (type === 'object.unknown') {
-      return res.status(400).json({ message: error.message })
+    await updateSchema.validateAsync(req.body);
+  } catch (err) {
+    const [{ type }] = err.details;
+    if (type === 'object.missing') {
+      return res.status(400).json({ message: 'missing fields' });
     }
-    return res.status(400).json({ message: `missing fields` })
+    return res.status(400).json({ message: err.message });
   }
-  next()
-}
+  next();
+};
 
-module.exports = {
-    validateCreate, validateUpdate
-}
+export const validateUpdateFavorite = async (req, res, next) => {
+  try {
+    await updateFavoriteSchema.validateAsync(req.body);
+  } catch (err) {
+    const [{ type }] = err.details;
+    if (type === 'object.missing') {
+      return res.status(400).json({ message: 'missing field favorite' });
+    }
+    return res.status(400).json({ message: err.message });
+  }
+  next();
+};
+
+export const validateId = async (req, res, next) => {
+  if (!Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: 'Invalid ObjectId' });
+  }
+  next();
+};
