a72df728ea4f6c9519dc50c25e8228447e1a2e8f
Anna-Sergeevna <4042808@gmail.com>
2022-01-27 18:31:05 +0200

    change config

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
+const { DB_HOST } = require("./config");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
   console.log(`Server running. Use our API on port: 3000`);
