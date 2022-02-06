2af17d3e3c8ae8b520de9eb43721a9636349fa13
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2022-02-07 01:54:11 +0200

    fix createToken

--- tokenService/createToken.js
+++ tokenService/createToken.js
@@ -4,7 +4,7 @@ const { User } = require("../models");
 const { SECRET_KEY } = process.env;
 
 const createToken = async (id) => {
-  const token = jwt.sign({ id }, SECRET_KEY, { expirens: "1h" });
+  const token = jwt.sign({ id }, SECRET_KEY, { expiresIn: "1h" });
   await User.findByIdAndUpdate(id, { token }, { new: true });
 
   return token;
