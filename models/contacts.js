af16caec7fb2ed652a08557dbcbe68899161bd73
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2022-02-06 22:36:30 +0200

    start

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,54 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contactsList = await fs.readFile(contactsPath);
+    const contactsListParse = JSON.parse(contactsList);
+    return contactsListParse;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contactsList = await fs.readFile(contactsPath);
+    const contactsListParse = JSON.parse(contactsList);
+    const contactByItem = await contactsListParse.filter((el) => {
+      return el.id === contactId;
+    });
+    return contactByItem;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contactsList = await fs.readFile(contactsPath);
+    const contactsListParse = JSON.parse(contactsList);
+    const isId = contactsListParse.some((el) => el.id === contactId);
+    if (isId) {
+      const contactsListWithoutItem = await contactsListParse.filter((el) => {
+        return el.id !== contactId;
+      });
+      const rewriteContactsListWithoutItem = await fs.writeFile(
+        contactsPath,
+        JSON.stringify(contactsListWithoutItem)
+      );
+      return true;
+    } else return false;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +56,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,42 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contactsList = await listContacts();
+  res.json(contactsList);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { params } = req;
+  const contactByItem = await getContactById(params.contactId);
+  res.json(contactByItem);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { params } = req;
+  const contactsWithoutItem = await removeContact(params.contactId);
+  console.log("contactsWithoutItem", contactsWithoutItem);
+  if (contactsWithoutItem) {
+    res.json({ message: "contact deleted" });
+  } else {
+    res.json({ message: "Not found" });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
