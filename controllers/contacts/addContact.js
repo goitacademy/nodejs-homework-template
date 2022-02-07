c7042993a1d2bbe935fbf947e98ab2332b5ec6b5
Ilja <tadden12@gmail.com>
2022-02-07 23:22:42 +0200

    Done

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,22 +1,13 @@
-const contacts = require('../../models/contacts');
+const {Contact, schemas} = require('../../models/contact');
 const createError = require('http-errors');
-const Joi = require('joi');
-
-
-const contactSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string(),
-    phone: Joi.string().required()
-});
 
 const addContact = async (req, res, next) => {
     try {
-        const { error } = contactSchema.validate(req.body);
+        const { error } = schemas.add.validate(req.body);
         if (error) {
             throw new createError(400, error.message);
         }
-        const { name, email, phone } = req.body;
-        const result = await contacts.addContact(name, email, phone);
+        const result = await Contact.create(req.body);
         res.status(201).json(result);
     } catch (error) {
         next(error)
