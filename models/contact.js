52a75ee41a2f79ff7d2bebf154c585a491fc5ddd
Ilja <tadden12@gmail.com>
2022-02-10 23:13:34 +0200

    done

--- models/contact.js
+++ models/contact.js
@@ -27,6 +27,11 @@ const contactSchema = Schema({
     favorite: {
         type: Boolean,
         default: false,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
     }
 }, { versionKey: false, timestamps: true });
 
