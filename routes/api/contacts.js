827b1df445561a71de6a99090317e4d1f8a8c0be
Temur Esanov <temuresanov01@gmail.com>
2022-02-19 23:33:22 +0200

    small fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'home work done 2' })
+  res.json({ message: 'template message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
