8b100002dcc86d349c84642460673fa2afdf291e
Temur Esanov <temuresanov01@gmail.com>
2022-02-19 23:06:54 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'home work done 2' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
