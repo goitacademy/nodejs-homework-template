1388e29763dd90670477d2276a52780259036896
oleksandrpalatniy <81517141+oleksandrpalatniy@users.noreply.github.com>
2022-02-20 01:44:09 +0200

    completed

---
+++ routes/api/contacts-validation-schem.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const contactsSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().required(),
+    phone: Joi.string().pattern(/[0-9]+/).min(3).max(30).required(),
+    })
+
+    module.exports = {contactsSchema}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,49 @@
 const express = require('express')
+const contactsModel = require('../../models/contacts')
+
+const {contactsSchema} = require('./contacts-validation-schem')
+const {validate} = require('../../middlewares/validation')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message proba' })
+  const contacts = await contactsModel.listContacts()
+  res.json({ status: 'success', code: 200, payload: {contacts} })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contact = await contactsModel.getContactById(req.params.contactId)
+  if (contact) {
+    return res.json({ status: 'success', code: 200, payload: {contact} })
+  }
+  return res
+  .status(404)
+  .json({ status: 'error', code: 404, message:'Not found' })
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validate(contactsSchema), async (req, res, next) => {
+  const contact = await contactsModel.addContact(req.body)
+  res.status(201).json({ status: 'success', code: 201, payload: {contact} })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contact = await contactsModel.removeContact(req.params.contactId)
+  if (contact) {
+    return res.json({ status: 'success', code: 200, message: 'Contact deleted' })
+  }
+  return res
+  .status(404)
+  .json({ status: 'error', code: 404, message:'Not found' })
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', validate(contactsSchema), async (req, res, next) => {
+  const contact = await contactsModel.updateContact(req.params.contactId, req.body)
+  if (contact) {
+    return res.json({ status: 'success', code: 200, payload: {contact} })
+  }
+  return res
+  .status(404)
+  .json({ status: 'error', code: 404, message:'Not found' })
 })
 
 module.exports = router
