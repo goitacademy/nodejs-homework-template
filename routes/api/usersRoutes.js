968234cb3a56be64a285393db080bec26b713fa7
Dima.2007 <d.oliynyk@outlook.com>
2022-02-20 17:20:02 +0300

    fx

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -5,8 +5,56 @@ const passport = require('passport')
 const secret = process.env.JWT_SECRET
 const gravatar = require('gravatar');
 
-const auth = require('../../middlewares/auth');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs').promises;
 
+const uploadDir = path.join(__dirname, '../../upload/avatars')
+const publicDir = path.join(__dirname, '../../public/avatars')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir)
+  },
+  filename: (req, file, cb) => {
+    const newFilename = `${new Date().getTime()}_${file.originalname}`;
+    cb(null, newFilename)
+  },
+})
+
+const upload = multer({
+  storage: storage,
+})
+
+router.patch('/avatars', upload.single('thumbnail'), async (req, res) => {
+  try {
+    const { path: temporaryName, originalname } = req.file;
+    const fileName = path.join(publicDir, originalname)
+    try{
+      await fs.rename(temporaryName, fileName);
+    } catch(err){
+      await fs.unlink(temporaryName);
+      console.log(err);
+      res.status(500).send(err);
+    }
+    // Перемістіть файл з папки завантажень до папки thumbnails
+    const newShow = await Show.create({
+      title: req.body.title,
+      description: req.body.description,
+      releasedAt: req.body.releasedAt,
+      thumbnailUrl: req.file.filename, // Назва файлу
+    });
+
+    res.json(newShow);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+
+const auth = require('../../middlewares/auth');
 
 router.post('/signup', async (req, res, next) => {
   const { email, password } = req.body
