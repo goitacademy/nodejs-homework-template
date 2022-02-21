08e218fdf7eaab8e4e59ce2fd320f4704d4e43c5
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2022-02-22 00:41:20 +0200

    Fix message for empty body

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -34,7 +34,10 @@ const updateContactsSchema = Joi.object({
         .min(3)
         .max(20)
         .pattern(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){3,14}(\s*)?$/)
-})
+}).min(1)
+    .messages({
+        'object.min': "missing fields"
+    })
 
 module.exports = {
     addContactsSchema,
