0a74c7760cc3025899b8252965a51ce733545589
Viktor Sinelnykov <Fenya.ua@gmail.com>
2022-02-21 13:34:43 +0200

    fix: message

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Home work done!" });
+  res.json({ message: "Home work №2 done!" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
