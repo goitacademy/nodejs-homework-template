64bf109ea3bceb89ade7e3a8db530b936c0bc26e
Taras Demchenko <meelenych@gmail.com>
2022-02-23 16:18:06 +0200

    helper added

---
+++ helpers/sendMail.js
@@ -0,0 +1,14 @@
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
+
+const { SENDGRID_API_KEY } = process.env;
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const sendMail = async (data) => {
+	const mail = { ...data, from: "meelenych@gmail.com" };
+	await sgMail.send(mail);
+	return true;
+};
+
+module.exports = sendMail;
