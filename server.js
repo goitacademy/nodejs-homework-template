cf4f82810a7dc704fc9d6cd7e9c0f18e34d43f41
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2022-03-09 14:54:38 +0200

    adds message for db connection

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = require('./app')
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
   .catch(err => {
     console.log(err.message)
     process.exit(1)
