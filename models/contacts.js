bef6209618ff0a89392bb6a32ccbc22b0687db78
Kamil Witkowski <53374963+KamWit7@users.noreply.github.com>
2022-03-10 21:14:24 +0100

    get, delte done, post start, put not yet

--- models/contacts.js
+++ models/contacts.js
@@ -1,10 +1,49 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const contactsPath = './models/contacts.json'
 
-const listContacts = async () => {}
+const getJsonContacts = async () => {
+  const data = await fs.readFile(contactsPath)
+  const contacts = await JSON.parse(data)
+  return contacts
+}
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await getJsonContacts()
+  console.table(contacts)
 
-const removeContact = async (contactId) => {}
+  return contacts
+}
+
+const getContactById = async (contactId) => {
+  const contacts = await getJsonContacts()
+
+  const contact = contacts.find(
+    (contact) => Number(contact.id) === Number(contactId)
+  )
+
+  console.table(contact)
+  return contact
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await getJsonContacts()
+
+  const isInContacts = () =>
+    contacts.find((contact) => Number(contact.id) === Number(contactId))
+
+  if (!isInContacts()) return
+
+  await fs.writeFile(
+    contactsPath,
+    JSON.stringify(
+      contacts?.filter((contact) => Number(contact.id) !== Number(contactId))
+    )
+  )
+
+  const newContacts = await getJsonContacts()
+  console.table(newContacts)
+  return newContacts
+}
 
 const addContact = async (body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,30 @@
 const express = require('express')
-
+const {
+  getContactById,
+  listContacts,
+  removeContact,
+} = require('./../../models/contacts')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await listContacts()
+  res.json({ message: contacts })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  console.log('rqg', req.params.contactId)
+  const contact = await getContactById(req.params.contactId)
+  res.json({ message: contact })
 })
 
 router.post('/', async (req, res, next) => {
+  console.log('req', req.body)
   res.json({ message: 'template message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = removeContact(req.params.contactId)
+  res.json({ message: contacts })
 })
 
 router.put('/:contactId', async (req, res, next) => {
