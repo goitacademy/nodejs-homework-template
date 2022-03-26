07d14a353b4694f372ee32c2c90a7069a638a187
Kamil Witkowski <53374963+KamWit7@users.noreply.github.com>
2022-03-26 19:04:16 +0100

    db fixex

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,16 +6,16 @@ const {
   addContact,
   removeContact,
   updateContact,
-} = require('./../../models/contacts-db')
+} = require('../../service/index')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: await listContacts() })
+  res.json({ message: await listContacts() }).status(200)
 })
 
 router.get('/:contactId', async (req, res, next) => {
   const contact = await getContactById(req.params.contactId)
   contact
-    ? res.json({ message: contact })
+    ? res.json({ message: contact }).status(200)
     : res.status(404).json({
         code: 404,
         message: `Not found task id`,
@@ -26,9 +26,9 @@ router.get('/:contactId', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   const { name, email, phone } = req.body
   if (!(name && email && phone))
-    return res.status(400).json({ message: 'missing required name field' })
+    return res.status(404).json({ message: 'missing required name field' })
 
-  res.json({ message: await addContact(req.body) }).status(201)
+  res.json({ message: await addContact(req.body) }).status(200)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
@@ -60,7 +60,7 @@ router.put('/:contactId', async (req, res, next) => {
 router.patch('/:contactId/favorite', async (req, res, next) => {
   const { favorite } = req.body
   if (!favorite)
-    return res.status(400).json({ message: 'missing required favorite field' })
+    return res.status(404).json({ message: 'missing required favorite field' })
 
   const contact = await updateContact(req.params.contactId, req.body)
   contact
