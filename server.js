296517df76ed519215648d1769259ded1beca26b
Ivan Suprun <81951117+Bender148@users.noreply.github.com>
2022-03-28 16:11:31 +0300

    add methods and routes/install joi&uuid

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
 })
