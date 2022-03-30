099ffb484342da85d99f9fdfb9679dc5bdfa2a8a
Temur Esanov <temuresanov01@gmail.com>
2022-03-30 12:57:14 +0300

    get, post, delete, put

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,39 @@
 const express = require('express')
-
+const contactsModel = require('../../models/contacts')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await contactsModel.listContacts()
+  res.json({ status: 'success', code: 200, payload: {contacts} })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+   const contacts = await contactsModel.getContactById(req.params.contactId)
+   if (contacts) {
+     return res.json({ status: 'success', code: 200, payload: {contacts} })
+   }
+   return res.status(404).json({status: 'eror', code: 404, massage: 'Not Faund'})
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await contactsModel.addContact(req.body)
+  res.status(201).json({ status: 'success', code: 201, payload: {contacts} })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await contactsModel.removeContact(req.params.contactId)
+   if (contacts) {
+     return res.json({ status: 'success', code: 200, payload: {contacts} })
+   }
+   return res.status(404).json({status: 'eror', code: 404, massage: 'Not Faund'})
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+   const contacts = await contactsModel.updateContact(req.params.contactId, req.body)
+   if (contacts) {
+     return res.json({ status: 'success', code: 200, payload: {contacts} })
+   }
+   return res.status(404).json({status: 'eror', code: 404, massage: 'Not Faund'})
 })
 
 module.exports = router
