7040524cd1e8144a9f5ed1941ce530ff618a4654
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-04-04 01:13:15 +0300

    contact add done

--- models/contacts/addContacts.js
+++ models/contacts/addContacts.js
@@ -1,17 +1,20 @@
-const { randomUUID } = require("crypto");
-const fs = require("fs/promises");
-const path = require("path");
-const contactsPath = path.join(__dirname, "../../db/contacts.json");
-const listContacts = require("./listContacts");
+// const { randomUUID } = require("crypto");
+// const fs = require("fs/promises");
+// const path = require("path");
+// const contactsPath = path.join(__dirname, "../../db/contacts.json");
+// const listContacts = require("./listContacts");
+const { getCollection } = require("./listContacts");
+const DB = require("../../db/db");
 const addContact = async (body) => {
-  const contact = await listContacts.listContacts();
+  const collection = await getCollection(DB, "contacts");
   const newContact = {
-    id: randomUUID(),
     ...body,
   };
-  const arrNew = [...contact, newContact];
-  await fs.writeFile(contactsPath, JSON.stringify(arrNew));
-  return arrNew;
+
+  // const arrNew = [...contact, newContact];
+  // await fs.writeFile(contactsPath, JSON.stringify(arrNew));
+  const result = await collection.insertOne(newContact);
+  return result;
 };
 
 module.exports = {
--- models/contacts/listContacts.js
+++ models/contacts/listContacts.js
@@ -16,16 +16,18 @@ const DB = require("../../db/db");
 
 const getCollection = async (db, nameCollection) => {
   const client = await db;
-  const collection = client.db().collection(nameCollection);
+  const collection = await client.db().collection(nameCollection);
   return collection;
 };
 
 async function listContacts() {
   const collection = await getCollection(DB, "contacts");
   const contacts = await collection.find({}).toArray();
+  console.log(collection);
   return contacts;
 }
 
 module.exports = {
   listContacts,
+  getCollection,
 };
