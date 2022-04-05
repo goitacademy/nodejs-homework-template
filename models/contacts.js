688e62ef0eaa915cab4b2d39886ccf42d0afd0d6
PawelS1988 <szatkowskipawel1988@gmail.com>
2022-04-05 13:51:00 +0200

    done... i think...

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,88 @@
-// const fs = require('fs/promises')
+const res = require("express/lib/response");
+const fs = require("fs/promises");
+const contactsPath = "./models/contacts.json";
 
-const listContacts = async () => {}
+const getJsonContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = await JSON.parse(data);
+  return contacts;
+};
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await getJsonContacts();
+  console.table(contacts);
 
-const removeContact = async (contactId) => {}
+  return contacts;
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await getJsonContacts();
 
-const updateContact = async (contactId, body) => {}
+  const contact = contacts.find(
+    (contact) => Number(contact.id) === Number(contactId)
+  );
+
+  console.table(contact);
+  return contact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await getJsonContacts();
+
+  const isInContacts = () =>
+    contacts.find((contact) => Number(contact.id) === Number(contactId));
+
+  if (!isInContacts()) return "Not found";
+
+  await fs.writeFile(
+    contactsPath,
+    JSON.stringify(
+      contacts?.filter((contact) => Number(contact.id) !== Number(contactId))
+    )
+  );
+
+  return "contact deleted";
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await getJsonContacts();
+
+  const newContact = {
+    id: String(contacts.length + 1),
+    name,
+    email,
+    phone,
+  };
+
+  fs.writeFile(contactsPath, JSON.stringify([...contacts, newContact]));
+
+  console.table(newContact);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await getJsonContacts();
+  let updated;
+
+  const isInContacts = () =>
+    contacts.find((contact) => Number(contact.id) === Number(contactId));
+
+  if (!isInContacts()) return "Not found";
+
+  fs.writeFile(
+    contactsPath,
+    JSON.stringify(
+      contacts.map((contact) => {
+        if (contact.id === contactId) {
+          updated = { ...contact, ...body };
+          return updated;
+        }
+        return contact;
+      })
+    )
+  );
+  return updated;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +90,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
