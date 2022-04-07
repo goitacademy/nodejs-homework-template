a616554acf79605f81542e9f42df825115834378
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-04-07 12:36:53 +0300

    edit update

--- models/contacts/updateContacts.js
+++ models/contacts/updateContacts.js
@@ -1,25 +1,3 @@
-// const fs = require("fs/promises");
-// const path = require("path");
-
-// const listContacts = require("./listContacts");
-// const contactsPath = path.join(__dirname, "../../db/db.js");
-
-// const updateContact = async (contactId, body) => {
-//   const contacts = await listContacts.listContacts();
-//   const index = contacts.findIndex((contact) => contact.id === contactId);
-
-//   if (index !== -1) {
-//     contacts[index] = { ...contacts[index], ...body };
-//     await fs.writeFile(contactsPath, JSON.stringify(contacts));
-//     return contacts[index];
-//   }
-//   return null;
-// };
-
-// module.exports = {
-//   updateContact,
-// };
-
 const DB = require("../../db/db");
 const { ObjectId } = require("mongodb");
 const { getCollection } = require("./listContacts");
@@ -28,7 +6,6 @@ const updateContact = async (contactId, body) => {
   console.log(collection);
 
   const objId = new ObjectId(contactId);
-  console.log(objId);
 
   const { value: result } = await collection.updateOne(
     { _id: objId },
