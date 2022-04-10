8f763716dea79acff785b71938ef466482d64060
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2022-04-11 01:58:54 +0300

    create test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const app = require('./app');
 
-const PORT = process.env.PORT || 3031;
-app.listen(PORT, (error) => {
-  if (error) console.error("Error at a server launch", error);
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, error => {
+  if (error) console.error('Error at a server launch', error);
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
