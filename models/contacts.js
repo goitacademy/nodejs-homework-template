0c60b585cf9b0ccdffaf5bad63deb3dfd54a162d
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-04-10 12:55:30 +0300

    edit validate

--- models/contacts.js
+++ models/contacts.js
@@ -1,22 +1,15 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: Number, required: true, unique: true },
+    date: { type: Date, default: () => Date.now() },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Contact = model("Contact", contactSchema);
 module.exports = Contact;
