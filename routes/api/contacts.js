61f06c7ea3107f482f995ed87e8d15e035f1cc58
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-04-16 12:07:59 +0300

    routers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -26,11 +26,24 @@ router.post("/", async (req, res, next) => {
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const fileteredContacts = contacts.filter(
+    (item) => item.id !== req.params.contactId
+  );
+  res.status(200).json({ message: "contact deleted", fileteredContacts });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { name, email, phone } = req.body;
+  let updatedContact = null;
+  contacts.forEach((contact) => {
+    if (contact.id === req.params.contactId) {
+      contact.name = name;
+      contact.email = email;
+      contact.phone = phone;
+      updatedContact = contact;
+    }
+  });
+  res.status(200).json({ updatedContact });
 });
 
 module.exports = router;
