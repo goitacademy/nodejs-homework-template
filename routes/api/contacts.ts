2be298bdc1e23cd0a077d2167f6b0d6f539c2feb
Ganenco Dmitri <ganenko.dmitrii@gmail.com>
2022-04-19 20:30:34 +0300

    Added joi and updated functions on contacts.ts

--- routes/api/contacts.ts
+++ routes/api/contacts.ts
@@ -9,19 +9,54 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactsModel.getContactById(req.params.contactId);
+  if (contact) {
+    return res.json({ status: "success", code: 200, payload: { contact } });
+  }
+  return res
+    .status(404)
+    .json({ status: "error", code: 404, message: "Not Found" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactsModel.addContact(req.body);
+  res.status(201).json({ status: "success", code: 201, payload: { contact } });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactsModel.removeContact(req.params.contactId);
+  if (contact) {
+    return res.json({ status: "success", code: 200, payload: { contact } });
+  }
+  return res
+    .status(404)
+    .json({ status: "error", code: 404, message: "Not Found" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactsModel.updateContact(
+    req.params.contactId,
+    req.body
+  );
+  if (contact) {
+    return res.json({ status: "success", code: 200, payload: { contact } });
+  }
+  return res
+    .status(404)
+    .json({ status: "error", code: 404, message: "Not Found" });
+});
+
+router.patch("/:contactId", async (req, res, next) => {
+  const contact = await contactsModel.updateContact(
+    req.params.contactId,
+    req.body
+  );
+  if (contact) {
+    return res.json({ status: "success", code: 200, payload: { contact } });
+  }
+  return res
+    .status(404)
+    .json({ status: "error", code: 404, message: "Not Found" });
 });
 
 module.exports = router;
