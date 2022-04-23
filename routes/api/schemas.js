261d25144e6daac5ed80ed6e787304d2d7151737
Pavel Sheremet <pashqa.sheremet@gmail.com>
2022-04-23 09:53:31 +0300

    add validation patterns

--- routes/api/schemas.js
+++ routes/api/schemas.js
@@ -1,12 +1,18 @@
 const joi = require('joi');
 
 exports.newContactSchema = joi.object({
-  name: joi.string().required(),
+  name: joi
+    .string()
+    .pattern(/^[a-zA-Z\s'’ʼ-]{3,30}$/)
+    .required(),
   email: joi.string().email().required(),
-  phone: joi.string().required(),
+  phone: joi
+    .string()
+    .pattern(/^[0-9()+\s-]{10,19}$/)
+    .required(),
 });
 
 exports.updateContactSchema = joi.object({
-  name: joi.string().required(),
-  phone: joi.string().required(),
+  name: joi.string().pattern(/^[a-zA-Z\s'’ʼ-]{3,30}$/),
+  phone: joi.string(),
 });
