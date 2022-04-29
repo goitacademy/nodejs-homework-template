413aad7c048984daf39fa3b677d257870b3bc675
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-04-29 23:54:07 +0300

    vse sdelal

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,54 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path=require("path")
+const { nanoid } =require('nanoid') 
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models", "contacts.json");
 
-const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const data= await fs.readFile(contactsPath)
+  const contacts = JSON.parse(data);
+  return contacts
+}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts =await listContacts()
+  const filteredContact=contacts.find(data=>{return data.id===contactId})
+  return filteredContact
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts()
+  const filteredContact=contacts.filter(data=>{return data.id!==contactId})
+  console.log(filteredContact);
+  fs.writeFile(contactsPath,JSON.stringify(filteredContact))
+}
+
+const addContact = async (body) => {
+  const data =await listContacts()
+  const newUser={
+    id:nanoid(),
+    name:body.name,
+    email:body.email,
+    phone:body.phone,
+  }
+  data.push(newUser)
+  
+  fs.writeFile(contactsPath,JSON.stringify(data))
+ 
+  return newUser
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts =await listContacts()
+  const filteredContact=contacts.find(data=>{return data.id===contactId})
+  filteredContact.name=body.name
+  filteredContact.email=body.email
+  filteredContact.phone=body.phone
+  fs.writeFile(contactsPath,JSON.stringify(contacts))
+  return filteredContact
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,54 @@
-const express = require('express')
+const express = require("express");
+const {userValidation}=require("../../validation/user")
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const data = await listContacts();
+  res.status(200).json(data);
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const data = await getContactById(req.params.contactId);
+  if (!data) {
+    res.status(404).json({ "message": "Not found" });
+  }
+  res.status(200).json(data);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const {error}=userValidation(req.body)
+  if(!error){
+    const result=await addContact(req.body)
+   return res.status(201).json(result);
+  }
+  res.status(400).json({ "message": "missing required name field"});
+ 
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const data = await getContactById(req.params.contactId);
+  if (!data) {
+    res.status(404).json({ "message": "Not found" });
+  }
+  await removeContact(req.params.contactId);
+  res.status(200).json({ "message": "contact deleted" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const {error}=userValidation(req.body)
+  if(!error){
+    const result=await updateContact(req.params.contactId,req.body)
+   return res.status(200).json(result);
+  }
+  res.status(400).json( {"message": "missing fields"});
+ 
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
