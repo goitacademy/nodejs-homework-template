92190f3c403dc51914d0ff4f798f9bac28e11432
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-04-30 18:09:06 +0300

    added controller

--- controllers/contacts/getContactsById.js
+++ controllers/contacts/getContactsById.js
@@ -5,7 +5,7 @@ const getContactsById = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findById(contactId);
   if (!result) {
-    throw createError(404);
+    throw createError(404, `contact with id=${contactId} not found`);
   }
   res.json({ status: 200, message: "success", data: result });
 };
---
+++ controllers/contacts/updateStatusContact.js
@@ -0,0 +1,24 @@
+const { Contact } = require("../../models");
+const { createError } = require("../../helpers");
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+  const contact = await Contact.findByIdAndUpdate(
+    contactId,
+    { favorite },
+    { new: true }
+  );
+  if (!contact) {
+    throw createError(404, `contact with id=${contactId} not found`);
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: contact,
+    },
+  });
+};
+
+module.exports = updateStatusContact;
