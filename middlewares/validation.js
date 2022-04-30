6d3d44506bee1828c227973eea642038a594bfb3
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-04-30 18:09:43 +0300

    fix: error messages

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,10 +1,14 @@
 const { createError } = require("../helpers");
 
-const validation = (schema) => {
+const validation = (schema, message = null) => {
   return (req, _, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(createError(error));
+      if (message) {
+        next(createError(400, message));
+      }
+      const { message: errMsg } = error;
+      next(createError(400, errMsg));
     }
     next();
   };
