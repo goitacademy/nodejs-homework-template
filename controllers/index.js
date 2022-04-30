c50e55fb8c359f78a62c29aeb515b4b077468013
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-04-30 18:08:19 +0300

    added reexport controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,14 @@ const getAllContacts = require("./contacts/getAllContacts");
 const getContactById = require("./contacts/getContactsById");
 const addContact = require("./contacts/addContact");
 const removeContact = require("./contacts/removeContact");
+const updateContact = require("./contacts/updateContact");
+const updateStatusContact = require("./contacts/updateStatusContact");
 
 module.exports = {
   getAllContacts,
   getContactById,
   addContact,
   removeContact,
+  updateContact,
+  updateStatusContact,
 };
