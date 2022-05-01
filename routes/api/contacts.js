4f673d0bf94f74aeafaacd22791d0c40035b7c36
IhorShvydkyi <igorshvydky35@gmail.com>
2022-05-01 12:59:22 +0300

    try first commit in hw02 branch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	res.json({ message: 'template message, first commit' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	res.json({ message: 'template message' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	res.json({ message: 'template message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	res.json({ message: 'template message' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	res.json({ message: 'template message' })
 })
 
 module.exports = router
