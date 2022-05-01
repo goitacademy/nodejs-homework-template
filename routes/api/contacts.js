d3e2af1b532d3c313d53cdd30c82c0b6a62b1584
IhorShvydkyi <igorshvydky35@gmail.com>
2022-05-01 14:33:44 +0300

    add get contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const Joi = require('joi')
 
 const router = express.Router()
 
+const {
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+	updateContact,
+} = require('../../models/contacts')
+
 router.get('/', async (req, res, next) => {
-	res.json({ message: 'template message, first commit' })
+	const contacts = await listContacts()
+	res.status(200).json({ contacts })
 })
 
 router.get('/:contactId', async (req, res, next) => {
