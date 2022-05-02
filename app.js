395f95bff6cf118cc6cb48a71a7267be7440a7d5
IhorShvydkyi <igorshvydky35@gmail.com>
2022-05-02 20:27:03 +0300

    remove db_host

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const { DB_HOST } = require('./config')
 
 const contactsRouter = require('./routes/api/contacts')
 
+mongoose
+	.connect(DB_HOST)
+	.then(() => console.log('Database connection successful'))
+	.catch((error) => console.log(error.message))
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -15,11 +22,11 @@ app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+	res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+	res.status(500).json({ message: err.message })
 })
 
 module.exports = app
