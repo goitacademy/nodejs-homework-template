1bda3cff3bef336645a3f9524c29dd7fa65fa898
IhorShvydkyi <igorshvydky35@gmail.com>
2022-05-03 08:52:46 +0300

    add book schema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,26 +3,28 @@ const Joi = require('joi')
 
 const router = express.Router()
 
-const {
-	listContacts,
-	getContactById,
-	removeContact,
-	addContact,
-	updateContact,
-} = require('../../models/contacts')
+const Book = require('../../models/contact')
 
-router.get('/', async (req, res, next) => {
-	const contacts = await listContacts()
-	res.status(200).json({ contacts })
-})
+// const {
+// 	listContacts,
+// 	getContactById,
+// 	removeContact,
+// 	addContact,
+// 	updateContact,
+// } = require('../../models/contacts')
 
-router.get('/:contactId', async (req, res, next) => {
-	const [contact] = await getContactById(req.params.contactId)
-	if (!contact) {
-		res.status(404).json({ message: 'Not found' })
-	}
-	res.status(200).json({ contact })
-})
+// router.get('/', async (req, res, next) => {
+// 	const contacts = await listContacts()
+// 	res.status(200).json({ contacts })
+// })
+
+// router.get('/:contactId', async (req, res, next) => {
+// 	const [contact] = await getContactById(req.params.contactId)
+// 	if (!contact) {
+// 		res.status(404).json({ message: 'Not found' })
+// 	}
+// 	res.status(200).json({ contact })
+// })
 
 router.post('/', async (req, res, next) => {
 	const schema = Joi.object({
@@ -55,44 +57,44 @@ router.post('/', async (req, res, next) => {
 	res.status(200).json({ newContact })
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-	const contact = await getContactById(req.params.contactId)
-	if (contact.length === 0) {
-		res.status(400).json({ message: 'Not found' })
-	}
-	await removeContact(req.params.contactId)
-	res.status(200).json({ message: 'Contact deleted' })
-})
+// router.delete('/:contactId', async (req, res, next) => {
+// 	const contact = await getContactById(req.params.contactId)
+// 	if (contact.length === 0) {
+// 		res.status(400).json({ message: 'Not found' })
+// 	}
+// 	await removeContact(req.params.contactId)
+// 	res.status(200).json({ message: 'Contact deleted' })
+// })
 
-router.put('/:contactId', async (req, res, next) => {
-	const schema = Joi.object({
-		name: Joi.string()
-			.pattern(/^[a-zA-Zа-яА-ЯёЁ\s]+$/)
-			.min(3)
-			.max(20)
-			.required(),
-		phone: Joi.string()
-			.length(10)
-			.pattern(/^[0-9]+$/)
-			.required(),
-		email: Joi.string()
-			.required()
-			.email({
-				minDomainSegments: 2,
-				tlds: { allow: ['com', 'net', 'uk', 'org'] },
-			}),
-	})
-	const validatedResult = schema.validate(req.body)
-	if (validatedResult.error) {
-		res.status(400).json({
-			status: validatedResult.error.details.map((x) => x.message),
-			message: `missing required ${validatedResult.error.details.map(
-				(x) => x.context.key
-			)} field`,
-		})
-	}
-	const updContact = await updateContact(req.params.contactId, req.body)
-	req.status(200).json({ updContact })
-})
+// router.put('/:contactId', async (req, res, next) => {
+// 	const schema = Joi.object({
+// 		name: Joi.string()
+// 			.pattern(/^[a-zA-Zа-яА-ЯёЁ\s]+$/)
+// 			.min(3)
+// 			.max(20)
+// 			.required(),
+// 		phone: Joi.string()
+// 			.length(10)
+// 			.pattern(/^[0-9]+$/)
+// 			.required(),
+// 		email: Joi.string()
+// 			.required()
+// 			.email({
+// 				minDomainSegments: 2,
+// 				tlds: { allow: ['com', 'net', 'uk', 'org'] },
+// 			}),
+// 	})
+// 	const validatedResult = schema.validate(req.body)
+// 	if (validatedResult.error) {
+// 		res.status(400).json({
+// 			status: validatedResult.error.details.map((x) => x.message),
+// 			message: `missing required ${validatedResult.error.details.map(
+// 				(x) => x.context.key
+// 			)} field`,
+// 		})
+// 	}
+// 	const updContact = await updateContact(req.params.contactId, req.body)
+// 	req.status(200).json({ updContact })
+// })
 
 module.exports = router
