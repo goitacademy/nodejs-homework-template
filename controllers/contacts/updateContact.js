25b978c225afa1223f19d51e9b11ac4ed4c3e1d7
YuraBatrak <yurabatrak@gmail.com>
2022-05-03 14:39:03 +0300

    update

---
+++ controllers/contacts/updateContact.js
@@ -0,0 +1,21 @@
+const { joiSchema } = require("../../models/contacts");
+const { createError } = require("../../helpers");
+const updateContact = async (req, res, next) => {
+  try {
+    const { error } = joiSchema.validate(req.body);
+    if (error) {
+      throw createError(400, "missing fields");
+    }
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    if (!result) {
+      throw createError(404);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = updateContact;
