75f8650d5a5f36f73ca5b6a6ea7061134ef40d63
Jaroslava8 <jaroslava.moroziuk@gmail.com>
2022-05-03 21:23:07 +0100

    hw04

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,13 +1,21 @@
 const { Contact } = require("../../models/index");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({});
-  console.log('get all contacts - successful')
+  const { _id } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  });
+
+  console.log("get all contacts - successful");
+
   res.json({
     status: "success",
     code: 200,
     data: {
-      result,
+      result: contacts,
     },
   });
 };
