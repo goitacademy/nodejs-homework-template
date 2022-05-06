7a28cbed9a9013e7b2cfb9fb0d5d86db8007d847
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-05-06 16:58:10 +0200

    Added pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,15 @@
 const { Contact } = require('../../models/contact');
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({}, '-createdAt -updatedAt');
+  const { _id } = req.user;
+  const { page = 1, limit = 5 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner: _id }, '-createdAt -updatedAt', {
+    skip,
+    limit: Number(limit),
+  }).populate('owner', '_id email ');
+
   res.json(result);
 };
 
