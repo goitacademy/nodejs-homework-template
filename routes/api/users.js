4eae9bfc8d61f51ff0d12e44f2ecb1a42a93c9cd
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-05-06 17:06:23 +0200

    Added user router

---
+++ routes/api/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const { authorization, validation } = require('../../middlewares');
+const { joiUserSchema, joiSubscriptionSchema } = require('../../models/user');
+const { ctrlWrapper } = require('../../helpers');
+const controllers = require('../../controllers/users');
+
+const router = express.Router();
+
+router.post(
+  '/signup',
+  validation(joiUserSchema),
+  ctrlWrapper(controllers.signup)
+);
+
+router.post(
+  '/login',
+  validation(joiUserSchema),
+  ctrlWrapper(controllers.login)
+);
+
+router.post('/logout', authorization, ctrlWrapper(controllers.logout));
+
+router.get('/current', authorization, ctrlWrapper(controllers.getCurrent));
+
+router.patch(
+  '/:userId/subscription',
+  validation(joiSubscriptionSchema),
+  ctrlWrapper(controllers.updateSubscription)
+);
+
+module.exports = router;
