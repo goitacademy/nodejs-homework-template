ffe80fb9465f503f73e7a86c58ab0f208b82486c
Kateryna <k.borysenko.kyiv@gmail.com>
2022-05-07 01:50:56 +0300

    Added Get by ID, Delete and  Patch  methods REST API

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -5,11 +5,9 @@ const Joi = require("joi");
 const validateAddedContact = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().min(2).max(20).required(),
-        email: Joi.string().email({
-            minDomainSegments: 2,
-            tlds: { allow: ["com", "net"] },
-        }).required(),
+        email: Joi.string().email().required(),
         phone: Joi.string().min(10).max(20).required(),
+        favorite: Joi.boolean()
     });
     const { error } = schema.validate(req.body);
     if (error) {
@@ -21,11 +19,9 @@ const validateAddedContact = (req, res, next) => {
 const validateUpdatedContact = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().min(2).max(20).required(),
-        email: Joi.string().email({
-            minDomainSegments: 2,
-            tlds: { allow: ["com", "net"] },
-        }),
+        email: Joi.string().email(),
         phone: Joi.string().min(10).max(20).required(),
+        favorite: Joi.boolean()
     });
     const { error } = schema.validate(req.body);
     if (error) {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,7 +4,7 @@ const { Contact } = require('../../models/contacts');
 
 const {
   validateAddedContact,
-  // validateUpdatedContact,
+  validateUpdatedContact,
 } = require('../../middlewares/validation');
 
 router.get('/', async (req, res) => {
@@ -15,7 +15,13 @@ router.get('/', async (req, res) => {
   res.send(contactsList);
 });
 
-// router.get('/:contactId', getContactById);
+router.get('/:id', async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    return res.status(500).json({ message: 'Contact with the given ID was not found !' });
+  }
+  res.status(200).json(contact);
+});
 
 router.post('/', validateAddedContact, async (req, res) => {
   let contact = new Contact({
@@ -32,8 +38,59 @@ router.post('/', validateAddedContact, async (req, res) => {
   res.send(contact);
 });
 
-// router.delete('/:contactId', removeContact);
+router.delete('/:id', async (req, res) => {
+  Contact.findByIdAndRemove(req.params.id)
+    .then(contact => {
+      if (contact) {
+        return res.status(200).json({ message: 'the contact is deleted!' });
+      } else {
+        return res.status(404).json({ message: "contact was not found!" });
+      }
+    }).catch(err => {
+      // обработка ошибки - если не придерживаться id-формата mongodb - сработает catch
+      return res.status(500).json({ error: err });
+    })
 
-// router.put('/:contactId', validateUpdatedContact, updateContact);
+});
+
+router.put('/:id', validateUpdatedContact, async (req, res) => {
+  const contact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      favorite: req.body.favorite
+    }, {
+    // для того, чтобы вернуть новые данные 
+    new: true
+  })
+
+  if (!contact)
+    return res.status(400).send('the contact cannot be updated!')
+
+  res.send(contact);
+});
+
+
+router.patch('/:id/favorite', async (req, res) => {
+  try {
+    if (!req.body.favorite) {
+      res.status(400).send('missing field favorite');
+    }
+    else {
+      const contact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        {
+          favorite: req.body.favorite
+        }, {
+        new: true
+      })
+      res.send(contact);
+    }
+  } catch (err) {
+    res.status(404).send('Not found');
+  }
+});
 
 module.exports = router;
