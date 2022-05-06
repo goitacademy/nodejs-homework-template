3ed09ae0b98707f571e9d6e4079e087a8cc52711
ZjeniiaSt <zeniiastupjak@gmail.com>
2022-05-06 18:15:30 +0200

    pagination

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,10 +2,12 @@ const { Contact } = require("../../models/contact");
 
 const getContacts = async (req, res, next) => {
   const { _id } = req.user;
-  const result = await Contact.find(
-    { owner: _id },
-    "-createdAt -updatedAt"
-  ).populate("owner", "email");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner: _id }, "-createdAt -updatedAt", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "email");
   res.json(result);
 };
 
