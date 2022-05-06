5cab173c933db854f59dd5bdd801f33aedfe9a7f
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-05-06 16:59:01 +0200

    Added main files

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
+const authorization = require('./authorization');
 const validation = require('./validation');
 const validationById = require('./validationById');
 
 module.exports = {
+  authorization,
   validation,
   validationById,
 };
