c32c17137ab7ca40f8bc70559f32695dd324759b
Jaroslava8 <jaroslava.moroziuk@gmail.com>
2022-05-07 12:41:38 +0100

    5hw

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+// const multer = require("multer");
+// const path = require("path");
+
 require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
 
 const usersRouter = require("./routes/api/users");
 const contactsRouter = require("./routes/api/contacts");
+const { upload } = require("./middlewares");
 
 const app = express();
 
@@ -16,6 +20,8 @@ app.use(logger(formatsLogger));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"))
+// const path = require("path");
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
@@ -29,5 +35,15 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+
+
+
+
+
+
+app.post("/public", upload.single("image"), async(req, res)=>{
+  console.log(req.file)
+})
 
 module.exports = app;
