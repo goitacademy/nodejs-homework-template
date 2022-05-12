576ab4a394210c1387cda0466e01fbfa5426dac8
Kateryna <k.borysenko.kyiv@gmail.com>
2022-05-12 16:54:16 +0300

    Added userValidation middleware

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -1,1 +1,21 @@
 const Joi = require("joi");
+
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const passwordPattern = /^[a-zA-Z0-9]{3,30}$/;
+
+const userValidation = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().pattern(new RegExp(emailPattern)).required(),
+    password: Joi.string().pattern(new RegExp(passwordPattern)).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.message);
+  }
+
+  next();
+};
+
+
+module.exports = userValidation;
