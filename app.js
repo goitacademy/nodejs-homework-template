ecd5fff0ce82c1adbe447aaea0f509f47b97e958
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-05-13 16:28:29 +0200

    Stworzono połączenie do mongoose

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
 
-const app = express()
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
