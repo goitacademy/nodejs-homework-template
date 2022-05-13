ca5eac0e82d9aeadf1f777e6ed7f9ea143e4d03a
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-05-13 18:39:25 +0200

    Update port

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log('Database connection successful');
 
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log('Server running. Use our API on port: 3000');
     });
   })
