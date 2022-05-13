2edbe388b66ba389c397ce1b8cb36505f057aba3
IhorShvydkyi <igorshvydky35@gmail.com>
2022-05-13 20:46:55 +0300

    add get current user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,4 +9,5 @@ const router = express.Router()
 router.post('/signup', validation(joiSignupSchema), ctrlWrapper(ctrl.signup))
 router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login))
 router.get('/logout', auth, ctrlWrapper(ctrl.logout))
+router.get('/current', auth, ctrlWrapper(ctrl.getCurrent))
 module.exports = router
