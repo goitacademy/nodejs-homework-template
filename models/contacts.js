665f57a3cdce614f73873e4cfe5541d21915f231
RoxanaRoxana <roxana0804@gmail.com>
2022-05-14 14:08:43 +0200

    fix

--- models/contacts.js
+++ models/contacts.js
@@ -5,9 +5,9 @@ const contactsPath = path.resolve("./models/contacts.json");
 
 const listContacts = async () => {
   try {
-    let bufferData = await fs.readFile(contactsPath);
-    let stData = bufferData.toString();
-    let data = JSON.parse(stData);
+    const bufferData = await fs.readFile(contactsPath);
+    const stData = bufferData.toString();
+    const data = JSON.parse(stData);
     return data;
   } catch (err) {
     console.log(err);
@@ -16,9 +16,9 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   try {
-    let bufferData = await fs.readFile(contactsPath);
-    let stData = bufferData.toString();
-    let data = JSON.parse(stData);
+    const bufferData = await fs.readFile(contactsPath);
+    const stData = bufferData.toString();
+    const data = JSON.parse(stData);
     const contact = data.find((item) => item.id === contactId);
     return contact;
   } catch (err) {
@@ -29,15 +29,15 @@ const getContactById = async (contactId) => {
 const removeContact = async (contactId) => {
   let contacts;
   try {
-    let bufferData = await fs.readFile(contactsPath);
-    let stData = bufferData.toString();
+    const bufferData = await fs.readFile(contactsPath);
+    const stData = bufferData.toString();
     contacts = JSON.parse(stData);
   } catch (err) {
     console.log(err);
   }
   if (contacts.find(({ id }) => id === contactId) !== undefined) {
     const filteredContacts = contacts.filter((item) => item.id !== contactId);
-    let updatedData = JSON.stringify(filteredContacts);
+    const updatedData = JSON.stringify(filteredContacts);
     fs.writeFile(contactsPath, updatedData);
     return {
       status: 200,
@@ -80,9 +80,9 @@ const updateContact = async (contactId, body) => {
 
   if (contacts.find(({ id }) => id === contactId) !== undefined) {
     let contact = contacts.find((item) => item.id === contactId);
-    name !== undefined ? (contact.name = name) : contact.name;
-    email !== undefined ? (contact.email = email) : contact.email;
-    phone !== undefined ? (contact.phone = phone) : contact.phone;
+    name ? (contact.name = name) : contact.name;
+    email ? (contact.email = email) : contact.email;
+    phone ? (contact.phone = phone) : contact.phone;
     return {
       status: 201,
       data: contact,
