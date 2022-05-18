d8e913fa6ab9ec2b97deb4a1a486f6d1b752eabb
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2022-05-18 15:28:04 +0200

    hw02

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,146 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const contacts = await fs
+    .readFile(contactsPath)
+    .then((data) => {
+      return {
+        data: JSON.parse(data.toString()),
+        status: "success",
+        code: 200,
+      };
+    })
+    .catch((error) => console.log(error.message));
+  return contacts;
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contact = await fs
+    .readFile(contactsPath)
+    .then((data) => {
+      return JSON.parse(data.toString()).find(
+        (contact) => contact.id === contactId,
+      );
+    })
+    .then((result) => {
+      if (result) {
+        return { data: result, status: "success", code: 200 };
+      } else {
+        return { message: "Not found", status: "error", code: 404 };
+      }
+    })
+    .catch((error) => console.log(error.message));
+  return contact;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  let length = 0;
+  const message = await fs
+    .readFile(contactsPath)
+    .then(async (data) => {
+      length = JSON.parse(data).length;
+      return JSON.parse(data);
+    })
+    .then((contacts) =>
+      fs
+        .writeFile(
+          contactsPath,
+          JSON.stringify(
+            contacts.filter((contact) => contact.id !== contactId),
+          ),
+        )
+        .catch((error) => console.log(error.message)),
+    )
+    .then(async () => {
+      return await fs
+        .readFile(contactsPath)
+        .then(async (data) => {
+          return JSON.parse(data).length === length
+            ? { message: "Not found", status: "error", code: 404 }
+            : { message: "contact deleted", status: "success", code: 200 };
+        })
+        .catch((error) => console.log(error.message));
+    })
+    .catch((error) => console.log(error.message));
+  return message;
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  let newId = 0;
+  const { name, email, phone } = body.value;
+  if (name === undefined || email === undefined || phone === undefined) {
+    return {
+      message: "missing required name field",
+      status: "error",
+      code: 404,
+    };
+  }
+  await fs
+    .readFile(contactsPath)
+    .then((data) => {
+      const array = JSON.parse(data.toString());
+      array.map((item) => Number(item.id) > newId && (newId = Number(item.id)));
+      fs.writeFile(
+        contactsPath,
+        JSON.stringify([
+          ...array,
+          {
+            id: (newId + 1).toString(),
+            name: name,
+            email: email,
+            phone: phone,
+          },
+        ]),
+      ).catch((error) => console.log(error.message));
+    })
+    .catch((error) => console.log(error.message));
+  return {
+    data: {
+      id: (newId + 1).toString(),
+      name: name,
+      email: email,
+      phone: phone,
+    },
+    status: "success",
+    code: 201,
+  };
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  if (name === undefined && email === undefined && phone === undefined) {
+    return { message: "missing fields", status: "error", code: 404 };
+  }
+  let chosenIndex = 0;
+  let filteredContact = {};
+  let updatedContact = {};
+  await fs
+    .readFile(contactsPath)
+    .then((data) => {
+      return JSON.parse(data);
+    })
+    .then((contacts) => {
+      chosenIndex = contacts.indexOf(
+        contacts.find((contact) => contact.id === contactId),
+      );
+      filteredContact = contacts.find((contact) => contact.id === contactId);
+      updatedContact = { ...filteredContact, ...body };
+      contacts.splice(chosenIndex, 1, updatedContact);
+      if (chosenIndex !== -1) {
+        fs.writeFile(contactsPath, JSON.stringify(contacts)).catch((error) =>
+          console.log(error.message),
+        );
+      }
+    })
+    .catch((error) => console.log(error.message));
+  if (chosenIndex === -1) {
+    return { message: "Not found", status: "error", code: 404 };
+  } else {
+    return { data: updatedContact, status: "success", code: 200 };
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +148,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
