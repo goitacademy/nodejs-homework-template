3f133ed7fcd5e2bbee7e778e7dc2a17c8efd20cb
alxtpl <alxtpl@gmail.com>
2022-05-23 11:01:19 +0200

    adding hw02-express

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
+app.use(logger('tiny'))
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
