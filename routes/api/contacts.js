00012938a6c2c3c2036c1f622c0ebe81c3e0c62b
alinasavuyk <87060358+alinasavuyk@users.noreply.github.com>
2022-05-24 13:32:47 +0300

    2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message 123' })
+  res.json({ message: 'template message 333' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
