a476db8602231f1ef5a5a8c4e78705a28245b703
Tyoma <risendrums777@gmail.com>
2022-05-27 01:47:55 +0300

    add hw2

---
+++ middlewares/validation.js
@@ -0,0 +1,53 @@
+const Joi = require('joi')
+
+const schemaAddContact = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'org', 'ru', 'ua'] },
+    })
+    .required(),
+  phone: Joi.string()
+    .pattern(/^[+]?[(]?[0-9]{3}[)]?[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/)
+    .required(),
+})
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .optional(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'org', 'ru', 'ua'] }
+    })
+    .optional(),
+  phone: Joi.string()
+    .pattern(/^[+]?[(]?[0-9]{3}[)]?[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/)
+    .optional(),
+}).min(1)
+
+const validate = (schema, res, obj, next) => {
+  const validationData = schema.validate(obj)
+
+  if (validationData.error) {
+    return res.status(400)
+      .json({ message: validationData.error.message.replace(/"/g, '') })
+  }
+
+  next()
+}
+
+module.exports = {
+  validationAddContact: (req, res, next) => {
+    return validate(schemaAddContact, res, req.body, next)
+  },
+  validationUpdateContact: (req, res, next) => {
+    return validate(schemaUpdateContact, res, req.body, next)
+  }
+}
