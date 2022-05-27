adbd714384ccc1609065e8e211fe15cd180d744e
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2022-05-27 14:20:41 +0200

    hw03 done

--- service/index.js
+++ service/index.js
@@ -4,13 +4,13 @@ const getAllContacts = () => Contact.find().lean()
 
 const getContactById = (contactId) => Contact.findById(contactId)
 
-const addContact = (body) => Contact.insertMany(body)
+const addContact = (body) => Contact.insertMany(body, {new: true})
 
 const removeContact = (id) => Contact.findOneAndDelete({_id: id})
 
-const updateContact = (id, body) => Contact.findOneAndUpdate({_id: id}, body)
+const updateContact = (id, body) => Contact.findOneAndUpdate({_id: id}, body, {new: true})
 
-const updateFavorite = (id, body) => Contact.findOneAndUpdate({_id: id}, body)
+const updateFavorite = (id, body) => Contact.findOneAndUpdate({_id: id}, body, {new: true})
 
 module.exports = {
   getAllContacts,
