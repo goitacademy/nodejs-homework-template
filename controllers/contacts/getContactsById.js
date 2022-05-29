7d95448ebeca41d6bf276c7866e4cc5f3b6ab825
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-29 18:17:59 +0300

    replace functions in separate folders

---
+++ controllers/contacts/getContactsById.js
@@ -0,0 +1,23 @@
+const contactsOperations = require("../../models/contacts");
+const createError = require("http-errors");
+
+const getContactsById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsOperations.getContactById(contactId);
+    if (!result) {
+      throw createError(404, "Not found");
+    }
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getContactsById;
---
+++ controllers/contacts/listContacts.js
@@ -0,0 +1,14 @@
+const contactsOperations = require("../../models/contacts");
+
+const listContacts = async (req, res, next) => {
+  try {
+    const result = await contactsOperations.listContacts();
+    res
+      .status(200)
+      .json({ status: "success", code: 200, data: { result: result } });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = listContacts;
