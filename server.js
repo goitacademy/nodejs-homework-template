324f6e1d02cc8f5fed797c0b5334cda9897d3e06
Olga Ponomarenko <oip@anadeainc.com>
2022-06-02 18:26:59 +0300

    add env variable

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('./app')
+require('dotenv').config()
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const PORT = process.env.PORT || 8081
+
+app.use()
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
 })
