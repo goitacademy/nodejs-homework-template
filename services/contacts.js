ccf1882ba0d6cf03701fdcbda3574a798ddea317
AliVik <85441270+AliVik@users.noreply.github.com>
2022-06-02 20:51:11 +0300

    replace contacts operations

---
+++ services/contacts.js
@@ -0,0 +1,29 @@
+const { Contact } = require("../models");
+
+const listContacts = async () => {
+  return await Contact.find();
+};
+
+const getContactById = async (contactId) => {
+  return await Contact.findById(contactId);
+};
+
+const removeContact = async (contactId) => {
+  return await Contact.findByIdAndDelete(contactId);
+};
+
+const addContact = async (body) => {
+  return await Contact.create(body);
+};
+
+const updateContact = async (id, body) => {
+  return await Contact.findByIdAndUpdate(id, body);
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
