387c6b89b701f13252078efdddde7321e5aedeb7
Olga Ponomarenko <oip@anadeainc.com>
2022-06-03 18:12:31 +0300

    contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,74 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const contactsPath = path.join(__dirname, '/contacts.json')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath, 'utf8')
+    return data
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const result = await listContacts()
+    .then(data => JSON.parse(data).find(({id}) => id === contactId))
+ 
+  return JSON.stringify(result)
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const result = await listContacts()
+    .then(data => JSON.parse(data).filter(({id}) => id !== contactId))
 
-const addContact = async (body) => {}
+    try {
+      await fs.writeFile(contactsPath, JSON.stringify(result))  
+    } catch (err) {
+        console.error(err)
+      }
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (newContact) => {
+  const isExistContact = getContactById(newContact.id).length
+
+  const result = await listContacts()
+    .then(data => [...JSON.parse(data), newContact])
+    
+    try {
+      if (!isExistContact) {
+        await fs.writeFile(contactsPath, JSON.stringify(result))
+      }
+      else { return `Сontact ${newContact.name} already exists!` }
+    } catch (err) {
+        console.error(err)
+      }
+
+  return JSON.stringify(newContact)
+}
+
+const updateContact = async (contactId, body) => {
+
+  const result = await listContacts().then(data => JSON.parse(data))
+  const arayNotChange = result.filter(({id}) => id !== contactId)
+
+  const findContact = result.find(({id}) => id === contactId)
+
+  console.log(findContact)
+
+  const updateContact = {
+    ...findContact,
+    ...body
+  }
+
+  try {
+    if (findContact.length) {
+      await fs.writeFile(contactsPath, JSON.stringify([...arayNotChange, updateContact]))
+      return JSON.stringify(updateContact)
+    }
+    else { return `Сontact ${body.name} don't exists!` }
+  } catch (err) {
+      console.error(err)
+    }
+  
+
+}
 
 module.exports = {
   listContacts,
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 8081
 
-app.use()
-
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
 })
