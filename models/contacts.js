f1dfa3fd5ec83d9278cd2a370c27625918df3196
@s_zhsn <s.zhosan@gmail.com>
2022-06-03 21:06:37 +0300

    hw-02-node-process

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,60 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  return JSON.parse(await fs.readFile(contactsPath));
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(({ id }) => id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const newContacts = contacts.filter(({ id }) => id !== contactId);
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    console.log(`Contact with id=${contactId} was removed`);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const contacts = await listContacts();
+  const newContact = { id: require("nanoid").nanoid(), name, email, phone };
+  const newContacts = [...contacts, newContact];
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return newContact;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const idToUpdate = contacts.findIndex((contact) => contact.id === contactId);
+  if (idToUpdate === -1) {
+    return null;
+  }
+  const { name, email, phone } = body;
+  contacts[idToUpdate] = { id: contactId, name, email, phone };
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contacts[idToUpdate];
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +62,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
