5f58b4b7ebc0da8ba62520c32a2b1afe1ad636b0
slabak <slabakxaker@gmail.com>
2022-06-03 10:27:12 +0300

    validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const contactsDB = require("../../models/contacts");
+const { addValidation, updateValidation } = require("../middlewares/middlewaresapi.js");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-    const allContacts = contactsDB.listContacts();
+    const allContacts = await contactsDB.listContacts();
     res.status(200).json({ message: "status 200", response: allContacts });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-    const contactByID = contactsDB.getContactById(req.params.contactId);
+    const contactByID = await contactsDB.getContactById(req.params.contactId);
     if (!contactByID) {
         res.status(404).json({ message: "status 404", response: contactByID });
         throw new Error(`Contact ${req.params.contactId} not found`);
@@ -18,8 +19,8 @@ router.get("/:contactId", async (req, res, next) => {
 });
 
 // add contact
-router.post("/", async (req, res, next) => {
-    const newContact = contactsDB(req.body);
+router.post("/", addValidation, async (req, res, next) => {
+    const newContact = await contactsDB(req.body);
     if (!newContact) {
         res.status(404).json({ message: "status 404", response: newContact });
         throw new Error(`Contact not created, i am sorry try again`);
@@ -36,8 +37,8 @@ router.delete("/:contactId", async (req, res, next) => {
     res.status(204).json({ message: "status 204", response: removeContact });
 });
 
-router.put("/:contactId", async (req, res, next) => {
-    const editContact = contactsDB(req.params.contactId, req.body);
+router.put("/:contactId", updateValidation, async (req, res, next) => {
+    const editContact = await contactsDB(req.params.contactId, req.body);
     if (!editContact) {
         res.status(404).json({ message: "status 404", response: editContact });
         throw new Error(`Contact ${req.params.contactId} not found`);
---
+++ routes/middlewares/middlewaresapi.js
@@ -0,0 +1,37 @@
+const joi = require("joi");
+
+const addValidation = (req, res, next) => {
+    const schema = joi.object({
+        name: joi.string().alphanum().min(3).max(30).required(),
+        email: joi
+            .string()
+            .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+            .required(),
+        phone: joi.string().min(3).max(30).required(),
+    });
+    const resultValidation = schema.validate(req.body);
+    if (resultValidation.error) {
+        return res.status(400).json({ message: "No validate", response: null });
+    }
+    next();
+};
+const updateValidation = (req, res, next) => {
+    const schema = joi.object({
+        name: joi.string().alphanum().min(3).max(30).required(),
+        email: joi
+            .string()
+            .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+            .required(),
+        phone: joi.string().min(3).max(30).required(),
+    });
+    const resultValidation = schema.validate(req.body);
+    if (resultValidation.error) {
+        return res.status(400).json({ message: "No validate", response: null });
+    }
+    next();
+};
+
+module.exports = {
+    addValidation,
+    updateValidation,
+};
