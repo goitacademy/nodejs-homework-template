d7b83de39dda43ffb7c8894b8903d454f287d032
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2022-06-03 14:13:54 +0300

    init

--- app.js
+++ app.js
@@ -13,13 +13,11 @@ app.use(logger(process.env.NODE_ENV === "dev" ? "dev" : "short"));
 
 app.use("/api/contacts", contactsRouter);
 
-// for not correct endpoints
 app.use((req, res) => {
   console.log("middleware1");
   res.status(404).json({ message: "Not found" });
 });
 
-// for all errors
 app.use((err, req, res, next) => {
   console.log("middleware2");
   res.status(err.status || 500).json({ message: err.message });
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: 3000`);
-});
+const { PORT = 3000, DB_HOST } = process.env;
+
+const DB_HOST1 = "";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("DB is working");
+    app.listen(PORT);
+  })
+  .then(() => {
+    console.log(`server is on ${PORT}`);
+  })
+  .catch((err) => {
+    console.log("ERROR", err);
+    process.exit(1);
+  });
