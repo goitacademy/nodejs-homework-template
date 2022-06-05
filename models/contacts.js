0b0360706aa1024585c56865a2c500b47659991f
o.shepel <o.shepel@abtollc.com>
2022-06-05 20:19:22 +0300

    add DB controller

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,71 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const write = async payload => {
+  await fs.writeFile(contactsPath, JSON.stringify(payload, null, 2));
+};
 
-const removeContact = async (contactId) => {}
+/**
+ * Returns all contacts from DB.
+ */
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+/**
+ * Returns contact with the specified ID.
+ */
+const getContactById = async contactId => {
+  const contacts = await listContacts();
+  return contacts.find(contact => contact.id === contactId);
+};
 
-const updateContact = async (contactId, body) => {}
+/**
+ * Removes contact with the specified ID.
+ */
+const removeContact = async contactId => {
+  const contacts = await listContacts();
+  for (let i = 0; i < contacts.length; i += 1) {
+    if (contacts[i].id === contactId) {
+      const removed = contacts.splice(i, 1).at(0);
+      await write(contacts);
+      return removed;
+    }
+  }
+  return null;
+};
+
+/**
+ * Adds contact with specified params.
+ * Generates new id automatically.
+ */
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const payload = { id: nanoid(), ...body };
+  contacts.push(payload);
+  await write(contacts);
+  return payload;
+};
+
+/**
+ * Updates existing contact fields with new values.
+ */
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  for (let i = 0; i < contacts.length; i += 1) {
+    if (contacts[i].id === contactId) {
+      const payload = {...contacts[i], ...body};
+      contacts.splice(i, 1, payload).at(0);
+      await write(contacts);
+      return payload;
+    }
+  }
+  return null;
+}
 
 module.exports = {
   listContacts,
