bef7b83fb37ee7e9d2ef46e342f18c3e0ffcd710
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-06-07 15:25:11 +0200

    Ostateczne poprawki

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.patch(
         })
         .catch(async (err) => {
           await fs.unlink(temporaryName)
-          next(err);
+          return res.status(400).json({ message: "this is not a photo" });
         });
 
       await fs.rename(temporaryName, fileName);
