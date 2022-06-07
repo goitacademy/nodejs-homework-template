119118345aca30cea3d05ec5c180d4051c5478f9
sokorevvvladimir <sokorevvvladimir@gmail.com>
2022-06-07 18:35:18 +0200

    v1 all in one file

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,111 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
+const router = express.Router();
+const schema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
 
-const router = express.Router()
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  phone: Joi.number(),
+});
+const contactsOperations = require("../../models/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await contactsOperations.listContacts();
+    res.json({
+      status: 200,
+      data: { contacts },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await contactsOperations.getContactById(contactId);
+    if (!contact) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error;
+    }
+    res.json({
+      status: 200,
+      data: { contact },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { body } = req;
+    const { error } = schema.validate(body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    const result = await contactsOperations.addContact(body);
+    res.status(201).json({
+      status: 201,
+      data: { result },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contactToRemove = await contactsOperations.removeContact(contactId);
+    if (!contactToRemove) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { body } = req;
+    const { error } = schema.validate(body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    const { contactId } = req.params;
+    const result = await contactsOperations.updateContact(contactId, body);
+
+    if (!result) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error;
+    }
+    res.status(200).json({
+      status: 200,
+      data: { result },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
