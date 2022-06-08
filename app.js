08b39a983672e756732fb46496ca78751cd5f274
SerheijTsotsorin <84991425+SerheijTsotsorin@users.noreply.github.com>
2022-06-08 20:11:39 +0300

    add .env

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const contactsRouter = require('./routes/api/contacts')
+dotenv.config();
 
-const app = express()
+
+const { DB_HOST } = process.env;
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);    
+  });
+
+
+const contactsRouter = require('./routes/api/contacts');
+// const { required } = require('joi');
+
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
-})
+});
 
 app.use((err, req, res, next) => {
   const { status = 500 } = err;
   res.status(status).json({ message: err.message })
-})
+});
 
-module.exports = app
+module.exports = app;
+
+// LzS5JwoB6QKsuiF8
+// 
