809ae99d5d1e78b0642d53a6a10f2d21bfcbe7d9
albert95gt <albitakacs@gmail.com>
2022-06-11 16:29:54 +0200

    refactoring

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,13 +3,16 @@ const { Contact } = require('../../models');
 const getAll = async (req, res, next) => {
   try {
     const { page = 1, limit = 10, favorite = false } = req.query;
+    const { _id } = req.user;
+
     const skipped = (page - 1) * limit;
     const skip = skipped < 0 ? 0 : skipped;
-    const { _id } = req.user;
+
     const contacts = await Contact.find({ owner: _id, favorite }, '', {
       skip,
       limit: Number(limit),
     }).populate('owner', '_id email subscription');
+
     res.json(contacts);
   } catch (error) {
     next(error);
