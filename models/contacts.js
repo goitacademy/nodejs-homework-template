ef05514a39f2bfed5045bfdd805d8a74d2bcaf0e
sergiosergio2 <vzdor5555@gmail.com>
2022-06-12 14:41:02 +0300

    add functions for file works

--- models/contacts.js
+++ models/contacts.js
@@ -17,11 +17,41 @@ const getContactById = async (contactId) => {
     
 };
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const data  = await listContacts();
+  const newData = data.filter(x => x.id !== contactId);
+ await fs.writeFile(contactsPath, JSON.stringify(newData), 'utf8');
+ return data.length !== newData.length;
+};
 
-const addContact = async (body) => {};
+const addContact = async (body) => {
+  
+    
+try {
+    const data = await listContacts();
+    const newData = [...data, body];
+    await fs.writeFile(contactsPath, JSON.stringify(newData), 'utf-8');
 
-const updateContact = async (contactId, body) => {};
+
+return body;
+} catch(err) {
+    console.error(err);
+}
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const data = await listContacts();
+    const elemById = data.find((elems) => elems.id === contactId);
+    elemById.name = body.name;
+    elemById.email = body.email;
+    elemById.phone = body.phone;
+    await fs.writeFile(contactsPath, JSON.stringify(data), 'utf8');
+    return elemById;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   listContacts,
