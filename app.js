c9bbfb18b04d0704434fbf7abd315cf315b7ffd2
Lyutink <krivenko.lyuda@gmail.com>
2022-06-13 15:57:44 +0300

    done

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+require('dotenv').config();
+
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(logger(process.env.NODE_ENV === 'dev' ? 'dev' : 'tiny'));
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/v1/contacts', contactsRouter)
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Not found22' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+   const { status = 500, message = "Internal Server Error" } = err;
+   res.status(status).json({message});
 })
 
-module.exports = app
+module.exports = app;
