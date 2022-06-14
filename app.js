a20261e8ce412229b717d378c1d40919765412a7
Lyutink <krivenko.lyuda@gmail.com>
2022-06-14 19:01:28 +0300

    fix messege error

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(logger(process.env.NODE_ENV === "dev" ? "dev" : "tiny"));
 
-app.use(cors())
-app.use(express.json())
-app.use(logger(process.env.NODE_ENV === 'dev' ? 'dev' : 'tiny'));
-
-app.use('/api/v1/contacts', contactsRouter)
+app.use("/api/v1/contacts", contactsRouter);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Not found22' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-   const { status = 500, message = "Internal Server Error" } = err;
-   res.status(status).json({message});
-})
+  const { status = 500, message = "Internal Server Error" } = err;
+  res.status(status).json({ message });
+});
 
 module.exports = app;
