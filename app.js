c964404186e449f97a72c52c7a527cafbc5ff614
ElenaStepanchuk <podolyak365@gmail.com>
2022-06-15 22:15:26 +0300

    .env

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
