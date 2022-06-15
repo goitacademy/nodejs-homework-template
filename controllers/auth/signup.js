6b2256a24d3ef4b55ded50757cea97b480a8eb17
AliVik <85441270+AliVik@users.noreply.github.com>
2022-06-15 18:58:06 +0300

    add password hashing

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,16 +1,17 @@
 const { Conflict } = require("http-errors");
-
 const { findUser, createUser } = require("../../services/auth");
+const { hashPassword } = require("../../middlewares/passwordHash");
 
 const signup = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await findUser({ email });
+    const hashedPassword = hashPassword(password);
 
     if (user) {
       throw new Conflict(`Email in use`);
     }
-    await createUser({ email, password });
+    await createUser(email, hashedPassword);
     res.status(201).json({
       user: {
         email,
