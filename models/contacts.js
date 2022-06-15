3759534dd69c0c6fecb3e7385dabe2d94db4c51c
nessy126 <nessy126@gmail.com>
2022-06-15 13:22:13 +0200

    finished with unknown mistake about error.status. Almost everywhere error.status = 500 - it's going from app.js. Probaply I have to throw smth different

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,68 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const contactsPath = path.join(__dirname, './contacts.json')
+const shortid = require('shortid');
 
-const listContacts = async () => {}
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+}
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const list = JSON.parse(await fs.readFile(contactsPath))
+  return list
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts()
+  const contact = contacts.find(contact => contactId === contact.id)
 
-const addContact = async (body) => {}
+  return contact;
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts()
+  const index = contacts.findIndex(item => item.id === contactId)
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  console.log(result);
+  return result;
+}
+
+const addContact = async (body) => {
+  const contacts = await listContacts()
+   const {
+     name,
+     email,
+     phone
+   } = body;
+
+   const newContact = {
+     id: shortid.generate(),
+     name,
+     email,
+     phone,
+  }
+
+  contacts.push(newContact)
+  await updateContacts(contacts)
+  
+  return newContact
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts()
+  const index = contacts.findIndex(item => item.id === contactId)
+   if (index === -1) {
+     return null;
+   }
+
+  contacts[index] = { contactId, ...body }
+  await updateContacts(contacts)
+  return contacts[index]
+}
 
 module.exports = {
   listContacts,
@@ -16,4 +70,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  updateContacts,
 }
