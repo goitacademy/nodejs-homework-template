67222bf4a8b4c1a8b3d9deafad0384b6724e9aab
chetkiy777 <egunka1988@gmail.com>
2022-06-15 22:15:22 +0300

    add crud operation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,36 @@
 const express = require('express')
+const contacts = require('../../models/contacts.json')
+const {v4} = require('uuid')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts);
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  const {id} = req.params
+  const result = contacts.find(item => item.id === id)
+  if (!result) {
+    res.status(404)
+  }
+  
+  res.json(result)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contact = {...req.body, id: v4()}
+  contacts.push(contact)
+  res.json(contact).status(201)
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', async (req, res, next) => {
+  const {id} = req.params
+  const removed =  contacts.filter(item => item.id !== id)
+  res.json(removed).status(200)
 })
 
-router.put('/:contactId', async (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
