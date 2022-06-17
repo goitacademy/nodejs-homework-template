6ba716bfb5d184428267c974050991846e66c2aa
Olga Ponomarenko <oip@anadeainc.com>
2022-06-17 15:37:56 +0300

    addContact

--- src/controllers/contacts/addContact.js
+++ src/controllers/contacts/addContact.js
@@ -3,15 +3,9 @@ const { Contact } = require('../../../models/contactSchema')
 const addContactController = async (req, res, next) => {
   const { body } = req
 
-    await Contact.create({body})
-      .then(data => {
-        res.status(201).json({ body: data, code: 201, status: 'success' })
-          console.log('body', body)
-      })
-      .catch(err => {
-        res.status(404).json({ message: err.Error, code: 404, status: 'falure' })
-        console.log(`error info: ${err}`)
-      })
+    await Contact.create(body)
+      .then(data => res.status(201).json({ body: data, code: 201, status: 'success' }))
+      .catch(err => res.status(400).json({ message: err.message, code: 400, status: 'falure' }))
 }
 
 module.exports = {
