353e7d9ad40c92c559641efe2ac5dcf8f979f4c4
Denis Fedash <0501054104d@gmail.com>
2022-06-18 12:13:38 +0300

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Not found!!!!' })
 })
 
 app.use((err, req, res, next) => {
