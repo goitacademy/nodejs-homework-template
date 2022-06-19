c40415f0a454d4a2385e6193cc94d04387383784
Aleksandr-Pod <stdfire@gmail.com>
2022-06-19 15:21:33 +0300

    'makeError-function'

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,20 +3,26 @@ const express = require('express')
 const router = express.Router()
 
 const action = require('../../models/contacts')
-const Joi = require('joi');
+const Joi = require('joi')
+const createError = require('../../helpers/error')
 
 const postSchema = Joi.object({
   name: Joi.string().max(50).required(),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ru'] } }).required(),
   phone: Joi.string().pattern(/^[0-9+()-_ ]*$/).max(20).required()
 })
+const putSchema = Joi.object({
+  name: Joi.string().max(50),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ru'] } }),
+  phone: Joi.string().pattern(/^[0-9+()-_ ]*$/).max(20)
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const result = await action.listContacts()
     res.json(result)
-  } catch (error) { next(error)
-    // const {status = 500, message = "Server error"} = error
-    // res.status(status).json({ message })
+  } catch (error) {
+    next(error)
   }
 })
 
@@ -24,14 +30,11 @@ router.get('/:contactId', async (req, res, next) => {
   try {
     const result = await action.getContactById(req.params.contactId)
     if (!result) {
-      const error = new Error('Not Found')
-      error.status = 404
-      throw error
+      throw createError(404)
     }
     res.json(result)
-  } catch (error) { next(error)
-    // const {status = 500, message = "Server error"} = error
-    // res.status(status).json({ message })
+  } catch (error) {
+    next(error)
   }
 })
 
@@ -42,16 +45,10 @@ router.post('/', async (req, res, next) => {
       error.status = 400
       throw new Error(error.message)
     }
-    // if (!req.body.name || !req.body.email || !req.body.phone) {
-    //   const error = new Error('missing required name field')
-    //   error.status = 400
-    //   throw error
-    // }
       const result = await action.addContact(req.body)
       res.json(result)
-  } catch (error) { next (error)
-    // const {status = 500, message = "Server error"} = error
-    // res.status(status).json({ message })
+  } catch (error) {
+    next(error)
   }
 })
 
@@ -59,35 +56,32 @@ router.delete('/:contactId', async (req, res, next) => {
   try {
     const result = await action.removeContact(req.params.contactId)
     if (result === null) {
-      const error = new Error('Not found')
-      error.status = 404
-      throw error
+      throw createError(404)
     }
     res.json({ message: 'contact deleted' })
-  } catch (error) { next(error)
-    // const { status = 500, message = "Server error" } = error
-    // res.status(status).json({message})
+  } catch (error) {
+    next(error)
   }
-
 })
 
 router.put('/:contactId', async (req, res, next) => {
+  
   try {
-    const {name, email, phone} = req.body
-    if (!name && !email && !phone) {
-      const error = new Error('missing fields')
+    const { error } = putSchema.validate(req.body)
+    if (error) {
       error.status = 400
-      throw error
+      throw new Error(error.message)
+    }
+  const {name, email, phone} = req.body
+    if (!name && !email && !phone) {
+      throw createError(400, 'Missing Fields')
     }
     const result = await action.updateContact(req.params.contactId, req.body)
     if (result === null) { // если контакта с таким id нет
-      const error = new Error('Not Found')
-      error.status = 404
-      throw error
+      throw createError(404)
     }
     res.json(result)
   } catch (error) { next(error) }
-  
 })
 
 module.exports = router
