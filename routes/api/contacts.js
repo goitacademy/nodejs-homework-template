e5b2e8c6457d6e8744bd4146d2ad5360781910c5
sergiosergio2 <vzdor5555@gmail.com>
2022-06-19 20:18:53 +0300

    add router favorite

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,17 +7,29 @@ const router = express.Router();
 
 
 router.get('/', async (req, res, next) => {
+  try {
   const base = await Contacts.find();
   res.status(200).json(base);
+  } catch (e) {
+    console.error(e);
+    next(e);
+
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
+  
+  try {
+  
   const data = await Contacts.findById(req.params.contactId);
   if (data) {
     res.status(200).json({ data });
   } else {
     res.status(400).json({ message: "Not found" });
   }
+} catch (e) {
+  console.error(e);
+}
   
 });
 
@@ -54,6 +66,32 @@ router.put('/:contactId', async (req, res, next) => {
 
   }
   res.status(404).json({ message: "Not found" });
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  const { contactId } = req.params;
+  const body = req.body;
+
+  if (typeof body?.favorite !== "boolean") {
+      res.status(400).json({ message: "missing field favorite" });
+  }
+  try {
+      const result = await Contacts.findByIdAndUpdate(
+          contactId,
+          { favorite: body.favorite },
+          { new: true }
+      );
+      if (result) {
+          res.status(200).json({ message: result });
+      } else {
+          res.status(404).json({
+              message: ` Not found task id: ${contactId}`,
+          });
+      }
+  } catch (e) {
+      console.error(e);
+      next(e);
+  }
 });
 
 module.exports = router;
