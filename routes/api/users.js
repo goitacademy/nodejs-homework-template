95ef9c19d42b3a04af08ab9d2f1f4f58d9586012
AliVik <85441270+AliVik@users.noreply.github.com>
2022-06-19 18:06:56 +0300

    add /current route

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { auth: ctrl, users: usersCtrl } = require("../../controllers");
+const { validation, auth } = require("../../middlewares");
+const { joiUserSchema } = require("../../models/user");
+
+router.post("/signup", validation(joiUserSchema), ctrl.signup);
+
+router.post("/login", validation(joiUserSchema), ctrl.login);
+
+router.get("/current", auth, usersCtrl.getCurrent);
+
+module.exports = router;
