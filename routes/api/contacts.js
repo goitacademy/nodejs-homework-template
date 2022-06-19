b0070860b87cb9395ffe6700c78ba7f2e557175b
chetkiy777 <egunka1988@gmail.com>
2022-06-20 01:24:44 +0300

    all operations done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,37 +1,97 @@
 const express = require('express')
-const contacts = require('../../models/contacts.json')
-const {v4} = require('uuid')
+const contacts = require('../../models/contacts')
+const Joi = require('joi')
+
 
 const router = express.Router()
 
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required()
+})
+
 router.get('/', async (req, res, next) => {
-  res.json(contacts);
+  try {
+    const result = await contacts.listContacts()
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+  
 })
 
 router.get('/:id', async (req, res, next) => {
-  const {id} = req.params
-  const result = contacts.find(item => item.id === id)
-  if (!result) {
-    res.status(404)
+  try {
+    const {id} = req.params
+    const result = await contacts.getContactById(id)
+    if (!result) {
+      const error = new Error("Not Found")
+      error.status = 404
+      throw error
+    }
+    res.json(result)
+  } catch (error) {
+      res.status(500).json({
+        message: error.message
+      })
   }
-  
-  res.json(result)
 })
 
 router.post('/', async (req, res, next) => {
-  const contact = {...req.body, id: v4()}
-  contacts.push(contact)
-  res.json(contact).status(201)
+    try {
+      const {error} = contactAddSchema.validate(req.body)
+      if (error) {
+       res.status(400).json({message: "must been required fields"})      }
+      const result = await contacts.addContact(req.body)
+      res.status(201).json(result)
+    } catch (error) {
+        res.status(500).json({
+        message: error.message
+      })
+    }
 })
 
 router.delete('/:id', async (req, res, next) => {
-  const {id} = req.params
-  const removed =  contacts.filter(item => item.id !== id)
-  res.json(removed).status(200)
+  try {
+    const {id} = req.params
+    const result =  await contacts.removeContact(id)
+    if (!result) {
+      const error = new Error("Not Found")
+      error.status = 404
+      throw error
+  }
+  res.status(200).json({
+    message: "contact deleted"
+  })
+  } catch (error) {
+      res.status(500).json({
+      message: error.message
+    })
+  }
 })
 
 router.put('/:id', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+      const {error} = contactAddSchema.validate(req.body)
+      if (error) {
+       res.status(400).json({message: "must been required fields"})      }
+      const {id} = req.params
+      const result = await contacts.updateContactById(id, req.body)
+      if (!result) {
+        res.status(404).json({
+          message: "Not Found"
+        })
+      }
+      res.json(result)
+    } catch (error) {
+        res.status(500).json({
+        message: error.message
+      })
+    }
 })
 
 module.exports = router
+
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const app = require('./app')
-const contactsRouter = require('./routes/api/contacts')
-
-app.use('/api/contacts', contactsRouter)
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
