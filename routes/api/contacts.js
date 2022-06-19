5d0c56ff27718472b4b1012c7116cd34ed48e2c2
sergiosergio2 <vzdor5555@gmail.com>
2022-06-19 15:32:04 +0300

    fix server connecting

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { v4: uuidv4 } = require("uuid");
+
 const router = express.Router();
 const {
   listContacts,
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require('./app');
-
+const mongoose = require('mongoose');
+const connecting = mongoose.connect(
+ ' mongodb+srv://1q2w3e:1q2w3e@cluster0.myxww8i.mongodb.net/?retryWrites=true',
+ {
+   db: "contacts",
+ }
+);
+connecting
+.then(() => {
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+  console.log(`Server running. Use our API on port: ${3000}`);
+  console.log("Database connection successful");
 });
+})
+.catch((err) => {
+  console.log(`Server not started.Error message: ${err.message}`)
+  process.exit(1);
+});
+
