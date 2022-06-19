3f94b30e76ff0d97ee1e06238f55302d484e27fc
sergiosergio2 <vzdor5555@gmail.com>
2022-06-19 15:55:54 +0300

    new schema model

--- models/contacts.js
+++ models/contacts.js
@@ -1,62 +1,26 @@
-const fs = require('fs').promises;
-const path = require('path');
-const contactsPath = path.resolve('./models/contacts.json');
 
-const listContacts = async () => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf8');
-    return JSON.parse(data);
-} catch(err) {
-    console.error(err);
-}
-};
-
-const getContactById = async (contactId) => {
-  const data  = await listContacts();
-    return data.find(x => x.id === contactId);
-    
-};
-
-const removeContact = async (contactId) => {
-  const data  = await listContacts();
-  const newData = data.filter(x => x.id !== contactId);
- await fs.writeFile(contactsPath, JSON.stringify(newData), 'utf8');
- return data.length !== newData.length;
-};
-
-const addContact = async (body) => {
-  
-    
-try {
-    const data = await listContacts();
-    const newData = [...data, body];
-    await fs.writeFile(contactsPath, JSON.stringify(newData), 'utf-8');
+const { Schema, model } = require("mongoose");
 
 
-return body;
-} catch(err) {
-    console.error(err);
-}
-};
+const contacts = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+  },
 
-const updateContact = async (contactId, body) => {
-  try {
-    const data = await listContacts();
-    const elemById = data.find((elems) => elems.id === contactId);
-    elemById.name = body.name;
-    elemById.email = body.email;
-    elemById.phone = body.phone;
-    await fs.writeFile(contactsPath, JSON.stringify(data), 'utf8');
-    return elemById;
-  } catch (err) {
-    console.error(err);
   }
-};
+);
+const Contacts = model("contacts", contacts);
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = Contacts;
