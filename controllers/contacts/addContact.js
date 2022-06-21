e3fa1c0e3ff1aea5f14b1ebfdaefa634e897b049
Denis Fedash <0501054104d@gmail.com>
2022-06-21 20:52:44 +0300

    finish

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,14 @@
+const contacts = require('../../models/contacts')
+const { createError } = require('../../helpers')
+const { contactAddSchema } = require('../../schema/schema')
+
+const addContact = async (req, res) => {
+  const { error } = contactAddSchema.validate(req.body)
+  if (error) {
+    throw createError(400, error.message)
+  }
+  const result = await contacts.addContact(req.body)
+  res.status(201).json(result)
+}
+
+module.exports = addContact
