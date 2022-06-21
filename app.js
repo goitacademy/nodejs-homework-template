a5e527df4a0b1531ca2bd6067e6820e3e4916233
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-06-21 15:01:25 +0300

    add Jimp hw05-avatars

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const contactsRouter = require('./routes/api/contacts');
 const authRouter = require('./routes/api/auth');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger(process.env.NODE_ENV === 'development' ? 'dev' : 'short'));
 app.use(cors());
 app.use(express.json());
