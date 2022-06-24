ce09720500d52bb33fb2083e5cdb358a83f36685
slabak <slabakxaker@gmail.com>
2022-06-24 20:30:29 +0300

    logger

--- controllers/contacts/contacts.js
+++ controllers/contacts/contacts.js
@@ -1,8 +1,8 @@
-const contactsDB = require("../../models/contacts");
+const contactModel = require("../../models/contacts");
 
 const getAllContacts = async (req, res, next) => {
     try {
-        const allContacts = await contactsDB.listContacts();
+        const allContacts = await contactModel.find({});
         res.status(200).json({ message: "status 200", response: allContacts });
     } catch (error) {
         res.status(404).json({ message: `Contacts not found`, response: null });
@@ -11,7 +11,7 @@ const getAllContacts = async (req, res, next) => {
 
 const getOneContact = async (req, res, next) => {
     try {
-        const contactByID = await contactsDB.getContactById(req.params.contactId);
+        const contactByID = await contactModel.find({ _id: req.params.contactId });
         res.status(200).json({ message: "status 200", response: contactByID });
     } catch (error) {
         res.status(404).json({ message: `Contact ${req.params.contactId} not found`, response: null });
@@ -20,8 +20,9 @@ const getOneContact = async (req, res, next) => {
 
 const addContact = async (req, res, next) => {
     try {
-        const newContact = await contactsDB.addContact(req.body);
-        res.status(200).json({ message: "status 200", response: newContact });
+        const { name, email, phone, favorite } = req.body;
+        const newContact = await contactModel.addContact({ name, email, phone, favorite });
+        res.status(200).json({ message: "status 201", response: newContact });
     } catch (error) {
         res.status(404).json({ message: "Contact not created, i am sorry try again", response: null });
     }
@@ -29,7 +30,7 @@ const addContact = async (req, res, next) => {
 
 const deletContact = async (req, res, next) => {
     try {
-        const removeContact = await contactsDB.removeContact(req.params.contactId);
+        const removeContact = await contactModel.findByIdAndDelete({ _id: req.params.contactId });
         res.status(204).json({ message: "status 204", response: removeContact });
     } catch (error) {
         res.status(404).json({ message: `Contact ${req.params.contactId} not found`, response: null });
@@ -38,7 +39,7 @@ const deletContact = async (req, res, next) => {
 
 const updateContact = async (req, res, next) => {
     try {
-        const editContact = await contactsDB.updateContact(req.params.contactId, req.body);
+        const editContact = await contactModel.findByIdAndUpdate({ _id: req.params.contactId }, req.body);
         res.status(200).json({ message: "status 200", response: editContact });
     } catch (error) {
         res.status(404).json({ message: `Contact ${req.params.contactId} not found`, response: null });
--- models/contacts.js
+++ models/contacts.js
@@ -1,61 +1,20 @@
-const fs = require("fs/promises");
-const path = require("path");
-const contactsPath = path.join(__dirname, "./contacts.json");
-const { nanoid } = require("nanoid");
+const { Schema, model } = require("mongoose");
 
-const listContacts = async () => {
-    const allContactsText = await fs.readFile(contactsPath);
-    const allContacts = JSON.parse(allContactsText);
-    return allContacts || null;
-};
+const schema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Set name for contact"],
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
+});
 
-const getContactById = async (contactId) => {
-    const allContacts = await listContacts();
-    const contactByID = allContacts.filter((item) => item.id === contactId);
-    return contactByID || null;
-};
-
-const removeContact = async (contactId) => {
-    const allContacts = await listContacts();
-    const contactByID = allContacts.filter((item) => item.id === contactId);
-    if (!contactByID) {
-        return null;
-    }
-    const newContacts = allContacts.filter((item) => item.id !== contactId);
-    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-    return contactByID;
-};
-
-const addContact = async (body) => {
-    if (!body) return null;
-    const allContacts = await listContacts();
-    const newContact = { id: nanoid(), ...body };
-    allContacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-    return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-    const allContacts = await listContacts();
-    let contactByID = null;
-    const newContacts = allContacts.map((item) => {
-        if (item.id === contactId) {
-            item = { id: contactId, ...body };
-            contactByID = item;
-        }
-        return item;
-    });
-    if (!contactByID) {
-        return null;
-    }
-    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-    return contactByID;
-};
-
-module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-    updateContact,
-};
+module.exports = model("contact", schema);
