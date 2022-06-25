b47b01c1ccee6a2858abdceb923c1bd873d59b87
Oleh Semennykov <sumcue@gmail.com>
2022-06-25 15:02:26 +0300

    add jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,15 +1,20 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
+  console.log(tempUpload);
   const { _id: id } = req.user;
-  const imageName = `${id}_${originalname}`;
+  const imageName = `${id}${path.extname(originalname)}`;
   try {
     const resultUpload = path.join(avatarsDir, imageName);
+    const resizeAvatarURL = await Jimp.read(tempUpload);
     await fs.rename(tempUpload, resultUpload);
+    resizeAvatarURL.resize(250, 250).write(resultUpload);
     const avatarURL = path.join("public", "avatars", imageName);
+
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
   } catch (error) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
-
-// const multer = require("multer");
-const path = require("path");
-// const fs = require("fs/promises");
-// const avatarDir = path.join(__dirname, "../../temp");
-
-
-const usersDir = path.join(__dirname, "../../public/avatars");
-console.log(usersDir);
-// const multerConfig = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, avatarDir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-//   limits: {
-//     fileSize: 2048,
-//   },
-// });
-// const upload = multer({
-//   storage: multerConfig,
-// });
 
 const { validation, auth, upload } = require("../../middlewars");
 const { users: controllers } = require("../../controllers");
@@ -42,26 +18,11 @@ router.patch(
   validation(joiSubscriptionSchema),
   controllers.updateSubscription
 );
-router.patch("/avatars",auth, upload.single("avatar"), controllers.updateAvatar )
-// router.post("/", upload.single("image"), async (req, res, next) => {
-//   const { path: tempUpload, originalname } = req.file;
-//   const resultUpload = path.join(usersDir, originalname);
-
-//   try {
-//     await fs.rename(tempUpload, resultUpload);
-//     const image = path.join("public", "avatars", originalname);
-//     const newContact = {
-//       name: req.body.name,
-//       id: v4(),
-//       image,
-//     };
-//     users.push(newContact);
-//     res.status(201).json(newContact);
-//   } catch (error) {
-//     // await fs.unlink(tempUpload);
-//     next(error);
-//   }
-// });
-
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  controllers.updateAvatar
+);
 
 module.exports = router;
