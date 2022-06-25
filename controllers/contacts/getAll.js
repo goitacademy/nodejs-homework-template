4e4a03bf608da290ae724112b184236d6fc7c304
ALEX321616 <zaplev@ukr.net>
2022-06-25 20:19:34 +0300

    finish

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,12 +2,14 @@ const { Contact } = require("../../models/contacts");
 
 const getAll = async (req, res, next) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 20 } = req.query;
+
   const skip = (page - 1) * limit;
   const contacts = await Contact.find({ owner: _id }, "", {
     skip,
     limit: Number(limit),
   }).populate("owner", "_id name email");
+
   res.json(contacts);
 };
 
