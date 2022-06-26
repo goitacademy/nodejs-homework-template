7c2d09edfcbe6e23ab076638280a716e17ef54d8
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2022-06-26 20:44:13 +0300

    All dz 3

---
+++ controllers/contacts/index.js
@@ -0,0 +1,15 @@
+const getAll = require("./getAll");
+const getById = require("./getById");
+const add = require("./add");
+const deleteById = require("./deleteById");
+const updateById = require("./updateById");
+const updateFavorite = require("./updateFavorite");
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  deleteById,
+  updateById,
+  updateFavorite,
+};
---
+++ models/contacts.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const contactShema = Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const ubdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(20).required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net", "ua"] },
+  }),
+  phone: Joi.string().length(14),
+});
+
+const updateFavorite = Joi.object({ favorite: Joi.bool().required() });
+
+const schemas = {
+  ubdateContact,
+  updateFavorite,
+};
+
+const Contact = model("contact", contactShema);
+
+module.exports = { schemas, Contact };
