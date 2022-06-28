ed202aed5ae93806987f2ddf0bfc47e5ffe36375
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-06-28 20:54:49 +0300

    test update

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,8 +1,27 @@
-// const register = require("../controllers/auth/register");
+const mongoose = require("mongoose");
+const request = require("supertest");
+
+require("dotenv").config();
+const app = require("../app");
+const { User } = require("../db/models/usersModel");
+const { login } = require("../controllers/auth");
 
 // // ответ должен иметь статус-код 200
 // // в ответе должен возвращаться токен
 // // в ответе должен возвращаться объект user с 2 полями email и subscription,
 // // имеющие тип данных String
 
-// describe("test register controller", () => {});
+describe("test register controller", () => {
+  let server;
+  beforeAll(() => (server = app.listen(3000)));
+  afterAll(() => server.close());
+
+  test("test login route", async () => {
+    const newUser = {
+      email: "nastia@mail.com",
+      password: "123456",
+    };
+
+    const user = await User.create(newUser);
+  });
+});
