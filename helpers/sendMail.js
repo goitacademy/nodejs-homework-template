ccee06e1016337e9964af876b82de6e6e24967dc
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-06-28 19:32:45 +0300

    update SendMail

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -5,13 +5,9 @@ const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendMail = async (data) => {
-  try {
-    const mail = { ...data, from: "nananastya.flad@gmail.com" };
-    await sgMail.send(mail);
-    return true;
-  } catch (error) {
-    throw error;
-  }
+  const mail = { ...data, from: "nananastya.flad@gmail.com" };
+  await sgMail.send(mail);
+  return true;
 };
 
 module.exports = sendMail;
