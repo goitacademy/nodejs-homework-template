42e9721fd1f9c2f52eceeefcbd1b533c6c893cbf
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-06-28 16:51:09 +0300

    add template for tests

---
+++ __tests__/login.test.js
@@ -0,0 +1,8 @@
+// const register = require("../controllers/auth/register");
+
+// // ответ должен иметь статус-код 200
+// // в ответе должен возвращаться токен
+// // в ответе должен возвращаться объект user с 2 полями email и subscription,
+// // имеющие тип данных String
+
+// describe("test register controller", () => {});
