17877fb8a76a6bad1c9a6cf22b9c830d945af6ba
Denis Fedash <0501054104d@gmail.com>
2022-07-01 20:40:46 +0300

    chnge env

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -2,6 +2,9 @@ const { User } = require('../../models/user')
 const { createError } = require('../../helpers')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const { SECRET_KEY } = process.env
 
 const login = async (req, res) => {
   const { email, password } = req.body
@@ -13,7 +16,7 @@ const login = async (req, res) => {
     throw createError(401, 'Email or password is wrong')
   }
   const payload = { id: user._id }
-  const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1h' })
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' })
   res.json({ token })
 }
 
