e5888a19df10b4c357e3608bb71c9da83320ac5c
Andrew Borys <borysandrey1988@gmail.com>
2022-07-05 23:11:48 +0300

    add controllers & DB mongoose, create & setup .env

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,7 @@
 const createError = require("./createError");
+const controllerWrapper = require("./controllerWrapper");
 
 module.exports = {
   createError,
+  controllerWrapper,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,107 +1,16 @@
 const express = require("express");
-const Joi = require("joi");
-const contacts = require("../../models/contacts");
-const { createError } = require("../../helpers/index");
 const router = express.Router();
+const controller = require("../../controllers/contacts");
+const { controllerWrapper } = require("../../helpers");
 
-const contactAddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+router.get("/", controllerWrapper(controller.getAll));
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-    // const { status = 500, message = "Server error" } = error;
-    // res.status(status).json(message);
-  }
-});
+router.get("/:id", controllerWrapper(controller.getById));
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contacts.getById(id);
-    if (!result) {
-      //= exemple-1 =//
-      // res.status(404).json({ message: "Not found" });
-      // return;
+router.post("/", controllerWrapper(controller.add));
 
-      //= exemple-2 =//
-      // const error = new Error("Not found");
-      // error.status = 404;
-      // throw error;
+router.delete("/:id", controllerWrapper(controller.removeById));
 
-      //= correct-exemple =//
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    //= exemple-1 =//
-    // res.status(500).json({
-    //   message: error.message,
-    // });
-
-    //= exemple-2 =//
-    // const { status = 500, message = "Server error" } = error;
-    // res.status(status).json(message);
-
-    //= correct-exemple =//
-    next(error);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  // const { id } = req.params;
-  // console.log(id);
-  try {
-    const { id } = req.params;
-    const result = await contacts.removeContact(id);
-    if (!result) {
-      throw createError(404);
-    }
-    res.json({
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    const body = req.body;
-    const { id } = req.params;
-
-    const { error } = contactAddSchema.validate(body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-
-    const result = await contacts.updateContact(id, body);
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:id", controllerWrapper(controller.updateById));
 
 module.exports = router;
