335593b004b67ba5163eb2284752bf3a6a944628
Andari757 <matviyskrypka@gmail.com>
2022-07-11 04:20:45 +0300

    1

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,46 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+const contactsPath = (
+  path.join('.', 'models', 'contacts.json')
+);
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const listContacts = () => {
+  return fs.readFile(contactsPath).then(data => JSON.parse(data));
+};
 
-const updateContact = async (contactId, body) => {}
+
+const getContactById = (id) => {
+  return listContacts().then(contacts => contacts.find(contact => contact.id === id));
+};
+
+
+const removeContact = (id) => {
+  return listContacts()
+    .then(contacts => contacts.filter(contact => contact.id !== id))
+    .then(contacts => JSON.stringify(contacts, undefined, 2))
+    .then(json => fs.writeFile(contactsPath, json));
+};
+
+
+const addContact = ({ name, email, phone }) => {
+  const getContactWithBiggestId = (contacts) => contacts.sort((a, b) => b.id - a.id)[0];
+  const makeId = (contacts) => (parseInt(getContactWithBiggestId(contacts).id) + 1).toString();
+  return listContacts()
+    .then(contacts => [...contacts, { id: makeId(contacts), name, email, phone }])
+    .then(contacts => JSON.stringify(contacts, undefined, 2))
+    .then(json => {
+      fs.writeFile(contactsPath, json)
+      return { name, email, phone }
+    });
+};
+
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,44 @@
+const { listContacts, getContactById, removeContact, addContact } = require('../../models/contacts');
 const express = require('express')
+const Joi = require("joi");
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+const propTypes = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+router.get('/', (req, res, next) => {
+  listContacts().then(contacts => {
+    res.status(200);
+    res.json({ contacts })
+  });
+
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:contactId', (req, res, next) => {
+  getContactById(req.params.contactId)
+    .then(contact => {
+      if (contact) {
+        res.status(200);
+        res.json(contact);
+      } else {
+        res.status(404);
+        res.json({ message: 'Not found' });
+      }
+    });
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  console.log(req.body)
+  const { error } = propTypes.validate(req.body);
+  if (error) {
+    throw new Error(error.message, 400);
+  }
+  addContact(req.body).then(contact => res.status(201).send(contact))
 })
 
 router.delete('/:contactId', async (req, res, next) => {
