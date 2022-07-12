d80d0dbf57b13d0e2a60fb285bb9dbee687ecfa4
chetkiy777 <egunka1988@gmail.com>
2022-07-12 18:45:30 +0300

    copy files from last hw

---
+++ controllers/auth/register.js
@@ -0,0 +1,24 @@
+
+const bcrypt = require("bcryptjs")
+const { User } = require("../../models/user")
+const { createError } = require("../../helpers")
+const gravatar = require('gravatar')
+const register = async (req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    if (user) {
+        throw createError(409, "Email in use")
+    }
+    const hashPassword = await bcrypt.hash(password, 10)
+    const avatarURL = gravatar.url(email)
+    const result = await User.create({...req.body, password: hashPassword, avatarURL})
+
+    res.status(201).json({
+        user: {
+            email: result.email,
+            subscription: result.subscription,
+        }
+    })
+}
+
+module.exports = register
---
+++ models/user.js
@@ -0,0 +1,52 @@
+const {Schema, model} = require("mongoose")
+const Joi = require("joi")
+
+const userSchema = Schema({
+  password: {
+    type: String,
+    minlength: 6,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  avatarURL: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+}, {versionKey: false, timestamps: true})
+
+const register = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  subscription: Joi.string().default("starter"),
+  token: Joi.string().default(null)
+})
+
+const login = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required()
+})
+
+const schemas = {
+  register,
+  login
+}
+
+
+const User = model("user", userSchema)
+
+module.exports = {
+    User,
+    schemas
+}
