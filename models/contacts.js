58c39aed2d4851cbd16ec2beda416a9751ebcacd
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-07-16 18:08:58 +0300

    complete

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContact = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === `${id}`);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === `${id}`);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(idx, 1);
+  await updateContact(contacts);
+  return removeContact;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContact(contacts);
+  return newContact;
+};
+
+const updateContactById = async (id, { name, email, phone }) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { id, name, email, phone };
+  await updateContact(contacts);
+  return contacts[idx];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +63,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+  updateContactById,
+};
