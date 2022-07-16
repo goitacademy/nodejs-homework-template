356dba74c21b4819cee85d83cc2ba9f25a75ca1c
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-07-16 17:02:00 +0300

    getAll, getById

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,40 @@
 const express = require('express')
 
 const router = express.Router()
+const contacts = require('../../models/contacts');
+const createError = require('../../helpers/createError');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) { 
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+
+    res.json({ message: 'template message' })
+  } catch (error) {
+    next(error);
+  }
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
