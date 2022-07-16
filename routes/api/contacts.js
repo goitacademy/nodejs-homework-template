24afbd076a417ee45026e254330223fe1be65e83
AntoninaHN <87104852+AntoninaHN@users.noreply.github.com>
2022-07-16 21:15:23 +0700

    3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,77 @@
 const express = require('express')
-
 const router = express.Router()
-
 router.get('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
-
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
-
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
-
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
-
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
-
 module.exports = router
+const fs = require('fs/promises')
+const path = require('path');
+const contactsPath = path.join(__dirname, './contacts.json')
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data)
+  return contacts;
+}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(item => item.id === contactId);
+  if(!contact){
+      return null;
+  }
+  return contact;
+}
+const removeContact = async (contactId) => {
+  const contact = await getContactById(contactId);
+  if(!contact){
+    return null;
+  }
+  const contacts = await listContacts();
+  const updateContacts = contacts.filter(item => item.id !== String(contactId));
+  await fs.writeFile(contactsPath, JSON.stringify(updateContacts, null, 2));
+  return contact;
+}
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const lastID = Number(contacts[contacts.length - 1].id)
+  const newContact = {
+      id: String(lastID + 1),
+      name: body.name,
+      email: body.email,
+      phone: body.phone,
+  }
+  const updateContacts = [...contacts, ...newContact]
+  await fs.writeFile(contactsPath, JSON.stringify(updateContacts, null, 2))
+  return newContact;
+}
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(item => item.id === contactId);
+  if(!contact) {
+    return null;
+  }
+  contact.name = body.name;
+  contact.email = body.email;
+  contact.phone = body.phone;
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return contact;
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+}
+
