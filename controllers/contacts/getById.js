ef31b613c6e7ae16b3a433e6fe236f277bbd1035
In Niko <inniko9111@gmail.com>
2022-07-17 14:39:29 +0300

    hw-02 done

---
+++ controllers/contacts/getById.js
@@ -0,0 +1,20 @@
+const { NotFound } = require('http-errors');
+
+const contactsOperations = require('../../models/contacts');
+
+const getById = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsOperations.getContactById(id);
+    if (!result) {
+        throw new NotFound('Not found');
+    }
+    res.json({
+        status: 'success',
+        code: 200,
+        data: {
+            result,
+        },
+    });
+};
+
+module.exports = getById;
