7c226db8b5f8a18ccb8ca21094120b7cb07670de
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-07-17 20:45:55 +0300

    fix

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
+const app = require("./app");
 
+const { PORT = 4000 } = process.env;
 
-
-
-const app = require('./app');
-const { PORT } = require('./port');
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
-// const app = require("./app");
-
-// const { PORT = 5500 } = process.env;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running. Use our API on port: ${PORT}`);
-// });
