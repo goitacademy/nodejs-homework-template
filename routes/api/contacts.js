dba7fd8ab47d9e298cf7c93885d3631b7d1a9380
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-07-19 22:52:17 +0300

    add get, get(id)

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,62 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const contacts = require('../../models/contacts');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
+
+router.get('/', async (_req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+
+    return res.json({
+      status: 'Success',
+      code: 200,
+      message: 'Contacts found',
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+
+    if (result) {
+      return res.json({
+        status: 'Success',
+        code: 200,
+        message: 'Contact found',
+        data: {
+          result,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'Error',
+        code: 404,
+        message: 'Not Found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
-module.exports = router
+module.exports = router;
