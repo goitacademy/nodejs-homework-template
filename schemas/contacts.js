a9060a2527488609202db38baa5223c67d7b72d1
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-07-20 13:28:35 +0300

    add

---
+++ schemas/contacts.js
@@ -0,0 +1,15 @@
+/* describe the data */
+
+const Joi = require('joi');
+
+const contactSchema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string()
+    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
+    .required(),
+});
+
+module.exports = {
+    contactSchema,
+};
