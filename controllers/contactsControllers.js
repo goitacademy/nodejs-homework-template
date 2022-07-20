a4aeeb47497bd0835eee3202fbccda25c6a16971
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-07-20 23:56:40 +0300

    finish

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,25 +1,12 @@
-const contactsServices = require("../services");
 const createError = require("../helpers");
-const contactAddSchema = require("../schemas");
-const { Contact } = require("../models");
+const { Contact, schemas } = require("../models");
 
 const listContacts = async (req, res) => {
   const result = await Contact.find({});
+  console.log(result);
   res.json({ status: "success", code: 200, payload: { result } });
 };
 
-// const addContact = async (req, res, next) => {
-//   try {
-//     const { error } = contactAddSchema.validate(req.body);
-//     if (error) {
-//       throw createError(400, error.message);
-//     }
-//     const result = await contactsServices.create(req.body);
-//     res.status(201).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 const addContact = async (req, res) => {
   const result = await Contact.create(req.body);
   res.status(201).json({
@@ -32,7 +19,7 @@ const addContact = async (req, res) => {
 const getContactById = async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsServices.getById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
       throw createError(404);
     }
@@ -41,15 +28,16 @@ const getContactById = async (req, res, next) => {
     next(error);
   }
 };
-
 const updateContact = async (req, res, next) => {
   try {
-    const { error } = contactAddSchema.validate(req.body);
+    const { error } = schemas.contactAddSchema.validate(req.body);
     if (error) {
       throw createError(400, error.message);
     }
     const { contactId } = req.params;
-    const result = await contactsServices.update(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
     if (!result) {
       throw createError(404);
     }
@@ -62,7 +50,7 @@ const updateContact = async (req, res, next) => {
 const removeContact = async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsServices.remove(contactId);
+    const result = await Contact.findByIdAndRemove(contactId);
     if (!result) {
       throw createError(404);
     }
@@ -72,10 +60,24 @@ const removeContact = async (req, res, next) => {
   }
 };
 
+const updateStatusContact = async (req, res) => {
+  const { params, body } = req;
+  const { id } = params;
+  if (!body) {
+    res.status(400).json({ message: "missing field favorite" });
+  }
+  const result = await Contact.findByIdAndUpdate(id, body, { new: true });
+  if (!result) {
+    throw createError(404);
+  }
+  res.json(result);
+};
+
 module.exports = {
   listContacts,
   getContactById,
   addContact,
   updateContact,
   removeContact,
+  updateStatusContact,
 };
--- models/contactsModels.js
+++ models/contactsModels.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const contactsSchema = Schema({
+const contactSchema = Schema({
   name: {
     type: String,
     required: [true, "Set name for contact"],
@@ -16,8 +17,25 @@ const contactsSchema = Schema({
     default: false,
   },
 });
-const Contact = model("contacts", contactsSchema);
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool(),
+});
+
+const updateFavorite = Joi.object({
+  favorite: Joi.bool().required(),
+});
+const schemas = {
+  contactAddSchema,
+  updateFavorite,
+};
+
+const Contact = model("contact", contactSchema);
 
 module.exports = {
   Contact,
+  schemas,
 };
