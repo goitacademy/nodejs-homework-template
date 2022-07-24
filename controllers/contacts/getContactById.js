0e3d74b01c7b61065ab909af039e4c021c499a69
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-07-24 22:21:05 +0300

    done

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -1,12 +1,19 @@
-const contacts = require("../../models/contacts");
+const { Contact } = require("../../models");
 const { createError } = require("../../helpers");
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw createError(404);
   }
-  res.json(result);
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
+
 module.exports = getContactById;
--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -1,17 +1,25 @@
-const contacts = require("../../models/contacts");
-const { addSchema } = require("../../schemas/contacts");
+const { Contact } = require("../../models");
 const { createError } = require("../../helpers");
+const { contactAddSchema } = require("../../models");
 
 const updateContact = async (req, res) => {
-  const { error } = addSchema.validate(req.body);
+  const { error } = contactAddSchema.validate(req.body);
   if (error) {
     throw createError(400, "missing fields");
   }
   const { contactId } = req.params;
-  const result = await contacts.updateContactById(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body);
   if (!result) {
     throw createError(404);
   }
-  res.json(result);
+
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
+
 module.exports = updateContact;
