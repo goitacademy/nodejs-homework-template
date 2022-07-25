e9247d54ddebf632cc2d80eb269037ae18c316b6
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-07-25 11:38:07 +0300

    Schema

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+const User = model("user", userSchema);
+
+const joiSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = { User, joiSchema };
