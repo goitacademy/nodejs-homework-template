4a89120665631084120c208a4ef4819651d90882
In Niko <inniko9111@gmail.com>
2022-07-30 20:39:33 +0300

    hw-03 done

---
+++ controllers/contacts/getAll.js
@@ -0,0 +1,14 @@
+const contactsOperations = require('../../models/contact');
+
+const getAll = async (req, res) => {
+    const contacts = await contactsOperations.listContacts();
+    res.json({
+        status: 'success',
+        code: 200,
+        data: {
+            result: contacts,
+        },
+    });
+};
+
+module.exports = getAll;
---
+++ middlewares/index.js
@@ -0,0 +1,7 @@
+const validation = require('./validation');
+const ctrlWrapper = require('./ctrlWrapper');
+
+module.exports = {
+    validation,
+    ctrlWrapper,
+};
