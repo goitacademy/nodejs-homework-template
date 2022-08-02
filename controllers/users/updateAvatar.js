6b4b5190dd5cd6ec1d0b8621da3055d53064b1c2
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-08-02 15:16:51 +0300

    finish

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,19 +1,24 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
-const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+const Jimp = require("jimp");
+
+const avatarsDir = path.join(__dirname, "../../public/avatars");
 
 const updateAvatar = async (req, res) => {
-  console.log(req.file);
   const { path: tempUpload, originalname } = req.file;
-
+  const { _id: id } = req.user;
+  const imageName = `${id}${originalname}`;
   try {
-    const resultUpload = (avatarsDir, originalname);
+    const resultUpload = path.join(avatarsDir, imageName);
+
+    const image = await Jimp.read(tempUpload);
+    image.resize(250, 250).write(resultUpload);
+
     await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("public", "avatars", imageName);
 
-    const avatarURL = path.join("public", "avatars", originalname);
-    await User.findByIdAndUpdate(req.user._id, { avatarURL });
-
+    await User.findByIdAndUpdate(id, { avatarURL });
     res.json({ avatarURL });
   } catch (error) {
     await fs.unlink(tempUpload);
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,19 +1,16 @@
 const multer = require("multer");
 const path = require("path");
-const tempDir = path.join(__dirname, "../", "temp");
+const tempDir = path.join(__dirname, "../temp");
 
 const multerConfiq = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb((null, tempDir));
-  },
+  destination: tempDir,
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
-  limits: {
-    fileSize: 2048,
-  },
 });
+
 const upload = multer({
   storage: multerConfiq,
 });
+
 module.exports = upload;
