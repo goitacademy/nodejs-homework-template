060985936be836ce3648621bac338625a3cfd2d7
anasty223 <86348493+anasty223@users.noreply.github.com>
2022-08-06 18:20:00 +0300

    .

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 const DB_HOST =
+<<<<<<< Updated upstream
   "mongodb+srv://Anastasya:gxs3mlp4CJULfD09@cluster0.i52ol9i.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+=======
+  "mongodb+srv://Anastasya:mLUwxbCsJrhxLS2N@cluster0.7x9nycx.mongodb.net/contacts?retryWrites=true&w=majority";
+// mLUwxbCsJrhxLS2N;
+>>>>>>> Stashed changes
 module.exports = { DB_HOST };
