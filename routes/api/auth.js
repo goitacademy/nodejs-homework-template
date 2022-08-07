0e2e718c9fa699512726e724dcbb07725ba27675
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-08-07 17:01:28 +0300

    add avt(middlwrs)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,7 +6,7 @@ const ctrl = require(`${basedir}/controllers/auth`);
 
 const { ctrlWrapper } = require(`${basedir}/helpers`);
 
-const { auth } = require(`${basedir}/middlewares`);
+const { auth, upload } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
@@ -19,5 +19,7 @@ router.get('/logout', auth, ctrlWrapper(ctrl.logout));
 router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
 
 router.patch('/subscription', auth, ctrlWrapper(ctrl.updateSubscription));
+
+router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.setAvatar));
 
 module.exports = router;
