9530eab80dd55da91c892ac38b1d5a1a1640af22
jokerav <jokeravdeveloper@gmail.com>
2022-08-08 19:37:23 +0300

    get all contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,33 @@
-const express = require('express')
+const express = require("express");
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.normalize("db/contacts.json");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+  res.json({
+    status: "success",
+    code: 200,
+    data,
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message for contactsById" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message POST Contacts" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message DELETE Contacts" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message EDIT Contacts" });
+});
 
-module.exports = router
+module.exports = router;
