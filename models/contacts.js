f14f613fd6c4aedd6addbe65b9a09d8083ffc1e2
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-08-08 20:12:31 +0300

    updating hw#2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,57 @@
-// const fs = require('fs/promises')
+const asyncHandler = require("./asyncHandler");
+const fs = require("fs/promises");
+const { join } = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const listContacts = async () => {}
+const contactsPath = join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await asyncHandler(fs.readFile(contactsPath));
+  return JSON.parse(contacts);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await asyncHandler(listContacts());
+  const contact = contacts.find(({ id }) => id === contactId);
+  return contact;
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts = await asyncHandler(listContacts());
+  const newContact = { id: uuidv4(), ...body };
+  const newListContacts = [...contacts, newContact];
+  await asyncHandler(
+    fs.writeFile(contactsPath, JSON.stringify(newListContacts, null, 2))
+  );
+  return newContact;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await asyncHandler(listContacts());
+  const contactIdx = contacts.findIndex(({ id }) => id === contactId);
+
+  if (contactIdx === -1) {
+    return null;
+  }
+  const [newContacts] = contacts.splice(contactIdx, 1);
+  await asyncHandler(
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  );
+  return newContacts;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await asyncHandler(listContacts());
+  const contactIdx = contacts.findIndex(({ id }) => id === contactId);
+  if (contactIdx === -1) {
+    return null;
+  }
+  contacts[contactIdx] = { id: contactId, ...body };
+  await asyncHandler(
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  );
+  return contacts[contactIdx];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +59,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,122 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+const { NotFound } = require("http-errors");
+const Joi = require("joi");
 
-const router = express.Router()
+const contactSchema = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .pattern(/^\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+$/)
+    .trim()
+    .required(),
+  email: Joi.string().email().trim().required(),
+  phone: Joi.string()
+    .min(10)
+    .pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/, "numbers")
+    .trim()
+    .required(),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      throw new NotFound("Not found");
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contact,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    const newContact = await addContact(req.body);
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        newContact,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const deletedContact = await removeContact(contactId, req.body);
+    if (deletedContact) {
+      res.status(200).json({
+        status: "success",
+        code: 200,
+        message: "contact deleted",
+      });
+    } else {
+      throw new NotFound("Not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    const { contactId } = req.params;
+    const updatedContact = await updateContact(contactId, req.body);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: {
+        updatedContact,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
