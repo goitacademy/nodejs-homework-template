8fe44469c960b5f618ca993ebe27ebb51c05f099
jokerav <jokeravdeveloper@gmail.com>
2022-08-08 21:16:03 +0300

    run dev

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,21 +7,25 @@ const router = express.Router();
 const listContacts = async () =>
   JSON.parse(await fs.readFile(contactsPath, "utf8"));
 
-router.get("/", async (req, res, next) => {
-  const data = await listContacts();
-  res.json({
-    status: "success",
-    code: 200,
-    data,
-  });
-});
+// router.get("/", async (req, res, next) => {
+//   const contacts = await listContacts();
+//   res.json({
+//     status: "success",
+//     code: 200,
+//     data: contacts,
+//   });
+// });
 
-router.get("/:contactId", async (req, res, next) => {
-  const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+router.get("/:id", async (req, res, next) => {
+  // const contacts = await listContacts();
+  const { id } = req.params;
+  // console.log("REQUEST");
+  const normID = id.toString();
+  // const item = contacts.filter((item) => item.id.toString() === normID);
   res.json({
     status: "success",
     code: 200,
-    data,
+    data: normID,
   });
 });
 
