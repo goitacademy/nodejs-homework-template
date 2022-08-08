a2e1295da2f01e3abcf93fe61c8a8b46e580ad19
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-08-08 23:46:23 +0300

    add route to update the avt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,16 +10,16 @@ const { auth, upload } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
-router.post('/signup', ctrlWrapper(ctrl.signup));
+router.post('/signup', ctrlWrapper(ctrl.signup)); // роут для реєстрації користувача
 
-router.post('/login', ctrlWrapper(ctrl.login));
+router.post('/login', ctrlWrapper(ctrl.login)); // роут для входу користувача
 
-router.get('/logout', auth, ctrlWrapper(ctrl.logout));
+router.get('/logout', auth, ctrlWrapper(ctrl.logout)); // роут для виходу користувача
 
-router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
+router.get('/current', auth, ctrlWrapper(ctrl.getCurrent)); // роут для поточного користувача
 
-router.patch('/subscription', auth, ctrlWrapper(ctrl.updateSubscription));
+router.patch('/subscription', auth, ctrlWrapper(ctrl.updateSubscription)); // роут для оновлення передплати користувача
 
-router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.setAvatar));
+router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar)); // роут для оновлення аватарки користувача
 
 module.exports = router;
