430c6f32b7db5da975e89502a7bf6475b0014310
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-08-08 23:34:47 +0300

    add commentary

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,16 +10,16 @@ const { auth } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
-router.get('/', auth, ctrlWrapper(ctrl.getAllContacts));
+router.get('/', auth, ctrlWrapper(ctrl.getAllContacts)); // роут для списку всіх контактів
 
-router.get('/:id', auth, ctrlWrapper(ctrl.getContactById));
+router.get('/:id', auth, ctrlWrapper(ctrl.getContactById)); // роут для контакту з id
 
-router.post('/', auth, ctrlWrapper(ctrl.addContact));
+router.post('/', auth, ctrlWrapper(ctrl.addContact)); // роут для створення контакту
 
-router.delete('/:id', auth, ctrlWrapper(ctrl.removeContact));
+router.delete('/:id', auth, ctrlWrapper(ctrl.removeContact)); // роут для видалення контакту
 
-router.put('/:id', auth, ctrlWrapper(ctrl.updateContact));
+router.put('/:id', auth, ctrlWrapper(ctrl.updateContact)); // роут для оновлення контакту
 
-router.patch('/:id/favorite', auth, ctrlWrapper(ctrl.updateStatusContact));
+router.patch('/:id/favorite', auth, ctrlWrapper(ctrl.updateStatusContact)); // роут для статусу, favorite
 
 module.exports = router;
