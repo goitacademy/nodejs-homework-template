90c6f636035d0d71a83bcc1ba527f980045829b5
jokerav <jokeravdeveloper@gmail.com>
2022-08-09 14:39:38 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,38 +2,41 @@ const express = require("express");
 const fs = require("fs/promises");
 const path = require("path");
 const contactsPath = path.normalize("db/contacts.json");
-
 const router = express.Router();
+
 const listContacts = async () =>
   JSON.parse(await fs.readFile(contactsPath, "utf8"));
 
-// router.get("/", async (req, res, next) => {
-//   const contacts = await listContacts();
-//   res.json({
-//     status: "success",
-//     code: 200,
-//     data: contacts,
-//   });
-// });
-
-router.get("/:id", async (req, res, next) => {
-  // const contacts = await listContacts();
-  const { id } = req.params;
-  // console.log("REQUEST");
+const getById = async (id) => {
+  const contacts = await listContacts();
   const normID = id.toString();
-  // const item = contacts.filter((item) => item.id.toString() === normID);
+  const index = contacts.findIndex((item) => item.id.toString() === normID);
+  if (index === -1) {
+    return { message: "Not found", code: 404 };
+  }
+  return { status: "success", code: 200, data: contacts[index] };
+};
+
+router.get("/", async (req, res, next) => {
+  const data = await listContacts();
   res.json({
     status: "success",
     code: 200,
-    data: normID,
+    data,
   });
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const data = await getById(id);
+  res.json(data);
 });
 
 router.post("/", async (req, res, next) => {
   res.json({ message: "template message POST Contacts" });
 });
 
-router.delete("/:contactId", async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   res.json({ message: "template message DELETE Contacts" });
 });
 
