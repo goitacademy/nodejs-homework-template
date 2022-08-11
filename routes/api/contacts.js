b89dc39111bac7e0840ee7e514dc61a5e5102d2e
jokerav <jokeravdeveloper@gmail.com>
2022-08-11 11:48:12 +0300

    переместил функции в model

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,67 +1,13 @@
 const express = require("express");
 const Joi = require("joi");
-const fs = require("fs/promises");
-const { nanoid } = require("nanoid");
-const path = require("path");
-const contactsPath = path.normalize("db/contacts.json");
 const router = express.Router();
-
-const listContacts = async () =>
-  JSON.parse(await fs.readFile(contactsPath, "utf8"));
-
-const getById = async (id) => {
-  const contacts = await listContacts();
-  const normID = id.toString();
-  const index = contacts.findIndex((item) => item.id.toString() === normID);
-  if (index === -1) {
-    return { message: "Not found" };
-  }
-  return { status: "success", data: contacts[index] };
-};
-const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(5),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-};
-const removeContact = async (id) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(
-    (item) => item.id.toString() === id.toString()
-  );
-  if (index === -1) {
-    return { message: "Not found" };
-  }
-  contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return { message: "contact deleted" };
-};
-const updateContact = async (id, body) => {
-  const contacts = await listContacts();
-  const { name, email, phone } = body;
-  console.log(id, name, email, phone);
-  contacts.forEach((item) => {
-    if (item.id.toString() === id.toString()) {
-      if (name) {
-        item.name = name;
-      }
-      if (email) {
-        item.email = email;
-      }
-      if (phone) {
-        item.phone = phone;
-      }
-    }
-  });
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return (await getById(id)) || { message: "missing fields" };
-};
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
 router.get("/", async (req, res, next) => {
   const data = await listContacts();
@@ -73,7 +19,7 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
-  const data = await getById(id);
+  const data = await getContactById(id);
   if (data.message) {
     res.status(404).json(data);
     return;
