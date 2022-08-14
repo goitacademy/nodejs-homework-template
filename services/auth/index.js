32320dbbf15dbd7867f2e8c688da730abb744a6e
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-08-15 01:44:56 +0300

    add

--- services/auth/index.js
+++ services/auth/index.js
@@ -2,10 +2,12 @@ const signup = require('./signup');
 const login = require('./login');
 const logout = require('./logout');
 const patch = require('./subscription');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
     signup,
     login,
     logout,
     patch,
+    sendEmail,
 };
