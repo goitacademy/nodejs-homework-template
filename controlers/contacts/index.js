e39a30bee0dd1aa8b02a3a8faa2ef63a31704eb4
Spec27 <shkvara27@gmail.com>
2022-08-15 18:39:34 +0300

    finished homework

---
+++ controlers/contacts/index.js
@@ -0,0 +1,13 @@
+const getAll = require("./getAll");
+const getById = require("./getById");
+const add = require("./add");
+const deleteContact = require("./deleteContact");
+const updateContactId = require("./updateById");
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  deleteContact,
+  updateContactId,
+};
