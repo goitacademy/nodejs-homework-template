b902ac0ad7d3657d2942c7cf1384f30a53e84b64
In Niko <inniko9111@gmail.com>
2022-08-19 13:02:59 +0300

    hw-05 done

---
+++ controllers/users/index.js
@@ -0,0 +1,13 @@
+const signUp = require("./signUp");
+const login = require("./login");
+const getCurrent = require("./getCurrent");
+const logout = require("./logout");
+const updateAvatar = require("./updateAvatar");
+
+module.exports = {
+  signUp,
+  login,
+  getCurrent,
+  logout,
+  updateAvatar,
+};
