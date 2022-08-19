42c5e4a137d7d5d2bfbfa4616ff314df349140f4
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-08-19 23:44:24 +0300

    hw 5

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { basedir } = global;
 const ctrl = require(`${basedir}/controllers/users`);
-const { auth, upload, resize } = require(`${basedir}/middlewares`);
+const { auth, upload } = require(`${basedir}/middlewares`);
 const { ctrlWrapper } = require(`${basedir}/helpers`);
 
 router.patch("/", auth, ctrlWrapper(ctrl.updateSubscriptionUser));
@@ -10,7 +10,6 @@ router.patch(
   "/avatars",
   auth,
   upload.single("avatar"),
-  resize,
   ctrlWrapper(ctrl.setAvatar)
 );
 
