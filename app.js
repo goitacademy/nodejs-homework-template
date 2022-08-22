da11d1f6cea2a901c7484eb34fc1157c6d5e648c
In Niko <inniko9111@gmail.com>
2022-08-22 11:44:34 +0300

    update

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
+
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
 
 app.use("/api/contacts", contactsRouter);
 
