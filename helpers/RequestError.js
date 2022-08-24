4a8d2d16af53eb4e015184f428edc0a681036761
Spec27 <shkvara27@gmail.com>
2022-08-24 19:33:20 +0300

    ok

--- helpers/RequestError.js
+++ helpers/RequestError.js
@@ -1,4 +1,12 @@
-const RequestError = (status, message) => {
+const messages = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+  409: "Conflict",
+};
+
+const RequestError = (status, message = messages[status]) => {
   const error = new Error(message);
   error.status = status;
   return error;
