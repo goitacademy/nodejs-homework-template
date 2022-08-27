d69f1d4b2ca7ddf07a3d0905f45a52b739be1ad0
Dmytrii Shypilov <95936002+Dmytrii-Shypilov@users.noreply.github.com>
2022-08-27 22:54:28 +0300

    initial files upload

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const dotenv = require("dotenv")
 
 const contactsRouter = require('./routes/api/contacts')
 
+dotenv.config()
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -19,7 +21,7 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  res.status(err.status=500).json({ message: err.message })
 })
 
 module.exports = app
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,24 @@
-// const fs = require('fs/promises')
+const {Schema, model} = require("mongoose")
 
-const listContacts = async () => {}
-
-const getContactById = async (contactId) => {}
-
-const removeContact = async (contactId) => {}
-
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+const contactSchema = Schema({
+name: {
+  type: String,
+  required: [true, "Set name for contact"]
+},
+email: {
+  type: String,
+  required: true
+},
+phone: {
+  type: String,
+  required: true
+},
+favorite: {
+  type: Boolean,
+  default: false
 }
+}, {versionKey: false})
+
+const Contact = model("contact", contactSchema)
+
+module.exports = Contact
