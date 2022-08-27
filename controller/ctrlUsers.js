beba3a1ba101b35731a295a66899c6331253903a
Tomasz Sokół <sokol.tomasz@gmail.com>
2022-08-27 12:49:47 +0200

    Update ctrlUsers.js

--- controller/ctrlUsers.js
+++ controller/ctrlUsers.js
@@ -10,15 +10,24 @@ const currentUser = async (req, res, next) => {
   const { _id } = req.user;
   try {
     const user = await service.getUserById(_id);
-    res.status(200).json({
-      status: "success",
-      code: 200,
-      message: "OK",
-      data: {
-        email: user.email,
-        subscription: user.subscription,
-      },
-    });
+    if (user) {
+      res.status(200).json({
+        status: "success",
+        code: 200,
+        message: "OK",
+        data: {
+          email: user.email,
+          subscription: user.subscription,
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: "error",
+        code: 404,
+        message: `user not found`,
+        data: "Not Found",
+      });
+    }
   } catch (e) {
     console.error(e);
     next(e);
