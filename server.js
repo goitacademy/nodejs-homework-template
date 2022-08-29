f5ad045077a72d178f0b117c5365e8049ae2eacb
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-08-29 12:52:18 +0300

    add contact

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { conectMongo } = require("./src/db/conection");
+const PORT = process.env.PORT;
+async function startApp() {
+  try {
+    await conectMongo();
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3068");
+    });
+    console.log("Database connection successful");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+}
+startApp();
