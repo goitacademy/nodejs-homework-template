1af22f6d80022842675d35a9e056b8c662e1ce7b
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-08-30 18:09:34 +0300

    update

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,13 +1,6 @@
-const { User } = require("../../models/user");
-
 const current = async (req, res) => {
   const { email } = req.user;
-  // const { _id } = req.user;
-  // // await User.findOne({ _id });
-  // const user = await User.findOne({ _id });
-  // if (!user) {
-  //   throw RequestError(401, "Not authorized");
-  // }
+
   res.json({
     email,
     subscription: "starter",
