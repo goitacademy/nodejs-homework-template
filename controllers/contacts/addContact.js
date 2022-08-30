7a1e0c6d34646ada78a5fd3aae0c29834d009d37
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-08-30 16:18:29 +0300

    +

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,7 +1,7 @@
 const { Contact } = require("../../models/contact");
 
 const addContact = async (req, res) => {
-  const { id: owner } = req.user;
+  const { _id: owner } = req.user;
   const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,7 +1,7 @@
 const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
-  const { id: owner } = req.user;
+  const { _id: owner } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
