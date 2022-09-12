1c7ce58117dc902ef8f602f1c4a4b61052ddb0d6
Kamil Przebieracz <dmjs23@gmail.com>
2022-09-12 16:39:06 +0200

    e

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST, PORT } = process.env;
-
 mongoose
   .connect(DB_HOST, console.log("Database connection successful"))
   .then(() => app.listen(PORT))
