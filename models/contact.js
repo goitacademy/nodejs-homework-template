8695040bde1868316529f24ae4450fbb061e1f7d
Kamil Przebieracz <dmjs23@gmail.com>
2022-09-12 16:45:05 +0200

    commit

---
+++ models/contact.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const contactUpdateStatusSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addContact: contactAddSchema,
+  updateStatusContact: contactUpdateStatusSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
