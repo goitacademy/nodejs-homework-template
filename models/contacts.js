2686182fc5a3ab95e7580c860d32b33a70ac101c
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-09-17 16:30:35 +0300

    Adds get,getbeid, post methods

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,50 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const id = uuidv4();
 
-const listContacts = async () => {}
+const contactsPath = path.resolve(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    return fs.readFile(contactsPath, 'utf8');
+  } catch (error) {
+    console.log(error);
+  } 
+}
+
+const getContactById = async (contactId) => {
+  try {
+    const contacts = JSON.parse(await listContacts());
+    const [contact] = contacts.filter(contact => Number(contact.id) === Number(contactId));
+    return contact;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  try {
+    const prevContacts = JSON.parse(await listContacts());
+    const { name, email, phone } = body;
 
-const updateContact = async (contactId, body) => {}
+    const newContact = { id, name, email, phone };
+    const allContacts = [...prevContacts, newContact];
+
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts), 'utf8');
+    console.log(newContact)
+    return newContact;
+  } catch (error) {
+    console.log(error); 
+  }
+}
+
+// addContact({name: "hhghfghghh", email: "jjhghghhghgjj", phone: "545454545" })
+
+const updateContact = async (contactId, body) => { }
+
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,31 @@
 const express = require('express')
 
+const { listContacts, getContactById, addContact} = require('../../models/contacts');
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = JSON.parse(await listContacts());
+  res.json({ contacts, message: 'success' });
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+router.get('/:contactId', async (req, res, next) => {  
+  const contact = await getContactById(req.params.contactId);
+  res.json({ contact, message: 'success' });
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contact = await addContact(req.body);
+  res.json({ contact, message: 'success' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'success' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'success' })
 })
 
 module.exports = router
