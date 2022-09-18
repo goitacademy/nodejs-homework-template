4e028815ad22fe12462974eb996a27704e098c10
YurijKovbasyuk <93015127+YurijKovbasyuk@users.noreply.github.com>
2022-09-18 15:34:47 +0300

    fix middelwares

--- middlewares/validationMiddlewares.js
+++ middlewares/validationMiddlewares.js
@@ -20,7 +20,7 @@ const putContactValidation = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().alphanum().min(3).max(30).optional(),
         email: Joi.string().email().min(4).max(30).optional(),
-        phone: Joi.string().pattern(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/).min(9).max(21).required(),
+        phone: Joi.string().pattern(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/).min(9).max(18).optional(),
     })
 
     const validationResult = schema.validate(req.body)
