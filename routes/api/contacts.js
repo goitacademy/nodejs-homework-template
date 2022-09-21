37cfaad0ed9d077aef858ff18f8b12f75a87a6ec
Nikita Kolotenko <tofeee63@gmail.com>
2022-09-21 13:49:01 +0300

    Start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'hw02 message' })
+  res.json({ message: 'template message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
