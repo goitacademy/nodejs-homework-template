2757fbb6527b43cc1aa8dd0376c30959ab6d92de
Dmytro Kulakov <96733856+unkindled11@users.noreply.github.com>
2022-09-22 22:54:27 +0300

    env

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const { connect } = require('mongoose');
+
+const app = require('./app');
+const { mongoUrl } = require('./config');
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+  console.log("Server running. Use our API on port: 3000");
+
+  connect(mongoUrl, { dbName: "db-contacts" })
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => console.log(err.message)); 
 })
+
+
