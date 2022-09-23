a1b67e96950de60bc3734cc256b67ea844cf9fc7
myroslav <myrkozar@gmail.com>
2022-09-23 19:15:00 +0300

    second HW is done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,54 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'message' });
-});
+const ctrl = require('../../controlers/contacts');
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+const { ctrlWrapper } = require('../../helpers');
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+const { validateBody } = require('../../middlewares');
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+const schema = require('../../schema/contactSchema');
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+router.get('/', ctrlWrapper(ctrl.getAllContacts));
+
+router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
+
+router.post(
+  '/',
+  validateBody(schema.contactsAddSchema),
+  ctrlWrapper(ctrl.addContacts)
+);
+
+router.delete('/:contactId', ctrlWrapper(ctrl.deleteContact));
+
+router.put(
+  '/:contactId',
+  validateBody(schema.contactsAddSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
 
 module.exports = router;
+
+// const contacts = require('../../models/contacts');
+// router.get('/:contactId', async (req, res, next) => {
+//   try {
+//     console.log(req.params);
+//     const { contactId } = req.params;
+//     const result = await contacts.getContactById(contactId);
+//     if (!result) {
+//       throw RequestError(404, 'Not found');
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const result = await contacts.listContacts();
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
