0d764b0e64462e835aa0db03b12e18f7126547d1
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-09-25 00:40:06 +0300

    updateStatusContact() does not work

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -1,9 +1,9 @@
-const { Contact } = require("../../models");
+const  updateById  = require("./updateById");
 
 const updateStatusContact = async(req, res) => {
     const { contactId } = req.params;
     const { favorite } = req.body;
-    const result = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
+    const result = await updateById(contactId, { favorite });
     
     if (!result) {
         const error = new Error("Not found");
@@ -21,3 +21,32 @@ const updateStatusContact = async(req, res) => {
 };
 
 module.exports = updateStatusContact;
+
+
+
+
+
+
+// const { Contact } = require("../../models");
+
+// const updateStatusContact = async(req, res) => {
+//     const { contactId } = req.params;
+//     const { favorite } = req.body;
+//     const result = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
+    
+//     if (!result) {
+//         const error = new Error("Not found");
+//             error.status = 404;
+//             res.status(404).json({
+//             message: `Product with id=${contactId} not found`
+//         });
+//         return;
+//     };
+//      res.json({
+//         status: "success",
+//         code: 200,
+//         data: result
+//     })
+// };
+
+// module.exports = updateStatusContact;
