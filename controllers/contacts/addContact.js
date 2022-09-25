f2df5de45ea18a517bdaba17162a8f680da470c6
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-09-25 20:01:50 +0300

    u

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,17 @@
+const {Contact, schemas} = require('../../models/contacts');
+
+const {createError} = require('../../middleware/createError');
+
+
+const addContact = async (req, res) => {
+
+    const { error } = schemas.add.validate(req.body)
+    if (error) {
+      throw createError(400, error.message)
+    }
+    const reply = await Contact.create(req.body)
+    res.status(201).json(reply)
+  }
+  
+  module.exports =addContact;
+
--- server.js
+++ server.js
@@ -1,5 +1,36 @@
 const app = require('./app')
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const { PORT, DB_HOST } = process.env;
+
+
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(PORT, () => {
+      console.log('Database connection successful')
+    }),
+  )
+  .catch(error => {
+    console.log(error)
+    process.exit(1)
+  })
+  
+
+  
+/*const mongoose = require("mongoose");
+const DB_HOST = "mongodb+srv://serg:gogogo@cluster0.nz0mya6.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const PORT = 3000
+
+
+
+mongoose.connect(DB_HOST)
+  .then(() => 
+      console.log('Database connection successful')
+    ,
+  )
+  .catch(error => {console.log(error)
+    process.exit(1)
+  })
+*/
