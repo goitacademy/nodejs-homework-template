77c076a9d8e911cae8267717dce2b993081e5407
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-09-25 20:22:32 +0300

    fix

--- schemas/contact.js
+++ schemas/contact.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  number: Joi.number().required(),
+  number: Joi.string().required(),
 });
 
 module.exports = addSchema;
