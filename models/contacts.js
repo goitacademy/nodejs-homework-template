1715134b4b5a6aba6a754c5cf6543573e5734666
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-09-26 02:06:50 +0300

    Finfish

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,85 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const path = require("path");
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const removeContact = async (contactId) => {}
+async function readData() {
+  const data = await fs.readFile(contactsPath, "utf-8");
 
-const addContact = async (body) => {}
+  return JSON.parse(data);
+}
 
-const updateContact = async (contactId, body) => {}
+async function writeData(array) {
+  const data = JSON.stringify(array, null, 2);
+  await fs.writeFile(contactsPath, data);
+}
+
+const listContacts = async () => {
+  const allContacts = await readData();
+
+  return allContacts;
+};
+
+const getContactById = async (contactId) => {
+  const allContacts = await readData();
+
+  const id = contactId.toString();
+  const oneContact = allContacts.find((contact) => contact.id === id);
+  return oneContact || null;
+};
+
+const removeContact = async (contactId) => {
+  const allContacts = await readData();
+
+  const id = contactId.toString();
+  const index = allContacts.findIndex((contact) => contact.id === id);
+
+  if (index === -1) {
+    return null;
+  }
+  const [removedContact] = allContacts.splice(index, 1);
+
+  await writeData(allContacts);
+
+  return removedContact;
+};
+
+const addContact = async (body) => {
+  const allContacts = await readData();
+  const { name, email, phone } = body;
+
+  const newContact = {
+    id: Date.now().toString(),
+    name,
+    email,
+    phone,
+  };
+
+  allContacts.push(newContact);
+
+  await writeData(allContacts);
+
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const allContacts = await readData();
+  const { name, email, phone } = body;
+
+  const id = contactId.toString();
+
+  const index = allContacts.findIndex((contact) => contact.id === id);
+
+  if (index === -1) {
+    return null;
+  }
+
+  allContacts[index] = { id, name, email, phone };
+
+  await writeData(allContacts);
+  return allContacts[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +87,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
