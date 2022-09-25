008d0a98417e0d8dfc65eeec4c6c61c66a316fa9
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-09-25 20:15:32 +0300

    done

---
+++ controllers/contacts/add.js
@@ -0,0 +1,6 @@
+const contacts = require("../../models/contacts");
+const add = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+module.exports = add;
