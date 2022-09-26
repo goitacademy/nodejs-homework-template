1ecc40c1796c5ecde7351f38ecb06727475d4986
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-09-26 14:03:17 +0300

    +

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,14 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path=require('path')
 
-const listContacts = async () => {}
+const contactsPath=path.join(__dirname,'/contacts.json')
+
+console.log(contactsPath)
+
+const listContacts = async () => {
+  const result=await fs.readFile(contactsPath,'utf-8')
+  return JSON.parse(result)
+}
 
 const getContactById = async (contactId) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,38 @@
+
 const express = require('express')
 
 const router = express.Router()
 
+const contacts=require("../../models/contacts")
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try{
+  const result= await contacts.listContacts()  
+  res.json(result)
+} catch(error) {
+  res.status(500).json({
+    message:error.message
+  })
+}}  )
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result= await contacts.getContactById
+  res.json( result)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result= await contacts.addContact()
+  res.json(result)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result=await contacts.removeContact
+  res.json(result)
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result=await contacts.updateContact()
+  res.json(result)
 })
 
 module.exports = router
