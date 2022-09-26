23e534008244c3db940399867df20507a0b8091b
velogitara <velogitara@gmail.com>
2022-09-27 00:47:15 +0300

    add key

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const fs = require('fs/promises');
-const moment = require('moment');
+// axe25paKUyxgJyWL;
 
-const contactsRouter = require('./routes/api/contacts');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const fs = require("fs/promises");
+const moment = require("moment");
+
+const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use(async (req, res, next) => {
-  const { method, url } = req;
-  const date = moment().format('DD-MM-YYYY_hh:mm:ss');
-  await fs.appendFile('server.log', `\n${method} ${url} ${date}`);
-  next();
+    const { method, url } = req;
+    const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+    await fs.appendFile("server.log", `\n${method} ${url} ${date}`);
+    next();
 });
 
-app.use('/api/contacts', contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+    res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
-  res.status(status).json({ message: message });
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message: message });
 });
 
 module.exports = app;
