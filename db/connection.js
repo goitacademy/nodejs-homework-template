4144e10b3ee392ceb9bae1be21d2e222f9cfb6c9
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-09-26 21:58:12 +0300

    1

--- db/connection.js
+++ db/connection.js
@@ -1,33 +1,33 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-const URL = process.env.MONGO_URL;
+const DB_HOST = process.env.MONGO_URL;
 const Schema = mongoose.Schema;
 
 const connectMongo = async () => {
-  const contacts = new Schema({
-    name: {
-      type: String,
-      minlength: 2,
-      maxlength: 7,
-      required: [true, "Name is required"],
-    },
-    email: {
-      type: String,
-      minlength: 2,
-      maxlength: 12,
-      unique: true,
-    },
-    phone: {
-      type: String,
-      minlength: 2,
-      maxlength: 12,
-      required: [true, "Phone is required"],
-      unique: true,
-    },
-    favorite: Boolean,
-  });
-  const Contacts = mongoose.model("contact", contacts);
-  // const client = await mongoose.connect(URL, {
+  // const contacts = new Schema({
+  //   name: {
+  //     type: String,
+  //     minlength: 2,
+  //     maxlength: 7,
+  //     required: [true, "Name is required"],
+  //   },
+  //   email: {
+  //     type: String,
+  //     minlength: 2,
+  //     maxlength: 12,
+  //     unique: true,
+  //   },
+  //   phone: {
+  //     type: String,
+  //     minlength: 2,
+  //     maxlength: 12,
+  //     required: [true, "Phone is required"],
+  //     unique: true,
+  //   },
+  //   favorite: Boolean,
+  // });
+  // const Contacts = mongoose.model("contact", contacts);
+  // mongoose.connect(DB_HOST, {
   //   useNewUrlParser: true,
   //   useCreateIndex: true,
   //   useUnifiedTopology: true,
