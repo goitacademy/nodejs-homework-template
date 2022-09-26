301fae978ce64d16e4b53da0176f5fc7edbbccca
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-09-26 23:10:47 +0300

    add controllers

---
+++ controllers/contacts/contactUpdate.js
@@ -0,0 +1,16 @@
+const contacts = require('../../models/contacts.js');
+
+const { RequestError } = require('../../helpers');
+
+const contactUpdate = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await contacts.updateContact(id, req.body);
+
+  if (!result) {
+    throw RequestError(404, 'Not found');
+  }
+  res.json(result);
+};
+
+module.exports = contactUpdate;
