23c7da85ab85fb69c6aab058e840b92fdc5826f8
Yuriy Pokhyl <yuriypokhyl@Yuriys-Air.Dlink>
2022-09-29 20:59:56 +0300

    bugfix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,17 +4,17 @@ const {validation, ctrlWrapper} = require('../../middlewares');
 const {contactSchema} = require('../../schemas')
 const {contacts: ctrl} = require('../../controllers');
 
-const validationMiddleware = validation(contactSchema);
+
 
 
 router.get('/', ctrlWrapper(ctrl.getAllContacts))
 
 router.get('/:id', ctrlWrapper(ctrl.getContactById))
 
-router.post('/', validationMiddleware, ctrlWrapper(ctrl.addContact))
+router.post('/', validation(contactSchema), ctrlWrapper(ctrl.addContact))
 
 router.delete('/:id', ctrlWrapper(ctrl.removeContact))
 
-router.put('/:id', validationMiddleware, ctrlWrapper(ctrl.updateContact));
+router.put('/:id', validation(contactSchema), ctrlWrapper(ctrl.updateContact));
 
 module.exports = router
