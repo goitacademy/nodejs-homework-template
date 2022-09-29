f1b605c622519ccf4fa8a5d8d1c2f00da5c6506e
Dmytro Kulakov <gambler11kulakov@gmail.com>
2022-09-29 19:10:22 +0300

    2

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,11 +5,24 @@ const router = Router();
 const { signupSchema } = require("../../schemas");
 const { validateSchema } = require("../../helpers");
 const User = require("../../models/userModel");
+const { createError } = require("../../helpers");
 
 router.post("/signup", async (req, res, next) => {
   try {
     validateSchema(signupSchema, req.body);
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw createError(409, "Email already exist");
+    }
+
+    const newUser = await User.create(req.body);
+
     res.send("signup");
+
+    return newUser;
   } catch (error) {
     next(error);
   }
