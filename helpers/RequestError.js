f0748ddb2d3a026122133fdd60542323254fb858
Yurii-Rykov <103988434+Yurii-Rykov@users.noreply.github.com>
2022-09-30 14:07:50 +0300

    save-work

---
+++ helpers/RequestError.js
@@ -0,0 +1,16 @@
+const messages = {
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbbiden',
+    404: 'Not found',
+    409: 'Conflic'
+}
+
+
+const RequestError = (status, message) => {
+    const error = new Error(message);
+    error.status = status;
+    return error;
+}
+
+module.exports = RequestError
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,93 @@
 const express = require('express')
 
+const contacts = require('../../models/contacts');
+
 const router = express.Router()
 
+const { RequestError } = require('../../helpers/RequestError');
+
+const Joi = require('joi');
+
+const addShema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+    console.log(result);
+
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id)
+
+    if (!result) {
+      throw RequestError(404, 'Not found')
+    }
+    res.json(result)
+    console.log(result);
+
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { name, email, phone } = req.params;
+    const { error } = addShema.validate({ name, email, phone })
+    if (error) {
+      throw RequestError(400, error.message);
+    }
+    const result = await contacts.addContact({ name, email, phone })
+    res.status(201).json(result);
+    console.log(result);
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContact(id)
+
+    if (!result) {
+      throw RequestError(404, 'Not found')
+    }
+    res.json(result)
+    console.log(result);
+
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { name, email, phone } = req.params;
+    const { error } = addShema.validate({ name, email, phone })
+    if (error) {
+      throw RequestError(400, error.message);
+    }
+    const result = await contacts.addContact({ name, email, phone })
+    if (!result) {
+      throw RequestError(404, 'Not Found')
+    }
+    res.json(result);
+    console.log(result);
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
