b62bb2c34f303a4caa0bb4ed4560216dc58280d9
PlatinumKellerman <platinumm@ukr.net>
2022-10-02 18:08:53 +0300

    in progress

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,35 +1,27 @@
 const express = require('express');
-
-const router = express.Router();
-
 const contacts = require('../../models/contacts');
+const router = express.Router();
+const { RequestError } = require('../../utils/');
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const result = await contacts.listContacts();
     res.json(result);
   } catch (error) {
-    res.status(500).json({
-      message: 'Server error',
-    });
+    next(error);
   }
 });
 
-router.get('/:contactId', async (req, res) => {
+router.get('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
     if (!result) {
-      const error = new Error('Not Found');
-      error.status = 404;
-      throw error;
+      throw RequestError(404, 'Not found');
     }
     res.json(result);
   } catch (error) {
-    const { status = 500, message = 'Server error' } = error;
-    res.status(status).json({
-      message: message,
-    });
+    next(error);
   }
 });
 
