b6d6613d2e1b0be424f69cd5f812e2843a78dc80
Snisarenko21 <sepalena@gmail.com>
2022-10-02 19:38:20 +0300

    del-comments

--- server.js
+++ server.js
@@ -1,24 +1,4 @@
-// const app = require("./app");
-// const mongoose = require("mongoose");
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
-
-// const DB_HOST =
-//   "mongodb+srv://Helen:y655ImAmOn4RfE0Z@cluster0.e6psikp.mongodb.net/contacts_reader?retryWrites=true&w=majority";
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     console.log("Database connection successful");
-//     // app.listen(PORT);
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
 const mongoose = require("mongoose");
-
 const app = require("./app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
