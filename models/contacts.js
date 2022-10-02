81afb635fec901db318736d4e70cede36b6eb95e
Yevhenii Multan <41617758+fatrathat@users.noreply.github.com>
2022-10-02 22:33:53 +0300

    update files

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,14 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const listContacts = async () => {};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +16,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
