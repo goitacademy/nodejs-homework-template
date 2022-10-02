85e807aa05fe76ba865df0fadb59009692b2a028
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-10-02 13:24:10 +0300

    Destructuring

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,4 +1,4 @@
-const Contact = require("../../models/contact");
+const {Contact} = require("../../models/contact");
 const RequsetError = require("../../helpers");
 
 const getById = async (req, res) => {
