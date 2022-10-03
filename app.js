1508e3c7f9fce9088772cd042a427c8238231e87
Vlad Shchelkunov <specter.ode@gmail.com>
2022-10-03 12:20:30 +0300

    add DB_HOST to env

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const dotenv = require("dotenv");
-
-dotenv.config();
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-
-const DB_HOST =
-  "mongodb+srv://Specter:Vova2705@cluster0.wg5unuj.mongodb.net/db-contacts?retryWrites=true&w=majority";
-const { PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
