13855472ef7e376f0de68b7d0b9f074687773a9a
irinaog <goncharirina08@gmail.com>
2022-10-04 16:22:27 +0300

    Update updateFavorite.js

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -3,6 +3,9 @@ const {RequestError} = require("../../helpers")
 
 const updateFavorite = async (req, res, next) => {
     const { id } = req.params;
+    if (!req.body) {
+        throw RequestError(400,"missing field favorite" )
+    }
     const result = await Contact.findByIdAndUpdate(id, req.body,"-createdAt  -updatedAt",{new:true});
     if (!result) {
         throw RequestError(404, 'Not found')
