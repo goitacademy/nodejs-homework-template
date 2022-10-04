de23398620965db36a12a4ceac146115baa7b1dd
Vlad Shchelkunov <specter.ode@gmail.com>
2022-10-04 11:39:01 +0300

    fix errorName

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,11 +1,11 @@
 const { Contact, joi } = require("../../models");
-const { RequestError } = require("../../helpers");
+const { requestError } = require("../../helpers");
 
 const addContact = async (req, res, next) => {
   try {
     const { error } = joi.addSchema.validate(req.body);
     if (error) {
-      throw RequestError(400, error.message);
+      throw requestError(400, error.message);
     }
     const contacts = await Contact.create(req.body);
     res.status(201).json(contacts);
