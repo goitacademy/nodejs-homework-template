5cb14066bd7111e4dd3cbd7581ad6d8c77467ddb
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-10-05 22:14:41 +0300

    done

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const getAll = require("./getAll");
+const getById = require("./getById");
+const add = require("./add");
+const updateById = require("./updateById");
+const deleteContact = require("./deleteContact");
+
+module.exports = {
+    getAll,
+    getById,
+    add,
+    updateById,
+    deleteContact
+};
