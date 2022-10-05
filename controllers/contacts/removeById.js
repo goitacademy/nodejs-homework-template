55d216a6c1c5cd5592fea50ad0aeb56b3c0afa1f
Yana Mironyak <yanika.mironyak@gmail.com>
2022-10-05 03:28:44 +0300

    update

--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -1,25 +1,20 @@
 const { NotFound } = require('http-errors');
-const contactsOperations = require('../../models/contacts');
+const { Contact } = require('../../models/contacts');
 
-const removeById = async (req, res, next) => {
-    try {
-        const { contactId } = req.params;
-        console.log(req.params)
-        const result = await contactsOperations.removeContact(contactId);
-        if (!result) {
-            throw NotFound(`Contact with id ${contactId} not found`);
-        }
-        res.json({
-            status: "success",
-            code: 200,
-            message: 'contact deleted',
-            data: {
-                result
-            }
-        });
-    } catch (error) {
-        next(error);
+const removeById = async (req, res) => {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndRemove(contactId);
+    if (!result) {
+        throw NotFound(`Contact with id ${contactId} not found`);
     }
+    res.json({
+        status: "success",
+        code: 200,
+        message: 'contact deleted',
+        data: {
+            result
+        }
+    });
 }
 
 module.exports = removeById;
