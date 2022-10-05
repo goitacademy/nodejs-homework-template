2b022faadfa462ef62b012c0fff8bc1280eaec77
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2022-10-05 16:29:08 +0300

    Delete messages in RequestError

--- helpers/RequestError.js
+++ helpers/RequestError.js
@@ -1,11 +1,3 @@
-const messages = {
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    403: 'Forbidden',
-    404: 'Not Found',
-    409: 'Conflict',
-};
-
 const RequestError = (status, message) => {
     const error = new Error(message);
     error.status = status;
