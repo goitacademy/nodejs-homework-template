d721dbd13cdc62b778b2ae139008c5ed253a8abe
Snizhana15 <snischana15.05@gmail.com>
2022-10-08 20:33:26 +0300

    settings

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Snizhana:VZcC6SZ0bB12isV7@cluster0.ncetjq6.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Datanse connect success"))
+  .catch(error => console.log(error.message));
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// VZcC6SZ0bB12isV7
