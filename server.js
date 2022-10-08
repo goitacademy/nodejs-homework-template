f0fab0ca9c333f8413842c0113de61e3b8477855
Snizhana15 <snischana15.05@gmail.com>
2022-10-08 21:25:08 +0300

    setings

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const {DB_HOST} = require('./config')
 
-mongoose.connect(DB_HOST)
-  .then(() => console.log("Datanse connect success"))
-  .catch(error => console.log(error.message));
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
+const { DB_HOST, PORT = 3000 } = process.env;
 
-// VZcC6SZ0bB12isV7
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
