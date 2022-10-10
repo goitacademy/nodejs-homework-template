4d59007a2fbdc04cdbf8230c0054cb3f90e68850
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-10-10 21:00:33 +0300

    get, get byId and post done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,66 @@
 const express = require('express')
+const Joi = require('joi')
+
+const contacts = require('../../models/contacts')
+
+const { RequestError } = require('../../helpers')
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const schema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(10).max(15).required(),
 })
 
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+  } catch (error) { 
+    next(error)
+  }
+})
+
+
+
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId)
+    if (!result) {
+      throw RequestError('Not found', 404)
+    }
+    res.json(result)
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = schema.validate(req.body)
+    if (error) {
+      throw RequestError(`missing required ${error.message}field`, 400)
+    }
+
+    const result = await contacts.addContact(req.body)
+    res.status(201).json(result)
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.removeContact(req.params.contactId)
+  res.json(result)
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.updateContact(req.params.contactId, req.body)
+  res.json(result)
 })
 
 module.exports = router
