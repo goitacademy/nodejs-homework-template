aaa64605652ee9568ada98ce2524cc6de89c4450
SashaDyts <lentrofan@gmail.com>
2022-10-10 12:06:01 +0300

    summ

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,7 @@
 const RequestError = require('./RequestError');
+const addSchema = require('./validation');
 
 module.exports = {
   RequestError,
+  addSchema,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,37 +1,10 @@
 const express = require('express');
-const Joi = require('joi');
 
 const contactsOperations = require('../../models/contacts');
 
-const { RequestError } = require('../../helpers');
+const { RequestError, addSchema } = require('../../helpers');
 
 const router = express.Router();
-
-const addSchema = Joi.object({
-  name: Joi.string()
-    .max(100)
-    .messages({
-      'string.empty': 'field must contain `name`',
-      'any.required': 'missing required `name` field',
-    })
-    .required(),
-  email: Joi.string()
-    .email()
-    .messages({
-      'string.empty': 'field must contain `email`',
-      'string.email': 'email is invalid',
-      'any.required': 'missing required `email` field',
-    })
-    .required(),
-  phone: Joi.string()
-    .pattern(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)
-    .messages({
-      'string.empty': 'field must contain `phone number`',
-      'string.pattern.base': 'phone number is invalid',
-      'any.required': 'missing required `phone` field',
-    })
-    .required(),
-});
 
 router.get('/', async (req, res, next) => {
   try {
