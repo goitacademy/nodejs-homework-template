8d81a1b01a61751062bda080ce7eb83affabade5
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-10-11 16:25:46 +0300

    added dotenv

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const app = require('./app')
+const app = require("./app");
+const chalk = require("chalk");
+require("dotenv").config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (error) => {
   if (error) console.error(error.message);
-  else console.log(`http://localhost:${PORT}/\nServer running. Use our API on port: ${PORT}`)
-})
+  else console.log(chalk.cyan.underline(`http://localhost:${PORT}`));
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
