1ac79b2fe9e5a778aff336ee35a2b0cd23229704
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2022-10-11 20:05:22 +0300

    jimp

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -1,3 +1,4 @@
+const Jimp = require('jimp');
 const path = require('path');
 const fs = require('fs/promises');
 
@@ -8,6 +9,11 @@ const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 const updateAvatar = async (req, res) => {
 
 const {path: tempUpload, originalname} = req.file;
+
+const img = await Jimp.read(tempUpload);
+await img.autocrop().cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE).writeAsync(tempUpload);
+
+
 const {_id: id} = req.user; 
 const imageName = `${id}_${originalname}`;
 
