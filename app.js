3a94fc51427453f7bae2b3f82519fa2095f1cc08
IrynaShashko <irishkashashko@gmail.com>
2022-10-11 16:49:32 +0300

    add database

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+// rU_YP5hCjhf_gXH; mongodb+srv://Iryna:rU_YP5hCjhf_gXH@cluster0.nn1itut.mongodb.net/test
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST } = process.env;
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
 
 const contactsRouter = require("./routes/api/contacts");
 
