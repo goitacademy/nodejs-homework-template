eaf93dbc5cf841c0e02c7c932bcd2a29e4a59e93
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2022-10-11 13:48:44 +0300

    unic

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -7,12 +7,14 @@ const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const updateAvatar = async (req, res) => {
 
-const {path: tempUpload, originalname} = req.file;  
+const {path: tempUpload, originalname} = req.file;
+const {_id: id} = req.user; 
+const imageName = `${id}_${originalname}`;
 
 try {
-const resultUpload = path.join(avatarsDir, originalname);
+const resultUpload = path.join(avatarsDir, imageName);
 await fs.rename(tempUpload, resultUpload)
-const avatarURL = path.join('public', 'avatars', originalname);
+const avatarURL = path.join('public', 'avatars', imageName);
 await User.findByIdAndUpdate(req.user._id, {avatarURL});
 
 res.status(200).json({avatarURL});
