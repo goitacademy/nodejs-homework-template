1c274ea8b973dc2f5eb88f92e73f88348ab2b0e3
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-10-11 14:18:29 +0300

    end

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const contacts = require("../../models/contacts");
-const router = express.Router();
-const Joi = require("joi");
-const contactsSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
-});
 
+const Contact = require("../../models/contact");
+const router = express.Router();
+
+const contactsSchema = require("../../models/contact");
 router.get("/", async (req, res, next) => {
-  const data = await contacts.listContacts();
+  const data = await Contact.find();
   res.json({
     status: "success",
     code: 200,
@@ -22,7 +18,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
+    const result = await Contact.findOne({ _id: contactId });
     if (!result) {
       res.status(404).json({
         status: "error",
@@ -50,7 +46,7 @@ router.post("/", async (req, res, next) => {
       error.status = 400;
       throw error;
     }
-    const newContact = await contacts.addContact(req.body);
+    const newContact = await Contact.create(req.body);
     res.status(201).json({
       status: "succes",
       code: 201,
@@ -65,7 +61,7 @@ router.post("/", async (req, res, next) => {
 
 router.delete("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
-  const removeContactyId = await contacts.removeContact(contactId);
+  const removeContactyId = await Contact.findByIdAndRemove({ _id: contactId });
   if (!removeContactyId) {
     res.status(404).json({
       status: "error",
@@ -98,7 +94,12 @@ router.put("/:contactId", async (req, res, next) => {
       });
     }
     const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
+
+    const result = await Contact.findByIdAndUpdate(
+      { _id: contactId },
+      req.body,
+      { new: true }
+    );
     if (!result) {
       res.status(404).json({
         status: "success",
@@ -115,4 +116,44 @@ router.put("/:contactId", async (req, res, next) => {
     next(error);
   }
 });
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { error } = contactsSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    if (!req.body) {
+      res.status(400).json({
+        status: "error",
+        code: 400,
+        message: "missing field favorite",
+      });
+    }
+
+    const { contactId } = req.params;
+
+    const result = await Contact.findByIdAndUpdate(
+      { _id: contactId },
+      req.body,
+      { new: true }
+    );
+    if (!result) {
+      res.status(404).json({
+        status: "error",
+        code: 404,
+        message: "Not found",
+      });
+    }
+    res.status(201).json({
+      status: "success",
+      code: 200,
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
