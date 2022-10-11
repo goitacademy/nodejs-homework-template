0896367ee8a5275b79e8fbe540a4a47356ee25b4
Svetlana Fedorenko <svetlana493@ukr.net>
2022-10-11 09:54:53 +0300

    5

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,5 @@
 const validation = require("./validation");
 const ctrlWrapper = require("./ctrlWrapper");
-const auth = require("./auth");
 const upload = require("./upload");
 
 module.exports = {
