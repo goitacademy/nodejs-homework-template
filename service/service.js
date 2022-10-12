d998f6261574061bbc0d7dff135a6b444b7ab4dd
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-10-12 14:31:17 +0300

    rewrites with Mongoose

---
+++ service/service.js
@@ -0,0 +1,33 @@
+const { Contact } = require("./schemas");
+
+const listContacts = () => {
+  return Contact.find();
+};
+
+const getContactById = (id) => {
+  return Contact.findById();
+};
+
+const removeContact = (id) => {
+  return Contact.findByIdAndRemove({ _id: id });
+};
+
+const addContact = (body) => {
+  return Contact.create(body);
+};
+
+const updateContact = (id, fields) => {
+  return Contact.findByIdAndUpdate({ _id: id }, fields, { new: true });
+};
+
+const updateStatus = (id, favorite) => {
+  return Contact.findByIdAndUpdate({ _id: id }, favorite, { new: true });
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
