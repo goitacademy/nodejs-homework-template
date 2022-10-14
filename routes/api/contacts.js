0c515870c05b69b2fa55a0cd52a49c167b75b14b
kriminskiy <93137873+kriminskiy@users.noreply.github.com>
2022-10-14 18:45:42 +0300

    v2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,15 +3,18 @@ const { validation, ctrlWrapper } = require("../../middlewares");
 const { contactSchema } = require("../../schemas");
 const { contacts: ctrl } = require("../../controllers");
 
-const validateMiddleware = validation(contactSchema);
-
 const router = express.Router();
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
 router.get("/:id", ctrlWrapper(ctrl.getByid));
 
-router.post("/", validateMiddleware, ctrlWrapper(ctrl.add));
+router.post(
+  "/",
+  validateMiddleware,
+  validation(contactSchema),
+  ctrlWrapper(ctrl.add)
+);
 
 router.put("/:id", validateMiddleware, ctrlWrapper(ctrl.updateById));
 
