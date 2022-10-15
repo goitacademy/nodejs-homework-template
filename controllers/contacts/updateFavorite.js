93bcf93f449550143c58516fe97186d2dfefc8e0
AlexandrRust <123_00@ukr.net>
2022-10-15 16:13:43 +0300

    fix controllers

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -4,7 +4,9 @@ const { RequestError } = require("../../helpers");
 
 const updateFavorite = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw RequestError(404, "Not found");
   }
