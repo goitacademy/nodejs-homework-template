51698707bb266e0491087506734d4d2a651e97d5
IrynaShashko <irishkashashko@gmail.com>
2022-10-15 19:22:33 +0300

    fix

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -2,7 +2,7 @@ const getAll = require("./getAll");
 const getById = require("./getById");
 const add = require("./add");
 const removeById = require("./removeById");
-const updateById = require("./removeById");
+const updateById = require("./updateById");
 const updateFavorite = require("./updateFavorite");
 
 module.exports = {
