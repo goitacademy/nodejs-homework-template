c40a8550b7e03e52c091ec64ea9740e838f03950
Julanna11 <joy93julanna@gmail.com>
2022-10-15 19:35:18 +0300

    mongodb

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -5,7 +5,7 @@ const { RequestError } = require("../../helpers");
 const add = async (req, res) => {
   const { error } = Contact.create(req.body);
   if (error) {
-    throw RequestError(400, "missing required name field");
+    throw RequestError(404, "missing required name field");
   }
   const result = await Contact.create(req.body);
   res.status(201).json(result);
--- middlewars/index.js
+++ middlewars/index.js
@@ -1,4 +1,5 @@
 const validateBody = require("./validateBody");
 const handleSaveError = require("./handleSaveError");
 const isValidId = require("./isValidId");
+
 module.exports = { validateBody, handleSaveError, isValidId };
