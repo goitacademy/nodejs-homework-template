41056a21a1e198b5e966767cff5433d8dc5e483f
AlekseySteblivets <alsteua@gmail.com>
2022-10-16 15:29:52 +0300

    added pagination to "getListContacts"

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -2,7 +2,13 @@ const { Contact } = require('../../models');
 
 const getListContacts = async (req, res) => {
   const { _id } = req.user;
-  const products = await Contact.find({ owner: _id }).populate('owner', '_id email subscription');
+  console.log('req.query-getListContacts:', req.query);
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const products = await Contact.find({ owner: _id }, '', {
+    skip,
+    limit: Number(limit),
+  }).populate('owner', '_id email subscription');
 
   res.json({
     status: 'success',
