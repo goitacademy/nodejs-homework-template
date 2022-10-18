f60f33fe47d39e3934eb9d84d78b886539e61e83
AnnaNesterenko <nura_arh@ukr.net>
2022-10-18 14:34:55 +0300

    addAllTasks

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,14 +1,13 @@
-const contactsOperations = require("../../models/contactsModels");
-const contactSchema = require("../../schema/schema");
+const { Contact, schemas } = require("../../models/contact");
 
 const addContact = async (req, res, next) => {
   try {
-    const { error } = contactSchema.validate(req.body);
+    const { error } = schemas.addSchema.validate(req.body);
     if (error) {
       error.status = 400;
       throw error;
     }
-    const result = await contactsOperations.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json({
       status: "success",
       code: 201,
