b577f7f76832343d89218c41a5aab8f0ef20f03e
Marya Samsonenko <samashushka7@gmail.com>
2022-10-18 13:32:53 +0300

    add hw3

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,5 @@
 const RequestError = require("./RequestError");
 const { logger } = require("./logger");
 const controllerWrapper = require("./controllerWrapper");
-
-module.exports = { RequestError, logger, controllerWrapper };
+const handleSaveErrors = require("./handleSaveErrors");
+module.exports = { RequestError, logger, controllerWrapper, handleSaveErrors };
--- models/contact.js
+++ models/contact.js
@@ -1,38 +1,49 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
+const { handleSaveErrors } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
-const schemaCreate = Joi.object({
+const addSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email({ tlds: { allow: false } }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
   phone: Joi.string().min(6).max(15).required(),
   favorite: Joi.bool(),
 });
 
-const schemaPatch = Joi.object({
+const updateFavoriteSchema = Joi.object({
   favorite: Joi.bool().required("missing field favorite"),
 });
 
+contactSchema.post("save", handleSaveErrors);
+
 const Contact = model("contact", contactSchema);
+const schemas = { addSchema };
 
 module.exports = {
   Contact,
-  schemaCreate,
-  schemaPatch,
+  schemas,
+  updateFavoriteSchema,
 };
