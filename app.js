9785b8202ccc333d87baaba19fe4ee7f9186f008
Maksym <maksstair@gmail.com>
2022-10-19 14:55:39 +0300

    start-hw-02

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const morgan = require("morgan");
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -10,6 +11,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
+app.use(morgan("tiny"))
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
