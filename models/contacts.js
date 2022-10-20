018bb5990b870a58844cf2e8d4f8b657e0bb4bdd
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-10-20 15:19:14 +0300

    first problem

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,59 @@
-// const fs = require('fs/promises')
+const path = require("path");
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+console.log(contactsPath);
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const result = await fs.readFile(contactsPath);
+    return JSON.parse(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    contacts.map((element) => {
+      if (Number(element.id) === contactId) {
+        console.log(element);
+        return element;
+      }
+    })
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    // contacts.filter(element => Number(element.id) === contactId);
+    contacts.splice(contactId - 1, 1);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const lastElementIndex = contacts.length + 1;
+    contacts.push({
+      id: `${lastElementIndex}`,
+      name: `${name}`,
+      email: `${email}`,
+      phone: `${phone}`
+    })
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const updateContact = async (contactId, body) => { }
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,20 @@ const express = require('express')
 
 const router = express.Router()
 
+const contactsOperations = require("../../models/contacts");
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contactsOperations.listContacts();
+    res.json(result)
+  } catch {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Server error",
+    })
+  }
+  next
 })
 
 router.get('/:contactId', async (req, res, next) => {
@@ -12,14 +24,17 @@ router.get('/:contactId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
+
 })
 
 module.exports = router
