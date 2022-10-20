a241a543d1b1b43f2cfd141b4cf262ae3a4902ba
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-10-20 23:44:38 +0300

    hw5

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,4 +1,6 @@
 const fs = require("fs/promises");
+const Jimp = require("jimp");
+
 const path = require("path");
 
 const { User } = require("../../models/user");
@@ -9,13 +11,18 @@ const updateAvatar = async (req, res) => {
   try {
     const { _id } = req.user;
     const { path: tempUpload, originalname } = req.file;
+
+    const img = await Jimp.read(tempUpload);
+    await img.cover(250, 250);
+    await img.writeAsync(tempUpload);
+
     const extention = originalname.split(".").pop();
     const filename = `${_id}.${extention}`;
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
-    const avatraURL = path.join("avatars", filename);
-    await User.findByIdAndUpdate(_id, { avatraURL });
-    res.json({ avatraURL });
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({ avatarURL });
   } catch (error) {
     fs.unlink(req.file.path);
     throw error;
