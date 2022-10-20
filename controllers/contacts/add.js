4cd795fcffdd9077906354491277cec0e6096edf
VitaliyKhmil <hmilvitaliy@gmail.com>
2022-10-20 22:22:59 +0300

    fix error 500

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,4 +1,4 @@
-const { Contact } = require("../../models/contact");
+const { Contact } = require("../../models/contacts");
 
 const add = async (req, res) => {
   const contact = await Contact.create(req.body);
