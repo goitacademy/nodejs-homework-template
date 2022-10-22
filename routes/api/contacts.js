f25eb0cc636ac7d05f3e42a29a83f4b5cc828fe0
Olena-Milham <nikimilh@gmail.com>
2022-10-22 21:58:32 +0100

    add routers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const Joi = require("joi");
 
 const contacts = require("../../models/contacts");
 
 const { RequestError } = require("../../helpers");
+
 const router = express.Router();
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -29,18 +37,47 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    console.log(req.body);
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw RequestError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw RequestError(404, "Not found");
+    }
+    res.json({
+      message: "Deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw RequestError(400, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw RequestError(404, "Not found");
+    }
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
