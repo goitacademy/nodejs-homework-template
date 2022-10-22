5ab8ddda5bb061c6a9783f60ca96c99a6ac7261e
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-10-22 20:20:27 +0300

    add schema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 
-const contactsSchema = new Schema({
+const contactsSchema = new Schema ({
   name: {
     type: String,
     required: [true, "Set name for contact"],
