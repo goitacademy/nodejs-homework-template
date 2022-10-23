64a898da1f39a3e611a1049f6c22cd996c5b0b1e
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-10-23 15:11:04 +0300

    added env file

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST } = process.env;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
 
 const contactsRouter = require("./routes/api/contacts");
 
