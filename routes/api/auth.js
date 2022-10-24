551166e43f2fc72a68e4b341ebcc7ab0b4fe92b9
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-10-24 18:58:06 +0300

    add fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -24,12 +24,6 @@ router.post("/logout", auth, ctrlWrapper(ctrl.logout));
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
-router.patch(
-  "/",
-  validation(userJoiSchema.subscriptionSchema),
-  auth,
-  ctrlWrapper(ctrl.updateSubscription)
-);
-
+router.patch("/", validation(userJoiSchema.subscriptionSchema), auth, ctrlWrapper(ctrl.updateSubscription));
 
 module.exports = router;
