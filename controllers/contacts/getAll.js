a7dcb57a25ae0bcde4abd79c241cf768530f6200
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-10-24 17:26:36 +0300

    add auth

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,6 +1,6 @@
 const service = require("../../service/contacts");
 
-const getAllContacts = async (req, res, next) => {
+const getAllContacts = async (req, res) => {
   const contacts = await service.getAllContacts();
   res.status(200).json({ status: "success", code: 200, data: contacts });
 };
