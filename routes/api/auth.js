0fae005d632abb1dd6740923a6f7bb88233fd770
Marya Samsonenko <samashushka7@gmail.com>
2022-10-25 14:08:57 +0300

    add user model

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { controllerWrapper } = require("../../helpers");
+const { validateBody } = require("../../middlewares");
+
+const router = express.Router();
+
+// router.post("/register");
+
+module.exports = router;
