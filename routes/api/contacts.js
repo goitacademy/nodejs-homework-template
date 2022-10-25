df615eb00aaea10c0aac81c7f99987b5144dc74d
Polosmakr <polosmakrr@gmail.com>
2022-10-25 20:59:15 +0300

    basic logic
    was created base logic REST API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,38 @@
 const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contacts = await listContacts();
+  res.json(contacts);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await getContactById(req.params.contactId);
+  res.json(contact);
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const addedContact = await addContact(req.body);
+
+  res.json(addedContact);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const removedContact = await removeContact(req.params.contactId);
+  res.json(removedContact);
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const updatedContact = await updateContact(req.params.contactId, req.body);
+  res.json(updatedContact);
 });
 
 module.exports = router;
