05ee9fce3de0625369b0dd62f34af458165a2ab0
Krasnonos <andreykrasnonos@icloud.com>
2022-10-28 12:55:04 +0300

    add gravatarUrl

--- services/authServices/signUpUser.js
+++ services/authServices/signUpUser.js
@@ -1,4 +1,5 @@
 const bcryptjs = require('bcryptjs');
+const gravatar = require('gravatar');
 const { User } = require('../../models/auth');
 const { createReject } = require('../../utils');
 
@@ -8,12 +9,15 @@ const signUpUser = async ({ email, password }) => {
     throw createReject(409, 'Email in use');
   }
 
+  const avatarURL = gravatar.url(email);
+
   const result = await User.create({
     email,
     password: await bcryptjs.hash(password, 10),
+    avatarURL,
   });
 
-  return { email: result.email, subscription: result.subscription };
+  return { email: result.email, subscription: result.subscription, avatarURL };
 };
 
 module.exports = signUpUser;
