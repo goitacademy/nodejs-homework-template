23b07224f96af58c963f13f4b7795bf73aac9858
Andrey1914 <anlikajw@gmail.com>
2022-10-30 11:03:41 +0100

    mongodb

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const { DB_URL, PORT } = process.env;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => app.listen(PORT))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
