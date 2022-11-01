90bf41e7a43c47f2cc3eabb0a85b1202eecac933
Denys Kuz <dkuz@gomage.com>
2022-11-01 23:48:12 +0200

    HW-2: Created Simple API

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const { status } = require('./status');
+
+require('dotenv').config();
+
 
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = process.env.NODE_ENV === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
@@ -15,11 +19,11 @@ app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(status.NOT_FOUND).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  res.status(status.INTERNAL_SERVER_ERROR).json({ message: err.message })
 })
 
 module.exports = app
--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,55 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path');
+const contactsPath = path.resolve('./models/contacts.json');
+const shortid = require('shortid');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const response = await fs.readFile(contactsPath);
+  return JSON.parse(response);
+} 
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const response = await fs.readFile(contactsPath);
+  return JSON.parse(response).filter(contact => contact.id == contactId)[0];
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const response = await fs.readFile(contactsPath).catch(err => console.error(err.message));
+  const contacts = JSON.parse(response);
+  const objWithIdIndex = contacts.findIndex((contact) => contact.id == contactId);
 
-const addContact = async (body) => {}
+  if (objWithIdIndex === -1) { 
+    throw new Error('User with this id does not exist');
+  }
 
-const updateContact = async (contactId, body) => {}
+  contacts.splice(objWithIdIndex, 1);
+  const contactsList = JSON.stringify([...contacts], null, '\t')
+  await fs.writeFile(contactsPath, contactsList).catch(err => console.error(err.message));
+  return response;
+}
+
+const addContact = async ({ name, email, phone }) => {
+  const allContacts = await fs.readFile(contactsPath).catch(err => console.error(err.message));
+  const contactNew = {id: shortid.generate(), name, email, phone }
+  const contactsList = JSON.stringify([contactNew, ...JSON.parse(allContacts)], null, '\t')
+  await fs.writeFile(contactsPath, contactsList).catch(err => console.error(err.message));
+  return JSON.parse(contactsList);
+}
+
+const updateContact = async (contactId, body) => {
+  const response = await fs.readFile(contactsPath).catch(err => console.error(err.message));
+  const contacts = JSON.parse(response);
+  const objWithIdIndex = contacts.findIndex((contact) => contact.id == contactId);
+  if (objWithIdIndex === -1) { 
+    throw new Error('User with this id does not exist');
+  }
+  contacts[objWithIdIndex] = {
+    ...contacts[objWithIdIndex],
+    ...body
+  };
+  await fs.writeFile(contactsPath, contacts).catch(err => console.error(err.message));
+  return contacts[objWithIdIndex];
+}
 
 module.exports = {
   listContacts,
