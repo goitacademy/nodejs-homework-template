3b85260a7dfb510b70db98f5bf4cd0d413f0fcd8
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-11-03 18:54:46 +0200

    Добавляет пул с дз

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,54 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.resolve("./models/contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  return parsedData;
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  const contactById = parsedData.find((contact) => contact.id === contactId);
+  console.table(contactById);
+  return contactById;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  const contactById = parsedData.filter((contact) => contact.id !== contactId);
+  console.table(contactById);
+  return contactById;
+};
 
-const addContact = async (body) => {}
+const addContact = async (name, email, phone) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  const id = Math.random().toString();
+  parsedData.push({ id, name, email, phone });
+  const jsonNewData = JSON.stringify(parsedData);
+  fs.writeFile(contactsPath, jsonNewData);
+  return jsonNewData;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, name, email, phone) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  parsedData.forEach((contact) => {
+    if (contact.id === contactId) {
+      contact.id = contactId;
+      contact.name = name;
+      contact.email = email;
+      contact.phone = phone;
+    }
+  });
+  const jsonNewData = JSON.stringify(parsedData);
+  fs.writeFile(contactsPath, jsonNewData);
+  return jsonNewData;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +56,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,67 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
 
-const router = express.Router()
+const router = express.Router();
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("/nodejs-homework-template-vm/models/contacts");
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.status(200).json({ message: "success", code: 200, contacts });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contactById = await getContactById(contactId);
+  if (!contactById) {
+    res.status(400).json({ message: "not found", code: 400 });
+  }
+  res.status(200).json({ message: "success", code: 200, contactById });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const scema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    phone: Joi.string().min(7).max(20).required(),
+  });
+  const validateContact = scema.validate(req.body);
+  if (validateContact.error) {
+    return res.status(400).json({ message: validateContact.error, code: 400 });
+  }
+  const { name, email, phone } = req.body;
+  const contact = await addContact(name, email, phone);
+  res.status(201).json({ message: "contact added", contact });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contacts = await removeContact(contactId);
+  res.status(201).json({ message: "contact deleted", contacts });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const scema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    phone: Joi.string().min(7).max(20).required(),
+  });
+  const validateContact = scema.validate(req.body);
+  if (validateContact.error) {
+    return res.status(400).json({ message: validateContact.error, code: 400 });
+  }
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  const contactByUpdate = await updateContact(contactId, name, email, phone);
+  res.status(201).json({ message: "contact deleted", contactByUpdate });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
