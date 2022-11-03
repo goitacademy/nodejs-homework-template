258a64e06dd4c52879b2dd69ec97597eb05eccce
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-11-03 22:28:26 +0100

    add API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,57 @@
-const express = require('express')
+const express = require('express');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts.js');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', async (req, res) => {
+  const contacts = await listContacts();
+  if (contacts) {
+    res.status(200).json(contacts);
+  } else {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', async (req, res) => {
+  const id = req.params.contactId;
+  const contact = await getContactById(id);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', async (req, res) => {
+  const { email, phone, name } = req.body;
+  await addContact({ email, phone, name });
+  res.status(201).send('created');
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', async (req, res) => {
+  const id = req.params.contactId;
+  const contact = await removeContact(id);
+  if (contact) {
+    res.status(201).send('created');
+  } else {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:contactId', async (req, res) => {
+  const id = req.params.contactId;
+  const contact = await updateContact(id, req.body);
+  if (contact) {
+    res.status(201).send('created');
+  } else {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-module.exports = router
+module.exports = router;
