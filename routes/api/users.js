b404583e67c2284df6b1e1f022eb1c5b0765bc9f
Toni1993 <77364364+Toni1993@users.noreply.github.com>
2022-11-03 22:06:57 +0100

    first commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const { validateBody, authenticate } = require('../../middlewares')
+const { validateBody, authenticate, upload, resize } = require('../../middlewares')
 
 const { ctrlWrapper } = require('../../helpers')
 
@@ -19,5 +19,7 @@ router.post('/login', validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login)
 router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent))
 
 router.get('/logout', authenticate, ctrlWrapper(ctrl.logout))
+
+router.patch('/avatars', authenticate, upload.single('avatar'), resize, ctrlWrapper(ctrl.updateAvatar))
 
 module.exports = router
