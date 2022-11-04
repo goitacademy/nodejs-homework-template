6a99360473a0cd44b52f11b36629919d86c3dc40
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-11-04 19:58:34 +0200

    resize/jimp

--- controlers/auth/updateAvatar.js
+++ controlers/auth/updateAvatar.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const { User } = require("../../models/Users");
+const Jimp = require("jimp");
 
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
@@ -10,6 +11,8 @@ const updateAvatar = async (req, res) => {
   const filename = `${_id}.${extention}`;
   const resultUpload = path.join(avatarDir, filename);
   await fs.rename(tempUpload, resultUpload);
+  const resizeAvatar = await Jimp.read(resultUpload);
+  await resizeAvatar.resize(250, 250).write(resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
