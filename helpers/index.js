8012a3ffb9b41153def604c0cd04626915154d81
luv_cr1m3s <luv_cr1m3s@protonmail.com>
2022-11-06 19:26:54 +0100

    foo

---
+++ helpers/index.js
@@ -0,0 +1,9 @@
+const RequestError = require('./RequestError');
+const ctrlWrapper = require('./ctrlWrapper');
+const handleSaveErrors = require('./handleSaveError');
+
+module.exports = {
+  RequestError,
+  ctrlWrapper,
+  handleSaveErrors,
+};
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const { HOST_DB, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(HOST_DB)
+  .then(() => {
+    app.listen(PORT);
+    console.log('Database connection successful');
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
