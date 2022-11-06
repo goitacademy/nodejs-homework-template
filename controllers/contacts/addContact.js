7a097a9fc3eef3c49ee081ab3310f72e04e2faef
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-11-06 22:24:36 +0200

    finally fix

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,32 +1,15 @@
-const Joi = require("joi");
 const contactsOperations = require("../../models/contactsFunctions");
 
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
-
 const addContact = async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      error.message = "missing required name field";
-      throw error;
-    }
-    const result = await contactsOperations.addContact(req.body);
+  const result = await contactsOperations.addContact(req.body);
 
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      result,
+    },
+  });
 };
 
 module.exports = addContact;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 const { contacts: ctrl } = require("../../controllers");
 const { ctrlWrapper } = require("../../middlewares");
+const { validation } = require("../../middlewares");
+const { contactSchema } = require("../../schemas");
+
+const validateMiddlware = validation(contactSchema);
 
 router.get("/", ctrlWrapper(ctrl.listContacts));
 
 router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
 
-router.post("/", ctrlWrapper(ctrl.addContact));
+router.post("/", validateMiddlware, ctrlWrapper(ctrl.addContact));
 
 router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
-router.put("/:contactId", ctrlWrapper(ctrl.updateById));
+router.put("/:contactId", validateMiddlware, ctrlWrapper(ctrl.updateById));
 
 module.exports = router;
