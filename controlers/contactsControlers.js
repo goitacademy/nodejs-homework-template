0584024e325d440fd7d3eaec39df9bc526108aa0
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-07 14:37:47 +0200

    1

--- controlers/contactsControlers.js
+++ controlers/contactsControlers.js
@@ -12,43 +12,49 @@ async function getAll(req, res, next) {
 }
 
 async function findOneById(req, res, next) {
-    const { id } = req.params;
-    const contact = await Contact.findById(id);
-    if (contact) {
-         res.json({ data: { contact } });
-    }
-     res.status(404).json({ message: 'Not Found' });
-    
+    try {
+        const { id } = req.params;
+        const contact = await Contact.findById(id);
+        return res.json({ data: { contact } });
+    } catch(err) {
+        next(err)
+        return res.status(404).json({ message: 'Not Found' });
+    }    
 }
 
 
 async function create(req, res, next) {
     try {
         const createdContacts = await Contact.create(req.body);
-        res.status(201).json({
+        return res.status(201).json({
             contact: createdContacts     
         })
     } catch(err) {
         next(err)
+        return res.status(404).json({ message: err.message });
     }
 }
 
 async function deleteById(req, res, next) {
-    const { id } = req.params;
-    const contact = await Contact.findByIdAndDelete(id);
-    if (contact) {
-         res.json({ data: { contact } });
+    try {
+        const { id } = req.params;
+        const contact = await Contact.findByIdAndDelete(id);
+        return res.json({ data: { contact } });
+    } catch(err) {
+        next(err)
+        return res.status(404).json({ message: 'Not Found' });
     }
-     res.status(404).json({ message: 'Not Found' });
 }
 
 async function updateById(req, res, next) {
-    const { id } = req.params;
-    const updateContact = await Contact.findByIdAndUpdate(id, req.body, {new: true});
-    if (updateContact) {
-         res.status(201).json({contact: updateContact});
+    try {
+       const { id } = req.params;
+        const updateContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+        return res.status(201).json({contact: updateContact});
+    } catch(err) {
+        next(err)
+        return res.status(404).json({ message: err.message });
     }
-     res.status(404).json({ message: 'Not Found' });
 }
 
 async function updateStatusContact(req, res, next) {
@@ -56,9 +62,10 @@ async function updateStatusContact(req, res, next) {
         const { contactId } = req.params;
         const { favorite } = req.body;
         const result = await Contact.findByIdAndUpdate(contactId,{ favorite },{ new: true });
-        res.status(200).json({ status: "success", code: 200, data: { result }, });
+        return res.status(200).json({ status: "success", code: 200, data: { result }, });
     } catch(err) {
         next(err)
+        return res.status(404).json({ message: 'Not Found' });
     }
 };
 
