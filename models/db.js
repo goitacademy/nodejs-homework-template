42d3fd713234d641cdb1899b4246241973c34294
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-08 00:57:17 +0200

    Create db.js

---
+++ models/db.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const {HOST_DB} = process.env;
+console.log("process:", process.env.HOST_DB);
+const db = mongoose.connect(HOST_DB, {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+});
+console.log("Database: ", db);
+mongoose.connection.on("connected", () => {
+  console.log("Database connection successful");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`Database connection error: ${err.message}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Database disconnected");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("Database connection closed and app termination");
+  process.exit(1);
+});
+
+module.exports = db;
