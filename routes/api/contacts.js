371b2920ca732973e54b39d5a1bb27f9894fe1f2
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-08 00:57:01 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+// const Contacts = require("../../models/db");
+// const Contacts = require("../../models/contactsDb");
 const Contacts = require("../../models/contacts");
 const validate = require("../../services/validation");
 
