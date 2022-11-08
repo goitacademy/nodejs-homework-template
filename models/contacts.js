bac774baae9bbfcaeab93e2c075cb4da5eca14c2
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-08 22:02:52 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const path = require("path");
 const {nanoid} = require("nanoid");
 
-const contactsPath = path.join(__dirname, "/contacts.json");
+const contactsPath = path.join(__dirname, "/contactsJSON.json");
 
 const listContacts = async () => {
   const list = await fs.readFile(contactsPath, "utf8");
