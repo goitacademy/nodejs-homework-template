3ba41c7e688c9015e4930615048b0469d5dd7a05
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-08 22:01:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// const app = require('./app')
 require("dotenv").config();
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
+
 const app = require("./app");
 const db = require("./models/db");
-// const PORT = 3333;
+
 const PORT = process.env.PORT || 3333;
 
 db.then(() => {
@@ -16,7 +13,5 @@ db.then(() => {
   console.log(`Server not running. Error message: ${err.message}`);
   process.exit(1);
 });
-// app.listen(PORT, () => {
-//   console.log(`Server running. Use our API on port: ${PORT}`);
-// });
+
 console.log("process:", process.env.PORT);
