ee89f62dd7ccd1780d9fa8da6164aa665e0d7070
Konstantin Ligus <ligus.konstantin@gmail.com>
2022-11-08 18:15:07 +0200

    delete log

--- middleware/validationBody.js
+++ middleware/validationBody.js
@@ -4,7 +4,6 @@ const validationBody = (schema) => {
       await schema.validateAsync(req.body);
       next();
     } catch (error) {
-      console.log(error.name);
       error.status = 400;
       next(error);
     }
