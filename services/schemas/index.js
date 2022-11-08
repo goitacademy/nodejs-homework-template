2ecd4644fa76e500bb17d6b18d394ea9cedd7af1
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-09 01:20:44 +0200

    Update index.js

--- services/schemas/index.js
+++ services/schemas/index.js
@@ -1,46 +1,46 @@
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const mongoose = require("mongoose");
+const {Schema, model} = mongoose;
 
 const contactSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, 'this field is required'],
+      required: [true, "this field is required"],
       unique: true,
     },
     email: {
       type: String,
-      required: [true, 'this field is required'],
+      required: [true, "this field is required"],
       unique: true,
     },
     phone: {
       type: String,
-      required: [true, 'this field is required'],
+      required: [true, "this field is required"],
       unique: true,
       validate: {
         validator: function (v) {
           return /\(\d{3}\)\s\d{3}-\d{4}/.test(v);
         },
-        message: props => `${props.value} is not a valid phone number!`,
+        message: (props) => `${props.value} is not a valid phone number!`,
       },
     },
     subscription: {
       type: String,
-      default: 'free',
-      enum: ['free', 'pro', 'premium'],
+      default: "free",
+      enum: ["free", "pro", "premium"],
     },
     password: {
       type: String,
-      default: 'password',
+      default: "password",
     },
     token: {
       type: String,
-      default: '',
+      default: "",
     },
   },
-  { versionKey: false },
+  {versionKey: false}
 );
 
-const Contact = model('contact', contactSchema);
+const Contact = model("contact", contactSchema);
 
 module.exports = Contact;
