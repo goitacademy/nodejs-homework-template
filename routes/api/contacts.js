79cbca47c5046882d4ceb579115cacc189181527
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-09 01:21:00 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const contactsController = require("../../controller");
-const {updateContact, createContact} = require("../../services/validation");
+const {
+  validUpdateContact,
+  validCreateContact,
+} = require("../../services/validation");
 
 router
   .get("/", contactsController.listContacts)
-  .post("/", createContact, contactsController.addContact);
+  .post("/", validCreateContact, contactsController.addContact);
 
 router
   .get("/:contactId", contactsController.getContactById)
   .delete("/:contactId", contactsController.removeContact)
-  .patch("/:contactId", updateContact, contactsController.updateContact);
+  .patch("/:contactId", validUpdateContact, contactsController.updateContact);
 
 module.exports = router;
