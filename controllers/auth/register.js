7138c2e1084d73cae3888d91c128603fa3d67caa
Larin Artem <larin_art@ukr.net>
2022-11-09 21:19:03 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -3,16 +3,21 @@ const { RequestError } = require("../../helpers");
 const { User } = require("../../models/user");
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, subscription } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw RequestError(409, `User with ${email} already exist`);
   }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  const result = await User.create({ email, password: hashPassword });
+  await User.create({
+    subscription,
+    email,
+    password: hashPassword,
+  });
   res.status(201).json({
     user: {
-      email: result.email,
+      email,
+      subscription,
     },
   });
 };
