8c231bf7ed493495002b5b1b80803c10005314ba
Kostiantyn <cashalot80@ukr.net>
2022-11-09 14:05:17 +0000

    add middleware and JoI

--- middlewares/validationMiddlevare.js
+++ middlewares/validationMiddlevare.js
@@ -1,1 +1,33 @@
+const Joi = require("joi");
 
+module.exports = {
+  addContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      email: Joi.string()
+        .email({ minDomainSegment: 2, tlds: { allow: ["com", "net"] } })
+        .required(),
+      phone: Joi.number().required(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error });
+    }
+    next();
+  },
+  putContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).optional(),
+      email: Joi.string()
+        .email({ minDomainSegment: 2, tlds: { allow: ["com", "net"] } })
+        .optional(),
+      phone: Joi.number().optional(),
+    });
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error });
+    }
+    next();
+  },
+};
