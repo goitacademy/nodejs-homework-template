dff8933c551202a40deb6fac6f1965b08b37b627
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-11-10 23:42:58 +0200

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,18 +1,88 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const { randomUUID } = require('crypto')
 
-const listContacts = async () => {}
+const contactsPath = path.resolve(__dirname, './contacts.json')
 
-const getContactById = async (contactId) => {}
+const read = async () => {
+  const binaryData = await fs.readFile(contactsPath)
+  const dataJSON = binaryData.toString()
+  const dataArray = JSON.parse(dataJSON)
+  return dataArray
+}
+const write = async (data) => {
+  const content = JSON.stringify(data)
+  await fs.writeFile(contactsPath, content, 'utf8')
+}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await read()
+  return contacts
+}
 
-const addContact = async (body) => {}
+const getById = async (contactId) => {
+  try {
+    const contacts = await read()
+    const contact = contacts.find((contact) => contact.id === contactId)
+    return contact
+  } catch (error) {
+    console.log(
+      `Error:${error} occurred when try to get contact with ID: ${contactId}`,
+    )
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await read()
+    const dataArrayUpdated = contacts.filter((el) => el.id !== contactId)
+    if (dataArrayUpdated.length === contacts.length) {
+      return null
+    }
+    await write(dataArrayUpdated)
+    return contactId
+  } catch (error) {
+    console.log(`Error:${error} occurred`)
+  }
+}
+
+const addContact = async (body) => {
+  const { name, email, phone } = body
+
+  try {
+    const dataArray = await read()
+    const generatedID = randomUUID()
+    const contact = { id: generatedID, name, email, phone }
+    dataArray.push(contact)
+    await write(dataArray)
+    return contact
+  } catch (error) {
+    console.log(`Error:${error} occurred when try to add contact`)
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await read()
+    let isUpdated = null
+    const dataArrayUpdated = contacts.map((el) => {
+      if (el.id === contactId) {
+        isUpdated = { ...el, ...body }
+        return isUpdated
+      }
+      return el
+    })
+
+    await write(dataArrayUpdated)
+    return isUpdated
+  } catch (error) {
+    console.log(`Error:${error} occurred when try to add contact`)
+  }
+}
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
