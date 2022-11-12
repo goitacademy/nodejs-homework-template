1c6c329f5c88ac109fd1a1c6763b6b7532b70628
Larin Artem <larin_art@ukr.net>
2022-11-12 23:31:04 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -10,24 +10,13 @@ const register = async (req, res) => {
     throw RequestError(409, `User with ${email} already exist`);
   }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-<<<<<<< HEAD
   const avatarURL = gravatar.url(email);
   await User.create({ email, avatarURL, password: hashPassword, subscription });
-=======
-  await User.create({
-    subscription,
-    email,
-    password: hashPassword,
-  });
->>>>>>> b0aa3a17f283877c301715d183652149061c692f
   res.status(201).json({
     user: {
       email,
       subscription,
-<<<<<<< HEAD
       avatarURL,
-=======
->>>>>>> b0aa3a17f283877c301715d183652149061c692f
     },
   });
 };
