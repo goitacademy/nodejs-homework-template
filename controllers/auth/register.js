891eb1876285936201c8cf72a74cfd8cc40ec980
luv_cr1m3s <luv_cr1m3s@protonmail.com>
2022-11-13 10:07:51 +0100

    hw04

---
+++ controllers/auth/register.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs');
+
+const { User } = require('../../models/user');
+const { RequestError } = require('../../helpers');
+
+const register = async (req, res) => {
+  const { email, password, subscription } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, 'Email in use');
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    subscription,
+  });
+  res.status(201).json({
+    email: result.email,
+    subscription: result.subscription,
+  });
+};
+
+module.exports = register;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,119 +1,22 @@
 const express = require('express');
-const { NotFound, BadRequest } = require('http-errors');
-const Joi = require('joi');
 
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  updateContact,
-  removeContact,
-} = require('../../models/contacts');
-
+const ctrl = require('../../controllers/contacts');
+const { ctrlWrapper } = require('../../helpers');
+const { schemas } = require('../../models/contact');
+const { validateBody, authenticate } = require('../../middlewares');
 const router = express.Router();
 
-const contactSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    .required(),
-  phone: Joi.string().min(5).max(20).required(),
-});
+router.get('/', authenticate, ctrlWrapper(ctrl.getAll));
+router.get('/:id', authenticate, ctrlWrapper(ctrl.getById));
+router.post('/', authenticate, validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
+router.delete('/:id', authenticate, ctrlWrapper(ctrl.removeById));
+router.put('/:id', authenticate, validateBody(schemas.addSchema), ctrlWrapper(ctrl.updateById));
 
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        result: contacts,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await getContactById(id);
-    if (!result) {
-      throw new NotFound(`Contact with id ${id} not found`);
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw new BadRequest('missing required name field');
-    }
-    const result = await addContact(req.body);
-    res.status(201).json({
-      status: 'success',
-      code: 201,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await removeContact(id);
-    if (!result) {
-      throw new NotFound(`Contact with id ${id} not found`);
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      message: 'contact deleted',
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw new BadRequest('missing fields');
-    }
-    const { id } = req.params;
-    const result = await updateContact(id, req.body);
-    if (!result) {
-      throw new NotFound(`Contact with id ${id} not found`);
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  '/:id/favorite',
+  authenticate,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrlWrapper(ctrl.updateFavorite),
+);
 
 module.exports = router;
