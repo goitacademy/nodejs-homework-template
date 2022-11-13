27a86170cd17601914b01679a67832567c094adf
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-13 23:41:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,20 +14,34 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
+app.use("/", (req, res) => {
   res.status(404).json({message: "Not found"});
 });
 
 app.use((err, req, res, next) => {
-  console.error(`app error: ${err.message}`);
-  res
-    .status(500)
+  console.error(`app error: ${err.message}, ${err.name}`);
 
-    .json({
-      status: "fail",
-      code: 500,
-      data: "Internal Server Error",
+  //
+  // так мы делали на занятии
+  if (err.name === "ValidationError" || err.name === "CastError") {
+    return res.status(400).json({
+      message: err.message,
     });
+  }
+
+  if (err.status) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+  //
+  //
+
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    data: "Internal Server Error",
+  });
 });
 
 module.exports = app;
