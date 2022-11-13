7b0031f2e22bb7b3b0644e5450335f5bfd1a5850
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-13 22:37:09 +0200

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,89 +1,66 @@
 const Contacts = require("../services");
 
 const getAllContact = async (_req, res, next) => {
-  try {
-    const contacts = await Contacts.getAll();
-    res.json({
-      status: "success",
-      code: 200,
-      message: "Contacts found",
-      data: {
-        contacts,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
+  const contacts = await Contacts.getAll();
+  res.json({
+    status: "success",
+    code: 200,
+    message: "Contacts found",
+    data: {
+      contacts,
+    },
+  });
 };
 
 const getContactById = async (req, res, next) => {
-  try {
-    const contact = await Contacts.getContactById(req.params.contactId);
+  const contact = await Contacts.getContactById(req.params.contactId);
 
-    res.json({
-      status: "success",
-      code: 200,
-      message: "Contact found",
-      data: {
-        contact,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
+  res.json({
+    status: "success",
+    code: 200,
+    message: "Contact found",
+    data: {
+      contact,
+    },
+  });
 };
 
 const addContact = async (req, res, next) => {
-  try {
-    const contact = await Contacts.addContact(req.body);
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      message: "Contact created",
-      data: {
-        contact,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
+  const contact = await Contacts.addContact(req.body);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    message: "Contact created",
+    data: {
+      contact,
+    },
+  });
 };
 
 const updateContact = async (req, res, next) => {
-  try {
-    const contact = await Contacts.updateContact(
-      req.params.contactId,
-      req.body
-    );
+  const contact = await Contacts.updateContact(req.params.contactId, req.body);
 
-    res.json({
-      status: "success",
-      code: 200,
-      message: "Contact updated",
-      data: {
-        contact,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
+  res.json({
+    status: "success",
+    code: 200,
+    message: "Contact updated",
+    data: {
+      contact,
+    },
+  });
 };
 
 const removeContact = async (req, res, next) => {
-  try {
-    const contact = await Contacts.removeContact(req.params.contactId);
+  const contact = await Contacts.removeContact(req.params.contactId);
 
-    res.json({
-      status: "success",
-      code: 200,
-      message: "Contact deleted",
-      data: {
-        contact,
-      },
-    });
-  } catch (e) {
-    next(e);
-  }
+  res.json({
+    status: "success",
+    code: 204,
+    message: "Contact deleted",
+    data: {
+      contact,
+    },
+  });
 };
 
 module.exports = {
