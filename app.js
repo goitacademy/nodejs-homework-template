bc96126cdfbf7f6233e4fa6611fe738be274391d
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-14 16:14:45 +0200

    1

--- app.js
+++ app.js
@@ -14,36 +14,19 @@ app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/contacts", contactsRouter);
 
-app.use("/", (req, res) => {
+app.use((req, res) => {
   return res.status(404).json({ message: "Not Found" });
 });
 
 app.use((err, req, res, next) => {
-  console.error(`app error: ${err.message}, ${err.name}`);
-
-  if (err.name === "ValidationError") {
-    return res.status(400).json({
-      message: err.message,
-    });
-  }
-
-  if (err.name === "CastError") {
-    return res.status(400).json({
-      message: err.message,
-    });
-  }
-
-  if (err.status) {
-    return res.status(err.status).json({
-      message: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    message: "Internal server error",
-  });
+  const { status = 500, message = "Server Internal error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = {
   app,
 };
+
+
+
+
