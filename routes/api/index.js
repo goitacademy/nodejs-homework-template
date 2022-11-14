7b8ed53b5c5cf1b19de30153d8160b0564002c68
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-14 17:40:11 +0200

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controller");
 const {wrapper} = require("../../helpers/tryCatch");
-const {validCreate, validUpdate} = require("../../middleWare/validation");
+// const {validCreate, validUpdate} = require("../../middleWare/validation");
 
 router.get("/", wrapper(controller.getAllContact));
-router.post("/", validCreate, wrapper(controller.addContact));
+router.post("/", wrapper(controller.addContact));
 router.get("/:contactId", wrapper(controller.getContactById));
 router.delete("/:contactId", wrapper(controller.removeContact));
-router.patch("/:contactId", validUpdate, wrapper(controller.updateContact));
+router.patch("/:contactId", wrapper(controller.updateContact));
 
 module.exports = router;
