179bef6b169932914009c05448c8039358306b69
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-14 17:46:27 +0200

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controller");
 const {wrapper} = require("../../helpers/tryCatch");
-// const {validCreate, validUpdate} = require("../../middleWare/validation");
 
 router.get("/", wrapper(controller.getAllContact));
 router.post("/", wrapper(controller.addContact));
