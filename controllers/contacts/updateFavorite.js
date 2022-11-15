ad73c8b90c145605c2152cc53470b1687b48cdb2
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-11-15 20:58:48 +0100

    1

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -5,9 +5,6 @@ const { Contact } = require("../../model");
 const updateFavorite = async (req, res) => {
       const { id } = req.params; 
       const { favorite } = req.body;
-      if(!req.body) {
-        throw new BadRequest("missing field favorite");
-      }
       const result = await Contact.findByIdAndUpdate(id, {favorite}, {new: true});
       if(!result) {
         throw new NotFound("Not found");
