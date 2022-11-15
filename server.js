052ae6465b28c11294f5674e74bbbe98b9a1e524
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-15 20:58:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const {HOST_DB, PORT} = require("./config");
+const db = mongoose.connect(HOST_DB);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch((err) => {
+  console.log(`Server not running. Error message: ${err.message}`);
+  process.exit(1);
+});
+
+// const start = async () => {
+//   try {
+//     mongoose.connect(HOST_DB);
+//     app.listen(PORT, () => {
+//       console.log(`Server running. Use our API on port: ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Failed to start server with error: ", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// start();
