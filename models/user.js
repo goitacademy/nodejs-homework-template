5f00a42c2c826680e52f4c1b269884f201f2edf0
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-15 20:58:00 +0200

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const usersSchema = new mongoose.Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const User = mongoose.model("user", usersSchema);
+
+module.exports = {User};
