eb2f5c66f8b2ad0b591ab889226493e1677ecd7a
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-15 13:08:51 +0200

    Update validBody.js

--- midlieware/validBody.js
+++ midlieware/validBody.js
@@ -1,8 +1,6 @@
-// const {ValidationError} = require("../helpers/errors");
 const validatorBody = (schema) => {
   return (req, res, next) => {
     const result = schema.validate(req.body);
-    console.log("ValidBodyresult:", result.error);
     if (result.error) {
       result.error = 400;
       next(result.error);
