e3a51f82af5570dc04286b6e9c7e8bebd274267f
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-15 12:25:04 +0200

    edits

--- controlers/contactsControlers.js
+++ controlers/contactsControlers.js
@@ -3,55 +3,72 @@ const { Contact } = require("../Validations/contactShema");
 const { createNotFoundHttpError } = require("../helpers/index");
 
 async function getAll(req, res, next) {
-  const { _id: owner } = req.user;
-  const { limit, page } = req.query;
-  console.log(":", limit, page);
+    const { _id: owner } = req.user;
+    const { limit, page } = req.query;
+    console.log(":", limit, page);
 
-  const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner }).skip(skip).limit(limit);
-
-  return res.json({
-    data: contacts,
-  });
+    const skip = (page - 1) * limit;
+    const contacts = await Contact.find({ owner }).skip(skip).limit(limit);
+    if (contacts) {
+        return res.json({
+            data: contacts,
+        });
+    }
+    return next(createNotFoundHttpError());
 }
 
 async function create(req, res, next) {
-  const { _id: owner } = req.user;
-  const contact = req.body;
-  const createdContact = await Contact.create({...contact,owner});
-  return res.status(201).json({
-    data: {
-      contacts: createdContact,
-    },
-  });
+    const { _id: owner } = req.user;
+    const contact = req.body;
+    const createdContact = await Contact.create({...contact,owner});
+    if (createdContact) {
+        return res.status(201).json({
+        data: {
+        contacts: createdContact,
+        },
+    });
+    }
 }
 
 async function deleteById(req, res, next) {
-  const { id } = req.params;
-  const contact = await Contact.findById(id);
-  if (contact) {
-    await Contact.findByIdAndDelete(id);
-    return res.json({ data: { contact } });
-  }
-  return next(createNotFoundHttpError());
+    const { id } = req.params;
+    const contact = await Contact.findById(id);
+    if (contact) {
+        await Contact.findByIdAndDelete(id);
+        return res.json({ data: { contact } });
+    }
+    return next(createNotFoundHttpError());
 }
 
-async function updateById(req, res, next) {
-  const { id } = req.params;
-  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
-
-  return res.json({ data: { contact: updatedContact } });
-}
 
 async function findOneById(req, res, next) {
-  const { id } = req.params;
-
-  const contact = await Contact.findById(id);
-  if (contact) {
+    const { id } = req.params;
+    const contact = await Contact.findById(id);
+    if (!contact) {
+        return next(createNotFoundHttpError());
+    }
     return res.json({ data: { contact } });
-  }
-  return next(createNotFoundHttpError());
 }
+
+
+async function updateById(req, res, next) {
+    const { id } = req.params;
+    const updateContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updateContact) {
+        throw new NotFound(`Contact with id=${id} not found`);
+    }
+    return res.status(201).json({contact: updateContact});
+}
+
+async function updateStatusContact(req, res, next) {
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+    const result = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
+    if (!result) {
+        throw new NotFound(`Not found`);
+    }
+    return res.status(200).json({ status: "success", code: 200, data: { result }, });
+};
 
 module.exports = {
   getAll,
@@ -59,4 +76,5 @@ module.exports = {
   deleteById,
   updateById,
   findOneById,
+  updateStatusContact
 };
