e592038a5ae4a0d5ffe637fdd4c443b744f49895
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-11-15 19:03:55 +0100

    end

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -1,12 +1,16 @@
 const Joi = require('joi');
+const { NotFound, BadRequest } = require('http-errors');
 const { Contact } = require("../../model");
 
 const updateFavorite = async (req, res) => {
       const { id } = req.params; 
       const { favorite } = req.body;
+      if(!req.body) {
+        throw new BadRequest("missing field favorite");
+      }
       const result = await Contact.findByIdAndUpdate(id, {favorite}, {new: true});
       if(!result) {
-        throw new NotFound("missing field favorite");
+        throw new NotFound("Not found");
       }
       res.json({
         status: "success",
