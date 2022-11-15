c84b0052218b6ef22c6b541d9574cb0255eda088
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-15 20:57:30 +0200

    Create error.js

---
+++ helpers/error.js
@@ -0,0 +1,16 @@
+const customError = ({status, message}) => {
+  const error = new Error(message);
+  error.status = status;
+
+  return error;
+};
+
+module.exports = {
+  customError,
+};
+
+// function NotFoundHttpError() {
+//   const err = new Error("Not Found");
+//   err.status = 404;
+//   return err;
+// }
