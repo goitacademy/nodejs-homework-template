3cdc537e8cdce04fc80da815b7d64c661b484146
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-15 23:45:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const {HOST_DB, PORT} = require("./config");
-const db = mongoose.connect(HOST_DB);
+// const db = mongoose.connect(HOST_DB);
 
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running!!! Use our API on port: ${PORT}`);
-  });
-}).catch((err) => {
-  console.log(`Server not running. Error message: ${err.message}`);
-  process.exit(1);
-});
+// db.then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server running!!! Use our API on port: ${PORT}`);
+//   });
+// }).catch((err) => {
+//   console.log(`Server not running. Error message: ${err.message}`);
+//   process.exit(1);
+// });
 
-// const start = async () => {
-//   try {
-//     mongoose.connect(HOST_DB);
-//     app.listen(PORT, () => {
-//       console.log(`Server running. Use our API on port: ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to start server with error: ", error.message);
-//     process.exit(1);
-//   }
-// };
+const start = async () => {
+  try {
+    mongoose.connect(HOST_DB);
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server with error: ", error.message);
+    process.exit(1);
+  }
+};
 
-// start();
+start();
