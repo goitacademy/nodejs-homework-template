5709238353657249a4c3acef81919309df66880b
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-16 15:14:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 require("./config/passport");
 
-// console.log("authRouter", authRouter);
 app.use("/api/users", authRouter);
 app.use("/api/user", authRouter);
 app.use("/", (req, res) => {
@@ -29,32 +28,3 @@ app.use((err, _, res, __) => {
 });
 
 module.exports = app;
-
-
-// const app = require("./app");
-// const mongoose = require("mongoose");
-// const {HOST_DB, PORT} = require("./config");
-// const db = mongoose.connect(HOST_DB);
-
-// db.then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server running!!! Use our API on port: ${PORT}`);
-//   });
-// }).catch((err) => {
-//   console.log(`Server not running. Error message: ${err.message}`);
-//   process.exit(1);
-// });
-
-// const start = async () => {
-//   try {
-//     mongoose.connect(HOST_DB);
-//     app.listen(PORT, () => {
-//       console.log(`Server running. Use our API on port: ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to start server with error: ", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// start();
