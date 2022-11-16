039963ece3bce93170cf20875213feae40d02dc3
Dmytro Chernovskyi <dimcher911@gmail.com>
2022-11-16 16:26:56 +0200

    changing port

--- bin/server.js
+++ bin/server.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = require('../app')
+const app = require('../app');
 
-const PORT = process.env.PORT || 3000
-const DB_HOST = process.env.DB_HOST
+const PORT = process.env.PORT1 || 3000;
+const DB_HOST = process.env.DB_HOST;
 
 const connection = mongoose.connect(DB_HOST, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 connection
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Database connection successful. Server running. Use our API on port: ${PORT}`)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Database connection successful. Server running. Use our API on port: ${PORT}`);
+        });
     })
-  })
-  .catch(err => {
-    console.log(`Server not running. Error message: ${err.message}`)
-    process.exit(1)
-  })
+    .catch((err) => {
+        console.log(`Server not running. Error message: ${err.message}`);
+        process.exit(1);
+    });
