7718a373d77602745c4eae724d04c46a134d7a9c
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-16 11:42:55 +0200

    1

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const mongoose = require("mongoose");
-
 const dotenv = require("dotenv");
 dotenv.config();
-
 const { app } = require("./app");
-
 const { HOST_DB, PORT = 3000 } = process.env;
 
-async function main() {
+
+(function () {
   try {
-    if (!HOST_DB) {
-      throw new Error("HOST_DB not set!");
-    }
-
-    await mongoose.connect(HOST_DB);
-    console.log("connected to mongodb");
-
-    app.listen(PORT, (err) => {
-      if (err) throw err;
-      console.log(`server is listening on port: ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error:", error.message);
-    process.exit(1);
+    mongoose.connect(HOST_DB);
+    console.log("Database connection successful")
+    app.listen(PORT, () => {
+      console.log(PORT)
+  })
+  } catch (err) {
+    console.log({ "Error": err.message });
+    process.exit(1)
   }
-}
-main();
+})();
+
+
