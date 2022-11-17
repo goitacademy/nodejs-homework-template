b829ccd6147e572508d33f5e34c0712679eff3c2
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-11-17 16:26:26 +0200

    hw3vm

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const app = express();
 const contactsRouter = require("./routes/api/contacts");
 
-const app = express();
-
+app.use((err, req, res, next) => {
+  res.status("500").json({ message: err.message });
+});
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,67 +1,36 @@
 const express = require("express");
-const Joi = require("joi");
 
 const router = express.Router();
+// const modelMiddleware = require("/nodejs-homework-template-vm/middlewears/models");
 const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("/nodejs-homework-template-vm/models/contacts");
-router.get("/", async (req, res, next) => {
-  const contacts = await listContacts();
-  res.status(200).json({ message: "success", code: 200, contacts });
-});
+  addContactValidation,
+  putContactVlidation,
+  patchContactVlidation,
+} = require("/nodejs-homework-template-vm/middlewears/validationMiddleware");
+const {
+  getContacts,
+  addContacts,
+  getContactsById,
+  putContacts,
+  removeContacts,
+  putContactsFav,
+} = require("/nodejs-homework-template-vm/contrillers/contactController");
+const {
+  asyncWrapper,
+} = require("/nodejs-homework-template-vm/helpers/apiHelpers");
 
-router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contactById = await getContactById(contactId);
-  if (!contactById) {
-    res.status(400).json({ message: "not found", code: 400 });
-  }
-  res.status(200).json({ message: "success", code: 200, contactById });
-});
+router.get("/", asyncWrapper(getContacts));
 
-router.post("/", async (req, res, next) => {
-  const scema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-      .required(),
-    phone: Joi.string().min(7).max(20).required(),
-  });
-  const validateContact = scema.validate(req.body);
-  if (validateContact.error) {
-    return res.status(400).json({ message: validateContact.error, code: 400 });
-  }
-  const { name, email, phone } = req.body;
-  const contact = await addContact(name, email, phone);
-  res.status(201).json({ message: "contact added", contact });
-});
+router.get("/:contactId", asyncWrapper(getContactsById));
 
-router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contacts = await removeContact(contactId);
-  res.status(201).json({ message: "contact deleted", contacts });
-});
+router.post("/", addContactValidation, asyncWrapper(addContacts));
 
-router.put("/:contactId", async (req, res, next) => {
-  const scema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-      .required(),
-    phone: Joi.string().min(7).max(20).required(),
-  });
-  const validateContact = scema.validate(req.body);
-  if (validateContact.error) {
-    return res.status(400).json({ message: validateContact.error, code: 400 });
-  }
-  const { contactId } = req.params;
-  const { name, email, phone } = req.body;
-  const contactByUpdate = await updateContact(contactId, name, email, phone);
-  res.status(201).json({ message: "contact deleted", contactByUpdate });
-});
+router.delete("/:contactId", asyncWrapper(removeContacts));
 
+router.put("/:contactId", putContactVlidation, asyncWrapper(putContacts));
+router.patch(
+  "/:contactId/favorite",
+  patchContactVlidation,
+  asyncWrapper(putContactsFav)
+);
 module.exports = router;
