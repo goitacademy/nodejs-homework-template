e377b8391bddaed6617c12bc4c626b74aa2d3fd9
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-18 17:49:44 +0200

    Update auth.js

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -1,44 +1,19 @@
-const jwt = require("jsonwebtoken");
-const {customError} = require("../helpers/error");
-const {User} = require("../models/user");
-const {SECRET} = require("../config");
+const passport = require("passport");
+require("../config/passport");
 
-const authMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization || " ";
-  const [tokenType, token] = authHeader.split(" ");
-
-  if (tokenType === "Bearer" && token) {
-    try {
-      const verifiedToken = jwt.verify(token, SECRET);
-
-      const user = await User.findById(verifiedToken.id);
-      console.log("user", user);
-      if (!user) {
-        next(customError({status: 400, message: "No user with such id"}));
-      }
-
-      if (!user.token) {
-        next(customError({status: 400, message: "Token is invalid"}));
-      }
-
-      req.token = token;
-      req.user = user;
-      console.log("token", token);
-      return next();
-    } catch (error) {
-      console.log("error", error);
-      if (error.name === "TokenExpiredError") {
-        next(customError(error.name));
-      }
-      if (error.name === "JsonWebTokenError") {
-        next(customError(error.name));
-      }
-
-      throw error;
+const authMiddleware = (req, res, next) => {
+  passport.authenticate("jwt", {session: false}, (err, user) => {
+    const token = req.get("Authorization")?.split(" ")[1];
+    console.log("token", token);
+    if (!user || err || token !== user.token) {
+      return next({
+        status: 401,
+        message: "Not authorized",
+      });
     }
-  }
-
-  return next(customError({status: 401, message: "Not authorized"}));
+    req.user = user;
+    return next();
+  })(req, res, next);
 };
 
 module.exports = {authMiddleware};
