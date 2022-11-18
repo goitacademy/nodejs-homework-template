2e99a9df472a6a1090ad5218576d17a73d429ed6
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-18 19:05:50 +0200

    Update auth.js

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -2,9 +2,12 @@ const passport = require("passport");
 require("../config/passport");
 
 const authMiddleware = (req, res, next) => {
+  // console.log("req", req.user);
   passport.authenticate("jwt", {session: false}, (err, user) => {
+    console.log("user::", user);
+    console.log("err", err);
     const token = req.get("Authorization")?.split(" ")[1];
-    console.log("token", token);
+    console.log("token:", token);
     if (!user || err || token !== user.token) {
       return next({
         status: 401,
