2e458a65515669e23ac395d6128d2365d2442b5e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 14:19:22 +0200

    Update auth.js

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -2,29 +2,30 @@ const {Unauthorized} = require("http-errors");
 const jwt = require("jsonwebtoken");
 const {User} = require("../models/user");
 const {SECRET} = require("../config");
-require("dotenv").config();
+// require("dotenv").config();
 
 // const SECRET = process.env.SECRET;
 
 const check = async (req, res, next) => {
+  console.log("req", req);
   try {
     const {authorization = 0} = req.headers;
 
     const [tokenType, token] = authorization.split(" ");
 
     if (tokenType !== "Bearer" || token === "") {
-      next(new Unauthorized("Not authorized"));
+      next(new Unauthorized("Not authorized!!!"));
     }
 
     const {id} = jwt.verify(token, SECRET);
     const user = await User.findById(id);
     if (!user) {
-      next(new Unauthorized("Not authorized"));
+      next(new Unauthorized("Not authorized!!!!"));
     }
     req.user = user;
     next();
   } catch (error) {
-    next(new Unauthorized("Not authorized"));
+    next(new Unauthorized("Not authorized!!!!!"));
   }
 };
 
