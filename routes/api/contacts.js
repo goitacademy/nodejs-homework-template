f55976a1df9c76dcca54470cb6fe78a67ab1b4b3
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-11-19 19:35:57 +0200

    Start (main) - 3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,34 @@
 const express = require('express')
-
 const router = express.Router()
 
+const contactsOperations = require("../../models/contacts")
+
+
+//------------------------------------------------------------
 router.get('/', async (req, res, next) => {
+  const contacts = await contactsOperations.listContacts
   res.json({ message: 'template message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
+  const contact = await contactsOperations.getContactById
   res.json({ message: 'template message' })
 })
 
 router.post('/', async (req, res, next) => {
+  const contact = await contactsOperations.addContact
   res.json({ message: 'template message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
+  const contact = await contactsOperations.removeContact
   res.json({ message: 'template message' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
+  const contact = await contactsOperations.updateContact
   res.json({ message: 'template message' })
 })
+
 
 module.exports = router
