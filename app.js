419331a67cce127926243b4a4a372f8a47c32aef
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 17:52:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// const auth = require("./middleware/auth");
-// const wrapper = require("./helpers/tryCatch");
 const contactsRouter = require("./routes/api");
 const authRouter = require("./routes/api/authRouter");
 
