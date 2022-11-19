ad70a6905cd3a2dbf22f4f865119c407988b0d79
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 17:52:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = mongoose.connect(HOST_DB);
 
 db.then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running!!! Use our API on port: ${PORT}`);
+    console.log(`Server running Use our API on port: ${PORT}`);
   });
 }).catch((err) => {
   console.log(`Server not running. Error message: ${err.message}`);
