d7ab77afd716fc70f02ef2aff41ce4990d79cee7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 09:02:21 +0200

    Update userValidation.js

--- schema/userValidation.js
+++ schema/userValidation.js
@@ -1,17 +1,17 @@
 const Joi = require("joi");
 
-const schemaPostUser = Joi.object({
+const schemaPost = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(5).required(),
   subscription: Joi.string().valid("starter", "pro", "business"),
 });
 
-const schemaPatchUser = Joi.object({
+const schemaPatch = Joi.object({
   subscription: Joi.string().valid("starter", "pro", "business").required(),
   _id: Joi.string(),
 });
 
 module.exports = {
-  schemaPostUser,
-  schemaPatchUser,
+  schemaPost,
+  schemaPatch,
 };
