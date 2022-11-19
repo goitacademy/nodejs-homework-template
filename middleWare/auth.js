2e21673ce4dbd3d859798944152fcadb17bd2b86
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 11:53:18 +0200

    Update auth.js

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -1,51 +1,63 @@
-const jwt = require("jsonwebtoken");
-
 const {Unauthorized} = require("http-errors");
-const User = require("../models/user");
-// const secret = process.env.SECRET;
+const jwt = require("jsonwebtoken");
+const {User} = require("../models/user");
+const {SECRET} = require("./config");
+require("dotenv").config();
 
-const authen = async (req, res, next) => {
-  const secret = process.env.SECRET;
+// const SECRET = process.env.SECRET;
 
-  const {authorization = 0} = req.headers;
-  if (!authorization) return next(new Unauthorized("Not authorized"));
-
-  const [typeToken, token] = authorization.split(" ");
-  if (typeToken !== "Bearer") return next(new Unauthorized(`!== "Bearer"`));
-
+const check = async (req, res, next) => {
   try {
-    const isAutorizate = jwt.verify(token, secret);
-    const user = await User.findOne({_id: isAutorizate._id});
-    if (!user || user.token !== token)
-      return next(new Unauthorized("Not authorized"));
-    req.userId = isAutorizate._id;
-    return next();
+    const {authorization = 0} = req.headers;
+
+    const [tokenType, token] = authorization.split(" ");
+
+    if (tokenType !== "Bearer" || token === "") {
+      next(new Unauthorized("Not authorized"));
+    }
+
+    const {id} = jwt.verify(token, SECRET);
+    const user = await User.findById(id);
+    if (!user) {
+      next(new Unauthorized("Not authorized"));
+    }
+    req.user = user;
+    next();
   } catch (error) {
-    if (error.name) return next(new Unauthorized(error.name));
-    return next(error);
+    next(new Unauthorized("Not authorized"));
   }
 };
-module.exports = authen;
 
-// const passport = require("passport");
-// require("../config/passport");
+module.exports = {
+  check,
+};
 
-// const authen = (req, res, next) => {
-//   // console.log("req", req.user);
-//   passport.authenticate("jwt", {session: false}, (err, user) => {
-//     console.log("user::", user);
-//     console.log("err", err);
-//     const token = req.get("Authorization")?.split(" ")[1];
-//     console.log("token:", token);
-//     if (!user || err || token !== user.token) {
-//       return next({
-//         status: 401,
-//         message: "Not authorized",
-//       });
-//     }
-//     req.user = user;
+// const jwt = require("jsonwebtoken");
+// const {SECRET} = require("./config");
+// const {Unauthorized} = require("http-errors");
+// const User = require("../models/user");
+// // const secret = process.env.SECRET;
+
+// const check = async (req, res, next) => {
+//   const secret = SECRET;
+
+//   const {authorization = 0} = req.headers;
+//   if (!authorization) return next(new Unauthorized("Not authorized!"));
+
+//   const [typeToken, token] = authorization.split(" ");
+//   if (typeToken !== "Bearer" || token === "")
+//     return next(new Unauthorized("Not authorized!!"));
+
+//   try {
+//     const isAutorizate = jwt.verify(token, secret);
+//     const user = await User.findOne({_id: isAutorizate._id});
+//     if (!user || user.token !== token)
+//       return next(new Unauthorized("Not authorized!!!"));
+//     req.userId = isAutorizate._id;
 //     return next();
-//   })(req, res, next);
+//   } catch (error) {
+//     if (error.name) return next(new Unauthorized(error.name));
+//     return next(error);
+//   }
 // };
-
-// module.exports = {authen};
+// module.exports = check;
