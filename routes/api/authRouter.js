cc99d0b77a10463ffe09ef8c2985437ee4acbc8e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 11:53:21 +0200

    Update authRouter.js

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -10,20 +10,20 @@ const {
 
 const {schemaPost, schemaPatch} = require("../../schema/userValidation");
 const {validatorBody} = require("../../middleware/validBody");
-const authen = require("../../middleware/auth");
+const {check} = require("../../middleWare/auth");
 const {wrapper} = require("../../helpers/tryCatch");
 
+router.get("/current", check, wrapper(currentUserCtrl));
+router.patch("/", check, subscriptCtrl);
+// router.patch("/subscription", check, subscriptCtrl);
 router.post("/singup", validatorBody(schemaPost), wrapper(singUpCtrl));
-
+router.get("/login", validatorBody(schemaPost), wrapper(singInCtrl));
 router.post("/login", validatorBody(schemaPost), wrapper(singInCtrl));
-
-router.get("/logout", authen, wrapper(singOutCtrl));
-
-router.get("/current", authen, wrapper(currentUserCtrl));
-
+router.get("/logout", check, wrapper(singOutCtrl));
+router.post("/logout", check, wrapper(singOutCtrl));
 router.patch(
   "/subscription",
-  authen,
+  check,
   validatorBody(schemaPatch),
   wrapper(subscriptCtrl)
 );
