3d27efc9e74baa790529187a94f81a22168b0547
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 17:52:27 +0200

    Update authRouter.js

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -7,23 +7,23 @@ const {
   subscriptCtrl,
 } = require("../../controller/auth");
 
-const {check} = require("../../middleWare/auth");
+const {checkToken } = require("../../middleWare/auth");
 const {wrapper} = require("../../helpers/tryCatch");
 const {schemaPost, schemaPatch} = require("../../schema/userValidation");
 const {validatorBody} = require("../../middleware/validBody");
 const router = express.Router();
 
-router.get("/current", check, wrapper(currentUserCtrl));
-router.patch("/", check, subscriptCtrl);
+router.get("/current", checkToken , wrapper(currentUserCtrl));
+router.patch("/", checkToken , subscriptCtrl);
 // router.patch("/subscription", check, subscriptCtrl);
 router.post("/singup", validatorBody(schemaPost), wrapper(singUpCtrl));
 router.get("/login", validatorBody(schemaPost), wrapper(singInCtrl));
 router.post("/login", validatorBody(schemaPost), wrapper(singInCtrl));
-router.get("/logout", check, wrapper(singOutCtrl));
-router.post("/logout", check, wrapper(singOutCtrl));
+router.get("/logout", checkToken , wrapper(singOutCtrl));
+router.post("/logout", checkToken , wrapper(singOutCtrl));
 router.patch(
   "/subscription",
-  check,
+  checkToken ,
   validatorBody(schemaPatch),
   wrapper(subscriptCtrl)
 );
