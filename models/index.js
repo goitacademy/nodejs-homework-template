09301765e8064c34f29353ec3c7b787545361df7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-11-19 15:40:28 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,27 +1,33 @@
 const mongoose = require("mongoose");
 const {Schema, model} = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
-    unique: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+      unique: true,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "user",
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-  owner: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "user",
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 const ContactMod = model("contact", contactSchema);
 
