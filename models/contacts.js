eb337c9325b1e4bb7cf1b7d28dd7cbda0c78cc60
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-11-22 12:36:23 +0200

    add validation middlewares

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,5 @@
-const Joi = require("joi");
 const service = require("../service/index");
+const getValidation = require("../middlewares/validationMiddlewares");
 
 const listContacts = async (res) => {
   try {
@@ -30,101 +30,51 @@ const removeContact = async (contactId, res) => {
 };
 
 const addContact = async (req, res) => {
-  const { name, email, phone, favorite } = req.body;
-
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(20),
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    }),
-    phone: Joi.string()
-      .length(10)
-      .pattern(/^[0-9]+$/),
-    favorite: Joi.boolean(),
-  });
-  const bodyIsValid = schema.validate({
-    name: name,
-    email: email,
-    phone: phone,
-    favorite: favorite,
-  });
+  const bodyIsValid = getValidation.addContactValid(req.body);
 
   if (bodyIsValid.error) {
     res.status(400).json({ message: bodyIsValid.error.message });
     return;
   }
 
-  if (name && email && phone) {
-    try {
-      const results = await service.createContact(req.body);
-      res.status(201).json(results);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
+  try {
+    const results = await service.createContact(req.body);
+    res.status(201).json(results);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-  res.status(400).json({ message: "missing field body" });
 };
 
 const updateContact = async (contactId, body, res) => {
-  const { name, email, phone } = body;
-
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(20),
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    }),
-    phone: Joi.string()
-      .length(10)
-      .pattern(/^[0-9]+$/),
-  });
-  const bodyIsValid = schema.validate({
-    name: name,
-    email: email,
-    phone: phone,
-  });
+  const bodyIsValid = getValidation.updateContactValid(body);
 
   if (bodyIsValid.error) {
     res.status(400).json({ message: bodyIsValid.error.message });
     return;
   }
 
-  if (name || email || phone) {
-    try {
-      const results = await service.updateContact(contactId, body);
-      res.status(200).json(results);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
+  try {
+    const results = await service.updateContact(contactId, body);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-  res.status(400).json({ message: "missing field body" });
 };
 
 const updateStatusContact = async (contactId, body, res) => {
-  const { favorite } = body;
-
-  const schema = Joi.object({
-    favorite: Joi.boolean(),
-  });
-  const bodyIsValid = schema.validate({
-    favorite: favorite,
-  });
+  const bodyIsValid = getValidation.updateStatusValid(body);
 
   if (bodyIsValid.error) {
     res.status(400).json({ message: bodyIsValid.error.message });
     return;
   }
 
-  if (favorite || favorite === false) {
-    try {
-      const results = await service.updateStatusContact(contactId, body);
-      res.status(200).json(results);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
+  try {
+    const results = await service.updateStatusContact(contactId, body);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-  res.status(400).json({ message: "missing field favorite" });
 };
 
 module.exports = {
