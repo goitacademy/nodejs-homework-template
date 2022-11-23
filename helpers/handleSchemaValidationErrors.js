9d44685461bad3dc53559a041e4c53449a82b010
Leevorum <di4111ua@gmail.com>
2022-11-23 13:30:24 +0200

    add update middleware error handler

--- helpers/handleSchemaValidationErrors.js
+++ helpers/handleSchemaValidationErrors.js
@@ -1,5 +1,4 @@
 const handleSchemaValidationErrors = (error, data, next) => {
-  console.log(error.name);
   if (error.name === "MongoServerError" && error.code === 11000) {
     error.status = 409;
   } else {
