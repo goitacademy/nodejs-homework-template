5835f0da154ce2aa197023d1895bf2ab77f4ed97
Oleksandr Tolochko <0504Fusion@gmail.com>
2022-11-29 00:48:57 +0200

    some changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -9,7 +9,7 @@ const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
--- models/contacts.js
+++ models/contacts.js
@@ -2,32 +2,21 @@ const fs = require("fs/promises");
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
-const contactsPath = path.join(__dirname, "./contacts.json");
-
+const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  try {
         const data = await fs.readFile(contactsPath);
         const contacts = JSON.parse(data);
         return contacts;
-    } catch (error) {
-        // error => console.log(error.message) 
-  }
 }
 
 const getContactById = async (contactId) => {
-      try {
         const contacts = await listContacts();
         const contact = contacts.find(contact => contact.id === contactId.toString());
         return contact;
-    } catch (error) {
-        // error => console.log(error.message) 
-    } 
-    
 }
 
 const removeContact = async (contactId) => {
-  try {
         const contacts = await listContacts();
         const contactIndex = contacts.findIndex(contact => contact.id === contactId.toString())
         if (contactIndex === -1) {
@@ -37,13 +26,9 @@ const removeContact = async (contactId) => {
         const newContactList = contacts.filter(contact => contact.id !== contactId.toString());
         await fs.writeFile(contactsPath, JSON.stringify(newContactList));
         return contacts[contactIndex];
-    } catch (error) {
-        // error => console.log(error.message) 
-  }
 }
 
 const addContact = async (body) => {
-  try {
     const { name, email, phone } = body;    
             const contactList = await listContacts();
             const newContact = {
@@ -55,15 +40,10 @@ const addContact = async (body) => {
             contactList.push(newContact);
             await fs.writeFile(contactsPath, JSON.stringify(contactList));
             return newContact;
-
-    } catch (error) {
-        // error => console.log(error.message) 
-  }
 }
 
 const updateContact = async (contactId, body) => {
-  try {
-      const { name, email, phone } = body;    
+      // const { name, email, phone } = body;    
      
         const contacts = await listContacts();
         const contactIndex = contacts.findIndex(contact => contact.id === contactId.toString())
@@ -74,9 +54,6 @@ const updateContact = async (contactId, body) => {
         const newContactList = contacts.filter(contact => contact.id !== contactId.toString());
         await fs.writeFile(contactsPath, JSON.stringify(newContactList));
         return contacts[contactIndex];
-    } catch (error) {
-        // error => console.log(error.message) 
-  }
 }
 
 module.exports = {
