099c36902151df4d2ba18d5bb0afbca471d4fb1e
meretska-sofiia <meretska.sofiia@gmail.com>
2022-11-30 00:52:50 +0200

    HW 03

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,130 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const dotenv = require("dotenv");
+dotenv.config();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const Contact = require("../../models/contact");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  contactValidation,
+  favoriteValidation,
+} = require("../../middlewares/validation");
+const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res) => {
+  const contacts = await Contact.find();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      contacts,
+    },
+  });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await Contact.findById(contactId);
+  if (contact) {
+    return res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contact,
+      },
+    });
+  }
+  return res.json({
+    status: "Not found",
+    code: 404,
+  });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", contactValidation, async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const contact = await Contact.create(req.body);
+  if (!name || !email || !phone) {
+    return res.json({
+      status: `missing required field`,
+      code: 400,
+    });
+  }
+  if (!contact) {
+    return res.json({
+      status: `Contact ${name} is already in contact list`,
+      code: 400,
+    });
+  }
+  return res.json({
+    status: "success",
+    code: 201,
+    data: { contact },
+  });
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contacts = await Contact.findByIdAndRemove(contactId);
+  if (!contacts) {
+    return res.json({
+      message: "Not found",
+      code: 404,
+    });
+  }
+  return res.json({
+    message: "Contact deleted",
+    code: 200,
+  });
+});
+
+router.put("/:contactId", contactValidation, async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    return res.json({
+      status: `missing field`,
+      code: 400,
+    });
+  }
+  const contact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!contact) {
+    return res.json({
+      message: "Not found",
+      code: 404,
+    });
+  }
+  return res.json({
+    status: "success",
+    code: 200,
+    data: { contact },
+  });
+});
+router.patch(
+  "/:contactId/favorite",
+  favoriteValidation,
+  async (req, res, next) => {
+    const { contactId } = req.params;
+
+    const contact = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+
+    if (!req.body) {
+      return res.json({
+        message: "missing field favorite",
+        code: 400,
+      });
+    }
+    return res.json({
+      status: "success",
+      code: 200,
+      data: { contact },
+    });
+  }
+);
+
+module.exports = router;
