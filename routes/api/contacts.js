eaa85079d1d6514c81d857cb1cf7423db76069fa
Ivan Shvayka <shvaykaofficial@gmail.com>
2022-12-01 23:35:57 +0200

    add rest api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,85 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
+const contacts = require("../../models/contacts");
+const { createError } = require("../../helpers/index");
+const router = express.Router();
 
-const router = express.Router()
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getById(id);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", async (req, res, next) => {
 
-module.exports = router
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json({
+      message: "contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const body = req.body;
+    const { id } = req.params;
+
+    const { error } = contactAddSchema.validate(body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+
+    const result = await contacts.updateContact(id, body);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
