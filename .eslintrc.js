da25eb752c6f3ed33f52654d5f6b2001de3ac375
Oleksandr Tolochko <0504Fusion@gmail.com>
2022-12-01 15:13:29 +0200

    lit fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
         "es2021": true,
         "node": true
     },
-    "extends": [
-        "google"
-    ],
+    "extends": ['standard', 'prettier'],
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaVersion": 12,
     },
     "rules": {
     }
-};
+}
