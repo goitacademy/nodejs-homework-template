b75db79618513f4ec45a2986036af3e4c8cef522
Peter <peteryurich@gmail.com>
2022-12-02 11:25:40 +0200

    test-com

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,11 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'home-get template message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'contactID-get template message' })
 })
 
 router.post('/', async (req, res, next) => {
