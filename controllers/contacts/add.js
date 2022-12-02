1ca779c73aa70cee187d332b8472dfeeb24a47e5
Abdeev Vladimir <107181781+abdeev@users.noreply.github.com>
2022-12-02 11:25:25 +0200

    qaa;sj;sd

---
+++ controllers/contacts/add.js
@@ -0,0 +1,11 @@
+// const Book = require("../../models/books");
+
+// async function add(req, res) {
+//   const { title, author, favorite, genre, isbn } = req.body;
+
+//   const result = await Book.create({ title, author, favorite, genre, isbn });
+
+//   res.status(201).json(result);
+// }
+
+// module.exports = add;
---
+++ controllers/contacts/index.js
@@ -0,0 +1,15 @@
+const add = require("./add");
+const getAll = require("./getAll");
+const getById = require("./getById");
+const removeById = require("./removeById");
+const updateById = require("./updateById");
+const updateFavoriteById = require("./updateFavoriteById");
+
+module.exports = {
+  add,
+  getAll,
+  getById,
+  removeById,
+  updateById,
+  updateFavoriteById,
+};
