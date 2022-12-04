984533f8dd2e0c078954a7dd865d7778adba81fe
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-12-04 19:59:07 +0200

    add Rest API
    add methods for working with local DB
    add routes
    add validation to POST and PUT requests

---
+++ middlewares/validationMiddleware.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+
+const newContactSchema = Joi.object({
+    name: Joi.string().pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/).min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(6).max(14).pattern(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/).required(),
+})
+const updateContactSchema = Joi.object({
+    name: Joi.string().pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/).min(3).max(30).allow(""),
+    email: Joi.string().email().allow(""),
+    phone: Joi.string().min(6).max(14).pattern(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/).allow(""),
+})  .min(1)
+  .max(3);
+
+
+
+
+module.exports = {
+    addContactValidation: (req, res, next) => { 
+        const validationResult = newContactSchema.validate(req.body);
+        // console.log(validationResult);
+        if (validationResult.error) {
+            // console.log(validationResult.error.details);
+            return res.status(400).json({ "message": validationResult.error.details })
+        }
+        next()
+    },
+    updateContactValidation: (req, res, next) => { 
+        // console.log(Object.values(req.body));
+        const { name, email, phone } = req.body;
+        if (name === '' && email === '' && phone === '') {
+            return res.status(400).json({ "message": "missing fields (at least one of the fields should be filled)" })
+        }
+
+        const validationResult = updateContactSchema.validate(req.body);
+        if (validationResult.error) {
+            return res.status(400).json({ "d": validationResult.error.details })
+        }
+        next()
+    }
+}
