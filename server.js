e75fa4ce705c6bd32a5ef4a61ccada3a8b895dfd
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-12-04 15:59:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,6 @@ app.use((err, _, res, __) => {
 const PORT = process.env.PORT || 3000;
 const uriDb = process.env.MONGO_DB;
 
-mongoose.Promise = global.Promise;
-
 const connection = mongoose.connect(uriDb, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
