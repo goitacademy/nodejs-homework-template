2360077be0a6cbe4fe09dc06f8f4c04a089e6bf4
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2022-12-06 12:05:04 +0200

    add {}, "-createdAt -updatedAt"

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,7 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const result = await Contact.find({}, "-createdAt -updatedAt");
   res.json(result);
 };
 
