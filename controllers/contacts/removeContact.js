2a4f15931cdc7b32adfbfbdac6885a13a04ec2b4
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-12-07 01:28:24 +0200

    Update removeContact.js

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -4,8 +4,7 @@ const {HttpErr} = require('../../helpers')
 const removeContact = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contacts.removeContact(contactId);
-  console.log(result)
-    
+     
   if (!result) {
     throw HttpErr(404, "Not found");
   }
