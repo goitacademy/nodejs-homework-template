8d9b8c88f272ef573e1e7118db14dbfa959515ac
Yana Pavlyk <pavlikyg@gmail.com>
2022-12-07 00:06:00 +0200

    hw done

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,4 +1,4 @@
-const HttpError = require('../helpers/HttpError');
+const {HttpError} = require('../helpers');
 
 const validateBody = (schema, errorMessage) => {
     const func = (req, res, next) => {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const controllers = require('../../controllers')
-const ctrlWrapper = require('../../helpers/ctrlWrapper')
+const {ctrlWrapper} = require('../../helpers')
 const validateBody = require('../../middlewares/validateBody')
 const schemas = require('../../schemas')
 
