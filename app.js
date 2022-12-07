0af4e6d68bcd72e9f3a8d5687796ee314a541c01
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-12-07 14:18:56 +0200

    цук

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errorHandler } = require("./src/helpers/apiHelpers");
 const contactsRouter = require("./src/routes/contactRoutes");
 const authRouter = require("./src/routes/authRoutes");
 const filesRouter = require("./src/routes/filesRoutes");
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -13,7 +14,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/avatars", express.static("public/avatars"));
+// app.use("/avatars", express.static("public/avatars"));
+app.use("/avatars", express.static(__dirname + "/src/public/avatars"));
+// app.use("/avatars", express.static("arc/public/avatars"));
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 app.use("/api/files", filesRouter);
