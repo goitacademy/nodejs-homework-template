515d94fbd923d8077b5c992f0dd71701dcc7b26c
VitaliiPozniakov <vertalen89@gmail.com>
2022-12-07 22:03:58 +0200

    add draft of third HW

--- bin/server.js
+++ bin/server.js
@@ -1,13 +1,9 @@
 const app = require('../app');
 const mongoose = require('mongoose');
 
+require("dotenv").config()
 
-const PORT = 3000
-const DB_HOST = "mongodb+srv://Vitalii:Fy420295@cluster0.d4bri3o.mongodb.net/db-contacts?retryWrites=true&w=majority"
-
-// const {DB_HOST, PORT = 3000} = process.env;
-
-// console.log(process.env)
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose
     .connect(DB_HOST)
