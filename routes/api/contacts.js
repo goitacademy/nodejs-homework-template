1a8f8be75580a45c22ca9094e274dc521201e903
Andrii-Smolianinov <andr380v@gmail.com>
2022-12-07 12:51:31 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,27 @@
 const express = require('express')
 
+const contacts = require("../../models/contacts.json")
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts)
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts)
 })
 
 module.exports = router
