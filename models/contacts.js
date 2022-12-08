8791b7e9cdd0ede7d2ff8e134527de5d3f35d71d
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-12-08 23:58:59 +0200

    add joi

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,7 @@
 // const fs = require('fs/promises')
 const path = require("path");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 const contactsPath = path.resolve("models/contacts.json");
 
 const listContacts = async () => {
@@ -16,7 +17,18 @@ const getContactById = async (contactId) => {
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async ({name, email, phone}) => {
+  const contacts = await listContacts();
+    const newContact = {
+      id:nanoid(),
+      name,
+      email,
+      phone,
+    }
+  contacts.push(newContact)
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return newContact
+}
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const contacts = require('../../models/contacts')
-const path = require("path");
+const {HttpError} = require('../../helpers')
+const Joi = require("joi");
 const router = express.Router()
+
+const addSchema = Joi.object({
+  name: Joi.string().required,
+  email: Joi.string().required,
+  phone: Joi.string().required,
+})
 
 router.get('/', async (req, res, next) => {
   try {
     const result = await contacts.listContacts()
     res.json(result)
-  } catch (err) {
-    res.status(500).json({
-      message: "Server error"
-    })
+  } catch (error) {
+    next(error)
   }
   
 })
@@ -20,21 +25,27 @@ router.get('/:id', async (req, res, next) => {
     const { id } = req.params
     const result = await contacts.getContactById(id)
     if (!result) {
-      res.status(404).json({
-      message:"Not found"
-      })
+      throw HttpError(404, 'Not Found')
     }
     res.json(result)
-  } catch (err) {
-    res.status(500).json({
-      message: "Server error"
-    })
+  } catch (error) {
+    next(error)
   }
   
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = addSchema.validate({ name: 'doren', email: 'adoren@gmail.net', phone: '(044) 454-1111' })
+    console.log(req.body)
+    if (error) {
+      throw HttpError(400, "Missing required field")
+    }
+    const result = await contacts.addContact(req.body)
+    res.status(201).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
