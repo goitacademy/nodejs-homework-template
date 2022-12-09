e95b2cba0cbeb713173f5bd56a3bafa0d85bb7b2
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-12-09 11:10:19 +0200

    add npm packet and fync in models

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,135 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const path = require("path");
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const removeContact = async (contactId) => {}
+const replaceFile = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const addContact = async (body) => {}
+const { nanoid } = require("nanoid");
 
-const updateContact = async (contactId, body) => {}
+const validator = require("validator");
+
+const listContacts = async () => {
+  try {
+    const list = await fs.readFile(contactsPath);
+    return JSON.parse(list);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const idContact = String(contactId);
+    const contacts = await listContacts();
+    const getContact = contacts.find((contact) => contact.id === idContact);
+    return getContact || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const idContact = String(contactId);
+    const contacts = await listContacts();
+    const indexContact = contacts.findIndex(
+      (contact) => contact.id === idContact
+    );
+    if (indexContact === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(indexContact, 1);
+    await replaceFile(contacts);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const addContact = async ({ name, email, phone }) => {
+  try {
+    if (!name) {
+      return `Name is required`;
+    }
+    if (!email) {
+      return `Email is required`;
+    }
+    if (!phone) {
+      return `Phone is required`;
+    }
+
+    const isValidEmail = validator.isEmail(email);
+    if (!isValidEmail) {
+      return `Email entered incorrectly`;
+    }
+
+    const isValidPhone = validator.isMobilePhone(String(phone));
+    if (!isValidPhone) {
+      return `Phone number entered incorrectly`;
+    }
+
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await replaceFile(contacts);
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const updateContactById = async (contactId, data) => {
+  try {
+    const idContact = String(contactId);
+    const contacts = await listContacts();
+    const indexContact = contacts.findIndex(
+      (contact) => contact.id === idContact
+    );
+    if (indexContact === -1) {
+      return null;
+    }
+    const indexName = contacts[indexContact].name;
+    const indexEmail = contacts[indexContact].email;
+    const indexPhone = contacts[indexContact].phone;
+
+    contacts[indexContact] = {
+      id: nanoid(),
+      name: data.name ?? indexName,
+      email: data.email ?? indexEmail,
+      phone: String(data.phone ?? indexPhone),
+    };
+    const isValidEmail = validator.isEmail(contacts[indexContact].email);
+    if (!isValidEmail) {
+      return `Email entered incorrectly`;
+    }
+
+    const isValidPhone = validator.isMobilePhone(
+      String(contacts[indexContact].phone)
+    );
+    if (!isValidPhone) {
+      return `Phone number entered incorrectly`;
+    }
+    await replaceFile(contacts);
+    return contacts[indexContact];
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
-  updateContact,
-}
+  removeContact,
+  updateContactById,
+  replaceFile,
+};
