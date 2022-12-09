31b3529e49b9571633d4c216d172f81da490e6f1
Leonid Haidash <Gaydashleo@gmail.com>
2022-12-09 14:27:16 +0200

    serverFix

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connection successful"))
-  .catch((error) => console.log(error.message));
+  .then(
+    () => app.listen(PORT),
+    console.log("Database connection successful"),
+    console.log("Server running. Use our API on port: 3000")
+  )
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// const { DB_HOST } = process.env;
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connection successful"))
+//   .catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
