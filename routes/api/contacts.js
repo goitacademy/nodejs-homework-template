e800bbe1a5a7a79de55d866579dd29ae1a4b796d
Dmitry Natalchuk <next1996@ukr.net>
2022-12-09 16:44:37 +0200

    fix file shemas and middleware

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,64 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  addContact,
-  updateContact,
-  listContacts,
-  getContactById,
-  removeContact,
-} = require("../../models/contacts");
-
 const { validation } = require("./middleware/validation");
 
 const {
   addContactValidation,
   changeContactValidation,
-} = require("./middleware/validationContact");
+} = require("../../middleware/schemas/validationContact");
 
-const getContacts = async (req, res) => {
-  const data = await listContacts();
-  res.status(200).json({ message: data });
-};
-
-const getContactByID = async (req, res) => {
-  try {
-    const { contactId } = req.params;
-    const data = await getContactById(contactId);
-    res.status(200).json({ message: data });
-  } catch (error) {
-    res.status(404).json({ message: "Not found" });
-  }
-};
-
-const postAddContact = async (req, res) => {
-  const data = await addContact(req.body);
-  res.status(201).json({ message: data });
-};
-
-const putChangeContact = async (req, res) => {
-  const { contactId } = req.params;
-  const data = await updateContact(contactId, req.body);
-  if (data) {
-    res.status(201).json({ message: data });
-  } else {
-    res.status(404).json({ message: "Not found" });
-  }
-};
-
-const deleteContact = async (req, res) => {
-  try {
-    const { contactId } = req.params;
-    const data = await removeContact(contactId);
-    if (data) {
-      res.status(200).json({ message: "contact deleted" });
-    } else {
-      res.status(404).json({ message: "Not found" });
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+const {
+  getContacts,
+  getContactByID,
+  postAddContact,
+  deleteContact,
+  putChangeContact,
+} = require("../../controllers/controllers");
 
 router.get("/", getContacts);
 
