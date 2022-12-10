011c6597057aff4845ffba2a935a43396cb35923
HalynaStep <gstep2003@gmail.com>
2022-12-10 13:31:12 +0200

    hw-05

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,13 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
 
 const { requestError } = require("../helpers/api.helpers");
 const { SECRET_KEY } = process.env;
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res, next) => {
   const { email, password, subscription } = req.body;
@@ -12,10 +16,12 @@ const register = async (req, res, next) => {
     return next(requestError(409, "Email in use"));
   }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const avatarURL = gravatar.url(email);
   const addUser = await User.create({
     email,
     password: hashPassword,
     subscription,
+    avatarURL,
   });
 
   res.status(201).json({
@@ -60,9 +66,26 @@ const logout = async (req, res, next) => {
   res.status(204).json();
 };
 
+const updateAvatar = async (req, res, next) => {
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const avatarName = `${id}_${originalname}`;
+  try {
+    const resultUpload = path.join(avatarDir, avatarName);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("public", "avatars", avatarName);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    res.status(200).json(avatarURL);
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    next(error);
+  }
+};
+
 module.exports = {
   register,
   login,
   getCurrent,
   logout,
+  updateAvatar,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,10 +6,12 @@ const {
   login,
   getCurrent,
   logout,
+  updateAvatar,
 } = require("../../controllers/auth.controller");
 
 const { validationBody } = require("../../middlewares/validationBody.js");
 const validationToken = require("../../middlewares/validationToken.js");
+const upload = require("../../middlewares/upload.js");
 const { schemaAuth } = require("../../schema/validationSchema");
 
 const { asyncWrapper } = require("../../helpers/api.helpers");
@@ -18,5 +20,11 @@ router.post("/signup", validationBody(schemaAuth), asyncWrapper(register));
 router.post("/login", validationBody(schemaAuth), asyncWrapper(login));
 router.get("/current", validationToken, asyncWrapper(getCurrent));
 router.get("/logout", validationToken, asyncWrapper(logout));
+router.patch(
+  "/avatars",
+  validationToken,
+  upload.single("avatar"),
+  asyncWrapper(updateAvatar)
+);
 
 module.exports = router;
