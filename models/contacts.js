3a54f4bae6c70570488a303cabd79304d3cc1804
Andrey Avdeev <a3avdeev@gmail.com>
2022-12-11 02:28:29 +0200

    add helpers and modify contacts in api before electicity turn off

--- models/contacts.js
+++ models/contacts.js
@@ -2,23 +2,27 @@ const fs = require("fs").promises;
 const { nanoid } = require("nanoid");
 const path = require("path");
 
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
 console.log(contactsPath);
 
+// общий параметр обновления списка
 const updateContactsList = async (contacts) =>
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
+// функция формирования всего списка контактов
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
+// функция доступа к контакту по ид
 async function getContactById(contactId) {
   const contacts = await listContacts();
   const result = contacts.find((item) => item.id === contactId.toString());
   return result;
 }
 
+// функция удаления контакта по ид
 async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId.toString());
@@ -30,6 +34,7 @@ async function removeContact(contactId) {
   return result;
 }
 
+// функция добавления нового контакта
 async function addContact(id, name, email, phone) {
   const contacts = await listContacts();
   const newContact = {
@@ -43,8 +48,19 @@ async function addContact(id, name, email, phone) {
   return newContact;
 }
 
-const updateContact = async (contactId, body) => {};
+// функция обновления данных в контакте по ид
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId.toString());
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, ...body };
+  await updateContactsList(contacts);
+  return contacts[index];
+};
 
+// экспортируем все эти функции
 module.exports = {
   listContacts,
   getContactById,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,35 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
+const { HttpError } = require("../../helpers/index");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
