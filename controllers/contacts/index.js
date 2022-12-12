4cd96655015ebd8acd46f3c5c20a08cb691bbd56
pavliv182 <uptn182@gmail.com>
2022-12-12 20:40:15 +0200

    removeById

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -2,7 +2,7 @@ const getAll = require("./getAll");
 const getById = require("./getById");
 const add = require("./add");
 const updateById = require("./updateById");
-const removeById = require("./removeByID");
+const removeById = require("./removeById");
 
 module.exports = {
   getAll,
