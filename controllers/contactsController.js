af4b3ae44949b6edbe973bad289a508a6d3a299b
Alexey <alexei20293@yahoo.com>
2022-12-13 14:35:35 +0200

    add toggle favorite feature

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,26 +1,30 @@
 
-const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../models/contacts');
+const { Contact } = require('../db/contactModel')
 
 const getContactsController = async (req, res) => { 
-  const contacts = await listContacts();
-  return res.status(200).json({ data: contacts});
+    const contacts = await Contact.find({});
+    res.status(200).json({ data: contacts});
 }
 
 const getContactByIdController = async (req, res) => {
-    const foundContact = await getContactById(String(req.params.contactId));
+    const {contactId} = req.params
+    const foundContact = await Contact.findById(String(contactId));
     if (foundContact) {
-        return res.status(200).json({ data: foundContact });
-    }
+            return res.status(200).json({ data: foundContact });
+        }
     return res.status(404).json({ message: "Not found" });
 }
 
 const addContactController = async (req, res ) => {
-    const newContact = await addContact(req.body);
+    const newContact = new Contact(req.body);
+    await newContact.save();
     return res.status(201).json({ data: newContact });
+    // add default favorite key - false!!!!!!!!!!!!!!!!!!!
 }
 
 const removeContactController = async (req, res) => {
-    const deletedContact = await removeContact(String(req.params.contactId));
+    const {contactId} = req.params
+    const deletedContact = await Contact.findByIdAndDelete(String(contactId));
     if (deletedContact) {
       return res.status(200).json({ message: "contact deleted" }); 
     }
@@ -28,17 +32,33 @@ const removeContactController = async (req, res) => {
 }
 
 const updateContactController = async (req, res) => {
-    const updatedContact = await updateContact(String(req.params.contactId), req.body);
+    const {contactId} = req.params
+    const { name, email, phone, favorite } = req.body
+    const updatedContact = await Contact.findByIdAndUpdate(String(contactId), {$set: { name, email, phone, favorite } }, { new: true });
+    
     if (updatedContact) {
-      return res.status(200).json({ data: updatedContact }); 
+        return res.status(200).json({ data: updatedContact }); 
+        // WORKING LIKE A PATCH METHOD !!!!!!!!
     }
     return res.status(404).json({ message: "Not found" });
 }
+
+const updateStatusContactController = async (req, res) => { 
+    const { contactId } = req.params;
+    const { favorite } = req.body
+    const updatedContact = await Contact.findByIdAndUpdate(String(contactId), { $set: { favorite } }, { new: true })
+    if (updatedContact) {
+        return res.status(200).json({ data: updatedContact });
+        }
+        return res.status(404).json({ message: "Not found" });
+}
+
 
 module.exports = {
     getContactsController,
     getContactByIdController,
     addContactController,
     removeContactController,
-    updateContactController
+    updateContactController,
+    updateStatusContactController
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { addContactValidation, updateContactValidation } = require('../../middlewares/validationMiddleware');
-const { getContactsController, getContactByIdController, addContactController, removeContactController, updateContactController } = require('../../controllers/contactsController');
+const { getContactsController, getContactByIdController, addContactController, removeContactController, updateContactController, updateStatusContactController } = require('../../controllers/contactsController');
 const { asyncWrapper } = require('../../helpers/apiHelpers');
 
 const router = express.Router()
@@ -10,4 +10,6 @@ router.get('/:contactId', asyncWrapper(getContactByIdController));
 router.post('/', addContactValidation, asyncWrapper(addContactController));
 router.delete('/:contactId', asyncWrapper(removeContactController));
 router.put('/:contactId', updateContactValidation, asyncWrapper(updateContactController));
+router.patch('/:contactId/favorite', asyncWrapper(updateStatusContactController));
+
 module.exports = router
