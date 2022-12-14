386af0b2275b3ef5a65aa67859c4eead2e36fcd3
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2022-12-14 18:32:28 +0200

    hw04Auth

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-
-const mongoose = require('mongoose');
-const { mongoURL } = require('./config');
-
-mongoose
-  .connect(mongoURL, { dbName: '03-mongodb' })
-  .then(() => console.log('Database connection successful'))
-  .catch(err => {
-    console.log(err.message);
-    process.exit(1);
-  });
-
+const auth = require('./middlewares/auth');
 const contactsRouter = require('./routes/api/contacts');
+const usersRouter = require('./routes/api/users');
 
 const app = express();
-app.use(express.json());
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/api/contacts', auth, contactsRouter);
+app.use('/users', usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const auth = require('../../middlewares/auth');
+const { usersPostSchema, usersSubscSchema } = require('../../schemas');
+const {
+  signup,
+  login,
+  logout,
+  current,
+  subscription,
+} = require('../../controller');
+
+const router = express.Router();
+
+router.post('/signup', usersPostSchema, signup);
+router.post('/login', usersPostSchema, login);
+router.get('/logout', auth, logout);
+router.get('/current', auth, current);
+router.patch('/', auth, usersSubscSchema, subscription);
+
+module.exports = router;
