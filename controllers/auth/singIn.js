4ad198b23ff5ef9084351f395036cfd83c36ab1d
analepekha <maiboroda0103@gmail.com>
2022-12-15 12:56:41 +0200

    done

--- controllers/auth/singIn.js
+++ controllers/auth/singIn.js
@@ -15,6 +15,7 @@ const singIn = async (req, res) => {
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   await User.findByIdAndUpdate(user._id, { token });
+
   res.status(200).json({
     status: "success",
     code: 200,
