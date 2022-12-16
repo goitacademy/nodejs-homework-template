8aec844268599f506a3957ea9d443a7f21c61fc6
stgran66 <stanislaw.grygoriew@gmail.com>
2022-12-16 12:27:22 +0100

    moved schemas and controllers to separate files and folders

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,80 +1,23 @@
 const express = require('express');
-const Joi = require('joi');
 
 const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require('../../models/contacts');
+  addContactController,
+  listContactsController,
+  getContactController,
+  deleteContactController,
+  updateContactController,
+} = require('../../utils/controllers');
 
 const router = express.Router();
 
-const addContactSchema = Joi.object({
-  name: Joi.string().alphanum().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
+router.get('/', listContactsController);
 
-const updateContactSchema = Joi.object({
-  name: Joi.string().alphanum(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-}).min(1);
+router.get('/:contactId', getContactController);
 
-router.get('/', async (req, res, next) => {
-  const data = await listContacts();
-  res.json(data);
-});
+router.post('/', addContactController);
 
-router.get('/:contactId', async (req, res, next) => {
-  const data = await getContactById(req.params.contactId);
-  if (data.length === 0) {
-    res.status(404).json({ message: 'Not found' });
-    return;
-  }
-  res.json(data);
-});
+router.delete('/:contactId', deleteContactController);
 
-router.post('/', async (req, res, next) => {
-  const { error } = await addContactSchema.validate(req.body);
-  if (error) {
-    console.log(error);
-    res.status(400).json({
-      message: 'missing or incorrect fields',
-    });
-    return;
-  }
-  const data = await addContact(req.body);
-  res.status(201).json(data);
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  const data = await removeContact(req.params.contactId);
-  if (data) {
-    res.json({ message: 'contact deleted' });
-    return;
-  }
-  res.status(404).json({ message: 'Not found' });
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  const { error } = await updateContactSchema.validate(req.body);
-  if (error) {
-    console.log(error);
-    res.status(400).json({
-      message: 'missing or incorrect fields',
-    });
-    return;
-  }
-
-  const updatedContact = await updateContact(req.params.contactId, req.body);
-  if (updatedContact) {
-    res.json(updatedContact);
-    return;
-  }
-  res.status(404).json({ message: 'Not found' });
-});
+router.put('/:contactId', updateContactController);
 
 module.exports = router;
