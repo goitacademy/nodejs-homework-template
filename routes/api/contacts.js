4449ee6759bdebac0aaf61d1f160043093cf20e9
PetroHrynyk <grynyk82@gmail.com>
2022-12-17 16:42:50 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,7 +6,7 @@ const Joi = require("joi");
 
 const contactsSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     phone: Joi.number().required(),
 })
 
