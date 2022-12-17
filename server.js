68ea0f84a72b03a1f724e3b3e9069e17d577520f
PetroHrynyk <grynyk82@gmail.com>
2022-12-17 16:24:45 +0200

    Done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const {PORT = 3000} = process.env;
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
 })
