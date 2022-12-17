e1c98e4c56224a3a26112737cf8f2668ed63f93b
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-12-17 23:51:58 +0200

    add part of functions to routes\api\contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,64 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
+const contacts = require("../../models/contacts.js");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  // res.json({ message: "template message get" });
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contactsList = await contacts.listContacts();
+    res.status(200).json(contactsList);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  try {
+    const id = req.params.contactId;
+    const oneContact = await contacts.getContactById(id);
+    if (!oneContact) {
+      const message = new Error("Not found");
+      message.status = 404;
+      throw message;
+    }
+    res.status(200).json(oneContact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  try {
+    const body = req.body;
+    const newContact = await contacts.addContact(body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  try {
+    const deleteContact = await contacts.removeContact(id);
+    res.json(deleteContact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  // res.json({ message: "template message" });
+  try {
+    const updContact = await contacts.addContact({ name, email, phone });
+    res.json(updContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
