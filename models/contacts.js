56a95f3381b4a60497b091b6800852e4c9ffe77b
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-12-18 13:54:21 +0200

    hw2+

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,69 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require('path');
+// const { v4: uuidv4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json')
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath);
+  return JSON.parse(result);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId)
+  if (index === -1) {
+    return null
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return result
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    // id: uuidv4(),
+    id: String(contacts.length + 1),
+    name,
+    email,
+    phone,
+  }
+  const contactList = JSON.stringify([...contacts, newContact], null, 2)
+  await fs.writeFile(contactsPath, contactList)
+  return newContact
+}
+
+const updateContact = async (contactId, { name, phone, email }) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId)
+  const [contact] = contacts.filter((el) => el.id === contactId)
+
+  if (!contact) {
+    return null
+  }
+  console.log(contact);
+  if (name) {
+    contact.name = name
+  }
+  if (phone) {
+    contact.phone = phone
+  }
+  if (email) {
+    contact.email = email
+  }
+
+  const [result] = contacts.splice(index, 1, contact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return result
+
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,106 @@
 const express = require('express')
+const contacts = require('../../models/contacts')
+const Joi = require("joi");
 
 const router = express.Router()
 
+const schemaCreateContacts = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
+
+const schemaUpdateContacts = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+})
+  .or("name", "email", "phone")
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactList = await contacts.listContacts()
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      contactList
+    },
+  })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const contact = await contacts.getContactById(contactId)
+
+  if (contact === null) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      contact
+    },
+  })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { name, phone, email } = req.body;
+  const { error, value } = schemaCreateContacts.validate({ name, phone, email })
+
+  if (error) {
+    return res.status(400).json({ message: "missing required name field" });
+  }
+
+  const addContact = await contacts.addContact(value)
+
+  res.json({
+    status: 'success',
+    code: 201,
+    data: {
+      addContact
+    },
+  })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+
+  const deleteContact = await contacts.removeContact(contactId)
+  if (deleteContact === null) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  res.json({
+    code: 200,
+    message: "contact deleted"
+  })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const { name, phone, email } = req.body;
+  const { error, value } = schemaUpdateContacts.validate({ name, phone, email })
+
+  if (error) {
+    return res.status(400).json({ message: "missing fields" });
+  }
+
+  const updContact = await contacts.updateContact(contactId, value)
+
+  if (updContact === null) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      updContact
+    },
+  })
 })
 
 module.exports = router
