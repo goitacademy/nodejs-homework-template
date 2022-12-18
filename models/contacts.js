3895a4f9a18b4dd7c8086ee8a0da10f201a01c47
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-18 12:22:41 +0100

    get contacts, get contact, delete contact

--- models/contacts.js
+++ models/contacts.js
@@ -1,18 +1,69 @@
 const path = require("path");
 const fs = require("fs").promises;
+// const nanoid = require("nanoid");
+// const Joi = require("joi");
 
 const contactsPath = path.resolve("./models/contacts.json");
 
 const listContacts = async () => {
-  const result = await fs.readFile(contactsPath);
-  return result.toString();
+  return JSON.parse(await fs.readFile(contactsPath));
 };
 
-const getContactById = async (contactId) => {};
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(
+    (contact) => contact.id === contactId.toString()
+  );
+  return contact;
+};
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) return null;
 
-const addContact = async (body) => {};
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  if (!name || !email || !phone) return null;
+  console.log(name, email, phone);
+
+    // const schema = Joi.object({
+  //   name: Joi.string().alpha().min(3).max(30).required(),
+  //   email: Joi.string()
+  //     .email({
+  //       minDomainSegments: 2,
+  //       tlds: {
+  //         allow: ["com", "net"],
+  //       }.required(),
+  //     })
+  //     .required(),
+  //   phone: Joi.string()
+  //     .regex(/^[0-9]{10}$/)
+  //     .messages({ "string.pattern.base": `Phone number must have 10 digits.` })
+  //     .required(),
+  // }).with("name", "email", "phone");
+  // const validationResult = schema.validate(body);
+  //  if (!validationResult ) return 0;
+
+  const contacts = JSON.parse(await fs.readFile(contactsPath));
+  const newId = 11
+  // const newId = nanoid();
+  // const newId = parseInt(contacts[contacts.length - 1].id) + 1;
+  const newContact = {
+    id: newId.toString(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
 const updateContact = async (contactId, body) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,83 +1,63 @@
 const express = require("express");
-const contacts = require("./contacts");
-const nanoid = require("nanoid");
-const Joi = require("joi");
-
-const router = express.Router();
-const contactsList = contacts.listContacts();
-
-router.get("/", async (req, res, next) => {
-  // викликає функцію listContacts
-  res.json({ contactsList, status: "success" });
-});
+const contacts = require("../../models/contacts");
 // app.get( "way", callback) коли прийде запит за цією адресою - зроби ''це''
 // якщо нема потрібної лінки, то кеннот гет ''лінк''
 // респонс.дсон - для відправляння джсону
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  const data = await contacts.listContacts();
+  res.json({ data, status: 200 });
+});
+
 router.get("/:contactId", async (req, res, next) => {
-  const { id } = req.params;
-  const myContact = contacts.getById(id);
-  if (!myContact) res.json({ message: "Not found", status: 404 });
-  // викликає функцію getById
-  res.json({ data: myContact, status: 200 });
+  const { contactId } = req.params;
+  const data = await contacts.getContactById(contactId);
+  if (!data) res.json({ message: "Not found", status: 404 });
+  res.json({ data, status: 200 });
+  // якщо помилка, то апп падає і неможливо надіслати наступний запит?
 });
 
 router.post("/", async (req, res, next) => {
-  const id = nanoid();
-  const { name, email, phone } = req.body;
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string().email(
-      {
-        minDomainSegments: 2,
-        tlds: {
-          allow: ["com", "net"],
-        },
-      }.required()
-    ),
-    phone: Joi
-      .string()
-      .regex(/^[0-9]{10}$/)
-      .messages({ "string.pattern.base": `Phone number must have 10 digits.` })
-      .required(),
-  }).with("name", "email", "phone");
-
-  const validationResult = schema.validate(req.body)
-  if (validationResult.error) {
-    return req.json({"status": validationResult.error})
+  const result = await contacts.addContact(req.body)
+  if (result === null) {
+    res.json({ "message": "missing required name field", "status": 400 });
+  } else if (result === 0) {
+    // req.json({ "status": validationResult.error }); 
+  } else {
+    res.json({ "data": result, "status": 201 });
   }
-  contacts.push({ id: id.toString(), name, email, phone });
-  res.json({ status: "success" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  // Викликає функцію removeContact
-  // bool = removeContact( req.params)
-  // if(bool){
-  // res.json({ "message": "contact deleted", "status": 200 });
-  // } else {
-  // res.json({ message: "Not found", status: 404 });
-  // }
-  // const contacts = contactsList.filter((it) => it.id !== req.params);
+  const { contactId } = req.params;
+  const bool = await contacts.removeContact(contactId);
+  if (bool === null) {
+    res.json({ message: "Not found", status: 404 });
+  } else {
+    res.json({ message: "contact deleted", status: 200 });
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  if (!req.body) {
-    res.json({ message: "missing fields", status: 400 });
-  }
-  const { name, email, phone } = req.body;
-  // bool = updateContact(contactId, body)
-  // if(bool){
-  // res.json({ "status": 200 });
-  // } else {
-  // res.json({ message: "Not found", status: 404 });
+  // if (!req.body) {
+  //   res.json({ message: "missing fields", status: 400 });
   // }
-  contacts.forEach((item) => {
-    if (item.id === req.params.id) {
-      item.name = name;
-      item.email = email;
-      item.phone = phone;
-    }
-  });
+  // const { name, email, phone } = req.body;
+  // const bool = await contacts.updateContact(contactId, body)
+  // if(bool){
+  // res.json({ "status": 201 });
+  // } else {
+  // res.json({ message: "missing required name field", status: 400 });
+  // }
+  // contacts.forEach((item) => {
+  //   if (item.id === req.params.id) {
+  //     item.name = name;
+  //     item.email = email;
+  //     item.phone = phone;
+  //   }
+  // });
 });
 
 module.exports = router;
