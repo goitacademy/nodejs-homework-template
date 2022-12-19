aefb6ae56ea440d92bc2881bef3c90004063b78d
Maryshchyn <maryshchyn89@gmail.com>
2022-12-19 19:29:22 +0200

    first check

--- models/contacts.js
+++ models/contacts.js
@@ -18,7 +18,17 @@ const getContactById = async (contactId) => {
     return result || null;
 }
 
-const removeContact = async (contactId) => { }
+const removeContact = async (contactId) => {
+  const contactsId = String(contactId)
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactsId);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result
+ }
 
 
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -70,13 +70,21 @@ router.post('/', async (req, res, next) => {
 
 router.delete('/:contactId', async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const result = await contactsOperations.removeContact(id);
-    if (!result) {
+    const id = req.params.contactId;
+    const result  = await contactsOperations.removeContact(id);
+    if (!result ) {
       throw createError(404, `Contact with id=${id} not found`)
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      message: 'contact deleted',
+      data: {
+        result 
       }
+    })
   } catch (error) {
-    next(error)
+    next(error);
   }
 })
 
