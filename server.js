8a503d2811254e9756cba0608b9e8ccdc0e1b0e9
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-12-20 23:28:25 +0200

    finished hw3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() => app.listen(PORT,()=>console.log("Database connection successful")))
   .catch((error) => {
     console.log(error);
     process.exit(1);
