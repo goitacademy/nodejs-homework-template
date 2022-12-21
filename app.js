74cd3e715c867fda7526bb2c402a6de16188a0e3
Danyil Stanislavov <stanislavov.danik@gmail.com>
2022-12-21 22:04:50 +0200

    Update password and user name

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+// Node - js - 29
 
-const contactsRouter = require('./routes/api/contacts')
+// mongodb+srv://Danyil:Node-js-29@cluster0.aruln3x.mongodb.net/test
 
-const app = express()
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const contactsRouter = require("./routes/api/contacts");
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use('/api/contacts', contactsRouter)
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
