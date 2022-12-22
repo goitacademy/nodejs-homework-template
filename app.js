03eec13f391281389a4a48a82512e90dbe9d2c92
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-12-22 20:25:44 +0200

    fix

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://user:12qazxsw21@cluster0.htdi11t.mongodb.net/db_contacts?retryWrites=true&w=majority";
 
-const contactsRouter = require('./routes/api/contacts')
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("BD connection succesful"))
+  .catch((error) => console.log(error));
 
-const app = express()
+const contactsRouter = require("./routes/api/contacts");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
