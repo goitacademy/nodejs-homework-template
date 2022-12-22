717a49eb5bc1db7b5f00c1cdb23b6e3a377c8be0
Maryshchyn <maryshchyn89@gmail.com>
2022-12-22 13:32:27 +0200

    changeById

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -1,11 +1,13 @@
 const getAll = require('./getAll');
 const getById = require("./getById");
 const add = require("./add");
-const updateById = require("./updateById")
+const updateById = require("./updateById");
+const changeById = require("./changeById")
 
 module.exports = {
     getAll,
     getById,
     add,
     updateById,
+    changeById,
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,7 @@
 const express = require('express');
-const createError = require('http-errors');
-const Joi = require("joi");
 
 const {products: ctrl}=require('../../controllers')
 
-const contactSchema = Joi.object({
-  name: Joi.required(),
-  email: Joi.required(),
-  phone: Joi.required()
-})
-
-const contactsOperations = require("../../models/contacts")
 
 const router = express.Router()
 
@@ -22,31 +13,6 @@ router.post('/', ctrl.add)
 
 router.delete('/:contactId', ctrl.updateById)
 
-
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-      const { contactId } = req.params;
-    const result = await contactsOperations.updateContact(contactId, req.body);
-    if (!result) {
-      throw createError(404, `Contact with id=${contactId} not found`)
-      }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        result
-      }
-    })
-
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:contactId', ctrl.changeById)
 
 module.exports = router
