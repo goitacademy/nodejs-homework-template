d80b10032c440176ec1bcaf4d0db9d4a28a645bf
Maryshchyn <maryshchyn89@gmail.com>
2022-12-22 13:27:10 +0200

    delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -18,45 +18,9 @@ router.get('/', ctrl.getAll);
 
 router.get('/:contactId', ctrl.getById)
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-    const result = await contactsOperations.addContact(req.body);
-    res.status(201).json({
-      status: 'success',
-      code: 201,
-      data: {
-        result
-      }
-    })
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/', ctrl.add)
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const result  = await contactsOperations.removeContact(id);
-    if (!result ) {
-      throw createError(404, `Contact with id=${id} not found`)
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      message: 'contact deleted',
-      data: {
-        result 
-      }
-    })
-  } catch (error) {
-    next(error);
-  }
-})
+router.delete('/:contactId', ctrl.updateById)
 
 
 
