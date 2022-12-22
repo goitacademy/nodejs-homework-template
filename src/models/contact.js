f2a18020e1c061fab0facbce6b64306cf5f86da7
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-12-22 11:03:26 +0200

    add

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,21 +1,24 @@
 const { Schema, model } = require("mongoose");
-const handleSchemaValidationErrors = require("../helpers/handlerSchemaValidation")
-const contactSchema = new Schema({
-		name: {
-			type: String,
-			required: [true, 'Set name for contact'],
-		},
-		email: {
-			type: String,
-		},
-		phone: {
-			type: String,
-		},
-		favorite: {
-			type: Boolean,
-			default: false,
-		},
-}, {versionKey: false, timestamps: true});
+const handleSchemaValidationErrors = require("../helpers/handlerSchemaValidation");
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleSchemaValidationErrors);
 
--- src/routes/api/contacts.js
+++ src/routes/api/contacts.js
@@ -17,7 +17,7 @@ const {
 } = require("../../controllers/contactsController.js");
 
 router.get("/", getContacts);
-router.get("/:contactId",isValidId, getContactId);
+router.get("/:contactId", isValidId, getContactId);
 router.post("/", validateSchema(createContactSchema), postContact);
 router.delete("/:contactId", isValidId, deleteContact);
 router.put("/:contactId", isValidId, putContact);
