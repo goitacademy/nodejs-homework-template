2e092a5c95d29e7b69b2702d2ba6954bd3eb074a
DenisVoron <Denis.voronko@gmail.com>
2022-12-23 00:55:08 +0200

    add crud

---
+++ middlewares/walidationMiddleware.js
@@ -0,0 +1,51 @@
+const Joi = require('joi');
+
+module.export = {
+    postValidation: (req, res, next) => {
+        const schema = Joi.object({
+            name: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+                .required(),
+            phone: Joi.number()
+                .min(5)
+                .max(15)
+                .required(),
+        })
+
+        const validationResult = schema.validate(req.body);
+        if (validationResult.error) {
+            return res.status(400).json({ message: "missing required name field" })
+        }
+
+        next();
+    },
+
+    putValidation: (req, res, next) => {
+        const schema = Joi.object({
+            name: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+                .required(),
+            phone: Joi.number()
+                .min(5)
+                .max(15)
+                .required(),
+        })
+
+        const validationResult = schema.validate(req.body);
+        if (validationResult.error) {
+            return res.status(400).json({ message: "missing fields" })
+        }
+
+        next();
+    }
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,46 @@ const express = require('express')
 
 const router = express.Router()
 
+const contacts = require('../../models/contacts');
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.listContacts();
+  res.status(200).json(result)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const result = await contacts.getContactById(id);
+  if (result === null) {
+    return res.status(404).json({ message: "Not found" })
+  }
+  res.json(result)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  const { name, email, phone } = req.body;
+
+  const result = await contacts.addContact({ name, email, phone })
+  res.status(201).json(result)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const result = await contacts.removeContact(id);
+  if (!result) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.status(200).json({ message: "contact deleted" })
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+
+  const { name, email, phone } = req.body;
+  const result = await contacts.updateContact(id, { name, email, phone });
+  res.status(200).json(result);
 })
 
 module.exports = router
