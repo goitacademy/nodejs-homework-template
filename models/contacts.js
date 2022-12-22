02aef6a98c8ef69fc43d24c6543b5d2ce8464709
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-12-23 01:49:52 +0200

    api add

--- models/contacts.js
+++ models/contacts.js
@@ -2,12 +2,12 @@ const fs = require('fs').promises;
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-const contactsPath = path.resolve('./contacts.json');
+const contactsPath = path.resolve('./models/contacts.json');
 
 const listContacts = async function () {
   try {
     const data = await fs.readFile(contactsPath, 'utf8');
-    console.log(data);
+    return data;
   } catch (err) {
     console.error(err);
   }
@@ -20,7 +20,7 @@ const getContact = async function (id) {
     const result = parsedData.filter(obj => {
       return obj.id === id;
     });
-    console.log(result);
+    return result;
   } catch (err) {
     console.error(err);
   }
@@ -33,6 +33,7 @@ const addContact = async function (name, email, phone) {
     const newContent = { id: uuidv4(), name, email, phone };
     parsedData.push(newContent);
     await fs.writeFile(contactsPath, JSON.stringify(parsedData), 'utf8');
+    return JSON.stringify(newContent.id);
   } catch (err) {
     console.error(err);
   }
@@ -41,15 +42,31 @@ const removeContact = async function (id) {
   try {
     const data = await fs.readFile(contactsPath, 'utf8');
     const parsedData = JSON.parse(data);
-    const result = parsedData.filter(obj => {
-      return obj.id !== id;
+    const result = parsedData.find(obj => {
+      return obj.id === id;
     });
-    await fs.writeFile(contactsPath, JSON.stringify(result), 'utf8');
+    if (result) {
+      const newResult = parsedData.filter(obj => {
+        return obj.id !== id;
+      });
+      await fs.writeFile(contactsPath, JSON.stringify(newResult), 'utf8');
+      return newResult;
+    }
+    return null;
   } catch (err) {
     console.error(err);
   }
 };
-const updateContact = async (contactId, body) => {};
+const updateContact = async (id, body) => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const parsedData = JSON.parse(data);
+    const newState = parsedData.map(obj => (obj.id === id ? body : obj));
+    await fs.writeFile(contactsPath, JSON.stringify(newState), 'utf8');
+  } catch (err) {
+    console.error(err);
+  }
+};
 module.exports = {
   listContacts,
   getContact,
