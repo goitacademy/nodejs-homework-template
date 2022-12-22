ee5bacc9b09e64b7f459d1a14f93af81c8917dd8
Mykola Berezhniuk <bermukola@gmail.com>
2022-12-22 13:17:58 +0200

    Change Schema

--- models/contact.js
+++ models/contact.js
@@ -4,13 +4,19 @@ const contactSchema = Schema(
   {
     name: {
       type: String,
+      minlength: 2,
+      maxlength: 70,
       required: [true, 'Set name for contact'],
     },
     email: {
       type: String,
+      minlength: 2,
+      maxlength: 70,
     },
     phone: {
       type: String,
+      required: [true, 'Set phone for contact'],
+      validate: /^\(\d\d\d\) \d\d\d-\d\d\d\d$/i,
     },
     favorite: {
       type: Boolean,
