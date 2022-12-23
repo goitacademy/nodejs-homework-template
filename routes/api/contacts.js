bdaa5044da612586b7c33572f383a417b4f5302c
Maryshchyn <maryshchyn89@gmail.com>
2022-12-23 20:42:20 +0200

    jj

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,84 +1,26 @@
 
 const express = require('express')
-const Joi = require('joi');
 
-const {HttpError} = require("../../helpers")
 
-const contactsOperations = require("../../models/contacts")
+
+
+const ctrl = require("../../controllers/contacts")
+
+
 
 const router = express.Router()
 
-const addSchema = Joi.object({
-  name: Joi.required(),
-  email: Joi.required(),
-  phone: Joi.required(),
-})
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await contactsOperations.listContacts()
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
-})
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsOperations.getContactById(contactId);
-    if (!result) {
-      
-      throw HttpError(404)
-    }
-    res.json(result)
-  }
-  catch (error) {
-    next(error)
-  }
-})
+router.get('/', ctrl.getAll)
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message)
-    }
-    const result = await contactsOperations.addContact(req.body)
-    res.status(201).json(result)
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/:contactId', ctrl.getById)
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const deletedContact = await contactsOperations.removeContact(id);
-    if (!deletedContact) {
-      throw HttpError(404);
-    }
-    res.status(200).json({
-      message: 'Contact deleted',
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post('/', ctrl.add)
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const body = req.body;
-    const result = await contactsOperations.updateContact(id, body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrl.deleteById);
+
+router.put("/:contactId", ctrl.chengeById);
 
 
 module.exports = router
