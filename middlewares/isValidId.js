c1b760b690538700e8cf43e881f2716cd961ec91
Peter <peteryurich@gmail.com>
2022-12-24 00:26:33 +0200

    isValidObjectId - false

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,11 +5,11 @@ const { HttpError } = require("../helpers/HttpErrors")
 const isValidId = (req, res, next) => {
     const { id } = req.params
 
-    console.log(isValidObjectId(id)) // false is always. why?
+console.log(isValidObjectId(id)) // "false" is always. why?
     
-    if (isValidObjectId(id)) {
-        next(HttpError(404, "Invalid id"))
-    }
+    // if (!isValidObjectId(id)) {
+    //     next(HttpError(404, "Invalid id"))
+    // }
     next()
     
 }
