6da04d97b09f1afdfd60090f2cbf1a96185e1cf1
MariiaC <mariiachvorun@gmail.com>
2022-12-24 15:36:21 +0200

    hw4-nodejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import logger from 'morgan';
 import cors from 'cors';
 
 import contactsRouter from './routes/api/contacts.js';
+import usersRouter from './routes/api/users.js';
 
 //важлива послідовність
 const app = express();
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
+app.use('/api/users', usersRouter);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- models/schemas/contactsModel.js
+++ models/schemas/contactsModel.js
@@ -16,6 +16,10 @@ const contactsSchema = new Schema({
         type: Boolean,
         default: false,
     },
+       owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
+    }
 });
 
 const Contact = model('contacts', contactsSchema);
