05f82a507762205fe0139bc83edf89ff7b773100
Anna Riabova <annaua04@gmail.com>
2022-12-24 15:24:16 +0200

    add helpers

---
+++ helpers/validateSchema.js
@@ -0,0 +1,13 @@
+const validateSchema = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      error.status = 400;
+      next(error);
+    }
+    next();
+  };
+};
+
+module.exports = { validateSchema };
