a31e4f1800e411c9d7ec3d771f56e48ae7e52128
Anna Riabova <annaua04@gmail.com>
2022-12-24 15:24:54 +0200

    task ContactsApi completed

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,123 +1,34 @@
 const express = require("express");
-
-const Joi = require("joi");
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
-
-const contactsOperations = require("../../models/contacts");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await contactsOperations.listContacts();
+const { ctrlWrapper } = require("../../middlewares");
+const { contacts: ctrl } = require("../../controllers");
 
-    res.json({
-      tstaus: "Success",
-      code: 200,
-      message: "Contacts received",
-      data: { contacts },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+const { validateSchema } = require("../../helpers/validateSchema");
+const schemas = require("../../schemas/shemasValidation");
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsOperations.getContactById(contactId);
+router.get("/", ctrlWrapper(ctrl.listContacts));
 
-    if (!contact) {
-      const error = new Error(`Contact with id=${contactId} not found`);
-      error.status = 404;
-      throw error;
-    }
+router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
 
-    res.json({
-      ststaus: "Success",
-      code: 200,
-      message: "Contact found",
-      data: { contact },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  "/",
+  validateSchema(schemas.contactSchema),
+  ctrlWrapper(ctrl.addContact)
+);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
+router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
+router.put(
+  "/:contactId",
+  validateSchema(schemas.contactSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
 
-    const contact = await contactsOperations.addContact(req.body);
-
-    res.status(201).json({
-      ststaus: "Success",
-      code: 201,
-      message: "Contact was added",
-      data: { contact },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsOperations.removeContact(contactId);
-
-    if (!contact) {
-      const error = new Error(`Contact with id=${contactId} not found`);
-      error.status = 404;
-      throw error;
-    }
-    res.json({
-      ststaus: "Success",
-      code: 200,
-      message: "Contact deleted",
-      data: {
-        contact,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-
-    const { contactId } = req.params;
-
-    const contact = await contactsOperations.updateContact(contactId, req.body);
-
-    if (!contact) {
-      const error = new Error(`Contact with id=${contactId} not found`);
-      error.status = 404;
-      throw error;
-    }
-
-    res.json({
-      code: 200,
-      data: { contact },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  "/:contactId/favorite",
+  validateSchema(schemas.contactSchema),
+  ctrlWrapper(ctrl.updateContactByFavorite)
+);
 
 module.exports = router;
