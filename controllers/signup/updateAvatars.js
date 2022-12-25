9807b67cbc3ec34a1b2746713cf94b034c346c14
AndreyKapus <kapustnikov@ukr.net>
2022-12-25 12:26:59 +0200

    final

--- controllers/signup/updateAvatars.js
+++ controllers/signup/updateAvatars.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models/users");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -11,6 +12,18 @@ const updateAvatars = async (req, res) => {
 
   try {
     const resultUpload = path.join(avatarsDir, originalname);
+
+    const img = await Jimp.read(tempUpload);
+    await img
+      .autocrop()
+      .contain(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER,
+        Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tempUpload);
+
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("public", "avatars", fileName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
