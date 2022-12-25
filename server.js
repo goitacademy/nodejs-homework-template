18798345129a25495a0ccf99faa36a06c005da93
Maryshchyn <maryshchyn89@gmail.com>
2022-12-25 16:28:21 +0200

    er

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+
+// rIIvlmrEdOJQinDK
+
+const mongoose = require("mongoose");
+
+const DB_HOST = "mongodb+srv://Yevgen:rIIvlmrEdOJQinDK@cluster0.a346oin.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true)
+
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+.catch(error => console.log(error.message))
+
 const app = require('./app')
 
 app.listen(3000, () => {
