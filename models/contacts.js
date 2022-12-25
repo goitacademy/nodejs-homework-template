79148c34f420d278e9eab02f85248bb0b0ce9689
Kseniia30 <kellyshikova3004@gmail.com>
2022-12-25 21:32:01 +0200

    add hw

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,42 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.resolve("models/contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(contacts);
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id === contactId);
+  return contact;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contact = await getContactById(contactId);
+  const contacts = await listContacts();
+  const newContactList = contacts.filter((item) => item.id !== contactId);
+  fs.writeFile(contactsPath, JSON.stringify(newContactList, null, 2));
+  return contact;
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = body;
+  contacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await getContactById(contactId);
+  const contactIndex = contacts.findIndex((item) => item.id === contactId);
+  const updatedContact = contacts[contactIndex];
+  updatedContact.body = body;
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return updatedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +44,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,62 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.status(200);
+  res.json(contacts);
+  next();
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const contact = await getContactById(req.params.contactId);
+  res.status(200);
+  res.json(contact);
+  next();
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  req.body = {
+    id: new Date(),
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+  if (!req.body) {
+    return res.json({ status: 400, message: "Body parameter is required" });
+  }
+  const contact = await addContact(req.body);
+  res.status(200);
+  res.json({ message: `Contact ${contact.name} was successfully added` });
+  next();
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const contact = await removeContact(req.params.contactId);
+  res.status(200);
+  res.json({ message: `Contact ${contact.name} was successfully deleted` });
+  next();
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  req.body = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+  const contact = await updateContact(req.body);
+  res.status(200);
+  res.json({ message: `Contact ${contact.name} was successfully changed` });
+  next();
+});
+
+module.exports = router;
