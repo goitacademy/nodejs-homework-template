ea5912a4324bd71ea5a7888e406422e03772c720
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-12-26 21:21:09 +0200

    controllers
    all controllers

---
+++ controllers/contacts/getById.js
@@ -0,0 +1,19 @@
+const contacts = require("../../models/contacts");
+const { HttpError } = require("../../helpers");
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getById;
--- models/contacts.js
+++ models/contacts.js
@@ -1,23 +1,3 @@
-// const fs = require('fs/promises')
-
-// const listContacts = async () => {}
-
-// const getContactById = async (contactId) => {}
-
-// const removeContact = async (contactId) => {}
-
-// const addContact = async (body) => {}
-
-// const updateContact = async (contactId, body) => {}
-
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// }
-
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
