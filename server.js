d88dd0b7f0f765a9f6201cee31ee788c02ceb804
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-12-26 13:05:46 +0100

    Домашнє завдання №3 Node

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() =>
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     })
   )
