03bc6403ffef787a734798991cd9f7a63d389ef9
AleksandrMarkitan <0688138@gmail.com>
2022-12-26 15:17:20 +0200

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -15,9 +15,6 @@ const getAll = async (req, res) => {
     }
   ).populate("owner", "email subscription"); // populate-особливий метод монгуса, який означає поширення запиту, він бере ID, який записаний в поле owner, через модель йде в базу даних по колекцію, яка записана в ref, знаходить там об'єкт з відповідним ID і підставляє увесь його зміст замість owner. Якщо не треба передавати всю інформацію користувача, то другим аргументом через пробіл можна передати ті поля які треба, як в даному випадку, це "email та subscription"
 
-  // if (favorite) {
-  //   result = result.filter((contact) => contact.favorite === true);
-  // }
   res.json(result);
 };
 
@@ -36,9 +33,16 @@ const add = async (req, res) => {
 };
 
 const apdateById = async (req, res, next) => {
-  const result = await Contact.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  const result = await Contact.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      owner: req.user._id,
+    },
+    req.body,
+    {
+      new: true,
+    }
+  );
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -46,9 +50,16 @@ const apdateById = async (req, res, next) => {
 };
 
 const updateStatusContact = async (req, res, next) => {
-  const result = await Contact.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  const result = await Contact.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      owner: req.user._id,
+    },
+    req.body,
+    {
+      new: true,
+    }
+  );
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -56,7 +67,10 @@ const updateStatusContact = async (req, res, next) => {
 };
 
 const deleteById = async (req, res, next) => {
-  const result = await Contact.findByIdAndRemove(req.params.id);
+  const result = await Contact.findOneAndRemove({
+    _id: req.params.id,
+    owner: req.user._id,
+  });
   if (!result) {
     throw HttpError(404, "Not found");
   }
