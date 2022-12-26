17f8c21bc2bb68600483a03c4ad5a69dc7aeec2b
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-12-26 12:59:12 +0100

    Домашнє завдання №3 Node

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
 // });
+
+dotenv.config();
 
 const { DB_HOST } = process.env;
 
