11e8637d63ac67a7d1071a10616dd9a7064e111c
PetroHrynyk <grynyk82@gmail.com>
2022-12-26 20:08:14 +0200

    fix

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 
 const {DB_HOST, PORT = 3000} = process.env;
 console.log(process.env.PORT)
-console.log(process.env.DB_HOST)
+
 mongoose.set("strictQuery", true)
   
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
     app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
