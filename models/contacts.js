493bc183c3140349ec498adf059bf1a58e314ece
Myroslav Semeniuk <97482927+myrsem@users.noreply.github.com>
2022-12-27 15:25:01 +0200

    finish task

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,67 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const {nanoid} = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = async(contacts) => await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+const getAll = async () => {
+    const data = await fs.readFile(contactsPath);
 
-const addContact = async (body) => {}
+    return JSON.parse(data);
+}
 
-const updateContact = async (contactId, body) => {}
+const getById = async(id) => {
+    const contacts = await getAll();
+    const result = contacts.find(item => item.id === id);
+
+    return result || null;
+}
+
+const add = async({name, email, phone}) => {
+    const contacts = await getAll();
+    const newContact = {
+        id: nanoid(),
+        name, 
+        email,
+        phone,
+    }
+
+    contacts.push(newContact);
+    await updateContacts(contacts);
+
+    return newContact;
+}
+
+const updateById = async(id, data) => {
+    const contacts = await getAll();
+    const index = contacts.findIndex(item => item.id === id);
+    if(index === -1) {
+        return null;
+    }
+
+    contacts[index] = {id, ...data};
+    await updateContacts(contacts);
+
+    return contacts[index]
+}
+
+const removeById = async(id) => {
+    const contacts = await getAll();
+    const index = contacts.findIndex(item => item.id === id);
+    if(index === -1){
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+    return result;
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    getAll,
+    getById,
+    add,
+    updateById,
+    removeById,
 }
