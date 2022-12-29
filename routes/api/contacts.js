ef19a299e9e583080650e5c15148a6819b462c39
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-12-29 21:35:57 +0200

    add bcrypt and jwt

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../../controllers/contacts");
+const { contacts: ctrl } = require("../../controllers/");
 
 router.get("/", ctrl.getAll);
 
