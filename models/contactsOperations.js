ec4c23ab3e7e159b483bbb1295c636d89bcf536c
Myroslav Sokolov <mirossoks@gmail.com>
2022-12-29 18:40:27 +0200

    add connect to database

--- models/contactsOperations.js
+++ models/contactsOperations.js
@@ -1,67 +1,92 @@
-const fs = require("fs/promises");
+// const fs = require("fs/promises");
 
 
-const path = require("path");
+// const path = require("path");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+// const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {
-  const dataString = await fs.readFile(contactsPath, "utf-8");
-  const data = JSON.parse(dataString);
-  return data;
-};
+// const listContacts = async () => {
+//   const dataString = await fs.readFile(contactsPath, "utf-8");
+//   const data = JSON.parse(dataString);
+//   return data;
+// };
 
-const getContactById = async (contactId) => {
-  const allContacts = await listContacts(contactsPath);
-  const contact = allContacts.find((contact) => contact.id === contactId);
-  if (contact === -1) {
-    return null;
-  } else {
-    return contact;
-  }
-};
+// const getContactById = async (contactId) => {
+//   const allContacts = await listContacts(contactsPath);
+//   const contact = allContacts.find((contact) => contact.id === contactId);
+//   if (contact === -1) {
+//     return null;
+//   } else {
+//     return contact;
+//   }
+// };
 
-const removeContact = async (contactId) => {
-  const allContacts = await listContacts(contactsPath);
-  const contactIndex = allContacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  if (contactIndex === -1) {
-    return null;
-  } else {
-    allContacts.splice(contactIndex, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-    return allContacts;
-  }
-};
+// const removeContact = async (contactId) => {
+//   const allContacts = await listContacts(contactsPath);
+//   const contactIndex = allContacts.findIndex(
+//     (contact) => contact.id === contactId
+//   );
+//   if (contactIndex === -1) {
+//     return null;
+//   } else {
+//     allContacts.splice(contactIndex, 1);
+//     await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+//     return allContacts;
+//   }
+// };
 
-const addContact = async (body) => {
-  const allContacts = await listContacts(contactsPath);
-  allContacts.push(body);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-  return body;
-};
+// const addContact = async (body) => {
+//   const allContacts = await listContacts(contactsPath);
+//   allContacts.push(body);
+//   await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+//   return body;
+// };
 
-const updateContact = async (contactId, body) => {
-  const allContacts = await listContacts(contactsPath);
-  const contactIndex = allContacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  if (contactIndex !== -1) {
-    allContacts[contactIndex].name = body.name;
-    allContacts[contactIndex].email = body.email;
-    allContacts[contactIndex].phone = body.phone;
-    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-    return allContacts[contactIndex];
-  } else {
-    return null;
-  }
-};
+// const updateContact = async (contactId, body) => {
+//   const allContacts = await listContacts(contactsPath);
+//   const contactIndex = allContacts.findIndex(
+//     (contact) => contact.id === contactId
+//   );
+//   if (contactIndex !== -1) {
+//     allContacts[contactIndex].name = body.name;
+//     allContacts[contactIndex].email = body.email;
+//     allContacts[contactIndex].phone = body.phone;
+//     await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+//     return allContacts[contactIndex];
+//   } else {
+//     return null;
+//   }
+// };
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
+
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  })
+
+model('', schema);
+
+const Contacts = model('contact', schema);
+
+module.exports = Contacts;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const contactsControls = require("../../controllers");
 const router = express.Router();
+// const Contacts = require('../../models/contactsOperations')
 
 const {ctrlWrapper} = require("../../middlewares")
 
@@ -14,5 +15,15 @@ router.post("/", ctrlWrapper(contactsControls.add));
 router.delete("/:contactId", ctrlWrapper(contactsControls.removeById));
 
 router.put("/:contactId", ctrlWrapper(contactsControls.updateById));
+
+// router.get('/', async (req, res, next) => {
+//     const contacts = await Contacts.find();
+//     res.json(contacts)
+// })
+
+// router.get('/', async (req, res, next) => {
+//     const contacts = await Contacts.create(req.body);
+//     res.json(contacts)
+// })
 
 module.exports = router;
