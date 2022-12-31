6e16e36544da5227617a857ae4a9d0d330b02452
Vadim luginets <pebeeo@gmail.com>
2022-12-31 18:07:34 +0200

    ready

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,64 @@
-const express = require('express')
+const { json } = require("express");
+const express = require("express");
+const db = require("/nodejs-homework-rest-api/models/contacts");
+const router = express.Router();
+const schemas = require("/nodejs-homework-rest-api/schemas/schemas");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const contacts = await db.listContacts();
+  res.json({ data: contacts });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await db.getContactById(contactId);
+  if (!contact) {
+    return res.status(404).json({ message: "not found" });
+  }
+  res.json({ data: contact });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const isValidData = schemas.post.validate(req.body);
+    if (isValidData.error) {
+      return res.status(400).json({ status: validationResult.error });
+    }
+    const newContact = req.body;
+    newContact.id = Date.now().toString();
+    await db.addContact(newContact);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await db.getContactById(contactId);
+  if (!contact) {
+    return res.status(404).json({ message: "not found" });
+  }
+  await db.removeContact(contactId);
+  res.status(200).json({ message: "contact deleted" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const isValidData = schemas.put.validate(req.body);
+    if (isValidData.error) {
+      return res.status(400).json({ status: validationResult.error });
+    }
+    const { contactId } = req.params;
+    const findContact = await db.getContactById(contactId);
+    if (findContact === null) {
+      return res.status(400).json({ msg: `Contact ${contactId} is not found` });
+    }
+    await db.updateContact(contactId, req.body);
+    res.json(req.body);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
---
+++ schemas/schemas.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+const post = Joi.object({
+  name: Joi.string().alphanum().min(3).max(20).required(),
+
+  phone: Joi.string().alphanum().min(9).max(20).required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+});
+const put = Joi.object({
+  name: Joi.string().alphanum().min(3).max(20),
+
+  phone: Joi.string().alphanum().min(9).max(20),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+});
+module.exports = {
+  post,
+  put,
+};
