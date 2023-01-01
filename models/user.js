dfa75a5e820d0d7c3bd611c6f9c44a8c29023fa1
OspanovaVita <vitaospanova@ukr.net>
2023-01-01 22:32:21 +0200

    start files

---
+++ models/user.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers')
+
+// схемa mongoose для пользователя
+const userSchema = new Schema({
+password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+}, { versionKey: false, timestamps: true })
+
+// схема бросает ошибку с нужным статусом
+userSchema.post("save", handleMongooseError);
+
+// Joi схема на регистрацию 
+const registerSchema = Joi.object({
+    name: Joi.string().trim().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    
+});
+
+// Joi схема на вход 
+const loginSchema = Joi.object({    
+    email: Joi.string().email().required(),
+    password: Joi.string().min().required(),
+    
+});
+
+const schemas = {
+    registerSchema,
+    loginSchema,
+}
+
+// создаём модель на основе mongoose схемы
+const User = model('user', userSchema)
+
+
+module.exports = { User, schemas };
