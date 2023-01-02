edb24de93b50fb83ce2c0c4989c499fdbab212a9
Yuriy Brovko <91721436+bilagava@users.noreply.github.com>
2023-01-02 17:07:39 +0200

    .

--- controllers/authorization/logIn.js
+++ controllers/authorization/logIn.js
@@ -7,12 +7,14 @@ require("dotenv").config();
 const logIn = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
     const candidate = await User.findOne({ email });
     if (!candidate || !bcrypt.compareSync(password, candidate.password)) {
       throw HttpError(401, "Email or password is wrong");
     }
 
     const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
     const token = jwt.sign(
       { id: candidate._id, email: candidate.email },
       JWT_SECRET_KEY,
