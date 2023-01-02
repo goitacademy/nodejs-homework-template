13b714476fdb7faa8f83c058670c783d5c33b9d4
Yuriy Brovko <91721436+bilagava@users.noreply.github.com>
2023-01-02 19:42:25 +0200

    owner

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,7 +3,10 @@ const { Contact } = require("../../models");
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
   try {
-    const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+    const result = await Contact.find(
+      { owner },
+      "-createdAt -updatedAt"
+    ).populate("owner", "email");
     res.json(result);
   } catch (error) {
     next(error);
