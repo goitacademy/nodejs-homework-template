b93bf51277228d7d1b1eeb99279ec64f47dc78bc
Vadim luginets <pebeeo@gmail.com>
2023-01-02 16:23:30 +0200

    callbacks removed into controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,64 +1,33 @@
 const { json } = require("express");
 const express = require("express");
-const db = require("/nodejs-homework-rest-api/models/contacts");
+const {
+  getListContacts,
+  getContactsById,
+  addNewContact,
+  deleteContactById,
+  changeContactById,
+} = require("../../controllers/controllers");
+
 const router = express.Router();
-const schemas = require("/nodejs-homework-rest-api/schemas/schemas");
 
 router.get("/", async (req, res, next) => {
-  const contacts = await db.listContacts();
-  res.json({ data: contacts });
+  getListContacts(req, res, next);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await db.getContactById(contactId);
-  if (!contact) {
-    return res.status(404).json({ message: "not found" });
-  }
-  res.json({ data: contact });
+  getContactsById(req, res, next);
 });
 
 router.post("/", async (req, res, next) => {
-  try {
-    const isValidData = schemas.post.validate(req.body);
-    if (isValidData.error) {
-      return res.status(400).json({ status: validationResult.error });
-    }
-    const newContact = req.body;
-    newContact.id = Date.now().toString();
-    await db.addContact(newContact);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
+  addNewContact(req, res, next);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await db.getContactById(contactId);
-  if (!contact) {
-    return res.status(404).json({ message: "not found" });
-  }
-  await db.removeContact(contactId);
-  res.status(200).json({ message: "contact deleted" });
+  deleteContactById(req, res, next);
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  try {
-    const isValidData = schemas.put.validate(req.body);
-    if (isValidData.error) {
-      return res.status(400).json({ status: validationResult.error });
-    }
-    const { contactId } = req.params;
-    const findContact = await db.getContactById(contactId);
-    if (findContact === null) {
-      return res.status(400).json({ msg: `Contact ${contactId} is not found` });
-    }
-    await db.updateContact(contactId, req.body);
-    res.json(req.body);
-  } catch (error) {
-    next(error);
-  }
+  changeContactById(req, res, next);
 });
 
 module.exports = router;
--- schemas/schemas.js
+++ schemas/schemas.js
@@ -2,14 +2,13 @@ const Joi = require("joi");
 const post = Joi.object({
   name: Joi.string().alphanum().min(3).max(20).required(),
 
-  phone: Joi.string().alphanum().min(9).max(20).required(),
-
   email: Joi.string()
     .email({
       minDomainSegments: 2,
       tlds: { allow: ["com", "net"] },
     })
     .required(),
+  phone: Joi.string().alphanum().min(9).max(20).required(),
 });
 const put = Joi.object({
   name: Joi.string().alphanum().min(3).max(20),
