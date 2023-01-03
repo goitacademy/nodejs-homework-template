5635a673f6d1caa77786404097e912817f6b44e0
Yuriy Brovko <91721436+bilagava@users.noreply.github.com>
2023-01-03 20:01:31 +0200

    ref code

--- controllers/authorization/avatar.js
+++ controllers/authorization/avatar.js
@@ -7,15 +7,15 @@ const avatarDir = path.join(__dirname, "../../", "public", "avatar");
 
 const avatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
-  const { _id: id } = req.user;
-  const imageName = `${id}_${originalname}`;
+  const { _id } = req.user;
+  const imageName = `${_id}_${originalname}`;
   try {
     const resultUpload = path.join(avatarDir, imageName);
     const img = await Jimp.read(`${tempUpload}`);
     img.resize(250, 250).write(tempUpload);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("public", "avatar", imageName);
-    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({ avatarURL });
   } catch (error) {
     await fs.unlink(tempUpload);
