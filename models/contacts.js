fe89337a49e590dde27328181d25c823f4e79804
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-01-03 16:35:22 +0200

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,54 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+// const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id == contactId);
+  return contact || null;
+}
 
-const addContact = async (body) => {}
+async function addContact(name, email, phone) {
+  // const id = nanoid();
+  const newProduct = { name, email, phone };
+  const contacts = await listContacts();
+  contacts.push(newProduct);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newProduct;
+}
 
-const updateContact = async (contactId, body) => {}
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIdx = contacts.findIndex((contact) => contact.id === contactId);
+  if (contactIdx === -1) {
+    return null;
+  }
+  const [removedContact] = contacts.splice(contactIdx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removedContact;
+}
+
+const updateContact = async (id, data) => {
+  const contacts = await listContacts();
+  const contactIdx = contacts.findIndex((contact) => contact.id === id);
+  if (contactIdx !== -1) {
+    return null;
+  }
+  // contacts[contactIdx].name = name;
+  // contacts[contactIdx].email = email;
+  // contacts[contactIdx].phone = phone;
+  contacts[contactIdx] = { ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return contacts[contactIdx];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +56,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,65 @@
-const express = require('express')
+const express = require("express");
+const models = require("../../models/contacts");
+const { HttpError } = require("../../models/helpers");
+const Joi = require("joi");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const contacts = await models.listContacts();
+  if (!contacts) next(HttpError(404, "Something wrong"));
+  res.json(contacts);
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await models.getContactById(contactId);
+  if (!contact) next(HttpError(404, "Contact not found"));
+  return res.json(contact);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const contactsSchema = Joi.object({
+    name: Joi.string().min(2).max(20).required(),
+    email: Joi.string()
+      .email({
+        minDomainSegments: 3,
+      })
+      .required(),
+    phone: Joi.string().min(10).max(13).required(),
+  });
+  const { error } = contactsSchema.validate(req.body);
+  if (error)
+    next(HttpError(400, "missing required name field or filled incorectly"));
+  const { name, email, phone } = req.body;
+  const newContact = await models.addContact(name, email, phone);
+  res.status(201).json(newContact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { id } = req.params;
+  await models.removeContact(id);
+  if (!id) next(HttpError(400, "Not found"));
+  res.status(200).json({ message: "contact deleted" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const contactsSchema = Joi.object({
+    name: Joi.string().min(2).max(20),
+    email: Joi.string().email({
+      minDomainSegments: 3,
+    }),
+    phone: Joi.string().min(10).max(13),
+  });
+  const { error } = contactsSchema.validate(req.body);
+  if (error) next(HttpError(400, "missing fields or filled incorrectly"));
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  const updatedContact = await models.updateContact(id, {
+    name,
+    email,
+    phone,
+  });
+  return res.status(200).json(updatedContact);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
