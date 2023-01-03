a03a9781ce54d5d103cccac11ce0d1ec16b6efec
JaRcomGitHub <jarcom@ukr.net>
2023-01-03 20:24:01 +0200

    add GET /api/contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,32 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {};
+const contactsPath = path.resolve("./models/contacts.json");
+
+async function readContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+async function writeContacts(contacts) {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, " ", 2));
+  } catch (error) {
+    console.log(error);
+    process.exit(2);
+  }
+}
+
+const listContacts = async () => {
+  const contacts = await readContacts();
+  return contacts;
+};
 
 const getContactById = async (contactId) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,29 @@
 const express = require("express");
+const { listContacts } = require("../../models/contacts");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const list = await listContacts();
+  // console.table(list);
+  res.json(list);
+  // res.json({ message: "template message1" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message2" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message3" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message4" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message5" });
 });
 
 module.exports = router;
