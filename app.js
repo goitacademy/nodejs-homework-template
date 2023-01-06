e7bad5c854afa6d5942634f8449be51675af2c30
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-01-06 20:30:05 +0200

    add mongoose

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+// const express = require("express");
+// const logger = require("morgan");
+// const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts");
+// const contactsRouter = require("./routes/api/contacts");
 
-const app = express();
+// const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+// app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
+// app.use((req, res) => {
+//   res.status(404).json({ message: "Not found" });
+// });
 
-app.use((err, req, res, next) => {
-  if (err.status)
-    res.status(err.status).json({
-      message: err.message,
-    });
+// app.use((err, req, res, next) => {
+//   if (err.status)
+//     res.status(err.status).json({
+//       message: err.message,
+//     });
 
-  res.status(500).json({ message: "Internal server error" });
-});
+//   res.status(500).json({ message: "Internal server error" });
+// });
 
-module.exports = app;
+// module.exports = app;
+
+const DB_HOST =
+  "mongodb+srv://Viktoriya:1111@cluster0.nszzwat.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("bjkhbkjh"))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
