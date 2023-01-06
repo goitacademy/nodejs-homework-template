9ed13d3410254793cce79a8f154f548ce0842c9f
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2023-01-06 21:28:31 +0200

    Minimized the avatar
    I processed the avatar with the jimp package and set the dimensions for it to 250 by 250

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const path = require('path');
+const Jimp = require('jimp');
 
 const { User } = require("../../models/user");
 
@@ -10,9 +11,20 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const FileName = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, FileName);
+  
+  await Jimp.read(tempUpload)
+  .then(avatar => {
+    return avatar.resize(250, 250).write(tempUpload);
+    // return avatar.resize(250, 250).write(resultUpload);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
   await fs.rename(tempUpload, resultUpload);
+  // await fs.unlink(tempUpload);
   const avatarURL = path.join('avatars', FileName);
-  await User.findByIdAndUpdate( _id, { avatarURL });
+  await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({
     avatarURL,
