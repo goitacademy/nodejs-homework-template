1bc9a89ae30427b2ce5a1f68973544dcfd8ba1c2
OlgaBorovik <jkz8686@gmail.com>
2023-01-07 18:05:51 +0200

    1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,48 @@
 const express = require('express')
+// const contacts = require('../../models/contacts')
+const { listContacts, getContactById, addContact, removeContact } = require('../../models/contacts')
 
-const router = express.Router()
+const contactsRouter = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.get('/', async (req, res, next) => {
+  const contacts = await listContacts()
+  console.log("contacts:", contacts)
+  res.status(200).json(contacts)
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.get('/:contactId', async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId)
+  if (contact) {
+    console.log("contact:", contact)
+    return res.status(200).json(contact)
+  }
+  return res.status(404).json({message: "Not found"})
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.post('/', async (req, res, next) => {
+  const { name, email, phone} = req.body;
+  const newContact = await addContact(body);
+  console.log("newContact:", newContact)
+  res.status(201).json(newContact);
+  
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// contactsRouter.delete('/:contactId', async (req, res, next) => {
+//   const { contactId } = req.params;
+//   if (contactId) {
+//     await removeContact(contactId)
+//     res.status(200).json({message: `contact ${contactId} deleted`})
+//     }
+//     return res.status(404).json({message: "Not found"})
+    
+    
+    
+  
+// })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// contactsRouter.put('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-module.exports = router
+module.exports = contactsRouter
