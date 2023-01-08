2d7bb6ad01a0ecb412ca1a8d18eb842d85dcc26b
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2023-01-08 18:39:32 +0200

    done

---
+++ middlewares/validate/schemas.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+const contactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+
+  phone: Joi.string().required(),
+});
+
+module.exports = {
+  contactSchema,
+};
