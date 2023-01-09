6efe93eaaf39fcf0ee30bb4969c0b952c3d2040c
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2023-01-09 22:10:21 +0200

    corrected the test

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,7 +3,6 @@ const { HttpError } = require('../helpers');
 const validateBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    console.log(error);
     if (error) {
       next(HttpError(400, error.message));
     }
