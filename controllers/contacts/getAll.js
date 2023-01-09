063a5ebc3ba00a3b224852522ff60b607d5ce40a
Maryshchyn <maryshchyn89@gmail.com>
2023-01-09 18:46:22 +0200

    v

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,10 @@ const {Contact} = require('../../models')
 
 const getAll = async (req, res) => {
     const { _id } = req.user;
-    const contacts = await Contact.find({owner: _id}).populate("owner", "_id email name phone");
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+    
+    const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
         res.json({
             status: "success",
             code: 200,
