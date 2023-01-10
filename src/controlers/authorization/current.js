153401f84489bc10021309dac6710498e4afe480
VladaBoiko <bermezha@gmail.com>
2023-01-10 16:26:36 +0200

    push

--- src/controlers/authorization/current.js
+++ src/controlers/authorization/current.js
@@ -1,27 +1,12 @@
 const { current } = require("../../services/auth");
-const jwt = require("jsonwebtoken");
 const { NotAuthorizedError } = require("../../helpers/errors");
 const currentController = async (req, res, next) => {
-  const [, token] = req.headers.authorization.split(" ");
-  if (!token) {
-    const err = new NotAuthorizedError("Give me a token, mazafaka!!!");
-    return err;
-  }
+  const { _id } = req.user;
   try {
-    const data = jwt.decode(token, process.env.JWT_SECRET);
-    console.log(data._id);
-    const userId = data._id;
-    const user = await current(userId);
-    if (!user || user.token !== token) {
-      const err = new NotAuthorizedError("Give me a token, mazafaka!!!");
-      return err;
-    }
-    req.user = user;
-
+    const user = await current(_id);
     res.status(200).json({
       user,
     });
-    // next();
   } catch (error) {
     const err = new NotAuthorizedError(
       "Will you give me a correct token, mazafaka&&&"
--- src/routes/api/authRouter.js
+++ src/routes/api/authRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controlers = require("../../controlers/authorization/index");
+const userMiddlevare = require("../../middlewares/userMiddlevare");
 
 router.post("/register", async (req, res, next) => {
   controlers.registrationController(req, res, next);
@@ -11,7 +12,7 @@ router.get("/login", async (req, res, next) => {
 router.post("/logout", async (req, res, next) => {
   controlers.logoutController(req, res, next);
 });
-router.get("/current", async (req, res, next) => {
+router.get("/current", userMiddlevare, async (req, res, next) => {
   controlers.currentController(req, res, next);
 });
 module.exports = router;
