dd642f21db03698a1b9fd514314e95ba1ee51d86
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-01-10 12:35:49 +0200

    done

--- middlewares/ctrlWrapper.js
+++ middlewares/ctrlWrapper.js
@@ -9,10 +9,11 @@ const ctrlWrapper = (ctrl) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  if (err.status)
-    res.status(err.status).json({
+  if (err.status) {
+    return res.status(err.status).json({
       message: err.message,
     });
+  }
 
   res.status(500).json({ message: "Internal server error" });
 };
