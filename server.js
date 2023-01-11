9dfb30b4f2237d8ae0cbfedc070e57d8082efc54
JaRcomGitHub <jarcom@ukr.net>
2023-01-11 19:57:29 +0200

    start

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require("./app");
+// const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose.set("strictQuery", false);
+dotenv.config();
+
+const { HOST_URI } = process.env;
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI);
+    console.log("Connected to mongodb!");
+  } catch (error) {
+    console.error("Error while connecting to mongodb", error.message);
+    process.exit(3);
+  }
+}
+
+main();
