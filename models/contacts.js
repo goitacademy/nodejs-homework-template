c71784f46a1a37975517413bece7cfa27a6d8214
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2023-01-12 01:53:40 +0200

    mongodb connected

--- models/contacts.js
+++ models/contacts.js
@@ -1,72 +1,31 @@
-const fs = require("fs/promises");
-const path = require("path");
-const contactsPath = path.resolve(__dirname, "./contacts.json");
-const { nanoid } = require("nanoid");
+const { Contacts } = require("./db-connection");
 
 const listContacts = async () => {
-  const contacts = await fs.readFile(contactsPath);
-  return JSON.parse(contacts);
+  const contacts = await Contacts.find({});
+  return contacts;
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await fs.readFile(contactsPath);
-  const contact = JSON.parse(contacts).find(
-    (contact) => contact.id === contactId
-  );
-
-  if (contact) {
-    return contact;
-  } else {
-    return false;
-  }
+  const contact = await Contacts.findById(contactId);
+  return contact;
 };
 
 const removeContact = async (contactId) => {
-  const contactsRaw = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(contactsRaw);
-  const updatedContacts = contacts.filter(
-    (contact) => contact.id !== contactId
-  );
-
-  if (contacts.length === updatedContacts.length) {
-    return false;
-  } else {
-    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
-    return true;
-  }
+  const removedContact = await Contacts.findByIdAndRemove(contactId);
+  return removedContact;
 };
 
 const addContact = async (body) => {
-  const id = nanoid();
-  const newContact = { id, ...body };
-
-  const contactsRaw = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(contactsRaw);
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-
+  const newContact = await Contacts.create(body);
   return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  const contactsRaw = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(contactsRaw);
-
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  const contact = contacts.find((contact) => contact.id === contactId);
-  if (body.name) {
-    contact.name = body.name;
-  }
-  if (body.email) {
-    contact.email = body.email;
-  }
-  if (body.phone) {
-    contact.phone = body.phone;
-  }
-  contacts.splice(index, 1, contact);
-
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-
+  const contact = await Contacts.findByIdAndUpdate(
+    contactId,
+    { ...body },
+    { new: true }
+  );
   return contact;
 };
 
---
+++ models/db-connection.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.set("strictQuery", false);
+
+const { HOST_URI } = process.env;
+
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI);
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Error while connecting to mongodb", error.message);
+    process.exit(1);
+  }
+}
+
+main();
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+const Contacts = mongoose.model("Contacts", schema);
+
+module.exports = {
+  Contacts,
+};
