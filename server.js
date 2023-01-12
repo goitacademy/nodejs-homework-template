220c292437dfb230f2065cbca72b62c34b19d2d1
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-01-12 21:54:36 +0100

    how 3 done added space

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ async function main() {
       console.log('Server running. Use our API on port: 3000');
     });
   } catch (error) {
-    console.error('main failed', error.message);
+    console.error('main failed', error.message);  
   }
 }
+
 
 main();
