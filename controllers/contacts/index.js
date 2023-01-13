da31394fe94b449612d890f88f5936833e4a6032
Valeriia Masiuk <valmas1997@gmail.com>
2023-01-13 22:39:40 +0200

    updates index.js

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -3,11 +3,13 @@ const getContactById = require("./getContactById");
 const addContact = require("./addContact");
 const updateContact = require("./updateContact");
 const removeContact = require("./removeContact");
+const updateFavoriteStatus = require("./updateFavoriteStatus");
 
 module.exports = {
     getAllContacts,
     getContactById,
     addContact,
     updateContact,
-    removeContact
+    removeContact,
+    updateFavoriteStatus
 }
