cc12cc7ae6f6cb4eaaba74a9be380bf26dd01f29
Oleksii Bykovskyi <bikovskij@ukr.net>
2023-01-13 22:09:14 +0200

    add task 3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,112 +1,25 @@
 const express = require("express");
-const Joi = require("joi");
+const {
+  addContactSchema,
+  changeContactSchema,
+  changeContactStatusSchema,
+} = require("./middleware/schemes/validationschemes");
+
+const { validation } = require("./middleware/validationBody");
 
 const router = express.Router();
 
-const contactsOperations = require("../../models/contacts");
+router.get("/");
 
-const schema = Joi.object({
-  name: Joi.string().min(3).max(40).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().min(5).max(20).required(),
-});
+router.get("/:contactId");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await contactsOperations.listContacts();
-    res.json({
-      status: "success",
-      code: 200,
-      data: { contacts },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validation(addContactSchema));
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsOperations.getContactById(contactId);
-    if (!contact) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: { contact },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId");
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { name, email, phone } = req.body;
-    const { error } = schema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.message });
-    }
-    const contact = await contactsOperations.addContact(name, email, phone);
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      data: { contact },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:contactId", validation(changeContactSchema));
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsOperations.removeContact(contactId);
-    if (!contact) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      message: "contact deleted",
-      data: { contact },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { name, email, phone } = req.body;
-    const { error } = schema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-    const contact = await contactsOperations.updateContact(
-      contactId,
-      name,
-      email,
-      phone
-    );
-    if (!contact) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        contact,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch("/:contactId/favorite", validation(changeContactStatusSchema));
 
 module.exports = router;
+// f9H9gGszGKpK
---
+++ routes/api/middleware/schemes/validationschemes.js
@@ -0,0 +1,32 @@
+const Joi = require("joi");
+
+const addContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().min(10),
+  favorite: Joi.boolean(),
+});
+
+const changeContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string().min(10).max(14),
+});
+
+const changeContactStatusSchema = Joi.object({
+  favorite: Joi.boolean(),
+});
+
+module.exports = {
+  addContactSchema,
+  changeContactSchema,
+  changeContactStatusSchema,
+};
