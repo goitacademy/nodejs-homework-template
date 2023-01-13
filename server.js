9b38cdfb545237fc85c19d65bc56fdf1f68f7a0b
olynka <98587532+olynka@users.noreply.github.com>
2023-01-13 22:01:40 +0200

    start

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./app')
+const mongoose = require('mongoose');
+const { required } = require('joi');
+const{Db_HOST}=required("./config")
+mongoose.connect(Db_HOST)
+  .then(() =>
+    app.listen(3000, () => console.log(`Database connection successful on PORT :3000`))
+  )
+.catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
