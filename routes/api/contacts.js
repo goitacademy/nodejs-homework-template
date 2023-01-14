6e2fcb34f64e67690c4bc2065f02d07e30fbd38c
Monika Marciniak <monixxa90@gmail.com>
2023-01-14 12:18:53 +0100

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const { contactValidate } = require("./../../utils/validator");
+const express = require("express");
+const { contactValidate } = require("./../../utils/validator.js");
 const {
   listContacts,
   getContactById,
@@ -8,23 +8,23 @@ const {
   generateNewContact,
   getUpdatedContact,
   updateContact,
-} = require("./../../models/contacts.json");
+} = require("./../../models/contacts.js");
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const contacts = await listContacts();
   res.status(200).json(contacts);
 });
 
-router.get('/:contactId', async (req, res, next) => {
+router.get("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
   if (!contact) return res.status(404).json({ message: "Not found" });
   if (contact) res.status(200).json(contact);
 });
 
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const { name, email, phone } = req.body;
   const { error } = contactValidate(req.body);
   if (error) return res.status(400).json({ message: error.details[0].message });
@@ -35,27 +35,27 @@ router.post('/', async (req, res, next) => {
   res.status(201).send(newContact);
 });
 
-router.delete('/:contactId', async (req, res, next) => {
+router.delete("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
   if (!contact) return res.status(404).json({ message: "Not found" });
   if (contact) {
     await removeContact(contactId);
-    res.status(200).json({ message: "Contact deleted" });
+    res.status(200).json({ message: "contact deleted" });
   }
 });
 
-router.put('/:contactId', async (req, res, next) => {
+router.put("/:contactId", async (req, res, next) => {
   const { name, email, phone } = req.body;
   const { contactId } = req.params;
   const { error } = contactValidate(req.body);
   if (error) return res.status(400).json({ message: error.details[0].message });
   if (!name && !email && !phone)
-    return res.status(400).json({ message: "Missing fields" });
+    return res.status(400).json({ message: "missing fields" });
   const contact = await getContactById(contactId);
   if (!contact) return res.status(404).json({ message: "Not found" });
   if (contact) {
-    const updateContact = await getUpdatedContact(contact, req.body);
+    const updatedContact = await getUpdatedContact(contact, req.body);
     await updateContact(contactId, req.body);
     res.status(200).send(updatedContact);
   }
