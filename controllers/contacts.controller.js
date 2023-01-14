4778e0a4fa20580bb19eb20374dbea6a7593c44f
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2023-01-14 21:49:42 +0000

    fix bugs

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,8 +1,9 @@
-const { HttpError } = require("../helpers/index");
+// const { HttpError } = require("../helpers/index");
+const { NotFoundContact, FailedToUpdate } = require("../helpers/index");
 const { Contact } = require("../models/contacts");
 
 async function getContacts(req, res, next) {
-  const { limit } = req.query;
+  const { limit = 0 } = req.query;
   return res.status(200).json(await Contact.find({}).limit(limit));
 }
 
@@ -10,7 +11,8 @@ async function getContact(req, res, next) {
   const { id } = req.params;
   const contact = await Contact.findById(id);
   if (!contact) {
-    return next(HttpError(404, "Contact not found"));
+    // return next(HttpError(404, "Contact not found"));
+    throw new NotFoundContact(`Contact with <${id}> not found`);
   }
   return res.status(200).json(contact);
 }
@@ -23,28 +25,38 @@ async function createContact(req, res, next) {
 
 async function deleteContact(req, res, next) {
   const { id } = req.params;
-  const contact = await Contact.findById(id);
+  const contact = await Contact.findByIdAndRemove(id);
   if (!contact) {
-    return next(HttpError(404, "Contact not found"));
+    // return next(HttpError(404, "Contact not found"));
+    throw new NotFoundContact(`Contact with <${id}> not found`);
   }
-  await Contact.findByIdAndRemove(id);
-  return res.status(200).json({ contact, message: "Contact deleted" });
+  return res
+    .status(200)
+    .json({ message: `Contact with name <${contact.name}> has been deleted` });
 }
 
 async function changeContact(req, res, next) {
   const { id } = req.params;
+  if (!req.body) {
+    throw new FailedToUpdate(`Missing field body`);
+  }
   const result = await Contact.findByIdAndUpdate(id, req.body, res);
   if (!result) {
-    return next(HttpError(404, "Contact not found"));
+    // return next(HttpError(404, "Contact not found"));
+    throw new NotFoundContact(`Contact with <${id}> not found`);
   }
   return res.status(200).json(result);
 }
 
 async function updateStatusContact(req, res, next) {
   const { id } = req.params;
+  if (!req.body) {
+    throw new FailedToUpdate(`Missing field favorite`);
+  }
   const result = await Contact.findByIdAndUpdate(id, req.body, res);
   if (!result) {
-    return next(HttpError(404, "Contact not found"));
+    // return next(HttpError(404, "Contact not found"));
+    throw new NotFoundContact(`Contact with <${id}> not found`);
   }
   return res.status(200).json(result);
 }
