69416c67133d9d94ae0ff90ca47df340d5bb2d26
Alona-Liubchenko <aylin888@rambler.ru>
2023-01-14 08:29:53 +0200

    p5

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,7 +6,7 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (!token || bearer !== "Bearer") {
     next(HttpError(401));
   }
 
