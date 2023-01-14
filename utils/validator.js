4cd27e2bf5bcc95a90109408a4eb49bf443b91b6
Monika Marciniak <monixxa90@gmail.com>
2023-01-14 12:23:55 +0100

    add details

--- utils/validator.js
+++ utils/validator.js
@@ -1,13 +1,13 @@
 const joi = require("joi");
 
 const contactSchema = joi.object({
-    name: joi.string(),
-    email: joi.string().email(),
-    phone: joi.string(),
+  name: joi.string(),
+  email: joi.string().email(),
+  phone: joi.string(),
 });
 
-const validator = (schema) => {
-    return schema.validate(body, { abortEarly: false });
+const validator = (schema) => (body) => {
+  return schema.validate(body, { abortEarly: false });
 };
 
 const contactValidate = validator(contactSchema);
