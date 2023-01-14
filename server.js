46e1beb795c28f98342298a318a5d2f46087c840
olynka <98587532+olynka@users.noreply.github.com>
2023-01-14 18:54:33 +0200

    start

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require('./app')
-const mongoose = require('mongoose');
-const { required } = require('joi');
-const{Db_HOST}=required("./config")
+const mongoose = require('mongoose')
+
+const Db_HOST="mongodb+srv://Vitya:5OiKWi1pOG9oKZ0H@cluster0.srjncdi.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(Db_HOST)
+  mongoose.set(`strictQuery`,true)
   .then(() =>
     app.listen(3000, () => console.log(`Database connection successful on PORT :3000`))
   )
