ba41eb344c4853f56a5b544fccd9a5df5cb3d182
Nikita Sokolov <nsokolov0309@gmail.com>
2023-01-16 01:46:17 +0200

    add updateStatusContact

--- controller/controller.contacts.js
+++ controller/controller.contacts.js
@@ -1,73 +1,115 @@
 const schema = require("../schemas/schemas");
 const Contacts = require("../models/contacts");
+const contactStatusSchema = require("../schemas/schemas");
 
 // get all Contacts
 const getContacts = async (req, res, next) => {
-  const contacts = await Contacts.find({});
-  res.status(200).json(contacts);
+  try {
+    const { limit } = req.query;
+
+    const contacts = await Contacts.find({}).limit(limit);
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // get contact by Id
 const getContact = async (req, res, next) => {
-  const { contactId } = req.params;
+  try {
+    const { contactId } = req.params;
 
-  const contact = await Contacts.findById(contactId);
+    const contact = await Contacts.findById(contactId);
 
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
+    if (!contact) {
+      res.status(404).json({ message: "Not found" });
+      return;
+    }
+
+    return res.status(200).json(contact);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(contact);
 };
 
 // delete contact by Id
 const deleteContact = async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await Contacts.findById(contactId);
+  try {
+    const { contactId } = req.params;
+    const contact = await Contacts.findById(contactId);
 
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
+    if (!contact) {
+      res.status(404).json({ message: "Not found" });
+    }
+
+    await Contacts.findByIdAndRemove(contactId);
+
+    res.status(200).json({ message: "contact is deleted" });
+  } catch (error) {
+    next(error);
   }
-
-  await Contacts.findByIdAndRemove(contactId);
-
-  res.status(200).json({ message: "contact is deleted" });
 };
 
 // create new contact
 const createNewContact = async (req, res, next) => {
-  const newContact = await Contacts.create(req.query);
-  res.status(201).json(newContact);
+  try {
+    const newContact = await Contacts.create(req.query);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // change contact by Id
 const changeContact = async (req, res, next) => {
-  const { error } = schema.validate(req.query);
-  const { contactId } = req.params;
+  try {
+    const { error } = schema.validate(req.query);
+    const { contactId } = req.params;
 
-  // audit required fields
-  if (error) {
-    res.status(400).json({ message: error.message });
+    // audit required fields
+    if (error) {
+      res.status(400).json({ message: error.message });
+    }
+
+    // audit contact by Id
+    const contact = await Contacts.findById(contactId);
+    if (!contact) {
+      res.status(404).json({ message: "Not found" });
+    }
+
+    const updatedContact = await Contacts.findOneAndUpdate(
+      contactId,
+      req.query
+    );
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
   }
-
-  // audit contact by Id
-  const contact = await Contacts.findById(contactId);
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
-  }
-
-  const updatedContact = await Contacts.findOneAndUpdate(contactId, req.query);
-
-  res.status(200).json(updatedContact);
 };
 
 const updateStatusContact = async (req, res, next) => {
-  const { contactId } = req.params;
+  try {
+    const { error } = contactStatusSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "missing field favorite" });
+    }
 
-  // audit contact by Id
-  const contact = await Contacts.findById(contactId);
+    const { contactId } = req.params;
+    const contact = await Contacts.findById(contactId);
+    if (!contact) {
+      res.status(404).json({ message: "Not found" });
+    }
 
-  if (!contact) {
-    res.status(404).json({ message: "Not found" });
+    const updatedContact = await Contacts.findByIdAndUpdate(
+      contactId,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
   }
 };
 
--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,12 +1,7 @@
 const Joi = require("joi");
 
-// validation for req.query
-const schema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().min(9).required(),
+const contactStatusSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
-module.exports = schema;
+module.exports = contactStatusSchema;
