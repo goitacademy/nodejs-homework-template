9cfd84a2ca7cb5f6001ca0556257eb1d8a42acfc
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-16 18:37:19 +0200

    contintue task1

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,23 @@
 // const fs = require('fs/promises')
+const contacts = require('./models/contacts.json');
 
-const listContacts = async () => {}
+const listContacts = async res => {
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result: contacts,
+    },
+  });
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async contactId => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async contactId => {};
 
-const addContact = async (body) => {}
+const addContact = async body => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +25,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,48 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+// const listContacts = require('../../models/contacts');
+const router = express.Router();
+const contacts = require('../../models/contacts.json');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  // console.log(req.url);
+  // res.send('<h2>hejlly</h2>');
+  // const contacts = await listContacts.getAll();
+  // res.json(contacts);
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result: contacts,
+    },
+  });
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  console.log(req.params);
+  const { contactId } = req.params;
+  const result = contacts.find(item => item.id === contactId);
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result,
+    },
+  });
+
+  res.json({ message: 'template message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
