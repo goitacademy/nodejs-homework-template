4f9faca9e0b67c75a938926ede0cfb63933fb079
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-16 15:51:21 +0200

    Update contactsModels.js

--- models/contactsModels.js
+++ models/contactsModels.js
@@ -1,5 +1,3 @@
-const fs = require("fs").promises;
-const path = require("path");
 const { Schema, model } = require("mongoose");
 
 const contactSchema = Schema({
@@ -21,10 +19,6 @@ const contactSchema = Schema({
 
 const Contact = model("contact", contactSchema);
 
-const contactsPath = path.resolve(
-  "/Users/aleksandr/git_hub_projects/nodejs-homework-rest-api/models/contacts.json"
-);
-
 const listContacts = async () => {
   try {
     const result = await Contact.find({});
@@ -36,56 +30,38 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   try {
-    const rawData = await fs.readFile(contactsPath, "utf8");
-    const data = JSON.parse(rawData);
-    const dataById = data.filter((el) => el.id === contactId);
-    return dataById;
+    const data = await Contact.find({});
+    const result = JSON.parse(data);
+    const resultById = result.filter((el) => el.id === contactId);
+    return resultById;
   } catch (error) {
     console.error(error.message);
   }
 };
 
-const removeContact = async (contactId) => {
-  try {
-    const rawData = await fs.readFile(contactsPath, "utf8");
-    const data = JSON.parse(rawData);
-    const ifIdInList = data.find((el) => el.id === contactId);
-    if (!ifIdInList) {
-      return ifIdInList;
-    } else {
-      const dataPostRemoved = data.filter((el) => el.id !== contactId);
-      const dataWithCorrectId = dataPostRemoved.map((el, index) => {
-        el.id = (index + 1).toString();
-        return el;
-      });
-
-      const dataToWrite = JSON.stringify(dataWithCorrectId, null, 2);
-
-      fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
-        if (err) throw err;
-        console.log("Data written to file");
-      });
-
-      return dataPostRemoved;
-    }
-  } catch (error) {
-    console.error(error.message);
-  }
-};
-
-// const addContact = async (body) => {
+// const removeContact = async (contactId) => {
 //   try {
-//     const { id, name, email, phone } = body;
 //     const rawData = await fs.readFile(contactsPath, "utf8");
 //     const data = JSON.parse(rawData);
-//     data.push({ id, name, email, phone });
-//     const dataToWrite = JSON.stringify(data, null, 2);
+//     const ifIdInList = data.find((el) => el.id === contactId);
+//     if (!ifIdInList) {
+//       return ifIdInList;
+//     } else {
+//       const dataPostRemoved = data.filter((el) => el.id !== contactId);
+//       const dataWithCorrectId = dataPostRemoved.map((el, index) => {
+//         el.id = (index + 1).toString();
+//         return el;
+//       });
 
-//     fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
-//       if (err) throw err;
-//       console.log("Data written to file");
-//     });
-//     return { id, name, email, phone };
+//       const dataToWrite = JSON.stringify(dataWithCorrectId, null, 2);
+
+//       fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
+//         if (err) throw err;
+//         console.log("Data written to file");
+//       });
+
+//       return dataPostRemoved;
+//     }
 //   } catch (error) {
 //     console.error(error.message);
 //   }
@@ -103,61 +79,85 @@ const addContact = async (body) => {
 
 const updateContact = async (contactId, body) => {
   try {
-    const { name, email, phone } = body;
-    const rawData = await fs.readFile(contactsPath, "utf8");
-    const data = JSON.parse(rawData);
-    const ifIdInList = data.find((el) => el.id === contactId);
-    if (!ifIdInList) {
-      return ifIdInList;
-    }
-    data.forEach((el) => {
-      if (el.id === contactId) {
-        el.name = name;
-        el.email = email;
-        el.phone = phone;
-      }
-    });
-    const dataToWrite = JSON.stringify(data, null, 2);
-
-    fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
-      if (err) throw err;
-      console.log("Data written to file");
-    });
-    return data[contactId - 1];
+    console.log();
   } catch (error) {
     console.error(error.message);
   }
 };
 
-const updateContactElement = async (body, contactId) => {
-  const { name, email, phone } = body;
-  const rawData = await fs.readFile(contactsPath, "utf8");
-  const data = JSON.parse(rawData);
-  const ifIdInList = data.find((el) => el.id === contactId);
-  if (!ifIdInList) {
-    return ifIdInList;
+const removeContact = async (contactId) => {
+  try {
+    console.log();
+  } catch (error) {
+    console.error(error.message);
   }
-  data.forEach((el) => {
-    if (el.id === contactId) {
-      if (name) {
-        el.name = name;
-      }
-      if (email) {
-        el.email = email;
-      }
-      if (phone) {
-        el.phone = phone;
-      }
-    }
-  });
-  const dataToWrite = JSON.stringify(data, null, 2);
-
-  fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
-    if (err) throw err;
-    console.log("Data written to file");
-  });
-  return data[contactId - 1];
 };
+
+const updateContactElement = async (contactId, body) => {
+  try {
+    console.log();
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+// const updateContact = async (contactId, body) => {
+//   try {
+//     const { name, email, phone } = body;
+//     const rawData = await fs.readFile(contactsPath, "utf8");
+//     const data = JSON.parse(rawData);
+//     const ifIdInList = data.find((el) => el.id === contactId);
+//     if (!ifIdInList) {
+//       return ifIdInList;
+//     }
+//     data.forEach((el) => {
+//       if (el.id === contactId) {
+//         el.name = name;
+//         el.email = email;
+//         el.phone = phone;
+//       }
+//     });
+//     const dataToWrite = JSON.stringify(data, null, 2);
+
+//     fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
+//       if (err) throw err;
+//       console.log("Data written to file");
+//     });
+//     return data[contactId - 1];
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
+
+// const updateContactElement = async (body, contactId) => {
+//   const { name, email, phone } = body;
+//   const rawData = await fs.readFile(contactsPath, "utf8");
+//   const data = JSON.parse(rawData);
+//   const ifIdInList = data.find((el) => el.id === contactId);
+//   if (!ifIdInList) {
+//     return ifIdInList;
+//   }
+//   data.forEach((el) => {
+//     if (el.id === contactId) {
+//       if (name) {
+//         el.name = name;
+//       }
+//       if (email) {
+//         el.email = email;
+//       }
+//       if (phone) {
+//         el.phone = phone;
+//       }
+//     }
+//   });
+//   const dataToWrite = JSON.stringify(data, null, 2);
+
+//   fs.writeFile(`${contactsPath}`, dataToWrite, (err) => {
+//     if (err) throw err;
+//     console.log("Data written to file");
+//   });
+//   return data[contactId - 1];
+// };
 
 module.exports = {
   listContacts,
