fd2004463625979bc15545c08cff098a97531514
OspanovaVita <vitaospanova@ukr.net>
2023-01-17 22:38:55 +0200

    fix authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,14 +9,14 @@ const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
     if (bearer !== "Bearer") { // перевіряємо чи перше слово Bearer
-        next(HttpError(401))
+        next(HttpError(401, 'no bearer'))
     }
     // перевіримо токен на валідність
-    try {
-        const { Id } = jwt.verify(token, SECRET_KEY); // чи видавали токен і чи шифрували ми токен потрібним ключем
-        const user = await User.findById(Id) // тут первыряємо чи э такий user с потрібним id
+    try {       
+        const { id } = jwt.verify(token, SECRET_KEY); // чи видавали токен і чи шифрували ми токен потрібним ключем
+        const user = await User.findById(id) // тут первыряємо чи э такий user с потрібним id
         if (!user || !user.token || user.token !== token) { // якщо користувача немає або немає токена в базі або не токен користувача
-            next(HttpError(401))
+            next(HttpError(401, 'no token'))
         }
         req.user = user; // записуємо інформацію про того хто робить запит
         next()
