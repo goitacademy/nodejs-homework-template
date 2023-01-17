bb72c6306945cdb43020f0cc0961cbbdffb78ccc
Anton <a.chertok@akatech.com.ua>
2023-01-17 15:20:41 +0200

    delete all

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-require("dotenv").config();
-
-const PORT = process.env.API_SERVER_PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
