423d6620a611bd05d357f31f35e14c0ac0da198c
Ivan Roschin <ivan.roschin86@gmail.com>
2023-01-18 22:35:35 +0200

    add patch

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -16,24 +16,3 @@ const validation = (schema) => {
 };
 
 module.exports = validation;
-
-// const Joi = require("joi");
-
-// module.exports = {
-//   addContactValidation: (req, res, next) => {
-//     const schema = Joi.object({
-//       name: Joi.string().alphanum().min(3).max(10).required(),
-//       email: Joi.string()
-//         .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-//         .required(),
-//       phone: Joi.number().required(),
-//     });
-
-//     const validationResult = schema.validate(req.body);
-
-//     if (validationResult.error) {
-//       return res.status(400).json({
-//         status: validationResult.error.details,
-//         message: `missing required field`,
-//       });
-//     }
