04a8ace9487d1318f9d9d9f10cf14a500cf9a407
OspanovaVita <vitaospanova@ukr.net>
2023-01-18 23:15:20 +0200

    add avatar size

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const path = require('path');
+const Jimp = require('jimp');
 
 const { User } = require('../../models/user');
 const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
@@ -9,17 +10,23 @@ const updateAvatar = async(req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const { _id } = req.user;
     const filename = `${_id}_${originalname}`;
+  
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
+
+    // обработаем аватарку
+    const resizeAvatar = await Jimp.read(resultUpload);
+    resizeAvatar.resize(250, 250).writeAsync(resultUpload);
+
     // перезаписали в базе
     const avatarURL = path.join('avatars', filename);
-    await User.findByIdAndUpdate(_id, {avatarURL})
+    await User.findByIdAndUpdate(_id, { avatarURL });
 
     // отправили на фронтент
     res.json({
         avatarURL,
-    })    
-    
+    }); 
+   
 }
 
 module.exports = updateAvatar;
