ed0f3561d6c5045cf7b47d452730ca8fdc8f923f
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-19 00:45:22 +0200

    added hash pass

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,5 +1,6 @@
 const { Conflict } = require('http-errors');
 const { User } = require('../../models');
+const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
   const { subscription, email, password } = req.body;
@@ -7,7 +8,9 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`Email ${email} in use`);
   }
-  const result = await User.create({ subscription, email, password });
+
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const result = await User.create({ subscription, email, password: hashPassword });
   res.status(201).json({
     status: 'success',
     code: 201,
