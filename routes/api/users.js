698c246de2b3f37c4d4b5bdf977cbbce1ce47fc0
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-01-18 23:37:49 +0200

    added schemas for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { validation, ctrlWrapper } = require('../../middlewares');
 const { users: ctrl } = require('../../controllers');
+const { schemas } = require('../../models/user');
 
-router.post('/register', ctrlWrapper(ctrl.register));
+router.post('/register', validation(schemas), ctrlWrapper(ctrl.register));
 
 module.exports = router;
