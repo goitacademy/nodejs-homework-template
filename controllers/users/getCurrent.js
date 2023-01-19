adbb9d67cdf1119ae593f7e412ec91ff0f61284c
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-19 20:23:14 +0200

    Update getCurrent.js

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,7 +1,7 @@
 const getCurrent = async (req, res) => {
   const { email, subscription } = req.user;
   const { authorization = "" } = req.headers;
-  const [token] = authorization.split(" ");
+  const [_, token] = authorization.split(" ");
   res.json({
     status: "success",
     code: 200,
