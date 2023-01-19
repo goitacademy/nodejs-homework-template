b25dba0bdb4b9a2de257a68200dadb511e489a7e
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-19 21:27:32 +0200

    1

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,10 +6,13 @@ const { auth: ctrl } = require("../../controllers/index");
 const {
   userRegisterValidation,
   userLoginValidation,
+  auth,
 } = require("../../middlewares/index");
 
 routerAuth.post("/register", userRegisterValidation, ctrl.register);
 
 routerAuth.post("/login", userLoginValidation, ctrl.login);
+
+routerAuth.get("/logout", auth, ctrl.logout);
 
 module.exports = routerAuth;
