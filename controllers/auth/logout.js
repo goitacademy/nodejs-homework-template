ff93c3d2f2f56dd4b07ca556fc29f7e455426d32
Vialii Hordiichuk <vgordiychuk5@gmail.com>
2023-01-19 09:25:41 +0200

    Update logout.js

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,7 +3,6 @@ const { User } = require("../../models/user");
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: null });
-  // res.status(204).json();
   res.json({
     status: "success",
     code: 204,
