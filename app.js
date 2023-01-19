ebee11c2fcd9284bd36532dd1463e507c438f99e
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-19 12:29:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/index");
+const authRouter = require("./routes/api/index");
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-
+app.use("/api/auth", authRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
