e303131cfef804a0ba7f3a3f8b78fb0ee56f1b5e
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2023-01-19 22:56:46 +0000

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -4,13 +4,7 @@ require("dotenv").config()
 const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-// 
-
-const sendEmail = async (data) => {
-    const email = { ...data, from: "roman_kozhushko@ukr.net" };
-    await sgMail.send(email);
-    return true;
-}
+/]}
 
 module.exports = sendEmail;
 
