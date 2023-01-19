901f3cecc41bd610574f42df9c6fc29a04786b08
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2023-01-19 16:48:16 +0200

    Create mongoose model for user

---
+++ users/schema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const UserSchema = mongoose.model("user", schema);
+
+module.exports = UserSchema;
