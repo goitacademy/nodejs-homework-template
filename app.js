a8a7474e6bf77d21dac4ce6ec1ee7030a7ef2145
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-19 12:19:31 +0200

    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/index");
-const authRouter = require("./routes/api/index");
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -12,7 +12,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
