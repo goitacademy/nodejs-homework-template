387c72521e4aec17aa25bf1c92d8d76065ea7a8f
JaRcomGitHub <jarcom@ukr.net>
2023-01-21 13:42:23 +0200

    add ?page&limit&favorite

--- src/controllers/contacts/getAllContacts.js
+++ src/controllers/contacts/getAllContacts.js
@@ -2,12 +2,20 @@ const { Contact } = require("../../schemas/contact");
 
 async function getAllContacts(req, res, next) {
   const { page = 1, limit = 20, favorite = null } = req.query;
-  if (favorite) {
-    console.log(page, limit, favorite);
+
+  let skip = 0;
+  if (page > 0 && limit > 0) {
+    skip = (page - 1) * limit;
   }
-  const contacts = await Contact.find({ owner: req.user.id })
+  const searchParam =
+    favorite === "true" || favorite === "false"
+      ? { owner: req.user.id, favorite: favorite }
+      : { owner: req.user.id };
+
+  const contacts = await Contact.find(searchParam)
     .select({ __v: 0, owner: 0 })
-    .limit(limit);
+    .skip(skip)
+    .limit(limit > 0 ? limit : 20);
   return res.json(contacts);
 }
 
