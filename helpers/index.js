fbd15b86b45ec7fb01e46dbaa4d1072648148854
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-01-21 14:40:16 +0200

    Added helper classs for FS operations

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const httpError = require("./httpErrorsHandler");
 const asyncMiddlewareWrapper = require("./asyncMiddlewareWrapper");
+const FSOperationsHelper = require("./FSOperationsHelper");
 
 module.exports = {
   httpError,
   asyncMiddlewareWrapper,
+  FSOperationsHelper,
 };
