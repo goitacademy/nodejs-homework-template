5526183a1fe627204f9c1b35933d7b090b4f278a
Nick Leso <nickleso.work@gmail.com>
2023-01-21 17:10:30 +0200

    Add controls

--- servises/contactsService.js
+++ servises/contactsService.js
@@ -1,34 +1,49 @@
 const { Contact } = require("../models/contactModels");
 
 const getContacts = async () => {
-  const contacts = await Contact.find({});
+  const contacts = await Contact.find();
   return contacts;
 };
 
-const getContactById = async (id) => {
-  const contact = await Contact.findOne({ _id: id });
-  console.log(contact);
+const getContactById = async (contactId) => {
+  const contact = await Contact.findById(contactId);
+  if (!contact) {
+    return console.log("-----------------NOOOO");
+  }
   return contact;
 };
 
-const addContact = async ({ name, email, phone, favorite = false }) => {
+const addContact = async ({ name, email, phone, favorite }) => {
   const contact = new Contact({ name, email, phone, favorite });
-  console.log(contact);
   await contact.save();
+  return contact;
 };
 
-// const updateContact = (id, fields) => {
-//   return Contact.findByIdAndUpdate({ _id: id }, fields, { new: true });
-// };
+const updateContact = async (contactId, { name, email, phone, favorite }) => {
+  const contact = await Contact.findByIdAndUpdate(
+    { _id: contactId },
+    { $set: { name, email, phone, favorite } }
+  );
+  return contact;
+};
 
-// const removeContact = (id) => {
-//   return Contact.findByIdAndRemove({ _id: id });
-// };
+const updateContactStatus = async (contactId, fields) => {
+  const contact = await Contact.findByIdAndUpdate({ _id: contactId }, fields, {
+    new: true,
+  });
+  return contact;
+};
+
+const removeContact = async (contactId) => {
+  const contact = await Contact.findByIdAndRemove({ _id: contactId });
+  return contact;
+};
 
 module.exports = {
   getContacts,
   getContactById,
   addContact,
-  //   changePostById,
-  //   deletePostById,
+  updateContact,
+  updateContactStatus,
+  removeContact,
 };
