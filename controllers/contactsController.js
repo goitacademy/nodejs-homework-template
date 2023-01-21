9d09445c1dae43f96a9f14916c8f6c76a3387ecd
Alex Dolhov <alexdolhov@gmail.com>
2023-01-21 20:23:25 +0200

    adds pagination & filtering by favorite
    - to use pagination & filter send { page, limit, fovorite } = req.body

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -4,7 +4,16 @@ const { HttpError, ctrlWrapper } = require('../helpers');
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt');
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find(
+    favorite ? { owner, favorite } : { owner },
+    '-createdAt -updatedAt',
+    {
+      skip,
+      limit,
+    }
+  ).populate('owner', 'email subscription');
   res.json(result);
 };
 
