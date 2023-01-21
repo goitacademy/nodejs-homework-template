663ab5ba3a46b902061f6a5cae1fc5e79b245555
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-21 17:57:11 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,53 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const { v4 } = require("uuid");
+const path = require("path");
+const contactsPath = path.resolve("./models/contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  const [contactById] = parsedData.filter((item) => item.id === contactId);
+  return contactById;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  const dataAfterRemove = parsedData.filter((item) => item.id !== contactId);
+  if (dataAfterRemove.length === parsedData.length) {
+    return false;
+  }
+  fs.writeFile(contactsPath, JSON.stringify(dataAfterRemove));
+  return true;
+};
 
-const addContact = async (body) => {}
+const addContact = async (name, email, phone) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
 
-const updateContact = async (contactId, body) => {}
+  const newContact = { name, email, phone, id: v4() };
+  parsedData.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(parsedData));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const parsedData = JSON.parse(data);
+  const contactIndex = parsedData.findIndex((item) => item.id === contactId);
+  const contactById = parsedData[contactIndex];
+  if (contactIndex === -1) {
+    return;
+  }
+  Object.assign(parsedData[contactIndex], body);
+  fs.writeFile(contactsPath, JSON.stringify(parsedData));
+  return contactById;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +55,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
