a2cfb4d6d4fb46a46d1428d999ba6ccb71bf7db1
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-21 17:56:47 +0200

    Create validation.js

---
+++ schemas/validation.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+
+module.exports = {
+  contactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .required(),
+      phone: Joi.number().min(7).required(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error });
+    }
+
+    next();
+  },
+};
