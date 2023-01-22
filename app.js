f59a764a9889a8477e2d474b023e7a74cfa8d758
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-01-22 14:46:04 +0200

    Removed leftovers - error handler was subtitute by new one

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
-
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
