23aedd5ccf714da500ffdc4aeba8177fb97cebe7
andrii-pozniak <andriipozniak@gmail.com>
2023-01-22 14:00:12 +0200

    create new branch

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Ek-2rFr7qy3ASB
 
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const contactsRouter = require('./routes/api/contacts')
 
-const app = express()
+const DB_HOST = 'mongodb+srv://Andrii:Ek-2rFr7qy3ASB@cluster0.mmeks8d.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+
+const app = express();
+
+mongoose.connect(DB_HOST)
+.then( () => console.log('Database connect success'))
+.catch( error => console.error(error.massage)) 
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
