58477be7929901e958672c4a44bb1cac45b2f078
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-01-22 16:27:29 +0200

    Added validation schemas for requests body

---
+++ schemas/contacts.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+
+const addContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+const updateContactSchema = Joi.object({
+  name: Joi.string().optional(),
+  email: Joi.string().optional(),
+  phone: Joi.string().optional(),
+}).or("name", "email", "phone");
+
+module.exports = {
+  addContactSchema,
+  updateContactSchema,
+};
