83aa888e3004278e79bacb9a6c5926e2357d2797
Andrey5353 <andreyae0117ab@gmail.com>
2023-01-24 01:24:43 +0200

    hw02-express

--- models/contacts.js
+++ models/contacts.js
@@ -1,32 +1,153 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async (req, res) => {
+const listContacts = async (req, res, next) => {
   try {
     const contacts = await fs.readFile(contactPath, "utf-8");
     const parsedContacts = JSON.parse(contacts);
 
     if (!parsedContacts.length) {
-      console.log("no contacts");
-      return res.status(404).json({ message: "no contacts found", code: 404 });
+      return res.status(404).json({ message: "no contacts", code: 404 });
     }
 
     res.json({ message: "list of contacts", code: 200, parsedContacts });
     console.table(parsedContacts);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
-const getContactById = async (contactId) => {};
+const getContactById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contacts = await fs.readFile(contactPath, "utf-8");
+    const parsedContacts = JSON.parse(contacts);
+    const contactById = parsedContacts.filter(
+      (contact) => contact.id === contactId
+    );
 
-const removeContact = async (contactId) => {};
+    if (!contactById.length) {
+      return res.status(404).json({
+        message: `id ${contactId} not found`,
+        code: 404,
+      });
+    }
 
-const addContact = async (body) => {};
+    res.json({
+      message: `contacn by id ${contactId}`,
+      code: 200,
+      contactById,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const updateContact = async (contactId, body) => {};
+const removeContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contacts = await fs.readFile(contactPath, "utf-8");
+    const parsedContacts = JSON.parse(contacts);
+    const contactById = parsedContacts.filter(
+      (contact) => contact.id === contactId
+    );
+
+    if (!contactById.length) {
+      return res.status(404).json({
+        message: `id ${contactId} not found`,
+        code: 404,
+      });
+    }
+
+    const contactsListAfterRemove = parsedContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+
+    res.status(200).json({
+      message: `contact by id ${contactId} deleted`,
+      code: 200,
+      contactsListAfterRemove,
+    });
+
+    await fs.writeFile(
+      contactPath,
+      JSON.stringify(contactsListAfterRemove),
+      "utf-8"
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addContact = async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const contacts = await fs.readFile(contactPath, "utf-8");
+    const parsedContacts = JSON.parse(contacts);
+
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+
+    parsedContacts.push(newContact);
+    await fs.writeFile(contactPath, JSON.stringify(parsedContacts), "utf-8");
+
+    res.status(201).json({
+      message: "contact created",
+      code: 201,
+      newContact,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { name, email, phone } = req.body;
+
+    const contacts = await fs.readFile(contactPath, "utf-8");
+    const parsedContacts = JSON.parse(contacts);
+
+    const contactById = parsedContacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (!contactById) {
+      console.log(`no contacts by id: '${contactId}' found`);
+
+      return res.status(404).json({
+        message: `id ${contactId} not found`,
+        code: 404,
+      });
+    }
+
+    parsedContacts.forEach((contact) => {
+      if (contact.id === contactId) {
+        contact.name = name;
+        contact.email = email;
+        contact.phone = phone;
+
+        res.status(200).json({
+          message: `updated contact by id: '${contactId}' `,
+          code: 200,
+          contact,
+        });
+      }
+    });
+
+    await fs.writeFile(contactPath, JSON.stringify(parsedContacts), "utf-8");
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   listContacts,
