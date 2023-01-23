8d6df907b5c0ba9a70a041130cc0287e7d0a8047
Antonyly <antonlyzun@gmail.com>
2023-01-23 10:05:18 +0200

    +

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
-const handleMongooseError = require("./HandleMongooseError")
+const handleMongooseError = require("./HandleMongooseError");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
     HttpError,
     ctrlWrapper,
     handleMongooseError,
+    sendEmail,
 };
