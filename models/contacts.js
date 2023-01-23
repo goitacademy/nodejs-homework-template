758f4765aade2b357298176f5facc46e372d054b
Andrey5353 <andreyae0117ab@gmail.com>
2023-01-23 03:53:51 +0200

    hw02-express

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,32 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async (req, res) => {
+  try {
+    const contacts = await fs.readFile(contactPath, "utf-8");
+    const parsedContacts = JSON.parse(contacts);
 
-const removeContact = async (contactId) => {}
+    if (!parsedContacts.length) {
+      console.log("no contacts");
+      return res.status(404).json({ message: "no contacts found", code: 404 });
+    }
 
-const addContact = async (body) => {}
+    res.json({ message: "list of contacts", code: 200, parsedContacts });
+    console.table(parsedContacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {};
+
+const removeContact = async (contactId) => {};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +34,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
