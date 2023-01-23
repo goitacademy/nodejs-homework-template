499981ea20194b57863025276012d85e2eeaa303
Andrey5353 <andreyae0117ab@gmail.com>
2023-01-23 04:27:28 +0200

    hw02-express

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("Server launch failed", err);
+  }
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
