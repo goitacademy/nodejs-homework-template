4431a84bc47838341d5e140e58f2e2dffb4bca2c
OlgaBorovik <jkz8686@gmail.com>
2023-01-23 19:16:39 +0200

    03-hw

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,15 +1,18 @@
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../models/contacts");
+const { Contact } = require("../models/contacts")
+
+
+// const {
+//   listContacts,
+//   getContactById,
+//   addContact,
+//   removeContact,
+//   updateContact,
+// } = require("../models/contacts");
 
 
 async function getContacts(req, res, next) {
   try {
-    const contacts = await listContacts();
+    const contacts = await Contact.find();
     console.log("contacts:", contacts);
     res.status(200).json(contacts);
   } catch (error) {
@@ -20,7 +23,7 @@ async function getContacts(req, res, next) {
 async function getContact(req, res, next) {
   try {
     const { contactId } = req.params;
-  const contact = await getContactById(contactId);
+  const contact = await Contact.findById(contactId );
   if (contact) {
     console.log("contact:", contact);
     return res.status(200).json(contact);
@@ -36,7 +39,7 @@ async function getContact(req, res, next) {
 async function createContact(req, res, next) {
   try {
     const { name, email, phone } = req.body;
-  const newContact = await addContact({ name, email, phone });
+  const newContact = await Contact.create({ name, email, phone });
   console.log("newContact:", newContact);
   return res.status(201).json(newContact);
   }
@@ -48,11 +51,11 @@ async function createContact(req, res, next) {
 async function deleteContact(req, res, next) {
   try {
     const { contactId } = req.params;
-  const contact = await getContactById(contactId);
+  const contact = await Contact.findById(contactId);
   if (!contact) {
     return res.status(404).json({ message: "Not found" });
   }
-  await removeContact(contactId);
+  await Contact.findByIdAndRemove(contactId);
   return res.status(200).json({ message: `contact ${contactId} deleted` });
   }
   catch (error) {
@@ -64,7 +67,7 @@ async function changeContact(req, res, next) {
   try {
     const { contactId } = req.params;
   const { name, email, phone } = req.body;
-  const updatedContact = await updateContact(contactId, { name, email, phone });
+  const updatedContact = await Contact.findByIdAndUpdate(contactId, { name, email, phone });
   if (!updatedContact) {
     return res.status(404).json({ message: "Not found" }); 
   }
--- models/contacts.js
+++ models/contacts.js
@@ -1,59 +1,89 @@
-const fs = require("fs/promises");
-const path = require("path");
-const { nanoid } = require("nanoid");
+const mongoose = require("mongoose")
 
-const contactsPath = path.resolve(__dirname, "contacts.json");
+const schema = mongoose.Schema(
+      {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+      }, {
+        versionKey: false,
+        timestamps: true,
+  }
+    )
 
-async function readContacts() {
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  return contacts;
-}
-
-async function writeContacts(contacts) {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-}
-
-const listContacts = async () => {
-  const contacts = await readContacts();
-  return contacts;
-};
-
-const getContactById = async (contactId) => {
-  const contacts = await readContacts();
-  const contact = contacts.find((item) => item.id === contactId);
-  return contact || null;
-};
-
-const removeContact = async (contactId) => {
-  const contacts = await readContacts();
-  const updatedContacts = contacts.filter((item) => item.id !== contactId);
-  await writeContacts(updatedContacts);
-};
-
-const addContact = async (body) => {
-  const id = nanoid(3);
-  const contacts = await readContacts();
-  const newContact = { id, ...body };
-  contacts.push(newContact);
-  await writeContacts(contacts);
-  return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-  const contacts = await readContacts();
-  const [contact] = contacts.filter((item) => item.id === contactId);
-  contact.name = body.name;
-  contact.email = body.email;
-  contact.phone = body.phone;
-  await writeContacts(contacts);
-  return contact;
-};
-
+    const Contact = mongoose.model("contact", schema)
+    
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+     Contact,
+   }
+
+// const fs = require("fs/promises");
+// const path = require("path");
+// const { nanoid } = require("nanoid");
+
+// const contactsPath = path.resolve(__dirname, "contacts.json");
+
+// async function readContacts() {
+//   const data = await fs.readFile(contactsPath);
+//   const contacts = JSON.parse(data);
+//   return contacts;
+// }
+
+// async function writeContacts(contacts) {
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+// }
+
+// const listContacts = async () => {
+//   const contacts = await readContacts();
+//   return contacts;
+// };
+
+// const getContactById = async (contactId) => {
+//   const contacts = await readContacts();
+//   const contact = contacts.find((item) => item.id === contactId);
+//   return contact || null;
+// };
+
+// const removeContact = async (contactId) => {
+//   const contacts = await readContacts();
+//   const updatedContacts = contacts.filter((item) => item.id !== contactId);
+//   await writeContacts(updatedContacts);
+// };
+
+// const addContact = async (body) => {
+//   const id = nanoid(3);
+//   const contacts = await readContacts();
+//   const newContact = { id, ...body };
+//   contacts.push(newContact);
+//   await writeContacts(contacts);
+//   return newContact;
+// };
+
+// const updateContact = async (contactId, body) => {
+//   const contacts = await readContacts();
+//   const [contact] = contacts.filter((item) => item.id === contactId);
+//   contact.name = body.name;
+//   contact.email = body.email;
+//   contact.phone = body.phone;
+//   await writeContacts(contacts);
+//   return contact;
+// };
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
