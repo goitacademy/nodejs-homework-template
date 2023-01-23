80bbaf1611f4e406770f4ee017d298450ec5c455
TatianaKYS <106411942+Dimon1989k@users.noreply.github.com>
2023-01-23 21:57:46 +0200

    done home work

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,60 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const readContacts = () => fs.readFile(contactsPath, "utf8");
+const writeContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf8");
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await readContacts();
 
-const addContact = async (body) => {}
+  return JSON.parse(contacts);
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const findedContact = contacts.find((contact) => contact.id === contactId);
+
+  return findedContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const removededContact = contacts.filter(
+    (contact) => contact.id !== contactId
+  );
+  await writeContacts(removededContact);
+  return removededContact;
+};
+
+const addContact = async (body) => {
+  const newUser = {
+    id: uuidv4(),
+    ...body,
+  };
+  const contacts = await listContacts();
+  contacts.push(newUser);
+  await writeContacts(contacts);
+
+  return newUser;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const { name, email, phone } = body;
+  contacts.forEach((element) => {
+    if (element.id === contactId) {
+      element.name = name;
+      element.email = email;
+      element.phone = phone;
+    }
+  });
+  await writeContacts(contacts);
+  return body;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +62,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
