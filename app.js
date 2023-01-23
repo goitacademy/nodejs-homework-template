74f9fe832e291478b890efc550a0a1666f5887de
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-01-23 17:10:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const contactsRouter = require('./routes/api/contacts')
+const { routerContacts } = require('./routes/api/contacts');
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// middelvwares
+app.use(cors());
+app.use(logger(formatsLogger));
+app.use(morgan("dev"));
+app.use(express.json()); // tell express to work with JSON
 
-app.use('/api/contacts', contactsRouter)
+// routes
+app.use("/api/contacts", routerContacts);
 
+app.use((req, res) => {
+  return res.status(404).json({ message: "Not found" });
+});
+
+// 404
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
+// error handling
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  if (err.status) {
+    res.status(err.status).json({
+      message: err.message,
+    });
+  }
+  res
+    .status(err.status || 500)
+    .json({ message: err.message } || { message: "Internal server error" });
+});
 
-module.exports = app
+module.exports = app;
