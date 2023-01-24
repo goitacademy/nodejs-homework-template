0c76e90d25d8e1820e18e06a0efca502cda29c83
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2023-01-24 20:42:31 +0000

    update

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 dotenv.config();
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 // mongoose.set("debug", true); // enable logging
 
 const { HOST_URI } = process.env;
-const app = require("./app");
+const app = require('./app');
 
 async function main() {
   try {
     await mongoose.connect(HOST_URI);
-    console.log("Mongodb connection successful!");
+    console.log('Mongodb connection successful!');
 
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log('Server running. Use our API on port: 3000');
     });
   } catch (error) {
-    console.error("Error:", error.message);
+    console.error('Error:', error.message);
     process.exit(1);
   }
 }
