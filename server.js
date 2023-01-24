901e11d55e2cf3c4a6f5ff6282c9fe8fd0616f0d
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-01-24 18:52:49 +0200

    change greeting message

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const app = require('./app');
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT=3000} = process.env;
 mongoose.set("strictQuery", true)
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    console.log("Server is running");
-    app.listen(3000, () => console.log("port 3000"));
+    console.log("Database connection successful");
+    app.listen(3000, () => console.log(`use port ${PORT}`));
   })
   .catch(error => {
     console.log(error.message);
