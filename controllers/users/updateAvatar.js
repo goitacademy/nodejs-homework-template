1afc213b5a21328d999f8eec3043c4fee8ccf3cc
Alina Proskurnitska <proskur1987@gmail.com>
2023-01-24 11:23:15 +0200

    add jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../model");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -10,7 +11,14 @@ const updateAvatar = async (req, res) => {
   const imageName = `${id}_${originalname}`;
   try {
     const resultUpload = path.join(avatarsDir, imageName);
-    await fs.rename(tempUload, resultUpload);
+    await Jimp.read(tempUload)
+      .then((avatar) => {
+        return avatar.resize(250, 250).quality(60).write(resultUpload);
+      })
+      .catch((error) => {
+        throw error;
+      });
+    // await fs.rename(tempUload, resultUpload);
     const avatarURL = path.join("public", "avatars", imageName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
