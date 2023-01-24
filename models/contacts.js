b061c4dbaa3aa883364d09accb23001b89d40b7a
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-01-24 17:19:34 +0200

    add validation for Contact

--- models/contacts.js
+++ models/contacts.js
@@ -4,19 +4,36 @@ const contactSchema = new Schema (  {
   name: {
     type: String,
     required: [true, 'Set name for contact'],
+    unique: true,
   },
   email: {
     type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+          validate: {
+            validator: function(email) {
+              const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/;
+              return re.test(email)
+          }, message: 'Please fill a valid email address'},
   },
   phone: {
     type: String,
     required: [true, 'Set phone number for contact'],
+    validate: {
+      validator: function(v) {
+        return /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
   },
   favorite: {
     type: Boolean,
     default: false,
   },
-});
+},
+{ versionKey: false, timestamps: true }
+);
 
 const Contact = model("contact", contactSchema);
 
