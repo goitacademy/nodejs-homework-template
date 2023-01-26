719bd4107cf8383f71f803725ac52cb80d5eb2ae
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-26 13:34:40 +0000

    Update 4 files

---
+++ models/contacts/getAll.js
@@ -0,0 +1,11 @@
+const fs = require("fs/promises");
+
+const filePath = require("./filePath");
+
+const getAll = async () => {
+  const data = await fs.readFile(filePath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+module.exports = getAll;
