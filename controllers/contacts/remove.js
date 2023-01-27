27adef758bf6564f7fddcf94bcf585596895085a
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-27 12:27:29 +0200

    err

--- controllers/contacts/remove.js
+++ controllers/contacts/remove.js
@@ -3,6 +3,11 @@ const contactsOperations = require("../../models");
 const remove = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contactsOperations.remove(contactId, req.body);
+  if (!result) {
+    const error = new Error("not found");
+    error.status = 404;
+    throw error;
+  }
   res.status(200).json({
     status: "success",
     code: 200,
--- controllers/contacts/update.js
+++ controllers/contacts/update.js
@@ -3,6 +3,11 @@ const contactsOperations = require("../../models");
 const update = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contactsOperations.update(contactId, req.body);
+  if (!result) {
+    const error = new Error("not found");
+    error.status = 404;
+    throw error;
+  }
   res.status(200).json({
     status: "success",
     code: 200,
