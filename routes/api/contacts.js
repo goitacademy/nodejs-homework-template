972a362af6cf02935b5398a24b5a7982297db396
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-27 11:30:25 +0200

    valid

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const validation = require("../../middlewares");
+const contactSchema = require("../../schemas");
+
 const { contacts: ctrl } = require("../../controllers");
+
+const validateMiddleware = validation(contactSchema);
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.add);
+router.post("/", validateMiddleware, ctrl.add);
 
 router.delete("/:contactId", ctrl.remove);
 
-router.put("/:contactId", ctrl.update);
+router.put("/:contactId", validateMiddleware, ctrl.update);
 
 module.exports = router;
