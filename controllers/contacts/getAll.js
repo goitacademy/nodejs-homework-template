2dca903dbff88abfad4114ab6c216405617ed529
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-01-28 01:41:21 +0200

    add filtration by favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,9 +2,11 @@ const {Contact} = require('../../models');
 
 const getAll = async (req, res) => {
     const {_id} = req.user;
-    const {page = 1, limit = 20} = req.query;
+    const {page = 1, limit = 20, favorite=false} = req.query;
     const skip = (page - 1) * limit;
-    const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
+    const condition = favorite ? { owner:_id, favorite } : {owner: _id};
+    const contacts = await Contact.find(condition, "-createdAt -updatedAt", {skip, limit: Number(limit)}).populate("owner", "_id email subscription");
+
     res.json({
         status: "success",
         code: 200,
