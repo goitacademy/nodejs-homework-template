871ddadcba2a673944f40162b627f811bda93759
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-27 10:14:09 +0000

    Update 4 files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+app.listen(3002, () => {
+  console.log("Server running. Use our API on port: 3002")
 })
