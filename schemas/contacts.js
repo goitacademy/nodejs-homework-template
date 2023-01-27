08afaca6bbe3ab4dc1620840e0a14aac3143cab1
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-27 12:20:44 +0200

    pattern

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,16 @@
 const Joi = require("joi");
 
 const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string()
+    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+    .required(),
+  email: Joi.string()
+    .pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
+    .required(),
+  phone: Joi.string()
+    .length(14)
+    .pattern(/^(.)+[0-9]+(.)+\s+[0-9]+(.)+[0-9]$/)
+    .required(),
 });
 
 module.exports = contactSchema;
