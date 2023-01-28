01fba44cbf2c8b3acf0837638cd7ff58448190cd
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-01-28 14:15:24 -0800

    set schema and model

--- src/controllers/contacts/addById.js
+++ src/controllers/contacts/addById.js
@@ -1,5 +1,6 @@
 // const {addContact} = require('../../models/index');
 const Contact = require("../../models/index");
+
 const addById = async (req, res) => {
   const result = await Contact.create(req.body);
   res.status(200).json({
--- src/models/contacts.js
+++ src/models/contacts.js
@@ -1,9 +1,17 @@
-const {Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
+
 const contactSchema = new Schema({
-name: String,
-email: String,
-phone: String,
-favorite: Boolean,
+  name: { type: String, required: [true, "Set name for contact"] },
+  email: { type: [String, "Set email for contact"], 
+    // match: /^(\d{3}) \d{3}-\d{2}-\d{2}$/, 
+    required: false },
+  phone: { type: [String, "Set phone for contact"], required: true },
+  favorite: { type: [Boolean, "Favorite must be true or false"], default: false },
+  //   enum  - choose from list
+  // genre:{ type: String, enum: ["fantastic", "love"], required: true}
+  //   match - specific format of number example: ISBN-13: 978-2-266-11156-0,
+  //   unique - дополнит задать настройки в indexes: create->name->field->sort any->options=create unique index
+  //   isbn:{ type: String, match: /^\d{3}-\d-\d{3}-\d{5}-\d$/, unique: true,  required: true}
 });
 
 const Contact = model("contact", contactSchema);
