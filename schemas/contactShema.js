987a709e71b01987b077b300341b31600e6ea07e
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-01-28 23:02:03 +0100

    Delete contactShema.js

--- schemas/contactShema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const addContactSchema = Joi.object({
-        name: Joi.string().min(3).required(),
-        phone: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
-    })
-
-module.exports = {
-        addContactSchema,
