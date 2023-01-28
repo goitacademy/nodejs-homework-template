96c8acf824e27c1d4841a550f943793af1a25e56
Anton <a.chertok@akatech.com.ua>
2023-01-28 13:25:00 +0200

    update one more time

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,22 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const {
+  getContacts,
+  getContact,
+  newContact,
+  putContact,
+  deleteContact,
+} = require("../../controllers/contactsController");
+const { contactValidation } = require("../../middleware/validationMiddleware");
 
-const router = express.Router()
+router.get("/", getContacts);
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", getContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", contactValidation, newContact);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", contactValidation, putContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", deleteContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
