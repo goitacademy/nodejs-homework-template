cd40cca68fe92e50a81d8be124b90b010be871b1
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-01-28 23:01:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set('strictQuery', false);
+
+// connection to Mongoose DB
+const { DB_HOST, PORT = 3006 } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(PORT, () => {
+    console.log(`Database connection successful use API on PORT: ${PORT}`)
+  }))
+    .catch(error => {
+      console.log(`Server not running. Error message: ${error.message}`);
+      process.exit(1);
+  })
+
+  
