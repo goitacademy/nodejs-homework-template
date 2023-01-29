0e950ac4edd577503fbb2671d0f6aea93ef7d9ee
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-01-29 14:34:14 +0200

    Added connection to DB fuctionality

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const app = require("./app");
+const app = require('./app');
+const connectToContactsDB = require('./services/contactsMongoDB');
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+//MAIN
+(async function () {
+  const isConnectedToDB = !(await connectToContactsDB());
+  if (!isConnectedToDB) process.exit(1);
+
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+})();
