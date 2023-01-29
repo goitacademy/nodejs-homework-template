fc1e672132375dc49619edbeb3df5a69eece9ed3
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2023-01-29 23:33:39 +0200

    hw2 done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,72 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path");
+const uniqid = require("uniqid");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data) || null;
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+    const data = await listContacts();
+    const result = data.find(
+      (contact) => contact.id === String(contactId)
+    );
+    return result;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await listContacts();
+    const result = data.filter(
+      (contact) => contact.id !== String(contactId)
+    );
+    console.log("data.length: ", data.length, "result.length: ", result.length);
+    if (data.length === result.length) {
+      return false;
+    }
+    fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
+    return true;
+  } catch (error) {
+    return error;
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try {
+    const data = await listContacts();
+    const newContact = {
+      id: uniqid(),
+      ...body,
+    };
+    const updateData = [...data, newContact];
+    fs.writeFile(contactsPath, JSON.stringify(updateData, null, 2));
+    return newContact;
+  } catch (error) {
+    return error;
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  try {
+    let data = await listContacts();
+    const index = data.findIndex(item => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const updateContact = {
+      ...data[index],
+      ...body,
+    };
+    data.splice(index, 1, updateContact);
+    fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    return updateContact;
+  } catch (error) {
+    return error;
+  }
+}
 
 module.exports = {
   listContacts,
