2e092284449c0287a1fe2697e1660cb5cda59c48
vitalik poliakov <vitalik211287@gmail.com>
2023-01-29 04:09:45 +0200

    02

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -19,7 +20,9 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  // eslint-disable-next-line no-unused-vars
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message: err.message });
 });
 
 module.exports = app;
