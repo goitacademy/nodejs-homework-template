e0400745ca0cba765205ce65331919831fd8ba84
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-01-29 15:22:26 +0200

    Namespace for joi schemas

--- models/contacts.js
+++ models/contacts.js
@@ -5,6 +5,19 @@ const { Schema } = require('mongoose');
 const { DB_HOST, NODE_ENV } = process.env;
 const isDevMode = NODE_ENV === 'development';
 
+async function connectToContactsDB() {
+  mongoose.set('strictQuery', false);
+  try {
+    await mongoose.connect(DB_HOST);
+    isDevMode && console.log(`Successfully connected to DB`);
+  } catch (error) {
+    isDevMode &&
+      console.log(`Can't connect to DB. Aborting process...\nError: ${error}`);
+    return 1;
+  }
+
+  return 0;
+}
 const contactScheme = new Schema(
   {
     name: {
@@ -25,36 +38,22 @@ const contactScheme = new Schema(
   { versionKey: false, timestamps: true }
 );
 
-const addContactSchema = joi.object({
-  name: joi.string().required(),
-  email: joi.string().required(),
-  phone: joi.string().required(),
-});
-
-const updateContactSchema = joi
-  .object({
-    name: joi.string().optional(),
-    email: joi.string().optional(),
-    phone: joi.string().optional(),
-  })
-  .or('name', 'email', 'phone');
-
-async function connectToContactsDB() {
-  mongoose.set('strictQuery', false);
-  try {
-    await mongoose.connect(DB_HOST);
-    isDevMode && console.log(`Successfully connected to DB`);
-  } catch (error) {
-    isDevMode &&
-      console.log(`Can't connect to DB. Aborting process...\nError: ${error}`);
-    return 1;
-  }
-
-  return 0;
-}
-
 const ContactModel = mongoose.model('contacts', contactScheme);
-const joiSchemas = { addContactSchema, updateContactSchema };
+const joiSchemas = {
+  addContactSchema: joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    phone: joi.string().required(),
+  }),
+
+  updateContactSchema: joi
+    .object({
+      name: joi.string().optional(),
+      email: joi.string().optional(),
+      phone: joi.string().optional(),
+    })
+    .or('name', 'email', 'phone'),
+};
 
 module.exports = {
   connectToContactsDB,
