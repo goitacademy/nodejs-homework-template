5d2f21d0078a98e44ed7a865ed0c5a7fe28d586c
Ivan Vodopianov <vis3010@gmail.com>
2023-01-29 18:28:14 +0200

    first

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app')
 
 app.listen(3777, () => {
-  console.log("Server running. Use our API on port: 3777")
+  // console.log("Server running. Use our API on port: 3777")
 })
+
+
+ 
