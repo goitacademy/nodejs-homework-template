428e0b5e2c0b6266b1d6f40077f85c71e11efb7f
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-01-30 22:42:09 +0200

    Add contacts handler fns

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,76 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const nanoid = require("nanoid");
 
-const listContacts = async () => {}
+const MOCK_DB_FILENAME = "contacts.json";
 
-const getContactById = async (contactId) => {}
+const dbPath = path.join(
+  __dirname,
+  MOCK_DB_FILENAME
+);
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(dbPath);
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(
+    (c) => c.id === contactId
+  );
+  if (!contact) return null;
+  return contact;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const toRemove = await getContactById(
+    contactId
+  );
+  if (!toRemove) return null;
+  const contacts = await listContacts();
+  await fs.writeFile(
+    dbPath,
+    JSON.stringify(
+      contacts.filter(
+        (contact) => contact.id !== contactId
+      )
+    )
+  );
+  return toRemove;
+};
+
+const addContact = async (body) => {
+  const newbie = { ...body, id: nanoid() };
+  const contacts = await listContacts();
+  await fs.writeFile(
+    dbPath,
+    JSON.stringify({
+      ...contacts,
+      ...newbie,
+    })
+  );
+  return newbie;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (index === -1) return null;
+
+  contacts[index] = {
+    ...contacts[index],
+    ...body,
+  };
+
+  await fs.writeFile(
+    dbPath,
+    JSON.stringify(contacts)
+  );
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +78,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
