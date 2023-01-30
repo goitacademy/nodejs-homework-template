fd87ef6e294652b3ef2eb973347239431675565a
h0wter <howter99@gmail.com>
2023-01-31 01:56:22 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,32 @@
 import express from "express";
+import { getContactById, listContacts } from "../../models/contacts.js";
+import { addContactSchema } from "../../schemas/contacts.js";
 
 export const contactsRouter = express.Router();
 
-contactsRouter.get("/api/contacts", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.get("/", async (req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 contactsRouter.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  try {
+    const result = await getContactById(contactId);
+    res.json(result);
+  } catch ({ status, message }) {
+    res.status(status).json({ message });
+  }
 });
 
 contactsRouter.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  console.log("body", req.body);
+  // const { error } = addContactSchema.validate(req.body);
+  // console.log(error);
 });
 
 contactsRouter.delete("/:contactId", async (req, res, next) => {
