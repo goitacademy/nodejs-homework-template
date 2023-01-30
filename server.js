2b937d7a6e8c9c4e7f5cd1f00402c114a082fa1b
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-30 14:57:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const app = require('./app')
+const app = require("./app");
+// require("dotenv").config();
+
+// npm install dotenv --save
+// PORT=3000
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log("Server running. Use our API on port: `${PORT}`");
+// });
 
 app.listen(3002, () => {
-  console.log("Server running. Use our API on port: 3002")
-})
+  console.log("Server running. Use our API on port: 3002");
+});
