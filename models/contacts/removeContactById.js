6b19fc8dcbc78c91d307f98cd2f17fcf0279e9e5
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-30 13:24:01 +0000

    Update removeContactById.js and updateContactById.js

--- models/contacts/removeContactById.js
+++ models/contacts/removeContactById.js
@@ -5,17 +5,13 @@ const getContactsList = require("./getContactsList");
 const removeContactById = async (contactId) => {
   const contactList = await getContactsList();
   const index = contactList.findIndex(({ id }) => id === contactId);
-  const removeContact = contactList[index];
-
   if (index === -1) {
     return null;
   }
-
-  contactList.splice(index, 1);
-  await fs.writeFile(filePath, JSON.stringify(contactList));
-
+  const removeContact = contactList[index];
+  const newContactList = contactList.filter((_, id) => id !== removeContact.id);
+  await fs.writeFile(filePath, JSON.stringify(newContactList));
   return removeContact;
 };
 
 module.exports = removeContactById;
-
--- models/contacts/updateContactById.js
+++ models/contacts/updateContactById.js
@@ -5,14 +5,13 @@ const getContactsList = require("./getContactsList");
 const updateContactById = async (id, body) => {
   const contactList = await getContactsList();
   const index = contactList.findIndex((item) => item.id === id);
-
   if (index === -1) {
     return null;
   }
-
-  contactList[index] = { id, ...body };
-  await fs.writeFile(filePath, JSON.stringify(contactList));
-  return contactList[index];
+  const updatedContact = contactList[index];
+  const updatedContactList = { ...contactList, ...{ id, ...body } };
+  await fs.writeFile(filePath, JSON.stringify(updatedContactList));
+  return updatedContact;
 };
 
 module.exports = updateContactById;
