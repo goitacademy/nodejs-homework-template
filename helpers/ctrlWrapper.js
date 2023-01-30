1f22834c448500eaaaff01179c0378f482f3eb5c
h0wter <howter99@gmail.com>
2023-01-30 19:56:40 +0200

    Create ctrlWrapper.js

---
+++ helpers/ctrlWrapper.js
@@ -0,0 +1,9 @@
+export const ctrlWrapper = (ctrl) => {
+  return async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
