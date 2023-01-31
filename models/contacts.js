95681929b5dfbeb225e76cfb024295cbc6cf2c3d
Makarov Yelisei <fenniz123@gmail.com>
2023-01-31 18:42:37 +0200

    summary

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,57 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path')
+const {v4} = require("uuid");
 
-const listContacts = async () => {}
+const filePath = path.join(__dirname, 'contacts.json')
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(filePath)
+    const list = JSON.parse(data)
+    return list;
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const data = await listContacts()
+  const contactById = await data.find(item => item.id === contactId)
+  return contactById
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts()
+  const idx =  contacts.findIndex(item => item.id === contactId)
+    if (idx === -1) {
+        return null
+    }
+    const contactsAfterDelete = await contacts.filter((_, index) => index !== idx)
+    await fs.writeFile(filePath, JSON.stringify(contactsAfterDelete))
+    return contacts[idx];
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+    const contacts = await listContacts()
+    const newContact = {...body, id: v4()};
+    contacts.push(newContact)
+  await fs.writeFile(filePath, JSON.stringify(contacts))
+  return newContact;
+}
+
+const updateContact = async (contactId, body) => {
+  const data = await listContacts()
+  const contactById = await data.find(item => item.id === contactId)
+  if(contactById === -1){
+        return null;
+  }
+  const updatedContacts = await data.map(contact => {
+    if (contact.id === contactId) {
+      return {...contact, ...body}
+    }
+    return contact
+  })
+
+  data[contactById] = { ...body, contactId }
+  await fs.writeFile(filePath, JSON.stringify(updatedContacts))
+  return data[contactById]
+}
 
 module.exports = {
   listContacts,
