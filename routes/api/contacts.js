d6e320d7415bdc0c5062740c1edbc2af00d7e120
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2023-01-31 22:45:11 +0300

    правка2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Home work №2 done!' })
+  res.json({ message: 'Home work №2 done!!' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
