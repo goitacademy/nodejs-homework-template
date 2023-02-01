b9a9f4dd02b396a844a992a76d5643e603865a53
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-02-01 22:30:54 +0200

    added users/signup route

--- controllers/users/signUp.js
+++ controllers/users/signUp.js
@@ -1,3 +1,19 @@
-const signUp = (req, res) => {};
+const { User } = require("../../models/user");
+const { Conflict } = require("http-errors");
+
+const signUp = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict(`${email} in use`);
+  }
+  const newUser = await User.create({ email, password });
+  res.status(201).json({
+    user: {
+      email,
+      subscription: "starter",
+    },
+  });
+};
 
 module.exports = signUp;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { validation, ctrlWrapper, isValidId } = require("../../middlewares");
+const { validation, ctrlWrapper } = require("../../middlewares");
+const { joiSchema } = require("../../models/user");
 const { users: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
-router.post("/signup", ctrlWrapper(ctrl.signUp));
+router.post("/signup", validation(joiSchema), ctrlWrapper(ctrl.signUp));
 
 module.exports = router;
