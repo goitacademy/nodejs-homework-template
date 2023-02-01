fec7840fce1dee14c00f11c088565d9f1320a25b
OlehStarko <starartds@gmail.com>
2023-02-01 19:40:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,3 +19,9 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
+
+const app = require("./app");
+
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
