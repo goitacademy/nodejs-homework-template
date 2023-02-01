7e82e883937a6865630ec7d89cb6089fc8aa98ba
kyzym <kyzym1@gmail.com>
2023-02-01 15:33:48 +0100

    add filtration by favorite

--- services/contacts/getAll.js
+++ services/contacts/getAll.js
@@ -2,13 +2,17 @@ const { Contact } = require('../../models/contact');
 
 const getAll = async (req) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+
+  const { page = 1, limit = 10, favorite } = req.query;
+
   const skip = (page - 1) * limit;
 
-  //   const favorite =
-  //     isFavorite === ('true' || 'false') ? isFavorite : '';
+  const searchParams =
+    favorite === ('true' || 'false')
+      ? { owner, favorite }
+      : { owner };
 
-  return await Contact.find({ owner }, null, {
+  return await Contact.find(searchParams, null, {
     skip,
     limit,
   }).populate('owner', 'subscription email');
--- services/users/logout.js
+++ services/users/logout.js
@@ -1,5 +1,9 @@
 const { User } = require('../../models/user');
 
 module.exports = async (id) => {
-  return User.findByIdAndUpdate(id, { token: null });
+  return User.findByIdAndUpdate(
+    id,
+    { token: null },
+    { new: true }
+  );
 };
