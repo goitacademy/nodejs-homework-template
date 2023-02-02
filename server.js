28598803ab41f3d5cfda5d2ada71cad6982f6b8a
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-02-02 12:22:33 +0000

    Update 4 files

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const { PORT = 3000, DB_HOST } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
+mongoose.set("strictQuery", false);
+
+main().catch((err) => {
+  console.error("Error while connecting to mongoDb.", err.message);
+  process.exit(1);
 });
+
+async function main() {
+   mongoose.connect(DB_HOST, () =>
+    console.log("Database connection successful")
+  );
+
+  app.listen(PORT, (err) =>
+    err
+      ? console.error("Error at server launch", err.message)
+      : console.log(`Server running. Use our API on port: ${PORT}`)
+  );
+}
