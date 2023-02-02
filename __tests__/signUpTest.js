de2591669bfd60631f8b18bfdcfc0bda0d2a9605
Vira Voronova <vera.voronova@hotmail.com>
2023-02-02 10:35:03 +0100

    test login  changed

--- __tests__/signUpTest.js
+++ __tests__/signUpTest.js
@@ -1,4 +1,3 @@
-//folder --> __tests__
 import * as dotenv from 'dotenv';
 import request from 'supertest';
 import app from '../app';
@@ -10,7 +9,7 @@ mongoose.set('strictQuery', true);
 
 const { MONGO_TEST_URL } = process.env;
 
-describe('logIn', () => {
+describe('signUp', () => {
   beforeAll(async () => {
     await mongoose.connect(MONGO_TEST_URL);
     console.log('Test database connection successful');
@@ -22,47 +21,33 @@ describe('logIn', () => {
     await mongoose.disconnect(MONGO_TEST_URL);
   });
 
-  it.skip('should logIn user with email and password', async () => {
+  it.skip('should signUp new user with email and password', async () => {
     const data = {
-      email: `login@email.com`,
+      email: `signup@email.com`,
       password: 'password',
     };
 
-    await request(app)
-      .post('/api/users/signup')
-      .send(data)
-      .set('Accept', 'application/json');
-
-    //TODO: add verify password
-
     const response = await request(app)
-      .post('/api/users/login')
+      .post('/api/users/signup')
       .send(data)
       .set('Accept', 'application/json');
 
     const { status } = response;
-    const { token, user } = response.body.data;
+    const { email, subscription } = response.body.data;
 
-    expect(status).toBe(200);
-    expect(token).toBeDefined();
-    expect(user).toBeDefined();
-    expect(user.email).toEqual(data.email);
+    expect(status).toBe(201);
+    expect(email).toEqual(data.email);
     expect(
-      (user.subscription === 'starter') |
-        (user.subscription === 'pro') |
-        (user.subscription === 'business')
+      (subscription === 'starter') |
+        (subscription === 'pro') |
+        (subscription === 'business')
     ).toBe(1);
   });
 
-  it.skip('should end with error in case of invalid credentials', async () => {
+  it.skip('should end with error "Conflict" in case of signup with same email', async () => {
     const data = {
-      email: `login2@email.com`,
+      email: `signup2@email.com`,
       password: 'password',
-    };
-
-    const invalidData = {
-      email: `login2@email.com`,
-      password: 'passwordInvalid',
     };
 
     await request(app)
@@ -71,10 +56,10 @@ describe('logIn', () => {
       .set('Accept', 'application/json');
 
     const response = await request(app)
-      .post('/api/users/login')
-      .send(invalidData)
+      .post('/api/users/signup')
+      .send(data)
       .set('Accept', 'application/json');
 
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(409);
   });
 });
