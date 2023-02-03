db2bf3365cd092d24188d5052b0f5705e9343d3f
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-02-03 14:32:02 +0200

    adds favorite filtration

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,17 +2,31 @@ const Contact = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id }, "", {
-    skip,
-    limit: Number(limit),
-  }).populate("owner", "_id email subscription");
-  res.json({
-    data: {
-      contacts,
-    },
-  });
+  if (favorite) {
+    const contacts = await Contact.find({ owner: _id, favorite }, "", {
+      skip,
+      limit: Number(limit),
+      favorite,
+    }).populate("owner", "_id email subscription");
+    res.json({
+      data: {
+        contacts,
+      },
+    });
+  } else {
+    const contacts = await Contact.find({ owner: _id }, "", {
+      skip,
+      limit: Number(limit),
+      favorite,
+    }).populate("owner", "_id email subscription");
+    res.json({
+      data: {
+        contacts,
+      },
+    });
+  }
 };
 
 module.exports = getAll;
