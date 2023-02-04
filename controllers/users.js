aa96ddbfd90cf8d44b8620d49bf60d5733ea70cc
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-02-04 22:00:12 +0200

    Temp

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,11 @@ const { httpError } = require('@root/helpers');
 const { UserModel } = require('@root/models');
 
 async function signup(req, res, next) {
-  // const contacts = await UserModel.find({});
+  const { email, password } = req.body;
+  console.log((await UserModel.find({ email })).length);
+
+  const hasUserWithEmail = !!(await UserModel.find({ email })).length;
+  console.log(hasUserWithEmail);
 
   res.status(200).json();
 }
