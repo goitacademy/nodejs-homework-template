e51a9b80e381cb21b4ef94bf2be9962f83554d24
AleksanrKotlyar <kotlyar.alek@gmail.com>
2023-02-05 18:33:34 +0200

    fixed filter for get contacts

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,17 +2,22 @@ const { Contact } = require("../../models");
 
 const listContacts = async (req, res) => {
 	const { _id } = req.user;
-	const { page = 1, limit = 20, favorite = false } = req.query;
+	const { page = 1, limit = 20, favorite } = req.query;
 	const skip = (page - 1) * limit;
 
-	const contacts = await Contact.find(
-		{ owner: _id, favorite: favorite },
-		"-createdAt -updatedAt",
-		{
-			skip,
-			limit: Number(limit),
-		}
-	).populate("owner", "id email");
+	const contacts = favorite
+		? await Contact.find(
+				{ owner: _id, favorite: favorite },
+				"-createdAt -updatedAt",
+				{
+					skip,
+					limit: Number(limit),
+				}
+		  ).populate("owner", "id email")
+		: await Contact.find({ owner: _id }, "-createdAt -updatedAt", {
+				skip,
+				limit: Number(limit),
+		  }).populate("owner", "id email");
 
 	res.json({
 		status: "success",
