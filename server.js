61ba2a7b0975976095dc41dc4c857c3d6925682a
Alexander Shabanov <dffleh1995@gmail.com>
2023-02-05 22:31:07 +0200

    first

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.set("strictQuery", false);
+
+const { HOST_URI } = process.evn;
 
 const port = 5151;
 
-app.listen(port, () => {
-  console.log(`Server running. Use our API on port: ${port}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI);
+    console.log("Database connection successful!");
+
+    app.listen(port, () => {
+      console.log(`Server running. Use our API on port: ${port}`);
+    });
+  } catch (error) {
+    console.error("Error while connecting to Mongodb", error.message);
+    process.exit(1);
+  }
+}
