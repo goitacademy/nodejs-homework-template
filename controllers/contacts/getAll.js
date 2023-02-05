7fd5b02739dde209409fedf8903e386fac1c17a0
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-02-05 21:03:26 +0200

    adds search by email

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,19 +2,9 @@ const Contact = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10, favorite, name } = req.query;
+  const { page = 1, limit = 10, favorite, name, email } = req.query;
   const skip = (page - 1) * limit;
-  if (favorite && name) {
-    const contacts = await Contact.find({ owner: _id, favorite, name }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id email subscription");
-    res.json({
-      data: {
-        contacts,
-      },
-    });
-  } else if (favorite && !name) {
+  if (favorite) {
     const contacts = await Contact.find({ owner: _id, favorite }, "", {
       skip,
       limit: Number(limit),
@@ -24,8 +14,18 @@ const getAll = async (req, res) => {
         contacts,
       },
     });
-  } else if (!favorite && name) {
+  } else if (name) {
     const contacts = await Contact.find({ owner: _id, name }, "", {
+      skip,
+      limit: Number(limit),
+    }).populate("owner", "_id email subscription");
+    res.json({
+      data: {
+        contacts,
+      },
+    });
+  } else if (email) {
+    const contacts = await Contact.find({ owner: _id, email }, "", {
       skip,
       limit: Number(limit),
     }).populate("owner", "_id email subscription");
