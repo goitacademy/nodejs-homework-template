d42fc1ab4b381a6229881626c9a52574f6672b3b
Виктор Селищев <selischev.victor.kharkov@gmail.com>
2023-02-05 21:39:37 +0200

    server DB connection

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const urlDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(urlDb, {
+  promiseLibrary: global.Promise,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(err =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
