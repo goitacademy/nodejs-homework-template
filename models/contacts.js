2017136f0e850bfa15f49e8b7a0e8196b3f41526
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-02-05 22:25:10 +0100

    create functions

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,83 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function saveContactsToFile(data) {
+	const content = JSON.stringify(data);
+	await fs.writeFile(contactsPath, content);
+}
 
-const removeContact = async (contactId) => {}
+async function getContactsFromFile() {
+	try {
+		return JSON.parse(await fs.readFile(contactsPath, "utf8"));
+	} catch (error) {
+		return console.error(error.message);
+	}
+}
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+	const data = await getContactsFromFile();
+	console.table(data);
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+	const contacts = await getContactsFromFile();
+	const filtredContacts = contacts.find((contact) => contact.id === contactId);
+	console.table(filtredContacts);
+};
+
+const removeContact = async (contactId) => {
+	const contacts = await getContactsFromFile();
+	const filtredContacts = contacts.find((contact) => contact.id !== contactId);
+	saveContactsToFile(filtredContacts);
+	console.table(filtredContacts);
+};
+
+const addContact = async (body) => {
+	const { name, email, phone } = body;
+	const contacts = await getContactsFromFile();
+	const newContact = {
+		id: Number(contacts[contacts.length - 1].id) + 1,
+		name: name,
+		email: email,
+		phone: phone,
+	};
+	contacts.push(newContact);
+	saveContactsToFile(contacts);
+	console.table(contacts);
+};
+
+const updateContact = async (contactId, body) => {
+	const { name, email, phone } = body;
+	const data = await getContactsFromFile();
+
+	const newContact = {
+		id: `${contactId}`,
+		name: name,
+		email: email,
+		phone: phone,
+	};
+
+	const updateContact = data.find((contact) => contact.id === contactId);
+
+	console.log(updateContact);
+
+	const updateContactId = data.indexOf(updateContact);
+
+	if (updateContactId === -1) {
+		return null;
+	} else {
+		data.splice(updateContactId, 1, newContact);
+		await saveContactsToFile(data);
+	}
+	return newContact;
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+	updateContact,
+};
