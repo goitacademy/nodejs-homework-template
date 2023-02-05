be9625ec6d0596c7fdec508e1f1567083344540d
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-02-05 21:03:32 +0200

    Не розумію, як зробити /current
    Допоможіть з цією проблемою

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 const signupRouter = require("./routes/api/signup");
+const usersRouter = require("./routes/api/users");
 
 const app = express();
 
@@ -16,13 +17,12 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", signupRouter);
-
+app.use("/api/users", usersRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message: err.message });
 });
---
+++ routes/api/users.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const { Unauthorized } = require("http-errors");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models/user");
+const { SECRET_KEY } = process.env;
+
+router.get("/current", async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  try {
+    const { email } = req.user;
+
+    if (bearer !== "Bearer") {
+      throw new Unauthorized("Not authorized");
+    }
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Unauthorized("Not authorized");
+    }
+    req.user = user;
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        user: {
+          email,
+        },
+      },
+    });
+    next();
+  } catch (error) {
+    if (error.message === "Invalid sugnature") {
+      error.status = 401;
+    }
+    next(error);
+  }
+});
+
+module.exports = router;
