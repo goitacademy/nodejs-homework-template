8fd06f7225ce6a275c46c0f6b1ace6af1a998a25
Rostyslav Kononenko <rastikon@gmail.com>
2023-02-05 01:31:13 +0100

    hw03_first_try

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,92 +1,141 @@
 const express = require("express");
-const Joi = require("joi");
-
-const contactSchema = Joi.object({
-  // id: Joi.string().required(),
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
 const router = express.Router();
 
-const contactsOperation = require("../../models/contacts");
+const {
+  Contact,
+  joiSchema,
+  joiSchemaUpdate,
+  joiSchemaFavorite,
+} = require("../../models/contact");
 
+// Показати всі контакти
 router.get("/", async (req, res, next) => {
   try {
-    const contacts = await contactsOperation.listContacts();
-    // res.json({ contacts });
-    res.json({ message: "template message", contacts });
+    const contacts = await Contact.find({});
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contacts,
+      },
+    });
   } catch (error) {
     next(error);
-    // res.status(500).json({ message: "Server error" });
   }
 });
 
+// Знайти контакт по id
 router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const result = await contactsOperation.getContactById(id);
-    if (!result) {
-      const error = new Error(`Contact with id=${id} not found`);
-      error.status = 404;
-      throw error;
+    const contact = await Contact.findById(id);
+    if (!contact) {
+      return null;
     }
-    res.json(result);
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contact,
+      },
+    });
   } catch (error) {
     next(error);
   }
 });
 
+// Додавання нового контакту
 router.post("/", async (req, res, next) => {
   try {
-    const { error } = contactSchema.validate(req.body);
+    const { error } = joiSchema.validate(req.body);
     if (error) {
       error.status = 400;
       res.json({ message: "missing required name field" });
       throw error;
     }
-    const result = await contactsOperation.addContact(req.body);
-    res.status(201).json(result);
+    const contactAdd = await Contact.create(req.body);
+
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contactAdd,
+      },
+    });
   } catch (error) {
     next(error);
   }
 });
 
+// Видалення контакту
 router.delete("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const result = await contactsOperation.removeContact(id);
+    const result = await Contact.findByIdAndRemove(id);
     if (!result) {
       const { error } = new Error(`Contact with id=${id} not found`);
       throw error;
     }
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: result,
+      },
+    });
   } catch (error) {
     next(error);
   }
-  res.json({ message: "contact deleted" });
+  // res.json({ message: "contact deleted" });
 });
 
+// Зміна значення поля по id
 router.put("/:id", async (req, res, next) => {
-  const schema = Joi.object({
-    // id: Joi.string(),
-    name: Joi.string(),
-    email: Joi.string(),
-    phone: Joi.string(),
-  });
   try {
-    const { error } = schema.validate(req.body);
+    const { error } = joiSchemaUpdate.validate(req.body);
     if (error) {
       error.status = 400;
       throw error;
     }
     const { id } = req.params;
 
-    const result = await contactsOperation.updateContact(id, req.body);
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
     if (!result) {
       return res.status(404).json({ message: "Not found" });
     }
     res.json({ result });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Зміна значення одного поля
+router.patch("/:id/favorite", async (req, res, next) => {
+  try {
+    const { error } = joiSchemaFavorite.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error({ message: "missing field favorite" });
+    }
+    const { id } = req.params;
+    const { favorite } = req.body;
+
+    const result = await Contact.findByIdAndUpdate(
+      id,
+      { favorite },
+      { new: true }
+    );
+    if (!result) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result,
+      },
+    });
   } catch (error) {
     next(error);
   }
