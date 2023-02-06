9c7c8224d57655af6615b1d935479cb6f6123f18
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-02-06 21:29:00 +0200

    fixed mentors comments

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,49 +1,39 @@
 const Contact = require("../../models/contact");
+const { NotFound } = require("http-errors");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
   const { page = 1, limit = 10, favorite, name, email } = req.query;
   const skip = (page - 1) * limit;
-  if (favorite) {
-    const contacts = await Contact.find({ owner: _id, favorite }, "", {
+
+  const filters = { owner: _id };
+
+  if (favorite) filters.favorite = favorite;
+
+  if (name) filters.name = name;
+
+  if (email) filters.email = email;
+
+  if (filters.name || filters.favorite || filters.email) {
+    const contacts = await Contact.find(filters, "", {
       skip,
       limit: Number(limit),
-    }).populate("owner", "_id email subscription");
-    res.json({
-      data: {
-        contacts,
-      },
-    });
-  } else if (name) {
-    const contacts = await Contact.find({ owner: _id, name }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id email subscription");
-    res.json({
-      data: {
-        contacts,
-      },
-    });
-  } else if (email) {
-    const contacts = await Contact.find({ owner: _id, email }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id email subscription");
+    }).populate("owner", "_id name email");
+
+    if (contacts.length === 0) {
+      throw new NotFound("Contact not found");
+    }
     res.json({
       data: {
         contacts,
       },
     });
   } else {
-    const contacts = await Contact.find({ owner: _id }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id email subscription");
-    res.json({
-      data: {
-        contacts,
-      },
-    });
+    const contacts = await Contact.find({ owner: _id }).populate(
+      "owner",
+      "_id name email"
+    );
+    res.json(contacts);
   }
 };
 
