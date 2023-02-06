8f6da893e644660673d4a885c3ba031c304db392
OlehStarko <starartds@gmail.com>
2023-02-06 20:17:43 +0200

    2

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,20 @@ const auth = require("../../middleware/auth");
 const router = express.Router();
 
 const { userController } = require("../../controllers");
-const {validateUser, validateUserUserSubscription} = require("../../middleware/validation");
+const {
+  validateUser,
+  validateUserUserSubscription,
+} = require("../../middleware/validation");
 
 router.post("/register", validateUser, userController.register);
 router.post("/login", validateUser, userController.login);
-router.post("/logout", auth, userController.logout); // auth
-router.post("/current", auth, userController.current); // auth
-router.patch("/", auth, validateUserUserSubscription, userController.updateSubscription); // auth
+router.post("/logout", auth, userController.logout);
+router.post("/current", auth, userController.current);
+router.patch(
+  "/",
+  auth,
+  validateUserUserSubscription,
+  userController.updateSubscription
+);
 
 module.exports = router;
