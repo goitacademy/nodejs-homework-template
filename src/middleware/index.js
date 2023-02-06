70080f7aded9c95c985ef6351e3e1327378e2295
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-02-06 12:38:07 -0800

    add upload middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,5 +2,6 @@ const joiValidation = require("./joiValidation");
 const isValidId = require("../middleware/isValidId");
 const ctrlWrapper = require("./ctrlWrapper")
 const authVerifyToken = require("./authVerifyToken")
+const upload = require("./upload")
 
-module.exports = { joiValidation, isValidId, ctrlWrapper, authVerifyToken };
+module.exports = { joiValidation, isValidId, ctrlWrapper, authVerifyToken, upload };
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { authVerifyToken, joiValidation, ctrlWrapper } = require("../../middleware");
+const { authVerifyToken, joiValidation, ctrlWrapper, upload } = require("../../middleware");
 const { users: ctrl } = require("../../controllers");
 const { joiUpdateUserSchema } = require("../../models/user");
 const validateJoiMiddleware = joiValidation(joiUpdateUserSchema);
@@ -9,5 +9,8 @@ const validateJoiMiddleware = joiValidation(joiUpdateUserSchema);
 
 router.get("/current", authVerifyToken, ctrlWrapper(ctrl.getCurrent));
 router.patch("/subscribe", authVerifyToken, validateJoiMiddleware, ctrlWrapper(ctrl.updateSubscribe));
+router.patch("/avatars", upload.single("avatar"));
+// router.patch("/avatars", upload.single("avatar"), authVerifyToken, validateJoiMiddleware, ctrlWrapper(ctrl.updateSubscribe));
+
 
 module.exports = router;
