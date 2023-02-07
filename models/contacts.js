90094c7cafcecd68a54adb00b96e7ee07bd6c3fd
Michał Murawski <m.a.murawski@wp.pl>
2023-02-07 23:59:53 +0100

    from hw2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,88 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("node:path");
 
-const listContacts = async () => {}
+const contactsPath = path.format({
+  root: "/ignored",
+  dir: "models",
+  base: "contacts.json",
+});
 
-const getContactById = async (contactId) => {}
+const idPath = path.format({
+  root: "/ignored",
+  dir: "models",
+  base: "id.json",
+});
 
-const removeContact = async (contactId) => {}
+const readLastId = async () => {
+  return fs.readFile(idPath).then((id) => {
+    return JSON.parse(id);
+  });
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  return fs.readFile(contactsPath).then((contacts) => {
+    return JSON.parse(contacts);
+  });
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  return fs
+    .readFile(contactsPath)
+    .then((contacts) =>
+      JSON.parse(contacts).find((contact) => contact.id === contactId)
+    );
+};
+
+const removeContact = async (contactId) => {
+  let isExist = false;
+  const contacts = await fs
+    .readFile(contactsPath)
+    .then((contacts) => JSON.parse(contacts))
+    .then((contacts) =>
+      contacts.filter((contact) => {
+        if (contact.id === contactId) {
+          isExist = true;
+        }
+        return contact.id !== contactId;
+      })
+    );
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return isExist;
+};
+
+const addContact = async (body) => {
+  let { id } = await readLastId();
+  console.log(id);
+  id = Number(id) + 1;
+  body = { id: `${id}`, ...body };
+  await fs.writeFile(idPath, JSON.stringify({ id }));
+  const contacts = await fs
+    .readFile(contactsPath)
+    .then((contacts) => JSON.parse(contacts))
+    .then((contacts) => [...contacts, body]);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return body;
+};
+
+const updateContact = async (contactId, body) => {
+  let isExist = false;
+  const contacts = await fs
+    .readFile(contactsPath)
+    .then((contacts) => JSON.parse(contacts))
+    .then((contacts) => {
+      contacts.forEach((contact, index, array) => {
+        if (contact.id === contactId) {
+          contact = { ...contact, ...body };
+          isExist = contact;
+          array.splice(index, 1, contact);
+        }
+      });
+
+      return contacts;
+    });
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return isExist;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +90,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
