85cfbfc8c34c1479d21b34eea6f041201d22d34b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-08 16:17:29 +0300

    Create getById.js

---
+++ controllers/contacts/getById.js
@@ -0,0 +1,36 @@
+// const createError = require("http-errors");
+const { contactOperations } = require("../../models");
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contactById = await contactOperations.getContactById(contactId);
+
+    if (!contactById) {
+      // throw createError(404, `Contact with id=${contactId} not found.`);
+
+      // const error = new Error("Not found");
+      // error.status = 404;
+      // throw error;
+
+      res.status(404).json({
+        status: "Error",
+        code: 404,
+        message: "Not found",
+      });
+      return;
+    }
+
+    res.json({
+      status: "Success",
+      code: 200,
+      data: {
+        result: contactById,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getById;
