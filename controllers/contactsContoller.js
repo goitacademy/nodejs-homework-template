5605b4a74446be304bc49a431f5958ec6082ad63
Reqvite <reqvite2233@gmail.com>
2023-02-08 17:54:13 +0200

    add codes to resp

--- controllers/contactsContoller.js
+++ controllers/contactsContoller.js
@@ -8,29 +8,29 @@ const {
 
 const getContacts = async (req, res) => {
   const contacts = await listContacts();
-  res.json({ contacts, status: "succes" });
+  res.json({ contacts, status: "succes", code: 200 });
 };
 
 const getContact = async (req, res) => {
   const [contact] = await getContactById(req.params.contactId);
   if (!contact) {
-    return res.status(404).json({ status: `Not found` });
+    return res.status(404).json({ status: `Not found`, code: 404 });
   }
-  res.json({ contact, status: "succes" });
+  res.json({ contact, status: "succes", code: 200 });
 };
 
 const addNewContact = async (req, res) => {
   const { name, email, phone } = req.body;
   const newContact = await addContact(name, email, phone);
-  res.status(201).json({ newContact, status: "succes" });
+  res.status(201).json({ newContact, status: "succes", code: 201 });
 };
 
 const deleteContact = async (req, res, next) => {
   const status = await removeContact(req.params.contactId);
   if (!status) {
-    return res.status(404).json({ status: `Not found` });
+    return res.status(404).json({ status: `Not found`, code: 404 });
   }
-  res.json({ status: `Сontact deleted` });
+  res.json({ status: `Сontact deleted`, code: 200 });
 };
 
 const updateContactById = async (req, res, next) => {
@@ -41,7 +41,7 @@ const updateContactById = async (req, res, next) => {
   if (!status) {
     return res.status(404).json({ status: `Not found` });
   }
-  res.json({ updatedContact, status: `succes` });
+  res.json({ updatedContact, status: `succes`, code: 200 });
 };
 
 module.exports = {
