8fbda6977300144f8653359b06b1e88f51879913
YurGoV <yurgov@gmail.com>
2023-02-08 13:59:25 +0200

    draft/all-routs-done

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,94 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
 
-const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+    try {
+        const result = await fs.readFile('./models/contacts.json', 'utf8');
+        return JSON.parse(result);
+    } catch (err) {
 
-const removeContact = async (contactId) => {}
+        return err;
+    }
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+    try {
+        const result = await fs.readFile('./models/contacts.json', 'utf8')// todo: listContacts?
+        const data = JSON.parse(result);
+        return data.filter(item => item.id === contactId);// todo: find?
+    } catch (err) {
+        return err;
+    }
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+    try {
+        const oldContacts = await listContacts();
+        const isContact = oldContacts.find(item => item.id === contactId);// todo: find?
+        console.log('isContact: ', isContact);
+        if (isContact) {
+            const newContacts = JSON.stringify(oldContacts.filter(item => item.id !== contactId));
+            await fs.writeFile('./models/contacts.json', newContacts, 'utf-8')
+            return {"message": "contact deleted"};
+        }
+        ;
+        return {"message": "Not found"};
+
+    } catch (err) {
+        return err;
+    }
+}
+
+const addContact = async (body) => {
+    try {
+        // console.log(await getContactById(body.id));
+        const oldContacts = await listContacts();
+        const newContacts = JSON.stringify([...oldContacts, body])
+        console.log(newContacts);
+
+        await fs.writeFile('./models/contacts.json', newContacts, 'utf-8')
+        const writtenContacts = await listContacts();
+        console.log(writtenContacts);
+        return body
+    } catch (err) {
+        return err;
+    }
+}
+
+const updateContact = async (contactId, body) => {
+    try {
+        const oldContacts = await listContacts();
+        const isContact = oldContacts.find(item => item.id === contactId);// todo: find?
+        console.log('isContact: ', isContact);
+        let newContact = false;
+        const newContacts = await oldContacts.map(contact => {
+            if (contact.id === contactId) {
+                newContact = {...contact, ...body};
+                return newContact
+            }
+            return contact
+        })
+
+        console.log(newContacts);
+
+        if (newContact) {
+            console.log('yahooo');
+            await fs.writeFile('./models/contacts.json', JSON.stringify(newContacts), 'utf-8')
+            return newContact;
+
+        } else {
+            return null
+        }
+    } catch (err) {
+        return err;
+    }
+
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
 }
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+app.listen(3000, (err) => {
+    if (err) {
+        console.error('Error at server launch:', err);
+    }
+    console.log("Server running. Use our API on port: 3000")
 })
