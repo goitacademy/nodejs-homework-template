5f775e15b3651c017c4884e730f4a323dee3babc
Dima Savkov <dimsaw85@gmail.com>
2023-02-09 14:37:12 +0200

    add router get

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
+const contacts = require("../../models/contacts.json");
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ contacts, message: "success" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const [contact] = contacts.filter((item) => item.id === contactId);
+
+  if (!contact) {
+    return res
+      .status(400)
+      .json({ status: `failure, we don't have contact with id: ${contactId}` });
+  }
+  res.json({ contact, message: "success" });
 });
 
 router.post("/", async (req, res, next) => {
