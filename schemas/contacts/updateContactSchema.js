5c254ece00f4cf05937b95091b48e59a1b311394
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-09 20:52:18 +0300

    Delete updateContactSchema.js

--- schemas/contacts/updateContactSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-}).min(1);
-module.exports = updateContactSchema;
