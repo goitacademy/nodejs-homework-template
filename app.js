7e7842d5edfca60809890b82a762c7a69e3c715a
OksanaKuzich <komoks255@gmail.com>
2023-02-09 21:47:13 +0200

    added package react

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 mongoose.set("strictQuery", false);
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -31,13 +33,34 @@ app.use(express.json());
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
-
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
+});
+
+const publicImgDir = path.join(__dirname, "public");
+
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, publicImgDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 2048,
+  },
+});
+
+const upload = multer({
+  storage: multerConfig,
+}); 
+
+app.post("/api/avatars", upload.single("image"), async (req, res) => {
+  console.log(req.file);
 });
 
 module.exports = app;
