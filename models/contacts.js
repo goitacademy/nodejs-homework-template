9f098320c56719067c5f0d8e75a1ce4793f45646
H4Y6 <h4y6@i.ua>
2023-02-09 20:48:15 +0200

    modified:   models/contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -3,8 +3,8 @@ const path = require("path");
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  const allBooks = await fs.readFile(contactsPath);
-  return JSON.parse(allBooks);
+  const allContacts = await fs.readFile(contactsPath);
+  return JSON.parse(allContacts);
 };
 
 const getContactById = async (contactId) => {
@@ -16,7 +16,16 @@ const getContactById = async (contactId) => {
   return idContact;
 };
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((c) => c.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const deleContact = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deleContact;
+};
 
 const addContact = async (body) => {};
 
