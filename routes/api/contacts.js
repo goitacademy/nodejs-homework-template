6c7395d51a1979ebb680dc943fcca8f21eb99135
Dima Savkov <dimsaw85@gmail.com>
2023-02-09 15:54:43 +0200

    fix problem in delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,6 +12,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
   const [contact] = contacts.filter((item) => item.id === contactId);
+  console.log(contact);
 
   if (!contact) {
     return res.status(400).json({ message: "Not found" });
@@ -33,12 +34,42 @@ router.post("/", async (req, res, next) => {
 router.delete("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
 
+  const [contact] = contacts.filter((item) => item.id === contactId);
+  console.log(contact);
+
+  if (!contact) {
+    return res.status(400).json({ message: "Not found" });
+  }
+
   contacts = contacts.filter((item) => item.id !== contactId);
+  console.log(contacts);
+
+  // if (!contacts) {
+  //   return res.status(400).json({ message: "Not found" });
+  // }
 
   res.status(200).json({ message: "contact deleted" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+
+  // contacts.forEach((contact) => {
+  //   if (contact.id !== req.params.contactId) {
+  //     return res.status(404).json({ message: "error" });
+  //   }
+  // });
+
+  const delcontact = contacts.forEach((contact) => {
+    if (contact.id === req.params.contactId) contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+  });
+  console.log(delcontact);
+  if (!delcontact) {
+    return res.status(404).json({ message: "error" });
+  }
+
   res.json({ message: "template message" });
 });
 
