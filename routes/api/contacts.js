4941f082b3861e297bed70adc9a3f283f97a4c0e
Dima Savkov <dimsaw85@gmail.com>
2023-02-09 13:54:58 +0200

    third commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -11,7 +11,7 @@ router.get("/:contactId", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "home work done" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
