bda006913b796638ec1301f7470f2003be7844c2
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-02-09 23:08:08 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 const createError = require("http-errors");
+const Joi = require("joi");
+
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.number().required(),
+});
 
 const contactsOperations = require("../../models/contacts");
 
@@ -31,6 +38,11 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
     const contact = await contactsOperations.addContact(req.body);
     res.status(201).json({
       status: "success",
