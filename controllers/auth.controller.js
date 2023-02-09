a3412933864001724ee43445f500bf3863ffe960
Alexander Shabanov <dffleh1995@gmail.com>
2023-02-09 20:00:27 +0200

    add register and login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,16 @@
 const { User } = require("../utils/schemas/schemaUser");
 const { HttpError } = require("../utils/helpers/httpError");
+const bcrypt = require("bcrypt");
 
 async function register(req, res, next) {
   const { email, password } = req.body;
-  // const { error } = schema.validate(req.body);
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   try {
     const savedUser = await User.create({
       email,
-      password,
+      password: hashedPassword,
     });
     res.status(201).json(savedUser);
   } catch (error) {
@@ -18,6 +21,24 @@ async function register(req, res, next) {
   }
 }
 
+async function login(req, res, next) {
+  const { email, password } = req.body;
+  const storedUser = await User.findOne({
+    email,
+  });
+
+  if (!storedUser) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, storedUser.password);
+
+  if (!isPasswordValid) {
+    throw Unauthorized("password is not valid");
+  }
+}
+
 module.exports = {
   register,
+  login,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { register } = require("../../controllers/auth.controller");
+const { register, login } = require("../../controllers/auth.controller");
 const { tryCatchWrapper } = require("../../utils/helpers/rtyCatchHelper");
 
 const authRouter = express.Router();
 
 authRouter.post("/register", tryCatchWrapper(register));
+authRouter.post("/login", tryCatchWrapper(login));
 
 module.exports = {
   authRouter,
