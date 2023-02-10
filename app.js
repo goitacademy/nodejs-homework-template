c07059d9e6b75315566add6987fb0222e87572f1
Michał Murawski <m.a.murawski@wp.pl>
2023-02-10 15:55:11 +0100

    add config-passport

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+require('./config/config-passport');
 app.use('/api/contacts', contactsRouter);
 app.use('/api/users', usersRouter);
 
