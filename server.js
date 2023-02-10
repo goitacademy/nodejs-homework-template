fdb470d72ae36f44e679b42731a9e955992fb487
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-02-10 23:27:07 +0100

    fix error

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = require("./app");
 const { DB_HOST, PORT } = process.env;
 
 mongoose
-  .connect(DB_HOST, console.log("Database connection successful"))
-  .then(() => app.listen(PORT))
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
+	.set("strictQuery", false)
+	.connect(DB_HOST, console.log("Database connection successful"))
+	.then(() => app.listen(PORT))
+	.catch((error) => {
+		console.log(error);
+		process.exit(1);
+	});
