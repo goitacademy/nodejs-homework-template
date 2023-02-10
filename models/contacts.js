6c91fcc189594ad9f1df2aebda6c28c937ca48ef
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-02-10 22:44:07 +0100

    controllers and models

--- models/contacts.js
+++ models/contacts.js
@@ -1,87 +1,45 @@
-const fs = require("fs").promises;
-const path = require("path");
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Set name for contact"],
+		},
+		email: {
+			type: String,
+		},
+		phone: {
+			type: String,
+		},
+		favorite: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ versionKey: false, timestamps: true }
+);
 
-async function saveContactsToFile(data) {
-	const content = JSON.stringify(data);
-	await fs.writeFile(contactsPath, content);
-}
+const contactAddSchema = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().required(),
+	phone: Joi.string().required(),
+	favorite: Joi.boolean(),
+});
 
-async function getContactsFromFile() {
-	try {
-		return JSON.parse(await fs.readFile(contactsPath, "utf8"));
-	} catch (error) {
-		return console.error(error.message);
-	}
-}
+const contactUpdateStatusSchema = Joi.object({
+	favorite: Joi.boolean().required(),
+});
 
-const listContacts = async () => {
-	const data = await getContactsFromFile();
-	console.table(data);
-	return data;
+const schemas = {
+	addContact: contactAddSchema,
+	updateStatusContact: contactUpdateStatusSchema,
 };
 
-const getContactById = async (contactId) => {
-	const contacts = await getContactsFromFile();
-	const filtredContact = contacts.find((contact) => contact.id === contactId);
-	console.table(filtredContact);
-	return filtredContact;
-};
-
-const removeContact = async (contactId) => {
-	const contacts = await getContactsFromFile();
-	const filtredContacts = contacts.filter((contact) => contact.id !== contactId);
-	saveContactsToFile(filtredContacts);
-	console.table(filtredContacts);
-	return filtredContacts;
-};
-
-const addContact = async (data) => {
-	const { name, email, phone } = data;
-	const contacts = await getContactsFromFile();
-	const newContact = {
-		id: (Number(contacts[contacts.length - 1].id) + 1).toString(),
-		name,
-		email,
-		phone,
-	};
-	contacts.push(newContact);
-	saveContactsToFile(contacts);
-	console.table(contacts);
-	return contacts;
-};
-
-const updateContact = async (contactId, body) => {
-	const { name, email, phone } = body;
-	const data = await getContactsFromFile();
-
-	const newContact = {
-		id: `${contactId}`,
-		name: name,
-		email: email,
-		phone: phone,
-	};
-
-	const updateContact = data.find((contact) => contact.id === contactId);
-
-	console.log(updateContact);
-
-	const updateContactId = data.indexOf(updateContact);
-
-	if (updateContactId === -1) {
-		return null;
-	} else {
-		data.splice(updateContactId, 1, newContact);
-		await saveContactsToFile(data);
-	}
-	return newContact;
-};
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-	listContacts,
-	getContactById,
-	removeContact,
-	addContact,
-	updateContact,
+	Contact,
+	schemas,
 };
