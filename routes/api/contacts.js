c426de43992c54ad75a43c50649f4e29289b3e3f
Michał Murawski <m.a.murawski@wp.pl>
2023-02-11 22:43:35 +0100

    authotization

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,33 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const contactsController = require('../../controller/contactsController');
-const passport = require('passport');
-
-const auth = (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (!user || err) {
-      return res.status(401).json({
-        status: 'error',
-        code: 401,
-        message: 'Unauthorized',
-        data: 'Unauthorized',
-      });
-    }
-    req.user = user;
-    next();
-  })(req, res, next);
-};
+const auth = require('../../auth/userAuth');
 
 router.get('/', auth, contactsController.get);
 
-router.get('/:id', contactsController.getById);
+router.get('/:id', auth, contactsController.getById);
 
-router.post('/', contactsController.create);
+router.post('/', auth, contactsController.create);
 
-router.delete('/:id', contactsController.removeById);
+router.delete('/:id', auth, contactsController.removeById);
 
-router.put('/:id', contactsController.update);
+router.put('/:id', auth, contactsController.update);
 
-router.patch('/:id/favorite', contactsController.updateStatus);
+router.patch('/:id/favorite', auth, contactsController.updateStatus);
 
 module.exports = router;
