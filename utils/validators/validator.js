b49833a61cb8c445ab23dff48ae6bb1606f333d4
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2023-02-11 20:49:48 +0100

    Update validator.js

--- utils/validators/validator.js
+++ utils/validators/validator.js
@@ -6,8 +6,6 @@ const contactSchema = joi.object({
   phone: joi.string().min(5),
 });
 
-const validator = (schema) => (body) => {
-  return schema.validate(body);
-};
+const validator = (schema) => (body) => schema.validate(body);
 
 export const contactValidator = validator(contactSchema);
