4148312b77727ea9820ca9a4f622a262e1e094c3
YurGoV <yurgov@gmail.com>
2023-02-11 10:23:45 +0200

    draft/validation-regexp-eslint

--- middlewars/postsValitation.js
+++ middlewars/postsValitation.js
@@ -5,13 +5,17 @@ module.exports = {
     addPostValidation: (req, res, next) => {
         const schema = Joi.object({
             name: Joi.string()
-                .pattern(new RegExp('^[a-zA-Zа-яА-Я0-9іІїЇєЄ\']{3,20}$'))
+                // .pattern(new RegExp('^[a-zA-Zа-яА-Я0-9іІїЇєЄ\']{3,20}$'))
+                .pattern(/^[a-zA-Zа-яА-Я0-9іІїЇєЄґҐ']{3,20}$/)
+
                 .required(),
             email: Joi.string()
                 .email({minDomainSegments: 2, tlds: {allow: ['com', 'net', 'ua', 'org', 'net']}})
                 .required(),
             phone: Joi.string()
-                .pattern(new RegExp('^[\+0-9]{13}$'))
+                // .pattern(new RegExp('^[+0-9]{13}$'))
+                .pattern(/^[+0-9]{13}$/)
+
                 .required(),
         })
         const validationResult = schema.validate(req.body);
@@ -24,12 +28,14 @@ module.exports = {
     updatePostValidation: (req, res, next) => {
         const schema = Joi.object({
             name: Joi.string()
-                .pattern(new RegExp('^[a-zA-Zа-яА-Я0-9іІїЇєЄ\']{3,20}$'))
+                // .pattern(new RegExp('^[a-zA-Zа-яА-Я0-9іІїЇєЄ\']{3,20}$'))
+                .pattern(/^[a-zA-Zа-яА-Я0-9іІЇєЄґҐ']{3,20}$/)
                 .optional(),
             email: Joi.string()
                 .email({minDomainSegments: 2, tlds: {allow: ['com', 'net', 'ua', 'org', 'net']}}),
             phone: Joi.string()
-                .pattern(new RegExp('^[\+0-9]{13}$'))
+                // .pattern(new RegExp('^[+0-9]{13}$'))
+                .pattern(/^[+0-9]{13}$/)
                 .optional(),
         })
         const validationResult = schema.validate(req.body);
@@ -39,5 +45,4 @@ module.exports = {
         }
         next();
     }
-}
-;
+};
