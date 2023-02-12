c48777af9f4311b14513cc9b5bffba4e694de3ac
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-02-12 11:51:03 +0200

    post user

--- controllers/users/controllerSingUpUser.js
+++ controllers/users/controllerSingUpUser.js
@@ -1,5 +1,18 @@
 const { addUser } = require('../../services/users');
 
-const controllerSingUpUser = (req, res, next) => {};
+const controllerSingUpUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await addUser(email, password);
+  res.json({
+    status: 'success',
+    code: 201,
+    data: {
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    },
+  });
+};
 
 module.exports = { controllerSingUpUser };
--- services/users/addUser.js
+++ services/users/addUser.js
@@ -1,8 +1,11 @@
 const { User } = require('../../schemas/modelUser');
 const bcrypt = require('bcrypt');
 
-const addUser = async ({ email, password }) => {
-  return await User.create({ email, password: bcrypt.hash(password, 10) });
+const addUser = async (email, password) => {
+  return await User.create({
+    email,
+    password: await bcrypt.hash(password, 10),
+  });
 };
 
 module.exports = { addUser };
