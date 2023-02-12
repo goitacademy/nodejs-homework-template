f6be98586b9a6272336074dcab310ca2912b32c9
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2023-02-12 17:00:28 +0300

    done!!

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,17 +1,22 @@
-
-const { HttpError } = require("../helpers/index");
-const {Contact} = require("../mod/contact")
-
+const { HttpError } = require('../helpers/index');
+const { Contact } = require('../mod/contact');
 
 // get list
 async function getContacts(req, res) {
-  const { limit = 5, page } = req.query;
+  const { id: owner } = req.user;
+  const { limit = 20, page = 1, favorite } = req.query;
   const skyp = (page - 1) * limit;
 
-  console.log("limit:", limit);
-  const contacts = await Contact.find({}).skip(skyp).limit(limit);
-  console.log("movies:", contacts);
-  return res.json(contacts);
+  console.log('limit:', limit);
+  const contacts = await Contact.find({ owner }).skip(Number(skyp)).limit(Number(limit));
+
+  if (!favorite) {
+    console.log('contacts:', contacts);
+    res.status(200).json(contacts);
+  } else {
+    const favContacts = await contacts.filter((contact) => contact.favorite.toString() === favorite);
+    return res.status(200).json(favContacts);
+  }
 }
 
 // get by id
@@ -20,66 +25,64 @@ async function getContact(req, res, next) {
   const contact = await Contact.findById(id);
 
   if (!contact) {
-    return next(HttpError(404, "Contact not found"));
+    return next(HttpError(404, 'Contact not found'));
   }
   return res.json(contact);
 }
 
 // post
 async function createContact(req, res, next) {
-  const {name, email, phone,  favorite} = req.body;
+  const { name, email, phone, favorite } = req.body;
   //   1 - own validation
   //   if (!title) {
   //     return next(HttpError(400, "title is required!"));
   //   }
   //   2 - with library
-//   const schema = Joi.object({
-//     title: Joi.string().min(3).required(),
-//   });
+  //   const schema = Joi.object({
+  //     title: Joi.string().min(3).required(),
+  //   });
 
-//   const { error } = schema.validate(req.body);
+  //   const { error } = schema.validate(req.body);
 
-//   if (error) {
-//     return next(HttpError(400, error.message));
-//   }
+  //   if (error) {
+  //     return next(HttpError(400, error.message));
+  //   }
 
-  console.log("name:", name, "email:", email, "phone:", phone, "favorite:", favorite);
-  const newContact = await Contact.create({name, email, phone, favorite});
+  console.log('name:', name, 'email:', email, 'phone:', phone, 'favorite:', favorite);
+  const newContact = await Contact.create({ name, email, phone, favorite });
   return res.status(201).json(newContact);
 }
 
-async function deleteContact (req, res, next) {
+async function deleteContact(req, res, next) {
   const { id } = req.params;
   const contact = await Contact.findById(id);
   if (!contact) {
-    return next(HttpError(404, "No contact"));
+    return next(HttpError(404, 'No contact'));
   }
   await Contact.findByIdAndRemove(id);
   return res.status(200).json(contact);
 }
 
-
 async function updateContact(req, res, next) {
   const { body } = req;
   const { id } = req.params;
-  const contact = await Contact.findByIdAndUpdate(id, body)
+  const contact = await Contact.findByIdAndUpdate(id, body);
   if (!contact) {
-    return next (HttpError(404, "Not found"));
+    return next(HttpError(404, 'Not found'));
   }
   return res.json(contact);
 }
-
 
 async function updateStatusContact(req, res, next) {
   const { body } = req;
   const { id } = req.params;
 
-  const contact = await Contact.findByIdAndUpdate(id, body, {new: true}) 
-    if (!contact) {
-      return next (HttpError(404, "Not found"));
-    }
-    return res.status(200).json(contact);
+  const contact = await Contact.findByIdAndUpdate(id, body, { new: true });
+  if (!contact) {
+    return next(HttpError(404, 'Not found'));
   }
+  return res.status(200).json(contact);
+}
 
 //     // ==== update ====
 //     // const contact = await Contact.findByIdAndUpdate(
@@ -88,8 +91,6 @@ async function updateStatusContact(req, res, next) {
 //     //   { new: true }
 //     // );
 //     // console.log('updated contact: ', contact);
-
-
 
 module.exports = {
   getContact,
