fb4c34ae6e8ddc310ca5da12a79801d20d0ffa61
InnaShtondenko <inna.mycase@gmail.com>
2023-02-12 21:49:21 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,67 @@
-// const fs = require('fs/promises')
+const path = require("path");
+const fs = require("fs").promises;
+const contactsPath = path.resolve("./models/contacts.json");
 
-const listContacts = async () => {}
+async function listContacts() {
+  const listOfContacts = await fs.readFile(contactsPath, "UTF-8");
+  return JSON.parse(listOfContacts);
+}
 
-const getContactById = async (contactId) => {}
+async function getContactById(contactId) {
+  const parsedContacts = await listContacts();
 
-const removeContact = async (contactId) => {}
+  const contact = parsedContacts.find((contact) => contact.id === contactId);
+  return contact;
+}
 
-const addContact = async (body) => {}
+async function removeContact(contactId) {
+  const parsedContacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+  const contactToBeDeleted = parsedContacts.find(({ id }) => id === contactId);
+  const filteredContacts = parsedContacts.filter(
+    (contact) => contact !== contactToBeDeleted
+  );
+
+  fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+
+  return contactToBeDeleted;
+}
+
+async function addContact({ name, email, phone }) {
+  const parsedContacts = await listContacts();
+
+  // Looking for the biggest id number (could be replaced by some id generator)
+  let biggestId = 0;
+  parsedContacts.map(({ id }) => (+id > biggestId ? (biggestId = +id) : null));
+  const newContactID = biggestId + 1;
+
+  const newContact = {
+    id: newContactID.toString(), // Converting to String to save DB var's types from example
+    name: name.toString(),
+    email: email.toString(),
+    phone: phone.toString(),
+  };
+
+  parsedContacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(parsedContacts));
+
+  return newContact;
+}
+
+async function updateContact(contactId, body) {
+  const parsedContacts = await listContacts();
+  let updatedContact = false;
+
+  //  У цьому випадку фор використаний для переривання проходження по масиву, після знаходження потрібного контакту, адже якщо потрібний контакт буде першим у списку, немає потреби проходитись по тисячях наступних контактів.
+  for (let i = 0; i < parsedContacts.length; i++) {
+    if (parsedContacts[i].id === contactId) {
+      parsedContacts[i] = { ...parsedContacts[i], ...body };
+      updatedContact = parsedContacts[i];
+      fs.writeFile(contactsPath, JSON.stringify(parsedContacts));
+      return updatedContact;
+    }
+  }
+}
 
 module.exports = {
   listContacts,
@@ -16,4 +69,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
