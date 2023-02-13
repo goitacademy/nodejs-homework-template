c161a1e8f61f444bd60c646fb6ef69c74675745d
Nelia Bochenkova <nelya.teslenko.95@gmail.com>
2023-02-13 20:37:57 +0200

    hw-2

---
+++ validation/createContactSchema.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const createContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+
+  phone: Joi.string().min(10).max(13).required(),
+});
+
+module.exports = createContactSchema;
