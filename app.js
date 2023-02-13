cb908f64ae5bbe773ecc3c3c05160faa447702b0
Oksana Konieva <anaska0489@gmail.com>
2023-02-13 18:33:35 +0200

    update app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
-const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
