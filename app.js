e389bbf8a637c473c05edb6206912483b734b065
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-02-14 21:40:27 +0200

    Added sharing static files functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('module-alias/register');
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const { contactsRouter, usersRouter } = require('./routes/api');
@@ -13,6 +14,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/contacts', contactsRouter);
 app.use('/api/users', usersRouter);
