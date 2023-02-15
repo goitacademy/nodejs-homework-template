57e045f173ad878b5fed384feb50ecdf22f19be2
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-15 22:09:40 +0300

    Update users.js
    Додає обробку маршруту логіну користувача

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { usersControllers } = require("../../controllers");
 const { usersValidation } = require("../../middlewares/users");
-const { authUserJoiSchema } = require("../../models/users");
+const { authUserJoiSchema, loginUserJoiSchema } = require("../../models/users");
 
 const router = express.Router();
 
 const validationUserAuth =
   usersValidation.userAuthValidation(authUserJoiSchema);
 
+const validationUserLogin =
+  usersValidation.userAuthValidation(loginUserJoiSchema);
+
 router.post("/register", validationUserAuth, usersControllers.register);
+router.post("/login", validationUserLogin, usersControllers.login);
 
 module.exports = router;
