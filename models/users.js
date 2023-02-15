ce1ec7159472191b4e6dde0688331693cd088116
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-15 22:10:05 +0300

    Update users.js
    Додає схему валідації логіну користувача

--- models/users.js
+++ models/users.js
@@ -31,7 +31,13 @@ const authUserJoiSchema = Joi.object({
   subscription: Joi.string().valid("starter pro business"),
 });
 
+const loginUserJoiSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().email().required(),
+});
+
 module.exports = {
   User,
   authUserJoiSchema,
+  loginUserJoiSchema,
 };
