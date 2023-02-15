3ce8b8c0532886bf61e9bebf5881047af9e2fb2a
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-02-15 16:18:47 +0200

    start

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,97 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const short = require("short-uuid");
+const { loadavg } = require("os");
+const path = require("path");
+const contactsPath = path.resolve("./models/contacts.json");
 
-const listContacts = async () => {}
+// npx nodemon --inspect ./server.js
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf8");
+    const contactsArr = JSON.parse(contacts);
+    const contactGet = contactsArr.find((contact) => contact.id === contactId);
+    return contactGet;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf8");
+    const contactsArr = JSON.parse(contacts);
+    const contactFromId = contactsArr.find(
+      (contact) => contact.id === contactId
+    );
+    if (!contactFromId) {
+      return null;
+    } else {
+      const contactDelete = contactsArr.filter(
+        (contact) => contact.id !== contactId
+      );
+      await fs.writeFile(contactsPath, JSON.stringify(contactDelete), "utf8");
+      return contactFromId;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const contactNew = {
+    id: short.generate("0123456789"),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf8");
+    const contactsArr = JSON.parse(contacts);
+    if (!name || !email || !phone) {
+      return contactsArr;
+    } else {
+      const newContactsArr = [...contactsArr, contactNew];
+      await fs.writeFile(contactsPath, JSON.stringify(newContactsArr), "utf8");
+      return newContactsArr;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  if (!name & !email & !phone) {
+    return;
+  }
+  
+    try {
+      const contacts = await fs.readFile(contactsPath, "utf8");
+      const contactsArr = JSON.parse(contacts);
+      const [contactChenge] = contactsArr.filter(
+        (contact) => contact.id === contactId
+      );
+      if (name) { contactChenge.name = name };
+      if (email) { contactChenge.email = email};
+      if (phone) {contactChenge.phone = phone};
+
+      await fs.writeFile(contactsPath, JSON.stringify(contactsArr), "utf8");
+      return contactsArr;
+    } catch (error) {
+      console.log(error);
+    }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +99,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,95 @@
-const express = require('express')
+// const fs = require("fs/promises");
+const express = require("express");
+const Joi = require("joi");
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.json({ contacts, status: "200" });
+ 
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const contact = await getContactById(req.params.contactId);
+  if (!contact) {
+    res.json({ message: "Not found", status: "404" });
+  }
+  res.json({ contact, status: "200" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  
+  const newContacts = await addContact(req.body);
+  if (!name || !email || !phone) {
+    res.json({ message: "missing required name field", status: "400" });
+  } else {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      })
+      .required(),
+    phone: Joi.string().min(6).max(10).required(),
+  });
+  const validRezalt = schema.validate(req.body);
+  if (validRezalt.error) {
+    res.json({ message: validRezalt.error, status: "400" });
+  }else(res.json({ newContacts, status: "201" }))
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
-module.exports = router
+    
+  }
+});
+
+
+
+router.delete("/:contactId", async (req, res, next) => {
+  const contactDelete = await removeContact(req.params.contactId);
+  if (!contactDelete) {
+    res.json({ message: "Not found", status: "404" });
+  } else {
+    res.json({ message: "contact deleted", status: "200" });
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  if (!req.body) {
+    res.json({ message: "missing fields", status: "404" });
+  }
+  if (req.body) {
+    const updateContacts = await updateContact(req.params.contactId, req.body);
+    if (updateContacts) {
+        const schema = Joi.object({
+          name: Joi.string().alphanum().min(3).max(30),
+          email: Joi.string()
+            .email({
+              minDomainSegments: 2,
+              tlds: { allow: ["com", "net"] },
+            }),
+          phone: Joi.string().min(6).max(10),
+        });
+      const validRezalt = schema.validate(req.body);
+      
+        if (validRezalt.error) {
+          res.json({ message: validRezalt.error, status: "400" });
+      }else( res.json({ updateContacts, status: "200" }))
+      
+     
+    } else res.json({ message: "Not found", status: "404" });
+  }
+   
+});
+
+module.exports = router;
