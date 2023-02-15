d9805cb905419f1352025114994d0e353f87577b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-15 21:16:24 +0300

    Create users.js
    Створює схему валідації та модель

---
+++ models/users.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const usersSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      minlength: 6,
+      //   unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const User = model("user", usersSchema);
+
+const authUserJoiSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().email().required(),
+  subscription: Joi.string().valid("starter pro business"),
+});
+
+module.exports = {
+  User,
+  authUserJoiSchema,
+};
