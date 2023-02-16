59dd7822802cdc12d0fb88d1812572cc13c959e5
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-16 15:50:12 +0300

    Update getAll.js
    Додає метод populate() для розширення інформації стосовно користувача, який виконав запит

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,10 @@ const { Contact } = require("../../models");
 const getAll = async (req, res, next) => {
   const { _id } = req.user;
   try {
-    const allContacts = await Contact.find({ owner: _id });
+    const allContacts = await Contact.find({ owner: _id }).populate(
+      "owner",
+      "_id email subscription"
+    );
     res.json({
       status: "Success",
       code: 200,
