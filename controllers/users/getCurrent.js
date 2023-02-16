213cb96194e82d97f0129fb3bed4f8c383eb725d
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-16 15:22:20 +0300

    Create getCurrent.js
    Додає обробник маршруту поточного користувача

---
+++ controllers/users/getCurrent.js
@@ -0,0 +1,16 @@
+const { User } = require("../../models");
+
+const getCurrent = async (req, res, next) => {
+  const { email, subscription } = req.user;
+
+  res.json({
+    status: "Success",
+    code: 200,
+    data: {
+      email,
+      subscription,
+    },
+  });
+};
+
+module.exports = getCurrent;
