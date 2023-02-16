9d643c6a0aed4031e5030fc33b53b07d332966c5
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-02-16 17:44:40 +0200

    Added handling multer error

--- src/middlewares/errors.middleware.ts
+++ src/middlewares/errors.middleware.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { responseError } from 'helpers/apiHelpers';
-import { BaseError, ServerError } from 'helpers/errors';
+import { BaseError, ServerError, ValidationError } from 'helpers/errors';
+import multer from 'multer';
 
 export const errorMiddleware = (error: Error, _req: Request, res: Response, _next: NextFunction) => {
   if (error instanceof BaseError) {
     return res.status(error.code).json(responseError(error));
+  }
+
+  if (error instanceof multer.MulterError) {
+    return res.status(400).json(responseError(new ValidationError(error.message)));
   }
 
   return res.status(500).json(responseError(new ServerError(error.message)));
