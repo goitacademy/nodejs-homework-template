5adc3949d78cac948b5490998d66713a9b2a0262
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-16 15:49:17 +0300

    Update add.js

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -3,7 +3,11 @@ const { Contact } = require("../../models");
 const add = async (req, res, next) => {
   const { _id } = req.user;
   try {
-    const newContact = await Contact.create({ ...req.body, owner: _id });
+    const newContact = await Contact.create({
+      ...req.body,
+      owner: _id,
+    });
+
     res.status(201).json({
       status: "Success",
       code: 201,
