280fc6804d8efd98151f45701290e42aa2d04653
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-02-17 14:46:12 +0300

    Update getAll.js
    Додає фільтрацію контактів по полю обраного (GET /contacts?favorite=true)

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,13 +3,27 @@ const { Contact } = require("../../models");
 const getAll = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10, favorite } = req.query;
     const skip = (page - 1) * limit;
 
     const allContacts = await Contact.find({ owner: _id }, "", {
       skip,
       limit: Number(limit),
     }).populate("owner", "_id email subscription");
+
+    if (favorite) {
+      const filterContactsByStatus = allContacts.filter(
+        (contact) => contact.favorite === Boolean(favorite)
+      );
+      res.json({
+        status: "Success",
+        code: 200,
+        data: {
+          result: filterContactsByStatus,
+        },
+      });
+      return;
+    }
 
     res.json({
       status: "Success",
