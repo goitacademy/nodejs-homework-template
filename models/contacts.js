daa0cff2f52bab9bbaec8b4367f63f81d949fdac
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-02-18 00:10:05 +0200

    start

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,110 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: id } = require("uuid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf8");
 
-const removeContact = async (contactId) => {}
+  return JSON.parse(contacts);
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+    const parsedContacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+    const contactById = parsedContacts.filter(({ id }) => id === contactId);
+
+    if (contactById.length === 0) {
+      return console.log(`There is no contact with id: ${contactId}`);
+    }
+
+    return [contactById];
+
+}
+
+const removeContact = async (contactId) => {
+    const contacts = await fs.readFile(contactsPath, "utf8");
+    const parsedContacts = JSON.parse(contacts);
+
+    const newList = parsedContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+
+    if (parsedContacts.length === newList.length) {
+      return console.log(`There is no contact with id: ${contactId} to remove`);
+    }
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(newList, null, "\t"),
+      "utf8"
+    );
+
+    const contactsAfterRemove = await fs.readFile(contactsPath, "utf8");
+
+    return JSON.parse(contactsAfterRemove);
+}
+
+const addContact = async (body) => {
+  const {name, email, phone} = body;
+
+  const newContact = { id: id(), name, email, phone };
+
+    const contacts = await fs.readFile(contactsPath, "utf8");
+    const parsedContacts = JSON.parse(contacts);
+
+    const nameToFind = parsedContacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    const emailToFind = parsedContacts.find(
+      (contact) => contact.email.toLowerCase() === email.toLowerCase()
+    );
+    const phoneToFind = parsedContacts.find(
+      (contact) => contact.phone === phone
+    );
+
+    if (nameToFind) {
+      return console.log(`The contact named ${name} already exists`);
+    };
+
+    if (emailToFind) {
+      return console.log(`The contact with email: ${email} already exists`);
+    };
+
+    if (phoneToFind) {
+      return console.log(
+        `The contact with phone number: ${phone} already exists`
+      );
+    };
+
+    const newList = [...parsedContacts, newContact];
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(newList, null, "\t"),
+      "utf8"
+    );
+    
+    return newList;
+}
+
+const updateContact = async (contactId, body) => {
+
+  const contacts = await listContacts();
+
+  const contactToUpdate = await getContactById(contactId);
+
+  const updatedContact = {...contactToUpdate, ...body};
+
+  const newList = [...contacts, updatedContact];
+
+  await fs.writeFile(
+    contactsPath,
+    JSON.stringify(newList, null, "\t"),
+    "utf8"
+  );
+
+  return newList;
+}
 
 module.exports = {
   listContacts,
@@ -16,4 +112,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+} 
