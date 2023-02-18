1c93f41305bdebf2478dea2916d004477bf13c57
kyzym <kyzym1@gmail.com>
2023-02-18 10:40:28 +0100

    add test

--- __tests__/test.js
+++ __tests__/test.js
@@ -12,6 +12,7 @@ const { DB_TEST_HOST, PORT } = process.env;
 
 describe('test auth routes', () => {
   let server;
+  // jest.setTimeout(23000);
 
   beforeAll(() => (server = app.listen(PORT)));
 
@@ -22,43 +23,38 @@ describe('test auth routes', () => {
   });
 
   afterEach((done) => {
-    mongoose.connection.db.dropCollection(() => {
-      mongoose.connection.close(() => done());
-    });
+    mongoose.connection.close(() => done());
   });
 
   test('test login route', async () => {
-    const newUser = {
-      email: 'newKyzym@gmail.com',
+    const loginUser = {
+      email: 'kyzym1@gmail.com',
       password: 'qwe123',
-      verify: true,
-      token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ViOTNlMDU4MDI0ZTQ3Y2JjMDk3YzIiLCJpYXQiOjE2NzYzODMyNDIsImV4cCI6MTY3NjQzNzI0Mn0.fOzCodMe7Z3Ov466NnELDfTE0cnsrZHHvk-cpOn84Zc',
-      avatarURL: 'someUrl.jpeg',
-      verificationCode: 'some123123123123CODe',
     };
 
-    const user = await User.create(newUser);
-
-    const loginUser = {
-      email: 'newKyzym@gmail.com',
+    const user = {
+      email: 'kyzym1@gmail.com',
       password: 'qwe123',
+      _id: '63f09591f0c426364a2673b1',
     };
 
     try {
       const response = await request(app)
         .post('/api/users/login')
         .send(loginUser);
-      console.log(response);
+
       expect(response.statusCode).toBe(200);
       const { body } = response;
 
-      expect(body.token).toByTruthy();
+      expect(body.token).toBeDefined();
 
       const { token } = await User.findById(user._id);
+
       expect(body.token).toBe(token);
     } catch (error) {
-      console.log(`${error}`.bgRed);
+      console.log(
+        `some error in catch ${error.message}`.bgRed
+      );
     }
   });
 });
