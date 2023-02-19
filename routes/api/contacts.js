fbfddc00087623dfa3976552e69a6e7208813b2b
Deniss Simonaits <dsimonaits@gmail.com>
2023-02-19 21:17:50 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,79 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
 
-const router = express.Router()
+const nodeId = require("node-id");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contactsList = await contacts.listContacts();
+    res.json(contactsList);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const contactId = req.params.contactId;
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contactById = await contacts.getContactById(contactId);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!contactById) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(contactById);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-module.exports = router
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, email, phone } = await req.body;
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ message: `missing required ${name} field` });
+    }
+
+    if (!email) {
+      return res
+        .status(400)
+        .json({ message: `missing required ${email} field` });
+    }
+
+    if (!phone) {
+      return res
+        .status(400)
+        .json({ message: `missing required ${phone} field` });
+    }
+
+    const body = {
+      id: nodeId(),
+      name,
+      email,
+      phone,
+    };
+
+    const contact = await contacts.addContact(body);
+    return res.status(201).json(contact);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
