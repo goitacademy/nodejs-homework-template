b3f6ab7617e0a5f3a2ab155277a496a6d4ba040e
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-02-19 23:53:30 +0200

    Jimp avatar

--- controllers/users/controllerUpdateAvatarUser.js
+++ controllers/users/controllerUpdateAvatarUser.js
@@ -1,5 +1,20 @@
-const controllerUpdateAvatarUser = (req, res) => {
-  // const { avatarFile } = req.file;
+const Jimp = require('jimp');
+const path = require('path');
+
+const controllerUpdateAvatarUser = async (req, res) => {
+  const { destination, filename } = req.file;
+  const [name, extention] = filename.split('.');
+  const tmpDir = path.join(destination, filename);
+  const publicAvatar = path.join(__dirname, '..', '..', 'public', 'avatars');
+
+  await Jimp.read(tmpDir)
+    .then(image => {
+      return image.resize(250, 250).write(tmpDir);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
   res.status(200).json({ avatarURL: 'тут будет ссылка на изображение' });
 };
 
