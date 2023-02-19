5f4dd7707c50c2caa70a9f1fc80d52ac433ccb59
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2023-02-19 21:57:35 +0600

    First commit, progress 80%, need to refactor to validation package

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,82 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.resolve(__dirname, "./contacts.json");
+const Generator = require("id-generator");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const res = await JSON.parse(data);
 
-const getContactById = async (contactId) => {}
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const result = JSON.parse(data);
 
-const addContact = async (body) => {}
+    const contact = result.find((evt) => evt.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+    return contact !== undefined ? contact : "Not found";
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const result = JSON.parse(data);
+    const contact = result.filter((evt) => evt.id !== contactId);
+    const findContact = result.find((evt) => evt.id === contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(contact), "utf8");
+
+    return findContact !== undefined ? findContact : "Not found";
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const addContact = async (body) => {
+  const g = new Generator();
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  try {
+    if (!body.name || !body.email || !body.phone) {
+      return "missing required name field";
+    } else {
+      const newContact = {
+        id: g.newId(),
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+      };
+      result.push(newContact);
+      await fs.writeFile(contactsPath, JSON.stringify(result), "utf8");
+      return newContact;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+const updateContact = async (contactId, body) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  const contact = result.filter((evt) => evt.id !== contactId);
+  const findContact = result.find((evt) => evt.id === contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(contact), "utf8");
+
+  try {
+    if (body.name || body.email || body.phone) {
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +84,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
