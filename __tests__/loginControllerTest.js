f1bbd2ebdc0cf3946b559781d03d585d0fe2cbeb
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-02-19 18:49:58 +0200

    Update loginControllerTest.js

--- __tests__/loginControllerTest.js
+++ __tests__/loginControllerTest.js
@@ -1,34 +1,39 @@
-const express = require("express");
 const request = require("supertest");
 const mongoose = require('mongoose');
 require('dotenv').config();
-const { ctrlLogin } = require('../src/controllers/authController');
+const app = require('../app');
+const { login } = require('../src/services/authService');
 
-const app = express();
 mongoose.set('strictQuery', false);
-
-app.post('/api/users/login', ctrlLogin);
 
 describe('Test login controller', () => {
     beforeAll(async () => {
-        app.listen(3000);
-        mongoose.connect(process.env.DB_HOST);
-        console.log('Server running with port 3000');
+        await mongoose.connect(process.env.DB_HOST);
+        console.log('Test database connection successful');
     });
 
     afterAll(async () => {
-        await mongoose.connection.close();
+        await mongoose.disconnect(process.env.DB_HOST);
     });
 
     test('Login return status 201', async () => {
         try {
-            const user = { email: "user@gmail.com", password: "123456" };
+            const email = "user@gmail.com";
+            const password = "123456";
+
+            const data = await login(email, password);
+
             const response = await request(app)
-                .post('/api/users/login')
-                .send(user)
+                .post('/api/auth/users/login')
+                .send(data)
+                .set('Accept', 'application/json');
             
-            console.log(response.status);
-            expect(response.statusCode).toBe(201);
+            const { status } = response;
+            const { token, user } = response.data;
+
+            expect(status).toBe(201);
+            expect(token).toBeDefined();
+            expect(user).toBeDefined();
         } catch (error) {
             console.log(error);
         }   
