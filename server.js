0d4e7fad774cc31cd8fd4c19382aee77b995675f
Kristian <95530852+Krisdziam@users.noreply.github.com>
2023-02-20 18:12:57 +0200

    connect mobgo

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const app = require("./app");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const start = async () => {
+  const client = await MongoClient.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  });
+const db = client.db()
+
+const Contacts = db.collection('contacts');
+const contacts = await Contacts.find({
+
+})  
+console.log(contacts);
+
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000");
+  });
+};
+
+start();
