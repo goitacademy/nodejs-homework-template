f3726f99e7e19bd6d7fbd0d9a645a0a5fc7968ac
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-02-22 00:34:22 +0200

    first commit for checking
    List of contacts, GetById, Remove - works.  need to test add and update

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,85 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const { v4 } = require("uuid");
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const removeContact = async (contactId) => {}
+ 
+// ================== function
+// ============ to get complete list of a contacts from .json file 
+const  listContacts = async () => {
+  const data = await  fs.readFile(contactsPath, "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+    }
+  })
+      const contactList = JSON.parse(data);
+      return contactList;
+}
 
-const addContact = async (body) => {}
+// ===========function to Find Contact by Id  ====================
 
-const updateContact = async (contactId, body) => {}
+const getById = async (contactId) => {
+  const contacts = await listContacts();
+  const contactById = contacts.find(
+    contact => contact.id.trim() === contactId.toString());
+  return contactById || null;
+} 
+  
+
+// ===========function to Remove (Delete) Contact by Id  ====================
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) =>
+    item.id === contactId.toString());
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return result;
+};
+
+
+// ====== function to add contact (push new Object to an array of Objects)
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: v4() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+
+// ===========function to Update(Change) Contact by Id  ====================
+
+const updateContact = async (id, body) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const contactUpdate = { ...contacts[index], ...body };
+   
+    const allContacts = contacts.filter((item) => item.id !== id);
+
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify([...allContacts, contactUpdate], null, 2)
+    );
+    return contactUpdate;
+};
+
+
+// ==== export of functions
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
-}
+};
