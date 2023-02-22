3942aa44d65599c2a5a824ce5def3431d6ab65e8
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-02-22 10:50:53 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://ania:ania@cluster0.goldnse.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+// mongoose.set("strictQurey", true);
+mongoose.set("strictQuery", true);
+
+mongoose.connect(DB_HOST).then(() =>
+  app
+    .listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    }))
+    .catch((error) => {
+      console.log(error.message);
+      process.exit(1);
+    })
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// })
