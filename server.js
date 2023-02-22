b85a265fb42b179b08797afb5188419d9fef3f5e
SerhiiNazarov <algazira89@gmail.com>
2023-02-23 00:31:55 +0200

    add env

--- server.js
+++ server.js
@@ -3,21 +3,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Serhii:FznHbzv2te8Qtxmw@cluster0.sdqsyn2.mongodb.net/?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
