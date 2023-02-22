4bee86a8583100fe8d8edb43712fbf57bc54e7a8
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-02-22 16:44:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
+    console.log("Database connection successful")
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
+    
   });
