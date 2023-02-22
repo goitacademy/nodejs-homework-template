32e7cde47b30b37111706f536425ab657dfb3f92
Yurii <Rapacta@gmail.com>
2023-02-21 18:17:47 -0700

    firs step done

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const cors = require('cors')
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(morgan('tiny'));
 
 app.use('/api/contacts', contactsRouter)
 
@@ -21,5 +21,6 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
+
 
 module.exports = app
--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,55 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const update = async (contact) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contact = await listContacts();
+  const resault = contact.find((item) => item.id === contactId);
+  return resault || null;
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contact = await listContacts();
+  const index = contact.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contact.splice(index, 1);
+  await update(contact);
+  return result;
+}
+
+const addContact = async (body) => {
+    const contact = await listContacts();
+  const newContact = {
+    id: nanoid(4),
+    ...body,
+  };
+  contact.push(newContact);
+  await update(contact);
+  return newContact;
+}
+
+const updateContact = async (contactId, body) => {
+    const books = await listContacts();
+  const index = books.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  books[index] = { contactId, ...body };
+  await update(books);
+  return books[index];
+}
 
 module.exports = {
   listContacts,
