a257c6ed453460307c24e7cf6b540c75bbe11564
SerhiiNazarov <algazira89@gmail.com>
2023-02-22 21:51:08 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+//  FznHbzv2te8Qtxmw
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Serhii:FznHbzv2te8Qtxmw@cluster0.sdqsyn2.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
