0a95255f8580ad24d3bbad08d377a8a43435f103
Nikita Polozov <npolozov0@gmail.com>
2023-02-23 13:02:55 +0200

    fix

--- models/contacts.js
+++ models/contacts.js
@@ -1,21 +1,50 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const uniqid = require("uniqid");
 
-const listContacts = async () => {};
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {
-  console.log("Hello");
+// TODO: задокументировать каждую функцию
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 };
 
-const removeContact = async (contactId) => {};
+const getContactById = async (id) => {
+  const contactId = String(id);
+  const books = await listContacts();
+  const result = books.find((item) => item.id === contactId);
+  return result || null;
+};
 
-const addContact = async (body) => {};
+const removeContact = async (id) => {
+  const contactId = String(id);
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
 
-const updateContact = async (contactId, body) => {};
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: uniqid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const books = require("../../models/contacts.json");
+const contacts = require("../../models/contacts");
 
 router.get("/", async (req, res, next) => {
-  res.json(books);
+  const result = await contacts.listContacts();
+  return res.json(result);
 });
 
 router.get("/:contactId", async (req, res, next) => {
