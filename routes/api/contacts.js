d6c1489e041cd2ad07cfafdbedf30ce010a4801e
SergY29 <yurtin.sergey.v@gmail.com>
2023-02-23 23:49:57 +0200

    add controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,95 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { listContacts, getContactById, addContact, removeContact, updateContact } = require('../../models/contacts')
+
 const { addContactValid, changeContactValid } = require('../../middlewares/validationMiddleware');
+const { getContacts, getContactsById, addNewContact, deleteContact, changeContact } = require('../../controllers/contactsControllers');
 
 
-
-router.get('/', async (req, res, next) => {
-  const data = await listContacts();
-  res.json({
-    status: 'success',
-    code: 200,
-    data,
-  })
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params
-  const data = await getContactById(contactId);
-
-  if (!data) {
-    res.status(404).json({ message: 'Not found' })
-    return
-  }
-
-  res.json({
-    status: 'success',
-    code: 200,
-    data,
-  })
-
-})
-
-router.post('/', addContactValid, async (req, res, next) => {
-  const { name, email, phone } = req.body
-  if (!name) {
-    res.status(400).json({ message: 'missing required name field' })
-    return
-  } if (!email) {
-    res.status(400).json({ message: 'missing required email field' })
-    return
-  } if (!phone) {
-    res.status(400).json({ message: 'missing required phone field' })
-    return
-  }
-
-
-
-  const data = await addContact({ name, email, phone });
-  res.status(201).json({
-    status: 'success',
-    code: 201,
-    data,
-  })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params
-  const result = await removeContact(contactId);
-
-  if (result === -1) {
-    res.status(404).json({ message: 'Not found' })
-    return
-  }
-
-  res.status(200).json({
-    message: 'contact deleted'
-  })
-})
-
-router.put('/:contactId', changeContactValid, async (req, res, next) => {
-  const { contactId } = req.params
-  const { name, email, phone } = req.body
-
-  if (!name || !email || !phone) {
-    res.status(400).json({ message: 'missing fields' })
-    return
-  }
-
-
-
-  const data = await updateContact(contactId, { name, email, phone });
-  if (!data) {
-    res.status(404).json({ message: 'Not found' })
-    return
-  }
-
-  res.json({
-    status: 'success',
-    code: 200,
-    data,
-  })
-})
-
+router.get('/', getContacts)
+router.get('/:contactId', getContactsById)
+router.post('/', addContactValid, addNewContact)
+router.delete('/:contactId', deleteContact)
+router.put('/:contactId', changeContactValid, changeContact)
 module.exports = router
