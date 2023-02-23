e1d196005702e56b25ea159f29d37d43d003de1b
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-02-23 09:54:29 +0200

    evn

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 // const { DB_HOST } = require('./config')
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT= 3000 } = process.env;
 
 // mongoose.set("strictQurey", true);
 mongoose.set("strictQuery", true);
 
-console.log(process.env);
+// console.log(process.env);
 
-mongoose.connect(DB_HOST).then(() =>
-  app
-    .listen(3000, () => {
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
-    }))
-    .catch((error) => {
-      console.log(error.message);
-      process.exit(1);
     })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
