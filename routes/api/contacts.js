6c0b0cdd7de043f0a2d69b20b069ed45ec42f7fa
SergY29 <yurtin.sergey.v@gmail.com>
2023-02-23 20:02:29 +0200

    all without joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -69,7 +69,18 @@ router.delete('/:contactId', async (req, res, next) => {
 router.put('/:contactId', async (req, res, next) => {
   const { contactId } = req.params
   const { name, email, phone } = req.body
+
+  if (!name || !email || !phone) {
+    res.status(400).json({ message: 'missing fields' })
+    return
+  }
+
   const data = await updateContact(contactId, { name, email, phone });
+  if (!data) {
+    res.status(404).json({ message: 'Not found' })
+    return
+  }
+
   res.json({
     status: 'success',
     code: 200,
