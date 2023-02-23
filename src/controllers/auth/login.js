617910e546dbb3c27bbef3d06eba34e7739c099e
Ivan Roschin <ivan.roschin86@gmail.com>
2023-02-23 21:52:17 +0200

    update

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -13,14 +13,12 @@ const login = async (req, res) => {
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "10h" });
   await User.findByIdAndUpdate(user._id, { token });
-  res.status(200).json({
-    data: {
-      token: token,
-      user: {
-        email,
-        subscription,
-        avatarURL: user.avatarURL,
-      },
+  res.json({
+    token,
+    user: {
+      email,
+      subscription,
+      avatarURL: user.avatarURL,
     },
   });
 };
