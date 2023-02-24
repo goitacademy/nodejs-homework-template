983eb4f54e3f7cb650345ac845be6f811c241d68
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-02-24 13:59:41 +0200

    update

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const express = require('express')
+const cors = require('cors');
+const mongoose = reqire('mongoose')
 const app = require('./app')
+reqire('dotenv').config()
+
+app.use(express.json());
+app.use(cors())
 
 const {  PORT = 3000 } = process.env;
 
