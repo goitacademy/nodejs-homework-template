c747cb9425115e8739a7529495b7cceaebc08f2c
Sergvah <104735864+Sergvah@users.noreply.github.com>
2023-02-24 10:33:41 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require('mongoose');
+const DB_HOST = "mongodb+srv://Serhii:W7KNMYqouvzbNmxZ@cluster0.ffz5qsc.mongodb.net/?retryWrites=true&w=majority"
+mongoose.set('strictQuery', false);
+mongoose.connect(DB_HOST)
+.then(() => { app.listen(3000)
+  console.log('Database connection successful')
 })
+.catch(error => {
+    console.log(error.message);
+    process.exit(1)
+})
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+  
+// })
+// /
