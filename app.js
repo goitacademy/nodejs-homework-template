647994922f1c7a2876dd07c6a970e5aa9679ae54
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-02-25 21:56:54 +0200

    add

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-dotenv.config();
-const { DB_HOST, PORT = 3000 } = process.env;
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
+
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-mongoose
-  .connect(DB_HOST)
-  .then(() => app.listen(PORT))
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
 
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
-// app.use((req, res) => {
-//   res.status(404).json({ message: "Not found" });
-// });
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
 
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = "Server error" } = err;
-//   res.status(status).json({ message: err.message });
-// });
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message: err.message });
+});
 
 module.exports = app;
--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-const Joi = require("joi");
+// const Joi = require("joi");
 const contactSchema = Schema({
   name: {
     type: String,
