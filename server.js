4780ee647da6ff2dba182da71e338bb177fd81e2
MaryKotliar <glinskayam@ukr.net>
2023-02-25 12:38:15 +0200

    Init hw03-mongodb

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const DB_HOST =
+  "mongodb+srv://glinskayam:WxUWQz39MJbHWeec@cluster0.ufgupe9.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
