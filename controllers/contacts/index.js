264fa8e19cd93f818a524545501e922f31b56e4f
Oksana Konieva <anaska0489@gmail.com>
2023-02-25 18:44:42 +0200

    update

---
+++ controllers/contacts/index.js
@@ -0,0 +1,15 @@
+const getAll = require("./getAll");
+const addContact = require("./addContact");
+const getById = require("./getById");
+const updateById = require("./updateById");
+const removeById = require("./removeById");
+const updateStatusContact = require("./updateStatusContact");
+
+module.exports = {
+  getAll,
+  addContact,
+  getById,
+  updateById,
+  removeById,
+  updateStatusContact
+};
