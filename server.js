dfd5e49a2494495d6442d276fba7fd0ac28c09d2
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-02-25 21:41:12 +0200

    connect mongoose

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(
+    'mongodb+srv://Tvinlee:lSnidN4VAebBsDqN@cluster0.rqfgtfx.mongodb.net/db-contacts?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(3000);
+    console.log('database connect sucsess');
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
