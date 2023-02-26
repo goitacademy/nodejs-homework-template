675cf0d58f0154b9461bbeec97fae13c02f91d97
EkaterinaKononenko <kaktysik4@ukr.net>
2023-02-26 22:02:47 +0200

    feat: add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://User:123456Qwer@cluster0.tmotho5.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000, () => {
+    console.log("Database connection successful");
+  }))
+  .catch((error) => {console.log(error.message);
+process.exit(1)});
+
+const app = require("./app");
+
+
