559a62c6638ea558ba3d0ccfe2012387c7eb8a86
PanRoman1 <104255693+PanRoman1@users.noreply.github.com>
2023-02-26 20:26:44 +0200

    hw3

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
