cf3e8bfb55b2b990631e46fd4a65a65fc4e297b5
jkl303 <303vladzoz@gmail.com>
2023-02-26 15:57:26 +0200

    setuped

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// const { DB_HOST, PORT = 3000 } = process.env;
+
+const DB_HOST =
+  "mongodb+srv://303vlad:EYF3TJOC18YKtb2f@cluster0.05vjugd.mongodb.net/db-contacts";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
