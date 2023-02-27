70c5ea48598bb61b5c47ef66fe66b5a599a7fb48
Rostyslav Kononenko <rastikon@gmail.com>
2023-02-27 19:36:15 +0100

    hw05_done

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const { User } = require("../../models");
 
@@ -8,8 +9,13 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`Email ${email} in use`);
   }
+  const avatarURL = gravatar.url(email);
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  const result = await User.create({ email, password: hashPassword });
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({
     status: "Created",
     code: 201,
@@ -17,6 +23,7 @@ const register = async (req, res) => {
       user: {
         email,
         password,
+        avatarURL,
       },
     },
   });
