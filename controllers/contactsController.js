30a8be3e371dbd69f505c0c9fea19510ffb6b17a
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-02-28 00:11:01 +0200

    start working on mongodb

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,59 +1,61 @@
-const { HttpError } = require("../helpers");
+// const { HttpError } = require("../helpers");
 const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  // listContacts,
+  // getContactById,
+  // removeContact,
+  // addContact,
+  // updateContact,
+  Contacts,
 } = require("../models/contacts");
 
 const getContacts = async (req, res, next) => {
-  const contacts = await listContacts();
+  const contacts = await Contacts.find({});
+  console.log(contacts);
   res.status(200).json(contacts);
 };
 
-const getContact = async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-  if (!contact) {
-    next(HttpError(404, "Not found"));
-  }
-  return res.status(200).json(contact);
-};
+// const getContact = async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const contact = await getContactById(contactId);
+//   if (!contact) {
+//     next(HttpError(404, "Not found"));
+//   }
+//   return res.status(200).json(contact);
+// };
 
-const postContact = async (req, res, next) => {
-  const body = req.body;
-  if (!body.name || !body.email || !body.phone) {
-    return res.status(400).json({ message: "missing required name field" });
-  }
-  const contact = await addContact(body);
-  res.status(201).json(contact);
-};
+// const postContact = async (req, res, next) => {
+//   const body = req.body;
+//   if (!body.name || !body.email || !body.phone) {
+//     return res.status(400).json({ message: "missing required name field" });
+//   }
+//   const contact = await addContact(body);
+//   res.status(201).json(contact);
+// };
 
-const deleteContact = async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-  if (!contact) {
-    next(HttpError(404, "Not found"));
-  }
-  await removeContact(contactId);
-  res.status(200).json({ message: "contact deleted" });
-};
+// const deleteContact = async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const contact = await getContactById(contactId);
+//   if (!contact) {
+//     next(HttpError(404, "Not found"));
+//   }
+//   await removeContact(contactId);
+//   res.status(200).json({ message: "contact deleted" });
+// };
 
-const putContact = async (req, res, next) => {
-  const { contactId } = req.params;
-  const body = req.body;
-  const contact = await updateContact(contactId, body);
-  if (!contact) {
-    return next(HttpError(404, "Not found"));
-  }
-  res.status(200).json(contact);
-};
+// const putContact = async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const body = req.body;
+//   const contact = await updateContact(contactId, body);
+//   if (!contact) {
+//     return next(HttpError(404, "Not found"));
+//   }
+//   res.status(200).json(contact);
+// };
 
 module.exports = {
-  getContact,
   getContacts,
-  postContact,
-  putContact,
-  deleteContact,
+  // getContact,
+  // postContact,
+  // putContact,
+  // deleteContact,
 };
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { HOST_URL } = process.env;
+
+const main = async () => {
+  try {
+    await mongoose.connect(HOST_URL);
+    console.log("Database connection successful");
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (error) {
+    console.error("Main failed", error.message);
+  }
+};
+main();
