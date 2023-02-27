662c7ac0d4ee529108c791f2c1f9bfe14874e287
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-02-27 15:09:13 +0200

    add fixes after review

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-
-// const router = express.Router();
-
-// const { contactsController } = require("../../controllers");
-
-// const { isValidId } = require("../../middlewares");
-
-// router.get("/", contactsController.listContacts);
-
-// router.get("/:id", isValidId, contactsController.getContactById);
-
-// router.post("/", contactsController.addContact);
-
-// router.delete("/:id", isValidId, contactsController.removeContact);
-
-// router.put("/:id", isValidId, contactsController.updateContact);
-
-// router.patch("/:id/favorite", isValidId, contactsController.updateStatusContact);
-
-// module.exports = router;
-
-///////////////////////////////////
-
 const express = require("express");
 
 const contactsRouter = express.Router();
