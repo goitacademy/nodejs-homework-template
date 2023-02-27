96f33dc30bfca1f19391565bbc6077440154687c
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-02-27 22:57:07 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,22 +1,29 @@
 const { Schema, model } = require("mongoose");
-const contactSchema = Schema({
-  password: {
-    type: String,
-    required: [true, "Set password for user"],
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+      minlength: 6,
+      maxlength: 15,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: "users",
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: String,
-  owner: {
-    type: SchemaTypes.ObjectId,
-    ref: "user",
-  },
-});
+  { versionKey: false, timestamps: true }
+);
+const User = model("user", userSchema);
+module.exports = { User };
