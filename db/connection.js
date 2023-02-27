e7a5f2a190c1fbfa762ae772c62e68979a674e78
RuslanDakeshev <bil08@outlook.com>
2023-02-27 01:09:18 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ dotenv.config();
 const connectMongoose = async () => {
     try {
       
-    // await mongoose.connect(`mongodb+srv://bil08:xQdJzpDH5vrNS3jU@cluster0.toyzwsp.mongodb.net/db-contacts?retryWrites=true&w=majority`);
+
         await mongoose.connect(process.env.MONGO_URL);
 
     console.log(`Database connection successful`);
