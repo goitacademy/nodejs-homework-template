1099806b8588c5442acdbc0081bc9a1f719e0270
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-02-27 18:35:11 +0200

    validErrors, host

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors');
-const mongoose = reqire('mongoose')
-const app = require('./app')
-reqire('dotenv').config()
+const mongoose = require('mongoose');
 
-app.use(express.json());
-app.use(cors())
+const app = require('./app');
 
-const {  PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(PORT))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
