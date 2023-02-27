d09bf4b8423dd74621227a378a4efd04c8a19af0
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-02-27 18:16:48 +0200

    controllers

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,55 +1,78 @@
-// const { V4MAPPED } = require('dns');
-// const fs = require('fs/promises');
-// const path = require('path');
-// const { v4 } = require('uuid');
-
 const { Contact } = reqire('../shemas/contacts.js');
 
-// const updateContacts = require('./updateContacts');
+const { HttpError } = require("../../routes/api/helpers");
 
-// const contactsPath = path.join(__dirname, 'contacts.json');
-
-const listContacts = async (req, res) => {
+const listContacts = async (req, res, next) => {
   const contact = Contact.find({});
-  res.json(contact);
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: contact,
+    },
+  });
 };
 
-const getContactById = async (req, res) => {
+const getContactById = async (req, res, next) => {
     const { id } = req.params;
-    const contact = await Contact.findById()
+    const contact = await Contact.findById(id);
+
+    if (!contact) {
+        return res.status(400).json({
+            status: 'Not found'
+        })
+    }
 };
 
-const removeContact = async contactId => {
-//   const allContacts = await listContacts();
-//   const idx = allContacts.findIndex(contact => contact.id === contactId);
-//   if (idx === -1) {
-//     return null;
-//   }
-//   const newContacts = allContacts.filter((_, index) => index !== idx);
-//   await updateContacts(newContacts);
-//   return allContacts[idx];
+const removeContact = async (req, res, next) => {
+    const { contactId } = req.params;
+    await Contact.findByIdAndRemove(contactId);
+    if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: result,
+    },
+  });
 };
 
-const addContact = async (req, res) => {
+const addContact = async (req, res, next) => {
   const contact = new Contact({
     name,
     phone,
     email,
   }) = req.body;
-  await contact.save();
-  res.json({ status: 'success' });
+  const result = await contact.save();
+  res.status(201).json({
+    status: "success",
+      code: 201,
+      data: {
+        result,
+    }
+  });
 };
 
-const updateContact = async (id, data) => {
-//   const allContacts = await listContacts();
-//   const idx = allContacts.findIndex(contact => contact.id === id);
-//   if (idx === -1) {
-//     return null;
-//   }
-//   allContacts[idx] = { id, ...data };
-//   await updateContacts(allContacts);
-
-//   return allContacts[idx];
+const updateContact = async (req, res, next) => {
+const { contactId } = req.params;
+const contact = new Contact({
+    name,
+    phone,
+    email,
+}) = req.body;
+    
+   const result =  await Contact.findByIdAndUpdate(id, {
+        $set: { name, phone, email }
+    })
+    res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
 
 module.exports = {
