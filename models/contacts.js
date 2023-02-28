f563ad2c995382367cd3d360f152cce636da8ce1
OlenaBielova <belova.elena11@gmail.com>
2023-02-28 11:49:17 +0200

    hw2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,58 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path");
+const uuid = require("uuid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (id) => {
+    const contactId = String(id);
+    const allContacts = await listContacts();
+    const contact = allContacts.find(contact => contact.id === contactId);
+  return contact || null;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (id) => {
+    const contactId = String(id);
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex(contact => contact.id === contactId);
+    const deletedContact = allContacts[index];
 
-const updateContact = async (contactId, body) => {}
+    if (index !== -1) {
+        allContacts.splice(index, 1);
+        
+        await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    }
+  return deletedContact || null;
+}
+
+const addContact = async (data) => {
+    const newContact = {
+        id: uuid.v4(),
+        ...data
+    };
+    const allContacts = await listContacts();
+    allContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+}
+
+const updateContact = async (id, data) => {
+  const contactId = String(id);
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex(contact => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  allContacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return allContacts[index];
+}
 
 module.exports = {
   listContacts,
