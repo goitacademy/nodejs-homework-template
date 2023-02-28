604125c6f4dd6f14022801ee65fe26644ba6fa04
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-02-28 16:54:52 +0200

    add

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,18 +1,22 @@
 const { Conflict } = require("http-errors");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
 const { User } = require("../../models");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if (!user) {
-    throw new Unauthorized(`Email ${email} not found`);
+  if (!user || !user.comparePassword(password)) {
+    throw new Unauthorized("Email or password is wrong!");
   }
-  const passCompare = bcrypt.compareSync(password, user.password);
-  if (!passCompare) {
-    throw new Unauthorized(`Email or password is wrong`);
-  }
+
+  // if (!user) {
+  //   throw new Unauthorized(`Email ${email} not found`);
+  // }
+  // const passCompare = bcrypt.compareSync(password, user.password);
+  // if (!passCompare) {
+  //   throw new Unauthorized(`Email or password is wrong`);
+  // }
 };
 
 module.exports = login;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
+const bcrypt = require("bcryptjs");
+
 const userSchema = Schema(
   {
     password: {
@@ -26,6 +28,11 @@ const userSchema = Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 const joiRegisterSchema = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().min(6).required(),
