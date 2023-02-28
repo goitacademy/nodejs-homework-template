49f57b123a9e9112d843e4fc08a4c22756b972d9
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-02-28 06:07:29 +0200

    + post, getById, delete

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,12 +1,5 @@
-// const { HttpError } = require("../helpers");
-const {
-  // listContacts,
-  // getContactById,
-  // removeContact,
-  // addContact,
-  // updateContact,
-  Contacts,
-} = require("../models/contacts");
+const { HttpError } = require("../helpers");
+const { Contacts } = require("../models/contacts");
 
 const getContacts = async (req, res, next) => {
   const contacts = await Contacts.find({});
@@ -14,33 +7,30 @@ const getContacts = async (req, res, next) => {
   res.status(200).json(contacts);
 };
 
-// const getContact = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const contact = await getContactById(contactId);
-//   if (!contact) {
-//     next(HttpError(404, "Not found"));
-//   }
-//   return res.status(200).json(contact);
-// };
+const getContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await Contacts.findById(contactId);
+  if (!contact) {
+    next(HttpError(404, "Not found"));
+  }
+  return res.status(200).json(contact);
+};
 
-// const postContact = async (req, res, next) => {
-//   const body = req.body;
-//   if (!body.name || !body.email || !body.phone) {
-//     return res.status(400).json({ message: "missing required name field" });
-//   }
-//   const contact = await addContact(body);
-//   res.status(201).json(contact);
-// };
+const postContact = async (req, res, next) => {
+  const body = req.body;
+  const contact = await Contacts.create(body);
+  res.status(201).json(contact);
+};
 
-// const deleteContact = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const contact = await getContactById(contactId);
-//   if (!contact) {
-//     next(HttpError(404, "Not found"));
-//   }
-//   await removeContact(contactId);
-//   res.status(200).json({ message: "contact deleted" });
-// };
+const deleteContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await Contacts.findById(contactId);
+  if (!contact) {
+    next(HttpError(404, "Not found"));
+  }
+  await Contacts.findByIdAndRemove(contactId);
+  res.status(200).json({ message: "contact deleted" });
+};
 
 // const putContact = async (req, res, next) => {
 //   const { contactId } = req.params;
@@ -54,8 +44,8 @@ const getContacts = async (req, res, next) => {
 
 module.exports = {
   getContacts,
-  // getContact,
-  // postContact,
+  getContact,
+  postContact,
   // putContact,
-  // deleteContact,
+  deleteContact,
 };
