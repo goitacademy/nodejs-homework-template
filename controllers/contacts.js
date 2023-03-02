5aa018db87f468e6803e2f45cd928d993be20f25
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-02 14:37:52 +0200

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,51 +1,72 @@
-const contacts = require("../models/contacts");
+const Contact = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-const listContacts = async (req, res) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+const listContacts = async(req, res) => {
+    const result = await Contact.find();
+    res.json(result);
 };
 
-const getContactById = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+const getContactById = async(req, res) => {
+    const { contactId } = req.params;
+    const result = await Contact.findById(contactId);
 
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
 
-  res.json(result);
+    res.json(result);
 };
 
-const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
-  res.status(201).json(result);
-};
-const removeContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-
-  res.json({ message: "Delete success" });
+const addContact = async(req, res) => {
+    const result = await Contact.create(req.body);
+    res.status(201).json(result);
 };
 
-const updateContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+const removeContact = async(req, res) => {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndRemove(contactId);
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
 
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
+    res.json({ message: "Delete success" });
+};
 
-  res.json(result);
+const updateContact = async(req, res) => {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(
+        contactId,
+        req.body, { new: true }
+    );
+
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+
+    res.json(result);
+};
+
+const updateStatusContact = async(req, res) => {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(
+        contactId,
+        req.body, {
+            new: true,
+        }
+    );
+
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+
+    res.json(result);
 };
 
 module.exports = {
-  listContacts: ctrlWrapper(listContacts),
-  getContactById: ctrlWrapper(getContactById),
-  addContact: ctrlWrapper(addContact),
-  removeContact: ctrlWrapper(removeContact),
-  updateContact: ctrlWrapper(updateContact),
+    listContacts: ctrlWrapper(listContacts),
+    getContactById: ctrlWrapper(getContactById),
+    addContact: ctrlWrapper(addContact),
+    removeContact: ctrlWrapper(removeContact),
+    updateContact: ctrlWrapper(updateContact),
+    updateStatusContact: ctrlWrapper(updateStatusContact),
 };
