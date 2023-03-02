e7ce2cc62a05ed36f6183821585fcdcb39675871
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-02 14:37:48 +0200

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
+const handleMongooseError = require("./handleMongooseError");
 
-module.exports = { HttpError, ctrlWrapper };
+module.exports = { HttpError, ctrlWrapper, handleMongooseError };
