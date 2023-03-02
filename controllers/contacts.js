a7fcd59b196a3a46f9821c8b558a08fa8b4cacb4
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-02 16:57:33 +0200

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/contacts");
+const { Contact } = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async(req, res) => {
