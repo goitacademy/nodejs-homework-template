d8d4ef8eb5abf472a2669c9b45e5c12e1a670d2e
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-02 17:11:41 +0200

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -4,7 +4,7 @@ const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
     const { contactId } = req.params;
-    if (isValidObjectId(contactId)) {
+    if (!isValidObjectId(contactId)) {
         next(HttpError(400, `${contactId} is not valid id`));
     }
     next();
