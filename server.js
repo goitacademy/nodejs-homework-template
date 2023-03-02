a32b84e9f5ff7cb4a1b059b81615a25a20159d58
Anakole <104356239+Anakole@users.noreply.github.com>
2023-03-02 16:57:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000);
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+    .connect(DB_HOST)
+    .then(() => {
+        app.listen(PORT);
+    })
+    .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+    });
