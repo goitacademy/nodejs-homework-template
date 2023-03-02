650d94eaad1c460b033faffe8e58f732e76b70ea
Yura <104792276+Yura-111@users.noreply.github.com>
2023-03-02 17:05:17 +0200

    Add

---
+++ controllers/contacts.js
@@ -0,0 +1,63 @@
+const { Contact } = require("../models/contact");
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const getAll = async (req, res,) => {
+  const result = await Contact.find();
+  res.status(200).json(result);
+};
+
+const getById = async (req, res) => {
+  const result = await Contact.findById(req.params.contactId);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json(result);
+};
+
+const add = async (req, res) => {
+  const data = req.body;
+
+  const result = await Contact.create(data);
+  res.status(201).json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
+const upDateById = async (req, res) => {
+  const data = req.body;
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, data, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+const upDateFavorite = async (req, res) => {
+  const data = req.body;
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, data, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  deleteById: ctrlWrapper(deleteById),
+  upDateById: ctrlWrapper(upDateById),
+  upDateFavorite: ctrlWrapper(upDateFavorite),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,29 @@
-const express = require('express')
+const express = require("express");
+const ctrl = require("../../controllers/contacts");
+const { validateBody, isValidId } = require("../../midlewares");
+const { schemas } = require("../../models/contact");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.getAll);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateBody(schemas.contactsSchema), ctrl.add);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", isValidId, ctrl.deleteById);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.contactsSchema),
+  ctrl.upDateById
+);
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.upDateFavorite
+);
 
-module.exports = router
+module.exports = router;
