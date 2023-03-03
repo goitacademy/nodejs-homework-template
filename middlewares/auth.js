3e42f017a9314461d1479ff352321de5495bbc4e
Tonya-Shyva <toshash69@gmail.com>
2023-03-03 09:52:28 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,16 +5,15 @@ const { SECRET_KEY } = process.env;
 const HttpError = require("./HttpError");
 
 const getCurrent = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401, "Not authirized"));
+  }
   try {
-    const { authorization = "" } = req.headers;
-    const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
-      console.log(bearer !== "Bearer");
-      throw HttpError(401, "Not authirized");
-    }
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) {
+    if (!user || !user.token || user.token !== token) {
       throw HttpError(401, "Not authirized");
     }
     req.user = user;
