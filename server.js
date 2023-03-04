683808c7e861b3b80bba0135fd08ed2c57a4c1ab
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-04 16:43:51 +0200

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const mongoose = require("mongoose");
-const DB_HOST = require("./config");
+const mongoose = require('mongoose')
+const DB_HOST = process.env;
 
 mongoose.set("strictQuery", true);
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => app.listen(3000, () => {
+mongoose.connect(DB_HOST).then(() => app.listen(3000, () => {
     console.log("Database connection successful");
   }))
   .catch((error) => {console.log(error.message);
