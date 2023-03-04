6f0a11cd4e1b078d873365f76a9a196dafc9a983
SlavaHlushko <slavahlushko90@gmail.com>
2023-03-04 19:48:36 +0200

    1

--- models/contacts.js
+++ models/contacts.js
@@ -1,63 +1,75 @@
-const fs = require("fs/promises");
-const path = require("path");
+// const fs = require("fs/promises");
+// const path = require("path");
 
-const { nanoid } = require("nanoid");
+// const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "../models/contacts.json");
+// const contactsPath = path.join(__dirname, "../models/contacts.json");
 
-const listContacts = async () => {
-  const result = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(result);
-};
+// const listContacts = async () => {
+//   const result = await fs.readFile(contactsPath, "utf-8");
+//   return JSON.parse(result);
+// };
 
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === contactId);
-  return result || null;
-};
+// const getContactById = async (contactId) => {
+//   const contacts = await listContacts();
+//   const result = contacts.find((item) => item.id === contactId);
+//   return result || null;
+// };
 
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
+// const removeContact = async (contactId) => {
+//   const contacts = await listContacts();
+//   const index = contacts.findIndex((item) => item.id === contactId);
+//   if (index === -1) {
+//     return null;
+//   }
 
-  const [result] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
-};
+//   const [result] = contacts.splice(index, 1);
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   return result;
+// };
 
-const addContact = async (body) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    ...body,
-  };
-  contacts.push(newContact);
+// const addContact = async (body) => {
+//   const contacts = await listContacts();
+//   const newContact = {
+//     id: nanoid(),
+//     ...body,
+//   };
+//   contacts.push(newContact);
 
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-  return newContact;
-};
+//   return newContact;
+// };
 
-const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
+// const updateContact = async (contactId, body) => {
+//   const contacts = await listContacts();
+//   const index = contacts.findIndex((item) => item.id === contactId);
+//   if (index === -1) {
+//     return null;
+//   }
 
-  const updatedContact = { ...contacts[index], ...body };
-  contacts.splice(index, 1, updatedContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return updatedContact;
-};
+//   const updatedContact = { ...contacts[index], ...body };
+//   contacts.splice(index, 1, updatedContact);
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   return updatedContact;
+// };
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
+
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Slava:O2OZZX4wBjQy2n2S@cluster0.pmj6ulj.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch((error) => console.log(error.message));
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Slava:O2OZZX4wBjQy2n2S@cluster0.pmj6ulj.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
