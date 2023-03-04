e352a6d32d5bf35beed98454e0a61a324d1982ca
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-04 17:34:13 +0200

    add PORT

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose')
-const DB_HOST = process.env;
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST).then(() => app.listen(3000, () => {
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
     console.log("Database connection successful");
-  }))
-  .catch((error) => {console.log(error.message);
-process.exit(1)});
-
-const app = require("./app");
+  });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 
