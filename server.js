e42f68b10c78f940e4108ac5ddc2f4575a30cdd5
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-03-04 20:58:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 const app = require("./src/app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
-  .connect(DB_HOST)
-  .then(app.listen(PORT, () => console.log("Database connect success")))
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log("Database connect success")))
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
