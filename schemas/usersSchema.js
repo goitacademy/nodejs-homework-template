5d23bbe09e9a6c161d779599206163886156a9f6
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-03-06 19:38:01 +0100

    add pagination, fix names, jwtpassword

---
+++ schemas/usersSchema.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const custom = Joi.defaults(() =>
+  Joi.object({
+    password: Joi.string(),
+    email: Joi.string().email(),
+    subscription: Joi.string().valid('starter', 'pro', 'business'),
+  })
+);
+
+const atLeastOne = custom.object().or('password', 'email', 'subscription');
+const allRequired = custom
+  .object()
+  .options({ presence: 'required' })
+  .required();
+
+module.exports = {
+  atLeastOne,
+  allRequired,
+};
