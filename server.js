086a513062e43129d2ee59e05dbaa8df5f4cad34
Yura <104792276+Yura-111@users.noreply.github.com>
2023-03-06 21:39:04 +0200

    Add

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const mongoose = require("mongoose");
 
-// const app = require('./app');
+const app = require('./app');
 
-// const { DB_HOST } = process.env;
-// const { PORT } = process.env;
+const { DB_HOST } = process.env;
+const { PORT } = process.env;
 
-// mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true);
 
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     app.listen(PORT);
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
-const DB_HOST = "mongodb+srv://zagorskyura:iQGERVi79iiQVJNl@cluster0.sqgu1xp.mongodb.net/db-contacts?retryWrites=true&w=majority"
- mongoose.connect(DB_HOST)
