71c04007abd0f21c1aedd2bad3eff55ccc11573c
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-03-06 14:28:58 +0200

    validation

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string().required().min(7).max(12).pattern(/^\d+$/),
 });
 
 const updateSchema = Joi.object({
