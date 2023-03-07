d6d6f31334c94512444a96fc4eab53f9aff27786
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-03-07 15:41:24 +0100

    commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,37 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-  },
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email is required'],
     unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
   },
   subscription: {
     type: String,
     enum: ["starter", "pro", "business"],
     default: "starter"
   },
-  token: {
-    type: String,
-    default: null,
-  },
 });
 
-const User = model('user', userSchema);
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
 
-module.exports = User;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt, null);
+  next();
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
