1e9870da181343b7e63d07535dc21011a7fa9d43
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2023-03-07 11:51:47 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -5,33 +5,56 @@ const crypto = require("crypto");
 const contactsPath = path.resolve("models", "contacts.json");
 
 const listContacts = async () => {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
-    return error;
-  }
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    const [contact] = JSON.parse(data).filter((cont) => cont.id === contactId);
-    if (contact) {
-      return contact;
-    }
-  } catch (error) {
-    return error;
+  const data = await fs.readFile(contactsPath, "utf8");
+  const [contact] = JSON.parse(data).filter((cont) => cont.id === contactId);
+  if (contact) {
+    return contact;
   }
 };
 
 const removeContact = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    const contacts = JSON.parse(data).filter(
-      (contact) => contact.id !== contactId
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contacts = JSON.parse(data).filter(
+    (contact) => contact.id !== contactId
+  );
+  if (JSON.parse(data).length > contacts.length) {
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, null, 2),
+      "utf-8"
     );
-    if (JSON.parse(data).length > contacts.length) {
+    return contacts;
+  }
+};
+
+const addContact = async (body) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  body.id = crypto.randomUUID();
+  const contacts = JSON.parse(data);
+  contacts.push(body);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+  return contacts;
+};
+
+const updateContact = async (contactId, body) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contacts = JSON.parse(data);
+  contacts.forEach(async (contact) => {
+    if (contact.id === contactId) {
+      if (body.name) {
+        contact.name = body.name;
+      }
+      if (body.email) {
+        contact.email = body.email;
+      }
+      if (body.phone) {
+        contact.phone = body.phone;
+      }
       await fs.writeFile(
         contactsPath,
         JSON.stringify(contacts, null, 2),
@@ -39,55 +62,8 @@ const removeContact = async (contactId) => {
       );
       return contacts;
     }
-  } catch (error) {
-    return error;
-  }
-};
-
-const addContact = async (body) => {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    body.id = crypto.randomUUID();
-    const contacts = JSON.parse(data);
-    contacts.push(body);
-    await fs.writeFile(
-      contactsPath,
-      JSON.stringify(contacts, null, 2),
-      "utf-8"
-    );
-    return contacts;
-  } catch (error) {
-    return error;
-  }
-};
-
-const updateContact = async (contactId, body) => {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    const contacts = JSON.parse(data);
-    contacts.forEach(async (contact) => {
-      if (contact.id === contactId) {
-        if (body.name) {
-          contact.name = body.name;
-        }
-        if (body.email) {
-          contact.email = body.email;
-        }
-        if (body.phone) {
-          contact.phone = body.phone;
-        }
-        await fs.writeFile(
-          contactsPath,
-          JSON.stringify(contacts, null, 2),
-          "utf-8"
-        );
-        return contacts;
-      }
-    });
-    return contacts;
-  } catch (error) {
-    return error;
-  }
+  });
+  return contacts;
 };
 
 module.exports = {
