8217ae0a265f6f7baac3cce61e2ef20b40893c23
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-03-07 14:30:27 +0100

    commit

---
+++ src/models/contact.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone is required'],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+});
+
+const Contact = model('contact', contactSchema);
+
+module.exports = Contact;
