df9d12a9312e7049d473432702fcefb4eddd00ff
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2023-03-07 14:39:53 +0200

    add changes
    add file users and usersRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
+const usersRouter = require("./routes/api/users");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -15,7 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
+
+app.use(res => {
+  res.status(200).json({ message: "Success" });
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
