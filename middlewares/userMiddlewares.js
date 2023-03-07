6bd0ad6bd3550aa604c2285de7e59aae7ec96af7
Olevova <olevova1983@gmail.com>
2023-03-07 20:24:59 +0200

    some part

--- middlewares/userMiddlewares.js
+++ middlewares/userMiddlewares.js
@@ -1,10 +1,12 @@
 const checkUserDate = (schema) => {
     return (req, res, next) => {
         const { error } = schema.validate(req.body);
-        console.log(schema.validate(req.body));
+        const field = error.message.split('"')[1];
+        console.log(field);
+        const message = `missing required ${field} field`
         if (error) {
             res.status(400).json({
-                message: "missing required name field"})
+                message,})
             next(error);
             return
         }
--- schema/userschema.js
+++ schema/userschema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const phoneRegexp = /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
 
 const userSchema = Joi.object({
-    name: Joi.string().required(),
+    name: Joi.string().required('ok'),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     phone: Joi.string().pattern(phoneRegexp).required()
 });
