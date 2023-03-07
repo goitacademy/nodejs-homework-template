8e6cc888a9da1e9d472cc2cb873d9b4c1a950963
Andry1343 <ua.gavrilets@gmail.com>
2023-03-07 21:59:26 +0200

    Done 03-mongodb

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Andrew:PnqjPj3qRUNly0MJ@database.1xc8rqm.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful")
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
