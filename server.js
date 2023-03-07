5bfdc8ce1ed43d09151cb3e9cfec468ac1cff065
ElenaObukhova1984 <e.obukhova84@gmail.com>
2023-03-07 21:29:48 +0200

    added mongoose, db_host

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose');
+
+const DB_HOST = 'mongodb+srv://Elena:GlSnjjtUyfQO5HqM@cluster0.lmairny.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(error => {
+    console.log(error.mesage);
+    process.exit(1);
+  })
+
