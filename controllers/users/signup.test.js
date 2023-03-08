ae3df4488c4fec6748182f069f71aff8512f1d49
Inna <isko.inna.94@gmail.com>
2023-03-08 13:36:45 +0200

    5

--- controllers/users/signup.test.js
+++ controllers/users/signup.test.js
@@ -3,17 +3,18 @@ const express = require("express");
 const request = require("supertest");
 
 const app = express();
-app.post("/users/login", signup);
+app.post("/users/signup", signup);
 
 describe("test is signup", () => {
   beforeAll(() => {
     app.listen(3000);
   });
+
   // afterAll(() => {
-  //   app.exit(1);
+  //   app.close();
   // });
   test("Status: 201 OK", async () => {
-    const result = await request(app).post("/users/login");
+    const result = await request(app).post("api/users/signup");
     expect(result.status).toBe(201);
   });
 });
