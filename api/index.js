9ed7272f6199373a1880d528996c5f159aed90c7
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-03-08 18:29:18 +0200

    final

--- api/index.js
+++ api/index.js
@@ -1,24 +1,40 @@
 const express = require('express');
-const controllersContacts = require('../controllers');
+const { contactsCntr } = require('../controllers/')
 const { validateBody, isValidId } = require("../middlewares");
 const {allSchemas} = require("../model/contactSchema")
 
 const router = express.Router();
 
-router.get('/', controllersContacts.get);
+router.get(
+    '/',
+    contactsCntr.get
+);
 
-router.get('/:id', isValidId, controllersContacts.getById);
+router.get(
+    '/:id',
+    isValidId,
+    contactsCntr.getById
+);
 
-router.post('/', validateBody(allSchemas.addJoiSchema), controllersContacts.create);
+router.post(
+    '/',
+    validateBody(allSchemas.addJoiSchema),
+    contactsCntr.create
+);
 
-router.put('/:id', isValidId, validateBody(allSchemas.addJoiSchema), controllersContacts.change);
+router.put(
+    '/:id',
+    isValidId,
+    validateBody(allSchemas.addJoiSchema),
+    contactsCntr.change
+);
 
 router.patch(
     "/:id/favorite",
     isValidId,
     validateBody(allSchemas.updateFovoriteJoiSchema),
-    controllersContacts.favorite);
+    contactsCntr.favorite);
 
-router.delete('/:id', isValidId, controllersContacts.remove)
+router.delete('/:id', isValidId, contactsCntr.remove)
 
 module.exports = router
