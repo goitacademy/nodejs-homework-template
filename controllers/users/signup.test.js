ed2311317a3741d5fc6e4605b40946e03cb5e88b
Inna <isko.inna.94@gmail.com>
2023-03-08 16:46:57 +0200

    5

--- controllers/users/signup.test.js
+++ controllers/users/signup.test.js
@@ -1,20 +1,23 @@
-const signup = require("./signup");
-const express = require("express");
-const request = require("supertest");
+// const signup = require("./signup");
+// const express = require("express");
+// const request = require("supertest");
 
-const app = express();
-app.post("/users/signup", signup);
+// jest.useRealTimers();
 
-describe("test is signup", () => {
-  beforeAll(() => {
-    app.listen(3000);
-  });
+// const app = express();
+// app.post("/api/users/signup", signup);
 
-  // afterAll(() => {
-  //   app.close();
-  // });
-  test("Status: 201 OK", async () => {
-    const result = await request(app).post("api/users/signup");
-    expect(result.status).toBe(201);
-  });
-});
+// describe("test is signup", () => {
+//   beforeAll(() => {
+//     app.listen(3000);
+//   });
+
+//   // afterAll(() => {
+//   //   process.exit(1);
+//   // });
+//   test("Status: 201 OK", async () => {
+//     const response = await request(app).post("/api/users/signup");
+//     console.log(response);
+//     expect(response.status).toBe(201);
+//   });
+// });
