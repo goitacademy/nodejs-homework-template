6d2c64ad5356f06e334a6db3fbf4467d651436fe
Yura <104792276+Yura-111@users.noreply.github.com>
2023-03-09 00:07:10 +0200

    Add

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,13 +1,13 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res, next) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find();
   res.status(200).json(result);
 };
 
 const getById = async (req, res, next) => {
-  const result = await contacts.getContactById(req.params.contactId);
+  const result = await Contact.findById(req.params.contactId);
   if (!result) {
     throw HttpError(404, "Not Found");
   }
@@ -17,13 +17,13 @@ const getById = async (req, res, next) => {
 const add = async (req, res, next) => {
   const data = req.body;
 
-  const result = await contacts.addContact(data);
+  const result = await Contact.create(data);
   res.status(201).json(result);
 };
 
 const deleteById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw HttpError(404, "Not Found");
   }
@@ -32,9 +32,21 @@ const deleteById = async (req, res, next) => {
 
 const upDateById = async (req, res, next) => {
   const data = req.body;
-
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, data);
+  const result = await Contact.findByIdAndUpdate(contactId, data, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+const upDateFavorite = async (req, res, next) => {
+  const data = req.body;
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, data, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, "Not Found");
   }
@@ -47,4 +59,5 @@ module.exports = {
   add: ctrlWrapper(add),
   deleteById: ctrlWrapper(deleteById),
   upDateById: ctrlWrapper(upDateById),
+  upDateFavorite: ctrlWrapper(upDateFavorite),
 };
