b0d74b01a3f7159c994b28e3aec1a8a05bea5e0d
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-03-09 15:40:19 +0200

    summ

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,82 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const contactsPath = path.join('models', 'contacts.json');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try { 
+        const buffer = await fs.readFile(contactsPath, "utf-8");
+        const lists = JSON.parse(buffer);
+        return lists
+    } catch (err) {
+        console.log(err.message)
+    }
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try{
+        const lists = await listContacts()
+        const contact = lists.filter( list => list.id === contactId)
+        if (!contact) {
+            return null
+        }
+        return contact
+    } catch (err) {
+        console.log(err.message)
+    }
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+        const buffer = await fs.readFile(contactsPath, "utf-8");
+        const lists = JSON.parse(buffer);
+        const updateContact = lists.filter(list => list.id !== contactId)
+        await fs.writeFile(contactsPath, JSON.stringify(updateContact), "utf8");
+        await listContacts()
+    } catch (err) {
+        console.log(err.message)
+    }
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  try {
+        const buffer = await fs.readFile(contactsPath, "utf-8");
+        const lists = JSON.parse(buffer);
+        const newContact = [
+            ...lists,
+            {
+            name,
+            email,
+            phone
+            }
+        ];
+        await fs.writeFile(contactsPath, JSON.stringify(newContact), "utf-8")
+        await listContacts()
+    } catch (err) {
+        console.log(err)
+    }
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  try {
+      const lists = await listContacts()
+      const contact = lists.filter(list => list.id === contactId)
+      const updatedContact = {
+      ...contact,
+      id: contactId,
+      name,
+      email,
+      phone,
+    };
+    lists.push(updatedContact);
+    await fs.writeFile(contactsPath, JSON.stringify(lists), "utf-8")
+    await listContacts()
+
+    } catch (err) {
+        console.log(err.message)
+    }
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const {
+  listContacts,
+  getContactByID,
+  addContact,
+  removeContact,
+} = require("./models/contacts.js");
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await listContacts()
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contacts
+      }
+  })
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Internal Server Error"
+    })
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
