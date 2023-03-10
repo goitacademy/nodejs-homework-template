375bf37479884e3cdfb23f8a2678538cb69cd530
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-10 19:17:02 +0200

    .

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST } = require("./config");
+const DB_HOST =
+  "mongodb+srv://taraskovalyshyn:BC1083KPbc9169ei@cluster0.tkez3bl.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
-
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
+    console.log("succsess");
+
     app.listen(3000);
   })
   .catch((error) => {
