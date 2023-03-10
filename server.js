5bd95d1807a94dd065f1f0c41cecb2b3b7d20685
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-10 17:27:22 +0200

    commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const {DB_HOST} = require("./config");
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(3000))
+  .then(() => {
+    app.listen(3000);
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
