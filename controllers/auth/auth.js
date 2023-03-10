c63bdb9cc6c961f5f72c5b133cb4276e5dd1455e
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-03-10 18:20:15 +0200

    1 lesson add

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,17 +1,53 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 const { User } = require("../../model/user");
 const { HttpError, controllersWrapper } = require('../../helpers');
 
-const register = async (req, res) => {
-    const newUser = await User.create(req.body);
-    const {name, email, password} = newUser
+require("dotenv").config();
+const {SECRET_KEY} = process.env
 
+const register = async (req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email});
+
+    if (user) {
+        throw HttpError(409, "Email already is use")
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const newUser = await User.create({...req.body, password: hashPassword});
+  
     res.status(201).json({
-        name,
-        email,
-        password
+        name: newUser.name,
+        email: newUser.email,
+        password: newUser.password
+    })
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw HttpError(401, "Email or password invalid")
+    }
+    const passwordConpare = await bcrypt.compare(password, user.password);
+    if (!passwordConpare) {
+        throw HttpError(401, "Email or password invalid")
+    }
+
+    const payload = {
+        id: user._id
+    }
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+    res.json({
+        token,
     })
 }
 
 module.exports = {
-    register: controllersWrapper(register)
+    register: controllersWrapper(register),
+    login: controllersWrapper(login)
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,9 @@ const {schemas} = require("../../model/user")
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema), cntr.register)
+router.post("/register", validateBody(schemas.registerSchema), cntr.register);
+
+router.post("/login", validateBody(schemas.loginSchema), cntr.login)
 
 
 module.exports = router;
