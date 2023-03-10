7d84f891a2599d5f8ca4025f37e24b16abd198bd
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2023-03-10 22:09:55 +0100

    HW-05

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,6 @@
 const { User } = require("../../models");
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -10,12 +11,14 @@ const signup = async (req, res) => {
       .status(409)
       .json({ status: "error", code: 409, data: { message: "Email in use" } });
   }
+  const avatarURL = gravatar.url(email);
   const hachPassword = await bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
   await User.create({
     email,
     password: hachPassword,
     subscription: "starter",
+    avatar: avatarURL,
   });
 
   return res.status(201).json({
@@ -25,6 +28,7 @@ const signup = async (req, res) => {
       user: {
         email,
         subscription: "starter",
+        avatarURL,
       },
     },
   });
