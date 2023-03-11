073df52dc194ab5d63fa8924a8f07e7fbdeb0629
Natalya Karpova <knataly0513@gmail.com>
2023-03-11 23:26:52 +0200

    Create index.js

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+const AppError = require('./appError');
+const catchAsync = require('./catchAsync');
+const validators = require('./joiValidators');
+
+module.exports = {
+  AppError,
+  catchAsync,
+  validators,
+};
