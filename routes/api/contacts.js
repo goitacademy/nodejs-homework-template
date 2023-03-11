e95d5105826ebd02c18944761e9d3deb8170b356
Natalya Karpova <knataly0513@gmail.com>
2023-03-11 19:12:31 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require('../../models/contacts');
 
 router.get('/api/contacts', async (req, res, next) => {
   
