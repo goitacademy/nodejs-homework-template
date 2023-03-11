ee289f81a537c0ad916f9ef71bac2ea4cf05874c
ElenaObukhova1984 <e.obukhova84@gmail.com>
2023-03-11 21:46:30 +0200

    added owner  to controllers

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -7,7 +7,10 @@ const { HttpError, ctrlWrapper } = require('../helpers');
 
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const {_id: owner} = req.user;
+    const {page = 1, limit = 10} = req.query;
+    const skip = (page - 1) * limit;
+  const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
   res.json(result);
  }
 
@@ -22,7 +25,8 @@ const getContactById = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+   const {_id: owner} = req.user;
+  const result = await Contact.create({...req.body, owner});
   if (!result) {
   return res.status(400).json({code: 400, message:"Unable to save in data base"});
 }
