ddce76cfdb8fea66815b0c2c55df8b9a19ef221e
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-11 19:15:18 +0200

    GET contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,36 @@
-const express = require('express')
+const contacts = require('../../models/contacts');
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const data = await contacts.listContacts();
+
+    res.json({
+      status: 'success',
+      code: 200,
+      data,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
