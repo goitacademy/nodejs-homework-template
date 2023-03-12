cf1fc7f01ce176fd2a0079f9cfcbaa9b375b65fc
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-12 12:07:37 +0200

    upd: getById controller

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -3,6 +3,18 @@ const createError = require('http-errors');
 
 const getById = async (req, res, next) => {
   try {
+    const { contactId } = req.params;
+    const result = await contactsOperations.getContactById(contactId);
+
+    if (!result) {
+      throw createError(404, 'Not found');
+    }
+
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result },
+    });
   } catch (err) {
     next(err);
   }
