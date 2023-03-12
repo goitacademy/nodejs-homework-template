5a5af4d0b49d4973d67adadc2b10bc331a53f7fa
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-12 12:07:24 +0200

    upd: getAll controller

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,6 +2,13 @@ const contactsOperations = require('../../models/contacts');
 
 const getAll = async (req, res, next) => {
   try {
+    const contactsList = await contactsOperations.listContacts();
+
+    res.status(200).json({
+      status: 'success',
+      code: 200,
+      data: { result: contactsList },
+    });
   } catch (err) {
     next(err);
   }
