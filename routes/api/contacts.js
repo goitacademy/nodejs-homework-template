8e189466817728b1db327df24fbe889be1ed382d
1-Dmytro-Shynkarenko <114245538+1-Dmytro-Shynkarenko@users.noreply.github.com>
2023-03-13 19:43:46 +0200

    commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,83 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi');
-const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../../models/contacts');
-const { HttpError } = require('../../helpers');
+const { getAll, getById, add, updateById, deleteById, updateFavourite } = require('../../controllers/contacts');
 
-const addScheme = Joi.object({
-  id: Joi.number().required,
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required()
-})
-
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const result = await getContactById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-})
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addScheme.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = addScheme.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const id = req.params.contactId;
-    const result = await updateContact(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const result = await removeContact(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({ message: "Delete success" })
-
-  } catch (error) {
-    next(error);
-  }
-})
+const { validateBody } = require('../../middlewares');
+const { schemas } = require('../../models/contact');
 
 
+router.get('/', getAll);
 
+router.get('/:contactId', getById);
 
-module.exports = router
+router.post('/', validateBody(schemas.addScheme), add);
+
+router.put('/:contactId', validateBody(schemas.addScheme), updateById);
+
+router.delete('/:contactId', deleteById);
+
+router.patch('/:contactId/favorite', validateBody(schemas.updateFavoriteSchema), updateFavourite);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const app = require('./app')
+const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
 
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(PORT, () => {
+    console.log("Server running. Use our API on port: 3000")
+  }))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
