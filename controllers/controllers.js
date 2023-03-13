02578c78aa6da069ef11de476f7e45f1c143de86
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2023-03-13 09:14:01 +0200

    fix-add-controllers

---
+++ controllers/controllers.js
@@ -0,0 +1,60 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../models/contacts");
+
+const getAllController = async (req, res) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json({ contacts });
+  } catch (error) {
+    return error;
+  }
+};
+
+const getByIdController = async (req, res) => {
+  try {
+    const contact = await getContactById(req.params.contactId);
+
+    res.status(200).json(contact);
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteController = async (req, res) => {
+  try {
+    await removeContact(req.params.contactId);
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    return error;
+  }
+};
+
+const changeContactController = async (req, res) => {
+  console.log(req);
+  try {
+    const updatedContact = await updateContact(req.params.contactId, req.body);
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    return error;
+  }
+};
+
+const addContactController = async (req, res) => {
+  try {
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {}
+};
+
+module.exports = {
+  getAllController,
+  getByIdController,
+  deleteController,
+  changeContactController,
+  addContactController,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,68 +1,30 @@
 const express = require("express");
+
+const {
+  getAllController,
+  getByIdController,
+  changeContactController,
+  deleteController,
+  addContactController,
+} = require("../../controllers/controllers");
+
 const {
   checkId,
   checkContactData,
   checkUpdateContactData,
 } = require("../../middlewares/contactsMiddleware");
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.status(200).json({ contacts });
-  } catch (error) {
-    return error;
-  }
-});
+router
+  .route("/")
+  .get(getAllController)
+  .post(checkContactData, addContactController);
 
-router.use("/:contactId", checkId);
-
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const contact = await getContactById(req.params.contactId);
-
-    res.status(200).json(contact);
-  } catch (error) {
-    return error;
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    await removeContact(req.params.contactId);
-    res.status(200).json({ message: "contact deleted" });
-  } catch (error) {
-    return error;
-  }
-});
-
-router.use("/:contactId", checkUpdateContactData);
-
-router.put("/:contactId", async (req, res, next) => {
-  console.log(req);
-  try {
-    const updatedContact = await updateContact(req.params.contactId, req.body);
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    return error;
-  }
-});
-
-router.use("/", checkContactData);
-
-router.post("/", async (req, res, next) => {
-  try {
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {}
-});
+router
+  .route("/:contactId")
+  .get(checkId, getByIdController)
+  .delete(checkId, deleteController)
+  .put(checkUpdateContactData, changeContactController);
 
 module.exports = router;
