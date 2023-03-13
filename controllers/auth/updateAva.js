348c355a4e9f88696337690fb3418cd5da761eb6
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-03-13 15:40:16 +0200

    finish

--- controllers/auth/updateAva.js
+++ controllers/auth/updateAva.js
@@ -6,10 +6,10 @@ const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const updateAva = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
-  console.log(req.file);
-  const resultUpload = path.join(avatarsDir, originalname);
+  const filename = `${req.user._id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join('avatars', originalname);
+  const avatarURL = path.join('avatars', filename);
   await User.findByIdAndUpdate(req.user._id, { avatarURL });
   res.json({ avatarURL });
 };
