2bf98c53116195c7d319e018c4803f1fe5b0086d
ievsepl <107007762+ievsepl@users.noreply.github.com>
2023-03-13 19:54:08 +0200

    correct errors 2

--- middleWares/joiContactValidator.js
+++ middleWares/joiContactValidator.js
@@ -2,9 +2,9 @@ const Joi = require("joi");
 
 const contactValidator = () => {
   const schema = Joi.object({
-    name: Joi.string().min(2).max(30).required(),
-    phone: Joi.string().min(7).max(15).required(),
-    email: Joi.string().email().required(),
+    name: Joi.string().min(2).max(30),
+    phone: Joi.string().min(7).max(15),
+    email: Joi.string().email(),
   });
 
   return (req, res, next) => {
