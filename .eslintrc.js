9d5fffed2331bce298e1f82544a45f81fd3409ca
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-03-14 11:08:51 +0200

    unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'prettier'],
+  extends: ["standard", "prettier", "jest", "node"],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {},
-}
+};
--- controllers/users/loginUserController.js
+++ controllers/users/loginUserController.js
@@ -2,7 +2,7 @@ const { loginUser } = require("../../service/users/");
 const loginUserController = async (req, res) => {
   const { email, password } = req.body;
   const data = await loginUser(email, password);
-  const { token } = data;
-  res.json({ status: "success", token });
+  const { token, subscription } = data;
+  res.json({ status: "success", token, data: { email, subscription } });
 };
 module.exports = loginUserController;
