8282f4997d27a94df189f139c2f742ca50a25d88
ElenaObukhova1984 <e.obukhova84@gmail.com>
2023-03-15 12:36:14 +0200

    added upload, gravatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
 
 const {User} = require("../models/user");
 
@@ -17,7 +18,9 @@ const register = async(req, res)=> {
 
     const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const avatarURL = gravatar.url(email);
+
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
 
     res.status(201).json({
         email: newUser.email,
