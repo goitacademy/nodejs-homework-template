b9dadfc6bf0cc688076bfb5089a558954762ab1f
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2023-03-15 17:03:32 +0200

    controllers done

--- models/contacts.js
+++ models/contacts.js
@@ -1,10 +1,3 @@
-// const fs = require('fs/promises')
-// const path = require('path');
-// const { v4 } = require('uuid');
-
-// const { connectMongo } = require('../db/connection');
-
-
 const Joi = require("joi");
 
 const contactsJoiSchema = Joi.object({
@@ -15,9 +8,10 @@ phone: Joi.string()
 .required(),
 });
 
-// const contactsPath = path.resolve(__dirname, "contacts.json");
-
 const ObjectId = require('mongodb').ObjectId;
+
+
+
 
 const listContacts = async (req, res) => {
   const contacts = await req.db.Contacts.find({}).toArray();
@@ -41,34 +35,9 @@ const getContactById = async (req, res) => {
 
 
 const removeContact = async (req, res) => {
-
-  const {contactId} = req.params;
+  const { contactId } = req.params;
   await req.db.Contacts.deleteOne({ _id: new ObjectId(contactId) });
-  
-  res.json({status: 'success'})
-  // try {
-  //   const { contactId } = req.params;
-  //   const data = await fs.readFile(contactsPath);
-  //   const contacts = JSON.parse(data);
-  //   const removedContact = contacts.find(item => item.id === contactId);
-  //   if (!removedContact) {
-  //     const error = new Error("Not found");
-  //     error.status = 404;
-  //     throw error;
-  //   }
-
-  //   const newContactList = contacts.filter(item => item.id !== contactId);
-  //   fs.writeFile(contactsPath, JSON.stringify(newContactList));
-    
-  //   res.json({
-  //     status: "success",
-  //     code: 200,
-  //     message: "contact deleted",
-  //     data: { newContactList },
-  //   });
-  // } catch (error) {
-  //   next(error);
-  // }
+  res.json({ status: 'success' })
 };
 
 
@@ -82,62 +51,26 @@ const addContact = async (req, res) => {
   }
     
   const { name, email, phone } = req.body;
-  await req.db.Contacts.insert({ name, email, phone });
+  await req.db.Contacts.insertOne({ name, email, phone });
   res.json({ status: 'success' });
 };
 
 
 
 const updateContact = async (req, res) => {
-  //   if (!req.body) {
-  //     const error = new Error('missing fields');
-  //     error.status = 400;
-  //     throw error;
-  //   }
-    
-    const { error } = contactsJoiSchema.validate(req.body);
-    if (error) {
-      error.message = "missing required name field";
-      error.status = 400;
-      throw error;
-    }
+  const { error } = contactsJoiSchema.validate(req.body);
+  if (error) {
+    error.message = "missing required name field";
+    error.status = 400;
+    throw error;
+  }
   const { contactId } = req.params;
-  const { name, email, phone } = req.body;
+  const { name, email, phone} = req.body;
   await req.db.Contacts.updateOne(
     { _id: new ObjectId(contactId) },
-    { $set: { name, email, phone } }
+    { $set: { name, email, phone} }
   );
   res.json({ status: 'success' });
-  //   const data = await fs.readFile(contactsPath);
-  //   const contacts = JSON.parse(data);
-    
-  //   const contact = contacts.find(item => item.id === contactId);
-    
-  //   if (!contact) {
-  //     const error = new Error('Not found');
-  //     error.status = 404;
-  //     throw error;
-  //   }
-    
-  //   contact.name = name;
-  //   contact.email = email;
-  //   contact.phone = phone;
-    
-  //   const contactIdx = contacts.findIndex(item => item.id === contactId);
-  //   contacts[contactIdx] = contact;
-    
-  //   fs.writeFile(contactsPath, JSON.stringify(contacts));
-    
-  //   res.json({
-  //     status: "success",
-  //     code: 200,
-  //     data: {
-  //       contact,
-  //     },
-  //   });
-  // } catch (error) {
-  //   next(error)
-  // }
 };
 
 
