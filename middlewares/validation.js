aaea07ea561f43c0b02de6ba7601afd3116cc17b
MishynaIra <gortenzia1987@gmail.com>
2023-03-15 17:29:51 +0200

    поправила json

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,7 +3,7 @@
 const validation = (schema) => {
     return(req, res, next) => {
         const {error} = schema.validate(req.body);
-        console.log(error.details[0].type);
+
         if(error && error.details[0].type === 'string.base'){
             error.status = 400;
             error.message= `missing required ${error.details[0].context.key} field`;
