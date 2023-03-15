f6b4ea4832ce165a18f57a9902caf5e32ae6aa71
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2023-03-15 18:49:49 +0200

    process

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const {connectMongo} = require('./db/connection')
 
 
 const start = async () => {
-
+  try {
   await connectMongo();
 
   app.listen(3000, () => {
     console.log("Server running. Use our API on port: 3000")
   });
+  } catch (err) {
+    console.error(`Failed to launch apllication with error: ${err.message}`);
+  }
+
+
 };
 
-start()
-  // .then(console.log)
-  // .catch(console.error)
-  // .finally(() => client.close());
+start();
+
