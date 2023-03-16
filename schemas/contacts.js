b5f50eda5855760d7b91069f56c62a8109843ae1
Alexei <feoalexei@gmail.com>
2023-03-17 00:19:36 +0200

    fix joi messages

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,13 +1,22 @@
 const Joi = require('joi');
 
 const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .required()
+    .messages({ 'any.only': 'missing required name field' }),
   email: Joi.string()
     .email({
       minDomainSegments: 2,
     })
-    .required(),
-  phone: Joi.string().min(5).max(15).required(),
+    .required()
+    .messages({ 'any.required': 'missing required email field' }),
+  phone: Joi.string()
+    .min(5)
+    .max(15)
+    .required()
+    .messages({ 'any.required': 'missing required phone field' }),
 }).min(1);
 
 module.exports = contactSchema;
