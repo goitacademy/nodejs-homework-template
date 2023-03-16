8b42dd69f136ad10a0eea7abc1613278cd33c963
ASyrotenko <andreysyrotenko@gmail.com>
2023-03-16 15:10:12 +0200

    refactor scheme

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,21 +1,7 @@
-const Joi = require("joi");
-
 const contactsOperations = require("../../models/contacts");
-
-const contactsSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
 const add = async (req, res, next) => {
   try {
-    const { error } = contactsSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-
     const result = await contactsOperations.addContact(req.body);
     res.status(201).json({
       status: "success",
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,20 @@
 const express = require("express");
 
-const { contacts: ctrl } = require("../../controllers/index");
+const { validation } = require("../../middlewares");
+const { contactsSchema } = require("../../schemas");
+const { contacts: ctrl } = require("../../controllers");
 
+const validationMiddleware = validation(contactsSchema);
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.add);
+router.post("/", validationMiddleware, ctrl.add);
 
 router.delete("/:contactId", ctrl.removeById);
 
-router.put("/:contactId", ctrl.updateById);
+router.put("/:contactId", validationMiddleware, ctrl.updateById);
 
 module.exports = router;
