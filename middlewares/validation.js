3a0599640cb10a54aaf5ac110728102420729b75
MishynaIra <gortenzia1987@gmail.com>
2023-03-16 12:02:40 +0200

    fixed code

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,10 +4,11 @@ const validation = (schema) => {
 
         if (error && error.details[0].type === 'string.base') {
             error.status = 400;
-            error.message = `missing required ${error.details[0].context.key} field`;
+            error.message = `please fill correctly  ${error.details[0].context.key} field`;
             next(error);
         } else if (error) {
             error.status = 400;
+            error.message = `missing required ${error.details[0].context.key} field`;
             next(error);
         } else {
             next()
