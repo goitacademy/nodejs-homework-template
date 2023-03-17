64cd758fe4fc44286b20a9122908f245bbb60a5c
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-03-17 18:23:27 +0100

    contacts.js
    ddeleteContact,
    updateContact - don't good function

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,56 +18,57 @@ const getContactById = async (id) => {
 };
 
 // elimunuje contact
-// const removeContact = async (contactId) => {
-//   try {
-//     const data = await fs.readFile(contactsPath);
-//     const filterContacts = JSON.parse(data).filter(
-//       (data) => Number(data.id) !== Number(contactId)
-//     );
-//     await fs.writeFile(contactsPath, JSON.stringify(filterContacts));
-//     return listContacts();
-//   } catch (err) {
-//     return err;
-//   }
-// };
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const filterContacts = JSON.parse(data).filter(
+      (data) => Number(data.id) !== Number(contactId)
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(filterContacts));
+    return listContacts();
+  } catch (err) {
+    return err;
+  }
+};
 
-// const addContact = async (name, email, phone) => {
-//   try {
-//     const data = await fs.readFile(contactsPath);
-//     const dataParse = JSON.parse(data);
-//     const contactIndex = Number(dataParse[dataParse.length - 1].id) + 1;
-//     const addData = [
-//       ...dataParse,
-//       {
-//         id: `${contactIndex}`,
-//         name,
-//         email,
-//         phone,
-//       },
-//     ];
-//     await fs.writeFile(contactsPath, JSON.stringify(addData));
+const addContact = async (name, email, phone) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const dataParse = JSON.parse(data);
+    const contactIndex = Number(dataParse[dataParse.length - 1].id) + 1;
+    const addData = [
+      ...dataParse,
+      {
+        id: `${contactIndex}`,
+        name,
+        email,
+        phone,
+      },
+    ];
+    await fs.writeFile(contactsPath, JSON.stringify(addData));
 
-//     return data;
-//   } catch (err) {
-//     return err;
-//   }
-// };
+    return data;
+  } catch (err) {
+    return err;
+  }
+};
 
-// const updateContact = async (id, newContact) => {
-//   const data = await fs.readFile(contactsPath);
+const updateContact = async (id, newContact) => {
+  const data = await fs.readFile(contactsPath);
+  const dataParse = JSON.parse(data);
 
-//   for (let i = 0; data.length; i++) {
-//     if (data[i].id === id) {
-//       data[i] = newContact;
-//       return;
-//     }
-//   }
-// };
+  for (let i = 0; i < dataParse.length; i++) {
+    if (dataParse[i].id === id) {
+      dataParse[i] = newContact;
+      return newContact;
+    }
+  }
+};
 
 module.exports = {
   listContacts,
   getContactById,
-  // removeContact,
-  // addContact,
-  // updateContact,
+  removeContact,
+  addContact,
+  updateContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,15 +3,16 @@ const express = require("express");
 const {
   listContacts,
   getContactById,
-  // removeContact,
-  // addContact,
-  // updateContact,
+  removeContact,
+  addContact,
+  updateContact,
 } = require("../../controllers/contacts");
-// const { contactSchema } = require("../../models/contact");
+const { contactSchema } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/contacts", async (req, res, next) => {
+// all contact view
+router.get("/", async (req, res, next) => {
   try {
     const contacts = await listContacts();
     res.status(200).json(contacts);
@@ -20,8 +21,8 @@ router.get("/contacts", async (req, res, next) => {
   }
 });
 
-// szukanie po ID
-router.get("/:contactId", async (req, res, next) => {
+// find contact by ID
+router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await getContactById(id);
@@ -34,55 +35,57 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-// router.post("/", (req, res, next) => {
-//   const { error } = contactSchema.validate(req.body);
-//   if (error) {
-//     return res.status(400).send(error.details[0].message);
-//   }
-//   try {
-//     const { name, phone, email } = req.body;
-//     const contact = addContact(name, phone, email);
+// add contact
+router.post("/", (req, res, next) => {
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  try {
+    const { name, email, phone } = req.body;
+    const contact = addContact(name, email, phone);
 
-//     return res.status(201).json(contact);
-//   } catch {
-//     return res.status(500).send("Something went wrong");
-//   }
-// });
+    return res.status(201).json(contact);
+  } catch {
+    return res.status(500).send("Something went wrong");
+  }
+});
 
-// eliminuje kontakt
-// router.delete("/:contactId", async (req, res, next) => {
-//   const { id } = req.params;
-//   if (!id) {
-//     return res.status(404).send("Not found");
-//   }
-//   try {
-//     removeContact(id);
-//     return res.status(204).send();
-//   } catch {
-//     return res.status(500).send("Something went wrong");
-//   }
-// });
+// delete contact
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(404).send("Not found");
+  }
+  try {
+    removeContact(id);
+    return res.status(204).send();
+  } catch {
+    return res.status(500).send("Something went wrong");
+  }
+});
 
-// router.put("/:contactId", (req, res, next) => {
-//   const { id } = req.params;
-//   if (!id) {
-//     return res.status(400).send("ID is required to perform delete");
-//   }
-//   const { error } = contactSchema.validate(req.body);
-//   if (error) {
-//     return res.status(400).send(error.details[0].message);
-//   }
-//   const contact = getContactById(id);
-//   if (!contact) {
-//     return res.status(404).send("User not found");
-//   }
+// updated contact
+router.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).send("ID is required to perform delete");
+  }
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  const contact = getContactById(id);
+  if (!contact) {
+    return res.status(404).send("Contact not found");
+  }
 
-//   try {
-//     updateContact(id, req.body);
-//     return res.status(200).send("User successfully updated");
-//   } catch {
-//     return res.status(500).send("Something went wrong");
-//   }
-// });
+  try {
+    updateContact(id, req.body);
+    return res.status(200).send("Contact successfully updated");
+  } catch {
+    return res.status(500).send("Something went wrong");
+  }
+});
 
 module.exports = router;
