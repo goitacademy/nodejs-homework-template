193091d5b954d5e95b9c6e076552c436c17edb4e
Roman Stelmakh <105305762+Roman0407@users.noreply.github.com>
2023-03-17 17:33:27 +0200

    new

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,106 @@
-const express = require('express')
+const express = require('express');
+const Joi = require("joi");
+const contacts = require("../../models/contacts");
+
+const{HttpError} = require("../../helpers");
 
 const router = express.Router()
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const result = await contacts.listContacts();
+    res.json(result);
+  }
+  catch(error){
+    next(error);
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const{contactId} = req.params;
+    const result = await contacts.getContactById(contactId);
+    if(!result){
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  }
+  catch(error){
+    next(error)
+  }
+  
 })
+
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addSchema.validate(req.body);
+     if(error) {
+      throw HttpError(400, "missing required name field");
+     }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  }
+  catch(error) {
+    next(error);
+  }
 })
+
+
+
+router.put("/:contactId", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).optional(),
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      })
+      .optional(),
+    phone: Joi.string().min(7).max(20).optional(),
+  });
+  const { error, value } = schema.validate(req.body);
+
+  if (error) {
+    res.status(400).json({ error: error.details });
+    return;
+  }
+  if (Object.keys(value).length === 0) {
+    res.status(400).json({ message: "missing fields" });
+    return;
+  }
+
+  const { contactId } = req.params;
+  const response = await contacts.updateContact(contactId, value);
+
+  if (!response) {
+    res.status(404).json({ message: "Not found" });
+    return;
+  }
+  res.status(200).json(response);
+});
+
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ try {
+  const {contactId} = req.params;
+  const result = await contacts.removeContact(contactId);
+  if(!result){
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Contact deleted"
+  })
+ }
+ catch(error) {
+  next(error);
+ }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,58 @@
-const app = require('./app')
+// const app = require('./app')
+// const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// const DB_HOST = "mongodb+srv://Roman:XH53-bcvknNeK_U@cluster0.rzyq7nr.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+// mongoose.set("strictQuery", true);
+
+// const mongoose.connect(DB_HOST)
+// .then(() => {console.log("Database connection successful");
+//   // app.listen(PORT);
+// })
+// .catch((err) => {
+//   console.log(err.message);
+//   // process.exit(1);
+// });
+
+
+
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
+// ....................................................................................
+// const app = require("./app");
+// const mongoose = require("mongoose");
+
+// const { DB_HOST, PORT = 3000 } = process.env;
+
+// mongoose.set("strictQuery", true);
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     console.log("Database connection successful");
+//     app.listen(PORT);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//     process.exit(1);
+//   });
+// ..................................................................................
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const DB_HOST = "mongodb+srv://Roman:XH53-bcvknNeK_U@cluster0.rzyq7nr.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    // console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
