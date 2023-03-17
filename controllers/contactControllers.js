fe04616ed053a9d2bd3f1781ba12a166954b09a7
Vadym <besarabvadim2@gmail.com>
2023-03-17 19:09:30 +0200

    test commit

---
+++ controllers/contactControllers.js
@@ -0,0 +1,1 @@
+// const Contact = require("../models/contactsModel");
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then((connection) => {
+//     console.log("Database connection successful!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
