eee8da854c3062d9497ca6be2b22a97125a67247
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2023-03-17 14:11:52 +0200

    st

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,62 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { v4 } = require("uuid");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async (req, res) => {
+  const contacts = JSON.parse(await fs.readFile(contactsPath));
+  
+  return contacts;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
 
-const addContact = async (body) => {}
+  const contactById = contacts.find(
+    (contact) => String(contact.id) === String(contactId)
+  );
 
-const updateContact = async (contactId, body) => {}
+  if (!contactById) {
+    return null;
+  }
+  return contactById;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+
+  const i = contacts.findIndex((contact) => String(contact.id) === String(contactId));
+  if (i === -1) {
+    return null;
+  }
+
+  const updateContacts = contacts.filter((_, idx) => idx !== i);
+  await fs.writeFile(contactsPath, JSON.stringify(updateContacts));
+
+  return contacts[i];
+};
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = { ...body, id: v4() };
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+};
+
+const updateContact = async (id, body) => {
+  const contacts = await listContacts();
+  const i = contacts.findIndex((contact) => String(contact.id) === String(id));
+  if (i === -1) {
+    return null;
+  }
+  contacts[i] = {id , ...body};
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return contacts[i];
+
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +64,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ utils/checkContactValidation.js
@@ -0,0 +1,24 @@
+const checkContactValidation = (req, res) => {
+  const { name, phone, email } = req.body;
+  if (!name) {
+    return res.json({
+      status: "error",
+      code: 400,
+      message: `Ooops! You missing required name field`,
+    });
+  } else if (!email) {
+    return res.json({
+      status: "error",
+      code: 400,
+      message: `Ooops! You missing required email field`,
+    });
+  } else if (!phone) {
+    return res.json({
+      status: "error",
+      code: 400,
+      message: `Ooops! You missing required phone field`,
+    });
+  }
+};
+
+module.exports = checkContactValidation;
