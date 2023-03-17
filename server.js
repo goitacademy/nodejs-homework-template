3742247b40fc87e11b5c4f260a460dafb7836f25
Roman Stelmakh <105305762+Roman0407@users.noreply.github.com>
2023-03-17 19:01:30 +0200

    yarn

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(error => {
     console.log(error.message);
