85ba57907b6fd11bc98a8c6bea41ae6379ac330a
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-18 21:06:48 +0200

    move dotenv require to app.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const app = require('./app');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const { DB_HOST } = process.env;
