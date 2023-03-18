c49b7c74392f699faeaa15362081823379680a24
Bohdan <nilsrondhuis@ukr.net>
2023-03-18 19:51:19 +0200

    use pattern reexport in dir controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../../controllers/contacts");
+const ctrl = require("../../controllers");
 const { schemas } = require("../../models/contacts");
 const { validateBody, isValidId } = require("../../middlewares");
 
-router.get("/", ctrl.getAll);
+router.get("/", ctrl.contacts.getAll);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.contacts.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.create);
+router.post("/", validateBody(schemas.addSchema), ctrl.contacts.create);
 
-router.delete("/:contactId", isValidId, ctrl.remove);
+router.delete("/:contactId", isValidId, ctrl.contacts.remove);
 
 router.put(
   "/:contactId",
   isValidId,
   validateBody(schemas.addSchema),
-  ctrl.update
+  ctrl.contacts.update
 );
 
 router.patch(
   "/:contactId/favorite",
   isValidId,
   validateBody(schemas.updateFavoriteSchema, "missing field favorite"),
-  ctrl.updateStatusContact
+  ctrl.contacts.updateStatusContact
 );
 
 module.exports = router;
