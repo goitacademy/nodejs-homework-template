eff4795fd91d984cf263e0a78d087b12eacf5ad2
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-18 21:18:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
