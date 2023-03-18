d5a6c20d6281b67365cfa27a6a45ae3bf831749f
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-18 20:41:06 +0200

    run server after connecting to database

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const { DB_HOST } = process.env;
 
-app.listen(PORT, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('database successfully connected');
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
