7c7153d9a03257d129e28289678259a66fe02d39
SergY29 <yurtin.sergey.v@gmail.com>
2023-03-19 18:36:40 +0200

    work 8

--- controllers/auth/getUserController.js
+++ controllers/auth/getUserController.js
@@ -1,50 +1,25 @@
-const jwt = require('jsonwebtoken');
-// const { login } = require('../../service/auth')
 const { NotAutorizedError } = require('../../helpers/errors')
+const { getCurrentUser } = require('../../service/auth')
 
-const User = require('../../models/auth')
 
 const getUserController = async (req, res, next) => {
     const authorization = req.headers.authorization;
 
     if (!authorization) {
-        next(new NotAutorizedError('Not authorized'))
-        return;
+        throw new NotAutorizedError('Not authorized')
     }
 
     const [tokenType, token] = req.headers.authorization.split(' ');
     // console.log(tokenType, token)
-    if (!token) {
-        next(new NotAutorizedError('Not authorized'))
-        return;
-    }
 
-    try {
-        const user = jwt.decode(token, process.env.JWT_SECRET)
-        const { _id } = user;
+    const { email, subscription } = await (getCurrentUser(tokenType, token))
 
-        const { email, subscription } = await User.findOne({ _id });
-
-
-        if (!user) {
-            next(new NotAutorizedError('Not authorized'))
-            return;
+    res.status(200).json({
+        "user": {
+            email,
+            subscription,
         }
-
-        res.status(200).json({
-            "user": {
-                email,
-                subscription,
-            }
-        })
-
-
-    } catch (error) {
-        next(new NotAutorizedError('Not authorized'))
-    }
-
-
-
+    })
 }
 
 module.exports = getUserController
--- service/auth/index.js
+++ service/auth/index.js
@@ -1,10 +1,12 @@
 const login = require('./login')
 const registration = require('./registration')
+const getCurrentUser = require('./getCurrentUser')
 
 
 
 module.exports = {
     login,
     registration,
+    getCurrentUser,
 
 };
