f6d7ed1cb136f833a831f53e1cdfe769d3d9c2fa
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-03-19 13:00:46 +0200

    Create contactsModel.js

---
+++ models/contactsModel.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose');
+
+const contactsSchema = new Schema(
+  { 
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+);
+
+const Contacts = model('contacts', contactsSchema);
+
+module.exports = Contacts;
