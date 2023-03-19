9742347cd5aa5dc96db574b301238f870bead973
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-03-19 13:00:36 +0200

    Update cotactController.js

--- controller/cotactController.js
+++ controller/cotactController.js
@@ -1,56 +1,119 @@
-const contacts = require('../models/contacts');
 
-function getRandomId(min, max) {
-    return `${Math.floor(Math.random() * max) + min}`
-  }
+const Contacts = require('../models/contactsModel');
+const {validationData} = require('../contactMiddleware/contactMiddleware')
 
-exports.getContactsList=async (req, res, next) => {   
-    res.status(200).json(await contacts.listContacts())  
+const getContactsList=async (req, res, next) => {
+    try{
+        res.status(200).json(await Contacts.find())
+    }catch(err){
+        console.log(err)
+    }      
 };
 
-exports.getContactById= async (req, res, next) => {
-    const {contactId} = req.params;     
-    const contact = await contacts.getContactById(contactId);
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' })
-    }
-    res.status(200).json(contact);
-};
-
-exports.createContact=async (req, res, next) => {      
-    const {name,email,phone} = req.body;
-    if (!name || !email || !phone) {           
-       return  res.status(400).json({ message: "missing required name field" })
-    } else {
-        const id=getRandomId(1,100)
-        res.status(201).json(await contacts.addContact(id,name,email,phone));
-    }     
-};
-
-exports.deleteContactById= async (req, res, next) => {
-    const {contactId} = req.params;
-    const contact = await contacts.getContactById(contactId);
-    if (contact) {
-      await contacts.removeContact(contactId)
-      res.status(200).json({ message: 'contact deleted' })
-    } else {
-      res.status(404).json( {message: 'Not found'});  
-    }  
-};
-
-exports.putUpdateContact = async (req, res, next) => {
+const getContactById= async (req, res, next) => {
     const {contactId} = req.params; 
-    const {name,email,phone} = req.body;
-    const contact = await contacts.getContactById(contactId);
-
-    if (!name || !email || !phone) {           
-        return  res.status(400).json({ message: `missing fields` })
-    }else if (!contact){
+    try{
+        const contact = await  Contacts.find({_id:String(contactId)});        
+        res.status(200).json(contact);
+    } catch(err){
+        console.log(err)
         return res.status(404).json({ message: 'Not found' })
     }     
-    res.status(200).json(await contacts.updateContact(contactId,name,email,phone));
 };
 
+const deleteContactById= async (req, res, next) => {
+    const {contactId} = req.params;
 
+    
+    try{
+        const contact = await  Contacts.find({_id:String(contactId)});
+        if (contact) {
+            await Contacts.findByIdAndRemove({_id:String(contactId)});     
+            res.status(200).json({ message: 'contact deleted' })
+        }
+    }catch(err){
+        console.log(err) 
+        res.status(404).json( {message: 'Not found'});
+    }     
+};
 
+const createContact=async (req, res, next) => {      
+    const {name,email,phone,favorite} = req.body; 
+    const {value,error}= await validationData(name, email, phone,favorite);
+    if (!name || !email || !phone) {                   
+        return  res.status(400).json({ message: "required data missing. check the fields name,email and phone" })
+    } 
+
+    if(error) {
+        return  res.status(400).json({ message: `${error.message}`})        
+    } 
+
+    try{       
+        res.status(201).json(await Contacts.create(
+            {name:value.name,
+            email:value.email,
+            phone:value.phone,
+            favorite:value.favorite || false})
+        )        
+    }catch(err){
+        console.log(err)    
+    }     
+};
+
+const putUpdateContact = async (req, res, next) => {
+    const {contactId} = req.params;
+    const {name,email,phone,favorite} = req.body; 
+    const {value,error}= await validationData(name, email, phone,favorite);
+    if (!name || !email || !phone) {                   
+        return  res.status(400).json({message: "required data missing. check the fields name,email and phone"})
+    } 
+
+    if(error) {
+        return  res.status(400).json({ message: `${error.message}`})        
+    } 
+    
+    try{        
+        res.status(200).json(await Contacts.findByIdAndUpdate({_id:String(contactId)}, 
+            {name:value.name,
+            email:value.email,
+            phone:value.phone,
+            favorite:value.favorite|| false}, { new: true }));
+           
+    }catch(err){
+        console.log(err)
+    }    
+};
+
+ const updateStatusContact=async(req, res, next) => {
+    const {contactId} = req.params; 
+    const {name,email,phone,favorite} = req.body; 
+    const {value,error}= await validationData(name, email, phone,favorite);
+    if (!name || !email || !phone|| !favorite) {                   
+        return  res.status(400).json({message: "required data missing. check the fields name,email,phone and favorite"})
+    } 
+
+    if(error) {
+        return  res.status(400).json({ message: `${error.message}`})        
+    } 
+
+    try{
+        res.status(200).json(await Contacts.findByIdAndUpdate({_id:String(contactId)}, 
+        {name:value.name,
+        email:value.email,
+        phone:value.phone,
+        favorite:value.favorite}, { new: true }));      
+
+    }catch(err){
+        console.log(err)
+    }    
+};
    
+
+module.exports = {
+    getContactsList,
+    getContactById,
+    deleteContactById,
+    createContact,
+    putUpdateContact,
+    updateStatusContact
+}
