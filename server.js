6bb46db6aacc02b3513aa9027dd79d6de0bca89c
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-03-19 13:01:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const app = require('./app')
+const app = require('./app');
+const dotenv=require('dotenv');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-const port = 3000;
+dotenv.config({path:'./.env'});
 
-app.listen(port, () => {
-  console.log(`Server running. Use our API on port: ${port}`)
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(logger(formatsLogger))
+
+mongoose.connect(process.env.MONGO_URL).then((con)=>{
+  console.log ('Database connection successful')  
+}).catch((err)=>{
+  console.log(err);
+  process.exit(1);
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running. Use our API on port: ${process.env.PORT}`)
 })
 
 
