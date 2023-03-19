ac408aee0edb29ecc95693af0f6ace72429db055
SweetChipss <kruglov.denis1208@gmail.com>
2023-03-19 02:10:04 +0200

    hw03

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DB_HOST = "mongodb + srv://Naxul:lkkM7fEvAxjNEAPk@cluster0.kgdyqjm.mongodb.net/contact_reader?retryWrites=true&w=majority"
+const DB_HOST = "mongodb+srv://Naxul:lkkM7fEvAxjNEAPk@cluster0.kgdyqjm.mongodb.net/contact_reader?retryWrites=true&w=majority"
 
 module.exports = {
    DB_HOST,
