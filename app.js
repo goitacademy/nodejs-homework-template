2b1a4d7ab2016634a28cac5f15c1fec1747c4bea
Patrycja Lamik <patrycjalamik94@gmail.com>
2023-03-20 13:41:56 +0100

    Revert "node_2"
    This reverts commit ed0aa7ef73aea753c6682e1b643541b2a4ab9c27.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const router = require("./routes/routes.js");
+const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
@@ -12,7 +12,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use(router);
+app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const contactsRouter = require("./contacts");
-
-const router = express.Router();
-
-router.use('/api/contacts', contactsRouter)
-
-module.exports = router;
