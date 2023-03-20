32ac03271e0b7db018e00a32d9e68f1709385656
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 00:14:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app')
+const app = require('./app');
+const dotenv = require('dotenv');
 
-app.listen(3000, () => {
+dotenv.config({ path: './.env' });
+
+
+app.listen(process.env.PORT || 4000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
