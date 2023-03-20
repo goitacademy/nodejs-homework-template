9931917ab23af6cb948a42acfc147dfc5998185e
Patrycja Lamik <patrycjalamik94@gmail.com>
2023-03-20 13:16:22 +0100

    popr node_2

--- models/user.js
+++ models/user.js
@@ -1,19 +1,10 @@
 const Joi = require("joi");
 
-class Contact {
-    constructor(id, name, email, phone) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-    }
-}
-
 const userSchema = Joi.object({
     id: Joi.number().required(),
     name: Joi.string().required(),
     phone: Joi.number().required(),
-    email: Joi.string().required(),
+    email: Joi.email().required(),
 })
 
 module.exports = { Contact, userSchema };
