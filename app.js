9a2e023d412d11daed560abdb15d1a6fcfc15305
Sasha.Solo <sasha.solomannik@gmail.com>
2023-03-21 00:12:32 +0200

    add env-app

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const { contactsRouter } = require("./routes");
+// const authRouter = require("./routes/api/auth");
+const { contactsRouter, authRouter, usersRouter } = require("./routes");
 
 const app = express();
+
+// const createHashPassword = async (password) => {
+//   const result = await bcrypt.hash(password, 10);
+//   const compareResult = await bcrypt.compare(password, result);
+// };
+
+// const { SECRET_KEY } = process.env;
+// const payload {
+//   id: ""
+// };
+
+// const token = jwt.sign(payload, SECRET_KEY, { expireIn: "23" })
+
+// const decodeToken = jwt.decode(token);
+
+// try {
+//   const { id } = jwt.verify(token, SECRET_KEY)
+//   const invalidToken = ""
+//   const result = jwt.verify(invalidToken, SECRET_KEY)
+// }
+// catch (error) { };
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -14,6 +36,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
