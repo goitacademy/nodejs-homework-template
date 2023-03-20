0b4a9dc0d00ac29be4c1eb19ca26801194f3fbbb
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-20 21:12:38 +0200

    upd: add contact with new validation

--- controllers/contacts/postContact.js
+++ controllers/contacts/postContact.js
@@ -1,23 +1,24 @@
 const createError = require('http-errors');
 
 const { Contact } = require('../../models/index');
-
 const { catchAsync } = require('../../utils/index');
 
-const schema = require('../../schemas/contactSchema');
+// const schema = require('../../schemas/contactSchema');
+const { contactDataValidator } = require('../../utils/ContactValidation');
 
 const postContact = catchAsync(async (req, res, next) => {
-  const body = req.body;
+  const { name, email, phone, favorite } = req.body;
 
-  const { error } = schema.validate(body);
+  const { error } = contactDataValidator(req.body);
   if (error) {
     throw createError(400, error.message);
   }
+
   if (
-    body.name === undefined ||
-    body.email === undefined ||
-    body.phone === undefined ||
-    body.favorite === undefined
+    name === undefined ||
+    email === undefined ||
+    phone === undefined ||
+    favorite === undefined
   ) {
     throw createError(400, 'missing required name field');
   }
