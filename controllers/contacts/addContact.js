9cebf49651a025de421286910af44f1fde350e2a
YevheniiaVier <gotashk1983@gmail.com>
2023-03-21 09:58:01 +0200

    corrects structure

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -2,6 +2,7 @@ const contactsOperations = require("../../models/contacts");
 
 const addContact = async (req, res) => {
   const contact = await contactsOperations.addContact(req.body);
+
   res.status(201).json({
     status: "success",
     code: 201,
--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -1,8 +1,14 @@
 const contactsOperations = require("../../models/contacts");
 
 const updateContact = async (req, res) => {
+  if (!req.body) {
+    const error = new Error(`Missing fields`);
+    error.status = 400;
+    throw error;
+  }
   const { contactId } = req.params;
   const contact = await contactsOperations.updateContact(contactId, req.body);
+
   if (!contact) {
     const error = new Error(`Contact with id: ${contactId} is not found`);
     error.status = 404;
