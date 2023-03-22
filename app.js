919869d8c90aa215cafff12ef94626aa45072f4a
aliesia-zahorui <aliesia@ukr.net>
2023-03-22 16:25:31 +0200

    create blank files structure for router 'users'

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts');
+const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "404 - Not found" });
---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const { users: ctrl } = require("../../controllers/index");
+
+const router = express.Router();
+
+module.exports = router;
