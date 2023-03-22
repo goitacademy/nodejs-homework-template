98ee03137ec0b00c645ff547d20dffcf257c9fa7
Maryna-Korbet <m22v86k@gmail.com>
2023-03-22 21:43:32 +0200

    fix bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
         id: user._id,
     }
     const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"})
-    await User.findByIdAndDelete(user._id, {token})
+    await User.findByIdAndUpdate(user._id, {token})
 
     res.json ({
         token,
