2c6586444204d135286ebe72cff0924def333e46
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-22 23:51:10 +0200

    base register logic

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,12 +1,32 @@
-const { catchAsync } = require("../../utils/index");
+const { catchAsync } = require('../../utils/index');
+const createError = require('http-errors');
 
-const { userRegisterValidator } = require("../../utils/index");
+const { userRegisterValidator } = require('../../utils/index');
+const { User } = require('../../models/index');
 
 const register = catchAsync(async (req, res, next) => {
-  const { error } = userRegisterValidator(req.body);
-  if (error) {
-    throw createError(400, error.message);
-  }
+  const { name, email, password } = req.body;
+  console.log('---> ~ register ~ name:', name);
+
+  // const { error } = userRegisterValidator(req.body);
+  // if (error) {
+  //   throw createError(400, error.message);
+  // }
+
+  // const user = User.findOne({ email });
+  // if (user) {
+  //   throw createError(409, `User with email ${email} already exist`);
+  // }
+
+  const newUser = await User.create({ name, email, password });
+
+  newUser.password = undefined;
+
+  res.status(201).json({
+    status: 'added',
+    code: 201,
+    data: { result: newUser },
+  });
 });
 
 module.exports = register;
