8569f9bf515ed492afdc7dfe3966ecf35ab6f67c
aliesia-zahorui <aliesia@ukr.net>
2023-03-22 16:51:35 +0200

    add joi check into registration

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,5 +1,12 @@
 const { catchAsync } = require("../../utils/index");
 
-const register = catchAsync(async (req, res, next) => {});
+const { userRegisterValidator } = require("../../utils/index");
+
+const register = catchAsync(async (req, res, next) => {
+  const { error } = userRegisterValidator(req.body);
+  if (error) {
+    throw createError(400, error.message);
+  }
+});
 
 module.exports = register;
