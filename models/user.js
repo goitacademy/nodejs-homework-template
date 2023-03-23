48f2efa9f9cb30990ae81f54782f21894f6b5053
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-23 14:34:16 +0200

    add to user model hash password and checkPassword

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 const userSchema = Schema(
   {
     name: {
@@ -24,6 +26,19 @@ const userSchema = Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+// Pre save hook // create save
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Custom method
+userSchema.methods.checkPassword = (candidate, hash) =>
+  bcrypt.compare(candidate, hash);
 
 const User = model('user', userSchema);
 
