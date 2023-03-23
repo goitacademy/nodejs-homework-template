3f80e578ac126f55e2c7d51a44638d7728da3964
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-23 14:37:07 +0200

    check user login
    validation
    password is valid
    email is valid

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -5,5 +5,31 @@ const { userLoginValidator } = require('../../utils');
 const { User } = require('../../models/index');
 
 const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const { error } = userLoginValidator({
+    email,
+    password,
+  });
+  if (error) {
+    throw createError(400, error.message);
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  console.log('---> ~ login ~ user:', user);
+  if (!user) {
+    throw createError(401, `"Email or password is wrong"`);
+  }
+  const passwordIsValid = await user.checkPassword(password, user.password);
+  if (!passwordIsValid) {
+    throw createError(401, `"Email or password is wrong"`);
+  }
+
+  res.status(200).json({
+    status: 'ok',
+    code: 201,
+    data: { result: { email: user.email, subscription: user.subscription } },
+  });
 });
+
 module.exports = login;
