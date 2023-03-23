931f3f2242a4702a5383eca4958e47aa7e3b5fed
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-23 13:22:44 +0200

    fix validation error

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,22 +1,22 @@
 const { catchAsync } = require('../../utils/index');
 const createError = require('http-errors');
 
-const { userRegisterValidator } = require('../../utils/index');
+const { userRegisterValidator } = require('../../utils');
 const { User } = require('../../models/index');
 
 const register = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
-  console.log('---> ~ register ~ name:', name);
 
-  // const { error } = userRegisterValidator(req.body);
-  // if (error) {
-  //   throw createError(400, error.message);
-  // }
+  const { error } = userRegisterValidator({ name, email, password });
+  if (error) {
+    throw createError(400, error.message);
+  }
 
-  // const user = User.findOne({ email });
-  // if (user) {
-  //   throw createError(409, `User with email ${email} already exist`);
-  // }
+  // check if entered email alreaddy exists
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist) {
+    throw createError(409, `User with email ${email} already exist`);
+  }
 
   const newUser = await User.create({ name, email, password });
 
