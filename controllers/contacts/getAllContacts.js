4224af3b910e49b9dd9c135a967e04986a4f1750
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-24 16:23:01 +0200

    Update getAllContacts.js

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,7 +3,7 @@ const { catchAsync } = require('../../utils/index');
 
 const getAllContacts = catchAsync(async (req, res, next) => {
   const { _id } = req.user;
-  const { page = 1, limit = 1 } = req.query;
+  const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   const contactsList = await Contact.find({ owner: _id }, '', {
     skip,
