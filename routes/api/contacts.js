d231dd94b7d3ca563747fb4ae3c099f0e6dc8cab
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-24 16:03:22 +0200

    upd: private route to create contact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get("/", ctrl.getAllContacts);
 router.post("/", ctrl.postContact);
+router.post('/', auth, ctrl.postContact);
 
 // router.use("/:contactId", checkContactId);
 
