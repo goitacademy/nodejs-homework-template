696d5c74d3345e85b921a5067ec3d0044e8675f6
YevheniiaVier <gotashk1983@gmail.com>
2023-03-24 12:32:40 +0200

    adds dbconnection

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const app = require("./app");
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Yevheniia:Yevheniia1983@cluster0.mrdwxn7.mongodb.net/contacts_book?retryWrites=true&w=majority";
+
 const { PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database is connected"))
+  .catch((err) => console.log(err.message));
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
