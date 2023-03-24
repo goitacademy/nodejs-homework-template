ab6504d05bb039fed436ecd2bbac435e0fd5ab8f
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-24 15:44:29 +0200

    rewrite authorization middlware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,28 +7,25 @@ const { User } = require('../models');
 const { SECRET_KEY } = process.env;
 
 const auth = catchAsync(async (req, res, next) => {
-  const { authorization = '' } = req.headers;
-  const [bearer, token] = authorization.split(' ');
-  if (bearer !== 'Bearer') {
-    throw createError(401, 'Not authorized');
-  }
+  const token =
+    req.headers.authorization?.startsWith('Bearer') &&
+    req.headers.authorization.split(' ')[1];
+  if (!token) return next(createError(401, 'Not authorized'));
+
+  let decoded;
 
   try {
-    const { id } = jwt.verify(token, SECRET_KEY);
-
-    const user = await User.findById(id);
-    if (!user) {
-      throw createError(401, 'Not authorized');
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    if (error.message === 'Invalid sugnature') {
-      error.status = 401;
-      throw error;
-    }
+    decoded = jwt.verify(token, SECRET_KEY);
+  } catch (err) {
+    return next(createError(401, 'Not authorized'));
   }
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) return next(createError(401, 'Not authorized'));
+
+  req.user = currentUser;
+
+  next();
 });
 
 module.exports = auth;
