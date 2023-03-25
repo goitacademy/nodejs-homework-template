f0295e5311a403ccee90707d148edc720eefd415
MishynaIra <gortenzia1987@gmail.com>
2023-03-25 17:00:51 +0200

    app js прпоисала маршрут и создала фаил для запросов

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users');
 
 const  dotenv = require('dotenv');
 dotenv.config();
+
+
 
 const app = express()
 
@@ -17,6 +20,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('./api/users', usersRouter);
 
 
 
---
+++ routes/api/users.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const router = express.Router();
+
+module.exports = router;
