9ccadf033a9159f30ff8cbe867c10e694d61b81c
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2023-03-25 23:31:22 +0200

    comit 4)

---
+++ routes/api/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const crtl = require("../../controllers/auth");
+
+const validateBody = require("../../middlewares/validateBody");
+const { schemas } = require("../../models/user");
+
+const router = express.Router();
+
+router.post("/register", validateBody(schemas.registerSchema), crtl.register);
+
+router.post("/login", validateBody(schemas.loginSchema), crtl.login);
+
+module.exports = router;
