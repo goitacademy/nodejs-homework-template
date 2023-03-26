85ee064ae27c77a14472fb3f39178b160602ebad
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-26 17:01:04 +0300

    upd: sorting by favourite field

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,12 +3,18 @@ const { catchAsync } = require('../../utils/index');
 
 const getAllContacts = catchAsync(async (req, res, next) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+
+  const { page = 1, limit = 10, favorite } = req.query;
+
   const skip = (page - 1) * limit;
-  const contactsList = await Contact.find({ owner: _id }, '', {
-    skip,
-    limit: Number(limit),
-  }).populate('owner', '_id name email');
+
+  const contactsList = await Contact.find({
+    owner: _id,
+    favorite: favorite || { $in: [true, false] },
+  })
+    .skip(skip)
+    .limit(Number(limit))
+    .populate('owner', '_id name email');
 
   res.status(200).json({
     status: 'success',
