d53df989efdb5d00adba55d3141494e69670ddc2
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-03-27 23:15:17 +0300

    init hw01-express

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,119 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
 
-const listContacts = async () => {}
+const contactsPath = path.resolve(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+/**
+ * Возвращает объект со всеми контактами в формате JSON.
+ * @return {json} contacts, файл c объектами контактов
+ */
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+/**
+ * Записывает контакы в файл contacts.json.
+ * @param {array} contacts, массив объектов с контактами.
+ */
+const writeContacts = async contacts => {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+/**
+ * Возвращает объект контакта с заданным ID.
+ * @param {string} contactId, ID контакта.
+ * @return {object} contact, объект с данными контакта.
+ */
+const getContactById = async contactId => {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(({ id }) => id === contactId);
 
-const updateContact = async (contactId, body) => {}
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Добавляет контакт и возвращает созданный объект.
+ * @param {string} name, имя.
+ * @param {string} email, электронная почта.
+ * @param {string} phone, номер телефона.
+ * @return {Object} contact, объект нового контакта.
+ */
+const addContact = async ({ name, email, phone }) => {
+  try {
+    const contact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    const contacts = await listContacts();
+    const newContacts = [...contacts, contact];
+    await writeContacts(newContacts);
+
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Удаляет контакт с заданным ID
+ * @param {string} contactId, ID контакта.
+ * @return {object} removedContact, объект удаленного контакта или undefined.
+ */
+const removeContact = async contactId => {
+  try {
+    const contacts = await listContacts();
+    const removedContact = contacts.find(({ id }) => id === contactId);
+
+    if (removedContact) {
+      const updatedContacts = contacts.filter(({ id }) => id !== contactId);
+      await writeContacts(updatedContacts);
+    }
+
+    return removedContact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Обнавляет контакт с заданным ID
+ * @param {string} contactId, ID контакта.
+ * @param {object} update, объект с измененными полями.
+ * @return {object} updatedContact, объект обновленного контакта или undefined.
+ */
+const updateContact = async (contactId, update) => {
+  const contacts = await listContacts();
+  const updatedContactIdx = contacts.findIndex(({ id }) => id === contactId);
+
+  if (updatedContactIdx === -1) {
+    return undefined;
+  }
+
+  const updatedContact = { ...contacts[updatedContactIdx], ...update };
+
+  contacts.splice(updatedContactIdx, 1, updatedContact);
+  await writeContacts(contacts);
+
+  return updatedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +121,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,27 +1,112 @@
-const express = require('express')
-const contacts = require("../../models/contacts")
+const express = require('express');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require('../../models/contacts');
+const Joi = require('joi');
+const CustomJoi = Joi.extend(require('joi-phone-number'));
 
-const router = express.Router()
+const router = express.Router();
 
+/**
+ * ============================ Получение всех контакта
+ */
 router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result)
-})
+  const result = await listContacts();
+  res.status(200).json(result);
+});
 
+/**
+ * ============================ Получение контакта по ID
+ */
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contactId = req.params.contactId;
+  const contact = await getContactById(contactId);
 
+  if (!contact) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  res.status(200).json(contact);
+});
+
+/**
+ * ============================ Добавление контакта
+ */
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const validationSchema = CustomJoi.object({
+    name: CustomJoi.string().min(3).max(30).required(),
 
+    email: CustomJoi.string()
+      .email({
+        minDomainSegments: 2,
+      })
+      .required(),
+
+    phone: CustomJoi.string()
+      .phoneNumber({ format: 'international' })
+      .required(),
+  });
+
+  const { error: validationError, value: validContact } =
+    validationSchema.validate(req.body);
+
+  if (validationError) {
+    return res.status(400).json({ message: validationError.message });
+  }
+
+  const contact = await addContact(validContact);
+  res.status(201).json(contact);
+});
+
+/**
+ * ============================ Удаление контакта
+ */
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contactId = req.params.contactId;
 
+  const removed = await removeContact(contactId);
+
+  if (removed) {
+    return res.status(200).json({ message: 'contact deleted' });
+  }
+
+  res.status(404).json({ message: 'Not found' });
+});
+
+/**
+ * ============================ Обновление контакта
+ */
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contactId = req.params.contactId;
 
-module.exports = router
+  const validationSchema = CustomJoi.object({
+    name: CustomJoi.string().min(3).max(30),
+
+    email: CustomJoi.string().email({
+      minDomainSegments: 2,
+    }),
+
+    phone: CustomJoi.string().phoneNumber({ format: 'international' }),
+  }).min(1);
+
+  const { error: validationError, value: validUpdate } =
+    validationSchema.validate(req.body);
+
+  if (validationError) {
+    return res.status(400).json({ message: validationError.message });
+  }
+
+  const updatedContact = await updateContact(contactId, validUpdate);
+
+  if (!updatedContact) {
+    return res.status(404).json({ message: 'Contact not found' });
+  }
+
+  return res.status(200).json(updatedContact);
+});
+
+module.exports = router;
