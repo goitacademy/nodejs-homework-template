b6f1d424ff12c7899fe53c7fc4e96de7f0df75c1
DmitryNikolin <ni-ko-dim@ukr.net>
2023-03-27 23:20:16 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, _, next) => {
   const { authorization = '' } = req.headers;
   const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== 'Bearer') {
+  if (bearer !== "Bearer" || !token) {
     next(createError(401));
   }
   try {
