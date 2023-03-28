df1c998fb08d03b947f4f7c094a5cedde1318898
IrynaKniazieva <gleb0812@gmail.com>
2023-03-28 23:48:32 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+}) 
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
