43780dfe7c17f48d27a7fb2a42d70c01ec9c3e77
ViktorZvonar <viktorzvonar@yahoo.com>
2023-03-28 22:11:34 +0300

    firsy api done

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,62 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const { nanoid } = require("nanoid");
 
-const getContactById = async (contactId) => {}
+const path = require("path");
 
-const removeContact = async (contactId) => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
 
-const updateContact = async (contactId, body) => {}
+// const getContactById = async (contactId) => {
+//   const contacts = await listContacts();
+//   const contact = contacts.find((item) => item.id === contactId);
+
+//   return contact ? contact : null;
+// };
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async (body) => {
+  const newContact = {
+    id: nanoid(),
+    name: body.name,
+    email: body.email,
+    phone: body.phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
-  getContactById,
+  // getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
