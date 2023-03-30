e9cfc125b9e06275ac2b59019c5db752ad62dd90
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-30 18:41:19 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,70 +1,19 @@
 const express = require("express");
-const contacts = require("../../models/contacts");
+const ctrlContacts = require("../../controllers/contacts");
 
 const router = express.Router();
 
-const Joi = require("joi");
-const schema = Joi.object({
-  name: Joi.string().required(),
-	email: Joi.string().required(),
-	phone: Joi.string().required(),
-})
 
-router.get('/', async (req, res, next) => {
-  const contactsList = await contacts.listContacts();
-  res.json({ status: 200, body: contactsList })
+router.get("/", ctrlContacts.get);
 
-});
+router.get('/:contactId', ctrlContacts.getById);
 
+router.post('/contacts', ctrlContacts.createContact)
 
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const getContact = await contacts.getContactById(contactId);
-  if (getContact) {
-    res.json({ status: 200, data: getContact });
-  } else {
-    res.json({ status: 404, message: "Not found" });
-  }
-});
+router.delete("/:contactId", ctrlContacts.removeContact)
 
-router.post('/', async (req, res, next) => {
-  console.log(req.body);
-  const body = req.body;
-  const { err } = schema.validate(req.body)
-  if (err) {
-    res.json({ status: 400, message: 'missing required name field' });
-  } else {
-    const newContact = await contacts.addContact(body);
-    res.json({ status: 201, data: newContact })
-  }
-});
+router.put("/:contactId", ctrlContacts.updateContact)
 
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const deleteContact = await contacts.removeContact(contactId);
-  if (deleteContact) {
-     res.json({ status: 200, data: deleteContact, message: 'contact deleted'
-    })
-  } else {
-    res.json({ status: 404, message: 'Not found' })
-  }
-})
+router.patch('/:contactId/favorite', ctrlContacts.updateFavorite)
 
-router.put('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const body = req.body;
-  const { err } = schema.validate(req.body);
-  if (err) {
-    console.log(err);
-    return res.json({ sttaus: 400, message: 'missing fields' });
-  } 
-  console.log(body);
-  const renameContact = await contacts.updateContact(contactId, body);
-  if (renameContact) {
-    res.json({ status: 200, data: renameContact });
-  }else {
-    res.json({ status: 404, message: 'Not found' });
-  }
-})
-
-module.exports = router
+module.exports = router;
