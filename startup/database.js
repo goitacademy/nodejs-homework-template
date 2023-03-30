8b34be6d0fc02fb2d235456d9673ca30bf182a18
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-30 19:31:23 +0200

    Update database.js

--- startup/database.js
+++ startup/database.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const dbPath = process.env.MONGO_SECRET;
 
-    const connectDatabase= async ()=> {
-        await mongoose
-            .connect(dbPath)
-            .then(() => console.log("Database connection successful..."))
-            .catch((err) => console.log(`Server not running. Error message: ${err.message}`));
+const connectDatabase = async () => {
+    await mongoose
+        .connect(dbPath)
+        .then(() => console.log("Database connection successful..."))
+        .catch((err) => {
+            console.log(`Server not running. Error message: ${err.message}`);
             process.exit(1);
-};
+        });
+}
 module.exports = { connectDatabase };
