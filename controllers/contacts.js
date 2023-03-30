a1b5834df0e7a4367599125c0b3b30f6bddcc232
Andrew Miller <ammoldervn@gmail.com>
2023-03-29 23:41:56 -0700

    start

---
+++ controllers/contacts.js
@@ -0,0 +1,56 @@
+const { HttpError } = require("../helpers/index");
+const models = require("../models/contacts");
+
+async function getContacts(req, res, next) {
+  const { limit } = req.query;
+  const contacts = await models.listContacts({ limit });
+  return res.json(contacts);
+}
+
+async function getContact(req, res, next) {
+  const { contactId } = req.params;
+  const contact = await models.getContactById(contactId);
+
+  if (!contact) {
+    return next(HttpError(404, "Not found"));
+  }
+  return res.json(contact);
+}
+
+async function createContact(req, res, next) {
+  const { name, email, phone } = req.body;
+  const newContact = await models.addContact(name, email, phone);
+  res.status(201).json(newContact);
+}
+
+async function deleteContact(req, res, next) {
+  const { contactId } = req.params;
+  const contact = await models.getContactById(contactId);
+
+  if (!contact) {
+    return next(HttpError(404, "Not found"));
+  }
+
+  await models.removeContact(contactId);
+  return res.status(200).json({ message: "contact deleted" });
+}
+
+async function changeContact(req, res, next) {
+  const { contactId } = req.params;
+  const body = req.body;
+  const changeContact = await models.updateContact(contactId, body);
+
+  if (!changeContact) {
+    return next(HttpError(400, "missing fields"));
+  }
+
+  res.status(200).json(changeContact);
+}
+
+module.exports = {
+  getContacts,
+  getContact,
+  createContact,
+  deleteContact,
+  changeContact,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,25 @@
-const express = require('express')
+const express = require("express");
+const { tryCatchWrapper } = require("../../helpers");
+const { validateBody } = require("../../middlewares/index");
+const { addContactSchema } = require("../../schemas/contacts");
+const router = express.Router();
 
-const router = express.Router()
+const {
+  getContacts,
+  getContact,
+  createContact,
+  deleteContact,
+  changeContact,
+} = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", tryCatchWrapper(getContacts));
+router.get("/:contactId", tryCatchWrapper(getContact));
+router.post(
+  "/",
+  validateBody(addContactSchema),
+  tryCatchWrapper(createContact)
+);
+router.delete("/:contactId", tryCatchWrapper(deleteContact));
+router.put("/:contactId", tryCatchWrapper(changeContact));
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
