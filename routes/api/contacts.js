b9504ead3466759103099d9f7a8201c3b5b4d713
Mary Danyliuk <marydanyliuk@gmail.com>
2023-03-30 16:27:22 +0300

    add api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,34 +1,100 @@
 const express = require("express");
+const Joi = require("joi");
 const contacts = require("../../models/contacts");
-// const listContacts = require("../../models/contacts");
+
+const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
-// router.get("/", contacts.listContacts);
+const addSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `"name" is required`,
+    "string.empty": `"name" cannot be empty`,
+    "string.base": `"name" must be string`,
+  }),
+  email: Joi.string().required().messages({
+    "any.required": `"email" is required`,
+    "string.empty": `"email" cannot be empty`,
+    "string.base": `"email" must be string`,
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": `"phone" is required`,
+    "string.empty": `"phone" cannot be empty`,
+    "string.base": `"phone" must be string`,
+  }),
+});
 
 router.get("/", async (req, res, next) => {
-  // const allContacts = contacts.listContacts();
-  // res.json(allContacts);
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Ther's no ${contactId} in phonebook`);
+      // const error = new Error(`Ther's no ${contactId} in phonebook`);
+      // error.status = 404;
+      // throw error;
+
+      // return res.status(404).json(`Ther's no ${contactId} in phonebook`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  console.log(req.body);
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contacts.add(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateById(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, `Book with ${contactId} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.deleteById(contactId);
+    if (!result) {
+      throw HttpError(404, `Book with ${contactId} not found`);
+    }
+    res.json({
+      message: "Delete success",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
