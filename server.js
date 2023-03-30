32ed5b299790c209e2ed3032978293a0acc45016
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-30 18:41:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
-const app = require('./app')
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { connectDatabase } = require("./startup/database.js");
+
+connectDatabase();
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+app.use(logger(formatsLogger));
+app.use(express.json());
+app.use(cors());
+
+const contactsRouter = require("./routes/api/contacts");
+app.use("/api/contacts", contactsRouter);
+
+
+
+ app.use((req, res) => {
+  res.status(404).json({
+    message: "Use api on routes: /api/contacts",
+    data: "Not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Server is listening on port ' + port);
+});
