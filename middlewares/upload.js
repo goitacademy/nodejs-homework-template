8b3215ae116e2f35b918da5f37a6984d78af89d0
MishynaIra <gortenzia1987@gmail.com>
2023-03-30 19:21:16 +0300

    middleware multer

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,3 +1,23 @@
-
+const path = require("path");
 const multer  = require('multer');
 
+
+const tempDir = path.join(__dirname, "../", "temp");
+
+const multerConfig = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, tempDir)
+    },
+    filename:(req, file, cb) => {
+        cb(null, file.originalname)
+    },
+    limits: {
+        fileSize: 2048
+    }
+})
+
+const upload = multer({
+    storage: multerConfig
+})
+
+module.exports = upload;
--- routes/api/avatar.js
+++ routes/api/avatar.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const {avatar: ctrl} = require('../../controllers');
 const auth = require("../../middlewares/auth");
+const upload = require("../../middlewares/upload");
 
-router.patch('/avatars', auth, ctrl.updateAvatar);
+router.patch('/avatars', auth, upload, ctrl.updateAvatar);
 
 module.exports = router;
