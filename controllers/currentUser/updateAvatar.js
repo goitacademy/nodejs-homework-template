b1509339461d3fb369ccb73ea1bbf3231203e102
MishynaIra <gortenzia1987@gmail.com>
2023-03-31 11:49:33 +0300

    сохраняю avatar под уник id

--- controllers/currentUser/updateAvatar.js
+++ controllers/currentUser/updateAvatar.js
@@ -6,11 +6,13 @@ const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
     const {path: tempUpload, originalname} = req.file;
+    const {_id: id} = req.user;
+    const imageName = `${id}_${originalname}`;
     try {
 
-        const resultUpload = path.join(avatarDir, originalname);
+        const resultUpload = path.join(avatarDir, imageName);
         await fs.rename(tempUpload, resultUpload);
-        const avatarURL = path.join("public", "avatars",originalname );
+        const avatarURL = path.join("public", "avatars",imageName );
         await User.findByIdAndUpdate(req.user._id, {avatarURL});
         res.json({avatarURL});
     } catch (error) {
