f97d3b45da1ff70bb979653535212cff09912217
MishynaIra <gortenzia1987@gmail.com>
2023-03-31 10:35:41 +0300

    error 401 поправила статус

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const {User} = require("../models");
 const jwt = require("jsonwebtoken");
-//
-// const createError = require('http-errors');
+
+
 
 // извлекаем заголовок авторизации
 // разделяем на 2 части с помощью split
