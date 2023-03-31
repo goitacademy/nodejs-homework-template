8f2b13928f94c0e8be98478b31b3352806a46818
Mary Danyliuk <marydanyliuk@gmail.com>
2023-04-01 00:38:52 +0300

    fixed according to review

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -17,8 +17,8 @@ const router = express.Router();
 
 router.get("/", getAllContacts);
 router.get("/:contactId", getById);
-router.post("/", add, validateAddContact);
-router.put("/:contactId", update, validateUpdContact);
+router.post("/", validateAddContact, add);
+router.put("/:contactId", validateUpdContact, update);
 router.delete("/:contactId", remove);
 
 module.exports = router;
--- validators/contactsValidators.js
+++ validators/contactsValidators.js
@@ -1,44 +1,26 @@
 const Joi = require("joi");
-const { HttpError } = require("../helpers");
 
-const addSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": `"name" is required`,
-    "string.empty": `"name" cannot be empty`,
-    "string.base": `"name" must be string`,
-  }),
-  email: Joi.string().required().messages({
-    "any.required": `"email" is required`,
-    "string.empty": `"email" cannot be empty`,
-    "string.base": `"email" must be string`,
-  }),
-  phone: Joi.string().required().messages({
-    "any.required": `"phone" is required`,
-    "string.empty": `"phone" cannot be empty`,
-    "string.base": `"phone" must be string`,
-  }),
+const addSchemaAdd = Joi.object({
+  name: Joi.string().trim().required(),
+  email: Joi.string().trim().email().required(),
+  phone: Joi.string().trim().required(),
 });
 
-const validateAddContact = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-  } catch (error) {
-    next(error);
+const validateAddContact = (req, res, next) => {
+  const { error } = addSchemaAdd.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: "missing required name field" });
   }
+  next();
 };
 
-const validateUpdContact = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-  } catch (error) {
-    next(error);
+const validateUpdContact = (req, res, next) => {
+  const { error } = addSchemaAdd.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: "missing required name field" });
   }
+
+  next(error);
 };
 
 module.exports = {
