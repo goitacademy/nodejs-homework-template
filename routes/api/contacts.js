8b24963cd04bc1c15c8a2c79b6687ba71433496a
IevgenLychman <gora.erikovich@gmail.com>
2023-03-31 14:22:27 +0300

    Second

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,13 +4,28 @@ const router = express.Router();
 
 const contacts = require("../../models/contacts");
 
+const { HttpError } = require("../../helpers");
+
 router.get("/", async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Contact with ${contactId} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
