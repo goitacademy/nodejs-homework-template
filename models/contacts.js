5ad7dac55ea6c78d52d4783425655a80e1e283d2
RuslanMiki <mikitchenko_ruslan@ukr.net>
2023-03-31 16:46:07 +0200

    finish1

--- models/contacts.js
+++ models/contacts.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 const path = require("path");
 const shortid = require("shortid");
 const contactsPath = path.resolve("./models/contacts.json");
