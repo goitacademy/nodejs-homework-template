64bd836c5bf2a48f6def45cc1b672e4c65884574
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-01 18:32:02 +0200

    Create routes.js

---
+++ routes/api/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const contactRouter = require("./contacts");
+
+const router = express.Router();
+router.use("/contacts", contactRouter);
+
+module.exports = router;
