72dc53f5e58be46dd1ba69aa97900c13852e6624
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2023-04-01 23:45:48 +0300

    +

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const controllersWraper = require("./contollersWraper");
 const handleMongooseError = require("./handleMongooseError");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   controllersWraper,
   handleMongooseError,
+  sendEmail,
 };
