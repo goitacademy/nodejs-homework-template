db4112600093cdf2bfc2c8a2d8e00d748e5b2384
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-02 14:50:57 +0200

    Update index.js

--- service/index.js
+++ service/index.js
@@ -8,8 +8,6 @@ const getAllContacts = () => {
 const getContactById = (id) => {
   if (mongoose.Types.ObjectId.isValid(id)) {
     return Contact.findOne({ _id: id })
-  } else {
-    return null
   }
 }
 
@@ -21,14 +19,14 @@ const removeContact = (id) => {
   if (mongoose.Types.ObjectId.isValid(id)) {
     return Contact.findOneAndRemove({ _id: id })
   }
-  return null
+  
 }
 
 const updateContact = (id, fields) => {
   if (mongoose.Types.ObjectId.isValid(id)) {
     return Contact.findByIdAndUpdate({ _id: id }, fields, { new: true })
   }
-  return null
+ 
 }
 
 module.exports = {
