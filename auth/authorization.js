305537a519577161c323a3d2336605d84fad9cda
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-03 19:56:30 +0200

    Create authorization.js

---
+++ auth/authorization.js
@@ -0,0 +1,18 @@
+const passport = require("passport");
+
+const authorization = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (!user || err) {
+      return res.status(401).json({
+        status: "error",
+        code: 401,
+        message: "Unauthorized",
+        data: "Unauthorized",
+      });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+module.exports = authorization;
