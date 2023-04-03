b58e3d623d4ea7412430243b07623a3c0a4a6244
Enjoy3325 <enjoy3325@gmail.com>
2023-04-03 13:02:27 +0300

    ++

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config();
 const logger = require('morgan')
 const cors = require('cors')
+const cookieParser = require('cookie-parser');
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -13,6 +14,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/contacts', contactsRouter)
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,32 +1,11 @@
 const express = require('express')
-const Joi = require('joi');
 
+
+const addShema = require('../../shemas');
 const contacts = require('../../models/contacts')
 const { HttpError } = require('../../helpers');
 
 const router = express.Router()
-
-
-const addShema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": `"name" is required`,
-    "string.empty": `"name" cannot be empty`,
-    "string.base": `"name" must be string`
-  }),
-  email: Joi.string().required().messages({
-    "any.required": `"email" is required`,
-    "string.empty": `"email" cannot be empty`,
-    "string.base": `"email" must be string`
-  }),
-  phone: Joi.number().required().messages({
-    "any.required": `"phone" is required`,
-    "number.empty": `"phone" cannot be empty`,
-    "number.base": `"phone" must be number`
-  }),
-  phone: Joi.binary().min(6),
-  phone: Joi.binary().max(16)
-})
-
 
 
 router.get('/', async (req, res, next) => {
