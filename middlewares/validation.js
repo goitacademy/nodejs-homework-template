d7e6a2f0b3d692fe8e88f2a1ee11bcbec635da79
Владимир Перетятько <djbob2000@gmail.com>
2023-04-03 16:08:50 +0300

    fix json

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -2,7 +2,7 @@ const validation = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      return res.status(400).send(error.details[0].message);
+      return res.status(400).json(error.details[0].message);
     }
     next();
   };
