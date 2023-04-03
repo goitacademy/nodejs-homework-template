2c6c309c8f9cd2f649820be02431d9e7352ba676
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-03 19:56:56 +0200

    Create usersSchema.js

---
+++ models/usersSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+        },
+        subscription: {
+            type: String,
+            enum: ["starter", "pro", "business"],
+            default: "starter",
+        },
+        token: {
+            type: String,
+            default: null,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
+
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(6));
+};
+
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = {User};
