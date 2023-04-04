b14561281a8e6baa47e7fde328d2d779bf53463f
MishynaIra <gortenzia1987@gmail.com>
2023-04-04 11:45:36 +0300

    создала маршрут+переверяю тело+контроллер

--- controllers/currentUser/index.js
+++ controllers/currentUser/index.js
@@ -1,8 +1,11 @@
 const getCurrent = require("./getCurrent");
 const updateAvatar = require("./updateAvatar")
 const verifyEmail = require("./verifyEmail");
+const writeVerifyEmail = require("./writeVerifyEmail");
+
 module.exports = {
     getCurrent,
     updateAvatar,
-    verifyEmail
+    verifyEmail,
+    writeVerifyEmail
 }
