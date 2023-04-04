f1d9241a2bf4649b98466dff48cf5ef5d37de728
TetianaKuzma <tetiana.b97@gmail.com>
2023-04-04 10:59:11 +0300

    Code refactoring

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contacts.routes");
 
 const app = express();
 
