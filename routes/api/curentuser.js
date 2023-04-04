214917575fb33ab3b17558305cafba7d03526525
MishynaIra <gortenzia1987@gmail.com>
2023-04-04 11:45:25 +0300

    создала маршрут+переверяю тело+контроллер

--- routes/api/curentuser.js
+++ routes/api/curentuser.js
@@ -6,11 +6,13 @@ const auth = require("../../middlewares/auth");
 
 const {currentUser: ctrl} = require("../../controllers");
 const upload = require("../../middlewares/upload");
+const {validation} = require("../../middlewares/validation");
+const {verifyEmailSchema} = require("../../models/users");
 
 
 router.get("/current", auth, ctrl.getCurrent);
 router.patch('/avatars', auth, upload.single("avatar"), ctrl.updateAvatar);
 router.get("/verify/:verificationToken ", ctrl.verifyEmail);
-router.post("/verify")
+router.post("/verify", validation(verifyEmailSchema), ctrl.writeVerifyEmail)
 
 module.exports = router;
