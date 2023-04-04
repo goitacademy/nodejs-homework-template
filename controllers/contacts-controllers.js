587c935e64ae6374efcc180eb9c49be1544ddbc7
Bogdan <bogdanberezka@gmail.com>
2023-04-04 16:32:28 +0300

    made-a-decorator-&-code-refactor

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,103 +1,56 @@
-const Joi = require("joi");
+const { ctrlWrapper } = require("../utils");
 
 const contacts = require("../models/contacts");
 
 const { HttpError } = require("../helpers");
 
-const addSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": `missing required "name"`,
-    "string.empty": `"name" cannot be empty`,
-  }),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required()
-    .messages({
-      "any.required": `missing required "email"`,
-      "string.empty": `"email" cannot be empty`,
-    }),
-  phone: Joi.number().required().messages({
-    "any.required": `missing required "phone"`,
-    "string.empty": `"phone" cannot be empty`,
-  }),
-});
-
-const getAllContacts = async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAllContacts = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
 };
 
-const getContactById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404);
   }
+  res.json(result);
 };
 
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+const addContact = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
 };
 
-const deleteContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
+const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
 
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-
-    res.status(200).json({
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+
+  res.status(200).json({
+    message: "contact deleted",
+  });
 };
 
-const updateContactById = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing fields");
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
+const updateContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
 
-    if (!result) {
-      throw HttpError(404, `Contact with ${contactId} not found`);
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, `Contact with ${contactId} not found`);
   }
+
+  res.json(result);
 };
 
 module.exports = {
-  getAllContacts,
-  getContactById,
-  addContact,
-  deleteContact,
-  updateContactById,
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  updateContactById: ctrlWrapper(updateContactById),
 };
--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -3,16 +3,24 @@ const express = require("express");
 //імпортуємо контроллери з окремого файлу який ми створили і винесли їх туди
 const ctrl = require("../../controllers/contacts-controllers");
 
+const { validateBody } = require("../../utils");
+
+const schemas = require("../../schemas/contacts");
+
 const router = express.Router();
 
 router.get("/", ctrl.getAllContacts);
 
 router.get("/:contactId", ctrl.getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
 router.delete("/:contactId", ctrl.deleteContact);
 
-router.put("/:contactId", ctrl.updateContactById);
+router.put(
+  "/:contactId",
+  validateBody(schemas.addSchema),
+  ctrl.updateContactById
+);
 
 module.exports = router;
