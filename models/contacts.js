d30ebb52ec6f2753b9f1f4852760f18390067028
Anna <michalowska.aneczka@gmail.com>
2023-04-04 21:54:47 +0200

    nie_działa
    pod odpaleniu lokalnie wyskakuje błąd: GET http://localhost:3000/ 404 (Not Found) nie wiem co jest źle

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,20 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactPath);
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +22,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
