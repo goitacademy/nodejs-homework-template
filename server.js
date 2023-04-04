017f4433c2e1129c0bc647dafd53af58152c036b
Olena <newsroommy@gmail.com>
2023-04-04 15:07:39 +0300

    add

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require("./app");
+// bs0E0UHJvKSvSReF
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
