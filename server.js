a28241228d981ccf1083775bf819f439d7cf9325
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-04 20:17:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+
+require("./config/config-passport");
 
 const { connectDatabase } = require('./startup/database.js');
 
 connectDatabase();
 const app = express();
 const router = require('./routes/api/routes.js');
-app.use(router);
+app.use("/api", router);
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(express.json());
