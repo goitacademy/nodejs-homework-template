35f01c52484031e81c21fbf1933641d939572cff
Mary Danyliuk <marydanyliuk@gmail.com>
2023-04-04 19:12:16 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://MaryDan:4W3dbq1I6FNwX1DA@cluster0.0qilsj8.mongodb.net/phonebook?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => console.log(error.message));
+
+const app = require("./app");
+
+// 4W3dbq1I6FNwX1DA
