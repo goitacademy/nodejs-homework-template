2c2a0bc0d236da906404f89f606d649789c442d9
Anna Bramarova <annabramarova@gmail.com>
2023-04-04 14:01:38 +0300

    add: helpers and db model
    refactor controllers with middleware

---
+++ helpers/controller-helpers.js
@@ -0,0 +1,26 @@
+const asyncMiddleware = (controller) => async (req, res, next) => {
+  try {
+    await controller(req, res, next);
+  } catch (error) {
+    console.error(error.message);
+    next(new Error('Internal server error'));
+  }
+};
+
+const HTTPError = (status, message) => {
+  const defaultMessage = {
+    400: 'Bad request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not found',
+  };
+  const error = new Error(message || defaultMessage[status]);
+  error.status = status;
+  return error;
+};
+
+module.exports = {
+  asyncMiddleware,
+  HTTPError,
+  notFoundMessage: HTTPError(404, 'Not found'),
+};
