09068e2b45efc9c020d343b2b0f9d03f205439d4
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-05 10:54:18 +0200

    Update validator.js

--- service/validator.js
+++ service/validator.js
@@ -14,9 +14,8 @@ const addContactSchema = Joi.object({
     .required(),
 
   phone: Joi.string()
-       .pattern(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im)
-        // .pattern(/^[0-9]+$/)
-         .required(),
+    .pattern(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im)
+    .required(),
 
   favorite: Joi.boolean().optional(),
 
