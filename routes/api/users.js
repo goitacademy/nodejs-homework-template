703c20e095f783945a1224ab64540647778fa8ea
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-05 22:49:30 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const ctrlUser = require("../../controllers/users");
+
+const router = express.Router();
 const authorization = require("../../auth/authorization");
 
 router.post("/signup", ctrlUser.signUp);
