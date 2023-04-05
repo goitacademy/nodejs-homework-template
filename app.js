f0d785e9b32b9ee046e7a226b98cd96ec98e2063
ViktorZvonar <viktorzvonar@yahoo.com>
2023-04-05 18:33:15 +0300

    final

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 
 const cors = require("cors");
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
