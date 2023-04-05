adb44a2297db431dbddf004ce226a5637bb08cc8
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-05 20:52:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,21 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
-require("./config/config-passport");
+
 
 const { connectDatabase } = require('./startup/database.js');
 
 connectDatabase();
 const app = express();
-const router = require('./routes/api/routes.js');
-app.use("/api", router);
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(express.json());
 app.use(cors());
 
+require("./config/config-passport");
+const router = require('./routes/api/routes.js');
+app.use("/api", router);
 
 app.use((req, res) => {
   res.status(404).json({
@@ -31,7 +33,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
   console.log('Server is listening on port ' + port)
 });
