511dbf18994a8b3bddee8a977c0c38360415400e
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-04-05 16:50:20 +0200

    Update logout.js
    logout updated

--- routes/api/logout.js
+++ routes/api/logout.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const { logoutUser } = require("../../controllers/users");
 
 // const User = require("../../models/user");
 const auth = require("../../auth/auth");
+const jwtSecret = process.env.JWT_SECRET;
 
 router.get("/logout", auth, async (req, res) => {
   try {
-    const { token } = req.body;
-    const user = await logoutUser.logout(token);
+    const { token } = req.headers.authorization;
+    const verify = jwt.verify(token, jwtSecret);
+    const user = await logoutUser.logout(verify);
     res.status(204).send("Logout success", user);
   } catch (error) {
     res.status(500).send("Server error");
