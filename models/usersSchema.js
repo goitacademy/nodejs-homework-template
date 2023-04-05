304077154886c347d44cd7efede45c8392c5ffe1
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-05 20:52:38 +0200

    Update usersSchema.js

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -3,30 +3,30 @@ const bCrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-    {
-        password: {
-            type: String,
-            required: [true, "Password is required"],
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            unique: true,
-        },
-        subscription: {
-            type: String,
-            enum: ["starter", "pro", "business"],
-            default: "starter",
-        },
-        token: {
-            type: String,
-            default: null,
-        },
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
     },
-    {
-        versionKey: false,
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
 );
 
 userSchema.methods.setPassword = function (password) {
