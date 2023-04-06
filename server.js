d057610e69310ad0ea1b0134cb4b3385f55641b7
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-06 21:51:24 +0300

    handle error, complete process

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ mongoose
     app.listen(3000);
     console.log("Database connection successful");
   })
-  .catch((error) => error.message);
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
