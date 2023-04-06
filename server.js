749d176f6ddb5e1bcb166113eb76c1cd9b011cc8
Mary Danyliuk <marydanyliuk@gmail.com>
2023-04-06 11:01:39 +0300

    upd server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://MaryDan:4W3dbq1I6FNwX1DA@cluster0.0qilsj8.mongodb.net/phonebook?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://MaryDan:4W3dbq1I6FNwX1DA@cluster0.0qilsj8.mongodb.net/phonebook?retryWrites=true&w=majority";
 const app = require("./app");
+
+// console.log(process.env.DB_HOST);
+
+const { DB_HOST } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
@@ -10,6 +15,9 @@ mongoose
       console.log("Database connection successful");
     });
   })
-  .catch((error) => console.log(error.message));
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 // 4W3dbq1I6FNwX1DA
