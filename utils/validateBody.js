e9613f0caa234ea5efe2dd2eba274a5b5e7c8efb
Serhii Markov <goo.komalel@gmail.com>
2023-04-06 19:26:06 +0300

    Update validateBody.js

--- utils/validateBody.js
+++ utils/validateBody.js
@@ -6,9 +6,9 @@ const validateBody = schema => {
         if (error) {
             console.log("error! --- ", error);
             // next(res.status(400).json({ "message": "missing fields"}));
-            const missingField = error.message.replace("is required", "").trim();
-            console.log("missingField! --- ", missingField);
-            next(HttpError(400, `missing ${missingField} name field`));
+            const missingField = error.message.replace("is required", "").trim().slice(1, -1);
+            console.log("missingField! --- ", missingField.length);
+            next(HttpError(400, `missing ${missingField} field`));
         }
         next();
     }
