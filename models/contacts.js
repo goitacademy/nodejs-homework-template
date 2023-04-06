67cbcf99d5b2586f413ad6bbac275db505b4b636
Maksym Lytovchenko <lmb1988@ukr.net>
2023-04-06 11:57:55 +0300

    test

--- models/contacts.js
+++ models/contacts.js
@@ -3,7 +3,7 @@ const { nanoid } = require("nanoid");
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "contacts.json");
-
+// console.log(contactsPath);
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
