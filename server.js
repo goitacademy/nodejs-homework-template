592ca0dd8c9f1a239d59106ed451d5969bf2fd9a
Enjoy3325 <enjoy3325@gmail.com>
2023-04-07 00:02:15 +0300

    connected mongoose in project

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
+const app = require('./app')
+const DB_HOST = "mongodb+srv://Artem:FGCMRJhY0Omj8qG0@cluster0.1uz7aty.mongodb.net/contacts_directory?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+  .then(()=>app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
-})
+  }))
+.catch(error=>console.log(error.message))
+// Artem  - логин
+// FGCMRJhY0Omj8qG0  - пароль
+
