d52bc6e8f9d5682d6a622aa8cf12e74eb58a19ad
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-06 11:37:00 +0200

    Update authorization.js

--- auth/authorization.js
+++ auth/authorization.js
@@ -4,10 +4,7 @@ const authorization = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user) => {
     if (!user || err) {
       return res.status(401).json({
-        status: "error",
-        code: 401,
-        message: "Unauthorized",
-        data: "Unauthorized",
+       message: "Unauthorized",
       });
     }
     req.user = user;
