4ec7c7764beb34f7d4a838d5d6f31673cb2f450e
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-06 15:27:03 +0300

    test of postman work

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require("express");
-// const logger = require('morgan')
-// const cors = require('cors')
+const logger = require("morgan");
+const cors = require("cors");
 
-// const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 const contacts = require("./models/contacts.json");
 const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 app.get("/contacts", (req, res) => {
   res.json(contacts);
 });
 
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
 module.exports = app;
-// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-// app.use(logger(formatsLogger))
-// app.use(cors())
-// app.use(express.json())
-
-// app.use('/api/contacts', contactsRouter)
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
-// })
-
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message })
-// })
--- middleware/validation.js
+++ middleware/validation.js
@@ -1,15 +1,15 @@
-const Joi = require("joi");
+// const Joi = require("joi");
 
-const validateBody = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      return res
-        .status(400)
-        .json({ message: "тут додати той меседж, як по ТЗ" });
-    }
-    next();
-  };
-};
+// const validateBody = (schema) => {
+//   return (req, res, next) => {
+//     const { error } = schema.validate(req.body);
+//     if (error) {
+//       return res
+//         .status(400)
+//         .json({ message: "тут додати той меседж, як по ТЗ" });
+//     }
+//     next();
+//   };
+// };
 
-module.exports = validateBody;
+// module.exports = validateBody;
