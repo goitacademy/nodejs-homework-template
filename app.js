4c40420132b463742ec773549b98245b5460c348
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-06 10:45:12 +0300

    work with validation

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+// const logger = require('morgan')
+// const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+// const contactsRouter = require('./routes/api/contacts')
+const contacts = require("./models/contacts.json");
+const app = express();
+app.get("/contacts", (req, res) => {
+  res.json(contacts);
+});
 
-const app = express()
+module.exports = app;
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// app.use(logger(formatsLogger))
+// app.use(cors())
+// app.use(express.json())
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// app.use('/api/contacts', contactsRouter)
 
-app.use('/api/contacts', contactsRouter)
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found' })
+// })
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
-
-module.exports = app
+// app.use((err, req, res, next) => {
+//   res.status(500).json({ message: err.message })
+// })
