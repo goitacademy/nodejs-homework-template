9de3b31bc82a0cd1bff2337692f73031b8e00671
Stas-0k <brickbybrick15@gmail.com>
2023-04-07 00:16:43 +0300

    commit
    commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,42 @@
-// const fs = require('fs/promises')
+const shortid = require('shortid');
+const fs = require("fs").promises;
+const path = require("node:path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "UTF8");
+  const contacts = JSON.parse(data);
 
-const removeContact = async (contactId) => {}
-
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  return contacts;
 }
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item=>item.id===contactId);
+    if(!idx){
+        return null;
+    }
+    const result = contacts.filter((_,index)=>index!==idx);
+    await fs.writeFile(contactsPath,JSON.stringify(result));
+    return contacts[idx]  
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {id:shortid .generate(), name, email, phone }
+    contacts.push(newContact)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts))
+    return newContact
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact, };
