7e599cfdfcc507691614a031f17de76c70bb446b
Olena <newsroommy@gmail.com>
2023-04-07 10:05:11 +0300

    add PORT to .env

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(3000))
+  .then(() => app.listen(PORT))
   .catch((error) => console.log(error.message));
