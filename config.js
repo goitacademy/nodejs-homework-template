b331a4196aa97d9b7e75f4eb7029829389126fd3
Olena <newsroommy@gmail.com>
2023-04-07 09:56:43 +0300

    move DB_HOST to .env

--- config.js
+++
@@ -1,5 +0,0 @@
-// bs0E0UHJvKSvSReF
-const DB_HOST =
-  "mongodb+srv://Olena:bs0E0UHJvKSvSReF@cluster0.5zw2llw.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-module.exports = { DB_HOST };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const app = require("./app");
+
 const { DB_HOST } = process.env;
 
 mongoose
