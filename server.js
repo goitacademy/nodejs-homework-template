0125ca916443e5f235bb24262134616387249c61
Olena <newsroommy@gmail.com>
2023-04-07 09:31:22 +0300

    move DB_HOST

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-// bs0E0UHJvKSvSReF
+const { DB_HOST } = require("./config");
 
-const DB_HOST =
-  "mongodb+srv://Olena:bs0E0UHJvKSvSReF@cluster0.5zw2llw.mongodb.net/db-contacts?retryWrites=true&w=majority";
 mongoose
   .connect(DB_HOST)
   .then(() => app.listen(3000))
