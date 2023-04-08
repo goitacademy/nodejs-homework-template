19adcc8d81b081eff8f018d0e6a8e886841f7b13
ViktorZvonar <viktorzvonar@yahoo.com>
2023-04-08 14:59:15 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,7 +5,7 @@ const createError = require("http-errors");
 const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    throw createError(404, `Not valid id`);
+    throw createError(404, `Not found`);
   }
   next();
 };
