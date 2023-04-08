97a5aa3d8677592f61c3f4d19a7ff4e715aa89b8
IrynaKniazieva <gleb0812@gmail.com>
2023-04-09 00:11:42 +0300

    add dotenv

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 // xDVS8o8JCXOUmW6H
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const DB_HOST = "mongodb+srv://ira:xDVS8o8JCXOUmW6H@cluster0.qsijuzb.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+.then(() => app.listen(3000))
+.catch(error => console.log(error.message));
+
+
