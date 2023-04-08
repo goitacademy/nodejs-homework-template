3ba981ed697089313012a1f4e1c1c42ef3f28f62
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-08 22:49:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_CONTACTS, PORT } = process.env;
+const { DB_CONTACTS } = process.env;
 
 mongoose
   .connect(DB_CONTACTS)
   .then(() => {
-    app.listen(PORT);
+    app.listen(3000);
     console.log("Database connection successful");
   })
   .catch((error) => {
