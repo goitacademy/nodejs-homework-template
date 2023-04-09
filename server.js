a2a835bfcf1aa04602e82af0a39878071f255375
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2023-04-09 19:48:57 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+// dkc0mTH2ZtqsVAWK;
+const DB_HOST =
+  "mongodb+srv://Dima:dkc0mTH2ZtqsVAWK@cluster0.5hkfnsu.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => console.log(error.message));
