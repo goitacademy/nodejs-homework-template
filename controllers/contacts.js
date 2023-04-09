188e4304a0f90170471268b99853fe3604d3a6df
VadimKorobec <_s_e_t__@ukr.net>
2023-04-09 19:50:19 +0300

    Create contacts.js

---
+++ controllers/contacts.js
@@ -0,0 +1,55 @@
+const contacts = require("../models/contacts");
+const { dataValidator, IsEmptyObject } = require("../helpers/dataValidator");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const getAllContacts = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  if (dataValidator(req.body).error) {
+    throw HttpError(400, "missing required name field");
+  }
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res) => {
+  if (IsEmptyObject(req.body).error) {
+    throw HttpError(400, "missing fields");
+  }
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getById: ctrlWrapper(getById),
+  addContact: ctrlWrapper(addContact),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
