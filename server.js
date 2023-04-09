4ca040b26f5f25dd98550a9e1856f14c68230a50
Mary Danyliuk <marydanyliuk@gmail.com>
2023-04-09 08:48:42 +0300

    add env-example

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const app = require("./app");
 // const { DB_HOST } = process.env;
 
 const DB_HOST =
+  process.env.DB_HOST ||
   "mongodb+srv://MaryDan:4W3dbq1I6FNwX1DA@cluster0.0qilsj8.mongodb.net/phonebook?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     });
   })
