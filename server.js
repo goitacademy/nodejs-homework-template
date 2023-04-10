760cf33d75fd89358aedb49bf5886781235d03f8
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2023-04-10 19:18:37 +0300

    move DB_HOST to config.js & add config.js to gitignore

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-// dkc0mTH2ZtqsVAWK;
-const DB_HOST =
-  "mongodb+srv://Dima:dkc0mTH2ZtqsVAWK@cluster0.5hkfnsu.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose
   .connect(DB_HOST)
