b07738df0f1f206f4d2ba0ac29888d11a14b0048
VOLODYMYR LYSENKO <107991551+Lusiy321@users.noreply.github.com>
2023-04-11 15:50:12 +0300

    fixed

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,6 +6,8 @@ const {
   removeContact,
   updateContact,
 } = require("../../models/contacts");
+const validation = require("../../models/validation");
+const contactsSchema = require("../../models/schemas");
 
 const router = express.Router();
 
@@ -28,7 +30,7 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", validation(contactsSchema), async (req, res, next) => {
   try {
     const contacts = await addContact(req.body);
     res.status(201).json(contacts);
@@ -47,14 +49,18 @@ router.delete("/:contactId", async (req, res, next) => {
   }
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    await updateContact(contactId, req.body);
-    res.status(200).json({ message: "contact Updatet" });
-  } catch (error) {
-    next(res.status(404).json({ message: "Not found" }));
+router.put(
+  "/:contactId",
+  validation(contactsSchema),
+  async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      await updateContact(contactId, req.body);
+      res.status(200).json({ message: "contact Updatet" });
+    } catch (error) {
+      next(res.status(404).json({ message: "Not found" }));
+    }
   }
-});
+);
 
 module.exports = router;
