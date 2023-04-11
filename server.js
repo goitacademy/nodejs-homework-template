5ad85551445546f7892bf42b18b96707c99eae26
AH400796 <A.Hokhman@gmail.com>
2023-04-11 14:53:31 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const { DB_HOST, PORT } = process.env;
+// const DB_HOST =
+//   "mongodb+srv://AH400796:jpim400796@cluster0.6nmex0x.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
