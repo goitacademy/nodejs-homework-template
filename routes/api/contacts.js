4809e93914726448864be093403b2baed35be9a5
AniaPiWo <snowflake@wp.pl>
2023-04-11 14:12:08 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'henlo world' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
