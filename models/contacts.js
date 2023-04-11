78902a6ff5bd0c8ed9aa5c7f81c6fdbb6366f231
alena <evstafienko_a@ukr.net>
2023-04-11 18:25:33 +0300

    add materials from hw1

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,64 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require("path");
+const {nanoid} = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
+console.log("contactsPath", contactsPath);
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (!contact) {
+      return null;
+    }
+    return contact || null;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const addContact = async (body) => {}
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+    try {
+        const contacts = await listContacts();
+        const newContact = {
+            // yarn add nanoid@3.3.4
+            id:nanoid(),
+            ...name, 
+            ...email, 
+            ...phone,
+        };
+        contacts.push(newContact);
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+        return newContact;
+      } catch (error) {
+        console.log(error);
+      }
+}
 
 const updateContact = async (contactId, body) => {}
 
