9b7f981e0e89f2241edb27c441f3ff2a6bee12ca
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-04-11 23:15:46 +0200

    addmongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import  app from './app.js';
 import mongoose from 'mongoose';
-const DB_HOST="mongodb+srv://Andr:vViEen43jESAiZRm@cluster0.e9eyeso.mongodb.net/contacts-reader?retryWrites=true&w=majority";
+const DB_HOST="mongodb+srv://Andr:vViEen43jESAiZRm@cluster0.e9eyeso.mongodb.net/db-contacts?retryWrites=true&w=majority";
 mongoose.connect(DB_HOST)
 .then(()=>app.listen(8080, () => {
   console.log("Server running. Use our API on port: 3000")
