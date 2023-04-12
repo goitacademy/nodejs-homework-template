92a4746dbe7032eb3c6eeefe6f31a0d356d61829
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-04-13 00:15:23 +0200

    addenvfile

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import  app from './app.js';
 import mongoose from 'mongoose';
+import ('dotenv').config();
+
 const {DB_HOST} = process.env;
 
 mongoose.connect(DB_HOST)
-.then(()=>app.listen(8080, () => {
+.then(()=>app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 }))
 .catch(error=>console.log(error.message));
