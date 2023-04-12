88ee1b973d174859c35b494d5a7d148202f954e6
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-04-13 00:39:57 +0200

    addlocalandserverside

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import  app from './app.js';
 import mongoose from 'mongoose';
-import ('dotenv').config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const {DB_HOST} = process.env;
 
 mongoose.connect(DB_HOST)
-.then(()=>app.listen(3000, () => {
+.then(()=>app.listen(8080, () => {
   console.log("Server running. Use our API on port: 3000")
 }))
 .catch(error=>console.log(error.message));
