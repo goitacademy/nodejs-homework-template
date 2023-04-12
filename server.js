cbf7c00a33a419f3e5ebe44824900c7a54c68bda
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-04-12 23:48:51 +0200

    add config with process.env

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import  app from './app.js';
 import mongoose from 'mongoose';
-const DB_HOST="mongodb+srv://Andr:vViEen43jESAiZRm@cluster0.e9eyeso.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const {DB_HOST} = process.env;
+
 mongoose.connect(DB_HOST)
 .then(()=>app.listen(8080, () => {
   console.log("Server running. Use our API on port: 3000")
 }))
 .catch(error=>console.log(error.message));
-// vViEen43jESAiZRm
 
