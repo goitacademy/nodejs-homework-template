33aef72636baeb1883833142947106b7e0d70bfa
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-04-13 15:08:06 +0300

    add code hw-03

--- controllers/contacts/getContactsById.js
+++ controllers/contacts/getContactsById.js
@@ -1,21 +1,14 @@
 const { NotFound } = require('http-errors');
-
-const contactsModule = require('../../models/contacts');
+const { Contact } = require('../../models/contact');
 
 const getContactById = async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsModule.getContactById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
       throw new NotFound(`Contact with id=${contactId} not found`);
     }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        result,
-      },
-    });
+    res.json(result);
   } catch (error) {
     next(error);
   }
