d031c92bf38dd0a07e5eae416a5a183e0cf87caf
IevgenLychman <gora.erikovich@gmail.com>
2023-04-13 21:42:07 +0300

    4

--- middlwares/authenticate.js
+++ middlwares/authenticate.js
@@ -1,18 +1,28 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
-const newHttpError = require("../helpers");
+
+const newHttpError = require("../helpers/HttpError");
+
+const { User } = require("../models/user");
 
 const authenticate = async (req, res, next) => {
-  const { authorization } = req.body;
+  const { authorization } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== Bearer) {
+  if (bearer !== "Bearer") {
     next(newHttpError(401, "Not authorized"));
   }
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-  } catch (error) {}
+    const user = await User.findById(id);
+    if (!user) {
+      next(newHttpError(402, "Not authorized"));
+    }
+    next();
+  } catch (error) {
+    next(newHttpError(403, "Not authorized"));
+  }
 };
 
 module.exports = authenticate;
--- schemas/registrationSchema.js
+++ schemas/registrationSchema.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 const registrationSchema = Joi.object({
-  name: Joi.string().required(),
   password: Joi.string().required(),
   email: Joi.string().email().required(),
 });
