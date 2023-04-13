3d6bb393262208a0b242d5625ce7e69f422071e5
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-04-13 17:32:02 +0300

    Added validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,15 +4,21 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contact");
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
 router.get("/:contactId", ctrlWrapper(ctrl.getById));
 
-router.post("/", ctrlWrapper(ctrl.add));
+router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
 router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
 
-router.put("/:contactId", ctrlWrapper(ctrl.updateById));
+router.put(
+  "/:contactId",
+  validateBody(schemas.addSchema),
+  ctrlWrapper(ctrl.updateById)
+);
 
 module.exports = router;
