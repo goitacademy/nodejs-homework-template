b4af65724155b67eaf7152dad20b748a161122a3
VadimKorobec <_s_e_t__@ukr.net>
2023-04-14 17:07:16 +0300

    Update addContact.js

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/contact");
+const { Contact } = require("../models/contact");
 
 const addContact = async (req, res) => {
   const result = await Contact.create(req.body);
