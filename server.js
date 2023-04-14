713fdff27b0de86685533732e2ebf992b2bf14be
VadimKorobec <_s_e_t__@ukr.net>
2023-04-14 12:45:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Vadym:4ncFdX2jR3Nh7t7a@cluster0.6cdhoup.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
