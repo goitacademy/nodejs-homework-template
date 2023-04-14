6266478cc15100b91887f9cc0256cc21a6fad6a1
VadimKorobec <_s_e_t__@ukr.net>
2023-04-14 11:23:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Vadym:4ncFdX2jR3Nh7t7a@cluster0.6cdhoup.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+const app = require("./app");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
