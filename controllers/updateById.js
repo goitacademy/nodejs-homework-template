7d9d1c5cd5e3d41b9a554b881693941e0c49097d
VadimKorobec <_s_e_t__@ukr.net>
2023-04-14 17:48:38 +0300

    Update updateById.js

--- controllers/updateById.js
+++ controllers/updateById.js
@@ -2,7 +2,9 @@ const { Contact } = require("../models/contact");
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
 
   res.status(200).json(result);
 };
