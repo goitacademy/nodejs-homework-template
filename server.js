343b84d15d825ebd6c0462d64246b0cb6a913391
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-04-14 06:00:54 +0300

    server file

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000 url");
-});
+const DB_HOST =
+  "mongodb+srv://Yuliya:2d3PDln8OddwGswZ@cluster0.guuu5gj.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000 url");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
