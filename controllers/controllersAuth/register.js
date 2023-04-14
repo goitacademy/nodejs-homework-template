93ad55c252a508334a887b77f397ae9137628568
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-14 16:37:02 +0300

    conrollersAuth

---
+++ controllers/controllersAuth/register.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcryptjs");
+
+const { User } = require("../../models/user");
+
+const { HttpError } = require("../../helpers");
+
+const register = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw HttpError(409, "Email in use");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      ...req.body,
+      password: hashPassword,
+    });
+
+    res.status(201).json({
+      user: { email: newUser.email, subscription: newUser.subscription },
+    });
+  } catch (error) {
+    console.log("error");
+    next(error);
+  }
+};
+
+module.exports = register;
