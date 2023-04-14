bd762a58ce8e0ab76497aafda281662ab6fdf707
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-04-14 20:13:09 +0300

    fixed update method in  validateBody

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,7 +3,11 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema, statusCode, errorMessage) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    const emptyObj = !!Object.keys(req.body);
+    const isEmptyReqBody = Object.keys(req.body).length;
+    if (!isEmptyReqBody) {
+      next(HttpError(400, "missing fields"));
+    }
+    console.log(isEmptyReqBody);
     if (error) {
       next(
         HttpError(
@@ -13,9 +17,6 @@ const validateBody = (schema, statusCode, errorMessage) => {
       );
     }
 
-    if (emptyObj) {
-      next(HttpError(400, "missing fields"));
-    }
     next();
   };
   return func;
