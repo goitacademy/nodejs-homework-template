b9eaa26f21892457c94cbecf2b9969083b01a375
VadimKorobec <_s_e_t__@ukr.net>
2023-04-14 18:19:01 +0300

    Update bodyValidator.js

--- middlewares/bodyValidator.js
+++ middlewares/bodyValidator.js
@@ -2,6 +2,9 @@ const httpError = require("../helpers/httpError");
 
 const bodyValidator = (schema) => {
   const valid = (req, res, next) => {
+    if (!Object.keys(req.body).length) {
+      next(httpError(400, "missing fields"));
+    }
     const { error } = schema.validate(req.body);
     if (error) {
       next(httpError(400, error.message));
