113aa2f6189d18a07b3043e6e821b4c76acf03a2
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-14 16:37:43 +0300

    Delete handleMongooseError.js

--- middleware/handleMongooseError.js
+++
@@ -1,9 +0,0 @@
-// ЧОМУ не працює? в запиті все одно status 500
-
-const handleMongooseError = (error, data, next) => {
-  console.log("1234");
-  error.status = 400;
-  next();
-};
-
-module.exports = { handleMongooseError };
