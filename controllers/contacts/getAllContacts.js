ede2f05f4dafd4be1f1520f413fda14ed4ac43c3
Bogdan <bogdanberezka@gmail.com>
2023-04-14 10:24:23 +0300

    added-filtering -by-favorites

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -5,13 +5,26 @@ const getAllContacts = async (req, res) => {
   //Реалізовуємо пагінацію
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  // const { favorite } = req.query;
-
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "email subscription");
-  res.json(result);
+  //Реалізовуємо фільтрацію контактів по favorite
+  const { favorite } = req.query;
+  if (favorite === undefined) {
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    }).populate("owner", "email subscription");
+    res.json(result);
+  }
+  if (favorite) {
+    const result = await Contact.find(
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    ).populate("owner", "email subscription");
+    res.json(result);
+  }
 };
 
 module.exports = getAllContacts;
