be66c839e4fad8a4f8ccf354fe2239b4a9b78dd5
VadimKorobec <_s_e_t__@ukr.net>
2023-04-14 17:01:13 +0300

    Create index.js

---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+const addBodyValidator = require("./addBodyValidator");
+const bodyValidator = require("./bodyValidator");
+const isValidId = require("./isValidId");
+const updateFavoriteStatus = require("./updateFavoriteStatus");
+
+module.exports = {
+  addBodyValidator,
+  bodyValidator,
+  isValidId,
+  updateFavoriteStatus,
+};
