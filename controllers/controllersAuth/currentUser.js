3bbe89dabd6173cdec35ce413e7f63b997d25fd7
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-14 16:36:20 +0300

    create conrolleresAuth

---
+++ controllers/controllersAuth/currentUser.js
@@ -0,0 +1,14 @@
+const currentUser = async (req, res, next) => {
+  try {
+    const { email, subscription } = req.user;
+
+    res.json({
+      email,
+      subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = currentUser;
