d93b8e6e1dd6db481dc1b2f8ee18ba15d93e103e
VadimKorobec <_s_e_t__@ukr.net>
2023-04-15 15:24:05 +0300

    Delete addContact.js

--- controllers/addContact.js
+++
@@ -1,8 +0,0 @@
-const { Contact } = require("../models/contact");
-
-const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
-  res.status(201).json(result);
-};
-
-module.exports = addContact;
