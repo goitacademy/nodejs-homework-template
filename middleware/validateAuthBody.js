d073e1fad8b7898fcba7ac35e06f1198d6abb1c1
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-15 17:17:36 +0300

    Update validateAuthBody.js

--- middleware/validateAuthBody.js
+++ middleware/validateAuthBody.js
@@ -4,7 +4,7 @@ const validateAuthBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      throw HttpError(error.status, error.message);
+      throw HttpError(400, error.message);
     }
     next();
   };
