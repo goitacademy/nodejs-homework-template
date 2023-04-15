1ada8a4579037c95a6a8ff5411a0d55d1a3d994f
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-04-15 09:39:24 +0300

    add env file

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
-
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Yuliya:2d3PDln8OddwGswZ@cluster0.guuu5gj.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000 url");
-    });
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
