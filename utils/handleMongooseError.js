2828a90ce809a71dd9543bf679fd65c32e1a5afd
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-04-15 03:18:27 +0300

    next(HttpError(400, `Помилка від Joi ...);

--- utils/handleMongooseError.js
+++ utils/handleMongooseError.js
@@ -1,7 +1,15 @@
+
+const { HttpError }= require("../helpers/httpError");
+
 const handleMongooseError = (error, data, next)=> {
-    const {name, code} = error;
-    error.status = (name === "MongoServerError" && code === 11000) ? 409 : 400;
-    next();
+    if (error.name === 'ValidationError') {
+        next(HttpError(400, `Помилка від Joi або іншої бібліотеки валідації`));
+    } else {
+        const {name, code} = error;
+        error.status = (name === "MongoServerError" && code === 11000) ? 409 : 400;
+    }
+    next(error);
+
 };
 
 module.exports = handleMongooseError;
