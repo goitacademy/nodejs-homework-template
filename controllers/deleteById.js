038747ed1dceb923d357b5a4f6645e2649063d84
VadimKorobec <_s_e_t__@ukr.net>
2023-04-15 15:24:01 +0300

    Delete deleteById.js

--- controllers/deleteById.js
+++
@@ -1,9 +0,0 @@
-const { Contact } = require("../models/contact");
-
-const deleteById = async (req, res) => {
-  const { contactId } = req.params;
-  await Contact.findByIdAndRemove(contactId);
-  res.status(200).json({ message: "contact deleted" });
-};
-
-module.exports = deleteById;
