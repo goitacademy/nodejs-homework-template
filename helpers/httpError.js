1c25e2a9a9d1372f685ae5fe54ae60e6f88d4115
Viacheslav Haber <gaberslavik@gmail.com>
2023-04-17 02:12:48 +0300

    finished hw-4

--- helpers/httpError.js
+++ helpers/httpError.js
@@ -1,4 +1,10 @@
-const httpError = (status, message) => {
+const errorMessages = {
+	401: "Not authorized",
+	404: "Not found",
+	409: "Email in use",
+};
+
+const httpError = (status, message = errorMessages[status]) => {
 	const error = new Error(message);
 	error.status = status;
 	return error;
--- models/contactSchema.js
+++ models/contactSchema.js
@@ -18,8 +18,13 @@ const contactSchema = new Schema(
 			type: Boolean,
 			default: false,
 		},
+		owner: {
+			type: Schema.Types.ObjectId,
+			ref: "user",
+			required: true,
+		},
 	},
-	{ versionKey: false },
+	{ versionKey: false, timestamps: true },
 );
 
 const addSchema = Joi.object({
