ba807e252eae5d9afd2c11da52bd5e2faf9494f1
beligrous <beligrous@gmail.com>
2023-04-16 13:03:54 +0300

    fix __v adding

--- models/contact.js
+++ models/contact.js
@@ -1,21 +1,24 @@
 const { Schema, model } = require("mongoose");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: false }
+);
 
 contactSchema.post("save", (error, data, next) => {
   error.status = 400;
