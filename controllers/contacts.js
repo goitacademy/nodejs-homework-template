72c62e5b2fa14e7c5efb4e89ab245cb4afa2007b
VadimKorobec <_s_e_t__@ukr.net>
2023-04-16 22:01:18 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,7 +11,7 @@ const getById = async (req, res) => {
   const { contactId } = req.params;
   const contact = await Contact.findById(contactId);
   if (!contact) {
-    throw httpError(404, "Not found");
+    throw httpError(400, "Not found");
   }
   res.status(200).json(contact);
 };
