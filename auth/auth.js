f5a0aa2c14851c0d815e4e885ff2d95615f972da
dkusnieruk <dariusz.kusnieruk@wp.pl>
2023-04-16 11:39:19 +0200

    update
    log delete

--- auth/auth.js
+++ auth/auth.js
@@ -4,23 +4,23 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const auth = async (req, res, next) => {
   const token = req.headers.authorization;
-  console.log("token", token);
+  
   if (!token) {
     return res.status(401).send("No token provided");
   }
 
   try {
     const decoded = jwt.verify(token, jwtSecret);
-    console.log("decoded", decoded);
+    
     const userFound = await checkUserById(decoded.id);
-    console.log("userFound", userFound.token);
+   
     if (userFound && token == userFound.token) {
       req.user = userFound;
       next();
     } else {
       res.status(401).send("Not authorized");
     }
-  } catch (err) {
+  } catch(err) {
     return res.status(500).send(err);
   }
 };
