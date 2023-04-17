abb085f33a56d19bee405751eca203ed17d8e4fa
TetianaKuzma <tetiana.b97@gmail.com>
2023-04-18 00:23:35 +0300

    Update logout success response status

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -60,7 +60,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.json();
+  res.status(204).json();
 };
 
 module.exports = {
