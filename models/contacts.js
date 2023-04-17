dddc647165f83834f6f20e153bcfe9876c202360
kszczerbowski <kszczerbowski223@gmail.com>
2023-04-17 22:16:02 +0200

    new functions

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,89 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("node:path");
+const nanoid = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const ERROR_CODES = {
+  MISSING_FIELDS: 10,
+  NOT_FOUND: 20,
+  INVALID_PROPERTIES: 30,
+};
 
-const removeContact = async (contactId) => {}
+const ACCEPTED_PROPERTIES = ["name", "email", "phone"];
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const jsonList = JSON.parse(data.toString());
+  return jsonList;
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const jsonList = await listContacts();
+  if (jsonList.find((contact) => contact.id === contactId) === undefined) {
+    throw `Sorry, there is no contact with contact ID ${contactId}!`;
+  }
+  const searchedContact = jsonList.find((contact) => contact.id === contactId);
+  return searchedContact;
+};
+
+const removeContact = async (contactId) => {
+  const jsonList = await listContacts();
+  if (jsonList.find((contact) => contact.id === contactId) === undefined) {
+    throw `Sorry, there is no contact with contact ID ${contactId}!`;
+  }
+  const newList = jsonList.filter((contact) => contact.id !== contactId);
+  fs.writeFile(contactsPath, JSON.stringify(newList));
+};
+
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const jsonList = await listContacts();
+  const newContact = { id: nanoid.nanoid(), name, email, phone };
+  const newList = [...jsonList, newContact];
+  fs.writeFile(contactsPath, JSON.stringify(newList));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  const propertiesObject = { name, email, phone };
+
+  if (
+    Object.keys(propertiesObject).every(
+      (key) => propertiesObject[key] === undefined
+    )
+  )
+    throw { message: "Missing fields!", code: ERROR_CODES.MISSING_FIELDS };
+
+  const invalidProperties = Object.keys(body).filter(
+    (key) =>
+      ACCEPTED_PROPERTIES.find((element) => element === key) === undefined
+  );
+  if (invalidProperties.length > 0)
+    throw {
+      message: `Invalid properties passed: ${invalidProperties.join(", ")}!`,
+      code: ERROR_CODES.INVALID_PROPERTIES,
+    };
+
+  const jsonList = await listContacts();
+  if (jsonList.find((contact) => contact.id === contactId) === undefined)
+    throw {
+      message: `Can't find the searched contact!`,
+      code: ERROR_CODES.NOT_FOUND,
+    };
+
+  const contactToUpdate = jsonList.find((contact) => contact.id === contactId);
+  Object.keys(body).forEach((key) => (contactToUpdate[key] = body[key]));
+  fs.writeFile(contactsPath, JSON.stringify(jsonList));
+  return contactToUpdate;
+};
 
 module.exports = {
+  ERROR_CODES,
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,102 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts.js");
 
-const router = express.Router()
+const router = express.Router();
+const Joi = require("joi");
+const _ = require("lodash");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const newContactSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().required(),
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const updatedContactSchema = Joi.object({
+  name: Joi.string(),
+  phone: Joi.string(),
+  email: Joi.string(),
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res) => {
+  contacts
+    .listContacts()
+    .then((jsonList) => res.status(200).json(jsonList))
+    .catch((error) => res.status(418).json({ error }));
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  contacts
+    .getContactById(id)
+    .then((searchedContact) => res.status(200).json(searchedContact))
+    .catch((error) => res.status(418).json(error));
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res) => {
+  const { error, contact } = newContactSchema.validate(req.body);
+  if (_.isEmpty(req.body)) {
+    res.status(400).json({ message: "missing request body" });
+    return;
+  }
+  if (error !== undefined) {
+    res.status(400).json({ message: "missing properties" });
+    return;
+  }
+  const { name, email, phone } = contact;
+  const propertiesObject = { name, email, phone };
+  const missingProperties = Object.keys(propertiesObject).filter(
+    (key) => propertiesObject[key] === undefined
+  );
+  if (missingProperties.length > 0) {
+    res.status(400).json({
+      message: `missing required properties: ${missingProperties.join(", ")}`,
+    });
+    return;
+  }
+  contacts
+    .addContact(contact)
+    .then((newContact) => res.status(201).json({ newContact }));
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  contacts
+    .removeContact(id)
+    .then(() =>
+      res.status(200).json({ message: `contact with id ${id} deleted` })
+    )
+    .catch((error) => res.status(404).json(error));
+});
+
+router.put("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  if (_.isEmpty(req.body)) {
+    res.status(400).json({ message: "missing request body" });
+    return;
+  }
+  const { error, contact } = updatedContactSchema.validate(req.body);
+  if (error !== undefined) {
+    res.status(400).json({ message: "missing properties" });
+    return;
+  }
+  contacts
+    .updateContact(id, contact)
+    .then((updatedContact) => {
+      res.status(200).json({ updatedContact });
+    })
+    .catch((error) => {
+      switch (error.code) {
+        case contacts.ERROR_CODES.NOT_FOUND:
+          res.status(404).json(error.message);
+          break;
+        case contacts.ERROR_CODES.MISSING_FIELDS:
+        case contacts.ERROR_CODES.INVALID_PROPERTIES:
+          res.status(400).json(error.message);
+          break;
+        default:
+          res.status(400).json(error);
+      }
+    });
+});
+
+module.exports = router;
