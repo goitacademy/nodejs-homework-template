aeedff6b494743ed74fc00b37ac3c14088eaa6da
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-04-18 20:38:42 +0300

    up middlewares

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -6,7 +6,7 @@ const isValidId = (req,res,next) =>{
     const {contactId} = req.params;
 
     if(!isValidObjectId(contactId)){
-        next(HttpError(400, `${id} is not valid id`))
+        next(HttpError(400, `${contactId} is not valid id`))
     }
     next();
 }
