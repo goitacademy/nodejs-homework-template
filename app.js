3d8c2c9c366c38ee44af5485ae0da265b48e7fa5
SvitlanaChudak <svit.lemon@gmail.com>
2023-04-18 21:05:06 +0300

    done

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
-
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
