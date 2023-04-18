d072b5bab712d3c6ac89029ecd677bc1a49ed8a6
Kyrylo Savchenko <savchenko.kyrylo@gmail.com>
2023-04-18 23:57:29 +0300

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,51 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../..//models/contacts');
+
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.status(200).json(listContacts());
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    res.status(200).json(await getContactById(req.params.contactId));
+  } catch (err) {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  console.log(req.query);
+  try {
+    res.status(201).json(await addContact(req.body));
+  } catch (err) {
+    res.status(400).json({ message: 'missing required name field' });
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const result = await removeContact(req.params.contactId);
+    result ?? res.status(200).json({ message: 'contact deleted' });
+  } catch (err) {
+    res.status(404).json({ message: 'missing required name field' });
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    res.status(200).json(await updateContact(req.params.contactId, req.body));
+  } catch (err) {
+    res.status(err.status).json(err.message);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require('./app')
+const app = require('./app');
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log(
+    'Server running. Use our API link http://localhost:3000/api/contacts/ on port: 3000'
+  );
+});
