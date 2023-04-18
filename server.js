1434c964987fbb0821cee01f24c6d080901f6f2b
Anna <michalowska.aneczka@gmail.com>
2023-04-18 19:18:06 +0200

    testowanie_nie_działa
    Np. przy GET i http://localhost:3000/api/contacts/
    otrzymuję:
    {
        "status": "Database connection successful",
        "code": 200,
        "data": {
            "contacts": []
        }
    }
    
    po dodaniu nowego kontaktu i ponownej próbie wyświetlenia listy kontaktów otrzymuję:
    {
        "status": "Database connection successful",
        "code": 200,
        "data": {
            "contacts": [
                {
                    "_id": "643eca786747e3f8a412a339",
                    "createdAt": "2023-04-18T16:51:04.166Z",
                    "updatedAt": "2023-04-18T16:51:04.166Z"
                }
            ]
        }
    }

--- server.js
+++ server.js
@@ -1,5 +1,52 @@
-const app = require('./app')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const routerApi = require("./routes/api/contacts");
+app.use("/routes/api/contacts", routerApi);
+
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Use api on routes: /api/contacts",
+    data: "Not found",
+  });
+});
+
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+const uriDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection error:", error);
+    process.exit(1);
+  });
---
+++ service/index.js
@@ -0,0 +1,35 @@
+const contactList = require("./schema/contact");
+
+const getAllContacts = async () => {
+  return contactList.find();
+};
+
+const getContactById = (contactId) => {
+  return contactList.findOne({ _id: contactId });
+};
+
+const addContact = ({ name, email, phone }) => {
+  return contactList.create({ name, email, phone });
+};
+
+const updateContact = (contactId, { name, email, phone }) => {
+  return contactList.findByIdAndUpdate(
+    { _id: contactId },
+    { new: name, email, phone }
+  );
+};
+
+const removeContact = (contactId) => {
+  return contactList.findByIdAndRemove({ _id: contactId });
+};
+
+//const favorite...
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+  //favorite
+};
