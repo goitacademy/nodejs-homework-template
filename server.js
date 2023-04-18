891eb9da1400c9f3b5a6f945481c24948425db65
Anna Sobova <anna_sobova@ukr.net>
2023-04-18 15:59:36 +0300

    with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+
+const mongoose = require("mongoose");
 const app = require('./app')
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT , () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+
+
+const DB_HOST = "mongodb+srv://Anna1:KcEqXC5zx2tzbF3T@cluster0.mippplm.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+.then(()=>app.listen(3000))
+.catch(error => console.log(error.message));
+
+// require("dotenv").config();
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT , () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
