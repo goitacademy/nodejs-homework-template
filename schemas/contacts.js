d76f7833f4cb9d34fc62395e026d86e80cbdd431
yarqui <108986486+yarqui@users.noreply.github.com>
2023-04-18 14:07:53 +0300

    ➕ add updateScheme validation

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,10 +1,26 @@
 const Joi = require("joi");
 
 // Validates sent data for POST & PUT methods
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
+const addContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+    })
+    .required(),
   phone: Joi.string().required(),
+  /**no phone validation just yet ^\(\d{3}\) \d{3}-\d{4}$
+   */
 });
 
-module.exports = { addSchema };
+const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+  }),
+  phone: Joi.string(),
+  /**no phone validation just yet ^\(\d{3}\) \d{3}-\d{4}$
+   */
+});
+
+module.exports = { addContactSchema, updateContactSchema };
