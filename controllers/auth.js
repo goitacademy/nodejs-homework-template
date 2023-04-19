a05f401bf01c0ddb3a91c2506e0a18b0cc5abfe3
Anna Bramarova <annabramarova@gmail.com>
2023-04-20 01:31:09 +0300

    fix: user empty registration object

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,7 @@ const registerUser = async (req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = User.create({ ...req.body, password: hashPassword });
+    const newUser = await User.create({ ...req.body, password: hashPassword });
     res.status(201).json({
         user: { email: newUser.email, subscription: newUser.subscription }
     });
