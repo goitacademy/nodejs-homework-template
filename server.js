7d1e7f83b4f26fb62cf18b045faa83cb04e22e9f
VOLODYMYR LYSENKO <107991551+Lusiy321@users.noreply.github.com>
2023-04-19 11:39:49 +0300

    start

--- server.js
+++ server.js
@@ -1,5 +1,58 @@
-const app = require('./app')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = 3000;
+const DB_HOST = `mongodb+srv://admin:240188Lusiy@cluster0.gwfkspe.mongodb.net/test`;
+
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.get("/", async (req, res) => {
+  console.log(req.body);
+  res.status(200).json("Server started");
+});
+
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Use api on routes: /api/tasks",
+    data: "Not found",
+  });
+});
+
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+
+async function startApp() {
+  try {
+    mongoose.connect(DB_HOST, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`Server running. Use our API on http://localhost:${PORT}`)
+    );
+  } catch (e) {
+    console.log(`Server not running. Error message: ${e}`);
+  }
+}
+startApp();
