c3917d8c9119e371eb6eb6a03440bc67e8b9206a
YuriyRysak <yuriy.risak@gmail.com>
2023-04-20 18:03:30 +0300

    add files schemas end middlewares

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,8 @@
-const Joi = require('joi');
-
 const contacts = require("../models/index");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-const addSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required()
-  });
+
 
     const getAll =  async (req, res) => {        
         const result = await contacts.listContacts();
@@ -27,28 +21,18 @@ const addSchema = Joi.object({
         
     };
   
-    const postContactById =  async (req, res) => {
-        const {error} = addSchema.validate(req.body);
-        if(error) {
-            throw HttpError(400, "missing required name field");
-        }
-        const result = await contacts.addContact(req.body);
-        res.status(201).json(result);
-                
-    };
+    const postContactById =  async (req, res) => {       
+          const result = await contacts.addContact(req.body);
+          res.status(201).json(result);                
+        };
 
-    const putContactById = async (req, res) => {
-          const {error} = addSchema.validate(req.body);
-          if(error) {
-            throw HttpError(400, "missing fields");
-          }
+    const putContactById = async (req, res) => {          
           const {contactId} = req.params;
           const result = await contacts.updateContact(contactId, req.body);
           if(!result) {
             throw HttpError(404, "Not found");
           }
-          res.json(result);
-              
+          res.json(result);              
       };
 
     const deleteContactById = async (req, res) => {
