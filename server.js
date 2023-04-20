5190a51a878a320eced0ff7a01c0ec1bdaa949e5
Kyrylo Savchenko <savchenko.kyrylo@gmail.com>
2023-04-20 10:09:18 +0300

    DB connected

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const app = require('./app');
 
-app.listen(3000, () => {
-  console.log(
-    'Server running. Use our API link http://localhost:3000/api/contacts/ on port: 3000'
-  );
-});
+const mongoose = require('mongoose');
+const DB_HOST =
+  'mongodb+srv://Kirill:vfe1z5zYITk7OFrh@cluster0.ny3rsag.mongodb.net/contacts_reader?retryWrites=true&w=majority';
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(3000, () => {
+      console.log(
+        'Server running. Use our API link http://localhost:3000/api/contacts/ on port: 3000'
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
