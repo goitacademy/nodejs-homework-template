9d68251e66e2b95ae83a97deed190872f21ab0f8
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-20 15:05:32 +0300

    update conrollers register and create controllers updateAvatar

--- controllers/controllersAuth/index.js
+++ controllers/controllersAuth/index.js
@@ -3,5 +3,13 @@ const login = require("./login");
 const logout = require("./logout");
 const currentUser = require("./currentUser");
 const updateSubscription = require("./updateSubscription");
+const updateAvatar = require("./updateAvatar");
 
-module.exports = { register, login, logout, currentUser, updateSubscription };
+module.exports = {
+  register,
+  login,
+  logout,
+  currentUser,
+  updateSubscription,
+  updateAvatar,
+};
--- controllers/controllersAuth/register.js
+++ controllers/controllersAuth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
@@ -13,15 +14,18 @@ const register = async (req, res, next) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
     const newUser = await User.create({
       ...req.body,
       password: hashPassword,
+      avatarURL,
     });
 
     res.status(201).json({
       user: { email: newUser.email, subscription: newUser.subscription },
     });
   } catch (error) {
+    console.log(...req.body);
     console.log("error");
     next(error);
   }
