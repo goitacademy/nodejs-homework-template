760d5ca9c42ffd39532c38eeb72d38e839e7ece7
VadimKorobec <_s_e_t__@ukr.net>
2023-04-21 14:17:08 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
 const { User } = require("../models/user");
 const { httpError, ctrlWrapper } = require("../helpers");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { SECRET_KEY } = process.evn;
 
 const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -7,14 +12,31 @@ const registerUser = async (req, res) => {
   if (user) {
     throw httpError(409, "Email in use");
   }
-  const newUser = await User.create(req.body);
-
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
     email: newUser.email,
-    password: newUser.password,
+    subscription: newUser.subscription,
   });
+};
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw httpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw httpError(401, "Email or password is wrong");
+  }
+
+  const payload = { id: user._id };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 };
 
 module.exports = {
   registerUser: ctrlWrapper(registerUser),
+  userLogin: ctrlWrapper(userLogin),
 };
