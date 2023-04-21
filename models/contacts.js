9d68d2e91fa1f3cccbbc567f0399381b268e3deb
Daniel Lisitsyn <danielfox1521878@gmail.com>
2023-04-21 21:39:12 +0300

    hw02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,62 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+async function updateContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
 
-const addContact = async (body) => {}
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
 
-const updateContact = async (contactId, body) => {}
+async function updateContactById(id, { name, email, phone }) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, name, email, phone };
+  await updateContacts(contacts);
+  return contacts[index];
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+  updateContactById,
+};
---
+++ routes/api/contacts-routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const schemas = require("../../schemas/contacts");
+
+const controllersContacts = require("../../controllers/contacts-controllers");
+const validateBody = require("../../utils/validateBody");
+
+const router = express.Router();
+
+router.get("/", controllersContacts.listContacts);
+
+router.get("/:contactId", controllersContacts.getContactById);
+
+router.post(
+  "/",
+  validateBody(schemas.contactAddSchema),
+  controllersContacts.addContact
+);
+
+router.put("/:contactId", controllersContacts.updateContactById);
+
+router.delete("/:contactId", controllersContacts.removeContact);
+
+module.exports = router;
