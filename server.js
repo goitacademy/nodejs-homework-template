0b33314af9afea72fe5cf721a80cbc1ed93da54c
YuriyRysak <yuriy.risak@gmail.com>
2023-04-21 14:33:12 +0300

    add mongoose+conect db
    proces.exit(1)

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Yuriy:aI0URzTX0IR7vfIJ@cluster0.s22pvdl.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
