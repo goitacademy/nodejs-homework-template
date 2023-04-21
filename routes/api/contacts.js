ff0a9bf8ebc9c55fd09e15cd579fad618d69d272
NovikoffOleh <novikoff10000@gmail.com>
2023-04-21 20:30:13 +0300

    upd files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,109 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
+
+const{
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+
+const {HttpError} = require("../../helpers");
+
+const addPosts = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `"name" is required`,
+    "string.empty": `"name" cannot be empty`,
+    "string.base": `"name" must be string`,
+  }),
+
+  email: Joi.string().required().messages({
+    "any.required": `"email" is required`,
+    "string.empty": `"email" cannot be empty`,
+    "string.base": `"email" must be string`,
+  }),
+  phone: Joi.number().required().messages({
+    "any.required": `"phone" is required`,
+  }),
+
+})
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'hello Kitty' })
-})
+  try {
+    const result = await listContacts()
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+  
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const {contactId} = req.params;
+    const result = await getContactById(contactId)
+    if (!result) {
+      throw HttpError (404, `Контакт с ${contactId}не найден`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+  
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addPosts.validate(req.body);
+    if (error) {
+      throw HttpError (400, error.message);
+    }
+    const result = await addContact(req.body);
+
+    res.status (201).json(result)
+    } 
+    catch (error) {
+    next(error);
+  }
+  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params;
+    const result = await removeContact(contactId);
+    if (!result) {
+      throw HttpError (404, `Контакт с ${contactId}не найден`);
+    }
+
+    res.json({
+      message: "Видалено повністю"
+    })
+  } catch (error) {
+    next(error);
+  }
+  
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addPosts.validate(req.body);
+    
+    if (error) {
+      throw HttpError (400, error.message);
+    }
+    const {contactId} = req.params;
+    const result = await updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError (404, 'Контакт не знайдено');
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 })
 
 module.exports = router
