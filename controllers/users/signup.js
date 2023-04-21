8bed015fdd1a1afa7eec5ef23bd5f26fd3e0319f
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-04-21 21:32:29 +0300

    hw05

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,24 +1,31 @@
 const bcrypt = require('bcrypt');
 const { User } = require('../../models');
 const { Conflict } = require('http-errors');
+const gravatar = require('gravatar');
 
-const signup = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-      throw new Conflict(409, 'Email in use');
-    }
-
-    const newUser = new User({
-      email,
-      password: await bcrypt.hash(password, 10),
-    });
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
+const signup = async (req, res) => {
+  const { password, email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict('Email in use');
   }
+  const avatarURL = gravatar.url(email, {
+    protocol: 'http',
+    s: '250',
+  });
+
+  const newUser = new User({
+    email,
+    password: await bcrypt.hash(password, 10),
+    avatarURL,
+  });
+  await newUser.save();
+  res.json({
+    user: {
+      email: email,
+      avatarURL: avatarURL,
+    },
+  });
 };
 
 module.exports = signup;
