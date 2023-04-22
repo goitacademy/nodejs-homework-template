41c1520966b7aedbc50a7fe560c8ce59f3f7e216
flappykled <dima04089804@gmail.com>
2023-04-22 22:04:39 +0300

    homework3

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,66 @@
-// const fs = require('fs/promises')
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const listContacts = async () => {}
+const phoneRegexp = /^[\(]\d{3}[\)]\s\d{3}[\-]\d{4}$/;
 
-const getContactById = async (contactId) => {}
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+    required: [true, "Set email for contact"],
+  },
+  phone: {
+    type: String,
+    match: phoneRegexp,
+    required: [true, "Set phone for contact"],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const removeContact = async (contactId) => {}
+contactSchema.post("save", (error, data, next) => {
+  error.status = 400;
+  next();
+});
 
-const addContact = async (body) => {}
+const addSchema = Joi.object({
+  name: Joi.string()
+    .messages({
+      "any.required": "missing required name field",
+    })
+    .required(),
+  email: Joi.string()
+    .messages({
+      "any.required": "missing required email field",
+    })
+    .required(),
+  phone: Joi.string()
+    .pattern(phoneRegexp)
+    .messages({
+      "any.required": "missing required phone field",
+    })
+    .required(),
+  favorite: Joi.boolean(),
+});
 
-const updateContact = async (contactId, body) => {}
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean()
+    .messages({
+      "any.required": "missing field favorite",
+    })
+    .required(),
+});
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, schemas };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,30 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const ctrl = require("../../controllers/contacts");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.getAll);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", isValidId, ctrl.deleteById);
 
-module.exports = router
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
+
+module.exports = router;
