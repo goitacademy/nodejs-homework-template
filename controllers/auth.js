b894dfa18f8d50c32803e75d4e7b9ffafcdc705e
Anna Bramarova <annabramarova@gmail.com>
2023-04-22 04:23:14 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ const registerUser = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+    const newUser = User.create({ ...req.body, password: hashPassword, avatarURL });
     res.status(201).json({
         user: { email: newUser.email, subscription: newUser.subscription }
     });
