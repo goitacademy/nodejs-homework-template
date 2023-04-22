db8cfdfd24fb702ce6ca1ede3e5be0a43dec1a17
TarasTikan <tikantaras1@gmail.com>
2023-04-22 20:57:18 +0300

    add ada

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,45 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+const filePath = path.join(__dirname, "contacts.json");
+const contacts = async (contact) =>
+  fs.writeFile(filePath, JSON.stringify(contact, null, 2));
+async function listContacts() {
+  const list = await fs.readFile(filePath);
+  return JSON.parse(list);
+}
 
-const listContacts = async () => {}
+async function getContactById(contactId) {
+  const list = await listContacts();
+  const oneContact = list.find((item) => item.id === contactId);
+  return oneContact || null;
+}
 
-const getContactById = async (contactId) => {}
+async function removeContact(contactId) {
+  const list = await listContacts();
+  const findIndex = list.findIndex((item) => item.id === contactId);
+  if (findIndex === -1) return null;
+  const [result] = list.splice(findIndex, 1);
+  await contacts(list);
+  return result;
+}
 
-const removeContact = async (contactId) => {}
-
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
+async function addContact({ name, email, phone }) {
+  const list = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  list.push(newContact);
+  await contacts(list);
+  return newContact;
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,29 @@
 const express = require('express')
-
+const contacts = require('../../models/contacts')
 const router = express.Router()
-
+const HttpError = require('../../helpers/HttpError')
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'home message' })
+ try {
+   const result = await contacts.listContacts();
+   res.json(result);
+ } catch (error) {
+     const { status = 500, message = "Server error" } = error;
+     res.status(status).json({ message });
+ }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: "home message" });
+  try {
+    const { contactId } = req.params;
+     const result = await contacts.getContactById(contactId);
+     if(!result) {
+      throw HttpError(404, 'Not Found')
+     }
+     res.json(result)
+  } catch (error) {
+    const {status = 500, message= "Server error" } = error
+    res.status(status).json({ message});
+  }
 })
 
 router.post('/', async (req, res, next) => {
