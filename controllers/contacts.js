181e424f6427de5036ba64c54eb2768119774ddf
Sergey <sergey.cherenkov96@gmail.com>
2023-04-23 00:19:26 +0300

    done

---
+++ controllers/contacts.js
@@ -0,0 +1,75 @@
+const { HttpError } = require("../helpers/HttpError");
+const contacts = require("../models/contacts");
+const Joi = require("joi");
+const { ctrlWrapper } = require("../helpers/index");
+
+const contactsSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+const getContacts = async (req, res, next) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const { error } = contactsSchema.validate(req.body);
+
+  if (error) {
+    throw HttpError(400, "missing required name field");
+  }
+
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const deleteContacts = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json({ message: "contact deleted" });
+  // res.status(204).send();
+};
+
+const changeContact = async (req, res) => {
+  const { error } = contactsSchema.validate(req.body);
+
+  if (error) {
+    throw HttpError(400, "missing fields");
+  }
+
+  const { contactId } = req.params;
+
+  const result = await contacts.updateContact(contactId, req.body);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+module.exports = {
+  getContacts: ctrlWrapper(getContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  deleteContacts: ctrlWrapper(deleteContacts),
+  changeContact: ctrlWrapper(changeContact),
+};
---
+++ helpers/ctrlWrapper.js
@@ -0,0 +1,13 @@
+const ctrlWrapper = (ctrl) => {
+  const wrapperFunc = async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  return wrapperFunc;
+};
+
+module.exports = ctrlWrapper;
