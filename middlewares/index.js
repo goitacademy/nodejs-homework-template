5861e3a288666d8a1ac32fe7cc64f4e1d85c7966
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-04-23 21:07:53 +0300

    23.04.2023

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const validateBodyPost = require("./validateBodyPost");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
 
 module.exports = {
   validateBody,
   validateBodyPost,
   isValidId,
+  authenticate,
 };
