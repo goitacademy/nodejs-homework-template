94ae30a64b9eb9ac64af6b06063bd81c8b7908e5
VadimKorobec <_s_e_t__@ukr.net>
2023-04-23 11:37:49 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,32 @@
 const router = require("express").Router();
-const { bodyValidator } = require("../../middlewares");
+const {
+  bodyValidator,
+  authenticate,
+  updateFavoriteStatus,
+} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
-const { registerUser, userLogin } = require("../../controllers/auth");
+const {
+  registerUser,
+  userLogin,
+  getCurrent,
+  logoutUser,
+  updateSubscription,
+} = require("../../controllers/auth");
 
 router.post("/register", bodyValidator(schemas.registerSchema), registerUser);
 
 router.post("/login", bodyValidator(schemas.loginSchema), userLogin);
+
+router.get("/current", authenticate, getCurrent);
+
+router.post("/logout", authenticate, logoutUser);
+
+router.patch(
+  "/",
+  authenticate,
+  updateFavoriteStatus(schemas.updateSubscriptionSchema, "subscription"),
+  updateSubscription
+);
 
 module.exports = router;
