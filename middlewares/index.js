fbb65657382b70d5cea9c12cdd97b0c9ae214bc8
VadimKorobec <_s_e_t__@ukr.net>
2023-04-23 11:37:56 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const addBodyValidator = require("./addBodyValidator");
 const bodyValidator = require("./bodyValidator");
 const isValidId = require("./isValidId");
 const updateFavoriteStatus = require("./updateFavoriteStatus");
+const authenticate = require("./authenticate");
 
 module.exports = {
   addBodyValidator,
   bodyValidator,
   isValidId,
   updateFavoriteStatus,
+  authenticate,
 };
