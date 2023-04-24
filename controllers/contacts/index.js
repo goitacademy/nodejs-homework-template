aca8a2635a06dc0e50d7ec778eb3dd0115bb147f
YuriyRysak <yuriy.risak@gmail.com>
2023-04-24 15:20:49 +0300

    add controllers contacts

---
+++ controllers/contacts/index.js
@@ -0,0 +1,14 @@
+const getAllContacts = require("./getAllContacts");
+
+module.exports = {
+  getAllContacts,
+};
+
+// // controllers/contacts/index.js
+
+// const getAllContacts = require('./getAllContacts');
+
+// module.exports = {
+//   getAllContacts,
+//   // Додайте сюди інші функції контролера з папки "contacts", якщо є
+// };
