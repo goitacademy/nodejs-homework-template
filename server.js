0914a7a39ad393b894fe99bd3bac46ff355398fb
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-04-24 21:54:21 +0300

    move DB_HOST to config.js

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Yuliya:1ahWEQTo1u3MxMS1@cluster0.zagjwqp.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(app.listen(3000))
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection success");
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
