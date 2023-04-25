bd90af217bc9d67060fc6d8d78e08ebfe6332b0c
Your Name <wtm@hotmail.com.au>
2023-04-25 12:08:31 +0300

    added middlewares/helpers/utils+app changed

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose")
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://nfedoriv:KIPX3CCtzHGanx5u@cluster0.wqtq2lb.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.connect(DB_HOST)
-.then(()=> app.listen(3000))
+.then(()=> app.listen(PORT))
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   })
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
---
+++ utils/validateBody.js
@@ -0,0 +1,40 @@
+const { HttpError } = require("../helpers");
+
+const validateAddContact = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      next(HttpError(400, error.message));
+    }
+    next();
+  };
+  return func;
+};
+
+const validateUpdateContact = (schema) => {
+  const func = (req, res, next) => {
+    // const { error } = schema.validate(req.body);
+    if (JSON.stringify(req.body) === "{}") {
+      throw HttpError(400, "Missing fields");
+    }
+    next();
+  };
+  return func;
+};
+const validateUpdateFavoriteContact = (schema) => {
+  const func = (req, res, next) => {
+    // const { error } = schema.validate(req.body);
+    if (JSON.stringify(req.body) === "{}") {
+      throw HttpError(400, "missing field favorite");
+    }
+    next();
+  };
+  return func;
+};
+
+module.exports = {
+  validateAddContact,
+  validateUpdateContact,
+  validateUpdateFavoriteContact,
+};
