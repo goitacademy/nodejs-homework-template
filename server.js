decf4f02b6be98e943fd52510d58e2ce4afa8148
Your Name <wtm@hotmail.com.au>
2023-04-25 10:11:28 +0300

    initial

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose")
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = "mongodb+srv://nfedoriv:KIPX3CCtzHGanx5u@cluster0.wqtq2lb.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+.then(()=> app.listen(3000))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
