7bc43951b6355dd55ea174076d1259af567c3442
Sergey <sergey.cherenkov96@gmail.com>
2023-04-26 21:48:04 +0300

    fix

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(3000, "Database connection successful"))
+  .then(() =>
+    app.listen(3000, () => console.log("Database connection successful"))
+  )
   .catch((error) => {
     console.log(error);
     process.exit(1);
