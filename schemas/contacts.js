6b753fb510ced0bc0abcfcd252577c00fcb9b4af
DenFedor <fedorenko.wo@gmail.com>
2023-04-27 14:48:29 +0300

    Added name,email validation patterns
    Name : a-z,A-Z;
    Email: should include @ and .

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,8 +1,8 @@
 const Joi = require('joi')
 
 const contactsSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
+  name: Joi.string().pattern(/[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/).required(),
+  email: Joi.string().pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/).required(),
   phone: Joi.string().required(),
 })
 
