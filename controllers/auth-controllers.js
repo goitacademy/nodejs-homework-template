53e5d08d64ffd6d6b1d2552860625041d1bf3c65
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2023-04-27 23:36:03 +0300

    add jimp

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
 
 const { HttpError } = require("../helpers");
 
@@ -95,6 +96,15 @@ const updateAvatar = async (req, res) => {
   const avatarName = `${_id}_${filename}`;
   const resultUpload = path.join(avatarsDir, avatarName);
   await fs.rename(tempupload, resultUpload);
+
+  await Jimp.read(resultUpload)
+    .then((image) => {
+      return image.cover(250, 250).write(resultUpload);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
   const avatarURL = path.join("avatars", avatarName);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
