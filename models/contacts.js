3ba2372b960850c4ab085641628e8721b2a953e9
Wiola Janczura <wiola.janczura@wp.pl>
2023-04-28 12:16:19 +0000

    update

--- models/contacts.js
+++ models/contacts.js
@@ -1,45 +1,50 @@
-const crypto = require('crypto')
-const DB = require('./db.js')
-const db = new DB('./contacts.json')
-
+const crypto = require("crypto");
+const DB = require("./db.js");
+const db = new DB("./contacts.json");
 
 const listContacts = async () => {
-  return await db.read()
-}
+  return await db.read();
+};
 
 const getContactById = async (contactId) => {
-  const contacts = await db.read()
-  const [contact] = contacts.filter((contact) => contact.id === contactId)
-  return contact
-}
+  const contacts = await db.read();
+  const [contact] = contacts.filter((contact) => contact.id === contactId);
+  return contact;
+};
 
 const removeContact = async (contactId) => {
-  const contacts = await db.read()
-  const index = contacts.findIndex((contact) => contact.id === contactId)
-  if(index !== -1) {
-    const [result] = contacts.splice(index, 1)
-    await db.write(contacts)
-    return result
-} return null}
-
-const addContact = async (body) => {  const contacts = await db.read()
-  const newContact = {
-  id: crypto.randomUUID(),
-  ...body
-}
-contacts.push(newContact)
-await db.write(contacts)
-return newContact}
-
-const updateContact = async (contactId, body) => {   const contacts = await db.read()
-  const index = contacts.findIndex((contact) => contact.id === contactId)
-  if(index !== -1) {
-      const contact = contacts[index]
-      contacts[index] = {...contact, ...body}
-      await db.write(contacts)
-      return contacts[index]
+  const contacts = await db.read();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const [result] = contacts.splice(index, 1);
+    await db.write(contacts);
+    return result;
   }
-  return null}
+  return null;
+};
+
+const addContact = async (body) => {
+  const contacts = await db.read();
+  const newContact = {
+    id: crypto.randomUUID(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await db.write(contacts);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await db.read();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const contact = contacts[index];
+    contacts[index] = { ...contact, ...body };
+    await db.write(contacts);
+    return contacts[index];
+  }
+  return null;
+};
 
 module.exports = {
   listContacts,
@@ -47,4 +52,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
