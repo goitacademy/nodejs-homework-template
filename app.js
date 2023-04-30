c42836d4498f77c7e7fdf80d328cad614693c4a0
dzaharo <desheviyhasl@gmail.com>
2023-04-30 18:14:00 +0300

    hw02

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const logger = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
@@ -8,18 +8,18 @@ const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
+app.use(morgan(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use((requirement, response) => {
+  response.status(404).json({ message: 'Not found contact' })
 })
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+app.use((error, requirement, response, next) => {
+  response.status(500).json({ message: error.message })
 })
 
 module.exports = app
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,118 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts.js");
+const { RequestError } = require('../../helpers');
+const {
+  bodySchema
+} = require('../../schemas/contacts');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (requirement, response, next) => {
+  try {
+    const contacts = await listContacts();
+    response.json(contacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (requirement, response, next) => {
+  try {
+    const contactId = requirement.params.contactId;
+    const contactById = await getContactById(contactId);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!contactById) {
+      throw RequestError(404, "Not found contact");
+    }
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    response.json(contactById);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.post("/", async (requirement, response, next) => {
+  try {
+    const validationResult = bodySchema.validate(requirement.body)
+    const body = requirement.body;
+
+    if (validationResult.error) {
+      throw RequestError(404, "missing required name field");
+    }
+
+    const newContact = await addContact(body);
+
+    response.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (requirement, response, next) => {
+  try {
+    const contactId = requirement.params.contactId;
+    const findContactById = await removeContact(contactId);
+
+    if (!findContactById) {
+      throw RequestError(404, "Not found contact");
+    }
+
+    response.json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (requirement, response, next) => {
+  try {
+    const contactId = requirement.params.contactId;
+    const body = requirement.body;
+
+    const validationResult = bodySchema.validate(body);
+
+    if (validationResult.error) {
+      throw RequestError(400, validationResult.error.details[0].message);
+    }
+
+    const currentContact = await getContactById(contactId);
+
+    if (!currentContact) {
+      throw RequestError(404, "Contact not found contact");
+    }
+
+   const { name, email, phone } = body;
+  
+
+    if (name) {
+      currentContact.name = name;
+    }
+
+    if (email) {
+      currentContact.email = email;
+    }    
+
+     if (phone) {
+    currentContact.phone = phone;
+    }
+
+    const contactUpdate = await currentContact(contactId, currentContact);
+
+    if (!contactUpdate) {
+      throw RequestError(404, "Not found update");
+    }
+
+    res.status(200).json(contactUpdate);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = router;
