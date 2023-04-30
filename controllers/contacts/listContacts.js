a3c5af45a11ca2ad60e1965870b42d91ac35c6fa
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-04-30 18:52:22 +0300

    upd

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,18 +1,16 @@
-const {Contact} = require("../../models");
+const { Contact } = require("../../models");
 
 const listContacts = async (req, res) => {
-    const {_id: owner} = req.user;
-   const {page=1, limit=10, favorite} = req.query;
-   const skip = (page -1) * limit;
+  const { _id } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
 
-   const filter = favorite ? { owner, favorite } : { owner }; 
-
-   const contacts = await Contact.find(filter, "-createdAt -updatedAt", {
-   skip, 
-   limit: Number(limit)})
-   .populate("owner", "name email subscription");
-  //  const contacts = await Contact.find({})   
-   res.json({
+  const contacts = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id name email subscription");
+  //  const contacts = await Contact.find({})
+  res.json({
     status: "success",
     code: 200,
     data: {
