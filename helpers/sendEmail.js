d419a4d4130ae26c0f597bc4f3ffd5816f383e83
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-05-01 21:32:31 +0300

    upd email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -4,7 +4,7 @@ const { SENGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENGRID_API_KEY);
 
 const sendEmail = async (data, next) => {
-  const email = { ...data, from: 'dasha_shlyakhovaya@ukr.net' };
+  const email = { ...data, from: 'yuriy.orekhov@gmail.com' };
   try {
     await sgMail.send(email);
     return true;
