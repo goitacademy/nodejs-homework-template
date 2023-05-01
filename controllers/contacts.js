be2ff11790f3a62b2e9469d3ec9abfb15b429b91
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-05-01 14:51:34 +0300

    fix favorite query

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,9 +6,10 @@ const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
+  const query = favorite ? { owner, favorite } : { owner };
   const result = await Contact.find(
-    { owner, favorite },
-    "-createdAt -updatedAt",
+    query,
+    {},
     {
       skip,
       limit,
