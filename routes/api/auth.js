c80fda728e8289b358689470b2d1122eca756a28
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-05-02 20:06:05 +0300

    02.05.23

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -16,6 +16,7 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 // signout
+
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch(
