b10cd3987e9b663cdab69bab9be8433ceb21dc2b
lassandra85 <lassandra85@gmail.com>
2023-05-02 16:05:50 +0200

    error 400

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,8 +2,13 @@ const { HttpError } = require("../helpers");
 
 const validateBody = schema => {
     const func = (req, res, next) => {
-        const { error } = schema.validate(req.body);
 
+        const keys = Object.keys(req.body);
+        if (keys.length === 0) {
+            next(HttpError(400, "missing required name field"));
+        }
+
+        const { error } = schema.validate(req.body);
         if (error) {
             next(HttpError(404, error.message));
         }
