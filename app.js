c1ce650d93e7f7d061d20342c3528f45c9530e7a
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-05-02 19:52:10 +0300

    updated router

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/contacts', contactsRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,42 @@
 const express = require('express')
+const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../../models/contacts')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  listContacts();
+  res.json({ message: 'template getAll message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId}=req.params;
+  getContactById(contactId);
+  res.json({ message: 'template getOne message' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {name,email,phone}=req.body;
+  if (!name || !email || !phone) return res.json({ message: 'template denied message' });
+  addContact({name,email,phone});
+  res.json({ message: 'template post message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId}=req.params;
+  removeContact(contactId);
+  res.json({ message: 'template delete message' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId}=req.params;
+  const {name,email,phone}=req.body;
+  let newBody={};
+  if (name) newBody.name=name;
+  if (email) newBody.email=email;
+  if (phone) newBody.phone=phone;
+  if (Object.keys(newBody).length===0) return res.json({ message: 'template edit denied message' })
+  updateContact(contactId,newBody);
+  res.json({ message: 'template edit message' })
 })
 
 module.exports = router
