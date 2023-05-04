27ee98345fe080d1e84087b61f06229e4f7e7e8e
alena <evstafienko_a@ukr.net>
2023-05-04 21:54:47 +0300

    add avatars

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -5,20 +5,20 @@ const { ctrlWrapper, HttpError } = require("../../helpers");
 const { User } = require("../../models");
 const { log } = require("console");
 
-const avatarsDir = path.join(__dirname,  "..", "..", "public", "avatars");
+const avatarsDir = path.join(__dirname, "..", "..", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
-    const {_id} = req.user;
-    const {path: tempUpload, originalname} = req.file;
+  const { path: tempUpload, originalname } = req.file;
   // path: tempUpload - шлях, originalname - імя під яким прийшов файл
-  const filename = `${_id}_${originalname}`
+  const { _id } = req.user;
+  const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, {avatarURL});
+  await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
     avatarURL,
-  })
+  });
 };
 
 module.exports = { updateAvatar: ctrlWrapper(updateAvatar) };
