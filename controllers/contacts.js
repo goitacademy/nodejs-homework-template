bbb77efcf08f87765023dead6359ee6868d2321b
Sergey <sergey.cherenkov96@gmail.com>
2023-05-05 16:44:53 +0300

    done v1

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -8,7 +8,13 @@ const {
 } = require("../models/contact");
 
 const getContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner");
   res.json(result);
 };
 
@@ -30,7 +36,9 @@ const addContact = async (req, res) => {
     throw HttpError(400, "missing required name field");
   }
 
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/auth");
+const authenticate = require("../../middlewares/authenticate");
 
+// signup
 router.post("/register", ctrl.register);
 
+// signin
 router.post("/login", ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+// logout
+
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
