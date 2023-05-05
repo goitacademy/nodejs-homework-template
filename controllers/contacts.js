b18f8b5fd9aea9ba72981d01b21df6223c558a8e
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-05-05 19:16:36 +0300

    Fixed bugs

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,8 @@
-const Joi = require("joi");
-
 const db = require("../models/contacts");
 
 const HttpError = require("../helpers/HttpError");
 
 const ctrlWrapper = require("../helpers/ctrlWrapper");
-
-const schemaAddContact = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
 const getAll = async (req, res) => {
   const result = await db.listContacts();
@@ -37,24 +29,14 @@ const remove = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const body = req.body;
-  const { error } = schemaAddContact.validate(body);
-
-  if (error) {
-    throw HttpError(400, error.message);
-  }
-  const result = await db.addContact(body);
+  const result = await db.addContact(req.body);
   res.status(201).json(result);
 };
 
 const update = async (req, res) => {
   const body = req.body;
   const { contactId } = req.params;
-  const { error } = schemaAddContact.validate(body);
 
-  if (error) {
-    throw HttpError(400, error.message);
-  }
   const update = await db.updateContact(contactId, body);
   if (!update) {
     throw HttpError(404, "Not found");
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,14 +4,18 @@ const router = express.Router();
 
 const ctrs = require("../../controllers/contacts");
 
+const validateBody = require("../../middllwares/validateBody");
+
+const schema = require("../../schemas/contactSchema");
+
 router.get("/", ctrs.getAll);
 
 router.get("/:contactId", ctrs.getById);
 
-router.post("/", ctrs.add);
+router.post("/", validateBody(schema), ctrs.add);
 
 router.delete("/:contactId", ctrs.remove);
 
-router.put("/:contactId", ctrs.update);
+router.put("/:contactId", validateBody(schema), ctrs.update);
 
 module.exports = router;
