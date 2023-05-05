84e94a0610a485fe1a6e44464b18db3dd2046b6c
VadimKorobec <_s_e_t__@ukr.net>
2023-05-05 16:36:27 +0300

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -3,6 +3,7 @@ const ctrlWrapper = require("./ctrlWrapper");
 const objectFieldsChecker = require("./objectFieldsChecker");
 const handleMongooseError = require("./handleMongooseError");
 const jimpResizer = require("./jimpResizer");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   httpError,
@@ -10,4 +11,5 @@ module.exports = {
   objectFieldsChecker,
   handleMongooseError,
   jimpResizer,
+  sendEmail,
 };
