2cb0d3112ecfabb1214d11ee454512c1ae66de93
VadimKorobec <_s_e_t__@ukr.net>
2023-05-06 17:23:51 +0300

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -13,29 +13,14 @@ const nodemailerConfig = {
   },
 };
 
-const sendEmail = async (data) => {
-  const transporter = nodemailer.createTransport(nodemailerConfig);
+const transporter = nodemailer.createTransport(nodemailerConfig);
 
-  transporter.sendMail(data, (error, info) => {
-    if (error) {
-      console.log(error.message);
-      return process.exit(1);
-    }
-    console.log("Email sent success", info.response);
-    return process.exit(1);
-  });
+const sendEmail = async (data) => {
+  const email = {
+    ...data,
+    from: "vadimkorobets@meta.ua",
+  };
+  await transporter.sendMail(email);
 };
 
 module.exports = sendEmail;
-
-// const email = {
-//   from: "vadimkorobets@meta.ua",
-//   to: "_s_e_t__@ukr.net",
-//   subject: "Test email",
-//   text: "Привет. Мы тестируем отправку писем!",
-// };
-
-// transport
-//   .sendMail(email)
-//   .then(() => console.log("Email send success"))
-//   .catch((error) => console.log(error.message));
