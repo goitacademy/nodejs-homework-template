744189a0d8e4a0191e0843b67b5be70ccce4644e
Viacheslav Plevako <cheryec@ukr.net>
2023-05-08 12:51:08 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+//3bYaYkJeaAFqpqNn
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Viacheslav:3bYaYkJeaAFqpqNn@cluster0.pf5qkht.mongodb.net/";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.messsage);
+    process.exit(1);
+  });
