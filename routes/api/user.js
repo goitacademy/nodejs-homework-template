6f8678b95d1dd78927fc8b19dd9679436dfbe64a
SerhiySBV <serhiybelyavskii@gmail.com>
2023-05-08 16:25:23 +0300

    login

--- routes/api/user.js
+++ routes/api/user.js
@@ -6,11 +6,15 @@ const {
   //   validateBodyPatch,
   //   isValidId,
 } = require("../../middlewares");
-
 const ctrl = require("../../controllers/auth");
+const { registerValidator, loginValidator } = require("../../models/user");
 
-const { registerValidator } = require("../../models/user");
+// Signup
 
 router.post("/register", validateBodyPost(registerValidator), ctrl.register);
+
+// Log In
+
+router.post("/login", validateBodyPost(loginValidator), ctrl.login);
 
 module.exports = router;
