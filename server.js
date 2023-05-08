f76a77b9d1c86e25e0e6c93d7f35a969597be7a4
Viacheslav Plevako <cheryec@ukr.net>
2023-05-08 13:18:02 +0300

    dell yarn.lock

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-//3bYaYkJeaAFqpqNn
+// 3bYaYkJeaAFqpqNn
 
 // const DB_HOST =
 //   "mongodb+srv://Viacheslav:3bYaYkJeaAFqpqNn@cluster0.pf5qkht.mongodb.net/contacts_list";
