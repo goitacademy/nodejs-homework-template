579403c7c5e75c225964258de77b7cc2385cfc46
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-05-09 21:01:24 +0200

    fixed PUT error

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -34,6 +34,9 @@ const updateContact = async (req, res) => {
   const { contactId } = req.params;
 
   const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpErrors(404);
+  }
   res.status(201).json(result);
 };
 
