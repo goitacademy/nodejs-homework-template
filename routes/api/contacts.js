6d55d7d0327dd81308dccc0f95d4644b3d900608
ElenPolyanska <evp387@gmail.com>
2023-05-09 13:04:06 +0300

    1st part

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -25,24 +25,3 @@ router.delete('/:id', ctrl.deleteById)
 
 
 module.exports = router;
-
-
-
-// ===================================================================================================
-// const express = require("express");
-
-// const router = express.Router();
-
-// const contactsControlles = require("../../contollers/contacts-controllers.js");
-
-// router.get("/", contactsControlles.getContacts);
-
-// router.get("/:id", contactsControlles.getContactsById);
-
-// router.post("/", contactsControlles.addContact);
-
-// router.put("/:id", contactsControlles.updateContact);
-
-// router.delete("/:id", contactsControlles.deleteContact);
-
-// module.exports = router;
