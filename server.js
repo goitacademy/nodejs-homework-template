e2045444e7e4e53f6c46532350b56d120d16951c
Inna Kukla <0969125133@ukr.net>
2023-05-10 00:58:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true)
 
-mongoose.connect(DB_HOST)
+mongoose.connect("mongodb+srv://Inna:19101974@cluster0.xeetnqb.mongodb.net/db-contacts?retryWrites=true&w=majority")
 .then(() => {
   app.listen(PORT);
   console.log("Server is running on port 3000")
