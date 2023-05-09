5f860adad41f755fca938d27eacbee3985778806
Inna Kukla <0969125133@ukr.net>
2023-05-09 22:09:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ mongoose.set('strictQuery', true)
 
 mongoose.connect(DB_HOST)
 .then(() => {
-  app.listen(PORT, function(){
-    console.log("Server is running on port 3000");
-  })
-  
+  app.listen(PORT);
+  console.log("Server is running on port 3000")
 })
 .catch(error => {
   console.log(error);
