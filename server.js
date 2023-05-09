de01588ef3e07ea6125c290175dfaa52472353f4
Inna Kukla <0969125133@ukr.net>
2023-05-09 21:51:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true)
 
 mongoose.connect(DB_HOST)
 .then(() => {
-  app.listen("3000", function(){
+  app.listen(PORT, function(){
     console.log("Server is running on port 3000");
   })
   
