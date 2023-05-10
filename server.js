8628df212a8045209e417146d05736a698e53d04
Inna Kukla <0969125133@ukr.net>
2023-05-10 21:13:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const {DB_HOST, PORT = 3000} = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.set('strictQuery', true)
+mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST)
-.then(() => {
-  app.listen(PORT);
-  console.log("Server is running on port 3000")
-})
-.catch(error => {
-  console.log(error);
-  process.exit(1)
-})
-
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
