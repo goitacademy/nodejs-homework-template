cd45f6254ad8229e1d140e94dd32ed0f0c3d6150
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-05-11 01:07:08 +0300

    update castums notifications

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,16 @@
 const Joi = require('joi');
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    'any.required': 'missing required name field',
+  }),
+  email: Joi.string().required().messages({
+    'any.required': 'missing required email field',
+    'string.email': 'invalid email format',
+  }),
+  phone: Joi.string().required().messages({
+    'any.required': 'missing required phone field',
+  }),
 });
 
 module.exports = {
