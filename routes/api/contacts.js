e78ab6f2a5be293ce1f3aedf28c0fbbe877052c8
Volodymyr <vovdata@gmail.com>
2023-05-11 17:11:43 +0200

    Additions to JOI schem
    Made JOI scheme more specific.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,10 +8,13 @@ const router = express.Router()
 const { HttpError } = require("../../helpers")
 
 const addSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
-})
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/).required().messages({
+  'string.pattern.base': 'Incorrect phone format. Example: (748) 206-2688',
+  'any.required': 'The phone field is required'
+}),
+});
 
 
 router.get('/', async (req, res, next) => {
