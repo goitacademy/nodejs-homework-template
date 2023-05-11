19b29246631596537888c09b610ed97625fa2725
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-05-11 16:09:06 +0300

    add start functions in contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,28 @@
-const express = require('express')
+const express = require('express');
+const contacts = require('../../models/contacts');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const result = await contacts.listContacts();
+  // res.json({ message: 'template message' });
+  res.json(result);
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
