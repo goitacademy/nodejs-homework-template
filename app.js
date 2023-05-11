3265414e27a29825e92574bdc9f807537653a60c
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-05-11 13:03:22 +0300

    add config.js

--- app.js
+++ app.js
@@ -23,4 +23,6 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
+//RBDPUJCnqwkzK5Or
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST = require("./config");
+
+// const DB_HOST =
+//   "mongodb+srv://Anastasia:RBDPUJCnqwkzK5Or@cluster0.kmhxppp.mongodb.net/contacts_64?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((e) => {
+    console.log(e.message);
+    process.exit(1);
+  });
