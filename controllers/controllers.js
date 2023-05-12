30912aa15959008d20874f390da614cd477fdb93
Oleksandr Makogin <sashamakohin@gmail.com>
2023-05-12 16:38:04 +0300

    added decoration for controllers

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -6,58 +6,40 @@ const {
   updateContactService,
 } = require("../services/contactsServices");
 
-const getContacts = async (req, res, next) => {
-  try {
-    const contacts = await getContactsService();
-    res.status(200).json(contacts);
-  } catch (error) {
-    next(error);
-  }
+const { ctrlWrapper } = require("../utils/decorators");
+
+const getContacts = async (req, res) => {
+  const contacts = await getContactsService();
+  res.status(200).json(contacts);
 };
 
-const getContact = async (req, res, next) => {
-  try {
-    const contact = await getContactService(req.params.contactId);
-    console.log(contact);
-    res.status(200).json(contact);
-  } catch (error) {
-    next(error);
-  }
+const getContact = async (req, res) => {
+  const contact = await getContactService(req.params.contactId);
+  console.log(contact);
+  res.status(200).json(contact);
 };
 
-const addContact = async (req, res, next) => {
-  try {
-    const newContact = await addContactService(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
+const addContact = async (req, res) => {
+  const newContact = await addContactService(req.body);
+  res.status(201).json(newContact);
 };
 
-const updateContact = async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const updatedContact = await updateContactService(id, req.body);
+const updateContact = async (req, res) => {
+  const id = req.params.contactId;
+  const updatedContact = await updateContactService(id, req.body);
 
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    next(error);
-  }
+  res.status(200).json(updatedContact);
 };
 
-const removeContact = async (req, res, next) => {
-  try {
-    const removedContact = await removeContactService(req.params.contactId);
-    res.status(200).json(removedContact);
-  } catch (error) {
-    next(error);
-  }
+const removeContact = async (req, res) => {
+  const removedContact = await removeContactService(req.params.contactId);
+  res.status(200).json(removedContact);
 };
 
 module.exports = {
-  getContacts,
-  getContact,
-  removeContact,
-  addContact,
-  updateContact,
+  getContacts: ctrlWrapper(getContacts),
+  getContact: ctrlWrapper(getContact),
+  removeContact: ctrlWrapper(removeContact),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
 };
---
+++ utils/decorators/ctrlWrapper.js
@@ -0,0 +1,13 @@
+const ctrlWrapper = (ctrl) => {
+  const fn = async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  return fn;
+};
+
+module.exports = ctrlWrapper;
