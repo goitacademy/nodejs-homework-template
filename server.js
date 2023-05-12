534c3547f750ccc75234687f38c157104ead1410
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-05-12 23:09:19 +0300

    adds connection to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connected successfully`);
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
