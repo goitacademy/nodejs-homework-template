2358c3a237b260c51aad6b3c7033b9f3c2108ee9
lassandra85 <lassandra85@gmail.com>
2023-05-12 22:32:43 +0200

    add1

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => app.listen(PORT, () => {
-    /* console.log("Server running. Use our API on port: 3000") */
     console.log("Database connection successful")
   }))
   .catch(error => {
