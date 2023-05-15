fa8e118ba59a7eb09c21282ab2bc0b372f8656d0
Kate Fanina <katherina.fanina@gmail.com>
2023-05-15 17:54:09 +0300

    rest API

---
+++ controllers/contact-controllers.js
@@ -0,0 +1,50 @@
+const contactsService = require('../models/contacts');
+
+const HttpError = require('../helpers');
+
+const { ctrlWrapper } = require('../decorators');
+
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const result = await contactsService.getContactById(req.params.contactId);
+  if (!result) {
+    throw HttpError(404, `Contacts with ${id} not found`);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
+};
+
+const deleteContactById = async (req, res) => {
+  const result = await contactsService.removeContact(req.params.contactId);
+  if (!result) {
+    throw HttpError(404, `Contacts with ${id} not found`);
+  }
+  res.json({
+    message: 'Delete success',
+  });
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContactById: ctrlWrapper(updateContactById),
+  deleteContactById: ctrlWrapper(deleteContactById),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,23 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
 
-const router = express.Router()
+const contactsController = require('../../controllers/contact-controllers');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemas = require('../../schemas/contact-schemas');
+const validateBody = require('../../decorators/validateBody');
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', contactsController.getAllContacts);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', contactsController.getContactById);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateBody(schemas.contactAddSchema), contactsController.addContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put(
+  '/:contactId',
+  validateBody(schemas.contactAddSchema),
+  contactsController.updateContactById
+);
 
-module.exports = router
+router.delete('/:contactId', contactsController.deleteContactById);
+
+module.exports = router;
