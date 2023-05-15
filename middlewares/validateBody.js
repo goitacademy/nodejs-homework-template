86b745897a3657fd3ab28f0b854dd5a88670230a
Havura Artur <havuraa@gmail.com>
2023-05-15 17:59:11 +0300

    fixing mistakes

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,11 +3,11 @@ const { RequestError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     if (Object.keys(req.body).length === 0) {
-      next(RequestError(400, "missing required name field"));
+      next(RequestError(400, "Missing fields"));
     } else {
       const { error } = schema.validate(req.body);
       if (error) {
-        next(RequestError(400, error.message));
+        next(RequestError(400, "missing required name field"));
       }
       next();
     }
