33de6e4b49af723cc96324eb391d8cd5a92a551c
SerhiySBV <serhiybelyavskii@gmail.com>
2023-05-15 18:41:10 +0300

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
-app.use("/user", authRouter);
+app.use("/users", authRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
