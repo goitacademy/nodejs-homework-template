5573a2ede19e94cc5aea6fa162c29e0c907f6621
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-16 14:43:23 +0300

    add dataBase

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const mongoose = "mongoose";
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Minaht:GiDeOn1983@cluster0.ccmrmnr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .cath((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
