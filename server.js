327bfffc3ba00319dd1af89f5b8aaec2785d15f7
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-16 15:15:09 +0300

    123

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const mongoose = "mongoose";
 const app = require("./app");
 
+mongoose.set("strictQwery", true);
+
 const DB_HOST =
-  "mongodb+srv://Minaht:GiDeOn1983@cluster0.ccmrmnr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+  "mongodb+srv://Minaht:gideon1983@cluster0.ccmrmnr.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose.set("strictQwery", true);
 
@@ -10,6 +12,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
+    console.log("connect base");
   })
   .cath((error) => {
     console.log(error.message);
