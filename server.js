c0c35e7324291f577ad878dc3a3aee80a1deb810
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-16 12:03:57 +0300

    port correct

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
 
 const DB_HOST =
   "mongodb+srv://Minaht:GiDeOn1983@cluster0.ccmrmnr.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose.set("strictQwery", true);
+
 mongoose
   .connect(DB_HOST)
-  .then(app.listen(3000))
+  .then(() => {
+    app.listen(3000);
+  })
   .catch(() => {
     console.log(error.message);
     process.exit(1);
