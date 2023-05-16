8357479daa2195bd9d6be855aa1ad37794ff421d
nastiaknik <anastazja.knihnitska@gmail.com>
2023-05-16 10:16:29 +0200

    Create a REST API for managing a collection of contacts
    Adds routes for endpoints such as retrieving all contacts, retrieving a contact by ID, adding a new contact, deleting a contact, and updating a contact. Validate the incoming data using the Joi package.

---
+++ controllers/contactsConrollers.js
@@ -0,0 +1,59 @@
+const contactsService = require("../models/contactsService");
+const HttpError = require("../helpers/HttpError");
+const ctrlWrapper = require("../decorators/ctrlWrapper");
+
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  console.log(req.body);
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const contact = await contactsService.getContactById(id);
+  if (!contact) {
+    throw new HttpError(404, `Contact with ${id} not found`);
+  }
+
+  const updatedContact = {
+    ...contact,
+    ...req.body,
+  };
+
+  const result = await contactsService.updateContact(id, updatedContact);
+  res.json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+
+  res.json({
+    message: "Contact deleted",
+  });
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  removeContact: ctrlWrapper(removeContact),
+};
