b5037fe212ad15d28c70c5d87ee19a87952bf8a9
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-16 14:15:47 +0300

    coppy project

--- server.js
+++ server.js
@@ -1,21 +1,5 @@
-const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Minaht:GiDeOn1983@cluster0.ccmrmnr.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-mongoose.set("strictQwery", true);
-
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000);
-  })
-  .catch(() => {
-    console.log(error.message);
-    process.exit(1);
-  });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
