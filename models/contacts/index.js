a56fd13c4854f09ac344f3345b22a526e90b0fd3
6ara6ac <106093131+6ara6ac@users.noreply.github.com>
2023-05-16 21:40:04 +0300

    upd home work

---
+++ models/contacts/index.js
@@ -0,0 +1,61 @@
+
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+
+ const contactsPath = path.join(__dirname, 'contacts.json');
+
+
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  }
+  
+  const getContactById = async(contactId) => {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId)
+    return result || null; 
+  }
+  
+  const removeContact = async (contactId) => {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => item.id === contactId)
+    if (idx === -1){
+        return null;
+    }
+    const [result] = contacts.splice(idx,1)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+    return result;
+}   
+  
+  const addContact = async(data) => {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        ...data
+    }
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  }
+
+  const updateContact = async (contactId, data) => {
+    const contacts = await listContacts()
+    const idx = contacts.findIndex(item => item.id === contactId)
+    if (idx === -1){
+      return null;
+  }
+    contacts[idx] = {contactId, ...data}
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+    return contacts[idx]
+  }
+
+
+  module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact
+  }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,89 @@
 const express = require('express')
+const Joi = require("joi");
+
+const contacts = require('../../models/contacts')
+const { HttpError } = require('../../helpers')
 
 const router = express.Router()
 
+const addShema = Joi.object({
+  name: Joi.string().required().min(3).max(30),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+  phone: Joi.string().required().min(3).max(30)
+})
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+}
+catch(error) {
+    next(error)
+}
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId)
+    if (!result) {
+      throw HttpError(404, 'Not found')
+    }
+    res.json(result)
+  }
+  catch(error){
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const { error } = addShema.validate(req.body)
+    if (error){
+      throw HttpError (400, "missing required name field")
+    }
+    const result = await contacts.addContact(req.body)
+    res.status(201).json(result)
+  }
+  
+  catch(error){
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId)
+    if(!result) {
+      throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Delete success"
+  })
+  }
+  catch(error){
+    next(error)
+  }
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const { error } = addShema.validate(req.body)
+    if(error){
+      throw HttpError (400, 'missing fields')
+    }
+    const { contactId } = req.params
+    const result = await contacts.updateContact(contactId, req.body)
+    if(!result) {
+      throw HttpError(404, "Not found");
+  }
+  res.json(result);
+  }
+  catch(error){
+    next(error)
+  }
 })
 
 module.exports = router
