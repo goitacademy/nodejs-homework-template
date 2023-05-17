959acda45c866fc97a20c58d24a8fb296a856ce6
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-17 17:37:34 +0300

    mongoose.set

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
+mongoose.set("strictQuery", true);
 const DB_HOST =
   "mongodb+srv://Minaht:GiDeOn1983@cluster0.ccmrmnr.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
