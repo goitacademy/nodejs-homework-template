ecd1dc6a6220a00dcab11b324c41a3fbe131b1f2
IhorC <igorchua@gmail.com>
2023-05-17 18:07:26 +0300

    commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,25 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message, GET' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  // res.json({ message: 'template message' })
+  res.send(`<h1>Contact id: ${req.params.contactId}</h1>`);
+  res.send(req);
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message, POST' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message, DELETE' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message, PUT' })
 })
 
 module.exports = router
