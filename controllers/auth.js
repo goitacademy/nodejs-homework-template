4bc26b1e46fe089e219e01f463eec4eed2bab0d1
Viacheslav Plevako <cheryec@ukr.net>
2023-05-17 19:51:08 +0300

    change bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { User } = require("../models/user");
