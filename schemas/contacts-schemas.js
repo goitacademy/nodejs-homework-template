d77e61b78837a8e559c10f46d71f40b4b2208ee8
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-05-18 22:42:41 +0300

    Modified contacts-schema

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,9 +1,11 @@
 import Joi from "joi";
 
 const contactAddSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().trim().min(2).max(50).required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(/^\+\d{12}$/)
+    .required(),
 }).options({
   messages: { "any.required": "missing required {{#label}} field" },
 });
