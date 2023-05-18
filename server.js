4c2586994148e19e5555f4cdee23b957869a6ac5
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-05-18 10:11:52 +0200

    add connect to base

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require('mongoose')
+const connectToBase = 'mongodb+srv://mazurfacker:UKl39FTb9topkEUl@mazurfacker.awka0v2.mongodb.net/contacts?retryWrites=true&w=majority'
+mongoose.connect(connectToBase)
+  .then(() => {
+  app.listen(3000)
+  }).catch(err => {
+    console.log(err.message)
+    process.exit(1)
 })
+
+
+// UKl39FTb9topkEUlnpm i mongoose
