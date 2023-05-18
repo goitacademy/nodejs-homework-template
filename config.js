676276e68e54f59c46a6e6fa4b013cdbdb87d336
Volodymyr Butyter <butytervolodia@gmail.com>
2023-05-18 13:07:47 +0300

    enviroment variable added

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = "mongodb+srv://Lindelof:Aaxo5rDb1ZHEqFEm@cluster0.hpkn1k8.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+exports.module = {
+    DB_HOST
+}
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Lindelof:Aaxo5rDb1ZHEqFEm@cluster0.hpkn1k8.mongodb.net/contacts_reader?retryWrites=true&w=majority";
-
-// const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', true);
 
