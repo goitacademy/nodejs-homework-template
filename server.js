73c3773fd92dbaf9f981eca6d2c4b3a4d88deeb4
Olexandr Tkachuk <temishena@gmail.com>
2023-05-19 16:42:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const DB_HOST =
 mongoose
 	.connect(DB_HOST)
 	.then(() => {
-		app.listen(3000, () => {
-			console.log("Server running. Use our API on port: 3000");
-		});
+		app.listen(3000);
 	})
 	.catch((error) => {
 		console.log(error.message);
