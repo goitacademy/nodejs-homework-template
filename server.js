d5a055eb1222a8c3b53be6622212a1332156612a
romis44 <romankovalchuk44@gmail.com>
2023-05-19 11:27:05 +0300

    delete id

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require('./app')
+const app = require("./app");
+
+// 9Rag22gTk8jC5HJz
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
