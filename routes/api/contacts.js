c1c2dc10f54b8c1d63600dbf9f023c07002089cb
Taisiya Tomashevska <taya22011985@gmail.com>
2023-05-19 18:02:27 +0300

    ADDED: route all and by id

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,34 @@
 const express = require('express')
 
-const router = express.Router()
+const router = express.Router();
+const contacts = require("../../models/contacts")
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template' })
+  try {
+    const results = await contacts.listContacts();
+  res.json(results);
+  } catch (error) {
+    res.status(500).json({
+      message:"Server error"
+    })
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  res.json(result)
+    
+  } catch (error) {
+    res.status(500).json({
+      message:"Server error"
+    })
+  }
+  }
+ 
+)
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
