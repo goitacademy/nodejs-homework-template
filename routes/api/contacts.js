690bdc158e0d6f82f1b6f4867776370a0764501b
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-05-19 16:01:21 +0300

    ADD  MongoDB + patch route
    done all route by mongoose methods
    added patch route for favorite
    added .env for DB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,32 @@
-const express = require("express");
+const express = require('express');
 
-const { validateBody } = require("../../middlewars");
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId } = require('../../middlewars');
 
 const router = express.Router();
-const ctrl = require("../../сontrollers/contacts");
+const ctrl = require('../../сontrollers/contacts');
+const { schemas } = require('../../models/contact');
 
-router.get("/", ctrl.getAll);
+router.get('/', ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get('/:contactId', isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post('/', validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteById);
+router.delete('/:contactId',isValidId, ctrl.deleteById);
 
 router.put(
-  "/:contactId",
+  '/:contactId',
+  isValidId,
+
   validateBody(schemas.addSchema),
   ctrl.updateContactById
+);
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
 );
 
 module.exports = router;
