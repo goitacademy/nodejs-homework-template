9581feb28d103bc4fd899348bd05717e3203fc02
Olexandr Tkachuk <temishena@gmail.com>
2023-05-19 16:39:01 +0300

    mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-	console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+	"mongodb+srv://Temishen:Vbl9122323@cluster0.b1rddgy.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		app.listen(3000, () => {
+			console.log("Server running. Use our API on port: 3000");
+		});
+	})
+	.catch((error) => {
+		console.log(error.message);
+		process.exit(1);
+	});
