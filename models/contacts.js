824286756a74746ff1a28a744be99015e6a07592
VovaKyrylenko <110337169+VovaKyrylenko@users.noreply.github.com>
2023-05-20 21:40:47 +0300

    final

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,82 @@
-// const fs = require('fs/promises')
+const fs = require('fs');
+const path = require('path')
+const contactsDirectory = path.join(__dirname, 'contacts.json')
+const Joi = require('joi')
 
-const listContacts = async () => {}
+const createSchema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required()
+});
 
-const getContactById = async (contactId) => {}
+const updateSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string()
+});
 
-const removeContact = async (contactId) => {}
-
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+function listContacts() {
+  const contactsData = fs.readFileSync(contactsDirectory);
+  const contacts = JSON.parse(contactsData);
+  return contacts;
 }
+
+function getById(id) {
+  const contactsData = fs.readFileSync(contactsDirectory);
+  const contacts = JSON.parse(contactsData);
+  
+  const contact = contacts.find((c) => c.id === id);
+  return contact;
+}
+
+function addContact(contact) {
+  const { error } = createSchema.validate(contact);
+
+  if (error) {
+    throw new Error('Validation error: ' + error.details[0].message);
+  }
+
+  const contactsData = fs.readFileSync(contactsDirectory);
+  const contacts = JSON.parse(contactsData);
+  contacts.push(contact);
+  fs.writeFileSync(contactsDirectory, JSON.stringify(contacts));
+}
+
+function removeContact(id) {
+  const contactsData = fs.readFileSync(contactsDirectory);
+  let contacts = JSON.parse(contactsData);
+  const contactIndex = contacts.findIndex((c) => c.id === id);
+  if (contactIndex !== -1) {
+    const contact = contacts.splice(contactIndex, 1)[0];
+    fs.writeFileSync(contactsDirectory, JSON.stringify(contacts));
+    return contact;
+  }
+  return null;
+}
+
+function updateContact(id, updatedFields) {
+  const { error, value } = updateSchema.validate(updatedFields);
+  if (error) {
+    throw new Error('Invalid data provided');
+  }
+
+  const contactsData = fs.readFileSync(contactsDirectory);
+  let contacts = JSON.parse(contactsData);
+  const contactIndex = contacts.findIndex((c) => c.id === id);
+  if (contactIndex !== -1) {
+    contacts[contactIndex] = { ...contacts[contactIndex], ...value };
+    fs.writeFileSync(contactsDirectory, JSON.stringify(contacts));
+    return contacts[contactIndex];
+  }
+  return null;
+}
+
+
+
+
+
+
+
+
+module.exports = { listContacts, getById, addContact, removeContact, updateContact };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,89 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
+const {nanoid} = require("nanoid");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  res.json(listContacts());
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  // Виклик функції getById для роботи з файлом contacts.json
+  const contact = getById(id);
+  if (contact) {
+    // Якщо такий id існує, повертаємо об'єкт контакту у форматі JSON зі статусом 200
+    res.json(contact);
+  } else {
+    // Якщо такого id не існує, повертаємо JSON з ключем "message": "Not found" і статусом 404
+    res.status(404).json({ message: "Contact with this id not found" });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const body = req.body;
+  console.log(req.body)
+  // Перевірка наявності обов'язкових полів у тілі запиту
+  if (!body.name || !body.email || !body.phone) {
+    // Якщо якісь обов'язкові поля відсутні, повертаємо JSON з ключем "message": "missing required name field" і статусом 400
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    // Генерація унікального ідентифікатора для контакту
+    const id = nanoid();
+    const contact = {
+      id: id,
+      name: body.name,
+      email: body.email,
+      phone: body.phone,
+    };
+    // Виклик функції addContact для збереження контакту в файлі contacts.json
+    addContact(contact);
+    // Повернення об'єкту з доданим ідентифікатором та статусом 201
+    res.status(201).json(contact);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  // Виклик функції removeContact для роботи з файлом contacts.json
+  const contact = removeContact(id);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (contact) {
+    // Якщо такий id існує, повертаємо JSON формату {"message": "contact deleted"} і статусом 200
+    res.json({ message: "contact deleted" });
+  } else {
+    // Якщо такого id не існує, повертаємо JSON з ключем "message": "Not found" і статусом 404
+    res.status(404).json({ message: "Not found" });
+  }
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const body = req.body;
+
+  // Перевірка наявності тіла запиту
+  if (!body) {
+    // Якщо тіло запиту відсутнє, повертаємо JSON з ключем "message": "missing fields" і статусом 400
+    res.status(400).json({ message: "missing fields" });
+  } else {
+    // Виклик функції updateContact для оновлення контакту в файлі contacts.json
+    const updatedContact = updateContact(id, body);
+
+    if (updatedContact) {
+      // Якщо контакт успішно оновлено, повертаємо оновлений об'єкт контакту та статус 200
+      res.json(updatedContact);
+    } else {
+      // Якщо контакт не знайдено, повертаємо JSON з ключем "message": "Not found" і статусом 404
+      res.status(404).json({ message: "Not found" });
+    }
+  }
+});
+
+module.exports = router;
