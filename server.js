97ae7df21e79fee8432bc03923639af8cb64553b
Ihor Statsenko <statsenkoin@gmail.com>
2023-05-20 21:29:02 +0300

    feature/ Create DB schema, update query

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set('strictQuery', true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Database connnection successful');
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
     });
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-console.log(process.env);
