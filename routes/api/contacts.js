ff55d53a65a96148d2958b9f2cb3032ed2face2a
Taisiya Tomashevska <taya22011985@gmail.com>
2023-05-20 18:10:27 +0300

    ADDED: method update and delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,28 +1,29 @@
-const express = require('express')
-
+const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
+
 const contacts = require("../../models/contacts");
-const { HttpError } = require('../../helpers');
-const { addContact } = require('../../models/contacts');
-const Joi = require('joi');
+const { HttpError } = require("../../helpers");
+const { addContact } = require("../../models/contacts");
+
 
 const addShema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  phone: Joi.string().required()
-})
+  phone: Joi.string().required(),
+});
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const results = await contacts.listContacts();
     res.json(results);
   } catch (error) {
     next(error);
   }
-  
-})
+});
 
-router.get('/:contactId', async (req, res, next) => {
+
+router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
@@ -30,34 +31,56 @@ router.get('/:contactId', async (req, res, next) => {
       throw HttpError(404, "Not found");
     }
 
-    res.json(result)
-    
+    res.json(result);
   } catch (error) {
     next(error);
   }
-}
-)
+});
 
-router.post('/', async (req, res, next) => {
+
+router.post("/", async (req, res, next) => {
   try {
     const { error } = addShema.validate(req.body);
     if (error) {
-      throw HttpError(error, error.message)
-     }
-    const result = await addContact(req.body)
-   res.status(201).json(result)
-    
+      throw HttpError(error, error.message);
+    }
+    const result = await addContact(req.body);
+    res.status(201).json(result);
   } catch (error) {
-    next(error); 
+    next(error);
   }
-})
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "Delete success" });
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = addShema.validate(req.body);
+    if (error) {
+      throw HttpError(error, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
