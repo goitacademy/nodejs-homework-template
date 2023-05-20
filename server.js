62a1ffcd3ff04139016c9fce4b35638c6fbcc5c1
SedaNoty <sedanoty5@gmail.com>
2023-05-20 17:42:57 +0300

    create config-js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Seda-Noty:$Seda$1979*&*1979$Noty$@cluster0.8qiyrj3.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const {DB_HOST} = require('./config')
 
 mongoose.set('strictQuery', true);
 
