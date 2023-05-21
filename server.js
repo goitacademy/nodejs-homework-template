ce9c91961d0ca9c01cdc2e4a930d459ae0ca35b7
OPaslavskiy <bankir.op@gmail.com>
2023-05-21 13:19:51 +0300

    init project (add bd)

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const BD_HOST =
+  "mongodb+srv://Paslawsky:3lEs6OB69YNWTSkz@cluster0.stkwc1y.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(BD_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
