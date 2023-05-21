3dc59c671034881301168c9bcd1e9f99198c7281
Anton Logvinenko <loav2236@gmail.com>
2023-05-21 19:43:57 +0300

    DB_HOST

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 const{DB_HOST, PORT=3000}= process.env;
 
+mongoose.set("strictQuery", true)
 mongoose.connect(DB_HOST).then(() => {
 
   app.listen(PORT, ()=>{console.log("Database connection successful")}) 
