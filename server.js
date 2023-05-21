a63f2d92edde924a8d82b62ddf897db4299a5384
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-05-22 00:37:06 +0300

    add contact

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const {DB_HOST,PORT=3001} =process.env;
-//const DB_HOST =
-//  "mongodb+srv://Yuliia:VlasiukYuliiaDataBaseUser@cluster0.84r98c6.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
 const app = require("./app");
 
 mongoose
