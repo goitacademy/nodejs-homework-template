8f02b72d96d928c5d9c5371cbdeac370fbdda991
SergeyShturma <sergeyshturma@gmail.com>
2023-05-21 13:22:51 +0300

    add logic in server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require("./app");
+// QrFaNAeNdlVCk5CD
+// mongodb+srv://Serzh:QrFaNAeNdlVCk5CD@cluster0.fkv4dak.mongodb.net/db-contacts?retryWrites=true&w=majority
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connection successful`);
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
