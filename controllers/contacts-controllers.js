7edb176797c4c1a6e28364a2c4ce1558a9c52a04
Olena Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2023-05-21 17:13:24 +0200

    add new ctrls and routers

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,16 +1,16 @@
-const contactsService = require('../models/contacts');
+const { Contact } = require('../models/contact');
 const { HttpError } = require('../helpers');
 const { ctrlWrapper } = require('../decorators');
 
 
 const getAllContacts = async (req, res, next) => {
-    const result = await contactsService.listContacts();
+    const result = await Contact.find();
     res.json(result);   
 };
 
 const getContactById = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await contactsService.getContactById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
         throw new HttpError(404);
     }
@@ -18,13 +18,13 @@ const getContactById = async (req, res, next) => {
 };
 
 const addContact = async (req, res, next) => {
-    const result = await contactsService.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
 };
 
 const deleteContact = async (req, res, next) => { 
     const { contactId } = req.params;
-    const result = await contactsService.removeContact(contactId);
+    const result = await Contact.findByIdAndRemove(contactId);
     if (!result) {
         throw new HttpError(404);
     }
@@ -35,7 +35,16 @@ const deleteContact = async (req, res, next) => {
 
 const updateContact = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await contactsService.updateContact(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+    if (!result) {
+        throw new HttpError(404);
+    }
+    res.json(result);
+};
+
+const updateStatusContact = async (req, res, next) => {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
     if (!result) {
         throw new HttpError(404);
     }
@@ -48,5 +57,6 @@ module.exports = {
     addContact: ctrlWrapper(addContact),
     deleteContact: ctrlWrapper(deleteContact),
     updateContact: ctrlWrapper(updateContact),
+    updateStatusContact: ctrlWrapper(updateStatusContact),
 };
  
