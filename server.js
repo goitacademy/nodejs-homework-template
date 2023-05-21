10a7403742ac5ec6e10c59f9a6661dd715c3db0d
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-05-21 23:02:24 +0300

    exit

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const mongoose = require("mongoose");
 
-const {DB_HOST} =process.env;
+const {DB_HOST,PORT=3001} =process.env;
 //const DB_HOST =
 //  "mongodb+srv://Yuliia:VlasiukYuliiaDataBaseUser@cluster0.84r98c6.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const app = require("./app");
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3001);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
-    //process.exit(1);
+   process.exit(1);
   });
 
-const app = require("./app");
 
 
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
