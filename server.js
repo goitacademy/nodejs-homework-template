e1564d7372f916b488d918f76bb0e86170f736f0
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-05-21 22:31:58 +0300

    db

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Yuliia:VlasiukYuliiaDataBaseUser@cluster0.84r98c6.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const {DB_HOST} =require("./config")
+//const DB_HOST =
+//  "mongodb+srv://Yuliia:VlasiukYuliiaDataBaseUser@cluster0.84r98c6.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_HOST)
