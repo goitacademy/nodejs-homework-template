b6553ec0370b2cfeed35db0ee4dc76d8f711978e
Anton Logvinenko <loav2236@gmail.com>
2023-05-21 18:58:06 +0300

    mongoose DBHoost

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST='mongodb+srv://anton:S1PtLmtSUtE3n7NU@cluster0.edy05dy.mongodb.net/my_contacts?retryWrites=true&w=majority'
+mongoose.connect(DB_HOST).then(() => {
+
+  app.listen(3000, ()=>{console.log("Database connection successful")}) 
+}
+).catch(error=>{
+  console.log(error.message);
+  process.exit(1)
 })
