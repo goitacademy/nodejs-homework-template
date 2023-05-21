6bc602c1a49c56fd5b8c7dd7ec29c3ddd1f5d83d
Hanna Franko <sei.wrcl@gmail.com>
2023-05-21 05:03:17 +0300

    update

--- models/contacts.js
+++ models/contacts.js
@@ -38,9 +38,21 @@ const addContact = async (body) => {
   return newContact;
 };
 
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
+
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 };
