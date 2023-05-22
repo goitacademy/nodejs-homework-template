a5503e1c60b04f3f85be7bdf8f93979f90a9a80a
Fotyniya <s.kozachenko@ekwo.dp.ua>
2023-05-22 21:04:00 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app')
+const DB_HOST = "mongodb+srv://Fotyniya:N_96tTnxyZZh9_q@cluster0.ufjiaui.mongodb.net/db-contacts?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful")
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1)
+  })
+
