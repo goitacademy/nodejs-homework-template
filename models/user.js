08ef90928506a49955157e191940afac35dcc3ad
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-23 23:52:31 +0300

    1

--- models/user.js
+++ models/user.js
@@ -31,4 +31,4 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Customs", userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const jsonParser = express.json();
 
 router.post("/register", jsonParser, (req, res, next) => {
-  // User.create(req.body);
+  User.create(req.body);
   console.log(req.bode);
   res.send("Register");
   next();
