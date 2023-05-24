8b086b5a77c7462aa34cebc9bc7b9c89ecd5b5e2
tsylepa <tsylepa@gmail.com>
2023-05-24 14:08:31 +0100

    Error messages fix

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -4,6 +4,8 @@ const contactAddSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+}).options({
+  messages: { "any.required": "missing required {{#label}} field" },
 });
 
 module.exports = { contactAddSchema };
