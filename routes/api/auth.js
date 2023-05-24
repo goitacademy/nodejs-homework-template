c49bb7cb3f4138dfc94f00143de57fc4e8ab6bc8
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-24 12:32:20 +0300

    1

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,7 +17,8 @@ router.post("/register", jsonParser, async (req, res, next) => {
       return res.status(409).json({ message: "User alredy exist" });
     }
 
-    const newUserPassword = await bcrypt.hash(newUser.password, 10);
+    newUserPassword = await bcrypt.hash(newUser.password, 10);
+
     User.create(newUser);
     return res.status(201).end();
   } catch (error) {
