35086868e0cf1d07abbec8a2fd2626a1343b2156
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-24 10:25:41 +0300

    user control

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+    },
     password: {
       type: String,
       required: [true, "Password is required"],
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,11 +4,22 @@ const router = express.Router();
 
 const jsonParser = express.json();
 
-router.post("/register", jsonParser, (req, res, next) => {
-  User.create(req.body);
-  console.log(req.bode);
-  res.send("Register");
-  next();
+router.post("/register", jsonParser, async (req, res, next) => {
+  const newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  try {
+    const currentUser = await User.findOne({ email: newUser.email });
+    if (currentUser === null) {
+      return res.status(409).json({ message: "User alredy exist" });
+    }
+    User.create(newUser);
+    return res.status(201).end();
+  } catch (error) {
+    return next(error.message);
+  }
 });
 
 module.exports = router;
