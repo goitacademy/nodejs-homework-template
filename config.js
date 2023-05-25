25b28465f2ee51d29c9e4276be3b82dbb90a64bb
Volodya Kushch <110915611+Volodya460@users.noreply.github.com>
2023-05-25 18:13:13 +0300

    test

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://Volodya:s8VCw26H4e9LczkR@cluster0.vobxqw4.mongodb.net/db-contacts";
+
+module.exports = {
+  DB_HOST,
+};
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Volodya:s8VCw26H4e9LczkR@cluster0.vobxqw4.mongodb.net/db-contacts";
+const { DB_HOST } = process.env;
+
+// const DB_HOST =
+//   "mongodb+srv://Volodya:s8VCw26H4e9LczkR@cluster0.vobxqw4.mongodb.net/db-contacts";
+mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() =>
