4a6c625d340582e529ac772be034201b0fe75fb3
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-05-25 13:22:56 +0300

    primary

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require("mongoose");
+
+const DB_HOST = "mongodb+srv://antontsyrkunov:36LZaWDuhlriOuhj@workingcluster.h5ib1ni.mongodb.net/contscts_reader?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true)
+
 const app = require('./app')
 
 const {PORT = 3000} = process.env;
 
-app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(PORT)
+  console.log("Server connected");
 })
+.catch(error =>  {
+  console.log(error.message);
+  process.exit(1);
+})
+
+
