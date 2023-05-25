0a504bd54bfd96c141c1804a5f904eeba3212c6a
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-05-25 23:12:02 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_URL="mongodb+srv://Diana:182fagMFMTZ9Kbfi@diana.qpbl3yu.mongodb.net/?retryWrites=true&w=majority"
+
+
+mongoose.connect(DB_URL)
+.then(()=>{
+  console.log("Database connection successful")
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
+})
+.catch((error)=>{
+  console.log(error.message);
+  process.exit(1);
 })
