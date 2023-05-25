b1beff2d7c831cc66e742b5923c159f299fd3a9d
Volodya Kushch <110915611+Volodya460@users.noreply.github.com>
2023-05-25 17:03:55 +0300

    test

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Volodya:s8VCw26H4e9LczkR@cluster0.vobxqw4.mongodb.net/db-contacts";
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((er) => {
+    console.log(er.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Database connection successful");
+// });
