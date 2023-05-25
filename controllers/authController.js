615bbe319f64397fa4f8922b8be02d6cacb899c4
Havura Artur <havuraa@gmail.com>
2023-05-25 21:34:53 +0300

    fixing some mistakes

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,7 +64,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.json({
+  res.status(204).json({
     message: "Logout success",
   });
 };
