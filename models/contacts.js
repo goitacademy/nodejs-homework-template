fc80190100d4ca3bc4a7d491be87bf68031b75b0
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-05-25 11:43:38 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -29,8 +29,6 @@ const getContactById = async (contactId) => {
 };
 
 const removeContact = async (contactId) => {
-  // const data = await fs.readFile(contactsPath, "utf-8");
-  // const contacts = JSON.parse(data);
   const contacts = await parsedData();
   const newContacts = contacts.filter((contact) => contact.id !== contactId);
   await fs.writeFile(contactsPath, JSON.stringify(newContacts));
@@ -38,8 +36,6 @@ const removeContact = async (contactId) => {
 };
 
 const addContact = async (body) => {
-  // const data = await fs.readFile(contactsPath, "utf-8");
-  // const contacts = JSON.parse(data);
   const contacts = await parsedData();
   const newContact = { id: uuidv4(), ...body };
   contacts.push(newContact);
@@ -48,8 +44,6 @@ const addContact = async (body) => {
 };
 
 const updateContact = async (contactId, body) => {
-  // const data = await fs.readFile(contactsPath, "utf-8");
-  // const contacts = JSON.parse(data);
   const contacts = await parsedData();
   const indexPerson = contacts.findIndex((contact) => contact.id === contactId);
   if (indexPerson === -1) {
