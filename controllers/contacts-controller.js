c7cebd324ad4737f655828c11c9c0221a1a4d327
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-05-25 14:14:34 +0300

    fix status 500

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,4 +1,4 @@
-const contactsService = require('../models/contactsService/contacts');
+const contactsService = require('../models/contactsService/contactsService.js');
 
 const { HttpError } = require('../helpers');
 
