c4fbddc3d92180e1dc497c0ff8b908915f96ebfe
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-05-25 18:18:11 +0300

    Working code singup

--- models/user.js
+++ models/user.js
@@ -37,13 +37,6 @@ const schema = new Schema(
   }
 );
 
-schema.pre("save", async function(next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  this.password = await bcrypt.hash(this.password, 12);
-});
-
 const User = model("user", schema);
 
 module.exports = { User };
