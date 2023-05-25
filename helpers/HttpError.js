ef78124f24e3dfd2c3bc08d27908e4e399a2eea3
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-05-26 00:14:00 +0300

    update error

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,15 +1,8 @@
-const messages = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    403: "Forbidden",
-    404: "Not Found",
-    409: "Conflict"
-}
-
-const HttpError = (status, message = messages[status]) => {
-    const error = new Error(message);
-    error.status = status;
-    return error;
+class HttpError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
 
 module.exports = HttpError;
