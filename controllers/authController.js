8652109c864a5a105d4d28a7f4d1931938c5ee42
Havura Artur <havuraa@gmail.com>
2023-05-26 22:05:01 +0300

    fixing mistake

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,8 +19,10 @@ const register = async (req, res) => {
 
   const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
-    email: newUser.email,
-    subscription: newUser.subscription,
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
   });
 };
 
