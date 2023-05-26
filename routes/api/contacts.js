4c8b31d05d1494bcd6677c6fcc3aace73aa384a4
talka19 <talka19.10@gmail.com>
2023-05-27 02:24:43 +0300

    changing the schema validation logic

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,17 +8,23 @@ const contacts = require('../../models/contacts');
 
 const HttpError = require("../../helpers");
 
-const addSchema = Joi.object({
-  name: Joi.string().required().messages({
-    'any.required': `missing required name field`
-  }),
-  email: Joi.string().required().messages({
-    'any.required': `missing required email field`
-  }),
-  phone: Joi.string().required().messages({
-    'any.required': `missing required phone field`
-  }),
-})
+const addSchema = Joi.object().custom((value, helpers) => {
+  if (Object.keys(value).length === 0) {
+    return helpers.error('object.empty');
+  }
+  
+  const keys = ['name', 'email', 'phone'];
+  const missingKeys = keys.filter(key => !Object.prototype.hasOwnProperty.call(value, key));
+  
+  if (missingKeys.length > 0) {
+    return helpers.error('object.missingKeys', { missingKeys });
+  }
+  
+  return value;
+}).messages({
+  'object.empty': 'missing fields',
+  'object.missingKeys': 'missing required fields: {{#missingKeys}}',
+});
 
 
 router.get('/', async (req, res, next) => {
@@ -75,9 +81,6 @@ router.delete('/:id', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
   try {
-    if (Object.keys(req.body).length === 0) {
-      throw HttpError(400,'missing fields' );
-    }
     const {error} = addSchema.validate(req.body);
     if(error) {
       throw HttpError(400, error.message);
