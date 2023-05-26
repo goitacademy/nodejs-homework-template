83e72f33f151f247d765c4d4492c750ea1bc1910
Oleksii Shevchenko <uzlabini@gmail.com>
2023-05-26 23:52:47 +0200

    Виправлення кода з використанням mangoose-методів

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,90 +1,78 @@
- const Joi = require("joi");
+const contactSchema = require("../models/contacts");
+const mongoose = require("mongoose");
 
-const contactService = require("../models");
-const { HttpError } = require("../helpers");
-
-const contactAddSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email(),
-    phone: Joi.string()
-      .regex(/^\d{3}-\d{2}-\d{2}$/)
-      .required(),
-  });
-
-  const getAllContacts = async (req, res, next) => {
-    try {
-      const result = await contactService.listContacts();
-      res.json(result);
-    } catch (error) {
-      next(error);
-    }
-  };
+const Contact = mongoose.model("Contact", contactSchema);
+const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getContactById = async (req, res, next) => {
-    try {
-      const { contactId } = req.params;
-      const result = await contactService.getContactById(contactId);
-      if (!result) {
-        throw HttpError(404, `Contact with ${contactId} is not found!`);
-      }
-      res.json(result);
-    } catch (error) {
-      next(error);
+  try {
+    const { contactId } = req.params;
+    const contact = await Contact.findById(contactId);
+    if (!contact) {
+      res.status(404).json({ message: `Contact with ID ${contactId} not found` });
+      return;
     }
-  };
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const addContact = async (req, res, next) => {
-    try {
-      const { error } = contactAddSchema.validate(req.body);
-      if (error) {
-        throw HttpError(400, error.message);
-      }
-      const result = await contactService.addContact(req.body);
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
+  try {
+    const { name, email, phone, favorite } = req.body;
+    const contact = new Contact({ name, email, phone, favorite });
+    const savedContact = await contact.save();
+    res.status(201).json(savedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContactById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { name, email, phone, favorite } = req.body;
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { name, email, phone, favorite },
+      { new: true }
+    );
+    if (!updatedContact) {
+      res.status(404).json({ message: `Contact with ID ${contactId} not found` });
+      return;
     }
-  };
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteContactById = async (req, res, next) => {
-    try {
-  const {contactId} = req.params;
-  const result = await contactService.removeContact(contactId);
-  if (!result) {
-    throw HttpError(404, `Contact with ${contactId} is not found!`);
+  try {
+    const { contactId } = req.params;
+    const deletedContact = await Contact.findByIdAndDelete(contactId);
+    if (!deletedContact) {
+      res.status(404).json({ message: `Contact with ID ${contactId} not found` });
+      return;
+    }
+    res.json({ message: "Contact deleted successfully" });
+  } catch (error) {
+    next(error);
   }
-  res.json({
-  message:"Delete success!"
-  })
-    }
-    catch(error) {
-      next (error);
-    }
-  };
+};
 
-  const updateContactById = async (req, res, next) => {
-    try {
-      const { error } = contactAddSchema.validate(req.body);
-      if (error) {
-        throw HttpError(400, error.message);
-      }
-      const { contactId } = req.params;
-      const result = await contactService.updateContactById(contactId, req.body);
-      if (!result) {
-        throw HttpError(404, `Contact with ${contactId} is not found!`);
-      }
-      res.json(result);
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  module.exports ={
-    getAllContacts,
-    getContactById,
-    addContact,
-    deleteContactById,
-    updateContactById
-  };
-  
- 
+module.exports = {
+  getAllContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  deleteContactById,
+};
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-const app = require('./app');
-const {DB_HOST} = process.env;
+const mongoose = require("mongoose");
+const app = require("./app");
+const { DB_HOST, PORT } = process.env;
 
-mongoose.connect(DB_HOST)
-         .then(()=> {
-          app.listen(3000, () => {
-            console.log("Server running. Use our API on port: 3000")
-          })
-         })
-         .catch( error => {
-          console.log(error.message);
-          process.exit(1);
-         })
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
 
