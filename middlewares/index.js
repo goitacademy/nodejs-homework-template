c87324b73f54fd46d90b28dc26dd6f524256e3d2
Ihor Statsenko <statsenkoin@gmail.com>
2023-05-26 23:30:49 +0300

    fix/ update helpers/middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,11 @@
 const validateBody = require('./validateBody');
 const validateFavorite = require('./validateFavorite');
-const handleMongooseError = require('./handleMongooseError');
 const isValidId = require('./isValidId');
+const authenticate = require('./authenticate');
 
 module.exports = {
   validateBody,
   validateFavorite,
-  handleMongooseError,
   isValidId,
+  authenticate,
 };
