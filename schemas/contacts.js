0840ee3ccc83bf20e2425465969fe6f03d1de3ca
IhorC <igorchua@gmail.com>
2023-05-26 23:21:20 +0300

    error messages fixed

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,15 +1,9 @@
 const Joi = require("joi");
 
 const contactUpdateSchema = Joi.object({
-    name: Joi.string().required().messages({
-    //   "string.empty": "missing required name field",
-    }),
-    email: Joi.string().required().messages({
-    //   "string.empty": "missing required email field",
-    }),
-    phone: Joi.string().required().messages({
-    //   "string.empty": "missing required phone field",
-    }),
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
 });
 
 module.exports = contactUpdateSchema
