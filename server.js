a3034a96f6e68e90971ce8ffef1baf65ed5b83ad
TetianaTitarenko <t.titarenko.f@gmail.com>
2023-05-27 18:05:38 +0100

    render

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
 const app = require('./app')
-const {DB_HOST} = require("./config")
+// const {DB_HOST} = require("./config")
+
+const DB_HOST = "mongodb+srv://ttitarenkof:K8izfGUTgBwRLSYN@cluster0.jyqy8k4.mongodb.net/my-contact?retryWrites=true&w=majority";
+
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
