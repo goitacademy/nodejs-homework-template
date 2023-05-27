1d5833d4654dc610685d72fc6b0e0b04f03561df
SedaNoty <sedanoty5@gmail.com>
2023-05-28 02:14:07 +0300

    create - login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const {User} = require("../models/user");
 const {HttpError, ctrlWrapper} = require("../helpers");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {SECRET_KEY} = process.env;
 
 const register = async(req, res) => {
     const {email, password} = req.body;
@@ -20,8 +22,32 @@ const register = async(req, res) => {
     });
 };
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user) {
+        throw HttpError(401, "Email or password is wrong");
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare) {
+        throw HttpError(401, "Email or password is wrong");
+    }
+    
+    const payload = {
+        id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '23h'});
+        
+    res.status(200).json({
+        token,
+        user: {
+          email: user.email,
+          subscription: user.subscription,
+        },
+      });
+}
 
 module.exports = {
     register: ctrlWrapper(register),
-    
+    login: ctrlWrapper(login),
 };
