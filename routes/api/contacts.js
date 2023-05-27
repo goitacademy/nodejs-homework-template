56ab746fad567fc6daa4a8c8fae7e01e0c27b914
Ruslan <shamiltat79@gmail.com>
2023-05-27 03:05:52 +0300

    list

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
+
+const contactsService = require("../../models/contacts");
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = contactsService.listContacts();
+  res.json(result);
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
