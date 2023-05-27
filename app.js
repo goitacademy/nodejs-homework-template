a6391302ebca34d8718186d271b1fbee18142457
Sergii <nezalonov.serhii@gmail.com>
2023-05-27 23:33:17 +0300

    fix: remove console.log

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-   console.log(err);
    const { status = 500, message = "Server error" } = err;
    res.status(status).json({
       message,
