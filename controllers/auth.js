f29e98ce914365e8d8c20f3c4cc3b0225fa30b2a
TetianaTitarenko <t.titarenko.f@gmail.com>
2023-05-27 17:44:49 +0100

    add config

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,19 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const {User} = require("../models/user");
 const {HttpError, ctrlWrapper} = require("../helpers");
 
 const register = async(req, res) => {
-    const {email} = req.body;
+    const {email, password} = req.body;
     const user = await User.findOne({email});
 
     if(user){
         throw new HttpError(409,"Email already in use")
     }
 
-    const newUser = await User.create(req.body)
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const newUser = await User.create({...req.body, password: hashPassword})
 
     res.status(201).json({
         email: newUser.email,
@@ -17,6 +21,29 @@ const register = async(req, res) => {
     })
 };
 
+const login = async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email})
+    if(!user) {
+        throw new HttpError(401, "Email or password invalid")
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare) {
+        throw new HttpError(401, "Email or password invalid")
+    }
+
+    const payload = {
+        id: user._id
+    };
+
+    const token = jwt.sign(payload, {expiresIn: "23h"})
+
+    res.json({ 
+        token,
+    })
+}
+
 module.exports = {
     register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
 }
