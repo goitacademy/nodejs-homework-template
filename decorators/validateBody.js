86d791b4c05b1914ffe4a7757ea4fee4ee7d501d
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-05-28 22:11:25 +0300

    brushed the code

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -4,9 +4,6 @@ const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      // if (Object.entries(req.body).length === 0) {
-      //   next(HttpError(400, `missing fields`));
-      // }
       next(HttpError(400, error.message));
     }
     next();
