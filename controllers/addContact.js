28ecf949da6ea60f88be8c82b3d923c621322954
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-05-28 16:26:01 +0300

    fix add

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -4,16 +4,16 @@
  const addContact = async (req, res, next) => {
     try {
       const { error } = addSchema.validate(req.body);
+      const result = await contacts.addContact(req.body);
+
       if (error) {
         throw res.status(400).json({ message: `missing required ${error.details[0].context.label} field` });
       }
 
-      const result = await contacts.addContact(req.body);
-      if (!result) {
-        throw res.status(404).json({ message: "Not found" });
-      }
-      console.log("Contact added! New lists of contacts");
-      res.status(201).json(result);
+     console.log("Contact added!");
+    res.status(201).json(result);
+ 
+
     } catch (error) {
       next(error);
     }
