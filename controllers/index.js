4f41a325a86f76f7ffc732166d1d5d6505ccbe13
benzotrav <93350704+benzotrav@users.noreply.github.com>
2023-05-29 15:10:43 -0400

    copy repo

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const getContactsList = require('./getContactsList');
+const getOneContact = require('./getOneContact');
+const postContact = require('./postContact');
+const deleteContact = require('./removeContact');
+const putContact = require('./putContact');
+
+module.exports = {
+  getContactsList,
+  getOneContact,
+  postContact,
+  deleteContact,
+  putContact,
+};
