f235bc342357a2024f2631f632dfc237ea6efc8c
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-05-30 00:37:08 +0300

    Unit test login
    attempt to make tests with jest

--- сontrollers/auth/login.js
+++ сontrollers/auth/login.js
@@ -5,15 +5,18 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = process.env;
 const login = async (req, res) => {
+  // if (!req.body) {
+  //   throw HttpError(401, 'Email or password is wrong');
+  // }
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw HttpError(401, 'Email or password is wrong');
+    throw  HttpError(401, 'Email or password is wrong');
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
 
   if (!passwordCompare) {
-    throw HttpError(401, 'Email or password is wrong');
+    throw  HttpError(401, 'Email or password is wrong');
   }
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
