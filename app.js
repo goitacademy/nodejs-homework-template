573120e254dbb309cbbd3b890fa4d57dd52fe371
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-05-30 14:37:49 +0300

    update to /users path in app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/auth', authRouter);
+app.use('/users', authRouter);
 app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
