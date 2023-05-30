a57274ad76a0534dd48e201d752e37fad8c8064a
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-05-30 20:22:57 +0300

    add mongoose logic

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const { PORT, DB_URL } = process.env;
+
+(async () => {
+  await mongoose.connect(DB_URL);
+  console.log("Database connection success");
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+})();
+
+
