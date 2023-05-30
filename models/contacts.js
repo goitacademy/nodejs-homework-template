d35b58421dd3510f0bae094832102ef1ceba1f96
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-30 16:02:15 +0300

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,54 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
-const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+const contactsPath = path.join(__dirname, "contacts.json")
 
-const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath)
+  return JSON.parse(data)}
+
+const getContactById = async(id) => {
+  const contacts = await listContacts();
+  const result  = contacts.find(item => item.id === id)
+  return result || null;
+}
+const removeContact = async (id) => {  
+  const contacts = await listContacts();
+  const index  = contacts.findIndex(item => item.id === id)
+  if (index === -1) {
+      return null;
+  }
+  const [contactById] = contacts.splice(index, 1)
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return contactById}
+
+const addContact = async ({name,email, phone}) => {
+  const contacts = await listContacts();
+    const newContact = {
+        id: uuidv4(),
+        name,
+        email, 
+        phone,
+    }
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+}
+
+const updateContact = async (id, data) => {
+  const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === id);
+    if(index === -1){
+        return null;
+    }
+    contacts[index] = {id, ...data};
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index];
+}
 
 module.exports = {
   listContacts,
@@ -17,3 +57,6 @@ module.exports = {
   addContact,
   updateContact,
 }
+
+  
+ 
