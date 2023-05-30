416f16ce97a57ab414dd7c34ba1d50aae61fb581
Evgeniy Strelbitskiy <evgenstb@gmail.com>
2023-05-30 16:50:30 +0300

    add settings to connect server mongo

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST =
+  "mongodb+srv://evgenstb:nglLosXuqJJzeBqL@contacts.bhadyzz.mongodb.net/db_conacts?retryWrites=true&w=majority";
+
+  mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST).then (()=> {
+  console.log("Connection for DB succesful")
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
+}).catch(error=> {
+  console.log(error.message);
+  process.exit(1)
 })
