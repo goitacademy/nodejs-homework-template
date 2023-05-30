c1f422ce8ff6616c62503ebbd01c8f7902ba6477
OPaslavskiy <bankir.op@gmail.com>
2023-05-30 21:53:48 +0300

    add error 409

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const { ctrlWrapper } = require("../decorators/");
 
 const register = async (req, res) => {
   const newUser = await User.create(req.body);
-  res.json({
+  res.status(201).json({
     email: newUser.email,
   });
 };
