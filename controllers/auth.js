8b8bc23349e11559fc84ef2a31aa36aef4a2f058
OPaslavskiy <bankir.op@gmail.com>
2023-05-30 21:57:43 +0300

    message for dublicate email

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,11 @@ const { HttpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators/");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
   const newUser = await User.create(req.body);
   res.status(201).json({
     email: newUser.email,
