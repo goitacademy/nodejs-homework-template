8bf45b82438a207c416452c6bc23d5506efeb2df
Ruslan <shamiltat79@gmail.com>
2023-05-31 00:37:19 +0300

    routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,15 +3,36 @@ const express = require('express');
 const router = express.Router();
 
 const contactsService = require("../../models/contacts");
+const {HttpError} = require("../../helpers");
 
 router.get('/', async (req, res, next) => {
-  const contacts  = await contactsService.listContacts();
+  try {
+    const contacts  = await contactsService.listContacts();
   res.json({result: contacts});
+  } catch (error) {
+    next(error);
+  }
+  
   
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params;
+  const result = await contactsService.getContactById(contactId);
+  if(!result){
+    
+    throw HttpError(404, `Contact with ${contactId} not found` )
+  }
+  res.json(result);
+  } 
+  catch (error) {
+    next(error);
+  }
+  
+
+
+  
 })
 
 router.post('/', async (req, res, next) => {
