e11d8e4dd2c16621216a55f2399eb4bdf5d96232
Taisiya Tomashevska <taya22011985@gmail.com>
2023-05-31 10:51:46 +0300

    test3

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
 const userSchema = new Schema({
@@ -18,7 +18,7 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
-        match: emailRegexp,
+        // match: emailRegexp,
         required: [true, 'Email is required'],
         unique: true,
       
@@ -38,12 +38,14 @@ userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().pattern(emailRegexp).required(),
+    // email: Joi.string().pattern(emailRegexp).required(),
+    email: Joi.string().required(),
     password: Joi.string().min(6).required()
 });
 
 const loginSchema = Joi.object({
-    email: Joi.string().email(emailRegexp).required(),
+    // email: Joi.string().email(emailRegexp).required(),
+     email: Joi.string().required(),
     password: Joi.string().min(6).required()
 });
 
