024e6aef3c5d25096363119c0087203f4e27d3bd
Ruslan <shamiltat79@gmail.com>
2023-06-01 01:11:25 +0300

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,50 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
-
-const contactsService = require("../../models/contacts");
-const {HttpError} = require("../../helpers");
-
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts  = await contactsService.listContacts();
-  res.json({result: contacts});
-  } catch (error) {
-    next(error);
-  }
-  
-  
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const {contactId} = req.params;
-  const result = await contactsService.getContactById(contactId);
-  if(!result){
-    
-    throw HttpError(404, `Contact with ${contactId} not found` )
-  }
-  res.json(result);
-  } 
-  catch (error) {
-    next(error);
-  }
-  
+const contactsController = require("../../controllers/contacts-controller");
+const schemas = require("../../schemas/contacts-schemas");
+const {validateBody} = require("../../decorators");
 
 
-  
-})
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', contactsController.getContacts);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', contactsController.getContactById)
 
-module.exports = router
+router.post('/', validateBody(schemas.contactsAddSchema), contactsController.addContact);
+
+router.put('/:contactId', validateBody(schemas.contactsAddSchema), contactsController.updateContact);
+
+
+router.delete('/:contactId', contactsController.removeContact);
+
+
+module.exports = router;
