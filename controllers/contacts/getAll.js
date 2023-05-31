edc01274f431b52b744316370425b9fb4631eadd
Taisiya Tomashevska <taya22011985@gmail.com>
2023-05-31 09:43:17 +0300

    ADDED:pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,9 @@ const { ctrlWrapper } = require("../../helpers");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const results = await Contact.find({owner}, "-createdAt -updatedAt");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const results = await Contact.find({ owner }, "-createdAt -updatedAt", {skip, limit}).populate("owner","name email");
   res.json(results);
 };
 
