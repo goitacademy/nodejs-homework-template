bf9d42f0fcb26b5cfb12d2503bc84865dae8215a
Taisiya Tomashevska <taya22011985@gmail.com>
2023-05-31 14:39:07 +0300

    ADDED: filter by favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,12 +3,22 @@ const { ctrlWrapper } = require("../../helpers");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+
+  const { page = 1, limit = 20, favorite } = req.query;
+  // console.log(req.query)
   const skip = (page - 1) * limit;
-  const results = await Contact.find({ owner }, "-createdAt -updatedAt", {skip, limit}).populate("owner","name email");
+  const query = { owner };
+  // console.log(query)
+  if (favorite !== undefined) {
+    query.favorite = favorite === "true";
+  }
+  const results = await Contact.find(query, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email subscription");
   res.json(results);
 };
 
 module.exports = {
-    getAll: ctrlWrapper(getAll),
-}
+  getAll: ctrlWrapper(getAll),
+};
