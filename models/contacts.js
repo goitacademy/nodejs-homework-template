6307f2179ba47c9b753c19b4d7283b57f3d162d0
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-05-31 20:23:52 +0300

    add code with mongoose db

--- models/contacts.js
+++ models/contacts.js
@@ -2,6 +2,8 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
+
+
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
