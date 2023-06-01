ba686fb27c012d4ffaad5692eb1d94a75c5f9775
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-06-01 16:52:19 +0300

    skip last changes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,23 +2,21 @@ const express = require('express');
 
 const contactsController = require("../../controllers/contacts-controllers");
 
-const ctrl = require("../../controllers/contacts");
-
 const { isValidId } = require("../../middleware/isValidId");
 
 const router = express.Router();
 
 
-router.get("/", ctrl.getAllContacts);
+router.get("/", contactsController.getAllContacts);
 
-router.get("/:id",isValidId, ctrl.getContactById);
+router.get("/:id",isValidId, contactsController.getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", contactsController.addContact);
 
-router.delete("/:id", isValidId, ctrl.removeContactById);
+router.delete("/:id", isValidId, contactsController.removeContactById);
 
-router.put("/:id", isValidId, ctrl.updateContactById);
+router.put("/:id", isValidId, contactsController.updateContactById);
 
-router.patch("/:id/favorite", isValidId, ctrl.updateFavorite)
+router.patch("/:id/favorite", isValidId, contactsController.updateFavorite);
 
 module.exports = router;
