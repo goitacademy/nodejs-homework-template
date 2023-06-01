b4e0602ba456f8cc3dcc9a8e72e84149e703a04d
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-06-01 17:34:14 +0300

    add fix to server.js

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const { DB_HOST, PORT = 3000 } = process.env;
-
-mongoose.set('strictQuery', true)
+const {DB_HOST}= process.env;
 
 mongoose.connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log("Database connection successful")
-    })
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });    
+.then(()=> {
+  app.listen(3000)
+  console.log("Database connection successful")
+} )
+.catch(error => {
+  console.log(error.message)
+  process.exit(1)
 
+
+})
 
 
