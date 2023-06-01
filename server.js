30643e9676c55d9f82a42e035049a13781393c05
Kate2610 <yekaterina.anisimova@gmail.com>
2023-06-01 14:40:59 +0300

    new

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST = "mongodb+srv://Kateryna:6252532katya@cluster0.d0lof7f.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+    })
+  
