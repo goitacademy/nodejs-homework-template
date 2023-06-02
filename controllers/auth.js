0944c91aa0e2f4846799e63acd5d55150e9a7979
Fotyniya <110336005+Fotyniya@users.noreply.github.com>
2023-06-02 13:48:36 +0300

    Update auth.js
    Logout success response

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,9 +64,7 @@ const logout = async (req, res) => {
     const {_id} = req.user;
     await User.findByIdAndUpdate( _id, {token: ""});
 
-    res.json(
-        "status: 204, No content"
-    )
+    res.status(204).send("No content")
 };
 
 const updateSubscriptionUser = async (req, res) => {
