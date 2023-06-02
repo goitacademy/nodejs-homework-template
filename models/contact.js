8511059fb443600c953422c1d97912d90445defe
userzahar <110730909+userzahar@users.noreply.github.com>
2023-06-02 21:49:46 +0300

    add heandlenongooserror

--- models/contact.js
+++ models/contact.js
@@ -1,11 +1,24 @@
 const {Schema,model}=require("mongoose");
+const Joi = require("joi");
 
+const {heandleMongoosError}=require("../helpers")
 const contactSchema = new Schema({
     name:{type:String, required:true},
     email:{type:String, required:true},
     phone:{type:String, required:true}
-});
+},{versionKey:false,timestamps:true});
+// default - дефолтне значення
+// enum:[] значення одне із
+// match: /\{2}-\{2}-\4$/ щоб записати дату 
+contactSchema.post("save",heandleMongoosError);
+
+const contactPush = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+  const schemas={contactPush};
 
 const Contact = model("contact", contactSchema);
 
-module.exports=Contact;
+module.exports={Contact,schemas};
