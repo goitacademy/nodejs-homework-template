555b17aa6ca14577cf53226abc2b053b1d79e8b4
userzahar <110730909+userzahar@users.noreply.github.com>
2023-06-03 22:55:34 +0300

    remove console.log

--- helpers/ctrlWrapper.js
+++ helpers/ctrlWrapper.js
@@ -1,7 +1,6 @@
 const ctrlWrapper = (ctrl) => {
   const func = async (req, res, next) => {
     try {
-      console.log("🥨", req);
       await ctrl(req, res, next);
     } catch (error) {
       next(error);
