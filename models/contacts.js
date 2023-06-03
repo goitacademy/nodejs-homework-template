2eadce2713bb2f76aa41dbb3d3374997c40a5017
Dmytro Nahornyi <d3049999@gmail.com>
2023-06-03 12:55:18 +0300

    update hw

---
+++ models/contacts.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const fs = require("fs/promises");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const allContacts = await listContacts();
+    const result = allContacts.find((contact) => contact.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex((contact) => contact.id === contactId);
+
+    if (index === -1) {
+      return null;
+    }
+
+    const [result] = allContacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async ({ name, email, phone }) => {
+  try {
+    const allContacts = await listContacts();
+    const { nanoid } = await import("nanoid");
+    const newContact = { id: nanoid(), name, email, phone };
+
+    allContacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+
+    allContacts[index] = { id: contactId, ...body };
+
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+    return allContacts[index];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
---
+++ utils/index.js
@@ -0,0 +1,7 @@
+const HttpError = require("./HttpError");
+const { ctrlWrapper } = require("../utils/ctrlWrapper");
+
+module.exports = {
+  HttpError,
+  ctrlWrapper,
+};
