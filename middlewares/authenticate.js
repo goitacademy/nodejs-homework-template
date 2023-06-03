268b2a982ee356a7075d9ef5d8f1ddd917b0f1a1
OPaslavskiy <bankir.op@gmail.com>
2023-06-03 11:39:11 +0300

    add msg error

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,20 +4,22 @@ const { User } = require("../models/user");
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
+  console.log("req.headers", req.headers);
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
+  console.log("token", token);
   if (bearer !== "Bearer") {
-    next(HttpError(401));
+    next(HttpError(401, "Invalid"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user) {
-      next(HttpError(401));
+      next(HttpError(401, "Invalid"));
     }
     next();
   } catch {
-    next(HttpError(401));
+    next(HttpError(401, "Invalid"));
   }
 };
 
