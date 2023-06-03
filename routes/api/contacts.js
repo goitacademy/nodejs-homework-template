e81229c198d18dd579aa1d4fcff25e3c4c7636df
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-06-03 15:19:45 +0300

    fix ctrls import

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,22 @@
 const express = require('express');
 
-const ctrl = require("../../controllers/contacts-ctrls");
+const {getAllContacts, getContactById, addContact, removeContactById, updateContactById, updateFavorite} = require("../../controllers/contacts-ctrls");
 
 const isValidId  = require("../../middleware");
 
 const router = express.Router();
 
 
-router.get("/", ctrl.getAllContacts);
+router.get("/", getAllContacts);
 
-router.get("/:id",isValidId, ctrl.getContactById);
+router.get("/:id",isValidId, getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", addContact);
 
-router.delete("/:id", isValidId, ctrl.removeContactById);
+router.delete("/:id", isValidId, removeContactById);
 
-router.put("/:id", isValidId, ctrl.updateContactById);
+router.put("/:id", isValidId, updateContactById);
 
-router.patch("/:id/favorite", isValidId, ctrl.updateFavorite);
+router.patch("/:id/favorite", isValidId, updateFavorite);
 
 module.exports = router;
