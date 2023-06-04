9b1b3b3d57c3045af3c4453b2756a58dade3eca1
VitaliyYakovenko <yakovenkovitaly1@gmail.com>
2023-06-04 10:49:00 +0300

    add controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,124 +1,149 @@
 const express = require('express');
-const Contact = require("../../models/contact");
-const utils = require("../../utils/index");
+const controllers = require("../../controllers/controllers");
 const { isValidId } = require("../../middlewares/index");
 
-const router = express.Router()
+const router = express.Router();
 
 
-router.get('/', async (req, res, next) => {
-  try {
-     const result = await Contact.find();
-    
-    if (!result) {
-        throw utils.HttpError(404, "Not Found");  
-    }
-    
-     res.json(result); 
-    }
-   catch (error) {
-      next(error)
-    }  
-})
+router.get('/', controllers.getAll);
 
+router.get('/:id', isValidId, controllers.getContactById);
 
+router.post('/', controllers.postContact);
 
-router.get('/:id', isValidId, async (req, res, next) => { 
-  try {
-     const {id} = req.params
-     const result = await Contact.findById(id);
+router.delete('/:id', isValidId, controllers.deleteContact);
 
-     if (!result) {
-       throw utils.HttpError(404, "Not Found");
-     }
+router.put('/:id', isValidId, controllers.putContact);
 
-     res.json(result);
-   } 
-    catch (error) {
-    next(error)
-  }
-})
-
-
-
-router.post('/', async (req, res, next) => {
-
-     try {
-       const { error } = utils.contactSchemaJoi.validate(req.body);
-       
-       if (error) {
-         throw utils.HttpError(400, "missing required name field");
-       }
-
-      const body = req.body;
-      Contact.create(body);     
-      res.status(201).json(body);
-     } catch (error) {
-         next(error)
-      }
-})
-
-
-
-router.delete('/:id' , isValidId , async (req, res, next) => {
-  try {
-       const { id } = req.params;
-       const result = await Contact.findByIdAndDelete(id);
-    
-       if (!result) {
-         throw utils.HttpError(404, "Not Found");
-       }
-    
-       res.json({message: "contact deleted"});
-
-     } catch (error) {
-         next(error)
-      }
-})
-
-
-
-router.put('/:id', isValidId, async (req, res, next) => {
-  try {
-    const { error } = utils.contactSchemaJoi.validate(req.body);
-    if (error) {
-      throw utils.HttpError(400, "missing fields");
-    }
-    const { id } = req.params;
-    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
-    
-    if (!result) {
-      throw utils.HttpError(404, "Not Found");
-    }
- 
-    res.json(result);
-      
-  } catch (error) {
-    next(error)
-  }
-});
-
-
-router.patch("/:id/favorite", isValidId, async (req, res, next) => {
-   try {
-    const { error } = utils.contactUpdateFavorite.validate(req.body);
-    if (error) {
-      throw utils.HttpError(400, "missing field favorite");
-    }
-    const { id } = req.params;
-    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
-    
-    if (!result) {
-      throw utils.HttpError(404, "Not Found");
-    }
-    res.json(result);
-      
-  } catch (error) {
-    next(error)
-  }
-  
-
-});
-
+router.patch("/:id/favorite", isValidId, controllers.patchContact);
 
 module.exports = router
+
+
+
+
+
+// const express = require('express');
+// const Contact = require("../../models/contact");
+// const utils = require("../../utils/index");
+// const { isValidId } = require("../../middlewares/index");
+
+// const router = express.Router()
+
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//      const result = await Contact.find();
+    
+//     if (!result) {
+//         throw utils.HttpError(404, "Not Found");  
+//     }
+    
+//      res.json(result); 
+//     }
+//    catch (error) {
+//       next(error)
+//     }  
+// })
+
+
+
+// router.get('/:id', isValidId, async (req, res, next) => { 
+//   try {
+//      const {id} = req.params
+//      const result = await Contact.findById(id);
+
+//      if (!result) {
+//        throw utils.HttpError(404, "Not Found");
+//      }
+
+//      res.json(result);
+//    } 
+//     catch (error) {
+//     next(error)
+//   }
+// })
+
+
+
+// router.post('/', async (req, res, next) => {
+
+//      try {
+//        const { error } = utils.contactSchemaJoi.validate(req.body);
+       
+//        if (error) {
+//          throw utils.HttpError(400, "missing required name field");
+//        }
+
+//       const body = req.body;
+//       Contact.create(body);     
+//       res.status(201).json(body);
+//      } catch (error) {
+//          next(error)
+//       }
+// })
+
+
+
+// router.delete('/:id' , isValidId , async (req, res, next) => {
+//   try {
+//        const { id } = req.params;
+//        const result = await Contact.findByIdAndDelete(id);
+    
+//        if (!result) {
+//          throw utils.HttpError(404, "Not Found");
+//        }
+    
+//        res.json({message: "contact deleted"});
+
+//      } catch (error) {
+//          next(error)
+//       }
+// })
+
+
+
+// router.put('/:id', isValidId, async (req, res, next) => {
+//   try {
+//     const { error } = utils.contactSchemaJoi.validate(req.body);
+//     if (error) {
+//       throw utils.HttpError(400, "missing fields");
+//     }
+//     const { id } = req.params;
+//     const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    
+//     if (!result) {
+//       throw utils.HttpError(404, "Not Found");
+//     }
+ 
+//     res.json(result);
+      
+//   } catch (error) {
+//     next(error)
+//   }
+// });
+
+
+// router.patch("/:id/favorite", isValidId, async (req, res, next) => {
+//    try {
+//     const { error } = utils.contactUpdateFavorite.validate(req.body);
+//     if (error) {
+//       throw utils.HttpError(400, "missing field favorite");
+//     }
+//     const { id } = req.params;
+//     const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    
+//     if (!result) {
+//       throw utils.HttpError(404, "Not Found");
+//     }
+//     res.json(result);
+      
+//   } catch (error) {
+//     next(error)
+//   }
+  
+
+// });
+
+
+// module.exports = router
