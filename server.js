671ba463643f91f1ba3f529b7444f1fc7574d441
NVZ1994 <nikita.zhuk.1994@gmail.com>
2023-06-05 16:06:50 +0300

    initiall commit

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Nikita:4SJCAKecoUG0CbYF@cluster0.7evflcw.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+const PORT = 3000;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
