55af5889a9d36dd7eb44ee1d2346ae7700ba97b6
Volodymyr <nebesnyivr@gmail.com>
2023-06-05 20:55:56 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Volodymyr:ZcnZ7sZ2zRxbvWgq@cluster0.9dazmdf.mongodb.net/my-contacts?retryWrites=true&w=majority";
+// ZcnZ7sZ2zRxbvWgq
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
