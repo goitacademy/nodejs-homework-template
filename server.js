ee0421b0a3e3e105269c89a96faaee5295c980ee
Vastasika <kykysia13@gmail.com>
2023-06-06 01:36:41 +0300

    start

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST = "mongodb+srv://Vastasika:QXVyX9gSJB41waJB@cluster0.j6my8ps.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
 })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+})
+
+
