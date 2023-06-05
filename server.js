f5f6e7bba1042a70252294c15cc9e84d03670c6e
Liudmyla <103444345+Liudmyla-ukraineif@users.noreply.github.com>
2023-06-05 14:16:11 +0300

    create connect mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Liudmyla:svcIhjzUu76Aobuk@cluster0.mxdcgdh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+// , () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
