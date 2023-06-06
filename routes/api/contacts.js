16203f46009f88745e94c0ba690471358d61daf8
Sergii <nezalonov.serhii@gmail.com>
2023-06-06 03:17:40 +0300

    added: middlewares to validateId

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,20 +9,23 @@ const {
 } = require("../../controllers/contacts");
 const { validateContactsBody } = require("../../middlewares");
 const { schemas } = require("../../models/contacts");
+const isValidId = require("../../middlewares/validateId");
 
 const router = express.Router();
 
 router.get("/", getAll);
-router.get("/:contactId", getContactById);
+router.get("/:contactId", isValidId, getContactById);
 router.post("/", validateContactsBody(schemas.contactsAddSchema), addContact);
-router.delete("/:contactId", removeContact);
+router.delete("/:contactId", isValidId, removeContact);
 router.put(
    "/:contactId",
+   isValidId,
    validateContactsBody(schemas.contactsAddSchema),
    updateContact
 );
-router.put(
+router.patch(
    "/:contactId/favorite",
+   isValidId,
    validateContactsBody(schemas.updateFavoriteSchema),
    updateStatusContact
 );
