de003991d36680c31f6498d255890fa3f749cf0f
Tetiana2023 <drofaj18@gmail.com>
2023-06-06 23:43:19 +0300

    add contacts.json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const {DB_HOST} = require("./config")
+const {DB_HOST} = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
