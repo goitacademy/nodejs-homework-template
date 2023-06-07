19a37817814b4e0f5a6ba186243e1af4e1091f2d
MaximKarlov <predator09071987@gmail.com>
2023-06-07 11:51:54 +0300

    start HW3

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const app = require('./app')
+const app = require('./app');
+
+//Predator
+//HZUQeubDmSn07G2I
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log('Server running. Use our API on port: 3000');
+});
