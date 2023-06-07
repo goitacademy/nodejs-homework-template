c0ffa35938972472dcb4b276105a32404dd5d0eb
MaximKarlov <predator09071987@gmail.com>
2023-06-07 13:10:31 +0300

    init HW3

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(
-    app.listen(3000, () => {
-      console.log('Server running. Use our API on port: 3000');
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     })
   )
   .catch(error => {
