4174c0bc5819e008d07b1fb9eaeb288cefdacd7b
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-07 14:51:02 +0300

    Create validation.js

---
+++ middlewares/validation.js
@@ -0,0 +1,16 @@
+const { HttpError } = require("../helpers");
+
+const validation = (schema) => (req, res, next) => {
+  const { body } = req;
+
+  const { error } = schema.validate(body);
+
+  if (error) {
+    const err = new HttpError(400, error.message);
+    next(err);
+  }
+
+  next();
+};
+
+module.exports = validation;
