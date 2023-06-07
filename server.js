ab08aba35265aaffba87218bac7ce78cb654b44a
MaximKarlov <predator09071987@gmail.com>
2023-06-07 12:34:47 +0300

    12345

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
-//Predator
-//HZUQeubDmSn07G2I
+const DB_HOST =
+  'mongodb+srv://Predator:HZUQeubDmSn07G2I@cluster0.q4kcwbt.mongodb.net/my-contacts?retryWrites=true&w=majority';
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+mongoose
+  .connect(DB_HOST)
+  .then(
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    })
+  )
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
