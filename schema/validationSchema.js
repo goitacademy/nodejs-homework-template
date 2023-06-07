ae08af8ad9e0d44cbfcad4ae2fb044622c5c934f
Andrii Valenko <andreyvalenko@gmail.com>
2023-06-07 18:42:02 +0300

    edits validation logic

--- schema/validationSchema.js
+++ schema/validationSchema.js
@@ -1,15 +1,20 @@
 const Joi = require("joi");
 
 const addContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().min(7).max(15).required(),
 });
 
 const editContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string().min(7).max(15),
 }).xor("name", "email", "phone");
 
 module.exports = { addContactSchema, editContactSchema };
