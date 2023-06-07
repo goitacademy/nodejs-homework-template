1a1d42708dc135c6a87d60b106da5ad002aa2ab4
MaxF1996 <maxeffect.apollo@gmail.com>
2023-06-07 14:42:48 +0300

    files structure

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const contactsControllers = require('../../models/contactsControllers');
+const contactsControllers = require('../../controllers/contactsControllers');
 
 router.get('/', contactsControllers.getAllContacts);
 
