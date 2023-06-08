0ca70c8e676fe9864eaf23054cd4f878af22e4ed
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-06-08 10:52:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use("/api/contacts", contactsRouter);
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
 });
-// * err - параметр, який передається з попереднього next() (при умові, що є всі 4 параметри). 1 - це те що передається з попер. next(), 2 - запит, 3 - відповідь, 4 - некст()
+
 app.use((err, _, res, next) => {
   const { status = 500 } = err;
   res.status(status).json({ message: err.message });
