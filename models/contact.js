d9e433c6d2334def68e8b612e8626b1fce241f3a
MaximKarlov <predator09071987@gmail.com>
2023-06-08 15:47:32 +0300

    fix error on __v

--- models/contact.js
+++ models/contact.js
@@ -1,23 +1,26 @@
 const { Schema, model } = require('mongoose');
 
-const ContactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const ContactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Set email for contact'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Set phone for contact'],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Set email for contact'],
-  },
-  phone: {
-    type: String,
-    required: [true, 'Set phone for contact'],
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false }
+);
 
 ContactSchema.post('save', (error, data, next) => {
   error.status = 400;
