07f1f096d95dfb081d0a06b268e1b3e853f7adda
dire <example@gmail.com>
2023-06-10 01:01:31 +0300

    final hw3

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,12 +1,12 @@
 const { isValidObjectId } = require("mongoose");
 
-// const { HttpError } = require("../helpers");
+const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
    const { contactId } = req.params;
    if (!isValidObjectId(contactId)) {
-	return res.status(404).json({ message: `${contactId} invalid id format`});
-	// next(HttpError(404, `${contactId} invalid id format`));
+	// return res.status(404).json({ message: `${contactId} invalid id format`});
+	next(HttpError(404, `${contactId} invalid id format`));
    }
    next();
 };
