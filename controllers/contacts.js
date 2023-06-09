e5e971a2443dcc980869d247f0d682ee7b12dd9c
VerbitskaAlisa <v.alica@ukr.net>
2023-06-09 23:50:37 +0300

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,17 +1,16 @@
-const {ctrlWrapper} = require("../helpers")
+const {ctrlWrapper, HttpError} = require("../helpers");
 
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
-const HttpError = require("../helpers");
 
 const getAll = async (req, res, next) => {
-      const result = await contacts.listContacts();
+      const result = await Contact.find();
       res.status(200).json(result)
   }
 
 const getById = async (req, res, next) => {
     const {contactId} = req.params;
-    const result = await contacts.getContactById(contactId);
+    const result = await Contact.findById(contactId);
     if(!result) {
       throw HttpError(404, "Not found");
     }
@@ -19,22 +18,31 @@ const getById = async (req, res, next) => {
 }  
 
 const add = async (req, res, next) => {
-    const result = await contacts.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
 };
 
 const updateById = async (req, res, next) => {
     const {contactId} = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
     if(!result) {
       throw HttpError(404, "Not found");
     }
     res.status(200).json(result);
 }
 
+const updateFavorite = async (req, res, next) => {
+  const {contactId} = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+  if(!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+}
+
 const deleteById = async (req, res, next) => {
     const {contactId} = req.params;
-    const result = await contacts.removeContact(contactId);
+    const result = await Contact.findByIdAndRemove(contactId);
     if(!result) {
       throw HttpError(404, "Not found");
     }
@@ -48,5 +56,6 @@ const deleteById = async (req, res, next) => {
     getById: ctrlWrapper(getById),
     add: ctrlWrapper(add),
     updateById: ctrlWrapper(updateById),
+    updateFavorite: ctrlWrapper(updateFavorite),
     deleteById: ctrlWrapper(deleteById),
   }
