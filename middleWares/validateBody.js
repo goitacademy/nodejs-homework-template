32fd90a997fffe71a6a6e7e5b5428e0070ad2d52
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-06-09 18:40:01 +0300

    update

--- middleWares/validateBody.js
+++ middleWares/validateBody.js
@@ -5,7 +5,7 @@ const validateBody = (schema) => {
     const { error } = schema.validate(req.body);
 
     if (error) {
-      next(RequestError(400, `missing fields: ${error.message}`));
+      next(RequestError(400, error.message));
     }
     next();
   };
