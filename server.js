b0a4f2eb318d9d6f0ca6336294e802e248b18817
dire <example@gmail.com>
2023-06-10 01:02:07 +0300

    final hw3

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-// const DB_HOST = "mongodb+srv://root:Copy582491@cluster0.lkgpudy.mongodb.net/db-contacts?retryWrites=true&w=majority"
-// const app = require('./app')
-
-
-// mongoose.connect(DB_HOST).then(()=>{
-// 	app.listen(3000);
-// 	console.log("Database connection successful");
-// }).catch(error => {
-// 	console.log(error.message);
-// 	process.exit(1);
-// }) 
-
-
-
 const app = require("./app");
 const mongoose = require("mongoose");
 
