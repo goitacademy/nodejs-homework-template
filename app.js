74581bc3d2ba63705cf18aa44dcff3029297f369
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-06-10 18:50:02 +0300

    update schema, app and request err

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const RequestError = require("./helpers/RequestError");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -19,8 +19,12 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json(message);
+  if (err instanceof RequestError) {
+    res.status(err.status).json({ message: err.message });
+  } else {
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message });
+  }
 });
 
 module.exports = app;
--- controllers/contact.js
+++ controllers/contact.js
@@ -11,7 +11,7 @@ const getById = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contacts.getContactById(contactId);
   if (!result) {
-    throw RequestError(404, "Not found");
+    throw new RequestError(404, "Not found");
   }
   res.json(result);
 };
@@ -24,7 +24,7 @@ const update = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
   if (!result) {
-    throw RequestError(404, "Not found");
+    throw new RequestError(404, "Not found");
   }
   res.json(result);
 };
@@ -33,7 +33,7 @@ const deleteById = async (req, res) => {
   const { contactId } = req.params;
   const result = await contacts.removeContact(contactId);
   if (!result) {
-    throw RequestError(404, "Not found");
+    throw new RequestError(404, "Not found");
   }
 
   res.json({
