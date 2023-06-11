a49f3424315f80d9794aa40a93c993a60c09e065
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-06-12 00:05:34 +0300

    add token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,5 +7,6 @@ const { schemas } = require("../../models/user");
 const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
