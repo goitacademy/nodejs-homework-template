ed4c5db04240d586cd53c6fce00df5f25a256f2c
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-06-11 18:13:12 +0300

    day2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,20 @@
 const User = require('../models/user');
+// const { HttpError } = require('../helpers');
+// const ctrlWrapper = require('../middlewares/ctrlWrapper');
 
 const register = async (req, res, next) => {
   try {
+     const { email } = req.body;
+      const user = await User.findOne({ email });
+       if (user) {
+  
+        throw res.status(409).json({message: "Email in use"})
+       }
     const newUser = await User.create(req.body);
     res.status(201).json({ email: newUser.email, name: newUser.name });
   } catch (error) {
     next(error);
   }
-};
 
+};
 module.exports = register;
--- models/user.js
+++ models/user.js
@@ -1,25 +1,32 @@
 const { Schema, model } = require('mongoose');
+const { HttpError } = require('../helpers');
 
-const contactSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ['starter', 'pro', 'business'],
+      default: 'starter',
+    },
+    token: {
+      type: String,
+      default: null,
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ['starter', 'pro', 'business'],
-    default: 'starter',
-  },
-  token: {
-    type: String,
-    default: null,
-  },
-});
-const User = model('user', contactSchema);
+  { versionKey: false }
+);
+
+userSchema.post('save', HttpError);
+
+const User = model('user', userSchema);
 
 module.exports = User;
