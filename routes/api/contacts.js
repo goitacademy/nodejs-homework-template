1bd41f48c026b3e8d519e6dbdcc66b22b71495d5
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2023-06-11 17:10:05 +0300

    creayeNewFiles

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,136 +1,73 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const Joi = require('joi');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require('../../models/contacts');
+const { validateContact } = require('../../Helper/helper');
 
-// Helper function to read contacts from JSON file
-function readContacts() {
-  const contactsPath = path.join(__dirname, '../../contacts.json');
-  const contactsData = fs.readFileSync(contactsPath, 'utf8');
-  return JSON.parse(contactsData);
-}
-
-// Helper function to write contacts to JSON file
-function writeContacts(contacts) {
-  const contactsPath = path.join(__dirname, '../../contacts.json');
-  fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
-}
-
-// Validation schema for contact data
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
-
-// GET /api/contacts
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const contacts = readContacts();
+    const contacts = await listContacts();
     res.json(contacts);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 });
 
-// GET /api/contacts/:id
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const { id } = req.params;
-    const contacts = readContacts();
-    const contact = contacts.find((c) => c.id === id);
-
-    if (!contact) {
-      return res.status(404).json({ message: 'Not found' });
+    const contact = await getContactById(id);
+    if (contact) {
+      res.json(contact);
+    } else {
+      res.status(404).json({ message: 'Not found' });
     }
-
-    res.json(contact);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 });
 
-// POST /api/contacts
-router.post('/', (req, res, next) => {
-  try {
+router.post('/', validateContact, async (req, res, next) => {
     const { name, email, phone } = req.body;
-
-    const { error } = contactSchema.validate({ name, email, phone });
-
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
+    try {
+      const contact = await addContact({ name, email, phone });
+      res.status(201).json(contact);
+    } catch (err) {
+      next(err);
     }
+  });
 
-    const newContact = {
-      id: uuidv4(),
-      name,
-      email,
-      phone,
-    };
-
-    const contacts = readContacts();
-    contacts.push(newContact);
-    writeContacts(contacts);
-
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const result = await removeContact(id);
+    if (result) {
+      res.json({ message: 'Contact deleted' });
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
-// DELETE /api/contacts/:id
-router.delete('/:id', (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
   try {
-    const { id } = req.params;
-    const contacts = readContacts();
-    const index = contacts.findIndex((c) => c.id === id);
-
-    if (index === -1) {
-      return res.status(404).json({ message: 'Not found' });
+    const contact = await updateContact(id, { name, email, phone });
+    if (contact) {
+      res.json(contact);
+    } else {
+      res.status(404).json({ message: 'Not found' });
     }
-
-    const deletedContact = contacts.splice(index, 1)[0];
-    writeContacts(contacts);
-
-    res.json({ message: 'Contact deleted' });
-  } catch (error) {
-    next(error);
-  }
-});
-
-// PUT /api/contacts/:id
-router.put('/:id', (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, email, phone } = req.body;
-
-    if (!name && !email && !phone) {
-      return res.status(400).json({ message: 'Missing fields' });
-    }
-
-    const { error } = contactSchema.validate({ name, email, phone });
-
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-
-    const contacts = readContacts();
-    const index = contacts.findIndex((c) => c.id === id);
-
-    if (index === -1) {
-      return res.status(404).json({ message: 'Not found' });
-    }
-
-    contacts[index].name = name || contacts[index].name;
-    contacts[index].email = email || contacts[index].email;
-    contacts[index].phone = phone || contacts[index].phone;
-    writeContacts(contacts);
-
-    res.json(contacts[index]);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 });
 
