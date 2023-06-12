55edcd1f16d22cfcd8fd73d46631bafecf2b27d8
KsenyaKoss <roxenn34@gmail.com>
2023-06-12 10:08:30 +0300

    FIXED: validation schema1

---
+++ decorators/validateData.js
@@ -0,0 +1,17 @@
+const { HttpError } = require("../helpers");
+
+const validateData = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (Object.keys(req.body).length === 0 || !req.body) {
+      next(HttpError(400, "missing fields"));
+    } else if (error) {
+      next(HttpError(400, error.message));
+    } else {
+      next();
+    }
+  };
+  return func;
+};
+
+module.exports = validateData;
