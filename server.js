9573ddfe43c4aa26e3e156b8aa13ba9e542ca2bb
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-06-12 21:33:48 +0300

    add db host, i mongoose

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Orest:aiDPlR8gTYtrAjVk@cluster0.mfbgn01.mongodb.net/contacts-reader";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
