717aa97e0210c95c867c02a52d8beb5f728ae7da
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-12 12:54:57 +0300

    Update ctrlWrapper.js

--- helpers/ctrlWrapper.js
+++ helpers/ctrlWrapper.js
@@ -1,11 +1,12 @@
 const ctrlWrapper = (ctrl) => {
-  return async (req, res, next) => {
+  const func = async (req, res, next) => {
     try {
       await ctrl(req, res, next);
     } catch (error) {
       next(error);
     }
   };
+  return func;
 };
 
 module.exports = ctrlWrapper;
