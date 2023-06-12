4eebd6390c82d84aa132c7f1312351b3496f2dae
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2023-06-12 21:29:34 +0200

    add message error

--- middlewars/validateBody.js
+++ middlewars/validateBody.js
@@ -1,10 +1,23 @@
 const validBody = (schema) => {
   const func = (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      next(
+    if (!req.body.name) {
+      next( 
         res.status(400).json({
-          message: "missing " + error.details[0].message + " field",
+          message: "missing required name field",
+        })
+      );
+    }
+    if (!req.body.email) {
+      next( 
+        res.status(400).json({
+          message: "missing required email field",
+        })
+      );
+    }
+    if (!req.body.phone) {
+      next( 
+        res.status(400).json({
+          message: "missing required phone field",
         })
       );
     }
@@ -18,6 +31,27 @@ const validEmptyBody = () => {
     const bodyEmpty = Object.keys(req.body).length === 0;
     if (bodyEmpty) {
       next(res.status(400).json({ message: "missing fields" }));
+    }
+    if (!req.body.name) {
+      next( 
+        res.status(400).json({
+          message: "missing required name field",
+        })
+      );
+    }
+    if (!req.body.email) {
+      next( 
+        res.status(400).json({
+          message: "missing required email field",
+        })
+      );
+    }
+    if (!req.body.phone) {
+      next( 
+        res.status(400).json({
+          message: "missing required phone field",
+        })
+      );
     }
     next();
   };
