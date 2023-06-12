c7eef0cb825d5dcb49e4a7732ddd331051dfed17
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-06-12 23:04:23 +0300

    fix Виправлено зауваження

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,9 +2,12 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      throw HttpError(400, "missing fields");
+    }
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      next(HttpError(400, "missing required name field"));
     }
     next();
   };
