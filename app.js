1807a43f18350f2ad92a96071a5016cffcf1f47e
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-12 15:06:28 -0400

    updated

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+const DB_HOST =
+  "mongodb+srv://Yuliia:YArRBmUXVIfvh7ru@cluster0.h8gga61.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
 
 app.use(logger(formatsLogger));
 app.use(cors());
