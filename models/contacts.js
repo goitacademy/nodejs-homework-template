f073bac8cf00e546798afc47b3973c10d7c2b6a5
Artem Shnurko <shnurko.artyom@gmail.com>
2023-06-12 21:16:24 +0200

    hw-2 done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,59 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  const contactsParse = JSON.parse(contacts);
+  return contactsParse;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = await contacts.find(
+    (contact) => Number(contact.id) === Number(contactId)
+  );
+  return contact;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = await contacts.findIndex(
+    (contact) => Number(contact.id) === Number(contactId)
+  );
 
-const updateContact = async (contactId, body) => {}
+  if (index === -1) {
+    return null;
+  }
+  const result = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+  return result;
+};
+
+const addContact = async (body) => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  const newContact = {
+    id: Math.random().toString(),
+    ...body,
+  };
+  const newContacts = [...JSON.parse(contacts), newContact];
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts), "utf-8");
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = await contacts.findIndex(
+    (contact) => Number(contact.id) === Number(contactId)
+  );
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id: contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +61,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
