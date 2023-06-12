2f7aae9f7f073863a79cc8bf159e0804819edd1f
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-06-13 01:49:02 +0300

    fix list

--- controllers/contactList.js
+++ controllers/contactList.js
@@ -1,10 +1,20 @@
 const {Contact} = require('../models/contact');
 
+// const contactList = async (req, res, next) => {
+//   try {
+//     const { _id: owner } = req.user;
+
+//     const result = await Contact.find({ owner });
+//     res.status(200).json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 const contactList = async (req, res, next) => {
   try {
-    const { _id: owner } = req.user;
+    const { _id} = req.user;
 
-    const result = await Contact.find({ owner });
+    const result = await Contact.find({ owner: _id });
     res.status(200).json(result);
   } catch (error) {
     next(error);
