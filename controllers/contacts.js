d572848f7addbd03b071ad407ccc3ee2a3e0dc98
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-12 12:54:55 +0300

    Create contacts.js

---
+++ controllers/contacts.js
@@ -0,0 +1,80 @@
+const { HttpError, ctrlWrapper } = require("../helpers");
+const { Contact } = require("../models/contact");
+
+const getContactsAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  const filter = { owner };
+
+  if (favorite) {
+    filter.favorite = favorite;
+  }
+
+  const result = await Contact.find(filter, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
+
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+
+  if (!result) {
+    throw new HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+
+  res.status(201).json(result);
+};
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+
+  if (!result) {
+    throw new HttpError(404, "Not found");
+  }
+
+  res.status(200).json({ message: "contact deleted" });
+};
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw new HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+const updateFavorite = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    throw new HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+module.exports = {
+  getContactsAll: ctrlWrapper(getContactsAll),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateFavorite: ctrlWrapper(updateFavorite),
+};
