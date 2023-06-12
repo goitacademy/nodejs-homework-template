d8ca034bf1b2dc47c453f523cc042225585e2038
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-12 15:21:12 -0400

    init project

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
