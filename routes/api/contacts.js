00fab746548b0e9eb2fd0c97238aac9c9c00154e
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2023-06-12 07:00:16 +0300

    donePOST

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,74 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require('../../models/contacts');
-const { validateContact } = require('../../Helper/helper');
+  getAllContacts,
+  getContact,
+  createContact,
+  deleteContact,
+  updateContactById,
+} = require('../../controllers/contactsController');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.json(contacts);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const contact = await getContactById(id);
-    if (contact) {
-      res.json(contact);
-    } else {
-      res.status(404).json({ message: 'Not found' });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/', validateContact, async (req, res, next) => {
-    const { name, email, phone } = req.body;
-    try {
-      const contact = await addContact({ name, email, phone });
-      res.status(201).json(contact);
-    } catch (err) {
-      next(err);
-    }
-  });
-
-router.delete('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const result = await removeContact(id);
-    if (result) {
-      res.json({ message: 'Contact deleted' });
-    } else {
-      res.status(404).json({ message: 'Not found' });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  const { name, email, phone } = req.body;
-  try {
-    const contact = await updateContact(id, { name, email, phone });
-    if (contact) {
-      res.json(contact);
-    } else {
-      res.status(404).json({ message: 'Not found' });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
+router.get('/', getAllContacts);
+router.get('/:id', getContact);
+router.post('/', createContact);
+router.delete('/:id', deleteContact);
+router.put('/:id', updateContactById);
 
 module.exports = router;
