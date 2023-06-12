2b786defd4503b6266e6aa6370274995c39e47ec
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-06-13 01:06:12 +0300

    1

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -1,14 +1,14 @@
-const Contact = require('../models/contact')
+const { Contact } = require('../models/contact');
 
 const addContact = async (req, res, next) => {
-   try {
-    const {_id: owner} = req.user
-     const result = await Contact.create(...req.body, owner);
-    console.log("Contact added!");
-   res.status(200).json(result);
-   } catch (error) {
-     next(error);
-   }
- };
+  try {
+    const { _id: owner } = req.user;
+    const result = await Contact.create(...req.body, owner);
+    console.log('Contact added!');
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-  module.exports = addContact;
+module.exports = addContact;
--- controllers/contactList.js
+++ controllers/contactList.js
@@ -1,13 +1,14 @@
-const Contact = require('../models/contact')
+const {Contact} = require('../models/contact');
 
-const contactList =  async (req, res, next) => {
-    try {
-      const result = await Contact.find();
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
-    }
+const contactList = async (req, res, next) => {
+  try {
+    const { _id: owner } = req.user;
+
+    const result = await Contact.find({ owner });
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
   }
+};
 
-
-   module.exports = contactList
+module.exports = contactList;
