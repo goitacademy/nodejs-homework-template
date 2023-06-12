690d204539e16a7b66d96cfcbd3258dd247e3eac
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-12 16:09:33 -0400

    part 1

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// const mongoose = require("mongoose");
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("Database connection successful"))
-//   .catch((error) => console.log(error.message));
 
 app.use(logger(formatsLogger));
 app.use(cors());
--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://Yuliia:YArRBmUXVIfvh7ru@cluster0.h8gga61.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-module.exports = {
-  DB_HOST,
-};
