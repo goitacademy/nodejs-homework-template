f4770837efbfd576e5124a9c26d931b93e052f6c
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-06-12 20:42:19 +0200

    task done

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,91 @@
-// const fs = require('fs/promises')
+import { readFile, writeFile } from "node:fs/promises";
+import { join } from "path";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+const directoryName = "models";
+const fileName = "contacts.json";
+const contactsPath = join(directoryName, fileName);
 
-const getContactById = async (contactId) => {}
+export const listContacts = async () => {
+  try {
+    const contacts = await readFile(contactsPath, { encoding: "utf-8" });
+    return JSON.parse(contacts);
+  } catch (error) {
+    throw new Error(`Something went wrong: ${error.message}`);
+  }
+};
 
-const removeContact = async (contactId) => {}
+export const getContactById = async (contactId) => {
+  try {
+    const parsedContacts = await listContacts();
+    const searchedContact = parsedContacts.find(
+      (contact) => contact.id === contactId
+    );
+    if (searchedContact) {
+      return searchedContact;
+    } else {
+      throw new Error(`There is no contact with this id: ${contactId}`);
+    }
+  } catch (error) {
+    throw new Error(`Something went wrong: ${error.message}`);
+  }
+};
 
-const addContact = async (body) => {}
+export const removeContact = async (contactId) => {
+  try {
+    const parsedContacts = await listContacts();
+    const contactIndex = parsedContacts.findIndex(
+      (contact) => contact.id === contactId
+    );
 
-const updateContact = async (contactId, body) => {}
+    if (contactIndex === -1) {
+      throw new Error(`There is no contact with this id ${contactId}`);
+    } else {
+      const newContactsList = parsedContacts.filter(
+        (contact) => contact.id !== contactId
+      );
+      await writeFile(contactsPath, JSON.stringify(newContactsList, null, 2));
+    }
+  } catch (error) {
+    throw new Error(`Something went wrong: ${error.message}`);
+  }
+};
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+export const addContact = async (body) => {
+  try {
+    const newContact = {
+      id: nanoid(),
+      ...body,
+    };
+
+    const parsedContacts = await listContacts();
+    const newContactsArray = [...parsedContacts, newContact];
+    writeFile(contactsPath, JSON.stringify(newContactsArray, null, 2));
+    return newContact;
+  } catch (error) {
+    throw new Error(`Something went wrong: ${error.message}`);
+  }
+};
+
+export const updateContact = async (contactId, body) => {
+  try {
+    const parsedContacts = await listContacts();
+    const contactToUpdate = parsedContacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (!contactToUpdate) {
+      throw new Error(`There is no contact with this id ${contactId}`);
+    } else {
+      const updatedContact = { ...contactToUpdate, ...body };
+      const newContactList = parsedContacts.filter(
+        (contact) => contact.id !== contactId
+      );
+      newContactList.push(updatedContact);
+      await writeFile(contactsPath, JSON.stringify(newContactList, null, 2));
+      return updatedContact;
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
