a62d92ea70f3a64dfc4d0b3e5f39f2757e42fd4f
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-12 12:55:05 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validation = require("./validation");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
 
 module.exports = {
   validation,
   isValidId,
+  authenticate,
 };
