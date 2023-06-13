5faba65f8a7714b97d1aff7dd1c4b77fad377cb0
serheihimself <sergm080@gmail.com>
2023-06-13 13:26:50 +0300

    server settings commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,23 +2,39 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
+
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAllContacts);
+router.get("/", authenticate, ctrl.getAllContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
-
-router.delete("/:contactId", ctrl.deleteContactById);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
 
 router.put(
   "/:contactId",
+  authenticate,
+  isValidId,
   validateBody(schemas.addSchema),
   ctrl.updateContactById
 );
+
+router.patch(
+  "/:contactId/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schemas.updateStatusContactSchema),
+  ctrl.updateStatusContact
+);
+
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteContactById);
 
 module.exports = router;
