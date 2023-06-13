54afecce167851db47645f700648f5045a5bcb03
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-06-14 00:26:26 +0300

    fix2

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -7,7 +7,7 @@ const validateBody = (schema) => {
     }
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, "missing required name field"));
+      next(HttpError(400, `missing name field ${error.message}`));
     }
     next();
   };
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -3,13 +3,13 @@ const Joi = require("joi");
 const addSchema = Joi.object({
   name: Joi.string().min(2).max(30).required(),
 
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
-  phone: Joi.string()
-    .required()
-    .pattern(/\(([0-9]{3})\)?([ .-])([0-9]{3})?([ .-])([0-9]{4})/),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().required(),
 });
 
 module.exports = {
