a22ce4c8c5b8dc7427c53a90318fb26a8b0c7d15
miamarichka <ilchyshyn.marichka@gmail.com>
2023-06-14 17:39:15 +0300

    adding db

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://miamarichka:7D7YLLf1GF72gaPR@cluster0.ngyks6g.mongodb.net/db-contacts?retryWrites=true&w=majority'
+
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  }))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
