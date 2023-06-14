948c932b3563bc6a43f9f14ff0accdba5b537f4d
jnosowska <joanna.nosowska.91@gmail.com>
2023-06-14 22:39:06 +0200

    hw03

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.Promise = global.Promise;
+
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST;
+
+mongoose.set('strictQuery', false);
+
+const connection = mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connection successful`);
+    });
+  })
+  .catch(err => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit();
+  });
