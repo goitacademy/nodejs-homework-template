301fa5bf867c309530a3a30d1025d5f94ce6519b
SLVladislav <bakalavrat.789@gmail.com>
2023-06-15 19:08:40 +0300

    add from 02HW files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,82 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const Joi = require('joi');
+
+const contacts = require('../../models/contacts');
+
+const router = express.Router();
+
+const { HttpError } = require('../../helper');
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const result = await contacts.listContacts();    
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if(!result){
+      throw HttpError(404, 'Not found');
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = addSchema.validate(req.body);
+    if( error ){
+      throw HttpError(400, "Missing required name field");
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+      const result = await contacts.removeContact(contactId);
+      if( !result ){
+        throw HttpError(404, 'Not found');
+      }
+    res.status(200).json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    const { error } = addSchema.validate(req.body);
+    if( error){
+      throw HttpError(404, "Not found");
+    }
+    
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const mongoose = require('mongoose');
 
-const {DB_HOST} = require('./config');
+const {DB_HOST} = require('./env');
 
 mongoose.connect(DB_HOST)
 .then(()=> {
@@ -13,3 +13,8 @@ mongoose.connect(DB_HOST)
 });
 
 
+// const app = require('./app');
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
