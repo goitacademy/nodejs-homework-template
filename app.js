f2ab414d89cb81db695497cbb9e8236168e5f89c
jnosowska <joanna.nosowska.91@gmail.com>
2023-06-15 20:06:18 +0200

    hm 4 - done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts');
+const usersRouter = require('./routes/api/users');
 
 const app = express();
 
@@ -13,7 +14,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+require('./config/config-passport');
 app.use('/api/contacts', contactsRouter);
+app.use('/api/users', usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
