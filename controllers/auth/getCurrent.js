d81087958eea33bdfa905aa86db48ec7c122fdc3
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-06-15 22:41:41 +0300

    add avatar to get current

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,8 +1,7 @@
 const getCurrent = async (req, res, next) => {
-  console.log('get current');
-  const { email, subscription } = req.user;
+  const { email, subscription, avatarURL } = req.user;
 
-  res.json({ email, subscription });
+  res.json({ email, subscription, avatarURL });
 };
 
 module.exports = getCurrent;
