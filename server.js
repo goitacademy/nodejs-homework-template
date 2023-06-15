bdcc7a33761df26ab7bdf3d5bf76fa2ef7769d16
Sergey Babenko <serg_babenko@i.ua>
2023-06-15 20:50:28 +0300

    commit1

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Sergey:M0O9mu4U0bIFW1v5@db-contacts.c7bsarb.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
