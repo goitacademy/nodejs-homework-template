a3eb2db6841889b2f8348d4d0447a4673bf42be6
zoria93 <artemzora4@gmail.com>
2023-06-16 18:22:48 +0300

    add mongoDB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,29 @@
 const express = require("express");
 
 const router = express.Router();
-const { validation } = require("../../middlewares");
-const schemas = require("../../schemas/contact");
-const { ctrlWrapper } = require("../../helpers");
+const { validation, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 const { contacts: ctrl } = require("../../controllers");
 
-router.get("/", ctrlWrapper(ctrl.getAllContacts));
-router.get("/:id", ctrlWrapper(ctrl.getById));
+router.get("/", ctrl.getAllContacts);
+router.get("/:id", isValidId, ctrl.getById);
 
-router.post("/", validation(schemas.addSchema), ctrlWrapper(ctrl.addContact));
+router.post("/", validation(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:id", ctrlWrapper(ctrl.deleteContact));
+router.delete("/:id", isValidId, ctrl.deleteContact);
 
 router.put(
   "/:id",
+  isValidId,
   validation(schemas.addSchema),
-  ctrlWrapper(ctrl.updateContact)
+  ctrl.updateContact
+);
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validation(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
 );
 
 module.exports = router;
--- schemas/index.js
+++
@@ -1,3 +0,0 @@
-// const addSchema = require("./contact");
-
-// module.exports = addSchema;
