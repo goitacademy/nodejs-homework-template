6b11df025690e7304e1e0e95bad67a9fb3ac75e5
AlexRose2603 <alexandra26korchak@gmail.com>
2023-06-16 19:07:44 +0300

    mongoose starts

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://AlexRose2603:JtdUszcsc2T0Hb1B@contacts-db.lnvqx23.mongodb.net/DB-Contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
