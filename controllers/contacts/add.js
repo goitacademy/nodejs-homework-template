d0867ca3267b651f31718047584e4cb1645ce7cb
Tetiana Klimenko <ktlimenko@gmail.com>
2023-06-16 18:53:50 +0300

    delete commit

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,13 +1,9 @@
- const Contact = require('../../models/contactModel');
-//  const { addContact } = require("../../models/contacts");
+const Contact = require("../../models/contactModel");
 
 const add = async (req, res) => {
-  //  const contactNew = await addContact(req.body);
-
-  // console.log('first',Contact.create)
   const contactNew = await Contact.create(req.body);
 
-  console.log('contactNew', contactNew);
+  console.log("contactNew", contactNew);
   res.status(201).json(contactNew);
 };
 
--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,10 +1,7 @@
 const Contact = require("../../models/contactModel");
-// const { listContacts } = require("../../models/contacts");
-//  console.log('Contact', Contact)
+
 const getAll = async (req, res) => {
-//  const result = await listContacts();
-  // console.log('Contact', Contact)
-   const result = await Contact.find({});
+  const result = await Contact.find({});
 
   res.json({
     status: "success",
