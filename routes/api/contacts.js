3ec53cceac375e07929cfa242c7ab1604f455150
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-06-16 21:22:36 +0300

    addet new validation, connect to DB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,33 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contact");
 
-const { validateBody } = require("../../middleWares");
+const { validateBody, isValidId } = require("../../middleWares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", isValidId, ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
+router.post("/", ctrl.add);
+
 router.post("/", validateBody(schemas.addschema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteById);
+router.delete("/:contactId", isValidId, ctrl.deleteById);
 
-router.put("/:contactId", validateBody(schemas.addschema), ctrl.update);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addschema),
+  ctrl.update
+);
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
