0a003a2910f7f55c89dd1b0ed8309c5e668a0d67
artyomakulov <artyomakulov@gmail.com>
2023-06-17 02:07:27 +0300

    add delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -55,18 +55,18 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-// router.delete("/:id", async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await contacts.removeContact(id);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     res.json({ message: "Delete success" });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndDelete(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "Delete success" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put("/:id", async (req, res, next) => {
   try {
