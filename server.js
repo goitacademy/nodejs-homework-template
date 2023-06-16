729c1dca42c3c8d0f9eae824dea21e4411d34053
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-06-16 12:38:47 +0300

    ADD: engines to package

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = require('./app');
 const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
+
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000);
     console.log("Server running. Use our API on port: 3000");
   })
-  .catch(error => console.log(error.message));
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+    });
