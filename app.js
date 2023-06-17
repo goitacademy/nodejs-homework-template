5913b4b326e019922a8ff9a39b509687f5fa4b45
OlgaMunaieva <asyssync@gmail.com>
2023-06-17 23:25:47 +0300

    connection render

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Rlm4NMZk1cKgYSDa
-const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://OlhaMunaieva:Rlm4NMZk1cKgYSDa@cluster0.i4fxsxn.mongodb.net/db-contacts?retryWrites=true&w=majority";
-mongoose.set("strictQuery", true);
+// const mongoose = require("mongoose");
+// const DB_HOST =
+//   "mongodb+srv://OlhaMunaieva:Rlm4NMZk1cKgYSDa@cluster0.i4fxsxn.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// mongoose.set("strictQuery", true);
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log("hi"))
-  .catch((error) => console.log(error.message));
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("hi"))
+//   .catch((error) => console.log(error.message));
 
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
-const { error } = require("./schemes/contacts");
+// const { error } = require("./schemes/contacts");
 
 const app = express();
 
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://OlhaMunaieva:Rlm4NMZk1cKgYSDa@cluster0.i4fxsxn.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
