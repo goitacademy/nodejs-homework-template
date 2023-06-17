cec74af61a8a8cb0cd0fdaa8016ab6cc1918ed99
Hennadii Borysevych <genuch3@gmail.com>
2023-06-17 13:24:58 +0300

    feat: add, delete, post methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,63 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
 
-const router = express.Router()
+const contacts = require("../../contacts");
+const { HTTPError } = require("../../utils");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const validationSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await contacts.listContacts();
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const data = await contacts.getContactById(contactId);
+    if (!data) {
+      throw HTTPError(404, "Not found");
+    }
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = validationSchema.validate(res.body);
+    if (error) {
+      throw HTTPError(400, error.message);
+    }
+    const data = await contacts.addContact(req.body);
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const data = await contacts.removeContact(contactId);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
