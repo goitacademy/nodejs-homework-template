440104c65b0402bd55e5c447d8a1af98a3e6bc23
Viktoriia Bieltsova <vikav0206@gmail.com>
2023-06-17 22:35:07 +0300

    add decorator

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,6 +4,8 @@ const contactsService = require("../models/contacts");
 
 const { HttpError } = require("../helpers");
 
+const { ctrlWrapper } = require("../decorators");
+
 const movieAddSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required().email(),
@@ -12,77 +14,57 @@ const movieAddSchema = Joi.object({
   "any.required": "missing required {#key} field",
 });
 
-const getAllContacts = async (req, res, next) => {
-  try {
-    const result = await contactsService.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
 };
 
-const getContactById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404);
   }
+  res.json(result);
 };
 
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = movieAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contactsService.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+const addContact = async (req, res) => {
+  const { error } = movieAddSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
 };
 
-const updateContactById = async (req, res, next) => {
-  try {
-    const { error } = movieAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contactsService.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateContactById = async (req, res) => {
+  const { error } = movieAddSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const { contactId } = req.params;
+  const result = await contactsService.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
 };
 
-const deleteContactById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json({
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
+const deleteContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404);
   }
+  res.json({
+    message: "contact deleted",
+  });
 };
 
 module.exports = {
-  getAllContacts,
-  getContactById,
-  addContact,
-  updateContactById,
-  deleteContactById,
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContactById: ctrlWrapper(updateContactById),
+  deleteContactById: ctrlWrapper(deleteContactById),
 };
