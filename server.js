1991a92bb3d9d1c2a6b6e78ad48b178d2fb7a1e4
Nika <99656127+NikaLunina@users.noreply.github.com>
2023-06-18 13:55:33 +0200

    add host to gitignore

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-
-const DB_HOST = "mongodb+srv://Nika:IiCKTfi0BNf61jtB@cluster0.dcvhfty.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+const{DB_HOST} = process.env
 
 mongoose.set('strictQuery', true)
 
 mongoose.connect(DB_HOST)
 .then(()=> {
+  console.log("Database connection successful")
   app.listen(3000)
 })
 .catch(error => {
