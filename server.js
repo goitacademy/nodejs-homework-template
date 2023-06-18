7ebf5232aa0c3fa25648f6b8019fc6700232ee1b
Your Name <pavlo.morozenko@ukr.net>
2023-06-18 20:04:38 +0200

    create env.example

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true);
 
