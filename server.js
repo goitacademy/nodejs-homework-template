b27547f8c0c89f5c22d98a236f50bad8edabbb06
AleksaYO <yura.aleksa.98@gmail.com>
2023-06-19 12:12:39 +0300

    add: env

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-
-const DB_HOST =
-  "mongodb+srv://AleksaYO:85aobA0vwH8aZyHP@cluster0.tw1zdtu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   })
