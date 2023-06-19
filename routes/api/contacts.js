3b7187ab6ced82f086edcdb962842764fb7fe87a
jnosowska <joanna.nosowska.91@gmail.com>
2023-06-19 16:39:43 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const contactsController = require('../../controller/contactsController');
+const auth = require('../../auth/userAuth');
 
-router.get('/', contactsController.get);
+router.get('/', auth, contactsController.get);
 
-router.get('/:id', contactsController.getById);
+router.get('/:id', auth, contactsController.getById);
 
-router.post('/', contactsController.create);
+router.post('/', auth, contactsController.create);
 
-router.delete('/:id', contactsController.removeById);
+router.delete('/:id', auth, contactsController.removeById);
 
-router.put('/:id', contactsController.update);
+router.put('/:id', auth, contactsController.update);
 
-router.patch('/:id/favorite', contactsController.updateStatus);
+router.patch('/:id/favorite', auth, contactsController.updateStatus);
 
 module.exports = router;
