17e1cc2e9655aaa35da333e484f1bdc26cab7080
Sanin Oleksandr <saninalexsanin@gmail.com>
2023-06-19 15:50:38 +0300

    added .env-example + renamed DB

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const app = require("./app");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
-// MONGO_URL =
-//   "mongodb+srv://Sanin:Sanin1992@contact-book-db.dd68ivs.mongodb.net/";
-//
-// це для ментора для перевірки
-
 const mongoose = require("mongoose");
 
 mongoose
