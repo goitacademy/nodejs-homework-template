9e633ae166ffb1db39a04efdea076856d66efdca
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-19 19:17:09 +0300

    adds helpers, adds try...catch to router-functions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,45 +1,77 @@
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// };
-
 const express = require('express');
 
 const router = express.Router();
 
 const contacts = require('../../models/contacts');
 
+const { HttpError } = require('../../helpers');
+
 router.get('/', async (req, res, next) => {
-  const allContacts = await contacts.listContacts();
-  // return console.table(allContacts);
-  res.json(allContacts);
+  try {
+    const allContacts = await contacts.listContacts();
+    // return console.table(allContacts);
+    res.json(allContacts);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({ message: 'Server error' });
+  }
 });
 
-router.get('/:contactId', async (contactId, res, next) => {
-  const contactById = await contacts.getContactById(contactId);
-  console.log(contactById);
-  res.json(contactById);
+router.get('/:contactId', async (req, res, next) => {
+  try {
+    const id = req.params.contactId;
+    const contactById = await contacts.getContactById(id);
+    if (!contactById) {
+      // res.status(404).json({ message: 'Not found' });
+      // const error = new Error('Not Found');
+      // error.status = 404;
+      // throw error;
+      throw HttpError(404, 'Not found');
+    }
+    // console.log(contactById);
+    res.json(contactById);
+  } catch (error) {
+    next(error);
+    // const { status = 500, message = 'Server error' } = error;
+    // res.status(status).json({ message });
+  }
 });
 
-router.post('/', async ({ name, email, phone }, res, next) => {
-  const newContact = await contacts.addContact({ name, email, phone });
-  // return console.log(newContact);
-  res.json(newContact);
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await contacts.addContact({ name, email, phone });
+    // return console.log(newContact);
+    res.json(newContact);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({ message: 'Server error' });
+  }
 });
 
-router.delete('/:contactId', async (contactId, res, next) => {
-  const removingContact = await contacts.removeContact(contactId);
-  // return console.log(removingContact);
-  res.json(removingContact);
+router.delete('/:contactId', async (req, res, next) => {
+  try {
+    const id = req.params.contactId;
+    const removingContact = await contacts.removeContact(id);
+    // return console.log(removingContact);
+    res.json(removingContact);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({ message: 'Server error' });
+  }
 });
 
 router.put('/:contactId', async (req, res, next) => {
-  const updatingContact = await contacts.updateContact(id, { name, email, phone });
-      // return console.log(updatingContact);
-  res.json(updatingContact);
+  try {
+    const id = req.params.contactId;
+    const { name, email, phone } = req.body;
+    const updatingContact = await contacts.updateContact(id, { name, email, phone });
+    // return console.log(updatingContact);
+    res.json(updatingContact);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
