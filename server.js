90581a84866d03e93d3b4daabe989edf72457cb5
TaniaMudryk <99959425+TaniaMudryk@users.noreply.github.com>
2023-06-19 15:22:32 +0300

    final

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+require("colors");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+mongoose.set("strictQuery", true);
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    console.log("\nDatabase connection successful".green);
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`.green);
+    });
+  })
+  .catch((err) => {
+    console.log("\nDatabase not running\n".red, err.toString());
+    process.exit(1);
+  });
---
+++ service/index.js
@@ -0,0 +1,32 @@
+const Contact = require("./schemas/contacts");
+
+const getAllContacts = async () => Contact.find();
+
+const getContactById = async (contactId) => Contact.findById(contactId);
+
+const createContact = async ({ name, email, phone, favorite }) => {
+  return Contact.create({ name, email, phone, favorite });
+};
+
+const updateContact = async (contactId, fields) => {
+  return Contact.findByIdAndUpdate(contactId, fields, {
+    new: true,
+    strict: "throw",
+    runValidators: true,
+  });
+};
+
+const updateStatusContact = async (contactId, favorite) => {
+  return Contact.findByIdAndUpdate(contactId, { favorite });
+};
+
+const deleteContact = async (contactId) => Contact.findByIdAndRemove(contactId);
+
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  updateStatusContact,
+  deleteContact,
+};
