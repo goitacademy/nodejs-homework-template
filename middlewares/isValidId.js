9c199785410de46a139c18f9c933814806c54a96
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-06-19 13:20:52 +0300

    FIX: remove isValidId func

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,16 +1,16 @@
-const { isValidObjectId } = require("mongoose");
-const { HttpError } = require("../helpers");
+// const { isValidObjectId } = require("mongoose");
+// const { HttpError } = require("../helpers");
 
-const isValidId = (req, res, next) => {
-    const { id } = req.params;
-    if (!isValidObjectId(id)) {
-        next(HttpError(400, `${id} is not valid id`))
-    };
+// const isValidId = (req, res, next) => {
+//     const { id } = req.params;
+//     if (!isValidObjectId(id)) {
+//         next(HttpError(400, `${id} is not valid id`))
+//     };
 
-    next();
-};
+//     next();
+// };
 
 
-module.exports = {
-    isValidId
-}
+// module.exports = {
+//     isValidId
+// }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,14 +3,14 @@ const ctrl = require("../../controllers/contacts");
 
 const router = express.Router();
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody } = require("../../middlewares");
 const { addScheme, updateFavoriteScheme } = require("../../schemes");
 
 router.get('/', ctrl.getAll);
-router.get('/:contactId', isValidId, ctrl.getById);
+router.get('/:contactId', ctrl.getById);
 router.post('/', validateBody(addScheme), ctrl.add);
-router.put('/:contactId', isValidId, validateBody(addScheme), ctrl.update);
-router.delete('/:contactId', isValidId, ctrl.remove);
+router.put('/:contactId', validateBody(addScheme), ctrl.update);
+router.delete('/:contactId', ctrl.remove);
 router.patch('/:contactId/favorite', validateBody(updateFavoriteScheme), ctrl.patchFavorite);
 
 
