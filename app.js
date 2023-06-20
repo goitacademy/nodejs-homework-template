b48c5b129a7ea7cf593aa97e5c1c3fb161c882e0
Ihor Karen <karenihor@gmail.com>
2023-06-20 23:12:17 +0300

    rename router file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contactsRouter");
 
 const app = express();
 
