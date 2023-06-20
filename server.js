d875df0ae3e5a11e650903c65429cac8f5efa1f8
galynary <107271972+galynary@users.noreply.github.com>
2023-06-21 01:01:34 +0300

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(1000);
+    app.listen(3000);
   console.log("Server running. Use our API on port: 3000")
 })
 .catch(error => { 
