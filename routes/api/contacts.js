a1ac5b27114f4abaf2f9bce02bef10445f859b2d
Matjelko <matjelko11@gmail.com>
2023-06-20 18:52:31 +0200

    Update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -21,6 +21,56 @@ router.get('/', async (req, res, next) => {
   })
 })
 
+router.get('/:contactId', async (req, res, next) => {
+  const contact = await Contact.findOne({ _id: req.params.contactId })
+  if(contact) {
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        contact: contact
+      }
+    })
+  } else {
+    res.status(404).json({ message: 'Not found' })
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  const newContact = await Contact.create(req.body)
+  if(newContact){
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        contact: newContact
+      }
+    })
+  } else {
+    res.status(400).json({ message: "Missing required name - field" })
+  }
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  const contact = await Contact.deleteOne({ _id: req.params.contactId })
+  if(contact){
+    res.status(200).json({ message: 'Contact deleted' })
+  } else {
+    res.status(404).json({ message: 'Not found' })
+  }
+})
+
+router.put('/:contactId', async (req, res, next) => {
+  const updatedContact = await Contact.findOneAndUpdate({ _id: req.params.contactId }, req.body , { new: true })
+
+  if(updatedContact){
+    res.status(200).json(updatedContact)
+  }
+  else{
+    res.status(404).json({ message: 'Not Found' })
+  }
+})
+
 // router.get('/', async (req, res, next) => {
 //   const data = await listContacts()
 //   res.json({
