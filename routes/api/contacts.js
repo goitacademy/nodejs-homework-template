89191aeee3131bb94087c0c21790e7cb931f301d
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-06-21 21:34:16 +0300

    did-stuff

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -22,7 +22,7 @@ router.get("/:contactId", async (req, res, next) => {
     res.json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
+  },
 });
 
 router.post("/", async (req, res, next) => {
