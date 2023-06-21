b538281bb5dd165d439edf1d175c95c2ea861aa3
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-06-21 14:51:17 +0300

    fix Jimp

--- controllers/users/updateAvtar.js
+++ controllers/users/updateAvtar.js
@@ -1,36 +1,30 @@
 const User = require('../../models/user');
 const path = require('path');
 const fs = require('fs/promises');
-// const Jimp = require('jimp');
+const Jimp = require('jimp');
 
 const avatarDir = path.resolve('public', 'avatars');
 
 const updateAvatarUser = async (req, res, next) => {
   try {
+   
     const { path: oldPath, filename } = req.file;
-
     const newPath = path.join(avatarDir, filename);
+
+    const image = await Jimp.read(oldPath);
+    await image.resize(250, 250)
+         .writeAsync(oldPath);
+  
     await fs.rename(oldPath, newPath);
     console.log(`oldPath`, oldPath);
     console.log(`newPath`, newPath);
-
-    // const newSizeAvatar = Jimp.read()
-    // .then((newPath) => {
-    //   return newPath
-    //     .resize(256, 256) // resize
-    //     .write(newPath); // save
-    // })
-    // .catch((err) => {
-    //   console.error(err);
-    // });
-
     const avatarURL = path.join('public', 'avatars', filename);
+ 
 
-
-    console.log(`avatarURL`, avatarURL);
 
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
-    res.json({ avatarURL });
+
+    res.json({avatarURL});
   } catch (error) {
     console.log(`updateAvatarError`);
     next(error);
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,5 @@
 const multer = require('multer');
 const path = require('path');
-// const Jimp = require('jimp');
 
 const destination = path.resolve('tmp');
 
