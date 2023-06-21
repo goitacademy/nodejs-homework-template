81040fdde616083ba99c4accabae7d0a28fdc1d1
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-06-21 17:22:16 +0300

    fix APP.js

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const logger = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
 require('dotenv').config();
+const path = require('path');
 
 const contactsRouter = require('./routes/api/contacts');
 const authRouter = require('./routes/api/auth');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-app.use(express.static('public'));
 
 app.use('/users', authRouter);
 app.use('/api/contacts', contactsRouter);
