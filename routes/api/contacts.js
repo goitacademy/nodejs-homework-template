1788b0423c90a4f166bb4d9402dad8b9bdbff864
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-06-22 10:12:58 +0300

    fix-delete-contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const contactsService = require("../../models/contacts");
 
+const { HttpError } = require("../../helpers");
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
@@ -19,17 +21,31 @@ router.get("/:contactId", async (req, res, next) => {
     console.log(req.params);
     const { contactId } = req.params;
     const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Contact not found");
+    }
     res.json(result);
   } catch (error) {
-    res.status(500).json({ message: error.message });
-  },
-});
-
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, `Contact not found with id: ${contactId}`);
+    }
+    res.json({ message: "Contact deleted successfully" });
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
+});
+
+router.post("/", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
