f82ca35839fd9df6bbc767941790cf7e5e233c39
Your Name <pavlo.morozenko@ukr.net>
2023-06-22 21:59:43 +0200

    Server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
-
 const app = require('./app');
-
 const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true);
-
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(()=> {
-    app.listen(3000)
+    console.log("Database connection successful");
+    app.listen(PORT);
   })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   })
-
-
