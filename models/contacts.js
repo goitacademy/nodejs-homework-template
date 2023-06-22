04d84b809c23609916e8d001e7066fc08635dc6e
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-06-22 10:12:32 +0300

    delete-contact-fix

--- models/contacts.js
+++ models/contacts.js
@@ -3,9 +3,10 @@ const path = require("path");
 
 const contactsPath = path.join(__dirname, "../models/contacts.json");
 
+const updateContacts = async (contacts) => await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath, "utf-8");
-  console.log(data);
   return JSON.parse(data);
 };
 
@@ -15,7 +16,21 @@ const getContactById = async (contactId) => {
   return result || null;
 };
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+
+  const existingContact = contacts.find((item) => item.id === contactId);
+
+  if (!existingContact) {
+    return null;
+  }
+
+  const result = contacts.filter((item) => item.id !== contactId);
+
+  await updateContacts(result);
+
+  return result;
+};
 
 const addContact = async (body) => {};
 
