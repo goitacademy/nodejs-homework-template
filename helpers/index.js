cf76cb44e759f5c45b3a0df0a856843f9d04f31f
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-06-24 00:07:47 +0300

    03-mongodb
    -create mongoose database
    -add updateStatusContact

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require("./HttpError");
 const ctrl = require("./ctrlWrapper");
+const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
   HttpError,
   ctrl,
+  handleMongooseError,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 
+const isValidId = require("../../middlewares");
+
 router.get("/", ctrl.listContacts);
 
 router.get("/:id", ctrl.getById);
@@ -11,6 +13,8 @@ router.get("/:id", ctrl.getById);
 router.post("/", ctrl.addContact);
 
 router.put("/:id", ctrl.updateById);
+
+router.patch("/:id/favorite", isValidId, ctrl.updateStatusContact);
 
 router.delete("/:id", ctrl.removeContact);
 
