1d277b4e61e11a37a3e54e53ace484363482aa08
M.Mostova <mmainfo15@gmail.com>
2023-06-23 16:46:13 +0300

    create upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
 const isValidId = require("./isValidId");
-const authenticate = require("./authenticate"); 
-const validateBody = require('./validateBody')
+const authenticate = require("./authenticate");
+const validateBody = require("./validateBody");
+const upload = require("./upload");
 
-
-module.exports = { isValidId, authenticate, validateBody };
+module.exports = { isValidId, authenticate, validateBody, upload };
