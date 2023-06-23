72d138e71b4aa4c8172d4dd5aef3818e4617612f
galynary <107271972+galynary@users.noreply.github.com>
2023-06-23 18:41:17 +0300

    change

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
- const DB_HOST= "mongodb+srv://Halyna:IQzrHHy05TvMi8Yw@cluster0.jkk4kun.mongodb.net/db-contacts";
+const {DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
+  // eslint-disable-next-line no-undef
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   console.log("Server running. Use our API on port: 3000")
 })
 .catch(error => { 
