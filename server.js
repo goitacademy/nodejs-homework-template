b8cb294d3e83f263627abf3b01cbffe7094a4d3f
Valerii Pozhydaiev <p_vf@i.ua>
2023-06-23 03:24:03 +0300

    init project, add structure

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const app = require('./app')
+require("dotenv").config();
+const PORT = process.env.PORT;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
