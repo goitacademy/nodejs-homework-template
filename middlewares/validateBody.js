7bbe72c854040b2617599939f418d87249f4097b
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-06-23 16:28:33 +0300

    1

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,9 +1,8 @@
 const { HttpError } = require("../helpers");
-const addSchema = require("../schemas/contacts");
 
-const validateBody = (addSchema) => {
+const validateBody = (schema) => {
   const func = (req, res, next) => {
-    const { error } = addSchema.validate(req.body);
+    const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, "missing required name field"));
     }
