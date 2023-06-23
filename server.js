411b2a2664dba1a9e7cdfb58e21ad926daab1ef8
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-06-23 20:51:13 +0300

    live server

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const app = require("./app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
-// live server: https://nodejs-homework-rest-api-34tv.onrender.com
+// live server: https://nodejs-homework-rest-api-34tv.onrender.com/api/contacts
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Server is running and connected to DB");
+      console.log("live server: https://nodejs-homework-rest-api-34tv.onrender.com/api/contacts");
     });
   })
   .catch((error) => {
