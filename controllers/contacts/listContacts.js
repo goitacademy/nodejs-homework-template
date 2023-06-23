6cd2fa3f314068a53ad6b9931a9e8d4bb63a8b2c
Ihor Karen <karenihor@gmail.com>
2023-06-23 23:53:55 +0300

    favorite filter

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,7 +4,24 @@ const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  const query = req.query;
+
+  let result = null;
+
+  if (query.favorite === "true") {
+    result = await Contact.find(
+      { owner, favorite: true },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    );
+
+    res.json(result);
+  }
+
+  result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
   });
