256fbd7226bfdbcdec937099fd1472c9af63e723
Stepan Skichko <st.skichko@gmail.com>
2023-06-24 15:02:16 +0300

    delete trash

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,28 +1,3 @@
-// const express = require('express')
-
-// const router = express.Router()
-
-// router.get('/', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.get('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.post('/', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.delete('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.put('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// module.exports = router
 const express = require('express');
 const Joi = require('joi');
 
