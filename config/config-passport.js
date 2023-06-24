7659eec6adccff29b9ceeb0cc93d8e3767bf7697
Matjelko <matjelko11@gmail.com>
2023-06-24 18:13:43 +0200

    final version

---
+++ config/config-passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport')
+const passportJWT = require('passport-jwt')
+
+const User = require('../models/users')
+
+require('dotenv').config()
+
+const secret = process.env.SECRET
+
+const ExtractJWT = passportJWT.ExtractJwt
+const Strategy = passportJWT.Strategy
+const params = {
+    secretOrKey: secret,
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+}
+
+passport.use(
+    new Strategy(params, function(payload, done) {
+        User.find({ _id: payload.id })
+            .then(([user]) => {
+                if (!user) {
+                    return done(new Error("User is not here anymore"))
+                }
+                return done(null, user)
+            })
+            .catch((err) => done(err))
+    })
+)
--- models/contacts.js
+++ models/contacts.js
@@ -16,6 +16,10 @@ const contactSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  }
 })
 
 const Contact = mongoose.model("contact", contactSchema);
