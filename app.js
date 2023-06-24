f2f7c6fcfeacc15a700c1c83d7aed5c4930247df
Iryna Holova <gis.golova@gmail.com>
2023-06-24 16:03:03 +0300

    fixed bug

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
-  console.log(err)
   res.status(status).json({ message });
 });
 
