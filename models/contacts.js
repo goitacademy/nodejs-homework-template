31256d989e57f5b8f41b17e57a6bc272412abe51
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-25 20:32:20 +0300

    add  first part

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,44 @@
-// const fs = require('fs/promises')
+const fs = require('node:fs/promises');
+const path = require('path');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+
+const listContacts = async () => {
+  return JSON.parse(await fs.readFile(contactsPath, "utf-8"))
+};
+
+const getContactById = async (id) => {
+  console.log(id);
+  const contacts =  await listContacts();
+  const findContact = contacts.find((contact) => contact.id === id);
+  if (findContact) {
+      return findContact;
+  } 
+  return null;
+
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const {name, email, phone} = body;
+  const contacts = await JSON.parse(await fs.readFile(contactsPath));
+  const newContact = {
+      id: Date.now().toString(),
+      name,
+      email,
+      phone,
+    };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 
-const updateContact = async (contactId, body) => {}
+}
+
+const updateContact = async (contactId, body) => {
+
+}
 
 module.exports = {
   listContacts,
