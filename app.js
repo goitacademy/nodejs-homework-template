4ac37b78501052c60b1173524175976e249760dd
Artem Shnurko <shnurko.artyom@gmail.com>
2023-06-25 12:54:08 +0200

    upd

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
