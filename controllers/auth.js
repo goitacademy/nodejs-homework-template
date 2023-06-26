81ba01755853e6c1915d3389d962732c2195a578
Your Name <pavlo.morozenko@ukr.net>
2023-06-26 16:48:23 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,12 +4,17 @@ const { HttpError, ctrlWrapper } = require('../helpers');
 const { modelNames } = require('mongoose');
 
 const register = async(req, res) => {
-    const newUser = await User.create(req.body);
-
-    res.json({
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+        throw HttpError(409, 'Email in use');
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
+    res.status(201).json({
         email: newUser.email,
         name: newUser.name,
-    })
+    });
 }
 
 module.exports = {
