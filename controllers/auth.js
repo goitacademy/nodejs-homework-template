69cb81b25a66ccd4450a290874518b4bdda41645
Your Name <pavlo.morozenko@ukr.net>
2023-06-26 16:30:10 +0200

    register create

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,17 @@
 const { User } = require('../models/user');
+
+const { HttpError, ctrlWrapper } = require('../helpers');
+const { modelNames } = require('mongoose');
+
+const register = async(req, res) => {
+    const newUser = await User.create(req.body);
+
+    res.json({
+        email: newUser.email,
+        name: newUser.name,
+    })
+}
+
+module.exports = {
+    register: ctrlWrapper(register),
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+const ctrl = require('../../controllers/auth');
+
 const {validateBody} = require('../../middlewares');
 
 const {schemas} = require('../../models/user');
 
 const router = express.Router();
 
-router.post('/register', validateBody(schemas.registerSchema));
+router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 
 
 module.exports = router;
