8a05fe3a780fb1192d3e35adc56081ee90c6173f
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-06-26 21:46:38 +0300

    change icon to Cloudinary

--- controllers/auth/updateAva.js
+++ controllers/auth/updateAva.js
@@ -2,8 +2,16 @@ const path = require('path');
 const fs = require('fs').promises;
 const { User } = require('../../models/user');
 const { resize } = require('../../helpers');
+const cloudinary = require('cloudinary').v2;
 
 const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_API_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const updateAva = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
@@ -11,9 +19,26 @@ const updateAva = async (req, res) => {
   const filename = `${req.user._id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join('avatars', filename);
-  await User.findByIdAndUpdate(req.user._id, { avatarURL });
-  res.json({ avatarURL });
+  const avatarURL = path.join(avatarsDir, filename);
+  console.log(avatarURL);
+  try {
+    const { url: avatarUrl } = await cloudinary.uploader.upload(avatarURL);
+    await User.findByIdAndUpdate(req.user._id, { avatarUrl });
+    res.json({ avatarUrl });
+    // console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // console.log(result);
+  // const { path: tempUpload, originalname } = req.file;
+  // await resize(tempUpload);
+  // const filename = `${req.user._id}_${originalname}`;
+  // const resultUpload = path.join(avatarsDir, filename);
+  // await fs.rename(tempUpload, resultUpload);
+  // const avatarURL = path.join('avatars', filename);
+  // await User.findByIdAndUpdate(req.user._id, { avatarURL });
+  // res.json({ avatarURL });
 };
 
 module.exports = updateAva;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const app = require('./app');
 const mongoose = require('mongoose');
+const cloudinary = require('cloudinary').v2;
 
 const { DB_HOST } = process.env;
+
+if (typeof process.env.CLOUDINARY_URL === 'undefined') {
+  console.warn('!! cloudinary config is undefined !!');
+  console.warn('export CLOUDINARY_URL or set dotenv file');
+} else {
+  console.log('cloudinary config:');
+  console.log(cloudinary.config());
+}
 
 mongoose.set('strictQuery', true);
 mongoose
