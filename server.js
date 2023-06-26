9656f018b3ab948c5ead9a16f4c6b3233010e199
Oleksandr <alex.vovchok@gmail.com>
2023-06-26 13:19:55 +0300

    added env

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const DB_HOST = "mongodb+srv://OleksVov:mNs7dMOaAynHRz1C@cluster0.xmyqeic.mongodb.net/?retryWrites=true&w=majority"
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
 .then(() => {
-  app.listen(3000)
+  app.listen(PORT, () => {
+    console.log("Database connection successful");
+  })
 })
 .catch(error => {
   console.log(error.message);
   process.exit(1);
 })
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
