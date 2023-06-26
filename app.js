6ca834650b196bd028fd5e578423cb89c83f71de
HaidaienkoK <gaidk1973@gmail.com>
2023-06-26 19:36:20 +0300

    fix error httpError

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// g5-CU.!2WdH_eSc
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -15,7 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const { User } = require("../models/user");
 
-const { ctrlWrapper, httpError } = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
 
@@ -12,7 +12,7 @@ const register = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user) {
-    throw httpError(409, "Email in use");
+    throw HttpError(409, "Email in use");
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
@@ -31,11 +31,11 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw httpError(401, "EmEmail or password is wrong");
+    throw HttpError(401, "EmEmail or password is wrong");
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw httpError(401, "Email or password is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
 
   const payload = {
@@ -80,7 +80,7 @@ const updateSubscription = async (req, res) => {
     new: true,
   });
 
-  if (!result) throw httpError(404, "Not found");
+  if (!result) throw HttpError(404, "Not found");
 
   return res.json(result);
 };
