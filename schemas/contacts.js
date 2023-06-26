4efa8fd6a5c8bff07230e11e1ef0a2e1e4caba3c
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-06-26 21:22:27 +0300

    Update contacts.js

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
+  name: Joi.string().min(3).max(25).required(),
+  email: Joi.string().email().required(),
   phone: Joi.string().required(),
 });
 
