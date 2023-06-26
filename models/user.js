ef99e383fcef8b6a9430e7fa46ad39efcd5b33ff
Your Name <pavlo.morozenko@ukr.net>
2023-06-26 14:49:04 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,8 +2,26 @@ const { Schema, model } = require('mongoose');
 const { handleMongooseError } = require('../helpers');
 const Joi = require('joi');
 
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const userSchema = new Schema(
     {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            match: emailRegexp,
+        },
+        password: {
+            type: String,
+            minlength: 6,
+            required: [true, 'Set password for user'],
+        }
     },
     { versionKey: false, timestamps: true }
   );
+
+  userSchema.post('save', handleMongooseError);
