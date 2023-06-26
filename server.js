12386e4350aed017ad3174cf824aaba51064e119
Oleksandr <alex.vovchok@gmail.com>
2023-06-26 10:47:45 +0300

    connected mongoose

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
+
 const app = require('./app');
 
+const DB_HOST = "mongodb+srv://OleksVov:mNs7dMOaAynHRz1C@cluster0.xmyqeic.mongodb.net/?retryWrites=true&w=majority"
 
+mongoose.set('strictQuery', true);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000)
 })
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+})
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
