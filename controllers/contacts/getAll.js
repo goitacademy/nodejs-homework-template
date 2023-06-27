27f4879aaf8f705be5fc4a0bbc21b64ed8f0ed0c
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-06-27 17:56:25 +0300

    ADDED: search params +skip +limit +favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,10 +2,12 @@ const { Contact } = require('../../models/contact');
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
+  const findOptions = favorite ? { owner, favorite } : { owner };
+
+  const result = await Contact.find(findOptions, '-createdAt -updatedAt', {
     skip,
     limit,
   }).populate('owner', 'email subscription');
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { validateBody, authenticate } = require('../../middlewares');
 const { schemas } = require('../../models/user');
-const ctrl = require('../../controllers/auth/auth');
+const ctrl = require('../../controllers/auth');
 
 const router = express.Router();
 
