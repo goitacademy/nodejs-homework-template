430f048f35aa98c0da0c310335acbdc03c944da8
Vladimir Ponomarov <ripper3061@gmail.com>
2023-06-27 10:46:02 +0300

    routes for get, getById

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises')
 const path = require("path");
+const { nanoid } = require("nanoid");
 const contactPath = path.resolve(__dirname, "contacts.json");
 
 const readContactsFile = async () => {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,29 @@
 const express = require('express')
-
+const {
+  listContacts,
+  getContactById,
+  // addContact,
+  // removeContact,
+  // updateContact,
+} = require('../../models/contacts');
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await listContacts();
+  res.status(200).json(contacts);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const contactById = await getContactById(contactId);
+    if (!contactById) {
+      return res.status(404).json({ message: 'Not Found' })
+    }
+    return res.status(200).json(contactById);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
