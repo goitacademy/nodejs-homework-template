effdd1157bf3dc55461cf914f76ed7a184be78ae
dyonns <kongfuri3228@gmail.com>
2023-06-27 23:42:47 +0300

    pr

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
