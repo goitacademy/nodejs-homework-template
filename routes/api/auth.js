dce7c6c0286c531f866820e60fe815fbdae2e992
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-06-27 17:27:23 +0300

    ADDED routes( logout + login + current) git commit -m ADDED

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validateBody } = require('../../middlewares');
+const { validateBody, authenticate } = require('../../middlewares');
 const { schemas } = require('../../models/user');
 const ctrl = require('../../controllers/auth/auth');
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
+router.get('/current', authenticate, ctrl.getCurrent);
+router.post('/logout', authenticate, ctrl.logout);
 
 module.exports = router;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,21 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 const ctrl = require('../../controllers/contacts');
-const { validateBody, isValidId } = require('../../middlewares');
+const { validateBody, isValidId, authenticate } = require('../../middlewares');
 const { schemas } = require('../../models/contact');
 
-router.get('/', ctrl.getAll);
-router.get('/:contactId', isValidId, ctrl.getById);
-router.post('/', validateBody(schemas.addSchema), ctrl.add);
-router.delete('/:contactId', isValidId, ctrl.removeById);
+router.get('/', authenticate, ctrl.getAll);
+router.get('/:contactId', authenticate, isValidId, ctrl.getById);
+router.post('/', authenticate, validateBody(schemas.addSchema), ctrl.add);
+router.delete('/:contactId', authenticate, isValidId, ctrl.removeById);
 router.put(
   '/:contactId',
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrl.updateById
 );
 router.patch(
   '/:contactId/favorite',
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
