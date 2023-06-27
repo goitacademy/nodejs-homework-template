235b66882bcad8522839647e452e8c671ab00d50
Sldvlld <sladvlad@gmail.com>
2023-06-27 20:04:48 +0300

    add vers

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const { DB_HOST } = require("./config");
-// add version
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
@@ -13,3 +13,5 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
+
+// add node version
