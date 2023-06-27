1befdeef677d101761f3928b06e110a51928c946
galynary <107271972+galynary@users.noreply.github.com>
2023-06-27 20:36:26 +0300

    router auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,21 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require("../../controllers/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validateBody, isValidId } = require("../../middlewares");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.listContacts);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", isValidId, ctrl.getContactById);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateBody, ctrl.addContact);
 
-module.exports = router
+router.delete("/:id", isValidId, ctrl.removeContact);
+
+router.put("/:id", isValidId, validateBody, ctrl.updateContact);
+
+router.patch("/:id/favorite", isValidId, validateBody, ctrl.updateFavorite);
+
+module.exports = router;
