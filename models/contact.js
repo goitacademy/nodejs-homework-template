9f8a67a0d166edb92e4b0ad65568f72e67e8bf4f
nadikkk <109106413+nadikkk@users.noreply.github.com>
2023-06-28 16:02:55 +0300

    fix prettier

--- models/contact.js
+++ models/contact.js
@@ -1,32 +1,33 @@
-const {Schema, model} = require('mongoose');
-const {handleMongooseError} = require("../helpers")
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
 
-
-const contactSchema = new Schema({
-	name: {
+const contactSchema = new Schema(
+  {
+    name: {
       type: String,
-      required: [true, 'Set name for contact'],
+      required: [true, "Set name for contact"],
     },
     email: {
       type: String,
-		unique: [true, 'Duplicated email'],
-		match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-		required: true,
+      unique: [true, "Duplicated email"],
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      required: true,
     },
     phone: {
       type: String,
-		match: /^\(\d{3}\) \d{3}-\d{4}$/,
-		required: true,
+      match: /^\(\d{3}\) \d{3}-\d{4}$/,
+      required: true,
     },
     favorite: {
       type: Boolean,
       default: false,
     },
-}, {versionKey: false, timestamps: true});
+  },
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleMongooseError);
 
-const Contact = model('contact', contactSchema);
+const Contact = model("contact", contactSchema);
 
 module.exports = Contact;
-
