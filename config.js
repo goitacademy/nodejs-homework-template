a01fb8a1009c955349200ba73c9c75afe5a9905e
Oleg <sirov@ukr.net>
2023-06-29 00:14:58 +0300

    add DB connection

---
+++ config.js
@@ -0,0 +1,4 @@
+const DB_HOST =
+  "mongodb+srv://Oleh:FYehGSieDDGMteNW@cluster0.xmzjm9y.mongodb.net/db-contacts";
+
+module.exports = { DB_HOST };
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const { DB_HOST } = require("./config");
+mongoose.set("strictQuery", "true");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
-
-
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("DB connect sussess");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
