9ad9b4c580f4e7e212eca2dc7ab9fad787a3d3dc
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-06-28 11:23:33 +0300

    fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/contact");
+const Contact = require("../models/contacts");
 const { HttpError } = require("../helpers");
 
 const listContacts = async (req, res, next) => {
