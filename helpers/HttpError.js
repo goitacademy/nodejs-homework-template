881b8c88e736cec96a93e2c111ae0b128bd3b1f7
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-06-28 18:52:15 +0300

    ADD: Backup

---
+++ helpers/HttpError.js
@@ -0,0 +1,15 @@
+const messagesList = {
+	400: 'Bad Request',
+	401: 'Unauthorized',
+	403: 'Forbidden',
+	404: 'Not Found',
+	409: 'Conflict'
+};
+
+const HttpError = (status, message = messagesList[status]) => {
+	const error = new Error(message);
+	error.status = status;
+	return error;
+};
+
+module.exports = HttpError;
