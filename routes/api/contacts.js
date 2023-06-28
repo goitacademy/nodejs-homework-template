da2a83f697f4499be8e50713152f14973f3fdce7
galynary <107271972+galynary@users.noreply.github.com>
2023-06-28 09:07:42 +0300

    change schema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const {
+	addSchema,
+	updateFavoriteSchema,
+	updateSchema,
+} = require("../../models/contact");
+
 const ctrl = require("../../controllers/contacts");
 
 const { validateBody, isValidId } = require("../../middlewares");
@@ -8,14 +14,19 @@ const router = express.Router();
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", isValidId, ctrl.getContactById);
+router.get("/:id", isValidId(addSchema), ctrl.getContactById);
 
-router.post("/", validateBody, ctrl.addContact);
+router.post("/", validateBody(addSchema), ctrl.addContact);
 
-router.delete("/:id", isValidId, ctrl.removeContact);
+router.delete("/:id", isValidId(addSchema), ctrl.removeContact);
 
-router.put("/:id", isValidId, validateBody, ctrl.updateContact);
+router.put("/:id", isValidId(updateSchema), validateBody, ctrl.updateContact);
 
-router.patch("/:id/favorite", isValidId, validateBody, ctrl.updateFavorite);
+router.patch(
+	"/:id/favorite",
+	isValidId,
+	validateBody(updateFavoriteSchema),
+	ctrl.updateFavorite
+);
 
 module.exports = router;
