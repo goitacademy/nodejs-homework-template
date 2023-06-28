cb4581e244a2a2417454e070f46eb1329c10560f
Your Name <pavlo.morozenko@ukr.net>
2023-06-28 15:44:02 +0200

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -18,13 +18,6 @@ const contactSchema = new Schema(
       type: Boolean,
       default: false,
     },
-
-    // owner: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: 'user',
-    //   required: true,
-    // },
-    
   },
   { versionKey: false, timestamps: true }
 );
