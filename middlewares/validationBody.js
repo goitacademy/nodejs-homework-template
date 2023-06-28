16f3b217c58f0c46c743897395c1e9117366abe9
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-06-28 16:49:06 +0300

    +

--- middlewares/validationBody.js
+++ middlewares/validationBody.js
@@ -3,10 +3,19 @@ const { requestError } = require("../utils");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+
     if (!Object.keys(req.body).length) {
       next(requestError(400, "missing  fields"));
     } else if (error) {
-      next(requestError(400, "missing required name field"));
+      next(
+        requestError(
+          400,
+          `missing required ${error.message.slice(
+            1,
+            error.message.lastIndexOf('"')
+          )} field`
+        )
+      );
     }
     next(error);
   };
