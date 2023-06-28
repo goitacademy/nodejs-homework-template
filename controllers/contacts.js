627799aadd6e594ca5954b75d1f79150c056e70b
Your Name <pavlo.morozenko@ukr.net>
2023-06-28 15:43:53 +0200

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,6 @@ const { HttpError, ctrlWrapper } = require('../helpers');
 const { Contact } = require('../models/contact');
 
 const getAll = async (req, res) => {
-  // const {_id: owner} = req.user;
   const result = await Contact.find();
   res.json(result);
 };
