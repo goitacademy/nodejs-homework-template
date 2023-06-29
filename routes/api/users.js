19af05678e23dd1363fe438f38b82deba984f31e
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-06-29 15:24:19 +0200

    Contacts api works

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const Contact = require("../../service/schemas/contact");
 const router = express.Router();
 
 router.post("/login", async (req, res, next) => {});
 
 router.post("/signup", async (req, res, next) => {});
 
-router.get("/contacts", async (req, res, next) => {});
+router.get("/contacts", async (req, res, next) => {
+  const contacts = await Contact.find();
+  res.json({
+    status: "success",
+    code: 200,
+    data: { contacts },
+  });
+});
 
 module.exports = router;
