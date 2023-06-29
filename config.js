ed471b86e1f75f3264b35ce870a1974bb28db4d6
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-06-29 16:49:17 +0300

    add env

--- config.js
+++
@@ -1,4 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://Oleh:FYehGSieDDGMteNW@cluster0.xmzjm9y.mongodb.net/db-contacts";
-
-module.exports = { DB_HOST };
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = require("./config");
+const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set("strictQuery", "true");
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("DB connect sussess");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log("Server running.");
     });
   })
   .catch((error) => {
