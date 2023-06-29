f3a6338a7cbf01a3e52b69134cc9ee396d43a14d
Sldvlld <sladvlad@gmail.com>
2023-06-29 12:16:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
