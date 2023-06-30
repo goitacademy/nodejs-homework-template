711b09958c9eba1e5233e14bfe4b0431047e206a
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:37:16 +0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+};
