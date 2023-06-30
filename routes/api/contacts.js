e3e161aeed34db885ebf2bac4240483200c2dbde
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:34:23 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,31 +1,34 @@
 const express = require("express");
 
-const { validation, ctrlWrapper } = require("../../middlewares");
+const { auth, validation, ctrlWrapper } = require("../../middlewares");
 const { contactJoiSchema, favoriteJoiSchema } = require("../../schemas");
 const { contacts: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", auth, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", auth, ctrlWrapper(ctrl.getById));
 
 router.post(
   "/",
+  auth,
   validation(contactJoiSchema, "missing required name field"),
   ctrlWrapper(ctrl.add)
 );
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+router.delete("/:contactId", auth, ctrlWrapper(ctrl.removeById));
 
 router.put(
   "/:contactId",
+  auth,
   validation(contactJoiSchema, "missing fields"),
   ctrlWrapper(ctrl.updateById)
 );
 
 router.patch(
   "/:contactId/favorite",
+  auth,
   validation(favoriteJoiSchema, "missing field favorite"),
   ctrlWrapper(ctrl.updateFavorite)
 );
