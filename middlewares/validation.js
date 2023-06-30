4a806a259e038044cde89df56806c1ac792d13cf
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:33:48 +0300

    Update validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,7 +3,7 @@ const validation = (schema, message) => {
     const { error } = schema.validate(req.body);
     if (error) {
       error.status = 400;
-      error.message = message;
+      error.message = message || error.message;
       next(error);
     }
     next();
