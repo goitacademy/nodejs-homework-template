b8a3ab792ff9e6e3b86f771664664735f884ba2d
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:36:32 +0300

    Update updateFavorite.js

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -5,6 +5,7 @@ const { Contact } = require("../../models");
 const updateFavorite = async (req, res) => {
   const { contactId } = req.params;
   const { favorite } = req.body;
+
   const result = await Contact.findByIdAndUpdate(
     contactId,
     { favorite },
