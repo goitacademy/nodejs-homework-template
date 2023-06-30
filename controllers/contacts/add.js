8422cd5811f8aebe7960c1aa8d2910497aa55e77
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:36:15 +0300

    Update add.js

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,7 +1,16 @@
+const { Conflict } = require("http-errors");
+
 const { Contact } = require("../../models");
 
 const add = async (req, res) => {
-  const newContact = await Contact.create(req.body);
+  const { _id } = req.user;
+  const { name } = req.body;
+
+  const user = await Contact.findOne({ name });
+  if (user) {
+    throw new Conflict("The name is already in use");
+  }
+  const newContact = await Contact.create({ ...req.body, owner: _id });
   res
     .status(201)
     .json({ message: "contact successfully added", result: newContact });
