2609e6fb29c862ed0ab6e072c46939dc5c36c27b
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:32:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const usersRouter = require("./routes/api/users");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -14,6 +15,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
+app.use("/api/current", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
