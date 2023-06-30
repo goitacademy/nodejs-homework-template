00800e9040b67334bb929794c5a9104a9a49c309
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-06-30 14:37:49 +0300

    Update getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,10 +1,19 @@
 const { Contact } = require("../../models");
 
 const getAll = async (req, res) => {
-  const contacts = await Contact.find({}, "-createdAt -updatedAt");
+  const { _id } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const contacts = await Contact.find({ owner: _id }, "-createdAt -updatedAt", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id name email");
+
   if (!contacts) {
     return null;
   }
+
   res.status(200).json({
     message: "the request for all contacts was made successfully",
     result: { contacts },
