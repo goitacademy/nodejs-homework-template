3db2c5c48b0a116840958e1b36694b8ea51b71ec
andriy-h80 <andrey.hanzel@gmail.com>
2023-06-30 21:17:14 +0300

    step #1 done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,32 @@
 const express = require('express')
 
+const contacts = require('../../models/contacts')
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.listContacts();
+  res.json(result)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.getContactById();
+  res.json(result)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.addContact();
+  res.json(result)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.removeContact();
+  res.json(result)
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.updateContact();
+  res.json(result)
 })
 
 module.exports = router
