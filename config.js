17938ac297964ea897891dab8c9abe6a70a4566e
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-07-01 23:54:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DB_HOST = "mongodb+srv://kseniafihas:wzeHitRqZ7hp5Lg4@cluster0.ze08jaq.mongodb.net/contacts?retryWrites=true&w=majority";
+const DB_HOST = "mongodb+srv://kseniafihas:wzeHitRqZ7hp5Lg4@cluster0.ze08jaq.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 module.exports = {
     DB_HOST,
