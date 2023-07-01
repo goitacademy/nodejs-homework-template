ca283ed6593b3644f25a66dd57e856d3e51756e5
Yevhen Tsapko <evgentsa@gmail.com>
2023-07-01 22:49:13 +0300

    Update valideteBody.js

--- middlewares/valideteBody.js
+++ middlewares/valideteBody.js
@@ -2,8 +2,6 @@ const HttpError = require("../helpers/HttpError");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    console.log(schema);
-    console.log(error);
     if (error) {
       next(HttpError(400, error.message));
     }
