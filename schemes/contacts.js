bd6d62872d6f4f222a87343a0bc42cc9d5695050
Olexander Zakatov <sashazakatov17@gmail.com>
2023-07-01 22:22:14 +0300

    Update contacts.js

--- schemes/contacts.js
+++ schemes/contacts.js
@@ -4,6 +4,7 @@ const contactsScheme = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
+    favorite: Joi.boolean().required(),
 })
 
 module.exports = contactsScheme;
