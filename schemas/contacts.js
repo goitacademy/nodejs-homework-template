669de48ce1741beec7f0e1b9c9af8f387ac86d66
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-01 19:18:02 +0300

    fix

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,7 +4,7 @@ const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean(),
 });
 
 const updateFavoriteSchema = Joi.object({
