6785c1e57c9be550aa2e6ca07fa2a9ee698c7435
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-07-02 16:06:24 +0300

    Update validationFavorite.js

--- middlewares/validationFavorite.js
+++ middlewares/validationFavorite.js
@@ -2,8 +2,8 @@ const { requestError } = require("../utils");
 
 const validationFavorite = (schema) => {
   const func = (req, _, next) => {
-    const body = req.body;
-    const { error } = schema.validate(body);
+    // const body = req.body;
+    // const { error } = schema.validate(body);
     const filledBodyFavorite = Object.keys(req.body).length;
 
     if (!filledBodyFavorite) {
