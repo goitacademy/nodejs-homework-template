99a5742e2ba9e4d4392ce576733b4fa0f255c944
kirhal <kirillgaleckiy@gmail.com>
2023-07-02 16:06:20 +0200

    env

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const app = require('./app');
 
 const mongoose = require('mongoose');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() =>
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log('Database connection successful');
     })
   )
