47dec4cc58e0ab92c40b248f6bc176b842eb69ad
Oleksandr <alex.vovchok@gmail.com>
2023-07-02 12:17:00 +0300

    added bcrypt

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,14 +1,20 @@
+const bcrypt = require("bcrypt");
+
 const {User} = require("../../models/user");
+
 const {HttpError} = require("../../helpers");
 
 const register = async(req, res) => {
-    const {email} = req.body;
+    const {email, password} = req.body;
+
     const user = await User.findOne({email})
     if(user) {
         throw HttpError(409,"Email in use");
     }
     
-    const newUser = await User.create(req.body)
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const newUser = await User.create({...req.body, password: hashPassword})
 
     res.status(201).json({
         name: newUser.name,
