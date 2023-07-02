ae3f466871e45aa51b9d4e94ff2384abec13b78f
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-07-02 23:39:33 +0300

    in process

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Serhii:Wt1r1sid8CzBALat@cluster0.m8qwrou.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
