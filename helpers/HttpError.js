022a46de73fe1399f9d022a0dc86871bd31ff149
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-07-02 12:27:55 +0300

    fix

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,6 +1,22 @@
-const HttpError = (status, message) => {
-  const error = new Error(message);
-  error.status = status;
-  return error;
+const messages = {
+  200: "OK",
+  201: "Created",
+  204: "No Content",
+  400: "Client Error",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+  422: "Unprocessable entity",
+  500: "Internal server error",
+  default: "Something went wrong, please try again later...",
 };
+
+class HttpError extends Error {
+  constructor(status = 500, message = messages[status] || messages.default) {
+    super(message);
+    this.status = status;
+  }
+}
+
 module.exports = HttpError;
