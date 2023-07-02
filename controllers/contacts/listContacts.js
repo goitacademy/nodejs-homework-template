325c04380e3af9a88cf3a3fde22e98e044fd182c
OlgaMunaieva <asyssync@gmail.com>
2023-07-02 22:58:24 +0300

    pagination and filter

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,13 +2,20 @@ const Contact = require("../../models/contact");
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  // const { page = 1, limit = 10 } = req.query;
-  // const skip = (page - 1) * limit;
-  // const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-  //   skip,
-  //   limit,
-  // }).populate("owner", "name email");
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find(
+    { owner, favorite: true },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  );
+  // const result = await Contact.find(
+  //   { owner },
+  //   "-createdAt -updatedAt"
+  // ).populate("owner", "name email");
   res.json(result);
 };
 
