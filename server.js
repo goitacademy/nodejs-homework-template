61affaeb610f260944a0a8ea5ccebb1bd5991864
kirhal <kirillgaleckiy@gmail.com>
2023-07-02 15:01:20 +0200

    mongoose added

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose');
+
+const DB_HOST =
+  'mongodb+srv://babazela:J8ak5Puns@cluster0.3cynhsz.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log('Database connection successful');
+    })
+  )
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
