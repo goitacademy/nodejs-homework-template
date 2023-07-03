be0401d79ca076972c629ed8dd056fb0015b78db
Ruslan-Chulakov <AN_124_100@ukr.net>
2023-07-03 17:19:39 +0300

    hw-2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,67 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const { nanoid } = require("nanoid")
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+ const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(({ id }) => id === contactId);
 
-const addContact = async (body) => {}
+  if (index === -1) {
+    return null;
+  }
 
-const updateContact = async (contactId, body) => {}
+  return contacts.slice(index, index + 1);
+}
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+};
+
+const updateContact = async (id, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((el) => el.id === id);
+
+  if (index === -1) {
+    console.log("cant change element with such id");
+    return null;
+  }
+
+  contacts[index] = { id, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
+
+const removeContact = async (contactId) => {
+   const contacts = await listContacts();
+   const index = contacts.findIndex(({ id }) => id === contactId);
+
+   if (index === -1) {
+     return null;
+   }
+   const [result] = contacts.splice(index, 1);
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+   return result;
+}
+
+
+
+
 
 module.exports = {
   listContacts,
