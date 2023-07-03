d55f56caf694021dd9a330e5a79eb315639b8751
VerbitskaAlisa <v.alica@ukr.net>
2023-07-03 16:02:58 +0300

    add pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,9 @@ const { Contact } = require("../../models");
 
 const getAll = async (req, res, next) => {
       const {_id: owner} = req.user;
-      const result = await Contact.find({owner});
+      const {page = 1, limit = 20} = req.query;
+      const skip = (page - 1) * limit;
+      const result = await Contact.find({owner}, {skip, limit});
       res.status(200).json(result)
   }
 
