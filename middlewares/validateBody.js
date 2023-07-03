aad84bff7c3725b23db28867d892811b962941d3
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-03 18:03:19 +0300

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,6 +3,12 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+    if (!Object.keys(req.body).length) {
+      throw HttpError(
+        400,
+        req.route.methods.patch ? `missing field favorite` : `missing fields`
+      );
+    }
     if (error) {
       const requiredField = error.message
         .replace(`"`, "")
