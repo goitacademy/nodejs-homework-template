4ca2c616ce16ade40f17b3c4beea85615f9c7087
Ihor Karen <karenihor@gmail.com>
2023-07-04 11:54:53 +0300

    port

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
+
+const port = process.env.PORT || config.get("serverPort");
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(port);
     console.log("Database connection successful");
   })
   .catch((error) => {
