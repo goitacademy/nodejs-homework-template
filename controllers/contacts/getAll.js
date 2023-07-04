39aa7d3588cc418467c941757624784867070588
Inna Pokydko <inna.pokydko@gmail.com>
2023-07-04 16:13:58 +0300

    limit update

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,7 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const {_id: owner} = req.user;
-  const {page = 1, limit = 5} = req.query;
+  const {page = 1, limit = 20} = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "email");
   res.json(result);
