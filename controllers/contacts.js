3fe2a1733e37dbdb60a95ab4113e1a678700d85f
Anna <anytka141528@gmail.com>
2023-07-04 23:26:30 +0300

    done

---
+++ controllers/contacts.js
@@ -0,0 +1,48 @@
+const contacts = require("../models/contacts");
+
+const { HttpError, controlWrapper } = require("../helpers");
+
+const listContacts = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.getContactById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "Contact is deleted" });
+};
+
+module.exports = {
+  listContacts: controlWrapper(listContacts),
+  getContactById: controlWrapper(getContactById),
+  addContact: controlWrapper(addContact),
+  updateContact: controlWrapper(updateContact),
+  removeContact: controlWrapper(removeContact),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,19 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const controller = require("../../controllers/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validator } = require("../../middlewares");
+const scheme = require("../../schemes/contacts");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", controller.listContacts);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", controller.getContactById);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validator(scheme.objectStructure), controller.addContact);
+
+router.delete("/:id", validator(scheme.objectStructure), controller.updateContact);
+
+router.put("/:id", controller.removeContact);
 
 module.exports = router
