c58bc82f908e455f46a22adcecb23bac844c0255
polokir <lolak_228@ukr.net>
2023-07-04 11:37:54 +0300

    env aded

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const app = require('./app')
 const mongoose = require('mongoose');
+require('dotenv').config();
 const start = () => {
   app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
 
-mongoose.connect('mongodb+srv://kirilpolozhenets:1@mycluster.3zkaon9.mongodb.net/')
+mongoose.connect(process.env.DB_URL)
     .then(()=>console.log("DB connected"))
     .catch((err)=>console.log('DB connection ERROR',err));
 }
