c5e9cba8de12514709789b5bbb9d6df341bceee3
Inna Pokydko <inna.pokydko@gmail.com>
2023-07-04 14:55:16 +0300

    update schema

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,8 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const {_id: owner} = req.user;
+  const result = await Contact.find({owner}, "-createdAt -updatedAt").populate("owner", "email");
   res.json(result);
 };
 
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const asyncHandler = require("express-async-handler");
 
 const User = require("../models/users")
 
@@ -7,23 +6,23 @@ const HttpError = require("../helpers/HttpError");
 
 const { SECRET_KEY } = process.env;
 
-const authenticate = asyncHandler(async (req, res, next) => {
+const authenticate = async (req, res, next) => {
   const { authorization = " " } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    throw HttpError(401);
+    next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) {
-      throw HttpError(401);
+    if (!user) {
+      next(HttpError(401));
     }
     req.user = user;
     next();
   } catch (error) {
-    throw HttpError(401);
+    next(HttpError(401));
   }
-});
+};
 
 module.exports = authenticate;
