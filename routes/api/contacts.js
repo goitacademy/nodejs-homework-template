54cd9acb04162b795a16caf768ff137bdef02de0
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-07-04 14:41:26 +0300

    add all methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,31 @@ const express = require('express')
 
 const router = express.Router()
 
+const contacts = require('../../models/contacts')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.listContacts()
+  res.json(result)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    const result = await contacts.getContactById('qdggE76Jtbfd9eWJHrssH')
+  res.json(result)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.addContact({ name: 'Stanislav', email: 'pr', phone: '45' })
+  res.json(result)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.removeContact("uBOr5u4WwWe8bW9QK_EMA")
+  res.json(result)
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contacts.updateContact("e6ywwRe4jcqxXfCZOj_1")
+  res.json(result)
 })
 
 module.exports = router
