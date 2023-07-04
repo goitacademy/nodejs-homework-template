e48ca5d43ed32a33c3f7a19b617ad0a9e25eaa72
Dennis355 <Snidevichden@gmail.com>
2023-07-04 12:38:19 +0300

    fix_valid

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -3,7 +3,9 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+      res.status(400).json({ message: "missing fields" });
+    }
     if (error) {
       next(HttpError(400, error.message));
     }
--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,5 +1,5 @@
 const { isValidObjectId } = require("mongoose");
-// из mongoose берем свойство isValidObgectId у mongoose
+
 const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
