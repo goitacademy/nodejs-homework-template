e360871ad75d95ae3f0c1313630bb623d1babef5
Inna Pokydko <inna.pokydko@gmail.com>
2023-07-05 20:08:49 +0300

    add avatar

--- models/users.js
+++ models/users.js
@@ -1,3 +1,4 @@
+const { string } = require("joi");
 const { Schema, model } = require("mongoose");
 
 const handleMongooseError = require("../helpers/handleMongooseError");
@@ -18,16 +19,18 @@ const userSchema = new Schema(
       enum: ["starter", "pro", "business"],
       default: "starter",
     },
-    token: String,
-  },
-  { versionKey: false, timestamps: true }
+    token: {
+      String,
+    },
+    avatarURL: {
+      type: String,
+      required: true,
+    },
+  }, { versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", handleMongooseError);
 
-
 const User = model("user", userSchema);
 
 module.exports = User;
-
-
