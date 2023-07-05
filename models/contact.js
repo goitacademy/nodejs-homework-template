c74d1da42d75673947fd998770daf3827745bd44
olena-nikitchenko <helnikitka@gmail.com>
2023-07-05 21:18:07 +0300

    code refactoring

--- models/contact.js
+++ models/contact.js
@@ -33,7 +33,9 @@ const addSchema = Joi.object({
 });
 
 const updateStatusContactSchema = Joi.object({
-    favorite: Joi.boolean().required(),
+    favorite: Joi.boolean().required().messages({
+        'any.required': 'missing field favorite',
+    }),
 });
 
 const schemas = {
