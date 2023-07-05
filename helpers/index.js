0170e51f3892771a5cb14c6f50efea4a4cc252e0
Olena <anyanyme@gmail.com>
2023-07-05 17:20:59 +0200

    done hw-02

---
+++ helpers/index.js
@@ -0,0 +1,6 @@
+const HttpError = require("./HttpError");
+const controllerWrapper = require("./controllerWrapper");
+const validateData = require("./validateData");
+const checkBody = require("./checkBody");
+
+module.exports = { HttpError, controllerWrapper, validateData, checkBody };
---
+++ helpers/validateData.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+const HttpError = require("./HttpError");
+
+// Joi схема це опис вимог до обєкта
+const requiredFieldsSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  // номер телефону повинен бути у форматі (111) 111-1111
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/)
+    .required(),
+});
+
+const validateData = (request, response, next) => {
+  const { error } = requiredFieldsSchema.validate(request.body);
+  if (error) {
+    const fieldName = error.details[0].path[0];
+    throw HttpError(400, `missing required ${fieldName} field`);
+  }
+  next();
+};
+
+module.exports = validateData;
