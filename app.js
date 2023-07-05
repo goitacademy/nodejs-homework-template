081106019bd66fc677b5806990c3ff44c9886054
Sergey Donyk <doncarlos@i.ua>
2023-07-05 23:42:06 +0300

    addMongooseToServer

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+// 6aW8ACM88Ob9FowD, Sergey
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const DB_HOST = "mongodb+srv://Sergey:6aW8ACM88Ob9FowD@cluster0.v3b1qey.mongodb.net/db-contacts"
+mongoose.set('strictQuery', true)
+mongoose.connect(DB_HOST)
+.then(() => console.log('Connected to db-contacts'))
+.catch(error => console.log(error.message))
 
 const contactsRouter = require('./routes/api/contacts')
 
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./app')
-
-app.listen(3000, () => {
+const mongoose = require("mongoose")
+const DB_HOST = "mongodb+srv://Sergey:6aW8ACM88Ob9FowD@cluster0.v3b1qey.mongodb.net/db-contacts"
+mongoose.connect(DB_HOST)
+mongoose.set('strictQuery', true)
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000)
   console.log("Server running. Use our API on port: 3000")
 })
+.catch(error => {
+  console.log(error.message)
+  process.exit(1)
+})
+
+// console.log("Server running. Use our API on port: 3000")
