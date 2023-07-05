21ec7d104fdcd59eb917f3514e8cb2e0927a25e0
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-07-05 10:42:59 +0200

    Add error handling

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,51 +4,76 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json(await contactsModel.listContacts());
+  try {
+    res.json(await contactsModel.listContacts());
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const contact = await contactsModel.getContactById(req.params.contactId);
-  if (contact) {
-    res.json(contact);
-    return;
-  }
-  res.status(404).json({ message: "Not found" });
-});
-
-router.post("/", async (req, res, next) => {
-  const contact = { name: req.body.name, email: req.body.email, phone: req.body.phone };
-  const validationResult = contactsUtils.validateContact(contact);
-  if (validationResult.error === undefined) {
-    const addedContact = await contactsModel.addContact(contact);
-    res.status(201).json(addedContact);
-    return;
-  }
-  res.status(400).json({ message: validationResult.error });
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  const removed = await contactsModel.removeContact(req.params.contactId);
-  if (removed) {
-    res.json({ message: "contact deleted" });
-    return;
-  }
-  res.status(404).json({ message: "Not found" });
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  const contact = { name: req.body.name, email: req.body.email, phone: req.body.phone };
-  const validationResult = contactsUtils.validateContact(contact);
-  if (validationResult.error === undefined) {
-    const updatedContact = await contactsModel.updateContact(req.params.contactId, contact);
-    if (updatedContact) {
-      res.status(200).json(updatedContact);
+  try {
+    const contact = await contactsModel.getContactById(req.params.contactId);
+    if (contact) {
+      res.json(contact);
       return;
     }
     res.status(404).json({ message: "Not found" });
-    return;
+  } catch (e) {
+    console.log(e.message);
+    next(e);
   }
-  res.status(400).json({ message: validationResult.error });
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const contact = { name: req.body.name, email: req.body.email, phone: req.body.phone };
+    const validationResult = contactsUtils.validateContact(contact);
+    if (validationResult.error === undefined) {
+      const addedContact = await contactsModel.addContact(contact);
+      res.status(201).json(addedContact);
+      return;
+    }
+    res.status(400).json({ message: validationResult.error });
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const removed = await contactsModel.removeContact(req.params.contactId);
+    if (removed) {
+      res.json({ message: "contact deleted" });
+      return;
+    }
+    res.status(404).json({ message: "Not found" });
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const contact = { name: req.body.name, email: req.body.email, phone: req.body.phone };
+    const validationResult = contactsUtils.validateContact(contact);
+    if (validationResult.error === undefined) {
+      const updatedContact = await contactsModel.updateContact(req.params.contactId, contact);
+      if (updatedContact) {
+        res.status(200).json(updatedContact);
+        return;
+      }
+      res.status(404).json({ message: "Not found" });
+      return;
+    }
+    res.status(400).json({ message: validationResult.error });
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
 });
 
 module.exports = router;
