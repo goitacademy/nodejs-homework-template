35b478205da84270dbdd1ae05c7c051b41a10a0f
Crash-afftar <sashakhymenko88@gmail.com>
2023-07-05 23:35:04 +0300

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -24,3 +25,5 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+// N4gkynSsXmdIAT0n;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Aleksandr:N4gkynSsXmdIAT0n@cluster0.zocznma.mongodb.net/contacts-reader?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connections successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
