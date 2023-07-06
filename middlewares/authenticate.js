05d459d9b7cde693209304b44093af3e3368d90d
Oleksandr <alex.vovchok@gmail.com>
2023-07-06 17:32:48 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -15,7 +15,7 @@ const authenticate = async (req, res, next) => {
     try {
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        if(!user || !user.token || user.token !== token) {
+        if(!user.token) {
             next(HttpError(401));
         }
         req.user = user;
