5ab89c8403486e662c96f752d9fb94da92d69aa9
Andrew Ketsko <monicman82@gmail.com>
2023-07-06 13:04:24 +0300

    joi error

--- middleware/validateBody.js
+++ middleware/validateBody.js
@@ -1,17 +1,31 @@
 const Joi = require("joi");
 const contactSchema = Joi.object({
-  name: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(10).max(15).required(),
+  name: Joi.string()
+    .min(3)
+    .required()
+    .error(() => new Error("name")),
+  email: Joi.string()
+    .email()
+    .required()
+    .error(() => new Error("email")),
+  phone: Joi.string()
+    .min(10)
+    .max(15)
+    .required()
+    .error(() => new Error("phone")),
 });
 
 const validateBody = (req, res, next) => {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      res.status(400).json({ message: `${error.message} field` });
-    } else {
-      next();
-    }
-  };
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    // const [value] = error.message.match(/"\w*"/);
+    // console.log(value);
+    res
+      .status(400)
+      .json({ message: `missing required ${error.message} field` });
+  } else {
+    next();
+  }
+};
 
 module.exports = validateBody;
