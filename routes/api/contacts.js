cdafab7bcdacfe6f1fe9aece1b44f3a51b62f3e7
angelkapv81 <angelkapv81@gmail.com>
2023-07-06 19:45:58 +0300

    add error codes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,89 @@
+// contacts.js
+
 const express = require("express");
-const contacts = require("../../models/contacts");
+
 const router = express.Router();
+const contacts = require("../../models/actions");
 
 router.get("/", async (req, res, next) => {
-    // res.json({ message: "template message" });
-    res.json(await contacts.listContacts());
+    const allContacts = await contacts.listContacts();
+    res.status(200).json(allContacts);
 });
 
 router.get("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
-    // res.json({ message: `${contactId}` });
-    res.json(await contacts.getContactById(contactId));
+
+    // Виклик функції для отримання контакту за його id
+    const contact = await contacts.getContactById(contactId);
+
+    // Перевірка наявності контакту
+    if (!contact) {
+        return res.status(404).json({ message: "Not found" });
+    }
+
+    // Відправка відповіді з контактом та статусом 200
+    res.status(200).json(contact);
 });
 
-router.post("/", async (req, res, next) => {});
+router.use(express.urlencoded({ extended: true }));
+router.post("/", async (req, res, next) => {
+    const { name, email, phone } = req.body;
+
+    // Перевірка наявності обов'язкових полів
+    if (!name || !email || !phone) {
+        return res.status(400).json({ message: "missing required fields" });
+    }
+
+    // Створення нового контакту з унікальним ідентифікатором
+    const newContact = {
+        id: generateUniqueId(),
+        name,
+        email,
+        phone,
+    };
+
+    // Виклик функції додавання контакту
+    const addedContact = await contacts.addContact(newContact);
+
+    // Відправка відповіді з доданим контактом та статусом 201
+    res.status(201).json(addedContact);
+});
 
 router.delete("/:contactId", async (req, res, next) => {
-    res.json({ message: "template message" });
+    const { contactId } = req.params;
+
+    const deletedContact = await contacts.removeContact(contactId);
+
+    // Перевірка наявності видаленого контакту
+    if (!deletedContact) {
+        return res.status(404).json({ message: "Not found" });
+    }
+
+    res.json({ message: "contact deleted" });
 });
 
+router.use(express.urlencoded({ extended: true }));
 router.put("/:contactId", async (req, res, next) => {
-    res.json({ message: "template message" });
+    const { contactId } = req.params;
+    const { name, email, phone } = req.body;
+
+    // Перевірка наявності тіла запиту
+    if (!name || !email || !phone) {
+        return res.status(400).json({ message: "missing fields" });
+    }
+
+    // Виклик функції оновлення контакту
+    const updatedContact = await contacts.updateContact(contactId, {
+        name,
+        email,
+        phone,
+    });
+
+    // Перевірка результату оновлення контакту
+    if (!updatedContact) {
+        return res.status(404).json({ message: "Not found" });
+    }
+    res.json(updatedContact);
 });
 
 module.exports = router;
