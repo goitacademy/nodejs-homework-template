b5a194423e52608717e1ef4d0f5f83a33be3f074
YuliaOd <106181095+YuliaOd@users.noreply.github.com>
2023-07-06 15:54:39 +0300

    06.07

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const app = require('./app')
 
 const mongoose = require('mongoose')
-const DB_HOST = 'mongodb+srv://Yulia:GX8OVh82Qbiqe3gN@cluster0.kyn9nif.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+
+const {DB_HOST} = process.env
+
 
 mongoose.connect(DB_HOST)
 .then(() => {
-  app.listen(3000)
+  console.log('Database connection successful');
+  app.listen(3000);
 })
 .catch(error => {
   console.log(error.message);
