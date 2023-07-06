9833b4268ce502a99e01c3ea6fdb99361e050434
andriy-h80 <andrey.hanzel@gmail.com>
2023-07-06 14:43:38 +0300

    fixed some bugs

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,6 +2,9 @@ const {HttpError} = require("../helpers");
 
 const validateBody = schema => {
     const func = (req, res, next) => {
+        if(Object.keys(req.body).length === 0){
+            next(HttpError(400, "missing field"));
+        }
         const { error } = schema.validate(req.body);
         if(error){
             const fieldName = error.details[0].context.label;
