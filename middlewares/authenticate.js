33822c538ee73006884ef9874263472dcdac12cb
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-07-06 13:40:10 +0300

    Update authenticate-middleware
    Added check for the presence of a token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,7 +10,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = '' } = req.headers;
   const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== 'Bearer') {
+  if (bearer !== 'Bearer' || !token) {
     next(HttpError(401));
   }
 
