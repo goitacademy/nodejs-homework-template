2e8cffffb829602268927da921aba4a9b94a8dc3
kirhal <kirillgaleckiy@gmail.com>
2023-07-07 17:22:08 +0200

    double email err

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -5,7 +5,13 @@ const { HttpError, ctrlWrapper } = require('../../helpers');
 const register = async (req, res, next) => {
   const newUser = await User.create(req.body);
 
-  res.status(201).json({ email: newUser.email, password: newUser.password });
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      password: newUser.password,
+      subscription: newUser.subscription,
+    },
+  });
 };
 
 module.exports = { register: ctrlWrapper(register) };
--- helpers/handleMongooseErr.js
+++ helpers/handleMongooseErr.js
@@ -1,5 +1,12 @@
 const handleMongooseError = (error, data, next) => {
-  error.status = 400;
+  const { name, code } = error;
+  const status = name === 'MongoServerError' && code === 11000 ? 409 : 400;
+  const errMessage =
+    name === 'MongoServerError' && code === 11000
+      ? 'Email in use'
+      : error.message;
+  error.status = status;
+  error.message = errMessage;
   next();
 };
 
