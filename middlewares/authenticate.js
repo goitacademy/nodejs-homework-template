79b02b50c3d7e5e6df8b9d10d5004254349742ad
Ihor Karen <karenihor@gmail.com>
2023-07-07 14:05:13 +0300

    token error

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,18 +10,18 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer" && token) {
-    next(HttpError(401));
+    throw HttpError(401);
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token || user.token !== token) {
-      next(HttpError(401));
+      throw HttpError(401);
     }
     req.user = user;
     next();
   } catch {
-    next(HttpError(401));
+    throw HttpError(401);
   }
 };
 
