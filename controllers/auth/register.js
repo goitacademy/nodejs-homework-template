7077a57369d9ca4e27632bf2570d970da17e23d4
kirhal <kirillgaleckiy@gmail.com>
2023-07-07 17:46:38 +0200

    createHashPassword bcrypt

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,7 +2,21 @@ const { User } = require('../../models/user');
 
 const { HttpError, ctrlWrapper } = require('../../helpers');
 
+const bcrypt = require('bcypt');
+
+const createHashPassword = async password => {
+  const salt = await bcrypt.genSalt(10);
+  const result = await bcrypt.hash(password, salt);
+};
+
 const register = async (req, res, next) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, 'Email in use');
+  }
+
   const newUser = await User.create(req.body);
 
   res.status(201).json({
--- helpers/handleMongooseErr.js
+++ helpers/handleMongooseErr.js
@@ -1,10 +1,6 @@
 const handleMongooseError = (error, data, next) => {
   const { name, code } = error;
   const status = name === 'MongoServerError' && code === 11000 ? 409 : 400;
-  const errMessage =
-    name === 'MongoServerError' && code === 11000
-      ? 'Email in use'
-      : error.message;
   error.status = status;
   error.message = errMessage;
   next();
