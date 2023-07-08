121f1e7361c0f6e65ce78e6922952c9b268d8e67
olena-nikitchenko <helnikitka@gmail.com>
2023-07-09 02:35:13 +0300

    added files tu step_3-5

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -40,10 +40,30 @@ const login = async (req, res) => {
         id: user._id,
     };
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+    await User.findByIdAndUpdate(user._id, { token });
     res.json({ token });
+};
+
+const getCurrent = async (req, res) => {
+    const { email, password } = req.user;
+    res.json({
+        email,
+        password,
+    });
+};
+
+const logout = async (req, res) => {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { token: '' });
+
+    res.json({
+        message: 'Logout success',
+    });
 };
 
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,19 +4,25 @@ const router = express.Router();
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody, isValidId } = require('../../middlewares');
+const { validateBody, isValidId, authenticate } = require('../../middlewares');
 
 const { schemas } = require('../../models/contact');
 
-router.get('/', ctrl.listContacts);
+router.get('/', authenticate, ctrl.listContacts);
 
-router.get('/:contactId', isValidId, ctrl.getContactById);
+router.get('/:contactId', authenticate, isValidId, ctrl.getContactById);
 
-router.post('/', validateBody(schemas.addSchema), ctrl.addContact);
+router.post('/', authenticate, validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete('/:contactId', isValidId, ctrl.removeContact);
+router.delete('/:contactId', authenticate, isValidId, ctrl.removeContact);
 
-router.put('/:contactId', isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
+router.put(
+    '/:contactId',
+    authenticate,
+    isValidId,
+    validateBody(schemas.addSchema),
+    ctrl.updateContact
+);
 router.patch(
     '/:contactId/favorite',
     isValidId,
