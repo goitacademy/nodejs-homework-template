9289075b37db17f15fca19e30fdd0299b28ef503
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-07-08 13:19:18 +0300

    Updating

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-const mongoose = require('mongoose');
+const app = require('./app')
 
-const app = require('./app'); 
-const e = require('express');
-
-const DB_Host = "mongodb+srv://Ishtvan:Us3XPk10iJjh6ELN@cluster0.tg7suj0.mongodb.net/contacts_finder?retryWrites=true&w=majority"
-
-mongoose.set('strictQuery', true);
-
-mongoose.connect(DB_Host)
-  .then(() => {
-    app.listen(3000)
-})
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000")
 })
