4d5cbe6aacad3a3c610b81f4fee4e2cadba6e146
Oleksandr <alex.vovchok@gmail.com>
2023-07-08 22:45:17 +0300

    new avatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -2,13 +2,14 @@ const path = require("path");
 
 const fs = require("fs/promises");
 
-const {User} = require("../../models/user")
+const {User} = require("../../models/user");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async(req, res) => {
     const{_id} = req.user;
     const{path: tempUpload, originalname} = req.file;
+ 
     const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
