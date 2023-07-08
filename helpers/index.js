b7a1b2a322f8d56bd5eeec5f01ec1cb38472db1c
Siracenco Serghei <siracencoserghei@gmail.com>
2023-07-08 20:14:54 +0200

    completed the minimum requirement

---
+++ helpers/index.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const httpError = require(path.join(__dirname, 'httpError'))
+
+module.exports = {
+    httpError
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,56 +1,114 @@
 const express = require("express");
+const Joi = require("joi")
 const path = require("path");
-
+const { httpError} = require(path.resolve(__dirname, "../../helpers"))
 const {
   listContacts,
   getContactById,
   addContact,
   updateContact,
   removeContact,
-} = require(path.join(__dirname, "..", "..", "models", "contacts"));
+} = require(path.resolve(__dirname, "../../models/contacts"));
+
+const addSchema = Joi.object({
+  name: Joi.string()
+  .trim()
+  .required(),
+  email: Joi.string()
+  .trim()
+  .email()
+  .required(),
+  phone: Joi.string()
+  .trim()
+  .regex(/^\+\d{1}\d{8,15}$/)
+  .messages({
+    'string.pattern.base': 'Phone number must start with a plus sign (+) and have 9 to 16 digits.',
+  })
+  .required()
+});
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const contacts = await listContacts();
-  const message = "your contacts: ";
-  res.json({ message, contacts });
+  try {
+    const contacts = await listContacts();
+    if(!contacts) {
+      throw httpError(404, "Not found")
+    }
+    res.status(200).json({ contacts });
+    
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
+  try {
   const contactId = req.params.contactId; // Получаем значение contactId из параметров запроса
   const contactById = await getContactById(contactId);
-  const message = "your contactById: ";
-  res.json({ message, contactById });
+  if(!contactById) {
+    throw httpError(404, "Not found")
+  }
+  res.status(200).json({ contactById });
+
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body; // Извлекаем данные из тела запроса
+  try {
+    const {error} = addSchema.validate(req.body)
+    if(error) {
+      throw httpError(400, error.message)
+    }
+    const { name, email, phone } = req.body; // Извлекаем данные из тела запроса
   // console.log(req.body)
   const newContact = await addContact({ name, email, phone }); // Передаем параметр contact
-  const message = "Your new contact: ";
-  res.json({ message, newContact });
+
+  res.status(201).json({ newContact });
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const contactId = req.params.contactId; // Получаем значение contactId из параметров запроса
-  const removedById = await removeContact(contactId);
-  const message = "your removed contactById: ";
-  res.json({ message, removedById });
+  try {
+    const contactId = req.params.contactId; // Получаем значение contactId из параметров запроса
+    const removedById = await removeContact(contactId);
+
+    if(!removedById) {
+      throw httpError(404, "Not found")
+    }  
+    res.json({
+      message: "Delete success"
+    })  
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  // console.log(req.body, req.params)
-
+  try {
+    const {error} = addSchema.validate(req.body)
+    if(error) {
+      throw httpError(400, error.message)
+    }
   const contactId = req.params.contactId; // Получаем значение contactId из параметров запроса
   const { name, email, phone } = req.body; // Извлекаем данные из тела запроса
 
   const newContact = { name, email, phone }; // Создаем объект newContact
 
   const updatedContact = await updateContact(contactId, newContact); // Передаем параметры contactId и newContact
+  if(!updatedContact) {
+    throw httpError(404, "Not found")
+  }
 
-  const message = "your updated contactById: ";
-  res.json({ message, updatedContact });
+  res.json({ updatedContact });
+
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
