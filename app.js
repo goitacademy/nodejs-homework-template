5951debdcc9446f4643b8451f50192cf0830e542
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-07-08 22:16:53 +0300

    change auth into users route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const contactsRouter = require("./routes/api/contacts");
-const authRouter = require("./routes/api/auth")
+const authRouter = require("./routes/api/users")
 
 const app = express();
 
