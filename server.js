18bfeb616f29f476169af8b96d28b936df1b6b27
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2023-07-08 13:41:30 +0300

    finish

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const app = require('./app')
 
-app.listen(3000, () => {
+// const PORT = 5003
+// app.listen(PORT, ()=>{
+//   console.log("Server running. Good job!!! Use our API on port: 5000")
+// })
+
+app.listen(3003, () => {
   console.log("Server running. Good job! Use our API on port: 3000")
 }) 
+
+console.log(__dirname)
