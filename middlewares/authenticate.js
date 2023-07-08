df95c1ac21d66384de1ad9033b475a3609fcd6c6
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-07-08 08:30:01 +0300

    chek token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,7 +10,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
 
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (token && bearer !== "Bearer") {
     next(HttpError(401));
   }
 
