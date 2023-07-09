b691aa847e1ab755e38a391c77ee26f640e4e86a
Your Name <pavlo.morozenko@ukr.net>
2023-07-09 14:59:34 +0300

    create upload.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,8 @@
 const { HttpError } = require('../helpers');
 const { User } = require('../models/user');
+
 const jwt = require('jsonwebtoken');
+
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
@@ -8,18 +10,18 @@ const authenticate = async (req, res, next) => {
     const [bearer, token] = authorization.split(' ');
 
     if (!token || bearer !== 'Bearer') {
-        next(HttpError(401));
+        next(HttpError(401, 'Not authorized'));
     }
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if (!user || !user.token || user.token !== token) {
-        next(HttpError(401));
+        next(HttpError(401, 'Not authorized'));
         }
         req.user = user;
         next();
     } catch (error) {
-        next(HttpError(401));
+        next(HttpError(401, 'Not authorized'));
     }
 };
 
