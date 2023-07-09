25a33d4c1dcb16b95d9ddd1105e285f8342ea37e
Siracenco Serghei <siracencoserghei@gmail.com>
2023-07-09 21:40:34 +0200

    add mistake deleted string

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,23 @@
+const express = require("express");
 
+const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
 
-  const ctrl =require("../../controllers/contacts")
-  const {validateBody} = require("../../middlewares")
-  const schemas = require("../../schemas/contacts")
+const router = express.Router();
 
-  const router = express.Router();
+router.get("/", ctrl.getAll);
 
-  router.get("/", ctrl.getAll);
+router.get("/:contactId", ctrl.getById);
 
-  router.get("/:contactId", ctrl.getById);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-  router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.delete("/:contactId", ctrl.deleteById);
 
-  router.delete("/:contactId", ctrl.deleteById);
+router.put(
+  "/:contactId",
+  validateBody(schemas.addSchema),
+  ctrl.updateContactById
+);
 
-  router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateContactById);
-  
-  module.exports = router;
+module.exports = router;
