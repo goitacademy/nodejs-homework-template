b6262ad97bdbbbff83bb3fffd2d06799b0cec021
Dmytro <DSerSmart@gmail.com>
2023-07-09 20:41:33 +0300

    hw3

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const app = require('./app')
+const app = require("./app");
 const mongoose = require("mongoose");
-
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.set('strictQuery',false);
+mongoose.set("strictQuery", false);
 mongoose
-  .connect(DB_HOST, {useNewUrlParser: true})
+  .connect(DB_HOST, { useNewUrlParser: true })
   .then(() =>
     app.listen(PORT, () => {
       console.log("Database connection successful");
@@ -20,3 +19,4 @@ mongoose
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000")
 // })
+//hw3
