b7b8ef5e6bbafb32b99952cf2bfe6134ed510c2e
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-07-09 20:54:35 +0300

    Updating

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+//WXyY!9yq6EH7K%_
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts');
+const contactsRouter = require("./routes/api/contacts");
+
+require('dotenv').config();
 
 const app = express();
 
@@ -13,8 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
-
-app.listen(3000);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
