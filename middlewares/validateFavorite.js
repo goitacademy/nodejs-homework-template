15b941107b7c66ec5dade9ba70c8be95dfe61305
Tytula Oleksandr <alekstitula84@gmail.com>
2023-07-09 18:20:18 +0300

    Update validateFavorite.js

--- middlewares/validateFavorite.js
+++ middlewares/validateFavorite.js
@@ -1,7 +1,7 @@
 const { RequestError } = require("../helpers");
 
 const validateFavorite = (req, res, next) => {
-    if (!req.body || Object.keys(req.body).length === 0) {
+    if (!req.body || Object.keys(req.body).length === undefined) {
         throw RequestError(400, "missing field favorite");
     }
     next();
