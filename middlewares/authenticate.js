5d423338d5c2996967f97f5912bcd8a753ab02fe
olena-nikitchenko <helnikitka@gmail.com>
2023-07-09 20:46:50 +0300

    fixed code

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,20 +8,23 @@ const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
     const { authorization = '' } = req.headers;
+    if (!authorization || authorization === '') {
+        return next(HttpError(401, 'No token provided'));
+    }
     const [bearer, token] = authorization.split(' ');
-    if (bearer !== 'Bearer') {
-        next(HttpError(401));
+    if (bearer !== 'Bearer' || !token) {
+        next(HttpError(401, 'Invalid token format'));
     }
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if (!user || !user.token || user.token !== token) {
-            next(HttpError(401));
+            next(HttpError(401, 'Invalid token'));
         }
         req.user = user;
         next();
     } catch {
-        next(HttpError(401));
+        next(HttpError(401, 'Invalid token'));
     }
 };
 
