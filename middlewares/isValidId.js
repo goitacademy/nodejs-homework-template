ed939a3ecc1af560265c6ceeaa6d68b9c8fbcd47
Roman Petrenko <rpetrenko0105@gmail.com>
2023-07-09 15:54:24 +0300

    fix: відредаговує назву функції HttpError

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,9 +1,9 @@
 const { isValidObjectId } = require("mongoose");
-const { HttpErrors } = require("../helpers/HttpError");
+const { HttpError } = require("../helpers/HttpError");
 
 const isValidId = (req, res, next) => {
   if (!isValidObjectId(req.params)) {
-    next(HttpErrors(400, `${req.params} is not valid id`));
+    next(HttpError(400, `${req.params} is not valid id`));
   }
 
   next();
