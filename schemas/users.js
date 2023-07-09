148dcfed987f9bc94c3df3c0d043568d0538328c
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-09 12:39:53 +0300

    fix

--- schemas/users.js
+++ schemas/users.js
@@ -1,16 +1,14 @@
 const Joi = require("joi");
 
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
 const registerSchema = Joi.object({
   name: Joi.string().required(),
   password: Joi.string().min(8).required(),
-  email: Joi.string().pattern(emailRegex).required(),
+  email: Joi.string().required(),
 });
 
 const loginSchema = Joi.object({
   password: Joi.string().min(8).required(),
-  email: Joi.string().pattern(emailRegex).required(),
+  email: Joi.string().required(),
 });
 
 module.exports = {
