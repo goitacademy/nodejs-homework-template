2dfb949ddef193329f6dee7c6baedf737fd4646e
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-07-09 19:22:38 +0300

    hw-03 done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,91 +1,17 @@
 const express = require("express");
 
-const contacts = require("../../models/contactsApi");
 const router = express.Router();
-const Joi = require("joi");
+const ctrl = require("../../controllers/contacts")
 
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+router.get("/", ctrl.getAll)
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contact = await contacts.listContacts();
-    res.json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:contactId", ctrl.getById)
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    console.log(req.params);
-    const { contactId } = req.params;
+router.post("/",ctrl.addContact)
 
-    const result = await contacts.getContactById(contactId);
+router.delete("/:contactId", ctrl.deletecontact)
 
-    if (!result) {
-      const error = new Error("Not found");
-      error.status = 404;
-      throw error;
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:contactId", ctrl.updateContact)
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      error.message = "required filelds not exist";
-      throw error;
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = contacts.removeContact(contactId);
-    if (!result) {
-      const error = new Error("Not found");
-      error.status = 404;
-      throw error;
-    }
-    res.json({ message: "Delete success" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      error.message = "required filelds not exist";
-      throw error;
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      const error = new Error("Not found");
-      error.status = 404;
-      throw error;
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
+router.patch("/:contactId/favorite", ctrl.updateFavorite)
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const app = require('./app')
+const mongoose = require("mongoose")
+const dbHost = 'mongodb+srv://oleg:zhuk2012@cluster0.ojamrqw.mongodb.net/contacts_reader?retryWrites=true&w=majority'
+mongoose.set('strictQuery', true)
+
+
+mongoose.connect(dbHost)
+.then(()=>{
+  console.log("Database connection successful")
+})
+.catch(error=>{
+  console.log(error.message);
+  process.exit(1)
+})
+
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
