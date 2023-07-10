ca4870655d82bf84924678924acfea65bf59b8f2
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-10 21:07:48 +0300

    Update getContactById.js

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -1,21 +1,20 @@
-const contactsOperations = require('../../models/contacts')
+const { Contact } = require("../../models");
 
 const getContactById = async (req, res) => {
-       const {id} = req.params;
-    const result = await contactsOperations.getContactById(id);
-    if(!result){
-      const error = new Error(`Contact with id=${id} not found`);
-      error.status = 404;
-      throw error;
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-      result
-      }
-    });
-  } 
-  
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  if (!result) {
+    const error = new Error(`Contact with id=${id} not found`);
+    error.status = 404;
+    throw error;
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
+};
 
-  module.exports = getContactById
+module.exports = getContactById;
