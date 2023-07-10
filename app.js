32d4dfdf9226df6abc75c3bbbe1c4e388998bb5f
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-10 15:35:44 +0300

    fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-app.use("/api/auth", authRouter);
+app.use("/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
