0c6aec9091f815a76909c8d8c786ebda55cd3571
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-07-10 09:18:39 +0300

    function rest

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,36 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("./contacts");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/api/contacts", async (req, res, next) => {
+  res.json({ message: "template message" });
+  await listContacts();
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/api/contacts/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+  await getContactById(req);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/api/contacts", async (req, res, next) => {
+  res.json({ message: "template message" });
+  await addContact(req);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/api/contacts/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+  await removeContact(req);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/api/contacts/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+  await updateContact(req, res);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
