613c13be9ee7589782a1e3e37be6ad244dbcb990
Crash-afftar <sashakhymenko88@gmail.com>
2023-07-10 23:05:30 +0300

    update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -35,7 +35,7 @@ const updateById = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
-    throw new HttpError(404, 'Not found');
+    throw new HttpError(404, "Not found");
   }
   res.json(result);
 };
--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,7 @@ const validateBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      next(new HttpError(400, error.message));
     }
     next();
   };
