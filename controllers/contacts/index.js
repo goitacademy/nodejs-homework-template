bc3a1f55ca9c7abb56776b4803c9db32a286162e
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-10 21:07:51 +0300

    Update index.js

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -3,11 +3,13 @@ const getContactById = require('./getContactById');
 const addContact = require('./add');
 const updateById = require('./updateById');
 const removeById = require('./removeById');
+const updateStatus = require('./updateStatus');
 
 module.exports = {
     getAll,
     getContactById,
     addContact,
     updateById,
-    removeById
+    removeById,
+    updateStatus
 }
