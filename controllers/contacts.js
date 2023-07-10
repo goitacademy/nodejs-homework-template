ee384452aec6f36f7e117682ca24a45fafc48008
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-07-10 14:39:52 +0300

    added pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   // console.log(result);
   res.json(result);
 };
@@ -20,7 +25,7 @@ const getById = async (req, res) => {
 const add = async (req, res) => {
   const { _id: owner } = req.user;
   const result = await Contact.create({ ...req.body, owner });
-  // console.log(result);
+  console.log(result);
   res.status(201).json(result);
 };
 const deleteById = async (req, res) => {
