a62ff4f0a55f5fe3bb136f37dd4cc041695d96b7
Davydiuk Dmytro <davudyuck0501@gmail.com>
2023-07-11 15:38:17 +0300

    Configured the connection to the database

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const app = require('./app');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST;
+
+mongoose.Promise = global.Promise;
+
+const connectionDB = mongoose.connect(DB_HOST);
+
+connectionDB
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server running. Use our API on port: ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log(`Server not running. Error message: ${err.message}`);
+		process.exit(1);
+	});
