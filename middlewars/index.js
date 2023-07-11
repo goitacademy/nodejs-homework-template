59d7ae6c034669da1a2b69fb53004d3f4c80bfe6
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2023-07-11 13:22:26 +0300

    finish

---
+++ middlewars/index.js
@@ -0,0 +1,4 @@
+const validateBody = require('./validationBody');
+const isValidId = require('./isValidId');
+const validateFavorite = require('./valedationBodyFavorite');
+module.exports = { validateBody, isValidId, validateFavorite };
