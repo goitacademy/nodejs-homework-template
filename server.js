52030c742ca8d493e21e4c701358db3e445bc310
SerhiiShymko <shimko456@gmail.com>
2023-07-11 16:23:43 +0300

    comit

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+//@ GET /api/contacts
+
+//@ GET /api/contacts/:id
+
+//@ POST /api/contacts
+
+//@ DELETE /api/contacts/:id
+
+//@ PUT /api/contacts/:id
