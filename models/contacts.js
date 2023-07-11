ff16e1aebc3be2f99ece74d00ac504a6869065b5
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-07-11 21:08:33 +0300

    s

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-const { nanoid } = require("nanoid");
+// const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
