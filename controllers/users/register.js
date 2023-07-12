3cb7cef214567767300b2a3ff5d220a77ec66e60
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-12 12:23:34 +0300

    fix

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -3,22 +3,19 @@ const { HttpError } = require("../../helpers");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res, next) => {
-  try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user) {
       throw HttpError(409, "Email in use");
     }
-    
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({
-      ...req.body,
-      password: hashedPassword,
+    const newUser = await User.create({ ...req.body, password: hashedPassword });
+    res.status(201).json({
+      user: {
+        email: newUser.email,
+        subscription: "starter",
+      },
     });
-    res.status(201).json({ email: newUser.email, subscription: "starter" });
-  } catch (error) {
-    next(error);
-  }
-};
+  };
 
 module.exports = register;
