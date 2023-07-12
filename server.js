44b2bc9c4949553e1ef99e1b808170b0dae520c2
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-07-12 12:53:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = require("./app");
 const { DB_HOST } = process.env;
 
 mongoose
-  .connect(DB_HOST, { useNewUrlParser: true })
+  .connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(3000, () => {
       console.log("Database connection successful");
