72591f422991ea687977ad02d8ddf5729a45539a
kirhal <kirillgaleckiy@gmail.com>
2023-07-12 17:22:06 +0200

    prepare

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs/promises');
+
+const tempDir = path.join(__dirname, 'tmp');
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: multerConfig });
+
 const authRouter = require('./routes/api/auth');
 const contactsRouter = require('./routes/api/contacts');
 
@@ -13,6 +28,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/users', authRouter);
 app.use('/api/contacts', contactsRouter);
