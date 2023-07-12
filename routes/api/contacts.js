4a3f348628f5a69acbc6d0ff46a696d93b09475f
NovicovMykhailo <mikenovicov@gmail.com>
2023-07-12 14:21:24 +0300

    add main function

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,46 @@
-const express = require('express')
+import express from "express";
+import { listContacts, getContactById, removeContact, addContact, updateContact } from "../../models/contacts.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res) => {
+  const contacts = await listContacts();
+  res.json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  console.log(req.params);
+  const contact = await getContactById(req.params.contactId);
+  res.json(contact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res) => {
+  if (req.body.name && req.body.email && req.body.phone) {
+    const addedContact = await addContact(req.body);
+    res.json(addedContact).status(201).end();
+  } else {
+    res.status(400).json({ message: "not valid body" });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res) => {
+  const deletedContact = await removeContact(req.params.contactId);
+  res.json(deletedContact).status(204).end();
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const { params, body } = req;
+  const isBodyEmpty = Object.keys(body).length === 0 ? true : false;
 
-module.exports = router
+  if (!isBodyEmpty && params.contactId) {
+    const updatedContact = await updateContact(params.contactId, body);
+    
+    updatedContact ?
+      res.json(updatedContact).status(201).end() :
+      res.status(400).json({ message: "id not found" });
+  } else {
+    res.status(400).json({ message: "body was not sent" });
+  }
+});
+
+export default router;
