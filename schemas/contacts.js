ed5b45ebd7aa16a687a8aaf9eac1b8ad66014f0e
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-07-13 12:59:10 +0300

    fix schemas (email and phone)

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,8 +2,12 @@ const Joi = require("joi");
 
 const schemaContacts = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .required(),
 });
 
-module.exports = {schemaContacts};
+module.exports = { schemaContacts };
