c9e8301fd1d8ceee777fb0d2657b265bf8cbdda9
Sldvlld <sladvlad@gmail.com>
2023-07-13 13:56:28 +0300

    add jimp function

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models/user");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -9,7 +10,14 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarDir, filename);
-  await fs.rename(tempUpload, resultUpload);
+  try {
+    const image = await Jimp.read(tempUpload);
+    image.cover(250, 250).write(tempUpload);
+    await fs.rename(tempUpload, resultUpload);
+  } catch {
+    await fs.unlink(tempUpload);
+    throw new Error("Image processing error");
+  }
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
