e88d50b5ec8b0fe4d91fee97498e07b6698e941e
SerhiiShymko <shimko456@gmail.com>
2023-07-13 15:38:05 +0300

    install morgan dotenv

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,12 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
 
 const getContactById = async (contactId) => {}
 
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const app = require("./app");
+const express = require("express");
+// const app = require("./app");
+
+const app = express();
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
 
 //@ GET /api/contacts
-
+app.get("/ping", (req, res) => {
+  res.send("HELLO");
+});
 //@ GET /api/contacts/:id
 
 //@ POST /api/contacts
