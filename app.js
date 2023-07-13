171f50fd887abae5e6a2a91c34846d0e0d1d3c21
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-07-13 20:05:40 +0300

    s

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const dotenv = require("dotenv");
+
 const contactsRouter = require("./routes/api/contacts");
+
+dotenv.config();
 
 const app = express();
 
