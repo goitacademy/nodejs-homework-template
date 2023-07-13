e4a7ca597f3d61613bf4da7d713e245c5ca7db97
Anna <anytka141528@gmail.com>
2023-07-13 19:35:09 +0300

    finish fix

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,46 @@
-// const fs = require('fs/promises')
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const listContacts = async () => {}
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favourite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const getContactById = async (contactId) => {}
+contactSchema.post("save", (error, data, next) => {
+  error.status = 400;
+  next();
+});
 
-const removeContact = async (contactId) => {}
+const Contact = model("contact", contactSchema);
 
-const addContact = async (body) => {}
+const objectStructure = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favourite: Joi.boolean(),
+});
 
-const updateContact = async (contactId, body) => {}
+const updateFavouriteSchema = Joi.object({
+  favourite: Joi.boolean().required(),
+});
+const schemas = { objectStructure, updateFavouriteSchema };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  Contact,
+  schemas,
+};
