8eddefc49d59ad3586dba4657cf9f07f28b2abf3
Anna <anytka141528@gmail.com>
2023-07-13 20:48:27 +0300

    finish

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
 mongoose.Promise = global.Promise;
 dotenv.config();
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: 'Not found' });
+});
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message })
+  res.status(status).json({ message });
 });
 
 module.exports = app;
