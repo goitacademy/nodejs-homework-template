579d90e5c440e11e79d1646f07672c3d590db940
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-14 18:33:51 +0300

    fix

--- schemas/users.js
+++ schemas/users.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
   password: Joi.string().min(8).required(),
   email: Joi.string().required(),
 });
