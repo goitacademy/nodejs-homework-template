520bb6a1b8e1d28daa6af0bbb1c4a838368ae9a7
polokir <lolak_228@ukr.net>
2023-07-14 10:43:20 +0300

    done

--- controller/userController.js
+++ controller/userController.js
@@ -3,10 +3,11 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const HttpError = require("../error/errorHandler");
 const modifier = require('../middlewares/modifier');
-
+const gravatar = require('gravatar');
 const fs = require("fs/promises");
 const path = require("path");
-const avatarDir = path.join(__dirname, "../../", "public", "avatars");
+
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 require("dotenv").config();
 
 class UserController {
@@ -63,10 +64,11 @@ class UserController {
       }
 
       const hashPassword = await bcrypt.hash(password, 12);
-
+      const avatarURL = gravatar.url(email);
       const newUser = await User.create({
         ...req.body,
         password: hashPassword,
+        avatarURL
       });
       res.status(201).json({
         user: { email: newUser.email, subscription: newUser.subscription },
@@ -95,14 +97,14 @@ class UserController {
 
   async uploadAvatar(req,res){
     const { _id } = req.user;
-    const { path: tempUpload, originalname } = req.file;
+    const { path: tempDirectory, originalname } = req.file;
     const fileName = `${_id}_${originalname}`;
   
-    const resultUpload = path.join(avatarDir, fileName);
+    const destinationFile = path.join(avatarDir, fileName);
   
-    await modifier(tempUpload);
+    await modifier(tempDirectory);
   
-    await fs.rename(tempUpload, resultUpload);
+    await fs.rename(tempDirectory, destinationFile);
   
     const avatarURL = path.join("avatars", fileName);
     await User.findByIdAndUpdate(_id, { avatarURL });
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const authenticate = require('../../middlewares/authenticate');
 const useJoi = require('../../validations/user');
 const {validateBody} = require('../../middlewares/contacts')
+const loader = require('../../middlewares/loader');
+
 
 router.post("/register", validateBody(useJoi.registerSchema), userController.register);
 router.post("/login", validateBody(useJoi.loginSchema), userController.login);
 router.post("/logout", authenticate, userController.logout);
 router.get("/current", authenticate, userController.getUser);
 router.patch("/",authenticate,validateBody(useJoi.subscriptionSchema),userController.updateSubscription);
-router.post('/avatar',authenticate,userController.uploadAvatar);
+router.patch('/avatars',authenticate,loader.single("avatar"),userController.uploadAvatar);
 
 module.exports = router;
 
