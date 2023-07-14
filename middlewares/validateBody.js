8175d42293731c87bdc85bbffcc9ddfffd7bd58f
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-07-14 10:28:28 +0300

    fix joi schema

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,8 +3,9 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+    console.log(error)
     if (error) {
-      next(HttpError(400, "missing required name field"));
+      next(HttpError(400, error.message));
     }
     next();
   };
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,8 +2,10 @@ const Joi = require("joi");
 
 const addSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().min(10).max(15).required(),
 });
 
 module.exports = {
