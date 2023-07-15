86b1eabeedc1951f49cc759df87555fcdf18e0e4
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-07-15 19:22:04 +0300

    upd

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 const contactsRouter = require('./routes/api/contacts')
-
+const DB_HOST = "mongodb+srv://Yaroslav:0442062697Yh@cluster0.zbgpvpo.mongodb.net/contacts?retryWrites=true&w=majority"
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
+mongoose.connect(DB_HOST).then(()=> console.log("success")).catch(error => console.log(error.message))
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 // eslint-disable-next-line no-undef
-app.use(upload.none());
+//app.use(upload.none());
 
 app.use('/api/contacts', contactsRouter)
 
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+const DB_HOST = "mongodb+srv://Yaroslav:0442062697Yh@cluster0.zbgpvpo.mongodb.net/contacts?retryWrites=true&w=majority";
 
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.connect(DB_HOST)
+.then(()=> {
+  app.listen(3000)
 })
+.catch(error => {console.log(error.message);
+  process.exit(1)}
+  )
