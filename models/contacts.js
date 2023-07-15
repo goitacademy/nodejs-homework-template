9c30ba8c291a3d29a3755d13b90f1bd5661e0e8d
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-07-15 23:27:30 +0300

    Update code

--- models/contacts.js
+++ models/contacts.js
@@ -1,31 +1,78 @@
-import fs from 'fs/promises';
-import path from 'path';
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const contacsPath = path.resolve('models', 'contacts.json');
+const contacsPath = path.resolve("models", "contacts.json");
+
+const updateContacts = (contacts) =>
+  fs.writeFile(contacsPath, JSON.stringify(contacts, null, 2));
 
 export const listContacts = async () => {
   const contacts = await fs.readFile(contacsPath);
   return JSON.parse(contacts);
-}
+};
 
 export const getContactById = async (contactId) => {
   const contacts = await listContacts();
-   
-  const foundContact = contacts.find(contact => contact.id === contactId);
+
+  const foundContact = contacts.find((contact) => contact.id === contactId);
 
   return foundContact || null;
-}
+};
 
-const removeContact = async (contactId) => {}
+export const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
 
-const addContact = async (body) => {}
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
 
-const updateContact = async (contactId, body) => {}
+  contacts.push(newContact);
+
+  await updateContacts(contacts);
+
+  return newContact;
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [contactToRemove] = contacts.splice(index, 1);
+
+  await updateContacts(contacts);
+
+  return contactToRemove;
+};
+
+export const updateContactById = async (contactId, { name, email, phone }) => {
+  const contacts = await listContacts();
+
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { contactId, name, email, phone };
+
+  await updateContacts(contacts);
+
+  return contacts[index];
+};
 
 export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+  updateContactById,
+};
--- models/helpters/HttpError.js
+++ models/helpters/HttpError.js
@@ -1,5 +1,6 @@
 const messageList = {
   404: "Not found",
+  400: "missing required name field",
 };
 
 const HttpError = (status, message = messageList[status]) => {
