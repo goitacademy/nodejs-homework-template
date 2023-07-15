6e7c666ca8e109fb2c8654956c03b206e753e9df
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-15 10:24:47 +0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const bcrypt = require('bcryptjs');
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+      minlength: 6,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.methods.setPassword = function(password){
+    this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  };
+  
+  userSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+  };
+  
+
+const joiRegisterSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const joiLoginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  joiRegisterSchema,
+  joiLoginSchema,
+};
