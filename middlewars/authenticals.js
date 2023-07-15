f4754a704e4d06edbbfd12bac677a590e5ee185b
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2023-07-15 22:29:43 +0300

    fixed bug

--- middlewars/authenticals.js
+++ middlewars/authenticals.js
@@ -6,7 +6,7 @@ const { HttpError } = require("../helpers");
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
   try {
