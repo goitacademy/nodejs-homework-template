ab8f63656afd78a80635aba680af494196b593e2
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-07-17 01:00:53 +0300

    add sendgird package

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+const sgMail = require("@sendgrid/mail")
+
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/users");
 
@@ -15,6 +17,10 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"))
+
+const {SENGIRD_API_KEY} = process.env
+
+sgMail.setApiKey(SENGIRD_API_KEY)
 
 const { DB_HOST } = process.env;
 
