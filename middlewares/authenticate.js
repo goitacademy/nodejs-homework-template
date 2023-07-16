cf9242a354e71b1326699b8f71cd83279cd3628c
Валентина Сіра <113180300+ValentynaSira1@users.noreply.github.com>
2023-07-16 09:27:39 +0300

    *

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,10 +9,12 @@ const {HttpError, ctrlWrapper} = require("../../helpers");
 const authenticate = async (req, res, next) => {
     const {authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
-        next(HttpError(401))
-    }
+    
     try {
+        if (bearer !== "Bearer") {
+            next(HttpError(401))
+        }
+        
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if(!user || !user.token || user.token !== token) {
