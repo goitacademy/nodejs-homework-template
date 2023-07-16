e596adf2629a1bee21cd7ca456bdf2b302ec0ee7
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-07-16 21:43:03 +0200

    ok

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,68 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const { nanoid } = require('nanoid')
 
-const listContacts = async () => {}
+// путь к contacts.json
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, '..', 'models', 'books.json')
+// console.log(contactsPath)
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath)
+  return JSON.parse(data)
 
-const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+}
+
+const getContactById = async (id) => {
+  const contacts = await listContacts()
+  const result = contacts.find(item => item.id === id)
+  return result || null
+
+}
+
+const removeContact = async (id) => {
+  const contacts = await listContacts()
+
+  const index = contacts.findIndex(item => item.id === id);
+  if(index === -1){
+      return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+const addContact = async (body) => {
+  const contacts = await listContacts()
+
+  // создаем новую. Добавляем id и данные которые приходят 
+  const newContact = {
+    //
+    id: nanoid(),
+    ...body
+  }
+ 
+  contacts.push(newContact)
+  // перезаписываем все
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+ 
+  return newContact
+}
+
+const updateContact = async (id, body) => {
+  const contacts = await listContacts()
+  const index = contacts.findIndex(item => item.id === id)
+  if (index === -1) {
+    return null
+  }
+  // перезаписываем книгу по индексу books[index]
+  contacts[index] = { id, ...body }
+  // перезаписываем JSON
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+  return contacts[index]
+
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,48 @@
 const express = require('express')
+// для проверки поступающих обьектов на сервер, чтобы соответствовали требованиям
+const Joi = require("joi")
+// импортируем обьект методов отправки запросов
+const contacts = require('../../models/contacts')
+
+const {HttpError} = require("../../helpers")
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+// создаем обязательный стандарт передаваемого обьекта 
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  author:Joi.string().required()
 })
 
+
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+  } catch (error) {
+    next(error)
+  
+  }
+ 
+})
+// id или  
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { id } = req.params
+    const result = await contacts.getContactById(id)
+    if(!result){
+            throw HttpError(404, "Not found")
+          }
+  
+    res.json(result)
+  } catch (error) {
+  
+
+    next(error)
+ 
+  }
+
+  // res.json({ message: 'template message' })
 })
 
 router.post('/', async (req, res, next) => {
