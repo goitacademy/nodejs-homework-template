1a98135475bab1c2c4abc13ea658c49bef490e3c
Олександр <moskaletzsanych@gmail.com>
2023-07-16 22:41:30 +0300

    fix-string-base-in-schema

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import contactsController from '../../controllers/contacts-controller.js';
 import { validateBody } from '../../decorators/index.js';
-import { isEmptyBody } from '../../middlewares/index.js';
+import { isEmptyBody } from '../../middlewars/index.js';
 
 import contactsSchemas from '../../schemas/contacts-schemas.js';
 
--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -11,6 +11,7 @@ const contactsAddSchema = Joi.object({
     'any.required': 'missing required email field!',
     'string.empty': "Email can't be empty!",
     'string.email': 'Invalid email format!',
+    'string.base': 'Email must be a string!',
     'string.max': 'Email must not exceed 49 characters!',
   }),
   phone: Joi.string()
@@ -21,6 +22,7 @@ const contactsAddSchema = Joi.object({
         "Phone number must contain only digits, spaces, and the following characters: '()', '+', and '-'!",
       'any.required': 'missing required phone field!',
       'string.empty': "Phone number can't be empty!",
+      'string.base': 'Phone number must be a string!',
     }),
 });
 
