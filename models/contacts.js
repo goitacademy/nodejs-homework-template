dadd3a6efd98cd8fc752449bbe639048bd28ed5c
AMIHbKA <bishop0013forum@gmail.com>
2023-07-16 21:22:28 +0300

    init

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,15 @@
 // const fs = require('fs/promises')
+// init
 
-const listContacts = async () => {}
+const listContacts = async () => {};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +17,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
