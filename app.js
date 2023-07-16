f4477ef3b248e056caa613549166a012fdb5f646
NadiaOl <115547708+NadiaOl@users.noreply.github.com>
2023-07-16 11:57:17 +0300

    налаштування

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-const contactsRouter = require('./routes/api/contacts')
+import moviesRouter from "./routes/api/movies-router.js";
+import contactsRouter from "./routes/api/contacts-router.js";
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/movies", moviesRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
@@ -19,7 +21,8 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({ message, })
 })
 
-module.exports = app
+export default app
