740d814f9c4ef22e477eed99b353c2fad2aa6ef6
NikitaSimakov <106473909+NikitaSimakov@users.noreply.github.com>
2023-07-16 14:20:37 +0300

    add controllers folder

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,71 +1,86 @@
 import express from "express";
+// import {
+//   getContactById,
+//   listContacts,
+//   addContact,
+//   removeContact,
+//   updateContact,
+// } from "../../models/contacts.js";
+// import { HttpError } from "../../helpers/HttpError.js";
+// import { schemaAdd, schemaUpdate } from "../../helpers/schema.js";
 import {
-  getContactById,
-  listContacts,
-  addContact,
-  removeContact,
-  updateContact,
-} from "../../models/contacts.js";
-import { HttpError } from "../../helpers/HttpError.js";
-import { schemaAdd, schemaUpdate } from "../../helpers/schema.js";
+  deleteContact,
+  getContact,
+  getListContacts,
+  postNewContact,
+  putContact,
+} from "../../controllers/controllers.js";
 
 export const contactsRouter = express.Router();
 
-contactsRouter.get("/", async (req, res, next) => {
-  try {
-    const data = await listContacts();
-    res.json(data);
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouter.get("/", getListContacts);
 
-contactsRouter.get("/:contactId", async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-    const data = await getContactById(id);
-    if (!data) {
-      throw HttpError(404, `Contact with Id: ${id} not found`);
-    }
-    res.json(data);
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouter.get("/:contactId", getContact);
 
-contactsRouter.post("/", async (req, res, next) => {
-  try {
-    const { error } = schemaAdd.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const data = await addContact(req.body);
-    res.status(201).json(data);
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouter.post("/", postNewContact);
 
-contactsRouter.delete("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  try {
-    const deleting = await removeContact(id);
-    if (!deleting) return HttpError(404, "Not found");
-    return res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouter.delete("/:contactId", deleteContact);
 
-contactsRouter.put("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  try {
-    const { error } = schemaAdd.validate(req.body);
-    if (error) throw HttpError(400, "missing fields");
-    const newContact = await updateContact(id, req.body);
-    if (!newContact) throw HttpError(404, "Not found");
-    return res.json(newContact);
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouter.put("/:contactId", putContact);
+// contactsRouter.get("/", async (req, res, next) => {
+//   try {
+//     const data = await listContacts();
+//     res.json(data);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+// contactsRouter.get("/:contactId", async (req, res, next) => {
+//   try {
+//     const id = req.params.contactId;
+//     const data = await getContactById(id);
+//     if (!data) {
+//       throw HttpError(404, `Contact with Id: ${id} not found`);
+//     }
+//     res.json(data);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// contactsRouter.post("/", async (req, res, next) => {
+//   try {
+//     const { error } = schemaAdd.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const data = await addContact(req.body);
+//     res.status(201).json(data);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// contactsRouter.delete("/:contactId", async (req, res, next) => {
+//   const id = req.params.contactId;
+//   try {
+//     const deleting = await removeContact(id);
+//     if (!deleting) return HttpError(404, "Not found");
+//     return res.json({ message: "contact deleted" });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// contactsRouter.put("/:contactId", async (req, res, next) => {
+//   const id = req.params.contactId;
+//   try {
+//     const { error } = schemaAdd.validate(req.body);
+//     if (error) throw HttpError(400, "missing fields");
+//     const newContact = await updateContact(id, req.body);
+//     if (!newContact) throw HttpError(404, "Not found");
+//     return res.json(newContact);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
