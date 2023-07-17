47cda97143f3395e6c69369f47c16430721e3cc2
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-07-17 22:48:45 +0300

    init project

--- app.js
+++ app.js
@@ -24,3 +24,10 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Andriy:P*uWves3rr343-L@cluster0.5zpculu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
