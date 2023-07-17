a198f0facce5e77094698aa2e15397efc68fc22e
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-17 22:23:49 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,7 +16,7 @@ const auth = async (req, res, next) => {
     }
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) {
+    if (!user || !user.token || user.token !== token) {
       throw new Unauthorized("Not authorized");
     }
     req.user = user;
