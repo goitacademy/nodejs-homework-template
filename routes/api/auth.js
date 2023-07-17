c96a022dbbf4f3d0570b911fd2505db603942599
Your Name <pavlo.morozenko@ukr.net>
2023-07-17 11:03:38 +0300

    routes update

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const ctrl = require('../../controllers/auth');
-const { validateBody, authenticate } = require('../../middlewares');
+const { validateBody, authenticate, upload } = require('../../middlewares');
 const { schemas } = require('../../models/user');
 
 const router = express.Router();
 
 router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 
+router.get('/verify/:verificationToken', ctrl.verifyEmail);
+
+router.post('/verify', validateBody(schemas.verifyEmailSchema), ctrl.resendVerifyEmail);
+
 router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
 router.get('/current', authenticate, ctrl.getCurrent);
 
 router.post('/logout', authenticate, ctrl.logout);
+
+router.patch(
+    '/avatars',
+    authenticate,
+    upload.single("avatar"),
+    ctrl.updateAvatar
+);
 
 module.exports = router;
