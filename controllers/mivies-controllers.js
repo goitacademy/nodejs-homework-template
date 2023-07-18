4863bf92794a781be905e913ed11e95fc1199cdb
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-07-18 12:25:17 +0300

    fixed

--- controllers/mivies-controllers.js
+++ controllers/mivies-controllers.js
@@ -4,9 +4,15 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const contactsAddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "any.required": "missing required name field",
+  }),
+  email: Joi.string().required().messages({
+    "any.required": "missing required email field",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "missing required phone field",
+  }),
 });
 
 const getAll = async (req, res) => {
@@ -24,47 +30,38 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res, next) => {
-  try {
-    const { error } = contactsAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing required name field");
-    }
-    const result = await contactsService.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+  const { error } = contactsAddSchema.validate(req.body);
+  // console.log(error.message);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
 };
 
 const delleteById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const result = await contactsService.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.json({ message: "contact deleted" });
 };
 
 const updateById = async (req, res, next) => {
-  try {
-    const { error } = contactsAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing required name field");
-    }
-    const { contactId } = req.params;
+  const { contactId } = req.params;
 
-    const result = await contactsService.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  const result = await contactsService.updateContact(contactId, req.body);
+  const { length } = Object.keys(req.body);
+  console.log(length);
+  if (length === 0) {
+    throw HttpError(400, "missing fields");
   }
+  const { error } = contactsAddSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+  res.json(result);
 };
 
 export default {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,3 @@
-// const express = require('express')
 import express from "express";
 import contactsController from "../../controllers/mivies-controllers.js";
 
