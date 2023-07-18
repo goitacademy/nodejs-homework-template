db4d2ccf72e7f2b1fcda9c15ad0ac6285344c912
kirhal <kirillgaleckiy@gmail.com>
2023-07-18 16:23:24 +0200

    register verify sent

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -14,6 +14,9 @@ const login = async (req, res) => {
     throw HttpError(401, 'Email or password is wrong');
   }
 
+  if (!user.verify) {
+    throw HttpError(401, 'Email not verify');
+  }
   const comparePassword = await bcrypt.compare(password, user.password);
   if (!comparePassword) {
     throw HttpError(401, 'Email or password is wrong');
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcrypt');
 const gravatar = require('gravatar');
+const { nanoid } = require('nanoid');
 
 const { User } = require('../../models/user');
 
-const { HttpError, ctrlWrapper } = require('../../helpers');
+const { HttpError, ctrlWrapper, sendEmail } = require('../../helpers');
+
+const { BASE_URL } = process.env;
 
 // const createHashPassword = async password => {
 //   //   const salt = await bcrypt.genSalt(10);
@@ -21,11 +24,21 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Verify email',
+    html: `<a target="_blanc" href="${BASE_URL}/users/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: {
