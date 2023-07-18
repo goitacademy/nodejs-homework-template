ff32c2df6f0f4285c00c7667c8b0e13bf5538376
Nikita Kozachkov <112970676+nikitakozachkov@users.noreply.github.com>
2023-07-18 18:13:38 +0300

    hw 03 done

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection succesful");
+    });
+  })
+  .catch(() => process.exit(1));
