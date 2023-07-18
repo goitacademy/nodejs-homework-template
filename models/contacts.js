fe0c4a1acaf25f31080ead86cd11f5f6be6735e6
Mazur Roman <superioritywork@gmail.com>
2023-07-18 08:29:27 +0200

    add task

--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,59 @@
-const listContacts = async () => {};
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const getContactById = async (contactId) => {};
+const contactsPath = path.resolve("models", "contacts.json");
 
-const removeContact = async (contactId) => {};
+function updateContactsStorage(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
 
-const addContact = async (body) => {};
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
 
-const updateContact = async (contactId, body) => {};
+async function getContactById(contactId) {
+  const contact = await listContacts();
+  const result = contact.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) return null;
+  const [result] = contacts.splice(index, 1);
+  await updateContactsStorage(contacts);
+  return result || null;
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContactsStorage(contacts);
+  return newContact;
+}
+
+async function updateContactsById(id, { name, email, phone }) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) return null;
+  contacts[index] = { id, name, email, phone };
+  await updateContactsStorage(contacts);
+  return contacts[index];
+}
 
 export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContactsById,
 };
