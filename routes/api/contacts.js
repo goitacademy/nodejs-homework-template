0cdf69cc0a391f617dd2140618ed2fbfe3218882
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-07-18 20:10:10 +0300

    add complete task

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,83 @@
 const express = require('express')
-
+ const contacts = require("../../models/contacts")
 const router = express.Router()
+const HttpError = require("../../helper")
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const response = await contacts.listContacts()
+    if(!response){
+      throw HttpError(404, "Not Found")
+    }
+    res.status(response.status).json(response)
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+try {
+  const {contactId} = req.params
+const response = await contacts.getContactById(contactId)
+if(!response){
+  throw HttpError(404, "Not Found")
+}
+  res.status(response.status).json(response)
+} catch (error) {
+  next(error)
+}
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {body} = req
+    const response = await contacts.addContact(body)
+    if(!response){
+      throw HttpError(404, "Not Found")
+    }else if (response.status === 400){
+      throw HttpError(response.status, response.message)
+    }
+    res.status(response.status).json(response)
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params
+    const response = await contacts.removeContact(contactId)
+    if(!response){
+      throw HttpError(404, "Not Found")
+    }
+    res.status(response.status).json(response)
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params
+    const {body} = req
+    const response = await contacts.updateContact(contactId, body)
+    if(!response){
+      throw HttpError(404, "Not Found")
+    }else if (response.status === 400){
+      console.log(response.status)
+      console.log(response.message)
+      throw HttpError(response.status, response.message)
+    }
+    res.status(response.status).json(response)
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 module.exports = router
+
