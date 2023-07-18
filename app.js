2878ceec9ade588c321de9259afa8eaaf5667d7f
NataliiaDyrkach <n.dyrkach@gmail.com>
2023-07-18 10:13:56 +0300

    corrected the errors(message)

--- app.js
+++ app.js
@@ -18,14 +18,9 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = "Server error" } = err
-//   res.status(status).json({ message: err.message })
-// })
-
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
-    msg: err.message,
+    message: err.message,
   });
 });
 
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,16 @@
 const Joi = require("joi");
 
 const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
+  name: Joi.string().required().messages({
+    "any.required": 'missing required name field',
+  }),
+  email: Joi.string().required().messages({
+    "any.required": 'missing required email field',
+  }),
+  phone: Joi.number().required().messages({
+    "any.required": 'missing required phone field',
+  }),
 });
 
 module.exports = contactSchema;
+
