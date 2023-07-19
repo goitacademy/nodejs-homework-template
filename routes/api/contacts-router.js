c5fce30fface75a919ab7659b1e8f2904dd054eb
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-07-19 13:41:39 +0200

    add put and delete methods and joi

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,17 +1,30 @@
 import express from "express";
 
 import contactsService from "../../models/contacts.js";
+import HttpError from "../../helpers/HttpError.js";
+
+import Joi from "joi";
 
 const contactsRouter = express.Router();
+
+const constactsAddSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": "missing required name field",
+  }),
+  email: Joi.string().required().messages({
+    "any.required": "missing required email field",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "missing required phone field",
+  }),
+});
 
 contactsRouter.get("/", async (req, res, next) => {
   try {
     const result = await contactsService.listContacts();
     res.json(result);
-  } catch (err) {
-    res.status(500).json({
-      message: err.message,
-    });
+  } catch (error) {
+    next(error);
   }
 });
 
@@ -20,30 +33,63 @@ contactsRouter.get("/:id", async (req, res, next) => {
     const { id } = req.params;
     const result = await contactsService.getContactsById(id);
     if (!result) {
-      const error = new Error(`Contacts with id=${id} not found`);
-      error.status = 404;
-      throw error;
+      throw HttpError(404);
     }
     res.json(result);
-  }
-  catch (err) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message,
-    });
+  } catch (error) {
+    next(error);
   }
 });
 
 contactsRouter.post("/", async (req, res, next) => {
+  try {
+    const { error } = constactsAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.addContacts(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+
   res.json({ message: "template message" });
 });
 
-contactsRouter.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) { 
+      throw HttpError(404)
+    }
+    res.json({
+      message: "Contact removed"
+    });
+  }
+  catch (error) {
+    next(error);
+  }
+  
 });
 
-contactsRouter.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.put("/:id", async (req, res, next) => {
+  try {
+     const { error } = constactsAddSchema.validate(req.body);
+     if (error) {
+       throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+
+    const result = await contactsService.updateContactsById(id, req.body);
+     if (!result) {
+       throw HttpError(404);
+     }
+     res.status(201).json(result);
+  }
+  catch (error) {
+    next(error);
+  }
 });
 
 export default contactsRouter;
