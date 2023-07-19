361d4b1145c9f5f1c84215610c04f3c5ae36749b
Oleksandr <115025828+Oleksandr-Tsimbaliuk@users.noreply.github.com>
2023-07-19 16:29:00 +0300

    add branch, mongoose, host

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Alex:MAZojSXrUVkX0p6x@cluster0.sy55ttu.mongodb.net/";
+// pasword: MAZojSXrUVkX0p6x
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
