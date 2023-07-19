5476a45b09c017afe2d3cf8efe4e23bc3ea99567
Oleksandr Siukov <as-service2008@ukr.net>
2023-07-19 13:24:27 +0300

    work on error

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,10 +1,17 @@
 const Joi = require("joi");
 
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+const addSchema = Joi.object()
+  .keys({
+    name: Joi.string().label("name").alphanum(),
+    email: Joi.string().label("email").email(),
+    phone: Joi.string().label("phone"),
+  })
+  .length(3)
+  .and("name", "email", "phone")
+  .messages({
+    "object.length": "missing  fields ",
+    "object.and": "missing required {#missingWithLabels} field ",
+  });
 
 module.exports = {
   addSchema,
