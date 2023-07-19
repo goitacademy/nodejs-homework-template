eacb11f675c76d1555ca1fe0b079e5c1174a56a0
Anna Kravets <annakravets1906@gmail.com>
2023-07-19 07:28:04 +0300

    Added mongoose

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-// const app = require('./app');
+import mongoose from 'mongoose';
+
 import app from './app.js';
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+const DB_HOST =
+  'mongodb+srv://aannakravets:QZqEdhvM5qtR60bR@cluster1.qcldh5t.mongodb.net/db-contacts';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+    console.log('Database connection successful');
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
