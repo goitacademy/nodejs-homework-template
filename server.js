8d076d65fce838d83eede2660e68f43decce667d
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-07-19 16:04:24 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-import app from "./app.js";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-	console.log("Server running. Use our API on port: 3000");
-});
+import app from "./app.js";
+// ndD2DO6jbAW6K90S
+
+const DB_HOST =
+	"mongodb+srv://Iuliia-Tyndyk:ndD2DO6jbAW6K90S@cluster0.yx1qcvt.mongodb.net/my-contacts?retryWrites=true&w=majority";
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		app.listen(3000, () => {
+			console.log("Server running. Use our API on port: 3000");
+		});
+	})
+	.catch((error) => console.log(error.message));
