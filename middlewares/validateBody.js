437417cfbb3388d753d9a63ceaeb9941f5da84b7
Oleg <olegbilas@gmail.com>
2023-07-20 16:35:00 +0300

    Update validateBody.js

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,7 @@ const validateBody = (schema, message) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(generateHTTPError(400, error.message));
+      next(generateHTTPError(400, message));
     }
     next();
   };
