356a54623690e5676f92c319c8a70a8774f99f3a
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-07-20 23:03:59 +0300

    env

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const app = require('./app')
 const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
-
-const DB_HOST = 'mongodb+srv://Nik:cWvpQY1qQUUF2SXT@cluster0.0e5jsvn.mongodb.net/Contacts_reader?retryWrites=true&w=majority'
+const {DB_HOST, PORT = 3000} = process.env
 
 mongoose.set('strictQuery', true)
+
 mongoose.connect(DB_HOST)
-  .then(()=> {app.listen(3000)})
+  .then(()=> {app.listen(PORT)})
   .catch(error => {console.log(error.message); process.exit(1)})
