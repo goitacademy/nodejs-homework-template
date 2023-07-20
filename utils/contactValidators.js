16189a3e1bee68cb745f6333316d0106e4e9a775
angelkapv81 <angelkapv81@gmail.com>
2023-07-20 16:05:25 +0300

    change favorite validator

--- utils/contactValidators.js
+++ utils/contactValidators.js
@@ -9,6 +9,6 @@ exports.createContactDataValidator = (data) =>
       name: Joi.string().min(3).max(30).required(),
       email: Joi.string().email().required(),
       phone: Joi.string().regex(PHONE_REGEX).required(),
-      favorite: Joi.boolean().required(),
+      favorite: Joi.boolean(),
     })
     .validate(data);
