bdc927f6605578575358d886a973a3ed8c9903c0
Oksana <oksana.sydorenko0691@icloud.com>
2023-07-20 22:07:42 +0300

    add

---
+++ controllers/addContact.js
@@ -0,0 +1,11 @@
+const {Contact} = require("../../models/contact")
+
+const {ControllerWrapper} = require("../../utils/index");
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+
+module.exports = ControllerWrapper(addContact);
---
+++ controllers/listContacts.js
@@ -0,0 +1,11 @@
+const {Contact} = require("../../models/contact")
+
+const {ControllerWrapper} = require("../../utils/index");
+
+const listContacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);    
+};
+
+
+module.exports = ControllerWrapper(listContacts);
