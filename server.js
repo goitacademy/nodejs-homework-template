8306b5c4c377ccb650240d0d2860c7dccdc9eb1c
Sergii Podobrii <podobri@ukr.net>
2023-07-20 17:11:33 +0300

    123

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = require("./app");
 
 const DB_HOST =
   "mongodb+srv://Mamont:Uo7YHhFP7JezbKHa@cluster0.ovlu5iw.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
+const { PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   })
