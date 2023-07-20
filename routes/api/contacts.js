9a16b4159141805470795f07e0f787e538617f28
MaiiaHa <maiia.robeiko@ukr.net>
2023-07-21 00:25:05 +0300

    new initial

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Home work" });
+  res.json({ message: "Home work 2" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
