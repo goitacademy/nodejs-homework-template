020d44d2f1874ec09bed656a549aa9b1117edf4d
Phokoos <hulkovskij@gmail.com>
2023-07-20 21:55:45 +0300

    Wrappered controllers

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,56 +1,38 @@
 import HttpError from "../helpers/HttpError.js";
 import { addContact, getContactById, listContacts, removeContact, updateContact } from "../models/contacts.js";
 
-export const listContactsController = async (req, res, next) => {
-	try {
-		const list = await listContacts()
-		res.json(list)
-	} catch (error) {
-		next(error)
-	}
-}
+import { ctrlWrapper } from '../decorators/index.js'
 
-export const getContactByIdController = async (req, res, next) => {
-	try {
-		const contact = await getContactById(req.params.contactId)
-		if (!contact) {
-			throw HttpError(404, "Not found")
-		}
-		res.json(contact)
-	} catch (error) {
-		next(error);
-	}
-}
+export const listContactsController = ctrlWrapper(async (req, res, next) => {
+	const list = await listContacts()
+	res.json(list)
+})
 
-export const addContactController = async (req, res, next) => {
-	try {
-		const contact = await addContact(req.body);
-		res.status(201).json(contact)
-	} catch (error) {
-		next(error)
+export const getContactByIdController = ctrlWrapper(async (req, res, next) => {
+	const contact = await getContactById(req.params.contactId)
+	if (!contact) {
+		throw HttpError(404, "Not found")
 	}
-}
+	res.json(contact)
+})
 
-export const removeContactController = async (req, res, next) => {
-	try {
-		const contact = await removeContact(req.params.contactId);
-		if (!contact) {
-			throw HttpError(404, "Not found")
-		}
-		res.json({ "message": "contact deleted" })
-	} catch (error) {
-		next(error)
-	}
-}
+export const addContactController = ctrlWrapper(async (req, res, next) => {
+	const contact = await addContact(req.body);
+	res.status(201).json(contact)
+})
 
-export const updateContactController = async (req, res, next) => {
-	try {
-		const contact = await updateContact(req.params.contactId, req.body);
-		if (!contact) {
-			throw HttpError(404, "Not found")
-		}
-		res.json(contact)
-	} catch (error) {
-		next(error)
+export const removeContactController = ctrlWrapper(async (req, res, next) => {
+	const contact = await removeContact(req.params.contactId);
+	if (!contact) {
+		throw HttpError(404, "Not found")
 	}
-}
+	res.json({ "message": "contact deleted" })
+})
+
+export const updateContactController = ctrlWrapper(async (req, res, next) => {
+	const contact = await updateContact(req.params.contactId, req.body);
+	if (!contact) {
+		throw HttpError(404, "Not found")
+	}
+	res.json(contact)
+})
