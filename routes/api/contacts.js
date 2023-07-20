3b2960f4cd9715fc3652d6ef2afa7ddca24b4f7d
MaiiaHa <maiia.robeiko@ukr.net>
2023-07-21 00:11:19 +0300

    initial

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Home work 02" });
+  res.json({ message: "Home work" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "Home work 02" });
+  res.json({ message: "Home work" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "Home work 02" });
+  res.json({ message: "Home work" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "Home work 02" });
+  res.json({ message: "Home work" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "Home work 02" });
+  res.json({ message: "Home work" });
 });
 
 module.exports = router;
