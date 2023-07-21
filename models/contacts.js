91a8750e65eed595c6289577aa84944ff1027615
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-07-21 07:19:31 +0200

    new ropository

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,71 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+// const listContacts = async () => {};
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+// const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+// const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+// const updateContact = async (contactId, body) => {};
 
-module.exports = {
+// export default {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
+
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  console.log(index);
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require('./app')
+import app from "./app.js"
+// const app = require('./app')
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
