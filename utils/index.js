521077fd1a99317ac7a731c058774dfe62b5e6e5
Ivan Klimov <klimov128@gmail.com>
2023-07-22 05:15:12 +0300

    delete findContactById

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,7 @@
-const findContactById = require('./findContactById');
 const { tryCatchWrapper } = require('./tryCatchWrapper');
 const { userDataValidator, userEditDataValidator } = require('./userValidator');
 
 module.exports = {
-  findContactById,
   userDataValidator,
   userEditDataValidator,
   tryCatchWrapper,
