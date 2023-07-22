4fa5628a52170f85ec500a51bb66ec8f85a6b39b
Vladyslav Hrytsenko <unknownghost84.vg@gmail.com>
2023-07-22 14:33:07 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -17,7 +17,7 @@ const contactSchema = new Schema({
           type: Boolean,
           default: false,
         },
-})
+},{versionKey:false});
 
 contactSchema.pre("findOneAndUpdate", handleUpdateValidate);
 
