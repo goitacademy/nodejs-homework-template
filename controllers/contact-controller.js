f20629b78e7fa5b234210c9dda53f892e9104767
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-07-22 14:12:24 +0300

    add code to hw2

---
+++ controllers/contact-controller.js
@@ -0,0 +1,59 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../models/contacts.js");
+
+const { HttpError } = require("../helpers/index.js");
+
+const { ctrlWrapper } = require("../decorators/index.js");
+
+const getAll = async (req, res) => {
+  const result = await listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await getContactById(id);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+  res.json(result);
+};
+
+const add = async (req, res, next) => {
+  const result = await addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+  res.json(result);
+};
+
+const deleteById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await removeContact(id);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+
+  res.json({
+    message: "Delete success",
+  });
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,83 +1,26 @@
 const express = require("express");
 
 const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts.js");
-const { HttpError } = require("../../helpers");
+  getAll,
+  getById,
+  add,
+  updateById,
+  deleteById,
+} = require("../../controllers/contact-controller.js");
+const { validateBody } = require("../../decorators/index.js");
+const { isEmptyBody } = require("../../middlewares/index");
 const { contactSchema } = require("../../schema/contacts.js");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", getAll);
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await getContactById(id);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", getById);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", isEmptyBody, validateBody(contactSchema), add);
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await removeContact(id);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
+router.delete("/:id", deleteById);
 
-    res.json({
-      message: "Delete success",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await updateContact(id, req.body);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:id", isEmptyBody, validateBody(contactSchema), updateById);
 
 module.exports = router;
