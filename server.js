f5d2b968796f85d50aa22eacf9581c819bf02074
Ivan Klimov <klimov128@gmail.com>
2023-07-22 05:20:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const DB_URL = require('./DB_URL');
 
 mongoose
   .connect(DB_URL)
-  .then(() => console.log('Mongo DB successfully connected..'))
+  .then(() => {
+    console.log('Mongo DB successfully connected..');
+
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
   .catch((err) => {
     console.log(err);
 
     process.exit(1);
   });
-
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
