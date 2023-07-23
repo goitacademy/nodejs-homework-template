365056de3b17a2b8aab8c9d8adf7fb6e4ea44bdf
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-07-23 12:10:44 +0300

    fix routing

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -3,13 +3,13 @@ import { HttpError } from "../helpers/index.js";
 import { CtrlWrapper } from "../decorators/index.js";
 
 // * Get ALL
-export const getAll = () => async (req, res) => {
+export const getAll = async (req, res) => {
   const result = await contactsService.listContacts();
   res.json(result);
 };
 
 // * Get by ID
-export const getById = () => async (req, res) => {
+export const getById = async (req, res) => {
   const { contactId } = req.params;
   const result = await contactsService.getContactById(contactId);
   if (!result) throw HttpError(404);
@@ -18,7 +18,7 @@ export const getById = () => async (req, res) => {
 };
 
 // * Post NEW
-export const add = () => async (req, res) => {
+export const add = async (req, res) => {
   const result = await contactsService.addContact(req.body);
   if (result) throw HttpError(404, "missing required name field");
 
@@ -26,14 +26,14 @@ export const add = () => async (req, res) => {
 };
 
 // * Delete
-export const removeById = () => async (req, res) => {
+export const removeById = async (req, res) => {
   const result = await contactsService.removeContact(req.params.contactId);
   if (result) throw HttpError(404);
   res.json({ message: "contact deleted" });
 };
 
 // *Update
-export const updateById = () => async (req, res, next) => {
+export const updateById = async (req, res, next) => {
   const result = await contactsService.updateContact(
     req.params.contactId,
     req.body
