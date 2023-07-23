d94b302f03d6b9d6e602a21cf74fc500db2be703
Siracenco Serghei <siracencoserghei@gmail.com>
2023-07-23 20:04:16 +0200

    update router users

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/users', usersRouter)
+app.use('/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
