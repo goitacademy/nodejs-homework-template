36aace4762abf38050899b54a417116f3403b470
emmabaka <bakaemma88@gmail.com>
2023-07-24 15:47:49 +0300

    new commit

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,8 +1,7 @@
-const HttpError = require("../helpers/HttpError");
+const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   return (req, res, next) => {
-    console.log(req);
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
