404e4ae6c3f67f14e881cb4ff36d8d157075a34e
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-07-25 01:03:09 +0300

    finaly

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,9 +1,17 @@
 import Joi from "joi";
 
 const addContactSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
-export default {addContactSchema,}
+const contactUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+export default {
+  addContactSchema,
+  contactUpdateFavoriteSchema,
+};
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 //const app = require('./app')
+import mongoose from 'mongoose';
+import app from './app.js';
+import dotenv from 'dotenv';
 
-import app from './app.js'
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const {DB_HOST, PORT} = process.env;
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000, () => {
+  //console.log(`Server running. Use our API on port: ${PORT}`)
+  console.log('"Database connection successful"')
 })
+})
+.catch(error => {
+  console.log(error.message);
+  procces.exit(1);
+})
+
+
