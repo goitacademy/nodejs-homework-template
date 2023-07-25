08fc24c1f2f590d821988c745d7deb4178911759
OleksandrNovyk <novykav@gmail.com>
2023-07-25 13:11:28 +0300

    234

--- models/contacts.js
+++ models/contacts.js
@@ -2,6 +2,7 @@ const fs = require("fs").promises;
 const path = require("path");
 const { nanoid } = require("nanoid");
 
+
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
