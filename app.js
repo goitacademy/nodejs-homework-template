1f4344fa9ea1bdaf27444cdcd5b4db92b32dc5bb
Serhii Klokun <klokuns@gmail.com>
2023-07-26 23:33:47 +0300

    DB-HOST

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
-
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger =
+  app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://klokunSerhii:juNYSAkoje986zRg@cluster0.yg83efa.mongodb.net/contact	?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
