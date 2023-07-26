7f26febee16f7730288a19f46d6b51551edded33
Fransua Boltof <bolt2000@ukr.net>
2023-07-26 20:14:36 +0300

    +

--- helpers/index.js
+++ helpers/index.js
@@ -1,12 +1,10 @@
 const ctrlWrapper = require("./wrapper");
 const handleMongooseError = require("./handleMongooseError");
 const HttpError = require("./HttpErrors");
-const createHashPassword = require("./createHashPassword");
 
 
 module.exports = {
   ctrlWrapper,
   handleMongooseError,
   HttpError,
-  createHashPassword,
 };
