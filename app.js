ba6373dbcee4ed7e3a1df49b8bfcc9c8a4c1d5fe
Omelianov Nicolai <omelianov.n@gmail.com>
2023-07-26 16:45:47 +0300

    fixed get request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
 const contactsRouter = require('./routes/api/contacts');
 
@@ -12,14 +12,15 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/routes/api/contacts', contactsRouter)
+app.use("/api/contacts", contactsRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((req, res, next) => {
-  res.status(500).json({ message: "Server error" })
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message, })
 })
 
 
