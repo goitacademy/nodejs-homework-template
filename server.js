abbd03be7bedc0b99484204f48bc1290ceb594e3
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-26 15:32:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-import mongoose from "mongoose";
-
-import dotenv from "dotenv";
-
 import app from "./app.js";
 
+import mongoose from "mongoose";
+
+import dotenv from "dotenv"
+
 dotenv.config()
+const {DB_HOST, PORT} = process.env;
 
-const { DB_HOST, PORT } = process.env;
-
-mongoose.connect(DB_HOST)
-  .then(() => {
-  app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
-  })
-  .catch(error => {
+mongoose
+  .connect(DB_HOST)
+  .then(app.listen(PORT, () => {
+    console.log("Database connection successful");
+  }))
+  .catch((error) => {
     console.log(error.message);
-    process.exit(1)
-  })
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
+    process.exit(1);
+  });
