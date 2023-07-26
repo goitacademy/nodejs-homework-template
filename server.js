cd80a60cf287d46beea13d26da960e0b26eb0eba
Serhii Klokun <klokuns@gmail.com>
2023-07-26 23:05:21 +0300

    monggose

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://klokunSerhii:juNYSAkoje986zRg@cluster0.yg83efa.mongodb.net/contact	?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
