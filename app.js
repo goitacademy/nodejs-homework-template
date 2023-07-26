a62f214bb531f9f7d39ed6d606864ede6db1def9
NatiRumi <NatiRumiantseva@gmail.com>
2023-07-26 15:27:11 +0300

    start

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"))
 
 app.use("/api/users", usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: "Not found" })
 })
 
 // app.use((error, req, res, next) => {
