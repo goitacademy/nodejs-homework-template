274c2cae35f560c19ffbb178f6b093bf7c736d4d
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-27 00:07:43 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -30,7 +30,15 @@ const userSchema = Schema(
     avatarURL: {
       type: String,
       required: true
-    }
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -55,10 +63,15 @@ const joiLoginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const verifyEmailSchema = Joi.object({
+  email: Joi.string().required(),
+});
+
 const User = model("user", userSchema);
 
 module.exports = {
   User,
   joiRegisterSchema,
   joiLoginSchema,
+  verifyEmailSchema,
 };
