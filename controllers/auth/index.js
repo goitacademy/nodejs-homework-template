3dd92fa1f18821a7ed21049e7a85f30b661135b9
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-27 00:07:19 +0300

    Update index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,9 +1,13 @@
 const register = require('./register');
 const login = require('./login');
 const logout = require('./logout');
+const verifyEmail = require('./verifyEmail');
+const resendVerifyEmail = require('./resendVerifyEmail.js')
 
 module.exports = {
     register,
     login,
-    logout
+    logout,
+    verifyEmail,
+    resendVerifyEmail,
 }
