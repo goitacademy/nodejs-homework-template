3940e44b169ae0b56c05e2181c55bc951aa3db5b
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-26 15:32:45 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,11 +1,41 @@
-import { Shema, modal } from "mongoose";
+// import { Shema, modal } from "mongoose";
 
-const contactShema = new Shema({
-    name: String,
-    phone: String,
-    email: String,
-});
+// const contactShema = new Shema({
+//     name: String,
+//     phone: String,
+//     email: String,
+// });
 
-const Contact = model("contact", contactShema);
+// const Contact = model("contact", contactShema);
+
+// export default Contact;
+
+import { Schema, model } from "mongoose";
+import { handleSaveError, validateAtUpdate} from "./hooks.js";
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+      favorite: {
+        type: Boolean,
+        default: false,
+      },
+}, {versionKey: false, timestamps: true});
+
+
+contactSchema.pre("findOneAndUpdate", validateAtUpdate );
+
+contactSchema.post("save", handleSaveError );
+contactSchema.post("findOneAndUpdate", handleSaveError );
+
+const Contact = model("contact", contactSchema);
 
 export default Contact;
