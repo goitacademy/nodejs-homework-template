41ceb7218317654dac10a963cf84f74342ce72bc
Serhii Izotov <sergiyizotov@gmail.com>
2023-07-27 00:07:32 +0300

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,8 +1,12 @@
 const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
+const { nanoid } = require('nanoid');
+require("dotenv").config();
 
-
+const { sendEmail } = require("../../services");
 const { User } = require("../../models");
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,9 +16,19 @@ const register = async (req, res) => {
   }
 const avatarURL = gravatar.url(email);
 
-  const newUser = new User({ email, avatarURL });
+const verificationToken = nanoid();
+
+  const newUser = new User({ email, avatarURL, verificationToken });
   newUser.setPassword(password);
   newUser.save();
+
+  const verifyEmail = {
+    to: email,
+    subject: "Please verify your email",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201);
   res.json({
