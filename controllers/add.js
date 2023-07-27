3d94f71fd7e30da4e64e03a1b48675610b2a29f2
Elya <elya.kuntuganova@gmail.com>
2023-07-27 15:38:33 +0600

    Updated add.js & updateById.js

--- controllers/add.js
+++ controllers/add.js
@@ -3,11 +3,10 @@ const contacts = require('../models/contacts');
 async function add(req, res) {
     const { name, email, phone } = req.body;
     const result = await contacts.addContact(name, email, phone);
-    if (result) {
-    res.status(201).json(result);
-    } else {
-    res.status(500).json({ message: 'Failed to add the contact' });
+    if (!result) {
+    res.status(500).json({ message: 'Failed to add the contact' });  
     }
+    res.status(201).json(result);
 }
 
 module.exports = add;
