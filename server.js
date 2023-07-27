e46e24f047bb37f34b9f2df3ef9509b38270de04
Anton K <akarpenko83@gmail.com>
2023-07-27 19:39:04 +0300

    dotenv added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const mongoose = require('mongoose');
-// const { DB_HOST } = require('./config');
+
 const { DB_HOST } = process.env;
 
 mongoose.set('strictQuery', true);
