f5f28e7d478f7692bcc591d7bdfe64081bd9e45e
Omelianov Nicolai <omelianov.n@gmail.com>
2023-07-27 21:01:46 +0300

    fixed update and return updated contact

--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -4,7 +4,7 @@ const { RequestError } = require("../../helpers")
 
 const updateContact = async (req, res) => {
     const { id } = req.params;
-    const result = await contacts.updateContact(id, req.body);
+    const result = await contacts.updateContact(id, req.body, { new: true });
     if (!result) {
         throw RequestError(404, "Not found")
     }
