e29f612d595b829481cdcb1800752bfa05b28d5a
Omelianov Nicolai <omelianov.n@gmail.com>
2023-07-27 21:01:05 +0300

    fixed remove contact

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -3,8 +3,8 @@ const contacts = require("../../models/contacts")
 const { RequestError } = require("../../helpers")
 
 const removeContact = async (req, res) => {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
     if (!result) {
         throw RequestError(404, "Not found")
     }
