83d8c29217af1a11b34bfed091788b8ef2089dd8
Anton K <akarpenko83@gmail.com>
2023-07-27 23:47:03 +0300

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,57 +1,57 @@
-const fs = require('fs/promises');
-const { nanoid } = require('nanoid');
-const path = require('path');
-const contactsPath = path.join(__dirname, './contacts.json');
+// const fs = require('fs/promises');
+// const { nanoid } = require('nanoid');
+// const path = require('path');
+// const contactsPath = path.join(__dirname, './contacts.json');
 
-const listContacts = async () => {
- const contacts = await fs.readFile(contactsPath);
- return JSON.parse(contacts);
-};
+// const listContacts = async () => {
+//  const contacts = await fs.readFile(contactsPath);
+//  return JSON.parse(contacts);
+// };
 
-const getContactById = async contactId => {
- const contacts = await listContacts();
- const contact = contacts.find(contact => contact.id === contactId) || null;
- return contact;
-};
+// const getContactById = async contactId => {
+//  const contacts = await listContacts();
+//  const contact = contacts.find(contact => contact.id === contactId) || null;
+//  return contact;
+// };
 
-const removeContact = async contactId => {
- const contacts = await listContacts();
- const removedContact = contacts.find(contact => contact.id === contactId);
- if (!removedContact) {
-  console.log('not found contact');
-  return null;
- }
- const updatedContacts = contacts.filter(contact => contact.id !== contactId);
- await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), 'utf-8');
- return true;
-};
+// const removeContact = async contactId => {
+//  const contacts = await listContacts();
+//  const removedContact = contacts.find(contact => contact.id === contactId);
+//  if (!removedContact) {
+//   console.log('not found contact');
+//   return null;
+//  }
+//  const updatedContacts = contacts.filter(contact => contact.id !== contactId);
+//  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), 'utf-8');
+//  return true;
+// };
 
-const addContact = async ({ name, email, phone }) => {
- const contacts = await listContacts();
- const newContact = {
-  id: nanoid(),
-  name,
-  email,
-  phone,
- };
- contacts.push(newContact);
- await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8');
- return newContact;
-};
+// const addContact = async ({ name, email, phone }) => {
+//  const contacts = await listContacts();
+//  const newContact = {
+//   id: nanoid(),
+//   name,
+//   email,
+//   phone,
+//  };
+//  contacts.push(newContact);
+//  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8');
+//  return newContact;
+// };
 
-const updateContact = async (id, data) => {
- const contacts = await listContacts();
- const index = contacts.findIndex(contact => contact.id === id);
- if (index === -1) return null;
- contacts[index] = { id, ...data };
- await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8');
- return contacts[index];
-};
+// const updateContact = async (id, data) => {
+//  const contacts = await listContacts();
+//  const index = contacts.findIndex(contact => contact.id === id);
+//  if (index === -1) return null;
+//  contacts[index] = { id, ...data };
+//  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8');
+//  return contacts[index];
+// };
 
-module.exports = {
- listContacts,
- getContactById,
- removeContact,
- addContact,
- updateContact,
-};
+// module.exports = {
+//  listContacts,
+//  getContactById,
+//  removeContact,
+//  addContact,
+//  updateContact,
+// };
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set('strictQuery', true);
 mongoose
  .connect(DB_HOST)
  .then(() => {
   console.log('Database connection successful');
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
    console.log('Server running. Use our API on port: 3000');
   });
  })
