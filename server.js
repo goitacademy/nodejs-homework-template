acd2b0e729559a5314a9b3a7b36cdceae8f09f7a
Anton K <akarpenko83@gmail.com>
2023-07-27 17:46:17 +0300

    hide mongodb credentials

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const app = require('./app');
 const mongoose = require('mongoose');
-
-const DB_HOST =
- 'mongodb+srv://goit:pem3z910@cluster0.lucwigc.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const { DB_HOST } = require('./config');
 
 mongoose.set('strictQuery', true);
 mongoose
