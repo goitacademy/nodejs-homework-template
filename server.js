5812e24b721154add4bbc50e6cb347b11ceb3be2
Anton K <akarpenko83@gmail.com>
2023-07-27 17:25:49 +0300

    branch init

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
- console.log('Server running. Use our API on port: 3000');
-});
+const DB_HOST =
+ 'mongodb+srv://goit:pem3z910@cluster0.lucwigc.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose.set('strictQuery', true);
+mongoose
+ .connect(DB_HOST)
+ .then(() => {
+  console.log('Database connection successful');
+  app.listen(3000, () => {
+   console.log('Server running. Use our API on port: 3000');
+  });
+ })
+ .catch(err => {
+  console.error('Database connection error:', err.message);
+  process.exit(1);
+ });
