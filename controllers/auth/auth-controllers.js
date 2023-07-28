47ee8b702fd7556afcfcb3599e01dba678edfc2a
Serhii Naboka <drifterzzz@gmail.com>
2023-07-28 16:57:47 +0300

    upd

--- controllers/auth/auth-controllers.js
+++ controllers/auth/auth-controllers.js
@@ -2,8 +2,14 @@ const User = require("../../models/user");
 const { HttpError } = require("../../helpers");
 const ctrlWrapper = require("../../decorators");
 
-const signup = async (req, res) => {};
-
+const signup = async (req, res) => {
+	const newUser = await User.create(req.body);
+	
+	res.status(201).json({
+		name: newUser.name,
+		email: newUser.email,
+	})
+};
 
 module.exports = {
   signup: ctrlWrapper(signup),
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const authRouter = express.Router();
 
-const {userSignUpSchema} = require("../../schemas");
-const { validation, isValidId, isAmptyBody } = require("../../middlewares");
+const { userSignUpSchema } = require("../../schemas");
+const { validation, isAmptyBody } = require("../../middlewares");
 const validateMiddleWare = validation(userSignUpSchema);
 const ctrl = require("../../controllers/auth/auth-controllers");
 
 authRouter.post("signup", isAmptyBody, validateMiddleWare, ctrl.signup);
-
 
 module.exports = authRouter;
