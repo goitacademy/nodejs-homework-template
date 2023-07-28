21994cfe0c81e39e3b4a2a379676ca9f6da183f4
IgorPetrenko72 <i.v.petrenko@gmail.com>
2023-07-28 20:15:12 +0200

    avatar

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
+const upload = require("./uoload")
 
 module.exports = {
     validateBody,
     isValidId,
     authenticate,
+    upload,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ctrl = require("../../controllers/auth");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 const router = express.Router();
@@ -13,6 +13,8 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 // router.patch('/:contactId/favorite', authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
