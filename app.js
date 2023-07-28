21f299f41a50d1a217b3f1944094ededae8a5d18
AndrewGordienko2704 <109688447+AndrewGordienko2704@users.noreply.github.com>
2023-07-28 22:51:40 +0300

    hw-04

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
 const authRoter = require("./routes/api/auth");
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/api/contacts');
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/auth", authRoter);
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
