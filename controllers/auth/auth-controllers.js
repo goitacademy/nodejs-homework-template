fe05dcc7d2538915c96b20f3eaa59425d167c7ce
Serhii Naboka <drifterzzz@gmail.com>
2023-07-29 19:50:10 +0300

    add token instruments

--- controllers/auth/auth-controllers.js
+++ controllers/auth/auth-controllers.js
@@ -1,47 +1,54 @@
 const User = require("../../models/user");
-const { HttpError } = require("../../helpers");
 const ctrlWrapper = require("../../decorators");
+const { HttpError } = require("../../helpers");
 const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+dotenv.config();
+const { JWT_SECRET } = process.env;
 
 const signup = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
 
-  if (user) throw HttpError(409, "Email in use");
+	if (user) throw HttpError(409, "Email in use");
 
-  const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+	const hashPassword = await bcrypt.hash(password, 10);
+	const newUser = await User.create({ ...req.body, password: hashPassword });
 
-  res.status(201).json({
-    user: {
-      email: newUser.email,
-      subscription: newUser.subscription,
-    },
-  });
+	res.status(201).json({
+		user: {
+			email: newUser.email,
+			subscription: newUser.subscription,
+		},
+	});
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) throw HttpError(401, "Email or password is wrong");
 
-  if (!user) throw HttpError(401, "Email or password is wrong");
+	const passwordCompare = await bcrypt.compare(password, user.password);
+	if (!passwordCompare) throw HttpError(401, "Email or password is wrong");
 
-  const passwordCompare = await bcrypt.compare(password, user.password);
+	const payload = {
+		id: user._id
+	};
 
-  if (!passwordCompare) throw HttpError(401, "Email or password is wrong");
+	const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
 
-  const token = "sdFsJL3dd2de7Y2j";
-
-  res.json({
-    token,
-    user: {
-      email: user.email,
-      subscription: user.subscription,
-    },
-  });
+	res.json({
+		token,
+		user: {
+			email: user.email,
+			subscription: user.subscription,
+		},
+	});
 };
 
 module.exports = {
-  signup: ctrlWrapper(signup),
-  login: ctrlWrapper(login),
+	signup: ctrlWrapper(signup),
+	login: ctrlWrapper(login),
 };
