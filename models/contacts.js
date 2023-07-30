389bf81baf26ccb7dd99c8e763a02fa702ddfe59
Oleg <ikslprint@gmail.com>
2023-07-30 14:54:42 +0300

    add func listContacts, getByID and removeCOntact

--- models/contacts.js
+++ models/contacts.js
@@ -1,10 +1,30 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path');
 
-const listContacts = async () => {}
+const filePath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(filePath);
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const contact = allContacts.find(contact => contact.id === contactId);
+  return contact || null;
+
+}
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex(contact => contact.id === contactId)
+  if (index === -1) return null;
+
+  const [result] = allContacts.splice(index,1);
+  await fs.writeFile(filePath, JSON.stringify(allContacts, null, 2))
+  console.log(result);
+  return result.id;
+}
 
 const addContact = async (body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,20 @@
 const express = require('express')
 
+const contacts = require('../../models/contacts')
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const allContacts = await contacts.listContacts();
+  res.json(allContacts);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+
+  const contactByID = await contacts.getContactById(contactId);
+  res.json(contactByID)
+ 
 })
 
 router.post('/', async (req, res, next) => {
@@ -15,7 +22,9 @@ router.post('/', async (req, res, next) => {
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+  const removeContactById = await contacts.removeContact(contactId)
+  res.json(removeContactById)
 })
 
 router.put('/:contactId', async (req, res, next) => {
