2b5829370c815c2496e86ab0c653da136b48349b
Serhii Naboka <drifterzzz@gmail.com>
2023-07-30 13:50:58 +0300

    upd

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,9 +1,13 @@
 const jwt = require("jsonwebtoken");
+
 const dotenv = require("dotenv");
+
 dotenv.config();
+
 const { JWT_SECRET } = process.env;
 
 const { HttpError } = require("../helpers");
+
 const ctrlWrapper = require("../decorators");
 
 const User = require("../models/user");
@@ -12,18 +16,19 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") throw HttpError(401, "Unauthorized");
+  if (bearer !== "Bearer") throw HttpError(401, "Not authorized");
 
   try {
     const { id } = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(id);
 
-    if (!user) throw HttpError(401, "Unauthorized");
+    if (!user) throw HttpError(401, "Not authorized");
 
     next();
   } catch (error) {
-    throw HttpError(401, "Unauthorized");
+    throw HttpError(401, "Not authorized");
   }
 };
+
 
 module.exports = ctrlWrapper(authenticate);
--- models/contact.js
+++ models/contact.js
@@ -1,24 +1,32 @@
 const { Schema, model } = require("mongoose");
-const { handleSaveError, handleUpdValidate } = require("../models/hooks")
+const { handleSaveError, handleUpdValidate } = require("../models/hooks");
 
-const contactSchema = new Schema({
-	name: {
-		type: String,
-		required: [true, 'Set name for contact'],
-	},
-	email: {
-		type: String,
-		required: [true, 'Set email for contact'],
-	},
-	phone: {
-		type: String,
-		required: [true, 'Set phone for contact'],
-	},
-	favorite: {
-		type: Boolean,
-		default: false,
-	},
-}, { versionKey: false, timestamps: true });
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email for contact"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.pre("findOneAndUpdate", handleUpdValidate);
 
@@ -27,6 +35,5 @@ contactSchema.post("findOneAndUpdate", handleSaveError);
 contactSchema.post("save", handleSaveError);
 
 const Contact = model("contact", contactSchema);
-
 
 module.exports = Contact;
