86b370bc6a73ac2a48c63777066f6f6bb7010dd4
Олександр <moskaletzsanych@gmail.com>
2023-07-30 17:53:37 +0300

    pagination-added

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -5,7 +5,12 @@ import { HttpError } from '../helpers/index.js';
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner });
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, '', { skip, limit }).populate(
+    'owner',
+    'email'
+  );
   res.json(result);
 };
 
