d24be0945e21025c099df622ffc637fd199c37fb
Fransua Boltof <bolt2000@ukr.net>
2023-07-31 19:03:26 +0300

    +

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,9 +1,8 @@
 const handleMongooseError = (error, data, next) => {
   const { name, code } = error;
-  const status = (name === "MongoServerError" && code === 11000) ? 409 : 400;
-  
+  const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+
   error.status = status;
-  // console.log(error);
   next();
 };
 
