c13e5abeadf42c8c135d83e312cf0a72b0f99126
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-07-31 12:45:23 +0300

    Jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -3,16 +3,24 @@ const { User } = require("../../models/users");
 const { ctrlWrapper } = require("../../helpers");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
-const updateAvatar = async (req, res) => {
-  const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
-  const filename = `${_id}_${originalname}`;
-
+const updateAvatar = async (req, res, next) => {
   try {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
+
+    await Jimp.read(tempUpload)
+      .then((image) => {
+        return image.resize(250, 250).write(resultUpload);
+      })
+      .catch((error) => {
+        throw error;
+      });
 
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = `/avatars/${filename}`;
@@ -22,8 +30,7 @@ const updateAvatar = async (req, res) => {
       avatarURL,
     });
   } catch (error) {
-    await fs.unlink(tempUpload);
-    throw error;
+    next(error);
   }
 };
 
