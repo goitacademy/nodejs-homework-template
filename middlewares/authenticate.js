8b54afdc979917b86188f6fbc348dec8fbc0ec26
OleksandrNovyk <novykav@gmail.com>
2023-07-31 18:42:35 +0300

    1234

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
   try {
