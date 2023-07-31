2665e5151d0e2bee3d217fb2f83aaec93bc8df6c
TarasDer <tderev.switch@gmail.com>
2023-07-31 23:00:32 +0300

    changed mongoose schema

--- models/contact.js
+++ models/contact.js
@@ -2,22 +2,25 @@ import { Schema, model } from "mongoose";
 
 import { handleSaveError, handleUpdateValidate } from "./hooks.js";
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false }
+);
 
 contactSchema.pre("findOneAndUpdate", handleUpdateValidate);
 
