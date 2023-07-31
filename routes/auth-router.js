0dc369aef652cdaed4df44a1e51f6fc44b7fffe6
Олександр <moskaletzsanych@gmail.com>
2023-07-31 07:05:43 +0300

    Update auth-router.js

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -24,7 +24,7 @@ authRouter.get('/current', authenticate, authController.getCurrent);
 authRouter.post('/logout', authenticate, authController.logout);
 
 authRouter.patch(
-  '/subscription',
+  '/',
   authenticate,
   isEmptySubscription,
   validateBody(usersSchemas.userUpdateSubscriptionSchema),
