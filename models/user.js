4f70a73e7e7a302d214def42e74a425faec96e70
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-08-01 13:06:40 +0200

    .

--- models/user.js
+++ models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const { Schema, model } = require('mongoose');
 const { handleMongooseError } = require('../helpers');
 
@@ -21,48 +20,11 @@ const userSchema = new Schema ({
     enum: ["starter", "pro", "business"],
     default: "starter"
   },
-  token: String,
-  avatarURL: String,
+  token: String
 }, {versionKey: false})
-=======
-const { Schema, model } = require("mongoose");
-const { handleMongooseError } = require("../helpers");
-
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-const userSchema = new Schema(
-  {
-    password: {
-      type: String,
-      required: [true, "Set password for user"],
-      minlength: 7,
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      match: emailRegexp,
-      unique: true,
-    },
-    subscription: {
-      type: String,
-      enum: ["starter", "pro", "business"],
-      default: "starter",
-    },
-    token: String,
-    avatarURL: String,
-  },
-  { versionKey: false }
-);
->>>>>>> Stashed changes
 
 userSchema.post('save', handleMongooseError)
 
-<<<<<<< Updated upstream
 const User = model('user', userSchema)
 
 module.exports = User
-=======
-const User = model("user", userSchema);
-
-module.exports = User;
->>>>>>> Stashed changes
