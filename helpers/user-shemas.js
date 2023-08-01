5be0b5e75617249681c05a12e0e41c6a2e4e46e0
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-01 17:09:34 +0300

    Update user-shemas.js

--- helpers/user-shemas.js
+++ helpers/user-shemas.js
@@ -3,13 +3,13 @@ import joi from 'joi';
 import { emailRegexp } from '../constans/user-constans.js';
 
 const userSignupShema = joi.object({
-    email: joi.string().pattern(emailRegexp).required,
-    password: joi.string().min(6).required,
+    email: joi.string().pattern(emailRegexp).required(),
+    password: joi.string().min(6).required(),
 })
 
 const userSigninShema = joi.object({
-    email: joi.string().pattern(emailRegexp).required,
-    password: joi.string().required,
+    email: joi.string().pattern(emailRegexp).required(),
+    password: joi.string().required(),
 })
 
 export default {
