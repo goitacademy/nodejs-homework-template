406af0a0cc7234c97a233f452c45055ae318ce21
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-01 11:15:24 +0300

    Create auth-router.js

---
+++ routes/api/auth-router.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import authController from "../../controller/auth-controller.js";
+
+// import authenticate from "../../middlewars/authenticate.js";
+
+const authRouter = express.Router();
+
+authRouter.post('/register', authController.signup );
+
+authRouter.post('/login', authController.signin);
+
+authRouter.get('/current', authenticate, authController.getCurrent);
+
+authRouter.post('/logout', authenticate, authController.logout )
+
+export default authRouter;
