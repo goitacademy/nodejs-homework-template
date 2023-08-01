733db15109b7bac2e3ac172d0091b7830b707c59
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-01 12:41:02 +0300

    Update auth-router.js

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -6,12 +6,12 @@ import authenticate from "../../middlewars/authenticate.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/register', authController.signup );
+authRouter.post('/register', authController.signup);
 
 authRouter.post('/login', authController.signin);
 
 authRouter.get('/current', authenticate, authController.getCurrent);
 
-authRouter.post('/logout', authenticate, authController.logout )
+authRouter.post('/logout', authenticate, authController.logout);
 
 export default authRouter;
