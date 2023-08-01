14cf158e536c974056d6bac163adf7a87cde18b5
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-01 12:11:33 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,20 +4,39 @@ import { handleSaveError, validateAtUpdate } from "./hooks.js";
 
 import { emailRegexp } from "../constans/user-constans.js";
 
-const userShema = new Schema({
-    name {
-    type: String,
-    required: true
-},
-    email {
-    type: String,
-    math: emailRegexp,
-    unique: true,
-    required: true
-},
-    password {
-    type: String,
-    minlenth: 6,
-    required: true
-},
-}, { versionKey: false, timestamps: true });
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.pre("findOneAndUpdate", validateAtUpdate);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const User = model("user", userSchema);
+
+export default User;
