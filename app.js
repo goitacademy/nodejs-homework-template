b5a06c36a72f2b138822cd6e36e5fe5b620fb98d
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-01 12:10:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,28 @@ import router from "./routes/api/contactsRouter.js";
 
 import authRouter from "./routes/api/auth-router.js";
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
 
-app.use('/api/contacst', authRouter)
-app.use('/api/contacts', router)
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/contacst', authRouter);
+
+app.use('/api/contacts', router);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
-})
+});
 
 app.use((err, req, res, next) => {
-  const {status = 500, message = "Server error"} = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message, })
-})
+});
 
-export default app
-
+export default app;
+  
