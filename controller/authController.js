f450e52725aa2e4142b7c40b00d7481face6975c
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-01 11:15:07 +0300

    Create authController.js

---
+++ controller/authController.js
@@ -0,0 +1,14 @@
+import User from '../models/user.js';
+
+import HttpError from '../helpers/HttpError.js';
+
+import userShemas from '../helpers/user-shemas.js';
+
+import bcrypt from "bcryptjs";
+
+import jwt from "jsonwebtoken";
+
+import "dotenv/config";
+
+
+
