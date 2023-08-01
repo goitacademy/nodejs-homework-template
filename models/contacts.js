d07a4f69cf75a80730a416eff0a78f983df8aad9
Pavlo-V-M <letter.screen.1@gmail.com>
2023-08-01 13:25:39 +0300

    test-1

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,68 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+// Отримуємо данні із файлу contacts.json.
+// Створюємо абсолютний шлях до файлу із даними, які треба обробити.
+const contactsPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = contacts => {
+	return fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const removeContact = async (contactId) => {}
+// Створюємо методи обробки данних.
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) { 
+    return null;
+  };
+  const [result] = contacts.splice(index, 1);
+  // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  await updateContacts(contacts);
+  return result || null;
+}
 
-module.exports = {
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ... body
+  }
+  contacts.push(newContact);
+  // await fs.readFile(contactsPath, JSON.stringify(contacts, null, 2));
+  await updateContacts(contacts);
+  return newContact;
+}
+
+const updateContact = async(contactId, {name, email, phone}) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) { 
+    return null;
+  }
+  contacts[index] = { id: contactId, name, email, phone };
+  // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  await updateContacts(contacts);
+  return contacts[index];
+}
+
+// Створюємо достуність методів обробки данних в інших файлах
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
