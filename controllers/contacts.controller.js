ed2d258853ec9995799e4e82ea0897ea2e4340d1
hasin01 <81334215+hasin01@users.noreply.github.com>
2023-08-02 11:03:48 +0300

    full

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,3 +1,4 @@
+
 const fs = require("fs/promises");
 const { nanoid } = require("nanoid");
 const path = require("path");
@@ -8,7 +9,6 @@ const listContacts = async (req, res, next) => {
     const data = await fs.readFile(contactsPath);
     const contacts = JSON.parse(data);
     res.status(200).json(contacts);
-    return contacts;
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
@@ -18,19 +18,18 @@ const listContacts = async (req, res, next) => {
 const getContactById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    if (id !== -1) {
-      const data = await fs.readFile(contactsPath);
-      const contacts = JSON.parse(data);
-      const contact = contacts.find((c) => c.id === id);
-      if (!contact) {
-        res.status(404).json({ message: "Not Found" });
-      }
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((c) => c.id === id);
+    if (!contact) {
+      res.status(404).json({ message: "Not Found" });
+    } else {
       res.status(200).json(contact);
       console.log(contact);
-      return contact;
     }
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -40,18 +39,12 @@ const removeContact = async (req, res, next) => {
     const data = await fs.readFile(contactsPath);
     const contacts = JSON.parse(data);
     const contactIndex = contacts.findIndex((el) => el.id === contactId);
-    const removedContact = contacts.find((el) => el.id === contactId);
     if (contactIndex === -1) {
       console.log(null);
-      return;
-    }
-    contacts.splice(contactIndex, 1);
-    console.log(removedContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    if (!removedContact) {
-      res.status(404).json({ message: "Not found" });
     } else {
-      removeContact(contactId);
+      contacts.splice(contactIndex, 1);
+      console.log(contacts[contactIndex]);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
       res.status(200).json({ message: "Contact deleted" });
     }
   } catch (error) {
@@ -71,7 +64,6 @@ const addContact = async (req, res, next) => {
     await fs.writeFile(contactsPath, JSON.stringify(contacts));
     res.status(201).json(newContact);
     console.log(newContact);
-    return newContact;
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
@@ -83,19 +75,22 @@ const updateContact = async (req, res, next) => {
   const contacts = JSON.parse(data);
   const { contactId } = req.params;
   const contactIndex = contacts.findIndex((el) => el.id === contactId);
-  const contact = contacts.find((el) => el.id === contactId);
-  const updatedContact = { ...contact, ...req.body };
-  contacts[contactIndex] = updatedContact;
-  try {
-    if (!req.body) {
-      return res.status(400).json({ message: "Missing fields" });
+  if (contactIndex === -1) {
+    res.status(404).json({ message: "Not Found" });
+  } else {
+    const updatedContact = { ...contacts[contactIndex], ...req.body };
+    contacts[contactIndex] = updatedContact;
+    try {
+      if (!req.body) {
+        res.status(400).json({ message: "Missing fields" });
+      } else {
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+        res.status(200).json(updatedContact);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: "Not found" });
     }
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    res.status(200).json(updatedContact);
-    return updatedContact;
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({ message: "Not found" });
   }
 };
 
