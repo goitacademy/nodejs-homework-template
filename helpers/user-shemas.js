5c73edbd06c5a44eecefcabf50451d0c750ac382
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-02 22:06:17 +0300

    Update user-shemas.js

--- helpers/user-shemas.js
+++ helpers/user-shemas.js
@@ -3,13 +3,24 @@ import joi from 'joi';
 import { emailRegexp } from '../constans/user-constans.js';
 
 const userSignupShema = joi.object({
-    email: joi.string().pattern(emailRegexp).required(),
-    password: joi.string().min(6).required(),
+    email: joi.string()
+        .pattern(emailRegexp)
+        .required()
+        .messages({ "any.required": 'missed required email field' }),
+    password: joi.string()
+        .min(6)
+        .required()
+        .messages({ "any.required": 'missed required password field' }),
 })
 
 const userSigninShema = joi.object({
-    email: joi.string().pattern(emailRegexp).required(),
-    password: joi.string().required(),
+    email: joi.string()
+        .pattern(emailRegexp).
+        required()
+        .messages({ "any.required": 'missed required email field' }),
+    password: joi.string()
+        .required()
+        .messages({ "any.required": 'missed required password field' }),
 })
 
 export default {
