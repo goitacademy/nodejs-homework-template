da181e9338d3f7d60740caee801c4af1d1198b0e
DenisSkv <19phantom95@gmail.com>
2023-08-02 22:16:11 +0300

    hw03_done

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+require("dotenv").config();
+
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
