d361363cf7998fc46f8ebcef8c964ee3f96e46ba
BogdanTitsky <bogdan.titsky@gmail.com>
2023-08-04 15:00:15 +0300

    end

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,58 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
 
-const listContacts = async () => {}
+const { nanoid } = require('nanoid');
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+    const allContacts = await fs.readFile(contactsPath);
+    return JSON.parse(allContacts);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+    const allContacts = await listContacts();
+    const result = allContacts.find((contact) => contactId === contact.id);
+    return result || null;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = allContacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return result;
+};
+
+const addContact = async (body) => {
+    const allContacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        ...body,
+    };
+    allContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    allContacts[index] = { id: contactId, ...body };
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return allContacts[index];
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
+};
