32643e2742173c6b2aafdb1a3e4990655c2a6d8b
Eduard <edyabrug91@gmail.com>
2023-08-04 14:42:55 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Eduard:edik02092001@cluster0.chinkxb.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
