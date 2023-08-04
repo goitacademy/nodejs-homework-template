1e6f237bf934e8dce42f2f8276dde590a47c1da9
Eduard <edyabrug91@gmail.com>
2023-08-04 15:16:50 +0300

    move DB_HOST to config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const DB_HOST =
+  "mongodb+srv://Eduard:edik02092001@cluster0.chinkxb.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+module.exports = { DB_HOST };
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Eduard:edik02092001@cluster0.chinkxb.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
