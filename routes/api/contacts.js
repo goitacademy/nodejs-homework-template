d5434ca9ea25eb3fe018ff64fa09a107d691c52f
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-08-06 10:32:02 +0200

    add validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,105 @@
-const express = require('express')
+const express = require("express");
+const {schema} = require('../../utils/validation')
+const router = express.Router();
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message get' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.json({
+    status: "200",
+    data: {
+      contacts,
+    },
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (contact) {
+    res.json({
+      status: 200,
+      data: {
+        contact,
+      },
+    });
+  } else {
+    res.json({
+      status: 404,
+      message: "Not found",
+    });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const responseBody = schema.validate(req.body)
+  if (responseBody.error) {
+    res.json({
+      message: responseBody.error.message
+    })
+  } else {
+    const newContact = await addContact(req.body)
+     res.json({
+    status: "201",
+    data: {
+      newContact,
+    },
+  });
+  }
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contacts = await removeContact(contactId);
+
+  if (contacts) {
+    res.json({
+      status: 200,
+      message: "contact deleted",
+    });
+  } else {
+    res.json({
+      status: 404,
+      message: "Not found",
+    });
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+
+  const { contactId } = req.params;
+  const responseBody = schema.validate(req.body)
+  if (responseBody.error) {
+    res.json({
+      status: 400,
+      message: responseBody.error.message
+     
+    })
+  } else {
+    const contact = await updateContact(contactId, req.body);
+    if (contact) {
+      res.json({
+        status: 200,
+        data: { contact },
+      });
+    } else {
+      res.json({
+        status: 404,
+        message: "Not found",
+      });
+    }
+    }
+  });
+  
+
+
+module.exports = router;
---
+++ utils/validation.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string()
+    .required(),
+    email: Joi.string()
+    .email()
+    .required(),
+    phone: Joi.number().required(),
+    // id: Joi.string().required(),
+}) 
+
+module.exports = {schema}
