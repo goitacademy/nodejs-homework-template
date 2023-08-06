0813b164d5d932822de271abac8a1f003fe05167
petekostrytsya <petekostrytsya@gmail.com>
2023-08-06 09:13:25 +0300

    add Controllers

--- models/contacts.js
+++ models/contacts.js
@@ -1,60 +1,45 @@
 const fs = require('fs/promises')
 const path = require('path')
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.join(__dirname, 'contacts.json')
 
 const listContacts = async () => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8')
+    const data = await fs.readFile(contactsPath)
     const contacts = JSON.parse(data)
     return contacts
-  } catch (err) {
-    console.log(err)
-  }
 }
 
 const getContactById = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8')
+    const data = await fs.readFile(contactsPath)
     const contacts = JSON.parse(data)
     return contacts.find((contact) => contact.id === contactId)
-  } catch (err) {
-    console.log(err)
-  }
 }
 
 const removeContact = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8')
+    const data = await fs.readFile(contactsPath)
     const contacts = JSON.parse(data)
     const index = contacts.findIndex((contact) => contact.id === contactId)
     contacts.splice(index, 1)
     await fs.writeFile(contactsPath, JSON.stringify(contacts))
     return contacts
-  } catch (err) {
-    console.log(err)
-  }
 }
 
-const addContact = async (body) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8')
-    const contacts = JSON.parse(data)
-    const newContact = {
-      id: contacts.length + 1,
-    ...body,
-    }
-    contacts.push(newContact)
-    await fs.writeFile(contactsPath, JSON.stringify(contacts))
-    return newContact
-  } catch (err) {
-    console.log(err)
-  }
+const addContact = async (name, email, phone) => {
+    const contacts = await listContacts();
+        const newContact = {
+            id: nanoid(),
+            name,
+            email,
+            phone,
+        };
+        contacts.push(newContact);
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+        return newContact;
 }
 
 const updateContact = async (contactId, body) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8')
+    const data = await fs.readFile(contactsPath)
     const contacts = JSON.parse(data)
     const index = contacts.findIndex((contact) => contact.id === contactId)
     contacts[index] = {
@@ -63,9 +48,6 @@ const updateContact = async (contactId, body) => {
     }
     await fs.writeFile(contactsPath, JSON.stringify(contacts))
     return contacts
-  } catch (err) {
-    console.log(err)
-  }
 }
 
 module.exports = {
