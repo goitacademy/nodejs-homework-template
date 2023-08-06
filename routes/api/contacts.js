20cfce08c0f9417ea0740da89d1d7f7ee513a9fc
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-08-06 18:09:45 +0200

    routing

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,110 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const nanoid = require("nanoid");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactsFunctions = require("../../models/contacts");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/api/contacts", (req, res, next) => {
+  res.json({
+    status: "Success",
+    code: 200,
+    data: contactsFunctions.listContacts(),
+  });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/api/contacts/:id", (req, res, next) => {
+  const { id } = req.body;
+  const [contact] = contactsFunctions.getContactById(id);
+  if (!contact) {
+    res.json({
+      status: "Error",
+      code: 404,
+      message: "Not found",
+    });
+  } else {
+    res.json({
+      status: "200",
+      code: 200,
+      data: { contact },
+    });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/api/contacts", (req, res, next) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.json({
+      status: "400",
+      code: 400,
+      message: "missing required name - field",
+    });
+  } else {
+    const contact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    const [addedContact] = contactsFunctions.addContact(contact);
+    res.json({
+      status: "200",
+      code: 200,
+      data: { addedContact },
+    });
+  }
+});
 
-module.exports = router
+router.delete("/api/contacts/:id", (req, res, next) => {
+  const { id } = req.params;
+  if (!id) {
+    res.json({
+      status: "404",
+      code: 404,
+      message: "Not found",
+    });
+  } else {
+    contactsFunctions.removeContact(id);
+    res.json({
+      status: "200",
+      code: 200,
+      message: "Contact deleted",
+    });
+  }
+});
+
+router.put("/api/contacts/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.json({
+      status: "400",
+      code: 400,
+      message: "Missing fields",
+    });
+  } else {
+    const body = {
+      id,
+      name,
+      email,
+      phone,
+    };
+    const [updatedContact] = contactsFunctions.updateContact(id, body);
+    if (!updatedContact) {
+      res.json({
+        status: "200",
+        code: 200,
+        data: { updatedContact },
+      });
+    } else {
+      res.json({
+        status: "404",
+        code: 404,
+        message: "Not found",
+      });
+    }
+  }
+});
+
+module.exports = router;
