e622e638838c27c8fd50685bd29ee689db219fd9
Joanna K <joannakleinrm@gmail.com>
2023-08-06 14:58:04 +0200

    addContact added

--- models/contacts.js
+++ models/contacts.js
@@ -28,11 +28,39 @@ const getContactById = async (contactId) => {
     );
 };
 
+const addContact = async (body) => {
+  const date = new Date();
+
+  const contacts = await fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contacts) => {
+      const contactId = (
+        Math.floor(Math.random() * 1000) * parseInt(date.getTime() / 100)
+      ).toString();
+      body = { id: contactId, ...body };
+      return [...contacts, body];
+    });
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return body;
+};
+
 const removeContact = async (contactId) => {
-
-}
-
-const addContact = async (body) => {}
+  let isContact = false;
+  const contacts = fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contacts) =>
+      contacts.filter((contact) => {
+        if (contact.id === contactId) {
+          isContact = true;
+        }
+        return contact.id !== contactId;
+      })
+    );
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return isContact;
+};
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router();
 
-const { listContacts, getContactById } = require('../../models/contacts');
+const { listContacts, getContactById, addContact } = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
   const contacts = await listContacts();
@@ -38,11 +38,38 @@ router.get('/:contactId', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { name, email, phone } = await req.body;
+    const contact = await addContact({ name, email, phone });
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: { contact },
+    });
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (contact) {
+      res.json({
+        status: 'success',
+        code: 200,
+        data: { contact },
+      });
+      return;
+    }
+    res.status(404).json({
+      status: "Not found",
+      code: 404,
+    })
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
