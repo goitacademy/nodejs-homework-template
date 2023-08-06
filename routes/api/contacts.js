f70a66a8c5d8450bf0010470048297e1f9a9ad33
Artemon Chik <zagrousaspear@gmail.com>
2023-08-06 15:28:16 +0200

    completed hw-02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,81 @@
 const express = require("express");
-
+const Joi = require("joi");
+const contacts = require("../../models/contacts");
 const router = express.Router();
-
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 router.get("/", async (req, res, next) => {
-  res.json({ message: "New" });
+  try {
+    const results = await contacts.listContacts();
+    res.status(200).json(results);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const response = await contacts.getContactById(contactId);
+    if (!response) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      const type = error.details.map(({ path }) => path);
+      return res.status(400).json({
+        message: `missing required ${type} field`,
+      });
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (result) {
+      return res.status(200).json({
+        message: "contact deleted",
+      });
+    }
+    res.status(404).json({ message: "Not found" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      const type = error.details.map(({ path }) => path);
+      return res.status(400).json({ message: `missing fields ${type}` });
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
