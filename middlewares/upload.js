f0beeeb68bc53ac92a27661c67a17cbc0cbd9795
Serhii Naboka <drifterzzz@gmail.com>
2023-08-06 13:43:44 +0300

    upd

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 
-const destination = path.resolve("temp");
+const destination = path.resolve("tmp");
 
 const storage = multer.diskStorage({
 	destination,
