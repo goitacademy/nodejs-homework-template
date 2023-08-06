c7ab3a55f1a3256353a262803cd96dd03b7a86d1
Elborn666 <elborn18@gmail.com>
2023-08-06 12:45:45 +0300

    всі кроки зроблені (наче б то працює)

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,52 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const {nanoid} = require('nanoid');
 
-const listContacts = async () => {}
+const contactPath = path.join(__dirname, "../models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const allContacts = await fs.readFile(contactPath)
+  return JSON.parse(allContacts);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (id) => {
+  const contactId = await listContacts();
+  const getId = contactId.find(item => item.id === id)
+  return getId || null;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (id) => {
+  const deleteContact = await listContacts();
+  const index = deleteContact.findIndex(item => item.id === id)
+  if (index === -1) {
+    return null;
+  }
+  const [result] = deleteContact.splice(index, 1)
+  await fs.writeFile(contactPath, JSON.stringify(deleteContact, null, 2))
+  return result;
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  const addContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  }
+  addContacts.push(newContact);
+  await fs.writeFile(contactPath, JSON.stringify(addContacts, null, 2))
+  return newContact;
+}
+
+const updateContact = async (id, body) => {
+  const updateContacts = await listContacts();
+  const index = updateContacts.findIndex(item => item.id === id)
+  if (index === -1) {
+    return null;
+  }
+  updateContacts [index] = {id, ...body};
+  await fs.writeFile(contactPath, JSON.stringify(updateContacts, null, 2))
+  return updateContacts [index];
+}
 
 module.exports = {
   listContacts,
