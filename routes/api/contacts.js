2d894905a8c197e0ef81138ebcf565b40d7ac0a9
Joanna K <joannakleinrm@gmail.com>
2023-08-06 23:10:46 +0200

    validation / joi install

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const Joi = require('joi');
 
 const router = express.Router();
 
-const { listContacts, getContactById, addContact, updateContact, removeContact } = require('../../models/contacts');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact } = require('../../models/contacts');
+
 // const contacts = listContacts();
+
+// validation of POST body
+const contactValidation = Joi.defaults(() =>
+  Joi.object({
+    name: Joi.string().pattern(
+      /^([A-ZĄĆĘŁŃÓŚŹŻ]+'?[a-ząćęłńóśźż]+|[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+'?[a-ząćęłńóśźż]+) ([A-ZĄĆĘŁŃÓŚŹŻ]+'?[a-ząćęłńóśźż]+|[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+'?[a-ząćęłńóśźż]+)$/
+    ),
+    email: Joi.string().email(),
+    phone: Joi.string().pattern(
+      /^([+][0-9]{0,4})?[\s]?([(][0-9]{1,3}[)])?[\s]?[0-9]{2,3}[-\s]?[0-9]{2,3}[-\s]?[0-9]{2,4}$/
+    ),
+  })
+);
+
+const schemaRequired = contactValidation
+  .object()
+  .options({ presence: "required" })
+  .required();
+
+const schema = contactValidation.object().or("name", "email", "phone");
+
 
 router.get('/', async (req, res, next) => {
   const contacts = await listContacts();
@@ -41,6 +69,15 @@ router.get('/:contactId', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const { name, email, phone } = await req.body;
+    const validation = schemaRequired.validate({ name, email, phone });
+    if (validation.error) {
+      res.status(400).json({
+        message: validation.error.details[0].message,
+        code: 400,
+      });
+      return;
+    }
+
     const contact = await addContact({ name, email, phone });
     res.status(201).json({
       status: "success",
@@ -60,9 +97,7 @@ router.delete('/:contactId', async (req, res, next) => {
       res.json({
         status: "success",
         code: 200,
-        // data: { contact },
       });
-      // return;
     } else { 
       res.status(404).json({
         status: "Not found",
@@ -78,6 +113,15 @@ router.put('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const { name, email, phone } = req.body;
+    const validation = schema.validate({ name, email, phone });
+    if (validation.error) {
+      res.status(400).json({
+        message: validation.error.details[0].message,
+        code: 400,
+      });
+      return;
+    }
+
     const isContact = await updateContact(contactId);
     isContact.name = name;
     isContact.email = email;
