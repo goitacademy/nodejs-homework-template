faef5d2fb135ce4ff2f235669f4dc2b3d6f7d111
Sofia <116076116+Sofia1903@users.noreply.github.com>
2023-08-06 11:48:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST).then(() => {
+// mongoose.connect(DB_HOST).then(() => {
 
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
-  });
-  console.log("Database connect success");
-})
-.catch((error) => {
-  console.log(error.message);
+//   app.listen(PORT, () => {
+//     console.log(`Server running. Use our API on port: ${PORT}`);
+//   });
+//   console.log("Database connect success");
+// })
+// .catch((error) => {
+//   console.log(error.message);
   
-  process.exit(1);
-});
+//   process.exit(1);
+// });
 
 
 
