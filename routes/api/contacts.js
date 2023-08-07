8bf1e7597592c5e155740dd3bd38fc99edee0b76
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-08-07 23:12:39 +0200

    test upd

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -15,7 +15,7 @@ router.post('/', async (req, res, next) => {
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message!' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
