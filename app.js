1cf30c0e12246c708d9d300592d53b73536887a2
Oleksandr Khomiak <khomiak@gmail.com>
2023-08-07 20:33:08 +0300

    first

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 // ========ROUTES======== //
 app.use("/users", usersRouter);
