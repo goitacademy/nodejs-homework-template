e3a2828f7da17d59894bfdc7b03913684a4405e0
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-08-07 18:57:48 +0300

    up

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Інфа готовенька' })
+  res.json({ message: 'Інфа готовенька оп оп оп' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
