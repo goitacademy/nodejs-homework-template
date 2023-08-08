e1e8b9e3ae75c5c7d12ff66463c465e95c25230b
Nikita <bazilik2452@gmail.com>
2023-08-08 14:18:22 +0200

    dell comments

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,14 +2,11 @@ const { Contact } = require("../models/contact");
 
 const listContacts = async () => {
   const contacts = await Contact.find();
-  //   return JSON.parse(contacts);
+
   return contacts;
 };
 
 const getContactById = async (contactId) => {
-  //   const contacts = await listContacts();
-  //   const contact = contacts.find((el) => el.id === contactId);
-  //   return contact || null;
   const contact = await Contact.findById(contactId);
   return contact;
 };
