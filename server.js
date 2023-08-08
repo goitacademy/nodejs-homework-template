3acd118ae916e34f695f576de947b733b3615ce1
Pavlo-V-M <letter.screen.1@gmail.com>
2023-08-08 16:51:24 +0300

    test-1

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+import DB_HOST from "./config.js";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => { 
+    app.listen(3000)
+  })
+  .catch(error => { 
+    console.log(error.message);
+    process.getMaxListeners(1);
+  })
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
