aa55e84520596013d873307211df5c54db050531
Enital <117684114+Enital@users.noreply.github.com>
2023-08-08 20:36:42 +0300

    08-20.36

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,69 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+const { contactPutSchema, contactPostSchema } = require("../../validate");
+const crypto = require("crypto");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    return res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await getContactById(id);
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", contactPostSchema, async (req, res, next) => {
+  const body = req.body;
+  try {
+    const newContact = await addContact({ id: crypto.randomUUID(), ...body });
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await removeContact(id);
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:id", contactPutSchema, async (req, res, next) => {
+  const { id } = req.params;
+  const body = req.body;
 
-module.exports = router
+  try {
+    if (Object.keys(body).length === 0) {
+      res.status(400).json({ message: "missing fields" });
+      return;
+    }
+    const putContact = await updateContact(id, body);
+    res.status(200).json(putContact);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+module.exports = router;
---
+++ validate/contactPostSchema.js
@@ -0,0 +1,17 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
+
+const contactPostSchema = (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json(error.message);
+  }
+  next();
+};
+
+module.exports = contactPostSchema;
