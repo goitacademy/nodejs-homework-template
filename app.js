aef077c02cb2b6fee6b62ba2bbb14ea4a8677460
Burmikk <byrmik@gmail.com>
2023-08-08 22:14:03 +0300

    edit app routes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger/swagger-file.json");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./swagger/swagger-file.json");
 
 const contactsRouter = require("./routes/api/contacts-routes");
 const authRouter = require("./routes/api/auth-routes");
@@ -21,8 +21,7 @@ app.use(express.static("public"));
 
 app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
-
-app.use("/api-docs", apiRouter);
+app.use("/api-doc", apiRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../../swagger/swagger-file.json");
 
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+router.use("/", swaggerUi.serve);
+router.get("/", swaggerUi.setup(swaggerDocument));
+
+module.exports = router;
