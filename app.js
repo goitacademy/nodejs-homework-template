2ca8cc61450d138b8b004ba6e3bbd72d19b4b126
Burmikk <byrmik@gmail.com>
2023-08-08 23:27:18 +0300

    edit app js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
-app.use("/api-docs", apiRouter);
+// app.use("/api-docs", apiRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
