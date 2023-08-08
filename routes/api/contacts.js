ab403e102f84c04e20227e7f5f8e968035350b3a
Yevhenii Malinovskiy <118572078+Travellin91@users.noreply.github.com>
2023-08-08 23:04:45 +0300

    +наче все працює, як на мене :)

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,110 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
+const { nanoid } = require("nanoid/non-secure");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-const router = express.Router()
+const schemaPost = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  phone: Joi.string()
+    .pattern(/^(?:\+38)?0\d{9}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid phone number format",
+      "any.required": "Phone number is required",
+    }),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemaPut = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30),
+  phone: Joi.string()
+    .pattern(/^(?:\+38)?0\d{9}$/)
+    .messages({
+      "string.pattern.base": "Invalid phone number format",
+      "any.required": "Phone number is required",
+    }),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.status(200).json(contacts);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await getContactById(req.params.contactId);
+    res.status(200).json(contact);
+  } catch (err) {
+    res.status(404).json({ message: "Contact not found" });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
 
-module.exports = router
+    if (!name || !email || !phone) {
+      throw new Error("missing required name field");
+    }
+    const result = schemaPost.validate({ name, email, phone });
+    if (result.error) {
+      throw new Error(`${result.error.message}`);
+    } else {
+      console.log("Phone number is valid");
+    }
+
+    const id = nanoid();
+    const newContact = { id, name, email, phone };
+
+    await addContact(newContact);
+    res.status(201).json(newContact);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const newArr = await removeContact(req.params.contactId);
+    console.log(newArr);
+    res.status(200).json({ message: "contact deleted" });
+  } catch (err) {
+    res.status(404).json({ message: "Not found" });
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    if (Object.keys(req.body).length === 0)
+      return res.status(400).json({ message: "missing fields" });
+    const result = schemaPut.validate({ ...req.body });
+    if (result.error) {
+      throw new Error(`${result.error.message}`);
+    } else {
+      console.log("Phone number is valid");
+    }
+    const updatedContact = await updateContact(req.params.contactId, req.body);
+    res.status(200).json(updatedContact);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
+
+module.exports = router;
