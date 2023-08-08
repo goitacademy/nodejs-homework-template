8e1de11015df13f96d35cb8cbd9468918fd94feb
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-08-08 09:46:10 +0200

    done?

--- models/contacts.js
+++ models/contacts.js
@@ -1,3 +1,4 @@
+// const fs = require('fs/promises')
 const fs = require("fs");
 const path = require("path");
 const { v1: uuidv1 } = require("uuid");
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+app.listen(3002, () => {
+console.log("Server running. Use our API on port: 3000");
 });
 
 const argv = require("yargs").argv;
