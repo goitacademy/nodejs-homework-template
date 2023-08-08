c828166181d3b08baaf31989664dc4c9e9822714
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-08-07 20:00:17 -0700

    clean up addContacts

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,9 +1,6 @@
-// const createError = require("http-errors");
-// const { contactSchema } = require("../../schemas");
 const { contacts: service } = require("../../services");
 
 const addContact = async (req, res) => {
-    
     const result = await service.addContact(req.body);
     
     res.status(201).json({
