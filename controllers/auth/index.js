cd1845e9b8b849dec0b30e1a0c25066a8f3c14a9
Casimova Marina <maracasimova@gmail.com>
2023-08-10 19:52:15 +0300

    controllers

---
+++ controllers/auth/index.js
@@ -0,0 +1,13 @@
+const register = require("./register");
+const login = require("./login");
+const getCurrent = require("./getCurrent");
+const logout = require("./logout");
+// const subscription = require("./subscription");
+
+module.exports = {
+  register,
+  login,
+  getCurrent,
+  logout,
+//   subscription,
+};
---
+++ controllers/auth/login.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { User } = require("../../models/user");
+
+const { RequestError } = require("../../helpers");
+
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw RequestError(401, "Email or password wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw RequestError(401, "Email or password wrong");
+  }
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+  });
+};
+
+module.exports = login;
