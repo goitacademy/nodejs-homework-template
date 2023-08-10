f0a6855175610730d5ec99271bfe7457568888a1
AndriiZlt <andreyzlot@gmail.com>
2023-08-10 17:11:51 +0300

    env

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/HW03-MONGODB?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/HW03-MONGODB?retryWrites=true&w=majority";
+
+const { DB_HOST } = process.env;
+
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
