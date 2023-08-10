f8ae58916601505e9f8465636d15c2036d620889
AndriiZlt <andreyzlot@gmail.com>
2023-08-10 16:25:40 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/HW03-MONGODB?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connect success");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
