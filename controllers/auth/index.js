962a94c169d1e11e2b3927be7609f6dd52c0c714
Casimova Marina <maracasimova@gmail.com>
2023-08-10 23:13:25 +0300

    Update index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,12 +2,10 @@ const register = require("./register");
 const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
-// const subscription = require("./subscription");
 
 module.exports = {
   register,
   login,
   getCurrent,
   logout,
-//   subscription,
 };
