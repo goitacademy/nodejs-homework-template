93d76748c613a9cac5b2e951be5bd3e27b51394c
BogdanTitsky <bogdan.titsky@gmail.com>
2023-08-10 11:59:22 +0300

    hw-3
    Перевірте

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require('./HttpError.js');
 const ctrlWrapper = require('./ctrlWrapper.js');
+const handleMongooseError = require('../helpers/handleMongooseError');
 
 module.exports = {
     HttpError,
     ctrlWrapper,
+    handleMongooseError,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,22 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody } = require('../../middlewares');
+const { validateBody, isValidId } = require('../../middlewares');
 
-const schemas = require('../../schemas/contacts');
+const { schemas } = require('../../models/contacts');
 
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
 
-router.get('/:contactId', ctrl.getById);
+router.get('/:contactId', isValidId, ctrl.getById);
 
-router.post('/', validateBody(schemas.schemaPost), ctrl.addContact);
+router.post('/', validateBody(schemas.schemaPut), ctrl.addContact);
 
-router.delete('/:contactId', ctrl.deleteContact);
+router.delete('/:contactId', isValidId, ctrl.deleteContact);
 
-router.put('/:contactId', validateBody(schemas.schemaPut), ctrl.updateContact);
+router.put('/:contactId', isValidId, validateBody(schemas.schemaPut), ctrl.updateContact);
+
+router.patch('/:contactId/favorite', isValidId, validateBody(schemas.schemaUpdateFavorite), ctrl.updateStatusContact);
 
 module.exports = router;
