0d4277be04a71dd11945d8bc8cce3b6a9c570fb2
AMIHbKA <bishop0013forum@gmail.com>
2023-08-11 06:36:32 +0300

    done

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,7 +7,7 @@ const { User } = require("../models/user");
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
 
