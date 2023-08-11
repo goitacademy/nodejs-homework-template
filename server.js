033abe0e751f96970f95087f3ee7f5ab9df50398
AndriiZlt <andreyzlot@gmail.com>
2023-08-11 15:07:38 +0300

    env remove

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-// const DB_HOST =
-//   "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const DB_HOST =
+  "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-const { DB_HOST, PORT = 3000 } = process.env;
+// const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 mongoose
