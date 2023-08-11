6a728b4516bae4c767731788360b30058f6dc850
AleksandrBreslavets <sasha.bresl2612005@gmail.com>
2023-08-11 18:24:08 +0300

    upd validationBody

--- middlewars/validationBody.js
+++ middlewars/validationBody.js
@@ -3,7 +3,7 @@ const { HttpError } = require("../helpers");
 const validationBody = (schema, errMessage) => {
     return (req, res, next) => {
         const { error } = schema.validate(req.body);
-        if (error) next(HttpError(400, "Missing required name field"));
+        if (error) next(HttpError(400, errMessage));
         next();
     };
 };
