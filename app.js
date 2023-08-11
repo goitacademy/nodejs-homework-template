568f377f31d11db285702ae73e00dc6651065e17
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-08-11 10:56:45 +0300

    Step-1 Added userSchema

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const usersRouter = require("./routes/api/users");
 const contactsRouter = require("./routes/api/contacts");
 const { error } = require("./schemas/contacts");
 
@@ -27,6 +28,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = Schema({
+  password: {
+    type: String,
+    required: [true, "Set password for user"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: String,
+});
+
+const User = model("contact", userSchema);
+
+module.exports = User;
