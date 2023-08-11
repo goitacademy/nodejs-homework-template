475f9b6cf8571f9a8eb7b79fca7040ac84a6f339
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-08-11 17:22:04 +0300

    Additional: Added pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,7 @@ const { Contact } = require("../../models");
 const getAll = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 20 } = req.query;
     const skip = (page - 1) * limit;
     const contacts = await Contact.find({ owner: _id }, "", {
       skip,
