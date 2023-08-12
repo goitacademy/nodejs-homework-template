5ff354e7d0bf6da099f0d366fb624ef5b71f3b9e
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-08-12 21:09:41 +0300

    Additional: Added favorite  filter

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,17 +3,29 @@ const { Contact } = require("../../models");
 const getAll = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite } = req.query;
     const skip = (page - 1) * limit;
-    const contacts = await Contact.find({ owner: _id }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "_id, email subscription");
-    res.json({
-      status: "success",
-      code: 200,
-      data: { result: contacts },
-    });
+    if (favorite) {
+      const contacts = await Contact.find({ owner: _id, favorite }, "", {
+        skip,
+        limit: Number(limit),
+      }).populate("owner", "_id, email subscription");
+      res.json({
+        status: "success",
+        code: 200,
+        data: { result: contacts },
+      });
+    } else {
+      const contacts = await Contact.find({ owner: _id }, "", {
+        skip,
+        limit: Number(limit),
+      }).populate("owner", "_id, email subscription");
+      res.json({
+        status: "success",
+        code: 200,
+        data: { result: contacts },
+      });
+    }
   } catch (error) {
     next(error);
   }
