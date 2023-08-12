5c3b6375d1db15886a52cd348a2724367c54103d
Vladyslav <dyadyavadya308@gmail.com>
2023-08-12 15:55:07 +0300

    db connected

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const app = require('./app')
+const { mongoose } = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = 3001
+const DB_HOST =
+  "mongodb+srv://Vlad-dyadya:JT6AvmjNI3WehBf6@cluster0.ohx0zsm.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("connect seccesfull");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
