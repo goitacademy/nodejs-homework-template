d79e0508ca996e8d2dff7d0afeba5330b1d07f6c
Jacek Pasierb <jack_byk@o2.pl>
2023-08-12 11:26:43 +0200

    Delete lib morgan

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
+
+
 app.use(cors())
 app.use(express.json())
 
