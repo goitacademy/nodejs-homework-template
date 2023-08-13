d979c7d0c982196c63491224e3affd782fe9a0d1
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-08-13 17:52:05 +0300

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,10 +1,29 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
+const Joi = require('joi');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contacts = await listContacts();
+    res.json({
+      status: 'succes',
+      code: 200,
+        data: {
+          contacts,
+        },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
