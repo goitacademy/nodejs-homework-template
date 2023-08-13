a4f839c2995ab4fac2d401eedbb9b196a2e99100
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-08-13 23:00:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not foundd" });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
