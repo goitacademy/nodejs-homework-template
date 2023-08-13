4b8c1542f6c876fee05b941d572d024f49df1b8f
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-08-13 12:52:53 +0200

    add config.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+// lGiTTyySO8TS97OT
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.error("Error connecting to MongoDB: ", error.message);
+    process.exit(1);
+  });
