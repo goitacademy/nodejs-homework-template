8317dd06cc2f0069e92dfd6312433489132182b6
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-08-13 11:09:08 +0300

    hw-02-v1

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,53 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const path = require("path");
 
-const getContactById = async (contactId) => {}
+const { nanoid } = require("nanoid");
 
-const removeContact = async (contactId) => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+	const contacts = await fs.readFile(contactsPath, "utf-8");
+	return JSON.parse(contacts);
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+	const contacts = await listContacts();
+	const result = contacts.find(({ id }) => id === contactId);
+	return result || null;
+};
+
+const removeContact = async (contactId) => {
+	const contacts = await listContacts();
+	const index = contacts.findIndex(({ id }) => id === contactId);
+	if (index === -1) return null;
+	const [result] = contacts.splice(index, 1);
+	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+	return result;
+};
+
+const addContact = async (body) => {
+	const { name, email, phone } = body;
+	const contacts = await listContacts();
+	const newContact = { id: nanoid(), name, email, phone };
+	contacts.push(newContact);
+	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+	return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+	const contacts = await listContacts();
+	const index = contacts.findIndex(({ id }) => id === contactId);
+	if (index === -1) return null;
+	contacts[index] = { id: contactId, ...body };
+	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+	return contacts[index];
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+	updateContact,
+};
