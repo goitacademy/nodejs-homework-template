0faf6b91a69ad1a61cb4c5f734e1e86a43d868f8
Krzysztof R <plkrozbicki@gmail.com>
2023-08-14 12:59:13 +0100

    Homework v 1.0

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,115 @@
-// const fs = require('fs/promises')
+import * as path from 'path';
+import { readFile, writeFile } from 'fs/promises';
+import { nanoid } from 'nanoid';
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('./models', 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const readContactsFile = async () => {
+  try {
+    const contactsJson = await readFile(contactsPath);
+    const contacts = JSON.parse(contactsJson);
+    return contacts;
+  } catch (err) {
+    console.error('Error reading contacts from file: ', err);
+    throw err;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const writeContactsFile = async data => {
+  try {
+    await writeFile(contactsPath, JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error('Error writing contacts to file: ', err);
+    throw err;
+  }
+};
 
-const addContact = async (body) => {}
+export const listContacts = async () => {
+  try {
+    const contacts = await readContactsFile();
+    if (contacts) return contacts;
+    if (!contacts) console.log(`No contacts found`);
+    return false;
+  } catch (err) {
+    console.log('Error getting contact list: ', err);
+    throw err;
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+export const getContactById = async contactId => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+      console.log(`There is no contact with id ${contactId} !`);
+      return false;
+    }
+    return contacts[index];
+  } catch (err) {
+    console.log(`Error getting contact with id ${contactId}: `, err);
+    throw err;
+  }
+};
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+export const removeContact = async contactId => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+      console.log(`There is no contact with id ${contactId} !`);
+      return false;
+    }
+    contacts.splice(index, 1);
+    await writeContactsFile(contacts);
+    console.log(`Contact with id ${contactId} removed successfully`);
+    return true;
+  } catch (err) {
+    console.log(`Error removing contact with id ${contactId}: `, err);
+    throw err;
+  }
+};
+
+export const addContact = async body => {
+  try {
+    const contacts = await listContacts();
+    if (contacts) {
+      const { name, email, phone } = body;
+      const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone,
+      };
+      contacts.push(newContact);
+      await writeContactsFile(contacts);
+      console.log('Contact has been added successfully');
+      return newContact;
+    }
+  } catch (err) {
+    console.log('Error adding new contact: ', err);
+    throw err;
+  }
+};
+
+export const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await listContacts();
+    if (!contacts) {
+      return false;
+    }
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+      console.log(`There is no contact with id ${contactId} !`);
+      return false;
+    }
+    const contact = contacts[index];
+    const updatedContact = { ...contact, ...body };
+    contacts[index] = updatedContact;
+    await writeContactsFile(contacts);
+    console.log(`Contact has been updated successfully.`);
+    return updatedContact;
+  } catch (err) {
+    console.error('An error occurred while updating contact: ', err);
+    throw err;
+  }
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,102 @@
-const express = require('express')
+import * as express from 'express';
 
-const router = express.Router()
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} from './../../models/contacts.js';
+
+export const router = express.Router();
+const sendErrorResponse = (res, status, message) => {
+  res.status(status).json({ message });
+};
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contacts = await listContacts();
+    if (!contacts) {
+      sendErrorResponse(res, 404, `Contact list not found`);
+      return false;
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { contacts },
+    });
+  } catch (err) {
+    sendErrorResponse(res, 500, `An error occurred while getting the contact list: ${err}`);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await getContactById(id);
+    if (!contact) {
+      sendErrorResponse(res, 404, `Contact with id ${id} not found`);
+      return false;
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { contact },
+    });
+  } catch (err) {
+    sendErrorResponse(res, 500, `An error occurred while getting the contact: ${err}`);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const body = req.body;
+  try {
+    const contact = await addContact(body);
+    if (!contact) {
+      sendErrorResponse(res, 404, `Contact not found`);
+      return false;
+    }
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { contact },
+    });
+  } catch (err) {
+    sendErrorResponse(res, 500, `An error occurred while adding the contact: ${err}`);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const isContactRemoved = await removeContact(id);
+    if (!isContactRemoved) {
+      sendErrorResponse(res, 404, `Contact with id ${id} not found`);
+      return false;
+    }
+    res.status(204).json({
+      message: `Contact with ID ${id} has been successfully removed.`,
+    });
+  } catch (err) {
+    sendErrorResponse(res, 500, `An error occurred while removing the contact: ${err}`);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const body = req.body;
+  try {
+    const updatedContact = await updateContact(id, body);
+    if (!updatedContact) {
+      sendErrorResponse(res, 404, `Contact with id ${id} not found`);
+      return false;
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { updatedContact },
+    });
+  } catch (err) {
+    sendErrorResponse(res, 500, `An error occurred while updating the contact: ${err}`);
+  }
+});
