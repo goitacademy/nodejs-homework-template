bf1d8faede9e04298fc837bbc4860924fd65bacb
unknown <p.burchenkov@ukr.net>
2023-08-14 14:00:34 +0300

    fixed

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,7 +3,6 @@ const { HttpError } = require("../helpers");
 
 const isValidId = (req, _, next) => {
   const { contactId } = req.params;
-  console.log(contactId);
   if (!isValidObjectId(contactId)) {
     next(HttpError(404, `${contactId} is not valid id`));
   }
