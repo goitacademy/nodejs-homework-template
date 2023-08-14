3b4e46c8e80861b18f054b818a86214ddc8bc392
Anna Sergienko <annaserg02@gmail.com>
2023-08-14 09:50:59 +0300

    fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -23,9 +23,7 @@ const addContact = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
   const { id } = req.params;
-  if (req.body === undefined) {
-    throw HttpError(400, "missing field favorite");
-  }
+ 
   const result = await Contact.findByIdAndUpdate(id,  req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
--- middlewares/chekBody.js
+++ middlewares/chekBody.js
@@ -3,7 +3,7 @@ const HttpError = require('../helpers');
 const checkBody = (req, res, next) => {
   const body = req.body;
   if (Object.keys(body).length === 0) {
-    throw HttpError(400, 'missing fields');
+    throw HttpError(400, 'missing field');
   }
   next();
 };
