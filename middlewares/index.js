2350f34c9e738b3c7a17d1cfe57e72c213cc97dc
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-14 23:12:08 +0300

    create validators

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+const validateBody = require("./validateBody");
+const handleMongooseError = require('./handleMongooseError');
+const isValidId = require('./isValidId');
+
+module.exports = {
+    validateBody,
+    handleMongooseError,
+    isValidId
+}
--- schemas/validatorContacts.js
+++ schemas/validatorContacts.js
@@ -4,8 +4,14 @@ const addSchema = joi.object({
 	name: joi.string().min(3).required(),
 	email: joi.string().email().required(),
 	phone: joi.string().min(5).required(),
+	favorite: joi.boolean(),
 });
+
+const updateFavoriteSchema = joi.object({
+  favorite: joi.boolean().required(),
+})
 
 module.exports = {
 	addSchema,
+	updateFavoriteSchema,
 }
