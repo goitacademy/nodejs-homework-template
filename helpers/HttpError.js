a657b5d08a5e3a997a1d657bc4faf1150decf294
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-14 19:24:57 +0300

    separation error-func

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,7 +1,7 @@
 const HttpError = (status, message) => {
     const error = new Error(message);
     error.status = status;
-    console.log('ERROR', error.status);
+    // console.log('ERROR', error.status);
     return error;
 }
 
