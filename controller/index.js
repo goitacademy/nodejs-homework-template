df909375f0056a509393b4686690ac09ed397184
Bartek <Barteknr1@gmail.com>
2023-08-15 00:07:51 +0200

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -122,22 +122,13 @@ const updateStatusContact = async (req, res, next) => {
             res.status(400).json({ error: error.message });
         }
         const result = await service.updateStatusContact(contactId, { favorite });
-        if (result) {
-            res.json({
-                status: 'success',
-                code: 200,
-                data: {
-                    contact: result,
-                },
-            })
-        } else {
-            res.status(404).json({
-                status: 'error',
-                code: 404,
-                message: `Not found contact id: ${contactId}`,
-                data: 'Not Found',
-            })
-        }
+        res.json({
+            status: 'success',
+            code: 200,
+            data: {
+                contact: result,
+            },
+        })
     } catch (err) {
         console.error(err);
         next(err);
