08914a396d269089f8ec428db19dfde9b6fd7285
MolchanovSergii <molchanse@gmail.com>
2023-08-15 21:38:57 +0300

    add

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// iBZsIfwMHchJphax
+
+const DB_HOST =
+  "mongodb+srv://Sergii:iBZsIfwMHchJphax@cluster0.uvvqxha.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
