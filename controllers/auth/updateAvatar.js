bcdf2309bef6f2316b8130ad9b589747488072b0
AMIHbKA <bishop0013forum@gmail.com>
2023-08-15 21:00:07 +0300

    done

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,8 @@
 const { User } = require("../../models/user");
 const path = require("path");
-const fs = require("fs/promises");
+// const fs = require("fs/promises");
+const Jimp = require("jimp");
+const { HttpError } = require("../../utilities");
 
 const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
@@ -9,13 +11,20 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.json({
-    avatarURL,
-  });
+  try {
+    const avatar = await Jimp.read(tempUpload);
+    avatar.resize(250, 250).quality(70).write(resultUpload);
+    // await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({
+      avatarURL,
+    });
+  } catch {
+    throw HttpError(500);
+  }
 };
 
 module.exports = updateAvatar;
