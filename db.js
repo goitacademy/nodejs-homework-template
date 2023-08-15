3fce47e34bb59388aef8bdc2e60873b67b0480eb
Bogdddan <bogdankrajnik95@gmail.com>
2023-08-15 22:15:18 +0300

    hw-03

---
+++ db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const DB_URI = process.env.DB_URI;
+
+mongoose
+    .connect(DB_URI)
+    .then(() => console.log("database connected"))
+    .catch((error) => console.error(error));
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const routes = require("./routes");
+
+require("./db");
+
+const app = express();
+
+app.use("/api", routes);
+
+app.use((req, res, next) => {
+  res.status(404).send({ message: "Not Found" });
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send({ message: "Internal Server Error" });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
