af76c6247decef5d06621aeacf486fe7c2a94a52
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-08-16 23:54:16 +0200

    maybe done

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,9 @@
 const validateBody = require("./validateBody");
+const isValidId = require("./isValidId");
+const isValidFavorite = require("./isValidId");
 
 module.exports = {
 	validateBody,
+	isValidId,
+	isValidFavorite,
 };
