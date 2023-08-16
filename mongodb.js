43fcbb15d39e7dacd696607aff3fbf55c5ce369e
VernihoraIvan <vernihorai@gmail.com>
2023-08-16 11:33:15 +0200

    database connection added

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+// const { MongoClient } = require("mongodb");
+
+// const DB_URI =
+//   "mongodb+srv://vernihorai:tHnSajUDVTtaAayL@custer0.jt1g0a8.mongodb.net/";
+
+// const client = new MongoClient(DB_URI);
+
+// client.connect();
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// run();
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+require("dotenv").config();
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+
+const DB_URI = process.env.DB_URI;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => console.log(error));
