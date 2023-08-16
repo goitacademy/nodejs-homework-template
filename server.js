a6f5dc974eecc6247068fbd7741a0ce5e6c63b98
Tkachenko Natalia <nataprofits@gmail.com>
2023-08-16 15:12:24 +0100

    env

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
-const app = require('./app')
-const{DB_HOST} = process.env;
+const app = require("./app");
 
-mongoose.set('strictQuery', true);
-mongoose.connect(DB_HOST)
-.then(() => {
-  app.listen(3000)
-})
-.catch(error => {
-  console.log(error.message);
-  process.exit(1);
-})
+const { DB_HOST, PORT = 3000 } = process.env;
 
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
