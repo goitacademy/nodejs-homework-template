6995070887a13a7da9fcc597894f11e31e75981e
Victoria Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2023-08-16 18:42:50 +0300

    add fixes

--- controller/users/registerUser.js
+++ controller/users/registerUser.js
@@ -5,15 +5,6 @@ const service = require("../../model/users");
 const { catchAsync } = require("../../utils/errorHandlers");
 
 const register = catchAsync(async (req, res) => {
-  // const { password } = req.body;
-
-  // const salt = bcrypt.genSaltSync(10);
-  // const hashPassword = await bcrypt.hash(password, salt);
-
-  // const newUser = await service.createUser({
-  //   ...req.body,
-  //   password: hashPassword,
-  // });
   const newUser = await service.createUser(req.body);
 
   res.status(201).json({
