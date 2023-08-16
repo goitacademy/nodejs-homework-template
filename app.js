78d52d7ee1c031789432d095cff1d1fe621cbcb6
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-08-16 13:16:52 +0300

    add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+
+
+
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+
+const DB_HOST =
+  "mongodb+srv://Oleksandr:wQylMtSYtfOk6cCn@cluster0.mkrqwuv.mongodb.net/contacts_reader";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000");
+  })})
+  .catch(error=> {console.log(error.message)
+  process.exit(1)});
