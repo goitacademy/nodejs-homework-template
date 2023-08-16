31839a2ec9281b928586883ad88bc5fca90a35a6
barkode <konstantinks@gmail.com>
2023-08-16 01:09:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const path = require("path");
 const app = require("./app");
+require("colors");
+const configPath = path.join(__dirname, "./config/.env");
+require("dotenv").config({ path: configPath });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+app.listen(SERVER_PORT, () => {
+  console.log(
+    `Server running. Use our API on port: ${SERVER_PORT}`.black.bgBrightGreen
+  );
 });
