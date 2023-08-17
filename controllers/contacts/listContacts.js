82c5045a089925e411235e96e845c9c8150e73ea
MolchanovSergii <molchanse@gmail.com>
2023-08-17 19:38:33 +0300

    get fix

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,8 +2,12 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (_, res, next) => {
   try {
-    const contacts = await Contact.find();
-    res.json({ status: "success", code: 200, data: { result: contacts } });
+    const contacts = await Contact.find({}, "-createdAt -updatedAt");
+    res.json({
+      status: "success",
+      code: 200,
+      data: { result: contacts },
+    });
   } catch (error) {
     next(error);
   }
