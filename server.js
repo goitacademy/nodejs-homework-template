f0d81b97261e73c06bfbe815e1018b1fbb0cef92
Evgeniy <yaremenko.x@gmail.com>
2023-08-17 15:03:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const DB_HOST =
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(3000))
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
   .catch((error) => {
     console.log(error.message);
     procces.exit(1);
   });
-
-// () => {
-//   console.log("Server running. Use our API on port: 3000");
-// };
