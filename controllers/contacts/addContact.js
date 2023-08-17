ea858d1dfb9fe552887a194b9096c0fa5d33352c
MolchanovSergii <molchanse@gmail.com>
2023-08-17 17:48:36 +0300

    summary

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,15 +1,14 @@
-// const contactsOperations = require("../../models/contacts");
 const Contact = require("../../models/contact");
-// const contactSchema = require("../../shemas/contacts");
+const { schemas } = require("../../models/contact");
 
 const addContact = async (req, res, next) => {
   try {
-    // const { error } = contactSchema.validate(req.body);
+    const { error } = schemas.addSchema.validate(req.body);
 
-    // if (error) {
-    //   error.status = 400;
-    //   throw error;
-    // }
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
     const result = await Contact.create(req.body);
     res.status(201).json({ status: "success", code: 201, data: { result } });
   } catch (error) {
--- models/contact.js
+++ models/contact.js
@@ -1,24 +1,45 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email for contact"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Set email for contact"],
-  },
-  phone: {
-    type: String,
-    required: [true, "Set phone for contact"],
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
+  { versionKey: false, timestamps: true }
+);
+
+const addSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(4).required(),
+  favorite: Joi.bool(),
 });
+
+const handlerErrors = (error, data, next) => {
+  console.log("handler working");
+};
+
+contactSchema.post("save", handlerErrors);
+
+const schemas = {
+  addSchema,
+};
 
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+module.exports = { Contact, schemas };
