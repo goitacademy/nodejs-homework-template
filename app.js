6b22f8ae407b3b5dac90c14e57d658e62a752b9b
m.backi <backimichal@gmail.com>
2023-08-17 21:32:25 +0200

    poprawka

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const contactsRouter = require("./routes/api/contacts.routes");
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const uriDb = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/db-contacts?retryWrites=true&w=majority`;
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+mongoose
+  .connect(uriDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  });
 
-app.use('/api/contacts', contactsRouter)
+app.use("/api", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
