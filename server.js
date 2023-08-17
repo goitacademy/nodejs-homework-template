659b8032b7a31ce7fabcd8e539c7855e8e50ca9c
barkode <konstantinks@gmail.com>
2023-08-17 22:56:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
 require("colors");
 require("dotenv").config();
 
-const { DEV_SERVER_PORT = 3000 } = process.env;
+const { DEV_SERVER_PORT = 3000, DB_USER, DB_PASS, DB_NAME } = process.env;
 
-app.listen(DEV_SERVER_PORT, () => {
-  console.log(
-    `Server running. Use our API on port: ${DEV_SERVER_PORT}`.black
-      .bgBrightGreen
-  );
-});
+const HOST = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.domin4s.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(HOST)
+  .then(() =>
+    app.listen(DEV_SERVER_PORT, () => {
+      console.log(
+        `Server running. Use our API on port: ${DEV_SERVER_PORT}`.black
+          .bgBrightGreen
+      );
+    })
+  )
+  .catch((error) => {
+    console.log(error.message.black.bgBrightRed);
+    process.exit(1);
+  });
