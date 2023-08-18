98788235c509fe44ec9afd9df884d97c4bf67a09
barkode <konstantinks@gmail.com>
2023-08-18 02:14:51 +0100

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
+const dbConnection = require("./dbConnection");
 
-module.exports = { HttpError, ctrlWrapper };
+module.exports = { HttpError, ctrlWrapper, dbConnection };
