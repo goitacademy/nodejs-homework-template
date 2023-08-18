cb140d0ac6dae9db8ae2ec59186e255174bfbded
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-08-18 12:05:42 +0300

    summary

--- models/contacts.js
+++ models/contacts.js
@@ -8,14 +8,23 @@ const {
   changeContact,
 } = require("./requests.js");
 
-
-
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "string.base": `"name" should be a type of 'text'`,
+    "string.empty": `"name" cannot be an empty field`,
+    "any.required": `missing required name field`,
+  }),
+  email: Joi.string().required().messages({
+    "string.base": `"email" should be a type of 'text'`,
+    "string.empty": `"email" cannot be an empty field`,
+    "any.required": `missing required email field`,
+  }),
+  phone: Joi.string().required().messages({
+    "string.base": `"phone" should be a type of 'text'`,
+    "string.empty": `"phone" cannot be an empty field`,
+    "any.required": `missing required phone field`,
+  }),
 });
-
 
 const listContacts = async (req, res, next) => {
   try {
@@ -44,9 +53,9 @@ const addContact = async (req, res, next) => {
   try {
     const { error } = addSchema.validate(req.body);
     if (error) {
-      const missingFieldName = error.details[0].path.toString();
+      const missingFieldName = error.details[0].message;
 
-      throw HttpError(400, `missing required ${missingFieldName} field`);
+      throw HttpError(400, missingFieldName);
     }
     const contactToCreate = await createContact(req.body);
     res.status(201).json(contactToCreate);
@@ -79,10 +88,9 @@ const updateContact = async (req, res, next) => {
       throw HttpError(400, `missing fields`);
     }
     if (error) {
-      throw HttpError(
-        400,
-        `missing required ${error.details[0].path.toString()} field`
-      );
+      const missingFieldName = error.details[0].message;
+
+      throw HttpError(400, missingFieldName);
     }
 
     const { id } = req.params;
