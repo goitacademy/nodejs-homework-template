f43e52ab461bd22d61bb682189c4dbe49ba731cf
barkode <konstantinks@gmail.com>
2023-08-18 02:15:06 +0100

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
-const {
-  getAll,
-  getById,
-  add,
-  updateById,
-  deleteById,
-} = require("../../controllers/contacts");
+const ctrl = require("../../controllers/contacts");
 
 const { validateBody } = require("../../middlewares");
 
 const schema = require("../../schemas/contacts");
 
-router.get("/", getAll);
+router.get("/", ctrl.getAll);
 
-router.get("/:contactId", getById);
+// router.get("/:contactId", getById);
 
-router.post("/", validateBody(schema.addSchema), add);
+// router.post("/", validateBody(schema.addSchema), add);
 
-router.put("/:contactId", validateBody(schema.addSchema), updateById);
+// router.put("/:contactId", validateBody(schema.addSchema), updateById);
 
-router.delete("/:contactId", deleteById);
+// router.delete("/:contactId", deleteById);
 
 module.exports = router;
