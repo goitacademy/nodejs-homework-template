a30e8907cecf08ea13c955a0b8fb65e25e74f769
barkode <konstantinks@gmail.com>
2023-08-18 18:01:42 +0100

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 const validateBody = require("./validateBody");
 const errorHandler = require("./errorHandler");
+const isValidId = require("./isValidId");
 
-module.exports = { validateBody, errorHandler };
+module.exports = { validateBody, errorHandler, isValidId };
