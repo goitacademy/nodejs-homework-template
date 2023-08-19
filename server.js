ce6e139f39f12d8cbe8926bae34911eed8b93fe2
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-08-20 02:21:09 +0300

    create link mongodb database

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const colors = require("colors");
+
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		app.listen(3000, () =>
+			console.log(`Server running. Use our API on port: ${PORT}`.blue.bold)
+		);
+		console.log("Database connection successful".green.bold);
+	})
+	.catch((e) => {
+		console.log(e.message.red.bold);
+		process.exit(1);
+	});
