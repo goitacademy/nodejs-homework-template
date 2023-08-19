c549c9d2459e81892758d33cb63ca35ace5f7789
nataprofits <nataprofits@gmail.com>
2023-08-19 08:17:51 +0100

    fixed export schema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
 const { validateBody, isValidId, validateUpdateStatusContact } = require("../../middlewares");
-const schemas = require("../../models/contact");
+const {schemas} = require("../../models/contact");
 
 router.get("/", ctrl.getAll);
 
