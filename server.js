d5484653bd28ad6f19468f9ae42775ab8504fbaf
Utilizator <Stanislavbox24@gmail.com>
2023-08-19 12:49:22 +0300

    Connected to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://lUtilizatorl:Stas240984@cluster0.cz8qneh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
