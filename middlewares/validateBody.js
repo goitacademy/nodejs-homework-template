c38739e67b632a8f8a54b1e4879f43218347f7c2
nataprofits <nataprofits@gmail.com>
2023-08-19 07:26:11 +0100

    fixed  path

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,31 +1,33 @@
-const {HttpError} = require("../helpers");
-
+const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
-  const funk = (req, res, next) => {
-    if(Object.keys(req.body).length === 0){
-      throw HttpError(400, "missing fields");
+  const func = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+      next(HttpError(400, "missing fields"));
     }
-    const {error} = schema.validate(req.body);
-    if(error) {
-      next(HttpError(400, error.message));
-    }
-    next();
-  }
-  return funk;
-};
 
-const validateUpdateStatusContact = (schema) => {
-  const func =(req, res, next) => {
-    const{error} = schema.validate(req.body);
-    if(error){
+    const { error } = schema.validate(req.body);
+    if (error) {
       next(HttpError(400, error.message));
     }
     next();
   };
+
   return func;
-}
+};
+
+
+const validateUpdateStatusContact = (schema) => {
+ const func = (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+   next(HttpError(400, error.message));
+  }
+  next();
+ };
+ return func;
+};
 module.exports = {
-  validateUpdateStatusContact,
-  validateBody
+ validateUpdateStatusContact,
+ validateBody,
 };
