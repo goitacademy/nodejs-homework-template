b7ac116900be0e8c5760e68b9ee08db30d017a6a
Casimova Marina <maracasimova@gmail.com>
2023-08-20 14:53:46 +0300

    added folders and install gravatar, multer, jimp

--- models/user.js
+++ models/user.js
@@ -27,6 +27,10 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
+    avatarURL: {
+      type: String,
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true }
 );
