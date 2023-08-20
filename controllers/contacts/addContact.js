7b6f6afe26370bf04d37e5f0cd6cfc2b6485049f
Evgeniy <yaremenko.x@gmail.com>
2023-08-20 12:35:57 +0300

    update project

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,14 @@
+const { Contact, schemas } = require("../../models/contact");
+const { HttpError } = require("../../helpers");
+
+const addContact = async (req, res) => {
+  const { error } = schemas.addSchema.validate(req.body);
+  if (error) {
+    console.log(error);
+    throw HttpError(400, error.message);
+  }
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+module.exports = addContact;
---
+++ controllers/contacts/index.js
@@ -0,0 +1,17 @@
+const { ctrlWrapper } = require("../../helpers");
+
+const listContacts = require("./listContacts");
+const getContactById = require("./getContactById");
+const addContact = require("./addContact");
+const removeContact = require("./removeContact");
+const updateContact = require("./updateContact");
+const updateStatusContact = require("./updateStatusContact");
+
+module.exports = {
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
