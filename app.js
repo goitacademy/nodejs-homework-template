9ac3394ed52c994264a064f8561cd73ec846bba2
Sergey Kuran <kuran.serg@gmail.com>
2023-08-20 17:41:23 +0300

    Refactoring

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", contactsRouter);
 app.use(globalError);
-
-app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,49 +1,67 @@
 /* eslint-disable no-unused-vars */
 const {
-  getAllContactsService,
-  getContactByIdService,
-  addContactService,
-  updateContactService,
-  removeContactService,
+  listContacts,
+  getById,
+  addContact,
+  updateContact,
+  removeContact,
 } = require("../services/services");
 
-const getAllContacts = async (req, res, next) => {
+const getAllContactsController = async (req, res, next) => {
   try {
-    const contactsJson = await getAllContactsService();
-    res.json(contactsJson);
+    const contactsJson = await listContacts();
+
+    res.status(200).json(contactsJson);
   } catch (error) {
     next(error);
   }
 };
 
-const getContactById = async (req, res, next) => {
+const getContactByIdController = async (req, res, next) => {
   try {
-    const { contactId } = req.params;
-    console.log(req.params, "req params conrol");
-    const dataContacts = await getContactByIdService(contactId);
-    res.status(200).json(dataContacts);
+    const { id } = req.params;
+    const dataContacts = await getById(id);
+
+    if (dataContacts) {
+      res.status(200).json(dataContacts);
+    }
+    res.status(404).json({ message: "Not found" });
   } catch (error) {
     next(error);
   }
 };
 
-const createContact = async (req, res, next) => {
+const createContactController = async (req, res, next) => {
   try {
-    const { body } = req.body;
-    const dataContacts = await addContactService(body);
+    const { body } = req;
+    const requiredField = ["name", "email", "phone"];
+
+    const objectKeys = Object.keys(body);
+    const findValue = requiredField.filter(
+      (value) => !objectKeys.includes(value)
+    );
+
+    if (findValue.length > 0) {
+      res.status(400).json({ message: `Missing required ${findValue} field` });
+    }
+    const dataContacts = await addContact(body);
 
     res.status(201).json(dataContacts);
-    console.log(dataContacts, "create");
   } catch (error) {
     next(error);
   }
 };
 
-const updateContact = async (req, res, next) => {
+const updateContactController = async (req, res, next) => {
   try {
-    const { contactId } = req.params;
-    const { body } = req.body;
-    const dataContacts = updateContactService(contactId, body);
+    const { id } = req.params;
+    const { body } = req;
+
+    if (!body) {
+      res.status(400).json({ message: "Missing fields" });
+    }
+
+    const dataContacts = updateContact(id, body);
 
     res.status(200).json(dataContacts);
   } catch (error) {
@@ -51,21 +69,23 @@ const updateContact = async (req, res, next) => {
   }
 };
 
-const deleteContact = async (req, res, next) => {
+const deleteContactController = async (req, res, next) => {
   try {
-    const { contactId } = req.params;
-    const dataContacts = await removeContactService(contactId);
-
-    res.status(200).json(dataContacts);
+    const { id } = req.params;
+    const dataContacts = await removeContact(id);
+    if (dataContacts) {
+      res.status(200).json({ message: "Contact deleted" });
+    }
+    res.status(404).json({ Message: "Not found" });
   } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
-  getAllContacts,
-  getContactById,
-  createContact,
-  updateContact,
-  deleteContact,
+  getAllContactsController,
+  getContactByIdController,
+  createContactController,
+  updateContactController,
+  deleteContactController,
 };
