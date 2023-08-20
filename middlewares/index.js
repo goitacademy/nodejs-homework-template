0bfa4d591129876f8cc329ab6753297da8f62354
Casimova Marina <maracasimova@gmail.com>
2023-08-20 19:42:13 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const uploadAvatar = require("./uploadAvatar");
 
 module.exports = {
   validateBody,
   authenticate,
+  uploadAvatar,
 };
