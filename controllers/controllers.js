2e4fd0eb1f1d1864318b3ad091b2206d69c4076c
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-08-21 18:08:59 +0300

    Some updates

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
-const Contact = require("../models/contact")
+const Contact = require("../models/contact");
 
-const { ResponseError } = require("../helpers");
+const { ResponseError, ctrlWrapper } = require("../helpers");
 
 const addSchema = Joi.object({
   name: Joi.string().required(),
@@ -12,114 +12,86 @@ const addSchema = Joi.object({
 
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required(),
-  
 });
 
 const getAll = async (req, res) => {
-  try {
-    const{_id: owner} = req.user
-    const result = await Contact.find({owner});
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({
-      message: "Server error",
-    });
-  }
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner });
+  res.json(result);
 };
 
 const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Contact.findById(id);
-    if (!result) {
-      throw ResponseError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  if (!result) {
+    throw ResponseError(404, "Not found");
   }
+  res.json(result);
 };
 
 const post = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      console.log(error);
-      const [path] = error.details[0].path;
-      throw ResponseError(400, `missing requaired ${path} field`);
-    }
-    const { _id: owner } = req.user;
-    const result = await Contact.create({ ...req.body, owner });
-    res.status(201).json(result);
-    if (!result) {
-      throw ResponseError(404, "Not found");
-    }
-  } catch (error) {
-    next(error);
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    console.log(error);
+    const [path] = error.details[0].path;
+    throw ResponseError(400, `missing requaired ${path} field`);
+  }
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
+  if (!result) {
+    throw ResponseError(404, "Not found");
   }
 };
 
 const put = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
+  const { error } = addSchema.validate(req.body);
 
-    if (error) {
-      console.log(error);
-      const [path] = error.details[0].path;
-      throw ResponseError(400, `missing requaired ${path} field`);
-    }
-    const { id } = req.params;
-    const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
-    if (!result) {
-      throw ResponseError(404, "Not found");
-    }
-
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+  if (error) {
+    console.log(error);
+    const [path] = error.details[0].path;
+    throw ResponseError(400, `missing requaired ${path} field`);
   }
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw ResponseError(404, "Not found");
+  }
+
+  res.status(200).json(result);
 };
 
 const patch = async (req, res, next) => {
-  try {
-    const { error } = updateFavoriteSchema.validate(req.body);
+  const { error } = updateFavoriteSchema.validate(req.body);
 
-    if (error) {
-      console.log(error);
-      const [path] = error.details[0].path;
-      throw ResponseError(400, `missing ${path} field`);
-    }
-    const { id } = req.params;
-    const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
-    if (!result) {
-      throw ResponseError(404, "Not found");
-    }
-
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+  if (error) {
+    console.log(error);
+    const [path] = error.details[0].path;
+    throw ResponseError(400, `missing ${path} field`);
   }
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw ResponseError(404, "Not found");
+  }
+
+  res.status(200).json(result);
 };
 
-
 const remove = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Contact.findByIdAndRemove(id);
-    if (!result) {
-      throw ResponseError(404, "Not found");
-    }
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) {
+    throw ResponseError(404, "Not found");
   }
+  res.json({ message: "contact deleted" });
 };
 
 module.exports = {
-  getAll,
-  getById,
-  post,
-  put,
-  patch,
-  remove,
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  post: ctrlWrapper(post),
+  put: ctrlWrapper(put),
+  patch: ctrlWrapper(patch),
+  remove: ctrlWrapper(remove),
 };
