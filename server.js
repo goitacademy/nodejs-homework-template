673c8f4991becb57a0d06ac21a8856876c0da57a
robertkosciuszko <114183493+robertkosciuszko@users.noreply.github.com>
2023-08-21 18:37:09 +0200

    uppdate

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+require("colors");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST;
+
+mongoose.set("strictQuery", true);
+
+const connection = mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    console.log("\nDatabase connection successful".green);
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`.green);
+    });
+  })
+  .catch((err) => {
+    console.log("\nDatabase not running\n".red, err.toString());
+    process.exit(1);
+  });
+
+function signalHandler() {
+  mongoose.disconnect();
+  console.log("\nDatabase disconnected\n".red);
+  process.exit();
+}
+process.on("SIGINT", signalHandler);
