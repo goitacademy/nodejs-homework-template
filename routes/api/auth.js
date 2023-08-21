322145bef670acb39571b89cd0f1bfbdf5b24a92
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-08-21 17:53:10 +0300

    create hw-05

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validateBody, authenticate } = require("../../middlewares/");
+const { validateBody, authenticate, upload } = require("../../middlewares/");
 
 const ctrl = require("../../controllers/auth");
 
@@ -18,6 +18,8 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
-router.post("/logout", authenticate, ctrl.logout )
+router.post("/logout", authenticate, ctrl.logout)
+
+router.patch("/avatars", authenticate, upload.single('avatar'), ctrl.updateAvatar )
 
 module.exports = router;
