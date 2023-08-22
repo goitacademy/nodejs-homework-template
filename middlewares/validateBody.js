6ce532c887d6a9aa256e0ead2c2f37d030c03c51
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-08-22 17:14:43 +0300

    improve validateBody

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,9 +1,13 @@
 const { HttpError } = require("../helpers");
-const { addSchema } = require("../schemas/contacts");
 
 const validateBody = (schema) => {
-  const func = (req, res, next) => {
-    const { error } = addSchema.validate(req.body);
+  const func = (req, _, next) => {
+    if (Object.keys(req.body).length === 0) {
+      const error = new Error("missing fields");
+      error.status = 400;
+      return next(error);
+    }
+    const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
     }
