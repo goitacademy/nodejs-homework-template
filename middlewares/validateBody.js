3a4434176f03d6344490d79220ce65f6a72aaf01
VeronikaVS90 <vsokolovanika@gmail.com>
2023-08-22 22:00:12 +0300

    Fix/POST

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,14 +2,27 @@ const { HttpError } = require("../helpers");
 
 const validateBody = schema => {
     const func = (req, res, next) => {
-const { error } = schema.validate(req.body);
-        if (error) {
-            next(HttpError(400, error.message));
+        const { error } = schema.validate(req.body);
+        const emptyBody = Object.keys(req.body).length === 0;
+        const missingField = schema._ids._byKey.size !== Object.keys(req.body).length;
+
+        if (emptyBody) {
+            next(HttpError(400, "missing fields"));
         }
-        next()
-    }
+
+        if (error) {
+            if (missingField) {
+                next(
+                    HttpError(400, `missing required ${error.details[0].path[0]} field`)
+                );
+            } else {
+                next(HttpError(400, error.message));
+            }
+        }
+        next();
+    };
 
     return func;
-}
+};
 
 module.exports = validateBody;
