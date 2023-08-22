fc65587155f59ee7ccb15ead52ab37f8cbd15efe
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-08-22 19:25:11 +0300

    fixed mistake

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  res.status(err.status || 500).json({ message: err.message });
 });
 
 module.exports = app;
