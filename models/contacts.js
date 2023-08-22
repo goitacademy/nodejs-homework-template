71b048cdaf19ba3801abb7119cb2404a54a317c1
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-08-22 10:09:25 +0200

    init fix fork

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,76 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const Joi = require("joi");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
-const getContactById = async (contactId) => {}
+const contactsFilePath = path.join(__dirname, "contacts.json");
 
-const removeContact = async (contactId) => {}
+const saveContacts = async (contacts) => {
+  await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsFilePath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contact.id === contactId);
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    contacts.splice(index, 1);
+    await saveContacts(contacts);
+    return true;
+  }
+  return false;
+};
+
+const addContact = async (body) => {
+  const { error } = contactSchema.validate(body);
+  if (error) {
+    throw new Error(`Validation error: ${error.details[0].message}`);
+  }
+
+  const contacts = await listContacts();
+  const newContact = {
+    id: uuidv4(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await saveContacts(contacts);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const { error } = contactSchema.validate(body);
+  if (error) {
+    throw new Error(`Validation error: ${error.details[0].message}`);
+  }
+
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    contacts[index] = { ...contacts[index], ...body };
+    await saveContacts(contacts);
+    return contacts[index];
+  }
+  return null;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +78,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
