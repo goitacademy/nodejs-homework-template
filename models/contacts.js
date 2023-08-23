9dd90963652423460d986ae6d322f5bb4c9f467a
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-08-23 13:20:09 +0200

    added type: module to package.json and refactored all import statements, created a ll methods in models/contacts.js and installed nanoid package

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,86 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+const readContacts = async () => {
+  try {
+    const data = await fs.readFile("./contacts.json", "utf-8");
+    const contacts = JSON.parse(data);
 
-const getContactById = async (contactId) => {}
+    return contacts;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await readContacts();
 
-const addContact = async (body) => {}
+    return contacts;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await readContacts();
+    const contact = contacts.find((item) => item.id === contactId);
 
-module.exports = {
+    return contact;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await readContacts();
+    const filteredContacts = contacts.filter((item) => item.id !== contactId);
+    await fs.writeFile("./contacts.json", JSON.stringify(filteredContacts));
+
+    return filteredContacts;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await readContacts();
+    const newContact = { ...body, id: nanoid() };
+    contacts.push(newContact);
+    await fs.writeFile("./contacts.json", JSON.stringify(contacts));
+
+    return newContact;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await readContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+
+    // if (index === -1) {
+    //   return null;
+    // }
+
+    contacts[index] = { ...contacts[index], ...body };
+
+    await fs.writeFile("./contacts.json", JSON.stringify(contacts));
+
+    return contacts[index];
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,37 @@
-const express = require('express')
+import express from "express";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} from "../../models/contacts.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!contacts) {
+    res.status(404).json({ message: "Not found" });
+  }
+  res.json({ contacts });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+export default router;
