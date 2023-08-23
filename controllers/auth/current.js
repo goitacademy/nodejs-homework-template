13f467a05fab6a7d84f417a3c7c83bd61cdb4c2f
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-08-23 13:46:31 +0300

    update

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,27 +1,8 @@
 const { User } = require("../../models");
 const { HttpError } = require("../../utils");
 
-const current = async (req, res, next) => {
-  const { _id: id } = req.user;
-  const user = await User.findById(id).exec();
-  if (!user) {
-    throw HttpError(401, "Not authorized");
-  }
-  res.json({
-    status: "success",
-    code: 200,
-    user: {
-      email: user.email,
-      subscription: user.subscription,
-    },
-  });
-  // const { email, name, subscription } = req.user;
-  // res.json({
-  //   status: "success",
-  //   code: 200,
-  //   email,
-  //   name,
-  //   subscription,
-  // });
+const current = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.status(200).json({ email, subscription });
 };
 module.exports = current;
--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,20 +3,13 @@ const { HttpError } = require("../../utils");
 
 const logout = async (req, res, next) => {
   const { _id: id } = req.user;
-  try {
-    const user = await User.findByIdAndUpdate(
+   await User.findByIdAndUpdate(
       id,
       { token: null },
       { new: true }
     ).exec();
-    res.json({ message: "Logout success" });
-    if (!user) {
-      throw HttpError(401, "Not authorized");
-    }
+    
     res.status(204).json();
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+
 };
 module.exports = logout;
