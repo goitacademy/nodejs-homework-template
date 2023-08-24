5d58fafdc4996e2bd62bad8cbe1afb5d61de545a
Tkachenko Natalia <nataprofits@gmail.com>
2023-08-24 14:13:25 +0100

    first version

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,10 @@ const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({}, "-createdAt -updateAt");
+  const {_id: owner} = req.user;
+  const{page = 1, limit = 20} = req.query;
+  const skip = (page -1) * limit;
+  const result = await Contact.find({owner}, "-createdAt -updateAt", {skip, limit}).populate("owner", "email password");
   res.status(200).json(result);
 };
 
@@ -17,7 +20,8 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+ const{_id: owner} = req.user;
+  const result = await Contact.create({...req.body, owner});
   res.status(201).json(result);
 };
 
@@ -53,6 +57,7 @@ const deleteById = async (req, res) => {
     message: "contact deleted",
   });
 };
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getById: ctrlWrapper(getById),
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,10 @@
 const {validateBody, validateUpdateStatusContact} = require("./validateBody");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate")
 
 module.exports = {
     validateBody,
     isValidId,
     validateUpdateStatusContact,
+    authenticate,
 }
