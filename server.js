7aae2e1728aa94593d4883babbf81c4789cd4738
Yura Basiuk <yurik.kamenev@gmail.com>
2023-08-25 20:04:32 +0300

    add DB_host

--- server.js
+++ server.js
@@ -2,19 +2,10 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Yura:rsXpoGXlyGMQyeLD@cluster0.gtrmdvs.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("Database connect success"))
-//   .catch((error) => console.log(error.message));
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
 mongoose
   .connect(DB_HOST)
   .then(() => {
@@ -24,7 +15,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
