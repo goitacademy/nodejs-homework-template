6c03a89098859ab9076a22141d525afde4d54335
OleksiyMunenko <oleksiymun@gmail.com>
2023-08-25 19:12:27 +0300

    start

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = "mongodb+srv://Oleksiy:6oXvPOtaXTrg6hSQ@cluster0.xb1mzbn.mongodb.net/contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+	.then(() => {
+		console.log("Database connect success")
+		app.listen(3000);
+	}).catch(error => {
+		console.log(error.message);
+		process.exit(1);
+	});
+
