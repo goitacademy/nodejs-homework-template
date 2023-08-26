c35ed4ec05fddb9a310b11543b97d21e80eb63d1
nataprofits <nataprofits@gmail.com>
2023-08-26 11:59:56 +0100

    fixed logout router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,7 +7,7 @@ const ctrl = require("../../controllers/auth");
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
-router.get("/logout", authenticate, ctrl.logout);
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
 
