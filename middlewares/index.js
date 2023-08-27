ff95ef9d83cd4fe95fa09c30ee12ade79fa90018
Evgeniy <yaremenko.x@gmail.com>
2023-08-27 22:02:30 +0300

    start

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
+const uploat = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
+  uploat,
 };
