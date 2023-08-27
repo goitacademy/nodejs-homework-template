e26f93aaf67f65d8d552ca9d549aeb5f30997f17
Casimova Marina <maracasimova@gmail.com>
2023-08-27 12:46:43 +0300

    controllers

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,11 +1,15 @@
 const register = require("./register");
+const verify = require("./verify");
+const resendEmail = require("./resendEmail");
 const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
-const updateAvatar = require("./updateAvatar")
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   register,
+  verify,
+  resendEmail,
   login,
   getCurrent,
   logout,
