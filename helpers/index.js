fd1cb4f07b341be2a5fd23c3fb7b9afbbd819017
Casimova Marina <maracasimova@gmail.com>
2023-08-27 12:47:19 +0300

    helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const RequestError = require("./RequestError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleSaveErrors = require("./handleSaveErrors");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   RequestError,
   ctrlWrapper,
   handleSaveErrors,
+  sendEmail,
 };
