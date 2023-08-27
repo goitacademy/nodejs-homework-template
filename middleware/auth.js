9870a38e643967ad32e93abc57a52424b943d367
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-08-27 11:26:28 +0200

    hw-4 v.1.01

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 
-function auth(req, res, next) {
+async function auth(req, res, next) {
   const authHeader = req.headers.authorization;
   if (typeof authHeader !== "string") {
     return res.status(401).json({ error: "No token provided" });
@@ -9,28 +9,16 @@ function auth(req, res, next) {
 
   const [bearer, token] = authHeader.split(" ", 2);
 
-  if (bearer !== "Bearer") {
-    return res.status(401).json({ error: "No token provided" });
+  // if (bearer !== "Bearer") {
+  //   return res.status(401).json({ error: "No token provided" });
+  // }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    req.user = { id: decoded.id, name: decoded.name };
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Not authorized" });
   }
-
-  jwt.verify(token, process.env.JWT_SECRET, async (err, decode) => {
-    if (err) {
-      if (err.name === "Not authorized" || err.name === "JsonWebTokenError") {
-        return res.status(401).send({ message: "Not authorized" });
-      }
-      return next(err);
-    }
-    try {
-      const user = await User.findById(decode.id);
-
-      if (user.token !== token) {
-        return res.status(401).send({ message: "Not authorized" });
-      }
-      req.user = { id: decode.id, name: decode.name };
-      next();
-    } catch (error) {
-      next(error);
-    }
-  });
 }
 module.exports = auth;
