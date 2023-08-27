3333c75433276fc30b5eda2f3046fd446381030e
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-08-27 19:32:53 +0300

    query filter

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,12 +4,16 @@ const { ctrlWrapper } = require("../../helpers");
 
 const listContacts = async (req, res) => {
 	const { _id: owner } = req.user;
-	const { page = 1, limit = 20 } = req.query;
+	const { page = 1, limit = 20, favorite = true || false } = req.query;
 
-	const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-		skip: (page - 1) * limit,
-		limit,
-	}).populate("owner", "email");
+	const result = await Contact.find(
+		{ owner, favorite },
+		"-createdAt -updatedAt",
+		{
+			skip: (page - 1) * limit,
+			limit,
+		}
+	).populate("owner", "email");
 
 	res.status(200).json({
 		code: 200,
