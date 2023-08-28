f805895f57a6c71e151e1e25b17eb38ebb51a229
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-08-28 11:04:27 +0200

    hw3

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,12 @@
-// const fs = require('fs/promises')
+const listContacts = async () => {};
 
-const listContacts = async () => {}
+const getContactById = async (contactId) => {};
 
-const getContactById = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const addContact = async (body) => {};
 
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +14,34 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
+const { Schema, model } = require("mongoose");
+
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+      match:
+        /(^[A-Z]{1}[a-z]{1,14} [A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/,
+    },
+    email: {
+      type: String,
+      match: /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim,
+    },
+    phone: {
+      type: String,
+      required: true,
+      match: /^((\+?3)?8)?((0\(\d{2}\)?)|(\(0\d{2}\))|(0\d{2}))\d{7}$/,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const Contact = model("contact", contactSchema);
+
+module.exports = Contact;
