4ef13f2c6ed7e87a573dda6eb1a7d75aa30a3687
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-08-28 13:13:40 +0300

    Added HW-05 to master branch

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers').users;
 const { authorizeUser } = require('../midlewares/authorization');
+const { uploadMiddleware } = require("../midlewares/upload");
 
 router.post('/signup', userController.register.register);
 router.post('/login', userController.login.login);
@@ -10,5 +11,12 @@ router.get('/current', authorizeUser, userController.current.current);
 router.get('/verify/:verificationToken', userController.verifyUserByToken.verifyUserByToken);
 router.patch('/:userId/subscription', authorizeUser, userController.updateSubscription.updateSubscription);
 router.delete("/", userController.deleteUserByMail.deleteUserByMail);
+router.patch(
+  "/avatars",
+  authorizeUser,
+  uploadMiddleware.single("avatar"),
+  userController.updateAvatar.updateAvatar
+);
+
 
 module.exports = router;
