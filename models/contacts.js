8b341ba366200a4d5558d53d7f0194e956fc1a16
Łukasz <lukaszkogut1996@gmail.com>
2023-08-28 19:04:58 +0200

    ,

--- models/contacts.js
+++ models/contacts.js
@@ -1,51 +1,51 @@
-const ContactModel = require("./model");
-const UserModel = require("./model");
+const ContactModel = require('./users/model')
+const UserModel = require('./users/model')
 
 const listContacts = async () => {
-  return UserModel.find();
-};
+	return UserModel.find()
+}
 
 const getContactById = async contactId => {
-  return UserModel.findById(contactId);
-};
+	return UserModel.findById(contactId)
+}
 
 const addContact = async contact => {
-  try {
-    return UserModel.create(contact);
-  } catch (e) {
-    console.error(e);
-  }
-};
+	try {
+		return UserModel.create(contact)
+	} catch (e) {
+		console.error(e)
+	}
+}
 
 const removeContact = async contactId => {
-  try {
-    return UserModel.findByIdAndRemove(contactId);
-  } catch (e) {
-    console.error(e);
-  }
-};
+	try {
+		return UserModel.findByIdAndRemove(contactId)
+	} catch (e) {
+		console.error(e)
+	}
+}
 
 const updateContact = async (contactId, contact) => {
-  try {
-    return UserModel.findByIdAndUpdate(contactId, contact);
-  } catch (e) {
-    console.error(e);
-  }
-};
+	try {
+		return UserModel.findByIdAndUpdate(contactId, contact)
+	} catch (e) {
+		console.error(e)
+	}
+}
 
 const updateStatusContact = async (contactId, body) => {
-  try {
-    return UserModel.findByIdAndUpdate(contactId, body);
-  } catch (e) {
-    console.error(e);
-  }
-};
+	try {
+		return UserModel.findByIdAndUpdate(contactId, body)
+	} catch (e) {
+		console.error(e)
+	}
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-  updateStatusContact,
-};
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+	updateContact,
+	updateStatusContact,
+}
--- models/schema.js
+++ models/schema.js
@@ -1,16 +1,16 @@
-const Joi = require("joi");
+const Joi = require('joi')
 
 const schema = Joi.object({
-  name: Joi.string().min(2).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
+	name: Joi.string().min(2).required(),
+	email: Joi.string().email().required(),
+	phone: Joi.string().required(),
+})
 
 const statusSchema = Joi.object({
-  favorite: Joi.bool().required(),
-});
+	favorite: Joi.bool().required(),
+})
 
 module.exports = {
-  schema,
-  statusSchema,
-};
+	schema,
+	statusSchema,
+}
