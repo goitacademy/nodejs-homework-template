2a3ffca6ce23b3f2ab8d916e882b6290958794dd
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-08-29 00:33:08 +0300

    authenticate middleware
    token verification

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
 	const { authorization = "" } = req.headers;
 	const [bearer, token] = authorization.split(" ");
-	if (bearer !== "Bearer") next(HttpError(401));
+	if (bearer !== "Bearer" && !token) next(HttpError(401));
 
 	try {
 		const { id } = jwt.verify(token, SECRET_KEY);
@@ -20,7 +20,7 @@ const authenticate = async (req, res, next) => {
 
 		next();
 	} catch {
-		next(HttpError(401, "Not authorized"));
+		next(HttpError(401));
 	}
 };
 
