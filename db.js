33bcf655f01f80edf1c7f71c454074f5d38ed5fb
Łukasz <lukaszkogut1996@gmail.com>
2023-08-29 20:13:29 +0200

    change code in  model.js and users.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.DB_HOST, {
--- models/users/model.js
+++ models/users/model.js
@@ -1,33 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: {
-    type: String,
-    default: null,
-  },
-});
+	password: {
+		type: String,
+		required: [true, 'Password is required'],
+	},
+	email: {
+		type: String,
+		required: [true, 'Email is required'],
+	},
+	subscription: {
+		type: String,
+		enum: ['starter', 'pro', 'business'],
+		default: 'starter',
+	},
+	token: {
+		type: String,
+		default: null,
+	},
+})
 
 schema.methods.setPassword = function (password) {
-  this.password = bCrypt.hashSync(password, bCrypt.genSaltSync(6));
-};
+	this.password = bCrypt.hashSync(password, bCrypt.genSaltSync(6))
+}
 
 schema.methods.validPassword = function (password) {
-  return bCrypt.compareSync(password, this.password);
-};
+	return bCrypt.compareSync(password, this.password)
+}
 
-const UserModel = mongoose.model("users", schema);
+const UserModel = mongoose.model('users', schema)
 
-module.exports = UserModel;
+module.exports = UserModel
