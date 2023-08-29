0704e27d463920aa4cab59c32ea1a75cb2af09d7
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-08-29 12:11:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config
+const { DB_HOST, PORT = 3000 } = process.env;
+console.log(DB_HOST);
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`Server running. Use our API on port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
