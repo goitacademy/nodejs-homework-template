9974c40fb420e4aff4554f07084fa6512a90b6bc
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-08-30 21:31:39 +0200

    Update users.js

--- server/routes/controllers/users.js
+++ server/routes/controllers/users.js
@@ -93,6 +93,7 @@ const login = async (req, res, next) => {
       data: {
         token,
         user: {
+          username: user.username,
           email: user.email,
           subscription: user.subscription,
         },
