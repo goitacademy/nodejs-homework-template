5d201bcba1433c59487b7315e5535415ea4b4253
Nataliya Rieger <artgallery2018@gmx.ch>
2023-08-30 19:19:11 +0200

    update

---
+++ controllers/contacts.js
@@ -0,0 +1,51 @@
+const contacts = require("../models/contacts");
+
+const { HttpError } = require("../helpers");
+const { ctrlWrapper } = require("../helpers");
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.getContactById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const add = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Delete success",
+  });
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,95 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function withErrorHandling(callback) {
+  try {
+    return await callback();
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
 
-const removeContact = async (contactId) => {}
+async function listContacts() {
+  return withErrorHandling(async () => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    console.log(data);
+    return JSON.parse(data);
+  });
+}
 
-const addContact = async (body) => {}
+async function getContactById(contactId) {
+  return withErrorHandling(async () => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts.find((contact) => contact.id === contactId) || null;
+  });
+}
 
-const updateContact = async (contactId, body) => {}
+async function removeContact(contactId) {
+  return withErrorHandling(async () => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contactToRemove = contacts.find((contact) => contact.id === contactId);
+
+    if (!contactToRemove) {
+      return null;
+    }
+
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), "utf-8");
+
+    return contactToRemove;
+  });
+}
+
+async function addContact(name, email, phone) {
+  return withErrorHandling(async () => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+
+    return newContact;
+  });
+}
+
+async function updateContact(contactId, updatedFields) {
+  return withErrorHandling(async () => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+
+    const contactToUpdate = contacts.find((contact) => contact.id === contactId);
+
+    if (!contactToUpdate) {
+      return null;
+    }
+
+    const updatedContact = {
+      ...contactToUpdate,
+      ...updatedFields,
+    };
+
+    const updatedContacts = contacts.map((contact) =>
+      contact.id === contactId ? updatedContact : contact
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2), "utf-8");
+
+    return updatedContact;
+  });
+}
 
 module.exports = {
   listContacts,
@@ -16,4 +97,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
