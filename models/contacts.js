f117a184901ee1cb9e0e038cf10d4a8588bc3fac
Silich Alexander <sili41995@gmail.com>
2023-08-31 14:08:20 +0300

    update

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,5 @@
 const fs = require('fs/promises');
-const path = require('path');
 const { nanoid } = require('nanoid');
-
-const contactsPath = path.join(__dirname, 'contacts.json');
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
