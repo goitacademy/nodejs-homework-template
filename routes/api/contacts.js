b90d1e560a46904081a84b87736210aa0bfb4be4
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-08-31 11:15:56 +0300

    add files task 3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,32 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", ctrl.getContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
+router.put(
+  "/:id",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
 
-router.delete("/:id", ctrl.removeContact);
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
+
+router.delete("/:id", isValidId, ctrl.removeContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(
+    "mongodb+srv://Roman:12345roman@cluster0.drocj57.mongodb.net/db-contacts?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
