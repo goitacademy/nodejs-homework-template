d80f5a9d737f0b8a173c604a2a43b6582a613521
Kateryna1706 <silckatya@gmail.com>
2023-08-31 22:12:12 +0200

    fixed schema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,10 +1,18 @@
 const Joi = require("joi");
 
 const addSchema = Joi.object({
-  title: Joi.string().required(),
-  author: Joi.string().required(),
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+const updateSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
 });
 
 module.exports = {
   addSchema,
+  updateSchema,
 };
