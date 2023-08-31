e2900ba0958334bf6d86f5666423469ce03de6e9
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-08-31 13:33:13 +0300

    update

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -6,7 +6,7 @@ const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "alina_vol_r@ukr.net" };
+  const email = { ...data, from: "alina.riabova.dev@gmail.com" };
   await sgMail.send(email);
   return true;
 };
