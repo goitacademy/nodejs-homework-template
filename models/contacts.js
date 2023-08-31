14861761fd55aaeb65c959694a9e7acb79e86382
Jakub Piksa <kuba.piksa@wp.pl>
2023-08-31 17:45:37 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,10 +1,37 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const Joi = require('joi')
 
-const listContacts = async () => {}
+const contactsFilePath = path.join(__dirname, 'contacts.json') 
 
-const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsFilePath, 'utf-8')
+  
+    return JSON.parse(data)}
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts()
+  const contact = contacts.find((contact) => contact.id === contactId)
+  
+  if (!contact) {
+      
+    throw new Error('Contact not found')
+  } else {
+  
+    return contact
+  }
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await getContactsList()
+  const updatedContacts = contacts.filter(
+        (contact) => contact.id !== contactId
+  )
+  
+    await fs.writeFile(contactsFilePath, JSON.stringify(updatedContacts))
+}
+
 
 const addContact = async (body) => {}
 
