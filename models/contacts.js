6dc2be2400a325a38a7daabe961294143085b9a6
michalmazurdev <m.mazur40@gmail.com>
2023-09-01 17:55:48 +0200

    body validation moved to rotes

--- models/contacts.js
+++ models/contacts.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { v4: uuidv4 } = require("uuid");
-const Joi = require("joi");
 
 const contactsPath = path.resolve("models", "contacts.json");
 
@@ -11,39 +10,32 @@ const listContacts = async () => {
     const contacts = JSON.parse(contactsJSON);
     return contacts;
   } catch {
-    console.log("error when reading the file");
+    return null;
   }
 };
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
-  const contact = contacts.find((c) => c.id === contactId);
+  const contact = contacts.find((contact) => contact.id === contactId);
   return contact;
 };
 
 const removeContact = async (contactId) => {
   const contacts = await listContacts();
-  const index = contacts.findIndex((c) => c.id === contactId);
+  const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index !== -1) {
     contacts.splice(index, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return { message: "contact deleted" };
+    return {
+      message: `Contact with id ${contactId} has been deleted.`,
+    };
   } else {
-    return { message: "Not found" };
+    return null;
   }
 };
-const postBodyScheme = Joi.object({
-  email: Joi.string().email().required(),
-  name: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
 const addContact = async (body) => {
   const contacts = await listContacts();
-  const validatedBody = postBodyScheme.validate(body);
-  if (validatedBody.error?.details.length > 0) {
-    return { message: "Your request is not in proper format." };
-  }
   const { name, email, phone } = body;
   const newContact = {
     id: uuidv4(),
@@ -56,21 +48,10 @@ const addContact = async (body) => {
   return newContact;
 };
 
-const putBodyScheme = Joi.object({
-  email: Joi.string().email(),
-  name: Joi.string(),
-  phone: Joi.string(),
-});
-
 const updateContact = async (contactId, body) => {
   const contacts = await listContacts();
-  const index = contacts.findIndex((c) => c.id === contactId);
+  const index = contacts.findIndex((contact) => contact.id === contactId);
   const contactToUpdate = contacts[index];
-  const validatedBody = putBodyScheme.validate(body);
-
-  if (validatedBody.error?.details.length > 0) {
-    return { message: "Your request is not in proper format." };
-  }
   const updatedContact = { ...contactToUpdate, ...body };
 
   if (index !== -1) {
@@ -78,7 +59,7 @@ const updateContact = async (contactId, body) => {
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return updatedContact;
   } else {
-    return { message: "Not found" };
+    return null;
   }
 };
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,55 +1,87 @@
 const express = require("express");
 const pls = require("../../models/contacts");
 const router = express.Router();
+const Joi = require("joi");
 
 // @ GET /api/contacts
 router.get("/", async (req, res, next) => {
   const response = await pls.listContacts();
+  if (!response) {
+    res.status(400).json({
+      message: "An error occured on attempt to read the contacts file",
+    });
+  }
   res.json(response);
 });
 
 // @ GET /api/contacts/:id
 router.get("/:contactId", async (req, res, next) => {
   const response = await pls.getContactById(req.params.contactId);
-  if (response === undefined) {
-    res.status(404).json({ message: "Not found" });
+  if (!response) {
+    return res
+      .status(404)
+      .json(`Contact with id ${req.params.contactId} has not been found`);
   } else {
-    res.json(response);
+    return res.json(response);
   }
 });
 
 // @ DELETE /api/contacts/:id
 router.delete("/:contactId", async (req, res, next) => {
   const response = await pls.removeContact(req.params.contactId);
-  if (response.message === "Not found") {
-    res.status(404).json(response);
+  if (!response) {
+    return res.status(404).json({
+      message: `Contact with id ${req.params.contactId} has not been found`,
+    });
   } else {
-    res.status(200).json(response);
+    return res.status(200).json(response);
   }
+});
+
+const postBodyScheme = Joi.object({
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
 });
 
 // @ POST /api/contacts
 router.post("/", async (req, res, next) => {
-  const response = await pls.addContact(req.body);
-  if (response.message === "Your request is not in proper format.") {
-    res.status(400).json(response);
-  } else {
-    res.status(201).json(response);
+  const validatedBody = postBodyScheme.validate(req.body);
+  if (validatedBody.error?.details.length > 0) {
+    return res
+      .status(400)
+      .json({ message: "Your request is not in proper format." });
   }
+  const response = await pls.addContact(req.body);
+  return res.status(201).json(response);
+});
+const putBodyScheme = Joi.object({
+  email: Joi.string().email(),
+  name: Joi.string(),
+  phone: Joi.string(),
 });
 
 // @ PUT /api/contacts/:id
 router.put("/:contactId", async (req, res, next) => {
-  const response = await pls.updateContact(req.params.contactId, req.body);
   if (Object.keys(req.body).length === 0) {
-    res.status(400).json({ message: "missing fields" });
+    return res.status(400).json({ message: "Missing fields" });
   }
-  if (response.message === "Not found") {
-    res.status(404).json(response);
-  } else if (response.message === "Your request is not in proper format.") {
-    res.status(400).json(response);
+
+  const validatedBody = putBodyScheme.validate(req.body);
+  if (validatedBody.error?.details.length > 0) {
+    return res
+      .status(400)
+      .json({ message: "Your request is not in proper format." });
+  }
+
+  const response = await pls.updateContact(req.params.contactId, req.body);
+
+  if (!response) {
+    return res.status(404).json({
+      message: `Contact with id ${req.params.contactId} has not been found`,
+    });
   } else {
-    res.status(200).json(response);
+    return res.status(200).json(response);
   }
 });
 
