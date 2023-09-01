a3270c5c48a5abaedaa895c51555c206fff8911c
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-09-01 13:48:02 +0300

    connected database

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+mongoose.set("strictQuery", true);
+
+const DB_HOST =
+  "mongodb+srv://Anton:ddance@cluster0.2visb3k.mongodb.net/contacts";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
