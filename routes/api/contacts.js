77a17fe68a6b238b3a8c840573b62b1b19d00f00
Roksolyanka <roksolyanakushnir@gmail.com>
2023-09-02 11:29:02 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'Homework done!' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
