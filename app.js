2180d1293cd4398061d48c1899ffa4bc9d3287f1
KarolinaKuszpa <117316629+KarolinaKuszpa@users.noreply.github.com>
2023-09-02 22:13:51 +0200

    monoodb

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
+mongoose
+    .connect(
+        'mongodb+srv://karolinakuszpa:Mongo2023@cluster0.4qtercj.mongodb.net/?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Database connection successful'))
+    .catch((err) => {
+        console.error('Database connection error', err)
+        process.exit(1)
+    })
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
@@ -15,11 +25,11 @@ app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+    res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message })
 })
 
 module.exports = app
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,38 @@
-// const fs = require('fs/promises')
+const Contact = require('./contactMongoose') // dostosuj ścieżkę do lokalizacji pliku modelu
 
-const listContacts = async () => {}
+const listContacts = async () => {
+    return await Contact.find({})
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+    return await Contact.findById(contactId)
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+    return await Contact.findByIdAndDelete(contactId)
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+    return await Contact.create(body)
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+    return await Contact.findByIdAndUpdate(contactId, body, { new: true })
+}
+
+const updateStatusContact = async (contactId, body) => {
+    return await Contact.findByIdAndUpdate(
+        contactId,
+        { favorite: body.favorite },
+        { new: true }
+    )
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
+    updateStatusContact,
 }
