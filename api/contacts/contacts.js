23738fdce402d6f16b8bb02abf802ca667eb119a
Andrii-Pershko <andriiinf@gmail.com>
2023-09-04 20:36:48 +0300

    update contact.js

--- api/contacts/contacts.js
+++ api/contacts/contacts.js
@@ -1,4 +1,4 @@
-const { cntrlWrappers } = require("../../helpers");
+const { cntrlWrappers, HttpError } = require("../../helpers");
 const { Contact } = require("../../models/contact");
 
 const Joi = require("joi");
@@ -18,6 +18,20 @@ const putSchema = Joi.object({
   phone: Joi.string(),
   email: Joi.string().email(),
 }).or("name", "phone", "email");
+
+const addContact = async (req, res, next) => {
+  const { _id: owner } = req.user;
+  const { error } = addSchema.validate(req.body);
+
+  if (error) {
+    const emptyRequired = error.details[0].path;
+    res.status(400).json({ message: `missing required ${emptyRequired}` });
+    return;
+  }
+
+  const newContact = await Contact.create({ ...req.body, owner });
+  res.status(201).json(newContact);
+};
 
 const listContacts = async (req, res, next) => {
   const { page = 1, limit = 20, favorite = null } = req.query;
@@ -42,52 +56,19 @@ const listContacts = async (req, res, next) => {
 
 const getContactById = async (req, res, next) => {
   const id = req.params.contactId;
+  const owner = req.user._id;
 
-  const conntactsById = await Contact.findById(id);
-
-  const owner = req.user._id; // Ід власника беремо з авторизованого користувача
-
-  if (!conntactsById) {
-    return res.status(404).json({ message: "Not found" });
-  }
-
-  if (owner !== conntactsById.owner) {
-    return res.status(403).json({ message: "Limited access" });
-  }
-
-  res.send(conntactsById);
-};
-
-const removeContact = async (req, res, next) => {
-  const id = req.params.contactId;
-
-  const owner = req.user._id; // Ід власника беремо з авторизованого користувача
-
-  const deleteContact = await Contact.findByIdAndRemove(id);
-
-  if (!deleteContact) {
-    res.status(404).json({ message: "Not found" });
-  }
-
-  if (owner !== deleteContact.owner) {
-    return res.status(403).json({ message: "Limited access" });
-  }
-
-  res.status(200).json({ message: "Contact deleted" });
-};
-
-const addContact = async (req, res, next) => {
-  const { _id: owner } = req.user;
-  const { error } = addSchema.validate(req.body);
-
-  if (error) {
-    const emptyRequired = error.details[0].path;
-    res.status(400).json({ message: `missing required ${emptyRequired}` });
-    return;
-  }
-
-  const newContact = await Contact.create({ ...req.body, owner });
-  res.status(201).json(newContact);
+  await Contact.findOne({ _id: id, owner })
+    .then((user) => {
+      if (user) {
+        return res.json(user);
+      } else {
+        res.status(404).json({ message: "Contact not found" });
+      }
+    })
+    .catch((err) => {
+      HttpError(err.status, `${err.message}`);
+    });
 };
 
 const updateContact = async (req, res, next) => {
@@ -97,28 +78,24 @@ const updateContact = async (req, res, next) => {
     return;
   }
 
-  const owner = req.user._id; // Ід власника беремо з авторизованого користувача
-
   const id = req.params.contactId;
-  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
-  if (updatedContact === null) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
+  const { _id: owner } = req.user;
 
-  if (owner !== updatedContact.owner) {
-    return res.status(403).json({ message: "Limited access" });
-  }
-
-  res.status(200).json(updatedContact);
+  await Contact.findOneAndUpdate({ _id: id, owner }, req.body, { new: true })
+    .then((updatedContact) => {
+      if (updatedContact) {
+        return res.status(200).json(updatedContact);
+      } else {
+        res.status(404).json({ message: "Contact not found" });
+      }
+    })
+    .catch((err) => {
+      HttpError(err.status, `${err.message}`);
+    });
 };
 
 const updateStatusContact = async (req, res, next) => {
   const { error } = updateFavoriteSchema.validate(req.body);
-
-  const owner = req.user._id; // Ід власника беремо з авторизованого користувача
 
   if (error) {
     const emptyRequired = error.details[0].path;
@@ -127,18 +104,36 @@ const updateStatusContact = async (req, res, next) => {
   }
 
   const id = req.params.contactId;
-  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
-  if (updatedContact) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
-  if (owner !== updatedContact.owner) {
-    return res.status(403).json({ message: "Limited access" });
-  }
+  const { _id: owner } = req.user;
 
-  res.status(200).json(updatedContact);
+  await Contact.findOneAndUpdate({ _id: id, owner }, req.body, { new: true })
+    .then((updatedContact) => {
+      if (updatedContact) {
+        return res.status(200).json(updatedContact);
+      } else {
+        res.status(404).json({ message: "Contact not found" });
+      }
+    })
+    .catch((err) => {
+      HttpError(err.status, `${err.message}`);
+    });
+};
+
+const removeContact = async (req, res, next) => {
+  const id = req.params.contactId;
+  const owner = req.user._id;
+
+  await Contact.findByIdAndRemove({ _id: id, owner })
+    .then((user) => {
+      if (user) {
+        return res.status(200).json({ user, message: "Contact deleted" });
+      } else {
+        res.status(404).json({ message: "Contact not found" });
+      }
+    })
+    .catch((err) => {
+      HttpError(err.status, `${err.message}`);
+    });
 };
 
 module.exports = {
