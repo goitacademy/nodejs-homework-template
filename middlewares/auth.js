1e7bf4c6670aef4e267a7455249fad8c63d70cd0
Bartek <Barteknr1@gmail.com>
2023-09-04 11:05:05 +0200

    bugfix: logout auth
    Methods are no longer available after logout - Unauthorized response

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 const auth = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (!user || err) {
+    if (!user || err || req.headers.authorization.split(' ')[1] !== user.token) {
       return res.status(401).json({
         status: 'error',
         code: 401,
