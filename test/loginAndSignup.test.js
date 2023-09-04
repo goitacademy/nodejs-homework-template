fb2d9b76ca2d8ad065523be2cc4f2bcbff50671a
SebastianZasadni <emblelele@gmail.com>
2023-09-04 14:26:56 +0200

    fix tests

--- test/loginAndSignup.test.js
+++ test/loginAndSignup.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../server');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 const dummyUser = {
     email: "testt@gmail.com",
@@ -14,9 +15,22 @@ describe('Test login and register', () => {
         const response = await request(app)
             .post('/api/users/signup')
             .send(dummyUser);
+        if (response.status === 201) {
+            const { email } = dummyUser;
+            const { _id } = await User.findOne({ email });
+            return await User.findByIdAndRemove(_id);
+        }
         expect(response.status).toBe(201);
     })
     test('Test login', async () => {
+        const hashedPassword = await bcrypt.hash(dummyUser.password, 10);
+        await User.create({
+            email: dummyUser.email,
+            password: hashedPassword,
+            subscription: dummyUser.subscription,
+            avatarURL: dummyUser.avatarURL
+        }
+        )
         const { email, password } = dummyUser;
         const response = await request(app)
             .post('/api/users/login')
