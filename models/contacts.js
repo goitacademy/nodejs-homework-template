cca734f713bc89745cc28c5ce19e3e356ab3ea60
Maria Talan <mashenka.talan@gmail.com>
2023-09-05 17:41:08 +0300

    Added core files.

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,69 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const {v4: uuid} = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  try {
+    const contactsList = await fs.readFile(contactsPath);
+    return JSON.parse(contactsList.toString());
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  const contactList = await listContacts();
+  if (Array.isArray(contactList)) {
+    const contact = contactList.find(item => item.id === contactId);
+    return contact || null;
+  }
 
-const addContact = async (body) => {}
+  return null;
+}
 
-const updateContact = async (contactId, body) => {}
+async function removeContact(contactId) {
+  const contactList = await listContacts();
+  if (Array.isArray(contactList)) {
+    const contactToRemove = contactList.find(item => item.id === contactId);
+    if (contactToRemove) {
+      const contactsToKeep = contactList.filter(item => item.id !== contactToRemove.id);
+      await writeFile(contactsPath, contactsToKeep);
+    }
+
+    return contactToRemove || null;
+  }
+
+  return null;
+}
+
+async function addContact(name, email, phone) {
+  const contactList = await listContacts() || [];
+  const contactToAdd = {
+    id: uuid(),
+    name,
+    email,
+    phone,
+  };
+  contactList.push(contactToAdd);
+  await writeFile(contactsPath, contactList);
+
+  return contactToAdd;
+}
+
+async function writeFile(filename, data) {
+  try {
+    await fs.writeFile(filename, JSON.stringify(data, null, 2));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
