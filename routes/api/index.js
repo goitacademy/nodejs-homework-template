6bff165d1d3d221ced23d96d1fe0609f71983a60
Yaroslav <117989939+iberikofer@users.noreply.github.com>
2023-09-05 12:00:26 +0300

    Updated index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const usersRoutes = require('./users')
 const contactsRoutes = require('./contacts')
-// const auth = require('../../middleware/auth')
 
 router.use('/users', usersRoutes)
 router.use('/contacts', contactsRoutes)
