002d8dd981a01979aea454f08dca98f22152b0f7
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-09-05 09:59:01 +0200

    init

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,140 @@
-const express = require('express')
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
+const schema = require("../../utils/validation");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const validateData = async (req, res, next) => {
+  try {
+    const body = await schema.validateAsync(req.body);
+    req.validatedData = body;
+    next();
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.details[0].message,
+    });
+  }
+};
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.use(express.json());
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res, next) => {
+  try {
+    const contacts = await listContacts();
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (contacts.length > 0) {
+      res.json({
+        status: 200,
+        data: {
+          contacts,
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: "Not found",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
 
-module.exports = router
+    if (contact) {
+      res.json({
+        status: 200,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: "Not found",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", validateData, async (req, res, next) => {
+  try {
+    const contact = { id: uuidv4(), ...req.validatedData };
+    await addContact(contact);
+
+    res.status(201).json({
+      status: 201,
+      data: {
+        contact,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.details[0].message,
+    });
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const condition = await removeContact(contactId);
+
+    if (condition) {
+      res.json({
+        status: 200,
+        message: "contact deleted",
+      });
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: "Not found",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:contactId", validateData, async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await updateContact(contactId, req.validatedData);
+
+    if (contact) {
+      res.json({
+        status: 200,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: "Not found",
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.details[0].message,
+    });
+  }
+});
+
+module.exports = router;
---
+++ utils/validation.js
@@ -0,0 +1,32 @@
+const Joi = require("joi");
+
+const customMessages = {
+  name: {
+    "string.pattern.base":
+      "the field 'name' must only contain letters, may contain spaces",
+  },
+  phone: {
+    "string.pattern.base":
+      "the field 'phone' must only contain numbers,alternatively it can contain a hyphen but no brackets,",
+  },
+};
+
+const schema = Joi.object({
+  name: Joi.string()
+    .pattern(/^[A-Za-z\s-]+$/)
+    .min(3)
+    .max(30)
+    .trim()
+    .required()
+    .messages(customMessages.name),
+  email: Joi.string().email().trim().required(),
+  phone: Joi.string()
+    .pattern(/^[\d-]+$/)
+    .min(3)
+    .max(16)
+    .trim()
+    .required()
+    .messages(customMessages.phone),
+});
+
+module.exports = schema;
