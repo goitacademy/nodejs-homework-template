35bf2ab11e652726aef7588955db7b31cf0e9a23
TetiPalii <TetianaPalii9491@gmail.com>
2023-09-05 12:31:53 +0200

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const ctrl = require('../../controllers/contactsCtrl');
 const validateId = require('../../middlewares/idValidId')
+const authenticate = require('../../middlewares/authenticate')
 
 const router = express.Router();
 
 
-router.get('/', ctrl.listContacts);
+router.get('/', authenticate, ctrl.listContacts);
 
-router.get('/:contactId',validateId, ctrl.getContactById);
+router.get('/:contactId',authenticate, validateId, ctrl.getContactById);
 
-router.post('/', ctrl.addContact);
+router.post('/',authenticate, ctrl.addContact);
 
-router.delete('/:contactId', validateId, ctrl.removeContact);
+router.delete('/:contactId',authenticate, validateId, ctrl.removeContact);
 
-router.put('/:contactId', validateId, ctrl.updateContact);
+router.put('/:contactId', authenticate, validateId, ctrl.updateContact);
 
-router.patch('/:contactId/favorite', validateId, ctrl.updateFavoriteContact);
+router.patch('/:contactId/favorite', authenticate, validateId, ctrl.updateFavoriteContact);
 
 
 module.exports = router
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const ctrl = require('../../controllers/authCtrl')
+const ctrl = require('../../controllers/authCtrl');
+const authenticate = require('../../middlewares/authenticate');
 const router = express.Router();
 
+
+
 router.post('/register', ctrl.register );
-router.post('/login', () => { });
+router.post('/login', ctrl.login);
+router.post('/logout', authenticate, ctrl.logout);
+router.get('/current', authenticate, ctrl.getCurrent);
+router.patch('/',authenticate,ctrl.updateSubscription);
 
 module.exports = router;
