299952f7afb61e8710a5a5f24da76e7aafc0ce1c
Guma96 <mateusz.kutarba96@gmail.com>
2023-09-05 16:03:18 +0200

    server and other file (routes, contacts, schema, joi)

--- routes/api/contacts.routes.js
+++ routes/api/contacts.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const contactController = require("../../controllers/contacts.controller");
 
 router.get("/", contactController.getAll);
