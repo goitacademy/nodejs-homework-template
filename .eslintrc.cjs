71ea012a2180fe3f7781aaa84ab5a7312d8ed1ba
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-09-06 22:39:12 +0300

    Renamed on .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
+    "commonjs": true,
+    "es2021": true,
+    "node": true,
   },
   extends: ['standard', 'prettier'],
   parserOptions: {
