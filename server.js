fd458e83e65809dfbcc831a2c58b9933d85107e4
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-09-06 23:35:55 +0300

    change server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import app from "./app.js";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000 ")
+ 
+mongoose.connect(process.env.DB_HOST)
+.then(()=>{
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000 ")
+  })
+})
+.catch(error => {
+  console.log(error.message)
+  process.exit(1)
 })
