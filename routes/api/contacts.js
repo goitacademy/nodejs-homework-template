8f5caf50ec7a8fb4701611c019440282a3bd8356
9Alla9 <konfetcka@gmail.com>
2023-09-07 09:23:59 +0300

    прибрала валидацию

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,12 @@
 const express = require("express"); // Підключення модуля Express для створення роутера
-const Joi = require("joi"); // Підключення модуля для валідації даних
 const ctrl = require("../../controllers/contacts"); // Підключення модуля контролера
-const { validateBody } = require("../../middlewares"); // Підключення мідлвари для валідації запиту
 
 const router = express.Router();
 
-const addSchema = Joi.object({
-  // Визначення схеми валідації для додавання контакту
-  name: Joi.string().required(), // Вимагається рядок з ім'ям
-  email: Joi.string().required(), // Вимагається рядок з електронною поштою
-  phone: Joi.string().required(), // Вимагається рядок з номером телефону
-});
-
 router.get("/", ctrl.getAll); // Роут для отримання всіх контактів
 router.get("/:contactId", ctrl.getById); // Роут для отримання контакту за його ідентифікатором
-router.post("/", validateBody(addSchema), ctrl.addContact); // Роут для додавання контакту з використанням мідлвари валідації
+router.post("/", ctrl.addContact); // Роут для додавання контакту з використанням мідлвари валідації
 router.delete("/:contactId", ctrl.deleteContact); // Роут для видалення контакту з використанням мідлвари валідації
-router.put("/:contactId", validateBody(addSchema), ctrl.updateContact); // Роут для оновлення контакту з використанням мідлвари валідації
+router.put("/:contactId", ctrl.updateContact); // Роут для оновлення контакту з використанням мідлвари валідації
 
 module.exports = router;
