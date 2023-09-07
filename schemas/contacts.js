b9ad8a8c7ca23fef004a6620643807f8bb08fb08
Denys Cherednychenko <cherydens@ukr.net>
2023-09-07 16:26:24 +0300

    Fix validation

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,15 +2,19 @@ const Joi = require('joi');
 
 const addContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().email().required(),
   phone: Joi.string().required(),
-});
+}).messages({ 'any.required': `{#key} is a required field` });
 
 const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-}).min(1);
+})
+  .min(1)
+  .messages({
+    'object.min': 'missing fields',
+  });
 
 module.exports = {
   addContactSchema,
