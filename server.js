23972a4b13277b0d65fa4f10b9c334c0e4ae887c
Nataliya Rieger <artgallery2018@gmx.ch>
2023-09-07 21:27:41 +0200

    update

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
+require("dotenv").config(); // Добавляем для загрузки переменных окружения
+
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const DB_HOST =
+  "mongodb+srv://NRie-23:AhTK002KYJ07D9pA@cluster0.qiigtmy.mongodb.net/contacts?retryWrites=true&w=majority";
 
-// mongoose.set("strictQuery", true);
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
   })
   .catch((error) => {
-    console.log(error.message);
+    console.error("Error starting the server:", error.message);
     process.exit(1);
   });
