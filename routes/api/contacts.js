79c1f0cb1a092535a7ffd1e6159731a3ad39cb14
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-09-07 12:21:28 -0500

    final hw02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,83 @@
-const express = require('express')
+const express = require('express');
+
+const contacts = require('../../models/contacts');
+const { createError } = require('../../helpers');
+const { contactSchema } = require('../../schemas');
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/', async (res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result)
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result)
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = contactSchema(req.body);
+    
+    if (error) {
+      throw createError(404, "missing required name")
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).send(result)
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    
+    if (!result) {
+      throw createError(404);
+    }
+
+    res.json({
+      message : 'deleted'  });
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    
+    const { error } = contactSchema(req.body);
+    if (error){
+      throw createError('Missing required fields');
+    }
+    
+    const { contactId } = req.params;
+      const result = await contacts.updateContactById(contactId, req.body );
+
+      if (!result) {
+        throw createError(404);
+      }
+
+      res.json(result);
+  } catch (error) {
+    next(error);
+  }
+
 })
 
 module.exports = router
