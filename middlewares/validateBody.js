ec8c72f8a8c0d725caaa585f4e1f4d3c71c616c6
Denys Cherednychenko <cherydens@ukr.net>
2023-09-07 16:51:29 +0300

    Fix validation error messages

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,8 +3,9 @@ const { HttpError } = require('../helpers');
 const validateBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
+
     if (error) {
-      next(new HttpError(400, error));
+      next(new HttpError(400, error.details[0].message));
     }
     next();
   };
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,7 +4,10 @@ const addContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
-}).messages({ 'any.required': `{#key} is a required field` });
+}).messages({
+  'any.required': `{#key} is a required field`,
+  'string.email': 'email field must be a valid email',
+});
 
 const updateContactSchema = Joi.object({
   name: Joi.string(),
