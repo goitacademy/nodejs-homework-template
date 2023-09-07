2e73b0f637ce4ccbc45387d358829faa86a4fffe
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-09-07 13:49:05 +0300

    add

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-// const app = require("./app.js");
+import mongoose from "mongoose";
 import app from "./app.js";
+import { error } from "console";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// const DB_HOST =
+//   "mongodb+srv://sonofra:e0pXN29alzLqVc6o@clusterhw.lbm4s15.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
