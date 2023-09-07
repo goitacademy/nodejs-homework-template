66777ad314f577c6b694a60fec29d9c18f79d391
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-09-07 21:08:31 +0200

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,10 +2,15 @@ const HttpError = require("../helpers/HttpError");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
+    if (!req.body) {
+            return next(HttpError(400, 'missing fields'));
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      return next(HttpError(400, 'missing required name field'));
     }
+
     next();
   };
   return func;
