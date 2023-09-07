43fa7c53e320dbfc5a02647fe4f2006245e90580
Serhii Ozeran <ozeransm@gmail.com>
2023-09-07 23:50:04 +0300

    start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
-
+console.log("jhgchasdchaj")
 router.get('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
