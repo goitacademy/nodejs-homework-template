5b9053d95b9c196c2c23055854fb93fa7f8e66e6
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-09-08 22:35:14 +0200

    updated files

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-const router = require("./routes/api/contacts-router.js");
+import router from "./routes/api/contacts-router.js";
 
-const app = express();
+const app = express(); // app web-server
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Home page</h1>");
+// });
+// app.get("/contacts", (req, res) => {
+//   console.log(req.url);
+//   console.log(req.method);
+//   res.send("<h1>Contacts</h1>");
+// });
+
+// app.listen(3000, () => console.log("Server listening on port 3000"));
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -22,4 +33,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-module.exports = app;
+export default app;
