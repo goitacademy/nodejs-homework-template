479c5be9958e29e6375418e551df5039718ae322
Ihor Poliakov <piv2003@gmail.com>
2023-09-08 19:59:23 +0200

    adding files

--- models/index.js
+++ models/index.js
@@ -1,14 +1,24 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+const updateContacts = async (allContacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+};
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +26,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
