480a2b4802a01b9b6c81c0a5d251b3affc00ba40
Zyabrik10 <sasamazurok@gmail.com>
2023-09-08 15:18:20 +0300

    Hw-nodejs-02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,108 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const createNewContactSchema = require("../../schemes/createNewContact/createNewContact");
+const updateContactSchema = require("../../schemes/updateContact/updateContact");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../contacts");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res) => {
+  try {
+    const contacts = await listContacts();
+    res.json({ satus: 200, data: contacts });
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  try {
+    const contactsById = await getContactById(req.params.contactId);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (contactsById) {
+      res.json({ satus: 200, data: contactsById });
+    } else {
+      res.json({ satus: 404, message: "Not found" });
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 
-module.exports = router
+router.post("/", async (req, res) => {
+  try {
+    const { email, name, phone } = req.query;
+
+    const { value, error } = createNewContactSchema.validate({
+      name,
+      email,
+      phone,
+    });
+
+    if (error) {
+      res.json({ satus: 400, message: error.details[0].message });
+      return;
+    }
+
+    const newContact = await addContact(value);
+
+    if (newContact) {
+      res.json({ satus: 201, data: newContact });
+    } else {
+      res.json({ satus: 400, message: "Contact exists already" });
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+router.delete("/:contactId", async (req, res) => {
+  try {
+    const contactsById = await removeContact(req.params.contactId);
+
+    if (contactsById) {
+      res.json({ satus: 200, message: "contact deleted" });
+    } else {
+      res.json({ satus: 404, message: "Not found" });
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+router.put("/:contactId", async (req, res) => {
+  try {
+    if (Object.keys(req.query).length === 0) {
+      res.json({ satus: 400, message: "missing fields" });
+      return;
+    }
+
+    const { value, error } = updateContactSchema.validate(req.query);
+
+    if (error) {
+      res.json({ satus: 400, message: error.details[0].message });
+      return;
+    }
+
+    const { contactId } = req.params;
+    const contactsById = await getContactById(contactId);
+
+    if (contactsById) {
+      const updatedContact = await updateContact(contactId, value);
+      res.json({ satus: 200, data: updatedContact });
+    } else {
+      res.json({ satus: 404, message: "Not found" });
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+module.exports = router;
---
+++ schemes/updateContact/updateContact.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+
+const scheme = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string(),
+});
+
+module.exports = scheme;
