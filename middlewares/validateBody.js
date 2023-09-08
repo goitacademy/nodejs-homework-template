599e614c2256dbf1389b89362d752342d11dad54
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-09-08 21:16:08 +0200

    fixxed

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -8,7 +8,22 @@ const validateBody = (schema) => {
 
     const { error } = schema.validate(req.body);
     if (error) {
-      return next(HttpError(400, 'missing required name field'));
+      
+      const requiredFields = schema.describe().keys;
+      const missingFields = [];
+
+      for (const field in requiredFields) {
+        if (requiredFields[field].flags && requiredFields[field].flags.presence === 'required') {
+          if (!req.body[field]) {
+            missingFields.push(field);
+          }
+        }
+      }
+
+      if (missingFields.length > 0) {
+        const message = `missing required ${missingFields.join(', ')} field${missingFields.length > 1 ? 's' : ''}`;
+        return next(HttpError(400, message));
+      }
     }
 
     next();
