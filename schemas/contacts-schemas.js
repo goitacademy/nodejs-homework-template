8ea72a3e5d1b78c66684030299312add3c6de992
Ihor Poliakov <piv2003@gmail.com>
2023-09-08 20:02:02 +0200

    Update contacts-schemas.js

---
+++ schemas/contacts-schemas.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().required(),
+});
+
+module.exports = { contactAddSchema };
