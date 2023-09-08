0b47fa8aaba5ef24cd41a1768aa863d3a9693e71
Jakub Piksa <kuba.piksa@wp.pl>
2023-09-08 13:30:45 +0300

    działa

--- app.js
+++ app.js
@@ -20,4 +20,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 module.exports = app;
