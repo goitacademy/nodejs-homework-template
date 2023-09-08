2cdcb9aa40444f4ff22a44fa51f2dc0716e7956c
Ihor Poliakov <piv2003@gmail.com>
2023-09-08 19:39:30 +0200

    Update contacts-controller.js

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,25 @@
+const contactsService = require("../models");
+const { HttpError } = require("../helpers");
+const { ctrlWrapper } = require("../decorators");
+
+const listContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+};
