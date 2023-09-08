338459e000a2bbfb5dc38685aaec002f839444c0
Ihor Poliakov <piv2003@gmail.com>
2023-09-08 20:03:39 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
+
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const updateContacts = async (allContacts) => {
@@ -12,13 +13,41 @@ const listContacts = async () => {
   return JSON.parse(data);
 };
 
-const getContactById = async (contactId) => {};
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const result = allContacts.find((item) => item.id === contactId);
+  return result || null;
+};
 
-const removeContact = async (contactId) => {};
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = allContacts.splice(index, 1);
+  await updateContacts(allContacts);
+  return result;
+};
 
-const addContact = async (body) => {};
+const addContact = async (body) => {
+  const allContacts = await listContacts();
+  const newContact = { id: nanoid(), ...body };
+  allContacts.push(newContact);
+  await updateContacts(allContacts);
+  return newContact;
+};
 
-const updateContact = async (contactId, body) => {};
+const updateContact = async (contactId, body) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  allContacts[index] = { id: contactId, ...body };
+  await updateContacts(allContacts);
+  return allContacts[index];
+};
 
 module.exports = {
   listContacts,
