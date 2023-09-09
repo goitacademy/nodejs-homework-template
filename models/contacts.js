4e38ac3fcb349028de200c13eb5692bb070787fb
Lizeth castillo <64451121+lizethcas@users.noreply.github.com>
2023-09-08 22:01:32 -0500

    MVC

--- models/contacts.js
+++ models/contacts.js
@@ -9,6 +9,7 @@ const ERROR_MESSAGES = {
   CONTACT_ALREADY_EXISTS: "The contact already exists",
   MISSING_PARAMETERS: "Missing required parameters: name, email, phone",
 };
+
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const contact = contacts.find((contact) => contact.id === contactId);
