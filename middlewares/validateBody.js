a8745379ec30f792335b5c396dc649bdc8c794f9
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-09-09 21:29:46 +0200

    fixschema

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,34 +1,23 @@
 const HttpError = require("../helpers/HttpError");
+const Joi = require('joi');
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
-    if (Object.keys(req.body).length === 0) {
-      return next(HttpError(400, 'missing fields'));
+    if (!req.body) {
+            return next(HttpError(400, 'missing fields'));
     }
 
     const { error } = schema.validate(req.body);
     if (error) {
-      
-      const requiredFields = schema.describe().keys;
-      const missingFields = [];
-
-      for (const field in requiredFields) {
-        if (requiredFields[field].flags && requiredFields[field].flags.presence === 'required') {
-          if (!req.body[field]) {
-            missingFields.push(field);
-          }
-        }
-      }
-
-      if (missingFields.length > 0) {
-        const message = `missing required ${missingFields.join(', ')} field${missingFields.length > 1 ? 's' : ''}`;
-        return next(HttpError(400, message));
-      }
+      return next(HttpError(400, error.message));
     }
 
     next();
   };
   return func;
 };
+
+
+
 
 module.exports = validateBody;
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,35 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    'string.empty': 'Name field is required',
+    'any.required': 'Name field is required',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.email': 'Invalid email format',
+    'string.empty': 'Email field is required',
+    'any.required': 'Email field is required',
+  }),
+  phone: Joi.string().required().messages({
+    'string.empty': 'Phone field is required',
+    'any.required': 'Phone field is required',
+  }),
 });
+
+const data = {
+  name: '',
+  email: 'invalid_email',
+  phone: '',
+};
+
+const result = addSchema.validate(data, { abortEarly: false });
+
+if (result.error) {
+  const errorMessages = result.error.details.map((error) => error.message);
+  console.log('Validation errors:');
+  console.log(errorMessages);
+} else {
+  console.log('Data is valid.');
+}
 
 module.exports = { addSchema };
