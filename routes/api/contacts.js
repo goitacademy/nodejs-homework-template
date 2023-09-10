e7d9bd57c7e77e66fa2ba2f4553bb869f21eb8d8
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-09-10 14:54:22 +0300

    contactscahema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import * as contactSchema from "../../schema/schema.js"
+import contactSchema from "../../schema/schema.js"
 import ctrlContacts from "../../controllers/ctrlContacts.js";
 
 import { isValidId } from "../../middlewars/index.js";
