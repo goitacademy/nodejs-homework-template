4a812f7dd0a6bbddaf547b4f1b919dd72b86e2fb
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-09-10 13:47:33 +0200

    poprawki

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-
-// const dotenv = require('dotenv');
-// dotenv.config();
 require('dotenv').config(); 
 
 const { authRouter, contactsRouter } = require('./routes/api');
--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,7 +1,7 @@
-const { User } = require('../../models');
-const { Unauthorized } = require('http-errors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const { User } = require("../../models");
+const { Unauthorized } = require("http-errors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const { SECRET_KEY } = process.env;
 
@@ -10,24 +10,21 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
   const { subscription } = user;
 
-  // console.log(user);
-  // console.log(subscription);
-
   const pasCompare = bcrypt.compareSync(password, user.password);
 
   if (!user || !pasCompare) {
-    throw new Unauthorized('Email or password is wrong');
+    throw new Unauthorized("Email or password is wrong");
   }
 
   const payload = {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
-    status: 'success',
+    status: "success",
     code: 200,
     data: {
       token,
