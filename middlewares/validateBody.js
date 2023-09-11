80d402d5eedd7d130af7e3f5a88882298a61d0ea
Kamilucha <brnkamila2@gmail.com>
2023-09-11 22:58:45 +0200

    Update validateBody.js

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,10 +1,13 @@
-const { HttpError } = require("../helpers");
+// const { HttpError } = require("../helpers");
  
 const validateBody = schema => {
     const func = (req, res, next) => {
         const { error } = schema.validate(req.body);
         if (error) {
-      next(HttpError(400, error.message));
+    //   next(HttpError(400, error.message));
+            return res.status(400).json({
+                message: error.message
+            })
     }
         next();
     }
