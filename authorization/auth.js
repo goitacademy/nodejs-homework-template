5b1f67a45a5ef8ab433e3a2a6be8945a7e4f4ddd
Guma96 <mateusz.kutarba96@gmail.com>
2023-09-11 22:58:37 +0200

    Update auth.js

--- authorization/auth.js
+++ authorization/auth.js
@@ -12,6 +12,31 @@ const auth = (req, res, next) => {
         },
       });
     }
+
+    const tokenFromHeader = req.headers.authorization;
+    if (!tokenFromHeader || !tokenFromHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        status: "fail",
+        code: 401,
+        data: "Unauthorized",
+        ResponseBody: {
+          message: "Token missing or invalid",
+        },
+      });
+    }
+
+    const token = tokenFromHeader.replace("Bearer ", "");
+    if (token !== user.token) {
+      return res.status(401).json({
+        status: "fail",
+        code: 401,
+        data: "Unauthorized",
+        ResponseBody: {
+          message: "Token mismatch",
+        },
+      });
+    }
+
     req.user = user;
     next();
   })(req, res, next);
