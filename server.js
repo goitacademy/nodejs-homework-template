bdbcafbe30c79b1b7a25d9f3ae6609f005b4d962
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2023-09-11 20:17:35 +0300

    add wev-server mangoDB

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://DonKanElion:uSLjnJQGrnxNplk9@cluster0.tio2ex9.mongodb.net/db-contacts";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connect success 🚀");
+    app.listen(3000, () => {
+      console.log("🐝 Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connect error 🙁", error.message);
+    process.exit(1);
+  });
+
+// DonKanElion
+// uSLjnJQGrnxNplk9
+
+// mongodb+srv://DonKanElion:uSLjnJQGrnxNplk9@cluster0.tio2ex9.mongodb.net/
+
+// mongodb+srv://DonKanElion:uSLjnJQGrnxNplk9@cluster0.tio2ex9.mongodb.net/
