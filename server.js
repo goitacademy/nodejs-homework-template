45980a21a927f23abcdfdaee58a98ff7fb23d1c1
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2023-09-13 15:51:34 +0300

    у

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const {DB_HOST} = require("./config")
+const {DB_HOST} = require("./confidential")
 
 mongoose.connect(DB_HOST)
   .then(() => {
