9d2687aa7d23b2b4f82e51e085bf3af57e7ab33a
Sergey4857 <yasak.sergey@gmail.com>
2023-09-13 23:27:21 +0300

    start

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const Host =
+  "mongodb+srv://Sergey:Kx455k5i9rdOre0K@cluster0.s8fhtp1.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(Host)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
