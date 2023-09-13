25dcc76e43969680862eb94a69c6ede551375798
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2023-09-13 14:49:18 +0300

    i

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Levandosp:Worldoftank@cluster0.if2rhl0.mongodb.net/contacts_db?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
