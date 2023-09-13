7509e657022c30acb548d5d63f8f968b723df745
Emil Nowak <emil32132@onet.pl>
2023-09-13 21:16:39 +0200

    Contact contaroller work

--- contacts/contacts.controller.js
+++ contacts/contacts.controller.js
@@ -6,11 +6,41 @@ import {
   patchContact,
 } from "./contacts.dao.js";
 
+export const getAllContactsHandler = async (_, res) => {
+  const contacts = await getAllContact();
+  return res.json({ contacts });
+};
+
 export const getContactHandler = async (req, res) => {
   const contactId = req.params.id;
   const contact = await getContact(contactId);
   if (!contact) {
-    res.status(404).send();
+    return res.status(404).send();
   }
-  return res.json({ contact });
+  res.json({ contact });
+};
+
+export const createContactHandler = async (req, res) => {
+  const contactToCreate = req.body;
+  if (!contactToCreate.email) {
+    return res.status(400).send({ error: "Email jest wymagany" });
+  }
+  const createdContact = await addContact(contactToCreate);
+  return res.status(201).json({ contact: createdContact });
+};
+
+export const updateContactHandler = async (req, res) => {
+  const contactId = req.params.id;
+  const contactToUpdate = req.body;
+  const updatedContact = await patchContact(contactId, contactToUpdate);
+  if (!updatedContact) {
+    return res.status(404).send({ message: "Nie znaleziono kontaktu" });
+  }
+  res.status(200).json(updatedContact);
+};
+
+export const deleteContactHandler = async (req, res) => {
+  const contactId = req.params.id;
+  await deleteContact(contactId);
+  return res.status(204).send();
 };
--- contacts/contacts.route.js
+++ contacts/contacts.route.js
@@ -1,51 +1,18 @@
 import { Router } from "express";
-import { getContactHandler } from "./contacts.controller.js";
 import {
-  addContact,
-  deleteContact,
-  getAllContact,
-  getContact,
-  patchContact,
-} from "./contacts.dao.js";
+  getAllContactsHandler,
+  getContactHandler,
+  createContactHandler,
+  updateContactHandler,
+  deleteContactHandler,
+} from "./contacts.controller.js";
 
 const contactsRouter = Router();
 
-contactsRouter.get("/", async (_, res) => {
-  const contacts = await getAllContact();
-  return res.json({ contacts });
-});
-
-contactsRouter.get("./:id", getContactHandler);
-
-contactsRouter.post(
-  "/",
-  (req, res, next) => {
-    const contactToCreate = req.body;
-    if (contactToCreate.email === undefined) {
-      return res.status(404).send({ error: "Email is required " });
-    }
-    next();
-  },
-  async (req, res) => {
-    const contactToCreate = req.body;
-    const createdContact = await addContact(contactToCreate);
-
-    return res.status(201).json({ contact: createdContact });
-  }
-);
-contactsRouter.patch("/:id", async (req, res) => {
-  const contactId = req.params.body;
-  const contactToPatch = req.body;
-
-  const patchedContact = await patchContact(contactId, contactToPatch);
-
-  return res.status(201).send({ user: patchedContact });
-});
-
-contactsRouter.delete("/:id", async (req, res) => {
-  const contactId = req.params.body;
-  await deleteContact(contactId);
-  return res.status(204).send();
-});
+contactsRouter.get("/", getAllContactsHandler);
+contactsRouter.get("/:id", getContactHandler);
+contactsRouter.post("/", createContactHandler);
+contactsRouter.patch("/:id", updateContactHandler);
+contactsRouter.delete("/:id", deleteContactHandler);
 
 export { contactsRouter };
