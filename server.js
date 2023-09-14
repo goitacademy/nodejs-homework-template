9ed4295c93a4c402348a22217b2d1c466141dc31
Sergey4857 <yasak.sergey@gmail.com>
2023-09-14 11:28:23 +0300

    +

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(Host)
+  .connect(
+    "mongodb+srv://Sergey:Kx455k5i9rdOre0K@cluster0.s8fhtp1.mongodb.net/db-contacts?retryWrites=true&w=majority"
+  )
   .then(() => {
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
