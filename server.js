28e3610cb8d03a5f7acbf6593a422c7d10422721
Brod9ga <gbonik@i.ua>
2023-09-14 23:34:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 
 
-import app from './app.js'
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
-=======
+
 import mongoose from 'mongoose'
 import app from './app.js'
 
