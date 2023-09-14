657de046a2b4737c7282fcc002df20dc9234412e
Igor0501 <s.igor0501@gmail.com>
2023-09-15 02:27:42 +0300

    виправив

--- models/contacts.js
+++ models/contacts.js
@@ -53,14 +53,13 @@ const updateContact = async (contactId, body) => {
   const contacts = await listContacts();
 
   const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) return null;
-  contacts[index] = { id: contactId, ...body };
-
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-  return contacts[index];
+  if (index === -1) { return null;
+  } else {
+    contacts[index] = { ...contacts[index], ...body };
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index];
+  }
 };
-
 
 module.exports = {
   listContacts,
