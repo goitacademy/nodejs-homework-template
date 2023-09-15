6cc5f58aa33fca881867c6fa8cdfea145ed2af3a
rafalsalega <105451060+rafalsalega@users.noreply.github.com>
2023-09-15 12:05:12 +0200

    Create routes, controller and services. Connect Mongo for Mongoose

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const contactsRouter = require('./routes/api/contacts')
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const contactsRouter = require("./routes/api/contacts.routes");
 
-app.use('/api/contacts', contactsRouter)
+const PORT = process.env.PORT || 4000;
+const uriDb = process.env.DATABASE_URL;
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+app.use(express.json());
+app.use("/api/v1", contactsRouter);
 
-module.exports = app
+connection.then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: [${err}]`);
+    process.exit(1);
+  });
