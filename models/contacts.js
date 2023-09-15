fd25689183d98e0ef442a89f4c73140918024613
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-09-15 16:39:27 -0500

    add function update and hundler error

--- models/contacts.js
+++ models/contacts.js
@@ -1,52 +1,93 @@
-const animalRepository = require('./contacts.json')
-const readJsonFiles = require('../untils/readJsonFiles')
-const writeJsonFile = require('../untils/writeJsonFile')
-const path = require('path')
-const { nanoid } = require('nanoid')
-const ERROR_TYPES = require('../constants/errorTypes')
-const createError = require('../untils/createError')
+const animalRepository = require("./contacts.json");
+const readJsonFiles = require("../untils/readJsonFiles");
+const writeJsonFile = require("../untils/writeJsonFile");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const ERROR_TYPES = require("../constants/errorTypes");
+const createError = require("../untils/createError");
 // const fs = require('fs/promises')
-const CONTACTS_PATH  = path.join(__dirname,'./contacts.json')
+const CONTACTS_PATH = path.join(__dirname, "./contacts.json");
 const listContacts = async () => {
-  const contactsList = await readJsonFiles(CONTACTS_PATH)
-  return contactsList
-}
+  const contactsList = await readJsonFiles(CONTACTS_PATH);
+  return contactsList;
+};
 
 const getContactById = async (contactId) => {
-  const contactsList = await listContacts()
+  const contactsList = await listContacts();
   const contact = contactsList.find((contact) => contact.id === contactId);
-  if(!contact){
-    const error = createError(ERROR_TYPES.NOT_FOUND,{
+  if (!contact) {
+    const error = createError(ERROR_TYPES.NOT_FOUND, {
       message: `Not found`,
       data: {},
-    })
-    throw error
+    });
+    throw error;
   }
-  return contact
-}
+  return contact;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contactList = await listContacts();
+  const contactIndex = contactList.findIndex((e) => e.id === contactId);
+  if (contactIndex === -1) {
+    const error = createError(ERROR_TYPES.NOT_FOUND, {
+      message: `Not found`,
+      data: {},
+    });
+    throw error;
+  }
+  contactList.splice(contactIndex, 1);
+  writeJsonFile(CONTACTS_PATH, contactList);
+  return "contact deleted";
+};
 
 const addContact = async (body) => {
-  const {name,email,phone} = body
-  if(!name || !email || !phone){
-    const error = createError(ERROR_TYPES.BAD_REQUEST,{
+  const { name, email, phone } = body;
+  if (!name || !email || !phone) {
+    const error = createError(ERROR_TYPES.BAD_REQUEST, {
       message: "missing required name field",
-      data:{}
-    })
-    throw error
+      data: {},
+    });
+    throw error;
   }
   const contact = {
-    id:nanoid(),
-    ...body
-  }
-  const contactsList = await listContacts()
-  contactsList.push(contact)
-  writeJsonFile(CONTACTS_PATH,contactsList)
-  return contact
-}
+    id: nanoid(),
+    ...body,
+  };
+  const contactsList = await listContacts();
+  contactsList.push(contact);
+  await writeJsonFile(CONTACTS_PATH, contactsList);
+  return contact;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  if (Object.keys(body).length === 0) {
+    const error = createError(ERROR_TYPES.BAD_REQUEST, {
+      message: "missing fields",
+      data: {},
+    });
+    throw error;
+  }
+  const contactList = await listContacts();
+  const { name, email, phone } = body;
+  const changeContactIndex = contactList.findIndex((e) => e.id === contactId);
+  const changeContact = contactList[changeContactIndex];
+  if (changeContactIndex === -1) {
+    const error = createError(ERROR_TYPES.NOT_FOUND, {
+      message: `Not found`,
+      data: {},
+    });
+    throw error;
+  }
+
+  contactList[changeContactIndex] = {
+    ...changeContact,
+    name: name || changeContact.name,
+    email: email || changeContact.email,
+    phone: phone || changeContact.phone,
+  };
+  await writeJsonFile(CONTACTS_PATH, contactList);
+  return contactList[changeContactIndex];
+};
 
 module.exports = {
   listContacts,
@@ -54,4 +95,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const contactService = require("../../models/contacts");
 const router = express.Router();
-const handlerError = require('../../middlewears/handlerError')
+const handlerError = require("../../middlewears/handlerError");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -27,26 +27,46 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-
 router.post("/", async (req, res, next) => {
-  try{
-  const {body} = req
-  const contact = await contactService.addContact(body)
-  res.status(201).json({
-    data:contact
-  })}catch(error){
-    next(error)
+  try {
+    const { body } = req;
+    const contact = await contactService.addContact(body);
+    res.status(201).json({
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
   }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const deleteContact = await contactService.removeContact(contactId);
+    res.status(200).json({
+      message: deleteContact,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const { body } = req;
+
+    const updateContact = await contactService.updateContact(contactId, body);
+
+    res.status(200).json({
+      data: updateContact,
+      message: "you pidor",
+    });
+  } catch (error) {
+    next(error)
+  }
 });
-router.use(handlerError)
+router.use(handlerError);
 
 // router.use((err, _, res, __) => {
 //   res.status(404).json({
