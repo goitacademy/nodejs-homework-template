9178ef568006360998dec2b0c3fbee19ba976215
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-09-15 09:30:06 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'done hw-02' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
