169380b83766c0b433f27551152241762819ae6f
rafalsalega <105451060+rafalsalega@users.noreply.github.com>
2023-09-15 13:03:07 +0200

    Finish

--- services/contacts.service.js
+++ services/contacts.service.js
@@ -1,8 +1,41 @@
 const Contact = require("../models/contact.model");
 
-const getAllContacts = async (query) => {
-    console.log("jestem");
-     return Contact.find(query);
+const getAll = async (query) => {
+  return Contact.find(query);
 };
 
-module.exports = { getAllContacts }
+const getOne = async (id) => {
+  return Contact.findById(id);
+};
+
+const create = async (body) => {
+  return Contact.create(body);
+};
+
+const update = async (id, body) => {
+  return Contact.findByIdAndUpdate(id, body, {
+    new: true,
+  });
+};
+
+const remove = async (id) => {
+  return Contact.findByIdAndRemove(id);
+};
+
+const updateStatusContact = async (id, favorite) => {
+  return Contact.findByIdAndUpdate(
+    id,
+    { favorite },
+    {
+      new: true,
+    }
+  );
+};
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  remove,
+  updateStatusContact,
+};
