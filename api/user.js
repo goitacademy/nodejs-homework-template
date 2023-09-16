7a60bb25eee6304edf0258887756fc4e4241ffc9
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-09-16 20:19:09 +0200

    init and udpate app.js

--- api/user.js
+++ api/user.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+// const fs = require("fs").promises;
+const uploadDir = path.join(process.cwd(), "public", "avatars");
 
 const {
 	signUpUser,
@@ -16,5 +20,27 @@ router.post("/login", loginUser);
 router.post("/logout", auth, logoutUser);
 
 router.get("/current", auth, getCurrentUser);
+
+// //multer
+// const storage = multer.diskStorage({
+// 	destination: uploadDir,
+// 	filename: (req, file, cb) => {
+// 		cb(null, file.originalname);
+// 	},
+// 	limits: {
+// 		fileSize: 1048576,
+// 	},
+// });
+
+// const upload = multer({
+// 	storage: storage,
+// });
+
+// upload.single("photo");
+// console.log(storage);
+
+// router.get("/avatars", upload.single("photo"), (req, res) => {
+// 	res.status(200).json({ message: "plik załadowany" });
+// });
 
 module.exports = router;
--- services/schemas/user.js
+++ services/schemas/user.js
@@ -25,6 +25,7 @@ const user = new Schema({
 		type: Schema.Types.ObjectId,
 		ref: "user",
 	},
+	avatarURL: { type: String },
 });
 
 user.methods.setPassword = function (password) {
