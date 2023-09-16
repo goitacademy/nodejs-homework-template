69e4507ba61d03f919954ec43dc1889e62a83218
Sara Motyka <saramotyka97@gmail.com>
2023-09-16 13:12:30 +0200

    files contacts.js modified

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,95 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
 
-const removeContact = async (contactId) => {}
+    if (contacts.length === 0) {
+      console.log("Lista kontaktów jest pusta.");
+    } else {
+      return contacts;
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((c) => c.id === contactId) || null;
+    if (contact) {
+      console.log(`Kontakt o ID ${contactId}`);
+    } else {
+      console.log(`Nie znaleziono kontaktu o ID ${contactId}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+
+    if (contacts.length === updatedContacts.length) {
+      console.log("Kontakt nie istnieje");
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const data = fs.readFile("contacts.json");
+    const contacts = JSON.parse(data);
+    const contactIndex = contacts.findIndex((c) => c.id === contactId);
+
+    if (contactIndex === -1) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Not found" }),
+      };
+    }
+    contacts[contactIndex] = { ...contacts[contactIndex], ...body };
+    await fs.writeFile("contacts.json", JSON.stringify(contacts, null, 2));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(contacts[contactIndex]),
+    };
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +97,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
