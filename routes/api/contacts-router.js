4a13c96884214452298e925d66701a9f03f84431
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-09-17 16:30:19 +0200

    completed task v.01

---
+++ routes/api/contacts-router.js
@@ -0,0 +1,83 @@
+import express from "express";
+import contactsService from "../../models/contacts.js";
+import Joi from "joi";
+import { HttpError } from "../../helpers/helpers.js";
+
+const contactsRouter = express.Router();
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.number().required(),
+});
+
+contactsRouter.get("/", async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Not found contact with id=${contactId}`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.post("/", async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.put("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+      const existingContact = await contactsService.getContactById(contactId);
+    if (!existingContact) {
+      throw HttpError(404, `Not found contact with id=${contactId}`);
+    }
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.updateContact(contactId, req.body);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+contactsRouter.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, `Not found contact with id=${contactId}`);
+    }
+    res.json({
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default contactsRouter;
