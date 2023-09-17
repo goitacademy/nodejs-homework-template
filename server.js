df227f435bc8387247fef96da496970cdb90e199
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-09-17 22:11:54 +0300

    database connection

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+const {
+  MONGO_DB_USER,
+  MONGO_DB_PASSWORD,
+  MONGO_DB_HOST,
+  MONGO_DB_DATABASE,
+} = require("./constants/env");
 
-app.listen(3030, () => {
-  console.log("Server running. Use our API on port: 3030");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3030);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
