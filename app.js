adfe487e72f3ad9aaa4707bf07e956ad32f7ac2c
Serzh1006 <skravchenko223@gmail.com>
2023-09-17 08:42:15 +0200

    message change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "URL not correct" });
+  res.status(404).json({ message: "URL is not correct" });
 });
 
 app.use((err, req, res, next) => {
