b30b2d34b3d1337e9c40aef865c478ef7275ca78
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-09-17 13:02:34 +0300

    finished hw02

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,91 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const crypto = require("crypto");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+/**
+ * The function `listContacts` reads a file containing contacts and returns them as a parsed JSON
+ * object.
+ * @returns The function `listContacts` returns a promise that resolves to an array of contacts.
+ */
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+/**
+ * The function `getContactById` retrieves a contact object from a list of contacts based on the
+ * provided contactId.
+ * @param contactId - The `contactId` parameter is the unique identifier of the contact that we want to
+ * retrieve from the list of contacts.
+ * @returns The function `getContactById` returns the contact object with the specified `contactId` if
+ * it exists in the list of contacts. If the contact is not found, it returns `null`.
+ */
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+};
+
+/**
+ * The `removeContact` function removes a contact from a list of contacts and updates the contacts
+ * file.
+ * @param contactId - The `contactId` parameter is the unique identifier of the contact that needs to
+ * be removed from the list of contacts.
+ * @returns the deleted contact.
+ */
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [deletedTask] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deletedTask;
+};
+
+/**
+ * The addContact function adds a new contact to a list of contacts and saves it to a file.
+ * @param body - The `body` parameter is an object that contains the data for the new contact. It
+ * includes properties as `name`, `email`, `phone`.
+ * @returns The function `addContact` is returning the newly created contact object.
+ */
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = { id: crypto.randomUUID(), ...body };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+/**
+ * The function `updateContact` updates a contact in a list of contacts by finding the contact with the
+ * specified ID, replacing its properties with the new values provided in the `body` parameter, and
+ * then saving the updated list to a file.
+ * @param contactId - The `contactId` parameter is the unique identifier of the contact that needs to
+ * be updated. It is used to find the index of the contact in the `contacts` array.
+ * @param body - The `body` parameter is an object that contains the updated information for the
+ * contact. It includes properties as `name`, `email`, `phone`.
+ * @returns the updated contact object if it exists in the contacts array, otherwise it returns null.
+ */
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...contacts[index], ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  const updatedContact = contacts[index];
+  return updatedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +93,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,96 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
+const { listContacts, getContactById, addContact, removeContact, updateContact } = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+/* This code is defining a route handler for a GET request to the root URL ("/"). */
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+/* This code is defining a route handler for a GET request to the "/:contactId" URL. It expects a
+parameter called "contactId" in the URL. */
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const getContact = await getContactById(contactId);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (getContact) {
+    res.status(200).json(getContact);
+  } else {
+    res.status(404).json({ message: "Contact not found" });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+/* This code is defining a route handler for a POST request to the root URL ("/"). */
+router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
 
-module.exports = router
+  const { error } = contactSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: "Missing required name field" });
+  }
+
+  try {
+    const newContact = await addContact({ name, email, phone });
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* This code is defining a route handler for a DELETE request to the "/:contactId" URL. It expects a
+parameter called "contactId" in the URL. */
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const deleteContact = await removeContact(contactId);
+
+    if (deleteContact) {
+      res.status(200).json({ message: "Contact deleted" });
+    } else {
+      res.status(404).json({ message: "Contact not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* This code is defining a route handler for a PUT request to the "/:contactId" URL. It expects a
+parameter called "contactId" in the URL. */
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
+
+  try {
+    const editContact = await updateContact(contactId, { name, email, phone });
+
+    if (editContact) {
+      res.status(200).json(editContact);
+    } else {
+      res.status(404).json({ message: "Contact not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
