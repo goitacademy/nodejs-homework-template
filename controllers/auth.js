07fd3cec6aa6cef91570969a19daa7d6fbdf55d9
9Alla9 <konfetcka@gmail.com>
2023-09-18 18:21:13 +0300

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,72 +4,72 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-require('dotenv').config();
-const {SECRET_KEY} = process.env;
+require("dotenv").config();
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
-  const { email, password } = req.body;  
-  const user = await User.findOne({ email }); 
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
   console.log("email:", email);
   console.log("user:", user);
-  if (user){
+  if (user) {
     throw HttpError(409, "Email already in use");
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  
-  const newUser = await User.create({...req.body, password: hashPassword});
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     email: newUser.email,
     name: newUser.name,
-  })
-}
+  });
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   console.log("email:", email);
   const user = await User.findOne({ email });
-  if(!user){
+  if (!user) {
     throw HttpError(401, "Email invalid");
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
-  if(!passwordCompare) {
+  if (!passwordCompare) {
     throw HttpError(401, "Password invalid");
   }
-  
+
   const payload = {
     id: user._id,
-  }
-  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"}); //{expiresIn: "23h"} - час можливості використання
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" }); //{expiresIn: "23h"} - час можливості використання
   console.log(token);
-  
+
   res.json({
     token,
-  })
+  });
 };
 
 const getCurrent = async (req, res) => {
-  const {email, name} = req.user;
+  const { email, name } = req.user;
 
   res.json({
-      email,
-      name,
-  })
+    email,
+    name,
+  });
 };
 
 const logout = async (req, res) => {
-  const {_id} = req.user;
-  await User.findByIdAndUpdate(_id, {token: ""});
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
   res.json({
-      message: "Logout success"
-  })                                                                                                                      
+    message: "Logout success",
+  });
 };
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 };
