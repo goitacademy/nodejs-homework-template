4ff6b6130be30ba65c0a51a25a0f44c94a7564a6
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-09-18 16:34:24 +0300

    йцу

--- app.js
+++ app.js
@@ -2,24 +2,45 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+// const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
+const port = 3000; 
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+app.get("/", (req, res) => {
+  res.send("Привет, мир!");
+});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("posted on /");
+});
 
-module.exports = app
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
+
+
+
+
+
+// app.use('/api/contacts', contactsRouter)
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found 2' })
+// })
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({ message: err.message })
+// })
+
+
+// module.exports = app
+
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,61 @@
-// const fs = require('fs/promises')
 
-const listContacts = async () => {}
+// const { nanoid } = require("nanoid");
+// const path = require("node:path");
 
-const getContactById = async (contactId) => {}
+// const fs = require("node:fs/promises");
 
-const removeContact = async (contactId) => {}
+// const DB_PATH = path.join(__dirname, "./contacts.json");
+// console.log(DB_PATH);
 
-const addContact = async (body) => {}
+// const listContacts = async () => {
+//     const data = await fs.readFile(DB_PATH, "utf-8");
 
-const updateContact = async (contactId, body) => {}
+//     return JSON.parse(data);
+  
+// }
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+// const getContactById = async (contactId) => {
+//   String(contactId);
+//   const contacts = await listContacts();
+//   const contact = contacts.find((contact) => contact.id === contactId);
+//   return contact;
+// }
+
+// const removeContact = async (contactId) => {
+//   const contacts = await listContacts();
+//   String(contactId);
+     
+//   const IndexContact = contacts.findIndex(
+//     (contact) => contact.id === contactId
+//   );
+
+//   contacts.slice(IndexContact, 1);
+//   await fs.writeFile(DB_PATH, JSON.stringify(contacts, null, 2));
+//   return contacts[IndexContact];
+  
+// }
+
+// const addContact = async (name, email, phone) => {
+
+//       const contacts = await listContacts();
+//       const newContact = {
+//         id: nanoid(),
+//         name,
+//         email,
+//         phone,
+//   };
+  
+//       contacts.push(newContact);
+//       await fs.writeFile(DB_PATH, JSON.stringify(contacts, null, 2));
+//       return newContact;
+// };
+
+// const updateContact = async (contactId, body) => {}
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// }
