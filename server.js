54ecf6752bcc651dda6e478085281371529bd195
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-09-19 14:11:48 +0300

    first

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const app = require("./app");
+
 const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Server running. Use our API on port: ${port}`);
-});
-
-
+const DB_HOST =
+  "mongodb+srv://InnaTer:PC5vc2t4yVhVikbG@cluster0.ra60b6t.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(port);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
