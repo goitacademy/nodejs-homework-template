9b563a100a0856218ea9d8aed6a92dc28dd8e88f
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-09-19 12:11:25 -0400

    logout corrected

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,9 +62,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.json({
-    message: "Logout success",
-  });
+  res.status(204).json({});
 };
 
 module.exports = {
