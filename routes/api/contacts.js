21fefed6c9f057628b63dac4682499991b3fc2b3
OleksandrZaitsev <apalamir17@gmail.com>
2023-09-19 20:47:13 +0300

    secondhw_try1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,21 @@
-const express = require('express')
+const express = require("express");
+const {
+  getAllContacts,
+  getContact,
+  createContact,
+  deliteContact,
+  changeContact,
+} = require("../../controllers/contacts");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", getAllContacts);
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", getContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", createContact);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", deliteContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", changeContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
