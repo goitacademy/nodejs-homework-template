312703945604a99a9ce43cfc860e697105cb9c61
Avtark <103372792+VolontyrPavel@users.noreply.github.com>
2023-09-19 21:17:17 +0300

    rtgr

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,5 @@
 const { Contact } = require("../models/contacts");
+
 const {
   addSchema,
   updateFavoriteSchema,
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST = 'mongodb+srv://Pavel:Xq5BEVkeDBuxmGlS@cluster0.29zd2pl.mongodb.net/db-contacts.contacts';
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
