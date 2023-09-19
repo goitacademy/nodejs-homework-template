b9a724c8a1b0b8accec2212c8e8b9484e875450a
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-09-19 20:51:43 +0300

    final

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+
+// const { DB_HOST } = require("./config");
+
+const DB_HOST =
+  "mongodb+srv://dariayashchuk2000:G0SEmoGtfBgcvTG6@cluster0.dqtopcj.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
