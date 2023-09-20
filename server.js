5bc3358b44b07c764e1b4c327004b047ed19d96d
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-09-20 21:59:16 +0200

    process.env commit

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.connect(process.env.DB_HOST)
+.then( () => {
+  app.listen(3000, () => {
+    console.log("Database connection successful.")
+  })
+}).catch(error => {
+  console.log(error);
+  process.exit(1)
 })
+
+
+
