945f19674bcc39f5ad78842eb91e2c7b096d0822
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-09-20 20:07:27 +0200

    hw-4

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import { contactsRouter } from "./routes/api/contacts.js";
+import { usersRouter } from "./routes/api/users.js";
 export const app = express();
+import { auth } from "./middlewares.js";
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -10,7 +12,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", auth, contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
