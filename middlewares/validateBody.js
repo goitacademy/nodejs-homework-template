d1c46c58110da27a8890790371ae8533d81dcf46
Soundlover1984 <denys.kovtun@gmail.com>
2023-09-21 23:03:14 +0300

    message error for PATCH added

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,18 +1,22 @@
 const { HttpError } = require("../helpers");
 
-const validateBody = (schema) => {
+const validateBody = (schema, method) => {
   const func = (req, res, next) => {
+    if (method === "PATCH" && !req.body.favorite) {
+      return res.status(400).json({ message: "missing field favorite" });
+    }
     const { error } = schema.validate(req.body);
     if (error) {
       let errorMessage = "";
 
-      if (error.details && error.details.length > 0) {
+      if (method === "POST" && error.details && error.details.length > 0) {
         const missingField = error.details[0].context.key;
         errorMessage = `missing required ${missingField} field`;
       }
-      if (Object.keys(req.body).length === 0) {
+      if (method === "PUT" && Object.keys(req.body).length === 0) {
         errorMessage = "missing fields";
       }
+
       next(HttpError(400, errorMessage));
     } else {
       next();
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,13 +12,13 @@ router.get("/", ctrl.listContacts);
 
 router.get("/:contactId", isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post("/", validateBody(schemas.addSchema, "POST"), ctrl.addContact);
 
 router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.put("/:contactId", isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
+router.put("/:contactId", isValidId, validateBody(schemas.addSchema, "PUT"), ctrl.updateContact);
 
-router.patch("/:contactId/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+router.patch("/:contactId/favorite", isValidId, validateBody(schemas.updateFavoriteSchema, "PATCH"), ctrl.updateFavorite);
 
 module.exports = router;
 
