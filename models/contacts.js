b0c3481b07fd9da5eb47e6d5372380f9f03b8a02
Olga16119 <tkachenko93@ukr.net>
2023-09-21 13:56:27 +0200

    change routes

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,55 @@
-// const fs = require('fs/promises')
+const path = require("path");
+const fs = require("fs/promises");
+const { v4: uuidv4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+const addContact = async (body) => { 
+  const contacts = await listContacts();
+  const newContact = {
+    id: uuidv4(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+  
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts = listContacts();
+  const index = contacts.findIndex(item => item.id === contactId)
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, ...body }
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+return contacts[index]
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,90 @@
 const express = require('express')
+const Joi = require("joi");
+const contacts=require('../../models/contacts')
+
+const {HttpError}= require('../../helpers')
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone:Joi.number().required(),
 })
 
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+  }
+  catch (error) {
+   next(error)
+  }
+  })
+
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result=await contacts.getContactById(contactId)
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.json(result)
+  }
+  catch (error) {
+next(error)
+  }
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = addSchema.validate(req.body)
+    if (error) {
+  throw HttpError(400, error.message)
+}
+    const result = await contacts.addContact(req.body)
+
+  res.status(201).json(result)
+  } catch (error) {
+    res.json({ message: "missing required name field" })
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result=await contacts.removeContact(contactId)
+    if (!result) {
+    throw HttpError(404, "Not found")
+    }
+    res.json({
+      message:"contact deleted"
+    })
+  } catch (error) {
+    next(error)
+}
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = addSchema.validate(req.body)
+    if (error) {
+      throw HttpError(400, error.message)
+    }
+    const { contactId } = req.params;
+      const result = await contacts.updateContact(contactId, req.body)
+    res.json(result)
+  } catch (error) {
+    res.json({
+      
+      message:"missing fields"
+    })
+    next(error)
+  }
+}
+)
 
 module.exports = router
+
