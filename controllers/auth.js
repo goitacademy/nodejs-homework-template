295b1d499baa091c6062b25e9372a83145f10124
Denys Tryfonov <trifonowden123@gmail.com>
2023-09-22 11:48:02 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,7 +68,8 @@ const getCurrent = async (req, res) => {
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204).end();
+  // res.status(204).end();
+  res.status(204).send();
 };
 
 const subscription = async (req, res) => {
