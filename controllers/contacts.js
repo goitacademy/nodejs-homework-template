b8555ad24c928c3df09bf79dbe6eb1e4b5a28425
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-09-22 19:14:18 +0300

    up

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,13 @@ const Contact = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
   res.json(result);
 };
 
@@ -16,7 +22,8 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
