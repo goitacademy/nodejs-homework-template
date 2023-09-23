f35806ed59975ee6b7a0bfbf785193ad81c78aeb
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-09-23 17:24:30 +0300

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,22 +2,22 @@ const express = require("express");
 
 const {
   getContacts,
-  // getContactById,
-  // addNewContact,
-  // deleteContact,
-  // putContact,
+  getContactById,
+  addNewContact,
+  deleteContact,
+  putContact,
 } = require("../../controllers/controllers");
 
 const router = express.Router();
 
 router.get("/", getContacts);
 
-// router.get("/:contactId", getContactById);
+router.get("/:contactId", getContactById);
 
-// router.post("/", addNewContact);
+router.post("/", addNewContact);
 
-// router.delete("/:contactId", deleteContact);
+router.delete("/:contactId", deleteContact);
 
-// router.put("/:contactId", putContact);
+router.put("/:contactId", putContact);
 
 module.exports = router;
