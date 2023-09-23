c1c9396d83b3cd4f57291945b279226d894736ce
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-09-23 16:52:05 +0300

    copy

---
+++ models/contacts copy.js
@@ -0,0 +1,11 @@
+import fs from "fs/promises";
+
+export const listContacts = async () => {};
+
+export const getContactById = async (contactId) => {};
+
+export const removeContact = async (contactId) => {};
+
+export const addContact = async (body) => {};
+
+export const updateContact = async (contactId, body) => {};
