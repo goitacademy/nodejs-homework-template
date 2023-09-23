97c5864527ae2793cb96002954983ecc945fbc52
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-09-23 20:59:00 +0300

    fixed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -25,7 +25,6 @@ const removeContact = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  console.log(req.body);
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
