51791d60a5b8ff75dd156d8c20a155cc56cb1ae7
VirLg <19virlg@gmail.com>
2023-09-24 14:01:17 +0300

    add

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+// const express = require('express')
+// const logger = require('morgan')
+// const cors = require('cors')
+// const contactsRouter = require('./routes/api/contacts')
+import { express } from 'express';
+// import { logger } from 'morgan';
+// import { cors } from 'cors';
+// import contactsRouter from "('./routes/api/contacts')";
 
-const contactsRouter = require('./routes/api/contacts')
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h2>Home page</h2>');
+});
+app.listen(3000, () => console.log('Server runin'));
 
-const app = express()
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// app.use('/api/contacts', contactsRouter);
 
-app.use('/api/contacts', contactsRouter)
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found' });
+// });
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+// app.use((err, req, res, next) => {
+//   res.status(500).json({ message: err.message });
+// });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
-
-module.exports = app
+export default app;
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,19 @@
 // const fs = require('fs/promises')
+// import fs from 'fs/promises';
+export const listContacts = async () => {};
 
-const listContacts = async () => {}
+export const getContactById = async contactId => {};
 
-const getContactById = async (contactId) => {}
+export const removeContact = async contactId => {};
 
-const removeContact = async (contactId) => {}
+export const addContact = async body => {};
 
-const addContact = async (body) => {}
+export const updateContact = async (contactId, body) => {};
 
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
