c09a8043c174f8a91f0c4faac9680724c1c19e9f
GKastet <127600582+GKastet@users.noreply.github.com>
2023-09-24 15:25:43 +0200

    MongoDB and Mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const ctrl = require('../../controllers/contacts')
-const {validateBody} = require('../../middlewares')
+const {validateBody, isValidId} = require('../../middlewares')
 const schemas = require('../../schemas/contacts')
 
 const router = express.Router();
@@ -9,8 +9,9 @@ const router = express.Router();
 router.get("/", ctrl.getAll);
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.get("/:id", ctrl.getContactById);
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContactById);
-router.delete("/:id", ctrl.deleteContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContactById);
+router.delete("/:id", isValidId, ctrl.deleteContactById);
+router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavoriteById);
 
 module.exports = router;
