dd64f34f6c03e58fe216fad15e2a72ed6127e159
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-09-24 16:15:28 +0300

    update ok

--- models/contacts 1dz.js
+++ models/contacts 1dz.js
@@ -2,34 +2,33 @@ import fs from "fs/promises";
 import path from "path";
 import { nanoid } from "nanoid";
 
-const contactPath = path.resolve("models", "contacts.json");
+const contactPath = path.resolve("db", "contacts.json");
 
 const updateContacts = (contacts) => {
   fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
 };
 
-export const listContacts = async () => {
+export async function listContacts() {
   const data = await fs.readFile(contactPath);
   return JSON.parse(data);
-};
+}
 
-export const getContactById = async (contactId) => {
+export async function getContactById(contactId) {
   const contacts = await listContacts();
   const results = contacts.find((contact) => contact.id === contactId);
   return results || null;
-};
+}
 
-export const removeContact = async (contactId) => {
+export async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index === -1) return null;
   const [results] = contacts.splice(index, 1);
   await updateContacts(contacts);
   return results;
-};
+}
 
-export const addContact = async (body) => {
-  const { name, email, phone } = body;
+export async function addContact(name, email, phone) {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
@@ -40,14 +39,4 @@ export const addContact = async (body) => {
   contacts.push(newContact);
   await updateContacts(contacts);
   return newContact;
-};
-
-export const updateContact = async (contactId, body) => {
-  const { name, email, phone } = body;
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) return null;
-  contacts[index] = { ...contacts[index], ...{ name, email, phone } };
-  await updateContacts(contacts);
-  return contacts[index];
-};
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 import express from "express";
 
 const router = express.Router();
