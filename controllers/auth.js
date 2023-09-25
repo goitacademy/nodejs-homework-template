3a35905bf91275ae39512fa7b10a4d3b248ea178
Trifonov Denys <trifonowden123@gmail.com>
2023-09-26 00:14:49 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,17 +46,18 @@ const registr = async (req, res) => {
   });
 };
 
-const verifyEmail = async(req, res) =>{
- const {verificationToken} = req.params
- const user = User.findOne({verificationToken})
- if(!user){
-  throw HttpError(404, "User not found")
- }
- await User.findByIdAndUpdate(user._id, {verify: true, verificationToken: ""})
+const verifyEmail = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+  
+  await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: "" });
 
- res.json({
-  message: "Verification successful"
- })
+  res.json({
+    message: "Verification successful"
+  });
 }
 
 const resendVerifyEmail = async (req, res) =>{
