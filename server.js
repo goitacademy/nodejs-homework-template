33fad5c30d9c1e00dee79c865f1d6e535485bb4a
em0nti <39101979+em0nti@users.noreply.github.com>
2023-09-25 14:14:48 +0300

    connect dotenv

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require("./app");
+const path = require('path');
+const configPath = path.join(__dirname, 'config', '.env');
+require('dotenv').config({ path: configPath });
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server running. Use our API on port: ${process.env.PORT}`.green.italic.bold
+  );
 });
