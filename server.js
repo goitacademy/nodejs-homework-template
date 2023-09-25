6c92a573fe0846390b4004246ded2d8d37638245
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-09-25 13:21:44 +0300

    uri DB-HOST to string

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require("./app");
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
-  .connect(DB_HOST)
+  .connect(`${DB_HOST}`)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Database connection successful");
