97e34000e580f8de7d3d3924449bdcb6df5a3a60
GOOSxE <der.goose.4308@gmail.com>
2023-09-26 21:29:05 +0200

    completed node-hw-03

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,17 +1,17 @@
-import contactsService from "../models/contacts/contacts.js";
+import Contact from "../models/Contact.js";
 // ? // Імпорт хелпера для створення помилки ;
 import { HttpError } from "../helpers/index.js";
 // ? // Імпорт врапера обгортки функцій в try/catch ;
-import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import { ctrlWrapper } from "../decorators/index.js";
 // ? // Контроллер обробки запиту на список всіх контактів ;
 const getAllContacts = async (req, res) => {
-  const result = await contactsService.getAllContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 // ? // Контроллер запиту на один контакт за айді ;
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsService.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw HttpError(404, `Contact with id: ${contactId} not found`);
   } else {
@@ -20,34 +20,36 @@ const getContactById = async (req, res) => {
 };
 // ? // Контроллер запиту додавання нового контакту з тілом запиту;
 const addContact = async (req, res) => {
-  const result = await contactsService.addContact(validateResult);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
-// ? // Контроллер запиту видалення існуючого контакту за айді ;
-const removeContactById = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contactsService.removeContactById(contactId);
-  if (!result) {
-    throw HttpError(404, `Contact with id: ${contactId} not found`);
-  } else {
-    res.status(200).json({ message: "Contact deleted successfully" });
-  }
-};
-// ? // Контроллер запиту оновлення існуючого контакту ;
+// ? // Контроллер запиту оновлення існуючого контакту за айді;
 const updateContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsService.updateContactById(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, `Contact with id: ${contactId} not found`);
   } else {
     res.json(result);
   }
 };
+// ? // Контроллер запиту видалення контакту за айді ;
+const removeContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+  if (!result) {
+    throw HttpError(404, `Contact with id: ${contactId} not found`);
+  } else {
+    res.status(200).json({ message: "Contact deleted successfully" });
+  }
+};
 
 export default {
   getAllContacts: ctrlWrapper(getAllContacts),
-  getContactById: ctrlWrapper(getContactById),
   addContact: ctrlWrapper(addContact),
-  removeContactById: ctrlWrapper(removeContactById),
+  getContactById: ctrlWrapper(getContactById),
   updateContactById: ctrlWrapper(updateContactById),
+  removeContactById: ctrlWrapper(removeContactById),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,26 +1,40 @@
 import express from "express";
 const contactsRouter = express.Router();
 // ? // імпорт мідлвари валідації переданого тіла контакту ;
-import contactValidation from "../../middleware/validation/contact-validation.js";
+import contactValidation from "../../middlewares/validation/contact-validation.js";
 // ? // Імпорт контроллеру взаємодії з контактами;
 import contactsController from "../../controllers/contacts-controller.js";
+
+import { isValidId } from "../../middlewares/validation/id-validation/index.js";
 // ? 1) Роутер запиту на список всіх контактів ;
 contactsRouter.get("/", contactsController.getAllContacts);
 // ? 2) Роутер запиту на один контакт за айді ;
-contactsRouter.get("/:contactId", contactsController.getContactById);
+contactsRouter.get("/:contactId", isValidId, contactsController.getContactById);
 // ? 3) Роутер додавання нового контакту з тілом запиту;
 contactsRouter.post(
   "/",
   contactValidation.addContactValidate,
   contactsController.addContact
 );
-// ? 4) Роутер запиту видалення існуючого контакту за айді ;
-contactsRouter.delete("/:contactId", contactsController.removeContactById);
-// ? 5) Роутер оновлення існуючого контакту ;
+// ? 4) Роутер оновлення існуючого контакту ;
 contactsRouter.put(
   "/:contactId",
-  contactValidation.addContactValidate,
+  isValidId,
+  contactValidation.updateContactById,
   contactsController.updateContactById
+);
+// ? 5) Роутер зміни статусу favorite контакту за айді ;
+contactsRouter.patch(
+  "/:contactId/favorite",
+  isValidId,
+  contactValidation.updateFavoriteFieldById,
+  contactsController.updateContactById
+);
+// ? 6) Роутер запиту видалення існуючого контакту за айді ;
+contactsRouter.delete(
+  "/:contactId",
+  isValidId,
+  contactsController.removeContactById
 );
 
 export default contactsRouter;
