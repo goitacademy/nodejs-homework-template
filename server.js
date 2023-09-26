37efd27d41d7ebf5c7bdc3a6c331004c8490f7a3
Inna Shkilna <shkinai.1995@gmail.com>
2023-09-26 19:09:07 +0300

    config

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const DB_HOST =
-  "mongodb+srv://Inna:kapitoshka2019@cluster0.guvsevh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 mongoose
