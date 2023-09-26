ef9760574f744d8bcb542f8c0e38ff696d09f892
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-09-26 08:30:45 +0200

    Mongoose methods, validation

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const app = require("./app");
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
-// const {DB_HOST} = require("./config");
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
-
 mongoose.set("strictQuery", true);
 
 mongoose
@@ -22,7 +16,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-
-// mongoose.connect(DB_HOST)
-// .then(() => console.log("Database connect success"))
-// .catch(error => console.log(error.message))
