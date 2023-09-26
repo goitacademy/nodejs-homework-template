5c8aba10ba5e2e392bc6128593722fb8b6c37cd9
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-09-26 18:45:27 +0200

    fix controler

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,79 +1,61 @@
 import contacts from "../models/contacts.js";
 import { HttpError } from "../helpers/HttpError.js";
 import Joi from "joi";
+import { ctrlWrapper } from "../helpers/ctrlWrapeer.js";
 
 const control = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
 });
-const getAll = async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contacts.getContactById(id);
-    console.log(result);
-    if (!result) {
-      throw HttpError(404, "Sorry. Not found.");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.getContactById(id);
+  console.log(result);
+  if (!result) {
+    throw HttpError(404, "Sorry. Not found.");
   }
+  res.json(result);
 };
 
-const postAddContact = async (req, res, next) => {
-  try {
-    const { error } = control.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+const postAddContact = async (req, res) => {
+  const { error } = control.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
 };
 
-const deleteById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contacts.removeContact(id);
-    if (!result) {
-      throw HttpError(404, "Sorry:) Not found.");
-    }
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Sorry:) Not found.");
   }
+  res.json({ message: "contact deleted" });
 };
-const putUpdateById = async (req, res, next) => {
-  try {
-    if (!req.body) {
-      throw HttpError(400, "missing fields");
-    }
-    const { id } = req.params;
-    const result = await contacts.updateContact(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Sorry. Not found.");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const putUpdateById = async (req, res) => {
+  if (!req.body) {
+    throw HttpError(400, "missing fields");
   }
+  const { id } = req.params;
+  const result = await contacts.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Sorry. Not found.");
+  }
+  res.json(result);
 };
 export default {
-  getAll,
-  getById,
-  postAddContact,
-  deleteById,
-  putUpdateById,
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  postAddContact: ctrlWrapper(postAddContact),
+  deleteById: ctrlWrapper(deleteById),
+  putUpdateById: ctrlWrapper(putUpdateById),
 };
--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,6 @@
 import HttpError from "./HttpError.js";
+import { ctrlWrapper } from "./ctrlWrapeer.js";
 export default {
   HttpError,
+  ctrlWrapper,
 };
