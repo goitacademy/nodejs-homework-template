082c795a9de4e337496cbf379c7122e76e7bf309
alexgoit <alexandrpuryaev@gmail.com>
2023-09-26 09:50:36 +0300

    fix

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,4 @@
 const HttpError = require("./HttpError");
-const ctrlWrapper = require("./ctrl.Wrapper");
+const ctrlWrapper = require("./ctrlWrapper");
 
 module.exports = { HttpError, ctrlWrapper };
