46ee1b5b74cb1fd04983144838984ace82de85b5
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-09-27 20:53:23 +0200

    done

---
+++ contacts/contacts.router.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { Router } = require("express");
+const {
+  listContactsHandler,
+  getContactByIdHandler,
+  removeContactHandler,
+  addContactHandler,
+  updateContactHandler,
+} = require("./contacts.controllers");
+
+const { contactValidationMiddleware } = require("./contacts.validators");
+
+const contactsRouter = Router();
+
+contactsRouter.get("/", listContactsHandler);
+
+contactsRouter.get("/:contactId", getContactByIdHandler);
+
+contactsRouter.post("/", contactValidationMiddleware, addContactHandler);
+
+contactsRouter.delete("/:contactId", removeContactHandler);
+
+contactsRouter.put(
+  "/:contactId",
+  contactValidationMiddleware,
+  updateContactHandler
+);
+
+module.exports = { contactsRouter };
