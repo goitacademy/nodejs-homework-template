b56cf5fbad0663494f6a21dfa254a2ab36a513c2
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-09-29 21:44:59 +0300

    connect mongoose

--- models/contacts.js
+++ models/contacts.js
@@ -1,70 +1,47 @@
-const fs = require('fs/promises')
-const { v4: uuidv4 } = require('uuid')
-const contactsPath = './models/contacts.json'
-const handleError = require('../helpers/handleError')
-
+const Contact = require('../shemas/contacts')
 
 const listContacts = async () => {
   return await handleError(async () => {
-    const data = await fs.readFile(contactsPath)
-    return JSON.parse(data)
+    const data = await Contact.find({})
+    return data
   })
 }
 
-const getContactById = async (contactId) => {
+const getContactById = async contactId => {
   return handleError(async () => {
-    const contacts = await listContacts()
-    const contact = contacts.find(({ id }) => id.toString() === contactId)
-    return contact
+    const contacts = await Contact.findOne({ _id: contactId })
+    // const contact = contacts.find(({ id }) => id.toString() === contactId)
+    return contacts
   })
 }
 
 const removeContact = async (contactId) => {
   return handleError(async () => {
-    const contacts = await listContacts()
-    const indexToRemove = contacts.findIndex((contact) => contact.id.toString() === contactId)
-
-    if (indexToRemove !== -1) {
-      contacts.splice(indexToRemove, 1)
-      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-      return true
-    } else {
-      return false
-    }
+    const contacts = await Contact.findByIdAndRemove({
+      _id: contactId,
+    })
+    // const indexToRemove = contacts.findIndex((contact) => contact.id.toString() === contactId)
+    return contacts
   })
 }
 
 const addContact = async (body) => {
   return handleError(async () => {
-    const contacts = await listContacts()
-
-    const newContact = {
-      id: uuidv4(),
-      ...body
-    }
-
-    contacts.push(newContact)
-    await fs.writeFile(contactsPath, JSON.stringify(contacts))
-
-    return newContact
+    const contacts = await Contact.create(body)
+    return contacts
   })
 }
 
 const updateContact = async (contactId, body) => {
   return await handleError(async () => {
-    const contacts = await listContacts();
-    const contactIndex = contacts.findIndex((contact) => contact.id === contactId);
-
-    if (contactIndex === -1) {
-      return null;
-    }
-
-    contacts[contactIndex] = { ...contacts[contactIndex], ...body };
-
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return contacts[contactIndex];
-  });
-};
+    const contact = await Contact.findByIdAndUpdate(
+      { _id: contactId },
+      { ...body },
+      { new: true }
+    )
+    return contact
+  })
+}
 
 module.exports = {
   listContacts,
