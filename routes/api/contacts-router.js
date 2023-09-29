f7111897a3a2b4f6ed47765f99849e0f72c459a8
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-29 20:58:00 +0200

    contacts-router

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -3,9 +3,19 @@ import express from "express";
 import * as contactsService from "../../models/contacts/index.js";
 
 import { HttpError } from "../../helpers/index.js";
+import Joi from "joi";
 
 const contactsRouter = express.Router();
 
+const contAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string().required(),
+});
+// працює
 contactsRouter.get("/", async (req, res, next) => {
   try {
     const result = await contactsService.listContacts();
@@ -14,7 +24,7 @@ contactsRouter.get("/", async (req, res, next) => {
     next(error);
   }
 });
-
+// працює
 contactsRouter.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -28,16 +38,51 @@ contactsRouter.get("/:id", async (req, res, next) => {
   }
 });
 
-// contactsRouter.post("/", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+contactsRouter.post("/", async (req, res, next) => {
+  try {
+    const { error } = contAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-// contactsRouter.delete("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+contactsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+    res.json({
+      message: "Delete success",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-// contactsRouter.put("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+contactsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = contAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = reg.params;
+
+    const result = await contactsService.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default contactsRouter;
