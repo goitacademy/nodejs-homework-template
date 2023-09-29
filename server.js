6a166aeb7287227ecf294340220f3cf3586e40d4
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-09-29 11:55:00 +0300

    connect db

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+// mm15ulNAnlgUY4f8\
+const DB_HOST = 'mongodb+srv://Nikita:mm15ulNAnlgUY4f8@cluster0.7u0vxxh.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=AtlasApp'
+
+mongoose.connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    }))
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1)
+  }) 
+
