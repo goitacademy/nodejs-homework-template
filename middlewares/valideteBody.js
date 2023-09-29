dda40ca70583de975aed2d1c1a8d1eaf25ad526b
liulka_s <asl.warmhaus@gmail.com>
2023-09-29 15:04:50 +0200

    fix message

--- middlewares/valideteBody.js
+++ middlewares/valideteBody.js
@@ -5,11 +5,29 @@ const validateBody = (schema) => {
       if (!Object.keys(req.body).length) {
        return next(HttpError(400, 'missing fields'));
       }
-      const {error} = schema.validate(req.body)
-      if (error) {
+    const {error} = schema.validate(req.body)
+    
+      if (error && Object.keys(req.body).length < 3) {
         const nameField = error.details[0].path[0];
       return next(HttpError(400,  `missing required ${nameField} field` ));
+    }
+    if (error && Object.keys(req.body).length === 3) {
+      const nameField = error.details[0].path[0];
+      let message;
+      switch (nameField) {
+        case "name":
+          message = "Invalid name field format. The name should only contain letters, spaces, hyphens, and apostrophes, and should not exceed 25 characters"
+          break
+        case "email":
+          message = "Invalid email format. Please enter a valid email address"
+          break
+        case "phone":
+          message = "Invalid phone field format. The phone number should be in the format (XXX) XXX-XXXX"
+          break
+        
       }
+      return next(HttpError(400,  `${message}` ))
+    }
       next()
     }
     return func
