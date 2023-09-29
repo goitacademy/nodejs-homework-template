e8957e522c18f6b5378a7f590324c6d4ce2a2827
alexgoit <alexandrpuryaev@gmail.com>
2023-09-29 14:41:24 +0300

    Added sort by favorite

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,13 +2,18 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite: reqFavorite = null } = req.query;
   const skip = (page - 1) * limit;
+  const favorite = reqFavorite === null ? { $exists: true } : reqFavorite;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
+  const result = await Contact.find(
+    { owner, favorite },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email");
 
   res.status(200).json(result);
 };
