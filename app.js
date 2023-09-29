51d469e745ce9caf159a6cbb1ffa442322b835aa
alexgoit <alexandrpuryaev@gmail.com>
2023-09-29 10:43:32 +0300

    refactoring

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRouter = require("./routes/api/auth");
-const contactsRouter = require("./routes/api/contacts");
+const { authRouter, contactsRouter } = require("./routes/api");
 
 const app = express();
 
