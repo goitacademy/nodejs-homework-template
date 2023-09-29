80a70220c6303e6525143396b2d25704001827a8
AlinaBielska <alinkabelka5@gmail.com>
2023-09-29 17:37:16 +0200

    added schema

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // const app = require('./app')
 const mongoose = require('mongoose');
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -13,24 +14,32 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
-})
+});
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
-})
+});
 
-module.exports = app
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000
+const uriDb = process.env.DB_HOST
 
 const connection = mongoose.connect(uriDb, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+connection
+.then(() => {
+  app.listen(PORT, function() {
+    console.log("Database connection successful")
+  })
+})
+.catch((err) =>
+console.log(`Server not running. Error message: ${err.message}`),
+);
+
+// module.exports = app
