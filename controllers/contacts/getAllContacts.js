9de5e751b85da51851004e7c4ca7bad7ab64dae0
alexgoit <alexandrpuryaev@gmail.com>
2023-09-29 12:32:45 +0300

    Added private route

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,7 +1,9 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
 
   res.status(200).json(result);
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,22 +3,35 @@ const router = express.Router();
 
 const { contacts: ctrl } = require("../../controllers");
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post(
+  "/",
+  authenticate,
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.add
+);
 
-router.delete("/:id", isValidId, ctrl.deleteById);
+router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
