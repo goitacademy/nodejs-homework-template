1788bd8b909026d09b414f14b8812606b0376543
alexgoit <alexandrpuryaev@gmail.com>
2023-09-29 14:12:48 +0300

    Update getCurrent.js

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,7 +1,7 @@
 const getCurrent = async (req, res) => {
-  const { email, name } = req.user;
+  const { email, subscription } = req.user;
 
-  res.status(200).json({ email, name });
+  res.status(200).json({ email, subscription });
 };
 
 module.exports = getCurrent;
