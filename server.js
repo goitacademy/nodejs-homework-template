aa8b2e06099e3257ee69f55fad8a439b9d589ff0
AlinaBielska <alinkabelka5@gmail.com>
2023-09-29 13:57:57 +0200

    moved app to server

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
-const app = require('./app')
+// const app = require('./app')
+const mongoose = require('mongoose');
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const contactsRouter = require('./routes/api/contacts')
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/contacts', contactsRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
+
+module.exports = app
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
