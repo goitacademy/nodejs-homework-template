7435460b2fb4d6ef97341b03b7d1c29c337e2838
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-09-30 15:25:24 +0300

    config db .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-const PORT = 4000;
+const { PORT = 4000, DB_HOST } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
