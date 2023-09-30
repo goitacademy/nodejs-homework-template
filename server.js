56c9d533a29958585eb5234cc04b73343bde0c76
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-09-30 15:36:02 +0300

    connect ok

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+import mongoose from "mongoose";
 import app from "./app.js";
+
 const { PORT = 4000, DB_HOST } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+// mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.massage);
+    process.exit(1);
+  });
