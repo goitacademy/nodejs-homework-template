3d13d36311887e6b7aab60c4d683b8c865ecdae1
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-09-30 18:03:18 +0300

    up

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -8,6 +8,9 @@ const multerConfig = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
+  limits: {
+    fileSize: 1048576,
+  },
 });
 
 const upload = multer({
