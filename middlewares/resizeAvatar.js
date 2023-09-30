153b44d80efaf03b255a61b94202625fa573755c
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-09-30 18:03:07 +0300

    up

--- middlewares/resizeAvatar.js
+++ middlewares/resizeAvatar.js
@@ -3,6 +3,7 @@ const fs = require("fs/promises");
 
 const resizeAvatar = async (req, res, next) => {
   const { path } = req.file;
+
   try {
     const image = await Jimp.read(path);
     image.resize(250, 250);
@@ -17,16 +18,3 @@ const resizeAvatar = async (req, res, next) => {
 };
 
 module.exports = resizeAvatar;
-
-//    try {
-//      const image = await Jimp.read(path);
-//      await image.resize(250, 250);
-//      await image.writeAsync(path);
-//      next();
-//    } catch (error) {
-//      error.status = 400;
-//      console.log(error);
-//      await fs.unlink(path);
-//      next(error);
-//    }
-//  };
