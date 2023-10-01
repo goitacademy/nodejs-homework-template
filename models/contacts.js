6c265ee6c9fe9d0a33d96b0e04a16129aa052ef9
fufushka <dimaukropdred@gmail.com>
2023-10-01 12:44:26 +0300

    added

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,67 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+export const contactsPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+export const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contactsList = JSON.parse(data);
 
-const removeContact = async (contactId) => {}
+    return contactsList;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+export const getContactById = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((c) => c.id === id);
+    return contact || null;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+export const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  const [result] = contacts.splice(index, 1);
+
+  await updateContacts(contacts);
+  return result;
+};
+
+export const addContact = async ({ name, email, phone }) => {
+  try {
+    const contacts = await listContacts();
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+
+    await updateContacts(contacts);
+
+    return newContact;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateContacts = async (contacts) => {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    throw error;
+  }
+};
