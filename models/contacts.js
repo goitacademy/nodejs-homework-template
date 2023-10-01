942632369ac3d399070913d3f5f6472137988666
Maciej Bartoszewski <bartoszewski.maciek@gmail.com>
2023-10-01 21:31:12 +0200

    init

--- models/contacts.js
+++ models/contacts.js
@@ -1,80 +1,78 @@
+/* eslint-disable */
 const fs = require("fs/promises");
 const path = require("node:path");
 const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
-let contactsCache = null;
-
-const loadContacts = async () => {
-  try {
-    const contacts = await fs.readFile(contactsPath, "utf-8");
-    contactsCache = JSON.parse(contacts);
-  } catch (error) {
-    throw new Error("Could not load contacts");
-  }
-};
-
-const saveContacts = async () => {
-  try {
-    await fs.writeFile(contactsPath, JSON.stringify(contactsCache));
-  } catch (error) {
-    throw new Error("Could not save contacts");
-  }
-};
 
 const listContacts = async () => {
-  if (!contactsCache) {
-    await loadContacts();
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    throw error;
   }
-  return contactsCache;
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  return contacts.find((contact) => contact.id === contactId) || null;
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index !== -1) {
-    contacts.splice(index, 1);
-    await saveContacts();
-    return true;
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index !== -1) {
+      contacts.splice(index, 1);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+      return true;
+    }
+    return false;
+  } catch (error) {
+    throw error;
   }
-  return false;
 };
 
 const addContact = async (body) => {
-  const contacts = await listContacts();
-  const { name, email, phone } = body;
-
-  if (!name || !email || !phone) {
-    throw new Error("Missing required name, email, or phone field");
+  try {
+    const contacts = await listContacts();
+    const { name, email, phone } = body;
+    if (!name || !email || !phone) {
+      throw new Error("Missing required name, email, or phone field");
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
+  } catch (error) {
+    throw error;
   }
-
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-
-  contacts.push(newContact);
-  await saveContacts();
-  return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    contacts[index] = { ...contacts[index], ...body };
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contacts[index];
+  } catch (error) {
+    throw error;
   }
-
-  contacts[index] = { ...contacts[index], ...body };
-  await saveContacts();
-  return contacts[index];
 };
 
 module.exports = {
