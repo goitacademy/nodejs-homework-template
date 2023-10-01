97a2936323b89e88865239eaa19dc8944999f5dc
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-10-01 10:14:43 +0200

    .

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,63 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = contacts => {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  return result || null;
+}
 
-module.exports = {
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  };
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result || null;
+}
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body
+  }
+  contacts.push(newContact);
+
+  await updateContacts(contacts);
+  return newContact;
+}
+
+const updateContact = async (contactId, { name, email, phone }) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id: contactId, name, email, phone };
+  await updateContacts(contacts);
+  return contacts[index];
+}
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
