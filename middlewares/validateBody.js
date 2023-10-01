68f594a997945b4e791b674b9b7137956ecebf31
Olga16119 <tkachenko93@ukr.net>
2023-10-01 20:16:57 +0200

    add validate

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,8 +3,12 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+
     if (Object.keys(req.body).length === 0) {
-      throw HttpError(400, "missing fields");
+      if (req.method === "PATCH") {
+        next(HttpError(400, "missing field favorite"));
+      }
+      next(HttpError(400, "missing fields"));
     }
     if (error) {
       next(HttpError(400, error.message));
