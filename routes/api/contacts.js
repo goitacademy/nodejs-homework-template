24205e2ec3bd677f3d52c31bc0899edf9fbb8d6c
andriy <evicandrij@gmail.com>
2023-10-02 20:09:23 +0300

    comit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,82 +1,17 @@
 const express = require('express')
-const Joi = require('joi')
-
-const contacts = require('../../models/contacts')
-const HttpError = require('../../helpers/index')
+const ctrl = require('../../controllers/contacts');
 
 const router = express.Router()
 
-const addShema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-})
-router.get('/', async (req, res, next) => {
-  const result  = await contacts.listContacts()
-  res.json(result)
-})
 
-router.get('/:contactId', async (req, res, next) => {
-try{
-  const {contactId} = req.params;
-  
-  const result  = await contacts.getContactById(contactId)
-  if(!result){
-    throw HttpError(404, 'not found contacts');
-  }
-  res.status(200).json(result);
-}
-  catch(error){
-    next(error)
-    }
-})
+router.get('/',ctrl.listContacts)
 
-router.post('/', async (req, res, next) => {
-try{
-  const {error} = addShema.validate(req.body)
-  if(error){
-    throw HttpError(400, error.message);
-  }
-  const result  = await contacts.addContact(req.body);
-  res.status(201).json(result)
-}
-catch(error){
-next(error)
-}
-})
+router.get('/:contactId',ctrl.getContactById)
 
-router.delete('/:contactId', async (req, res, next) => {
-try{
-  const {contactId} = req.params;
-  const result  = await contacts.removeContact(contactId);
-  if (!result) {
-     HttpError(404, "Not found");
-  }
-  res.status(200).json({
-    message: "contact deleted",
-  });
-}
-catch(error){
-next(error)
-}
-})
+router.post('/',ctrl.addContact)
 
-router.put('/:contactId', async (req, res, next) => {
-try{
-  const {error} = addShema.validate(req.body)
-  if(error){
-    throw HttpError(400, error.message);
-  }
-  const {contactId} = req.params;
-  const result  = await contacts.updateContact(contactId,req.body);
-  if (!result) {
-    HttpError(404, "Not found");
- }
-  res.status(200).json(result)
-}
-catch(error){
-next(error)
-}
-})
+router.delete('/:contactId',ctrl.removeContact)
+
+router.put('/:contactId',ctrl.updateContact)
 
 module.exports = router
