d569431ea72a314668b69da33c21a32c979311f6
Iryna Husieva <02guseva02@gmail.com>
2023-10-02 09:53:58 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,22 @@
 
 import express from 'express';
 import contactController from '../../controllers/contactController.js';
+import {isEmptyBody} from './../../middlewares/index.js';
+import { validateBody } from './../../decorators/index.js';
+import { contactsAddSchema } from './../../schemas/contacts-schemas.js';
 
-const router = express.Router();
+const contactAddValidate = validateBody(contactsAddSchema)
+
 const contactsRouter = express.Router();
-contactsRouter.get('/', contactController.getAllContacts)
 
-contactsRouter.get('/:id', contactController.getContactById)
+contactsRouter.get('/', contactController.getAllContacts);
 
-contactsRouter.post('/', contactController.addContact )
+contactsRouter.get('/:id', contactController.getContactById);
+
+contactsRouter.post('/', isEmptyBody, contactAddValidate, contactController.addContact)
 
 contactsRouter.delete('/:id', contactController.removeContact)
 
-contactsRouter.put('/:id', contactController.updateContact)
+contactsRouter.put('/:id', isEmptyBody, contactAddValidate, contactController.updateContact)
 
 export default contactsRouter;
