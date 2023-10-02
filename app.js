ad33250a2e21b836ce2f309045cc4a20c05da6bc
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-10-02 10:26:04 +0200

    commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import dotenv from "dotenv"; 
+
+dotenv.config(); 
+
 
 import contactsRouter from "./routes/api/contacts.js"; 
 
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import app from "./app.js";
+import mongoose from 'mongoose';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const DB_HOST = process.env.DB_HOST;
+
+// mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch(error => {
+    console.error('Database connection error:', error.message);
+    process.exit(1);
+  });
