f7407a8e38bc97fff9908e846c9fb816e2c11ea5
andriy <evicandrij@gmail.com>
2023-10-03 17:52:56 +0300

    comit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,14 +4,17 @@ const ctrl = require('../../controllers/contacts');
 const router = express.Router()
 
 
+const {validateBody}  = require('../../middlewares/validateBody');
+const  addSchema   = require('../../schemas/contacts');
+
 router.get('/',ctrl.listContacts)
 
 router.get('/:contactId',ctrl.getContactById)
 
-router.post('/',ctrl.addContact)
+router.post('/', validateBody(addSchema.addShema),ctrl.addContact)
 
 router.delete('/:contactId',ctrl.removeContact)
 
-router.put('/:contactId',ctrl.updateContact)
+router.put('/:contactId', validateBody(addSchema.addShema),ctrl.updateContact)
 
 module.exports = router
