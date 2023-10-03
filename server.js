7b8f7e247cae8871585ebac4fe49b676c8da2fbf
Svyntsov Andriy <svincov_a@ukr.net>
2023-10-03 03:32:51 +0300

    4

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import app from "./app.js";
 const {DB_HOST, PORT = 3000} = process.env;
 
 
-mongoose
-  .connect(DB_HOST)
+mongoose.connect(DB_HOST)
   .then(() =>
     app.listen(PORT, () => {
       console.log("Database connection successful");
