a6b67235a31f6ca546600bd512f3b68ce1047521
YuliyaKubar <113610848+YuliyaKubar@users.noreply.github.com>
2023-10-03 18:04:20 +0300

    test

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const { DB_HOST } = app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch((error) => console.log(error.message));
