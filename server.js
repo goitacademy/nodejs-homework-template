91c97a9bf56318af106454ffd54e96ece03a5da4
YuliyaKubar <113610848+YuliyaKubar@users.noreply.github.com>
2023-10-03 18:29:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const { DB_HOST } = app.listen(3000, () => {
+const DB_HOST =
+  "mongodb+srv://Yuliya:yJf8yT9hnarSMeJq@cluster0.dwyka8f.mongodb.net/db-contacts";
+app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connect success"))
-  .catch((error) => console.log(error.message));
+  .then(() => {
+    app.listen(300);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
