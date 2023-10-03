84b2806806980223ed1f27d5f9a29f8218cc4a43
Svyntsov Andriy <svincov_a@ukr.net>
2023-10-03 03:14:09 +0300

    done

--- server.js
+++ server.js
@@ -17,18 +17,3 @@ mongoose
     process.exit(1);
   });
 
-// const DB_HOST = "mongodb+srv://Andriy:2008A@cluster0.5nx8ibg.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=AtlasApp"
-
-// mongoose.connect(DB_HOST)
-// .then(() => {
-//   app.listen(3000, () => {
-//   console.log(`Server running. Use our API on port: ${PORT}`)
-// })
-// })
-// .catch(error => {
-//   console.log(error.message);
-//   process.exit(1);
-// })
-// 2008A
-
-
