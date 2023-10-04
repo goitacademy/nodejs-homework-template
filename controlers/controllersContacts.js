b67590ca8dbcec4cda424cb28a94e86547cb707c
VirLg <19virlg@gmail.com>
2023-10-04 14:16:56 +0300

    add

--- controlers/controllersContacts.js
+++ controlers/controllersContacts.js
@@ -1,25 +1,82 @@
+import Joi from 'joi';
+import { HttpError } from '../helpers/index.js';
 import * as contactServises from '../models/contacts.js';
-
+const contactAddShcema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 const getAll = async (req, res, next) => {
-  const result = res.json(await contactServises.listContacts(req, res, next));
+  try {
+    const result = await contactServises.listContacts();
 
-  return result;
+    if (!result) {
+      throw HttpError(404, "message: 'Not found'");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getById = async (req, res, next) => {
-  const result = res.json(await contactServises.getContactById(req, res, next));
-  return result;
+  try {
+    const result = await contactServises.getContactById(req, res, next);
+    if (!result) {
+      throw HttpError(404, "message: 'Not found'");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteById = async (req, res, next) => {
-  res.json(await contactServises.removeContact(req, res, next));
+  try {
+    const result = await contactServises.removeContact(req, res, next);
+    console.log('result', result);
+    if (!result) {
+      throw HttpError(404, `message: Not found `);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const add = async (req, res, next) => {
-  res.json(await contactServises.addContact(req, res, next));
+  try {
+    const createContact = await contactServises.addContact(req, res, next);
+    if (!Object.keys(req.body).length) throw HttpError(400, 'All fields empty');
+    const { error } = contactAddShcema.validate(req.body);
+    if (error) {
+      console.log('error', error.details[0].path);
+      throw HttpError(
+        404,
+        (error.message = `missing required ${error.details[0].path} field`)
+      );
+    }
+    res.status(201).json(createContact);
+  } catch (error) {
+    next(error);
+  }
 };
+
 export const put = async (req, res, next) => {
-  res.json(await contactServises.updateContact(req, res, next));
+  const { name, email, phone } = req.body;
+  const { contactId } = req.params;
+  const allContacts = await contactServises.listContacts();
+
+  const idxEl = allContacts.findIndex(el => el.id === contactId);
+  const { error } = contactAddShcema.validate(req.body);
+  try {
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    res.json(await contactServises.updateContact(req, res, next));
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default { getAll, getById, deleteById, add, put };
--- models/contacts.js
+++ models/contacts.js
@@ -2,86 +2,51 @@ import fs from 'fs/promises';
 import { nanoid } from 'nanoid';
 import path from 'path';
 import { HttpError } from '../helpers/index.js';
-import Joi from 'joi';
 
 const contactPath = path.resolve('models', 'contacts.json');
-const contactAddShcema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
 
-export const listContacts = async (req, res, next) => {
-  try {
-    const data = await fs.readFile(contactPath);
-    if (!data) {
-      throw HttpError(404, "message: 'Not found'");
-    }
-    return JSON.parse(data);
-  } catch (error) {
-    next(error);
-  }
+export const listContacts = async () => {
+  const data = await fs.readFile(contactPath);
+  return JSON.parse(data);
 };
+export const getContactById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const allContacts = await listContacts();
+  const resp = allContacts.find(el => el.id === contactId);
 
+  return resp;
+};
 export const removeContact = async (req, res, next) => {
   const { contactId } = req.params;
-  try {
-    const allContact = await listContacts();
-    const elIdx = allContact.findIndex(el => el.id === contactId);
-    const result = allContact[elIdx];
-    if (!result) {
-      throw HttpError(404, `message: Movie whith id ${contactId}  not found `);
-    }
-    const newArr = allContact.filter((el, idx) => idx !== elIdx);
-    await fs.writeFile(contactPath, JSON.stringify(newArr, null, 2));
-    return result;
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const getContactById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const allContacts = await listContacts();
-    const resp = allContacts.find(el => el.id === contactId);
-    if (!resp) {
-      throw HttpError(404, `message: Movie whith id ${contactId}  not found `);
-    }
-    return resp;
-  } catch (error) {
-    next(error);
-  }
+  console.log('contactId', contactId);
+  const allContact = await listContacts();
+  const elIdx = allContact.findIndex(el => el.id === contactId);
+  const newArr = allContact.filter((el, idx) => idx !== elIdx);
+  await fs.writeFile(contactPath, JSON.stringify(newArr, null, 2));
+  const result = allContact[elIdx];
+  return result;
 };
 
 export const addContact = async (req, res, next) => {
   const { name, email, phone } = req.body;
-  try {
-    if (!Object.keys(req.body).length) throw HttpError(400, 'All fields empty');
-    const { error } = contactAddShcema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const createContact = {
-      id: nanoid(),
-      name,
-      email,
-      phone,
-    };
-    const allContacts = await listContacts();
-    const newArr = [createContact, ...allContacts];
-    await fs.writeFile(contactPath, JSON.stringify(newArr, null, 2));
-    res.status(201).json(createContact);
-  } catch (error) {
-    next(error);
-  }
+  const createContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const allContacts = await listContacts();
+  const newArr = [createContact, ...allContacts];
+  await fs.writeFile(contactPath, JSON.stringify(newArr, null, 2));
+
+  return createContact;
 };
 
 export const updateContact = async (req, res, next) => {
-  const { name, email, phone } = req.body;
   const { contactId } = req.params;
   const allContacts = await listContacts();
   const idxEl = allContacts.findIndex(el => el.id === contactId);
+  const { name, email, phone } = req.body;
   const { error } = contactAddShcema.validate(req.body);
 
   try {
