47999c1e244f25242ac0f7eece9de50dc8d254cf
Alina Demchenko <demalina88@gmail.com>
2023-10-05 21:18:29 -0700

    mongo db

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ctrl =  require("../../controllers/contacts");
 const validateBody = require("../../middlewares/validateBody");
-const schemas = require("../../schemas/contacts");
+const schemas = require("../../schemas/contact");
 
 router.get("/", ctrl.listContacts);
 
