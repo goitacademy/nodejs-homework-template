68bc2d2f63dbf108c468e9b7ca0d05dac6b193a8
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-10-06 19:01:35 +0200

    refactor(models): delete files inside

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// require("dotenv").config();
-// const { PORT } = process.env;
+const dotenv = require("dotenv");
 
 const contactsRouter = require("./routes/api/contacts-router");
 
-const app = express();
+dotenv.config();
 
+const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
