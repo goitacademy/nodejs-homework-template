1fe34e1ff84a257a695d4428ecb24b8f0690f6ad
VirLg <19virlg@gmail.com>
2023-10-07 17:57:01 +0300

    add

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 import app from './app.js';
-
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+import mongoose from 'mongoose';
+const DB_HOST =
+  'mongodb+srv://Oleg:znIK9lYoMcrCiQSF@cluster0.ff9csdk.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=AtlasApp';
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
