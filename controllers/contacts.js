c2a30b2b312dd3a9452b1187c8c0594f8e0c0899
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-10-08 18:05:34 +0200

    fixed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -36,12 +36,14 @@ const add = async (req, res) => {
 }
 
 const updateById = async (req, res) => {
-        const {contactId } = req.params;
-        const result = await Contact.findByIdAndUpdate (contactId, req.body);
-        if (!result) {
-            throw HttpError(404, `Not found`);
-        }
-        res.status(200).json(result); 
+  const {contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });        
+      if (!result) {
+          throw HttpError(404, `Not found`);
+      }
+      res.status(200).json(result); 
 }
 
 
