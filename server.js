1a253d31db8220da780a37fe226d9747dff5fff8
andriy <evicandrij@gmail.com>
2023-10-08 13:21:15 +0300

    comit

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = require("./app");
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config();
-const { DB_HOST, PORT = 3000 } = process.env;
+
+const { DB_HOST, PORT = 300 } = process.env;
 mongoose.connect(DB_HOST).then(()=>
 app.listen(PORT, () => {
-   console.log("Server running. Use our API on port: 3000");
+   console.log("Server running. Use our API on port: 300");
 })
 ).catch(error =>{  
 console.log(error)
