d21722dbe7aa534ca4969dce4d1caf475a1fdf13
zemfira1 <craftyzema@gmail.com>
2023-10-08 16:51:29 -0400

    +

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = require("./app");
 const { error } = require("./schema");
 //PhHzLrFsBNB0Jy8f
 
-//const PORT = 5000;
+const PORT = 5000;
+const DB_HOST =
+  "mongodb+srv://Zemfira:PhHzLrFsBNB0Jy8f@cluster0.hxnvwh5.mongodb.net/my-contacts?retryWrites=true&w=majority";
 
 mongoose
-  .connect(process.env.DB_HOST)
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running. Use our API on port: ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
     console.log("Database connection successful");
   })
