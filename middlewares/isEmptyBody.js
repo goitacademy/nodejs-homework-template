2d156b444473d1f70e238b1c0c6aabf2beb6c4f1
Sergey Kuran <kuran.serg@gmail.com>
2023-10-08 23:22:17 +0300

    Add IsEmptyBodyFavorite funct.

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -1,8 +1,23 @@
 const { HttpError } = require("../middlewares/httpError");
+const {
+  validationFavorite,
+} = require("../utils/validation/contactValidationSchemas");
 
 const IsEmptyBody = (req, res, next) => {
   if (!Object.keys(req.body).length) {
-    return next(HttpError(400, "Missing fields"));
+    return next(HttpError(400, `Missing fields`));
+  }
+
+  next();
+};
+
+const IsEmptyBodyFavorite = (req, res, next) => {
+  const { error } = validationFavorite.validate(req.body);
+
+  if (error) {
+    return next(
+      HttpError(400, `Missing ${error.details[0].context.key} fields`)
+    );
   }
 
   next();
@@ -10,4 +25,5 @@ const IsEmptyBody = (req, res, next) => {
 
 module.exports = {
   IsEmptyBody,
+  IsEmptyBodyFavorite,
 };
