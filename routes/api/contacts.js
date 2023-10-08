b819b3f3571a4e4a450bf688da47bd0b92b989e0
fufushka <dimaukropdred@gmail.com>
2023-10-08 21:08:44 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,6 +9,7 @@ import express from "express";
 import HttpError from "../../heplers/index.js";
 import Joi from "joi";
 import Contact from "../../models/contactModel.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -94,6 +95,32 @@ router.put("/:contactId", async (req, res, next) => {
     }
 
     res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+
+    // Перевірте, чи `contactId` є валідним ObjectId
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(400).json({ message: "Invalid contactId" });
+    }
+
+    // Перевірте, чи контакт існує перед оновленням
+    const existingContact = await Contact.findById(contactId);
+    if (!existingContact) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    // Виконайте оновлення статусу контакту
+    existingContact.favorite = favorite;
+    await existingContact.save();
+
+    res.status(200).json(existingContact);
   } catch (error) {
     next(error);
   }
