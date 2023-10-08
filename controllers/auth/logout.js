80d934283e3a9a44c3a01cf6772d071feb91985e
yurchykyur <yurchyk.yur.s@gmail.com>
2023-10-08 22:25:56 +0300

    add .json();
    res.status(204).json();

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -5,7 +5,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(204);
+  res.status(204).json();
 };
 
 module.exports = { logout: ctrlWrapper(logout) };
