96aa4281887cc81d83e0e115d833b47684bbf5ac
zemfira1 <craftyzema@gmail.com>
2023-10-08 17:00:09 -0400

    romove DB_HOST

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const { error } = require("./schema");
 //PhHzLrFsBNB0Jy8f
 
 const PORT = 5000;
-const DB_HOST =
-  "mongodb+srv://Zemfira:PhHzLrFsBNB0Jy8f@cluster0.hxnvwh5.mongodb.net/my-contacts?retryWrites=true&w=majority";
 
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
