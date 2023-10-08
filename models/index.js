c4598b2c245e8ae69257d2813777bf539a20e56e
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-10-08 22:19:37 +0300

    remove oldest file

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-export * as contactsService from "./contacts.js";
+export * as contactsService from "../contacts old/contacts.js";
 export { default as ContactDB } from "./contact.js";
 export * from "./contact.js";
 export { default as UserDB } from "./user.js";
