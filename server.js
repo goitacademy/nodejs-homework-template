473c09bb1a61f94c5878535aa790798416fdb54b
andriy <evicandrij@gmail.com>
2023-10-08 13:13:33 +0300

    comit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.connect(DB_HOST).then(()=>
 app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: 3000");
+   console.log("Server running. Use our API on port: 3000");
 })
 ).catch(error =>{  
 console.log(error)
