14b0291b049efae34de1c41d9ed8ddff8a70ec14
Sergey Kuran <kuran.serg@gmail.com>
2023-10-08 16:29:25 +0300

    Edit code

--- services/services.js
+++ services/services.js
@@ -17,35 +17,19 @@ const addContact = async (body) => {
 };
 
 const updateContact = async (id, body) => {
-  const updateContact = await model.findByIdAndUpdate({ _id: id }, body, {
+  return await model.findByIdAndUpdate({ _id: id }, body, {
     new: true,
   });
-
-  if (!updateContact) {
-    throw new Error("This task does not exist");
-  }
-
-  return updateContact;
 };
 
 const removeContact = async (id) => {
-  const updateContact = await model.findByIdAndRemove({ _id: id });
-
-  if (!updateContact) {
-    throw new Error("This task does not exist");
-  }
-
-  return updateContact;
+  return await model.findByIdAndRemove({ _id: id });
 };
 
 const updateStatusContact = async (contactId, body) => {
-  const dataContacts = await model.findByIdAndUpdate({ _id: contactId }, body, {
+  return await model.findByIdAndUpdate({ _id: contactId }, body, {
     new: true,
   });
-
-  if (!dataContacts) throw new Error("Missing field favorite");
-
-  return dataContacts;
 };
 
 const serviceContact = {
