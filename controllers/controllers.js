55c3a1408bc67406670d103c5f50f2afee67014d
YuliyaKubar <113610848+YuliyaKubar@users.noreply.github.com>
2023-10-09 14:03:49 +0300

    update

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -34,7 +34,7 @@ const updateContact = async (req, res) => {
   res.json(result);
 };
 
-const updateFavorite = async (req, res) => {
+const updateStatusContact = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
@@ -59,6 +59,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   addContact: ctrlWrapper(addContact),
   updateContact: ctrlWrapper(updateContact),
-  updateFavorite: ctrlWrapper(updateFavorite),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
   deleteContact: ctrlWrapper(deleteContact),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 const ctrl = require("../../controllers/controllers");
-const { validateBody, isValidId } = require("../../middlewares");
+const isValidId = require("../../middlewares/isValidid");
+const validateBody = require("../../middlewares/validateBody");
 const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.getAllContacts);
@@ -21,8 +22,8 @@ router.put(
 router.patch(
   "/:id/favorite",
   isValidId,
-  validateBody(schemas.updateFavoriteSchema),
-  ctrl.updateFavorite
+  validateBody(schemas.updateStatusContact),
+  ctrl.updateStatusContact
 );
 
 router.delete("/:id", isValidId, ctrl.deleteContact);
