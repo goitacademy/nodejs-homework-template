9f806e538c7848080f02e98a1b78680e7f141a33
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-10-09 19:10:51 +0300

    first

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,92 @@
-// const fs = require('fs/promises')
+const fs = require('fs');
+const path = require('path');
 
-const listContacts = async () => {}
+const contactsPath = path.resolve(__dirname, 'contacts.json');
+const contactList = fs.readFileSync(contactsPath, 'utf-8');
+const contacts = JSON.parse(contactList);
 
-const getContactById = async (contactId) => {}
+const listContacts = () => {
+  console.log('List of contacts: ');
+  console.table(contacts);
 
-const removeContact = async (contactId) => {}
+  return contacts;
+};
 
-const addContact = async (body) => {}
+const getContactById = (contactId, errorMessage) => {
+  const foundContact = contacts.find((contact) => contact.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  if (!foundContact) {
+    throw new Error(errorMessage);
+  }
+
+  console.log(`Get contact by ID=${contactId}`);
+  console.table(foundContact);
+
+  return foundContact;
+};
+
+const removeContact = (contactId, errorMessage) => {
+  const newContacts = contacts.filter((contact) => contact.id !== contactId);
+
+  if (newContacts.length === contacts.length) {
+    console.log(errorMessage);
+    return newContacts;
+  }
+
+  console.log('Contact deleted successfully! New list of contacts : ');
+  console.table(newContacts);
+
+  fs.writeFile(contactsPath, JSON.stringify(newContacts), (error) => {
+    if (error) {
+      return console.log('error :', error);
+    }
+  });
+
+  return newContacts;
+};
+
+const addContact = (name, email, phone) => {
+  contacts.push({
+    id: contacts.length+1,
+    name: name,
+    email: email,
+    phone: phone,
+  });
+
+  console.log('Contacts added successfully! New list contacts : ');
+  console.table(contacts);
+
+  fs.writeFile(contactsPath, JSON.stringify(contacts), (error) => {
+    if (error) {
+      return console.log(error);
+    }
+  });
+  return contacts;
+};
+
+const updateContact = (contactId, name, email, phone, errorMessage) => {
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  if (!contact) {
+    console.log(errorMessage);
+    return;
+  }
+
+  contact.name = name;
+  contact.email = email;
+  contact.phone = phone;
+
+  console.log(`Contact with ID ${contactId} updated`);
+  console.table(contacts);
+
+  fs.writeFile(contactsPath, JSON.stringify(contacts), (error) => {
+    if (error) {
+      return console.log(error);
+    }
+  });
+
+  return contacts;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +94,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ routes/api/contacts.router.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const functionContacts = require('../../models/contacts.js');
+
+router.get('/', (req, res, next) => {
+  const contactList = functionContacts.listContacts();
+  res.status(200).json(contactList);
+});
+
+router.get('/:contactId', (req, res, next) => {
+  const contactId = req.params.contactId;
+
+  const foundContact = functionContacts.getContactById(
+    Number(contactId),
+    `Contact not found!`
+  );
+
+  if (!foundContact) {
+    return res.status(400).json({
+      message: `Contact not found!`,
+    });
+  }
+
+  res.status(200).json(foundContact);
+});
+
+router.post('/', (req, res, next) => {
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    return res.status(400).json({
+      message: 'Missing required name field',
+    });
+  }
+
+  const updateContact = functionContacts.addContact(name, email, phone);
+
+  res.status(201).json(updateContact);
+});
+
+router.delete('/:contactId', (req, res, next) => {
+  const contactId = req.params.contactId;
+
+  const newContacts = functionContacts.removeContact(
+    Number(contactId),
+    `Contact not found!`
+  );
+  if (!newContacts) {
+    return res.status(400).json({
+      message: `Contact not found!`,
+    });
+  }
+
+  const contactsList = functionContacts.listContacts();
+
+  if (newContacts.length === contactsList.length) {
+    return res.status(400).json({
+      message: `Contact not found!`,
+    });
+  }
+
+  res
+    .status(200)
+    .json({ message: `Contact with ID=${contactId} deleted successfully!` });
+});
+
+router.put('/:contactId', (req, res, next) => {
+  const contactId = req.params.contactId;
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    return res.status(400).json({
+      message: 'Missing required name field',
+    });
+  }
+
+  const updateContacts = functionContacts.updateContact(
+    Number(contactId),
+    name,
+    email,
+    phone,
+    `Contact not found!`
+  );
+
+  if (!updateContacts) {
+    return res.status(404).json({
+      message: `Contact not found!`,
+    });
+  }
+
+  res.status(200).json(updateContacts);
+});
+
+module.exports = router;
