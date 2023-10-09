d8e42d09edff370014c9cacedbfe2a7667af8b69
d1on1s1y <dionisiy.god.of.wine@gmail.com>
2023-10-09 10:51:37 +0300

    splitting into two files

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const fs = require("fs");
-
-const contactsRouter = require("./routes/api/contacts");
-
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -12,6 +9,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+const contactsRouter = require("./routes/api/contacts");
 
 app.use("/api/contacts", contactsRouter);
 
@@ -21,60 +20,6 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
-});
-
-const contactsFilePath = "modules/contacts.json";
-
-const loadContacts = () => {
-  try {
-    const data = fs.readFileSync(contactsFilePath, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
-    console.error("Помилка при читанні файла контактів:", error);
-    return [];
-  }
-};
-
-const contacts = loadContacts();
-
-app.get("/contacts", (req, res) => {
-  res.json(contacts);
-});
-
-app.post("/contacts", (req, res) => {
-  const newContact = req.body;
-  contacts.push(newContact);
-  res.status(201).json(newContact);
-});
-
-app.get("/contacts/:id", (req, res) => {
-  const contactId = req.params.id;
-  const contact = contacts.find((c) => c.id === contactId);
-  if (!contact) {
-    return res.status(404).json({ error: "Контакт не знайдений" });
-  }
-  res.json(contact);
-});
-
-app.put("/contacts/:id", (req, res) => {
-  const contactId = req.params.id;
-  const updatedContact = req.body;
-  const index = contacts.findIndex((c) => c.id === contactId);
-  if (index === -1) {
-    return res.status(404).json({ error: "Контакт не знайдений" });
-  }
-  contacts[index] = updatedContact;
-  res.json(updatedContact);
-});
-
-app.delete("/contacts/:id", (req, res) => {
-  const contactId = req.params.id;
-  const index = contacts.findIndex((c) => c.id === contactId);
-  if (index === -1) {
-    return res.status(404).json({ error: "Контакт не знайдений" });
-  }
-  contacts.splice(index, 1);
-  res.status(204).send();
 });
 
 const port = 3000;
---
+++ contacts.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+
+const contactsFilePath = "modules/contacts.json";
+
+const loadContacts = () => {
+  try {
+    const data = fs.readFileSync(contactsFilePath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Помилка при читанні файла контактів:", error);
+    return [];
+  }
+};
+
+const contacts = loadContacts();
+
+app.get("/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+app.post("/contacts", (req, res) => {
+  const newContact = req.body;
+  contacts.push(newContact);
+  res.status(201).json(newContact);
+});
+
+app.get("/contacts/:id", (req, res) => {
+  const contactId = req.params.id;
+  const contact = contacts.find((c) => c.id === contactId);
+  if (!contact) {
+    return res.status(404).json({ error: "Контакт не знайдений" });
+  }
+  res.json(contact);
+});
+
+app.put("/contacts/:id", (req, res) => {
+  const contactId = req.params.id;
+  const updatedContact = req.body;
+  const index = contacts.findIndex((c) => c.id === contactId);
+  if (index === -1) {
+    return res.status(404).json({ error: "Контакт не знайдений" });
+  }
+  contacts[index] = updatedContact;
+  res.json(updatedContact);
+});
+
+app.delete("/contacts/:id", (req, res) => {
+  const contactId = req.params.id;
+  const index = contacts.findIndex((c) => c.id === contactId);
+  if (index === -1) {
+    return res.status(404).json({ error: "Контакт не знайдений" });
+  }
+  contacts.splice(index, 1);
+  res.status(204).send();
+});
+module.exports = router;
