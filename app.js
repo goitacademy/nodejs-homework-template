78063ac9e9af7f5c6036acfcb3fc000a42b38ebc
KseniyaPin <kontr.mat@gmail.com>
2023-10-10 09:08:54 +0200

    init mongoose

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+// //  hw3------
+// const mongoose = require('mongoose');
+// const DB_HOST = "mongodb+srv://Kseniya:Mo25101981@cluster0.jihsow2.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// // mongoose.set("strictQuery", trye);
+
+// mongoose.connect(DB_HOST)
+// .then(() => console.log("Database connection successful"))
+// .catch(error => console.log(error.message));
+
+
+// // --------hw3
+
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+//  hw3------
+const mongoose = require('mongoose');
+const DB_HOST = "mongodb+srv://Kseniya:Mo25101981@cluster0.jihsow2.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose.set("strictQuery", trye);
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000, () => {
+    console.log("Database connection successful")
+  })
 })
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
+
+
+// --------hw3
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
