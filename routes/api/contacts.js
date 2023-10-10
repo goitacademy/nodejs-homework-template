c16ef72af04c49a1a9d627c4e12ae48f2f0d4a9d
KolBa228 <technotrashinfo@gmail.com>
2023-10-10 16:49:44 +0100

    done decorator

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,87 +2,16 @@ const express = require('express')
 
 const router = express.Router()
 
-const Joi = require('joi')
+const ctrl = require('../../controllers/contacts')
 
-const contacts = require('../../models/contacts')
+router.get('/', ctrl.getAll)
 
-const HttpErr = require('../../helpers/HttpErr')
+router.get('/:contactId', ctrl.getById)
 
-const addScheme = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required()
-})
+router.post('/', ctrl.addContact)
 
-router.get('/', async (req, res, next) => {
-  try {
-    const response = await contacts.listContacts();
-    res.json(response)
-  } catch (error) {
-    res.status(500).json({
-      message: 'server error'
-    })
-  }
+router.delete('/:contactId', ctrl.removeContact)
 
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const {contactId} = req.params
-    const response = await contacts.getContactById(contactId)
-    if (!response) {
-      throw HttpErr(404, 'Not Foun')
-    }
-    res.json(response)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addScheme.validate(req.body)
-    if (error) {
-      throw HttpErr(400, error.message)
-    }
-    const response = await contacts.addContact(req.body);
-    res.status(201).json(response);
-
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { id } = res.params;
-    const response = await contacts.removeContact(id)
-    if (!response) {
-      throw HttpErr(404, 'Not Foun')
-    }
-    res.status({
-      message: 'Гей чистка успішно виконана'
-    })
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = addScheme.validate(req.body)
-    if (error) {
-      throw HttpErr(400, error.message)
-    }
-    const { id } = res.params;
-    const response = await contacts.updateContact(id, req.body)
-    if (!response) {
-      throw HttpErr(404, 'Not Foun')
-    }
-    res.json(response);
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:contactId', ctrl.updateContact)
 
 module.exports = router
