83a30fb5b31a7f1b2133249e68afeebb5b43b18a
Liu3oIT <liutyivova995@gmail.com>
2023-10-10 23:16:20 +0300

    done  task

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,69 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
+const requestError = require("../../helpers/requestError");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  try {
+    const resolve = await contacts.listContacts();
+    return res.status(200).json(resolve);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const resolve = await contacts.getContactById(contactId);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!resolve) {
+      throw requestError(400);
+    }
+    return res.status(200).json(resolve);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const resolve = await contacts.addContact(name, email, phone);
+    if (!resolve) {
+      throw requestError(400);
+    }
+    return res.status(201).json(resolve);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const resolve = await contacts.removeContact(contactId);
+    if (!resolve) {
+      throw requestError(400);
+    }
+    return res.status(204).json(resolve);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, email, phone } = req.body;
+    const resolve = await contacts.updateContact(id, name, email, phone);
+    if (!resolve) {
+      throw requestError(404);
+    }
+    res.status(200).json(resolve);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
