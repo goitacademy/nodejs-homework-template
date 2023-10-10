2a19b1a58c33b1fe9b9dd4797397bbb1a40246fc
Marstepan12 <121106022+Marstepan12@users.noreply.github.com>
2023-10-10 21:45:54 +0300

    fix hw-02

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,11 +1,18 @@
 const { HttpError, changeOutputMessage } = require("../helpers");
 
-
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, changeOutputMessage(error.message)));
+      const requiredFields = Object.keys(schema.describe().keys);
+      const missingFields = requiredFields.filter((field) => !req.body[field]);
+
+      if (missingFields.length === 1) {
+        const fieldName = missingFields[0];
+        next(HttpError(400, changeOutputMessage(`missing required ${fieldName} field`)));
+      } else {
+        next(HttpError(400, changeOutputMessage("missing fields")));
+      }
     }
     next();
   };
