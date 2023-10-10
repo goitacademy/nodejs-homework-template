0321023f32acb523606942aa99089d3d30519566
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2023-10-11 01:50:56 +0300

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -9,25 +9,18 @@ const authenticate = async (req, res, next) => {
   const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") {
-    console.log("❌ Error auth 1");
     next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-    console.log("ID from token: ", id, "\nToken: ", token);
-
     const user = await User.findById(id);
 
-    console.log("USER bio: ", user);
-
     if (!user || !user.token || user.token !== token) {
-      console.log("❌ Error auth 2");
       next(HttpError(401));
     }
     req.user = user;
     next();
   } catch (error) {
-    console.log("❌ Error auth 3: ", error.message);
     next(HttpError(401));
   }
 };
