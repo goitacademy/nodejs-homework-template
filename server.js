2abacb936489886eef251eb60f63578b5f4902ad
KseniyaPin <kontr.mat@gmail.com>
2023-10-11 22:43:36 +0200

    server-3000

--- server.js
+++ server.js
@@ -8,15 +8,23 @@ mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
 .then(() => {
-  app.listen(3000, () => {
-    console.log("Database connection successful")
-  })
+  app.listen(3000)
 })
 .catch(error => {
   console.log(error.message);
   process.exit(1);
 });
 
+// mongoose.connect(DB_HOST)
+// .then(() => {
+//   app.listen(3000, () => {
+//     console.log("Database connection successful")
+//   })
+// })
+// .catch(error => {
+//   console.log(error.message);
+//   process.exit(1);
+// });
 
 // --------hw3
 
