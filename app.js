2fbaf37307a3ed6b9e3cda02d053a5f753018906
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-10-11 14:36:39 +0300

    12 tmp

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import multer from "multer";
+import path from "path";
 import "dotenv/config";
 import contactsRouter from "./routes/api/contacts-router.js";
 import authRouter from "./routes/api/auth-router.js";
@@ -13,7 +15,23 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+// multer
+const tempDir = path.resolve("temp");
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+export const upload = multer({ storage: multerConfig });
+
 app.use("/api/contacts", contactsRouter);
+
+// app.patch("/users/avatars", upload.single("avatarURL"), async (req, res) => {
+//   console.log("req.file :>> ", req.file);
+//   console.log("req.file :>> ", req.file);
+//   res.status(401).json({ message: `File have saved in ${req.file.path}` });
+// });
 
 app.use("/users", authRouter);
 
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -4,6 +4,9 @@ import authCtrl from "../../controllers/auth-controller.js";
 import { authSchema, subscriptionSchema } from "../../models/index.js";
 import { validateBody } from "../../decorators/index.js";
 import { authenticate } from "../../middlewares/index.js";
+//
+import multer from "multer";
+import { upload } from "../../app.js";
 
 const router = express.Router();
 
@@ -16,5 +19,11 @@ router.post("/login", validateBody(authSchema), authCtrl.login);
 router.get("/current", authenticate, authCtrl.getCurrent);
 
 router.post("/logout", authenticate, authCtrl.logout);
+
+// router.patch("/avatars", authenticate, upload.single("avatarURL"), async (req, res) => {
+//   console.log("req.file :>> ", req.file);
+//   console.log("req.file :>> ", req.file);
+//   res.status(401).json({ message: `File have saved in ${req.file.path}` });
+// });
 
 export default router;
