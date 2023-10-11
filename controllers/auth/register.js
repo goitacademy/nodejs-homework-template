5e4480e00600beac4bb1445ca6d1af0587590308
Filip-Kuta <114823329+Filip-Kuta@users.noreply.github.com>
2023-10-11 10:00:46 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -20,4 +20,4 @@ const register = async (req, res) => {
 
 module.exports = register;
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZTRlZTMxNWY5ODVhOWMzMmQzNzg0YyIsImlhdCI6MTY3NTk0NzU3OSwiZXhwIjoxNjc2MDMzOTc5fQ.goSnd0Y8aK24V6cp2wiy_pZymVKLtarebmFLtDi67bI;
+
