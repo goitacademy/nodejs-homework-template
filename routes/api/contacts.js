4ea35b7754ae014460d35f454d485508c05a2177
Liu3oIT <liutyivova995@gmail.com>
2023-10-11 23:24:29 +0300

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,6 +3,7 @@ const contacts = require("../../models/contacts");
 const requestError = require("../../helpers/requestError");
 const router = express.Router();
 const contactsController = require('../../controllers/contacts')
+
 router.get("/", contactsController.listContacts);
 
 router.get("/:contactId",contactsController.getContactById);
