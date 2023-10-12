4228ccdbfffd22451fff7c9c816304e1d92a9ca5
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-10-12 20:36:30 +0200

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,56 +1,19 @@
+// routes/api/contacts.js
+
 const express = require("express");
 const router = express.Router();
 const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../api/contacts");
+  getContacts,
+  getContact,
+  createContact,
+  updateContactById,
+  deleteContact,
+} = require("../../Controllers/contactsController");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.status(200).json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const contact = await getContactById(req.params.contactId);
-    res.status(200).json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    await removeContact(req.params.contactId);
-    res.status(200).json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const updatedContact = await updateContact(req.params.contactId, req.body);
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", getContacts);
+router.get("/:contactId", getContact);
+router.post("/", createContact);
+router.put("/:contactId", updateContactById);
+router.delete("/:contactId", deleteContact);
 
 module.exports = router;
