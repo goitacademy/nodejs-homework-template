dcd292fd883b9842a57682ff94f0b5657a121bde
VadymKrem <122228240+VadymKrem@users.noreply.github.com>
2023-10-12 23:32:42 +0300

    try

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+import mongoose from "mongoose";
+import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connection successful`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
