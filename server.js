96483c508f5d95c61857133ec403baccec8f87ad
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-10-13 19:27:05 +0300

    hw-3

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { PORT = 3000 } = process.env;
+const  DB_HOST = "mongodb+srv://Bohdan_shveda:q5Eu1Iz68JOSIfib@cluster0.l2rijaf.mongodb.net/db_contacts?retryWrites=true&w=majority"
+mongoose.set("strictQuery", true);
+
+const app = require("./app");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
