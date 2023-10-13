0de2d4077255e4be4da54c6e0b74da0f14024a63
KseniyaPin <kontr.mat@gmail.com>
2023-10-13 17:04:38 +0200

    basa no colection

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const app = require('./app')
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
-
-
 // hw3 ----
 // bR5ojK7bFN88iDUW
 const mongoose = require("mongoose");
 
-const {DB_HOST} = require('./config.js');
+const DB_HOST = "mongodb+srv://Oksana:bR5ojK7bFN88iDUW@cluster0.oglybn2.mongodb.net/contacts?retryWrites=true&w=majority";
 
 mongoose.set('strictQuery', true);
 
