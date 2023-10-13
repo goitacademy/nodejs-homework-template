649ef67700263e4b268d77d01fd35ae6ac5e5cd2
KseniyaPin <kontr.mat@gmail.com>
2023-10-13 18:16:19 +0200

    mongoose-true

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const app = require('./app');
 const mongoose = require('mongoose');
 //  U1XmmpMOEwO9HXnj
 
-const DB_HOST = "mongodb+srv://Oksana:U1XmmpMOEwO9HXnj@cluster0.oglybn2.mongodb.net/contacts-reader?retryWrites=true&w=majority";
+const DB_HOST = "mongodb+srv://Oksana:U1XmmpMOEwO9HXnj@cluster0.oglybn2.mongodb.net/contacts?retryWrites=true&w=majority";
 
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
 .then(() => {
   app.listen(3000, () => {
     console.log("Database connection successful")
   })
-  
 })
 .catch(error => {
   console.log(error.message);
