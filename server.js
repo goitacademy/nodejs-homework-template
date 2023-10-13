a1ce5b70064d2baac3aef54f199820046b682ba5
KseniyaPin <kontr.mat@gmail.com>
2023-10-13 13:25:37 +0200

    init-hw-3

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
+
+// hw3 ----
+// bR5ojK7bFN88iDUW
+const mongoose = require("mongoose");
+
+const DB_HOST = "mongodb+srv://Oksana:bR5ojK7bFN88iDUW@cluster0.oglybn2.mongodb.net/contacts-reader?retryWrites=true&w=majority";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000)
 })
+.catch(error => {
+  console.log(error.message)
+  process.exit(1);
+});
+
+
+
+// --------hw3
