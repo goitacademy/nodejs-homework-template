0b4ec2fa2f4dcc266de2210989604b95ac0af78b
KseniyaPin <kontr.mat@gmail.com>
2023-10-13 22:59:30 +0200

    example

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const app = require('./app');
-
 const mongoose = require('mongoose');
+
+// const app = require('./app');
+
 //  U1XmmpMOEwO9HXnj
 
 const DB_HOST = "mongodb+srv://Oksana:U1XmmpMOEwO9HXnj@cluster0.oglybn2.mongodb.net/contacts-reader?retryWrites=true&w=majority";
 
-// mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
-.then(() => {
-  app.listen(3000, () => {
-    console.log("Database connection successful")
-  })
-})
+.then(() => 
+// app.listen(3000)
+console.log('Alles good')
+)
 .catch(error => {
   console.log(error.message);
   process.exit(1)
