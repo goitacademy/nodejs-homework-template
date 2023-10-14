15d8c21733a6537e2155907c02a6bed993e0c512
KseniyaPin <kontr.mat@gmail.com>
2023-10-14 14:52:13 +0200

    next change

--- models/contacts.js
+++ models/contacts.js
@@ -1,30 +1,6 @@
 const fs = require('fs/promises');
 const {nanoid} = require("nanoid");
 
-// hw3------
-const { Schema, model } = require('mongoose');
-
-const contactsSchema = new Schema ({
-    name: {
-      type: String,
-      required: [true, 'Set name for contact'],
-    },
-    email: {
-      type: String,
-    },
-    phone: {
-      type: String,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-  })
-
-  const Contact = model('contact', contactsSchema)
-
-// ------- hw3
-
 const path = require("path");
 // const contactsPath = path.join(__dirname, "./contacts.json");
 const contactsPath = path.join(__dirname, "./contacts.json");
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 
 const app = require("./app.js");
 
 //  U1XmmpMOEwO9HXnj
 
-const DB_HOST = process.env.DB_HOST;
+const { DB_HOST } = require('./config.js');
+
+
+mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
 .then(() => 
