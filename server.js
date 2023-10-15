282df9ffb75cb695ec25135d3312890ecee566b4
Anna Samoilova <samoilova0299@gmail.com>
2023-10-15 18:20:03 +0300

    primary commit

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
+const DB_HOST =
+  "mongodb+srv://Anna:Venito65@cluster0.r2pzy0u.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose.set("strictQuery", true);
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
