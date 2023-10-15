6988055edc0655e30e9d75bab307765193117089
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-10-14 21:40:28 -0400

    created new branch

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+import mongoose from "mongoose";
 const app = require("./app");
-
+//7kSrOFNF4IuRtgCP
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
+uitgiug 
