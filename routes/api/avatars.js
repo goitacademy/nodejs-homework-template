96af12e34250b91b6b54363689187650b338b6d4
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-10-15 23:54:51 +0200

    fix

--- routes/api/avatars.js
+++ routes/api/avatars.js
@@ -1,45 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const jimp = require("jimp");
-const path = require("path");
+const upload = require("../../multerConfig");
+const { uploadAvatar } = require("../../Controllers/avatarController");
+const authMiddleware = require("../../Users/authMiddleware");
 
-const tmpFolder = path.join(__dirname, "/project-root/tmp");
-
-const upload = multer({
-  dest: tmpFolder,
-  limits: {
-    fileSize: 1000000,
-  },
-});
-
-router.patch("/", upload.single("avatar"), async (req, res, next) => {
-  try {
-    const { file } = req;
-
-    if (!file) {
-      return res.status(400).json({ message: "File not provided" });
-    }
-
-    const avatar = await jimp.read(file.path);
-    avatar.resize(250, 250).write(file.path);
-
-    const newFileName = `${Date.now()}-${file.originalname}`;
-    const avatarPath = path.join(
-      __dirname,
-      "../../public/avatars",
-      newFileName
-    );
-
-    await fs.rename(file.path, avatarPath);
-
-    req.user.avatarURL = `/avatars/${newFileName}`;
-    await req.user.save();
-
-    res.status(200).json({ avatarURL: req.user.avatarURL });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch("/", authMiddleware, upload.single("avatar"), uploadAvatar);
 
 module.exports = router;
