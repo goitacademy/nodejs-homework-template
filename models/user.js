0059e51386e3835a833e89a249006a73bb0ce272
Filip-Kuta <114823329+Filip-Kuta@users.noreply.github.com>
2023-10-16 13:22:20 +0200

    new

--- models/user.js
+++ models/user.js
@@ -25,6 +25,10 @@ const userSchema = new Schema(
       type: String,
       default: "",
     },
+    avatarUrl: {
+      type: String,
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true }
 );
