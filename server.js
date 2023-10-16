ca048e85684688995c7c24dec91d2827c1558d97
KseniyaPin <kontr.mat@gmail.com>
2023-10-16 07:46:52 +0200

    fix error const

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config({ path: 'ENV_FILENAME' });
+
 const app = require("./app.js");
 
 //  U1XmmpMOEwO9HXnj
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set('strictQuery', true);
 
@@ -16,4 +18,4 @@ mongoose.connect(DB_HOST)
 .catch(error => {
   console.log(error.message);
   process.exit(1)
-})
+});
