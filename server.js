2b0dd8493c120417914c21a50eebcffda9cb9883
AlinaBielska <alinkabelka5@gmail.com>
2023-10-17 21:20:57 +0200

    poprawki mentora 1

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/api', router)
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Not found' })
+app.use((_, res) => {
+  res.status(404).json({ message: 'Not found' });
 });
 
-app.use((req, res, next) => {
-  res.status(500).json({ message: 'Internal server error' })
+app.use((err, _, res, __) => {
+  res.status(500).json({ message: err.message });
 });
 
 const PORT = process.env.PORT || 3000
