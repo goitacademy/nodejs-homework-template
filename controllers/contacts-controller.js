f464e6c8ce37c8b34e722391b7ca55d74cd47f83
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-10-17 13:52:18 +0300

    added pagination and filter by favorite

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,14 +4,32 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
-  res.json(result);
+  const { id: owner } = req.user;
+
+  const { page = 1, limit = 20, favorite = false } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner })
+    .skip(skip)
+    .limit(limit)
+    .populate("owner", "email");
+
+  if (favorite === "true") {
+    const filteredContacts = result.filter(
+      (contact) => contact.favorite === true
+    );
+    res.json(filteredContacts);
+  } else {
+    res.json(result);
+  }
 };
 
 const getById = async (req, res) => {
+  const { id: owner } = req.user;
   const { contactId } = req.params;
 
-  const result = await Contact.findById(contactId);
+  const result = await Contact.findOne({ _id: contactId, owner });
 
   if (!result) {
     throw HttpError(404);
@@ -21,13 +39,19 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
 const updateById = async (req, res) => {
+  const { id: owner } = req.user;
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner },
+    req.body
+  );
 
   if (!result) {
     throw HttpError(404);
@@ -37,8 +61,13 @@ const updateById = async (req, res) => {
 };
 
 const updateStatusContact = async (req, res) => {
+  const { id: owner } = req.user;
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner },
+    req.body
+  );
 
   if (!result) {
     throw HttpError(404);
@@ -48,8 +77,10 @@ const updateStatusContact = async (req, res) => {
 };
 
 const deleteById = async (req, res) => {
+  const { id: owner } = req.user;
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndDelete(contactId);
+
+  const result = await Contact.findOneAndDelete({ _id: contactId, owner });
 
   if (!result) {
     throw HttpError(404);
