6d1ca24f2cf9ce3ff81caa29c4ec62fe6151a9ac
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-10-17 20:47:46 +0200

    config + mail test
    all working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+
 require("./config/passport");
 
 const contactsRouter = require("./routes/contacts.routes");
