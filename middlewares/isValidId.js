ca62d55ce60b8acd6524467380b3da9cc29176d2
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-10-17 13:52:52 +0300

    refactored

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,11 +1,25 @@
 import { isValidObjectId } from "mongoose";
 import { HttpError } from "../helpers/index.js";
 
-const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
+// const isValidId = (req, res, next) => {
+//   const { contactId } = req.params;
 
-  if (!isValidObjectId(contactId)) {
-    return next(HttpError(404, `${contactId} is not valid id`));
+//   if (!isValidObjectId(contactId)) {
+//     return next(HttpError(404, `${contactId} is not valid id`));
+//   }
+
+//   next();
+// };
+
+const isValidId = (req, res, next) => {
+  const idParams = Object.keys(req.params);
+
+  for (const param of idParams) {
+    const id = req.params[param];
+
+    if (!isValidObjectId(id)) {
+      return next(HttpError(404, `${id} is not a valid id`));
+    }
   }
 
   next();
