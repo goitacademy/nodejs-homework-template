fd03cb71f933e388a67de718e99c17c2f2ea6edb
OlgaBilynska <olgabilinskaya@gmail.com>
2023-10-17 21:26:28 +0300

    problem in launching the server

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const app = require("./app.js");
+// nB9_3mnLHNb2Maf
+
+const DB_HOST =
+  "mongodb+srv://Olga:nB9_3mnLHNb2Maf@cluster0.bp8q1bj.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
