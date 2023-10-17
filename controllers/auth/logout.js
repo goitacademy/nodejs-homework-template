15f0149463eaae9e7da895c74ab98062d7fb5dae
Filip-Kuta <114823329+Filip-Kuta@users.noreply.github.com>
2023-10-17 19:02:06 +0200

    new

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,11 +1,15 @@
 const { User } = require("../../models/user");
 
 const logout = async (req, res) => {
-  const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: "" });
-  res.json({
-    message: "Logout success",
-  });
+  try {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { token: "" });
+    res.json({
+      message: "Logout success",
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 module.exports = logout;
--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,12 +1,17 @@
 const { Contact } = require("../../models/contacts");
 
+
 const getAll = async (req, res) => {
-  const { _id: owner } = req.user;
-  const result = await Contact.find(
-    { owner },
-    "-createdAt -updatedAt"
-  ).populate("owner", "email subscription");
-  res.json(result);
+  try {
+    const { _id: owner } = req.user;
+    const result = await Contact.find(
+      { owner },
+      "-createdAt -updatedAt"
+    ).populate("owner", "email subscription");
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 module.exports = getAll;
