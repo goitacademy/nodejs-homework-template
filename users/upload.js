f55e65193cf3e9859dec51b9620ec14491ed4f0c
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-10-22 12:52:26 +0200

    Update upload.js

--- users/upload.js
+++ users/upload.js
@@ -14,11 +14,4 @@ const upload = multer({
   dest: path.join(__dirname, "tmp"),
 });
 
-// const upload = multer({
-//   // Odkomentuj, zeby zapisywać pliki na dysku
-//   // dest: path.join(__dirname, 'tmp'),
-//   limits: {
-//       fieldSize: 1048576
-//   }
-// });
 module.exports = upload;
