3c0f92081f23b81032e25331d0b5a724a6fff68c
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-10-24 14:36:14 +0200

    Fix ESLint issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["standard", "prettier"],
+  extends: ["eslint:recommended"],
+
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
