3a0cce4871241c655b5ad660923d6d1ecb752646
KolBa228 <technotrashinfo@gmail.com>
2023-10-24 19:50:44 +0100

    login fixing

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,10 @@ const login = async (req, res) => {
         id: user._id,
     };
 
-    const token = jwt.sign(payload, TOKEN_KEY, {expiresIn: '23h'})
+    const token = jwt.sign(payload, TOKEN_KEY, { expiresIn: '23h' })
+
+    user.token = token;
+    await user.save();
 
     res.json({
         token: token
