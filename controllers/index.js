1d4c34d8328fc48f35270d4fd8f3156de042b13e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 22:03:22 +0300

    Update index.js

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const {
+  listContactsController,
+  getContactByIdController,
+  addContactController,
+  removeContactController,
+  updateContactController,
+} = require("./contacts-controller");
+
+module.exports = {
+  listContactsController,
+  getContactByIdController,
+  addContactController,
+  removeContactController,
+  updateContactController,
+};
