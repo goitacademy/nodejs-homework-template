44a1fc73b1cc675b74f9048bc35e233f260ad053
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 21:17:52 +0300

    Update isEmptyBody.js

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -2,7 +2,7 @@ const { HttpError } = require("../helpers");
 
 const isEmptyBody = (req, res, next) => {
   if (!Object.keys(req.body).length) {
-    throw HttpError(400, "All fields empty");
+    return next(HttpError(400, "All fields empty"));
   }
 };
 
