b21608430c7423b806b056013028868d75eaf2dd
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 18:50:29 +0300

    Update contacts-router.js

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -42,6 +42,7 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
+    const validateResponse = movieAddSchema.validate(req.body);
     const result = await addContact(req.body);
     res.status(201).json(result);
   } catch (error) {
