07fa055d03ee5f29d7612b0e31be5615957ede7c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 21:38:43 +0300

    Update controllerWrapper.js

--- decorators/controllerWrapper.js
+++ decorators/controllerWrapper.js
@@ -1,7 +1,7 @@
 const controllerWrapper = (controller) => {
   const func = async (req, res, next) => {
     try {
-      await controller(req, res);
+      await controller(req, res, next);
     } catch (error) {
       next(error);
     }
