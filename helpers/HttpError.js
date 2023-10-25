ff8827d5aabcc1efe988753a3ecfdeb35d4b4bba
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 15:55:25 +0300

    Update HttpError.js

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,3 +1,11 @@
+const messageList = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+};
+
 const HttpError = (status, message) => {
   const error = new Error(message);
   error.status = status;
