1665231d6c1a285b5bf4acc251eb22acfff51872
KolBa228 <technotrashinfo@gmail.com>
2023-10-25 17:21:12 +0100

    fixed get mistake

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,75 +1,86 @@
-const addScheme = require('../models/contact')
-
-const { Contact } = require('../models/contact')
-
-const HttpErr = require('../helpers/HttpErr')
-
-const {ctrlWrapper} = require('../helpers/ctrlWrapper')
-
-
+const addScheme = require('../models/contact');
+const { Contact } = require('../models/contact');
+const HttpErr = require('../helpers/HttpErr');
+const { ctrlWrapper } = require('../helpers/ctrlWrapper');
 
 const getAll = async (req, res) => {
     const { _id: owner } = req.user;
-    const skip = (page - 1) * limit
     const { page = 1, limit = 3 } = req.query;
-    const response = await Contact.find({owner}, {skip: skip, limit}).populate('owner', 'email');
-    res.json(response)
+    const skip = (page - 1) * limit;
 
+    const response = await Contact.find({ owner })
+        .skip(skip)
+        .limit(limit)
+        .populate('owner', 'email');
+
+    res.json(response);
 };
 
 const getById = async (req, res) => {
-    const { contactId } = req.params
-    const response = await Contact.findById(contactId)
+    const { contactId } = req.params;
+    const response = await Contact.findById(contactId);
+
     if (!response) {
-        throw HttpErr(404, 'Not Foun')
+        throw HttpErr(404, 'Not Found');
     }
-    res.json(response)
+
+    res.json(response);
 };
 
 const addContact = async (req, res) => {
-        const { _id: owner } = req.user; 
-    const { error } = addScheme.validate(req.body)
+    const { _id: owner } = req.user;
+    const { error } = addScheme.validate(req.body);
+
     if (error) {
-        throw HttpErr(400, error.message)
+        throw HttpErr(400, error.message);
     }
-    const response = await Contact.create({...req.body, owner});
+
+    const response = await Contact.create({ ...req.body, owner });
     res.status(201).json(response);
 };
 
 const removeContact = async (req, res) => {
-    const { id } = res.params;
-    const response = await Contact.findByIdAndRemove(id)
+    const { id } = req.params;
+    const response = await Contact.findByIdAndRemove(id);
+
     if (!response) {
-        throw HttpErr(404, 'Not Foun')
+        throw HttpErr(404, 'Not Found');
     }
-    res.status({
-        message: 'Done'
-    })
+
+    res.json({ message: 'Done' });
 };
 
 const updateContact = async (req, res) => {
-    const { error } = addScheme.validate(req.body)
+    const { error } = addScheme.validate(req.body);
+
     if (error) {
-        throw HttpErr(400, error.message)
+        throw HttpErr(400, error.message);
     }
-    const { id } = res.params;
-    const response = await Contact.findOneAndUpdate(id, req.body, { new: true })
+
+    const { id } = req.params;
+    const response = await Contact.findOneAndUpdate({ _id: id }, req.body, { new: true });
+
     if (!response) {
-        throw HttpErr(404, 'Not Foun')
+        throw HttpErr(404, 'Not Found');
     }
+
     res.json(response);
 };
 
 const updateContactFavorite = async (req, res) => {
-    const { error } = addScheme.validate(req.body)
+    const { error } = addScheme.validate(req.body);
+
     if (error) {
-        throw HttpErr(400, error.message)
+        throw HttpErr(400, error.message);
     }
-    const { id } = res.params;
-    const response = await Contact.findOneAndUpdate(id, req.body, { new: true })
+
+    const { id } = req.params;
+    const response = await Contact.findOneAndUpdate({ _id: id }, req.body, { new: true });
+
     if (!response) {
-        throw HttpErr(404, 'Not Foun')
+        throw HttpErr(404, 'Not Found');
     }
+
     res.json(response);
 };
 
@@ -80,4 +91,4 @@ module.exports = {
     removeContact: ctrlWrapper(removeContact),
     updateContact: ctrlWrapper(updateContact),
     updateContactFavorite: ctrlWrapper(updateContactFavorite)
-}
+};
--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -1,26 +1,27 @@
 const HttpErr = require('./HttpErr');
 const jwt = require('jsonwebtoken');
 const TOKEN_KEY = 'hashfhhjgh1k2h3kjho9999888';
-const User = require('../models/user')
+const { User } = require('../models/user');
 
-const authenticate = async(req, res, next) => {
+const authenticate = async (req, res, next) => {
     const { authorization = '' } = req.headers;
     const [bearer, token] = authorization.split(' ');
     if (bearer !== 'Bearer') {
-        next(HttpErr(401))
-    };
+        next(HttpErr(401));
+    }
+
     try {
         const { id } = jwt.verify(token, TOKEN_KEY);
-        const user = await User.findOne(id);
+        const user = await User.findOne({ _id: id });
         if (!user || !user.token || user.token !== token) {
-            throw HttpErr(401)
-            
-        };
+            throw HttpErr(401);
+        }
+
         req.user = user;
         next();
     } catch (error) {
-        next(HttpErr(401))
-    };
+        next(HttpErr(401));
+    }
 };
 
 module.exports = authenticate;
