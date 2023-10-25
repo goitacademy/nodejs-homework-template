1d46ddca0415b4164848aa741704f5e01ef6844f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 20:28:41 +0300

    Update contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,6 +1,14 @@
 const Joi = require("joi");
 
-const { HttpError } = require("../../helpers");
+const { HttpError } = require("../helpers");
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../models");
 
 const contactAddSchema = Joi.object({
   title: Joi.string().required().messages({
