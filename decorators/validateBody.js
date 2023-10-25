71d7c3f0c8c331c1c84383956b786af6d2ff414c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 22:16:37 +0300

    Update validateBody.js

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -1,10 +1,13 @@
 const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
-  const { error } = schema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    next();
+  };
 
   return func;
 };
