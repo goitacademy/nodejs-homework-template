2cb060adadff09f0a9c0f38b6785e5a3092b935d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-25 18:11:46 +0300

    Update contacts-router.js

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -36,12 +36,8 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { contactId } = req.params;
-    const result = await getContactById(contactId);
-    if (!result) {
-      throw HttpError(404, `Contact with ${contactId} not found`);
-    }
-    res.json(result);
+    const result = await addContact(req.body);
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
