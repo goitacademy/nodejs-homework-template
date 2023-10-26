35f4362fed6af4139d43175ec830b10d60cfe745
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-27 00:26:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const { connect } = require("mongoose");
 
 //Mongodb project password: 5jHYaWBQHrsbTiO2
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://OlehKozub:5jHYaWBQHrsbTiO2@cluster0.6zr4dnp.mongodb.net/contacts-db?retryWrites=true&w=majority";
+connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
