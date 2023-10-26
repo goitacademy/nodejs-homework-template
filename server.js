138ac87e99639b9eaa62d5f9e47ae81cf8c3817f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-27 01:07:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { DB_HOST, PORT } = process.env;
 connect(DB_HOST)
   .then(() =>
     app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log(`Server running. Use our API on port: ${PORT}`);
     })
   )
   .catch((error) => {
