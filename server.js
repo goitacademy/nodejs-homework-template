86b7f20dc2c5f4736606f3265233765b804d5997
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-27 00:32:26 +0300

    npm i nanoid

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { connect } = require("mongoose");
 
 //Mongodb project password: 5jHYaWBQHrsbTiO2
 
+console.log(process.env);
+
 const DB_HOST =
   "mongodb+srv://OlehKozub:5jHYaWBQHrsbTiO2@cluster0.6zr4dnp.mongodb.net/contacts-db?retryWrites=true&w=majority";
 connect(DB_HOST)
