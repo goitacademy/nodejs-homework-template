7e490ebd930613861b8a7e53447fce9c301b25b3
arhip14 <kruvdyk1503@gmail.com>
2023-10-26 21:33:56 +0300

    initial

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'Home work done!' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
