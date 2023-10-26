3908e79b693cf162823fb25c0c85a90a4c0da42c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-27 00:59:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const { connect } = require("mongoose");
 
 //Mongodb project password: 5jHYaWBQHrsbTiO2
 
-console.log(process.env);
-
-const DB_HOST =
-  "mongodb+srv://OlehKozub:5jHYaWBQHrsbTiO2@cluster0.6zr4dnp.mongodb.net/contacts-db?retryWrites=true&w=majority";
 connect(DB_HOST)
   .then(() =>
     app.listen(3000, () => {
