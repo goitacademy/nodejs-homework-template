efd2e1af816e066edce2ccca873ee95afeab6acd
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2023-10-26 19:01:41 +0200

    fix

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 import contactsRouter from './routes/api/contacts.js';
 
 dotenv.config();
+
 const app = express();
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
--- models/hooks.js
+++ models/hooks.js
@@ -1,10 +1,10 @@
 export const handleSaveError = (error, data, next) => {
-  error.status = 400;
-  next();
-};
+    error.status = 400;
+    next();
+}
 
 export const runValidatorsAtUpdate = function (next) {
-  this.options.runValidators = true;
-  this.options.new = true;
-  next();
-};
+    this.options.runValidators = true;
+    this.options.new = true;
+    next();
+}
