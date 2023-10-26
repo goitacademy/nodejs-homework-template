4e246e6b84b4b21b8174372cc1316c5a93853601
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-27 01:02:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { connect } = require("mongoose");
 
 //Mongodb project password: 5jHYaWBQHrsbTiO2
 
-connect(DB_HOST)
+connect(process.env.DB_HOST)
   .then(() =>
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
