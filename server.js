62961f904fd52e30ebbbe0436e692678f358019c
tomaspalica <tomasss.palica@gmail.com>
2023-10-27 17:21:20 +0200

    connect mongoose database

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { DB_HOST: uriDb } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const connection = mongoose.connect(uriDb);
+
+connection
+  .then(
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((err) =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
