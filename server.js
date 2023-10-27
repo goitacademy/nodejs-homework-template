72e72b97e3d5854b5dda496d8742da034155bab8
MariaBetancur <mabo6241@gmail.com>
2023-10-26 21:59:03 -0500

    v1.5

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const app = require('./app')
-const mongoose =require("mongoose")
-const {DB_HOST,PORT=3000} = process.env
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.connect(DB_HOST)
-.then(()=>
-app.listen(PORT),
-console.log("Database connection successful"))
-.catch(error=>{
-console.log(error.message)
-process.exit(1)
- })
+mongoose.connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+    console.log('Connected to the database!');
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error.message);
+    process.exit(1);
+  });
