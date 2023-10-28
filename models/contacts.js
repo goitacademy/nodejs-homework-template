0009b004671961e90a845c552c0ac71c66041de7
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-10-28 18:28:58 -0400

    .

--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,66 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const getListContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  const result = JSON.parse(contacts);
+  return result;
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const id = String(contactId);
+  const contacts = await getListContacts();
+  const result = contacts.find(item => item.id === id);
+  return result || null;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const id = String(contactId);
+  const contacts = await getListContacts();
+  const index = contacts.findIndex(item => item.id === id);
+    if(index === -1){
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result || null;
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async ({name, email, phone}) => {
+  const contacts = await getListContacts();
+  const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+  }
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;    
+}
+
+const updateContact = async (contactId, {name, email, phone}) => {
+  const id = String(contactId);
+  const contacts = await getListContacts();
+  const index = contacts.findIndex(item => item.id === id);
+    if(index === -1){
+        return null;
+    }
+  contacts[index] = {
+    id,
+    name,
+    email,
+    phone,
+  };
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index] || null;
+}
 
 module.exports = {
-  listContacts,
+  getListContacts,
   getContactById,
   removeContact,
   addContact,
