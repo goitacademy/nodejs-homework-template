31463ca8bb826c1390e2124804ff9b5c75618fc2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-28 22:53:29 +0300

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -2,14 +2,6 @@ const { Schema, model } = require("mongoose");
 const { handleSaveError } = require("./hooks");
 const Joi = require("joi");
 
-const contactAddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
-
-module.exports = { contactAddSchema };
-
 const contactSchema = new Schema(
   {
     name: { type: String, required: true },
@@ -19,7 +11,13 @@ const contactSchema = new Schema(
   { versionKey: false, timestamps: true }
 );
 
-movieSchema.post("save", handleSaveError);
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+contactSchema.post("save", handleSaveError);
 
 const Contact = model("contact", contactSchema);
 
