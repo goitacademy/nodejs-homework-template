4e342588330144fbbdfcc03ba2f01dde07a7685a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-28 22:01:18 +0300

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -1,10 +1,13 @@
 const { Schema, model } = require("mongoose");
 
-const contactSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-});
+const contactSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const Contact = model("contact", contactSchema);
 
