30b4eb95bc9d5bd0f1b3506c675cf32c24a803fb
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-29 16:29:42 +0200

    Update contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -35,7 +35,9 @@ const removeContactController = async (req, res, next) => {
 
 const updateContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, `Contact with ${contactId} not found`);
   }
