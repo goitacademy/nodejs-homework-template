a111e5dc21271a0d970fa4dded81161e02178ce8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-31 00:01:04 +0200

    Update auth-controller.js

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -4,6 +4,9 @@ const { HttpError } = require("../../helpers");
 
 const { controllerWrapper } = require("../../decorators");
 
-const signup = async (req, res) => {};
+const signup = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(201).json({ username: user.username, email: user.email });
+};
 
 module.exports = { signup: controllerWrapper(signup) };
