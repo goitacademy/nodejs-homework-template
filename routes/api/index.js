1c698e6d40334914a07c5e06312ab6646da24e71
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-10-30 07:54:55 -0500

    Prueba routes index

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ const routerContacts = require('.//contacts');
 const routerIndex = express.Router();
 
 module.exports = () => {
+  // Aun no funciona
   routerIndex.use('/Contacts', routerContacts());
 
   return routerIndex;
