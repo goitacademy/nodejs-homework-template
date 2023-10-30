e5016041789f0d24cf2be6a45ff9d2efda66c8ff
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-30 18:49:40 +0200

    Update Users.js

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleSaveError, runValidatorsAtUpdate } = require("./hooks");
+
+const usersSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
