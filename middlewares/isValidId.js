b31943736345fb5f48dc125b0323bd326e9db686
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-30 21:09:35 +0200

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,9 +3,9 @@ const { isValidObjectId } = require("mongoose");
 const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    return next(HttpError(404, `${id} not valid id`));
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    return next(HttpError(404, `${contactId} not valid id`));
   }
 };
 module.exports = { isValidId };
