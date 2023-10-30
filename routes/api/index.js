6889fa01a2eeccf7c6a9c64fca1a378f2c1cd56a
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-10-30 07:59:07 -0500

    prueba rama

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,6 @@ const routerContacts = require('.//contacts');
 const routerIndex = express.Router();
 
 module.exports = () => {
-  // Aun no funciona
   routerIndex.use('/Contacts', routerContacts());
 
   return routerIndex;
