d399c30d3c960cc2ba6aeec2de4d138c5236665c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-30 22:53:06 +0200

    Update auth-controller.js

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -1,1 +1,7 @@
-const {} = require("../../models");
+const { User } = require("../../models");
+
+const { HttpError } = require("../../helpers");
+
+const { controllerWrapper } = require("../../decorators");
+
+const signup = async (req, res) => {};
