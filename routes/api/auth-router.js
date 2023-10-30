437384af55fadf18fba8690ee9009e4982408c6a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-30 23:34:06 +0200

    Update auth-router.js

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -4,6 +4,10 @@ const { signup } = require("../../controllers");
 
 const { isEmptyBody } = require("../../middlewares");
 
+const { validateBody } = require("../../decorators");
+
+const { userSignUpSchema, userSignInSchema } = require("../../models");
+
 const authRouter = express.Router();
 
 module.exports = { authRouter };
