e7b144f6b17a28ffca631ed2e8ad59feec5cfa79
Babenko Liudmyla <savelieva0509@gmail.com>
2023-10-31 16:03:25 +0200

    add validateBody

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,14 +4,17 @@ const controller = require("../../controllers/contacts")
 
 const router = express.Router();
 
+const { validateBody } = require("../../middlewares")
+const schemas = require("../../schemas/contacts")
+
 router.get("/", controller.listContacts );
 
 router.get("/:contactId", controller.getContactById );
 
-router.post("/", controller.addContact);
+router.post("/", validateBody(schemas.addSchema), controller.addContact);
 
 router.delete("/:contactId", controller.removeContact );
 
-router.put("/:contactId", controller.updateContactById );
+router.put("/:contactId", validateBody(schemas.addSchema),controller.updateContactById );
 
 module.exports = router;
