efb2901555279d4baf45828e443fa2bea12ff446
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-10-31 01:48:53 +0100

    initial draft of HW03-mongodb

---
+++ controllers/contacts/updateFavoriteStatus.js
@@ -0,0 +1,25 @@
+import Contact from "../../models/contactModel.js";
+export async function updateFavoriteStatus(req, res, next) {
+  const contactId = req.params.contactId;
+  const { favorite } = req.body;
+
+  if (favorite === undefined) {
+    return res.status(400).json({ message: "missing field favorite" });
+  }
+
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite },
+      { new: true } // Zwraca zaktualizowany dokument
+    );
+
+    if (updatedContact) {
+      return res.status(200).json(updatedContact);
+    } else {
+      return res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,21 @@
-const express = require('express')
+import express from "express";
+import { indexContacts } from "../../controllers/contacts/indexContacts.js";
+import { showContacts } from "../../controllers/contacts/showContacts.js";
+import { createContacts } from "../../controllers/contacts/createContacts.js";
+import { deleteContacts } from "../../controllers/contacts/deleteContacts.js";
+import { updateContacts } from "../../controllers/contacts/updateContacts.js";
+import { updateFavoriteStatus } from "../../controllers/contacts/updateFavoriteStatus.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", indexContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", showContacts);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", createContacts);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", deleteContacts);
+router.put("/:contactId", updateContacts);
+router.patch("/:contactId/favorite", updateFavoriteStatus);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+export default router;
