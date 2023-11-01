111853735652df922581a06dc76b89543e6f0d71
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-01 14:12:15 +0200

    Delete index.js

--- hash/index.js
+++
@@ -1,8 +0,0 @@
-const { hash } = require("bcryptjs");
-
-const hashPassword = async (password) => {
-  const result = await hash(password, 10);
-  return result;
-};
-
-module.exports = { hashPassword };
