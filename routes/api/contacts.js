8173386c8ec042e01cb05400779d0bc749b24b02
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-11-01 18:04:44 +0100

    routes done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,93 @@
 const express = require("express");
+const Joi = require("joi");
+
+const contactValidationSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .pattern(/^[0-9]{10}$/)
+    .required(),
+});
+
+const {
+  listContacts,
+  getById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts.js");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "message" });
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "message" });
+  try {
+    const contact = await getById(req.params.contactId);
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "message" });
+  const validationResult = contactValidationSchema.validate(req.body);
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ message: validationResult.error.details[0].message });
+  }
+
+  try {
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "message" });
+  try {
+    const result = await removeContact(req.params.contactId);
+    if (result) {
+      res.status(200).json({ message: "contact deleted" });
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "message" });
+  const validationResult = contactValidationSchema.validate(req.body);
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ message: validationResult.error.details[0].message });
+  }
+
+  try {
+    const updatedContact = await updateContact(req.params.contactId, req.body);
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
