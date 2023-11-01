2b80a7db604410bea6160e351134f33ed8f663e1
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-01 13:03:15 +0200

    Update hooks.js

--- models/hooks.js
+++ models/hooks.js
@@ -1,7 +1,7 @@
 const handleSaveError = (error, data, next) => {
-  const { name, code } = error;
+  const { username, code } = error;
 
-  error.status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  error.status = username === "MongoServerError" && code === 11000 ? 409 : 400;
   next();
 };
 
