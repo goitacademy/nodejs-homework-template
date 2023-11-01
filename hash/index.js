9e6e147e5f82b7a2411e21ee742428edf8424cb3
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-01 13:25:26 +0200

    Update index.js

--- hash/index.js
+++ hash/index.js
@@ -1,1 +1,6 @@
-const bcrypt = require("bcryptjs");
+const { hash } = require("bcryptjs");
+
+const hashPassword = async (password) => {
+  const result = await hash(password, 10);
+  return result;
+};
