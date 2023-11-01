e268d9321c6666e0880c1a54da91ba89979d5734
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-01 13:58:41 +0200

    Update auth-controller.js

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -4,14 +4,19 @@ const { HttpError } = require("../../helpers");
 
 const { controllerWrapper } = require("../../decorators");
 
+const { hashPassword } = require("../../hash");
+
 const signup = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, `${email} already in use`);
   }
 
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword(password),
+  });
 
   res.status(201).json({
     username: newUser.username,
