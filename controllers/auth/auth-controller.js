4d61f383f36deb554910121befdc83222fe7bee4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-01 12:39:12 +0200

    Update auth-controller.js

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -13,7 +13,10 @@ const signup = async (req, res) => {
 
   const newUser = await User.create(req.body);
 
-  res.status(201).json({ username: newUser.username, email: newUser.email });
+  res.status(201).json({
+    username: newUser.username,
+    email: newUser.email,
+  });
 };
 
 module.exports = { signup: controllerWrapper(signup) };
