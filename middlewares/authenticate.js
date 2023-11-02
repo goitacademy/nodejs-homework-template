8b804518563b1033d2af585d7fa482c775a78606
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-03 00:00:47 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,8 @@
 const { HttpError } = require("../helpers");
 
-const authenticate = async () => {};
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+};
 
 module.exports = { authenticate };
