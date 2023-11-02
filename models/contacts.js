b0989924678be3255d711a0ff3f196dbcd4f8953
AnnaHort <annahort3d@gmail.com>
2023-11-02 16:50:14 +0200

    add first part

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,51 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require("path");
+// const nanoid = require("nanoid")
 
-const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const removeContact = async (contactId) => {}
+// вписати ті файли які ми писали для роботи джейсона contacts.json (та як на минулій домашці)
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const results = contacts.find((item) => item.id === contactId);
+  return results || null;
+}
 
-const updateContact = async (contactId, body) => {}
+// const removeContact = async (contactId) => {
+//   const contacts = await listContacts();
+//   const index = contacts.findIndex(item => item.id === contactId);
+//   if( index === -1) {
+//     return null;
+//   }
+//   const [result] = contacts.splice([index], 1);
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   return result;
+  
+// }
+
+// const addContact = async (body) => {
+//   const allContacts = await listContacts();
+//   const newContact = {
+//     id: nanoid(),
+//     body
+//   };
+//   allContacts.push(newContact);
+//   await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+//   return newContact
+// }
+
+// const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  // removeContact,
+  // addContact,
+  // updateContact,
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,56 @@
-const express = require('express')
+// а тут ми маєм написати логіку, тобто коли приходить запит за контактом, треба взяти контакт або додати з джейсону і повернути
+// імпортуємо функції для роботи з контактами
+const contacts = require("../../models/contacts");
+// і використовуємо. Наприклад get-запит має повертати всі контакти
 
-const router = express.Router()
+const express = require("express");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const results = await contacts.listContacts();
+    res.json({ results });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// req - інфо про запит який прийшов
+// res - об'єкт який дозволяє налаштувати та відправити відповідь
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  console.log(req.params);
+  try {
+    const { contactId } = req.params;
+    const results = await contacts.getContactById(contactId);
+    if (!results) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error; // генерує помилку
+      // якщо ми генеруємо помилку всередині try то відразу перевидаємось на catch
+    }
+    res.json(results); // відправляємо відповідь
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.post("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
 
-module.exports = router
+// router.delete("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.put("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+module.exports = router;
