d2347785372169081f528f8297487f003c15d713
NataDent <cherkashyna.n@gmail.com>
2023-11-03 13:46:51 +0200

    Added schemas/contacts.js

---
+++ schemas/contacts.js
@@ -0,0 +1,9 @@
+const Joi = require("joi");
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.number().required(),
+});
+
+module.exports = { addSchema };
