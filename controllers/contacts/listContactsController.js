f0a6e433212acbedac8196dd9276c7ad95c10dfd
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-03 20:36:06 +0200

    Update listContactsController.js

--- controllers/contacts/listContactsController.js
+++ controllers/contacts/listContactsController.js
@@ -3,6 +3,7 @@ const { Contact } = require("../../models");
 const { controllerWrapper } = require("../../decorators");
 
 const listContactsController = async (req, res) => {
+  const { _id: owner } = req.user;
   const result = await Contact.find();
   res.json(result);
 };
