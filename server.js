9c2ff77bd8ebf2596e99825e3055c4ce287e1280
luminitza24 <cluminita24@yahoo.com>
2023-11-03 23:15:49 +0200

    added files

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+require("dotenv").config();
+const mongoDB = process.env.MONGO_URL;
+const port = process.env.PORT || 5000;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+app.listen(port, () => {
+  console.log(`The server is running at port ${port}`);
+  mongoose
+    .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Database connection successful");
+    })
+    .catch((err) => {
+      console.error("Error to connect to the database:", err);
+      mongoose.connection.close(() => {
+        console.log("Database connection has been closed due to an error.");
+      });
+    });
+});
