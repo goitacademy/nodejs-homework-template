ab251123a1387b96efc9f62ff4f0863d5d08c1de
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-03 22:56:12 +0200

    Update listContactsController.js

--- controllers/contacts/listContactsController.js
+++ controllers/contacts/listContactsController.js
@@ -4,6 +4,7 @@ const { controllerWrapper } = require("../../decorators");
 
 const listContactsController = async (req, res) => {
   const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
   const result = await Contact.find({ owner }).populate(
     "owner",
     "username email"
