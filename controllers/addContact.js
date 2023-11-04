c6d32ead30f23a0e2e5e7d77aa87bed4c103d989
Camila Mendez <mcmendezmanotas@gmail.com>
2023-11-03 23:05:30 -0500

    adicionando las validaciones

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -1,10 +1,17 @@
 const fs = require('fs').promises;
 const path = require('path');
 const contactsPath = path.join(__dirname,'../models', 'contacts.json');
-const crypto=require('crypto')
+const crypto = require('crypto')
+const { createContactSchema } = require('../validation/validation');
 
 const addContact = async (req, res) => {
+  const { error } = createContactSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ error: { "message": "missing required name field" } });
+  }
   try {
+    
     const data = await fs.readFile(contactsPath, 'utf-8');
     const contacts = JSON.parse(data);
     const newContact = {
--- controllers/updateContact.js
+++ controllers/updateContact.js
@@ -1,10 +1,15 @@
 const fs = require('fs').promises;
 const path = require('path');
-const contactsPath = path.join(__dirname,'../models', 'contacts.json');
+const contactsPath = path.join(__dirname, '../models', 'contacts.json');
+const { updateContactSchema } = require('../validation/validation');
 
 const updateContact = async (req, res) => {
   const { contactId } = req.params;
+  const { error } = updateContactSchema.validate(req.body);
 
+  if (error) {
+    return res.status(400).json({ error: { "message": "missing fields" }});
+  }
   try {
     const data = await fs.readFile(contactsPath, 'utf-8');
     const contacts = JSON.parse(data);
