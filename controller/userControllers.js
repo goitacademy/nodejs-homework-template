bbdd1d8adc30bcefeec6e413165737f6c7efd11e
tomaspalica <tomasss.palica@gmail.com>
2023-11-04 13:09:24 +0100

    fix registration

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -3,7 +3,7 @@ import { User } from "../service/schemas/User.js";
 
 export const registration = async (req, res, next) => {
   const { email, password, subscription } = req.body;
-  const user = await getUserByEmail(email);
+  const user = await User.findOne({ email });
   if (user) {
     return res.status(409).json({ message: "Email in use" });
   }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { registration } from "../../controller/userControllers.js";
 import { User } from "../../service/schemas/User.js";
+import { getUserByEmail } from "../../models/users.js";
 
 const router = express.Router();
 
@@ -10,5 +11,12 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/signup", registration);
+
+router.post("/test", async (req, res, next) => {
+  const { email } = req.body;
+  const test = await User.findOne({email});
+  console.log(test);
+  res.json(test);
+});
 
 export { router };
