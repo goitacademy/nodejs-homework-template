3abf83134e3586509e880ece2eb01e33e480c5d5
Andrii Chychkan <mywork1728@gmail.com>
2023-11-04 22:24:01 +0200

    added upd contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "/contacts.json");
 
@@ -10,22 +11,30 @@ const listContacts = async () => {
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const allContacts = await listContacts();
-    const result = await allContacts.find((el) => el.id === contactId);
-    if (!result) {
-      console.log("index is invalid, please check the input index!");
-      return null;
-    }
-    return result;
-  } catch (error) {
-    console.log(error);
+  const allContacts = await listContacts();
+  const result = allContacts.find((el) => el.id === contactId);
+  if (!result) {
+    console.log("index is invalid, please check the input index!");
+    return null;
   }
+  return result;
 };
 
 const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {};
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const contacts = await listContacts();
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
 const updateContact = async (contactId, body) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { listContacts, getContactById } = require("../../models/contacts");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+} = require("../../models/contacts");
 const app = express();
 const router = express.Router();
 
@@ -12,14 +16,15 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
   const data = await getContactById(contactId);
-  // if (!data) {
-  //   res.status(404).json({ message: "Not found" });
-  // }
+  if (!data) {
+    return next();
+  }
   res.send(data);
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const updContacts = await addContact(req.body);
+  res.status(201).json(updContacts);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
