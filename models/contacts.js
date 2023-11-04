d24f6abbf8cfe61382dc990ecfb46944d63f24b8
Camila Mendez <mcmendezmanotas@gmail.com>
2023-11-03 22:28:53 -0500

    le di orden a las carpetas

--- models/contacts.js
+++ models/contacts.js
@@ -1,104 +1,22 @@
-const fs = require('fs').promises;
-const path = require('path');
-const contactsPath = path.join(__dirname, 'contacts.json');
+const mongoose = require("mongoose")
 
-function generateUniqueId() {
-  // Generador de ID
-  const timestamp = Date.now().toString();
-  const randomNumber = Math.floor(Math.random() * 1000);
-  return `${timestamp}-${randomNumber}`;
-}
-
-
-
-const listContacts = async () => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    return JSON.parse(data);
-    
-  } catch (error) {
-    console.error(error);
-    return error;
+const contacts = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   }
-};
+)
 
-const getContactById = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    const contact = contacts.find((c) => c.id === contactId);
-    return contact ? [contact] : 'Contact not found';
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-};
-
-const removeContact = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    const contactIndex = contacts.findIndex((c) => c.id === contactId);
-    
-    if (contactIndex !== -1) {
-      contacts.splice(contactIndex, 1);
-      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-      return 'Contact removed successfully';
-    } else {
-      return 'Contact not found';
-    }
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-};
-
-const addContact = async (body) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    const newContact = {
-      id: generateUniqueId(),
-      ...body,
-    };
-
-    contacts.push(newContact);
-
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return 'Contact added successfully.';
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-};
-
-const updateContact = async (contactId, body) => {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    const contactIndex = contacts.findIndex((c) => c.id === contactId);
-
-    if (contactIndex !== -1) {
-      contacts[contactIndex] = {
-        ...contacts[contactIndex],
-        ...body,
-      };
-
-      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-      return 'Contact updated successfully';
-    } else {
-      return 'Contact not found';
-    }
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-};
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports= mongoose.model("contacts",contacts)
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,86 +1,20 @@
 const express = require('express');
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require('../../models/contacts');
-
+const controllers= require('../../controllers/index')
 const router = express.Router();
 
 // Obtener la lista de contactos
-router.get('/', async (req, res, next) => {
-  try {
-    const data = await listContacts();
-    res.status(200).json({ data});
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
+router.get('/',controllers.listContacts);
 
 // Obtener un contacto por su ID
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-
-  try {
-    const contact = await getContactById(contactId);
-
-    if (contact !== 'Contact not found') {
-      res.status(200).json({ data: contact });
-    } else {
-      res.status(404).json({ message: 'Contact not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
+router.get('/:contactId', controllers.getContactById);
 
 // Agregar un nuevo contacto
-router.post('/', async (req, res, next) => {
-  const { name, email, phone } = req.body;
-
-  try {
-    const result = await addContact({ name, email, phone });
-    res.status(201).json({ message: result });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
+router.post('/', controllers.addContact);
 
 // Eliminar un contacto por su ID
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-
-  try {
-    const result = await removeContact(contactId);
-
-    if (result === 'Contact removed successfully') {
-      res.status(204).json({ message: result });
-    } else {
-      res.status(404).json({ message: 'Contact not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
+router.delete('/:contactId', controllers.removeContact);
 
 // Actualizar un contacto por su ID
-router.put('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const { name, email, phone } = req.body;
-
-  try {
-    const result = await updateContact(contactId, { name, email, phone });
-
-    if (result === 'Contact updated successfully') {
-      res.status(200).json({ message: result });
-    } else {
-      res.status(404).json({ message: 'Contact not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
+router.put('/:contactId', controllers.updateContact);
 
 module.exports = router;
