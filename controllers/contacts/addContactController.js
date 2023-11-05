a610fb0808453d0b7198f35e7809e3f0b3517ac1
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-05 22:19:45 +0200

    Update addContactController.js

--- controllers/contacts/addContactController.js
+++ controllers/contacts/addContactController.js
@@ -4,6 +4,7 @@ const { controllerWrapper } = require("../../decorators");
 
 const addContactController = async (req, res) => {
   const { _id: owner } = req.user;
+
   const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
