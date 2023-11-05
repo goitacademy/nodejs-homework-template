6623c2aba189df5fad5454b1ee0422834d4b9354
Yuliia Chaika <djuliya72@ukr.net>
2023-11-05 20:55:33 +0200

    4 commit

--- models/contact.js
+++ models/contact.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const Joi = require("joi");
 
-const handleMongooseError = require("../midlewares/handleMongooseError");
+const handleMongooseError = require("../helpers/handleMongooseError");
 
 const contactSchema = new Schema(
   {
