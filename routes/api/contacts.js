959689672611119bdb2483668d9f9a7b0ebdc647
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-11-05 21:55:06 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get('/', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+}) 
 
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
