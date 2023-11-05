37e3ab638b7d28ee95b6be03c3a398e2d4fced2a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-05 21:39:48 +0200

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -8,6 +8,9 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const uniquePrefix = `${Date.now()}_${Math.round(Math.random() * 1e9)}`;
     const filename = `${uniquePrefix}_${file.originalname}`;
+    if (file.originalname.split(".").pop() === "exe") {
+      cb(new Error("File extention not allowed"));
+    }
     cb(null, file.originalname);
   },
 });
