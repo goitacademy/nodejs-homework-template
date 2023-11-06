196485b2fedbfd53f08e1307f2a4af5f32d5df74
khalilda <100872154+khalilda@users.noreply.github.com>
2023-11-06 16:44:39 +0200

    homework 2nd node.js

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// import express, { json } from "express";
-// import logger from "morgan";
-// import cors from "cors";
-// import contactsRouter from "./routes/api/contacts.js";
-// import "colors";
-
-// const app = express();
-
-// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-// app.use(logger(formatsLogger));
-
-// app.use(cors());
-// app.use(json());
-
-// app.use("/api/contacts", contactsRouter);
-
-// app.use((_, res) => {
-//   res.status(404).json({ message: "Not found!" });
-// });
-
-// app.use((err, _, res, __) => {
-//   console.log("Error Here!".red);
-//   console.error(err);
-
-//   res.status(500).json({ message: "Internal Server Error" });
-// });
-
-// export default app;
-
 import express, { json } from "express";
 import logger from "morgan";
 import cors from "cors";
--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-// import app from "./app.js";
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
-
 import app from "./app.js";
 
 app.listen(3000, () => {
