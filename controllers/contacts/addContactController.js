4e578d68f7d1af13328cbf08fb0401509e7a25b4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-06 23:06:58 +0200

    Update addContactController.js

--- controllers/contacts/addContactController.js
+++ controllers/contacts/addContactController.js
@@ -1,16 +1,10 @@
-const { unlink } = require("fs/promises");
-
 const { Contact } = require("../../models");
+
 const { controllerWrapper } = require("../../decorators");
-const { cloudinary } = require("../../helpers");
 
 const addContactController = async (req, res) => {
   const { _id: owner } = req.user;
-  const { url: avatar } = await cloudinary.uploader.upload(req.file.path, {
-    folder: "avatars",
-  });
-  await unlink(req.file.path);
-  const result = await Contact.create({ ...req.body, avatar, owner });
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
