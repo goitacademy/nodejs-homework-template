9a772473f9354be5e27e71924b61bca038c3bc42
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-11-06 21:39:56 +0200

    HMK-06/11/23

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,7 @@ const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, "missing required name field"));
+      next(HttpError(400, error.message));
     }
     next();
   };
