da5e70c2dc80485bc0d7b66550b1ae3b822760e4
khalilda <100872154+khalilda@users.noreply.github.com>
2023-11-06 15:08:40 +0200

    fixxxx

--- validation/contacts.js
+++ validation/contacts.js
@@ -54,6 +54,20 @@
 
 // export const contactValidate = contactValidator(contactSchema);
 
+// import joi from "joi";
+
+// export const contactSchema = joi.object({
+//   name: joi.string().min(2).required(),
+//   email: joi.string().required(),
+//   phone: joi.string().required(),
+// });
+
+// const contactValidator = (schema) => (data) => {
+//   return schema.validate(data, { abortEarly: false });
+// };
+
+// export const contactValidate = contactValidator(contactSchema);
+
 import joi from "joi";
 
 export const contactSchema = joi.object({
