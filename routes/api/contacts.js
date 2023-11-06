b59c53aba50bb7cc5daa77679fd3f42bbc57dc61
Babenko Liudmyla <savelieva0509@gmail.com>
2023-11-06 14:56:14 +0200

    add settings

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,21 +4,21 @@ const controller = require("../../controllers/contacts");
 
 const router = express.Router();
 
-const { validateBody } = require("../../middlewares");
-const addSchema = require("../../schemas/contacts");
+// const { validateBody } = require("../../middlewares");
+// const addSchema = require("../../schemas/contacts");
 
 router.get("/", controller.listContacts);
 
-router.get("/:contactId", controller.getContactById);
+// router.get("/:contactId", controller.getContactById);
 
-router.post("/", validateBody(addSchema), controller.addContact);
+// router.post("/", validateBody(addSchema), controller.addContact);
 
-router.delete("/:contactId", controller.removeContact);
+// router.delete("/:contactId", controller.removeContact);
 
-router.put(
-  "/:contactId",
-  validateBody(addSchema),
-  controller.updateContactById
-);
+// router.put(
+//   "/:contactId",
+//   validateBody(addSchema),
+//   controller.updateContactById
+// );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const DB_HOST =
-  "mongodb+srv://savelieva0509:kapitowka@cluster0.jaqpcqf.mongodb.net/contactsReader?retryWrites=true&w=majority";
+
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
