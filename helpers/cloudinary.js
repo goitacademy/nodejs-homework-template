1c40002172fbbb4d84e4b10b0a51e09d45ca4203
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-06 20:39:24 +0200

    Update cloudinary.js

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -5,3 +5,9 @@ config();
 
 const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
   process.env;
+
+v2.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
