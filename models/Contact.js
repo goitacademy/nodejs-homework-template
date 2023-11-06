0880ed63ae3e4ff1e1475e737aca74d0ecf79b21
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-06 22:37:38 +0200

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -19,10 +19,6 @@ const contactSchema = new Schema(
       type: Boolean,
       default: false,
     },
-    avatar: {
-      type: String,
-      required: true,
-    },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "user",
