e46f477e7caf8d628c036dfae4da69b028def3a0
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-11-07 23:50:44 +0200

    router-post

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,4 @@
-const {HttpError} = require('./HttpError');
+const HttpError = require('./HttpError');
 
 module.exports = {
     HttpError,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,14 @@
 const express = require('express')
-
+const Joi = require('joi')
 const contacts = require('../../models/contacts')
-
+const {HttpError} = require('../../helpers')
 const router = express.Router()
+
+const addSchemas = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
 
 router.get('/', async (req, res, next) => {
   try {
@@ -10,9 +16,10 @@ router.get('/', async (req, res, next) => {
     res.json(result)
   }
   catch(error) {
-    res.status(500).json({
-       message: 'Server error' 
-    })
+    next(error)
+    // res.status(500).json({
+    //    message: 'Server error' 
+    // })
   }
 })
 
@@ -21,9 +28,11 @@ router.get('/:id', async (req, res, next) => {
     const {id} = req.params
     const result = await contacts.getContactById(id)
     if(!result) {
-      const error = new Error('Not found')
-      error.status = 404
-      throw error
+      throw HttpError(404, 'Not found')
+      // const error = new Error('Not found')
+      // error.status = 404
+      // throw error
+
       // return res.status(404).json({
       //   massage: 'Not found'
       // })
@@ -31,18 +40,30 @@ router.get('/:id', async (req, res, next) => {
     res.json(result)
   }
   catch(error) {
-    const {status = 500, message= 'Server error'} = error
-    res.status(status).json({
-       message
-    })
-
+    next(error)
+    // const {status = 500, message= 'Server error'} = error
+    // res.status(status).json({
+    //    message
+    // })
   }
-
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try{
+    const {error} = addSchemas.validate(req.body)
+    if(error){
+      throw HttpError(400, error.message)
+    }
+    // console.log(error)
+   const result = await contacts.addContact(req.body)
+   res.status(201).json(result)
+  }
+  catch(error) {
+    next(error)
+  }
 })
+
+
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
