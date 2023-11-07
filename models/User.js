f17a899af2b6a391c6bc3b058bf9d22b30e0ce6d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-07 21:01:42 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -29,6 +29,14 @@ const userSchema = new Schema(
       required: true,
     },
     token: { type: String },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
