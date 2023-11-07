71b9f32da80526c67c8e70706527d25bf07bb808
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-11-07 21:43:37 +0100

    add DB_HOST

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://Anatolii:adminLocalHost05@cluster0.0yqagxt.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+module.exports = {
+  DB_HOST,
+};
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const { DB_HOST } = require("./config");
+//foadjajkpy
+// new adminLocalHost05
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
