39f0b8a23bc4f1aebed3ddaefc9ae6e9fa0ddaf3
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-07 19:12:33 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,40 +1,8 @@
-let ElasticEmail = require("@elasticemail/elasticemail-client");
-
 const { config } = require("dotenv");
-config();
-const { ELASTICEMAIL_API_KEY, ELASTICEMAIL_FROM } = process.env;
-
-const defaultClient = ElasticEmail.ApiClient.instance;
-
-const { apikey } = defaultClient.authentications;
-apikey.apiKey = ELASTICEMAIL_API_KEY;
-
-const api = new ElasticEmail.EmailsApi();
-
-const email = ElasticEmail.EmailMessageData.constructFromObject({
-  Recipients: [new ElasticEmail.EmailRecipient("oleg.kozub54@gmail.com")],
-  Content: {
-    Body: [
-      ElasticEmail.BodyPart.constructFromObject({
-        ContentType: "HTML",
-        Content: "<div>Test email</div>",
-      }),
-    ],
-    Subject: "Test email",
-    From: ELASTICEMAIL_FROM,
-  },
-});
-
-const callback = function (error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log("API called successfully.");
-  }
-};
-api.emailsPost(email, callback);
-
 const {} = require("nodemailer");
+config();
+
+
 const { UKR_NET_PASSWORD, UKR_NET_EMAIL } = process.env;
 
 const nodemailerConfig = {
@@ -46,3 +14,5 @@ const nodemailerConfig = {
     pass: UKR_NET_PASSWORD,
   },
 };
+
+const transport
