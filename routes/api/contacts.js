d2ead27c46cb6ea8e68017709d1cd16ada0902c2
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-11-07 19:03:20 +0200

    helpers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -11,15 +11,33 @@ router.get('/', async (req, res, next) => {
   }
   catch(error) {
     res.status(500).json({
-       message: 'template message' 
+       message: 'Server error' 
+    })
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const result = await contacts.getContactById(id)
+    if(!result) {
+      const error = new Error('Not found')
+      error.status = 404
+      throw error
+      // return res.status(404).json({
+      //   massage: 'Not found'
+      // })
+    }
+    res.json(result)
+  }
+  catch(error) {
+    const {status = 500, message= 'Server error'} = error
+    res.status(status).json({
+       message
     })
 
   }
 
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
 })
 
 router.post('/', async (req, res, next) => {
