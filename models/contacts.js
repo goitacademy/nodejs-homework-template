c1051d9b75bc3c584fd809b4bf56b95b6f7793fb
JoggerChe <120989445+BorovChe@users.noreply.github.com>
2023-11-07 17:59:24 +0200

    start

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,58 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+async function getById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+}
 
-const addContact = async (body) => {}
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+}
 
-const updateContact = async (contactId, body) => {}
+async function addContact(data) {
+    const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data
+  }
+
+contacts.push(newContact);
+await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+return newContact;
+}
+
+async function updateById(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === id);
+  if (index === -1) {
+      return null;
+  }
+  contacts[index] = {id, ...data};
+await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+return contacts[index];
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    listContacts,
+    getById,
+    removeContact,
+    addContact,
+    updateById,
 }
