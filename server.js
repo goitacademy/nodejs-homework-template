b9a49921f8b8197d291ce98dd747a83be82a5ee2
BerkovskiiAlex <berkovskii845@gmail.com>
2023-11-08 08:41:20 +0200

    mongo deploy

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 /** @format */
 
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Alex:0xpX8Q5Cgj3vKchm@cluster0.cb3i7l5.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
