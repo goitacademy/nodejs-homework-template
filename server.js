567ae73b787a7d44b9aab033ffc6f902293bd9f2
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-11-08 17:05:43 +0200

    step 1

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require('./app')
+import express from "express";
+
+const app = express();
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
