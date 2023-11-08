054a93e558b8eb85294695242ae6dc0db49455c6
Babenko Liudmyla <savelieva0509@gmail.com>
2023-11-08 14:38:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
   })
   .catch((error) => {
     console.log(error.message);
