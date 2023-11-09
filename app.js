3281fae7e2e8f77292c0b5316a90ea172e6b6e5e
Georgiy95 <urijpetrovskij@gmail.com>
2023-11-09 12:05:28 +0200

    commit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
 app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
