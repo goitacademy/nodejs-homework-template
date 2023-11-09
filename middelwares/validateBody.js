c39db6797fbbf96bd52d71d543adfbae0ea19b6f
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-11-09 10:17:54 +0200

    decoraties-validateBod

--- middelwares/validateBody.js
+++ middelwares/validateBody.js
@@ -3,12 +3,8 @@ const {HttpError} = require('../helpers')
 const validateBody = shema => {
     const func = (req, res, next)=> {        
       const {error} = shema.validate(req.body)
-      const numberOfKeys = Object.keys(req.body).length
-      if(error){
-        if(numberOfKeys === 3) {         
-          next(HttpError(400, "missing fields"))
-        }
-        next(HttpError(400, `missing required ${error.details[0].context.key} field`))
+      if(error){       
+          next(HttpError(400, error.message))
       } 
       next()
     }
