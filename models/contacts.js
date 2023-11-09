4f6f291b399714ee2b17161d6e69b02887631b2c
Alexey Luzanchuk <Luzanchuk123@gmail.com>
2023-11-09 11:05:08 +0200

    after rework commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,72 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const dataId = data.find((contact) => contact.id === contactId);
+  return dataId || null;
+}
 
-const addContact = async (body) => {}
+const updateContact = async (contactId, data) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  
+  if (index === -1) {
+    return null; 
+  }
+  
+  contacts[index] = { ...contacts[index], ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  
+  return contacts[index];
+};
 
-const updateContact = async (contactId, body) => {}
+
+async function removeContact(contactId) {
+  const data = await listContacts();
+  const index = data.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const newContacts = [...data.slice(0, index), ...data.slice(index + 1)];
+  await writeContacts(newContacts);
+
+  return data[index];
+}
+
+async function addContact({ name, email, phone }) {
+  const data = await listContacts();
+  const newContact = {
+    id: crypto.randomUUID(),
+    name,
+    email,
+    phone,
+  };
+
+  const newContacts = [...data, newContact];
+  await writeContacts(newContacts);
+
+  return newContact;
+}
+
+async function writeContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
   updateContact,
-}
+  removeContact,
+};
