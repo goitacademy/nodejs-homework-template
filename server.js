fe4a9759f6e0f970c851e533afba72b9a2402936
rasinoleg <rasinoleg3@gmail.com>
2023-11-09 13:33:07 +0200

    lesson-3

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+mongoose.connect(
+  "mongodb://mongodb+srv://rasinoleg3:wvFaCB0uwsGGuCH7@cluster0.32p2cwd.mongodb.net/db-contacts"
+);
