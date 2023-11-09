8e375a452827a5f10c5f4213b332c39aed1f9db4
NataDent <cherkashyna.n@gmail.com>
2023-11-09 18:13:12 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Nataly:mFid220821@cluster0.bjaedo8.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
