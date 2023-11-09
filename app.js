ad463cbb9a7df04c423e42a0d691e8d308b8c1e1
Georgiy95 <urijpetrovskij@gmail.com>
2023-11-09 12:02:09 +0200

    commit

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts');
-
 const app = express();
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger));
