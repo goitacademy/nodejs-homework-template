ce284928e08e26dace1be9c9b14fdb3380db8953
Skaro2336 <matveev.m0612@gmail.com>
2023-11-10 19:02:42 +0200

    hw-03

---
+++ helpers/validWrapper.js
@@ -0,0 +1,26 @@
+function validContact(schema) {
+    return (req, res, next) => {
+      const { error } = schema.validate(req.body);
+      if (error) {
+        return res.status(404).json({ message: "missing field" });
+      }
+  
+      return next();
+    };
+  }
+  
+  function validUpdateContact(schema) {
+    return (req, res, next) => {
+      const { error } = schema.validate(req.body);
+      if (error) {
+        return res.status(400).json({ message: error.message });
+      }
+  
+      return next();
+    };
+  }
+  
+  module.exports = {
+    validContact,
+    validUpdateContact,
+  };
