11526c81dd6af63a2fb0565ffb8761df576fd8d3
Aleksandr Borysenko <borysenko343@gmail.com>
2023-11-10 18:06:12 +0200

    add HW03

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
 
+
 const app = express();
+
+
+
+
+
+
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://borysenko343:bw9MUqkkf6cbMBvN@first-cluster.aupshba.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.massege);
+    process.exit(1);
+  });
