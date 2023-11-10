c5f261eaf94be8f481ee94bb962df914da7bb21c
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-11-10 09:18:21 -0500

    0000

--- app.js
+++ app.js
@@ -1,25 +1,149 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const Joi = require('joi');
+const fs = require('fs').promises;
+const path = require('path');
+const app = express();
 
-const contactsRouter = require('./routes/api/contacts')
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-const app = express()
+// Obtener la ruta absoluta del archivo 'contacts.json'
+const contactsFilePath = path.join(__dirname, 'contacts.json');
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// Middleware para validar el cuerpo de la solicitud
+function validateContact(contact) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+  });
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+  return schema.validate(contact);
+}
 
-app.use('/api/contacts', contactsRouter)
+// Ruta para obtener todos los contactos
+app.get('/api/contacts', async (req, res) => {
+  try {
+    const contactsData = await fs.readFile(contactsFilePath, 'utf8');
+    const contacts = JSON.parse(contactsData);
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
+// Ruta para obtener un contacto por ID
+app.get('/api/contacts/:id', async (req, res) => {
+  try {
+    const contactsData = await fs.readFile(contactsFilePath, 'utf8');
+    const contacts = JSON.parse(contactsData);
+    const contactId = req.params.id;
+    const contact = contacts.find((c) => c.id === parseInt(contactId));
+
+    if (!contact) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+
+    res.json(contact);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Ruta para agregar un nuevo contacto
+app.post('/api/contacts', async (req, res) => {
+  try {
+    const { error } = validateContact(req.body);
+
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
+    const contactsData = await fs.readFile(contactsFilePath, 'utf8');
+    const contacts = JSON.parse(contactsData);
+
+    const newContact = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 1,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+    };
+
+    contacts.push(newContact);
+
+    await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2), 'utf8');
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Ruta para eliminar un contacto por ID
+app.delete('/api/contacts/:id', async (req, res) => {
+  try {
+    const contactsData = await fs.readFile(contactsFilePath, 'utf8');
+    const contacts = JSON.parse(contactsData);
+
+    const contactId = req.params.id;
+    const contactIndex = contacts.findIndex((c) => c.id === parseInt(contactId));
+
+    if (contactIndex === -1) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+
+    contacts.splice(contactIndex, 1);
+
+    await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2), 'utf8');
+    res.json({ message: 'Contact deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Ruta para actualizar un contacto por ID
+app.put('/api/contacts/:id', async (req, res) => {
+  try {
+    const contactsData = await fs.readFile(contactsFilePath, 'utf8');
+    const contacts = JSON.parse(contactsData);
+
+    const contactId = req.params.id;
+    const contactIndex = contacts.findIndex((c) => c.id === parseInt(contactId));
+
+    if (contactIndex === -1) {
+      throw new Error('Not found');
+    }
+
+    const { error } = validateContact(req.body);
+
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
+    contacts[contactIndex] = {
+      id: contactId,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+    };
+
+    await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2), 'utf8');
+    res.json(contacts[contactIndex]);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Maneja las rutas no encontradas
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: 'Not found' });
+});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
-
-module.exports = app
+// Inicia el servidor en un puerto específico
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:3000${port}`);
+});
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,122 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs').promises; 
+const path = require('path');
+const Joi = require('joi');
+const router = express.Router();
 
-const router = express.Router()
+// Obtener la ruta absoluta del archivo 'contacts.json'
+const contactsFilePath = path.join(__dirname, 'contacts.json');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Función para cargar los datos desde contacts.json
+async function loadContacts() {
+  try {
+    const data = await fs.readFile(contactsFilePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    // Si el archivo no se encuentra, devuelve un array vacío
+    return [];
+  }
+}
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Función para guardar los datos en contacts.json
+async function saveContacts(contacts) {
+  await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2), 'utf8');
+}
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Middleware para validar el cuerpo de la solicitud
+function validateContact(contact) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+  });
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  return schema.validate(contact);
+}
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Cargar contactos iniciales
+let contacts;
 
-module.exports = router
+// Ruta para obtener todos los contactos
+router.get('/', async (req, res) => {
+  contacts = await loadContacts();
+  res.json(contacts);
+});
+
+// Ruta para obtener un contacto por ID
+router.get('/:id', async (req, res) => {
+  contacts = await loadContacts();
+  const contactId = req.params.id;
+  const contact = contacts.find((c) => c.id === parseInt(contactId));
+
+  if (!contact) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  res.json(contact);
+});
+
+// Ruta para agregar un nuevo contacto
+router.post('/', async (req, res) => {
+  contacts = await loadContacts();
+  const { error } = validateContact(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const newContact = {
+    id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 1,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+
+  contacts.push(newContact);
+  await saveContacts(contacts);
+  res.status(201).json(newContact);
+});
+
+// Ruta para eliminar un contacto por ID
+router.delete('/:id', async (req, res) => {
+  contacts = await loadContacts();
+  const contactId = req.params.id;
+  const contactIndex = contacts.findIndex((c) => c.id === parseInt(contactId));
+
+  if (contactIndex === -1) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  contacts.splice(contactIndex, 1);
+  await saveContacts(contacts);
+  res.json({ message: 'Contact deleted' });
+});
+
+// Ruta para actualizar un contacto por ID
+router.put('/:id', async (req, res) => {
+  contacts = await loadContacts();
+  const contactId = req.params.id;
+  const contactIndex = contacts.findIndex((c) => c.id === parseInt(contactId));
+
+  if (contactIndex === -1) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  const { error } = validateContact(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  contacts[contactIndex] = {
+    id: contacts[contactIndex].id,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+
+  await saveContacts(contacts);
+  res.json(contacts[contactIndex]);
+});
+
+module.exports = router;
