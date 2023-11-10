087ba2d043b70b17e76d9ddb63d61c616cb54c67
Iryna220 <ibiriszka951@gmail.com>
2023-11-10 20:29:03 +0100

    .env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://iryna220:M7skH5S02Xq3dTab@cluster0.gro6ykl.mongodb.net/?retryWrites=true&w=majority";
+
+const { DB_HOST } = process.env;
+
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
