b447ccfa1d4394710cf6818e5656e5c4b3f335b7
Aleksandr Borysenko <borysenko343@gmail.com>
2023-11-10 18:23:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
 
-
 const app = express();
-
-
-
-
-
-
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
