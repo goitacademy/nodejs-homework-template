11c062c456ac092639f1d9e719270cbf21249d39
Olena <petrova1512@gmail.com>
2023-11-11 15:19:45 +0100

    hw done

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,76 @@
+import contactsService from "../models/contacts.js";
+import { HttpError } from "../helpers/index.js";
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from "../schemas/contact-schema.js";
+
+const getAll = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+
+    console.log(error);
+    if (error) {
+      throw HttpError(400, `Missing required name field`);
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+    res.json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const { error } = contactUpdateSchema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contactsService.updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, `Not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { getAll, getById, add, deleteById, update };
