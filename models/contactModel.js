721f2dc044908e0cad971b532c6fc8c535479dcf
Andrii Chychkan <mywork1728@gmail>
2023-11-11 18:48:49 +0200

    update model

--- models/contactModel.js
+++ models/contactModel.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    min: [3, "too few symbols"],
-    max: 30,
-    required: [true, "User name required"],
+    required: [true, "Set name for contact"],
   },
   email: {
     type: String,
-    required: [true, "User name required"],
   },
   phone: {
     type: String,
-    required: [true, "User name required"],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
   },
 });
 
