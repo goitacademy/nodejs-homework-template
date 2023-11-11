03f3931da9019ca07c0889f8108391ce1bda293a
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2023-11-11 09:21:06 +0300

    done function

--- models/contacts.js
+++ models/contacts.js
@@ -27,26 +27,70 @@ async function getContactById(req, res, next) {
     try {
         const contact = await Contact.findById(contactId).exec();
         console.log(contact);
-      if(contact === null || contact === undefined){
-        return res.status(404).send({ message: "Not found" })
-      }
-      
-        res.send(contact)
+
+        if(contact === null || contact === undefined){
+          return res.status(404).send({ message: "Not found" })
+        }
+
+        res.send(contact);
     } catch (error) {
         next(error)
     }
 }
 
-async function removeContact(req, res, next){
-
-}
-
 async function addContact(req, res, next){
-
+  console.log('Handling POST /api/contacts request...');
+const contact = {
+  name: req.body.name,
+  email: req.body.email,
+  phone: req.body.phone,
+  favorite: req.body.favorite,
 }
 
+try {
+  const createContact = await Contact.create(contact);
+  console.log('Contact created:', createContact);
+    res.status(201).send(createContact);
+} catch (error) {
+  console.error('Error creating contact:', error);
+  next(error)
+}
+}
+
+async function removeContact(req, res, next){
+const {contactId} = req.params;
+
+try {
+  const result = await Contact.findByIdAndRemove(contactId)
+
+  if(result === null) {
+    return res.status(404).send({message: "Not found"})
+  }
+
+  res.status(200).send({message: "contact deleted"})
+  res.send({contactId})
+} catch (error) {
+  next(error)
+}
+}
+
+// Update Contact
 async function updateContact(req, res, next){
-    
+  const {contactId} = req.params;
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    favorite: req.body.favorite,
+  }
+
+  try {
+    const result = await Contact.findByIdAndUpdate(contactId, contact, { new: true})
+    console.log(result)
+    res.end()
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
