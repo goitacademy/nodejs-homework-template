3f630fd9b81d4248068c609be332acd5d35a5f2c
AdamSkubida <adasiow.s@gmail.com>
2023-11-12 16:57:56 +0100

    features/getAllContacts, getContactById,createContact,updateContact, updateStatus, removeContact

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+const routerApi = require("./api/index.js");
+
+const { DB_HOST: uriDb } = process.env;
+
+const connection = mongoose.connect(uriDb);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", routerApi);
+
+app.use((req, res) => {
+  res.status(404).json({ message: `whooops - ${req.path}` });
 });
+
+app.use((err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    res.status(400).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function startServer() {
+  try {
+    await connection;
+    console.log("DB connected");
+    app.listen(3000, function () {
+      console.log("API listening");
+    });
+  } catch (err) {
+    console.log("DB not connected, shutting down");
+    process.exit(1);
+  }
+}
+
+startServer();
