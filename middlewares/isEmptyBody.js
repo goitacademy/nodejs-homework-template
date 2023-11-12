7da2c330dfd80471fa8b066affad79cbbe81c07c
Volodymyr <vladikon70@gmail.com>
2023-11-12 17:34:31 +0100

    update files

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -2,8 +2,19 @@ const { HttpError } = require("../helpers");
 
 const isEmptyBody = async (req, res, next) => {
   const keys = Object.keys(req.body);
-  if (!keys.length) {
-    return next(HttpError(400, "Missing required field names"));
+  const field = [];
+  if (!keys.find((item) => item === "name")) field.push("name");
+  if (!keys.find((item) => item === "email")) field.push("email");
+  if (!keys.find((item) => item === "phone")) field.push("phone");
+  if (keys.length === 0) {
+    return next(HttpError(400, "All required field names are missing"));
+  } else if (keys.length < 3) {
+    return next(
+      HttpError(
+        400,
+        `Missing required '${field}' ${keys.length === 1 ? "fields" : "field"}`
+      )
+    );
   }
   next();
 };
