88f3c1c7b506e9f202af0a85dff6910e5b98ce2d
Romano1994o <135154365+Romano1994o@users.noreply.github.com>
2023-11-12 22:36:40 +0100

    finished

--- validation/contacts.js
+++ validation/contacts.js
@@ -4,11 +4,13 @@ const contactValidationchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean().default(false),
 });
 
 
 
 module.exports = {
   contactValidationchema,
+ 
   
 };
