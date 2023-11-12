7e283c70ff7d5c93f958d9bcab9efb2a0857a182
KatanES <businesskatan.es@gmail.com>
2023-11-12 22:09:26 +0100

    security finish

--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://Katan:3dTo7WFoKceVTvZa@cluster0.phlp8w3.mongodb.net/contacts_book?retryWrites=true&w=majority";
-
-module.exports = {
-  DB_HOST,
-};
--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 });
-
-mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
