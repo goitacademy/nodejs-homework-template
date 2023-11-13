31e32746f6a6003e9ba470d8a72a90d050067f19
Marini23 <marina.smile1m@gmail.com>
2023-11-13 20:12:52 +0100

    3

--- schemas/contact-shemas.js
+++ schemas/contact-shemas.js
@@ -10,16 +10,11 @@ export const contactAddSchema = Joi.object({
       "any.required": `missing required name field`,
       "string.pattern.base": `Name may contain only letters, apostrophe, dash, and spaces.`,
     }),
-  email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-    })
-    .required()
-    .messages({
-      "any.required": `missing required email field`,
-      "string.email":
-        "Please provide a valid email address with at least two domain segments",
-    }),
+  email: Joi.string().email().required().messages({
+    "any.required": `missing required email field`,
+    "string.email":
+      "Please provide a valid email address with at least two domain segments",
+  }),
   phone: Joi.string()
     .required()
     .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
@@ -30,7 +25,20 @@ export const contactAddSchema = Joi.object({
 });
 
 export const contactUpdateSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
+  name: Joi.string()
+    .min(2)
+    .max(20)
+    .pattern(new RegExp(/^[A-Za-z\s'-]+$/))
+    .messages({
+      "string.pattern.base": `Name may contain only letters, apostrophe, dash, and spaces.`,
+    }),
+  email: Joi.string().email().messages({
+    "string.email":
+      "Please provide a valid email address with at least two domain segments",
+  }),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .messages({
+      "string.pattern.base": `Phone number must be in the format (000) 000-0000`,
+    }),
 });
