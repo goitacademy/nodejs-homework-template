35d8c485075a35f4a56fd40015a324652f943e96
OsMax <www.osmax@gmail.com>
2023-11-13 15:25:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://OsMax:qAz123698745@cluster0.zlbj2ha.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
     console.log("Database connect success");
