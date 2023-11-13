8ffc7fa5f4dddc3a1f5f895f1a7774d07e1340e5
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-11-13 14:14:38 +0200

    ашч

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,8 @@
 const HttpError = require("./HttpError");
 const cntrlWrapper = require("./ctrlWrapper");
+const handleMongooseError = require("./handlesMongoose.Error");
 module.exports = {
   HttpError,
   cntrlWrapper,
+  handleMongooseError,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 const controllers = require("../../controler/contacts");
-const { validate } = require("../../middelwars");
-const schema = require("../../shemas/contacts");
+const { validateBody, isValidId, updateFavorite } = require("../../middelwars");
+const { addSchema } = require("../../models/contact");
 router.get("/", controllers.getAll);
 
-router.get("/:id", controllers.getById);
+router.get("/:id", isValidId, controllers.getById);
 
-router.post("/", validate(schema.addSchema), controllers.add);
+router.post("/", validateBody(addSchema), controllers.add);
 
-router.delete("/:id", controllers.deleteById);
+router.delete("/:id", isValidId, controllers.deleteById);
 
-router.put("/:id", validate(schema.addSchema), controllers.updateById);
-
+router.put("/:id", isValidId, validateBody(addSchema), controllers.updateById);
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(updateFavorite),
+  controllers.updateFavorite
+);
 module.exports = router;
