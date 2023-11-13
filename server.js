ff614d9ee3b32bf9dba4a424aa79091eeeaa060a
pnyekrasov <pnekrasov1910@gmail.com>
2023-11-14 01:25:31 +0200

    HW 5

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 
 const DB_URI =
   "mongodb+srv://pnekrasov1910:o3rWsBBXa5OTeEIl@cluster0.wfpzyqr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+  
+  // const DB_URI = process.env.DB_URI;
 
 mongoose
   .connect(DB_URI)
