395d89160797a3f7aff6b3f75170e24d75d96c7a
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-11-14 00:32:46 +0100

    add .env

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const DB_HOST =
-  "mongodb+srv://Anatolii:adminLocalHost05@cluster0.0yqagxt.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://Anatolii:adminLocalHost05@cluster0.0yqagxt.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-module.exports = {
-  DB_HOST,
-};
+// module.exports = {
+//   DB_HOST,
+// };
