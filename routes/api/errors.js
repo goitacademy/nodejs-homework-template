307d05261837e17443f1e35d5d98810f423ff0f5
Анна Котляр <kotlyaranya1771@gmail.com>
2023-11-13 18:35:37 +0200

    start hw3

--- routes/api/errors.js
+++ routes/api/errors.js
@@ -4,16 +4,19 @@ module.exports = {
     error.status = 400;
     return error;
   },
+
   unauthorized: (message = 'Unauthorized') => {
     const error = new Error(message);
     error.status = 401;
     return error;
   },
+
   forbidden: (message = 'Forbidden') => {
     const error = new Error(message);
     error.status = 403;
     return error;
   },
+  
   notFound: (message = 'Not found') => {
     const error = new Error(message);
     error.status = 404;
