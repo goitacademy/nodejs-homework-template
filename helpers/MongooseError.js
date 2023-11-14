80c0baddc18b3e37597fce99b285fd05c47f52cb
pnyekrasov <pnekrasov1910@gmail.com>
2023-11-14 21:28:12 +0200

    Update MongooseError.js

--- helpers/MongooseError.js
+++ helpers/MongooseError.js
@@ -1,6 +1,6 @@
 const MongooseError = (err, data, next) => {
   const { name, code } = err;
-  const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  const status = (name === "MongoServerError" && code === 11000) ? 409 : 400;
   err.status = status;
   next();
 };
