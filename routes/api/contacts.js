5aacef0b0c0c608cf69e9b9fbe8d9c909509585e
ilesyk <124560323+ilesyk@users.noreply.github.com>
2023-11-14 14:32:40 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,12 +12,12 @@ const router = express.Router();
 
 const validateContact = (data) => {
   const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required(),
+    name: Joi.string().required().error(new Error('Missing required name field')),
+    email: Joi.string().email().required().error(new Error('Missing required email field')),
+    phone: Joi.string().required().error(new Error('Missing required phone field')),
   });
 
-  return schema.validate(data);
+  return schema.validate(data, { abortEarly: false });
 };
 
 router.get("/", async (req, res) => {
@@ -37,18 +37,14 @@ router.get("/:contactId", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateContact(req.body);
+  try {
+    await validateContact(req.body);
 
-  if (error) {
-    const errorMessage = error.details
-      .map((detail) => detail.message.split('"').join(""))
-      .join(", ");
-    res.status(400).json({ message: `Missing fields ${errorMessage}` });
-    return;
+    const newContact = await addContact({ ...req.body, id: uniqueId() });
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-
-  const newContact = await addContact(req.body);
-  res.status(201).json(newContact);
 });
 
 router.delete("/:contactId", async (req, res) => {
@@ -66,28 +62,24 @@ router.delete("/:contactId", async (req, res) => {
 router.put("/:contactId", async (req, res) => {
   const { contactId } = req.params;
 
-  if (!req.body) {
+  if (Object.keys(req.body).length === 0) {
     res.status(400).json({ message: "Missing fields" });
     return;
   }
 
-  const { error } = validateContact(req.body);
+  try {
+    await validateContact(req.body);
 
-  if (error) {
-    const errorMessage = error.details
-      .map((detail) => detail.message.split('"').join(""))
-      .join(", ");
-    res.status(400).json({ message: `Missing fields ${errorMessage}` });
-    return;
-  }
+    const updatedFields = req.body;
+    const updatedContact = await updateContact(contactId, updatedFields);
 
-  const updatedFields = req.body;
-  const updatedContact = await updateContact(contactId, updatedFields);
-
-  if (updatedContact) {
-    res.json(updatedContact);
-  } else {
-    res.status(404).json({ message: "Not found" });
+    if (updatedContact) {
+      res.json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
