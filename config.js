bce36707465d7bffb33ad04bfea420ad2baea7c1
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-11-14 12:49:33 +0200

    deploy

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = 'mongodb+srv://Nik:RnbmTjkUhEKh8QpQ@cluster0.ipbnpbn.mongodb.net/contacts_reader'
+
+module.exports = {
+    DB_HOST
+}
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require('./app')
 
+// RnbmTjkUhEKh8QpQ
+const mongoose = require('mongoose')
+const DB_HOST = 'mongodb+srv://Nik:RnbmTjkUhEKh8QpQ@cluster0.ipbnpbn.mongodb.net/contacts_reader'
+// const {DB_HOST} = require('./config')
+mongoose.connect(DB_HOST)
+.then(() => 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+  console.log("Database connection successful")
 })
+)
+.catch(error => {
+  console.log(error.message)
+  process.exit(1)
+})
+
+
+
+
