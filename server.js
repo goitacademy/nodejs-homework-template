a7f8480c2339f9ff01e4891c0b85989ceafb9ced
10Artur <artyr11012002@gmail.com>
2023-11-14 21:25:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST = process.env.DB_HOST;
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server running. Use our API on port: 3000");
+        });
+        console.log("Database connection successful");
+    })
+    .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+    });
