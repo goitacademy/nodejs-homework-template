8735eb34324adb217f7329c99f169c8bfbc3b178
pnyekrasov <pnekrasov1910@gmail.com>
2023-11-15 00:16:03 +0200

    update

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-require("dotenv").config();
-
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
