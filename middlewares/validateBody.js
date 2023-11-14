ff0c103d54af85fb9972b4fe709e5f4579973a00
OsMax <www.osmax@gmail.com>
2023-11-14 12:33:00 +0200

    Update validateBody.js

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,6 @@ const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      console.log(error);
       next(HttpError(400, error.message));
     }
     next();
