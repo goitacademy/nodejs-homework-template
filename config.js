8ef63a049e4be0fbbf1bf0afaeb974b02d950e18
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-11-14 21:49:20 +0200

    add-keys-DB_HOST

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DB_HOST = 'mongodb+srv://Nik:RnbmTjkUhEKh8QpQ@cluster0.ipbnpbn.mongodb.net/contacts_reader'
+const DB_HOST = 'mongodb+srv://Nik:RnbmTjkUhEKh8QpQ@cluster0.ipbnpbn.mongodb.net/contacts_reader?retryWrites=true&w=majority'
 
 module.exports = {
     DB_HOST
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require('./app')
 
 // RnbmTjkUhEKh8QpQ
+// console.log(process.env)
 const mongoose = require('mongoose')
-const DB_HOST = 'mongodb+srv://Nik:RnbmTjkUhEKh8QpQ@cluster0.ipbnpbn.mongodb.net/contacts_reader'
+const DB_HOST = 'mongodb+srv://Nik:RnbmTjkUhEKh8QpQ@cluster0.ipbnpbn.mongodb.net/contacts_reader?retryWrites=true&w=majority'
 // const {DB_HOST} = require('./config')
+// const {DB_HOST} = process.env
 mongoose.connect(DB_HOST)
 .then(() => 
 app.listen(3000, () => {
