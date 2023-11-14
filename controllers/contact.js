dcf4e5b7cc05ab4a2adff8ecc198f54220a97363
Rhilim <arhilim@gmail.com>
2023-11-14 09:01:02 -0600

    joi

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,5 +1,13 @@
 const { isValidObjectId } = require("mongoose");
+const Joi = require('joi');
 const Contact = require("../models/contact");
+
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean().required(),
+});
 
 async function validateContactId(req, res, next) {
   const { contactId } = req.params;
@@ -33,12 +41,19 @@ async function getContact(req, res, next) {
 }
 
 async function createContact(req, res, next) {
+  const { error } = contactSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
   const contact = {
     name: req.body.name,
     email: req.body.email,
     phone: req.body.phone,
     favorite: Boolean(req.body.favorite),
   };
+
   try {
     const result = await Contact.create(contact);
     console.log(result);
