7407e68e61a7c8aac59b180efc9a2ade225e82f4
Arsher-Elena <lenashmigol02@gmail.com>
2023-11-15 23:28:37 +0200

    config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Elena:sZ9XF5yfqNf5Ydn4@cluster0.slyftqg.mongodb.net/contacts_book?retryWrites=true&w=majority"
+const {DB_HOST} = require("./config")
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
