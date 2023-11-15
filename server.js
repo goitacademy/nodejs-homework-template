9b7463a291abba475d2e59c792cd426cabc15275
Yevhen Sydorov <eugene.sidorov@me.com>
2023-11-15 15:32:52 +0100

    add port

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
     console.log("Database connection successful");
   })
