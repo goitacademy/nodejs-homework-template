66077cbd852341caccde1858aa8b58816602466b
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-11-15 20:02:52 +0200

    end

--- models/contacts.js
+++ models/contacts.js
@@ -1,25 +1,30 @@
 const { Schema, model } = require("mongoose");
-
-const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Set name for contact"],
-    },
-    email: {
-      type: String,
-      required: [true, "Set email for contact"],
-    },
-    phone: {
-      type: String,
-      required: [true, "Set phone for contact"],
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
   },
-  { versionKey: false, timestamps: true }
-);
+  email: {
+    type: String,
+    required: [true, "Set email for contact"],
+  },
+  phone: {
+    type: String,
+    required: [true, "Set phone for contact"],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+contactSchema.statics.listContacts = async function () {
+  try {
+    const contacts = await this.find();
+    return contacts;
+  } catch (error) {
+    throw new Error("Error fetching contacts");
+  }
+};
+
 const Contact = model("contact", contactSchema);
 module.exports = Contact;
