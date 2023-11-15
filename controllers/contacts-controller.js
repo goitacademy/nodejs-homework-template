d3442edd145decc93685540a1aec3e9770f81454
Yevhen Sydorov <eugene.sidorov@me.com>
2023-11-15 15:56:57 +0100

    add mongoose method

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,4 +1,4 @@
-import * as contactsService from "../models/contacts.js";
+import Contact from "../models/Contact.js";
 import { HttpError } from "../helpers/index.js";
 import {
   contactAddSchema,
@@ -7,70 +7,70 @@ import {
 
 export const getAllContacts = async (req, res, next) => {
   try {
-    const result = await contactsService.listContacts();
+    const result = await Contact.find();
     res.json(result);
   } catch (error) {
     next(error);
   }
 };
 
-export const getById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.getContactById(contactId);
+// export const getById = async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+//     const result = await contactsService.getContactById(contactId);
 
-    if (!result) {
-      throw HttpError(404, `Movie with id=${contactId} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+//     if (!result) {
+//       throw HttpError(404, `Movie with id=${contactId} not found`);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const add = async (req, res, next) => {
-  try {
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
+// export const add = async (req, res, next) => {
+//   try {
+//     const { error } = contactAddSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
 
-    const result = await contactsService.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+//     const result = await contactsService.addContact(req.body);
+//     res.status(201).json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const updateById = async (req, res, next) => {
-  try {
-    const { error } = contactUpdateSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contactsService.updateContactById(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${contactId} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+// export const updateById = async (req, res, next) => {
+//   try {
+//     const { error } = contactUpdateSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const { contactId } = req.params;
+//     const result = await contactsService.updateContactById(contactId, req.body);
+//     if (!result) {
+//       throw HttpError(404, `Movie with id=${contactId} not found`);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const deleteById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.deleteContactContact(contactId);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${contactId} not found`);
-    }
+// export const deleteById = async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+//     const result = await contactsService.deleteContactContact(contactId);
+//     if (!result) {
+//       throw HttpError(404, `Movie with id=${contactId} not found`);
+//     }
 
-    res.json({
-      message: "Delete success",
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.json({
+//       message: "Delete success",
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
