726b288a0077d86933324b66f2700a21000a5e61
Arsher-Elena <lenashmigol02@gmail.com>
2023-11-15 22:28:48 +0200

    init

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST = "mongodb+srv://Elena:sZ9XF5yfqNf5Ydn4@cluster0.slyftqg.mongodb.net/contacts_book?retryWrites=true&w=majority"
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000)
+})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
 })
