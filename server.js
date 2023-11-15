ee867913b8059afe71e8ba0afa0be7caa9b4e44f
NataDent <cherkashyna.n@gmail.com>
2023-11-15 17:05:49 +0200

    Corrected connect in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
