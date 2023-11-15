6b4b649766e339689a87a9e8056448dd2b4e4ab7
oldstuden <atikin3002@gmail.com>
2023-11-15 19:13:35 +0100

    add castom message

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -36,7 +36,7 @@ const deleteById = async (req, res) => {
   if (!result) {
     throw HttpError(404, `Contact with id = ${id} not found`);
   }
-  res.json({ message: 'Delete success' });
+  res.json({ message: 'contact deleted' });
 };
 export default {
   getAll: ctrlContactWrapper(getAll),
