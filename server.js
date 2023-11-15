81e53f35f315612fac9a7bab0f3a9d87d3d17ff3
NataDent <cherkashyna.n@gmail.com>
2023-11-15 17:04:35 +0200

    Corrected server.js

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
