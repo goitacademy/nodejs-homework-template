5f3a385563144e8e6675f54e36bcbb219cac0983
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-11-16 21:51:31 +0200

    add schema

--- schems/contacts-schems.js
+++ schems/contacts-schems.js
@@ -1,4 +1,24 @@
 const Joi = require("joi");
+const { Schema } = require("mongoose");
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 const addContactChema = Joi.object({
   name: Joi.string()
@@ -12,15 +32,22 @@ const addContactChema = Joi.object({
   phone: Joi.string()
     .required()
     .messages({ "any.required": `missing required "phone" field` }),
+  favorite: Joi.bool(),
 });
 
 const updateContactChema = Joi.object({
   name: Joi.string().min(3).max(15),
   email: Joi.string(),
   phone: Joi.string(),
+  favorite: Joi.bool(),
+});
+const updateFavoriteChema = Joi.object({
+  favorite: Joi.bool(),
 });
 
 module.exports = {
   addContactChema,
   updateContactChema,
+  updateFavoriteChema,
+  contactSchema,
 };
