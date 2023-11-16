e87d43f33f778cfa2e2fa64ad49c87616afb5d4b
Arsher-Elena <lenashmigol02@gmail.com>
2023-11-16 07:30:37 +0200

    server

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const app = require('./app')
 
 const {DB_HOST, PORT = 3000} = process.env;
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
-.then(() => {
-  app.listen(PORT)
-})
-.catch(error => {
-  console.log(error.message);
-  process.exit(1);
+    .then(() => 
+    app.listen(PORT, () => {
+      console.log("Database connection successful")
+    })
+    )
+    .catch(error => {
+      console.log(error.message)
+      process.exit(1)
 })
