09aac198173edebfdc1641e63ae76aa946573c50
NataDent <cherkashyna.n@gmail.com>
2023-11-16 16:38:14 +0200

    Fixed validateBody

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -1,10 +1,10 @@
 import { HttpError } from "../helpers/index.js";
 
 const validateBody = (schema) => {
-  const func = (req, _, next) => {
-    if (Object.keys(req.body).length === 0) {
-      next(HttpError(400, "missing fields"));
-    }
+  const func = (req, res, next) => {
+    // if (Object.keys(req.body).length === 0) {
+    //   next(HttpError(400, "missing fields"));
+    // }
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
