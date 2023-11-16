b8564129e1e11e35cf6777364ee112193c31aa9b
AnastasiiaMarchuk <stasijushka5@gmail.com>
2023-11-16 21:23:28 +0200

    env

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-const { DB_HOST } = process.env.DB_HOST;
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set('strictQuery', true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
-mongoose.set('strictQuery', true);
-
-// app.listen(3000, () => {
-//   console.log('Server running. Use our API on port: 3000');
-// });
