cfe3b24825e0ea5dad8d0c47d5acedbe93684f2a
Andrii Chychkan <mywork1728@gmail>
2023-11-16 23:23:43 +0200

    added email checkout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,13 @@
-const user = require("../models/user");
+const User = require("../models/user");
 
 async function register(req, res, next) {
   const { email, password } = req.body;
   try {
-    await user.create({ email, password });
+    const user = await User.findOne({ email: email }).exec();
+    if (user !== null) {
+      return res.status(409).send({ message: "user already register!" });
+    }
+    await User.create({ email, password });
     res.status(201).send({ message: "registration successfull" });
   } catch (error) {
     next(error);
