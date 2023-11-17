d5d254bf82ce43e3411e9e933e3a9877808c95f6
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-11-17 21:15:41 +0200

    destr

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,11 +1,11 @@
 // const moviesServise = require("../models/contacts.js");
 const HttpErr = require("../helpers/HttpError.js");
 // const contactChema = require("../schems/contacts-schems.js");
-const contactSchrems = require("../models/Contact.js");
+const { Contact, addContactChema } = require("../models/Contact.js");
 
 const getAllContacts = async (req, res, next) => {
   try {
-    const result = await contactSchrems.Contact.find();
+    const result = await Contact.find();
     res.status(200).json(result);
   } catch (error) {
     next(error);
@@ -15,7 +15,7 @@ const getAllContacts = async (req, res, next) => {
 const getContactById = async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactSchrems.Contact.findById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
       throw HttpErr(404, `Contact with id ${contactId} not found`);
     }
@@ -27,29 +27,29 @@ const getContactById = async (req, res, next) => {
 
 const addNewContact = async (req, res, next) => {
   try {
-    const { error } = contactSchrems.addContactChema.validate(req.body);
+    const { error } = addContactChema.validate(req.body);
     if (error) {
       throw HttpErr(404, error.message);
     }
-    const result = await contactSchrems.Contact.create(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 };
 
-// const deleteById = async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const result = await moviesServise.removeContact(contactId);
-//     if (!result) {
-//       throw HttpErr(404, `Contact with id ${contactId} not found`);
-//     }
-//     res.status(201).json({ message: "Contact deleted" });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const deleteById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndDelete(contactId);
+    if (!result) {
+      throw HttpErr(404, `Contact with id ${contactId} not found`);
+    }
+    res.status(201).json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
 
 // const updateContact = async (req, res, next) => {
 //   try {
@@ -70,6 +70,6 @@ module.exports = {
   getAllContacts,
   getContactById,
   addNewContact,
-  // deleteById,
+  deleteById,
   // updateContact,
 };
