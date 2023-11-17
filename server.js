dbd3cca7bf7f43fbfd9fe2a15109c62ff980b231
ilesyk <pavloyavorskyi@ukr.net>
2023-11-17 15:38:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST ="mongodb+srv://dreas:<password>@clustertest.p3vvkvv.mongodb.net/?retryWrites=true&w=majority"
+const DB_HOST = "mongodb+srv://dreas:vertigo1994@clustertest.p3vvkvv.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(DB_HOST).then(()=> app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
 })).catch(error => {
