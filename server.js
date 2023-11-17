9861dfd164146def5058973f54070f4178c7e6b5
Kate Zaharova <15071981ktz@gmail.com>
2023-11-17 20:35:17 +0200

    added engines to package.json

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 import app from "./app.js";
+import mongoose from "mongoose";
 
+//tXqh9e02unoMColZ
 
-app.listen(3000, () => {
+const DB_HOST="mongodb+srv://Kate:tXqh9e02unoMColZ@cluster0.a4kfnzb.mongodb.net/My-Contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000")
 })
+  })
+  .catch(error => { 
+    process.exit(1);
+    console.log(error.message);
+  })
+
+
+
