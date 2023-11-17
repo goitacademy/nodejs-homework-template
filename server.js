ffefa8d5dbbb364ec89a28c9fc8a03eb6520a513
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-11-18 00:19:34 +0100

    copy after deploy

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ import mongoose from 'mongoose';
 
 import app from './app.js';
 
-const DB_HOST =
-	'mongodb+srv://jinjerana:viZ2lSJHi7imw1ip@cluster0.bqbpun5.mongodb.net/my-contacts?retryWrites=true&w=majority';
+const { DB_HOST, PORT } = process.env;
 
 mongoose
 	.connect(DB_HOST)
 	.then(() => {
-		app.listen(3000, () => {
-			console.log('Server running. Use our API on port: 3000');
+		app.listen(PORT, () => {
+			console.log(`Server running. Use our API on port: ${PORT}`);
 		});
 	})
 	.catch((error) => {
