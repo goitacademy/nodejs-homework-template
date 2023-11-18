690fa8ed91891e533ad475e9686905454ec743b9
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-11-18 20:14:39 +0200

    s

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,7 +1,4 @@
 const handleMongooseError = (error, data, next)=>{
-    console.log('ddddddddddddd', error)
-    console.log('fffffffffffff', data)
-    console.log('ddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
     error.status = 400
     next()
 }
