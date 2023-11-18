bdb6236205bbe32b4af97d598a35bdbea7e41c51
Andrii Chychkan <mywork1728@gmail>
2023-11-18 11:17:42 +0200

    added loginization

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
+//register function
 async function register(req, res, next) {
   const { email, password } = req.body;
   try {
@@ -7,11 +9,34 @@ async function register(req, res, next) {
     if (user !== null) {
       return res.status(409).send({ message: "user already register!" });
     }
-    await User.create({ email, password });
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    await User.create({ email, password: passwordHash });
     res.status(201).send({ message: "registration successfull" });
   } catch (error) {
     next(error);
   }
 }
 
-module.exports = { register };
+//login function
+async function login(req, res, next) {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }).exec();
+    if (user === null) {
+      return res.status(401).send({ message: "email is incorrect" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch === false) {
+      return res.status(401).send({ message: "password is incorrect" });
+    }
+
+    return res.send({ token: "TOKEN" });
+  } catch (error) {
+    next(error);
+  }
+  res.send("OK");
+}
+module.exports = { register, login };
--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,6 @@ const jsonParser = express.json();
 const AunthController = require("../controllers/authController");
 
 router.post("/user/register", jsonParser, AunthController.register);
+router.post("/user/login", jsonParser, AunthController.login);
 
 module.exports = router;
