972387dffe9f933395d13c38cc6b0acd276ddee4
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-11-19 14:56:27 +0100

    added file tmp and now new avatars are stored there

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,14 +1,22 @@
 import { User } from "#models/User.js";
 import Jimp from "jimp";
 import path from "path";
+import fs from "fs/promises";
 
 export const updateAvatar = async (req, res, next) => {
   const { userId } = req.user;
   const { file } = req;
 
   try {
+    console.log(file);
+    const currentFilePath = new URL(import.meta.url).pathname;
+    const currentDir = path.dirname(currentFilePath);
+
     const imagePath = file.path;
-    const outputImagePath = path.join(__dirname, "../../public/avatars", file.filename);
+    const outputDir = path.join(currentDir, "../../public/avatars");
+    const outputImagePath = path.join(outputDir, file.filename);
+
+    await fs.mkdir(outputDir, { recursive: true });
 
     const image = await Jimp.read(imagePath);
     await image.resize(250, 250).write(outputImagePath);
