1e15d5f57c3fca0ecfa7a1152777f3ff1c0c6730
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-11-20 11:24:48 +0200

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,19 +4,32 @@ const contactsServices = require("../../models/contacts");
 
 const router = express.Router();
 const schema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "string.base": "Name should be a string",
+    "any.required": "Name is required",
+  }),
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
+    .email({ minDomainSegments: 2, tlds: { allow: ["com"] } })
+    .required()
+    .messages({
+      "string.base": "Email should be a string",
+      "string.email": "Email format is 'example@mail.com'",
+      "any.required": "Email is required",
+    }),
   phone: Joi.string()
     .pattern(/^\+380 \d{2} \d{3} \d{4}$/)
-    .required(),
+    .required()
+    .messages({
+      "string.base": "Phone should be a string",
+      "string.pattern.base": "Phone format is '+380 XX XXX XXXX'",
+      "any.required": "Phone is required",
+    }),
 });
 
 router.get("/", async (req, res, next) => {
   try {
     const data = await contactsServices.listContacts();
-    res.status(200).json({ data });
+    res.status(200).json(data);
   } catch (error) {
     next(error);
   }
@@ -33,7 +46,7 @@ router.get("/:contactId", async (req, res, next) => {
       throw error;
     }
 
-    res.status(200).json({ data });
+    res.status(200).json(data);
   } catch (error) {
     next(error);
   }
@@ -41,16 +54,19 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { error: validationResult } = schema.validate(req.body);
+    const { error: validationResult } = schema.validate(req.body, { abortEarly: false });
 
     if (validationResult) {
-      const error = new Error(validationResult.message);
+      const errorMessage = validationResult.details
+        .map((detail) => detail.message)
+        .join(". ");
+      const error = new Error(errorMessage);
       error.status = 400;
       throw error;
     }
 
     const data = await contactsServices.addContact(req.body);
-    res.status(201).json({ data });
+    res.status(201).json(data);
   } catch (error) {
     next(error);
   }
@@ -75,10 +91,13 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const { error: validationResult } = schema.validate(req.body);
+    const { error: validationResult } = schema.validate(req.body, { abortEarly: false });
 
     if (validationResult) {
-      const error = new Error(validationResult.message);
+      const errorMessage = validationResult.details
+        .map((detail) => detail.message)
+        .join(". ");
+      const error = new Error(errorMessage);
       error.status = 400;
       throw error;
     }
@@ -92,7 +111,7 @@ router.put("/:contactId", async (req, res, next) => {
       throw error;
     }
 
-    res.status(200).json({ data });
+    res.status(200).json(data);
   } catch (error) {
     next(error);
   }
