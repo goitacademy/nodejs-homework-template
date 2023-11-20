53c2194ab2aaf584d02ae2efb2b867bb2da6a070
Yanka28 <yanachepeluk@ukr.net>
2023-11-20 22:45:56 +0200

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // mg9zDJTBLTvL3K2X;
+// mongodb+srv://Ivanna:mg9zDJTBLTvL3K2X@cluster0.2cyfzlb.mongodb.net/my-contacts?retryWrites=true&w=majority
 
 import mongoose from 'mongoose';
 import app from './app.js';
-const DB_HOST =
-  'mongodb+srv://Ivanna:mg9zDJTBLTvL3K2X@cluster0.2cyfzlb.mongodb.net/my-contacts?retryWrites=true&w=majority';
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
