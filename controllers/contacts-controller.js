64d4ff2a464c84f321fcbef3c5ca7b31a35ed2c0
Yevhen Sydorov <eugene.sidorov@me.com>
2023-11-20 21:10:34 +0100

    update controllers and add Contact

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,65 +1,49 @@
 import Contact from "../models/Contact.js";
-import { HttpError } from "../helpers/index.js";
-import {
-  contactAddSchema,
-  contactUpdateSchema,
-} from "../schemas/contacts-schema.js";
 
-export const getAllContacts = async (req, res, next) => {
-  try {
-    const result = await Contact.find();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+import { ctrlWrapper } from "../decorators/index.js";
+
+import { HttpError } from "../helpers/index.js";
+
+const getAllContacts = async (req, res, next) => {
+  const result = await Contact.find({});
+  res.json(result);
 };
 
-// export const getById = async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const result = await contactsService.getContactById(contactId);
+const getById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
 
-//     if (!result) {
-//       throw HttpError(404, `Movie with id=${contactId} not found`);
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+  if (!result) {
+    throw HttpError(404, `Contact with id=${contactId} not found`);
+  }
+  res.json(result);
+};
 
-// export const add = async (req, res, next) => {
-//   try {
-//     const { error } = contactAddSchema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
+const add = async (req, res, next) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
 
-//     const result = await contactsService.addContact(req.body);
-//     res.status(201).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+const updateById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${contactId} not found`);
+  }
 
-// export const updateById = async (req, res, next) => {
-//   try {
-//     const { error } = contactUpdateSchema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
-//     const { contactId } = req.params;
-//     const result = await contactsService.updateContactById(contactId, req.body);
-//     if (!result) {
-//       throw HttpError(404, `Movie with id=${contactId} not found`);
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+  res.json(result);
+};
 
-// export const deleteById = async (req, res, next) => {
+const updateFavorite = async (req, res) => {
+  const { id } = req.params;
+  const data = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!data) {
+    throw HttpError(400, "missing field favorite");
+  }
+  res.json(data);
+};
+
+//  const deleteById = async (req, res, next) => {
 //   try {
 //     const { contactId } = req.params;
 //     const result = await contactsService.deleteContactContact(contactId);
@@ -74,3 +58,11 @@ export const getAllContacts = async (req, res, next) => {
 //     next(error);
 //   }
 // };
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  updateFavorite: ctrlWrapper(updateFavorite),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,46 @@
 import express from "express";
 
-import * as contactsController from "../../controllers/contacts-controller.js";
+import contactsController from "../../controllers/contacts-controller.js";
 
-import { isEmptyBody } from "../../middlewares/index.js";
+import { isEmptyBody, isValidId } from "../../middlewares/index.js";
+
+import { validateBody } from "../../decorators/index.js";
+
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+  updateFavoriteSchema,
+} from "../../models/Contact.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAllContacts);
 
-// contactsRouter.get("/:contactId", contactsController.getById);
+contactsRouter.get("/:contactId", isValidId, contactsController.getById);
 
-// contactsRouter.post("/", isEmptyBody, contactsController.add);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactAddSchema),
+  contactsController.add
+);
 
-// contactsRouter.delete("/:contactId", contactsController.deleteById);
+// contactsRouter.delete("/:contactId", isValidId, contactsController.deleteById);
 
-// contactsRouter.put("/:contactId", isEmptyBody, contactsController.updateById);
+contactsRouter.put(
+  "/:contactId",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactUpdateSchema),
+  contactsController.updateById
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBody,
+  validateBody(updateFavoriteSchema),
+  contactsController.updateFavorite
+);
 
 export default contactsRouter;
