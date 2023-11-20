8c14acbcf3f6f54cfc9ccbc3ee9766fd9a001d3f
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-11-20 14:59:34 +0200

    add branch

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,88 +1,58 @@
-import * as contactService from "../models/contacts.js";
+import Contact from "../models/Contact.js";
+
+import { ctrlWrapper } from "../decorators/index.js";
 
 import { HttpError } from "../helpers/index.js";
 
-import { contactAddSchema, contactUpdateSchema } from "../shemas/contact-shemas.js";
-
-const getAll = async (req, res, next) => {
-    try {
-        const result = await contactService.listContacts();
-        res.json(result);
-    }
-    catch (error) {
-        next(error);
-    }
+const getAll = async (req, res) => {
+    const result = await Contact.find({});
+    res.json(result);
 }
 
-const getById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const result = await contactService.getContactById(id);
-        if (!result) {
-            throw HttpError(404, `Contact with id=${id} not found`);
-        }
-        res.json(result);
+const getById = async (req, res) => {
+    const { id } = req.params;
+    // const result = await Contact.findOne({_id: id});
+    const result = await Contact.findById(id);
+    if (!result) {
+        throw HttpError(404, `Contact with id=${id} not found`);
     }
-    catch (error) {
-        next(error);
-    }
+    res.json(result);
 }
 
-const add = async(req, res, next)=> {
-    try {
-        const {error} = contactAddSchema.validate(req.body);
-        if(error){
-            throw HttpError(400, error.message);
-        }
-        const result = await contactService.addContact(req.body);
+const add = async (req, res) => {
+    const result = await Contact.create(req.body);
 
-        res.status(201).json(result);
-    }
-    catch(error) {
-        next(error);
-    }
+    res.status(201).json(result);
 }
 
-const updateById = async(req, res, next)=> {
-    try {
-        const {error} = contactUpdateSchema.validate(req.body);
-        if(error){
-            throw HttpError(400, error.message);
-        }
-        const {id} = req.params;
-        const result = await contactService.updateContactById (id, req.body);
-        if (!result) {
-            throw HttpError(404, `Contact with id=${id} not found`);
-        }
+const updateById = async (req, res) => {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body);
+    if (!result) {
+        throw HttpError(404, `Contact with id=${id} not found`);
+    }
 
-        res.json(result);
-    }
-    catch(error) {
-        next(error);
-    }
+    res.json(result);
 }
 
-const deleteById = async(req, res, next)=> {
-    try {
-        const {id} = req.params;
-        const result = await contactService.removeContact(id);
-        if (!result) {
-            throw HttpError(404, `Contact with id=${id} not found`);
-        }
+const deleteById = async (req, res) => {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndDelete(id);
+    if (!result) {
+        throw HttpError(404, `Contact with id=${id} not found`);
+    }
 
-        res.json({
-            message: "Delete success"
-        })
-    }
-    catch(error) {
-        next(error);
-    }
+    // res.status(204).send();
+
+    res.json({
+        message: "Delete success"
+    })
 }
 
 export default {
-    getAll,
-    getById,
-    add,
-    updateById,
-    deleteById,
+    getAll: ctrlWrapper(getAll),
+    getById: ctrlWrapper(getById),
+    add: ctrlWrapper(add),
+    updateById: ctrlWrapper(updateById),
+    deleteById: ctrlWrapper(deleteById),
 }
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-import app from "./app.js"
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+import app from "./app.js";
+
+const DB_HOST="mongodb+srv://KateSyvash:9DcDi55aHsPDDuXv@cluster0.43gb4ov.mongodb.net/?retryWrites=true&w=majority";
+ const PORT=3000;
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
