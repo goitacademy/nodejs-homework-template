ba01c5b65b3f43678083c35fca410ec41092aeac
LightPride <vlad.grinevich1@gmail.com>
2023-11-21 19:03:40 +0200

    router

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-// const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
