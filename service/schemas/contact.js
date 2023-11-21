a2020fa7ee3c1056dccb593be4ea0ce8500a62a0
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-11-21 20:43:06 +0100

    Create contact.js

---
+++ service/schemas/contact.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const contact = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Contact = model("contact", contact);
+
+module.exports = Contact;
