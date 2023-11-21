d210700d9af89f675abbd2c7f7d75929d47b4369
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-11-21 20:41:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const mongoose = require("mongoose");
 
 const contactsRouter = require('./routes/api/contacts')
 
