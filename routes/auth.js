b9438684263667ac2e96e638d5879f13d188cee0
Andrii Chychkan <mywork1728@gmail>
2023-11-21 20:44:53 +0200

    added schemas

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,9 @@ const jsonParser = express.json();
 
 const AunthController = require("../controllers/authController");
 
-router.post("/users/register", jsonParser, AunthController.register);
-router.post("/users/login", jsonParser, AunthController.login);
-router.post("/users/logout", auth, AunthController.logout);
-router.get("/users/current", auth, AunthController.current);
+router.post("/register", jsonParser, AunthController.register);
+router.post("/login", jsonParser, AunthController.login);
+router.post("/logout", auth, AunthController.logout);
+router.get("/current", auth, AunthController.current);
 
 module.exports = router;
