8e536701f4e722e8c8a7b4b15ec6922d10c2ac68
ilesyk <pavloyavorskyi@ukr.net>
2023-11-21 21:28:46 +0200

    corrections v2

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,7 +3,7 @@ const { isValidObjectId } = require("mongoose");
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    res.status(400).json(`${id} is not valid id`);
+    res.status(400).json({message: `${id} is not valid id`});
     return;
   }
   next();
