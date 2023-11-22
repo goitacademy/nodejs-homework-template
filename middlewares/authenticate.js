7751c515964eef8d58785155e37fe65cf26b95ab
NataDent <cherkashyna.n@gmail.com>
2023-11-22 11:33:41 +0200

    removed configDotenv

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -3,9 +3,6 @@ import { ctrlWrapper } from "../decorators/index.js";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { configDotenv } from "dotenv";
-
-configDotenv();
 
 const { JWT_SECRET } = process.env;
 
