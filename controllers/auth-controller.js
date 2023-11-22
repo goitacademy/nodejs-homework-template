8dac6db617048bae65d9fe8073fb262eba24f72c
NataDent <cherkashyna.n@gmail.com>
2023-11-23 00:01:11 +0200

    Corrected updateSubscription

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -70,10 +70,10 @@ const logout = async (req, res) => {
 };
 
 const updateSubscription = async (req, res) => {
-  const { _id } = req.user;
+  const { _id: owner } = req.user;
   const { subscription } = req.body;
   const result = await User.findByIdAndUpdate(
-    _id,
+    owner,
     { subscription },
     { new: true }
   );
