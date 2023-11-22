47f24e9ddeb93139de4f0a45b6c55b37b72c4923
10Artur <artyr11012002@gmail.com>
2023-11-22 22:31:06 +0200

    Create auth.js

---
+++ routes/api/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const AuthControll = require("../../controll/auth");
+const jsonParser = express.json();
+const auth = require("../../middlewares/auth");
+const { schemas } = require("../../models/user");
+const { validateFavorite } = require("../../middlewares");
+
+router.post(
+    "/register",
+    jsonParser,
+    validateFavorite(schemas.registerSchema),
+    AuthControll.register
+);
+router.post(
+    "/login",
+    jsonParser,
+    validateFavorite(schemas.loginSchema),
+    AuthControll.login
+);
+router.post("/logout", auth, AuthControll.logout);
+router.get("/current", auth, AuthControll.current);
+
+module.exports = router;
