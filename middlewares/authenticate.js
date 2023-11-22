4d00073f9d12b50270b704b45582b732216c32b6
NataDent <cherkashyna.n@gmail.com>
2023-11-22 11:28:14 +0200

    Corrected auth-controller

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,29 +1,89 @@
-import jwt from "jsonwebtoken";
 import { HttpError } from "../helpers/index.js";
+import { ctrlWrapper } from "../decorators/index.js";
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { configDotenv } from "dotenv";
 
 configDotenv();
 
 const { JWT_SECRET } = process.env;
 
-const authenticate = async (req, res, next) => {
-  const { autorization = "" } = req.headers;
-  const [bearer, token] = autorization.split(" ");
-  if (bearer !== "Bearer") {
-    next(HttpError(401));
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, `${email} already in use`);
   }
-  try {
-    const { id } = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(id);
-    if (!user || !user.token) {
-      next(HttpError(401));
-    }
-    req.user = user;
-    next();
-  } catch (error) {
-    next(HttpError(401, error.message));
-  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
 };
 
-export default authenticate;
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).json();
+};
+
+const updateSubscription = async (req, res) => {
+  const { _id } = req.user;
+  const { subscription } = req.body;
+  const result = await User.findByIdAndUpdate(
+    _id,
+    { subscription },
+    { new: true }
+  );
+  res.json(result);
+};
+
+export default {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateSubscription: ctrlWrapper(updateSubscription),
+};
