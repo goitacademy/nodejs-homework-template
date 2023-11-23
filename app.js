2ca4bb5de23c647b35e886c7e6e1918c52d57ef8
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-11-23 14:42:50 +0100

    commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-
-const contactsRouter = require('./routes/api/contacts')
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import moviesRouter from "./routes/api/movies-router.js";
 
 const app = express()
 
@@ -12,8 +11,8 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
 
+app.use("/api/movies", moviesRouter);
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
@@ -22,4 +21,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+export default app ; 
