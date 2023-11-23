ccd209acb551510760acb90655e3e20f7694dcb4
Robertw8 <oniedgaf@gmail.com>
2023-11-23 19:42:57 +0300

    fix validation

--- helpers/validationSchema.js
+++ helpers/validationSchema.js
@@ -1,8 +1,9 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(20),
+  name: Joi.string().min(3).max(20),
   email: Joi.string().email({ minDomainSegments: 2 }),
+  phone: Joi.string().min(9).max(15),
 });
 
 module.exports = {
