0e033d52973c7787ff7a1e47acf2458fbfafcfaf
rasinoleg <rasinoleg3@gmail.com>
2023-11-23 12:05:14 +0200

    lesson-5

--- models/user.js
+++ models/user.js
@@ -33,32 +33,4 @@ const userSchema = new mongoose.Schema(
 
 module.exports = mongoose.model("User", userSchema);
 
-// const mongoose = require("mongoose");
-// const { Schema } = mongoose;
 
-// const userSchema = new Schema({
-//   password: {
-//     type: String,
-//     required: [true, 'Set password for user'],
-//   },
-//   email: {
-//     type: String,
-//     required: [true, 'Email is required'],
-//     unique: true,
-//   },
-//   subscription: {
-//     type: String,
-//     enum: ["starter", "pro", "business"],
-//     default: "starter"
-//   },
-//   owner: {
-//     type: Schema.Types.ObjectId,
-//     ref: 'User',
-//   },
-//   token: String
-// }, {
-//   versionKey: false,
-//   timestamps: true,
-// });
-
-// module.exports = mongoose.model("User", userSchema);
