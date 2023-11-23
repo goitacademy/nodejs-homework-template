2dbbed156bc5f0e32acc05a0c6e9b3f1dd332a05
Andrii Chychkan <mywork1728@gmail>
2023-11-23 22:37:40 +0200

    addeed filename

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,18 @@
 const multer = require("multer");
 const path = require("node:path");
+const crypto = require("node:crypto");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log(file);
     cb(null, path.join(__dirname, "..", "tmp"));
+  },
+
+  filename: (req, file, cb) => {
+    const extname = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, extname);
+    const suffix = crypto.randomUUID();
+    cb(null, `${basename}-${suffix}${extname}`);
   },
 });
 
