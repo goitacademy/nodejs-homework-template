789fddefe41e8bc3dddd87827330ee2a5d3579af
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-11-23 17:18:53 +0100

    Adding finding by ID functionality

--- models/contacts.js
+++ models/contacts.js
@@ -6,11 +6,19 @@ export const listContacts = async () => {
     const contacts = JSON.parse(data);
     return contacts;
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
-export const getContactById = async (contactId) => {};
+export const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((item) => item.id === contactId);
+    return contact;
+  } catch (error) {
+    return error;
+  }
+};
 
 export const removeContact = async (contactId) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,21 @@
 import { Router } from "express";
-import { listContacts } from "../../models/contacts.js";
+import { listContacts, getContactById } from "../../models/contacts.js";
 
 const router = Router();
 
 router.get("/", async (req, res, next) => {
   const list = await listContacts();
-  res.send(list);
+  res.status(200).json(list);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
 });
 
 router.post("/", async (req, res, next) => {
