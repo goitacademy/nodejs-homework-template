6d6cd24e8f0b5e96ab98310c55a8599dfab5fbfe
Babenko Liudmyla <savelieva0509@gmail.com>
2023-11-23 20:57:58 +0200

    add avatarURL in SchemaUser

--- models/user.js
+++ models/user.js
@@ -24,6 +24,9 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
+    avatarURL: {
+      type: String,
+    },
   },
   { versionKey: false, timestamps: true }
 );
