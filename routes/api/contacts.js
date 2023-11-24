a576fa2767615f21fc61e69c83d0e2ea6a493654
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-11-24 10:56:12 -0500

    base de datos mongo 1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { promises: fs } = require('fs');
+const { promises: fs } = require('fs').promises;
 const Joi = require('joi');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
 const router = express.Router();
-const contactsFilePath = `${__dirname}/../../models/contacts.json`;
+const contactsFilePath = path.join(__dirname, '../../models/contacts.json');
+
 const contactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
@@ -43,6 +45,7 @@ router.get('/', async (req, res) => {
     const contacts = await loadContacts();
     res.json(contacts);
   } catch (error) {
+    console.error('Error getting contacts:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
@@ -54,11 +57,12 @@ router.get('/:id', async (req, res) => {
     const contact = contacts.find((c) => c.id === contactId);
 
     if (!contact) {
-      return res.status(404).json({ message: 'Not found' });
+      return res.status(404).json({ message: 'Contact not found' });
     }
 
     res.json(contact);
   } catch (error) {
+    console.error('Error getting contact by ID:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
@@ -77,12 +81,14 @@ router.post('/', async (req, res) => {
       name: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
+      favorite: req.body.favorite || false,
     };
 
     contacts.push(newContact);
     await saveContacts(contacts);
     res.status(201).json(newContact);
   } catch (error) {
+    console.error('Error creating contact:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
@@ -94,13 +100,14 @@ router.delete('/:id', async (req, res) => {
     const contactIndex = contacts.findIndex((c) => c.id === contactId);
 
     if (contactIndex === -1) {
-      return res.status(404).json({ message: 'Not found' });
+      return res.status(404).json({ message: 'Contact not found' });
     }
 
     const deletedContact = contacts.splice(contactIndex, 1)[0];
     await saveContacts(contacts);
     res.json({ message: 'Contact deleted', deletedContact });
   } catch (error) {
+    console.error('Error deleting contact:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
@@ -112,7 +119,7 @@ router.put('/:id', async (req, res) => {
     const contactIndex = contacts.findIndex((c) => c.id === contactId);
 
     if (contactIndex === -1) {
-      return res.status(404).json({ message: 'Not found' });
+      return res.status(404).json({ message: 'Contact not found' });
     }
 
     const { error } = validateContact(req.body);
@@ -126,15 +133,16 @@ router.put('/:id', async (req, res) => {
       name: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
+      favorite: req.body.favorite || false,
     };
 
     contacts[contactIndex] = updatedContact;
     await saveContacts(contacts);
     res.json(updatedContact);
   } catch (error) {
+    console.error('Error updating contact:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
 module.exports = router;
-
