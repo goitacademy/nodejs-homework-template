c2f9d64c1e94672fd3cfe7792b7d6960faaa6ccc
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-11-24 14:35:33 +0200

    f

---
+++ controllers/contact/deleteContact.js
@@ -0,0 +1,14 @@
+// controllers/contact/deleteContact.js
+import Contact from "../../models/Contact.js";
+import { HttpError } from "../../helpers/index.js";
+
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const data = await Contact.findByIdAndDelete(id);
+
+  if (!data) {
+    throw HttpError(404);
+  }
+
+  res.status(200).json({ message: "contact deleted" });
+};
