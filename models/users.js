5feb3e27de0f8c3658104f6eab5c53b5cf31d8c3
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-11-24 15:42:28 +0200

    add auth schema

---
+++ models/users.js
@@ -0,0 +1,55 @@
+import { Schema, model } from 'mongoose';
+import Joi from 'joi';
+import { handleSaveError, preUpdate } from './hooks.js';
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ['starter', 'pro', 'business'],
+      default: 'starter',
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post('save', handleSaveError);
+
+userSchema.pre('findOneAndUpdate', preUpdate);
+
+userSchema.post('findOneAndUpdate', handleSaveError);
+
+export const userSingupSchema = Joi.object({
+  password: Joi.string()
+    .required()
+    .messages({ 'any.required': 'missing required password field' }),
+  email: Joi.string()
+    .required()
+    .messages({ 'any.required': 'missing required email field' }),
+  subscription: Joi.string()
+    .valid('starter', 'pro', 'business')
+    .messages({ 'any.valid': 'must be starter, pro or business' }),
+});
+
+export const userLoginSchema = Joi.object({
+  email: Joi.string().required().messages({
+    'any.required': 'missing required email field',
+  }),
+  password: Joi.string().required().messages({
+    'any.required': 'missing required password field',
+  }),
+});
+
+const User = model('user', userSchema);
+
+export default User;
