383dffc005a9070ebea1f70f1f5f575ca4b41bfd
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-11-25 16:36:01 +0200

    mistake

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,18 +2,9 @@ const express = require("express");
 const controllers = require("../../controllers/ControllContacts");
 const router = express.Router();
 const validateBody = require("../../middlewares/validateBody");
-const {
-  registerValidation,
-  loginValidation,
-} = require("../../schemas/authSchema");
 
-router.post(
-  "/register",
-  validateBody(registerValidation),
-  controllers.register
-);
-
-router.post("/login", validateBody(loginValidation), controllers.login);
+router.post("/register", controllers.register);
+router.post("/login", controllers.login);
 router.post("/logout", controllers.logout);
 router.get("/current", controllers.current);
 
