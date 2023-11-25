12d9e6df193360a16213f94675bd6bf056ada739
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2023-11-25 11:22:45 +0300

    + tmp => public/avatars

--- controllers/avatars.js
+++ controllers/avatars.js
@@ -1,6 +1,13 @@
-
+const fs = require("fs/promises");
+const path = require("path");
 async function avatar(req, res, next) {
-    res.send(" Uploud avatar")
+  try {
+    fs.rename(
+      req.file.path,
+      path.join(__dirname, "..", "public/avatars", req.file.filename)  // перемещает картинки с временной папки tmp в постоянную папку public/avatars
+    );
+  } catch (error) {}
+  res.send(" Uploud avatar");
 }
 
 module.exports = {
