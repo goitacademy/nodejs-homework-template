c14cd33827e0207371a2a6cbccea8d622d976799
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-11-26 20:52:57 +0200

    2nd homework

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,88 @@
 const express = require('express')
-
+const contacts = require ('../../models/contacts') 
+const {HttpError} = require('../../helpers/HttpError')
+const Joi = require("joi")
 const router = express.Router()
-
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/', async (req, res) => {
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({ message: 'Server error' })
+  }
+  
+})  
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const result = await contacts.getContactById(id) 
+    console.log(result)
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.json(result)
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = addSchema.validate(req.body)
+    if (error) {
+      throw HttpError(400, error.message)
+    }
+    const result = await contacts.addContact(req.body)
+    res.status(201).json(result)
+  }
+  catch (error) {
+    next(error)
+  }
+  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const result = await contacts.removeContact(contactId)
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.json({ message: 'Contact deleted' })
+  }
+  catch (error) {
+    next(error)
+  }
+
+  
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = addSchema.validate(req.body)
+    if (error) {
+      throw HttpError(400, error.message)
+    }
+    const { contactId } = req.params
+    const result = await contacts.updateContact(contactId, req.body)
+    
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.json(result)
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const app = require('./app')
 
+// const contacts = require('./models/contacts.json')
+
+// app.use((req, res, next) => { 
+//   console.log('aaaaa')
+//   next()
+// })
+
+// app.get('/', (request, response) => { 
+//   response.send(console.log(request.url))
+// })
+
+// app.get('/contacts', (request, responce) => { 
+//   responce.json(contacts)
+// })
+
+// app.use((req, res)=> {
+//   // console.log(res)
+//   res.status(404).json({
+// 		message: "Not found"
+// 	})
+// })
+
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
