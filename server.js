98abe37ca1a99602e8e9ee4d2ed4656ee3fe3a9b
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-11-27 17:05:04 +0100

    connect with DB

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+//   xdnBsFqEZzOv2f0v
+const mongoose = require("mongoose");
 const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Antonina:xdnBsFqEZzOv2f0v@cluster0.lideuo9.mongodb.net/db_contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
 
-app.listen(3300, () => {
-  console.log("Server running. Use our API on port: 3300");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3300, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
