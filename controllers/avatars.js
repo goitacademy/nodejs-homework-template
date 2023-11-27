0fdd814872fd6ec4e3bf6545057c2c63641c65de
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2023-11-27 14:45:31 +0300

    +validation body/ + default img

--- controllers/avatars.js
+++ controllers/avatars.js
@@ -3,19 +3,29 @@ const path = require("path");
 const User = require("../models/user");
 const jimp = require("jimp");
 
+// дефолтный аватар
+  const defaultImg = path.join(
+    __dirname,
+    "..",
+    "puclic/avatars/default.jpg"
+  );
+
 async function avatar(req, res, next) {
   // редактируем фото через пакет jimp
-      const img = await jimp.read(req.file.path);
-      await img
-        .autocrop()
-        .cover(
-          250,
-          250,
-          jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
-        )
-        .writeAsync(req.file.path);
+  const img = await jimp.read(req.file.path);
+  await img
+    .autocrop()
+    .cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE)
+    .writeAsync(req.file.path);
 
   try {
+    // проверяем действительно ли файл прикреплён к запросу
+    if (!req.file) {
+      const img = await fs.readFile(defaultImg)
+      res.writeHead(200, { "Content-Type": "image/jpeg" });
+      res.end(img);
+      return;
+    }
     fs.rename(
       req.file.path,
       path.join(__dirname, "..", "public/avatars", req.file.filename) // перемещаем картинки с временной папки tmp в постоянную папку public/avatars
@@ -36,7 +46,6 @@ async function avatar(req, res, next) {
   } catch (error) {
     next(error);
   }
-  
 }
 
 module.exports = {
--- routes/api/avatars.js
+++ routes/api/avatars.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Ctrl = require("../../controllers/avatars");
 const upload = require("../../middleware/uploadAvatars");
+const isValidId = require("../../middleware/validationBody")
 
 // создаём роутер с методом патч для изменения одного файла в документе. с помощью upload берем один файл
-router.patch("/avatars", upload.single("avatar"), Ctrl.avatar);
+router.patch("/avatars", isValidId, upload.single("avatar"), Ctrl.avatar);
 
 module.exports = router;
