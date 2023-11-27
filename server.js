4709bf9124d5f752fb73e5ba938884038ebd8947
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-11-27 20:10:09 -0330

    added conect with mangoDB

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose')
+const DB_HOST =
+  "mongodb+srv://vitaliybeyar:7jZ2hPKRHZ7Oe1Dv@cluster0.tnlyzg7.mongodb.net/contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {app.listen(3000)})
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+
+  });
+
