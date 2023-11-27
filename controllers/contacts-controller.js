ceee277a58ecd77eee175bc0030252ea832d78fb
lusya0409 <lusya0409@gmail.com>
2023-11-27 15:39:14 +0200

    changes hw04

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,14 +4,24 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({}, "name email");
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10, ...filterParams } = req.query;
+  const skip = (page - 1) * limit;
+  const filter = { owner, ...filterParams };
+
+  const result = await Contact.find(filter, "name email", {
+    skip,
+    limit,
+  }).populate("owner", "email");
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
+  const { _id: owner } = req.user;
+
   // const result = await Contact.findOne({ _id: id });
-  const result = await Contact.findById(id);
+  const result = await Contact.findOne({ _id: id, owner });
 
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found!`);
@@ -20,13 +30,16 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
 const updateById = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body);
+  const { _id: owner } = req.user;
+
+  const result = await Contact.findOneAndUpdate({ _id: id, owner }, req.body);
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found!`);
   }
@@ -35,7 +48,9 @@ const updateById = async (req, res) => {
 
 const deleteById = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndDelete(id);
+  const { _id: owner } = req.user;
+
+  const result = await Contact.findOneAndDelete({ _id: id, owner });
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found!`);
   }
---
+++ routes/api/auth-router.js
@@ -0,0 +1,40 @@
+import express from "express";
+import authController from "../../controllers/auth-controller.js";
+import {
+  authenticate,
+  isEmptyBody,
+  isValidId,
+} from "../../middlewares/index.js";
+import { validaterBody } from "../../decorators/index.js";
+import {
+  userSigninSchema,
+  userSignupSchema,
+  updateSubscriptionSchema,
+} from "../../models/User.js";
+
+const authRouter = express.Router();
+
+authRouter.post(
+  "/register",
+  isEmptyBody,
+  validaterBody(userSignupSchema),
+  authController.signup
+);
+authRouter.post(
+  "/login",
+  isEmptyBody,
+  validaterBody(userSigninSchema),
+  authController.signin
+);
+authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.post("/logout", authenticate, authController.logout);
+
+authRouter.patch(
+  "/subscription",
+  authenticate,
+  validaterBody(updateSubscriptionSchema),
+  authController.updateSubscription
+);
+
+export default authRouter;
