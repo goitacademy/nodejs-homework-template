3ddb78c21d3eee00d289ce36d3215764fcd3fa50
Iryna Sokolovska <sokirina777@gmail.com>
2023-11-27 23:17:53 +0200

    fix one

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import contactsRouter from "./routes/api/contacts.js";
+import authRouter from "./routes/api/auth-router.js";
+import contactsRouter from "./routes/api/contacts-router.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
---
+++ models/Users.js
@@ -0,0 +1,51 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleSaveError, preUpdate } from "./hooks.js";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      minLength: 6,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    // token: String,
+  },
+  { versionKey: false }
+);
+
+userSchema.post("save", handleSaveError);
+userSchema.pre("findOneAndUpdate", preUpdate);
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+export const userSignupSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
+});
+export const userSigninSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model("user", userSchema);
+
+export default User;
