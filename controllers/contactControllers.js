cffaf1dc930bfd6dff4cf2f4dd5712bc60339e3b
Demian <demyan.korotchenko@gmail.com>
2023-11-28 15:58:35 +0100

    final

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -56,45 +56,56 @@ async function addContact(req, res, next) {
 
 async function updateContact(req, res, next) {
   const { contactId } = req.params;
-  const contact = {
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    favorite: req.body.favorite
-  }
+  const { name, email, phone, favorite } = req.body;
+
   try {
-    const result = await Contact.findByIdAndUpdate(contactId, contact, {new: true}); 
-    // console.log(result);
-    if (result === null){
+    const contact = await Contact.findById(contactId);
+
+    if (!contact) {
       return res.status(404).send("Contact Not Found");
     }
-    res.send(result);
+
+    if (contact.owner.toString() !== req.user.id) {
+      return res.status(403).json('Forbidden to update');
+    }
+    // Оновлюємо всі поля контакту
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+    contact.favorite = favorite;
+
+    const updatedContact = await contact.save();
+    
+    res.json({ message: 'Contact Updated successfully', data: updatedContact });
 
   } catch (err) {
     next(err);
-  }
-
-  
+  }  
 }
 
 async function removeContact(req, res, next) {
   const { contactId } = req.params;
 
   try {
-    const result = await Contact.findByIdAndDelete(contactId);
-    if (result === null) {
-      return res.status(404).send("Contact Not Found");
+    const contact = await Contact.findById(contactId);
+
+    // console.log(contact); 
+
+    if (!contact) {
+      return res.status(404).json({ error: 'Contact Not Found', message: 'Contact not found with the given ID' });
     }
 
-    if (result.owner !== req.user.id){
-      return res.status(403).send("Forbidden")
+    if (contact.owner.toString() !== req.user.id) {
+      return res.status(403).json('Forbidden');
     }
+
+    const result = await contact.deleteOne();
     res.json({ message: 'Contact deleted successfully', data: result });
-    // res.send(`Deleted book ${contactId})`);
   } catch (err) {
     next(err);
   }
 }
+
 
 async function updateStatusOfContact(req, res, next) {
   const { contactId } = req.params;
