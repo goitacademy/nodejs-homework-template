f3e5415cd69226b4051ceb105fb560a182905fdb
10Artur <artyr11012002@gmail.com>
2023-11-28 19:05:35 +0200

    Update avatars.js

--- controll/avatars.js
+++ controll/avatars.js
@@ -1,9 +1,17 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 const { User } = require("../models/user");
+const jimp = require("jimp");
 
 const uploadAvatar = async(req, res, next) => {
     try {
+        if (!req.file) {
+            throw new Error("No file attacched");
+        }
+
+        const image = await jimp.read(req.file.path);
+        await image.cover(250, 250).writeAsync(req.file.path);
+
         await fs.rename(
             req.file.path,
             path.join(__dirname, "..", "public/avatars", req.file.filename)
@@ -15,7 +23,7 @@ const uploadAvatar = async(req, res, next) => {
             req.user.id, { avatarURL }, { new: true }
         ).exec();
 
-        res.send("Upload avatar");
+        res.json({ avatarURL: user.avatarURL });
     } catch (error) {
         if (error.message === "No file attached") {
             res.send({ message: "Прикріпіть зображення !" });
