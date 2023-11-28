c3a4636ffec6e490d4b1c2a334b9d899aad9fec6
10Artur <artyr11012002@gmail.com>
2023-11-28 19:06:01 +0200

    Update avatars.js

--- routes/api/avatars.js
+++ routes/api/avatars.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const avatarControll = require("../../controll/avatars");
 const upload = require("../../middlewares/avatarsUpload");
+const auth = require("../../middlewares/auth");
 
 const router = express.Router();
 
-router.patch("/avatars", upload.single("avatar"), avatarControll.uploadAvatar);
+router.patch(
+    "/avatars",
+    auth,
+    upload.single("avatar"),
+    avatarControll.uploadAvatar
+);
 
 module.exports = router;
