54e241a2c1ca225707f1efeb926156f324c96ab0
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-11-27 21:06:53 -0330

    move DB_host

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require('./app')
 
-const mongoose = require('mongoose')
-const DB_HOST =
-  "mongodb+srv://vitaliybeyar:7jZ2hPKRHZ7Oe1Dv@cluster0.tnlyzg7.mongodb.net/contacts?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
+const {DB_HOST} = require('./config')
+
 
 mongoose.connect(DB_HOST)
   .then(() => {app.listen(3000)})
