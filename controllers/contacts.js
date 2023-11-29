2b0c546d6bc38897f19fec6aa761ae9a7243ea74
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-11-29 10:19:25 +0100

    fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -29,7 +29,7 @@ const add = async (req, res, next) => {
 
 const deleteById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = removeContact(contactId);
+  const result = await removeContact(contactId);
   if (!result) {
     res.status(404).json({ message: "Not found" });
   }
