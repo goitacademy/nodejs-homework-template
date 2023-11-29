8d320df56e2a1939845fca3d04e1dcb17fe3a75f
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-11-29 23:38:38 +0200

    wfe

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+import mongoose from 'mongoose';
+
 import { app } from "./app.js"
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://Sasha-hw-03:DtvxG9E9wHJLzJX8@cluster0.tsifo9g.mongodb.net/my-contacts?retryWrites=true&w=majority'
+// DtvxG9E9wHJLzJX8
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1)
+  })
+
+
