8c0747e68cbe78fe55009d7bc888dc3b5da8b0a2
Aleksandr Harianin <onlydipro@gmail.com>
2023-11-29 19:27:51 +0200

    start

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+
+const mongoose = require('mongoose')
+
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST = require('./config')
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successfull");
+    })
+  })
+  .catch(error =>{
+    console.log(error.message);
+    process.exit(1);
 })
+
+//Q9rDFYMcFH4QN7Md
+
