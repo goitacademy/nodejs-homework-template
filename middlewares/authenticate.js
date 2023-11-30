1bbf8bbfb5cf9a81e833d89997a2ce435ae0de54
Iryna Sokolovska <sokirina777@gmail.com>
2023-11-30 20:22:01 +0200

    fix three

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -19,6 +19,7 @@ const authenticate = async (req, res, next) => {
   try {
     const { id } = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(id);
+
     if (!user || !user.token || user.token !== token) {
       return next(HttpError(401, "Not authorized"));
     }
