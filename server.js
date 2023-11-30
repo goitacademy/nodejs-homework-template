5f9062dc69c85dfa02bffd7c302a58a05e479c44
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-11-30 17:29:38 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://vvendeavour:1HDx6H5QatOtLSw6@cluster0.j3yobes.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
