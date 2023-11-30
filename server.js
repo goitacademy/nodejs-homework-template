aaaee25ad3723a839a226589ca41b1d336560609
Svin07 <svin07@ukr.net>
2023-11-30 12:44:19 +0200

    add env

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const DB_HOST = require("./config");
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
