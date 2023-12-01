f81715e82fa0972c742edbf4b6fff65aab378a06
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-12-01 14:10:24 +0200

    b

--- models/user.js
+++ models/user.js
@@ -26,6 +26,10 @@ const userSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
