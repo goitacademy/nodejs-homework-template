1e0ba6bb7718be9969040c23e5e009d79b36929e
Gigapatut <125769111+Gigapatut@users.noreply.github.com>
2023-12-01 18:44:42 +0200

    hw03-previously

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
 // app.listen(3000, () => {
@@ -6,17 +8,18 @@ const app = require('./app')
 
 // ----------------
 
-const mongoose = require("mongoose");
+const DB_HOST = "mongodb+srv://Valentin:Suemz3HTR4lctpBw@cluster0.rektj99.mongodb.net/db-contacts?retryWrites=true&w=majority"
 
 // const dotenv = require("dotenv");
 // dotenv.config();
 
-const { DB_HOST, PORT = 3000 } = process.env;
+// const { DB_HOST, PORT = 3000 } = process.env;
+
 
 mongoose
     .connect(DB_HOST)
     
-    .then(() => app.listen(PORT))                         // console.log("Database connection successful"));
+    .then(() => app.listen(3000))                         // console.log("Database connection successful"));
 
     .catch((error) => {
         console.log(error.message);
