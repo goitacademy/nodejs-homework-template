04c08cf1cd2a428f17f4f9ee8b0e540be87aa714
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-12-02 01:10:45 +0200

    2

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -1,33 +1,34 @@
 // controllers/updateAvatar.js
-const jimp = require('jimp');
-const fs = require('fs/promises');
-const path = require('path');
+const jimp = require("jimp");
+const fs = require("fs/promises");
+const path = require("path");
 
 const updateAvatar = async (req, res) => {
   try {
-     // Проверяем, что req.file существует и имеет свойство path
-     if (!req.file || !req.file.path) {
-        return res.status(400).json({ message: 'No file uploaded' });
-      }
+    // Проверяем, что req.file существует и имеет свойство path
+    if (!req.file || !req.file.path) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
 
-        // Выводим в консоль информацию о файле
-    console.log('File info:', req.file);
+    // Выводим в консоль информацию о файле
+    console.log("File info:", req.file);
 
     // Выводим в консоль информацию о пользователе
-    console.log('User info:', req.user);
+    console.log("User info:", req.user);
 
     // Обработка аватарки с использованием jimp
     const image = await jimp.read(req.file.path);
     await image.resize(250, 250).write(req.file.path);
 
     // Генерация уникального имени файла
-    console.log('User ID:', req.user._id); 
-    console.log('File path:', req.file.path); 
-    const uniqueFileName = `${Date.now()}-${req.user._id}${path.extname(req.file.originalname)}`;
+    console.log("User ID:", req.user._id);
+    console.log("File path:", req.file.path);
+    const uniqueFileName = `${Date.now()}-${req.user._id}${path.extname(
+      req.file.originalname
+    )}`;
 
-     // Выводим в консоль уникальное имя файла
-     console.log('Unique file name:', uniqueFileName);
-
+    // Выводим в консоль уникальное имя файла
+    console.log("Unique file name:", uniqueFileName);
 
     // Перемещение аватарки из tmp в public/avatars
     await fs.rename(req.file.path, `public/avatars/${uniqueFileName}`);
@@ -40,7 +41,7 @@ const updateAvatar = async (req, res) => {
     res.json({ avatarURL: req.user.avatarURL });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
