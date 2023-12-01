faafe4daa95e3a7fa4d4f5241edd2463e0f30e7c
Kate Zaharova <15071981ktz@gmail.com>
2023-12-01 20:44:33 +0200

    in process

---
+++ middlewares/upload.js
@@ -0,0 +1,26 @@
+import multer from "multer";
+import path from "path";
+import { HttpError } from "../helpers/index.js";
+
+const destination = path.resolve("temp");
+
+const storage = multer.diskStorage({
+    destination,
+    filename: (req, file, cb) => {
+        const uniquePrefix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`;
+        const filename = `${uniquePrefix}_${file.originalname}`;
+        cb(null, filename);
+    }
+});
+
+const limits = {
+    fileSize: 5 * 1024 * 1024,
+};
+
+const fileFilter = (req, file, cb) => {
+    const extention = file.originalname.split(".").pop();
+    if (extention === "exe") {
+        return cb(HttpError(400, "Invalid file extention"));
+    }
+    cb(null, true);
+}
