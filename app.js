ba9dfc2fba0abf4b325a141456ec156e0243b341
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-12-02 21:51:29 +0100

    update server

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
     
-  res.status(500).json({ message: err.message });
+  res.status(3000).json({ message: err.message });
 });
 
 module.exports = app;
