291bf4e695da6226dad59babe1007be4606a4fc6
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-12-03 00:06:16 +0100

    fix  emailRegexp

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleMonngooseError } = require("../helpers");
 
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const emailRegexp = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
 
 const userSchema = new Schema(
   {
