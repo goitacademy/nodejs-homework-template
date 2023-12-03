b45259cd10ec4beeba4ca0991d8fe925c48bedeb
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-12-03 13:11:27 +0200

    fix

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -3,7 +3,7 @@ import { HttpError } from '../helpers/HttpError.js';
 export const isEmptyBody = async (req, res, next) => {
     const keys = Object.keys(req.body);
     if (!keys.length) {
-        return next(HttpError(400, "Body must have fields"))
+        return next(HttpError(400, "Missing field favorite"))
     }
     next()
 }
