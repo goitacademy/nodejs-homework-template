c98b8d77da330fe2adbebc317d9345f33566dfd9
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-12-03 23:47:30 +0100

    up

--- middlewares/emailService.js
+++ middlewares/emailService.js
@@ -1,7 +1,5 @@
-// emailService.js
-
-const ElasticEmail = require("@elasticemail/elasticemail-client");
 require("dotenv").config();
+const ElasticEmail = require("@elasticemail/elasticemail-client");
 
 const { ELASTIC_API_KEY, FROM_EMAIL } = process.env;
 
@@ -37,47 +35,3 @@ const sendEmail = async ({ to, subject, html }) => {
 };
 
 module.exports = { sendEmail };
-
-
-// require("dotenv").config();
-// const ElasticEmail = require("@elasticemail/elasticemail-client");
-
-// const { ELASTIC_API_KEY, EMAIL_FROM } = process.env;
-
-// const defaultClient = ElasticEmail.ApiClient.instance;
-// const { apikey } = defaultClient.authentications;
-// apikey.apiKey = ELASTIC_API_KEY;
-
-// const api = new ElasticEmail.EmailsApi();
-
-// const sendEmail = async ({ to, subject, html }) => {
-//   const email = ElasticEmail.EmailMessageData.constructFromObject({
-//     Recipients: [new ElasticEmail.EmailRecipient(to)],
-//     Content: {
-//       Body: [
-//         ElasticEmail.BodyPart.constructFromObject({
-//           ContentType: 'HTML',
-//           Content: html,
-//         }),
-//       ],
-//       Subject: subject,
-//       From: EMAIL_FROM ,
-//     },
-//   });
-
-//   const callback = function (error, data, response) {
-//     if (error) {
-//       console.error(error);
-//     } else {
-//       console.log('API called successfully.');
-//     }
-//   };
-
-//   api.emailsPost(email, callback);
-//   return true;
-// };
-
-// module.exports = {sendEmail};
-
-
- 
