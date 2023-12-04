b83274faa86611c4b93e8bbe39f1b5507901d7f3
Iryna Sokolovska <sokirina777@gmail.com>
2023-12-04 16:55:43 +0200

    fix five: виправила

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -39,7 +39,7 @@ const addContact = async (req, res, next) => {
       throw HttpError(400, error.message);
     }
     const { _id: owner } = req.user;
-    const result = await Contact.create(...req.body, owner);
+    const result = await Contact.create({ ...req.body, owner });
     res.status(201).json(result);
   } catch (error) {
     next(error);
