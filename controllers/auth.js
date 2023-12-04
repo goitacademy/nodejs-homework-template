82593a601154567161cf4145deeeb45d9839365b
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-12-04 11:07:31 +0100

    add bctypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,16 @@
+const bcrypt = require("bcrypt");
 const { User } = require("../models/user");
-// const { ctrlWrapper } = require("../helpers");
+const { HTTPError } = require("../helpers");
 
 const register = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw HTTPError(409, "Email already in use");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
     res.status(201).json({
       email: newUser.email,
       name: newUser.name,
@@ -15,5 +22,4 @@ const register = async (req, res, next) => {
 
 module.exports = {
   register,
-  // : ctrlWrapper(register),
 };
