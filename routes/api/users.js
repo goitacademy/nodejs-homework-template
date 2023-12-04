165cbc773e7cdb0c874941ba895a842f8877c5e8
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-12-04 15:14:16 +0200

    users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const UsersController = require("../../controllers/users/index.js");
 const jsonParser = express.json();
-const author = require("../../middleware/author");
-const { validateUsers } = require("../../helpers");
-const upload = require("../../middleware/upload.js");
-router.post("/register", jsonParser, validateUsers, UsersController.register);
-router.post("/login", jsonParser, validateUsers, UsersController.login);
-router.post("/logout", author, UsersController.logout);
-router.get("/current", author, UsersController.getCurrent);
-router.patch(
-  "/avatars",
-  upload.single("avatar"),
-  author,
-  UsersController.uploadAvatar
-);
-router.get("/verify/:verificationToken", UsersController.verify);
-router.post("/verify/", UsersController.verifyRepeated);
+
+const auth = require("../../middleware/auth");
+
+const upload = require("../../middleware/upload");
+
+const userController = require("../../controllers/users/index");
+
+router.get("/current", userController.getCurrent);
+router.patch("/", userController.updateSubscription);
+router.patch("/avatars", upload.single("avatar"), userController.uploadAvatar);
+router.post("/verify", jsonParser, userController.verify);
+
 module.exports = router;
