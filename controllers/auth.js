02af466077820379a2a1cd8d5a713548b0e232b5
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-12-05 22:09:33 +0100

    add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
 const { HTTPError } = require("../helpers");
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res, next) => {
   try {
@@ -20,6 +22,28 @@ const register = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HTTPError(401, "Email or password invalid");
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw HTTPError(401, "Email or password invalid");
+    }
+    const payload = {
+      id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+    res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,5 +6,6 @@ const ctrl = require("../../controllers/auth");
 const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
