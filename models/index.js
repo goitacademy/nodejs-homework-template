c3a0a739243aa14984f566ac2fe9cd26b19836e0
Robertw8 <oniedgaf@gmail.com>
2023-12-05 06:14:10 +0200

    create User schema

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const { Contact, contactJoiSchema } = require("./contact");
+const { User, userJoiSchema } = require("./user");
 
 module.exports = {
   Contact,
   contactJoiSchema,
+  User,
+  userJoiSchema,
 };
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: String,
+});
+
+const userJoiSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  subscription: Joi.string(),
+  token: Joi.string(),
+});
+
+const User = model("user", userSchema);
+
+module.exports = { User, userJoiSchema };
