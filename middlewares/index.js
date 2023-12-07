1490a76a38eedd3b483a3195b36d41f1f6ac3ade
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-12-07 19:32:26 +0100

    add authenticate + pagination

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validateBody = require("./validateBody");
 const isValidID = require("./isValidID");
+const authenticate = require("./authenticate");
 
 module.exports = {
   validateBody,
   isValidID,
+  authenticate,
 };
