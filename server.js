a62e7a99f27f3e1ff46e234fbc707a7a867104f1
Artchem <chemerys.artem@gmail.com>
2023-12-07 13:51:49 +0100

    add mongose, dotenv

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// opSLMHDs74IGKtyg
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+// console.log(process.env);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connect success");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
