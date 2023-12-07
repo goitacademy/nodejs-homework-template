2c8da89724802d3124ac66d21c3bd45756618928
elias-291356 <ikucher.002@gmail.com>
2023-12-07 13:33:58 +0400

    procces.env

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+import mongoose from "mongoose";
+import { app } from './app.js';
+import { DB_HOST } from "./config.js"
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
