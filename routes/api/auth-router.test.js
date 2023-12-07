1a401ebcaf0cb11cce1f114b0d889a85b942e187
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-12-07 22:33:45 +0200

    fix

--- routes/api/auth-router.test.js
+++ routes/api/auth-router.test.js
@@ -1,35 +1,56 @@
-import mongoose from "mongoose";
-import request from "supertest";
-
+import mongoose from 'mongoose';
 import app from '../../app.js';
+import request from 'supertest';
+// import { User } from '../../models/User.js';
 
 const { DB_TEST_HOST, PORT = 3000 } = process.env;
 
 describe('test /users', () => {
-    let server = null;
+  let server;
 
-    beforeAll(async () => {
-        await mongoose.connect(DB_TEST_HOST);
-        server = app.listen(PORT);
+  beforeAll(async () => {
+    server = app.listen(PORT);
+    await mongoose.connect(DB_TEST_HOST);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    server.close();
+  });
+
+//   afterEach(async () => {
+//     await User.deleteMany();
+//   });
+
+  test('test /register with correctData', async () => {
+    const userData = { email: 'test@example.com', password: 'password123' };
+    const response = await request(app).post('/users/register').send(userData);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.user).toEqual({
+      email: userData.email,
+      subscription: expect.any(String),
+      avatarURL: expect.any(String),
     });
+  });
 
-    afterAll(async () => {
-        await mongoose.connection.close();
-        server.close();
+  test('test /login with correctData', async () => {
+    const userData = { email: 'test@example.com', password: 'password123' };
+    const response = await request(app).post('/users/login').send(userData);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.user).toEqual({
+      email: userData.email,
+      subscription: expect.any(String),
     });
+    expect(response.body.token).toBeTruthy();
+  });
 
-    test("test /login with correctData", async () => {
-        const correctData = { email: "email@test.com", password: "12345678" };
-        const {
-            statusCode,
-            body: { token, user },
-        } = await request(app).post("/users/login").send(correctData);
+  test('test /login with incorrectData', async () => {
+    const userData = { email: 'nonexistent@example.com', password: 'wrongpassword' };
+    const response = await request(app).post('/users/login').send(userData);
 
-        expect(statusCode).toBe(200);
-        expect(user).toEqual({
-            email: expect.any(String),
-            subscription: expect.any(String),
-        });
-        expect(token);
-    });
+    expect(response.statusCode).toBe(401);
+    expect(response.body.message).toBe('Email or password is wrong');
+  });
 });
