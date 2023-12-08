371d30cc5297ed8fcdbd9354e054e6b94925d9d4
NataDent <cherkashyna.n@gmail.com>
2023-12-08 13:43:41 +0200

    Changed verify controller

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -51,13 +51,14 @@ const register = async (req, res) => {
 
 const verify = async (req, res) => {
   const { verificationToken } = req.params;
+  console.log(verificationToken);
   const user = await User.findOne({ verificationToken });
   if (!user) {
     throw HttpError(404, "User not found");
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
-    verificationToken: null,
+    verificationToken: " ",
   });
 
   res.json({
