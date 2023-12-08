4b675e411f2df1cef7f0a9b2f53df10596fc0674
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:33:11 +0200

    find owner by id

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,9 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+
+  const result = await Contact.find({ owner });
   res.json(result);
 };
 
