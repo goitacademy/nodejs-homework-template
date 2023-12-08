d2e044a8805d687abe9d568f48750b103d22d073
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:27:44 +0200

    added authenticate. validation user, token

--- middlerwares/index.js
+++ middlerwares/index.js
@@ -1,5 +1,9 @@
-const ctrlWrapper = require("./ctrlWrapper");
 const validation = require("./validation");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
 
-module.exports = { validation, ctrlWrapper, isValidId };
+module.exports = {
+  validation,
+  isValidId,
+  authenticate,
+};
