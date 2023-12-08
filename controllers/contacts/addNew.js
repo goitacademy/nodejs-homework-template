5f7a6a819ad5339c608b576ab81429c7799d1175
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:36:14 +0200

    the ability to add a new contact to a specific user

--- controllers/contacts/addNew.js
+++ controllers/contacts/addNew.js
@@ -1,8 +1,11 @@
 const { Contact } = require("../../models/contact");
 
 const addNew = async (req, res) => {
+  const { _id: owner } = req.user;
+
   const newContact = {
     ...req.body,
+    owner,
   };
   const result = await Contact.create(newContact);
   res.status(201).json(result);
