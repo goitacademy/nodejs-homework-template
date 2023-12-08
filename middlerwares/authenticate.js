d7733e216640f9ff044565456affa8638f6fea18
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:55:11 +0200

    update error message

--- middlerwares/authenticate.js
+++ middlerwares/authenticate.js
@@ -8,19 +8,19 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    next(HttpError(401, "Not authorized Beare"));
+    next(HttpError(401, "Not authorized"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !token) {
-      next(HttpError(401, "Not authorized Token"));
+      next(HttpError(401, "Not authorized"));
     }
     req.user = user;
 
     next();
   } catch {
-    next(HttpError(401, "Not authorized ERRR"));
+    next(HttpError(401, "Not authorized"));
   }
 };
 
