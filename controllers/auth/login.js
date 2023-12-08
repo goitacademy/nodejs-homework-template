895a11136fccb238c9b258ad86d1c5aae88f435c
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:26:29 +0200

    validation: user  and password. Resp token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -6,6 +6,23 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "4h" });
+
+  res.json({
+    token,
+    user: {
+      email,
+      subscription: "starter",
+    },
+  });
+};
 
 module.exports = login;
