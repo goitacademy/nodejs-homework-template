fbf41f66f50f37c6a2d142ef681355c93950a0eb
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-12-08 13:28:06 +0200

    add sendmail files

---
+++ helpers/sendEmail.js
@@ -0,0 +1,38 @@
+// import nodemailer from 'nodemailer';
+// import 'dotenv/config';
+
+// const { UKR_NET_PASSWORD, UKR_NET_EMAIL } = process.env;
+
+// const nodemailerConfig = {
+//   host: 'smtp.ukr.net',
+//   port: 465, // 25, 465, 2525
+//   secure: true,
+//   auth: {
+//     user: UKR_NET_EMAIL,
+//     pass: UKR_NET_PASSWORD,
+//   },
+// };
+// const transport = nodemailer.createTransport(nodemailerConfig);
+
+// const data = {
+//   from: UKR_NET_EMAIL,
+//   to: 'megasuslik666@gmail.com',
+//   subject: 'Test email',
+//   html: '<strong>Test email</strong>',
+// };
+
+// transport
+//   .sendMail(data)
+//   .then(() => {
+//     console.log('mail was send');
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
+
+// const sendEmail = (data) => {
+//   const email = { ...data, from: UKR_NET_EMAIL };
+//   return transport.sendMail(email);
+// };
+
+// export default sendEmail;
