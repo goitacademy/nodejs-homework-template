b6aed1539d1f6c392df8db7542d55e98c4852bf5
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:34:05 +0200

    added populate  by owner

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,7 @@ const { Contact } = require("../../models/contact");
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
 
-  const result = await Contact.find({ owner });
+  const result = await Contact.find({ owner }).populate("owner", "email -_id");
   res.json(result);
 };
 
