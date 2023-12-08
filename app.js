bd4a8b84606f6b8c0cb2cc055a64301b1b41d1f1
Anna-1987 <mag260910@gmail.com>
2023-12-08 16:14:23 +0200

    home-4

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv')
 
-const contactsRouter = require('./routes/api/contacts')
+const authRouter = require('./routes/api/auth');
+const contactsRouter = require('./routes/api/contacts');
 
+dotenv.config();
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -12,6 +15,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
@@ -19,7 +23,8 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  const { status = 500} = err;
+  res.status(status).json({ message: err.message });
+});
 
 module.exports = app
---
+++ controllers/contacts.js
@@ -0,0 +1,66 @@
+const {Contact} = require('../models/contact');
+const HttpError = require('../helpers/HttpError');
+const {ctrlWrapper} = require('../helpers/ctrWrapper');
+
+  const getAllContacts = async (req, res) => {
+      const{_id: owner} = req.user;
+      const{page = 1, limit = 20} = req.query;
+      const skip = (page - 1) * limit;
+      const result = await Contact.find({owner}, {skip, limit}).populate('owner', 'name email');
+      res.json(result);
+  }
+
+  const getContactById = async (req, res) => {
+    
+       const {id}  = req.params;
+       const result = await Contact.findById(id);
+       if(!result){
+         throw HttpError (404, 'Not found');
+       }
+        res.json(result);
+    }
+  
+   const addContact = async (req, res) => {
+    const{_id: owner} = req.user;
+    const result = await Contact.create({...req.body, owner});
+    res.status(201).json(result);
+    }
+
+  const deleteContactById = async (req, res) => {
+    const {id} = req.params;
+    const result = await Contact.findByIdAndDelete(id);
+    if(!result){
+      throw HttpError (404, 'Not found')
+    }
+    res.json({
+      message:'Delete success'
+    })
+  }
+  
+  const updateContact = async (req, res) => {
+    const{id} = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body,{new:true});
+    if(!result){
+     throw HttpError(404, 'Not found');
+    }
+    res.json(result);
+   }
+
+   const updateFavorite = async (req, res) => {
+    const{id} = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body,{new:true});
+    if(!result){
+     throw HttpError(404, 'Not found');
+    }
+    res.json(result);
+   }
+
+  module.exports = {
+    getAllContacts: ctrlWrapper(getAllContacts),
+    getContactById: ctrlWrapper(getContactById),
+    addContact: ctrlWrapper(addContact),
+    deleteContactById: ctrlWrapper(deleteContactById),
+    updateContact: ctrlWrapper(updateContact),
+    updateFavorite: ctrlWrapper(updateFavorite),
+  }
+  
