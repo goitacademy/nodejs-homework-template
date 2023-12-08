833f875295624665b1edfd78550447cc5e5b88cd
Sokilskill <vprovorotov@gmail.com>
2023-12-08 22:34:29 +0200

    pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,8 +2,13 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }).populate("owner", "email -_id");
+  const result = await Contact.find({ owner }, { skip, limit }).populate(
+    "owner",
+    "email -_id"
+  );
   res.json(result);
 };
 
