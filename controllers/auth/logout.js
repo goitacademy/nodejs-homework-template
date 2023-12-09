3d44ce692ab353ca931875a42d752a3d154bfb80
Sokilskill <vprovorotov@gmail.com>
2023-12-09 10:46:10 +0200

    logout

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,5 +1,9 @@
+const { User } = require("../../models/user");
+
 const logout = async (req, res) => {
-  console.log("logout");
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).json();
 };
 
 module.exports = logout;
