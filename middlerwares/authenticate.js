b0cceb022a8f55cb0e00f13469282c9fe06163d2
Sokilskill <vprovorotov@gmail.com>
2023-12-09 14:00:30 +0200

    Update authenticate.js
    перевірка токена який передається з токеном який зберігається

--- middlerwares/authenticate.js
+++ middlerwares/authenticate.js
@@ -13,7 +13,7 @@ const authenticate = async (req, res, next) => {
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !token) {
+    if (!user || !token || user.token !== token) {
       next(HttpError(401, "Not authorized"));
     }
     req.user = user;
