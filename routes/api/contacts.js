8fd3ce8e9e68fdfbdf214fd69bc6c292f2796ae9
a7ndriyyy <andrii.nastych@gmail.com>
2023-12-09 17:21:08 +0100

    done?

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+const ctrl = require("../../controllers/contacts");
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/", ctrl.listContacts);
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/:contactId", ctrl.getContactById);
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.post("/", ctrl.addContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.delete("/:contactId", ctrl.removeContact);
+
+router.put("/:contactId", ctrl.updateContact);
 
 module.exports = router;
