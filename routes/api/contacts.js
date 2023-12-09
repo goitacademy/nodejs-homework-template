31e63bdab5f472c602ac990831accf0b6e7c74db
Artchem <chemerys.artem@gmail.com>
2023-12-09 20:43:02 +0100

    update ctrl contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,32 +2,35 @@ const express = require("express");
 
 const router = express.Router();
 
-const controllers = require("../../controllers/contacts");
+// const controllers = require("../../controllers/contacts");
+const { ctrlWrapper } = require("../../helpers");
+
+const { contacts: ctrl } = require("../../controllers");
 
 const { validateBody, isValidId } = require("../../middlewares");
 
 const schemas = require("../../schemas/contacts");
 
-router.get("/", controllers.getAll);
+router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", isValidId, controllers.getById);
+router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getById));
 
-router.post("/", validateBody(schemas.addSchema), controllers.add);
+router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
-router.delete("/:contactId", isValidId, controllers.deleteById);
+router.delete("/:contactId", isValidId, ctrlWrapper(ctrl.deleteById));
 
 router.put(
   "/:contactId",
   isValidId,
   validateBody(schemas.addSchema),
-  controllers.updateById
+  ctrlWrapper(ctrl.updateById)
 );
 
 router.patch(
   "/:contactId/favorite",
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
-  controllers.updateFavorite
+  ctrlWrapper(ctrl.updateFavorite)
 );
 
 module.exports = router;
