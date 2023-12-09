beb63eedd1ba91f2dd62728cb02d563d90333fc9
Sokilskill <vprovorotov@gmail.com>
2023-12-09 14:56:53 +0200

    filter by favorite
    фільтрація контактів по полю обраного (GET /contacts?favorite=true)

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,13 +2,19 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "", { skip, limit }).populate(
-    "owner",
-    "email -_id"
-  );
+  const query = { owner };
+
+  if (favorite === "true") {
+    query.favorite = true;
+  }
+
+  const result = await Contact.find(query, "", {
+    skip,
+    limit,
+  }).populate("owner", "email -_id");
   res.json(result);
 };
 
