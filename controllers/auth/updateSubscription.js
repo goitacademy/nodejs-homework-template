2b89cd89020919f690ae7ff1d2ddc7b590eb7c5c
Sokilskill <vprovorotov@gmail.com>
2023-12-09 14:55:09 +0200

    added select
    повертає все крім того що є в select

--- controllers/auth/updateSubscription.js
+++ controllers/auth/updateSubscription.js
@@ -2,7 +2,10 @@ const { User } = require("../../models/user");
 
 const updateSubscription = async (req, res) => {
   const { _id } = req.user;
-  const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
+  const result = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+    select: { password: 0, token: 0 },
+  });
   res.json(result);
 };
 
