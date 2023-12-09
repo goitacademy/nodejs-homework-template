a2f7e4b792a499ef955ed82ec08f1099e7041b89
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-12-09 14:58:00 +0100

    moved DB_HOST to config.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Anastasiia:RlBkCxJvIODzMQ4F@cluster0.zzvkij2.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
+// const { DB_HOST } = process.env;
+console.log(process.env);
 mongoose
   .connect(DB_HOST)
   .then(() => {
