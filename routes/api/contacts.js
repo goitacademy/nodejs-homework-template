a2defeba166ce300f14f0da9c31d1d646e02a078
hotgorod <hotgorod@gmail.com>
2023-12-09 18:17:34 +0000

    check for empty body for put request added

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -53,11 +53,15 @@ router.post("/", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
+    if (Object.keys(req.body).length === 0) {
+      throw HttpError(400, "missing fields");
+    }
     const { error } = addSchema.validate(req.body);
     if (error) {
       throw HttpError(400, error.message);
     }
     const { contactId } = req.params;
+    
     const result = await contacts.updateContact(contactId, req.body);
       if (!result) {
         throw HttpError(404, "Not found");
