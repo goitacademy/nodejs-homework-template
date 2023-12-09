9f252e7a29e4cbf809340ae425274f3fd51afff7
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-12-09 20:05:09 +0200

    summ

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,76 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const contacts = require("../../models/contacts");
+const addShema = require("../../shema/contacts");
+const { HttpError } = require("../../helpers");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) {
+      throw HttpError(404, "Not found");
+    } else {
+      res.json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addShema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "missing required name field");
+    } else {
+      const result = await contacts.addContact(req.body);
+      res.status(201).json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = addShema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "missing fields");
+    } else {
+      const { contactId } = req.params;
+      const result = await contacts.updateContact(contactId, req.body);
+      if (!result) {
+        throw HttpError(404, "Not found");
+      }
+      res.status(201).json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
