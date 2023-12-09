f8c759456e0f85a47cb852000a102cd92c9b2ed1
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-12-09 15:17:10 +0100

    DB_HOST  = process.env;

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-const { DB_HOST } = require("./config");
-// const { DB_HOST } = process.env;
-console.log(process.env);
+// const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
