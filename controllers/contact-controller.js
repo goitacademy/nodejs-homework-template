f05baf089d20e31a2fb0cef6b8ab00adad9f3d0c
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-12-10 10:41:48 +0100

    put/bags
    "Not found"

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -23,7 +23,7 @@ const updateById = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
       if (!result) {
-          throw HttpError(400, `Contact with id=${id} was not found`);
+          throw HttpError(400, `Not found`);
       }
       res.json(result);
 };
