6debccdf0e71fcdc9e587b116715e47983caddc5
DenysShchypt <denysshchyptsovit@gmail.com>
2023-12-10 20:27:44 +0200

    fix jason

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const contacts = require('../models/contacts');
 
 const getAll = async (req, res) => {
   const result = await contacts.listContacts();
-  res.json(result)
+  res.json(result);
 };
 
 const getById = async (req, res) => {
