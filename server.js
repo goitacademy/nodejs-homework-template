99f2d76d3579a20a507b2f7e1ca0c72980882a82
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-12-10 19:30:01 +0100

    process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import app from "./app.js";
 import { DB_HOST } from "./config.js";
 
-
+const { DB_HOST } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
