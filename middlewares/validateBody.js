f04f9c4d8ef1d155bdaf725e18443d2d1112271e
Dimentiy88 <bodartinov88@gmail.com>
2023-12-10 19:12:36 +0100

    hw done

---
+++ middlewares/validateBody.js
@@ -0,0 +1,15 @@
+const { httpError } = require("../helpers");
+
+const validateBody = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(httpError(400, error.message));
+    }
+    next();
+  };
+
+  return func;
+};
+
+module.exports = validateBody;
