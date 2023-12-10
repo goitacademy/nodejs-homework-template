3b096c7831dbc685257c0143da987cde9e3331f4
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-12-10 17:16:50 +0200

    hw 02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,64 @@
-// const fs = require('fs/promises')
+import { nanoid } from "nanoid";
+import fs from "fs/promises";
+import path from "path";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models", "contacts.json");
+console.log(contactsPath);
 
-const getContactById = async (contactId) => {}
+const contactsService = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+async function listContacts() {
+  const buffer = await fs.readFile(contactsPath);
+  const data = JSON.parse(buffer);
+  return data;
+}
 
-const addContact = async (body) => {}
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contactId === contact.id) || null;
+}
 
-const updateContact = async (contactId, body) => {}
+async function removeContact(contactId) {
+  const contacts = await listContacts();
 
-module.exports = {
+  const contactToDelete = contacts.findIndex(
+    (contact) => contactId === contact.id
+  );
+  if (contactToDelete !== -1) {
+    const deletedContact = contacts.splice(contactToDelete, 1);
+    await contactsService(contacts);
+    return deletedContact;
+  } else {
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  if (
+    contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    )
+  ) {
+    return { message: "Contact exists" };
+  } else {
+    const contact = {
+      name,
+      email,
+      phone,
+      id: nanoid(),
+    };
+
+    contacts.push(contact);
+    await contactsService(contacts);
+    return contact;
+  }
+}
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,27 @@
-const express = require('express')
+import express from "express";
+import contacts from "../models/contacts.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+export default router;
