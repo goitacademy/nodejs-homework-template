9d469caecbe772124a148c7cdb99af3e6bf88f23
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-12-11 18:11:56 +0200

    fixing models

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,20 @@
 const express = require('express')
-
 const router = express.Router()
-
+const { validateBody } = require("../../middlewares/index");
+const schemas = require("../../schemas/contacts");
 const ctrl = require("../../controllers/contacts");
 
-router.get("/", ctrl.getAllContacts);
+router.get("/", ctrl.listContacts);
 
 router.get("/:contactId", ctrl.getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
 router.delete("/:contactId", ctrl.deleteContact);
 
 router.put(
   "/:contactId",
+  validateBody(schemas.updateSchema),
   ctrl.updateContact
 );
 
