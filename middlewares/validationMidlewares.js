5197f8d06cd974c9924d70b3e8a90e4669d3e7d5
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-12-11 16:38:42 +0200

    finish 2

--- middlewares/validationMidlewares.js
+++ middlewares/validationMidlewares.js
@@ -4,7 +4,6 @@ const validator = (schema) => {
       if (error) {
         error.status = 400;
         error.message = error.details[0].message;
-        // error.message = "missing required name field";
         next(error);
         return;
       }
