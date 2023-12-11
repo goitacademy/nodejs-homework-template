b7dbe21312f5c24da5adf85b638e9e05307d01e7
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-12-11 10:18:22 +0100

    decorators and validateBody added

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -3,86 +3,53 @@ import {
   listContacts,
   addContact,
   updateContactById,
-  removeContact
+  removeContact,
 } from "../models/contacts.js";
 import { HttpError } from "../helpers/index.js";
-import {
-  contactsAddSchema,
-  contactsUpdateSchema,
-} from "../schemas/contacts-schemas.js";
+import { ctrlWrapper } from "../decorators/index.js";
 
-const getAll = async (req, res, next) => {
-  try {
-    const result = await listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (req, res) => {
+  const result = await listContacts();
+  res.json(result);
 };
 
 const getById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await getContactById(contactId);
-    if (!result) {
-      HttpError(404, `Not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const result = await getContactById(contactId);
+  if (!result) {
+    HttpError(404, `Not found`);
   }
+  res.json(result);
 };
 
 const add = async (req, res, next) => {
-  try {
-    //перевіряю на вимоги по contactsAddSchema, якщо є помилка, то вона зберігається в полі error
-    const { error } = contactsAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await addContact(req.body);
-
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+  const result = await addContact(req.body);
+  res.status(201).json(result);
 };
 
 const removeById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await removeContact(contactId)
-    if (!result) {
-      throw HttpError(404, "Not found")
-    }
-    res.json({message: "contact deleted"})
-
-  } catch (error) {
-    next(error)
+  const { contactId } = req.params;
+  const result = await removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, `not found`);
   }
+  res.status(200).json({ message: "contact deleted" });
 };
 
 const updateById = async (req, res, next) => {
-  try {
-    const { error } = contactsUpdateSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await updateContactById(contactId, req.body);
-
-    if (!result) {
-      throw HttpError(404, "Not found")
-    }
-
-    res.json(result)
-  } catch (error) {}
+  const { contactId } = req.params;
+  const result = await updateContactById(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, `Not found`);
+  }
+  res.json(result);
 };
 
 export default {
-  getAll,
-  getById,
-  add,
-  removeById,
-  updateById,
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  removeById: ctrlWrapper(removeById),
+  updateById: ctrlWrapper(updateById),
 };
+
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,8 @@
 import express from "express";
-import contactsController from "../../controllers/contacts-controller.js"
+import contactsController from "../../controllers/contacts-controller.js";
 import isEmptyBody from "../../middlewares/isEmptyBody.js";
-
+import validateBody from "../../decorators/validateBody.js";
+import { contactsAddSchema, contactsUpdateSchema } from "../../schemas/contacts-schemas.js";
 
 export const router = express.Router();
 
@@ -9,9 +10,18 @@ router.get("/", contactsController.getAll);
 
 router.get("/:contactId", contactsController.getById);
 
-router.post("/", isEmptyBody, contactsController.add);
+router.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactsAddSchema),
+  contactsController.add
+);
 
 router.delete("/:contactId", contactsController.removeById);
 
-router.put("/:contactId", isEmptyBody, contactsController.updateById);
-
+router.put(
+  "/:contactId",
+  isEmptyBody,
+  validateBody(contactsUpdateSchema),
+  contactsController.updateById
+);
