4347994ec122f61a829840f58b4c23fdb1b1a41b
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-12-11 17:49:53 +0200

    adding routes (step 2)

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,19 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require("../../controllers/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.getAllContacts);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", ctrl.getContactById);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", ctrl.addContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", ctrl.deleteContact);
 
-module.exports = router
+router.put(
+  "/:contactId",
+  ctrl.updateContact
+);
+
+module.exports = router;
