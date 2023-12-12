e16481241372491644f6d2c40142c07fa5cdfbb3
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-12-12 22:31:17 +0200

    branch-02hw

---
+++ decorators/validateBody.js
@@ -0,0 +1,11 @@
+export const validateBody = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    next();
+  };
+
+  return func;
+};
