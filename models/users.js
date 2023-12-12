23f56bdc33989635e44f4a881b6c286f758ca62d
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-12-12 21:55:16 +0100

    add versionKey

--- models/users.js
+++ models/users.js
@@ -2,23 +2,26 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
 
-const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, "Set password for user"],
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: String,
-});
+  { versionKey: false, timestamps: true }
+);
 
 const userSignupSchema = Joi.object({
   password: Joi.string().required(),
