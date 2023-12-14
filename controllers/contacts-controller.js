4594d248856f6d51dc068d416c026b0a0a005d5d
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-12-15 01:09:49 +0200

    decorators done

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,80 +1,51 @@
 import * as contactsService from "../models/index.js";
 import { HttpError } from "../helpers/index.js";
-import {
-  contactAddSchema,
-  contactUpdateSchema,
-} from "../schemas/contact-schemas.js";
-const listContacts = async (req, res, next) => {
-  try {
-    const result = await contactsService.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+
+import { ctrlWrapper } from "../decorators/index.js";
+
+const listContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
 };
 
-const getContactById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsService.getContactById(id);
-    if (!result) {
-      throw HttpError(404, `Contact with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
   }
+  res.json(result);
 };
 
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contactsService.addContact(req.body);
+const addContact = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
 
-    res.status(201).json(result);
-  } catch {
-    next(error);
-  }
+  res.status(201).json(result);
 };
 
-const removeContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsService.removeContact(id);
-    if (!result) {
-      throw HttpError(404, `Contact with id=${id} not found`);
-    }
-    res.json({ message: "Delete success" });
-  } catch {
-    next(error);
+const removeContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
   }
+  res.json({ message: "Delete success" });
 };
 
-const updateById = async (req, res, next) => {
-  try {
-    const { error } = contactUpdateSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await contactsService.updateContact(id, req.body);
-    if (!result) {
-      throw HttpError(404, `Contact with id=${id} not found`);
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
   }
+
+  res.json(result);
 };
 
 export default {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateById,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateById: ctrlWrapper(updateById),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,16 +1,32 @@
 import express from "express";
 import contactsController from "../../controllers/contacts-controller.js";
 import { isEmptyBody } from "../../middlewares/index.js";
+import { validateBody } from "../../decorators/index.js";
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from "../../schemas/contact-schemas.js";
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.listContacts);
 
 contactsRouter.get("/:id", contactsController.getContactById);
 
-contactsRouter.post("/", isEmptyBody, contactsController.addContact);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactAddSchema),
+  contactsController.addContact
+);
 
 contactsRouter.delete("/:id", contactsController.removeContact);
 
-contactsRouter.put("/:id", isEmptyBody, contactsController.updateById);
+contactsRouter.put(
+  "/:id",
+  isEmptyBody,
+  validateBody(contactUpdateSchema),
+  contactsController.updateById
+);
 
 export default contactsRouter;
