90af6abc8f855ba3105e100b0875838b14c3cc79
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-12-14 23:43:23 +0200

    add-api-files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,28 @@
-const express = require('express')
+import { Router } from 'express';
+import * as serviceContacts from "../../models/contacts.js"; 
 
-const router = express.Router()
+const router = Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const result = await serviceContacts.listContacts();
+  res.json(result);
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const result = await serviceContacts.getContactById("AeHIrLTr6JkxGE6SN-0Rw");
+  res.json(result);
+});
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-})
+});
 
-module.exports = router
+export default router;
