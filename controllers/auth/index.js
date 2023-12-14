33f5a3076204e4e90ce9b4138b6ce5134c13dffc
Sokilskill <vprovorotov@gmail.com>
2023-12-14 19:25:17 +0200

    updateAvatar

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,6 +3,7 @@ const login = require("./login");
 const logout = require("./logout");
 const getCurrent = require("./getCurrent");
 const updateSubscription = require("./updateSubscription");
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   register,
@@ -10,4 +11,5 @@ module.exports = {
   logout,
   getCurrent,
   updateSubscription,
+  updateAvatar,
 };
