c2c8510d61438a3c833f590b97114a04025021c0
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-12-14 22:48:31 +0100

    update ES6

--- models/contacts.js
+++ models/contacts.js
@@ -10,7 +10,7 @@ const addContact = async (body) => {}
 
 const updateContact = async (contactId, body) => {}
 
-module.exports = {
+export default {
   listContacts,
   getContactById,
   removeContact,
