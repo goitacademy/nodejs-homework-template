56999de5ead19eaea00b2929646265f7a6ed026a
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2023-12-15 13:14:06 +0100

    add home work 2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,71 @@
 const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  contactBodySchema,
+} = require("../../models/contacts");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Hello home work" });
+  const contacts = await listContacts();
+  res.json(contacts);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contactId = req.params.contactId;
+  const contactById = await getContactById(contactId);
+
+  if (!contactById) {
+    res.status(404).json({ message: "Not Found" });
+    return;
+  }
+
+  res.json(contactById);
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const body = req.body;
+
+  const { error } = contactBodySchema.validate(body);
+  if (error) {
+    res.status(400).json(error);
+    return;
+  }
+  const newContact = await addContact(body);
+
+  res.json(newContact);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contactIdParam = req.params.contactId;
+  const removedContact = await removeContact(contactIdParam);
+  if (!removedContact) {
+    res.status(404).json({ message: "Not Found" });
+    return;
+  }
+  res.json(removedContact);
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contactIdParam = req.params.contactId;
+  const body = req.body;
+
+  const { error } = contactBodySchema.validate(body);
+  if (error) {
+    res.status(400).json(error);
+    return;
+  }
+  const updatedContact = await updateContact(contactIdParam, body);
+  if (!updatedContact) {
+    res.status(404).json({ message: "Not Found" });
+    return;
+  }
+
+  res.json(updatedContact);
 });
 
 module.exports = router;
