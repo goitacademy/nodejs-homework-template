bc3c0967c36ac790c0300158aad6c1c934bb55af
Anna-1987 <mag260910@gmail.com>
2023-12-15 13:35:09 +0200

    home-5

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv')
 
-const contactsRouter = require('./routes/api/contacts')
+const authRouter = require('./routes/api/users');
+const contactsRouter = require('./routes/api/contacts');
 
+dotenv.config();
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -11,7 +14,9 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
 
+app.use('/api/users', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
---
+++ routes/api/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const validateBody = require('../../middelewares/validateBody');
+const ctrl = require('../../controllers/users');
+const {schema} = require('../../models/user');
+const authenticate = require('../../middelewares/authenticate');
+const upload = require('../../middelewares/upload');
+
+const router = express.Router();
+
+router.post('/register', validateBody(schema.registerSchema), ctrl.register);
+
+router.post('/login', validateBody(schema.loginSchema), ctrl.login);
+
+router.get('/current', authenticate, ctrl.getCurrent);
+
+router.post('/logout', authenticate, ctrl.logout);
+
+router.patch('/:id/subscription', authenticate, validateBody(schema.subscriptionSchema), ctrl.subscription);
+
+router.patch('/avatars', authenticate, upload.single('avatar'), ctrl.updateAvatar);
+
+module.exports = router;
