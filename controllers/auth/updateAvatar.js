e7141559b737d5f4afb7b121e3047eaeebabbd8b
Sokilskill <vprovorotov@gmail.com>
2023-12-16 13:32:24 +0200

    added jimp for image resize

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,20 +1,29 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { User } = require("../../models/user");
+const Jimp = require("jimp");
 
+const tmpDir = path.join(__dirname, "../../", "tmp");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
 
   const { path: tempUpload, originalname } = req.file;
-  const filename = `${_id}_${originalname}`;
 
-  const resultUpload = path.join(avatarsDir, filename);
+  const image = await Jimp.read(tempUpload);
+  image.resize(250, 250);
 
-  await fs.rename(tempUpload, resultUpload);
+  const uniqueFinename = `${_id}_${originalname}`;
+  const resultUploadTmp = path.join(tmpDir, originalname);
+  const resultUpload = path.join(avatarsDir, uniqueFinename);
 
-  const avatarURL = path.join("avatars", filename);
+  await image.writeAsync(resultUploadTmp);
+
+  await fs.rename(resultUploadTmp, resultUpload);
+
+  const avatarURL = path.join("avatars", uniqueFinename);
+
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({ avatarURL });
 };
