1f6e8c8f5173ff8c9bf394185e48d1083008b63f
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-12-16 23:01:19 +0200

    init

---
+++ models/contacts/index.js
@@ -0,0 +1,19 @@
+// import fs from 'fs/promises'
+
+const listContacts = async () => {}
+
+const getContactById = async (contactId) => {}
+
+const removeContact = async (contactId) => {}
+
+const addContact = async (body) => {}
+
+const updateContact = async (contactId, body) => {}
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+}
