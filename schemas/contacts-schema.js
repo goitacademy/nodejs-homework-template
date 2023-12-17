23f93a9e5d5b3ee4c383f17edb4c74310096ffdf
OlegPanadiy <opanadiy@gmail.com>
2023-12-17 20:53:57 +0000

    HW update

--- schemas/contacts-schema.js
+++ schemas/contacts-schema.js
@@ -1,11 +1,16 @@
 import Joi from "joi";
 
 export const contactAddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "any.required": "Missing required name field",
+  }),
+  email: Joi.string().required().messages({
+    "any.required": "Missing required email field",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "Missing required phone field",
+  }),
 });
-
 export const contactUpdateSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
