93e52c16189d5b737d43b89717f06a56ab7d5a31
Вікторія <viktoriya23071@gmail.com>
2023-12-17 20:52:52 +0200

    Update updateFavorite.js

--- controller/contacts/updateFavorite.js
+++ controller/contacts/updateFavorite.js
@@ -3,29 +3,14 @@ const { Contact } = require("../../models/contact");
 const { schemas } = require("../../schemas/schemas");
 
 const updateFavorite = async (req, res, next) => {
-    try {
-        const { error } = schemas.updateFavoriteSchema.validate(req.body);
-        if (error) {
-            error.status = 400;
-            throw error;
-        }
-        const { contactId } = req.params;
-        const result = await Contact.findByIdAndUpdate(contactId, req.body, {
-            new: true,
-        });
-        if (!result) {
-            throw createError(404, 'missing field favorite');
-        }
-        res.json({
-            status: 'success',
-            code: 200,
-            data: {
-                result,
-            },
-        });
-    } catch (error) {
-        next(error);
-    }
+    const { contactId } = req.params;
+    if (!req.body) 
+    throw createError(404, 'missing field favorite');
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+    });
+    if (!result) throw createError(404, "Not found");
+    res.status(201).json(result);
 };
 
 module.exports = updateFavorite;
