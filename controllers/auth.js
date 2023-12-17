7c997c191e57d7e3f6d310b9b922e1b7d668df1a
Marianna Demchenko <mdemko2012@gmail.comm>
2023-12-17 20:04:06 +0100

    avatars

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const jimp = require("jimp");
+
 
 const {User} = require("../models/user");
 
@@ -10,7 +15,7 @@ const {
 
 const {SECRET_KEY} = process.env;
 
-console.log({SECRET_KEY});
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
     const {email, password} = req.body;
@@ -21,8 +26,9 @@ const register = async (req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+	const avatarURL = gravatar.url(email, { s: "250", d: "retro" });
 
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
 
     res.status(201).json({
 		user:{
@@ -76,9 +82,34 @@ const logout = async(req, res) => {
 	res.status(204).end();
 }
 
+const cropImage = async (path) => {
+	const image = await jimp.read(path);
+
+	await image
+		.autocrop()
+		.cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE)
+		.writeAsync(path);
+};
+
+const updateAvatar = async(req, res) => {
+	const {_id} = req.user; 
+	const {path: tempUpload, originalname} = req.file;
+	await cropImage(tempUpload);
+	const filename = `${_id}_${originalname}`;
+	const resultUpload = path.join(avatarsDir, filename);
+	await fs.rename(tempUpload, resultUpload);
+	const avatarURL = path.join("avatars", filename);
+	await User.findByIdAndUpdate(_id, {avatarURL});
+	
+	res.json({
+		avatarURL,
+	})
+}
+
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
 	getCurrent: ctrlWrapper(getCurrent),
 	logout: ctrlWrapper(logout),
+	updateAvatar: ctrlWrapper(updateAvatar),
 }
