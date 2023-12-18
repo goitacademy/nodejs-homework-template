c867ffda0b96179883bf25573f7c27c218d07cfa
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-12-18 17:30:45 +0200

    done

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -69,12 +69,12 @@ const deleteContact = async (req, res, next) => {
 const updateContact = async (req, res, next) => {
   try {
     const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
     const result = await contactsService.updateContact(req.params.id, req.body);
     if (!result) {
       throw HttpError(404);
-    }
-    if (error) {
-      throw HttpError(400, error.message);
     }
 
     res.json(result);
