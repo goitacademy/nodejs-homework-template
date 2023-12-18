4a3c0423d5a80ae7f5a2f5f78fb94260abc7baed
Black Chaos <black.chaos.no1@gmail.com>
2023-12-18 23:51:00 +0200

    feat: connect to DB

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,28 +1,34 @@
 const {catchAsync, HttpError} = require('../helpers');
-const contactsDB = require('../models/contacts')
+const {listContacts, addContact, getContactById, removeContact, updateContact, updateStatusContact} = require('../service')
 
 exports.getContacts = catchAsync(async (_, res) => {
-    res.json(await contactsDB.listContacts());
+    res.json(await listContacts());
 });
 
 exports.addContacts = catchAsync(async (req, res) => {
-    res.status(201).json(await contactsDB.addContact(req.body));
+    res.status(201).json(await addContact(req.body));
 });
 
 exports.getContactById = catchAsync(async (req, res) => {
-    const contact = await contactsDB.getContactById(req.params.contactId);
+    const contact = await getContactById(req.params.contactId);
     if(!contact) throw new HttpError(404, 'Not found')
     res.json(contact);
 });
 
 exports.deleteContact = catchAsync(async (req, res) => {
-    const contact = await contactsDB.removeContact(req.params.contactId);
+    const contact = await removeContact(req.params.contactId);
     if (!contact) throw new HttpError(404, 'Not found');
     res.json({ message: 'contact deleted' });
 });
 
 exports.updateContact = catchAsync(async (req, res) => {
-    const contact = await contactsDB.updateContact(req.params.contactId, req.body);
+    const contact = await updateContact(req.params.contactId, req.body);
     if (!contact) throw new HttpError(404, 'Not found');
     res.json(contact);
 });
+
+exports.updateStatus = catchAsync(async (req, res) => {
+    const contact = await updateStatusContact(req.params.contactId, req.body);
+    if (!contact) throw new HttpError(404, 'Not found');
+    res.json(contact);
+})
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { contactsController } = require('../../controllers');
 const { bodySchemaCreate, bodySchemaUpdate } = require('../../schemas/bodySchema');
-const { validateBody } = require('../../middlewares');
+const { validateBody, checkValidId } = require('../../middlewares');
 
 const router = express.Router();
 
@@ -9,11 +9,12 @@ router
     .route('/')
     .get(contactsController.getContacts)
     .post(validateBody.checkCreate(bodySchemaCreate), contactsController.addContacts);
-
+router.use('/:contactId', checkValidId);
 router
     .route('/:contactId')
     .get(contactsController.getContactById)
     .delete(contactsController.deleteContact)
     .put(validateBody.checkUpdate(bodySchemaUpdate), contactsController.updateContact);
+router.route('/:contactId/favorite').patch(validateBody.checkUpdateFavorite, contactsController.updateStatus);
 
 module.exports = router;
