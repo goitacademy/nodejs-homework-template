412bbd61baae10dcb10ccc894fdfe0f9d354f09f
YuliiaDubyniuk <yuliia.dubyniuk@gmail.com>
2023-12-19 22:20:54 -0500

    some structure correction

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
-const contactsRouter = require('./routes/api/contacts');
+const { contactsRouter } = require('./routes/api');
 
 const app = express();
 
---
+++ controllers/contacts/getAll.js
@@ -0,0 +1,8 @@
+const { Contact } = require('../../models/index');
+
+const getAll = async (req, res) => {
+  const result = await Contact.find();
+  res.status(200).json(result);
+};
+
+module.exports = getAll;
