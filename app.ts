a32d57aa1fc770982ef8d126984000b327bfeaf5
chertik77 <babychdenya@gmail.com>
2023-12-21 21:58:45 +0000

    finish

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
-import createHttpError from 'http-errors'
 import logger from 'morgan'
 import { contactsRouter } from './routes/api/contacts'
 
@@ -14,8 +13,8 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.use(() => {
-  createHttpError(400, 'Not found')
+app.use((_req, res: Response) => {
+  res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction): void => {
