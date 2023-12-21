19a948558d05738f0f95b8d3bbb061bf88f2c0b5
astasiempre <ero1494651@gmail.com>
2023-12-21 22:10:24 +0200

    update files

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
-app.use('/api/auth', authRouter)
+app.use('/', authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require('./app')
 const mongoose = require('mongoose');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 // mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
   console.log("Database connection successful")
 })
   })
