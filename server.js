e45bf8e1d896ea80bd3ef24beee185f6d3945be5
Valentun2 <pocomaxawrkt@gmail.com>
2023-12-21 11:52:31 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+
+import mongoose from 'mongoose'
+
 import app  from './app.js'
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST = "mongodb+srv://Valentun:qWLtn4MZ0fW5j9Kn@cluster0.x2miff7.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST).then(()=>
+{
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
+
+}
+).catch((err)=>{
+  console.log(err.message);
+  process.exit(1)
 })
+
