2950c7aab9b54232aa5f11014f3df4e93a46a1b6
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-12-21 21:26:39 +0100

    fixed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -23,7 +23,7 @@ const getContactById = async (req, res) => {
 };
 const addContact = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.create(...req.body, owner);
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 const updateContact = async (req, res) => {
