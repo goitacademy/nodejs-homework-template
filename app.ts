4e729f792825a4c364535f02699163408714bc26
chertik77 <babychdenya@gmail.com>
2023-12-21 18:52:27 +0000

    finish

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
+import createHttpError from 'http-errors'
 import logger from 'morgan'
 import { contactsRouter } from './routes/api/contacts'
 
@@ -13,10 +14,10 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.use((_, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use(() => {
+  createHttpError(400, 'Not found')
 })
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message })
+  res.status(500).json({ message: 'Internal Server Error' })
 })
--- routes/api/contacts.ts
+++ routes/api/contacts.ts
@@ -1,23 +1,55 @@
 import { Router } from 'express'
+import createHttpError from 'http-errors'
+import { addContact, getContactById, listContacts, removeContact, updateContact } from 'models/contacts'
+import { contactsSchema } from 'schemas/contactsSchema'
 
 export const contactsRouter = Router()
 
-contactsRouter.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.get('/', async (_, res, next) => {
+  try {
+    const contacts = await listContacts()
+    res.json(contacts)
+  } catch (error) {
+    next(error)
+  }
 })
 
-contactsRouter.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.get('/:id', async (req, res, next) => {
+  const { id } = req.params
+  const contact = await getContactById(id)
+  if (!contact) {
+    return next(createHttpError(404, `Movie with id=${id} not found`))
+  }
+  res.json(contact)
 })
 
 contactsRouter.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const parsedData = contactsSchema.safeParse(req.body)
+  if (!parsedData.success) {
+    return next(createHttpError(400, 'Bad request'))
+  }
+  const contact = await addContact(parsedData.data)
+  res.status(201).json(contact)
 })
 
-contactsRouter.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.delete('/:id', async (req, res, next) => {
+  const { id } = req.params
+  const contact = await removeContact(id)
+  if (!contact) {
+    return next(createHttpError(404, `Movie with id=${id} not found`))
+  }
+  res.json({ message: 'Deleted successfully' })
 })
 
-contactsRouter.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+contactsRouter.put('/:id', async (req, res, next) => {
+  const parsedData = contactsSchema.safeParse(req.body)
+  if (!parsedData.success) {
+    return next(createHttpError(400, 'Bad request'))
+  }
+  const { id } = req.params
+  const contact = await updateContact(id, req.body)
+  if (!contact) {
+    return next(createHttpError(404, `Movie with id=${id} not found`))
+  }
+  res.json(contact)
 })
