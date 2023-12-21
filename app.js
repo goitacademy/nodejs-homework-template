39dbc74bae8d06552047a64e72c21f54fcd91a71
ElenaKas8 <36508011+Ferst1@users.noreply.github.com>
2023-12-21 20:40:07 +0100

    app

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
 
 import contactsRouter from "./routes/api/contacts-router.js";
-import authRouter from "./routes/api/auth-router.js";
 
 const app = express();
 
@@ -14,10 +11,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); // let frontend to show the files from public/. and now frontend watching the public/ only.
 
-app.use("/api/auth", authRouter);
-app.use("/api/contacts", contactsRouter);
+app.use("/routes/api/contacts", contactsRouter); // /api/contacts ?
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
