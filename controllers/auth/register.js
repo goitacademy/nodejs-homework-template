52a00d9bd9e7f77bd79df587246fcbb32f028f43
Sokilskill <vprovorotov@gmail.com>
2023-12-22 19:32:19 +0200

    sendMail after registration

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,10 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
-const { HttpError } = require("../../helpers");
+const { HttpError, sendEmail } = require("../../helpers");
 const { User } = require("../../models/user");
+const { nanoid } = require("nanoid");
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,8 +15,21 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
 
-  await User.create({ ...req.body, password: hashPassword, avatarURL });
+  await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+    verificationToken,
+  });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/:${verificationToken}">verify you email</a>`,
+  };
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: {
