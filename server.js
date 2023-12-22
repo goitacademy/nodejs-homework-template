38da0e9108ccbde730515e1784e22a4b980704ed
Serhii Haber <haberserhii@icloud.com>
2023-12-22 11:37:27 +0200

    upd

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://haberserhii:SeHV0riXcNLSKRSH@maincluster.pcu4446.mongodb.net/my-contacts";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
