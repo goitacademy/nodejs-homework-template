a958bf7d5b5f4148c129197d8789cb6280e8eabf
Serhii Haber <haberserhii@icloud.com>
2023-12-22 12:12:45 +0200

    upd

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import logger from "morgan";
 import contactsRouter from "./routes/api/contactsRoute.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const DB_HOST =
-  "mongodb+srv://haberserhii:SeHV0riXcNLSKRSH@maincluster.pcu4446.mongodb.net/my-contacts";
+const { DB_HOST, PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
