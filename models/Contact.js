67c325baaee40a9bdf2981cefa60df16ecbb29da
Serhii Haber <haberserhii@icloud.com>
2023-12-22 12:36:14 +0200

    upd

--- models/Contact.js
+++ models/Contact.js
@@ -1,9 +1,20 @@
 import { Schema, model } from "mongoose";
 
 const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Contact = model("contact", contactSchema);
