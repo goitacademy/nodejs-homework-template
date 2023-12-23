fc2d5c07b1750896b1170e2c14125761071b0f98
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-12-24 00:13:31 +0200

    done

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// const express = require('express')
-// const logger = require('morgan')
-// const cors = require('cors')
-
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
-// const contactsRouter = require('./routes/api/contacts')
+import contactsRouter from "./routes/api/contacts-router.js";
 
-import contactsRouter from "./routes/api/contacts.js";
+// ============================================================
 
 const app = express();
 
@@ -25,9 +21,10 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({
+    message,
+  });
 });
-
-// module.exports = app
 
 export default app;
---
+++ controllers/contacts-controller.js
@@ -0,0 +1,57 @@
+import contactsService from '../models/contacts-service.js';
+
+import { HttpError } from '../helpers/index.js';
+import { ctrlWrapper } from '../decorators/index.js';
+
+// ============================================================
+
+const getAll = async (req, res) => {
+  const result = await contactsService.listContacts();
+
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  console.log(req.params);
+  const result = await contactsService.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, 'Contact not found');
+  }
+
+  res.json(result);
+};
+
+const add = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, 'Contact not found');
+  }
+
+  res.json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, 'Contact not found');
+  }
+
+  res.json({ message: 'contact deleted' });
+};
+
+export default {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
