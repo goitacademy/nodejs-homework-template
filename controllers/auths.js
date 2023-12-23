9cecac26ab542a68f3f1fd749ee04d285abc427f
a7ndriyyy <andrii.nastych@gmail.com>
2023-12-23 14:56:40 +0100

    commit

---
+++ controllers/auths.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { ctrlWrapper, HttpError } = require("../helpers");
+const { SEKRET_KEY } = process.env;
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user) throw HttpError(409, "Email in use");
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: "starter",
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) throw HttpError(401, "Email or password is wrong");
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) throw HttpError(401, "Email or password is wrong");
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SEKRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.json({ email, subscription });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json();
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+};
