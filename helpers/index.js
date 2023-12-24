4451f68eaf45f832efe117c1bc89d02f82ed7ed0
Вікторія <viktoriya23071@gmail.com>
2023-12-24 16:58:20 +0200

    HttpError

---
+++ helpers/index.js
@@ -0,0 +1,7 @@
+const handleSaveErrors = require("./handleSaveErrors");
+const RequestError = require("./RequestError");
+
+module.exports = {
+    handleSaveErrors,
+    RequestError,
+}
---
+++ middlewares/authenticate.js
@@ -0,0 +1,2 @@
+const jwt = require("jsonwebtoken");
+const user 
