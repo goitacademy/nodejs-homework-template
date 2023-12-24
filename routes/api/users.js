2eb410e818ab6e33c3a0268dcd2cb98baabca6d2
Вікторія <viktoriya23071@gmail.com>
2023-12-24 21:30:31 +0200

    sum

---
+++ routes/api/users.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const ctrl = require("../../controllers/users");
+const {validateBody, authenticate} = require("../../middlewares");
+const {userSchemas} =require("../../models");
+const router = express.Router();
+
+
+router.post(
+    "/register",
+    validateBody(userSchemas.registerSchema),
+    ctrl.register
+  );
+  
+  router.post("/login", validateBody(userSchemas.loginSchema), ctrl.login);
+  
+  router.post("/logout", authenticate, ctrl.logout);
+  
+  router.get("/current", authenticate, ctrl.getCurrent);
+  
+  router.patch(
+    "/",
+    authenticate,
+    validateBody(userSchemas.updSubscriptionSchema),
+    ctrl.updateSubscription
+  );
+  
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
 const app = require('./app');
 const mongoose = require('mongoose');
 const { DB_HOST, PORT = 3000 } = process.env;
-
+mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful");
-    app.listen(PORT, () => {
+      app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
