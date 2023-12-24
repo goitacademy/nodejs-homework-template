8d9270f10620a3865feb1a86f25ed34e626f2c80
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-12-24 22:57:40 +0200

    done

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,40 +1,14 @@
-import contactsService from "../models/contacts.js";
 import HttpError from "../helpers/http-error.js";
-import Joi from "joi";
-
-const addContactSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": "missing required name field",
-  }),
-  email: Joi.string().required().messages({
-    "any.required": "missing required email field",
-  }),
-  phone: Joi.string().required().messages({
-    "any.required": "missing required phone field",
-  }),
-});
-
-const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-});
+import Contact from "../models/Contact.js";
+import {
+  addContactSchema,
+  updateContactSchema,
+  updateStatusSchema,
+} from "../models/Contact.js";
 
 const getAll = async (req, res, next) => {
   try {
-    const result = await contactsService.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-};
-
-const getById = async (req, res, next) => {
-  try {
-    const result = await contactsService.getContactById(req.params.id);
-    if (!result) {
-      throw HttpError(404);
-    }
+    const result = await Contact.find();
     res.json(result);
   } catch (error) {
     next(error);
@@ -47,8 +21,20 @@ const add = async (req, res, next) => {
     if (error) {
       throw HttpError(400, error.message);
     }
-    const result = await contactsService.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const result = await Contact.findById(req.params.id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
   } catch (error) {
     next(error);
   }
@@ -56,7 +42,7 @@ const add = async (req, res, next) => {
 
 const deleteContact = async (req, res, next) => {
   try {
-    const result = await contactsService.removeContact(req.params.id);
+    const result = await Contact.findByIdAndDelete(req.params.id);
     if (!result) {
       throw HttpError(404);
     }
@@ -72,7 +58,10 @@ const updateContact = async (req, res, next) => {
     if (error) {
       throw HttpError(400, error.message);
     }
-    const result = await contactsService.updateContact(req.params.id, req.body);
+    const result = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     if (!result) {
       throw HttpError(404);
     }
@@ -83,4 +72,31 @@ const updateContact = async (req, res, next) => {
   }
 };
 
-export default { getAll, getById, add, deleteContact, updateContact };
+const updateStatusContact = async (req, res, next) => {
+  try {
+    const { error } = updateStatusSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!result) {
+      throw HttpError(404);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAll,
+  add,
+  getById,
+  updateContact,
+  deleteContact,
+  updateStatusContact,
+};
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,17 +1,26 @@
 import express from "express";
 import contactsController from "../../controllers/contacts-controller.js";
 import isEmptyBody from "../../middlewares/isEmptyBody.js";
+import isValidId from "../../middlewares/isValidId.js";
+import isEmptyBodyFav from "../../middlewares/isEmptyBodyFav.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAll);
-
-contactsRouter.get("/:id", contactsController.getById);
-
 contactsRouter.post("/", isEmptyBody, contactsController.add);
-
-contactsRouter.delete("/:id", contactsController.deleteContact);
-
-contactsRouter.put("/:id", isEmptyBody, contactsController.updateContact);
+contactsRouter.get("/:id", isValidId, contactsController.getById);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  contactsController.updateContact
+);
+contactsRouter.delete("/:id", isValidId, contactsController.deleteContact);
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBodyFav,
+  contactsController.updateStatusContact
+);
 
 export default contactsRouter;
