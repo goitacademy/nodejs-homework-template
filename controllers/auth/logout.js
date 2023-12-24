b1e16795cf34b0c2b1a611164a720a3304fccf1d
Вікторія <viktoriya23071@gmail.com>
2023-12-24 14:18:57 +0200

    Update logout.js

---
+++ controllers/auth/logout.js
@@ -0,0 +1,16 @@
+const { User } = require("../../models/user");
+
+const logout = async (req, res) => {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { token: null });
+
+    res.json({
+        status: "success",
+        code: 204,
+        data: {
+            message: "Logout success"
+        },
+    });
+};
+
+module.exports = logout;
