7102f9d75c484c37785b89e94fb084597d6f1ee1
Roman <jrj80mail@gmail.com>
2023-12-25 18:22:21 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -24,28 +24,41 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, "./contacts.json"); //*     "contacts.json" - теж можна записати
 // console.log(contactsPath);
 
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
+  console.log(data); //  виведе буфер в 16-му форматі
+  console.log(data.toString()); //  перетворить його у рядок
   return JSON.parse(data);
 }
 
-async function getContactById(id) {
+//! -------------   ВАРІАНТ-2 запису асинхронної ф-ції   -------------------
+// const listContacts = async () => {
+//   const data = await fs.readFile(contactsPath);
+//   return JSON.parse(data);
+// };
+
+async function getContactById(contactId) {
   const people = await listContacts();
-  const result = people.find((item) => item.id === id);
+  const result = people.find((item) => item.id === contactId);
   return result || null;
 }
 
-async function removeContact(id) {
+async function removeContact(contactId) {
   const people = await listContacts();
-  const index = people.findIndex((item) => item.id === id);
+  const index = people.findIndex((item) => item.id === contactId);
   if (index === -1) {
     return null;
   }
   const [result] = people.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(people, null, 3));
+  //! JSON.stringify(value, replacer, space);
+  //*    value: Об'єкт для перетворення у JSON.
+  //*    replacer: Ф-ція або масив для фільтрації включених у JSON об'єктів. Якщо ви не хочете застосовувати фільтр, можна використовувати 'null'.
+  //*    space: К-сть пробілів для відступу в JSON-рядку. Може бути числом або рядком. Також може бути використано для створення красивого форматування JSON-рядка.
+  //*           Якщо не треба відступів, ви можна використовувати 'null' або просто не вказувати цей параметр.
   return result;
 }
 
@@ -60,11 +73,48 @@ async function addContact(data) {
   return newContact;
 }
 
+// async function updateContact(contactId, data) {
+//   const people = await listContacts();
+//   const index = people.findIndex((contact) => contact.id === contactId);
+//   if (index === -1) return null;
+//   people[index] = { ...people[index], ...data };
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 3));
+//   return people[index];
+// }
+
+// Асинхронна ф-ція для оновлення контакту за його ідентифікатором та новими даними
+async function updateContact(contactId, data) {
+  // Отримати список контактів
+  const people = await listContacts();
+  // Знайти індекс контакту з вказаним ідентифікатором
+  const index = people.findIndex((contact) => contact.id === contactId);
+  // Якщо контакт не знайдено, повернути null
+  if (index === -1) return null;
+
+  // Оновити дані контакту, залишаючи інші дані незміненими
+  people[index] = { ...people[index], ...data };
+  // Записати оновлений список контактів у файл
+  await fs.writeFile(contactsPath, JSON.stringify(people, null, 3));
+  // Повернути оновлену інформацію про контакт
+  return people[index];
+}
+
+//! -------------   ВАРІАНТ-2 запису асинхронної ф-ції   -------------------
+// const updateContact = async (contactId, data) => {
+//   const people = await listContacts();
+//   const index = people.findIndex((contact) => contact.id === contactId);
+//   if (index === -1) return null;
+//   people[index] = { ...people[index], ...data };
+//   await fs.writeFile(contactsPath, JSON.stringify(people, null, 2));
+//   return people[index];
+// };
+
 const contacts = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 };
 
 module.exports = contacts;
