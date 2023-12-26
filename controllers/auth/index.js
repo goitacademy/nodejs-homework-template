4c22af6687f815b380fe8ddb6f8fffaff3a518e0
YuliiaDubyniuk <yuliia.dubyniuk@gmail.com>
2023-12-25 22:44:35 -0500

    hw done

---
+++ controllers/auth/index.js
@@ -0,0 +1,14 @@
+const { ctrlWrapper } = require("../../helpers/index.js");
+const register = require('./register.js');
+const login = require('./login.js');
+const getCurrent = require('./getCurrent.js');
+const logout = require('./logout.js');
+const updateUserSubscription = require('./updateUserSubscription.js');
+
+module.exports = {
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
+    updateUserSubscription: ctrlWrapper(updateUserSubscription)
+}
--- controllers/contacts/addNew.js
+++ controllers/contacts/addNew.js
@@ -1,7 +1,8 @@
 const { Contact } = require('../../models');
 
 const addNew = async (req, res) => {
-    const result = await Contact.create(req.body);
+    const { _id: owner } = req.user;
+    const result = await Contact.create({ ...req.body, owner });
     res.status(201).json(result);
 };
 
