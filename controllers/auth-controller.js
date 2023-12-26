e4ac91e54f7c520f1a9cc3ca4ec2a8268757b8fb
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2023-12-26 18:40:25 -0500

    update

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,8 +1,11 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
 import dotenv from "dotenv";
+import gravatar from "gravatar";
+import fs from "fs/promises";
+import path from "path";
 
+import User from "../models/user.js";
 import { HttpError } from "../helpers/index.js";
 
 import controllerWrapper from "../decorators/controllerWrapper.js";
@@ -10,6 +13,8 @@ import controllerWrapper from "../decorators/controllerWrapper.js";
 dotenv.config();
 
 const { JWT_SECRET } = process.env;
+
+const avatarPath = path.resolve("public", "avatars");
 
 const singup = async (req, res) => {
 	const { email, password } = req.body;
@@ -21,11 +26,24 @@ const singup = async (req, res) => {
 
 	const hashPassword = await bcrypt.hash(password, 10);
 
-	const newUser = await User.create({ ...req.body, password: hashPassword });
+	let avatarURL = gravatar.url(email, {
+		s: "200",
+		r: "pg",
+		d: "avatar",
+	});
+	console.log(avatarURL)
+
+	const { path: oldPath, filename } = req.file;
+	const newPath = path.join(avatarPath, filename);
+	await fs.rename(oldPath, newPath);
+	avatarURL = path.join("avatars", filename);
+
+	const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
 	res.status(201).json({
 		email: newUser.email,
 		subscription: newUser.subscription,
+		avatarURL,
 	});
 };
 
@@ -46,7 +64,7 @@ const singin = async (req, res) => {
 	};
 
 	const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
-	await User.findByIdAndUpdate(user._id, {token})
+	await User.findByIdAndUpdate(user._id, { token });
 
 	res.json({
 		token,
@@ -79,10 +97,33 @@ const updateSubscription = async (req, res) => {
 	res.json(result);
 };
 
+const updateAvatar = async (req, res) => {
+	const { token } = req.user;
+	let avatarURL = req.user.avatarURL
+	const { path: oldPath, filename } = req.file;
+	const newPath = path.join(avatarPath, filename);
+	await fs.rename(oldPath, newPath);
+	avatarURL = path.join("avatars", filename);
+
+	const result = await User.findOneAndUpdate({ token }, { avatarURL }, { new: true });
+	if(!result) {
+		throw HttpError(404, "User not found")
+	}
+	if(req.user.avatarURL) {
+		const oldAvatarPath = path.join(path.resolve("public", req.user.avatarURL))
+		await fs.unlink(oldAvatarPath)
+	}
+
+	res.json({
+		avatarURL: result.avatarURL
+	})
+};
+
 export default {
 	singup: controllerWrapper(singup),
 	singin: controllerWrapper(singin),
 	signout: controllerWrapper(signout),
 	getCurrent: controllerWrapper(getCurrent),
 	updateSubscription: controllerWrapper(updateSubscription),
+	updateAvatar: controllerWrapper(updateAvatar)
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,40 @@
-import express from "express"
+import express from "express";
 
-import authController from "../controllers/auth-controller.js"
+import authController from "../controllers/auth-controller.js";
 
-import { isEmptyBody}  from "../middlewares/index.js";
+import { isEmptyBody } from "../middlewares/index.js";
 import validateBody from "../decorators/validateBody.js";
 
 import { userSigninScheme, userSignupScheme, userUpdateSubscriptionScheme } from "../models/user.js";
 import authenticate from "../middlewares/authenticate.js";
+import  upload  from "../middlewares/upload.js";
+import resizeAvatar from "../middlewares/reseizeAvatar.js";
 
+const authRouter = express.Router();
 
+authRouter.post(
+	"/users/register",
+	upload.single("avatarURL"),
+	isEmptyBody,
+	validateBody(userSignupScheme),
+    resizeAvatar,
+	authController.singup
+);
 
-const authRouter = express.Router()
+authRouter.post("/users/login", isEmptyBody, validateBody(userSigninScheme), authController.singin);
 
-authRouter.post('/users/register', isEmptyBody, validateBody(userSignupScheme), authController.singup)
+authRouter.post("/users/logout", authenticate, authController.signout);
 
-authRouter.post('/users/login', isEmptyBody, validateBody(userSigninScheme), authController.singin)
+authRouter.get("/users/current", authenticate, authController.getCurrent);
 
-authRouter.post('/users/logout', authenticate, authController.signout)
+authRouter.patch(
+	"/users",
+	isEmptyBody,
+	authenticate,
+	validateBody(userUpdateSubscriptionScheme),
+	authController.updateSubscription
+);
 
-authRouter.get('/users/current', authenticate,  authController.getCurrent)
+authRouter.patch("/users/avatar", upload.single("avatarURL"), resizeAvatar, authenticate, authController.updateAvatar)
 
-authRouter.patch('/users', isEmptyBody, authenticate, validateBody(userUpdateSubscriptionScheme), authController.updateSubscription)
-
-export default authRouter
+export default authRouter;
