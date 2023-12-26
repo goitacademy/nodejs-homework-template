794823c4dfae59487c5fc3a6ecf30b7791f88cdf
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-12-26 14:51:48 +0100

    added authenticate middleware

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,20 +1,28 @@
 const express = require("express");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const router = express.Router();
 const { schemas } = require("../../models/contact");
 
 const cntrl = require("../../controllers/contacts");
-router.get("/", cntrl.getAll);
+router.get("/", authenticate, cntrl.getAll);
 
-router.get("/:id", isValidId, cntrl.getById);
+router.get("/:id", authenticate, isValidId, cntrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), cntrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), cntrl.add);
 
-router.delete("/:id", isValidId, cntrl.deleteById);
+router.delete("/:id", authenticate, isValidId, cntrl.deleteById);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), cntrl.update);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  cntrl.update
+);
+
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   cntrl.updateStatusContact
