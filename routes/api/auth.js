d34ff329b6998502d221ecc93c023ecd8f1a102f
YuliiaDubyniuk <yuliia.dubyniuk@gmail.com>
2023-12-26 12:30:21 -0500

    routes corrected

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require('../../controllers/auth');
 
-const { validateBody, isValidId, authenticate } = require('../../middlewares');
+const { validateBody, authenticate } = require('../../middlewares');
 
 const { schemas } = require('../../models/user');
 
