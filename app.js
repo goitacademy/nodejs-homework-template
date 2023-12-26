98870724fe391e6b0897cf43cd5b99611231c000
maershaa <valeria.work2022@gmail.com>
2023-12-26 08:48:39 +0200

    requests are ready
    ready
    GET /api/contacts
    GET /api/contacts/:id
    POST /api/contacts
    DELETE /api/contacts/:id
    PUT /api/contacts/:id

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express'); // Express - фреймворк для создания веб-серверов и API на Node.js
+const logger = require('morgan'); // Middleware для вывода в консоль HTTP-запросов
+const cors = require('cors'); // Middleware для обработки CORS (Cross-Origin Resource Sharing) в Express
+
 
 const contactsRouter = require('./routes/api/contacts')
 
-const app = express()
+const app = express() // app - наш веб-сервер  //Вызываем его как функцию
+
+
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+
+// !app.use() в Express используется middleware. Эти функции могут обрабатывать запросы, модифицировать объекты запросов и ответов или выполнять аутентификацию. Они принимают три аргумента: req (запрос), res (ответ) и next (следующая функция middleware).
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter) //все запросы, начинающиеся с /api/contacts, будут передаваться в contactsRouter для обработки
 
+
+// !обрабатывает запросы, попавшие на несуществующие страницы или маршруты в вашем Express-приложении.
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
+
+// !используется для обработки ошибок
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const {status = 500, message = 'Server error'} = err
+  res.status(status).json({ message })
 })
+
+
+// Пример мидлвэр - функции, которая обрабатывает запрос на /contacts
+/* app.get("/contacts", (request, response)=>{
+  console.log(request.url); // Выводит URL запроса
+  console.log(request.method); // Выводит HTTP метод запроса
+}
+) */
+
+
 
 module.exports = app
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,106 @@
 const express = require('express')
+const Joi = require('joi');//библиотека для проверки и валидации входных данных (например при добоавлении нового контакта чтобы был номер и имя, а не только имя)
+const contacts= require("../../models/contacts")
+const router = express.Router() //Этот объект позволяет группировать обработчики маршрутов, связанные с определенными путями URL то есть создает страничку записной книжки, а не новую книжку 
 
-const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const {HttpError} = require("../../helpers/index")
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email:Joi.string().required(),
+  phone:Joi.string().required(),
 })
 
+
+
+
+
+router.get('/', async (req, res, next) => {
+try {
+  const result = await contacts.listContacts()
+  res.json(result)
+  // console.table(result)
+} catch (error) {
+next(error) //Express ищет не простo следующий обработчик, а именнo обработчик ошибок. В нашем случае это хранщаяся в файле app.js последняя middleware  
+} })
+
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+const {id} = req.params;
+console.log(req.params)
+const result = await contacts.getContactById(id)
+if(!result){
+  throw HttpError(404,"Not Found");
+}
+
+res.json(result)
+
+  } catch (error) {
+    next(error) 
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+
+const {error} = addSchema.validate(req.body) //проверяем на соответствие требованиям, указанным в схеме
+console.log("error:", error)
+
+if(error){ // если по схеме ошибка в получаемых данных, то выбрасываем ошибку
+  throw HttpError(404,error.message)
+}
+    // console.log(req.body)
+    const result = await contacts.addContact(req.body)
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)  
+  }
 })
+
+
+
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params
+    const result = await contacts.deleteContact(contactId)
+    console.log(result)
+    if(!result){ //если результат null 
+      throw HttpError(404,"Not Found");
+    }
+    res.json({
+      message: "Delete success"
+    })
+    console.table('result', result); 
+  } catch (error) {
+    next(error)  
+
+  }
+
+ 
 })
 
+
+// PUT запрос всегда все обновляет => если мы что-то меняем, то перезаписываем полностью наше элемент и вводим все поля, и те которые изменились, и те которые нет
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addSchema.validate(req.body) 
+    console.log("req.params:", req.params)
+    console.log("error:", error)
+    if(error){ 
+      throw HttpError(400,error.message)
+    }
+        const {contactId} = req.params
+        const result = await contacts.updateContact(contactId, req.body)
+        if(!result){ //если результат null т.е. нет книги стаким id
+          throw HttpError(404,"Not Found");
+        }
+        res.json(result)
+        console.log('result', result); 
+        } catch (error) {
+          next(error)  
+  }
 })
 
 module.exports = router
