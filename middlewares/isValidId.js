7014b3033bca5976a7aba0050d4a28136c4d82ba
Ruslan <manoiloruslan@gmail.com>
2023-12-26 13:19:16 +0200

    final fix

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,7 +5,7 @@ const isValidId = (req, res, next) => {
     const { id } = req.params;
 
     if (!isValidObjectId(id)) {
-        return next(HttpError(404, `${id} is not valid`))
+        return next(HttpError(400, `${id} is not valid`))
     };
 
     next();
--- models/Contact.js
+++ models/Contact.js
@@ -18,7 +18,7 @@ const contactSchema = new Schema({
         type: Boolean,
         default: false,
     },
-});
+}, {versionKey: false});
 
 contactSchema.post('save', handleSaveError);
 
