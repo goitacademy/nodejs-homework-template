5e15a0c0f5974e2d1e8433d3109af0df1e7aa372
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-12-27 19:11:14 +0200

    completed hw 05

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,13 @@
 const validateBody = require('./validateBody');
 const isValidId = require('./isValidId');
+const isBodyEmpty = require('./IsBodyEmpty');
+const authenticate = require('./authenticate');
+const upload = require('./upload');
 
 module.exports = {
   validateBody,
   isValidId,
+  isBodyEmpty,
+  authenticate,
+  upload,
 };
