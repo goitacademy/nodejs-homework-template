7a994882c652456f17ae706bf880ab4ade4f5b16
Roman <jrj80mail@gmail.com>
2023-12-27 21:50:57 +0200

    +put +delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -91,12 +91,45 @@ router.post("/", async (req, res, next) => {
   // res.json({ message: "template message" });
 });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+// router.delete("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.put("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await contacts.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    // res.status(204).send()
+    res.json({
+      message: "Delete success",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(4001, () => {
-  console.log("Server running. Use our API on port: 4001");
+app.listen(3002, () => {
+  console.log("Server running. Use our API on port: 3002");
 });
