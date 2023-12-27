a6aad307f2c8e0814a972c6b139fd741b22ab29c
Вікторія <viktoriya23071@gmail.com>
2023-12-27 22:24:36 +0200

    midddlew

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,12 +3,12 @@ const register = require('./register.js');
 const login = require('./login.js');
 const getCurrent = require('./getCurrent.js');
 const logout = require('./logout.js');
-const updateUserSubscription = require('./updateUserSubscription.js');
+const updateSubscription = require('./updateSubscription.js');
 
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
-    updateUserSubscription: ctrlWrapper(updateUserSubscription)
+    updateSubscription: ctrlWrapper(updateSubscription)
 }
