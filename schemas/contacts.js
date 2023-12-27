0144778959b1b1b8bd135d4f96f2ca58393098d7
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-12-27 09:52:53 +0200

    fix addSchema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,15 @@
 const Joi = require("joi");
+
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .replace(/[^\d]+/g, "")
+    .min(10)
+    .max(10)
+    .required(),
 });
+
 module.exports = {
   addSchema,
 };
