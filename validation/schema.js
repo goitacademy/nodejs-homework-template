e8ef354c856fdae3e14d45fde6e852f84bc3aa79
HalynaLastivka <lastivkagalina@gmail.com>
2023-12-28 04:14:37 +0200

    Update schema.js

--- validation/schema.js
+++ validation/schema.js
@@ -4,6 +4,7 @@ const schema = Joi.object({
   name: Joi.string().min(3).max(40).required(),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 module.exports = schema;
