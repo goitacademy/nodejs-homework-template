4d9c7dad07502b7ed7ad53e9d4ecadda12e31edd
Mia4678 <125677316+Mia4678@users.noreply.github.com>
2023-12-28 16:11:21 +0100

    Revert "update"

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,8 +3,6 @@ const express = require('express');
 const ctrl = require('../../controllers/auth');
 const validateBody = require('../../middlewares/validateBody');
 const  authenticate = require('../../middlewares/authenticate');
-const upload = require('../../middlewares/upload');
-
 
 const { schemas } = require('../../models/user');
 
@@ -17,8 +15,6 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
-
-router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 
 module.exports = router;
