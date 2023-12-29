31c66489db823da288040b5ea14cb8ec1d31ca2f
Nikolaihait4 <jet1nikolai@gmail.com>
2023-12-29 01:07:28 +0100

    first try

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authentificate = require("./authentificate");
+const upload = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   authentificate,
+  upload,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validateBody, authentificate } = require("../../middlewares");
+const { validateBody, authentificate, upload } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -19,5 +19,12 @@ router.post("/login", validateBody(schemas.userRegisterSchema), ctrl.login);
 router.get("/current", authentificate, ctrl.getCurrent);
 
 router.post("/logout", authentificate, ctrl.logout);
+
+router.patch(
+  "/users/avatars",
+  authentificate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
