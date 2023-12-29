24095a4a6563678a40b48c07a8c71f9fe86b3035
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-12-29 09:48:47 +0200

    fixed the error on 404 with an invalid id

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -4,7 +4,7 @@ const { HttpError } = require("../helpers");
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} is not valid id`));
+    next(HttpError(404, `${id} is not valid id`));
   }
   next();
 };
