b109762fa7e4c5b3af391e2cbb9c832ac7b9ab37
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-12-29 15:26:38 +0100

    add hw 02

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,65 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
 
-const removeContact = async (contactId) => {}
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const writeContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+
+  const result = contacts.find(({ id }) => id === contactId);
+
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+
+  const index = contacts.findIndex(({ id }) => id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await writeContacts(contacts);
+
+  return result;
+};
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = { id: crypto.randomUUID(), ...body };
+
+  contacts.push(newContact);
+  await writeContacts(contacts);
+
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+
+  const index = contacts.findIndex(({ id }) => id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { contactId, ...body };
+  writeContacts(contacts);
+
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +67,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
