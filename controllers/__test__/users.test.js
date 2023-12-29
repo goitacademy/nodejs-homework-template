0fa7368faa34548663ef2934a04c6f16048a30e1
Matviienko Ruslana <brauniesrus@gmail.com>
2023-12-29 09:45:23 +0200

    test2

--- controllers/__test__/users.test.js
+++ controllers/__test__/users.test.js
@@ -8,19 +8,29 @@ describe("test POST /users/login", () => {
     afterEach(() => console.log("AFTER EACH"));
     afterAll(() => console.log("AFTER ALL"));
 
-    it('should return user object and jwt', async() => {
+    it('should return user object and jwt', async () => {
         const testData = {
-          email: "avatarka@test.ua",
-          password: "pass_1234",
+            email: "avatarka@test.ua",
+            password: "pass_1234",
         };
         const result = await request(app).post("/api/users/login").send(testData);
 
+        // expect(result.statusCode).toBe(200);
+        // expect(result.body).toEqual(
+        //   expect.objectContaining({
+        //     token: expect.any(String),
+        //     user: expect.any(Object),
+        //   })
+        //);
+
         expect(result.statusCode).toBe(200);
         expect(result.body).toEqual(
-          expect.objectContaining({
-            token: expect.any(String),
-            user: expect.any(Object),
-          })
+            expect.objectContaining({
+                ResponseBody: expect.objectContaining({
+                    token: expect.any(String),
+                    user: expect.any(Object),
+                })
+            })
         );
     })
 })
