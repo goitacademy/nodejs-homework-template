27c95adc0c4566dff7a5d0f108b5361f5c32ac60
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-12-30 16:18:48 +0200

    almost complete contactsApi

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,58 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path');
+const crypto = require('crypto');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8')
+    const array = JSON.parse(data);
+    return array;
+  } catch (err) {
+    console.error(err.message)
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await listContacts();
+    const contact = data.find(contact => contact.id === contactId);
+    return contact
+  } catch (err) {
+    console.error(err.message)
+  }
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await listContacts();
+    const indexOfContact = data.findIndex(contact => contact.id === contactId);
+    if (indexOfContact === -1) {
+      return false;
+    };
+    const newArray = data.slice(0, indexOfContact).concat(data.slice(indexOfContact + 1));
+    await fs.writeFile(contactsPath, JSON.stringify(newArray, null, 2));
+    return true;
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
+const addContact = async (body) => {
+  try {
+    const data = await listContacts();
+    const contact = {
+      id: crypto.randomUUID().toString(),
+    ...body,
+    };
+    const newArray = data.concat(contact)
+    await fs.writeFile(contactsPath, JSON.stringify(newArray, null, 2));
+    return contact;
+  } catch (err) {
+    console.error(err.message)
+  }
+}
 
 const updateContact = async (contactId, body) => {}
 
