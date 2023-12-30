e1c3b0e419cad95370b7b333585ffad88c47fca4
Sergey Lutsenko <lytsenko.89@gmail.com>
2023-12-30 17:12:57 +0200

    fix_validatition

--- middlewares/validatitionMiddleware.js
+++ middlewares/validatitionMiddleware.js
@@ -1,9 +1,10 @@
+const HttpError = require("../helpers/HttpError.js");
+
 const validate = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
     if (error) {
-      error.status = 400;
-      throw error;
+      throw HttpError(404, error.message);
     }
 
     next();
