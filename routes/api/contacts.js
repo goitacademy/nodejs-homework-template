94224a569ac55ba87f1869464802b8e556106413
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-12-30 16:19:44 +0200

    almost complete routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,79 @@ const express = require('express')
 
 const router = express.Router()
 
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  // updateContact,
+} = require('../../models/contacts.js');
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const data = await listContacts();
+  res.json({
+    status: 'success',
+    code: 200,
+    message: 'contacts listed',
+    data: data
+  })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const id = req.params.contactId;
+  const data = await getContactById(id);
+  if (data === undefined) {
+    res.json({
+      status: 'error',
+      code: 404,
+      message: "Not found",
+    })
+  } else {
+    res.json({
+      status: 'success',
+      code: 200,
+      message: 'contact found',
+      data: data
+    })
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { name, email, phone } = req.body;
+  if (name === '' || email === '' || phone === '') {
+    res.json({
+      status: 'error',
+      code: 400,
+      message: "missing required fields"
+    })
+  } else {
+    const data = await addContact(req.body);
+      res.json({
+        status: 'success',
+        code: 201,
+        message: 'contact created',
+        data: data
+      })
+  };
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const isDeleted = await removeContact(id);
+  if (isDeleted === false) {
+    res.json({
+      status: 'error',
+      code: 404,
+      message: "Not found",
+    })
+  } else {
+    res.json({
+      status: 'success',
+      code: 200,
+      message: "contact deleted",
+    })
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
