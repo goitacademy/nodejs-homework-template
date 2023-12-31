d6c5a2685ad964843a76ee91c29161d67fc9922d
Protsak Petro <49090552+ProtsakPetro@users.noreply.github.com>
2023-12-31 16:20:35 +0100

    done

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(
-    console.log("Database connection successful"),
+  .then(() => {
+    console.log("Database connection successful");
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
-    })
-  )
+    });
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
