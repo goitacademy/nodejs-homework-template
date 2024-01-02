16d08e2d0be70e1b979175cbcc60adaf18008934
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2024-01-02 14:19:11 +0200

    init

---
+++ models/User.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
