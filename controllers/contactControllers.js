2e97a21eefa441d4461cb732075e817bf3452283
Black Chaos <black.chaos.no1@gmail.com>
2024-01-04 12:32:20 +0200

    fix: fix response add contact

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,8 +1,8 @@
-const {catchAsync, HttpError} = require('../helpers');
-const {contactService} = require('../service')
+const { catchAsync, HttpError } = require('../helpers');
+const { contactService } = require('../service');
 
 exports.getContacts = catchAsync(async (req, res) => {
-    const {contacts, total} = await contactService.listContacts(req.query, req.user);
+    const { contacts, total } = await contactService.listContacts(req.query, req.user);
     res.json({
         contacts,
         total,
@@ -10,12 +10,14 @@ exports.getContacts = catchAsync(async (req, res) => {
 });
 
 exports.addContacts = catchAsync(async (req, res) => {
-    res.status(201).json(await contactService.addContact(req.body, req.user));
+    const contact = await contactService.addContact(req.body, req.user);
+    contact.owner = contact.owner._id;
+    res.status(201).json(contact);
 });
 
 exports.getContactById = catchAsync(async (req, res) => {
     const contact = await contactService.getContactById(req.params.contactId);
-    if(!contact) throw new HttpError(404)
+    if (!contact) throw new HttpError(404);
     res.json(contact);
 });
 
@@ -35,4 +37,4 @@ exports.updateStatus = catchAsync(async (req, res) => {
     const contact = await contactService.updateStatusContact(req.params.contactId, req.body);
     if (!contact) throw new HttpError(404);
     res.json(contact);
-})
+});
