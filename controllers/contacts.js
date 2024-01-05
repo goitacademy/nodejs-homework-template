655845808ee7bcb950294525c9e24fbcd30b29c5
Kreal11 <kirillshyrokov2@gmail.com>
2024-01-06 00:41:07 +0200

    added Contact.find for all Contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,52 +1,53 @@
-const contactsFunctions = require("../models/contacts");
+// const contactsFunctions = require("../models/contacts");
 
-const { httpError } = require("../helpers");
+// const { httpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators");
+const Contact = require("../models/contact");
 
 const listContacts = async (req, res, next) => {
-  const data = await contactsFunctions.listContacts();
+  const data = await Contact.find();
   res.json(data);
 };
 
-const getContactById = async (req, res, next) => {
-  const { id } = req.params;
-  const contact = await contactsFunctions.getContactById(id);
-  if (!contact) {
-    throw httpError(404, `Contact with ID ${id} not found`);
-  }
-  res.json(contact);
-};
+// const getContactById = async (req, res, next) => {
+//   const { id } = req.params;
+//   const contact = await contactsFunctions.getContactById(id);
+//   if (!contact) {
+//     throw httpError(404, `Contact with ID ${id} not found`);
+//   }
+//   res.json(contact);
+// };
 
-const addContact = async (req, res, next) => {
-  const body = req.body;
-  const createdContact = await contactsFunctions.addContact(body);
-  res.status(201).json(createdContact);
-};
+// const addContact = async (req, res, next) => {
+//   const body = req.body;
+//   const createdContact = await contactsFunctions.addContact(body);
+//   res.status(201).json(createdContact);
+// };
 
-const removeContact = async (req, res, next) => {
-  const { id } = req.params;
-  const deletedContact = await contactsFunctions.removeContact(id);
-  if (!deletedContact) {
-    throw httpError(404, `Book with ID ${id} not found`);
-  }
-  res.json(deletedContact);
-  res.status(204);
-};
+// const removeContact = async (req, res, next) => {
+//   const { id } = req.params;
+//   const deletedContact = await contactsFunctions.removeContact(id);
+//   if (!deletedContact) {
+//     throw httpError(404, `Book with ID ${id} not found`);
+//   }
+//   res.json(deletedContact);
+//   res.status(204);
+// };
 
-const updateContact = async (req, res, next) => {
-  const { id } = req.params;
-  const body = req.body;
-  const updatedContact = await contactsFunctions.updateContact(id, body);
-  if (!updatedContact) {
-    throw httpError(404, `Book with ID ${id} not found`);
-  }
-  res.json(updatedContact);
-};
+// const updateContact = async (req, res, next) => {
+//   const { id } = req.params;
+//   const body = req.body;
+//   const updatedContact = await contactsFunctions.updateContact(id, body);
+//   if (!updatedContact) {
+//     throw httpError(404, `Book with ID ${id} not found`);
+//   }
+//   res.json(updatedContact);
+// };
 
 module.exports = {
   listContacts: ctrlWrapper(listContacts),
-  getContactById: ctrlWrapper(getContactById),
-  addContact: ctrlWrapper(addContact),
-  removeContact: ctrlWrapper(removeContact),
-  updateContact: ctrlWrapper(updateContact),
+  // getContactById: ctrlWrapper(getContactById),
+  // addContact: ctrlWrapper(addContact),
+  // removeContact: ctrlWrapper(removeContact),
+  // updateContact: ctrlWrapper(updateContact),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,24 @@
 const express = require("express");
 
 const contactsControllers = require("../../controllers/contacts");
-const { validateBody } = require("../../decorators");
-const { contactsSchema } = require("../../validators");
+// const { validateBody } = require("../../decorators");
+// const { contactsSchema } = require("../../validators");
 
 const router = express.Router();
 
 router.get("/", contactsControllers.listContacts);
-router.post(
-  "/",
-  validateBody(contactsSchema.createContactsSchema),
-  contactsControllers.addContact
-);
+// router.post(
+//   "/",
+//   validateBody(contactsSchema.createContactsSchema),
+//   contactsControllers.addContact
+// );
 
-router.get("/:id", contactsControllers.getContactById);
-router.delete("/:id", contactsControllers.removeContact);
-router.put(
-  "/:id",
-  validateBody(contactsSchema.createContactsSchema),
-  contactsControllers.updateContact
-);
+// router.get("/:id", contactsControllers.getContactById);
+// router.delete("/:id", contactsControllers.removeContact);
+// router.put(
+//   "/:id",
+//   validateBody(contactsSchema.createContactsSchema),
+//   contactsControllers.updateContact
+// );
 
 module.exports = router;
