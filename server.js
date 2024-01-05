7e01dd9fda730efcfb7949de6bb13e8e76841e87
Kreal11 <kirillshyrokov2@gmail.com>
2024-01-05 22:37:17 +0200

    created database, connected it to server.js

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const { envsConfig } = require("./configs");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(envsConfig.dbHost)
+  .then(() => {
+    app.listen(envsConfig.port, () => {
+      console.log(`Server running. Use our API on port: ${envsConfig.port}`);
+    });
+  })
+  .catch((e) => {
+    console.log(console.log(e));
+    process.exit(1);
+  });
