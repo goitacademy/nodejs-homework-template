7ed8c9a5ffcb7b968deea46ed974186db9b9c768
Olena <helensmart2323@gmail.com>
2024-01-05 22:47:40 +0100

    server

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const { envsConfig } = require("./configs");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// mongodb + srv://Olena:3144223@cluster0.ic2uyyp.mongodb.net/?retryWrites=true&w=majority
+
+mongoose
+  .connect(envsConfig.dbHost)
+  .then(() => {
+    app.listen(envsConfig.port, () => {
+      console.log(`Server running. Use our API on port: ${envsConfig.port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection error");
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
