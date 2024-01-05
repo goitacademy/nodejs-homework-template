6c3c1aa4116f7c359de3377b5f3b0dd5cdfc8c77
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-01-05 03:50:08 +0200

    fix validateContact middlewares

--- middlewares/validateContact.js
+++ middlewares/validateContact.js
@@ -1,12 +1,13 @@
 const { HttpError } = require("../helpers");
 
-const validateBody = (schema) => {
-  const func = (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      next(HttpError(400, error.message));
-    } else {
+const validateBody = (Contact) => {
+  const func = async (req, res, next) => {
+    try {
+      const contact = new Contact(req.body);
+      await contact.validate();
       next();
+    } catch (error) {
+      next(HttpError(400, error.message));
     }
   };
 
