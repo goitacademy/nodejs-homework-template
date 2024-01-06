c5de0cc4676fa07e649c3613bf384e189d28cec2
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2024-01-06 01:24:23 +0100

    .

--- models/index.js
+++ models/index.js
@@ -1,23 +1,23 @@
-const { model, Schema } = require("mongoose");
+// const { model, Schema } = require("mongoose");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: [true, "Duplicated"],
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  favorite: Boolean,
-  role: {
-    type: String,
-    enum: ["admin", "moderator", "user"],
-    default: "user",
-  },
-});
+// const userSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: [true, "Duplicated"],
+//   },
+//   phone: {
+//     type: String,
+//     required: true,
+//   },
+//   favorite: Boolean,
+//   role: {
+//     type: String,
+//     enum: ["admin", "moderator", "user"],
+//     default: "user",
+//   },
+// });
--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
 
-// dotenv.config({
-//   path: process.env.NODE_ENV === "production" ? "./envs/p" : "./envs/dev.env",
-// });
-
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((err) => {
     console.log(err);
