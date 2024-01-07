be9fd0c7410d1c0f782d3f600d300581ea1355a7
a7ndriyyy <andrii.nastych@gmail.com>
2024-01-07 18:29:05 +0100

    Merge branch 'master' of https://github.com/a7ndriyyy/nodejs-homework-rest-api into 03-mongodb

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require("./app");
 
-
 const mongoose = require("mongoose");
 
 const { DB_HOST } = process.env;
@@ -10,14 +9,12 @@ mongoose
   .then(() => {
     console.log(DB_HOST);
     console.log("Database connection successful");
-    app.listen(3000);
+    app.listen(3000, () => {
+      console.log("Server Running. Use our API on port: 3000");
+    });
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-app.listen(3000, () => {
-  console.log("Server Running. Use our API on port: 3000");
-});
-
+mongoose.set("strictQuery", true);
