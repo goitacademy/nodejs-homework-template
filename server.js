567b7276d0da59438f20edef43be035c05343f96
Pavlo2005 <savyukpavlo8@gmail.com>
2024-01-07 15:16:55 +0100

    commit

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-import app from "./app.js";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+import app from "./app.js";
+// aXj8aRiLhNDYQFUU
+const DB_HOST = "mongodb+srv://Pavlo00600:aXj8aRiLhNDYQFUU@db-contacts.gs8hryf.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
