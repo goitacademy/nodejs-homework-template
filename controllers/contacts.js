64627ad3f4bf5c8e4b44630e40aef24004525ada
Mykola1612 <fermer2007pavlovich@gmail.com>
2024-01-09 00:14:59 +0100

    Summary

---
+++ controllers/contacts.js
@@ -0,0 +1,45 @@
+const contacts = require("..//models/contacts.js");
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const listContacts = async (req, res, next) => {
+  const result = await contacts.listContacts();
+  res.status(200).json(result);
+};
+
+const getContactById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res, next) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const removeContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  return res.status(200).json({ message: "contact deleted" });
+};
+
+const updateContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  res.json(result);
+};
+
+module.exports = {
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,81 +1,29 @@
 const express = require("express");
-const Joi = require("joi");
+
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody } = require("../../middlewares");
+
+const schema = require("../../schemas/contacts");
 
 const router = express.Router();
 
-const contacts = require("../../models/contacts.js");
+router.get("/", ctrl.listContacts);
 
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+router.get("/:contactId", ctrl.getContactById);
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  "/",
+  validateBody(schema.addSchema, "missing required name field"),
+  ctrl.addContact
+);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      return res.status(404).json({
-        message: "Not found",
-      });
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrl.removeContact);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: "missing required name field" });
-    }
-
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    console.log("result: ", result);
-
-    if (!result) {
-      return res.status(404).json({ message: "Not found" });
-    }
-    return res.status(200).json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: "missing fields" });
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put(
+  "/:contactId",
+  validateBody(schema.addSchema, "missing fields"),
+  ctrl.updateContact
+);
 
 module.exports = router;
