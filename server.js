c14d8f6a85111e735d0d37c943d773004df5c924
Viktoriya-ri <viktoriya23071@gmail.com>
2024-01-08 11:26:59 +0200

    s

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
-
 const app = require('./app');
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
+
 mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Database connection successful");
+    app.listen(5000, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
+    console.log("Database connect success");
   })
-  .catch(error => {
-    console.log(error.message);
+  .catch((error) => {
+    console.log(`Server not running. Error message: ${error.message}`);
     process.exit(1);
-  })
- 
+  });
