9a32d535e16d07664e627bd2cef1fd3f31e0f4ab
a7ndriyyy <andrii.nastych@gmail.com>
2024-01-08 18:33:30 +0100

    commit

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const { DB_HOST } = process.env;
 
+console.log(DB_HOST);
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log(DB_HOST);
-    console.log("Database connection successful");
+    console.log("Database connections successful");
     app.listen(3000, () => {
       console.log("Server Running. Use our API on port: 3000");
     });
