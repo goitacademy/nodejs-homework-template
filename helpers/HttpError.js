2b6ecbb48b87571eeb14196a939ee66d3f91ca3c
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-09 11:27:35 +0100

    update
    all works

---
+++ helpers/HttpError.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+const HttpError = (status, message) => {
+    const error = new Error(message);
+    error.status = status;
+    return error;
+}
+
+module.exports = HttpError;
---
+++ helpers/index.js
@@ -0,0 +1,6 @@
+// @ts-nocheck
+const HttpError = require('./HttpError');
+
+module.exports = {
+    HttpError,
+}
