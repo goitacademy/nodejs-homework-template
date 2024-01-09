68552124d030dc5ad0e725ab2e77177614aa29d3
Matviienko Ruslana <brauniesrus@gmail.com>
2024-01-09 12:48:18 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,9 @@ const { SECRET_KEY } = process.env;
  * @призначення для регістриції користувача
  */
 const registerUser = async (req, res, next) => {
-  const { email, password, subscription } = await contactServices.signup(req.body);
+  const { email, password, subscription } = await contactServices.signup(
+    req.body
+  );
 
   res.status(201).json({
     user: {
@@ -21,10 +23,14 @@ const registerUser = async (req, res, next) => {
 
 /**
  * @призначення для авторизації користувача
+ * ///
  */
 
 const loginUser = async (req, res, next) => {
-  const { user, token } = await contactServices.login(req.body.email, req.body.password);
+  const { user, token } = await contactServices.login(
+    req.body.email,
+    req.body.password
+  );
 
   res.status(200).json({
     ResponseBody: {
@@ -35,12 +41,11 @@ const loginUser = async (req, res, next) => {
       },
     },
   });
-
-}
+};
 
 const logoutUser = async (req, res, next) => {
   const { msg } = await contactServices.logOut(req.user);
-  
+
   res.json({
     message: msg,
   });
@@ -49,32 +54,33 @@ const logoutUser = async (req, res, next) => {
 const getCurrentUser = async (req, res, next) => {
   const { email, subscription } = await contactServices.getCurrent(req.user);
 
-    res.json({
-      email: email,
-      subscription: subscription,
-    });
+  res.json({
+    email: email,
+    subscription: subscription,
+  });
 };
 
 const updateUserSubscription = async (req, res, next) => {
-  const { email, subscription } = await contactServices.updateSubscription(req.user, req.body);
- 
+  const { email, subscription } = await contactServices.updateSubscription(
+    req.user,
+    req.body
+  );
+
   res.status(200).json({
     email: email,
     subscription: subscription,
   });
 };
 
-
 const updateUserAvatar = async (req, res, next) => {
   if (!req.file) throw HttpError(400);
 
-    const { avatarURL } = await contactServices.updateAvatar(req.user, req.file);
+  const { avatarURL } = await contactServices.updateAvatar(req.user, req.file);
 
   res.status(200).json({
     avatarURL: avatarURL,
   });
 };
-
 
 module.exports = {
   registerUser: ctrlWrapper(registerUser),
