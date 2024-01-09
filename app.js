5ade94dedd073d4a273361cc886bb2309a1c662c
iVladyuser <korol.v09@gmail.com>
2024-01-09 17:27:03 +0100

    added mongoDB with mongoose

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+require("dotenv").config();
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
