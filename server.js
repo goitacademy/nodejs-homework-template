bd3a43dd9dbc1bebf2154590490751dfe6927dd2
a7ndriyyy <andrii.nastych@gmail.com>
2024-01-09 10:25:03 +0100

    correct

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config();
+require("dotenv").config();
 
 const { DB_HOST } = process.env;
 
