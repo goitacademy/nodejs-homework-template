a92ee6f7ea2d5ed71f0dae0671c9b99780b99f28
Viktoriya-ri <viktoriya23071@gmail.com>
2024-01-10 18:34:15 +0200

    виправлення помилок

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use("/api/users", authRouter);
+app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,16 +8,16 @@ const { schemas } = require('../../models/user.js');
 
 const router = Router();
 
-router.post("/users/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
-router.post("/users/login", validateBody(schemas.loginSchema), ctrl.login);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
-router.get("/users/current", authenticate, ctrl.getCurrent);
+router.get("/current", authenticate, ctrl.getCurrent);
 
-router.post('/users/logout', authenticate, ctrl.logout);
+router.post('/logout', authenticate, ctrl.logout);
 
-router.patch('/users/:id', authenticate, ctrl.updateSubscription);
+router.patch('/', authenticate, ctrl.updateSubscription);
 
-router.patch('/users/avatars', authenticate, upload.single('avatar'), ctrl.updateAvatar);
+router.patch('/avatars', authenticate, upload.single('avatar'), ctrl.updateAvatar);
 
 module.exports = router;
