f3a56fc7f0367a5803dc82e81ffd047e3a136e6b
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2024-01-10 15:38:18 +0200

    initial

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const contactsRouter = require('./routes/api/contacts');
+const usersRouter = require('./routes/api/users');
+const path = require('path');
 
-const contactsRouter = require('./routes/api/contacts')
 
-const app = express()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use('/api/contacts', contactsRouter)
+// MongoDB connection string
+const uri = "mongodb+srv://vspasedust:eaiyDSBkLIZ623xy@cluster0.kz32p8l.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1);
+  }
+}
+
+// Connect to MongoDB when the application starts
+connectToDatabase();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/contacts', contactsRouter);
+app.use('/api/users', usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: 'Not found' });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+
+module.exports = app;
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/users');
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Not authorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (!user || token !== user.token) {
+      return res.status(401).json({ message: 'Not authorized' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Not authorized' });
+  }
+};
+
+module.exports = authMiddleware;
