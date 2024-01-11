d783097060f143d88e7a481fb776b2464354a3a4
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-11 11:21:02 +0100

    readme exchange

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('./app')
+const { listContacts, getContactById } = require('./models/contacts')
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
