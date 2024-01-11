430d6ffcb23fb1c146e072dd32b17c6979325ead
Roman <jrj80mail@gmail.com>
2024-01-11 18:46:52 +0200

    Pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,7 +6,13 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 const listContacts = async (req, res) => {
   // const result = await Contact.find();
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
 
   res.json(result);
 };
