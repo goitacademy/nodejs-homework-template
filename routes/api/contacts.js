1ecbcbc7c5d3eb6536419ade2a7cf8b2f19a3519
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-11 10:35:11 +0100

    rotes get/get:contactID

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,41 @@
 const express = require('express')
-
 const router = express.Router()
+// const { schemaAdd, schemaUpdate } = require("../../validator/validator");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts');
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json({ message: contacts });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error(error);
+  }
+});
+
+
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const response = await getContactById(contactId);
+
+    if (response !== null) {
+      res.status(200).json({ message: response });
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error(error);
+  }
+});
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
