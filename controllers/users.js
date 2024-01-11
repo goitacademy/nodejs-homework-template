52ad501f7304a55fddfebbbccd54651d620d1b58
Roman <jrj80mail@gmail.com>
2024-01-11 15:28:17 +0200

    MongooseError Up

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,22 @@
 const { User } = require("../models/user");
 
-const { ctrlWrapper } = require("../helpers");
+const { httpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw httpError(409, "Email in use");
+  }
+
   const newUser = await User.create(req.body);
 
   res.status(201).json({
-    user: { email: newUser.email, subscription: newUser.subscription },
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
   });
 };
 
