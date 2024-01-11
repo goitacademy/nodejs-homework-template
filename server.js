8e22f7e151a62fff0c94ab8a9d89c0292365548e
Tamias <n.tamia5@icloud.com>
2024-01-11 19:41:48 +0200

    03-mongodb

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_Host =
+  "mongodb+srv://werb:9JFYNJiOUnDGw3mf@cluster0.ouq18we.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_Host)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
