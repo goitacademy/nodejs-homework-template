41892645f50a1db6fa893d6e7dbc03591e7ece88
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-11 12:50:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('./app')
-const { listContacts, getContactById } = require('./models/contacts')
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
