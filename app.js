a30eeb4d3c61c3927151c0ba23ae0cb0df4e8c5d
a7ndriyyy <andrii.nastych@gmail.com>
2024-01-12 17:16:24 +0100

    commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,8 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
-
+const authenficate = require("./authenficate");
 module.exports = {
   validateBody,
   isValidId,
+  authenficate,
 };
