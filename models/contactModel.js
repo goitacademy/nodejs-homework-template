9243be24cc0db30a1e75a1802611efbd58bc3b24
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-01-12 14:35:58 +0400

    Your commit message here

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,24 +1,23 @@
 const mongoose = require("mongoose");
 
-const { Schema } = mongoose;
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
-  },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const Contact = mongoose.model("Contact", contactSchema);
-
-module.exports = Contact;
+module.exports = mongoose.model("Contact", contactSchema);
