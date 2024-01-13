68d97c5a3838df2cc8fe865fe2ba9b2175108ef2
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-13 16:23:30 +0100

    Creating seperate js file for validation.

---
+++ js/validation.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+
+export const schemaReq = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net", "pl"] },
+  }),
+  phone: Joi.string().pattern(
+    new RegExp("/^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$/")
+  ),
+}).and("name", "email", "phone");
+
+export const schema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net", "pl"] },
+  }),
+  phone: Joi.string().pattern(
+    new RegExp("/^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$/")
+  ),
+});
