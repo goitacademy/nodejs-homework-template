ffc4062437b850b432b1827b49ed18896ca148bc
Roman <jrj80mail@gmail.com>
2024-01-14 19:50:17 +0200

    Step3_gravatar

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
 
 const { User } = require("../models/user");
 
@@ -16,8 +17,13 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
