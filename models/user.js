6ebba2a5eb388d031176a8b04d9fc40413f18d66
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-01-14 15:44:23 +0200

    create user schema

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Set password for user'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: String
+}
+);
+
+const User = model("User", userSchema);
+
+
+module.exports = {
+  User,
+  userSchema,
+};
