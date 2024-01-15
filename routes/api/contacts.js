b86597182493b8c0349e6aac8c8f643961c4775a
krystianklimczak <krystianklimczak1996r.com>
2024-01-15 19:37:32 +0100

    import controllers and use instead of raw async functions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,86 +1,16 @@
-const express = require('express');
+import express from 'express';
+import { indexContacts } from '../../controllers/contacts/indexContacts.js';
+import { showContacts } from '../../controllers/contacts/showContacts.js';
+import { createContacts } from '../../controllers/contacts/createContact.js';
+import { updateContacts } from '../../controllers/contacts/updateContacts.js';
+import { deleteContacts } from '../../controllers/contacts/deleteContacts.js';
+
 const router = express.Router();
-const Joi = require('@hapi/joi');
-const tasks = require('../../models/contacts');
 
-const schema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    .required(),
-  phone: Joi.string().required(),
-});
+router.get('/', indexContacts);
+router.get('/:contactId', showContacts);
+router.post('/', createContacts);
+router.put('/:contactId', updateContacts);
+router.delete('/:contactId', deleteContacts);
 
-const schemaSecond = Joi.object({
-  name: Joi.string(),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-  phone: Joi.string(),
-});
-
-router.get('/', async (req, res, next) => {
-  const contacts = await tasks.listContacts();
-
-  res.status(200).json({ contacts, itemCount: contacts.length });
-});
-
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await tasks.getContactById(contactId);
-
-  if (!contact) {
-    res.status(404).json({ message: 'Not found' });
-  } else {
-    res.status(200).json(contact);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  const result = schema.validate(req.body);
-  if (result.error) {
-    return res.status(400).json({ message: result.error.message });
-  }
-
-  const { name, email, phone } = req.body;
-  const newContact = await tasks.addContact({ name, email, phone });
-
-  if (!newContact) {
-    res.status(400).json({ message: 'Contact already exist' });
-  } else {
-    res.status(201).json(newContact);
-  }
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const isDeleted = await tasks.removeContact(contactId);
-
-  if (!isDeleted) {
-    res.status(404).json({ message: 'Not found' });
-  } else {
-    res.status(200).json({ message: 'Contact deleted' });
-  }
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  const result = schemaSecond.validate(req.body);
-  if (result.error) {
-    return res.status(400).json({ message: result.error.message });
-  }
-
-  const { contactId } = req.params;
-  const { name, email, phone } = req.body;
-
-  if (!name && !email && !phone) {
-    return res.status(400).json({ message: 'Missing fields' });
-  }
-
-  const contactToUpdate = await tasks.updateContact(contactId, { name, email, phone });
-
-  if (!contactToUpdate) {
-    res.status(404).json({ message: 'Not found' });
-  } else {
-    res.status(200).json(contactToUpdate);
-  }
-});
-
-module.exports = router;
+export { router };
