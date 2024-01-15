07001a1deedd3b2d13f59cf8e955f4efea68864a
Dmitry Sukhorukov <ciplator@gmail.com>
2024-01-15 09:56:04 +0200

    fix

--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,5 @@
-const fs = require("fs/promises");
-const path = require("path");
+const fs = require ("fs/promises");
+const path = require ("path");
 const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "./contacts.json");
 
