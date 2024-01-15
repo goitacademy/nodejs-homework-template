e60064c955b9af0ea62b9a6075c6be609df4cef4
sidlicki <130383232+sidlicki@users.noreply.github.com>
2024-01-15 20:14:19 +0100

    homework 3 try 2

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,21 +2,24 @@ const express = require("express");
 
 const ContactController = require("../controllers/contact");
 
+const isValidId = require("../schemas/isValidId");
+
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get("/", ContactController.getContacts);
 
-router.get("/:id", ContactController.getContact);
+router.get("/:id", isValidId, ContactController.getContact);
 
 router.post("/", jsonParser, ContactController.createContact);
 
-router.put("/:id", jsonParser, ContactController.updateContact);
+router.put("/:id", isValidId, jsonParser, ContactController.updateContact);
 
-router.delete("/:id", ContactController.deleteContact);
+router.delete("/:id", isValidId, ContactController.deleteContact);
 
 router.patch(
   "/:id/favorite",
+  isValidId,
   jsonParser,
   ContactController.changeContactFavorite
 );
