17712f0d2ce50aabacafe07502cfba315998e6bf
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-01-15 04:09:45 +0200

    fixed app bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const {contactsRouter, authRouter} = require('./routes/api')
 
 const app = express()
 
@@ -13,13 +13,17 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/auth', authRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  if (err.code === 11000) {
+    res.status(409).json({ message: 'Email in use' })
+  }
+  res.status(err.statusCode).json({ message: err.message })
 })
 
 module.exports = app
