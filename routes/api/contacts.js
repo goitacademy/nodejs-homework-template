c3ccac600d6a06ed3c7aebbef6573005946f0bd0
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-15 16:25:10 +0100

    Refactoring code to use controllers.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,82 +1,20 @@
 import { Router } from "express";
+import { createContact } from "../../controllers/contacts/createContacts";
+import { deleteContact } from "../../controllers/contacts/deleteContacts";
+import { indexContacts } from "../../controllers/contacts/indexContacts";
+import { showContact } from "../../controllers/contacts/showContacts";
+import { updateContact } from "../../controllers/contacts/updateContacts";
 
 const router = Router();
 
-router.get("/", async (req, res, next) => {
-  const contactsList = await contacts.listContacts();
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      contactsList,
-    },
-  });
-});
+router.get("/", indexContacts);
 
-router.get("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const contact = await contacts.getContactById(id);
-  contact.length !== 0 &&
-    res.json({
-      stats: "success",
-      code: 200,
-      data: {
-        ...contact,
-      },
-    });
-  contact.length === 0 &&
-    res.json({
-      status: "error",
-      code: 400,
-      message: "Not found",
-    });
-});
+router.get("/:contactId", showContact);
 
-router.post("/", async (req, res, next) => {
-  const body = req.body;
-  const val = schemaReq.validate(body);
-  if (val.error) {
-    res.json({
-      status: "error",
-      code: 400,
-      message: val.error.message,
-    });
-    return;
-  }
-  const data = await contacts.addContact(body);
-  res.json({
-    status: "sucess",
-    code: 201,
-    data: data,
-  });
-});
+router.post("/", createContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const deleted = await contacts.removeContact(id);
-  deleted
-    ? res.json({ status: "success", code: 200, message: "Contact deleted" })
-    : res.json({ status: "error", code: "404", message: "Not found" });
-});
+router.delete("/:contactId", deleteContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const body = req.body;
-  const val = schema.validate(body);
-  if (val.error) {
-    res.json({
-      status: "error",
-      code: 400,
-      message: val.error.message,
-    });
-    return;
-  }
-  const data = await contacts.updateContact(id, body);
-  res.json({
-    status: "sucess",
-    code: 201,
-    data: data,
-  });
-});
+router.put("/:contactId", updateContact);
 
 export default router;
