caa807483b128ad573f36d305c5e130189bed511
Viktoriya-ri <viktoriya23071@gmail.com>
2024-01-16 11:20:52 +0200

    !

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -7,6 +7,7 @@ const logout = require("./logout");
 const updateSubscription = require("./updateSubscription");
 const updateAvatar = require('./updateAvatar');
 const verifyEmail = require('./verifyEmail');
+const resendVerifyEmail = require('./resendVerifyEmail');
 
 module.exports = {
   register: ctrlWrapper(register),
@@ -16,4 +17,5 @@ module.exports = {
   updateSubscription: ctrlWrapper(updateSubscription),
   updateAvatar: ctrlWrapper(updateAvatar),
   verifyEmail:ctrlWrapper(verifyEmail),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 };
--- controllers/users/resendVerifyEmail.js
+++ controllers/users/resendVerifyEmail.js
@@ -11,5 +11,18 @@ const resendVerifyEmail = async(req, res) => {
 
     if(!user) throw HttpError(401, "Email not found");
 
-    if(user.verify) throw HttpError(400, "Verification has already been")
-}
+    if(user.verify) throw HttpError(400, "Verification has already been passed");
+
+    const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Confirm email</a>`,
+    };
+    await sendEmail(verifyEmail);
+    
+    res.status(200).json({
+        message: "Verification email sent",
+    })
+};
+
+module.exports = resendVerifyEmail;
