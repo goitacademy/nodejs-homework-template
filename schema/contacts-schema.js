49c2a5abb1f4ed60695577f77c1e888c03ce405f
Vitalii Kyi <kyi.profy@gmail.com>
2024-01-16 12:41:17 +0200

    fixed - added the favorite property to the joi schema

--- schema/contacts-schema.js
+++ schema/contacts-schema.js
@@ -4,12 +4,14 @@ const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 const updateSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
+  favorite: Joi.boolean(),
 });
 
 const updateFavoriteSchema = Joi.object({
