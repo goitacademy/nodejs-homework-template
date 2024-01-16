5df76fb6c125d449009d8072200854fdd05115b2
ailsoya <akalitata53@gmail.com>
2024-01-16 19:02:07 +0200

    6

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../../models/user')
 const { RequestError } = require('../../helpers')
-const { SECRET_KEY } = process.env
+const SECRET_KEY = process.env.SECRET_KEY
 
 const login = async (req, res, next) => {
     try {
