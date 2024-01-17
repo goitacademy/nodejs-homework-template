f826f31a6c8b70f333721ae1808a23616feccd41
dn280786lea <ekaterina.lubenets@gmail.com>
2024-01-17 08:17:41 +0100

    up
    up

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,19 +9,27 @@ const addSchema = Joi.object({
   name: Joi.string().required().messages({
     "any.required": "Missing required name field",
   }),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required()
+    .messages({
+      "any.required": "Missing required email field",
+    }),
+  phone: Joi.number().required().messages({
+    "any.required": "Missing required phone field",
   }),
-  phone: Joi.number().required(),
 });
+
 const updateSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
   }),
-  phone: Joi.number().required(),
+  phone: Joi.number(),
 });
 
 router.get("/", async (req, res, next) => {
@@ -67,7 +75,7 @@ router.delete("/:contactId", async (req, res, next) => {
     const result = await contacts.removeContact(contactId);
     console.log(result);
     if (!result) {
-      throw new HttpError(404, "Not found");
+      res.status(404).json({ message: "Not found" });
     }
     res.json({
       message: "contact deleted",
@@ -79,11 +87,17 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({ message: "missing fields" });
+      return;
+    }
+
     const { error } = updateSchema.validate(req.body);
     if (error) {
       res.status(400).json({ message: error.message });
       return;
     }
+
     const { contactId } = req.params;
     const data = req.body;
     const result = await contacts.updateById(contactId, data);
@@ -91,10 +105,10 @@ router.put("/:contactId", async (req, res, next) => {
       res.status(404).json({ message: "Not found" });
       return;
     }
+
     res.json(result);
   } catch (error) {
     next(error);
   }
 });
-
 module.exports = router;
