a6fd46d1dfd34961251722a817bcc1ce14dfc8f5
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2024-01-17 20:49:01 +0100

    fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,20 +1,15 @@
 const contacts = require("../models/contacts");
 const { HttpError } = require("../utils");
-const Joi = require("joi");
+
+
 const ctrlWrapper = require("../utils/ctrl.Wrapper");
 
-const AddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
-});
-
-const getContacts = async (req, res, next) => {
+const getContacts = async (req, res) => {
   const result = await contacts.getContacts();
   res.json(result);
 };
 
-const getContactById = async (req, res, next) => {
+const getContactById = async (req, res) => {
   const { id } = req.params;
   const result = await contacts.getContactById(id);
 
@@ -24,16 +19,13 @@ const getContactById = async (req, res, next) => {
   res.json(result);
 };
 
-const addContact = async (req, res, next) => {
-  const { error } = AddSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, "Missing required name field");
-  }
+const addContact = async (req, res) => {
+
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
 
-const removeContact = async (req, res, next) => {
+const removeContact = async (req, res) => {
   const { id } = req.params;
   const result = await contacts.removeContact(id);
   if (!result) {
@@ -44,11 +36,8 @@ const removeContact = async (req, res, next) => {
   });
 };
 
-const updateById = async (req, res, next) => {
-  const { error } = AddSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, "Missing required name field");
-  }
+const updateById = async (req, res) => {
+
   const { id } = req.params;
   const result = await contacts.updateById(id, req.body);
   if (!result) {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,17 @@
 const express = require("express");
-
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
 
 router.get("/", ctrl.getContacts);
 
 router.get("/:contactId", ctrl.getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", validateBody(schemas.AddSchema), ctrl.addContact);
 
 router.delete("/:contactId", ctrl.removeContact);
 
-router.put("/:contactId", ctrl.updateById);
+router.put("/:contactId", validateBody(schemas.AddSchema), ctrl.updateById);
 
 module.exports = router;
