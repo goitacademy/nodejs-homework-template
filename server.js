8b9f1156b3edd56323af898015023e7ec4f1d652
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-18 16:16:44 +0100

    Trying swagger... failed.

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import app from "./app.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
