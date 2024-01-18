f0ad62a203c4c16341c9625e9484e29cf70fe58f
Christian <120150697+Vitammiin@users.noreply.github.com>
2024-01-18 18:49:16 -0500

    hm4n 18/01

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
-
-require("dotenv").config();
 
 const app = express();
 
