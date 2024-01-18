6098797d785dd213576fb5d809e48c0af8113552
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2024-01-18 16:04:38 +0200

    hw02

---
+++ controllers/contacts.js
@@ -0,0 +1,49 @@
+const contacts = require("../models");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const getAllContacts = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.getContactById(id);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.removeContact(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "Delete success" });
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contacts.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  updateContact: ctrlWrapper(updateContact),
+};
