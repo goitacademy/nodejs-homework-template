2b08f7666671864dccf31e127e68e5fcc2cad2c4
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-18 19:59:00 +0100

    Updating node.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import app from "./app.js";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" };
+import mongoose from "mongoose";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerDocument from "./swagger.json" assert { type: "json" };
+import "dotenv/config.js";
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST = process.env.DATABASE_URL;
+const dbConnection = mongoose.connect(DB_HOST);
+
+dbConnection
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((e) => {
+    console.log(`Error occured: ${e.message}`);
+  });
