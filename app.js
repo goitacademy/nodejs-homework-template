67fed0a7a716977314e733ca9033db9755cad81d
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-18 11:55:13 +0100

    "dotenv" config / add .env / DB_URL

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const contactRoutes = require("./routes/api/contacts.routes")
+const contactRoutes = require("./routes/api/contacts.routes");
 
 const PORT = process.env.PORT || 4000;
 
-const connection = mongoose.connect(process.env.DATABASE_URL, {
+const connection = mongoose.connect(process.env.DB_URL, {
   dbName: "db-contacts",
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", contactRoutes);
 
 connection
@@ -27,3 +30,4 @@ connection
     console.error(`Error while establishing connection: [${error}]`);
     process.exit(1);
   });
+module.exports = app;
