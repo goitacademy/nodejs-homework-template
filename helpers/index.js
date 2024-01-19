52a54c5013a8a914e63763cd87194a9f1c2f6e46
Roman <jrj80mail@gmail.com>
2024-01-19 15:12:12 +0200

    Step_2.1

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./hangleMongooseError");
+const sendEmail = require("./sendEmail");
 
-module.exports = { HttpError, ctrlWrapper, handleMongooseError };
+module.exports = { HttpError, ctrlWrapper, handleMongooseError, sendEmail };
