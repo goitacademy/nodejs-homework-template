197ea3de4ec7c4a9eee1e9bcc12320b7d11d7200
Misetr <135700846+Misetr@users.noreply.github.com>
2024-01-19 23:19:52 +0200

    mongodb

--- models/contacts.js
+++ models/contacts.js
@@ -17,3 +17,21 @@ module.exports = {
   addContact,
   updateContact,
 }
+
+const express = require('express');
+const router = express.Router();
+const Contact = require('../models/contact'); // Путь к модели Contact
+
+// Здесь будут другие маршруты (GET, POST, DELETE и т.д.)
+
+// PATCH маршрут для обновления поля favorite контакта
+router.patch('/:contactId/favorite', async (req, res, next) => {
+  // ... код, который я предоставил ранее ...
+});
+
+// Функция для обновления поля favorite контакта
+async function updateStatusContact(contactId, update) {
+  // ... реализация функции ...
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const app = require('./app')
+const mongoose = require("mongoose");
+const DB_HOST = "mongodb+srv://jackm:6UpkeCkVjpzWmBdA@cluster0.p9x2mdu.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+//6UpkeCkVjpzWmBdA
+console.log(process.env.DB_HOST);
+ mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+ 
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch(() => {
+   
+    console.log("Connection error");
+    process.exit(1);
+  });
+  
+  
+
