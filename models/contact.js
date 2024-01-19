85927898f989f21b9befb337aefed8517be2e1f3
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2024-01-19 22:02:20 +0100

    test

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,5 @@
 const fs = require("fs/promises");
-const {nanoid} = require("nanoid");
+const { nanoid } = require("nanoid");
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "contacts.json");
