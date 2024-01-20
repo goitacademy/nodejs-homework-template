5c5c6d152f29de17e80d23768563d7f56a1392f1
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-20 06:16:59 +0100

    Changing names of files and directories.

---
+++ services/js/contacts.helpers.js
@@ -0,0 +1,29 @@
+import { Contact } from "../schemas/contacts.schema.js";
+
+// Returns all contacts
+function getAll() {
+  return Contact.find({});
+}
+
+// Returns contact with specific ID
+function getOne(contactId) {
+  return Contact.findById(contactId);
+}
+
+//   Removes contact
+function remove(contactId) {
+  return Contact.findByIdAndDelete(contactId);
+}
+
+//   Adds new contact
+function create(body) {
+  const { name, phone, email, favourite } = body;
+  return Contact.create({ name, email, phone, favourite });
+}
+
+function update(id, body) {
+  const { name, phone, email, favourite } = body;
+  return Contact.findByIdAndUpdate(id, { name, email, phone, favourite });
+}
+
+export { getAll, getOne, remove, create, update };
---
+++ services/schemas/contacts.schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const contact = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Contact = mongoose.model("contact", contact);
