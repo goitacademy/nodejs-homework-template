c90f34754f5cb9d8bca9ba534e22708583a2d984
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-20 07:36:10 +0100

    Adding schema and model for user.

---
+++ services/schemas/auth.schema.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const user = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+export const User = model("user", user);
