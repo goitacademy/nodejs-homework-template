9348b5f9f54be09e6c74791eba7c30724cf19294
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-20 12:40:03 +0100

    update file , add "owner",

--- service/contacts.service.js
+++ service/contacts.service.js
@@ -1,11 +1,11 @@
 import Contact from "../models/contact.model.js";
 
-const getAll = async () => {
-  return Contact.find();
+const getAll = async (query) => {
+  return Contact.find(query);
 };
 
-const getOne = async (id) => {
-  return Contact.findById(id);
+const getOne = async (id, userId) => {
+  return Contact.findOne({ _id: id, owner: userId });
 };
 
 const create = async (data) => {
@@ -16,12 +16,16 @@ const update = async (id, data) => {
   return Contact.findByIdAndUpdate(id, data, { new: true });
 };
 
-const updateFavorite = async (id, favorite) => {
-  return Contact.findByIdAndUpdate(id, { favorite }, { new: true });
+const updateFavorite = async (id, userId, favorite) => {
+  return Contact.findOneAndUpdate(
+    { _id: id, owner: userId },
+    { favorite },
+    { new: true }
+  );
 };
 
-const remove = async (id) => {
-  return Contact.findByIdAndDelete(id);
+const remove = async (id, userId) => {
+  return Contact.findOneAndDelete({ _id: id, owner: userId });
 };
 
 export default {
