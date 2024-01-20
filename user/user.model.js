3cbe079c18867db27451c1c6994e38c4ff7a156a
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-20 12:40:30 +0100

    Create user.model.js

---
+++ user/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const Schema = mongoose;
+
+const user = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+user.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(6));
+};
+
+user.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("user", user, "users");
+
+module.exports = User;
