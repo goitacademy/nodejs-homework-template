bfcb5a01642430ee4f76722cd5faa3086a6fb2ad
Anastasiia211 <nastik121@ukr.net>
2024-01-20 13:14:25 +0200

    fixed favorite

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -7,9 +7,9 @@ import { contactAddSchema, contactUpdateSchema, updateFavoriteSchema} from "../m
 const getAll = async (req, res, next) => {
     try {
         const { _id: owner } = req.user;
-        const { page = 1, limit = 2 } = req.query;
+        const { page = 1, limit = 2 , favorite = false } = req.query;
         const skip = (page - 1) * limit;
-        const result = await Contact.find({ owner }, "-createdAt -updatedAt", {skip, limit}).populate("owner", "username");
+        const result = await Contact.find({ owner, favorite }, "-createdAt -updatedAt", {skip, limit}).populate("owner", "username");
         res.json(result);
     }
     catch (error) {
