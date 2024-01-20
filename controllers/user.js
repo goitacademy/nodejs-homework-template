35b979510caa8a7cdef25033ef5b03a20a59d58b
bober2005 <yaroslavd1290@gmail.com>
2024-01-20 13:59:55 +0200

    7

--- controllers/user.js
+++ controllers/user.js
@@ -5,67 +5,87 @@ const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { SECRET_KEY } = process.env;
 
+const generateToken = (userId) => {
+  const payload = {
+    id: userId,
+  };
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+};
+
+const updateUserToken = async (userId, token) => {
+  await User.findByIdAndUpdate(userId, { token });
+};
+
 const register = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (user) {
-    throw HttpError(409, "Email in use");
+    if (user) {
+      throw HttpError(409, "Email in use");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
+
+    res.status(201).json({
+      user: {
+        email: newUser.email,
+        subscription: newUser.subscription,
+      },
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
   }
-
-  const hashPassword = await bcrypt.hash(password, 10);
-
-  const newUser = await User.create({ ...req.body, password: hashPassword });
-  res.status(201).json({
-    user: {
-      email: newUser.email,
-      subscription: newUser.subscription,
-    },
-  });
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw HttpError(401, "Email or password is wrong");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    const token = generateToken(user._id);
+    await updateUserToken(user._id, token);
+
+    res.json({
+      token,
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
   }
-  const passwordCompare = await bcrypt.compare(password, user.password);
-  if (!passwordCompare) {
-    throw HttpError(401, "Email or password is wrong");
-  }
-
-  const payload = {
-    id: user._id,
-  };
-
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(user._id, { token });
-
-  res.json({
-    token,
-    user: {
-      email: user.email,
-      subscription: user.subscription,
-    },
-  });
 };
 
 const getCurrent = async (req, res) => {
-  const { email, subscription } = req.user;
+  try {
+    const { email, subscription } = req.user;
 
-  res.json({
-    email,
-    subscription,
-  });
+    res.json({
+      email,
+      subscription,
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
+  }
 };
 
 const logout = async (req, res) => {
-  const { _id } = req.user;
+  try {
+    const { _id } = req.user;
 
-  await User.findByIdAndUpdate(_id, { token: " " });
+    await User.findByIdAndUpdate(_id, { token: " " });
 
-  res.status(204).json();
+    res.status(204).json();
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
+  }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const mongoose = require("mongoose");
-
 const app = require("./app");
 
 const { DB_HOST } = process.env;
+const PORT = process.env.PORT || 3000;
 
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(DB_HOST)
+  .connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((error) => {
-    console.log(error.message);
+    console.error("Error connecting to the database:", error.message);
     process.exit(1);
   });
+
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to the database");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected from the database");
+});
+
+app.on("error", (err) => {
+  console.error("Express app error:", err.message);
+});
