6867614ce64a0c9c3a743fe48be1d5e7f082d202
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-20 06:30:15 +0100

    Using destructuring form body.

--- modules/contacts/controllers/create.js
+++ modules/contacts/controllers/create.js
@@ -3,14 +3,14 @@ import { schemaReq } from "../../../services/js/contacts.validation.js";
 
 export async function createContact(req, res, next) {
   try {
-    const body = req.body;
-    const val = schemaReq.validate(body);
+    const { name, email, phone, favourite } = req.body;
+    const val = schemaReq.validate({ name, email, phone, favourite });
     if (val.error) {
       return res.status(400).json({
         message: val.error.message,
       });
     }
-    const data = await create(body);
+    const data = await create({ name, email, phone, favourite });
     return res.status(201).json({ data });
   } catch (e) {
     return res.status(500).json(`An error occured: ${e.message}`);
--- modules/contacts/controllers/update.js
+++ modules/contacts/controllers/update.js
@@ -1,17 +1,37 @@
-import { update } from "../../../services/js/contacts.helpers.js";
+import {
+  update,
+  changeFavourite,
+} from "../../../services/js/contacts.helpers.js";
 import { schema } from "../../../services/js/contacts.validation.js";
 
 export async function updateContact(req, res, next) {
   try {
     const id = req.params.contactId;
-    const body = req.body;
+    const { name, email, phone } = req.body;
     const val = schema.validate(body);
     if (val.error) {
       return res.status(400).json({
         message: val.error.message,
       });
     }
-    const data = await update(id, body);
+    const data = await update(id, { name, email, phone });
+    return res.status(201).json(data);
+  } catch (e) {
+    return res.status(500).json(`An error occured: ${e}`);
+  }
+}
+
+export async function updateFavourite(req, res, next) {
+  try {
+    const id = req.params.contactId;
+    const { favourite } = req.body;
+    const val = schema.validate(body);
+    if (val.error) {
+      return res.status(400).json({
+        message: val.error.message,
+      });
+    }
+    const data = await changeFavourite(id, { favourite });
     return res.status(201).json(data);
   } catch (e) {
     return res.status(500).json(`An error occured: ${e}`);
