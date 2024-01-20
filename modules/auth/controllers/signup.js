755dad0ffdff820d32662206bde1c4a7e2fca9e6
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-20 08:41:31 +0100

    First steps to write code.

--- modules/auth/controllers/signup.js
+++ modules/auth/controllers/signup.js
@@ -1,8 +1,16 @@
 import { signupUser } from "../services/auth.helpers.js";
+import { userValidation } from "../services/auth.validation.js";
 
 export async function signup(req, res, next) {
   try {
-    res.status(200).json("Test completed.");
+    const { email, password } = req.body;
+    const val = userValidation.validate({ email, password });
+    if (val.error) {
+      return res.status(400).json(val.error);
+    }
+    const data = await signupUser({ email, phone });
+    console.log(data);
+    return res.status(200).json("Test completed.");
   } catch (e) {
     return res.status(500).json(`An error occured: ${e.message}`);
   }
