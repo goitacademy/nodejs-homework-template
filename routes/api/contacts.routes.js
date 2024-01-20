b4f9fdd2a8739e6b269a2973701a79d37ec936a7
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-01-20 12:38:31 +0100

    update router path : add "auth"

--- routes/api/contacts.routes.js
+++ routes/api/contacts.routes.js
@@ -1,18 +1,19 @@
 import express from "express";
+import auth from "../../middlewares/auth.js"
 import contactsController from "../../controllers/contacts.controller.js";
 
 const router = express.Router();
 
-router.get("/contacts", contactsController.get);
+router.get("/contacts",auth, contactsController.get);
 
-router.get("/contacts/:id", contactsController.getById);
+router.get("/contacts/:id", auth, contactsController.getById);
 
-router.post("/contacts", contactsController.create);
+router.post("/contacts", auth,contactsController.create);
 
-router.put("/contacts/:id", contactsController.update);
+router.put("/contacts/:id", auth, contactsController.update);
 
-router.patch("/contacts/:id/favorite", contactsController.updateFavorite);
+router.patch("/contacts/:id/favorite", auth, contactsController.updateFavorite);
 
-router.delete("/contacts/:id", contactsController.remove);
+router.delete("/contacts/:id",auth,  contactsController.remove);
 
 export { router };
