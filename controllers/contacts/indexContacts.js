a8b55e4bccd9f4a6676c9e51703b343cc514dde0
Viache-Slav <slavikfedko@gmail.com>
2024-01-21 15:44:35 +0100

    porady mentora 2
    Rozbicie na kontrolery (indexContacts.js, showContacts.js)

---
+++ controllers/contacts/indexContacts.js
@@ -0,0 +1,24 @@
+import { listContacts } from '../../models/contacts.js';
+
+async function indexContacts(req, res, next) {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json({
+      contacts,
+    });
+  } catch (err) {
+    res.status(500).json(`An error occurred: ${err}`);
+  }
+}
+
+export {indexContacts};
+
+
+// router.get('/api/contacts', async (req, res, next) => {
+//     try {
+//       const contacts = await listContacts();
+//       res.status(200).json(contacts);
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
---
+++ controllers/contacts/showContacts.js
@@ -0,0 +1,37 @@
+
+import { getContactById } from '../../models/contacts.js';
+
+async function showContacts(req, res, next) {
+  const { contactId } = req.params;
+
+  try {
+    const contact = await getContactById(contactId);
+
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: 'Contact not found' });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { showContacts };
+
+
+
+
+// router.get('/api/contacts/:contactId', async (req, res, next) => {
+//     const { contactId } = req.params;
+//     try {
+//       const contact = await getContactById(contactId);
+//       if (contact) {
+//         res.status(200).json(contact);
+//       } else {
+//         res.status(404).json({ message: 'Not found' });
+//       }
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
