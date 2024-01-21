722b3a2b2ca956664caf0d8148eaf6f1b01628bd
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-01-22 01:39:02 +0200

    add routes into app.js
    and fixed response error bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const {contactsRouter, authRouter} = require('./routes/api')
+const {contactsRouter, authRouter, userRouter} = require('./routes/api')
 
 const app = express()
 
@@ -11,9 +11,11 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
@@ -23,7 +25,7 @@ app.use((err, req, res, next) => {
   if (err.code === 11000) {
     res.status(409).json({ message: 'Email in use' })
   }
-  res.status(err.statusCode).json({ message: err.message })
+  res.status(500).json({ message: err.message })
 })
 
 module.exports = app
