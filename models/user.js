992976cf3761ce96d88b2469c559ec47579a805c
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-01-22 01:39:28 +0200

    update user model

--- models/user.js
+++ models/user.js
@@ -16,7 +16,11 @@ const userSchema = new Schema({
     enum: ["starter", "pro", "business"],
     default: "starter"
   },
-  token: String
+  token: String,
+  avatarUrl: {
+    type: String,
+    required: true,
+  }
 }, {versionKey: false, timestamps: true}
 );
 
