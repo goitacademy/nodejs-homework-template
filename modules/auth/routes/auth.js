ee873c4a3387eb70f7d9c1287bc9a159209a93eb
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-23 23:43:15 +0100

    Creating routes and adding middlewares.

--- modules/auth/routes/auth.js
+++ modules/auth/routes/auth.js
@@ -1,17 +1,23 @@
 import { Router } from "express";
-import { signup } from "../controllers/signup.js";
+
+import "../services/auth.strategy.js";
+import auth from "../middlewares/auth.js";
+
 import { login } from "../controllers/login.js";
+import { signup } from "../controllers/signup.js";
 import { logout } from "../controllers/logout.js";
 import { current } from "../controllers/current.js";
+import { validateAuth } from "../middlewares/validation.js";
+import { userValidation } from "../../users/services/auth.validation.js";
 
 const router = Router();
 
-router.post("/signup", signup);
+router.post("/signup", validateAuth(userValidation), signup);
 
-router.post("/login", login);
+router.post("/login", validateAuth(userValidation), login);
 
-router.post("/logout", logout);
+router.post("/logout", auth, logout);
 
-router.post("/current", current);
+router.post("/current", auth, current);
 
 export default router;
