33ae9aaa6dd3467e4c6df8a2dc98f673cbd04a8e
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-23 19:39:41 +0100

    Creating controller for logout endpoint.

--- modules/auth/controllers/logout.js
+++ modules/auth/controllers/logout.js
@@ -2,6 +2,14 @@ import { logoutUser } from "../services/auth.helpers.js";
 
 export async function logout(req, res, next) {
   try {
+    const id = req.user.id;
+    const user = await logoutUser(id);
+    if (user.error) {
+      res.status(401).json(user.error);
+    }
+    await user.clearToken();
+    await user.save();
+    return res.status(204).json("No Content");
   } catch (e) {
     return res.status(500).json(`An error occured: ${e.message}`);
   }
