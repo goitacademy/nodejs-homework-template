9e16bae8d0d32e5770655fe33fade9b0d5bdb4ec
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2024-01-23 19:24:59 +0100

    logout-added
    Endpoint /users/logout created
    
    Added token validation stub to the route.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const contactsRouter = require('./routes/api/contacts');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/api/users');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -24,7 +24,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', usersRouter);
+app.use('/api/users', authenticateToken, usersRouter);
 
 app.use('/api/contacts', authenticateToken, contactsRouter);
 
