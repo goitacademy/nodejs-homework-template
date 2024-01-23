1fce2bcdef30641b801c61df1b62605636d8803f
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-23 19:39:13 +0100

    Creating controller for current endpoint.

--- modules/auth/controllers/current.js
+++ modules/auth/controllers/current.js
@@ -2,6 +2,12 @@ import { currentUser } from "../services/auth.helpers.js";
 
 export async function current(req, res, next) {
   try {
+    const id = req.user.id;
+    const user = await currentUser(id);
+    if (user.error) {
+      return res.status(401).json(user.error);
+    }
+    return res.status(200).json({ ...user });
   } catch (e) {
     return res.status(500).json(`An error occured: ${e.message}`);
   }
