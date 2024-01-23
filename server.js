abee516b72324e4fc79af209e96bdb797f26a45a
dn280786lea <ekaterina.lubenets@gmail.com>
2024-01-23 21:49:34 +0100

    03
    03

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app')
+const mongosse = require("mongosse");
+const DB_HOST =
+  "mongodb+srv://katerynalubenets:DoAaEeKcpnlzWwXm@cluster0.s4xnwyt.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongosse.set("strictQuery", true);
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
