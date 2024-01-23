e9c2a389d863783343688871cbb5417cbbdc88a2
Zhihare <128489441+Zhihare@users.noreply.github.com>
2024-01-23 17:06:25 +0200

    Update serverConfig.js

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,6 +1,6 @@
 const serverConfig = {
-	mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017',
-	dbname: process.env.DB_NAME ?? 'testdb',
+	mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://one_user_one:pgnP9v_CJrxy%40BJ@cluster0.pnwidkg.mongodb.net/',
+	dbname: process.env.DB_NAME ?? 'db-contacts',
 	appName: process.env.PROJECT_NAME ?? 'Default name',
 	port: process.env.PORT ? +process.env.PORT : 3000,
 	environment: process.env.NODE_ENV ?? 'development',
