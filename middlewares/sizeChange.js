17a72f94d4a2364208faac4587f2101b9030d1d0
Serhii Haber <haberserhii@icloud.com>
2024-01-24 13:35:38 +0200

    Update sizeChange.js

--- middlewares/sizeChange.js
+++ middlewares/sizeChange.js
@@ -1,13 +1,12 @@
 import Jimp from "jimp";
 import fs from "fs/promises";
-
 const sizeChange = async (req, res, next) => {
   const { path: tmpDir, originalname } = req.file;
   console.log(req.file);
 
   Jimp.read(tmpDir)
     .then((image) => {
-      return image.resize(250, 250).write(tmpDir);
+      return image.resize(250, 250).writeAsync(tmpDir);
     })
     .then(() => {
       next();
