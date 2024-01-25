25b4df19a9754a2aea6603aa47eb172654794e5a
AnastasiaGOIT <hudzevych.music@gmail.com>
2024-01-25 14:15:40 +0200

    fix

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -4,9 +4,14 @@ const { HttpError } = require("../helpers/HttpError");
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400), `${id} is not valid`);
+    next(HttpError(400), "Not found");
   }
   next();
 };
+// const isValidId = (req, res, next) => {
+//   const { id } = req.params;
+//   Types.ObjectId.isValid(id);
+//   if (!isValidId) throw new HttpError(404, "Contact not found");
+// };
 
 module.exports = isValidId;
