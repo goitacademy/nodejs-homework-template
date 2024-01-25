3f58da978f2a5ce553c119517f4e2bbb05f78223
AlexKosha <ovelikoshich@gmail.com>
2024-01-25 22:49:36 +0100

    back_end

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+// 0GtzUoFgm7OmxC6G
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://oleksiy:0GtzUoFgm7OmxC6G@cluster0.lsge9jo.mongodb.net/books_reader?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
