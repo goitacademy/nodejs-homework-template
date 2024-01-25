fc468c43ea9934258f712dbc959c4ed483f38663
dn280786lea <ekaterina.lubenets@gmail.com>
2024-01-25 15:55:45 +0100

    up
    up

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const mongosse = require("mongosse");
-const DB_HOST =
-  "mongodb+srv://katerynalubenets:DoAaEeKcpnlzWwXm@cluster0.s4xnwyt.mongodb.net/db-contacts?retryWrites=true&w=majority";
-mongosse.set("strictQuery", true);
-const app = require("./app");
+const mongoose = require("mongoose");
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const app = require("./app");
+const { DB_HOST, PORT = 4003 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
