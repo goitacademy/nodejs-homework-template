916835c5609aba61ec83c24fcb63800a9f20e93e
AntKhom <antonhomenko1988@gmail.com>
2024-01-26 10:17:14 -0500

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,28 +1,29 @@
 const jwt = require("jsonwebtoken");
 
-const {User} = require("../models/user");
+// const { User } = require("../models/user.js");
 
 const {HttpError} = require("../helpers");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
     const {authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ");
     if(bearer !== "Bearer") {
-        next(HttpError(401));
+        next(HttpError(401,"Not authorized"));
     }
     try {
-        const {id} = jwt.verify(token, SECRET_KEY);
-        const user = await User.findById(id);
-        if(!user || !user.token || user.token !== token) {
-            next(HttpError(401)); 
-        }
-        req.user = user;
-        next();
+        const { id } = jwt.verify(token, SECRET_KEY);
+        console.log(id);
+        // const user = User.findById(id); 
+        // if(!user || !user.token || user.token !== token) {
+        //     next(HttpError(401)); 
+        // }
+        // req.user = user;
+         next();
     }
     catch {
-        next(HttpError(401));
+        next(HttpError(401, "Not authorized"));
     }
 }
 
