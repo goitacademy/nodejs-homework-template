a0270c23d44fe99db9c0755063ce3f0dcba1471b
Misetr <135700846+Misetr@users.noreply.github.com>
2024-01-27 21:33:39 +0200

    fix auth js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,17 @@ const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/logout', (req, res) => {
+  req.logout(); 
+  res.redirect(process.env.AUTH0_LOGOUT_URL); 
+});
+router.get('/current', (req, res) => {
+  if (!req.oidc.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not authenticated' });
+  }
+ 
+  res.json(req.oidc.user);
+});
 
 
 
