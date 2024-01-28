e9ed4a9a3a18eb63ab0365a94ab5e51b85ab0b98
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-28 14:05:19 +0100

    Adding multer middleware.

--- modules/users/middlewares/avatar.js
+++ modules/users/middlewares/avatar.js
@@ -3,10 +3,17 @@ import multer from "multer";
 
 export const tmpDir = path.join(process.cwd(), "tmp");
 export const avatarDir = path.join(process.cwd(), "public/avatars");
+export const test1 = () => {
+  console.log(process.cwd());
+};
+
+export function test2() {
+  console.log(process.cwd());
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, tmpDir);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
