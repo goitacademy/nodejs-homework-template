7103379e63647f7c7ce2b844cdd6c7266ca2e5e8
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-28 14:04:01 +0100

    Adding new endpoint with middlewares that enables adding avatar image of user checking authorization.

--- modules/users/routes/users.js
+++ modules/users/routes/users.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import "../../auth/services/auth.strategy.js";
 import auth from "../../auth/middlewares/auth.js";
-import { updateUser } from "../controllers/update.js";
+import { uploadMiddleware } from "../middlewares/avatar.js";
+import { updateUser, updateAvatar } from "../controllers/update.js";
 
 const router = Router();
 
 router.patch("/", auth, updateUser);
+router.patch("/avatars", auth, uploadMiddleware.single("avatar"), updateAvatar);
 
 export default router;
