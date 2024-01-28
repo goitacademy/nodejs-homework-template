427562e81ec978748ce370b7cd97586754a59e67
Денис Бабич <chertik@MacBook-Pro-Denis.local>
2024-01-28 21:29:33 +0000

    add d

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/api/users', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((_, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Not foundd' })
 })
 
 app.use((err, _, res, __) => {
