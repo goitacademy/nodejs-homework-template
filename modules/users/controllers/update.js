2a10d797f34f699d0afdf6043ad7c2e4f58c7f30
mattmalicki <mateusz.malicki96@gmail.com>
2024-01-28 14:04:48 +0100

    Adding new controller function that updates avatar of user.

--- modules/users/controllers/update.js
+++ modules/users/controllers/update.js
@@ -1,15 +1,29 @@
 import { User } from "../schemas/user.schema.js";
 
-export async function updateUser(req, res, next) {
-  const subscriptions = ["starter", "pro", "buisness"];
-  const { subscription } = req.body;
-  if (!subscriptions.includes(subscription)) {
-    return res.status(400).json("Invalid subscription");
+export async function updateUser(req, res) {
+  try {
+    const subscriptions = ["starter", "pro", "buisness"];
+    const { subscription } = req.body;
+    if (!subscriptions.includes(subscription)) {
+      return res.status(400).json("Invalid subscription");
+    }
+    const id = req.user.id;
+    const user = await User.findByIdAndUpdate(id, {
+      subscription: subscription,
+    });
+    if (!user) {
+      return res.status(404).json("Not found");
+    }
+    return res.status(200).json("User updated.");
+  } catch (e) {
+    return res.status(500).json(e.message);
   }
-  const id = req.user.id;
-  const user = await User.findByIdAndUpdate(id, { subscription: subscription });
-  if (!user) {
-    return res.status(404).json("Not found");
+}
+
+export async function updateAvatar(req, res) {
+  try {
+    return res.status(200).json("Avatar uploaded.");
+  } catch (e) {
+    return res.status(500).json(e.message);
   }
-  return res.status(200).json("User updated.");
 }
