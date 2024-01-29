962af5177f95a04b36d6a30677f02ded689fdb9a
Mateusz Kubiak <smuku.mk@gmail.com>
2024-01-29 18:25:04 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/api/contacts.js";
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
+const connection = mongoose.connect(process.env.DATABASE_URL);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
