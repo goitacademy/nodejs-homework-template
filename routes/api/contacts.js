7b1f8d078ce46dc6c133a715bc58cd5740f154bd
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2024-01-31 23:19:50 +0100

    return

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -25,7 +25,7 @@ const putSchema = Joi.object({
 router.get("/", async (req, res, next) => {
   try {
     const contacts = await listContacts();
-    res.json({
+    return res.json({
       status: "success",
       code: 200,
       data: {
@@ -42,7 +42,7 @@ router.get("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
     if (contact) {
-      res.json({ status: "success", code: 200, data: { contact } });
+      return res.json({ status: "success", code: 200, data: { contact } });
       return;
     }
     res.status(404).json({ message: "Not Found", code: 404 });
@@ -56,7 +56,7 @@ router.post("/", async (req, res, next) => {
     const { error, value } = postSchema.validate(req.body);
 
     if (error) {
-      res.status(400).json({
+      return res.status(400).json({
         status: "failure",
         code: 400,
         message: "Missing required field",
@@ -83,13 +83,13 @@ router.delete("/:contactId", async (req, res, next) => {
 
     if (contact) {
       await removeContact(contactId);
-      res.json({
+      return res.json({
         status: "success",
         code: 200,
         message: "contact deleted",
       });
     } else {
-      res.json({
+      return res.json({
         status: "failure",
         code: 404,
         message: "Not found",
@@ -105,7 +105,7 @@ router.put("/:contactId", async (req, res, next) => {
     const { error, value } = putSchema.validate(req.body);
 
     if (error) {
-      res.status(400).json({
+      return res.status(400).json({
         status: "failure",
         code: 400,
         message: "Missing fields",
@@ -119,13 +119,13 @@ router.put("/:contactId", async (req, res, next) => {
 
     if (contact) {
       await updateContact(contactId, body);
-      res.json({
+      return res.json({
         status: "success",
         code: 200,
         message: "Contact was updated",
       });
     } else {
-      res.json({
+      return res.json({
         status: "failure",
         code: 404,
         message: "Not found",
