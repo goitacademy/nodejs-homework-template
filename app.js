2d0759275b13520819c56f081c34413e59bb56de
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-01-31 23:23:56 +0100

    working with configurations

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import contactsRouter from "#routes/api/contacts.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/contacts", contactsRouter);
+
+app.listen(PORT, () => {
+  console.log(`Serwer działa. Używaj API na porcie: ${PORT}`);
+});
