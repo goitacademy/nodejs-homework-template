f620a40ac2aa7bbc00dbf20729b03b141875bfe1
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-01-31 23:09:26 +0100

    Starting from server

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
-// const { contactsRouter } = require();
-require("dotenv").config();
-
-const connection = mongoose.connect(uriDb);
