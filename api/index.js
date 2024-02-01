863ccb4baf92fd91b47169fe5123cd62ca733203
Mateusz Kubiak <smuku.mk@gmail.com>
2024-02-01 20:47:22 +0100

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+import * as ctrlContact from "../controller";
+
+router.get("/", ctrlContact.get);
+router.get("/:contactId", ctrlContact.getById);
+router.post("/", ctrlContact.create);
+router.put("/:contactId", ctrlContact.update);
+router.patch("/:contactId/favorite", ctrlContact.updateStatus);
+router.delete("/:contactId", ctrlContact.remove);
+
+export default router;
