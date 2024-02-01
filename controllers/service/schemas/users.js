3f1ae489aa6fd6788ee03e9af0c30f7e6bb5ae05
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-02 00:43:36 +0100

    Prepare and step 1 done

---
+++ controllers/service/schemas/users.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const users = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const User = model("user", users, "users");
+
+export { User };
