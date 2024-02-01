1626c80ab4aebd55eddf3768581d22e60b9b03a1
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-01 23:38:04 +0100

    clean

--- controllers/contacts/indexContacts.js
+++
@@ -1,8 +0,0 @@
-import {listContacts} from '../handlers.js'
-
-async function indexContacts(req, res, next) {
-    const contacts = listContacts();  
-    return res.json(contacts).status(200); 
-}
-
-export { indexContacts };
---
+++ node_modules/nanoid/non-secure/index.js
@@ -0,0 +1,27 @@
+import { urlAlphabet } from '../index.js'
+
+let customAlphabet = (alphabet, size) => {
+  return () => {
+    let id = ''
+    // A compact alternative for `for (var i = 0; i < step; i++)`.
+    let i = size
+    while (i--) {
+      // `| 0` is more compact and faster than `Math.floor()`.
+      id += alphabet[Math.random() * alphabet.length | 0]
+    }
+    return id
+  }
+}
+
+let nanoid = (size = 21) => {
+  let id = ''
+  // A compact alternative for `for (var i = 0; i < step; i++)`.
+  let i = size
+  while (i--) {
+    // `| 0` is more compact and faster than `Math.floor()`.
+    id += urlAlphabet[Math.random() * 64 | 0]
+  }
+  return id
+}
+
+export { nanoid, customAlphabet }
