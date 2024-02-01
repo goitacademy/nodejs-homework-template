a62aa752684f4ba6d983424ff80bb95f851e66fc
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2024-02-01 20:56:34 +0100

    controllers

---
+++ controllers/contacts.js
@@ -0,0 +1,154 @@
+const service = require("../service");
+
+const get = async (_, res, next) => {
+  try {
+    const results = await service.getAllContacts();
+    return res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contacts: results,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+const getById = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const result = await service.getContactById(contactId);
+    if (result) {
+      return res.json({
+        status: "success",
+        code: 200,
+        data: { contact: result },
+      });
+    } else {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: `Not found contact id: ${contactId}`,
+        data: "Not Found",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+const create = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  console.log(req.body);
+
+  try {
+    const result = await service.createContact({ name, email, phone });
+
+    return res.status(201).json({
+      status: "success",
+      code: 201,
+      data: { contact: result },
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+const remove = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    const result = await service.removeContact(contactId);
+    if (result) {
+      return res.json({
+        status: "success",
+        code: 200,
+        data: { contact: result },
+        message: "Contact removed. File saved with updated contacts list",
+      });
+    } else {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: `Not found contact id: ${contactId}`,
+        data: "Not Found",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+const update = async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+
+  try {
+    const result = await service.updateContact(contactId, {
+      name,
+      email,
+      phone,
+    });
+    if (result) {
+      return res.json({
+        status: "success",
+        code: 200,
+        data: { contact: result },
+      });
+    } else {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: `Not found contact id: ${contactId}`,
+        data: "Not Found",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+const updateFavourite = async (req, res, next) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+
+  if (favorite === undefined) {
+    return res.status(400).json({ message: "Missing field favorite" });
+  }
+  try {
+    const result = await service.updateStatusContact(contactId, favorite);
+    if (result) {
+      return res.json({
+        status: "success",
+        code: 200,
+        data: { contact: result },
+      });
+    } else {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: `Not found task id: ${contactId}`,
+        data: "Not Found",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+module.exports = {
+  get,
+  getById,
+  remove,
+  create,
+  update,
+  updateFavourite,
+};
