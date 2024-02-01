bbc52ab050b7800e838106bdb78dd33a33c01336
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2024-02-01 22:18:39 +0100

    uri error

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,7 +5,7 @@ const {
   addContact,
   removeContact,
   updateContact,
-} = require("../../models/contacts");
+} = require("../models/contacts");
 
 const Joi = require("joi");
 
