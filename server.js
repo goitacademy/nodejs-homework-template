d6cdf405115e72a0f93c30ff9644f07c0c1f92d5
chernyakinamarina@gmail.com <chernyakinamarina@gmail.com>
2024-02-02 16:42:01 +0200

    connected to db

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
 const app = require('./app');
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+const URI = process.env.DB_HOST;
+
+console.log(process.env.DB_HOST);
+mongoose
+  .connect(URI)
+  .then(() => {
+    app.listen(3000, () => console.log('Database connection successful'));
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
