d5d580d901dc793c9821d991dee1ded15871e4c2
AntKhom <antonhomenko1988@gmail.com>
2024-02-02 09:53:06 -0500

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,7 +30,7 @@ const register = async (req, res) => {
     const verifyEmail = {
         to: email,
         subject: "Verify email",
-        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`
+        html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click verify email</a>`
     };
 
     await sendEmail(verifyEmail);
@@ -71,7 +71,7 @@ const resendVerifyEmail = async(req, res)=> {
     const verifyEmail = {
         to: email,
         subject: "Verify email",
-        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${user.verificationToken}">Click verify email</a>`
+        html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Click verify email</a>`
     };
 
     await sendEmail(verifyEmail);
