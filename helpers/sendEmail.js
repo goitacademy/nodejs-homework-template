7b90db87a7fc2bb1221f8354509ffb11c6c74ee8
Serhii Haber <haberserhii@icloud.com>
2024-02-02 10:22:22 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,14 +1,14 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
-const { UKR_NET_PASSWORD, UKR_NET_FORM } = process.env;
+const { UKR_NET_PASSWORD, UKR_NET_FROM } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.ukr.net",
   port: 465,
   secure: true,
   auth: {
-    user: UKR_NET_FORM,
+    user: UKR_NET_FROM,
     pass: UKR_NET_PASSWORD,
   },
 };
@@ -16,7 +16,7 @@ const nodemailerConfig = {
 const tranport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = (data) => {
-  const email = { ...data, from: UKR_NET_FORM };
+  const email = { ...data, from: UKR_NET_FROM };
   return tranport.sendMail(email);
 };
 
