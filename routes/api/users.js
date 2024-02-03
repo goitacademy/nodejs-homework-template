f3fc5f893ce90440d1d8a420a09ed5b17ff8514a
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-04 00:00:23 +0100

    login done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import registerUser from "../../controllers/users/signupRouters.js";
+import loginUser from "../../controllers/users/loginRouters.js";
 
 
 const router = Router();
 
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 
 export default router;
