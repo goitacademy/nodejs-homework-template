8ca8609c3c02ef95116a7498724646396f394c3a
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-02-03 10:22:23 +0100

    Implement user authentication middleware
    - Extract token from Authorization header
    - Verify and decode the token using JWT
    - Find user in the database using decoded information
    - Throw an error if user not found or authentication fails
    - Return a 401 status with an error message on authentication failure

--- controllers/contacts/createContacts.js
+++ controllers/contacts/createContacts.js
@@ -1,5 +1,18 @@
 // Kontroler do obsługi tworzenia nowego kontaktu
 import { addContact } from "#models/contacts.js";
+import User from "#models/users.js";
+
+const autehnticateUser = async (req, res, next) => {
+try {
+  const token = req.header("Authorization").replace("Bearer ", "");
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await User.findOne({ _id: decoded._id, token });
+  if (!user) {
+    throw new Error();
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate" });
+  }
+}
 
 async function createContacts(req, res, next) {
   const { body } = req;
