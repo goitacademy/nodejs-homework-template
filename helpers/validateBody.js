e976f296ea76cad44fd37e3e6a938028339e96e3
chernyakinamarina@gmail.com <chernyakinamarina@gmail.com>
2024-02-03 15:42:35 +0200

    update controllers for model

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,16 +1,15 @@
 const { HttpError } = require('../helpers/HttpError');
 const {
-  createContactSchema,
-  updateContactSchema,
-} = require('../schemas/contactsSchemas');
+  schemas,
+} = require('../models/contact');
 
 const validateBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
 
-    if (error && schema == createContactSchema) {
+    if (error && schema === schemas.createContactSchema) {
       throw HttpError(400, error.message);
-    } else if (error && schema == updateContactSchema) {
+    } else if (error && schema === schemas.updateContactSchema) {
       throw HttpError(400, 'Body must have at least one field');
     }
 
