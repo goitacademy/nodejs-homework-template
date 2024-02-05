d0162d9ca448336422bfb60e524066a1ca704383
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-05 23:57:10 +0100

    Ad.2 fixed

--- controllers/service/gravatar.js
+++ controllers/service/gravatar.js
@@ -1,7 +1,7 @@
 import gravatar from "gravatar";
 
 const createAvatar = (email) => {
-  const newAvatar = gravatar.url(email);
+  const newAvatar = gravatar.url(email, { s: 250, protocol: "https" });
   return newAvatar;
 };
 
