b531315e551afe6647428959b271898d6949232b
Malwina9086 <malwina.lukjanska1@gmail.com>
2024-02-09 20:49:47 +0000

    done hw03mongodb
    hw done

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const app = require('./app')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const db = mongoose.connect(uriDb, {
+  useUnifiedTopology: true,
+  dbName: "db-contacts",
+});
+
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log("Database connection successful");
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch((err) =>
+  console.log(`Server not running. Error message: ${err.message}`)
+);
