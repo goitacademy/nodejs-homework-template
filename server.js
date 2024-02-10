f6e2513a88cd0ec20d8f521135d19346a9a165bb
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-10 15:14:29 +0100

    create connect with mongosse

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 import { app } from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DATABASE_URL;
+
+const connection = mongoose.connect(uriDb);
+
+connection
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, function () {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(`Server not running. Error message: ${error.message}`);
+    process.exit(1);
+  });
