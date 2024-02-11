ed687e90a745228740fbac256d3d0d5a286b94c8
Agata Wajda <agawaj98@gmail.com>
2024-02-11 23:27:25 +0100

    Added mongoose

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const db = mongoose.connect("");
+
+db.then(() => {
+	console.log("db is running");
+}).catch((error) => console.log(error));
 
 app.use(express.urlencoded());
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
