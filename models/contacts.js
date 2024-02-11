f43d4227bd23bb9b668c2ff45f965e999b1ca4c1
Malwina9086 <malwina.lukjanska1@gmail.com>
2024-02-11 19:50:13 +0000

    made changes in hw03

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,90 @@
-// const fs = require('fs/promises')
+const Contact = require("../schema/contacts.schema");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const contacts = await Contact.find();
+    return contacts;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contact = await Contact.findById(contactId);
+    if (!contact) {
+      throw new Error("Contact not found");
+    }
+    return contact;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const removedContact = await Contact.findByIdAndRemove(contactId);
+    if (!removedContact) {
+      throw new Error("Contact not found");
+    }
+    return removedContact;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  try {
+    const newContact = await Contact.create(body);
+    return newContact;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(contactId, body, {
+      new: true,
+    });
+
+    if (!updatedContact) {
+      throw new Error("Contact not found");
+    }
+
+    return updatedContact;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateStatusContact = async (contactId, body) => {
+  try {
+    if (body.favorite === undefined) {
+      throw new Error("Missing field 'favorite'");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite: body.favorite },
+      { new: true }
+    );
+
+    if (!updatedContact) {
+      throw new Error("Contact not found");
+    }
+
+    return updatedContact;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +92,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+  updateStatusContact,
+};
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const db = mongoose.connect(uriDb, {
+  useUnifiedTopology: true,
+  dbName: "db-contacts",
+});
+
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log("Database connection successful");
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch((err) =>
+  console.log(`Server not running. Error message: ${err.message}`)
+);
