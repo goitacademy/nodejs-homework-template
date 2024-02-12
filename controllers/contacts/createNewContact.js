0617262bf60f6c54acfe3d07c5ed386e72b9f1b0
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-12 20:59:38 +0100

    change exports in validator

---
+++ controllers/contacts/createNewContact.js
@@ -0,0 +1,33 @@
+import { validateAddContact } from "../../validator.js";
+import { createContact, getContactByName } from "../../service/index.js";
+
+export async function createNewContact(req, res, next) {
+  const { name, email, phone, favorite } = req.body;
+  const { error } = validateAddContact(req.body);
+  try {
+    if (error) {
+      console.log(error);
+      return res.json({ status: 400, msg: "Missing fields" });
+    }
+
+    const searchContact = await getContactByName(name);
+
+    if (searchContact) {
+      return res.status(400).json({
+        status: "failed",
+        code: 400,
+        data: "Not found",
+        message: `Contact with name: '${name}' already exsist`,
+      });
+    } else {
+      const newContact = await createContact({ name, email, phone, favorite });
+      return res.status(201).json({
+        status: "success",
+        code: 201,
+        data: { contact: newContact },
+      });
+    }
+  } catch (error) {
+    res.status(500).json(`Error message: ${error}`);
+  }
+}
