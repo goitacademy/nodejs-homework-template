ba3dcba2bba9a094da2d826b396f2194ecfa9922
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-12 12:59:58 +0100

    Create schema Contact

---
+++ service/schemas/contacts.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const contacts = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Contact = mongoose.model("contact", contacts);
---
+++ validation.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const schema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().required(),
+});
