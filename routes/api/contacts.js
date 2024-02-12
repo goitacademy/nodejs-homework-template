40377d9e6feb9c8e478099ab20d0827df8d38d0f
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-12 13:28:32 +0100

    implement import alliases

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,14 @@
 import express from "express";
 
-import { showContacts } from "../../controllers/contacts/showContacts.js";
+import { showContacts } from "#controllers/contacts/showContacts.js";
 
-import { showContactById } from "../../controllers/contacts/showContactById.js";
+import { showContactById } from "#controllers/contacts/showContactById.js";
 
-import { createContact } from "../../controllers/contacts/createContact.js";
+import { createContact } from "#controllers/contacts/createContact.js";
 
-import { deleteContact } from "../../controllers/contacts/deleteContact.js";
+import { deleteContact } from "#controllers/contacts/deleteContact.js";
 
-import { updateContact } from "../../controllers/contacts/updateContact.js";
+import { updateContact } from "#controllers/contacts/updateContact.js";
 
 const router = express.Router();
 
