31ec71afc3c808ad56883e3b40e14529538976ca
Mart <marta.nikiel@yahoo.com>
2024-02-14 12:14:55 +0100

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/contacts");
+const userController = require("../controllers/users");
 const { auth } = require("../middlewares/auth");
 
 router.post("/signup", auth, userController.register);
