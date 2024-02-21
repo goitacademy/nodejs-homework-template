39fad1af8ab736ba57cb45f35d7464ce768421f4
Mariusz-72 <mmbwdap@gmail.com>
2024-02-21 21:49:11 +0100

    update 2.1 R

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,104 @@
-const express = require('express')
+const express = require("express");
+const { Contacts } = require("../schema");
+const router = express.Router();
+const { validateJoi } = require("./validation");
 
-const router = express.Router()
+router.get("/", async (__, res) => {
+  const contacts = await Contacts.find();
+  try {
+    res.json({
+      status: "success",
+      code: 200,
+      data: contacts,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Bad Request",
+    });
+  }
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await Contacts.findById(contactId);
+    res.json({
+      status: "success",
+      code: 200,
+      data: contact,
+      message: "Contact has been found",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Bad Request",
+    });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res) => {
+  const body = req.body;
+  const contact = new Contacts(body);
+  const validate = validateJoi(contact);
+  try {
+    await validate.value.save();
+    res.json({
+      status: "success",
+      code: 201,
+      data: contact,
+      message: "Contact has been found created",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Bad Request",
+    });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  try {
+    const deleteContact = await Contacts.deleteOne({ _id: contactId });
+    res.json({
+      status: "success",
+      code: 200,
+      data: deleteContact,
+      message: "Contact has been deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Bad Request",
+    });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const body = req.body;
+  const contact = await Contacts.findOneAndUpdate({ _id: contactId }, body);
+  const validate = validateJoi(contact);
+  try {
+    await validate.value.save();
+    res.json({
+      status: "success",
+      code: 200,
+      data: contact,
+      message: "Contact has been created/updated",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Bad Request",
+    });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
