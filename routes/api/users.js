095648dc6dd947aa5e8f6c21e06111d1aa66cabf
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-22 14:04:29 +0100

    create rout users/current

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ import authMiddleware from "../../auth.js";
 import { logOut } from "../../controllers/users/logOut.js";
 import { logIn } from "../../controllers/users/logIn.js";
 import { signUp } from "../../controllers/users/signUp.js";
+import { current } from "../../controllers/users/current.js";
 
 const router = express.Router();
 
@@ -12,5 +13,7 @@ router.post("/signup", signUp);
 router.post("/login", logIn);
 
 router.get("/logout", authMiddleware, logOut);
+
+router.get("/current", authMiddleware, current);
 
 export { router };
