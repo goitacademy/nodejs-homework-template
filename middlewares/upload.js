c67e914e433e3b1720d77bffadbcd0352c34013e
Mart <marta.nikiel@yahoo.com>
2024-02-23 19:46:31 +0100

    finished homework 5

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,68 +1,20 @@
-const fs = require("fs").promises;
-
-const path = require("path");
 const multer = require("multer");
+const path = require("path");
 
-const imageStore = path.join(process.cwd(), "public", "avatars");
-const uploadDir = path.join(process.cwd(), "tmp");
+const tempDir = path.join(__dirname, "../", "temp");
 
-const extensionWhiteList = [".jpg", ".jpeg", ".png", ".gif"];
-const mimetypeWhiteList = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
-
-const isAccessible = async (path) => {
-  try {
-    await fs.access(path);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-const createFolderIfNotExist = async (folder) => {
-  try {
-    if (!(await isAccessible(folder))) {
-      await fs.mkdir(folder);
-    }
-  } catch (e) {
-    console.log("No access to folder");
-    process.exit(1);
-  }
-};
-
-const storage = multer.diskStorage({
+const multerConfig = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, tempDir);
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, file.originalname);
   },
-  limits: {
-    fileSize: 1048576,
-  },
+  limits: { fileSize: 2048 },
 });
 
-const uploadMiddleware = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    const extension = path.extname(file.originalname).toLowerCase();
-    const mimetype = file.mimetype;
-    if (
-      !extensionWhiteList.includes(extension) ||
-      !mimetypeWhiteList.includes(mimetype)
-    ) {
-      return cb(new Error("Wrong file format"));
-    }
-    return cb(null, true);
-  },
-  limits: {
-    fileSize: 8 * 1024 * 16,
-  },
+const upload = multer({
+  storage: multerConfig,
 });
 
-module.exports = {
-  storage,
-  uploadDir,
-  imageStore,
-  createFolderIfNotExist,
-  uploadMiddleware,
-};
+module.exports = upload;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST, PORT = 3777 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .set("strictQuery", false)
