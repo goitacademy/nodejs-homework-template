2fdea44a432d7e07fb55a3568f39c2ee3ffe721c
Mart <marta.nikiel@yahoo.com>
2024-02-23 16:44:23 +0100

    added models/contacts.js and api/contacts.js

---
+++ models/contacts.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "ua"] },
+    })
+    .required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool(),
+});
+
+const favoriteJoiSchema = Joi.object({
+  favorite: Joi.bool().required(),
+});
+
+const schemas = {
+  joiSchema,
+  favoriteJoiSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, schemas };
