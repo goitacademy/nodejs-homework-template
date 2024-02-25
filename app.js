66f9bfbeee68d23779420429e277d32ac02bfbf9
Mariusz-72 <mmbwdap@gmail.com>
2024-02-25 21:19:38 +0100

    update 2.3

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
+const signup = require("./controllers/signup");
+const login = require("./controllers/login");
+const current = require("./controllers/current");
+const logout = require("./controllers/logout");
+const { middleware } = require("./controllers/middleware");
 
 const app = express();
 
@@ -14,6 +19,12 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
+
+app.use(express.urlencoded());
+app.use("/api/users", signup);
+app.use("/api/users", login);
+app.use("/api/users", middleware, current);
+app.use("/api/users", middleware, logout);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
