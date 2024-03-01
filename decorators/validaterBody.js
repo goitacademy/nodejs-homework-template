d93450fbc7547eba9ee4867a925cc08da62d3379
DariaRodovnichenko <125218414+DariaRodovnichenko@users.noreply.github.com>
2024-03-01 23:02:36 +0100

    hw03

---
+++ decorators/validaterBody.js
@@ -0,0 +1,15 @@
+import { HttpError } from "../helpers/index.js";
+
+const validateBody = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return next(HttpError(400, error.message));
+    }
+    next();
+  };
+
+  return func;
+};
+
+export default validateBody;
