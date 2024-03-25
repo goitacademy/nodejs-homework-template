c5430685206813b81b098220a0a431ef3d9795a4
dmgrabski <dmgrabski97@gmail.com>
2024-03-25 10:37:36 +0100

    fixed branch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'hello world' })
+  res.json({ message: 'template message' })
 })
 
 router.post('/', async (req, res, next) => {
